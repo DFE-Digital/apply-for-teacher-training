df93dea5aa7d4db5f490dc1b62a7de09f6d46e40
Himal Mandalia <himalmandalia@gmail.com>
2019-01-24 22:01:33 +0000

    Use UUIDs and enable PosgreSQL extension

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,9 @@ module ApplyToBecomeATeacher
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
   end
 end
