0be3d635fedb7661ddc03dea0abb94d86b5ec9e2
Himal Mandalia <himalmandalia@gmail.com>
2019-01-25 16:15:11 +0000

    Protect non Home#index actions with Devise auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_candidate!
+
   def after_sign_in_path_for(resource_or_scope)
     if resource_or_scope == :admin_user || resource_or_scope.is_a?(Admin::User)
       admin_root_path
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  before_action :authenticate_candidate!, except: :index
+
   def index
   end
 end
