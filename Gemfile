63f02fb1b2d18b527cea35b0ef4ffe9fa5255e4f
Himal Mandalia <himalmandalia@gmail.com>
2019-02-07 11:23:54 +0000

    Add SimpleCov and config for RSpec and Cucumber

--- Gemfile
+++ Gemfile
@@ -61,6 +61,7 @@ group :test do
   gem 'database_cleaner'
   gem 'shoulda-matchers', '4.0.0.rc1'
   gem 'rails-controller-testing'
+  gem 'simplecov', require: false
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,9 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+
+require 'simplecov'
+SimpleCov.start 'rails'
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
