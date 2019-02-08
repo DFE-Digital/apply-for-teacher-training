a4de08f6c743d2e54b52a702109e7dbafc58e89c
Himal Mandalia <himalmandalia@gmail.com>
2019-02-08 15:14:38 +0000

    Apply GOV UK styles and content

---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,31 @@
+module DeviseHelper
+  def devise_error_messages!
+    return "" unless devise_error_messages?
+
+    messages = resource.errors.map do |field, message|
+      content_tag(:li, "#{field} #{message}".humanize)
+    end.join
+
+    sentence = I18n.t("errors.messages.not_saved",
+                      count: resource.errors.count,
+                      resource: resource.class.model_name.human.downcase)
+
+    html = <<-HTML
+    <div class="govuk-error-summary" aria-labelledby="error-summary-title" role="alert" tabindex="-1" data-module="error-summary" id="error_explanation">
+      <h2 class="govuk-error-summary__title" id="error-summary-title">#{sentence}</h2>
+      <div class="govuk-error-summary__body">
+        <ul class="govuk-list govuk-error-summary__list">
+          #{messages}
+        </ul>
+      </div>
+    </div>
+    HTML
+
+    html.html_safe
+  end
+
+  def devise_error_messages?
+    !resource.errors.empty?
+  end
+
+end
