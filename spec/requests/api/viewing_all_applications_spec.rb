acf7179b7202949b380b874fb57a1e26ef360718
Kyle <kjdchapman@gmail.com>
2019-03-05 14:20:03 +0000

    DRY up application endpoint spec

--- spec/requests/api/viewing_all_applications_spec.rb
+++ spec/requests/api/viewing_all_applications_spec.rb
@@ -1,7 +1,13 @@
-describe 'GET applications', type: :request do
+describe 'GET applications' do
+  let(:json_response) { JSON.parse(response.body) }
+
   before do
     headers = { "ACCEPT" => "application/json" }
     get '/api/applications', headers: headers
+  end
+
+  it 'responds with JSON' do
+    expect(response.content_type).to eq("application/json")
   end
 
   it 'responds with a success code' do
@@ -9,22 +15,24 @@ describe 'GET applications', type: :request do
   end
 
   it 'contains three applications' do
-    expect(JSON.parse(response.body).count).to eq(3)
+    expect(json_response.count).to eq(3)
   end
 
   context 'first application' do
+    let(:first_application) { json_response.first }
+
     it 'has an id' do
-      expect(JSON.parse(response.body).first['id'])
+      expect(first_application['id'])
         .to_not be_blank
     end
 
     it 'has a first name' do
-      expect(JSON.parse(response.body).first['first_name'])
+      expect(first_application['first_name'])
         .to_not be_blank
     end
 
     it 'has an email' do
-      expect(JSON.parse(response.body).first['email'])
+      expect(first_application['email'])
         .to_not be_blank
     end
   end
