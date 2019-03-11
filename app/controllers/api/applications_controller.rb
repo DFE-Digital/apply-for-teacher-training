e02a138ef84ef9bba8153efea76e2809358d71eb
Kyle <kjdchapman@gmail.com>
2019-03-11 14:35:06 +0000

    Back API with flat file (w/3 fake applications)

--- app/controllers/api/applications_controller.rb
+++ app/controllers/api/applications_controller.rb
@@ -1,13 +1,6 @@
 class Api::ApplicationsController < ActionController::API
   def index
-    render json: [
-      {
-        id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-        first_name: 'hello',
-        email: 'example@email.com'
-      },
-      {},
-      {}
-    ]
+    fake_applications = File.read('lib/data/fake_applications.json')
+    render json: fake_applications
   end
 end
