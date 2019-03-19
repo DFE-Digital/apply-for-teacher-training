3f4d2cf6086ed5f0a76a9cf414aeb7d7ba4ada40
Kyle <kjdchapman@gmail.com>
2019-03-19 10:28:03 +0000

    Add endpoint for viewing one application

--- app/controllers/api/applications_controller.rb
+++ app/controllers/api/applications_controller.rb
@@ -1,6 +1,22 @@
 class Api::ApplicationsController < ActionController::API
   def index
-    fake_applications = File.read('lib/data/fake_applications.json')
-    render json: fake_applications
+    render json: applications
+  end
+
+  def show
+    if matching_application.present?
+      render json: matching_application
+    else
+      head :not_found
+    end
+  end
+
+private
+  def matching_application
+    applications.find{|app| app['id'] == params[:id]}
+  end
+
+  def applications
+    JSON.parse(File.read('lib/data/fake_applications.json'))
   end
 end
