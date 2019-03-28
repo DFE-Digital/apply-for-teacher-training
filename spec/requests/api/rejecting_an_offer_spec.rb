8d71ca08c9c13191f667d5ea7374dbcbbc99c11b
Kyle <kjdchapman@gmail.com>
2019-03-28 10:15:56 +0000

    Add basic reject endpoint (#48)
    Similarly to #47, this commit does not include:
    - storing or logging that clients have made an offer
    - acting differently if the application has already been responded to
    - a good description of the result in the response
    - any concept of ownership for these resources

---
+++ spec/requests/api/rejecting_an_offer_spec.rb
@@ -0,0 +1,37 @@
+describe 'rejecting an offer' do
+  before do
+    headers = { "ACCEPT" => "application/json" }
+    patch "/api/applications/#{id}/reject", headers: headers
+  end
+
+  context 'with an id not matching any application' do
+    let(:id) { "nonsense-code" }
+
+    it 'responds with JSON' do
+      expect(response.content_type).to eq("application/json")
+    end
+
+    it 'responds with a not found code' do
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
+  context 'with an id matching an application' do
+    let(:id) { '3fa85f64-5717-4562-b3fc-2c963f66afa6' }
+
+    it 'responds with JSON' do
+      expect(response.content_type).to eq("application/json")
+    end
+
+    it 'responds ok' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'returns the relevant application' do
+      expect(JSON.parse(response.body)).to include(
+        'id' => id,
+        'first_name' => "Christopher"
+      )
+    end
+  end
+end
