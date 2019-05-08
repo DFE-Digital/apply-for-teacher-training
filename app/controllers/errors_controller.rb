0042a47a274821aa70f534db0baf4bb6ef27ec70
Kyle <kjdchapman@gmail.com>
2019-05-08 11:26:08 +0100

    Fix new version's linting violations

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,9 +1,9 @@
 class ErrorsController < ApplicationController
   skip_before_action :verify_authenticity_token,
-    only: %i[not_found internal_server_error forbidden unauthorised]
+                     only: %i[not_found internal_server_error forbidden unauthorised]
 
   skip_before_action :authenticate_candidate!,
-    only: %i[not_found internal_server_error forbidden unauthorised]
+                     only: %i[not_found internal_server_error forbidden unauthorised]
 
   def not_found
     render status: :not_found
