cecd389b5ae3d4ef853cb5fad5c792fbf285b10f
Kyle <kjdchapman@gmail.com>
2019-06-19 16:29:49 +0100

    Delete old prototype app
    This existing app has few tests, some functionality we don't need yet,
    and some unused features.
    
    Rather than unpicking this, we feel it's best to start this app afresh.

--- app/controllers/admin/application_controller.rb
+++
@@ -1,5 +0,0 @@
-class Admin::ApplicationController < ActionController::Base
-  layout 'admin'
-
-  before_action :authenticate_admin_user!
-end
--- db/seeds.rb
+++
@@ -1,7 +0,0 @@
-Admin::User.create!(
-  [
-    {
-      email: 'example@example.com', password: 'testing123', password_confirmation: 'testing123'
-    },
-  ]
-)
