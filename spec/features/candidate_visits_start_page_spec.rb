9f366b590b40d86e4f417793370f983653f7d5b7
Duncan Brown <duncanjbrown@gmail.com>
2019-06-24 16:53:29 +0100

    Add Start Page content from the prototype

--- spec/features/candidate_visits_start_page_spec.rb
+++ spec/features/candidate_visits_start_page_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 describe 'Candidate visits start page' do
-  it 'works' do
+  it 'displays a summary of the service' do
     visit '/'
     expect(page).to have_content(
-      'Apply for postgraduate teacher training'
+      'Apply for postgraduate teacher training is a new GOV.UK service'
     )
   end
 end
