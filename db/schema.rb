2c22652f239fe1e2d2393420f9d97bec7316643d
Duncan Brown <duncanjbrown@gmail.com>
2019-07-02 15:32:23 +0100

    Enable pgcrypto postgres extension
    This enables Rails to use UUIDs as primary keys.
    
    https://edgeguides.rubyonrails.org/active_record_postgresql.html#uuid-primary-keys

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_07_02_143141) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
 end
