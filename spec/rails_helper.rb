e5f43d671dba98490c3994d5fe3e382311ba4399
Duncan Brown <duncanjbrown@gmail.com>
2019-07-03 15:41:21 +0100

    Add i18n in test environment
    Allow t() in specs so we can refer to i18n keys rather than brittle
    strings:
    https://thoughtbot.com/blog/better-tests-through-internationalization.
    
    Raise when a translation is not found so we can avoid checking for the
    presence of `nil`.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -57,4 +57,6 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.include AbstractController::Translation
 end
