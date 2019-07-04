6baf0ef583b82a2a82acaad1fc9bd5c4c0697f08
Duncan Brown <duncanjbrown@gmail.com>
2019-07-04 15:38:44 +0100

    Use i18n to identify field labels in specs
    We use it everywhere else...

--- spec/system/candidate_fills_in_personal_details_spec.rb
+++ spec/system/candidate_fills_in_personal_details_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'A candidate filling in their personal details' do
 
     expect(page).to have_content t('application_form.personal_details_section.heading')
 
-    fill_in 'Title', with: 'Dr'
+    fill_in t('application_form.personal_details_section.title.label'), with: 'Dr'
     fill_in t('application_form.personal_details_section.first_name.label'), with: 'John'
     fill_in t('application_form.personal_details_section.preferred_name.label'), with: 'Dr Doe'
     fill_in t('application_form.personal_details_section.last_name.label'), with: 'Doe'
@@ -17,26 +17,26 @@ RSpec.describe 'A candidate filling in their personal details' do
     expect(page).to have_content t('application_form.personal_details_section.heading')
     expect(page).to have_content t('application_form.review_answers')
 
-    expect(page).to have_content('Title')
+    expect(page).to have_content(t('application_form.personal_details_section.title.label'))
     expect(page).to have_content('Dr')
-    expect(page).to have_content('First name')
+    expect(page).to have_content(t('application_form.personal_details_section.first_name.label'))
     expect(page).to have_content('John')
-    expect(page).to have_content('Last name')
+    expect(page).to have_content(t('application_form.personal_details_section.last_name.label'))
     expect(page).to have_content('Doe')
-    expect(page).to have_content('Name you prefer to be addressed by')
+    expect(page).to have_content(t('application_form.personal_details_section.preferred_name.label'))
     expect(page).to have_content('Doe')
 
     click_on t('application_form.submit')
 
     expect(page).to have_content t('application_form.application_complete')
 
-    expect(page).to have_content('Title')
+    expect(page).to have_content(t('application_form.personal_details_section.title.label'))
     expect(page).to have_content('Dr')
-    expect(page).to have_content('First name')
+    expect(page).to have_content(t('application_form.personal_details_section.first_name.label'))
     expect(page).to have_content('John')
-    expect(page).to have_content('Last name')
+    expect(page).to have_content(t('application_form.personal_details_section.last_name.label'))
     expect(page).to have_content('Doe')
-    expect(page).to have_content('Name you prefer to be addressed by')
+    expect(page).to have_content(t('application_form.personal_details_section.preferred_name.label'))
     expect(page).to have_content('Dr Doe')
   end
 end
