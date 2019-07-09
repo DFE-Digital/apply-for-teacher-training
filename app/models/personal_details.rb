07264db242a7dd0d5f8ad658ae06cca9131f3830
Kyle <kjdchapman@gmail.com>
2019-07-09 14:41:43 +0100

    Mark all personal detail fields as mandatory

--- app/models/personal_details.rb
+++ app/models/personal_details.rb
@@ -1,3 +1,7 @@
 class PersonalDetails < ApplicationRecord
-  validates :title, presence: :true
+  validates :title, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :date_of_birth, presence: true
+  validates :nationality, presence: true
 end
--- spec/models/personal_details_spec.rb
+++ spec/models/personal_details_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 describe PersonalDetails, type: :model do
   it { is_expected.to validate_presence_of :title }
+  it { is_expected.to validate_presence_of :first_name }
+  it { is_expected.to validate_presence_of :last_name }
+  it { is_expected.to validate_presence_of :date_of_birth }
+  it { is_expected.to validate_presence_of :nationality }
 end
