2be41c64411e4335c29a1c2d0779f6788fead935
Duncan Brown <duncanjbrown@gmail.com>
2019-07-09 14:19:11 +0100

    Refactor Personal details form-filling in spec
    We would like to reuse these details so we can test varying them on the
    Check your answers page, so pull out a function and define a stock hash
    of VALID_PERSONAL_DETAILS.

--- spec/system/candidate_fills_in_personal_details_spec.rb
+++ spec/system/candidate_fills_in_personal_details_spec.rb
@@ -1,24 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe 'A candidate filling in their personal details' do
+  VALID_PERSONAL_DETAILS = {
+    first_name: 'John',
+    last_name: 'Doe',
+    title: 'Dr',
+    preferred_name: 'Dr Doe',
+    nationality: 'British',
+    date_of_birth: Date.new(1997, 3, 13)
+  }.freeze
+
   it 'can enter details into the form, and see them on the finished application' do
     visit '/'
     click_on t('application_form.begin_button')
 
     expect(page).to have_content t('application_form.personal_details_section.heading')
 
-    fill_in t('application_form.personal_details_section.title.label'), with: 'Dr'
-    fill_in t('application_form.personal_details_section.first_name.label'), with: 'John'
-    fill_in t('application_form.personal_details_section.preferred_name.label'), with: 'Dr Doe'
-    fill_in t('application_form.personal_details_section.last_name.label'), with: 'Doe'
-
-    within '.govuk-date-input' do
-      fill_in 'Day', with: 13
-      fill_in 'Month', with: 3
-      fill_in 'Year', with: 1997
-    end
-
-    fill_in t('application_form.personal_details_section.nationality.label'), with: 'British'
+    fill_in_personal_details(VALID_PERSONAL_DETAILS)
 
     click_on t('application_form.save_and_continue')
 
@@ -28,29 +26,44 @@ RSpec.describe 'A candidate filling in their personal details' do
     within '.govuk-summary-list' do
       expect_description_list_item(
         t('application_form.personal_details_section.title.label'),
-        'Dr'
+        VALID_PERSONAL_DETAILS[:title]
       )
       expect_description_list_item(
         t('application_form.personal_details_section.first_name.label'),
-        'John'
+        VALID_PERSONAL_DETAILS[:first_name]
       )
       expect_description_list_item(
         t('application_form.personal_details_section.last_name.label'),
-        'Doe'
+        VALID_PERSONAL_DETAILS[:last_name]
       )
       expect_description_list_item(
         t('application_form.personal_details_section.preferred_name.label'),
-        'Dr Doe'
+        VALID_PERSONAL_DETAILS[:preferred_name]
       )
       expect_description_list_item(
         t('application_form.personal_details_section.nationality.label'),
-        'British'
+        VALID_PERSONAL_DETAILS[:nationality]
       )
     end
 
     click_on t('application_form.submit')
 
     expect(page).to have_content t('application_form.application_submitted')
+  end
+
+  def fill_in_personal_details(details)
+    fill_in t('application_form.personal_details_section.title.label'), with: details[:title]
+    fill_in t('application_form.personal_details_section.first_name.label'), with: details[:first_name]
+    fill_in t('application_form.personal_details_section.preferred_name.label'), with: details[:preferred_name]
+    fill_in t('application_form.personal_details_section.last_name.label'), with: details[:last_name]
+
+    within '.govuk-date-input' do
+      fill_in 'Day', with: details[:date_of_birth].day
+      fill_in 'Month', with: details[:date_of_birth].month
+      fill_in 'Year', with: details[:date_of_birth].year
+    end
+
+    fill_in t('application_form.personal_details_section.nationality.label'), with: details[:nationality]
   end
 
   # search for a <dt> with an expected name adjacent to a <dd> with an expected value
