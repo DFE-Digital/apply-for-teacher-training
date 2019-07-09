b165ec7e42531ffd4166b18b71c39e2483fe0bf1
Kyle <kjdchapman@gmail.com>
2019-07-09 14:52:12 +0100

    Link header error messages to relevant field
    Notably, this implementation does not work for multiple validation
    errors, as it only displays the first.
    
    It also requires the mirroring setup of ids on the form groups to be
    id'd manually, which seems like work.
    
    Lastly, the gsub implementation doesn't feel like idiomatic Rails to
    me - I suspect we could do this using a helper.

--- spec/system/candidate_leaves_personal_details_blank_spec.rb
+++ spec/system/candidate_leaves_personal_details_blank_spec.rb
@@ -11,4 +11,9 @@ describe 'A candidate leaves personal details blank' do
     expect(page).to have_content('There is a problem')
     expect(page).to have_content('Title can\'t be blank')
   end
+
+  it 'has an anchor links in the error message' do
+    expect(page).to have_link(nil, href: '#title')
+    expect(page).to have_link(nil, href: '#first-name')
+  end
 end
