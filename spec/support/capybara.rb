521634e762421906f203039ba85402137d9c1854
Duncan Brown <duncanjbrown@gmail.com>
2019-07-11 12:00:27 +0100

    Improve error summary focusing spec
    We can't define a default_driver for Capybara as Rails system specs
    override it, so put the definition of our js driver and default driver
    into before hooks that call ApplicationSystemTestCase#driven_by.

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,1 +1,11 @@
 require 'capybara/rails'
+
+RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    driven_by(:rack_test)
+  end
+
+  config.before(:each, type: :system, js: true) do
+    driven_by(:selenium_chrome_headless)
+  end
+end
--- spec/system/candidate_leaves_personal_details_blank_spec.rb
+++ spec/system/candidate_leaves_personal_details_blank_spec.rb
@@ -1,24 +1,15 @@
 require 'rails_helper'
 
 describe 'A candidate leaves personal details blank' do
-  before do
+  it 'shows a summary with clickable links', js: true do
     visit '/'
     click_on t('application_form.begin_button')
     click_on t('application_form.save_and_continue')
-  end
 
-  it 'indicates there is a problem' do
     expect(page).to have_content('There is a problem')
-    expect(page).to have_content('Enter your title')
-  end
 
-  it 'has an anchor links in the error message' do
-    expect(page).to have_link(nil, href: '#personal_details_title')
-    expect(page).to have_link(nil, href: '#personal_details_first_name')
-  end
+    click_on 'Enter your first name'
 
-  it 'highlights fields that need attention' do
-    expect(page).to have_css('.govuk-form-group--error')
-    expect(page).to have_css('.govuk-input--error')
+    expect(page).to have_selector('#personal_details_first_name:focus')
   end
 end
