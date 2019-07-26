ce5bfd61419b11c97ddce5e0c94b772df7de1a79
Kyle <kjdchapman@gmail.com>
2019-07-26 11:31:38 +0100

    Better name for field anchor link method

--- app/helpers/error_summary_helper.rb
+++ app/helpers/error_summary_helper.rb
@@ -1,5 +1,5 @@
 module ErrorSummaryHelper
-  def anchor_link(record, field)
+  def field_anchor_link(record, field)
     "##{record.model_name.param_key}_#{field}"
   end
 end
--- spec/helpers/error_summary_helper_spec.rb
+++ spec/helpers/error_summary_helper_spec.rb
@@ -7,7 +7,7 @@ describe ErrorSummaryHelper do
       let(:active_model_mock) { double(model_name: model_name_mock) }
 
       it 'returns active_model_field_name' do
-        result = helper.anchor_link(active_model_mock, :field_name)
+        result = helper.field_anchor_link(active_model_mock, :field_name)
         expect(result).to eq('#active_model_field_name')
       end
     end
