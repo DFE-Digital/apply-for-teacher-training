a0f31f1237ea2bad60630070560d349a09a04505
Kyle <kjdchapman@gmail.com>
2019-07-26 11:59:43 +0100

    Use real ActiveModel instead of mocking
    We have no control over the ActiveModel interface, so the pain
    exposed by the test couldn't lead to any useful action. Better to
    have a spec that is more tightly coupled and less painful in this
    case.

--- spec/helpers/error_summary_helper_spec.rb
+++ spec/helpers/error_summary_helper_spec.rb
@@ -2,13 +2,12 @@ require 'rails_helper'
 
 describe ErrorSummaryHelper do
   describe '#field_anchor_link' do
-    context 'given a ActiveModel record and a field_name' do
-      let(:model_name_mock) { double(param_key: 'active_model') }
-      let(:active_model_mock) { double(model_name: model_name_mock) }
+    context 'given an ActiveModel and a field_name' do
+      class ExampleModel; include ActiveModel::Model; end
 
-      it 'returns active_model_field_name' do
-        result = helper.field_anchor_link(active_model_mock, :field_name)
-        expect(result).to eq('#active_model_field_name')
+      it 'returns example_model_field_name' do
+        result = helper.field_anchor_link(ExampleModel.new, :field_name)
+        expect(result).to eq('#example_model_field_name')
       end
     end
   end
