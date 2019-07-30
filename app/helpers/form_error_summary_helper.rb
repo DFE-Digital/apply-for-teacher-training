6beed266e7a2c8845fa5074b0c313fac8e26bd63
Kyle <kjdchapman@gmail.com>
2019-07-30 11:53:15 +0100

    Kwargify error summary helper method

--- app/helpers/form_error_summary_helper.rb
+++ app/helpers/form_error_summary_helper.rb
@@ -1,5 +1,5 @@
 module FormErrorSummaryHelper
-  def field_anchor_link(model_name, field)
+  def field_anchor_link(model_name:, field:)
     "##{model_name.param_key}_#{field}"
   end
 end
--- spec/helpers/form_error_summary_helper_spec.rb
+++ spec/helpers/form_error_summary_helper_spec.rb
@@ -6,7 +6,10 @@ describe FormErrorSummaryHelper do
       let(:naming_mock) { double(param_key: 'example_model') }
 
       it 'returns the correct anchor link for that model & field' do
-        result = helper.field_anchor_link(naming_mock, :field_name)
+        result = helper.field_anchor_link(
+          model_name: naming_mock,
+          field: :field_name
+        )
         expect(result).to eq('#example_model_field_name')
       end
     end
