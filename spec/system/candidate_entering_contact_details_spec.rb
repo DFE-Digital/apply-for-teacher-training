1506337b812bf5c0606f6023de1b73676452cd0a
Duncan Brown <duncanjbrown@gmail.com>
2019-07-30 15:05:19 +0100

    Add contact_details and degrees to end-to-end spec
    The candidate_completing_an_application_spec spec now runs through the
    entire application form.
    
    To make that happen this commit:
    
    - extracts the form-filling logic for contact details and degrees into
    helper modules, in line with what we did for personal details
    - includes those in the end to end spec
    - fixes a bug revealed by that end to end spec, which is that completing
    contact details takes you to the summary page, not to the "Add a degree"
    page
    - corrects copy-pasta in the Degree spec (we visited
    contact_details/new, not degrees/new)

--- spec/system/candidate_entering_contact_details_spec.rb
+++ spec/system/candidate_entering_contact_details_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'A candidate entering contact details' do
-  include TestHelpers::PersonalDetails
+  include TestHelpers::ContactDetails
 
   context 'when details are correct' do
     before do
@@ -11,12 +11,10 @@ describe 'A candidate entering contact details' do
       click_on t('application_form.save_and_continue')
     end
 
-    describe 'check_your_answers page' do
-      it 'contains phone number' do
-        expect(page).to have_content('Phone number')
-        expect(page).to have_content('Email address')
-        expect(page).to have_content('Postal address')
-      end
+    it 'sees a summary of those details' do
+      visit '/check_your_answers'
+
+      expect(page).to have_content('Phone number 1234567890')
     end
 
     context 'and wishes to amend their details' do
@@ -40,19 +38,5 @@ describe 'A candidate entering contact details' do
       click_on 'Enter your phone number'
       expect(page).to have_selector('#contact_details_phone_number:focus')
     end
-  end
-
-private
-
-  def fill_in_contact_details
-    details = {
-      phone_number: '1234567890',
-      email_address: 'john.doe@example.com',
-      address: 'Westminster, London SW1P 1QW'
-    }
-
-    fill_in t('application_form.contact_details_section.phone_number.label'), with: details[:phone_number]
-    fill_in t('application_form.contact_details_section.email_address.label'), with: details[:email_address]
-    fill_in t('application_form.contact_details_section.address.label'), with: details[:address]
   end
 end
