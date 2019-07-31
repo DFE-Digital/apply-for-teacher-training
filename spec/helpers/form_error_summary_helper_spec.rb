818b07cbbf4f3832631ac6420e1608fada813612
Duncan Brown <duncanjbrown@gmail.com>
2019-07-31 17:16:37 +0100

    Use a verifying double to satisfy Rubocop-rspec
    Rubocop-rspec, following the RSpec docs
    (https://relishapp.com/rspec/rspec-mocks/docs/verifying-doubles),
    prefers verifying doubles to plain doubles, and I think I like the
    additional explicitness this brings to the spec.

--- spec/helpers/form_error_summary_helper_spec.rb
+++ spec/helpers/form_error_summary_helper_spec.rb
@@ -2,8 +2,11 @@ require 'rails_helper'
 
 describe FormErrorSummaryHelper do
   describe '#field_anchor_link' do
-    context 'given a field_name' do
-      let(:naming_mock) { double(param_key: 'example_model') }
+    context 'given a model_name and field' do
+      let(:naming_mock) do
+        instance_double(ActiveModel::Name, param_key: 'example_model')
+      end
+
 
       it 'returns the correct anchor link for that model & field' do
         result = helper.field_anchor_link(
