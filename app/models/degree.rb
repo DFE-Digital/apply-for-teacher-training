6a53188d5ebe3b3e82a9fd53ce486c2e23a8a326
alexdesi <alexdesi@gmail.com>
2019-08-01 17:13:00 +0100

    Add various validations for all the fields.

--- app/models/degree.rb
+++ app/models/degree.rb
@@ -1,7 +1,20 @@
 class Degree < ApplicationRecord
+  MAX_LENGTHS = {
+    class_of_degree: 20,
+    institution: 100,
+    subject: 100,
+    type_of_degree: 20
+  }.freeze
+
   validates :type_of_degree, presence: true
   validates :subject, presence: true
   validates :institution, presence: true
   validates :class_of_degree, presence: true
   validates :year, presence: true
+
+  validates :year, numericality: { greater_than: 1899, less_than_or_equal_to: DateTime.now.year }
+
+  MAX_LENGTHS.each do |field, max|
+    validates field, length: { maximum: max }
+  end
 end
--- spec/models/degree_spec.rb
+++ spec/models/degree_spec.rb
@@ -1,9 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Degree, type: :model do
+  let(:current_year) { DateTime.now.year }
+
   it { is_expected.to validate_presence_of(:type_of_degree) }
   it { is_expected.to validate_presence_of(:subject) }
   it { is_expected.to validate_presence_of(:institution) }
   it { is_expected.to validate_presence_of(:class_of_degree) }
   it { is_expected.to validate_presence_of(:year) }
+
+  it { is_expected.to validate_length_of(:type_of_degree).is_at_most(20) }
+  it { is_expected.to validate_length_of(:subject).is_at_most(100) }
+  it { is_expected.to validate_length_of(:institution).is_at_most(100) }
+  it { is_expected.to validate_length_of(:class_of_degree).is_at_most(20) }
+
+  it { is_expected.to validate_numericality_of(:year).is_greater_than(1899).is_less_than_or_equal_to(current_year) }
 end
