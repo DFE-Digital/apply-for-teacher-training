f5a97e43f5ee45389fba6ea8e696ca3132ccbbd3
Wen Ting Wang <chubberlisk@outlook.com>
2019-08-06 16:05:18 +0100

    Add title to pages using page_title helper

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe ApplicationHelper do
+  describe '#page_title' do
+    context 'given a page is not defined in the translation file' do
+      it 'returns the application title' do
+        page_title = helper.page_title(:meow)
+
+        expect(page_title).to eq(t('page_titles.application'))
+      end
+    end
+
+    context 'given a page is defined in the translation file' do
+      it 'returns the page name with the application title' do
+        page_title = helper.page_title(:personal_details)
+
+        expect(page_title).to eq("#{t('page_titles.personal_details')} - #{t('page_titles.application')}")
+      end
+    end
+  end
+end
