cad73f56c67a84293a0e7833577452f60306112e
Wen Ting Wang <chubberlisk@outlook.com>
2019-08-06 16:05:18 +0100

    Add title to pages using page_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def page_title(page)
+    page_title_translation_key = "page_titles.#{page}"
+
+    if I18n.exists?(page_title_translation_key)
+      "#{t(page_title_translation_key)} - #{t('page_titles.application')}"
+    else
+      t('page_titles.application')
+    end
+  end
 end
