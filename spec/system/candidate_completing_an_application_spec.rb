5c73b09728d57810d1a31b04e11f9d48d8935675
Duncan Brown <duncanjbrown@gmail.com>
2019-08-15 14:43:19 +0100

    POST to GOV.UK Notify on application submission
    There is no content in the posted message yet but this is the basic
    scaffolding for sending it.

--- spec/system/candidate_completing_an_application_spec.rb
+++ spec/system/candidate_completing_an_application_spec.rb
@@ -7,6 +7,11 @@ describe 'A candidate completing an application for teacher training' do
   include TestHelpers::NonDegreeQualifications
 
   context 'when all the forms are correctly filled in' do
+    let!(:notify_request) do
+      stub_request(:post, /api.notifications.service.gov.uk/)
+        .to_return(status: 200, body: '{}', headers: {})
+    end
+
     before do
       visit '/'
       click_on t('application_form.begin_button')
@@ -24,10 +29,16 @@ describe 'A candidate completing an application for teacher training' do
       click_on t('application_form.save_and_continue')
     end
 
-    it 'successfully submits the application' do
+    it 'can see the success page' do
       click_on t('application_form.submit')
 
       expect(page).to have_content t('application_form.application_submitted')
+    end
+
+    it 'has submitted their application' do
+      click_on t('application_form.submit')
+
+      expect(notify_request).to have_been_made
     end
   end
 end
