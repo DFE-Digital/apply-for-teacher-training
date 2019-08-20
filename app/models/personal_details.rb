37595911dde8457c5a6a0927648bb7f5a6ad6c06
Wen Ting Wang <chubberlisk@outlook.com>
2019-08-20 15:25:01 +0100

    Remove date of birth from personal details

--- app/models/personal_details.rb
+++ app/models/personal_details.rb
@@ -1,5 +1,4 @@
 class PersonalDetails < ApplicationRecord
-  DATE_OF_BIRTH_LIMIT = 1900
   MAX_LENGTHS = {
     first_name: 50,
     last_name: 50,
@@ -7,17 +6,7 @@ class PersonalDetails < ApplicationRecord
     title: 4,
   }.freeze
 
-  validates :first_name, :last_name, :title, :date_of_birth, presence: true
+  validates :first_name, :last_name, :title, presence: true
 
   MAX_LENGTHS.each { |field, max| validates field, length: { maximum: max } }
-
-  validate :date_of_birth, :date_is_not_too_old
-
-private
-
-  def date_is_not_too_old
-    if date_of_birth.present? && date_of_birth.year < DATE_OF_BIRTH_LIMIT
-      errors.add(:date_of_birth, :too_old, year: DATE_OF_BIRTH_LIMIT)
-    end
-  end
 end
--- spec/support/test_helpers/personal_details.rb
+++ spec/support/test_helpers/personal_details.rb
@@ -5,20 +5,13 @@ module TestHelpers
         first_name: 'John',
         last_name: 'Doe',
         title: 'Dr',
-        preferred_name: 'Dr Doe',
-        date_of_birth: Date.new(1997, 3, 13)
+        preferred_name: 'Dr Doe'
       }
 
       fill_in t('application_form.personal_details_section.title.label'), with: details[:title]
       fill_in t('application_form.personal_details_section.first_name.label'), with: details[:first_name]
       fill_in t('application_form.personal_details_section.preferred_name.label'), with: details[:preferred_name]
       fill_in t('application_form.personal_details_section.last_name.label'), with: details[:last_name]
-
-      within '.govuk-date-input' do
-        fill_in 'Day', with: details[:date_of_birth].day
-        fill_in 'Month', with: details[:date_of_birth].month
-        fill_in 'Year', with: details[:date_of_birth].year
-      end
     end
   end
 end
