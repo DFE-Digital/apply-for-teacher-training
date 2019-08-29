57cffe7d3a4f8d11a73745e39cc057c1a38d9952
Jake Benilov <benilov@gmail.com>
2019-08-29 23:31:14 +0100

    Add application_states feature, stub out steps

---
+++ features/step_definitions/application_states.rb
@@ -0,0 +1,11 @@
+Given("an application in {string} state") do |orginal_application_state|
+  pending
+end
+
+When(/a (\w+) (.*)/) do |actor, action|
+  pending
+end
+
+Then("the new application state is {string}") do |new_application_state|
+  pending
+end
