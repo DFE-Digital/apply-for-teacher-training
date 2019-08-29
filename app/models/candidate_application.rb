54e63e88fd61a9ba8630cad52cb37953ddcf5285
Jake Benilov <benilov@gmail.com>
2019-08-29 23:51:28 +0100

    Implement reference submission state transition

--- app/models/candidate_application.rb
+++ app/models/candidate_application.rb
@@ -1,2 +1,16 @@
 class CandidateApplication < ApplicationRecord
+  include AASM
+
+  aasm column: 'state' do
+    state :references_pending, initial: true
+    state :application_complete
+
+    event :submit_reference do
+      transitions from: :references_pending, to: :application_complete, if: :done_by_referee?
+    end
+  end
+
+  def done_by_referee?(actor)
+    actor == 'referee'
+  end
 end
