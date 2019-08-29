17059232b5e077e03518f06730c0b09ba2b5ca57
Jake Benilov <benilov@gmail.com>
2019-08-29 23:51:28 +0100

    Implement reference submission state transition

--- features/step_definitions/application_states.rb
+++ features/step_definitions/application_states.rb
@@ -1,11 +1,12 @@
 Given("an application in {string} state") do |orginal_application_state|
-  pending
+  @application = CandidateApplication.new(state: orginal_application_state.gsub(" ", "_"))
 end
 
 When(/a (\w+) (.*)/) do |actor, action|
-  pending
+  event_name = action.gsub(" ", "_").to_sym
+  @application.send(event_name, actor)
 end
 
 Then("the new application state is {string}") do |new_application_state|
-  pending
+  expect(@application.state).to eq(new_application_state.gsub(" ", "_"))
 end
