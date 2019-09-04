20f97da2f9ec82805f5163570796ced82555645e
Jake Benilov <benilov@gmail.com>
2019-09-04 23:37:54 +0100

    Add training location code, populate it using feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_04_222037) do
+ActiveRecord::Schema.define(version: 2019_09_04_223237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -104,6 +104,7 @@ ActiveRecord::Schema.define(version: 2019_09_04_222037) do
     t.integer "provider_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "code"
   end
 
 end
--- features/step_definitions/courses.rb
+++ features/step_definitions/courses.rb
@@ -1,19 +1,36 @@
 Given("the following providers:") do |table|
   table.hashes.each do |row|
-    Provider.create!(
+    provider = Provider.create!(
       code: row['provider code'],
       accredited_body: row['is an accredited body?'] == 'Y'
     )
+
+    if row['provider training locations']
+      training_location_codes = row['provider training locations'].split(", ")
+      training_location_codes.each do |code|
+        TrainingLocation.create!(
+          code: code,
+          provider: provider
+        )
+      end
+    end
   end
 end
 
 Given("the following courses:") do |table|
   table.hashes.each do |row|
-    Course.create!(
+    course = Course.create!(
       course_code: row['course code'],
       provider: Provider.find_by!(code: row['provider code']),
       accredited_body: Provider.find_by!(code: row['accredited body'] || row['provider code']) # self-accredited if not specified
     )
+
+    if row['course training locations']
+      course_training_location_codes = row['course training locations'].split(", ")
+      course_training_location_codes.each do |training_location_code|
+        course.training_locations << course.provider.training_locations.find_by!(code: training_location_code)
+      end
+    end
   end
 end
 
