236ff5e0e023a2a8e7d3086974d039da5cf370bd
Jake Benilov <benilov@gmail.com>
2019-09-04 23:30:21 +0100

    Add join table between courses and training locations

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,7 @@
 class Course < ApplicationRecord
   belongs_to :provider
   belongs_to :accredited_body, class_name: "Provider", foreign_key: :accredited_body_provider_id
+
+  has_many :courses_training_locations
+  has_many :training_locations, through: :courses_training_locations
 end
