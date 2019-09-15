ad2ee34a9683d241f8a862928c5450edd14ad5b0
Jake Benilov <benilov@gmail.com>
2019-09-15 23:42:54 +0100

    Finish moving CandidateApplication authorisation to Pundit policy

--- app/policies/candidate_application_policy.rb
+++ app/policies/candidate_application_policy.rb
@@ -13,7 +13,7 @@ class CandidateApplicationPolicy
   end
 
   def done_by_provider?
-    @user == 'provider'
+    @user.start_with?('provider')
   end
 
   alias_method :submit?, :done_by_candidate?
@@ -26,6 +26,12 @@ class CandidateApplicationPolicy
   alias_method :confirm_conditions_met?, :done_by_provider?
   alias_method :confirm_onboarding?, :done_by_provider?
   alias_method :reject?, :done_by_provider?
-  alias_method :add_conditions?, :done_by_provider?
-  alias_method :amend_conditions?, :done_by_provider?
+
+  def update_conditions?
+    provider_code = @user.scan(/provider \((.*)\)/).flatten.last
+    done_by_provider? && @candidate_application.course.provider_or_accredited_body?(provider_code)
+  end
+
+  alias_method :add_conditions?, :update_conditions?
+  alias_method :amend_conditions?, :update_conditions?
 end
