1964260ddad17997ba1433416d5016b1616f42ec
Jake Benilov <benilov@gmail.com>
2019-09-18 22:49:36 +0100

    Generalise 'reject by default' timeframe to general timeframe
    This allows the persistence of 'decline by default' in the same
    data structure.

--- app/models/reject_by_default_timeframe.rb
+++ app/models/reject_by_default_timeframe.rb
@@ -1,5 +1,2 @@
-class RejectByDefaultTimeframe < ApplicationRecord
-  def self.applicable_at(time)
-    where('? >= from_time and ? < to_time', time, time).first
-  end
+class RejectByDefaultTimeframe < Timeframe
 end
