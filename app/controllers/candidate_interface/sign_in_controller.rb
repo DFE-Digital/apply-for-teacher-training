d4919d190857b322a79561364e534227864eace0
alexdesi <alexdesi@gmail.com>
2019-09-27 11:27:16 +0100

    Add the email used for Sign in flow.

--- app/controllers/candidate_interface/sign_in_controller.rb
+++ app/controllers/candidate_interface/sign_in_controller.rb
@@ -5,7 +5,22 @@ module CandidateInterface
     end
 
     def create
+      @candidate = Candidate.find_by(email_address: candidate_params[:email_address])
+
+      if @candidate.present?
+        # create the magic link and send the email
+        # MagicLinkSignUp.call(candidate: @candidate)
+
+        AuthenticationMailer.sign_in_email(to: @candidate.email_address, token: '1234567890').deliver!
+      end
+
       render :show
+    end
+
+  private
+
+    def candidate_params
+      params.require(:candidate).permit(:email_address)
     end
   end
 end
--- spec/system/candidate_interface/candidate_signing_in_spec.rb
+++ spec/system/candidate_interface/candidate_signing_in_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 describe 'A candidate signing in' do
+  include TestHelpers::SignUp
+
   before do
     visit '/'
     click_on 'sign in'
@@ -8,16 +10,39 @@ describe 'A candidate signing in' do
     fill_in t('authentication.sign_in.email_address.label'), with: 'new_candidate@example.com'
   end
 
-  it 'sees the the sign in page' do
+  it 'sees the sign in page' do
     expect(page).to have_content 'Enter the email address you used to register with'
+  end
+
+  context 'when the email does not exist' do
+    it 'sees the "Check your email" page' do
+      fill_in t('authentication.sign_in.email_address.label'), with: 'non_existent_candidate@example.com'
+      click_on t('authentication.sign_up.button')
+
+      # It redirect to the home
+      expect(page).to have_content 'Check your email'
+    end
   end
 
   context 'who successfully signs in' do
     it 'sees the "Check your email" page' do
-      fill_in t('authentication.sign_in.email_address.label'), with: 'new_candidate@example.com'
+      fill_in t('authentication.sign_in.email_address.label'), with: 'candidate@example.com'
       click_on t('authentication.sign_up.button')
 
       expect(page).to have_content 'Check your email'
+    end
+
+    it 'receives the email' do
+      visit '/candidate/sign-up'
+      fill_in_sign_up
+
+      visit '/candidate/sign-in'
+      fill_in t('authentication.sign_in.email_address.label'), with: 'april@pawnee.com'
+      click_on 'Continue'
+
+      open_email('april@pawnee.com')
+
+      expect(current_email.body).to have_content 'Click the link below to sign in to the Apply'
     end
   end
 end
