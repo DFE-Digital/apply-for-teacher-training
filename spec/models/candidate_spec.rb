56da478593370ca688fde3ac7f62b8de1e3da2ea
alexdesi <alexdesi@gmail.com>
2019-10-01 10:39:27 +0100

    Add email validation for sign-up

--- spec/models/candidate_spec.rb
+++ spec/models/candidate_spec.rb
@@ -3,9 +3,14 @@ require 'rails_helper'
 RSpec.describe Candidate, type: :model do
   subject { FactoryBot.create(:candidate) }
 
-  it { is_expected.to validate_presence_of :email_address }
-  it { is_expected.to validate_length_of(:email_address).is_at_most(250) }
-  it { is_expected.to validate_uniqueness_of :email_address }
+  describe 'a valid candidate' do
+    it { is_expected.to validate_presence_of :email_address }
+    it { is_expected.to validate_length_of(:email_address).is_at_most(250) }
+    it { is_expected.to validate_uniqueness_of :email_address }
+
+    it { is_expected.to allow_value('candidate@example.com').for(:email_address) }
+    it { is_expected.not_to allow_value('candidate.com').for(:email_address) }
+  end
 
   describe '#delete' do
     it 'deletes all dependent records through cascading deletes in the database' do
