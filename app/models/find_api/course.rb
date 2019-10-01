5b2a0bee844d6be0c1ccd220dba25d09157eb250
Theodor Vararu <theo@vararu.org>
2019-10-01 14:32:38 +0100

    6 - Pull in RECRUITMENT_CYCLE_YEAR and FIND_BASE_URL from env vars
    These should be configurable.

--- app/models/find_api/course.rb
+++ app/models/find_api/course.rb
@@ -1,6 +1,7 @@
 module FindAPI
   class Course < JsonApiClient::Resource
-    self.site = 'https://bat-qa-mcbe-as.azurewebsites.net/api/v3/'
+    RECRUITMENT_CYCLE_YEAR = ENV.fetch('RECRUITMENT_CYCLE_YEAR') { 2020 }
+    self.site = ENV.fetch('FIND_BASE_URL')
 
     belongs_to :recruitment_cycle, through: :provider, param: :recruitment_cycle_year
     belongs_to :provider, param: :provider_code
@@ -8,7 +9,7 @@ module FindAPI
     property :name, type: :string
 
     def self.fetch(provider_code, course_code)
-      where(recruitment_cycle_year: '2020')
+      where(recruitment_cycle_year: RECRUITMENT_CYCLE_YEAR)
         .where(provider_code: provider_code)
         .find(course_code)
         .first
