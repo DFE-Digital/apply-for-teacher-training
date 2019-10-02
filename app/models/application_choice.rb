e0a72242e7cd13c246a36b659f23888e36c3db3d
Duncan Brown <duncanjbrown@gmail.com>
2019-10-02 12:47:32 +0100

    Convert status enum to a string
    Using a string in the database means this value remains human-readable
    without Rails to map it for us

--- app/models/application_choice.rb
+++ app/models/application_choice.rb
@@ -1,5 +1,8 @@
 # Candidates can apply to multiple courses via a single Application Form.
 class ApplicationChoice < ApplicationRecord
   belongs_to :application_form, touch: true
-  enum status: { application_complete: 0, conditional_offer: 1 }
+  enum status: {
+    application_complete: 'application_complete',
+    conditional_offer: 'conditional_offer',
+  }
 end
