6c8debe6247655171ddc9d1af7891760e5fc0ef3
alexdesi <alexdesi@gmail.com>
2019-10-07 16:34:26 +0100

    Require metadata in POST bodies for Vendor API
    - Add metadata validation
    - Add schema for metadata error

--- app/controllers/vendor_api/confirm_candidate_enrolment_controller.rb
+++ app/controllers/vendor_api/confirm_candidate_enrolment_controller.rb
@@ -1,10 +1,14 @@
 module VendorApi
+  class InvalidMetadata < StandardError; end
+
   class ConfirmCandidateEnrolmentController < VendorApiController
     rescue_from ActiveRecord::RecordNotFound, with: :application_not_found
+    rescue_from InvalidMetadata, with: :invalid_metadata
 
     def confirm
-      application_choice = ApplicationChoice.find(params[:application_id])
+      check_metadata!
 
+      application_choice = ApplicationChoice.find(params[:application_id])
       result = ConfirmEnrolment.new(application_choice: application_choice).call
 
       if result.successful?
@@ -12,6 +16,28 @@ module VendorApi
           data: SingleApplicationPresenter.new(result.application_choice).as_json,
         }
       end
+    end
+
+  private
+
+    def check_metadata!
+      meta = params[:meta]
+
+      unless meta.present? &&
+          meta.keys.include?('attribution') &&
+          meta.keys.include?('timestamp')
+
+        raise InvalidMetadata
+      end
+    end
+
+    def invalid_metadata(_e)
+      render json: {
+        errors: [{
+          error: 'MetadataMissing',
+          message: 'A valid meta key, containing timestamp and attribution, was not included on the request body',
+        }],
+      }, status: 422
     end
   end
 end
