0e5faf20d21eb2308f7beace782040875785b220
Tijmen Brommet <tijmen@gmail.com>
2019-10-08 11:21:08 +0100

    Link to the current service in the header
    This makes sure that the provider interface header displays "Manage
    teacher training applications " instead of "Apply for teacher
    training", and links to the proper homepage.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,26 @@ module ApplicationHelper
       t('page_titles.application')
     end
   end
+
+  def service_name
+    case current_namespace
+    when 'provider_interface'
+      'Manage teacher training applications'
+    when 'candidate_interface'
+      'Apply for teacher training'
+    end
+  end
+
+  def service_link
+    case current_namespace
+    when 'provider_interface'
+      provider_interface_path
+    when 'candidate_interface'
+      candidate_interface_start_path
+    end
+  end
+
+  def current_namespace
+    params[:controller].split('/').first
+  end
 end
