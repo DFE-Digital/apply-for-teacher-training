27cbf126c9cf938e80527c32bc18073140521cf9
Tijmen Brommet <tijmen@gmail.com>
2019-10-10 09:13:45 +0100

    Use helpers for make API calls in request specs
    Soon, all requests will have to have a API token and all POST requests
    will have to send the `meta` data in the request body. To make this
    easier this commit adds a helper that we can use to add defaults to the
    request body and helpers. It doesn't do anything yet though.

--- spec/requests/vendor_api/get_single_application_spec.rb
+++ spec/requests/vendor_api/get_single_application_spec.rb
@@ -6,13 +6,13 @@ RSpec.describe 'Vendor API - GET /api/v1/applications/:id', type: :request do
   it 'returns a response that is valid according to the OpenAPI schema' do
     application_choice = create(:application_choice, provider_ucas_code: 'ABC')
 
-    get "/api/v1/applications/#{application_choice.id}"
+    get_api_request "/api/v1/applications/#{application_choice.id}"
 
     expect(parsed_response).to be_valid_against_openapi_schema('SingleApplicationResponse')
   end
 
   it "returns a not found error if the application can't be found" do
-    get '/api/v1/applications/asu7dvt87asd'
+    get_api_request '/api/v1/applications/asu7dvt87asd'
 
     expect(response).to have_http_status(404)
 
