5c0fcf344f7edd3008a89cca308014a6885d5f47
Theodor Vararu <theo@vararu.org>
2019-10-14 11:36:23 +0100

    Add PersonalDetailsReviewPresenter

---
+++ app/presenters/candidate_interface/personal_details_review_presenter.rb
@@ -0,0 +1,75 @@
+module CandidateInterface
+  class PersonalDetailsReviewPresenter
+    def initialize(form)
+      @form = form
+    end
+
+    def present
+      [
+        name_row,
+        date_of_birth_row,
+        nationality_row,
+        english_main_language_row,
+        language_details_row
+      ].compact
+    end
+
+  private
+
+    def name_row
+      {
+        key: I18n.t('application_form.personal_details.name.label'),
+        value: @form.name,
+        action: 'name',
+      }
+    end
+
+    def date_of_birth_row
+      {
+        key: I18n.t('application_form.personal_details.date_of_birth.label'),
+        value: @form.date_of_birth.strftime('%e %B %Y'),
+        action: 'date of birth',
+      }
+    end
+
+    def nationality_row
+      {
+        key: I18n.t('application_form.personal_details.nationality.label'),
+        value: [@form.first_nationality, @form.second_nationality].reject(&:blank?).to_sentence,
+        action: 'nationality',
+      }
+    end
+
+    def english_main_language_row
+      {
+        key: I18n.t('application_form.personal_details.english_main_language.label'),
+        value: @form.english_main_language.titleize,
+        action: 'if English is your main language',
+      }
+    end
+
+    def language_details_row
+      if @form.english_main_language?
+        english_main_language_details_row if @form.english_language_details.present?
+      else
+        other_language_details_row if @form.other_language_details.present?
+      end
+    end
+
+    def english_main_language_details_row
+      {
+        key: I18n.t('application_form.personal_details.english_main_language_details.label'),
+        value: @form.english_language_details,
+        action: 'other languages',
+      }
+    end
+
+    def other_language_details_row
+      {
+        key: I18n.t('application_form.personal_details.other_language_details.label'),
+        value: @form.other_language_details,
+        action: 'english languages qualifications',
+      }
+    end
+  end
+end
---
+++ spec/presenters/candidate_interface/personal_details_review_presenter_spec.rb
@@ -0,0 +1,186 @@
+require 'rails_helper'
+
+FactoryBot.define do
+  factory :personal_details_form, class: CandidateInterface::PersonalDetailsForm do
+    date_of_birth = Faker::Date.birthday
+
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    day { date_of_birth.day }
+    month { date_of_birth.month }
+    year { date_of_birth.year }
+    first_nationality { Faker::Nation.nationality }
+    second_nationality { Faker::Nation.nationality }
+    english_main_language { %w[yes no].sample }
+    english_language_details { Faker::Lorem.paragraph_by_chars(number: 200) }
+    other_language_details { Faker::Lorem.paragraph_by_chars(number: 200) }
+  end
+end
+
+RSpec.describe CandidateInterface::PersonalDetailsReviewPresenter do
+  describe '#present' do
+    it 'includes hashes for the name and date of birth' do
+      personal_details_form = FactoryBot.build(
+        :personal_details_form,
+        first_name: 'Max',
+        last_name: 'Caulfield',
+        day: 21,
+        month: 9,
+        year: 1995,
+      )
+
+      expect(present(personal_details_form)).to include(
+        row_for(:name, 'Max Caulfield'),
+        row_for(:date_of_birth, '21 September 1995'),
+      )
+    end
+
+    context 'when presenting nationality' do
+      it 'includes a hash for a single nationality' do
+        personal_details_form = FactoryBot.build(
+          :personal_details_form,
+          first_nationality: 'British',
+          second_nationality: nil,
+        )
+
+        expect(present(personal_details_form)).to include(
+          row_for(:nationality, 'British')
+        )
+      end
+
+      it 'includes a hash for dual nationalities' do
+        personal_details_form = FactoryBot.build(
+          :personal_details_form,
+          first_nationality: 'British',
+          second_nationality: 'Spanish',
+        )
+
+        expect(present(personal_details_form)).to include(
+          row_for(:nationality, 'British and Spanish')
+        )
+      end
+    end
+
+    context 'when presenting English as the main language' do
+      it 'includes a hash with "Yes"' do
+        personal_details_form = FactoryBot.build(
+          :personal_details_form,
+          english_main_language: 'yes',
+          english_language_details: '',
+          other_language_details: '',
+        )
+
+        expect(present(personal_details_form)).to include(
+          row_for(:english_main_language, 'Yes')
+        )
+      end
+
+      it 'excludes a hash for English language details if blank' do
+        personal_details_form = FactoryBot.build(
+          :personal_details_form,
+          english_main_language: 'yes',
+          english_language_details: '',
+          other_language_details: '',
+        )
+
+        expect(present(personal_details_form)).not_to include(
+          row_for(:english_main_language_details, '')
+        )
+      end
+
+      it 'excludes a hash for other language details if given' do
+        personal_details_form = FactoryBot.build(
+          :personal_details_form,
+          english_main_language: 'yes',
+          english_language_details: '',
+          other_language_details: 'Broken? Oh man, are you cereal?',
+        )
+
+        expect(present(personal_details_form)).not_to include(
+          row_for(:other_language_details, 'Broken? Oh man, are you cereal?')
+        )
+      end
+
+      it 'includes a hash for English language details if given' do
+        personal_details_form = FactoryBot.build(
+          :personal_details_form,
+          english_main_language: 'yes',
+          english_language_details: 'Mi nombre es Max.',
+          other_language_details: '',
+        )
+
+        expect(present(personal_details_form)).to include(
+          row_for(:english_main_language_details, 'Mi nombre es Max.')
+        )
+      end
+    end
+
+    context 'when presenting English not as the main language' do
+      it 'includes a hash with "No"' do
+        personal_details_form = FactoryBot.build(
+          :personal_details_form,
+          english_main_language: 'no',
+          english_language_details: '',
+          other_language_details: '',
+        )
+
+        expect(present(personal_details_form)).to include(
+          row_for(:english_main_language, 'No')
+        )
+      end
+
+      it 'excludes a hash for other language details if blank' do
+        personal_details_form = FactoryBot.build(
+          :personal_details_form,
+          english_main_language: 'no',
+          english_language_details: '',
+          other_language_details: '',
+        )
+
+        expect(present(personal_details_form)).not_to include(
+          row_for(:other_language_details, '')
+        )
+      end
+
+      it 'excludes a hash for English language details if given' do
+        personal_details_form = FactoryBot.build(
+          :personal_details_form,
+          english_main_language: 'no',
+          english_language_details: 'Mi nombre es Max.',
+          other_language_details: '',
+        )
+
+        expect(present(personal_details_form)).not_to include(
+          row_for(:english_main_language_details, 'Mi nombre es Max.')
+        )
+      end
+
+      it 'includes a hash for other language details if given' do
+        personal_details_form = FactoryBot.build(
+          :personal_details_form,
+          english_main_language: 'no',
+          english_language_details: '',
+          other_language_details: 'Broken? Oh man, are you cereal?',
+        )
+
+        expect(present(personal_details_form)).to include(
+          row_for(:other_language_details, 'Broken? Oh man, are you cereal?')
+        )
+      end
+    end
+  end
+
+  def present(form)
+    CandidateInterface::PersonalDetailsReviewPresenter
+      .new(form)
+      .present
+  end
+
+  def row_for(key, value)
+    {
+      key: t("application_form.personal_details.#{key.to_s}.label"),
+      value: value,
+      action: t("application_form.personal_details.#{key.to_s}.change_action"),
+    }
+  end
+end
