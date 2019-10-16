8af6b9e8919fbf14a14b3cddec10070c50755ece
Tijmen Brommet <tijmen@gmail.com>
2019-10-16 09:07:21 +0100

    Add a test that state machine states match the Rails enum
    Just to be sure!

--- spec/services/application_state_change_spec.rb
+++ spec/services/application_state_change_spec.rb
@@ -3,11 +3,13 @@ require 'rails_helper'
 RSpec.describe ApplicationStateChange do
   describe '#valid_states' do
     it 'has human readable translations' do
-      expect(
-        ApplicationStateChange.valid_states
-      ).to eql(
-        I18n.t('application_choice.status_name').keys
-      )
+      expect(ApplicationStateChange.valid_states)
+        .to eql(I18n.t('application_choice.status_name').keys)
+    end
+
+    it 'corresponding enum entries' do
+      expect(ApplicationStateChange.valid_states)
+        .to eql(ApplicationChoice.statuses.keys.map(&:to_sym))
     end
   end
 end
