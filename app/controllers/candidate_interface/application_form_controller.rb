f1f290e2791c968b88cc2b59da8b3140af46b7f9
Theodor Vararu <theo@vararu.org>
2019-10-16 11:57:51 +0100

    Add completed label to personal details section

--- app/controllers/candidate_interface/application_form_controller.rb
+++ app/controllers/candidate_interface/application_form_controller.rb
@@ -5,6 +5,7 @@ module CandidateInterface
     def show
       redirect_to candidate_interface_application_form_path if params[:token]
       @application_form = current_candidate.current_application
+      @application_form_presenter = CandidateInterface::ApplicationFormPresenter.new(@application_form)
     end
   end
 end
