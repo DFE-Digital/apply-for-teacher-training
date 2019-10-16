cad4692d881bb02d12ee370d4f95eee4c67a64d2
Theodor Vararu <theo@vararu.org>
2019-10-16 12:22:44 +0100

    Fake a submission for show and tell

--- app/controllers/candidate_interface/application_submit_controller.rb
+++ app/controllers/candidate_interface/application_submit_controller.rb
@@ -2,6 +2,22 @@ module CandidateInterface
   class ApplicationSubmitController < CandidateInterfaceController
     before_action :authenticate_candidate!
 
-    def show; end
+    def show
+      @application_form = current_candidate.current_application
+    end
+
+    def submit
+      @application_form = current_candidate.current_application
+
+      # TODO: Show and Tell HACK
+      application_not_submitted = @application_form.application_choices.first.status != 'application_complete'
+      if application_not_submitted
+        @application_form.application_choices.each do |application_choice|
+          ApplicationStateChange.new(application_choice).submit!
+        end
+      end
+
+      render :success
+    end
   end
 end
