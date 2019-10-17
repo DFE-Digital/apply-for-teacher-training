86e29a3bfb906fd63c385ab1ede14802ced9aad7
Theodor Vararu <theo@vararu.org>
2019-10-17 17:05:57 +0100

    Run prettier on app/frontend/packs/application.js

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -1,25 +1,25 @@
-require.context('govuk-frontend/govuk/assets');
-import { initAll as govUKFrontendInitAll } from 'govuk-frontend';
-import 'accessible-autocomplete/dist/accessible-autocomplete.min.css';
-import accessibleAutocomplete from 'accessible-autocomplete';
+require.context("govuk-frontend/govuk/assets");
+import { initAll as govUKFrontendInitAll } from "govuk-frontend";
+import "accessible-autocomplete/dist/accessible-autocomplete.min.css";
+import accessibleAutocomplete from "accessible-autocomplete";
 
-import '../styles/application.scss';
+import "../styles/application.scss";
 govUKFrontendInitAll();
 
 try {
   const nationalitySelects = [
-    '#candidate-interface-personal-details-form-first-nationality-field',
-    '#candidate-interface-personal-details-form-second-nationality-field'
+    "#candidate-interface-personal-details-form-first-nationality-field",
+    "#candidate-interface-personal-details-form-second-nationality-field"
   ].forEach(id => {
     const nationalitySelect = document.querySelector(id);
 
     // Replace "Select a nationality" with empty string
-    nationalitySelect.querySelector("[value='']").innerHTML = '';
+    nationalitySelect.querySelector("[value='']").innerHTML = "";
 
     accessibleAutocomplete.enhanceSelectElement({
       selectElement: nationalitySelect
-    })
-  })
-} catch(err) {
-  console.error('Could not enhance nationality select:', err)
+    });
+  });
+} catch (err) {
+  console.error("Could not enhance nationality select:", err);
 }
