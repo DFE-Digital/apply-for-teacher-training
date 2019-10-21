bf4856337c39053e7551d237ea3a2963e5cb0f80
Wen Ting Wang <chubberlisk@outlook.com>
2019-10-21 11:41:25 +0100

    Reorder methods to follow order in scenario for readability

--- spec/system/candidate_interface/candidate_submitting_application_spec.rb
+++ spec/system/candidate_interface/candidate_submitting_application_spec.rb
@@ -17,26 +17,37 @@ RSpec.feature 'Candidate submit the application' do
     create_and_sign_in_candidate
   end
 
-  def and_i_can_see_my_application_has_been_successfully_submitted
-    expect(page).to have_content 'Application successfully submitted'
-  end
-
-  def then_i_can_submit_the_application
-    click_button'Submit application'
-  end
-
-  def and_i_confirm_my_application
-    click_link 'Continue'
-  end
-
   def and_i_filled_in_personal_details_and_review_my_application
     and_i_filled_in_personal_details
     and_i_visit_the_application_form_page
     when_i_click_on_check_your_answers
   end
 
+  def and_i_filled_in_personal_details
+    visit candidate_interface_personal_details_edit_path
+    candidate_fills_in_personal_details(scope: 'application_form.personal_details')
+
+    click_button t('complete_form_button', scope: 'application_form.personal_details')
+  end
+
   def and_i_visit_the_application_form_page
     visit candidate_interface_application_form_path
+  end
+
+  def when_i_click_on_check_your_answers
+    click_link 'Check your answers before submitting'
+  end
+
+  def and_i_confirm_my_application
+    click_link 'Continue'
+  end
+
+  def then_i_can_submit_the_application
+    click_button'Submit application'
+  end
+
+  def and_i_can_see_my_application_has_been_successfully_submitted
+    expect(page).to have_content 'Application successfully submitted'
   end
 
   def then_i_can_review_my_application
@@ -48,16 +59,5 @@ RSpec.feature 'Candidate submit the application' do
     expect(page).to have_content '6 April 1937'
     expect(page).to have_content 'British and American'
     expect(page).to have_content "I'm great at Galactic Basic so English is a piece of cake"
-  end
-
-  def when_i_click_on_check_your_answers
-    click_link 'Check your answers before submitting'
-  end
-
-  def and_i_filled_in_personal_details
-    visit candidate_interface_personal_details_edit_path
-    candidate_fills_in_personal_details(scope: 'application_form.personal_details')
-
-    click_button t('complete_form_button', scope: 'application_form.personal_details')
   end
 end
