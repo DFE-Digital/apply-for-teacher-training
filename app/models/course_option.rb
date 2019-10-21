cfa3642572374a590888a3cc6c7f7311692dca34
Steve Hook <steve.hook@gmail.com>
2019-10-21 10:43:35 +0100

    Refactor to move validation condition into method

--- app/models/course_option.rb
+++ app/models/course_option.rb
@@ -4,9 +4,11 @@ class CourseOption < ApplicationRecord
   has_many :application_choices
 
   validates :vacancy_status, presence: true
-  validate :validate_providers, if: -> { site.present? && course.present? }
+  validate :validate_providers
 
   def validate_providers
+    return unless site.present? && course.present?
+
     return if site.provider == course.provider
 
     errors.add(:site, 'must have the same Provider as the course')
