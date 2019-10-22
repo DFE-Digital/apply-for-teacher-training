1cdc1df07b09ac501fdcee0cb9fe5fd85c5faa7c
Wen Ting Wang <chubberlisk@outlook.com>
2019-10-22 16:59:31 +0100

    Add ability to add phone number to contact details

---
+++ spec/system/candidate_interface/candidate_entering_contact_details_spec.rb
@@ -0,0 +1,71 @@
+require 'rails_helper'
+
+RSpec.feature 'Entering their contact details' do
+  include CandidateHelper
+
+  scenario 'Candidate submits their contact details' do
+    given_i_am_not_signed_in
+    and_i_visit_the_contact_details_page
+    then_i_should_see_the_homepage
+
+    given_i_am_signed_in
+    and_i_visit_the_site
+
+    when_i_click_on_contact_details
+    # and_i_incorrectly_fill_in_my_phone_number
+    # and_i_submit_the_form
+    # then_i_should_see_validation_errors
+
+    when_i_fill_in_my_phone_number
+    and_i_submit_the_form
+    # and_i_fill_in_my_address
+    # and_i_submit_the_form
+    then_i_can_check_my_answers
+
+    # when_i_submit_my_details
+    # then_i_should_see_the_form
+    # and_that_the_section_is_completed
+
+    # when_i_click_on_contact_details
+    # then_i_can_check_my_revised_answers
+  end
+
+  def given_i_am_not_signed_in; end
+
+  def and_i_visit_the_contact_details_page
+    visit candidate_interface_contact_details_edit_path
+  end
+
+  def then_i_should_see_the_homepage
+    expect(page).to have_current_path(candidate_interface_start_path)
+  end
+
+  def given_i_am_signed_in
+    create_and_sign_in_candidate
+  end
+
+  def and_i_visit_the_site
+    visit candidate_interface_application_form_path
+  end
+
+  def when_i_click_on_contact_details
+    click_link t('page_titles.contact_details')
+  end
+
+  def and_i_incorrectly_fill_in_my_phone_number
+    fill_in t('application_form.contact_details.phone_number.label'), with: 'A phone number'
+  end
+
+  def and_i_submit_the_form
+    click_button t('application_form.contact_details.complete_form_button')
+  end
+
+  def when_i_fill_in_my_phone_number
+    fill_in t('application_form.contact_details.phone_number.label'), with: '07700 900 982'
+  end
+
+  def then_i_can_check_my_answers
+    expect(page).to have_content t('application_form.contact_details.phone_number.label')
+    expect(page).to have_content '07700 900 982'
+  end
+end
