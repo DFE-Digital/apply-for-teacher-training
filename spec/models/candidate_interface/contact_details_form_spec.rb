1ac71e497a4bad36ad5c5e0efabb918bc4708bc7
Wen Ting Wang <chubberlisk@outlook.com>
2019-10-24 11:21:16 +0100

    Add #save_address to ContactDetailsForm and rename #save to #save_base

--- spec/models/candidate_interface/contact_details_form_spec.rb
+++ spec/models/candidate_interface/contact_details_form_spec.rb
@@ -1,36 +1,60 @@
 require 'rails_helper'
 
 RSpec.describe CandidateInterface::ContactDetailsForm, type: :model do
-  let(:data) do
-    {
-      phone_number: '07700 900 982',
-    }
-  end
-
   describe '.build_from_application' do
     it 'creates an object based on the provided ApplicationForm' do
+      data = {
+        phone_number: Faker::PhoneNumber.cell_phone,
+        address_line1: Faker::Address.street_name,
+        address_line2: Faker::Address.street_address,
+        address_line3: Faker::Address.city,
+        address_line4: Faker::Address.country,
+        postcode: Faker::Address.postcode,
+      }
       application_form = build_stubbed(:application_form, data)
-      contact_details = CandidateInterface::ContactDetailsForm.build_from_application(
-        application_form,
-      )
+      contact_details = CandidateInterface::ContactDetailsForm.build_from_application(application_form)
 
       expect(contact_details).to have_attributes(data)
     end
   end
 
-  describe '#save' do
+  describe '#save_base' do
     it 'returns false if not valid' do
       contact_details = CandidateInterface::ContactDetailsForm.new
 
-      expect(contact_details.save(ApplicationForm.new)).to eq(false)
+      expect(contact_details.save_base(ApplicationForm.new)).to eq(false)
     end
 
     it 'updates the provided ApplicationForm if valid' do
-      application_form = build(:application_form, data)
-      contact_details = CandidateInterface::ContactDetailsForm.new(data)
+      form_data = { phone_number: Faker::PhoneNumber.cell_phone }
+      application_form = build(:application_form)
+      contact_details = CandidateInterface::ContactDetailsForm.new(form_data)
 
-      expect(contact_details.save(application_form)).to eq(true)
-      expect(application_form).to have_attributes(data)
+      expect(contact_details.save_base(application_form)).to eq(true)
+      expect(application_form).to have_attributes(form_data)
+    end
+  end
+
+  describe '#save_address' do
+    it 'returns false if not valid' do
+      contact_details = CandidateInterface::ContactDetailsForm.new
+
+      expect(contact_details.save_address(ApplicationForm.new)).to eq(false)
+    end
+
+    it 'updates the provided ApplicationForm with the address fields if valid' do
+      form_data = {
+        address_line1: Faker::Address.street_name,
+        address_line2: Faker::Address.street_address,
+        address_line3: Faker::Address.city,
+        address_line4: Faker::Address.country,
+        postcode: Faker::Address.postcode,
+      }
+      application_form = build(:application_form)
+      contact_details = CandidateInterface::ContactDetailsForm.new(form_data)
+
+      expect(contact_details.save_address(application_form)).to eq(true)
+      expect(application_form).to have_attributes(form_data)
     end
   end
 end
