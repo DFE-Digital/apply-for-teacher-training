cda808df93a36f2792465a60e46ccd2085e14da4
Wen Ting Wang <chubberlisk@outlook.com>
2019-10-24 14:01:42 +0100

    Refactor contact details buttons so each are individual

--- spec/system/candidate_interface/candidate_entering_contact_details_spec.rb
+++ spec/system/candidate_interface/candidate_entering_contact_details_spec.rb
@@ -13,13 +13,13 @@ RSpec.feature 'Entering their contact details' do
 
     when_i_click_on_contact_details
     # and_i_incorrectly_fill_in_my_phone_number
-    # and_i_submit_the_form
+    # and_i_submit_my_phone_number
     # then_i_should_see_validation_errors
 
     when_i_fill_in_my_phone_number
-    and_i_submit_the_form
+    and_i_submit_my_phone_number
     and_i_fill_in_my_address
-    and_i_submit_the_form
+    and_i_submit_my_address
     then_i_can_check_my_answers
 
     # when_i_submit_my_details
@@ -56,22 +56,26 @@ RSpec.feature 'Entering their contact details' do
     fill_in t('application_form.contact_details.phone_number.label'), with: 'A phone number'
   end
 
-  def and_i_submit_the_form
-    click_button t('application_form.contact_details.complete_form_button')
-  end
-
   def when_i_fill_in_my_phone_number
     fill_in t('application_form.contact_details.phone_number.label'), with: '07700 900 982'
   end
 
-  def then_i_can_check_my_answers
-    expect(page).to have_content t('application_form.contact_details.phone_number.label')
-    expect(page).to have_content '07700 900 982'
+  def and_i_submit_my_phone_number
+    click_button t('application_form.contact_details.base.button')
   end
 
   def and_i_fill_in_my_address
     fill_in t('application_form.contact_details.address_line1.label'), with: '42 Much Wow Street'
     fill_in t('application_form.contact_details.address_line3.label'), with: 'London'
     fill_in t('application_form.contact_details.postcode.label'), with: 'SW1P 3BT'
+  end
+
+  def and_i_submit_my_address
+    click_button t('application_form.contact_details.address.button')
+  end
+
+  def then_i_can_check_my_answers
+    expect(page).to have_content t('application_form.contact_details.phone_number.label')
+    expect(page).to have_content '07700 900 982'
   end
 end
