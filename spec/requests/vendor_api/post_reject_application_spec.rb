55669a9987fd501c8e2dd01526b0e21b39a32215
Steve Hook <steve.hook@gmail.com>
2019-10-24 12:08:38 +0100

    Generalise audit request spec for rejecting applications

--- spec/requests/vendor_api/post_reject_application_spec.rb
+++ spec/requests/vendor_api/post_reject_application_spec.rb
@@ -38,7 +38,11 @@ RSpec.describe 'Vendor API - POST /applications/:application_id/reject', type: :
         post_api_request "/api/v1/applications/#{application_choice.id}/reject", params: request_body
       }.to(change { application_choice.audits.count })
       expect(application_choice.audits.last.user).to be_present
-      expect(application_choice.audits.last.user.full_name).to eq VendorApiSpecHelpers::VALID_METADATA[:attribution][:full_name]
+      %i[full_name email user_id].each do |attribute|
+        expect(application_choice.audits.last.user.send(attribute)).to eq(
+          VendorApiSpecHelpers::VALID_METADATA[:attribution][attribute],
+        )
+      end
     end
   end
 
