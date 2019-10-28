7a4f5f742673b62a7f783cc8f4340b1d1ba7f0b9
Steve Hook <steve.hook@gmail.com>
2019-10-28 12:50:09 +0000

    Refactor spec by reusing api token
    It's probably better to have a single api token per example rather than
    generating a new one for every request.

--- spec/requests/vendor_api/audit_trail_spec.rb
+++ spec/requests/vendor_api/audit_trail_spec.rb
@@ -29,13 +29,12 @@ RSpec.describe 'Vendor API - audit trail', type: :request do
 
   it 'updates the audit trail but reuses a VendorApiUser with same vendor_user_id when rejecting an application' do
     application_choice = create_application_choice_for_currently_authenticated_provider(status: 'application_complete')
-    unhashed_token = VendorApiToken.create_with_random_token!(provider: currently_authenticated_provider)
     create(
       :vendor_api_user,
       full_name: VendorApiSpecHelpers::VALID_METADATA[:attribution][:full_name],
       email_address: 'jane.smith@example.com',
       vendor_user_id: VendorApiSpecHelpers::VALID_METADATA[:attribution][:user_id],
-      vendor_api_token_id: VendorApiToken.find_by_unhashed_token(unhashed_token).id,
+      vendor_api_token_id: VendorApiToken.find_by_unhashed_token(api_token).id,
     )
     request_body = {
       "data": {
@@ -47,10 +46,6 @@ RSpec.describe 'Vendor API - audit trail', type: :request do
       post_api_request(
         "/api/v1/applications/#{application_choice.id}/reject",
         params: request_body,
-        headers: {
-          'Authorization' => "Bearer #{unhashed_token}",
-          'Content-Type' => 'application/json',
-        },
       )
     }.to(change { VendorApiUser.count }.by(0))
     expect(application_choice.audits.last.user.email_address).to eq 'jane@example.com'
