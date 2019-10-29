8d0e647662f4d7d947d9b5e70cbb7cfa08e4635a
Wen Ting Wang <chubberlisk@outlook.com>
2019-10-29 15:33:37 +0000

    Add DegreesForm with basic validation
    This adds presence and length validation for `qualification_type`,
    `subject` and `institution_name` which are the only fields on the
    first page of adding an undergraduate degree.

---
+++ spec/models/candidate_interface/degrees_form_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe CandidateInterface::DegreesForm, type: :model do
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:qualification_type) }
+    it { is_expected.to validate_presence_of(:subject) }
+    it { is_expected.to validate_presence_of(:institution_name) }
+
+    it { is_expected.to validate_length_of(:qualification_type).is_at_most(255) }
+    it { is_expected.to validate_length_of(:subject).is_at_most(255) }
+    it { is_expected.to validate_length_of(:institution_name).is_at_most(255) }
+  end
+
+  describe '#save_base' do
+    it 'returns false if not valid' do
+      degree = CandidateInterface::DegreesForm.new
+
+      expect(degree.save_base(ApplicationForm.new)).to eq(false)
+    end
+
+    it 'updates the provided ApplicationForm if valid' do
+      form_data = {
+        qualification_type: 'BA',
+        subject: 'Doge',
+        institution_name: 'University of Much Wow',
+      }
+      application_form = create(:application_form)
+      degree = CandidateInterface::DegreesForm.new(form_data)
+
+      expect(degree.save_base(application_form)).to eq(true)
+      expect(application_form.application_qualifications.degree.first)
+        .to have_attributes(form_data)
+    end
+  end
+end
