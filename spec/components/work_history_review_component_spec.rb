b8204c2a2e58e4334556bed1ef1c23d3f91cc140
Theodor Vararu <theo@vararu.org>
2019-10-29 17:04:04 +0000

    Pass in a header to work experience items

--- spec/components/work_history_review_component_spec.rb
+++ spec/components/work_history_review_component_spec.rb
@@ -2,12 +2,15 @@ require 'rails_helper'
 
 RSpec.describe WorkHistoryReviewComponent do
   it 'renders component with correct structure' do
-    application_form = build(:completed_application_form)
+    application_form = build(:completed_application_form, work_experiences_count: 1)
 
     result = render_inline(WorkHistoryReviewComponent, application_form: application_form)
 
     application_form.application_work_experiences.each do |work|
       expect(result.text).to include(work.role)
+      if (work.working_with_children)
+        expect(result.text).to include(t('application_form.work_history.working_with_children'))
+      end
     end
   end
 end
