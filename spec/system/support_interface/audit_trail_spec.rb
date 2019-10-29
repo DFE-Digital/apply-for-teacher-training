30ae31e0a292a4e0eac85060fb1de837e1a8e815
Steve Hook <steve.hook@gmail.com>
2019-10-29 11:11:27 +0000

    Assert that audit items are returned in correct order

--- spec/system/support_interface/audit_trail_spec.rb
+++ spec/system/support_interface/audit_trail_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.feature 'See applications' do
+  around do |example|
+    Timecop.freeze(Time.zone.local(2019, 10, 1, 12, 0, 0)) do
+      example.run
+    end
+  end
+
   scenario 'Support user visits application audit page' do
     given_i_am_a_support_user
     and_there_is_an_application_in_the_system_logged_by_a_candidate
@@ -17,11 +23,20 @@ RSpec.feature 'See applications' do
 
   def and_there_is_an_application_in_the_system_logged_by_a_candidate
     candidate = create :candidate, email_address: 'alice@example.com'
+
     Audited.audit_class.as_user(candidate) do
-      @application_choice = create(
-        :application_choice,
-        application_form: create(:application_form, first_name: 'Alice', last_name: 'Wunder'),
+      application_form = create(
+        :application_form,
+        first_name: 'Alice',
+        last_name: 'Wunder',
+        candidate: candidate,
       )
+      Timecop.freeze(Time.zone.local(2019, 10, 1, 12, 0, 1)) do
+        @application_choice = create(
+          :application_choice,
+          application_form: application_form,
+        )
+      end
     end
   end
 
@@ -38,7 +53,13 @@ RSpec.feature 'See applications' do
   end
 
   def then_i_should_be_able_to_see_history_events
-    expect(page).to have_content 'Create Application Choice - alice@example.com (Candidate)'
-    expect(page).to have_content 'Create Application Form - alice@example.com (Candidate)'
+    within('tbody tr:eq(1)') do
+      expect(page).to have_content '2019-10-01 12:00:01'
+      expect(page).to have_content 'Create Application Choice - alice@example.com (Candidate)'
+    end
+    within('tbody tr:eq(2)') do
+      expect(page).to have_content '2019-10-01 12:00:00'
+      expect(page).to have_content 'Create Application Form - alice@example.com (Candidate)'
+    end
   end
 end
