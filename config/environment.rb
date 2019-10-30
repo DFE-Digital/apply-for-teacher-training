dfe8f8085040ce6ead941a9cd144872b1ff0ff8d
Michael Nacos <m.nacos@gmail.com>
2019-10-30 10:44:51 +0000

    avoid constant autoloading deprecation warnings

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
 # Load the Rails application.
 require_relative 'application'
 
+# Avoid constant autoloading in initializers deprecation warnings
+require './app/lib/azure_environment'
+require './app/lib/logstash_logging'
+require './app/warden/magic_link_strategy'
+
 # Initialize the Rails application.
 Rails.application.initialize!
