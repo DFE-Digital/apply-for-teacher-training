f2a90ad1634f5688c801f76f9603513ba19f2921
Wen Ting Wang <chubberlisk@outlook.com>
2019-10-31 13:33:45 +0000

    Add DegreesReviewComponent

---
+++ app/components/degrees_review_component.rb
@@ -0,0 +1,68 @@
+class DegreesReviewComponent < ActionView::Component::Base
+  validates :application_form, presence: true
+
+  def initialize(application_form:)
+    @application_form = application_form
+    @degrees_form = CandidateInterface::DegreesForm.build_from_application(
+      @application_form,
+    )
+  end
+
+  def degree_form_rows(degree_form)
+    [
+      qualification_row(degree_form),
+      award_year_row(degree_form),
+      grade_row(degree_form),
+    ]
+  end
+
+  def formatted_degree_title(qualification_type, subject)
+    "#{qualification_type} #{subject}"
+  end
+
+private
+
+  attr_reader :application_form
+
+  def qualification_row(degree_form)
+    {
+      key: t('application_form.degree.qualification.label'),
+      DANGEROUS_html_value: formatted_qualification(degree_form.qualification_type, degree_form.subject, degree_form.institution_name),
+      action: t('application_form.degree.qualification.change_action'),
+      change_path: '#',
+    }
+  end
+
+  def award_year_row(degree_form)
+    {
+      key: t('application_form.degree.award_year.review_label'),
+      value: degree_form.award_year,
+      action: t('application_form.degree.award_year.change_action'),
+      change_path: '#',
+    }
+  end
+
+  def grade_row(degree_form)
+    {
+      key: t('application_form.degree.grade.review_label'),
+      value: formatted_grade(degree_form.grade, degree_form.predicted_grade),
+      action: t('application_form.degree.grade.change_action'),
+      change_path: '#',
+    }
+  end
+
+  def formatted_qualification(qualification_type, subject, institution_name)
+    [formatted_degree_title(qualification_type, subject), institution_name]
+      .map { |line| sanitize(line, tags: []) }
+      .join('<br>')
+  end
+
+  def formatted_grade(grade, predicted_grade)
+    case grade
+    when 'first', 'upper_second', 'lower_second', 'third'
+      t("application_form.degree.grade.#{grade}.label")
+    else
+      predicted_grade ? "#{grade} (Predicted)" : grade
+    end
+  end
+end
---
+++ spec/components/degrees_review_component_spec.rb
@@ -0,0 +1,68 @@
+require 'rails_helper'
+
+RSpec.describe DegreesReviewComponent do
+  let(:application_form) do
+    create(:application_form) do |form|
+      form.application_qualifications.create(
+        level: 'degree',
+        qualification_type: 'BA',
+        subject: 'Woof',
+        institution_name: 'University of Doge',
+        grade: 'first',
+        predicted_grade: false,
+        award_year: '2008',
+      )
+      form.application_qualifications.create(
+        level: 'degree',
+        qualification_type: 'BA',
+        subject: 'Meow',
+        institution_name: 'University of Cate',
+        grade: '2:1',
+        predicted_grade: true,
+        award_year: '2010',
+      )
+    end
+  end
+
+  it 'renders component with correct values for a qualification' do
+    result = render_inline(DegreesReviewComponent, application_form: application_form)
+
+    expect(result.css('.app-summary-card__title').text).to include('BA Woof')
+    expect(result.css('.govuk-summary-list__key').text).to include(t('application_form.degree.qualification.label'))
+    expect(result.css('.govuk-summary-list__value').to_html).to include('BA Woof<br>University of Doge')
+    expect(result.css('.govuk-summary-list__actions a')[0].attr('href')).to include('#')
+    expect(result.css('.govuk-summary-list__actions').text).to include("Change #{t('application_form.degree.qualification.change_action')}")
+  end
+
+  it 'renders component with correct values for an award year' do
+    result = render_inline(DegreesReviewComponent, application_form: application_form)
+
+    expect(result.css('.app-summary-card__title').text).to include('BA Woof')
+    expect(result.css('.govuk-summary-list__key').text).to include(t('application_form.degree.award_year.review_label'))
+    expect(result.css('.govuk-summary-list__value').text).to include('2008')
+    expect(result.css('.govuk-summary-list__actions').text).to include("Change #{t('application_form.degree.award_year.change_action')}")
+  end
+
+  it 'renders component with correct values for a grade' do
+    result = render_inline(DegreesReviewComponent, application_form: application_form)
+
+    expect(result.css('.app-summary-card__title').text).to include('BA Woof')
+    expect(result.css('.govuk-summary-list__key').text).to include(t('application_form.degree.grade.review_label'))
+    expect(result.css('.govuk-summary-list__value').text).to include('First')
+    expect(result.css('.govuk-summary-list__actions').text).to include("Change #{t('application_form.degree.grade.change_action')}")
+  end
+
+  it 'renders component with correct values for a predicted grade' do
+    result = render_inline(DegreesReviewComponent, application_form: application_form)
+
+    expect(result.css('.app-summary-card__title').text).to include('BA Meow')
+    expect(result.css('.govuk-summary-list__value').text).to include('2:1 (Predicted)')
+  end
+
+  it 'renders component with correct values for multiple degrees' do
+    result = render_inline(DegreesReviewComponent, application_form: application_form)
+
+    expect(result.css('.app-summary-card__title').text).to include('BA Woof')
+    expect(result.css('.app-summary-card__title').text).to include('BA Meow')
+  end
+end
