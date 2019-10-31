713e007b4644f49676b7246069ff25e1e887c993
Steve Hook <steve.hook@gmail.com>
2019-10-31 23:14:57 +0000

    Refactor components into more manageable sub-components

--- app/components/support_interface/audit_trail_component.rb
+++ app/components/support_interface/audit_trail_component.rb
@@ -8,38 +8,8 @@ module SupportInterface
       @application_form = application_form
     end
 
-    def audit_entry_label(audit)
-      "#{audit_entry_event_label(audit)} - #{audit_entry_user_label(audit)}"
-    end
-
-    def format_audit_value(value)
-      if value.is_a? Array
-        "#{value[0] || 'nil'} → #{value[1] || 'nil'}"
-      else
-        value.to_s
-      end
-    end
-
-    def audit_value_present?(value)
-      if value.is_a? Array
-        value.any?(&:present?)
-      else
-        value.present?
-      end
-    end
-
-    def audit_entry_event_label(audit)
-      "#{audit.action.capitalize} #{audit.auditable_type.titlecase}"
-    end
-
-    def audit_entry_user_label(audit)
-      if audit.user_type == 'Candidate'
-        "#{audit.user.email_address} (Candidate)"
-      elsif audit.user_type == 'VendorApiUser'
-        "#{audit.user.email_address} (Vendor API)"
-      else
-        '(Unknown User)'
-      end
+    def audits
+      application_form.audits
     end
 
     attr_reader :application_form
---
+++ app/components/support_interface/audit_trail_item_component.rb
@@ -0,0 +1,39 @@
+module SupportInterface
+  class AuditTrailItemComponent < ActionView::Component::Base
+    include ViewHelper
+
+    validates :audit, presence: true
+
+    def initialize(audit:)
+      @audit = audit
+    end
+
+    def audit_entry_event_label
+      "#{audit.action.capitalize} #{audit.auditable_type.titlecase}"
+    end
+
+    def audit_entry_user_label
+      if audit.user_type == 'Candidate'
+        "#{audit.user.email_address} (Candidate)"
+      elsif audit.user_type == 'VendorApiUser'
+        "#{audit.user.email_address} (Vendor API)"
+      else
+        '(Unknown User)'
+      end
+    end
+
+    def changes
+      audit.audited_changes.select { |_, values| audit_value_present?(values) }
+    end
+
+    def audit_value_present?(value)
+      if value.is_a? Array
+        value.any?(&:present?)
+      else
+        value.present?
+      end
+    end
+
+    attr_reader :audit
+  end
+end
