c6b027bce3a299be0c3b1f53c1683eae97f72a6d
Duncan Brown <duncanjbrown@gmail.com>
2019-11-04 15:09:53 +0000

    Refactor SyncProvidersFromFind to be singular
    - Have it operate on arguments instead of on baked-in state, meaning it
    has fewer reasons to change
    - Remove the now-redundant each loop inside it
    - Give the parameter a name

--- app/controllers/support_interface/providers_controller.rb
+++ app/controllers/support_interface/providers_controller.rb
@@ -5,7 +5,12 @@ module SupportInterface
     end
 
     def sync
-      SyncProvidersFromFind.call
+      # TODO: We're launching the pilot with these 3 providers, but at some point
+      # we'll want to expand to others and we will need a better mechanism to
+      # manage these.
+      providers = %w[R55 1N1 S31]
+
+      providers.each { |p| SyncProviderFromFind.call(provider_code: p) }
       redirect_to action: 'index'
     end
   end
