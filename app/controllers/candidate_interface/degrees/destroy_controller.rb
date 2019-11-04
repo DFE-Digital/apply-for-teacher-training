1ef11b0056284535a41b9baf8b3fe9671c39da5b
Wen Ting Wang <chubberlisk@outlook.com>
2019-11-04 10:35:44 +0000

    Add ability to delete a degree

---
+++ app/controllers/candidate_interface/degrees/destroy_controller.rb
@@ -0,0 +1,23 @@
+module CandidateInterface
+  class Degrees::DestroyController < CandidateInterfaceController
+    def confirm_destroy
+      application_form = current_candidate.current_application
+      @degree = DegreesForm.find_by_application(application_form, degrees_params[:id])
+    end
+
+    def destroy
+      current_candidate.current_application
+        .application_qualifications
+        .find(degrees_params[:id])
+        .destroy!
+
+      redirect_to candidate_interface_degrees_review_path
+    end
+
+  private
+
+    def degrees_params
+      params.permit(:id)
+    end
+  end
+end
