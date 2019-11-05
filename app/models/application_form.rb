cf63a2f75a2ca401ad1bf3975045e196a9a6b987
Steve Hook <steve.hook@gmail.com>
2019-11-05 16:54:30 +0000

    Remove redundant method

--- app/models/application_form.rb
+++ app/models/application_form.rb
@@ -16,9 +16,5 @@ class ApplicationForm < ApplicationRecord
     references.any? && references.all?(&:complete?)
   end
 
-  def complete?
-    application_choices.any? && application_choices.first.application_complete?
-  end
-
   audited
 end
