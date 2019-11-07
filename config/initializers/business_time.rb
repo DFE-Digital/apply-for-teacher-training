a8b655a42519e4e6e511a084f8dfb350f0096db6
Steve Hook <steve.hook@gmail.com>
2019-11-07 16:10:53 +0000

    Ensure that BusinessTime working hours cover the whole day

--- config/initializers/business_time.rb
+++ config/initializers/business_time.rb
@@ -2,4 +2,5 @@ Holidays.between(Date.civil(2019, 1, 1), 2.years.from_now, :gb_eng, :observed).m
   BusinessTime::Config.holidays << holiday[:date]
 end
 
+BusinessTime::Config.beginning_of_workday = '0:00 am'
 BusinessTime::Config.end_of_workday = '11:59 pm'
