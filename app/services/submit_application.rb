25e5406db66540522b2a7ebab709b2468bb4af35
Steve Hook <steve.hook@gmail.com>
2019-11-08 15:53:23 +0000

    Add logic to set `edit_by` in SubmitApplication service
    Not sure whether this is the right place to do this, I don't
    particularly like any of the alternatives:
    
    - Using `after_save` callbacks on the `ApplicationChoice` model
    - Using callbacks / overridden methods on the `ApplicationStateChange` class
    
    I've avoided these because I'm not keen on adding business logic to
    either.

--- app/services/submit_application.rb
+++ app/services/submit_application.rb
@@ -10,7 +10,7 @@ class SubmitApplication
     submit_application
 
     application_form.update!(support_reference: GenerateSupportRef.call,
-                             submitted_at: Time.now)
+                             submitted_at: Time.zone.now)
 
     CandidateMailer.submit_application_email(application_form).deliver_now
   end
@@ -20,6 +20,7 @@ private
   def submit_application
     ActiveRecord::Base.transaction do
       application_choices.each do |application_choice|
+        application_choice.edit_by = ApplicationDates.new(application_form).edit_by
         ApplicationStateChange.new(application_choice).submit!
       end
     end
--- spec/services/submit_application_spec.rb
+++ spec/services/submit_application_spec.rb
@@ -6,14 +6,21 @@ RSpec.describe SubmitApplication do
       application_form = create(:application_form)
       create(:application_choice, application_form: application_form, status: 'unsubmitted')
       create(:application_choice, application_form: application_form, status: 'unsubmitted')
+      application_dates = instance_double(
+        ApplicationDates,
+        edit_by: Time.zone.local(2019, 11, 11, 11, 59, 0),
+      )
+      allow(ApplicationDates).to receive(:new).and_return(application_dates)
 
       Timecop.freeze do
         SubmitApplication.new(application_form).call
 
-        expect(application_form.submitted_at.utc).to eq Time.now.utc
+        expect(application_form.submitted_at).to eq Time.zone.now
         expect(application_form.application_choices[0]).to be_awaiting_references
         expect(application_form.application_choices[1]).to be_awaiting_references
         expect(application_form.support_reference).not_to be_empty
+        expect(application_form.application_choices[0].edit_by).to eq application_dates.edit_by
+        expect(application_form.application_choices[1].edit_by).to eq application_dates.edit_by
       end
     end
   end
