3b62a81fc605f2fce1ede59463b40d9252d9fead
Tijmen Brommet <tijmen@gmail.com>
2019-11-08 12:41:12 +0000

    Use consistent naming for the Apply from Find page
    This makes the name of the Apply from Find page consistent with the
    designs and prototype.
    
    https://trello.com/c/96nQagv3

--- app/controllers/candidate_interface/apply_from_find_controller.rb
+++ app/controllers/candidate_interface/apply_from_find_controller.rb
@@ -1,5 +1,7 @@
 module CandidateInterface
-  class ApplyingController < CandidateInterfaceController
+  # The Apply from Find page is the landing page for candidates coming from the
+  # Find postgraduate teacher training (https://find-postgraduate-teacher-training.education.gov.uk/)
+  class ApplyFromFindController < CandidateInterfaceController
     skip_before_action :authenticate_candidate!
     skip_before_action :require_basic_auth_for_ui, if: -> { ENV['DISABLE_BASIC_AUTH_FOR_LANDING_PAGE'] }
 
