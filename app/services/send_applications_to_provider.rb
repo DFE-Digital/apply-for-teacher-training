84d1fb2424e25227b68f954a7d6fe95c6ec9348c
Steve Hook <steve.hook@gmail.com>
2019-11-08 21:32:43 +0000

    Start service to send applications to providers

---
+++ app/services/send_applications_to_provider.rb
@@ -0,0 +1,7 @@
+class SendApplicationsToProvider
+  def call
+    ApplicationChoice.ready_to_send_to_provider.each do |application_choice|
+      ApplicationStateChange.new(application_choice).send_to_provider
+    end
+  end
+end
---
+++ spec/services/send_applications_to_provider_spec.rb
@@ -0,0 +1,13 @@
+RSpec.describe SendApplicationsToProvider do
+  it 'sends applications that were submitted 6 working days ago with 2 references' do
+  end
+
+  it 'DOES NOT send applications that were submitted 6 working days ago with 1 reference' do
+  end
+
+  it 'DOES NOT send applications that were submitted 6 working days ago with no references' do
+  end
+
+  it 'DOES NOT send applications that were submitted 5 working days ago with 2 references' do
+  end
+end
