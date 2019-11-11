6a32c28a92e9c7058a6fc8344e03b444c3c94a84
Steve Hook <steve.hook@gmail.com>
2019-11-11 18:07:28 +0000

    Refactor query object
    also add missing transition to cucumber feature

--- app/services/get_application_choices_ready_to_send_to_provider.rb
+++ app/services/get_application_choices_ready_to_send_to_provider.rb
@@ -1,10 +1,20 @@
 class GetApplicationChoicesReadyToSendToProvider
   def self.call
-    ApplicationChoice
+    application_choices_past_edit_by(
+      application_choices_with_references_complete(
+        ApplicationChoice.where(status: :application_complete),
+      ),
+    )
+  end
+
+  def self.application_choices_past_edit_by(scope)
+    scope.where('edit_by < ?', Time.zone.now)
+  end
+
+  def self.application_choices_with_references_complete(scope)
+    scope
       .joins(application_form: :references)
       .where('feedback is not null')
-      .where('edit_by < ?', Time.zone.now)
-      .where(status: :application_complete)
       .group('application_choices.id')
       .having('count("references"."feedback") >= 2')
   end
