8b2b3e15f3c087ed1607bb5cf5d04a5c7656541c
Paul Hayes <fofr@users.noreply.github.com>
2019-11-11 16:19:46 +0000

    Handle not picking a file or wrong file type

--- app/controllers/support_interface/import_references_controller.rb
+++ app/controllers/support_interface/import_references_controller.rb
@@ -1,11 +1,10 @@
 module SupportInterface
   class ImportReferencesController < SupportInterfaceController
-    def index
-    end
+    def index; end
 
     def import
       csv_file = params[:csv_file]
-      if csv_file && csv_file.content_type == "text/csv"
+      if csv_file && csv_file.content_type == 'text/csv'
         @output = ImportReferencesFromCsv.call(csv_file: csv_file)
 
         @output.each do |result|
@@ -15,6 +14,9 @@ module SupportInterface
         end
 
         @successes, @errors = @output.partition { |o| o[:updated] }
+      else
+        @error = 'You must upload a CSV file'
+        render :index
       end
     end
   end
