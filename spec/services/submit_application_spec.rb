f1405d00349fbd27b1e662b8ae11049ec9b6fec9
Steve Hook <steve.hook@gmail.com>
2019-11-11 15:12:25 +0000

    Don't stub `ApplicationDates`

--- spec/services/submit_application_spec.rb
+++ spec/services/submit_application_spec.rb
@@ -6,21 +6,17 @@ RSpec.describe SubmitApplication do
       application_form = create(:application_form)
       create(:application_choice, application_form: application_form, status: 'unsubmitted')
       create(:application_choice, application_form: application_form, status: 'unsubmitted')
-      application_dates = instance_double(
-        ApplicationDates,
-        edit_by: Time.zone.local(2019, 11, 11, 11, 59, 0),
-      )
-      allow(ApplicationDates).to receive(:new).and_return(application_dates)
 
-      Timecop.freeze do
+      Timecop.freeze(Time.zone.local(2019, 11, 11, 15, 0, 0)) do
+        expected_edit_by = Time.zone.local(2019, 11, 18).end_of_day
         SubmitApplication.new(application_form).call
 
         expect(application_form.submitted_at).to eq Time.zone.now
         expect(application_form.application_choices[0]).to be_awaiting_references
         expect(application_form.application_choices[1]).to be_awaiting_references
         expect(application_form.support_reference).not_to be_empty
-        expect(application_form.application_choices[0].edit_by).to eq application_dates.edit_by
-        expect(application_form.application_choices[1].edit_by).to eq application_dates.edit_by
+        expect(application_form.application_choices[0].edit_by).to eq expected_edit_by
+        expect(application_form.application_choices[1].edit_by).to eq expected_edit_by
       end
     end
   end
