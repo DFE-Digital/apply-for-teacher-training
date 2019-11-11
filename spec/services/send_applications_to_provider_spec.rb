e91ce87232a09dbfaacc528e8f5b849bdc06d3dc
Steve Hook <steve.hook@gmail.com>
2019-11-11 12:14:19 +0000

    Fill in the other SendApplicationsToProvider specs

--- spec/services/send_applications_to_provider_spec.rb
+++ spec/services/send_applications_to_provider_spec.rb
@@ -1,14 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe SendApplicationsToProvider do
-  it 'sends applications that were submitted 6 working days ago with 2 references' do
+  def create_application_with_references(days_ago: 6)
     application_form = create :application_form
-    application_choice = create :application_choice, application_form: application_form, status: 'unsubmitted'
-    Timecop.travel(10.days.ago) do
+    create :application_choice, application_form: application_form, status: 'unsubmitted'
+    Timecop.travel(days_ago.business_days.ago) do
       SubmitApplication.new(application_form.reload).call
     end
-    references = create_list :reference, 2, :unsubmitted, application_form: application_form
-    references.each do |reference|
+    create_list :reference, 2, :unsubmitted, application_form: application_form
+    application_form
+  end
+
+  it 'sends an application submitted 6 working days ago with 2 references' do
+    application_form = create_application_with_references
+    application_form.references.each do |reference|
       ReceiveReference.new(
         application_form: application_form,
         referee_email: reference.email_address,
@@ -16,15 +21,36 @@ RSpec.describe SendApplicationsToProvider do
       ).save
     end
     SendApplicationsToProvider.new.call
-    expect(application_choice.reload.status).to eq 'awaiting_provider_decision'
+    expect(application_form.application_choices.first.reload.status).to eq 'awaiting_provider_decision'
   end
 
-  it 'DOES NOT send applications that were submitted 6 working days ago with 1 reference' do
+  it 'DOES NOT send an application submitted 6 working days ago with no references' do
+    application_form = create_application_with_references
+    SendApplicationsToProvider.new.call
+    expect(application_form.application_choices.first.reload.status).to eq 'awaiting_references'
   end
 
-  it 'DOES NOT send applications that were submitted 6 working days ago with no references' do
+  it 'DOES NOT send an application submitted 6 working days ago with 1 reference' do
+    application_form = create_application_with_references
+    ReceiveReference.new(
+      application_form: application_form,
+      referee_email: application_form.references.first.email_address,
+      feedback: 'seems ok',
+    ).save
+    SendApplicationsToProvider.new.call
+    expect(application_form.application_choices.first.reload.status).to eq 'awaiting_references'
   end
 
-  it 'DOES NOT send applications that were submitted 5 working days ago with 2 references' do
+  it 'DOES NOT send an applications submitted 5 working days ago with 2 references' do
+    application_form = create_application_with_references(days_ago: 5)
+    application_form.references.each do |reference|
+      ReceiveReference.new(
+        application_form: application_form,
+        referee_email: reference.email_address,
+        feedback: 'seems ok',
+      ).save
+    end
+    SendApplicationsToProvider.new.call
+    expect(application_form.application_choices.first.reload.status).to eq 'application_complete'
   end
 end
