99c4ec55f31218dd85002a333970148837178ac8
Wen Ting Wang <chubberlisk@outlook.com>
2019-11-11 11:52:03 +0000

    Add action_path handling to SummaryCardComponent
    This allows actions to not just be a "Change" action.

--- spec/components/summary_card_component_spec.rb
+++ spec/components/summary_card_component_spec.rb
@@ -27,4 +27,19 @@ RSpec.describe SummaryCardComponent do
 
     expect(result.css('.govuk-summary-list__value').to_html).to include('Whoa Drive<br>Wewvile<br>London')
   end
+
+  it 'renders component with correct struture using action_path' do
+    rows = [
+      key: 'Please enter the sound a cat makes',
+      value: 'Meow',
+      action: 'Enter cat sounds',
+      action_path: '/cat/sounds',
+    ]
+    result = render_inline(SummaryCardComponent, rows: rows)
+
+    expect(result.css('.govuk-summary-list__key').text).to include('Please enter the sound a cat makes')
+    expect(result.css('.govuk-summary-list__value').text).to include('Meow')
+    expect(result.css('.govuk-summary-list__actions a').attr('href').value).to include('/cat/sounds')
+    expect(result.css('.govuk-summary-list__actions').text).to include('Enter cat sounds')
+  end
 end
