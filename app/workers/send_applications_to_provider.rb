47376be1b7f32ae6f72bf878e63985a41ef1e864
Steve Hook <steve.hook@gmail.com>
2019-11-12 12:45:57 +0000

    Integrate SendApplicationsToProvider with Sidekiq & Clockwork

--- app/workers/send_applications_to_provider.rb
+++ app/workers/send_applications_to_provider.rb
@@ -1,6 +1,8 @@
-# This service class will be called as a background job, scheduled to run nightly
+# This worker will be scheduled to run nightly
 class SendApplicationsToProvider
-  def call
+  include Sidekiq::Worker
+
+  def perform(*)
     GetApplicationChoicesReadyToSendToProvider.call.each do |application_choice|
       ApplicationStateChange.new(application_choice).send_to_provider!
     end
--- config/clock.rb
+++ config/clock.rb
@@ -7,4 +7,5 @@ class Clock
   include Clockwork
 
   every(5.minutes, 'ClockworkCheck') { ClockworkCheck.perform_async }
+  every(1.day, 'SendApplicationsToProvider', at: '01:00') { SendApplicationsToProvider.perform_async }
 end
