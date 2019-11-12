60d7572c7f051ef5bcfa9d59ef12b6a562f1e773
Duncan Brown <duncanjbrown@gmail.com>
2019-11-12 15:07:58 +0000

    Move current_provider_user into ProviderInterfaceController
    Authentication flow will be common to all these controllers, so group it

--- app/controllers/provider_interface/application_choices_controller.rb
+++ app/controllers/provider_interface/application_choices_controller.rb
@@ -1,7 +1,7 @@
 module ProviderInterface
   class ApplicationChoicesController < ProviderInterfaceController
     def index
-      application_choices = GetApplicationChoicesForProvider.call(provider: current_user.provider)
+      application_choices = GetApplicationChoicesForProvider.call(provider: current_provider_user.provider)
         .order(updated_at: :desc)
 
       @application_choices = application_choices.map do |application_choice|
@@ -10,20 +10,10 @@ module ProviderInterface
     end
 
     def show
-      application_choice = GetApplicationChoicesForProvider.call(provider: current_user.provider)
+      application_choice = GetApplicationChoicesForProvider.call(provider: current_provider_user.provider)
         .find(params[:application_choice_id])
 
       @application_choice = ApplicationChoicePresenter.new(application_choice)
-    end
-
-  private
-
-    # Stub out the current user and their organisation. Will be replaced
-    # by a proper ProviderUser when implementing Signin.
-    def current_user
-      fake_user_class = Struct.new(:provider)
-      fake_provider = Provider.find_by(code: 'ABC')
-      fake_user_class.new(fake_provider)
     end
   end
 end
--- app/controllers/provider_interface/provider_interface_controller.rb
+++ app/controllers/provider_interface/provider_interface_controller.rb
@@ -3,5 +3,27 @@ module ProviderInterface
     include BasicAuthHelper
     before_action :require_basic_auth_for_ui
     layout 'application'
+
+    helper_method :current_provider_user
+
+  private
+
+    # Stub out the current user and their organisation. Will be replaced
+    # by a proper ProviderUser when implementing Signin.
+    def current_provider_user
+      if session[:provider_user]
+        fake_user_class = Struct.new(:provider, :email_address)
+        fake_provider = Provider.find_by(code: 'ABC')
+
+        fake_user_class.new(
+          fake_provider,
+          session['provider_user']['email_address'],
+        )
+      end
+    end
+
+    def authenticate_provider_user!
+      redirect_to provider_interface_sign_in_path unless current_provider_user
+    end
   end
 end
