044f10644fbd262796539da2b8ccf9224f80ae5f
Wen Ting Wang <chubberlisk@outlook.com>
2019-11-12 12:01:57 +0000

    Add ability to say no to having volunteering or school experience

---
+++ app/controllers/candidate_interface/volunteering/experience_controller.rb
@@ -0,0 +1,31 @@
+module CandidateInterface
+  class Volunteering::ExperienceController < CandidateInterfaceController
+    def show
+      @volunteering_experience_form = VolunteeringExperienceForm.new
+    end
+
+    def submit
+      @volunteering_experience_form = VolunteeringExperienceForm.new(volunteering_experience_form_params)
+
+      if @volunteering_experience_form.valid?
+        if @volunteering_experience_form.experience == 'no'
+          redirect_to candidate_interface_review_volunteering_experience_path
+        else
+          render :show
+        end
+      else
+        render :show
+      end
+    end
+
+  private
+
+    def volunteering_experience_form_params
+      return nil unless params.has_key?(:candidate_interface_volunteering_experience_form)
+
+      params.require(:candidate_interface_volunteering_experience_form).permit(
+        :experience,
+      )
+    end
+  end
+end
