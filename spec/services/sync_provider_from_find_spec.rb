7c1736234d2b213b08d454083006f492cea6d6ce
Robert McHugh <robert@madetech.com>
2019-11-13 12:02:22 +0000

    Refactor find_api_helper to move accrediting provider functionality into it's own stub
    Moved the stubbing with accrediting provider into its own stub to simplify tests
    and allow the testing of when the course accrediting provider is nil.

--- spec/services/sync_provider_from_find_spec.rb
+++ spec/services/sync_provider_from_find_spec.rb
@@ -4,18 +4,15 @@ RSpec.describe SyncProviderFromFind do
   include FindAPIHelper
 
   describe 'ingesting provider, courses, sites and course_options for a provider_code' do
-    before do
+
+    it 'correctly creates all the entities' do
       stub_find_api_provider_200(
         provider_code: 'ABC',
         course_code: '9CBA',
         site_code: 'G',
         findable: true,
-        accrediting_provider_code: 'DEF',
-        accrediting_provider_name: 'Test Accrediting Provider',
       )
-    end
 
-    it 'correctly creates all the entities' do
       SyncProviderFromFind.call(provider_code: 'ABC')
 
       course_option = CourseOption.last
@@ -24,8 +21,23 @@ RSpec.describe SyncProviderFromFind do
       expect(course_option.course.code).to eq '9CBA'
       expect(course_option.course.exposed_in_find).to be true
       expect(course_option.site.code).to eq 'G'
-      expect(course_option.course.accrediting_provider.code).to eq 'DEF'
-      expect(course_option.course.accrediting_provider.name).to eq 'Test Accrediting Provider'
+    end
+
+    it 'correctly handles accrediting providers' do
+      stub_find_api_provider_200_with_accrediting_provider(
+        provider_code: 'ABC',
+        course_code: '9CBA',
+        site_code: 'G',
+        accrediting_provider_code: 'DEF',
+        accrediting_provider_name: 'Test Accrediting Provider',
+      )
+
+        SyncProviderFromFind.call(provider_code: 'ABC')
+
+        course_option = CourseOption.last
+
+        expect(course_option.course.accrediting_provider.code).to eq 'DEF'
+        expect(course_option.course.accrediting_provider.name).to eq 'Test Accrediting Provider'
     end
   end
 end
