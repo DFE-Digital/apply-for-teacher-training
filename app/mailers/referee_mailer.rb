756f1ab3127a4dda1f6dbda25c286cce9b516b38
Paul Hayes <fofr@users.noreply.github.com>
2019-11-13 09:40:39 +0000

    Personalise referee email
    - Include candidate name
    - Generate a link to a prefilled Google form
    - Missing referee name as not in the model yet

--- app/mailers/referee_mailer.rb
+++ app/mailers/referee_mailer.rb
@@ -1,10 +1,21 @@
 class RefereeMailer < ApplicationMailer
   def reference_request_email(application_form, reference)
-    @application_form = application_form
     @reference = reference
+    @candidate_name = "#{application_form.first_name} #{application_form.last_name}"
+    @google_form_url = google_form_url_for(@candidate_name, @reference)
 
     view_mail(GENERIC_NOTIFY_TEMPLATE,
               to: reference.email_address,
-              subject: t('reference_request.email.subject'))
+              subject: t('reference_request.email.subject', candidate_name: @candidate_name))
+  end
+
+  def google_form_url_for(candidate_name, reference)
+    t('reference_request.google_form_url') + '?' +
+      {
+        t('reference_request.email_entry') => reference.email_address,
+        t('reference_request.reference_id_entry') => reference.id,
+        t('reference_request.candidate_name_entry') => candidate_name,
+        t('reference_request.referee_name_entry') => 'TODO: Referee name',
+      }.to_query.gsub('+', '%20')
   end
 end
--- spec/mailers/referee_mailer_spec.rb
+++ spec/mailers/referee_mailer_spec.rb
@@ -6,16 +6,29 @@ RSpec.describe RefereeMailer, type: :mailer do
   describe 'Send request reference email' do
     let(:application_form) { build(:completed_application_form) }
     let(:reference) { application_form.references.first }
+    let(:candidate_name) { "#{application_form.first_name} #{application_form.last_name}" }
     let(:mail) { mailer.reference_request_email(application_form, reference) }
 
     before { mail.deliver_now }
 
     it 'sends an email with the correct subject' do
-      expect(mail.subject).to include(t('reference_request.email.subject'))
+      expect(mail.subject).to include(t('reference_request.email.subject', candidate_name: candidate_name))
     end
 
     it 'sends an email with the correct heading' do
-      expect(mail.body.encoded).to include('provide a reference for')
+      expect(mail.body.encoded).to include("provide a reference for #{candidate_name}")
+    end
+
+    it 'sends an email with a link to a prefilled Google form' do
+      body = mail.body.encoded
+      expect(body).to include(t('reference_request.google_form_url'))
+      expect(body).to include("=#{reference.id}")
+      expect(body).to include("=#{CGI.escape(reference.email_address)}")
+      # TODO: Referee name
+    end
+
+    it 'encodes spaces as %20 rather than + in the Google form parameters for correct prefilling' do
+      expect(mail.body.encoded).to include("=#{candidate_name.gsub(' ', '%20')}")
     end
   end
 end
