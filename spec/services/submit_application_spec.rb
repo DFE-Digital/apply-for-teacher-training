52b42f149d2bb4e5d583efc540e3d6a5988c2ac1
Paul Hayes <fofr@users.noreply.github.com>
2019-11-14 17:24:41 +0000

    Add tests to check that referees receive emails
    Test in system spec rather than service spec.

--- spec/services/submit_application_spec.rb
+++ spec/services/submit_application_spec.rb
@@ -16,30 +16,6 @@ RSpec.describe SubmitApplication do
       expect(application_form.support_reference).not_to be_empty
     end
 
-    context 'sending emails' do
-      let(:mailer) { double }
-
-      before { allow(mailer).to receive(:deliver_now) }
-
-      it 'sends an application submitted email' do
-        allow(CandidateMailer).to receive(:submit_application_email).and_return(mailer)
-
-        SubmitApplication.new(application_form).call
-        expect(CandidateMailer).to have_received(:submit_application_email).with(application_form)
-        expect(mailer).to have_received(:deliver_now)
-      end
-
-      it 'sends a reference request email to each referee' do
-        allow(RefereeMailer).to receive(:reference_request_email).and_return(mailer)
-
-        SubmitApplication.new(application_form).call
-        application_form.references.each do |r|
-          expect(RefereeMailer).to have_received(reference_request_email).with(application_form, r)
-          expect(mailer).to have_received(:deliver_now)
-        end
-      end
-    end
-
     it 'sets application_form.submitted_at' do
       Timecop.freeze(Time.zone.local(2019, 11, 11, 15, 0, 0)) do
         expected_edit_by = Time.zone.local(2019, 11, 18).end_of_day
