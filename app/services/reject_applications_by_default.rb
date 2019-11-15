7b69778ce6aebcc3a032f665cd2c0c95d6f962f5
Steve Hook <steve.hook@gmail.com>
2019-11-15 22:51:32 +0000

    Add sweeper service class to find and reject by default

---
+++ app/services/reject_applications_by_default.rb
@@ -0,0 +1,8 @@
+# This worker will be scheduled to run nightly
+class RejectApplicationsByDefault
+  def call
+    GetApplicationChoicesReadyToRejectByDefault.call.each do |application_choice|
+      RejectApplicationByDefault.new(application_choice: application_choice).call
+    end
+  end
+end
