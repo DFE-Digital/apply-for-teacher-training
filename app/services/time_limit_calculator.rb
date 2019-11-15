44c6ce5614c30fdef36a6b63dc844bfab53841ea
Steve Hook <steve.hook@gmail.com>
2019-11-15 15:32:19 +0000

    Some working specs - needs refactoring

--- app/services/time_limit_calculator.rb
+++ app/services/time_limit_calculator.rb
@@ -7,6 +7,27 @@ class TimeLimitCalculator
   end
 
   def call
-    0
+    time_limits_for_rule = TimeLimit.where(rule: rule)
+    time_limits_for_rule.each do |time_limit|
+      if time_limit.from_date &&
+          time_limit.to_date &&
+          effective_date <= time_limit.to_date &&
+          effective_date >= time_limit.from_date
+        return time_limit.limit
+      end
+    end
+    time_limits_for_rule.each do |time_limit|
+      if time_limit.from_date &&
+          effective_date >= time_limit.from_date
+        return time_limit.limit
+      end
+    end
+    time_limits_for_rule.each do |time_limit|
+      if time_limit.to_date &&
+          effective_date <= time_limit.to_date
+        return time_limit.limit
+      end
+    end
+    time_limits_for_rule.find { |time_limit| time_limit.from_date.nil? && time_limit.to_date.nil? }&.limit
   end
 end
--- spec/services/time_limit_calculator_spec.rb
+++ spec/services/time_limit_calculator_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe TimeLimitCalculator do
+  around do |example|
+    Timecop.freeze(Time.zone.local(2019, 6, 1)) do
+      example.run
+    end
+  end
+
   it 'returns default value with just a default time limit' do
     create :time_limit, rule: :reject_by_default, limit: 20
     calculator = TimeLimitCalculator.new(
@@ -8,5 +14,36 @@ RSpec.describe TimeLimitCalculator do
       effective_date: Time.zone.now,
     )
     expect(calculator.call).to eq 20
+  end
+
+  it 'returns value for rule with `from_date` when effective date matches rule' do
+    create :time_limit, rule: :reject_by_default, limit: 10, from_date: 10.days.ago
+    create :time_limit, rule: :reject_by_default, limit: 20
+    calculator = TimeLimitCalculator.new(
+      rule: :reject_by_default,
+      effective_date: Time.zone.now,
+    )
+    expect(calculator.call).to eq 10
+  end
+
+  it 'returns value for default rule rather than one with `from_date` when effective date does not match rule' do
+    create :time_limit, rule: :reject_by_default, limit: 10, from_date: 10.days.from_now
+    create :time_limit, rule: :reject_by_default, limit: 20
+    calculator = TimeLimitCalculator.new(
+      rule: :reject_by_default,
+      effective_date: Time.zone.now,
+    )
+    expect(calculator.call).to eq 20
+  end
+
+  it 'returns value for rule with `to_date` and `from_date` when effective date matches rule' do
+    create :time_limit, rule: :reject_by_default, limit: 5, from_date: 5.days.ago, to_date: 5.days.from_now
+    create :time_limit, rule: :reject_by_default, limit: 10, from_date: 10.days.ago
+    create :time_limit, rule: :reject_by_default, limit: 20
+    calculator = TimeLimitCalculator.new(
+      rule: :reject_by_default,
+      effective_date: Time.zone.now,
+    )
+    expect(calculator.call).to eq 5
   end
 end
