761572c2c55ba4e91fb5e8de74f48a57533a408c
Duncan Brown <duncanjbrown@gmail.com>
2019-11-18 09:40:13 +0000

    Correctly reference the DFE_SIGN_IN_ISSUER ENV var
    We changed this to _URI in a rebase, and didn't update Azure, which meant
    ENV['DFE_SIGN_IN_URI'] was nil and caused a runtime error.
    
    https://sentry.io/organizations/dfe-bat/issues/1336427884/?project=1765973&referrer=slack
    
    On reflection _URI is not an improvement anyway, as this is not the only
    useful URI provided by Sign-in (we'll need one for the DSI user API
    too). So rather than push through the change to _URI in the Azure
    pipeline, revert it to "_ISSUER" and be done.
    
    If we find ourselves creating needing more variables to configure the
    various DSI APIs then we can do some renaming later.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,7 @@ OmniAuth.config.logger = Rails.logger
 dfe_sign_in_identifier = ENV['DFE_SIGN_IN_CLIENT_ID']
 dfe_sign_in_secret = ENV['DFE_SIGN_IN_SECRET']
 dfe_sign_in_redirect_uri = URI.join(HostingEnvironment.application_url, '/auth/dfe/callback')
-dfe_sign_in_issuer_uri = ENV['DFE_SIGN_IN_URI'].present? ? URI(ENV['DFE_SIGN_IN_URI']) : nil
+dfe_sign_in_issuer_uri = ENV['DFE_SIGN_IN_ISSUER'].present? ? URI(ENV['DFE_SIGN_IN_ISSUER']) : nil
 
 options = {
   name: :dfe,
