387af903d2565f80c86a1133200518ec7d6ff42f
Paul Hayes <fofr@users.noreply.github.com>
2019-11-18 16:40:17 +0000

    Render qualifications using new components
    Create QualificationRow and QualificationsTable components
    Indicate if a grade is predicted or not
    Use friendly versions of grades for degrees

---
+++ spec/components/provider_interface/qualification_row_component_spec.rb
@@ -0,0 +1,55 @@
+require 'rails_helper'
+
+RSpec.describe ProviderInterface::QualificationRowComponent do
+  it 'renders a qualification table row' do
+    qualification = build_stubbed(
+      :application_qualification,
+      level: :degree,
+      qualification_type: 'BSc',
+      subject: 'Psychology',
+      award_year: '2018',
+      grade: :upper_second,
+    )
+
+    result = render_inline(described_class, qualification: qualification)
+
+    expect(result.text).to include('BSc Psychology')
+    expect(result.text).to include('2018')
+    expect(result.text).to include('2:1')
+  end
+
+  it 'renders a qualification with a predicted grade' do
+    qualification = build_stubbed(
+      :application_qualification,
+      level: :degree,
+      qualification_type: 'MEng',
+      subject: 'Engineering',
+      award_year: '2020',
+      grade: :first,
+      predicted_grade: true,
+    )
+
+    result = render_inline(described_class, qualification: qualification)
+
+    expect(result.text).to include('MEng Engineering')
+    expect(result.text).to include('2020')
+    expect(result.text).to include('First (predicted)')
+  end
+
+  it 'renders a qualification with a free text grade' do
+    qualification = build_stubbed(
+      :application_qualification,
+      level: :degree,
+      qualification_type: 'BSc',
+      subject: 'Chemistry',
+      award_year: '2001',
+      grade: 'I did my best',
+    )
+
+    result = render_inline(described_class, qualification: qualification)
+
+    expect(result.text).to include('BSc Chemistry')
+    expect(result.text).to include('2001')
+    expect(result.text).to include('I did my best')
+  end
+end
--- spec/system/provider_interface/see_individual_application_spec.rb
+++ spec/system/provider_interface/see_individual_application_spec.rb
@@ -11,6 +11,8 @@ RSpec.describe 'A Provider viewing an individual application' do
     when_i_visit_that_application_in_the_provider_interface
 
     then_i_should_see_the_candidates_degrees
+    and_i_should_see_the_candidates_gcses
+    and_i_should_see_the_candidates_other_qualifications
   end
 
   def given_i_am_a_provider_user_authenticated_with_dfe_sign_in
@@ -22,13 +24,9 @@ RSpec.describe 'A Provider viewing an individual application' do
     course_option = course_option_for_provider_code(provider_code: 'ABC')
     application_form = create(:application_form)
 
-    create(:application_qualification,
-           application_form: application_form,
-           level: :degree,
-           qualification_type: 'BSc',
-           subject: 'Psychology',
-           award_year: '2018',
-           grade: :upper_second)
+    create_list(:application_qualification, 1, application_form: application_form, level: :degree)
+    create_list(:application_qualification, 2, application_form: application_form, level: :gcse)
+    create_list(:application_qualification, 3, application_form: application_form, level: :other)
 
     @application_choice = create(:application_choice,
                                  status: 'awaiting_provider_decision',
@@ -41,6 +39,14 @@ RSpec.describe 'A Provider viewing an individual application' do
   end
 
   def then_i_should_see_the_candidates_degrees
-    expect(page).to have_content('BSc — Psychology')
+    expect(page).to have_selector('[data-qa="qualifications-table-degree"] tbody tr', count: 1)
+  end
+
+  def and_i_should_see_the_candidates_gcses
+    expect(page).to have_selector('[data-qa="qualifications-table-gcse-or-equivalent"] tbody tr', count: 2)
+  end
+
+  def and_i_should_see_the_candidates_other_qualifications
+    expect(page).to have_selector('[data-qa="qualifications-table-other-qualification"] tbody tr', count: 3)
   end
 end
