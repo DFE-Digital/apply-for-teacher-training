42a44df2c0655f11ec72bb9fdb7178b0c7b880f4
Tijmen Brommet <tijmen@gmail.com>
2019-11-18 10:51:25 +0000

    Sync data from Find every 15 minutes

--- config/clock.rb
+++ config/clock.rb
@@ -7,5 +7,6 @@ class Clock
   include Clockwork
 
   every(5.minutes, 'ClockworkCheck') { ClockworkCheck.perform_async }
+  every(15.minutes, 'SyncFromFind') { SyncFromFind.perform_async }
   every(1.hour, 'SendApplicationsToProvider', at: '**:05') { SendApplicationsToProviderWorker.perform_async }
 end
