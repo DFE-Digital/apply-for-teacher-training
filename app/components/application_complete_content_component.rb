8ee5925538e7d1eec49bee643ba0814d02ce8532
Theodor Vararu <theo@vararu.org>
2019-11-18 17:19:06 +0000

    Remove dates from ApplicationCompleteContentComponent
    These have been delegated to ApplicationDateComponent

--- app/components/application_complete_content_component.rb
+++ app/components/application_complete_content_component.rb
@@ -8,22 +8,6 @@ class ApplicationCompleteContentComponent < ActionView::Component::Base
     @dates = ApplicationDates.new(@application_form)
   end
 
-  def submitted_at_date
-    @dates.submitted_at.strftime('%e %B %Y')
-  end
-
-  def respond_by_date
-    @dates.respond_by.strftime('%e %B %Y')
-  end
-
-  def edit_by_date
-    @dates.edit_by.strftime('%e %B %Y')
-  end
-
-  def formatted_days_remaining
-    pluralize(@dates.days_remaining_to_edit, 'day')
-  end
-
 private
 
   attr_reader :application_form
