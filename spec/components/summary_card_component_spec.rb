f9c5ba8c0742536b0273013f6b267c7b70c4667c
Paul Hayes <fofr@users.noreply.github.com>
2019-11-19 11:59:28 +0000

    Add test for rendering content in summary card

--- spec/components/summary_card_component_spec.rb
+++ spec/components/summary_card_component_spec.rb
@@ -1,14 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe SummaryCardComponent do
-  it 'renders a summary list component for rows' do
-    rows = [
-      key: 'Name:',
-      value: 'Lando Calrissian'
+  let(:rows) do
+    [
+      key: 'Character',
+      value: 'Lando Calrissian',
     ]
-    result = render_inline(SummaryCardComponent, rows: rows)
+  end
 
-    expect(result.css).to include('.govuk-summary-list')
+  it 'renders a summary list component for rows' do
+    result = render_inline(SummaryCardComponent, rows: rows)
     expect(result.css('.govuk-summary-list__value').text).to include('Lando Calrissian')
+    expect(result.css('.govuk-summary-list__key').text).to include('Character')
+  end
+
+  it 'renders content at the top of a summary card' do
+    result = render_inline(SummaryCardComponent, rows: rows) { 'In a galaxy' }
+    expect(result.text).to include('In a galaxy')
   end
 end
