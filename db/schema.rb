6ab72fa258821f3320f8909caca97bf0f2fb489e
alexdesi <alexdesi@gmail.com>
2019-11-20 16:24:48 +0000

    Fix display for empty missing explanation field.

--- db/schema.rb
+++ db/schema.rb
@@ -101,8 +101,8 @@ ActiveRecord::Schema.define(version: 2019_11_18_161155) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "other_uk_qualification_type", limit: 100
-    t.index ["application_form_id"], name: "index_application_qualifications_on_application_form_id"
     t.text "missing_explanation"
+    t.index ["application_form_id"], name: "index_application_qualifications_on_application_form_id"
   end
 
   create_table "audits", force: :cascade do |t|
