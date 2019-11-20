9da9337d1f07b66ceb7fade7d0ff9203b6cea371
alexdesi <alexdesi@gmail.com>
2019-11-20 14:41:50 +0000

    Make use of locales for gcse labels

--- spec/system/candidate_interface/candidate_entering_gcse_with_missing_qualification_spec.rb
+++ spec/system/candidate_interface/candidate_entering_gcse_with_missing_qualification_spec.rb
@@ -30,7 +30,7 @@ RSpec.feature 'Candidate entering GCSE details' do
   end
 
   def and_i_enter_the_missing_explanation
-    fill_in 'If you are already on your way to gaining this qualification, give us details.',
+    fill_in t('gcse_edit_type.label.missing_explanation'),
             with: "I'm expecting to complete my Biology course on next July"
   end
 
