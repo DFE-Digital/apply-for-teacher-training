587c049399db9a2aecc4c662d49baf460df47490
Steve Hook <steve.hook@gmail.com>
2019-11-20 16:18:47 +0000

    Rename variable following feedback

--- app/services/check_breaks_in_work_history.rb
+++ app/services/check_breaks_in_work_history.rb
@@ -3,22 +3,18 @@ class CheckBreaksInWorkHistory
     def call(application_form)
       jobs = application_form.application_work_experiences.sort_by(&:start_date)
 
-      previous_end_date = nil
+      latest_end_date = nil
       jobs.each do |job|
-        return true if month_or_more_break_between_dates?(previous_end_date, job.start_date)
+        return true if month_or_more_break_between_dates?(latest_end_date, job.start_date)
 
         return false if less_than_month_to_current_date?(job.end_date)
 
-        previous_end_date = [previous_end_date, job.end_date].compact.max
+        latest_end_date = [latest_end_date, job.end_date].compact.max
       end
-      month_or_more_break_between_end_date_and_current_date?(previous_end_date)
+      month_or_more_break_between_end_date_and_current_date?(latest_end_date)
     end
 
   private
-
-    def current_role?(job)
-      job.end_date.nil?
-    end
 
     def month_or_more_break_between_end_date_and_current_date?(end_date)
       month_or_more_break_between_dates?(end_date, Time.zone.now)
