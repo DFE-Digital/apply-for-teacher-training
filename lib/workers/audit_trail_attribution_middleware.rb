4e4cee4c9f98bb5f3f4ed9cca653389864e9357f
Steve Hook <steve.hook@gmail.com>
2019-11-20 12:05:12 +0000

    Change label for background processing 'user'

--- lib/workers/audit_trail_attribution_middleware.rb
+++ lib/workers/audit_trail_attribution_middleware.rb
@@ -2,7 +2,7 @@ module Workers
   class AuditTrailAttributionMiddleware
     def initialize(_options = nil); end
 
-    AUDIT_USER_NAME = '(GOV.UK Apply User)'.freeze
+    AUDIT_USER_NAME = '(Automated process)'.freeze
 
     def call(_worker, _msg, _queue)
       Audited.audit_class.as_user(AUDIT_USER_NAME) do
