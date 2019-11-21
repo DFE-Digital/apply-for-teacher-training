220f19d14ab0c391adb1c8cead7d2e995018b996
Steve Hook <steve.hook@gmail.com>
2019-11-21 17:05:12 +0000

    Configure a list of the PII params to mask in Rails log

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,22 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password]
+SANITIZED_REQUEST_PARAMS = %i[
+  password
+  email
+  first_name
+  last_name
+  full_name
+  email_address
+  address_line1
+  address_line2
+  address_line3
+  address_line4
+  country
+  postcode
+  date_of_birth
+  phone_number
+  magic_link_token
+  date_of_birth
+].freeze
+Rails.application.config.filter_parameters += SANITIZED_REQUEST_PARAMS
