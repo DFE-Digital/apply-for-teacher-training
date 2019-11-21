fe8e43539f9d0a7e93f46e6091ce28cabc3dca98
Tijmen Brommet <tijmen@gmail.com>
2019-11-21 16:10:25 +0000

    See which courses are available on DFE Apply before starting
    This adds a page where candidates can see what courses are available on
    DfE Apply.

--- app/models/course.rb
+++ app/models/course.rb
@@ -7,6 +7,8 @@ class Course < ApplicationRecord
   validates :level, presence: true
   validates :code, uniqueness: { scope: :provider_id }
 
+  scope :visible_to_candidates, -> { where(exposed_in_find: true, open_on_apply: true) }
+
   CODE_LENGTH = 4
 
   # This enum is copied verbatim from Find to maintain consistency
