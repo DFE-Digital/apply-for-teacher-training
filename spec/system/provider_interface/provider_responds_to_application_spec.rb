8616aad28b4c3a1692926b3b8129e1b19c92561b
Duncan Brown <duncanjbrown@gmail.com>
2019-11-21 17:12:33 +0000

    Trim down some provider_interface system specs
    see_applications_spec asserted on the presence of fields in the application_form
    brought along with each application_choice. Whilst it's nice to be able
    to make string-against-string assertions this makes the factories more
    complicated and the test harder to work with (discovered while trying to
    work with it - see next commit)
    
    provider_responds_to_application_spec copy-pasted the factories from
    see_see_applications_spec, but didn't assert on the name anyway. So
    those creates() can be winnowed out too.
    
    While we're here, we prefer to have one scenario per system spec file.
    In this file we had three, so improve it by combining two specs making
    assertions against the same view state.

--- spec/system/provider_interface/provider_responds_to_application_spec.rb
+++ spec/system/provider_interface/provider_responds_to_application_spec.rb
@@ -5,18 +5,22 @@ RSpec.feature 'Provider responds to application' do
   include DfESignInHelpers
 
   let(:course_option) { course_option_for_provider_code(provider_code: 'ABC') }
-  let(:application_awaiting_provider_decision) {
-    create(:application_choice, status: 'awaiting_provider_decision', course_option: course_option, application_form: create(:completed_application_form, first_name: 'Alice', last_name: 'Wunder'))
-  }
-  let(:application_rejected) {
-    create(:application_choice, status: 'rejected', course_option: course_option, application_form: create(:completed_application_form, first_name: 'Alice', last_name: 'Wunder'))
-  }
 
-  scenario 'Provider can respond to application currently awaiting_provider_decision' do
+  let(:application_awaiting_provider_decision) do
+    create(:application_choice, status: 'awaiting_provider_decision', course_option: course_option)
+  end
+
+  let(:application_rejected) { create(:application_choice, status: 'rejected', course_option: course_option) }
+
+  scenario 'Provider can respond to an application currently awaiting_provider_decision' do
     given_i_am_a_provider_user_authenticated_with_dfe_sign_in
     when_i_visit_a_application_with_status_awaiting_provider_decision
     then_i_can_see_its_status application_awaiting_provider_decision
     and_i_can_respond_to_the_application
+
+    when_i_click_to_respond_to_the_application
+    then_i_am_given_the_option_to_make_an_offer
+    and_i_am_given_the_option_to_reject_the_application
   end
 
   scenario 'Provider cannot respond to application currently rejected' do
@@ -24,14 +28,6 @@ RSpec.feature 'Provider responds to application' do
     when_i_visit_a_application_with_status_rejected
     then_i_can_see_its_status application_rejected
     and_i_cannot_respond_to_the_application
-  end
-
-  scenario 'Provider is given two options when responding to an application' do
-    given_i_am_a_provider_user_authenticated_with_dfe_sign_in
-    when_i_visit_a_application_with_status_awaiting_provider_decision
-    and_i_click_to_respond_to_the_application
-    then_i_am_given_the_option_to_make_an_offer
-    and_i_am_given_the_option_to_reject_the_application
   end
 
   def given_i_am_a_provider_user_authenticated_with_dfe_sign_in
@@ -67,7 +63,7 @@ RSpec.feature 'Provider responds to application' do
     expect(page).not_to have_content 'Respond to application'
   end
 
-  def and_i_click_to_respond_to_the_application
+  def when_i_click_to_respond_to_the_application
     click_on 'Respond to application'
   end
 
--- spec/system/provider_interface/see_applications_spec.rb
+++ spec/system/provider_interface/see_applications_spec.rb
@@ -33,9 +33,9 @@ RSpec.feature 'See applications' do
     course_option = course_option_for_provider_code(provider_code: 'ABC')
     other_course_option = course_option_for_provider_code(provider_code: 'ANOTHER_ORG')
 
-    create(:application_choice, status: 'awaiting_provider_decision', course_option: course_option, application_form: create(:completed_application_form, first_name: 'Alice', last_name: 'Wunder'))
-    create(:application_choice, status: 'awaiting_provider_decision', course_option: course_option, application_form: create(:completed_application_form, first_name: 'Bob'))
-    create(:application_choice, status: 'awaiting_provider_decision', course_option: other_course_option, application_form: create(:completed_application_form, first_name: 'Charlie'))
+    @my_provider_choice1  = create(:application_choice, status: 'awaiting_provider_decision', course_option: course_option)
+    @my_provider_choice2  = create(:application_choice, status: 'awaiting_provider_decision', course_option: course_option)
+    @other_provider_choice = create(:application_choice, status: 'awaiting_provider_decision', course_option: other_course_option)
   end
 
   def when_i_visit_the_provider_page
@@ -43,20 +43,20 @@ RSpec.feature 'See applications' do
   end
 
   def then_i_should_see_the_applications_from_my_organisation
-    expect(page).to have_content 'Alice'
-    expect(page).to have_content 'Bob'
+    expect(page).to have_content @my_provider_choice1.application_form.first_name
+    expect(page).to have_content @my_provider_choice2.application_form.first_name
   end
 
   def but_not_the_applications_from_other_providers
-    expect(page).not_to have_content 'Charlie'
+    expect(page).not_to have_content @other_provider_choice.application_form.first_name
   end
 
   def when_i_click_on_an_application
-    click_on 'Alice'
+    click_on @my_provider_choice1.application_form.first_name
   end
 
   def then_i_should_be_on_the_application_view_page
     expect(page).to have_content 'Application for'
-    expect(page).to have_content 'Alice Wunder'
+    expect(page).to have_content @my_provider_choice1.application_form.first_name
   end
 end
