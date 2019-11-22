cc21d55d50772e38513c61e755fa2a9c2444610b
Steve Hook <steve.hook@gmail.com>
2019-11-22 09:11:43 +0000

    Remove duplicate and sort sanitized params

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -2,21 +2,20 @@
 
 # Configure sensitive parameters which will be filtered from the log file.
 SANITIZED_REQUEST_PARAMS = %i[
-  password
-  email
-  first_name
-  last_name
-  full_name
-  email_address
   address_line1
   address_line2
   address_line3
   address_line4
   country
-  postcode
   date_of_birth
-  phone_number
+  email
+  email_address
+  first_name
+  full_name
+  last_name
   magic_link_token
-  date_of_birth
+  password
+  phone_number
+  postcode
 ].freeze
 Rails.application.config.filter_parameters += SANITIZED_REQUEST_PARAMS
