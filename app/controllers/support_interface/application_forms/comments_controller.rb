8897b1f58665d7e1130b3bcd0c5cec2a59ede9cf
Steve Hook <steve.hook@gmail.com>
2019-11-24 23:58:56 +0000

    Make new routes a bit more RESTful

---
+++ app/controllers/support_interface/application_forms/comments_controller.rb
@@ -0,0 +1,11 @@
+module SupportInterface
+  module ApplicationForms
+    class CommentsController < SupportInterfaceController
+      def new
+      end
+
+      def create
+      end
+    end
+  end
+end
