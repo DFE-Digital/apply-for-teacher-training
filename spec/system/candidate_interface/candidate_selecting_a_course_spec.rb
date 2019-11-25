d1db8affee6e2bad23b724fc855a389a5b4b515b
Tijmen Brommet <tijmen@gmail.com>
2019-11-25 08:25:35 +0000

    Merge candidate course choice acceptance test

--- spec/system/candidate_interface/candidate_selecting_a_course_spec.rb
+++ spec/system/candidate_interface/candidate_selecting_a_course_spec.rb
@@ -5,10 +5,10 @@ RSpec.feature 'Selecting a course' do
 
   scenario 'Candidate selects a course choice' do
     given_i_am_signed_in
-    and_i_visit_the_site
+    and_data_from_find_exists
 
-    given_data_from_find_exists
-    when_i_click_on_course_choices
+    when_i_visit_the_site
+    and_i_click_on_course_choices
     and_i_click_on_add_course
     and_i_choose_that_i_know_where_i_want_to_apply
     and_i_choose_a_provider
@@ -17,63 +17,44 @@ RSpec.feature 'Selecting a course' do
     then_i_see_my_completed_course_choice
 
     # attempt to add the same course
-    and_i_click_on_add_another_course
+    when_i_click_on_add_another_course
     and_i_choose_that_i_know_where_i_want_to_apply
     and_i_choose_a_provider
     and_i_choose_a_course
     and_i_choose_a_location
-    then_i_see_an_error_message
+    then_i_see_a_message_that_ive_already_chosen_the_course
 
     when_i_mark_this_section_as_completed
     when_i_click_continue
-    and_that_the_section_is_completed
-  end
+    then_i_see_that_the_section_is_completed
 
-  scenario 'Candidate deletes a course choice' do
-    given_i_am_signed_in
-    and_i_visit_the_site
-
-    given_data_from_find_exists
-    when_i_click_on_course_choices
-    and_i_click_on_add_course
-    and_i_choose_that_i_know_where_i_want_to_apply
-    and_i_choose_a_provider
-    and_i_choose_a_course
-    and_i_choose_a_location
-    then_i_see_my_completed_course_choice
-
-    when_i_delete_my_course_choice
-    and_i_confirm
+    and_i_click_on_course_choices
+    and_i_delete_my_course_choice
+    and_i_confirm_that_i_want_to_delete_my_choice
     then_i_no_longer_see_my_course_choice
   end
-
-  def given_i_am_not_signed_in; end
 
   def given_i_am_signed_in
     create_and_sign_in_candidate
   end
 
-  def and_i_visit_the_site
-    visit candidate_interface_application_form_path
-  end
-
-  def given_data_from_find_exists
+  def and_data_from_find_exists
     provider = create(:provider, name: 'Gorse SCITT', code: '1N1')
     site = create(:site, name: 'Main site', code: '-', provider: provider)
     course = create(:course, name: 'Primary', code: '2XT2', provider: provider, exposed_in_find: true)
     create(:course_option, site: site, course: course, vacancy_status: 'B')
   end
 
-  def when_i_click_on_course_choices
+  def when_i_visit_the_site
+    visit candidate_interface_application_form_path
+  end
+
+  def and_i_click_on_course_choices
     click_link 'Course choices'
   end
 
   def and_i_click_on_add_course
     click_link 'Continue'
-  end
-
-  def and_i_click_on_add_another_course
-    click_link 'Add another course'
   end
 
   def and_i_choose_that_i_know_where_i_want_to_apply
@@ -102,16 +83,12 @@ RSpec.feature 'Selecting a course' do
     expect(page).to have_content('Main site')
   end
 
-  def when_i_delete_my_course_choice
-    click_link t('application_form.courses.delete')
+  def when_i_click_on_add_another_course
+    click_link 'Add another course'
   end
 
-  def and_i_confirm
-    click_button t('application_form.courses.confirm_delete')
-  end
-
-  def then_i_no_longer_see_my_course_choice
-    expect(page).not_to have_content('Primary (2XT2)')
+  def then_i_see_a_message_that_ive_already_chosen_the_course
+    expect(page).to have_css('.govuk-error-summary', text: 'You have already selected this course')
   end
 
   def when_i_mark_this_section_as_completed
@@ -119,15 +96,23 @@ RSpec.feature 'Selecting a course' do
     check t('application_form.courses.complete.completed_checkbox')
   end
 
+  def then_i_see_that_the_section_is_completed
+    expect(page).to have_css('#course-choices-badge-id', text: 'Completed')
+  end
+
   def when_i_click_continue
     click_button 'Continue'
   end
 
-  def and_that_the_section_is_completed
-    expect(page).to have_css('#course-choices-badge-id', text: 'Completed')
+  def and_i_delete_my_course_choice
+    click_link t('application_form.courses.delete')
   end
 
-  def then_i_see_an_error_message
-    expect(page).to have_css('.govuk-error-summary', text: 'You have already selected this course')
+  def and_i_confirm_that_i_want_to_delete_my_choice
+    click_button t('application_form.courses.confirm_delete')
+  end
+
+  def then_i_no_longer_see_my_course_choice
+    expect(page).not_to have_content('Primary (2XT2)')
   end
 end
