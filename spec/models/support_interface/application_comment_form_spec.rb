b1908125ef9acbe79e983f3e0312c690fe7c4bc8
Steve Hook <steve.hook@gmail.com>
2019-11-25 15:21:07 +0000

    Add a form model for adding application comments

---
+++ spec/models/support_interface/application_comment_form_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe SupportInterface::ApplicationCommentForm, type: :model do
+  let(:form_data) do
+    {
+      comment: Faker::Lorem.paragraph_by_chars(number: 200),
+    }
+  end
+
+  describe '#save' do
+    it 'returns false if not valid' do
+      application_form = FactoryBot.create(:application_form)
+      expect(described_class.new.save(application_form)).to be false
+    end
+
+    it 'updates the provided ApplicationForm audit trail with the new comment if valid' do
+      application_form = FactoryBot.create(:application_form)
+      expect(described_class.new(form_data).save(application_form)).to be true
+      expect(application_form.reload.audits.last.comment).to eq form_data[:comment]
+    end
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:comment) }
+  end
+end
