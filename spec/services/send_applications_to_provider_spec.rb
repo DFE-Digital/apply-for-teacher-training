0932ad0137df4c32a21eeb14c2ea14e05451381e
Duncan Brown <duncanjbrown@gmail.com>
2019-11-25 12:02:36 +0000

    Refactor specs
    - remove a spare assignment of application_form in the factory
    - visually split up arrange, act and assert steps in spec bodies
    - use let to reduce ceremony inside each spec
    - remove duplicated specs (also present in GetApplicationChoicesReadyToSendToProvider)

--- spec/services/send_applications_to_provider_spec.rb
+++ spec/services/send_applications_to_provider_spec.rb
@@ -1,38 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe SendApplicationsToProvider do
-  def create_application_with_references
-    application_form = create :application_form
-    create :application_choice, application_form: application_form, status: 'unsubmitted'
-    create_list :reference, 2, :unsubmitted, application_form: application_form
-    SubmitApplication.new(application_form.reload).call
-    application_form.reload
-  end
-
-  def complete_reference(reference)
-    ReceiveReference.new(
-      application_form: reference.application_form,
-      referee_email: reference.email_address,
-      feedback: 'seems ok',
-    ).save
-  end
-
-  it 'sends an application submitted 6 working days ago with 2 references' do
-    application_form = create_application_with_references
-    application_form.references.each do |reference|
-      complete_reference(reference)
-    end
-    Timecop.travel(6.business_days.from_now) do
-      SendApplicationsToProvider.new.call
-    end
-    expect(application_form.application_choices.first.reload.status).to eq 'awaiting_provider_decision'
-  end
-
-  it 'DOES NOT send an application submitted 6 working days ago with no references' do
-    application_form = create_application_with_references
-    Timecop.travel(6.business_days.from_now) do
-      SendApplicationsToProvider.new.call
-    end
-    expect(application_form.application_choices.first.reload.status).to eq 'awaiting_references'
-  end
 end
