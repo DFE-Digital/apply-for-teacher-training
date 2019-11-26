9b5199dec2caa9a8342c1d8f975f207c492e0f2a
dankmitchell <dan@danmitchell.co.uk>
2019-11-26 11:18:25 +0000

    Add cookie banner
    Taken from BAT: PUBLISH

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -1,9 +1,13 @@
 require.context("govuk-frontend/govuk/assets");
 import { initAll as govUKFrontendInitAll } from "govuk-frontend";
 import initNationalityAutocomplete from "./nationality-autocomplete";
+import cookieMessage from "./cookie-banner";
 
 import "accessible-autocomplete/dist/accessible-autocomplete.min.css";
 import "../styles/application.scss";
 govUKFrontendInitAll();
 
 initNationalityAutocomplete();
+
+var $cookieMessage = document.querySelector('[data-module="cookie-message"]');
+new cookieMessage($cookieMessage).init();
