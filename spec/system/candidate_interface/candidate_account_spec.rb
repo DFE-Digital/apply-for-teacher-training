89a4065f6e6bdbb5c0f39c0857609bd1ad76cd7a
Robert McHugh <robert@madetech.com>
2019-11-27 13:13:17 +0000

    Update sign_in controller to use candidate validations and add system spec
    Updated sign_in contrller to use the candidate validations for email
    format and pressense.
    
    Added steps in candidate_account_spec system spec to test sign in form errors

--- spec/system/candidate_interface/candidate_account_spec.rb
+++ spec/system/candidate_interface/candidate_account_spec.rb
@@ -21,6 +21,10 @@ RSpec.feature 'Candidate account' do
     then_i_should_be_signed_out
 
     when_i_click_the_signin_link
+    and_i_submit_an_invalid_email_address
+    then_i_see_form_errors_on_the_page
+
+    when_i_click_the_signin_link
     and_i_submit_my_email_address
     then_i_receive_an_email_with_a_signin_link
     when_i_click_on_the_link_in_my_email
@@ -108,5 +112,14 @@ RSpec.feature 'Candidate account' do
     Timecop.travel(Time.now + 7.days + 1.second) do
       visit candidate_interface_application_form_path
     end
+  end
+
+  def and_i_submit_an_invalid_email_address
+    fill_in t('authentication.sign_up.email_address.label'), with: 'invalid email'
+    click_on t('authentication.sign_up.button_continue')
+  end
+
+  def then_i_see_form_errors_on_the_page
+    expect(page).to have_content 'There is a problem'
   end
 end
