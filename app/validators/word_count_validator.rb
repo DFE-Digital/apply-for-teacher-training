534c423fcc3b539e878fd5e6d4dab2000660a92e
Paul Hayes <fofr@users.noreply.github.com>
2019-11-27 09:42:54 +0000

    Put word count validation messages in locale file
    Previously we weren’t passing in a custom message, and error messages read quite generically, "Reduce the word count for X".
    
    We had some `too_long` strings in the yml but they were never being used.
    
    Switch to a custom error message, `too_many_words`, add a default message that includes the number of words and add to becoming a teacher, subject knowledge and interview preferences.

--- app/validators/word_count_validator.rb
+++ app/validators/word_count_validator.rb
@@ -5,7 +5,7 @@ class WordCountValidator < ActiveModel::EachValidator
     # This RegExp should match the implementation in govuk-frontend:
     # https://github.com/alphagov/govuk-frontend/blob/aa30ee76a5f84e230a323bb92d341285a6da3a10/src/govuk/components/character-count/character-count.js#L82
     if value.scan(/\S+/).size > options[:maximum]
-      record.errors[attribute] << (options[:message] || "Reduce the word count for #{attribute.to_s.humanize(capitalize: false)}")
+      record.errors.add(attribute, :too_many_words, count: options[:maximum])
     end
   end
 end
