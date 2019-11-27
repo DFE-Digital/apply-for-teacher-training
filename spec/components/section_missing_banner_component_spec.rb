eb07deebc697e448949afb1c3af553e3562ea014
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2019-11-27 16:51:26 +0000

    Add test for SectionMissingBannerComponent

---
+++ spec/components/section_missing_banner_component_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe SectionMissingBannerComponent do
+  it 'renders a section missing banner component' do
+    result = render_inline(SectionMissingBannerComponent, section: 'degrees', section_path: '#', text: 'Degree(s) are not marked as completed')
+    expect(result.css('.app-banner__message .govuk-body').text).to include('Degree(s) are not marked as completed')
+    expect(result.css('.app-banner__message .govuk-link')).to be_present
+  end
+end
