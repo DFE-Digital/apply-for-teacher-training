815748016828b32b51a714c5b62e4967b759cec7
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2019-11-27 16:34:24 +0000

    Refactor SectionMissingBannerComponent

--- app/components/section_missing_banner_component.rb
+++ app/components/section_missing_banner_component.rb
@@ -1,10 +1,11 @@
 class SectionMissingBannerComponent < ActionView::Component::Base
-  validates :text, :section, presence: true
+  validates :section, :section_path, :text, presence: true
 
-  def initialize(text:, section:)
-    @text = text
+  def initialize(section:, section_path:, text:)
     @section = section
+    @section_path = section_path
+    @text = text
   end
 
-  attr_reader :text, :section
+  attr_reader :section, :section_path, :text
 end
