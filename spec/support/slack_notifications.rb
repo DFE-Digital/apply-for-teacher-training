978e36b7377e37afb757db84d264c49e66d84e59
Tijmen Brommet <tijmen@gmail.com>
2019-11-28 10:18:57 +0000

    Add test helpers for Slack notifications
    These helpers make it easier to make sure we're sending the correct
    Slack messages in system tests.

---
+++ spec/support/slack_notifications.rb
@@ -0,0 +1,12 @@
+RSpec.configure do |config|
+  config.before do
+    stub_request(:post, "https://example.com/slack-webhook").
+      to_return(status: 200, body: "{}")
+  end
+
+  def expect_slack_message_with_text(text)
+    expect(WebMock).to have_requested(:post, "https://example.com/slack-webhook").with { |req|
+      JSON.parse(req.body)['text'].match(text)
+    }
+  end
+end
