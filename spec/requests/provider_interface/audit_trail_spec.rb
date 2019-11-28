98c51f1cbc4a8935822cf7b590c7e1f2b7fb6110
Steve Hook <steve.hook@gmail.com>
2019-11-28 09:28:34 +0000

    Simplify data setup in spec

--- spec/requests/provider_interface/audit_trail_spec.rb
+++ spec/requests/provider_interface/audit_trail_spec.rb
@@ -2,31 +2,28 @@ require 'rails_helper'
 
 RSpec.describe 'Provider interface - audit trail', type: :request do
   def create_application
-    application_form = create :application_form
-    application_choice = create(
+    create(
       :application_choice,
-      application_form: application_form,
-      status: 'awaiting_provider_decision',
+      :awaiting_provider_decision,
     )
-    provider = create :provider, code: 'ABC'
-    application_choice.course.update(accrediting_provider: provider)
-    application_choice
   end
 
-  before do
+  def set_provider_permission(application_choice)
+    provider_code = application_choice.course.provider.code
     allow(ProviderUser).to receive(:load_from_session)
       .and_return(
         ProviderUser.new(
           email_address: 'alice@example.com',
-          dfe_sign_in_uid: 'ABC',
+          dfe_sign_in_uid: provider_code,
         ),
     )
 
-    allow(Rails.application.config).to receive(:provider_permissions).and_return(ABC: 'ABC')
+    allow(Rails.application.config).to receive(:provider_permissions).and_return(provider_code => provider_code)
   end
 
   it 'creates audit records attributed to the authenticated provider' do
     application_choice = create_application
+    set_provider_permission(application_choice)
     expect {
       post provider_interface_application_choice_create_offer_path(
         application_choice_id: application_choice.id,
