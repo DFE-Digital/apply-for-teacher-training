9a34a17f6e4a04c8fe5ca73a67f5982c6a1618d2
Michael Nacos <m.nacos@gmail.com>
2019-12-02 12:56:06 +0000

    use redis db 1 for dev, redis db 9 for tests to protect devs from weirdness

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,15 @@
 require 'workers/audit_trail_attribution_middleware'
 
+redis_url = ENV.fetch('REDIS_URL')
+redis_url = redis_url.gsub(/\/\d+$/, '/9') if Rails.env.test? # hardcode db to isolate test data
+
 Sidekiq.configure_server do |config|
+  config.redis = { url: redis_url }
   config.server_middleware do |chain|
     chain.add Workers::AuditTrailAttributionMiddleware
   end
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: redis_url }
 end
