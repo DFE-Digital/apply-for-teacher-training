c703a2f2ff81f5a0962ddcfe31d7893f8dd915ad
Steve Hook <steve.hook@gmail.com>
2019-12-02 11:11:15 +0000

    No longer need specs for support basic auth

--- spec/system/basic_auth_spec.rb
+++ spec/system/basic_auth_spec.rb
@@ -36,30 +36,4 @@ RSpec.describe 'Require basic authentication', type: :request do
       expect(response).to have_http_status(200)
     end
   end
-
-  context 'support_interface' do
-    it 'requests without basic auth get 401' do
-      ClimateControl.modify SUPPORT_USERNAME: 'foo', SUPPORT_PASSWORD: 'bar' do
-        get support_interface_applications_path
-      end
-
-      expect(response).to have_http_status(401)
-    end
-
-    it 'requests with invalid basic auth get 401' do
-      ClimateControl.modify SUPPORT_USERNAME: 'foo', SUPPORT_PASSWORD: 'bar' do
-        get support_interface_applications_path, headers: basic_auth_headers('wrong', 'auth')
-      end
-
-      expect(response).to have_http_status(401)
-    end
-
-    it 'requests with valid basic auth get 200' do
-      ClimateControl.modify SUPPORT_USERNAME: 'foo', SUPPORT_PASSWORD: 'bar' do
-        get support_interface_applications_path, headers: basic_auth_headers('foo', 'bar')
-      end
-
-      expect(response).to have_http_status(200)
-    end
-  end
 end
