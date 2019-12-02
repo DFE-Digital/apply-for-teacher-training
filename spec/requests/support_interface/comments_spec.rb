0adca8d2bd2ab0e124e467fff32801298e2a3e7d
Steve Hook <steve.hook@gmail.com>
2019-12-02 16:56:16 +0000

    Fix further spec regressions

--- spec/requests/support_interface/comments_spec.rb
+++ spec/requests/support_interface/comments_spec.rb
@@ -5,14 +5,18 @@ RSpec.describe 'Support interface - Application Comments', type: :request, with_
     create :application_form
   end
 
-  def auth_header
-    {
-      'HTTP_AUTHORIZATION' =>
-        ActionController::HttpAuthentication::Basic.encode_credentials(
-          ENV.fetch('SUPPORT_USERNAME'),
-          ENV.fetch('SUPPORT_PASSWORD'),
+  def set_provider_permission
+    allow(SupportUser).to receive(:load_from_session)
+      .and_return(
+        SupportUser.new(
+          email_address: 'alice@example.com',
+          dfe_sign_in_uid: 'ABC',
         ),
-    }
+    )
+  end
+
+  before do
+    set_provider_permission
   end
 
   it 'creates application comments in the audit trail' do
@@ -22,7 +26,6 @@ RSpec.describe 'Support interface - Application Comments', type: :request, with_
       post(
         support_interface_application_form_comments_path(application_form_id: application_form.id),
         params: { support_interface_application_comment_form: { comment: 'foo' } },
-        headers: auth_header,
       )
     }.to(change { application_form.reload.audits.count }.by(1))
   end
@@ -34,7 +37,6 @@ RSpec.describe 'Support interface - Application Comments', type: :request, with_
       post(
         support_interface_application_form_comments_path(application_form_id: application_form.id),
         params: { support_interface_application_comment_form: { comment: '' } },
-        headers: auth_header,
       )
     }.not_to(change { application_form.reload.audits.count })
   end
