6be9387b5ccadadefc816ad659ce77b619e4ba6c
Michael Nacos <m.nacos@gmail.com>
2019-12-03 10:40:55 +0000

    add REDIS_URL to .env.test

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,15 +1,7 @@
 require 'workers/audit_trail_attribution_middleware'
 
-redis_url = ENV.fetch('REDIS_URL') rescue 'redis://localhost:6379/1'
-redis_url = redis_url.gsub(/\/\d+$/, '/9') if Rails.env.test? # hardcode db to isolate test data
-
 Sidekiq.configure_server do |config|
-  config.redis = { url: redis_url }
   config.server_middleware do |chain|
     chain.add Workers::AuditTrailAttributionMiddleware
   end
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = { url: redis_url }
 end
