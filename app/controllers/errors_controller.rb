07cb1ef7449d7962a4f5450cabeabf47c113918c
Theodor Vararu <theo@vararu.org>
2019-12-04 17:10:59 +0000

    Add support for error 422

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -3,6 +3,10 @@ class ErrorsController < ApplicationController
     render 'not_found.html', status: :not_found
   end
 
+  def unprocessable_entity
+    render 'unprocessable_entity.html', status: :unprocessable_entity
+  end
+
   def internal_server_error
     render status: :internal_server_error
   end
