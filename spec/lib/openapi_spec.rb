ec286924bcc992bcd0799135e57a3f1a4c309491
Tijmen Brommet <tijmen@gmail.com>
2019-12-09 10:31:10 +0000

    Add missing examples and description

---
+++ spec/lib/openapi_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe 'OpenAPI spec' do
+  document = Openapi3Parser.load_file('config/vendor-api-0.8.0.yml')
+
+  it 'is a valid OpenAPI spec' do
+    expect(document).to be_valid, document.errors.to_a.inspect
+  end
+
+  document.components.schemas.each do |schema_name, schema|
+    describe schema_name do
+      it 'requires all of the keys to be present' do
+        required_properties = schema.required.to_a
+        actual_properties = schema.properties.map { |property_name, _| property_name }
+
+        expect(required_properties).to match_array(actual_properties)
+      end
+
+      schema.properties.each do |property_name, property|
+        next unless property.type.in?(%w[string boolean])
+
+        it "requires description for #{schema_name}.#{property_name}" do
+          expect(property.description).not_to be_nil
+        end
+
+        it "requires example for #{schema_name}.#{property_name}" do
+          expect(property.example).not_to be_nil
+        end
+      end
+    end
+  end
+end
