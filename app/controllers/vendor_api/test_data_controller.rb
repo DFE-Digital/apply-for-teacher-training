dd0eb2484d8f62bae33abbe68cb753d798612cb6
Steve Hook <steve.hook@gmail.com>
2019-12-09 10:13:28 +0000

    Ensure that test-data/regenerate defaults to 100

--- app/controllers/vendor_api/test_data_controller.rb
+++ app/controllers/vendor_api/test_data_controller.rb
@@ -2,8 +2,11 @@ module VendorApi
   class TestDataController < VendorApiController
     before_action :check_this_is_a_test_environment
 
+    MAX_COUNT = 100
+    DEFAULT_COUNT = 100
+
     def regenerate
-      GenerateTestData.new([params[:count].to_i, 100].min, current_provider).generate
+      GenerateTestData.new(count_param, current_provider).generate
       render json: { data: { message: 'OK, regenerated the test data' } }
     end
 
@@ -13,6 +16,10 @@ module VendorApi
       if HostingEnvironment.production?
         render status: 400, json: { data: { message: 'Sorry, you can only generate test data in test environments' } }
       end
+    end
+
+    def count_param
+      [(params[:count] || DEFAULT_COUNT).to_i, MAX_COUNT].min
     end
   end
 end
