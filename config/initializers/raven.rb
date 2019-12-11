dc31d0e0fb64374b88271e0522c22a4920ef6ed5
Tijmen Brommet <tijmen@gmail.com>
2019-12-11 11:20:08 +0000

    Ignore ActionController::BadRequest
    This is a user error, so not our problem.

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -2,6 +2,7 @@ Raven.configure do |config|
   config.current_environment = HostingEnvironment.environment_name
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
   config.excluded_exceptions += [
+    'ActionController::BadRequest',
     'ActionController::UnknownHttpMethod',
     'ActionDispatch::Http::Parameters::ParseError',
   ]
