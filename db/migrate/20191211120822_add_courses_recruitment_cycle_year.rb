80e08b38b9ec871ff09a7699eabf473d0e61870e
Steve Hook <steve.hook@gmail.com>
2019-12-12 14:00:07 +0000

    Tidy up migration and make reversible

--- db/migrate/20191211120822_add_courses_recruitment_cycle_year.rb
+++ db/migrate/20191211120822_add_courses_recruitment_cycle_year.rb
@@ -1,7 +1,6 @@
 class AddCoursesRecruitmentCycleYear < ActiveRecord::Migration[6.0]
   def change
     add_column :courses, :recruitment_cycle_year, :integer
-    Course.update_all recruitment_cycle_year: 2020
-    change_column :courses, :recruitment_cycle_year, :integer, null: false
+    change_column_null :courses, :recruitment_cycle_year, false, 2020
   end
 end
