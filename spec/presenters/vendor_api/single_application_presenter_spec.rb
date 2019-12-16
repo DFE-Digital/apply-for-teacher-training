063c34c46fcb3cfb00efaee71e94001e632c9d48
Tijmen Brommet <tijmen@gmail.com>
2019-12-16 12:57:42 +0000

    Refactor SingleApplicationPresenter spec
    This avoids setting up `let` for the entire spec suite, which makes it
    easier to add new specs.

--- spec/presenters/vendor_api/single_application_presenter_spec.rb
+++ spec/presenters/vendor_api/single_application_presenter_spec.rb
@@ -1,26 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe VendorApi::SingleApplicationPresenter do
-  let(:application_form) do
-    create(:completed_application_form) do |form|
-      form.first_nationality = 'British'
-      form.second_nationality = 'American'
-    end
-  end
-  let(:application_choice) do
-    create(:application_choice, status: 'awaiting_provider_decision', application_form: application_form)
-  end
-
-  describe '#candidate' do
+  describe 'attributes.candidate.nationality' do
     it 'returns nationality in the correct format' do
-      single_application_presenter = VendorApi::SingleApplicationPresenter.new(application_choice)
+      application_form = create(:completed_application_form, :with_completed_references, first_nationality: 'British', second_nationality: 'American')
+      application_choice = create(:application_choice, status: 'awaiting_provider_decision', application_form: application_form)
 
-      expect(single_application_presenter.as_json[:attributes][:candidate][:nationality]).to eq(%w[GB US])
+      response = VendorApi::SingleApplicationPresenter.new(application_choice).as_json
+
+      expect(response.to_json).to be_valid_against_openapi_schema('Application')
+      expect(response[:attributes][:candidate][:nationality]).to eq(%w[GB US])
     end
   end
 
   describe '#as_json' do
     context 'given a relation that includes application_qualifications' do
+      let(:application_choice) do
+        create(:application_choice, status: 'awaiting_provider_decision', application_form: create(:completed_application_form))
+      end
+
       let(:given_relation) { GetApplicationChoicesForProvider.call(provider: application_choice.provider) }
       let!(:presenter) { VendorApi::SingleApplicationPresenter.new(given_relation.first) }
 
