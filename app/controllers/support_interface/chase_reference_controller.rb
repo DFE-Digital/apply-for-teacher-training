d1f0aedf1bc13365619717b9806f9619ec25fceb
Wen Ting Wang <chubberlisk@outlook.com>
2019-12-16 16:06:01 +0000

    Add ability to send chase email to a referee in support
    Co-authored-by: Theo Vararu <theo@vararu.org>

---
+++ app/controllers/support_interface/chase_reference_controller.rb
@@ -0,0 +1,19 @@
+module SupportInterface
+  class ChaseReferenceController < SupportInterfaceController
+    def show
+      @reference = Reference.find(params[:reference_id])
+      @application_form = @reference.application_form
+    end
+
+    def chase
+      @reference = Reference.find(params[:reference_id])
+      @application_form = @reference.application_form
+
+      RefereeMailer.reference_request_chaser_email(@application_form, @reference).deliver
+
+      flash[:success] = t('application_form.referees.chase_success')
+
+      redirect_to support_interface_application_form_path(@application_form)
+    end
+  end
+end
