c0e15031fb6e1d0cc143bfe589c9c0005002cf0f
Tijmen Brommet <tijmen@gmail.com>
2019-12-18 12:11:43 +0000

    Refactor DfE session code
    This is a refactor to make it easier to implement session expiry.
    
    This:
    
    - Makes sure we only access the `session['dfe_sign_in_user’]` session
    via the DfESignInUser class
    - Simplifies passing in the omniauth_hash
    - Tidies up some other code

--- app/lib/dfe_sign_in.rb
+++ app/lib/dfe_sign_in.rb
@@ -1,13 +1,4 @@
 module DfESignIn
-  DfESignInSession = Struct.new(:email_address, :uid)
-
-  def self.parse_auth_hash(openid_auth_hash)
-    DfESignInSession.new(
-      openid_auth_hash['info']['email'],
-      openid_auth_hash['uid'],
-    )
-  end
-
   def self.bypass?
     Rails.env.development? && ENV['BYPASS_DFE_SIGN_IN'] == 'true'
   end
