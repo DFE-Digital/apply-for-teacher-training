037242532e5ffc7a849a9a142edf61909733de1e
Duncan Brown <duncanjbrown@gmail.com>
2019-12-19 14:54:32 +0000

    Set declined_at when declining offers

--- app/services/decline_offer.rb
+++ app/services/decline_offer.rb
@@ -5,6 +5,7 @@ class DeclineOffer
 
   def save!
     ApplicationStateChange.new(@application_choice).decline!
+    @application_choice.update!(declined_at: Time.zone.now)
     StateChangeNotifier.call(:offer_declined, application_choice: @application_choice)
   end
 end
