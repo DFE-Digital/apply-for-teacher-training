4cc1acf8ee34c10c46b81c05cf1ba1aeda44a30f
Tijmen Brommet <tijmen@gmail.com>
2019-12-19 11:48:41 +0000

    Add missing info to provider interface
    This adds nationality, date of birth and address to the provider
    interface. They previously were missing.

--- app/components/application_summary_component.rb
+++ app/components/application_summary_component.rb
@@ -16,8 +16,11 @@ class ApplicationSummaryComponent < ActionView::Component::Base
   def rows
     [
       name_row,
-      email_row,
+      date_of_birth_row,
+      nationality_row,
       phone_number_row,
+      email_row,
+      address_row,
     ].compact
   end
 
@@ -46,6 +49,37 @@ private
         value: phone_number,
       }
     end
+  end
+
+  def nationality_row
+    formatted_nationalities = [@application_form.first_nationality, @application_form.second_nationality].reject(&:blank?).to_sentence
+
+    {
+      key: 'Nationality',
+      value: formatted_nationalities,
+    }
+  end
+
+  def date_of_birth_row
+    {
+      key: 'Date of birth',
+      value: @application_form.date_of_birth.strftime('%e %B %Y'),
+    }
+  end
+
+  def address_row
+    full_address = [
+      @application_form.address_line1,
+      @application_form.address_line2,
+      @application_form.address_line3,
+      @application_form.address_line4,
+      @application_form.postcode,
+    ].reject(&:blank?)
+
+    {
+      key: 'Address',
+      value: full_address,
+    }
   end
 
   attr_reader :application_form
