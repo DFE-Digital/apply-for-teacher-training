d18cbdabe0f5957433a67075f100cc0748bef917
Theodor Vararu <theo@vararu.org>
2019-12-20 16:53:49 +0000

    Remove unused instance variables in make_an_offer service

--- app/services/make_an_offer.rb
+++ app/services/make_an_offer.rb
@@ -13,14 +13,6 @@ class MakeAnOffer
     @application_choice = application_choice
     @offer_conditions = offer_conditions
     @course_data = course_data
-
-    unless @course_data.nil?
-      @provider_code = course_data[:provider_code]
-      @course_code = course_data[:course_code]
-      @recruitment_cycle_year = course_data[:recruitment_cycle_year]
-      @study_mode = course_data[:study_mode]
-      @site_code = course_data[:site_code]
-    end
   end
 
   def save
@@ -56,37 +48,43 @@ private
   def validate_course_data
     return if @course_data.nil?
 
-    @provider = Provider.find_by(code: @provider_code)
-    if @provider.nil?
-      errors.add(:offered_course, "provider #{@provider_code} does not exist")
+    provider_code = @course_data[:provider_code]
+    course_code = @course_data[:course_code]
+    recruitment_cycle_year = @course_data[:recruitment_cycle_year]
+    study_mode = @course_data[:study_mode]
+    site_code = @course_data[:site_code]
+
+    provider = Provider.find_by(code: provider_code)
+    if provider.nil?
+      errors.add(:offered_course, "provider #{provider_code} does not exist")
       return
     end
 
-    @course = @provider.courses.find_by(
-      code: @course_code,
-      recruitment_cycle_year: @recruitment_cycle_year,
-      study_mode: @study_mode,
+    course = provider.courses.find_by(
+      code: course_code,
+      recruitment_cycle_year: recruitment_cycle_year,
+      study_mode: study_mode,
     )
-    if @course.nil?
-      errors.add(:offered_course, "#{@course_code} from recruitment cycle year #{@recruitment_cycle_year} with study mode #{@study_mode} does not exist or does not belong to provider #{@provider_code}")
+    if course.nil?
+      errors.add(:offered_course, "#{course_code} from recruitment cycle year #{recruitment_cycle_year} with study mode #{study_mode} does not exist or does not belong to provider #{provider_code}")
       return
     end
 
-    @site = @provider.sites.find_by(code: @site_code)
-    if @site.nil?
-      errors.add(:offered_course, "site #{@site_code} does not exist or does not belong to provider #{@provider_code}")
+    site = provider.sites.find_by(code: site_code)
+    if site.nil?
+      errors.add(:offered_course, "site #{site_code} does not exist or does not belong to provider #{provider_code}")
       return
     end
 
-    @course_option = CourseOption.find_by(course: @course, site: @site, study_mode: @study_mode)
+    @course_option = CourseOption.find_by(course: course, site: site, study_mode: study_mode)
     if @course_option.nil?
       errors.add(:offered_course, 'does not exist')
       return
     end
 
     current_provider = @application_choice.course_option.course.provider
-    providers_dont_match = current_provider != @provider
-    errors.add(:offered_course, "does not belong to provider #{current_provider.code}, it belongs to #{@provider.code}") if providers_dont_match
+    providers_dont_match = current_provider != provider
+    errors.add(:offered_course, "does not belong to provider #{current_provider.code}, it belongs to #{provider.code}") if providers_dont_match
   end
 
   def validate_offer_conditions
