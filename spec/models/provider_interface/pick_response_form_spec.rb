4b3fdd1746acb7267b09bd6cb2fd2a0603f8b9e3
Steve Hook <steve.hook@gmail.com>
2020-01-02 17:43:44 +0000

    Add a more meaningful error message

--- spec/models/provider_interface/pick_response_form_spec.rb
+++ spec/models/provider_interface/pick_response_form_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe ProviderInterface::PickResponseForm do
   describe 'validations' do
-    it { is_expected.to validate_presence_of :decision }
+    it { is_expected.to validate_presence_of(:decision).with_message('Select which response you are making') }
   end
 end
