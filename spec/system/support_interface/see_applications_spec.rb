07c0ef4fc3082c4b590cb18a23e7e5d2d5a36fb2
csabagyorfi <csaba@madetech.com>
2020-01-07 12:24:54 +0000

    Show application support reference on Support Dashboard

--- spec/system/support_interface/see_applications_spec.rb
+++ spec/system/support_interface/see_applications_spec.rb
@@ -7,7 +7,10 @@ RSpec.feature 'See applications' do
     given_i_am_a_support_user
     and_there_are_applications_in_the_system
     and_i_visit_the_support_page
-    then_i_should_see_the_applications
+    then_i_should_see_the_latest_applications
+
+    when_i_follow_the_link_to_applications
+    then_i_should_see_the_application_references
   end
 
   def given_i_am_a_support_user
@@ -24,9 +27,19 @@ RSpec.feature 'See applications' do
     visit support_interface_path
   end
 
-  def then_i_should_see_the_applications
+  def then_i_should_see_the_latest_applications
     expect(page).to have_content @completed_application.candidate.email_address
     expect(page).to have_content @application_with_reference.candidate.email_address
     expect(page).to have_content @unsubmitted_application.candidate.email_address
+  end
+
+  def when_i_follow_the_link_to_applications
+    click_link 'applications list'
+  end
+
+  def then_i_should_see_the_application_references
+    expect(page).to have_content @completed_application.support_reference
+    expect(page).to have_content @application_with_reference.support_reference
+    expect(page).to have_content @unsubmitted_application.support_reference
   end
 end
