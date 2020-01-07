6eba21b73c2fb42a9cd40e2f091683492cf21732
Steve Hook <steve.hook@gmail.com>
2020-01-07 14:17:56 +0000

    Display the lastest application form update on the candidate view

--- app/components/support_interface/candidates_table_component.rb
+++ app/components/support_interface/candidates_table_component.rb
@@ -12,7 +12,7 @@ module SupportInterface
           candidate_id: candidate.id,
           process_state: ProcessState.new(candidate.last_updated_application).state,
           candidate_link: govuk_link_to(candidate.email_address, support_interface_candidate_path(candidate)),
-          updated_at: candidate.updated_at.to_s(:govuk_date_and_time),
+          updated_at: candidate.last_updated_application&.updated_at&.to_s(:govuk_date_and_time),
         }
       end
     end
--- spec/components/support_interface/candidates_table_component_spec.rb
+++ spec/components/support_interface/candidates_table_component_spec.rb
@@ -1,17 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe SupportInterface::CandidatesTableComponent do
-  let(:candidates) { create_list(:candidate, 3) }
+  let(:today) { Time.zone.local(2020, 1, 7, 12, 0, 0) }
+
+  let(:application_forms) { create_list(:application_form, 3, updated_at: 1.day.ago) }
+  let(:candidates) { application_forms.map(&:candidate) }
+
+  around do |example|
+    Timecop.freeze(today) do
+      example.run
+    end
+  end
 
   subject(:component) { described_class.new(candidates: candidates) }
-
-  def render_result
-    render_inline(described_class, candidates: candidates)
-  end
 
   it 'renders the candidate emails' do
     candidates.collect(&:email_address).each do |email_address|
       expect(render_result.text).to include(email_address)
     end
+  end
+
+  it 'renders the date last updated' do
+    expect(render_result.text).to include(application_forms.first.updated_at.to_s(:govuk_date_and_time))
+  end
+
+  def render_result
+    render_inline(described_class, candidates: candidates)
   end
 end
