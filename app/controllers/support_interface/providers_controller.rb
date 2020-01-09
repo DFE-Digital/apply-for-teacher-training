51bfdf900b9d12190cbdcb74132a8e9de7a6721a
Steve Hook <steve.hook@gmail.com>
2020-01-09 11:05:07 +0000

    Finish off the system spec

--- app/controllers/support_interface/providers_controller.rb
+++ app/controllers/support_interface/providers_controller.rb
@@ -15,25 +15,25 @@ module SupportInterface
     end
 
     def open_all_courses
-      update_provider { |provider| provider.courses.update_all(open_on_apply: true) }
+      update_provider('Successfully updated all courses') { |provider| provider.courses.update_all(open_on_apply: true) }
     end
 
     def enable_course_syncing
-      update_provider { |provider| provider.update!(sync_courses: true) }
+      update_provider('Successfully updated provider') { |provider| provider.update!(sync_courses: true) }
     end
 
     def disable_course_syncing
-      update_provider { |provider| provider.update!(sync_courses: false) }
+      update_provider('Successfully updated provider') { |provider| provider.update!(sync_courses: false) }
     end
 
-    private
+  private
 
-    def update_provider
+    def update_provider(success_message)
       @provider = Provider.find(params[:provider_id])
 
       yield @provider
 
-      flash[:success] = 'Successfully updated all courses'
+      flash[:success] = success_message
       redirect_to support_interface_provider_path(@provider)
     end
   end
--- spec/system/support_interface/provider_sync_courses_spec.rb
+++ spec/system/support_interface/provider_sync_courses_spec.rb
@@ -3,14 +3,20 @@ require 'rails_helper'
 RSpec.feature 'See provider course syncing' do
   include DfESignInHelpers
 
-  scenario 'User switches provider to sync courses' do
+  scenario 'User switches sync courses on Provider' do
     given_i_am_a_support_user
     and_a_provider_exists
     when_i_visit_the_providers_page
     then_i_see_that_the_provider_is_not_configured_to_sync_courses
 
     when_i_click_on_a_provider
-    and_i_click_on_the_enable_course_syncing_button
+    then_i_see_that_course_syncing_is_off
+
+    when_i_click_on_the_enable_course_syncing_button
+    then_i_see_that_course_syncing_is_on
+
+    when_i_click_on_the_disable_course_syncing_button
+    then_i_see_that_course_syncing_is_off
   end
 
   def given_i_am_a_support_user
@@ -33,7 +39,19 @@ RSpec.feature 'See provider course syncing' do
     click_link 'ABC College'
   end
 
-  def and_i_click_on_the_enable_course_syncing_button
+  def then_i_see_that_course_syncing_is_off
+    expect(page).to have_content('Course synching for this provider is switched off')
+  end
+
+  def when_i_click_on_the_enable_course_syncing_button
     click_button 'Enable course syncing from Find'
+  end
+
+  def then_i_see_that_course_syncing_is_on
+    expect(page).to have_content('Course synching for this provider is switched on')
+  end
+
+  def when_i_click_on_the_disable_course_syncing_button
+    click_button 'Disable course syncing from Find'
   end
 end
