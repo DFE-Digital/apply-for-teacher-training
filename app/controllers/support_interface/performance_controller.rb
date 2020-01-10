ae4c605912b336fea05b668b2c79da55c3419a51
Jake Benilov <benilov@gmail.com>
2020-01-10 11:49:23 +0000

    Remove authentication from the service performance page in Support
    This allows this page to be displayed on Big Visible Displays.

--- app/controllers/support_interface/performance_controller.rb
+++ app/controllers/support_interface/performance_controller.rb
@@ -1,5 +1,7 @@
 module SupportInterface
   class PerformanceController < SupportInterfaceController
+    skip_before_action :authenticate_support_user!, only: :index
+
     def index
       @statistics = PerformanceStatistics.new
     end
