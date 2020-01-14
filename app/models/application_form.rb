be4586d59252f74f3673d3feb84bb103f54e05dd
alexdesi <alexdesi@gmail.com>
2020-01-14 14:54:22 +0000

    Fix N + 1 query problem

--- app/models/application_form.rb
+++ app/models/application_form.rb
@@ -93,7 +93,7 @@ class ApplicationForm < ApplicationRecord
   end
 
   def science_gcse_needed?
-    application_choices.any? do |application_choice|
+    application_choices.includes(%i[course_option course]).any? do |application_choice|
       application_choice.course_option.course.primary_course?
     end
   end
