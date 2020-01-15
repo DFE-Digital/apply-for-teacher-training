3dae158d9bb8043850f2761a7ba1c31627e32fcf
Steve Hook <steve.hook@gmail.com>
2020-01-15 16:27:40 +0000

    Add outline for ProviderMailer

---
+++ app/mailers/provider_mailer.rb
@@ -0,0 +1,11 @@
+class ProviderMailer < ApplicationMailer
+  def account_created(provider_user)
+    @provider_user = provider_user
+
+    view_mail(
+      GENERIC_NOTIFY_TEMPLATE,
+      to: provider_user.email_address,
+      subject: t('provider_account_created.email.subject')
+    )
+  end
+end
