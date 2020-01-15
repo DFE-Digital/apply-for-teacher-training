9b35001e11f552f2ea52cd6eac1b9f386e00b0a4
David Gisbey <davidgisbey@gmail.com>
2020-01-15 14:35:52 +0000

    Update wording of methods in candidate_apply_from_find_spec
    These don't seem to respect the conventions.

--- spec/system/candidate_interface/candidate_apply_from_find_spec.rb
+++ spec/system/candidate_interface/candidate_apply_from_find_spec.rb
@@ -4,24 +4,24 @@ RSpec.describe 'A candidate arriving from Find with a course and provider code' 
   scenario 'seeing their course information on the landing page' do
     given_the_pilot_is_open
 
-    when_i_have_arrive_from_find_with_invalid_course_parameters
+    when_i_arrive_from_find_with_invalid_course_parameters
     then_i_should_see_an_error_page
 
     when_i_arrive_from_find_to_a_course_that_is_ucas_only
     then_i_should_see_the_landing_page
     and_i_should_see_the_provider_and_course_codes
     and_i_should_see_the_course_name
-    and_i_should_be_able_to_apply_through_ucas_only
+    then_i_should_be_able_to_apply_through_ucas_only
 
     when_i_arrive_from_find_to_a_course_that_is_open_on_apply
-    and_i_should_be_able_to_apply_through_apply
+    then_i_should_be_able_to_apply_through_apply
 
     when_i_visit_the_available_courses_page
-    i_should_see_the_available_providers_and_courses
+    then_i_should_see_the_available_providers_and_courses
 
     given_the_pilot_is_not_open
     when_i_arrive_from_find_to_a_course_that_is_open_on_apply
-    and_i_should_be_able_to_apply_through_ucas_only
+    then_i_should_be_able_to_apply_through_ucas_only
   end
 
   def given_the_pilot_is_not_open
@@ -32,7 +32,7 @@ RSpec.describe 'A candidate arriving from Find with a course and provider code' 
     FeatureFlag.activate('pilot_open')
   end
 
-  def when_i_have_arrive_from_find_with_invalid_course_parameters
+  def when_i_arrive_from_find_with_invalid_course_parameters
     visit candidate_interface_apply_from_find_path providerCode: 'NOT', courseCode: 'REAL'
   end
 
@@ -64,11 +64,11 @@ RSpec.describe 'A candidate arriving from Find with a course and provider code' 
     expect(page).to have_content 'Biology (XYZ1)'
   end
 
-  def and_i_should_be_able_to_apply_through_ucas_only
+  def then_i_should_be_able_to_apply_through_ucas_only
     expect(page).to have_content 'You must apply for this course on UCAS'
   end
 
-  def and_i_should_be_able_to_apply_through_apply
+  def then_i_should_be_able_to_apply_through_apply
     expect(page).to have_content 'You can apply for this course using a new GOV.UK service'
   end
 
@@ -76,7 +76,7 @@ RSpec.describe 'A candidate arriving from Find with a course and provider code' 
     visit candidate_interface_providers_path
   end
 
-  def i_should_see_the_available_providers_and_courses
+  def then_i_should_see_the_available_providers_and_courses
     expect(page).to have_content 'Biology'
     expect(page).to have_content 'Potions'
   end
