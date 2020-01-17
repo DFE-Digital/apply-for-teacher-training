5d021f6b716650890cabd3c1145f4611c56332f3
Steve Hook <steve.hook@gmail.com>
2020-01-17 08:17:56 +0000

    Refactor spec to remote `let` etc.

--- spec/services/submit_application_spec.rb
+++ spec/services/submit_application_spec.rb
@@ -2,20 +2,22 @@ require 'rails_helper'
 
 RSpec.describe SubmitApplication do
   describe 'Submit an application' do
-    let(:application_form) { create(:application_form) }
-
-    before do
+    def create_application_form
+      application_form ||= create(:application_form)
       create(:application_choice, application_form: application_form, status: 'unsubmitted')
       create(:application_choice, application_form: application_form, status: 'unsubmitted')
+      application_form
     end
 
     it 'updates the application to Submitted' do
+      application_form = create_application_form
       SubmitApplication.new(application_form).call
       expect(application_form.application_choices[0]).to be_awaiting_references
       expect(application_form.application_choices[1]).to be_awaiting_references
     end
 
     it 'sets application_form.submitted_at' do
+      application_form = create_application_form
       Timecop.freeze(Time.zone.local(2019, 11, 11, 15, 0, 0)) do
         expected_edit_by = Time.zone.local(2019, 11, 18).end_of_day # business days
         SubmitApplication.new(application_form).call
@@ -27,6 +29,7 @@ RSpec.describe SubmitApplication do
     end
 
     it 'sends Slack notifications' do
+      application_form = create_application_form
       allow(SlackNotificationWorker).to receive(:perform_async)
       SubmitApplication.new(application_form).call
       expect(SlackNotificationWorker).to have_received(:perform_async).once # per application_form, not application_choices
@@ -40,6 +43,7 @@ RSpec.describe SubmitApplication do
       end
 
       it 'sets the edit_by timestamp to now and pushes the status on to `awaiting_provider_decision`' do
+        application_form = create_application_form
         now = Time.zone.local(2019, 11, 11, 15, 0, 0)
         Timecop.freeze(now) do
           SubmitApplication.new(application_form).call
