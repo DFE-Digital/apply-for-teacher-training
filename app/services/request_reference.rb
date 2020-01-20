669cd0541ba34c510560bff20c9f6fda566b02fc
Steve Hook <steve.hook@gmail.com>
2020-01-20 16:09:40 +0000

    Implement logic for auto-referees
    Still need to invoke the `ReceiveReference` service here.

--- app/services/request_reference.rb
+++ app/services/request_reference.rb
@@ -1,6 +1,8 @@
 class RequestReference
   attr_accessor :referee_params, :referee, :application_form
 
+  BOT_EMAIL_ADDRESSES = ['refbot1@example.com', 'refbot2@example.com'].freeze
+
   def initialize(referee_params:, application_form:)
     self.referee_params = referee_params
     self.application_form = application_form
@@ -8,6 +10,21 @@ class RequestReference
 
   def call
     @referee = application_form.application_references.build(referee_params)
+    auto_approve_in_sandbox
     @referee.save
+  end
+
+private
+
+  def auto_approve_in_sandbox
+    @referee.feedback = I18n.t('new_referee_request.auto_approve_feedback') if sandbox? && email_address_is_a_bot?
+  end
+
+  def sandbox?
+    ENV.fetch('SANDBOX') { 'false' } == 'true'
+  end
+
+  def email_address_is_a_bot?
+    BOT_EMAIL_ADDRESSES.include?(@referee.email_address)
   end
 end
---
+++ spec/services/request_reference_spec.rb
@@ -0,0 +1,67 @@
+require 'rails_helper'
+
+RSpec.describe RequestReference do
+  context 'outside the Sandbox environment' do
+    it 'saves the reference without feedback' do
+      application_form = FactoryBot.create :application_form
+
+      action = RequestReference.new(
+        application_form: application_form,
+        referee_params: {
+          email_address: 'bob@example.com',
+          name: 'Bob',
+          relationship: 'Teacher',
+        },
+      )
+
+      expect(action.call).to be true
+      expect(action.referee.persisted?).to be true
+      expect(action.referee.name).to eq 'Bob'
+      expect(action.referee.feedback).to be_nil
+    end
+  end
+
+  context 'in the Sandbox environment' do
+    around do |example|
+      ClimateControl.modify(SANDBOX: 'true') do
+        example.run
+      end
+    end
+
+    it 'saves the reference without feedback if the email_address does not match the auto-referees' do
+      application_form = FactoryBot.create :application_form
+
+      action = RequestReference.new(
+        application_form: application_form,
+        referee_params: {
+          email_address: 'bob@example.com',
+          name: 'Bob',
+          relationship: 'Teacher',
+        },
+      )
+
+      expect(action.call).to be true
+      expect(action.referee.persisted?).to be true
+      expect(action.referee.name).to eq 'Bob'
+      expect(action.referee.feedback).to be_nil
+    end
+
+    it 'saves the reference with automatic feedback if the email_address does match one of the auto-referees' do
+      application_form = FactoryBot.create :application_form
+
+      action = RequestReference.new(
+        application_form: application_form,
+        referee_params: {
+          email_address: 'refbot1@example.com',
+          name: 'Bob',
+          relationship: 'Teacher',
+        },
+      )
+
+      expect(action.call).to be true
+      expect(action.referee.persisted?).to be true
+      expect(action.referee.name).to eq 'Bob'
+      expect(action.referee.feedback).not_to be_nil
+    end
+  end
+end
