49f26fcee28cc7aa2562aa02e3444e780f03e5fe
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2020-01-20 16:09:01 +0000

    Add product page for providers

--- spec/support/test_helpers/dfe_sign_in_helpers.rb
+++ spec/support/test_helpers/dfe_sign_in_helpers.rb
@@ -8,12 +8,12 @@ module DfESignInHelpers
   alias :provider_exists_in_dfe_sign_in :user_exists_in_dfe_sign_in
 
   def provider_signs_in_using_dfe_sign_in
-    visit provider_interface_path
+    visit provider_interface_sign_in_path
     click_button 'Sign in using DfE Sign-in'
   end
 
   def support_user_signs_in_using_dfe_sign_in
-    visit support_interface_path
+    visit support_interface_sign_in_path
     click_button 'Sign in using DfE Sign-in'
   end
 
