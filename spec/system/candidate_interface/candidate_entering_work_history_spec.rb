b0cb539b811f6d817c6fd7715dc22aa848b5d7f9
alexdesi <alexdesi@gmail.com>
2020-01-21 16:24:39 +0000

    Add work break between works

--- spec/system/candidate_interface/candidate_entering_work_history_spec.rb
+++ spec/system/candidate_interface/candidate_entering_work_history_spec.rb
@@ -4,55 +4,63 @@ RSpec.feature 'Entering their work history' do
   include CandidateHelper
 
   scenario 'Candidate submits their work history' do
-    given_i_am_signed_in
-    and_i_visit_the_site
+    Timecop.travel(Time.zone.local(2019, 12, 1)) do
+      given_i_am_signed_in
+      and_i_visit_the_site
 
-    when_i_click_on_work_history
-    then_i_should_see_a_list_of_work_lengths
+      when_i_click_on_work_history
+      then_i_should_see_a_list_of_work_lengths
 
-    when_i_omit_choosing_from_the_list_of_work_lengths
-    then_i_should_see_work_history_length_validation_errors
+      when_i_omit_choosing_from_the_list_of_work_lengths
+      then_i_should_see_work_history_length_validation_errors
 
-    when_i_choose_more_than_5_years
-    then_i_should_see_the_job_form
+      when_i_choose_more_than_5_years
+      then_i_should_see_the_job_form
 
-    when_i_fill_in_the_job_form_with_incorrect_date_fields
-    then_i_should_see_date_validation_errors
-    and_i_should_see_the_incorrect_date_values
+      when_i_fill_in_the_job_form_with_incorrect_date_fields
+      then_i_should_see_date_validation_errors
+      and_i_should_see_the_incorrect_date_values
 
-    when_i_fill_in_the_job_form
-    then_i_should_see_my_completed_job
+      when_i_fill_in_the_job_form
+      then_i_should_see_my_completed_job
 
-    when_i_click_on_delete_entry
-    and_i_confirm
-    then_i_should_be_asked_for_an_explanation
+      when_i_click_on_delete_entry
+      and_i_confirm
+      then_i_should_be_asked_for_an_explanation
 
-    when_i_click_on_add_job
-    and_i_fill_in_the_job_form
-    then_i_should_see_my_completed_job
+      when_i_click_on_add_job
+      and_i_fill_in_the_job_form # 5/2014 - 1/2019
+      then_i_should_see_my_completed_job
 
-    when_i_click_on_add_another_job
-    and_i_fill_in_the_job_form_with_another_job_with_a_break
-    then_i_should_see_my_second_job
-    and_i_should_be_asked_to_explain_the_break_in_my_work_history
+      when_i_click_on_add_another_job
+      and_i_fill_in_the_job_form_with_another_job_with_a_break # 3/2019 - current
+      and_i_do_not_fill_in_end_date
 
-    when_i_click_to_enter_break_explanation
-    then_i_see_the_work_history_break_form
+      then_i_should_see_my_second_job
 
-    when_i_fill_in_the_work_history_break_form
-    then_i_see_my_explanation_for_breaks_in_my_work_history
+      and_i_should_see_the_length_of_the_gap_in_months # 1 months
+      and_i_should_not_see_0_months_gap
 
-    when_i_click_on_change
-    and_i_change_the_job_title_to_be_blank
-    then_i_should_see_validation_errors
+      and_i_should_be_asked_to_explain_the_break_in_my_work_history
 
-    when_i_change_the_job_title
-    then_i_should_see_my_updated_job
+      when_i_click_to_enter_break_explanation
+      then_i_see_the_work_history_break_form
 
-    when_i_mark_this_section_as_completed
-    and_i_click_on_continue
-    then_i_should_see_the_form
-    and_that_the_section_is_completed
+      when_i_fill_in_the_work_history_break_form
+      then_i_see_my_explanation_for_breaks_in_my_work_history
+
+      when_i_click_on_change
+      and_i_change_the_job_title_to_be_blank
+      then_i_should_see_validation_errors
+
+      when_i_change_the_job_title
+      then_i_should_see_my_updated_job
+
+      when_i_mark_this_section_as_completed
+      and_i_click_on_continue
+      then_i_should_see_the_form
+      and_that_the_section_is_completed
+    end
   end
 
   def given_i_am_signed_in
@@ -175,7 +183,7 @@ RSpec.feature 'Entering their work history' do
     choose 'Full-time'
 
     within('[data-qa="start-date"]') do
-      fill_in 'Month', with: '2'
+      fill_in 'Month', with: '3'
       fill_in 'Year', with: '2019'
     end
 
@@ -186,8 +194,18 @@ RSpec.feature 'Entering their work history' do
     click_button t('application_form.work_history.complete_form_button')
   end
 
+  def and_i_do_not_fill_in_end_date; end
+
   def then_i_should_see_my_second_job
     expect(page).to have_content('Chief of Xenomorph Procurement and Research')
+  end
+
+  def and_i_should_see_the_length_of_the_gap_in_months
+    expect(page).to have_content('You have a break in your work history (1 month)')
+  end
+
+  def and_i_should_not_see_0_months_gap
+    expect(page).not_to have_content('(0 months)')
   end
 
   def and_i_should_be_asked_to_explain_the_break_in_my_work_history
