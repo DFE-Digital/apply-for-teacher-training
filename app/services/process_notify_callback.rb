7429f4823d6441ddbec6389ed2f07acba3c69164
Wen Ting Wang <chubberlisk@outlook.com>
2020-01-23 10:59:17 +0000

    Update candidate if sign up bounced in ProcessNotifyCallback
    Set `sign_up_email_bounced` and `hide_in_reporting` to true.

--- app/services/process_notify_callback.rb
+++ app/services/process_notify_callback.rb
@@ -1,23 +1,33 @@
 class ProcessNotifyCallback
+  EXPECTED_EMAIL_TYPES = %w[reference_request sign_up_email].freeze
+
   def initialize(notify_reference:, status:)
-    @environment, @email_type, @reference_id = notify_reference.split('-')
+    @environment, @email_type, @id = notify_reference.split('-')
     @status = status
     @not_found = false
   end
 
   def call
-    return unless same_environment? && reference_request_email? && permanent_failure_status?
+    return unless same_environment? && expected_email_type? && permanent_failure_status?
 
-    ActiveRecord::Base.transaction do
-      reference = ApplicationReference.find(@reference_id)
+    if reference_request_email?
+      ActiveRecord::Base.transaction do
+        reference = ApplicationReference.find(@id)
 
-      reference.update!(feedback_status: 'email_bounced')
+        reference.update!(feedback_status: 'email_bounced')
 
-      SendNewRefereeRequestEmail.call(
-        application_form: reference.application_form,
-        reference: reference,
-        reason: :email_bounced,
-      )
+        SendNewRefereeRequestEmail.call(
+          application_form: reference.application_form,
+          reference: reference,
+          reason: :email_bounced,
+        )
+      end
+    end
+
+    if sign_up_email?
+      candidate = Candidate.find(@id)
+
+      candidate.update!(sign_up_email_bounced: true, hide_in_reporting: true)
     end
   rescue ActiveRecord::RecordNotFound
     @not_found = true
@@ -33,8 +43,16 @@ private
     @environment == HostingEnvironment.environment_name
   end
 
+  def expected_email_type?
+    EXPECTED_EMAIL_TYPES.include?(@email_type)
+  end
+
   def reference_request_email?
     @email_type == 'reference_request'
+  end
+
+  def sign_up_email?
+    @email_type == 'sign_up_email'
   end
 
   def permanent_failure_status?
--- spec/services/process_notify_callback_spec.rb
+++ spec/services/process_notify_callback_spec.rb
@@ -21,8 +21,9 @@ RSpec.describe ProcessNotifyCallback do
     application_form = create(:application_form)
     create(:reference, feedback_status: 'feedback_requested', application_form: application_form)
   end
+  let(:candidate) { create(:candidate, sign_up_email_bounced: false, hide_in_reporting: false) }
 
-  context 'when expected Notify reference is provided' do
+  context 'when expected Notify reference is provided with reference request' do
     let(:notify_reference) { "example_env-reference_request-#{reference.id}" }
 
     context 'with permanent-failure status' do
@@ -54,6 +55,40 @@ RSpec.describe ProcessNotifyCallback do
     end
   end
 
+  context 'when expected Notify reference is provided with sign up email' do
+    let(:notify_reference) { "example_env-sign_up_email-#{candidate.id}" }
+
+    context 'with permanent-failure status' do
+      let(:status) { 'permanent-failure' }
+
+      it 'updates sign up email bounced to true for candidate' do
+        process_notify_callback = ProcessNotifyCallback.new(notify_reference: notify_reference, status: status)
+
+        process_notify_callback.call
+
+        expect(candidate.reload.sign_up_email_bounced).to eq(true)
+      end
+
+      it 'updates hide in reporting to true for candidate' do
+        process_notify_callback = ProcessNotifyCallback.new(notify_reference: notify_reference, status: status)
+
+        process_notify_callback.call
+
+        expect(candidate.reload.hide_in_reporting).to eq(true)
+      end
+
+      it 'sets not found to true if candidate cannot be found' do
+        allow(Candidate).to receive(:find).with(candidate.id.to_s).and_raise(ActiveRecord::RecordNotFound)
+
+        process_notify_callback = ProcessNotifyCallback.new(notify_reference: notify_reference, status: status)
+
+        process_notify_callback.call
+
+        expect(process_notify_callback).to be_not_found
+      end
+    end
+  end
+
   context 'when unexpected Notify reference is provided' do
     let(:status) { 'permanent-failure' }
 
@@ -64,11 +99,21 @@ RSpec.describe ProcessNotifyCallback do
       it_behaves_like "a callback that doesn't change the feedback status of a reference"
     end
 
-    context 'with email type not reference request' do
-      let(:email_type) { 'survey_email' }
+    context 'with email type not reference request or sign up email bounced' do
+      let(:email_type) { 'example_email' }
       let(:notify_reference) { "example_env-#{email_type}-#{reference.id}" }
 
       it_behaves_like "a callback that doesn't change the feedback status of a reference"
+
+      it 'does not update sign up email bounced and hide in reporting to true for candidate' do
+        notify_reference = "example_env-#{email_type}-#{candidate.id}"
+        process_notify_callback = ProcessNotifyCallback.new(notify_reference: notify_reference, status: status)
+
+        process_notify_callback.call
+
+        expect(candidate.reload.sign_up_email_bounced).to eq(false)
+        expect(candidate.reload.hide_in_reporting).to eq(false)
+      end
     end
   end
 end
