4bf97bab927d8dde4bd18d69634dc6af4994fe4d
Jake Benilov <benilov@gmail.com>
2020-01-28 18:34:16 +0000

    Report on application forms on the service performance page
    Break down application forms by their process state.

--- spec/system/support_interface/service_performance_spec.rb
+++ spec/system/support_interface/service_performance_spec.rb
@@ -2,14 +2,15 @@ require 'rails_helper'
 
 RSpec.feature 'Service performance' do
   scenario 'View service statistics' do
-    given_there_are_candidates_in_the_system
+    given_there_are_candidates_and_application_forms_in_the_system
 
     when_i_visit_the_service_performance_page
     then_i_should_see_the_total_count_of_candidates
+    and_i_should_see_the_total_count_of_application_forms
   end
 
-  def given_there_are_candidates_in_the_system
-    create_list(:candidate, 2)
+  def given_there_are_candidates_and_application_forms_in_the_system
+    create_list(:application_form, 3)
   end
 
   def when_i_visit_the_service_performance_page
@@ -18,7 +19,13 @@ RSpec.feature 'Service performance' do
 
   def then_i_should_see_the_total_count_of_candidates
     within '#total-sign-ups' do
-      expect(page).to have_content '2'
+      expect(page).to have_content '3'
+    end
+  end
+
+  def and_i_should_see_the_total_count_of_application_forms
+    within '#total-application-forms' do
+      expect(page).to have_content '3'
     end
   end
 end
