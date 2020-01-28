98c97d8b1e87e0a4c42765e804045394a3b880c1
Jake Benilov <benilov@gmail.com>
2020-01-28 18:24:28 +0000

    Add app form totals and statuses to PerformanceStatistics
    This will be used for a service report on application forms.

--- app/models/performance_statistics.rb
+++ app/models/performance_statistics.rb
@@ -23,8 +23,59 @@ class PerformanceStatistics
   FROM
       raw_data".freeze
 
+  APPLICATION_FORM_STATUS_COUNTS_QUERY = "
+  WITH raw_data AS (
+      SELECT
+          f.id,
+          COUNT(f.id) FILTER (WHERE f.id IS NOT NULL) application_forms,
+          COUNT(ch.id) FILTER (WHERE f.id IS NOT NULL) application_choices,
+          CASE
+            WHEN ARRAY_AGG(DISTINCT ch.status) = '{NULL}' THEN ARRAY['0', 'unsubmitted']
+            WHEN ARRAY_AGG(DISTINCT ch.status) = '{unsubmitted}' THEN ARRAY['0', 'unsubmitted']
+            WHEN 'awaiting_references' = ANY(ARRAY_AGG(ch.status)) THEN ARRAY['1', 'awaiting_references']
+            WHEN 'application_complete' = ANY(ARRAY_AGG(ch.status)) THEN ARRAY['2', 'waiting_to_be_sent']
+            WHEN 'awaiting_provider_decision' = ANY(ARRAY_AGG(ch.status)) THEN ARRAY['3', 'awaiting_provider_decisions']
+            WHEN 'offer' = ANY(ARRAY_AGG(ch.status)) THEN ARRAY['5', 'awaiting_candidate_response']
+            WHEN 'enrolled' = ANY(ARRAY_AGG(ch.status)) THEN ARRAY['8', 'enrolled']
+            WHEN 'recruited' = ANY(ARRAY_AGG(ch.status)) THEN ARRAY['7', 'recruited']
+            WHEN 'pending_conditions' = ANY(ARRAY_AGG(ch.status)) THEN ARRAY['6', 'pending_conditions']
+            WHEN ARRAY_REMOVE(ARRAY_REMOVE(ARRAY_REMOVE(ARRAY_REMOVE(ARRAY_AGG(DISTINCT ch.status), 'withdrawn'), 'rejected'), 'declined'), 'conditions_not_met') = '{}' THEN ARRAY['4', 'ended_without_success']
+            ELSE ARRAY['9', 'unknown_state']
+          END status
+      FROM
+          candidates c
+      LEFT JOIN
+          application_forms f ON f.candidate_id = c.id
+      LEFT JOIN
+          application_choices ch ON ch.application_form_id = f.id
+      WHERE
+          NOT c.hide_in_reporting
+      GROUP BY
+          f.id
+  )
+  SELECT
+      raw_data.status[2],
+      COUNT(*)
+  FROM
+      raw_data
+  GROUP BY
+      raw_data.status
+  ORDER BY
+      raw_data.status[1]".freeze
+
   def [](key)
     candidate_counts[key.to_s]
+  end
+
+  def application_form_status_counts
+    @application_form_status_counts ||= ActiveRecord::Base
+      .connection
+      .execute(APPLICATION_FORM_STATUS_COUNTS_QUERY)
+      .to_a
+  end
+
+  def application_form_counts_total
+    application_form_status_counts.sum { |row| row['count'].to_i }
   end
 
 private
