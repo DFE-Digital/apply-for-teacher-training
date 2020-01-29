dcc0ee45fac53b27ee455d5c71477b0ac67e7c7a
Wen Ting Wang <chubberlisk@outlook.com>
2020-01-29 09:33:01 +0000

    Fix broken previews for authentication emails

--- app/mailers/previews/authentication_mailer_preview.rb
+++ app/mailers/previews/authentication_mailer_preview.rb
@@ -1,9 +1,9 @@
 class AuthenticationMailerPreview < ActionMailer::Preview
   def sign_up_email
-    AuthenticationMailer.sign_up_email(to: "#{SecureRandom.hex}@example.com", token: SecureRandom.hex)
+    AuthenticationMailer.sign_up_email(candidate: FactoryBot.build_stubbed(:candidate), token: SecureRandom.hex)
   end
 
   def sign_in_email
-    AuthenticationMailer.sign_in_email(to: "#{SecureRandom.hex}@example.com", token: SecureRandom.hex)
+    AuthenticationMailer.sign_in_email(candidate: FactoryBot.build_stubbed(:candidate), token: SecureRandom.hex)
   end
 end
