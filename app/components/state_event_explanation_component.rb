7e06cf72ef356c2194aec4ec1c12fa8bb980cf43
Tijmen Brommet <tijmen@gmail.com>
2020-01-30 10:13:36 +0000

    Move state explanation components into the top-level namespace
    This will allow us to re-use these components in the API docs.

---
+++ app/components/state_event_explanation_component.rb
@@ -0,0 +1,36 @@
+class StateEventExplanationComponent < ActionView::Component::Base
+  include ViewHelper
+
+  attr_reader :from_state, :event, :namespace, :machine
+
+  def initialize(from_state:, event:, machine:)
+    @from_state = from_state
+    @event = event
+    @machine = machine
+    @namespace = machine.i18n_namespace
+  end
+
+  def emails_sent_from_event
+    if I18n.exists?("#{namespace}events.#{from_state}-#{event.name}.emails")
+      I18n.t!("#{namespace}events.#{from_state}-#{event.name}.emails")
+    else
+      []
+    end
+  end
+
+  def human_transitions_to
+    I18n.t!("#{namespace}application_states.#{transitions_to}.name")
+  end
+
+  def transitions_to
+    event.transitions_to.to_s
+  end
+
+  def by
+    I18n.t!("#{namespace}events.#{from_state}-#{event}.by")
+  end
+
+  def description
+    I18n.t!("#{namespace}events.#{from_state}-#{event.name}.description")
+  end
+end
---
+++ app/components/state_explanation_component.rb
@@ -0,0 +1,23 @@
+class StateExplanationComponent < ActionView::Component::Base
+  include ViewHelper
+
+  attr_reader :state, :namespace, :machine
+
+  def initialize(state:, machine:)
+    @state = state
+    @machine = machine
+    @namespace = machine.i18n_namespace
+  end
+
+  def state_name
+    state.name.to_s
+  end
+
+  def human_state_name
+    I18n.t!("#{namespace}application_states.#{state_name}.name")
+  end
+
+  def state_description
+    I18n.t!("#{namespace}application_states.#{state_name}.description")
+  end
+end
