bef0bae55066a02a5557e93826d3fbaad74c2478
Tijmen Brommet <tijmen@gmail.com>
2020-02-03 12:37:25 +0000

    Write test to make sure we allow more than one reference in the future
    We’re likely to be requesting more references, and we need to make sure
    that the app doesn’t crash on having more than 2 references.

--- app/services/receive_reference.rb
+++ app/services/receive_reference.rb
@@ -1,5 +1,6 @@
 class ReceiveReference
   attr_reader :reference, :feedback
+  delegate :application_form, to: :reference
 
   def initialize(reference:, feedback:)
     @reference = reference
@@ -16,14 +17,19 @@ class ReceiveReference
 private
 
   def progress_application_if_enough_references_have_been_submitted
-    application_form = reference.application_form
-
-    return unless application_form.application_references_complete?
+    return unless there_are_now_enough_references_to_progress?
 
     application_form.application_choices.each do |application_choice|
       ApplicationStateChange.new(application_choice).references_complete!
     end
 
     SendApplicationsToProvider.new.call
+  end
+
+  # Only progress the applications if the reference that is being submitted is
+  # the 2nd referee, since there might be more than 2 references per form
+  # in the future.
+  def there_are_now_enough_references_to_progress?
+    application_form.application_references.feedback_provided.count == ApplicationForm::MINIMUM_COMPLETE_REFERENCES
   end
 end
--- spec/services/receive_reference_spec.rb
+++ spec/services/receive_reference_spec.rb
@@ -45,4 +45,23 @@ RSpec.describe ReceiveReference do
 
     expect(application_form.reload.application_choices).to all(be_awaiting_provider_decision)
   end
+
+  it 'is okay with a 3rd reference being provided' do
+    application_form = create(:completed_application_form)
+    create(:application_choice, application_form: application_form, status: 'awaiting_references', edit_by: 1.day.ago)
+    create(:reference, :complete, application_form: application_form)
+    reference = create(:reference, :unsubmitted, application_form: application_form)
+
+    ReceiveReference.new(
+      reference: reference,
+      feedback: 'A reference',
+    ).save!
+
+    another_reference = create(:reference, :unsubmitted, application_form: application_form)
+
+    ReceiveReference.new(
+      reference: another_reference,
+      feedback: 'A 3rd reference',
+    ).save!
+  end
 end
