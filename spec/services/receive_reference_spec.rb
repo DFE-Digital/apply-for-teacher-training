a6ed435ec6e9965bafb8cf27ea972d66734d5ce5
Tijmen Brommet <tijmen@gmail.com>
2020-02-03 12:27:24 +0000

    Tidy up ReceiveReference test

--- spec/services/receive_reference_spec.rb
+++ spec/services/receive_reference_spec.rb
@@ -2,11 +2,10 @@ require 'rails_helper'
 
 RSpec.describe ReceiveReference do
   it 'updates the reference on an application form with the provided text' do
-    application_form = FactoryBot.create(:completed_application_form, references_count: 0)
-    application_form.application_choices.each { |choice| choice.update(status: 'awaiting_references') }
-    application_form.application_references << build(:reference, :unsubmitted, email_address: 'ab@c.com')
-    reference = build(:reference, :unsubmitted, email_address: 'xy@z.com')
-    application_form.application_references << reference
+    application_form = create(:completed_application_form)
+    create(:application_choice, application_form: application_form, status: 'awaiting_references')
+    create(:reference, :unsubmitted, application_form: application_form)
+    reference = create(:reference, :unsubmitted, application_form: application_form)
 
     ReceiveReference.new(
       reference: reference,
@@ -19,11 +18,10 @@ RSpec.describe ReceiveReference do
   end
 
   it 'progresses the application choices to the "application complete" status once all references have been received' do
-    application_form = FactoryBot.create(:completed_application_form, references_count: 0)
-    application_form.application_choices.each { |choice| choice.update(status: 'awaiting_references', edit_by: 1.day.from_now) }
-    reference = build(:reference, :unsubmitted, email_address: 'ab@c.com')
-    application_form.application_references << reference
-    application_form.application_references << build(:reference, :complete)
+    application_form = create(:completed_application_form)
+    create(:application_choice, application_form: application_form, status: 'awaiting_references', edit_by: 1.day.from_now)
+    create(:reference, :complete, application_form: application_form)
+    reference = create(:reference, :unsubmitted, application_form: application_form)
 
     ReceiveReference.new(
       reference: reference,
@@ -35,11 +33,10 @@ RSpec.describe ReceiveReference do
   end
 
   it 'progresses the application choices to the "awaiting_provider_decision" status once all references have been received if edit_by has elapsed' do
-    application_form = FactoryBot.create(:completed_application_form, references_count: 0)
-    application_form.application_choices.each { |choice| choice.update(status: 'awaiting_references', edit_by: 1.day.ago) }
-    reference = build(:reference, :unsubmitted, email_address: 'ab@c.com')
-    application_form.application_references << reference
-    application_form.application_references << build(:reference, :complete)
+    application_form = create(:completed_application_form)
+    create(:application_choice, application_form: application_form, status: 'awaiting_references', edit_by: 1.day.ago)
+    create(:reference, :complete, application_form: application_form)
+    reference = create(:reference, :unsubmitted, application_form: application_form)
 
     ReceiveReference.new(
       reference: reference,
