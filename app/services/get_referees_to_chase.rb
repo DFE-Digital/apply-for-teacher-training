6f0719b62e6529a4bb4b494c592576cc9b907adb
David Gisbey <davidgisbey@gmail.com>
2020-02-03 16:02:21 +0000

    Refactor out redundant logic
    Not really sure why i was using application choice before
    rather than just using the references.

--- app/services/get_referees_to_chase.rb
+++ app/services/get_referees_to_chase.rb
@@ -1,17 +1,13 @@
 class GetRefereesToChase
   def perform
-    application_choices = ApplicationChoice.awaiting_references
+    references = ApplicationReference.includes(:application_form).feedback_requested
 
-    choices_with_applications_submitted_in_the_last_5_days = application_choices.select do |application_choice|
-      application_choice.application_form.submitted_at < Time.zone.now - 5.days
+    references_with_applications_submitted_in_the_last_5_days = references.select do |reference|
+      reference.application_form.submitted_at < Time.zone.now - 5.days
     end
 
-    application_forms = choices_with_applications_submitted_in_the_last_5_days.map(&:application_form).uniq
-
-    references = application_forms.map(&:application_references).flatten
-
-    references_feedback_requested = references.select(&:feedback_requested?)
-
-    references_feedback_requested.select { |reference| reference.chasers_sent.referee_mailer_reference_request_chaser_email.blank? }
+    references_with_applications_submitted_in_the_last_5_days.select do |reference|
+      reference.chasers_sent.referee_mailer_reference_request_chaser_email.blank?
+    end
   end
 end
