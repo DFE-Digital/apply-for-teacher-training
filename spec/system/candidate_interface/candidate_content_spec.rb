ffbd9a67e4ed7bb4dd9338ad6956f9eebf3cc6be
Tijmen Brommet <tijmen@gmail.com>
2020-02-05 17:25:25 +0000

    Merge all candidate content tests into one

---
+++ spec/system/candidate_interface/candidate_content_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+RSpec.feature 'Candidate content' do
+  scenario 'Candidate views the content pages' do
+    given_i_am_on_the_start_page
+    when_i_click_on_accessibility
+    then_i_can_see_the_accessibility_statement
+
+    when_i_click_on_the_cookie_policy
+    then_i_can_see_the_cookie_policy
+
+    when_i_click_on_the_privacy_policy
+    then_i_can_see_the_privacy_policy
+
+    when_i_click_on_the_terms_of_use
+    then_i_can_see_the_terms_candidate
+  end
+
+  def given_i_am_on_the_start_page
+    visit '/'
+  end
+
+  def when_i_click_on_accessibility
+    within('.govuk-footer') { click_link t('layout.accessibility') }
+  end
+
+  def then_i_can_see_the_accessibility_statement
+    expect(page).to have_content(t('page_titles.accessibility'))
+  end
+
+  def when_i_click_on_the_cookie_policy
+    within('.govuk-footer') { click_link t('layout.cookie_policy') }
+  end
+
+  def then_i_can_see_the_cookie_policy
+    expect(page).to have_content(t('page_titles.cookies_candidate'))
+  end
+
+  def when_i_click_on_the_privacy_policy
+    within('.govuk-footer') { click_link t('layout.privacy_policy') }
+  end
+
+  def then_i_can_see_the_privacy_policy
+    expect(page).to have_content(t('page_titles.privacy_policy'))
+  end
+
+  def when_i_click_on_the_terms_of_use
+    within('.govuk-footer') { click_link t('layout.terms_of_use') }
+  end
+
+  def then_i_can_see_the_terms_candidate
+    expect(page).to have_content(t('page_titles.terms_candidate'))
+  end
+end
