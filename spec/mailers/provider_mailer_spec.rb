5bc0e1956291ec3cc3073ae35be686060ea4a1a7
csabagyorfi <csaba@madetech.com>
2020-02-06 15:59:26 +0000

    Refactor provider mailer spec

--- spec/mailers/provider_mailer_spec.rb
+++ spec/mailers/provider_mailer_spec.rb
@@ -5,6 +5,17 @@ RSpec.describe ProviderMailer, type: :mailer do
 
   subject(:mailer) { described_class }
 
+  let(:course_option) { course_option_for_provider_code(provider_code: 'ABC') }
+  let(:application_choice) do
+    build_stubbed(:submitted_application_choice,
+                  course_option: course_option,
+                  application_form:
+                    build_stubbed(
+                      :completed_application_form,
+                   ))
+  end
+  let(:provider_user) { application_choice.provider.provider_users.first }
+
   describe 'Send account created email' do
     before do
       @provider_user = build_stubbed(:provider_user)
@@ -26,131 +37,105 @@ RSpec.describe ProviderMailer, type: :mailer do
 
   describe 'Send application submitted email' do
     before do
-      @course_option = course_option_for_provider_code(provider_code: 'ABC')
-      @application_choice = create(:application_choice, status: 'application_complete', edit_by: Time.zone.today,
-        course_option: @course_option,
-        application_form:
-          create(
-            :completed_application_form,
-            submitted_at: Time.zone.today,
-        ))
-      @provider_user = @application_choice.provider.provider_users.first
-      @mail = mailer.application_submitted(@provider_user, @application_choice)
+      @mail = mailer.application_submitted(provider_user, application_choice)
     end
 
     it 'sends an email with the correct subject' do
       expect(@mail.subject).to include(
         t('provider_application_submitted.email.subject',
-          course_name_and_code: @application_choice.course.name_and_code),
+          course_name_and_code: application_choice.course.name_and_code),
       )
     end
 
     it 'addresses the provider user by name' do
-      expect(@mail.body.encoded).to include("Dear #{@provider_user.full_name}")
+      expect(@mail.body.encoded).to include("Dear #{provider_user.full_name}")
     end
 
     it 'includes the candidate name' do
-      expect(@mail.body.encoded).to include("#{@application_choice.application_form.full_name} submitted an application for")
+      expect(@mail.body.encoded).to include("#{application_choice.application_form.full_name} submitted an application for")
     end
 
     it 'includes the course details' do
-      expect(@mail.body.encoded).to include(@application_choice.course.name)
-      expect(@mail.body.encoded).to include(@application_choice.course.code)
+      expect(@mail.body.encoded).to include(application_choice.course.name)
+      expect(@mail.body.encoded).to include(application_choice.course.code)
     end
 
     it 'includes a link to the application' do
-      expect(@mail.body.encoded).to include(provider_interface_application_choice_url(@application_choice))
+      expect(@mail.body.encoded).to include(provider_interface_application_choice_url(application_choice))
     end
   end
 
   describe 'Send application rejected by default email' do
     before do
-      @course_option = course_option_for_provider_code(provider_code: 'ABC')
-      @submission_date = Time.zone.today - 40.days
-      @application_choice = create(:application_choice, status: 'rejected',
-        course_option: @course_option,
-        application_form:
-          create(
-            :completed_application_form,
-            submitted_at: @submission_date,
-        ))
-      @provider_user = @application_choice.provider.provider_users.first
-      @mail = mailer.application_rejected_by_default(@provider_user, @application_choice)
+      @mail = mailer.application_rejected_by_default(provider_user, application_choice)
     end
 
     it 'sends an email with the correct subject' do
       expect(@mail.subject).to include(
         t('provider_application_rejected_by_default.email.subject',
-          candidate_name: @application_choice.application_form.full_name),
+          candidate_name: application_choice.application_form.full_name),
         )
     end
 
     it 'addresses the provider user by name' do
-      expect(@mail.body.encoded).to include("Dear #{@provider_user.full_name}")
+      expect(@mail.body.encoded).to include("Dear #{provider_user.full_name}")
     end
 
     it 'includes the candidate name' do
-      expect(@mail.body.encoded).to include("#{@application_choice.application_form.full_name} submitted an application for")
+      expect(@mail.body.encoded).to include("#{application_choice.application_form.full_name} submitted an application for")
     end
 
     it 'includes the course details' do
-      expect(@mail.body.encoded).to include(@application_choice.course.name)
-      expect(@mail.body.encoded).to include(@application_choice.course.code)
+      expect(@mail.body.encoded).to include(application_choice.course.name)
+      expect(@mail.body.encoded).to include(application_choice.course.code)
     end
 
     it 'includes a readable submission date' do
-      expect(@mail.body.encoded).to include("on #{@submission_date.to_s(:govuk_date).strip}")
+      submission_date = application_choice.application_form.submitted_at
+      expect(@mail.body.encoded).to include("on #{submission_date.to_s(:govuk_date).strip}")
     end
 
     it 'includes a link to the application' do
-      expect(@mail.body.encoded).to include(provider_interface_application_choice_url(application_choice_id: @application_choice.id))
+      expect(@mail.body.encoded).to include(provider_interface_application_choice_url(application_choice_id: application_choice.id))
     end
   end
 
   describe 'Send provider decision chaser email' do
     before do
-      @course_option = course_option_for_provider_code(provider_code: 'ABC')
-      @application_choice = create(:submitted_application_choice,
-                                   course_option: @course_option,
-                                   application_form:
-                                     create(
-                                       :completed_application_form,
-                                    ))
-      @provider_user = @application_choice.provider.provider_users.first
-      @mail = mailer.chase_provider_decision(@provider_user, @application_choice)
+      @mail = mailer.chase_provider_decision(provider_user, application_choice)
     end
 
     it 'sends an email with the correct subject' do
       expect(@mail.subject).to include(
         t('provider_application_waiting_for_decision.email.subject',
-          candidate_name: @application_choice.application_form.full_name),
+          candidate_name: application_choice.application_form.full_name),
         )
     end
 
     it 'addresses the provider user by name' do
-      expect(@mail.body.encoded).to include("Dear #{@provider_user.full_name}")
+      expect(@mail.body.encoded).to include("Dear #{provider_user.full_name}")
     end
 
     it 'includes the candidate name' do
-      expect(@mail.body.encoded).to include("#{@application_choice.application_form.full_name} submitted an application for")
+      expect(@mail.body.encoded).to include("#{application_choice.application_form.full_name} submitted an application for")
     end
 
     it 'includes the course details' do
-      expect(@mail.body.encoded).to include(@application_choice.course.name)
-      expect(@mail.body.encoded).to include(@application_choice.course.code)
+      expect(@mail.body.encoded).to include(application_choice.course.name)
+      expect(@mail.body.encoded).to include(application_choice.course.code)
     end
 
     it 'includes a readable submission date' do
-      submission_date = @application_choice.application_form.submitted_at
+      submission_date = application_choice.application_form.submitted_at
       expect(@mail.body.encoded).to include("on #{submission_date.to_s(:govuk_date).strip}")
     end
 
     it 'includes a link to the application' do
-      expect(@mail.body.encoded).to include(provider_interface_application_choice_url(application_choice_id: @application_choice.id))
+      expect(@mail.body.encoded).to include(provider_interface_application_choice_url(application_choice_id: application_choice.id))
     end
 
     it 'includes a readable RBD date' do
-      rbd_date = @application_choice.reject_by_default_at
+      rbd_date = application_choice.reject_by_default_at
       expect(@mail.body.encoded).to include("by #{rbd_date.to_s(:govuk_date).strip}")
     end
   end
