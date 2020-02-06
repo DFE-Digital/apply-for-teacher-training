fbf041a902ddf28d8f970c388dc197f4e5dfe694
Jake Benilov <benilov@gmail.com>
2020-02-06 22:39:23 +0000

    Extend the markdown renderer to pre-process with ERB
    If a file <template_name>.md.erb file exists, then put it through
    ERB first and then through the markdown renderer, otherwise just render it.

--- app/helpers/content_helper.rb
+++ app/helpers/content_helper.rb
@@ -1,6 +1,12 @@
 module ContentHelper
   def render_content_page(page_name)
-    @converted_markdown = Govuk::MarkdownRenderer.render(File.read("app/views/content/#{page_name}.md")).html_safe
+    path_to_source = "app/views/content/#{page_name}.md"
+    markdown_source = if File.exist?("#{path_to_source}.erb")
+                        ERB.new(File.read("#{path_to_source}.erb")).result(binding)
+                      else
+                        File.read(path_to_source)
+                      end
+    @converted_markdown = Govuk::MarkdownRenderer.render(markdown_source).html_safe
     @page_name = page_name
     render 'rendered_markdown_template'
   end
