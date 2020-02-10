8867668d47097dea23e7c1abff0ddd6f4a82ce46
Tijmen Brommet <tijmen@gmail.com>
2020-02-10 13:58:25 +0000

    Calculate reference chase date correctly
    We currently look at the `created_at` for references when deciding when
    to sent a chaser. We should look at the time was sent, otherwise we’re
    going to shorten the allowed period for referees.

--- app/services/get_referees_to_chase.rb
+++ app/services/get_referees_to_chase.rb
@@ -2,7 +2,7 @@ class GetRefereesToChase
   def self.call
     ApplicationReference
       .feedback_requested
-      .where(['created_at < ?', Time.zone.now - 5.days])
+      .where(['requested_at < ?', Time.zone.now - 5.days])
       .where.not(id: ChaserSent.reference_request.select(:chased_id))
   end
 end
