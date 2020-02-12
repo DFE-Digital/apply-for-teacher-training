7bad3fbf253f0c555731370bf414992c2004aee5
Theodor Vararu <theo@vararu.org>
2020-02-12 17:13:46 +0000

    Write basic Cypress end to end test

---
+++ cypress/integration/candidate.spec.js
@@ -0,0 +1,41 @@
+describe("Candidate", () => {
+  before(() => {
+    Cypress.Cookies.defaults({
+      whitelist: "_apply_for_postgraduate_teacher_training_session"
+    });
+  });
+
+  it("can load the home page", () => {
+    cy.visit("https://qa.apply-for-teacher-training.education.gov.uk/");
+    cy.contains("Start now");
+  });
+
+  it("can check for eligibility", () => {
+    cy.contains("Start now").click();
+    cy.contains("Check we’re ready for you to use this service");
+  });
+
+  it("can reach sign up page", () => {
+    cy.get(
+      "#candidate-interface-eligibility-form-eligible-citizen-yes-field"
+    ).click();
+    cy.get(
+      "#candidate-interface-eligibility-form-eligible-qualifications-yes-field"
+    ).click();
+
+    cy.contains("Continue").click();
+    cy.contains("Create an Apply for teacher training account");
+  });
+
+  it("can sign up", () => {
+    cy.get("#candidate-interface-sign-up-form-email-address-field").type(
+      "theo@vararu.org"
+    );
+    cy.get(
+      "#candidate-interface-sign-up-form-accept-ts-and-cs-true-field"
+    ).click();
+
+    cy.contains("Continue").click();
+    cy.contains("Check your email");
+  });
+});
