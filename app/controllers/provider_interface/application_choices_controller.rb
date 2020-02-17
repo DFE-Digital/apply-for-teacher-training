b87239d3c237147f6c732800b37c7c20fc5ba840
willmcb <william.mcbrien@digital.education.gov.uk>
2020-02-17 14:38:42 +0000

    Refactor sort params extraction

--- app/controllers/provider_interface/application_choices_controller.rb
+++ app/controllers/provider_interface/application_choices_controller.rb
@@ -1,11 +1,11 @@
 module ProviderInterface
   class ApplicationChoicesController < ProviderInterfaceController
     def index
-      @sort_order = params[:sort_order] ||= 'desc'
-      @sort_by = params[:sort_by] ||= 'last-updated'
+      @sort_order = params[:sort_order].eql?('asc') ? 'asc' : 'desc'
+      @sort_by = params[:sort_by].presence || 'last-updated'
 
       application_choices = GetApplicationChoicesForProviders.call(providers: current_provider_user.providers)
-                              .order(query_mapping[@sort_by])
+        .order(ordering_arguments(@sort_by, @sort_order))
 
       @application_choices = application_choices
     end
@@ -17,12 +17,12 @@ module ProviderInterface
 
   private
 
-    def query_mapping
+    def ordering_arguments(sort_by, sort_order)
       {
-        'course' => { 'courses.name' => @sort_order },
-        'last-updated' => { 'application_choices.updated_at' => @sort_order },
-        'name' => { 'last_name' => @sort_order, 'first_name' => @sort_order },
-      }
+        'course' => { 'courses.name' => sort_order },
+        'last-updated' => { 'application_choices.updated_at' => sort_order },
+        'name' => { 'last_name' => sort_order, 'first_name' => sort_order },
+      }[sort_by]
     end
   end
 end
