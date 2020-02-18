bf14ef089ba64d32a9823ad944fe266729c873bb
David Gisbey <davidgisbey@gmail.com>
2020-02-18 12:30:51 +0000

    Refactor bug caused by SendCandidateRejectionEmail
    This service is currently triggering sentry errors.
    This is due to a else statement failing due to
    invalid data on QA

--- app/services/send_candidate_rejection_email.rb
+++ app/services/send_candidate_rejection_email.rb
@@ -7,16 +7,19 @@ class SendCandidateRejectionEmail
 
   def call
     candidate_application_choices = application_choice.application_form.application_choices
-    number_of_pending_decisions = candidate_application_choices.select(&:awaiting_provider_decision?).count
+    number_of_pending_decisions = candidate_application_choices.awaiting_provider_decision.count
+    number_of_offers = candidate_application_choices.offer.count
 
     if candidate_application_choices.all?(&:rejected?)
       CandidateMailer.send(:application_rejected_all_rejected, application_choice).deliver_later
+      add_audit_comment(application_choice)
     elsif number_of_pending_decisions.positive?
       CandidateMailer.send(:application_rejected_awaiting_decisions, application_choice).deliver_later
-    else
+      add_audit_comment(application_choice)
+    elsif number_of_offers.positive?
       CandidateMailer.send(:application_rejected_offers_made, application_choice).deliver_later
+      add_audit_comment(application_choice)
     end
-    add_audit_comment(application_choice)
   end
 
 private
