f83d3830a75e329b7fee16110e99dcea74439070
Theodor Vararu <theo@vararu.org>
2020-02-18 13:09:00 +0000

    Refactor all render calls to use new syntax
    ActionView-Component@1.10.0 deprecates the old syntax.
    
    Done by using `s/render\((.*)Component,\s+(.*)\)/render($1Component.new($2))/`

--- app/components/candidate_interface/offer_review_component.rb
+++ app/components/candidate_interface/offer_review_component.rb
@@ -43,7 +43,7 @@ module CandidateInterface
     def conditions_row
       {
         key: 'Conditions',
-        value: render(OfferConditionsReviewComponent, conditions: @course_choice.offer['conditions'], provider: @course_choice.course.provider.name),
+        value: render(OfferConditionsReviewComponent.new(conditions: @course_choice.offer['conditions'], provider: @course_choice.course.provider.name)),
       }
     end
 
