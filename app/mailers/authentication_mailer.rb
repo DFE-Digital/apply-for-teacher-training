2f2c6ba02f86d76e5cd51a1930f8d09a431ed753
Steve Hook <steve.hook@gmail.com>
2020-02-20 12:55:03 +0000

    Enforce feature flag on mailer magic link generation

--- app/mailers/authentication_mailer.rb
+++ app/mailers/authentication_mailer.rb
@@ -1,9 +1,6 @@
 class AuthenticationMailer < ApplicationMailer
   def sign_up_email(candidate:, token:)
-    @magic_link = candidate_interface_authenticate_url(
-      token: token,
-      u: Encryptor.encrypt(candidate.id),
-    )
+    @magic_link = candidate_interface_authenticate_url(magic_link_params(token, candidate))
 
     view_mail(GENERIC_NOTIFY_TEMPLATE,
               to: candidate.email_address,
@@ -12,10 +9,7 @@ class AuthenticationMailer < ApplicationMailer
   end
 
   def sign_in_email(candidate:, token:)
-    @magic_link = candidate_interface_authenticate_url(
-      token: token,
-      u: Encryptor.encrypt(candidate.id),
-    )
+    @magic_link = candidate_interface_authenticate_url(magic_link_params(token, candidate))
 
     view_mail(GENERIC_NOTIFY_TEMPLATE,
               to: candidate.email_address,
@@ -26,5 +20,13 @@ class AuthenticationMailer < ApplicationMailer
     view_mail(GENERIC_NOTIFY_TEMPLATE,
               to: to,
               subject: t('authentication.sign_in_without_account.email.subject'))
+  end
+
+private
+
+  def magic_link_params(token, candidate)
+    params = { token: token }
+    params[:u] = Encryptor.encrypt(candidate.id) if FeatureFlag.active?('improved_expired_token_flow')
+    params
   end
 end
