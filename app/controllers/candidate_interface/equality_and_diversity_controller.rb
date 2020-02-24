6444c7e4985ad6a5a63e1f30b3ce9b3e173ef56d
Wen Ting Wang <chubberlisk@outlook.com>
2020-02-24 16:44:48 +0000

    Add ability to choose and update sex for equality and diversity
    Co-authored-by: Csaba Gyorfi <csaba@madetech.com>

--- app/controllers/candidate_interface/equality_and_diversity_controller.rb
+++ app/controllers/candidate_interface/equality_and_diversity_controller.rb
@@ -1,5 +1,27 @@
 module CandidateInterface
   class EqualityAndDiversityController < CandidateInterfaceController
     def start; end
+
+    def edit_sex
+      @sex = EqualityAndDiversity::SexForm.build_from_application(current_application)
+    end
+
+    def update_sex
+      @sex = EqualityAndDiversity::SexForm.new(sex: sex_param)
+
+      if @sex.save(current_application)
+        redirect_to candidate_interface_review_equality_and_diversity_path
+      else
+        render :edit_sex
+      end
+    end
+
+    def review; end
+
+  private
+
+    def sex_param
+      params.dig(:candidate_interface_equality_and_diversity_sex_form, :sex)
+    end
   end
 end
--- spec/system/candidate_interface/candidate_entering_equality_and_diversity_information_spec.rb
+++ spec/system/candidate_interface/candidate_entering_equality_and_diversity_information_spec.rb
@@ -14,6 +14,24 @@ RSpec.feature 'Entering their equality and diversity information' do
 
     when_i_choose_not_to_complete_equality_and_diversity
     then_i_can_submit_my_application
+
+    when_i_am_on_the_equality_and_diversity_page
+    and_i_choose_to_complete_equality_and_diversity
+    then_i_am_asked_to_choose_my_sex
+
+    when_i_try_and_submit_without_choosing_my_sex
+    then_i_see_an_error_to_choose_my_sex
+
+    when_i_choose_my_sex
+    and_i_click_on_continue
+    then_i_can_review_my_answer
+
+    when_i_click_change_sex
+    then_i_am_asked_to_choose_my_sex
+
+    when_i_choose_a_different_sex
+    and_i_click_on_continue
+    then_i_can_review_my_updated_sex
   end
 
   def given_i_am_signed_in
@@ -53,5 +71,51 @@ RSpec.feature 'Entering their equality and diversity information' do
 
   def then_i_can_submit_my_application
     expect(page).to have_content('Submit application')
+  end
+
+  def when_i_am_on_the_equality_and_diversity_page
+    visit candidate_interface_start_equality_and_diversity_path
+  end
+
+  def and_i_choose_to_complete_equality_and_diversity
+    click_link 'Continue'
+  end
+
+  def then_i_am_asked_to_choose_my_sex
+    expect(page).to have_content('What is your sex?')
+  end
+
+  def when_i_try_and_submit_without_choosing_my_sex
+    click_button 'Continue'
+  end
+
+  def then_i_see_an_error_to_choose_my_sex
+    expect(page).to have_content('Choose your sex')
+  end
+
+  def when_i_choose_my_sex
+    choose 'Male'
+  end
+
+  def and_i_click_on_continue
+    click_button 'Continue'
+  end
+
+  def then_i_can_review_my_answer
+    expect(page).to have_content('Check your answers')
+    expect(page).to have_content('Male')
+  end
+
+  def when_i_click_change_sex
+    click_link 'Change sex'
+  end
+
+  def when_i_choose_a_different_sex
+    choose 'Female'
+  end
+
+  def then_i_can_review_my_updated_sex
+    expect(page).to have_content('Check your answers')
+    expect(page).to have_content('Female')
   end
 end
