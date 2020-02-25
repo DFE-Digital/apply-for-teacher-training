1d7519cc2a14789b2940ba7a46dbe9a3bfbb8041
Steve Hook <steve.hook@gmail.com>
2020-02-25 14:56:17 +0000

    Add kaminari

--- app/controllers/provider_interface/application_choices_controller.rb
+++ app/controllers/provider_interface/application_choices_controller.rb
@@ -6,6 +6,8 @@ module ProviderInterface
 
       application_choices = GetApplicationChoicesForProviders.call(providers: current_provider_user.providers)
         .order(ordering_arguments(@sort_by, @sort_order))
+        .page(1)
+        .per(10)
 
       if FeatureFlag.active?('provider_application_filters')
         raise 'feature not implemented yet'
