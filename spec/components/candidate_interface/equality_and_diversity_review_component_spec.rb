f7192a76699992db9d1a331499a965634b8554a1
Wen Ting Wang <chubberlisk@outlook.com>
2020-02-25 12:42:54 +0000

    Update equality and diversity review component to show disabilities
    Co-authored-by: Csaba Gyorfi <csaba@madetech.com>

--- spec/components/candidate_interface/equality_and_diversity_review_component_spec.rb
+++ spec/components/candidate_interface/equality_and_diversity_review_component_spec.rb
@@ -4,15 +4,32 @@ RSpec.describe CandidateInterface::EqualityAndDiversityReviewComponent do
   let(:application_form) do
     build_stubbed(
       :application_form,
-      equality_and_diversity: { 'sex' => 'male' },
+      equality_and_diversity: { 'sex' => 'male', 'disabilities' => %w(no) },
     )
   end
 
-  it 'renders component with correct equality and diversity information' do
-    result = render_inline(described_class.new(application_form: application_form))
+  context 'when there are disabilities' do
+    it 'renders component with correct equality and diversity information' do
+      application_form.equality_and_diversity = { 'sex' => 'male', 'disabilities' => ['Blind', 'Deaf', 'Learning Difficulties'] }
 
-    expect(result.css('.govuk-summary-list__key').text).to include('Sex')
-    expect(result.css('.govuk-summary-list__value').text).to include('Male')
+      result = render_inline(described_class.new(application_form: application_form))
+
+      expect(result.css('.govuk-summary-list__key').text).to include('Sex')
+      expect(result.css('.govuk-summary-list__value').text).to include('Male')
+      expect(result.css('.govuk-summary-list__key').text).to include('Disability')
+      expect(result.css('.govuk-summary-list__value').text).to include('Yes (Blind, Deaf and Learning Difficulties)')
+    end
+  end
+
+  context 'when there no disabilities' do
+    it 'renders component with correct equality and diversity information' do
+      application_form.equality_and_diversity = { 'sex' => 'male', 'disabilities' => [] }
+
+      result = render_inline(described_class.new(application_form: application_form))
+
+      expect(result.css('.govuk-summary-list__key').text).to include('Disability')
+      expect(result.css('.govuk-summary-list__value').text).to include('No')
+    end
   end
 
   context 'when editable' do
