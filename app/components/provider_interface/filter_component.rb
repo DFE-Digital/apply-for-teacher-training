cd1e4c007d69d0d3d074d0289298a16760b3bdf7
willmcb <william.mcbrien@digital.education.gov.uk>
2020-02-26 10:57:32 +0000

    Refactor filter component
    Use an array of values to call the FilterCheckboxComponent to
    DRY out the filter_component.html.erb file.

--- app/components/provider_interface/filter_component.rb
+++ app/components/provider_interface/filter_component.rb
@@ -9,5 +9,14 @@ module ProviderInterface
       @current_sort_by = page_state.sort_by
       @filter_options = page_state.filter_options
     end
+
+    def status_filter_checkbox_names_text
+      [
+        ["pending_conditions", "Accepted"], ["recruited", "Conditions met"],
+        ["declined", "Declined"], ["awaiting_provider_decision", "New"],
+        ["offer", "Offered"], ["rejected", "Rejected"],
+        ["withdrawn", "Application withdrawn"], ["offer_withdrawn", "Offer withdrawn"]
+      ]
+    end
   end
 end
