50c1efd2fa615b916db0c2c2b5079ed0ff810a52
willmcb <william.mcbrien@digital.education.gov.uk>
2020-02-26 12:58:17 +0000

    Add FilterCheckboxComponent

---
+++ app/components/provider_interface/filter_checkbox_component.rb
@@ -0,0 +1,17 @@
+module ProviderInterface
+  class FilterCheckboxComponent < ActionView::Component::Base
+    include ViewHelper
+
+    attr_reader :name, :text
+
+    def initialize(name:, text:, filter_options:)
+      @name = name
+      @text = text
+      @filter_options = filter_options
+    end
+
+    def should_be_checked
+      @filter_options.include?(@name) ? 'checked' : nil
+    end
+  end
+end
