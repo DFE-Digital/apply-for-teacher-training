cc3a55f87df12625be664c3c0b5f77f1744d21f5
Steve Hook <steve.hook@gmail.com>
2020-02-27 10:53:29 +0000

    Refactor paginator into a component
    This is partly so that we can customise the summary message while
    including a paginator in view remains a one-liner

---
+++ app/components/paginator_component.rb
@@ -0,0 +1,26 @@
+class PaginatorComponent < ActionView::Component::Base
+  attr_reader :scope
+
+  def initialize(scope:)
+    @scope = scope
+  end
+
+  def render?
+    @scope.total_pages > 1
+  end
+
+  def page_start
+    ((@scope.current_page - 1) * @scope.limit_value) + 1
+  end
+
+  def page_end
+    [
+      @scope.current_page * @scope.limit_value,
+      total,
+    ].min
+  end
+
+  def total
+    @scope.total_count
+  end
+end
---
+++ spec/components/paginator_component_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe PaginatorComponent do
+  def rendered_component(current_page: 1, total_count: 12)
+    page_size = 25
+    relation = ApplicationChoice.all.page(1)
+    scope = object_double(
+      relation,
+      total_count: total_count,
+      limit_value: page_size,
+      current_page: current_page,
+      total_pages: (total_count.to_f / page_size).ceil,
+    )
+    component = described_class.new(scope: scope)
+    allow(component).to receive(:paginate).and_return('paginator')
+    render_inline(component)
+  end
+
+  describe 'rendering PaginatorComponent' do
+    context 'when there is only one page' do
+      it 'renders nothing' do
+        expect(rendered_component.text).to eq ''
+      end
+    end
+
+    context 'when we are on the first of two pages' do
+      it 'renders correct summary message' do
+        expect(rendered_component(current_page: 1, total_count: 29).text).to include 'Showing 1 to 25 of 29'
+      end
+    end
+
+    context 'when we are on the second of two pages' do
+      it 'renders correct summary message' do
+        expect(rendered_component(current_page: 2, total_count: 29).text).to include 'Showing 26 to 29 of 29'
+      end
+    end
+
+    context 'when we are on the second of three pages' do
+      it 'renders correct summary message' do
+        expect(rendered_component(current_page: 2, total_count: 59).text).to include 'Showing 26 to 50 of 59'
+      end
+    end
+  end
+end
