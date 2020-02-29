870ee868a22a26bbdef5cd93ceaf53aaed13edb1
csabagyorfi <csaba@madetech.com>
2020-02-29 15:09:30 +0000

    Refactor ethnic background form

--- spec/models/candidate_interface/equality_and_diversity/ethnic_background_form_spec.rb
+++ spec/models/candidate_interface/equality_and_diversity/ethnic_background_form_spec.rb
@@ -15,22 +15,35 @@ RSpec.describe CandidateInterface::EqualityAndDiversity::EthnicBackgroundForm, t
 
     context 'when ethnic background is unlisted' do
       it 'creates an object with ethnic background set to another and other background is ethnic background' do
-        application_form = build_stubbed(:application_form, equality_and_diversity: { 'ethnic_group' => 'Asian or Asian British', 'ethnic_background' => 'Unlisted ethnic background' })
+        application_form = build_stubbed(:application_form,
+                                         equality_and_diversity: { 'ethnic_group' => 'Asian or Asian British', 'ethnic_background' => 'Unlisted ethnic background' })
 
         form = CandidateInterface::EqualityAndDiversity::EthnicBackgroundForm.build_from_application(application_form)
 
-        expect(form.ethnic_background).to eq('Another Asian or Asian British background')
+        expect(form.ethnic_background).to eq('Another Asian background')
         expect(form.other_background).to eq('Unlisted ethnic background')
       end
     end
 
     context 'when ethnic background is another background' do
       it 'creates an object with ethnic background' do
-        application_form = build_stubbed(:application_form, equality_and_diversity: { 'ethnic_group' => 'Asian or Asian British', 'ethnic_background' => 'Another Asian or Asian British background' })
+        application_form = build_stubbed(:application_form,
+                                         equality_and_diversity: { 'ethnic_group' => 'Asian or Asian British', 'ethnic_background' => 'Another Asian background' })
 
         form = CandidateInterface::EqualityAndDiversity::EthnicBackgroundForm.build_from_application(application_form)
 
-        expect(form.ethnic_background).to eq('Another Asian or Asian British background')
+        expect(form.ethnic_background).to eq('Another Asian background')
+        expect(form.other_background).to eq(nil)
+      end
+    end
+
+    context 'when ethnic background is not present' do
+      it 'creates an object with empty ethnic background' do
+        application_form = build_stubbed(:application_form, equality_and_diversity: { 'ethnic_group' => 'Asian or Asian British', 'ethnic_background' => nil })
+
+        form = CandidateInterface::EqualityAndDiversity::EthnicBackgroundForm.build_from_application(application_form)
+
+        expect(form.ethnic_background).to eq(nil)
         expect(form.other_background).to eq(nil)
       end
     end
@@ -76,7 +89,7 @@ RSpec.describe CandidateInterface::EqualityAndDiversity::EthnicBackgroundForm, t
 
     context 'when ethnic background is another background' do
       it 'updates the application form with the other background value if other background provided' do
-        form = CandidateInterface::EqualityAndDiversity::EthnicBackgroundForm.new(ethnic_background: 'Another Asian or Asian British background', other_background: 'Unlisted ethnic background')
+        form = CandidateInterface::EqualityAndDiversity::EthnicBackgroundForm.new(ethnic_background: 'Another Asian background', other_background: 'Unlisted ethnic background')
 
         form.save(application_form)
 
@@ -84,11 +97,11 @@ RSpec.describe CandidateInterface::EqualityAndDiversity::EthnicBackgroundForm, t
       end
 
       it 'updates the application form with the ethnic background value if other background is not provided' do
-        form = CandidateInterface::EqualityAndDiversity::EthnicBackgroundForm.new(ethnic_background: 'Another Asian or Asian British background', other_background: nil)
+        form = CandidateInterface::EqualityAndDiversity::EthnicBackgroundForm.new(ethnic_background: 'Another Asian background', other_background: nil)
 
         form.save(application_form)
 
-        expect(application_form.equality_and_diversity).to eq('ethnic_group' => 'Asian or Asian British', 'ethnic_background' => 'Another Asian or Asian British background')
+        expect(application_form.equality_and_diversity).to eq('ethnic_group' => 'Asian or Asian British', 'ethnic_background' => 'Another Asian background')
       end
     end
   end
