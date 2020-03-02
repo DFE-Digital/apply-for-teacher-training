fb737d8f95a3f0ed02a5f963b6d56970f559b664
Malcolm Baig <malcolm.baig@gmail.com>
2020-03-02 10:26:17 +0000

    Alphabetise the list of feature flags

--- app/services/feature_flag.rb
+++ app/services/feature_flag.rb
@@ -1,32 +1,32 @@
 class FeatureFlag
   FEATURES = %w[
-    pilot_open
-    force_ok_computer_to_fail
-    training_with_a_disability
-    edit_application
-    send_dfe_sign_in_invitations
-    improved_expired_token_flow
-    work_breaks
-    experimental_api_features
-    confirm_conditions
-    show_new_referee_needed
-    automated_referee_chaser
+    application_withrawn_provider_email
+    automated_decline_by_default_candidate_chaser
     automated_provider_chaser
-    provider_change_response
-    send_reference_confirmation_email
+    automated_referee_chaser
     automated_referee_replacement
     candidate_rejected_by_provider_email
-    notify_candidate_of_new_reference
-    automated_decline_by_default_candidate_chaser
+    confirm_conditions
     decline_by_default_notification_to_candidate
-    offer_accepted_provider_emails
     decline_by_default_notification_to_provider
-    application_withrawn_provider_email
-    offer_declined_provider_emails
-    equality_and_diversity
-    provider_application_filters
-    provider_interface_pagination
     dfe_sign_in_incident
+    edit_application
+    equality_and_diversity
+    experimental_api_features
+    force_ok_computer_to_fail
+    improved_expired_token_flow
+    notify_candidate_of_new_reference
+    offer_accepted_provider_emails
+    offer_declined_provider_emails
+    pilot_open
+    provider_application_filters
+    provider_change_response
+    provider_interface_pagination
+    send_dfe_sign_in_invitations
+    send_reference_confirmation_email
+    show_new_referee_needed
+    training_with_a_disability
+    work_breaks
   ].freeze
 
   def self.activate(feature_name)
