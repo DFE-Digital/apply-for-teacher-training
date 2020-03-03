3a533739b9b170b3c5daea7b484d8088684e8ca6
willmcb <william.mcbrien@digital.education.gov.uk>
2020-03-03 13:37:13 +0000

    Add ability to toggle filter

--- app/controllers/provider_interface/application_choices_controller.rb
+++ app/controllers/provider_interface/application_choices_controller.rb
@@ -10,7 +10,9 @@ module ProviderInterface
       application_choices = application_choices.page(params[:page] || 1)
 
       if FeatureFlag.active?('provider_application_filters')
-        raise 'feature not implemented yet'
+         @available_filters = available_filters
+         @filter_visible = params[:filter_visible] ||= 'true'
+         @application_choices = application_choices
       else
         @application_choices = application_choices
       end
@@ -29,6 +31,72 @@ module ProviderInterface
         'last-updated' => { 'application_choices.updated_at' => sort_order },
         'name' => { 'last_name' => sort_order, 'first_name' => sort_order },
       }[sort_by]
+    end
+
+    def available_filters
+      [
+        {
+          heading: 'status',
+          checkbox_config: [
+            {
+              name: 'pending-conditions',
+              text: 'Accepted',
+              value: 'pending_conditions',
+            },
+            {
+              name: 'recruited',
+              text: 'Conditions met',
+              value: 'recruited',
+            },
+            {
+              name: 'declined',
+              text: 'Declined',
+              value: 'declined',
+            },
+            {
+              name: 'awaiting-provider-decision',
+              text: 'New',
+              value: 'awaiting_provider_decision',
+            },
+            {
+              name: 'offer',
+              text: 'Offered',
+              value: 'offer',
+            },
+            {
+              name: 'rejected',
+              text: 'Rejected',
+              value: 'rejected',
+            },
+            {
+              name: 'withdrawn',
+              text: 'Application withdrawn',
+              value: 'withdrawn',
+            },
+            {
+              name: 'offer-withdrawn',
+              text: 'Withdrawn by us',
+              value: 'offer_withdrawn',
+            },
+          ],
+ },
+        {
+          heading: 'provider',
+          checkbox_config: [
+            {
+              name: 'somerset-scitt-consortium',
+              text: 'Somerset SCITT consortium',
+              value: '1',
+            },
+            {
+              name: 'the-beach-teaching-school',
+              text: 'The Beach Teaching School',
+              value: '2',
+            },
+
+          ],
+ },
+      ]
     end
   end
 end
