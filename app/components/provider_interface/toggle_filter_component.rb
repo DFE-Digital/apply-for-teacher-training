e39dc545729ac12973918351061dad31b7f8d707
willmcb <william.mcbrien@digital.education.gov.uk>
2020-03-03 16:44:04 +0000

    Make hide and show respect filtering

--- app/components/provider_interface/toggle_filter_component.rb
+++ app/components/provider_interface/toggle_filter_component.rb
@@ -2,12 +2,13 @@ module ProviderInterface
   class ToggleFilterComponent < ActionView::Component::Base
     include ViewHelper
 
-    attr_reader :sort_order, :current_sort_by, :filter_visible, :filter_options
+    attr_reader :sort_order, :current_sort_by, :filter_visible, :filter_options, :filter_selections
 
-    def initialize(sort_order:, current_sort_by:, filter_visible:)
+    def initialize(sort_order:, current_sort_by:, filter_visible:, filter_selections:)
       @sort_order = sort_order
       @current_sort_by = current_sort_by
       @filter_visible = filter_visible
+      @filter_selections = filter_selections
     end
 
     def toggle_filter
