5a4b08c48a25d7769e35cd6e0e43bdaf3c764c65
Steve Hook <steve.hook@gmail.com>
2020-03-04 16:11:42 +0000

    Add a component preview

--- spec/components/provider_interface/work_history_component_spec.rb
+++ spec/components/provider_interface/work_history_component_spec.rb
@@ -1,11 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe ProviderInterface::WorkHistoryComponent do
-  it 'renders each history item' do
-    application_form = instance_double(ApplicationForm)
-    allow(application_form).to receive(:application_work_experiences).and_return([])
+  context 'with an empty history' do
+    it 'renders nothing' do
+      application_form = instance_double(ApplicationForm)
+      allow(application_form).to receive(:application_work_experiences).and_return([])
+      allow(application_form).to receive(:application_work_history_breaks).and_return([])
 
-    rendered = render_inline(described_class, application_form: application_form)
-    expect(rendered).to eq :foo
+      rendered = render_inline(described_class, application_form: application_form)
+      expect(rendered.text).to eq ''
+    end
+  end
+
+  context 'with work experiences' do
+    it 'renders nothing' do
+      application_form = instance_double(ApplicationForm)
+      experiences = [
+        build(
+          :application_work_experience,
+          start_date: Date.new(2019, 10, 1),
+          end_date: Date.new(2019, 12, 1),
+        ),
+        build(
+          :application_work_experience,
+          start_date: Date.new(2020, 1, 1),
+        ),
+      ]
+      allow(application_form).to receive(:application_work_experiences).and_return(experiences)
+      allow(application_form).to receive(:application_work_history_breaks).and_return([])
+
+      rendered = render_inline(described_class, application_form: application_form)
+      expect(rendered.text).to eq ''
+    end
   end
 end
