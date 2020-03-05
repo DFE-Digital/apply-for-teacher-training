111463b4d1738b67261b84013601860bb6f8f90a
Steve Hook <steve.hook@gmail.com>
2020-03-05 17:42:56 +0000

    Additional specs for volunteering
    Not sure at this stage whether volunteering will be added to a single
    timeline of experiences or remain as a separate section in the profile.

--- spec/components/provider_interface/work_history_component_spec.rb
+++ spec/components/provider_interface/work_history_component_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe ProviderInterface::WorkHistoryComponent do
       allow(application_form).to receive(:application_work_experiences).and_return([])
       allow(application_form).to receive(:application_work_history_breaks).and_return([])
 
-      rendered = render_inline(described_class, application_form: application_form)
+      rendered = render_inline(described_class.new(application_form: application_form))
       expect(rendered.text).to eq ''
     end
   end
@@ -40,7 +40,7 @@ RSpec.describe ProviderInterface::WorkHistoryComponent do
       allow(application_form).to receive(:application_work_experiences).and_return(experiences)
       allow(application_form).to receive(:application_work_history_breaks).and_return([])
 
-      rendered = render_inline(described_class, application_form: application_form)
+      rendered = render_inline(described_class.new(application_form: application_form))
       expect(rendered.text).to include 'October 2014 - December 2019'
       expect(rendered.text).to include 'Sheep herder - Full-time'
       expect(rendered.text).to include 'January 2020 - Present'
@@ -78,7 +78,7 @@ RSpec.describe ProviderInterface::WorkHistoryComponent do
       allow(application_form).to receive(:application_work_experiences).and_return(experiences)
       allow(application_form).to receive(:application_work_history_breaks).and_return([])
 
-      rendered = render_inline(described_class, application_form: application_form)
+      rendered = render_inline(described_class.new(application_form: application_form))
       expect(rendered.text).to include 'October 2014 - March 2018'
       expect(rendered.text).to include 'Sheep herder - Full-time'
       expect(rendered.text).to include 'Unexplained break in work history (1 year and 10 months)'
@@ -88,8 +88,16 @@ RSpec.describe ProviderInterface::WorkHistoryComponent do
   end
 
   context 'with work experiences and explained work break' do
-    it 'renders work experience details and unexplained break' do
+    it 'renders work experience details and explained break' do
       application_form = instance_double(ApplicationForm)
+      breaks = [
+        build(
+          :application_work_history_break,
+          start_date: Date.new(2018, 2, 1),
+          end_date: Date.new(2019, 12, 1),
+          reason: 'I found pig farming very stressful and needed to take time off work',
+        ),
+      ]
       experiences = [
         build(
           :application_work_experience,
@@ -100,12 +108,6 @@ RSpec.describe ProviderInterface::WorkHistoryComponent do
           working_pattern: '',
           organisation: 'Bobs Farm',
           details: 'Livestock management',
-        ),
-        build(
-          :application_work_history_break,
-          start_date: Date.new(2018, 2, 1),
-          end_date: Date.new(2019, 12, 1),
-          reason: 'I found pig farming very stressful and needed to take time off work',
         ),
         build(
           :application_work_experience,
@@ -119,15 +121,58 @@ RSpec.describe ProviderInterface::WorkHistoryComponent do
         ),
       ]
       allow(application_form).to receive(:application_work_experiences).and_return(experiences)
-      allow(application_form).to receive(:application_work_history_breaks).and_return([])
+      allow(application_form).to receive(:application_work_history_breaks).and_return(breaks)
 
-      rendered = render_inline(described_class, application_form: application_form)
+      rendered = render_inline(described_class.new(application_form: application_form))
       expect(rendered.text).to include 'October 2014 - February 2018'
       expect(rendered.text).to include 'Sheep herder - Full-time'
       expect(rendered.text).to include 'Explained break in work history (1 year and 10 months)'
       expect(rendered.text).to include 'I found pig farming very stressful and needed to take time off work'
       expect(rendered.text).to include 'January 2020 - Present'
       expect(rendered.text).to include 'Pig herder - Part-time'
+    end
+  end
+
+  context 'with work, volunteering experiences and explained work break' do
+    it 'renders work, volunteering experience details and explained break' do
+      application_form = instance_double(ApplicationForm)
+      breaks = [
+        build(
+          :application_work_history_break,
+          start_date: Date.new(2018, 2, 1),
+          end_date: Date.new(2019, 12, 1),
+          reason: 'I found pig farming very stressful and needed to take time off work',
+        ),
+      ]
+      experiences = [
+        build(
+          :application_work_experience,
+          start_date: Date.new(2014, 10, 1),
+          end_date: Date.new(2018, 2, 1),
+          role: 'Sheep herder',
+          commitment: 'full_time',
+          working_pattern: '',
+          organisation: 'Bobs Farm',
+          details: 'Livestock management',
+        ),
+        build(
+          :application_volunteering_experience,
+          start_date: Date.new(2020, 1, 1),
+          end_date: nil,
+          details: 'Childrens entertainer',
+          working_with_children: true,
+        ),
+      ]
+      allow(application_form).to receive(:application_work_experiences).and_return(experiences)
+      allow(application_form).to receive(:application_work_history_breaks).and_return(breaks)
+
+      rendered = render_inline(described_class.new(application_form: application_form))
+      expect(rendered.text).to include 'October 2014 - February 2018'
+      expect(rendered.text).to include 'Sheep herder - Full-time'
+      expect(rendered.text).to include 'Explained break in work history (1 year and 10 months)'
+      expect(rendered.text).to include 'I found pig farming very stressful and needed to take time off work'
+      expect(rendered.text).to include 'January 2020 - Present'
+      expect(rendered.text).to include 'Childrens entertainer'
     end
   end
 end
