8ac70e44e4b649cd7ffe0ea307d0767247560c52
Steve Hook <steve.hook@gmail.com>
2020-03-06 17:47:16 +0000

    Fix specs for VolunteeringHistoryComponent

--- spec/components/provider_interface/volunteering_history_component_spec.rb
+++ spec/components/provider_interface/volunteering_history_component_spec.rb
@@ -19,24 +19,28 @@ RSpec.describe ProviderInterface::VolunteeringHistoryComponent do
           :application_volunteering_experience,
           start_date: Date.new(2020, 1, 1),
           end_date: nil,
-          details: 'Childrens entertainer',
+          role: 'Childrens entertainer',
+          details: 'I performed magic tricks at parties',
           working_with_children: true,
+          working_pattern: '3 hours per week',
         ),
         build(
           :application_volunteering_experience,
           start_date: Date.new(2018, 3, 1),
           end_date: Date.new(2018, 6, 30),
-          details: 'Playgroup helper',
+          role: 'Playgroup helper',
+          details: 'I helped out a local Playgroup',
           working_with_children: true,
+          working_pattern: '1 day per week',
         ),
       ]
       allow(application_form).to receive(:application_volunteering_experiences).and_return(experiences)
 
       rendered = render_inline(described_class.new(application_form: application_form))
-      expect(rendered.text).to include 'January 2018 - June 2018'
-      expect(rendered.text).to include 'Playgroup helper'
+      expect(rendered.text).to include 'March 2018 - June 2018'
+      expect(rendered.text).to include 'Playgroup helper - 1 day per week'
       expect(rendered.text).to include 'January 2020 - Present'
-      expect(rendered.text).to include 'Childrens entertainer'
+      expect(rendered.text).to include 'Childrens entertainer - 3 hours per week'
     end
   end
 end
