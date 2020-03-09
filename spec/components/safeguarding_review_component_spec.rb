a708f43246a0c900e5c79b5cc13ac3bd90178420
Wen Ting Wang <chubberlisk@outlook.com>
2020-03-09 17:47:11 +0000

    Add component to review any safeguarding issues

---
+++ spec/components/safeguarding_review_component_spec.rb
@@ -0,0 +1,63 @@
+require 'rails_helper'
+
+RSpec.describe SafeguardingReviewComponent do
+  context 'when safeguarding issues has a value of "Yes"' do
+    it 'displays "Yes" for sharing safeguarding issues and "Not entered" for relevant information' do
+      application_form = build_stubbed(:application_form, safeguarding_issues: 'Yes')
+
+      result = render_inline(SafeguardingReviewComponent.new(application_form: application_form))
+
+      expect(result.text).to include('Do you want to share any information which could have an impact on your application?')
+      expect(result.text).to include('Yes')
+      expect(result.text).to include('Relevant information')
+      expect(result.text).to include('Not entered')
+    end
+  end
+
+  context 'when safeguarding issues has a value of "No"' do
+    it 'displays "No" for sharing safeguarding issues and "Not entered" for relevant information' do
+      application_form = build_stubbed(:application_form, safeguarding_issues: 'No')
+
+      result = render_inline(SafeguardingReviewComponent.new(application_form: application_form))
+
+      expect(result.text).to include('Do you want to share any information which could have an impact on your application?')
+      expect(result.text).to include('No')
+      expect(result.text).not_to include('Relevant information')
+    end
+  end
+
+  context 'when safeguarding issues has details' do
+    it 'displays "Yes" for sharing safeguarding issues and the details for relevant information' do
+      application_form = build_stubbed(:application_form, safeguarding_issues: 'I have a criminal conviction.')
+
+      result = render_inline(SafeguardingReviewComponent.new(application_form: application_form))
+
+      expect(result.text).to include('Do you want to share any information which could have an impact on your application?')
+      expect(result.text).to include('Yes')
+      expect(result.text).to include('Relevant information')
+      expect(result.text).to include('I have a criminal conviction.')
+    end
+  end
+
+  context 'when editable' do
+    it 'renders the component with change links' do
+      application_form = build_stubbed(:application_form, safeguarding_issues: 'Yes')
+
+      result = render_inline(SafeguardingReviewComponent.new(application_form: application_form, editable: true))
+
+      expect(result.text).to include('Change if you want to share any safeguarding issues')
+      expect(result.text).to include('Change relevant information for safeguarding issues')
+    end
+  end
+
+  context 'when not editable' do
+    it 'renders the component without change links' do
+      application_form = build_stubbed(:application_form, safeguarding_issues: 'Yes')
+
+      result = render_inline(SafeguardingReviewComponent.new(application_form: application_form, editable: false))
+
+      expect(result.text).not_to include('Change if you want to share any safeguarding issues')
+      expect(result.text).not_to include('Change relevant information for safeguarding issues')
+    end
+  end
+end
