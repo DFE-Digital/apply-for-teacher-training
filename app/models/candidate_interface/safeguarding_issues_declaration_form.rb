749f72a901c418a38e42dd0974cf138ae62f6822
Wen Ting Wang <chubberlisk@outlook.com>
2020-03-09 14:44:34 +0000

    Add #save method for SafeguardingIssuesDeclarationForm

--- app/models/candidate_interface/safeguarding_issues_declaration_form.rb
+++ app/models/candidate_interface/safeguarding_issues_declaration_form.rb
@@ -6,5 +6,15 @@ module CandidateInterface
 
     validates :share_safeguarding_issues, presence: true
     validates :safeguarding_issues, word_count: { maximum: 400 }
+
+    def save(application_form)
+      return false unless valid?
+
+      if share_safeguarding_issues == 'Yes' && safeguarding_issues.present?
+        application_form.update(safeguarding_issues: safeguarding_issues)
+      else
+        application_form.update(safeguarding_issues: share_safeguarding_issues)
+      end
+    end
   end
 end
--- spec/models/candidate_interface/safeguarding_issues_declaration_form_spec.rb
+++ spec/models/candidate_interface/safeguarding_issues_declaration_form_spec.rb
@@ -1,6 +1,89 @@
 require 'rails_helper'
 
 RSpec.describe CandidateInterface::SafeguardingIssuesDeclarationForm, type: :model do
+  describe '#save' do
+    let(:application_form) { create(:application_form) }
+
+    context 'when sharing safeguarding issues is blank' do
+      it 'returns false' do
+        form = CandidateInterface::SafeguardingIssuesDeclarationForm.new
+
+        expect(form.save(application_form)).to be(false)
+      end
+    end
+
+    context 'when sharing safeguarding issues is "No"' do
+      it 'returns true' do
+        form = CandidateInterface::SafeguardingIssuesDeclarationForm.new(share_safeguarding_issues: 'No')
+
+        expect(form.save(application_form)).to be(true)
+      end
+
+      it 'updates safeguarding issues of the application form to "No"' do
+        form = CandidateInterface::SafeguardingIssuesDeclarationForm.new(share_safeguarding_issues: 'No')
+
+        form.save(application_form)
+
+        expect(application_form.safeguarding_issues).to eq('No')
+      end
+    end
+
+    context 'when sharing safeguarding issues is "Yes" but no details provided' do
+      it 'returns true' do
+        form = CandidateInterface::SafeguardingIssuesDeclarationForm.new(
+          share_safeguarding_issues: 'Yes',
+          safeguarding_issues: '',
+        )
+
+        expect(form.save(application_form)).to be(true)
+      end
+
+      it 'updates safeguarding issues of the application form to provided issues if empty string' do
+        form = CandidateInterface::SafeguardingIssuesDeclarationForm.new(
+          share_safeguarding_issues: 'Yes',
+          safeguarding_issues: '',
+        )
+
+        form.save(application_form)
+
+        expect(application_form.safeguarding_issues).to eq('Yes')
+      end
+
+      it 'updates safeguarding issues of the application form to provided issues if nil' do
+        form = CandidateInterface::SafeguardingIssuesDeclarationForm.new(
+          share_safeguarding_issues: 'Yes',
+          safeguarding_issues: nil,
+        )
+
+        form.save(application_form)
+
+        expect(application_form.safeguarding_issues).to eq('Yes')
+      end
+    end
+
+    context 'when sharing safeguarding issues is "Yes" and details provided' do
+      it 'returns true' do
+        form = CandidateInterface::SafeguardingIssuesDeclarationForm.new(
+          share_safeguarding_issues: 'Yes',
+          safeguarding_issues: 'I have a criminal conviction.',
+        )
+
+        expect(form.save(application_form)).to be(true)
+      end
+
+      it 'updates safeguarding issues of the application form to provided issues' do
+        form = CandidateInterface::SafeguardingIssuesDeclarationForm.new(
+          share_safeguarding_issues: 'Yes',
+          safeguarding_issues: 'I have a criminal conviction.',
+        )
+
+        form.save(application_form)
+
+        expect(application_form.safeguarding_issues).to eq('I have a criminal conviction.')
+      end
+    end
+  end
+
   describe 'validations' do
     it { is_expected.to validate_presence_of(:share_safeguarding_issues) }
 
