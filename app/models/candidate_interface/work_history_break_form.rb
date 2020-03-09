56dd349b09b6dfed44e22ac00f5eb22f79978976
Wen Ting Wang <chubberlisk@outlook.com>
2020-03-09 09:47:00 +0000

    Add validation to prevent start date in the future for work breaks

--- app/models/candidate_interface/work_history_break_form.rb
+++ app/models/candidate_interface/work_history_break_form.rb
@@ -7,6 +7,7 @@ module CandidateInterface
                   :end_date_day, :end_date_month, :end_date_year, :reason
 
     validate :start_date_valid
+    validate :start_date_before_current_year_and_month, if: :start_date_valid?
     validate :end_date_valid
     validate :end_date_before_current_year_and_month, if: :end_date_valid?
     validate :start_date_before_end_date, if: :start_date_and_end_date_valid?
