c883843dbb76ae76739022062fd063a3c38e4a08
David Gisbey <davidgisbey@gmail.com>
2020-03-09 16:22:01 +0000

    Remove redundant tests

--- spec/models/application_form_spec.rb
+++ spec/models/application_form_spec.rb
@@ -79,50 +79,10 @@ RSpec.describe ApplicationForm do
       end
     end
 
-    context 'when a candidate has added a course choice' do
-      it 'returns false' do
-        application_form = create(:application_form)
-        create(:application_choice, application_form: application_form)
-        expect(application_form.blank_application?).to be_falsey
-      end
-    end
-
-    context 'when a candidate has added a job' do
+    context 'when a candidate has amended their application' do
       it 'returns false' do
         application_form = create(:application_form)
         create(:application_work_experience, application_form: application_form)
-        expect(application_form.blank_application?).to be_falsey
-      end
-    end
-
-    context 'when a candidate has added a volunteering experience' do
-      it 'returns false' do
-        application_form = create(:application_form)
-        create(:application_volunteering_experience, application_form: application_form)
-        expect(application_form.blank_application?).to be_falsey
-      end
-    end
-
-    context 'when a candidate has added a qualificaion' do
-      it 'returns false' do
-        application_form = create(:application_form)
-        create(:application_qualification, application_form: application_form)
-        expect(application_form.blank_application?).to be_falsey
-      end
-    end
-
-    context 'when a candidate has added a reference' do
-      it 'returns false' do
-        application_form = create(:application_form)
-        create(:reference, application_form: application_form)
-        expect(application_form.blank_application?).to be_falsey
-      end
-    end
-
-    context 'when a candidate has added a work history break' do
-      it 'returns false' do
-        application_form = create(:application_form)
-        create(:application_work_history_break, application_form: application_form)
         expect(application_form.blank_application?).to be_falsey
       end
     end
