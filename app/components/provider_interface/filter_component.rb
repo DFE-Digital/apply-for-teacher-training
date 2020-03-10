9688fa466dc73dfda7803e534b6c67265b0f67bb
willmcb <william.mcbrien@digital.education.gov.uk>
2020-03-10 16:59:58 +0000

    Remove methods that had bee refactored to different component

--- app/components/provider_interface/filter_component.rb
+++ app/components/provider_interface/filter_component.rb
@@ -15,22 +15,6 @@ module ProviderInterface
       applied_filters.dig(heading, name) ? true : false
     end
 
-    def build_tag_url_query_params(heading:, tag_value:, applied_filters:)
-      tag_applied_filters = applied_filters.clone
-      tag_applied_filters[heading] = applied_filters[heading].except(tag_value)
-      tag_applied_filters
-    end
-
-    def retrieve_tag_text(heading, lookup_val)
-      available_filters.each do |available_filter|
-        if available_filter.key(heading)
-          available_filter[:checkbox_config].each do |checkbox_config|
-            return checkbox_config[:text].to_s if checkbox_config.key(lookup_val)
-          end
-        end
-      end
-    end
-
     def filtering_page_path(*args)
       send(@path, *args)
     end
