5ce59695aa86f1330bf635068cae388c577da78a
willmcb <william.mcbrien@digital.education.gov.uk>
2020-03-10 12:34:29 +0000

    Fix specs

--- app/controllers/provider_interface/application_choices_controller.rb
+++ app/controllers/provider_interface/application_choices_controller.rb
@@ -6,10 +6,8 @@ module ProviderInterface
       @page_state = ProviderApplicationsPageState.new(params: params, application_choices: application_choices)
 
       if FeatureFlag.active?('provider_application_filters')
-        @filter_visible = @page_state.filter_visible
-        @filter_selections = @page_state.filter_selections
         application_choices = FilterApplicationChoicesForProviders.call(application_choices: application_choices,
-                                                                        filters: @filter_selections)
+                                                                        filters: @page_state.filter_selections)
       end
       application_choices = application_choices.page(params[:page] || 1)
       @application_choices = application_choices.order(@page_state.applications_odering_query)
@@ -18,12 +16,6 @@ module ProviderInterface
     def show
       @application_choice = GetApplicationChoicesForProviders.call(providers: current_provider_user.providers)
         .find(params[:application_choice_id])
-    end
-
-  private
-
-    def filter_params
-      params.permit(:filter_visible, filter_selections: { status: {}, provider: {} })
     end
 
   end
