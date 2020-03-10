2dabc24344c0091024e90c49f9257b30c9c984aa
Duncan Brown <duncanjbrown@gmail.com>
2020-03-10 13:56:30 +0000

    Update actionview-component require process
    Following a change to the docs at
    https://github.com/github/view_component#installation and
    https://github.com/github/view_component#setting-up-rspec, part of a
    change to rename this gem to view_component (because it's not going to
    be in Rails 6.1).
    
    https://github.com/github/view_component/issues/206

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,7 @@ require File.expand_path('../config/environment', __dir__)
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-require 'action_view/component/test_helpers'
+require 'view_component/test_helpers'
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -69,7 +69,7 @@ RSpec.configure do |config|
 
   config.include FactoryBot::Syntax::Methods
 
-  config.include ActionView::Component::TestHelpers
+  config.include ViewComponent::TestHelpers, type: :component
 
   config.before { Faker::UniqueGenerator.clear }
 
