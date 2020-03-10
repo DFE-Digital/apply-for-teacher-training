77e77a79cd8c8d4c7bfe8b8a59fc1a011856c5d0
csabagyorfi <csaba@madetech.com>
2020-03-10 17:10:39 +0000

    Add ReferenceRelationshipForm to update relationship with candidate

---
+++ app/models/referee_interface/reference_relationship_form.rb
@@ -0,0 +1,28 @@
+module RefereeInterface
+  class ReferenceRelationshipForm
+    include ActiveModel::Model
+    attr_accessor :relationship_confirmation, :relationship_correction, :candidate
+
+    validates :relationship_confirmation, presence: true
+    validate :presence_of_relationship_correction
+
+    def self.build_from_reference(reference:)
+      relationship_confirmation = reference.relationship_correction.blank? ? 'yes' : 'no' unless reference.relationship_correction.nil?
+      new(relationship_confirmation: relationship_confirmation, relationship_correction: reference.relationship_correction, candidate: reference.application_form.full_name)
+    end
+
+    def save(application_reference)
+      return false unless valid?
+
+      application_reference.update!(relationship_correction: relationship_correction)
+    end
+
+    def presence_of_relationship_correction
+      return if relationship_confirmation.nil?
+      return if relationship_confirmation == 'yes' || relationship_correction.present?
+
+      relationship_correction_blank = I18n.t('activemodel.errors.models.referee_interface/reference_relationship_form.attributes.relationship_correction.blank', candidate: candidate)
+      errors.add(:relationship_correction, relationship_correction_blank)
+    end
+  end
+end
