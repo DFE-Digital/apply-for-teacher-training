42e74542c3dfd05957d7bad51048c70b9c122b3d
willmcb <william.mcbrien@digital.education.gov.uk>
2020-03-10 15:05:11 +0000

    Refactor ColumnHeadingComponent
    Refactor ColumnHeadingComponent so that is only aware of what it needs
    to be aware of. Additional params are passed as hash and merged into URL
    it creates.

--- app/components/provider_interface/sortable_column_heading_component.rb
+++ app/components/provider_interface/sortable_column_heading_component.rb
@@ -2,15 +2,14 @@ module ProviderInterface
   class SortableColumnHeadingComponent < ActionView::Component::Base
     include ViewHelper
 
-    attr_reader :column_name, :current_sort_by, :css_class, :filter_selections, :filter_visible
+    attr_reader :column_name, :current_sort_by, :css_class, :additional_params
 
-    def initialize(sort_order:, column_name:, current_sort_by:, css_class:, filter_selections:, filter_visible:)
+    def initialize(sort_order:, column_name:, current_sort_by:, css_class:, additional_params:)
       @sort_order = sort_order
       @column_name = column_name
       @current_sort_by = current_sort_by
       @css_class = css_class
-      @filter_selections = filter_selections
-      @filter_visible = filter_visible
+      @additional_params = additional_params
     end
 
     def default_sort_order
