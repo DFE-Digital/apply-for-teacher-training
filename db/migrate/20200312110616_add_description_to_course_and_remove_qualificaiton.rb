0d62f8214fce3e2354d906610231e00bff88252f
David Gisbey <davidgisbey@gmail.com>
2020-03-12 11:08:00 +0000

    Edit course table to add description and remove redundant columns

---
+++ db/migrate/20200312110616_add_description_to_course_and_remove_qualificaiton.rb
@@ -0,0 +1,17 @@
+class AddDescriptionToCourseAndRemoveQualificaiton < ActiveRecord::Migration[6.0]
+  def up
+    change_table :courses, bulk: true do |t|
+      t.string :description
+      t.remove :apply_from_date
+      t.remove :qualification
+    end
+  end
+
+  def down
+    change_table :courses, bulk: true do |t|
+      t.remove :description
+      t.datetime :apply_from_date
+      t.string :qualification
+    end
+  end
+end
