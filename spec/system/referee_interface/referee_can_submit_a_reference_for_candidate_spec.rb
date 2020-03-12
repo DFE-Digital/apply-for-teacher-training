cb58626fa4ef1ef7ef8ce01c23b050b74387c6d8
csabagyorfi <csaba@madetech.com>
2020-03-12 15:21:54 +0000

    Remove the new multi page referee journey from the old spec
    We want to be able to test if the old Referee can submit reference scenario still works without the feature flag

--- spec/system/referee_interface/referee_can_submit_a_reference_for_candidate_spec.rb
+++ spec/system/referee_interface/referee_can_submit_a_reference_for_candidate_spec.rb
@@ -5,57 +5,28 @@ RSpec.feature 'Referee can submit reference', sidekiq: true, with_audited: true 
 
   scenario 'Referee submits a reference for a candidate' do
     FeatureFlag.activate('training_with_a_disability')
-    FeatureFlag.activate('referee_confirm_relationship_and_safeguarding')
 
     given_a_candidate_completed_an_application
     when_the_candidate_submits_the_application
     then_i_receive_an_email_with_a_magic_link
-
     when_i_try_to_access_the_reference_page_with_invalid_token
     then_i_see_page_not_found
 
     when_i_click_on_the_link_within_the_email
-    then_i_am_asked_to_confirm_my_relationship_with_the_candidate
-
-    when_i_click_on_continue
-    then_i_see_an_error_to_confirm_my_relationship_with_the_candidate
-
-    when_i_confirm_that_the_described_relationship_is_not_correct
-    and_i_click_on_continue
-    then_i_see_an_error_to_enter_my_relationship_with_the_candidate
-
-    when_i_confirm_that_the_described_relationship_is_correct
-    and_i_click_on_continue
-    then_i_see_the_safeguarding_page
-
-    when_i_click_on_continue
-    then_i_see_an_error_to_choose_if_i_know_any_safeguarding_concerns
-
-    when_i_choose_the_candidate_is_not_suitable_for_working_with_children
-    and_i_click_on_continue
-    then_i_see_an_error_to_enter_my_safeguarding_concerns
-
-    when_i_choose_the_candidate_is_suitable_for_working_with_children
-    and_i_click_on_continue
     then_i_see_the_reference_comment_page
     and_i_see_the_list_of_the_courses_the_candidate_applied_to
 
     when_i_fill_in_the_reference_field
-    and_i_click_on_continue
-    then_i_see_the_reference_review_page
-
     and_i_click_the_submit_reference_button
     then_i_see_am_told_i_submittted_my_refernce
     then_i_see_the_confirmation_page
     and_i_receive_an_email_confirmation
     and_an_audit_comment_is_added
     and_the_candidate_receives_a_notification
-
     when_i_choose_to_be_contactable
     and_i_click_the_finish_button
     then_i_see_the_thank_you_page
     and_i_am_told_i_will_be_contacted
-
     when_i_retry_to_edit_the_feedback
     then_i_see_the_thank_you_page
   end
@@ -90,48 +61,8 @@ RSpec.feature 'Referee can submit reference', sidekiq: true, with_audited: true 
     current_email.click_link(@reference_feedback_url)
   end
 
-  def then_i_am_asked_to_confirm_my_relationship_with_the_candidate
-    expect(page).to have_content("Confirm how you know #{@application.full_name}")
-  end
-
   def when_i_click_on_continue
     click_button 'Continue'
-  end
-
-  def then_i_see_an_error_to_confirm_my_relationship_with_the_candidate
-    expect(page).to have_content('Choose if the described relationship is correct')
-  end
-
-  def when_i_confirm_that_the_described_relationship_is_not_correct
-    choose 'No'
-  end
-
-  def then_i_see_an_error_to_enter_my_relationship_with_the_candidate
-    expect(page).to have_content("Enter your relationship to #{@application.full_name}")
-  end
-
-  def when_i_confirm_that_the_described_relationship_is_correct
-    choose 'Yes'
-  end
-
-  def then_i_see_the_safeguarding_page
-    expect(page).to have_content("Do you know of any reason why #{@application.full_name} should not work with children?")
-  end
-
-  def then_i_see_an_error_to_choose_if_i_know_any_safeguarding_concerns
-    expect(page).to have_content("Select if you know of any reason why #{@application.full_name} should not work with children")
-  end
-
-  def when_i_choose_the_candidate_is_not_suitable_for_working_with_children
-    choose 'Yes'
-  end
-
-  def then_i_see_an_error_to_enter_my_safeguarding_concerns
-    expect(page).to have_content("Enter a reason why #{@application.full_name} should not work with children")
-  end
-
-  def when_i_choose_the_candidate_is_suitable_for_working_with_children
-    choose 'No'
   end
 
   def and_i_click_on_continue
