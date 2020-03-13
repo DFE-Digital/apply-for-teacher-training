e53d0053ce588cf3da84073c59f739266a6cb395
Steve Hook <steve.hook@gmail.com>
2020-03-13 11:35:14 +0000

    Implement outline for individual timeline items

--- app/components/provider_interface/application_timeline_component.rb
+++ app/components/provider_interface/application_timeline_component.rb
@@ -6,5 +6,28 @@ module ProviderInterface
     def initialize(application_choice:)
       @application_choice = application_choice
     end
+
+    Event = Struct.new(:title, :actor, :date)
+
+  private
+
+    def events
+      audits = StateChangeAudits.new(application_choice: application_choice)
+      audits.map do |audit|
+        Event.new(
+          title_for(audit),
+          actor_for(audit),
+          audit.created_at,
+        )
+      end
+    end
+
+    def title_for(_audit)
+      'Application submitted'
+    end
+
+    def actor_for(_audit)
+      'candidate'
+    end
   end
 end
