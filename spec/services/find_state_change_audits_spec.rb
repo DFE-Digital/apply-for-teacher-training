2e726512fd18f4a7651d05dfdb55ce83b7b6d53f
Steve Hook <steve.hook@gmail.com>
2020-03-15 22:03:54 +0000

    Add an extra spec for FindStateChangeAudits

--- spec/services/find_state_change_audits_spec.rb
+++ spec/services/find_state_change_audits_spec.rb
@@ -19,11 +19,29 @@ RSpec.describe FindStateChangeAudits, with_audited: true do
       candidate = create :candidate, email_address: 'alice@example.com'
       Audited.audit_class.as_user(candidate) do
         application_choice = create :application_choice, status: :unsubmitted
-        SubmitApplication.new(application_choice.application_form).call
+        ApplicationStateChange.new(application_choice).submit!
         result = described_class.new(
           application_choice: application_choice,
         ).call
         expect(result).to eq [application_choice.audits.last]
+      end
+    end
+  end
+
+  context 'for an accepted application' do
+    it 'returns 5 status change events' do
+      candidate = create :candidate, email_address: 'alice@example.com'
+      Audited.audit_class.as_user(candidate) do
+        application_choice = create :application_choice, status: :unsubmitted
+        ApplicationStateChange.new(application_choice).submit!
+        ApplicationStateChange.new(application_choice).references_complete!
+        ApplicationStateChange.new(application_choice).send_to_provider!
+        ApplicationStateChange.new(application_choice).make_offer!
+        ApplicationStateChange.new(application_choice).accept!
+        result = described_class.new(
+          application_choice: application_choice,
+        ).call
+        expect(result.count).to be 5
       end
     end
   end
