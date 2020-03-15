8d931cc8b6e736f09163e2054d5b3d55a642c82d
Steve Hook <steve.hook@gmail.com>
2020-03-15 21:51:39 +0000

    First attempt to filter out status change audits

--- app/services/find_state_change_audits.rb
+++ app/services/find_state_change_audits.rb
@@ -6,6 +6,14 @@ class FindStateChangeAudits
   end
 
   def call
-    []
+    application_choice.audits.select do |audit|
+      state_change?(audit)
+    end
+  end
+
+private
+
+  def state_change?(audit)
+    audit.action == 'update' && audit.audited_changes.has_key?('status')
   end
 end
