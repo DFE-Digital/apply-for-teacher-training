f6e6ec0a0ca3e6a0ea255b8927a7c264bbe44359
Tijmen Brommet <tijmen@gmail.com>
2020-03-16 13:39:20 +0000

    Improve readability of MVP search

--- app/controllers/support_interface/email_log_controller.rb
+++ app/controllers/support_interface/email_log_controller.rb
@@ -4,7 +4,9 @@ module SupportInterface
       @emails = Email.order(id: :desc).includes(:application_form).limit(1000)
 
       %w[to subject mailer mail_template notify_reference application_form_id delivery_status].each do |column|
-        @emails = @emails.where(column => params[column]) if params[column]
+        next unless params[column]
+
+        @emails = @emails.where(column => params[column])
       end
     end
   end
