46168b9174cdd8c4cfa0341ec16fc353f4ea0ef1
Wen Ting Wang <chubberlisk@outlook.com>
2020-03-16 10:38:36 +0000

    Add test to ensure that initial reference request email works
    After a chaser reference request email is sent.

--- spec/system/referee_interface/referee_can_use_initial_and_chaser_sign_in_links_spec.rb
+++ spec/system/referee_interface/referee_can_use_initial_and_chaser_sign_in_links_spec.rb
@@ -5,11 +5,14 @@ RSpec.feature 'Referee can use sign in link in the initial and chaser email', si
     given_the_confirm_relationship_and_safeguarding_feature_flag_is_active
     and_i_am_a_referee_of_an_submitted_application
     and_i_received_the_initial_reference_request_email
-    when_i_click_on_the_link_within_the_email
+    when_i_click_on_the_link_within_the_initial_email
     then_i_am_asked_to_confirm_my_relationship_with_the_candidate
 
     given_i_received_the_chaser_reference_request_email
-    when_i_click_on_the_link_within_the_email
+    when_i_click_on_the_link_within_the_chaser_email
+    then_i_am_asked_to_confirm_my_relationship_with_the_candidate
+
+    when_i_click_on_the_link_within_the_initial_email
     then_i_am_asked_to_confirm_my_relationship_with_the_candidate
   end
 
@@ -26,13 +29,10 @@ RSpec.feature 'Referee can use sign in link in the initial and chaser email', si
     RefereeMailer.reference_request_email(@application, @reference).deliver_now
   end
 
-  def when_i_click_on_the_link_within_the_email
+  def when_i_click_on_the_link_within_the_initial_email
     open_email(@reference.email_address)
 
-    matches = current_email.body.match(/(http:\/\/localhost:3000\/reference\?token=[\w-]{20})/)
-    reference_feedback_url = matches.captures.first unless matches.nil?
-
-    current_email.click_link(reference_feedback_url)
+    click_sign_in_link(current_emails.first)
   end
 
   def then_i_am_asked_to_confirm_my_relationship_with_the_candidate
@@ -41,5 +41,18 @@ RSpec.feature 'Referee can use sign in link in the initial and chaser email', si
 
   def given_i_received_the_chaser_reference_request_email
     RefereeMailer.reference_request_chaser_email(@application, @reference).deliver_now
+  end
+
+  def when_i_click_on_the_link_within_the_chaser_email
+    open_email(@reference.email_address)
+
+    click_sign_in_link(current_emails.second)
+  end
+
+  def click_sign_in_link(email)
+    matches = email.body.match(/(http:\/\/localhost:3000\/reference\?token=[\w-]{20})/)
+    reference_feedback_url = matches.captures.first unless matches.nil?
+
+    email.click_link(reference_feedback_url)
   end
 end
