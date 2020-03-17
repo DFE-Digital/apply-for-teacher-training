6dd25f4db8fc794f30ab7377b330330a3f11d259
willmcb <william.mcbrien@digital.education.gov.uk>
2020-03-17 14:51:15 +0000

    Refactor filter_component view to be more readable

--- app/components/provider_interface/filter_component.rb
+++ app/components/provider_interface/filter_component.rb
@@ -18,5 +18,14 @@ module ProviderInterface
     def filtering_page_path(*args)
       send(@path, *args)
     end
+
+    def is_candidates_name_search_field?(filter_group)
+      filter_group[:heading].parameterize.eql?('candidate-s-name')
+    end
+
+    def form_group_fields_greater_than_one?(filter_group)
+      filter_group[:input_config].size > 1
+    end
+
   end
 end
