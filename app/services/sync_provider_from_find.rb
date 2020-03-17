221f309966c90b3d775ef1548461fe604a9bc427
Malcolm Baig <malcolm.baig@gmail.com>
2020-03-17 15:37:33 +0000

    Refactor SyncProviderFromFind
    - Use attribute readers for the initialising attributes to make them
      easier to access in the class body.
    - Use an accessor for the provider record so that it doesn't have to be
      passed in explicitly to every method that acts on it.
    - Apply the Extract Method refactoring to #call to make the discrete
      steps of this operation clearer.
    - Extract a CourseVacancyStatus object, so that its behaviour can be
      tested exhaustively without multiple passes through the entire .call
      method.

--- app/services/sync_provider_from_find.rb
+++ app/services/sync_provider_from_find.rb
@@ -1,53 +1,61 @@
 class SyncProviderFromFind
   def self.call(provider_code:, provider_name: nil, sync_courses: false)
-    new.call(provider_code, provider_name, sync_courses)
+    new(provider_code, provider_name, sync_courses).call
   end
 
-  def call(provider_code, provider_name, sync_courses)
-    provider = create_or_update_provider(provider_name, provider_code, sync_courses)
+  attr_reader :provider_code, :provider_name, :sync_courses
+  attr_accessor :provider
+
+  def initialize(provider_code, provider_name, sync_courses)
+    @provider_code = provider_code
+    @provider_name = provider_name
+    @sync_courses = sync_courses
+  end
+
+  def call
+    @provider = create_or_update_provider
 
     return unless provider.sync_courses?
 
+    find_provider = fetch_provider_from_find_api
+
+    update_provider_details_with_api_response(find_provider)
+
+    find_provider.courses.each do |find_course|
+      create_or_update_course(find_course)
+    end
+  end
+
+
+private
+
+  def create_or_update_provider
+    Provider.find_or_create_by(code: provider_code).tap do |provider_record|
+      provider_record.sync_courses = sync_courses if sync_courses
+      provider_record.name = provider_name if provider_name.present?
+      provider_record.save!
+    end
+  end
+
+  def fetch_provider_from_find_api
     # Request provider, all courses and sites.
     #
     # For the full response, see:
     # https://api2.publish-teacher-training-courses.service.gov.uk/api/v3/recruitment_cycles/2020/providers/1N1/?include=sites,courses.sites
-    find_provider = FindAPI::Provider
+    FindAPI::Provider
       .current_cycle
       .includes(:sites, courses: [:sites, site_statuses: [:site]])
       .find(provider_code)
       .first
-
-    update_provider(provider, find_provider)
-
-    find_provider.courses.each do |find_course|
-      create_or_update_course(find_course, provider)
-    end
   end
 
-  def create_or_update_provider(provider_name, provider_code, sync_courses)
-    provider = Provider.find_or_create_by(code: provider_code)
-    if sync_courses
-      provider.sync_courses = sync_courses
-      provider.save!
-    end
-    update_provider_name(provider, provider_name) if provider_name.present?
-
-    provider
-  end
-
-  def update_provider_name(provider, provider_name)
-    provider.name = provider_name
-    provider.save!
-  end
-
-  def update_provider(provider, find_provider)
+  def update_provider_details_with_api_response(find_provider)
     provider.region_code = find_provider.region_code.strip if find_provider.region_code
     provider.name = find_provider.provider_name if find_provider.provider_name
     provider.save!
   end
 
-  def create_or_update_course(find_course, provider)
+  def create_or_update_course(find_course)
     course = provider.courses.find_or_create_by(code: find_course.course_code)
     course.name = find_course.name
     course.level = find_course.level
@@ -57,6 +65,7 @@ class SyncProviderFromFind
     course.course_length = find_course.course_length
     course.recruitment_cycle_year = find_course.recruitment_cycle_year
     course.exposed_in_find = find_course.findable?
+
     if find_course[:accrediting_provider].present?
       accrediting_provider = Provider.find_or_create_by(code: find_course[:accrediting_provider][:provider_code]) do |accredit_provider|
         accredit_provider.name = find_course[:accrediting_provider][:provider_name]
@@ -95,10 +104,10 @@ class SyncProviderFromFind
           study_mode: mode,
         )
 
-        vacancy_status = derive_vacancy_status(
-          status_description: find_site_status.vac_status,
-          study_mode: course_option.study_mode,
-        )
+        vacancy_status = CourseVacancyStatus.new(
+          find_site_status.vac_status,
+          course_option.study_mode,
+        ).derive
         course_option.update(vacancy_status: vacancy_status)
       end
     end
@@ -106,20 +115,27 @@ class SyncProviderFromFind
     course
   end
 
-  def derive_vacancy_status(status_description:, study_mode:)
-    case status_description
-    when 'no_vacancies'
-      :no_vacancies
-    when 'both_full_time_and_part_time_vacancies'
-      :vacancies
-    when 'full_time_vacancies'
-      study_mode == 'full_time' ? :vacancies : :no_vacancies
-    when 'part_time_vacancies'
-      study_mode == 'part_time' ? :vacancies : :no_vacancies
-    else
-      raise InvalidFindVacancyStatusError, status_description
+  class CourseVacancyStatus
+    def initialize(find_status_description, study_mode)
+      @find_status_description = find_status_description
+      @study_mode = study_mode
     end
-  end
 
-  class InvalidFindVacancyStatusError < StandardError; end
+    def derive
+      case @find_status_description
+      when 'no_vacancies'
+        :no_vacancies
+      when 'both_full_time_and_part_time_vacancies'
+        :vacancies
+      when 'full_time_vacancies'
+        @study_mode == 'full_time' ? :vacancies : :no_vacancies
+      when 'part_time_vacancies'
+        @study_mode == 'part_time' ? :vacancies : :no_vacancies
+      else
+        raise InvalidFindStatusDescriptionError, @find_status_description
+      end
+    end
+
+    class InvalidFindStatusDescriptionError < StandardError; end
+  end
 end
