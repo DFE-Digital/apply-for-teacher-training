cfbf0e5862689b5fef41297ab05a111fafbac6ad
Steve Hook <steve.hook@gmail.com>
2020-03-18 22:59:20 +0000

    Update references to TestApplications

--- app/workers/generate_test_applications.rb
+++ app/workers/generate_test_applications.rb
@@ -4,20 +4,20 @@ class GenerateTestApplications
   def perform
     raise 'You can\'t generate test data in production' if HostingEnvironment.production?
 
-    TestApplications.create_application states: [:unsubmitted]
-    TestApplications.create_application states: [:awaiting_references]
-    TestApplications.create_application states: [:application_complete]
-    TestApplications.create_application states: [:awaiting_provider_decision] * 3
-    TestApplications.create_application states: [:offer] * 2
-    TestApplications.create_application states: %i[offer rejected]
-    TestApplications.create_application states: [:rejected] * 2
-    TestApplications.create_application states: [:offer_withdrawn]
-    TestApplications.create_application states: [:declined]
-    TestApplications.create_application states: [:accepted]
-    TestApplications.create_application states: [:accepted_no_conditions]
-    TestApplications.create_application states: [:recruited]
-    TestApplications.create_application states: [:conditions_not_met]
-    TestApplications.create_application states: [:enrolled]
-    TestApplications.create_application states: [:withdrawn]
+    TestApplications.new.create_application states: [:unsubmitted]
+    TestApplications.new.create_application states: [:awaiting_references]
+    TestApplications.new.create_application states: [:application_complete]
+    TestApplications.new.create_application states: [:awaiting_provider_decision] * 3
+    TestApplications.new.create_application states: [:offer] * 2
+    TestApplications.new.create_application states: %i[offer rejected]
+    TestApplications.new.create_application states: [:rejected] * 2
+    TestApplications.new.create_application states: [:offer_withdrawn]
+    TestApplications.new.create_application states: [:declined]
+    TestApplications.new.create_application states: [:accepted]
+    TestApplications.new.create_application states: [:accepted_no_conditions]
+    TestApplications.new.create_application states: [:recruited]
+    TestApplications.new.create_application states: [:conditions_not_met]
+    TestApplications.new.create_application states: [:enrolled]
+    TestApplications.new.create_application states: [:withdrawn]
   end
 end
