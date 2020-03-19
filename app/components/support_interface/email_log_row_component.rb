b58b05333454df652eda54ebd2d3e07961b5cf8e
Tijmen Brommet <tijmen@gmail.com>
2020-03-19 11:28:59 +0000

    Move complex code into the email log row component

--- app/components/support_interface/email_log_row_component.rb
+++ app/components/support_interface/email_log_row_component.rb
@@ -7,5 +7,15 @@ module SupportInterface
     def initialize(email:)
       @email = email
     end
+
+    def status_tag
+      tag.span(email.delivery_status.humanize, class: "govuk-tag #{email.delivered? ? nil : 'govuk-tag--red'}")
+    end
+
+    def application_link
+      return unless email.application_form
+
+      govuk_link_to("#{email.application_form.full_name} (#{email.application_form.support_reference})", support_interface_application_form_path(email.application_form))
+    end
   end
 end
