be982185f07d3e4b1263dd118cb666602c3b9aab
Tijmen Brommet <tijmen@gmail.com>
2020-03-19 12:25:44 +0000

    Tidy up the email summary list
    - Move reference as a title, because it's not too important
    - Remove application when not applicable

--- app/components/support_interface/email_log_row_component.rb
+++ app/components/support_interface/email_log_row_component.rb
@@ -8,6 +8,23 @@ module SupportInterface
       @email = email
     end
 
+    def summary_list_rows
+      rows = [
+        { key: 'Status', value: status_tag },
+        { key: 'Type', value: email.humanised_email_type },
+        { key: 'To', value: email.to },
+        { key: 'Subject', value: email.subject.inspect },
+      ]
+
+      if email.application_form
+        rows << { key: 'Application', value: application_link }
+      end
+
+      rows
+    end
+
+  private
+
     def status_tag
       colour_tag = {
         not_tracked: 'govuk-tag--grey',
@@ -19,12 +36,10 @@ module SupportInterface
         technical_failure: 'govuk-tag--orange',
       }[email.delivery_status.to_sym]
 
-      tag.span(email.delivery_status.humanize, class: "govuk-tag #{colour_tag}")
+      tag.span(email.delivery_status.humanize, class: "govuk-tag #{colour_tag}", title: "Notify reference: `#{email.notify_reference}`")
     end
 
     def application_link
-      return unless email.application_form
-
       govuk_link_to("#{email.application_form.full_name} (#{email.application_form.support_reference})", support_interface_application_form_path(email.application_form))
     end
   end
