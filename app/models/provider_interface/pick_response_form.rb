33e48aa3a9142dd642f55ac72313ca61bcf1b90f
Steve Laing <steve.laing@gmail.com>
2020-03-19 16:56:47 +0000

    Allow different responses for changing new offer

--- app/models/provider_interface/pick_response_form.rb
+++ app/models/provider_interface/pick_response_form.rb
@@ -2,7 +2,14 @@ module ProviderInterface
   class PickResponseForm
     include ActiveModel::Model
 
+    VALID_CHANGE_DECISIONS = %w[edit_course edit_course_option edit_provider].freeze
+    VALID_DECISIONS = (%w[new_offer new_reject] + VALID_CHANGE_DECISIONS).freeze
+
     attr_accessor :decision
-    validates :decision, presence: true
+    validates :decision, inclusion: { in: VALID_DECISIONS }
+
+    def decision_is_change?
+      VALID_CHANGE_DECISIONS.include?(decision)
+    end
   end
 end
