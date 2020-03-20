baafa69f20c4cdfb6a3c4e7177d67a0834edf1ab
Theodor Vararu <theo@vararu.org>
2020-03-20 15:30:42 +0000

    Add decline_by_default_at to RecalculateDates worker

--- app/workers/recalculate_dates.rb
+++ app/workers/recalculate_dates.rb
@@ -2,19 +2,48 @@ class RecalculateDates
   include Sidekiq::Worker
 
   def perform(*)
-    application_choices = ApplicationChoice.where(status: :awaiting_provider_decision).includes(:application_form)
-    application_choices.find_each do |application_choice|
-      time_limit = TimeLimitCalculator.new(
-        rule: :reject_by_default,
-        effective_date: application_choice.application_form.submitted_at,
-      ).call
+    ApplicationChoice
+      .where(status: :awaiting_provider_decision)
+      .includes(:application_form)
+      .find_each do |application_choice|
+        update_reject_by_default(application_choice)
+      end
 
-      days = time_limit[:days]
-      time = time_limit[:time_in_future]
+    ApplicationChoice
+      .where(status: :offer)
+      .includes(:application_form)
+      .find_each do |application_choice|
+        update_decline_by_default(application_choice)
+      end
+  end
 
-      application_choice.reject_by_default_days = days
-      application_choice.reject_by_default_at = time
-      application_choice.save!
-    end
+private
+
+  def update_reject_by_default(application_choice)
+    time_limit = TimeLimitCalculator.new(
+      rule: :reject_by_default,
+      effective_date: application_choice.application_form.submitted_at,
+    ).call
+
+    days = time_limit[:days]
+    time = time_limit[:time_in_future]
+
+    application_choice.reject_by_default_days = days
+    application_choice.reject_by_default_at = time
+    application_choice.save!
+  end
+
+  def update_decline_by_default(application_choice)
+    time_limit = TimeLimitCalculator.new(
+      rule: :decline_by_default,
+      effective_date: application_choice.offered_at,
+    ).call
+
+    days = time_limit[:days]
+    time = time_limit[:time_in_future]
+
+    application_choice.decline_by_default_days = days
+    application_choice.decline_by_default_at = time
+    application_choice.save!
   end
 end
--- spec/workers/recalculate_dates_spec.rb
+++ spec/workers/recalculate_dates_spec.rb
@@ -1,12 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe RecalculateDates do
+  around do |example|
+    Timecop.freeze(Time.zone.local(2020, 3, 20)) do
+      example.run
+    end
+  end
+
   after do
     unmock_holidays
   end
 
   it 'recalculates reject_by_default_at for a submitted application choice' do
-    application_form = create(:completed_application_form, :with_completed_references, submitted_at: Time.zone.local(2020, 3, 20))
+    application_form = create(:completed_application_form, :with_completed_references, submitted_at: Time.zone.now)
     application_choice = create(:submitted_application_choice, application_form: application_form)
 
     mock_holidays
@@ -16,6 +22,24 @@ RSpec.describe RecalculateDates do
     new_reject_by_default = Time.zone.local(2020, 5, 21).end_of_day
 
     expect(application_choice.reload.reject_by_default_at).to be_within(1.second).of new_reject_by_default
+  end
+
+  it 'recalculates decline_by_default_at for a submitted application choice with an offer' do
+    application_form = create(:completed_application_form, :with_completed_references, submitted_at: Time.zone.now)
+    application_choice = create(
+      :submitted_application_choice, :with_offer,
+      application_form: application_form,
+      decline_by_default_at: 10.business_days.from_now,
+      offered_at: Time.zone.now
+    )
+
+    mock_holidays
+
+    RecalculateDates.new.perform
+
+    new_decline_by_default = Time.zone.local(2020, 4, 6).end_of_day
+
+    expect(application_choice.reload.decline_by_default_at).to be_within(1.second).of new_decline_by_default
   end
 
   def mock_holidays
