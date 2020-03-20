6ad57be6d121f31b4068f35262796467600b46e1
willmcb <william.mcbrien@digital.education.gov.uk>
2020-03-20 09:27:53 +0000

    Swap chained .fetch for one .dig

--- app/models/provider_interface/provider_applications_page_state.rb
+++ app/models/provider_interface/provider_applications_page_state.rb
@@ -43,7 +43,7 @@ module ProviderInterface
     def remove_candiates_name_search_if_empty(filter_selections)
       return filter_selections if filter_selections.empty?
 
-      filter_selections.delete(:search) if filter_selections.fetch(:search, {}).fetch(:candidates_name, '') == ''
+      filter_selections.delete(:search) if filter_selections.dig(:search, :candidates_name) == ''
       filter_selections
     end
 
