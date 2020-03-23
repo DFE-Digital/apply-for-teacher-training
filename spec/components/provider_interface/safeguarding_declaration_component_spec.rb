8719fcf3e4bf24f39acfcca8b59c9133f1468234
willmcb <william.mcbrien@digital.education.gov.uk>
2020-03-23 16:43:55 +0000

    Refactor spec to make it more explicit
    Also adds spec to check that if safeguarding is prose rather
    than 'Yes' or 'No' the component displays correctly.

--- spec/components/provider_interface/safeguarding_declaration_component_spec.rb
+++ spec/components/provider_interface/safeguarding_declaration_component_spec.rb
@@ -1,25 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe ProviderInterface::SafeguardingDeclarationComponent do
-  context 'when safeguarding issues has a value of "Yes"' do
-    it 'displays "Yes" for sharing safeguarding issues and "Not entered" for relevant information' do
+  let(:heading) { 'Criminal convictions and professional misconduct' }
+  let(:has_shared_text) { 'The candidate has shared information related to safeguarding. Please contact them directly for more details.' }
+  let(:has_not_shared_text) { 'No information shared' }
+
+
+  context 'when the candidate has entered "Yes" as an answer to the safeguarding question' do
+    it 'displays the correct text' do
       application_form = build_stubbed(:application_form, safeguarding_issues: 'Yes')
 
       result = render_inline(described_class.new(application_form: application_form))
 
-      expect(result.text).to include('Criminal convictions and professional misconduct')
-      expect(result.text).to include('The candidate has shared information related to safeguarding. Please contact them directly for more details.')
+      expect(result.text).to include(heading)
+      expect(result.text).to include(has_shared_text)
     end
   end
 
-  context 'when safeguarding issues has a value of "No"' do
-    it 'displays "No" for sharing safeguarding issues and "Not entered" for relevant information' do
+  context 'when the candidate has shared information related to safeguarding' do
+    it 'displays the correct text' do
+      application_form = build_stubbed(:application_form, safeguarding_issues: 'I have a criminal conviction.')
+      result = render_inline(described_class.new(application_form: application_form))
+
+      expect(result.text).to include(heading)
+      expect(result.text).to include(has_shared_text)
+    end
+  end
+
+  context 'when the candidate has not shared information related to safeguarding' do
+    it 'displays the corrent text' do
       application_form = build_stubbed(:application_form, safeguarding_issues: 'No')
 
       result = render_inline(described_class.new(application_form: application_form))
 
-      expect(result.text).to include('Criminal convictions and professional misconduct')
-      expect(result.text).to include('No information shared')
+      expect(result.text).to include(heading)
+      expect(result.text).to include(has_not_shared_text)
     end
   end
 end
