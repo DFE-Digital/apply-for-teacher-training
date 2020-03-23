83dd9109993f5ac496156642a0f737d38069fd15
Steve Hook <steve.hook@gmail.com>
2020-03-23 11:11:41 +0000

    Move calls to ProviderMailer outside transaction
    This one wasn't crashing but it makes no sense to have this side effect
    inside the transaction.

--- app/services/decline_offer_by_default.rb
+++ app/services/decline_offer_by_default.rb
@@ -6,14 +6,19 @@ class DeclineOfferByDefault
   end
 
   def call
+    application_choices = []
+
     ActiveRecord::Base.transaction do
       application_form.application_choices.offer.each do |application_choice|
         application_choice.update!(declined_by_default: true, declined_at: Time.zone.now)
         ApplicationStateChange.new(application_choice).decline_by_default!
+        application_choices << application_choice
+      end
+    end
 
-        application_choice.provider.provider_users.each do |provider_user|
-          ProviderMailer.declined_by_default(provider_user, application_choice).deliver_later
-        end
+    application_choices.each do |application_choice|
+      application_choice.provider.provider_users.each do |provider_user|
+        ProviderMailer.declined_by_default(provider_user, application_choice).deliver_later
       end
     end
 
