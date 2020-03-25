12a9e5bdbbf44f0349378b8d4d96fd8c4a20a24e
willmcb <william.mcbrien@digital.education.gov.uk>
2020-03-25 10:59:38 +0000

    Initial commit of ApplicationCardCompoment

---
+++ app/components/provider_interface/application_card_component.rb
@@ -0,0 +1,16 @@
+module ProviderInterface
+  class ApplicationCardComponent < ActionView::Component::Base
+
+    attr_accessor :accrediting_provider, :application_choice, :application_choice_path,
+                  :candidate_name, :course_name_and_code, :course_provider_name, :updated_at
+
+    def initialize(application_choice:)
+      @accrediting_provider = application_choice.accrediting_provider
+      @application_choice = application_choice
+      @candidate_name = application_choice.application_form.full_name
+      @course_name_and_code = application_choice.offered_course.name_and_code
+      @course_provider_name = application_choice.offered_course.provider.name
+      @updated_at = application_choice.updated_at.to_s(:govuk_date_short_month)
+    end
+  end
+end
