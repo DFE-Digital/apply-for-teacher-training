fd5afb899857d2417518bfd5f123dda746374ee1
csabagyorfi <csaba@madetech.com>
2020-03-26 17:00:24 +0000

    Update PickSiteForm to allow updating an exiting course choice

--- spec/models/candidate_interface/pick_site_form_spec.rb
+++ spec/models/candidate_interface/pick_site_form_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe CandidateInterface::PickSiteForm, type: :model do
         course_option_id: create(:course_option).id,
       )
 
-      expect(pick_site_form).to be_valid
+      expect(pick_site_form).to be_valid(:save)
 
       pick_site_form.save
 
@@ -21,7 +21,23 @@ RSpec.describe CandidateInterface::PickSiteForm, type: :model do
         course_option_id: create(:course_option).id,
       )
 
-      expect(pick_site_form).not_to be_valid
+      expect(pick_site_form).not_to be_valid(:save)
+    end
+  end
+
+  describe '#update' do
+    it 'updates the course_option for an existing course choice' do
+      application_choice = create(:application_choice)
+      new_course_option = create(:course_option)
+
+      expect(application_choice.course_option.id).not_to eq(new_course_option.id)
+
+      CandidateInterface::PickSiteForm.new(
+        application_form: application_choice.application_form,
+        course_option_id: new_course_option.id,
+      ).update(application_choice)
+
+      expect(application_choice.course_option.id).to eq(new_course_option.id)
     end
   end
 end
