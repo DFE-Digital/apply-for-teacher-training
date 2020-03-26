cfda71c867591d2fbb095e4a2db88ed88588c50e
Wen Ting Wang <chubberlisk@outlook.com>
2020-03-26 10:31:11 +0000

    wip

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -12,3 +12,18 @@ govUKFrontendInitAll();
 initNationalityAutocomplete();
 initProvidersAutocomplete();
 initCoursesAutocomplete();
+
+if (document.getElementsByClassName("govuk-textarea").length >= 1) {
+  console.log("there's a textarea")
+  const $form = document.getElementsByTagName("form")[0]
+
+  if ($form) {
+    $form.addEventListener("submit", function() {
+      window.onbeforeunload = null;
+    });
+  }
+
+  window.onbeforeunload = function() {
+    return "";
+  };
+}
