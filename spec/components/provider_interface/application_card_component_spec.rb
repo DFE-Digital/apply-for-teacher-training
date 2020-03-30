3c973c895d5d25d7274f3f3ab1c84b414c4d77a0
willmcb <william.mcbrien@digital.education.gov.uk>
2020-03-30 15:48:52 +0100

    Refactor HTML to make it more accessible

--- spec/components/provider_interface/application_card_component_spec.rb
+++ spec/components/provider_interface/application_card_component_spec.rb
@@ -10,12 +10,19 @@ RSpec.describe ProviderInterface::ApplicationCardComponent do
            name: 'Hoth Teacher Training')
   end
 
+  let(:accrediting_provider) do
+    create(:provider,
+           :with_signed_agreement,
+           code: 'XYZ',
+           name: 'Yavin University')
+  end
+
   let(:course_option) do
     course_option_for_provider(provider: current_provider,
                                course: create(:course,
                                               name: 'Alchemy',
                                               provider: current_provider,
-                                              accrediting_provider: current_provider))
+                                              accrediting_provider: accrediting_provider))
   end
 
   let(:application_choice) do
@@ -28,37 +35,58 @@ RSpec.describe ProviderInterface::ApplicationCardComponent do
                                                          updated_at: Date.parse('25-03-2020'))
   end
 
-  let(:result) { render_inline described_class.new(application_choice: application_choice, index: 1) }
+  let(:result) { render_inline described_class.new(application_choice: application_choice) }
 
-  let(:card) { result.css('[data-qa=application-card-1]') }
-
-  let(:card_primary) { card.css('> div.app-application-card__primary') }
-
-  let(:card_secondary) { card.css('> div.app-application-card__secondary') }
+  let(:card) { result.css('.app-application-card').to_html }
 
   describe 'rendering' do
     it 'renders the name of the candidate' do
-      expect(card_primary.css('> h3').text).to include('Jim James')
+      expect(card).to include('Jim James')
     end
 
     it 'renders the name of education provider' do
-      expect(card_primary.css('> p:nth-of-type(1)').text).to include('Hoth Teacher Training')
+      expect(card).to include('Hoth Teacher Training')
     end
 
     it 'renders the name of the course' do
-      expect(card_primary.css('> p:nth-of-type(2)').text).to include('Alchemy')
+      expect(card).to include('Alchemy')
     end
 
     it 'renders the name of the accredited provider' do
-      expect(card_primary.css('> p:nth-of-type(3)').text).to include('Hoth Teacher Training')
+      expect(card).to include('Yavin University')
     end
 
     it 'renders the status of the application' do
-      expect(card_secondary.css('> div').text).to include('Application withdrawn')
+      expect(card).to include('Application withdrawn')
     end
 
     it 'renders the last updated date' do
-      expect(card_secondary.css('> p').text).to include('25 Mar 2020')
+      expect(card).to include('25 Mar 2020')
+    end
+
+    context 'when there is no accrediting provider' do
+      let(:course_option_without_accrediting_provider) do
+        course_option_for_provider(provider: current_provider,
+                                   course: create(:course,
+                                                  name: 'Baking',
+                                                  provider: current_provider))
+      end
+
+      let(:application_choice_without_accrediting_provider) do
+        create(:application_choice,
+               :awaiting_provider_decision,
+               course_option: course_option_without_accrediting_provider,
+               status: 'withdrawn', application_form: create(:application_form,
+                                                             first_name: 'Jim',
+                                                             last_name: 'James'),
+                                                             updated_at: Date.parse('25-03-2020'))
+      end
+
+      let(:result) { render_inline described_class.new(application_choice: application_choice_without_accrediting_provider) }
+
+      it 'renders the course provider name instead' do
+        expect(result.css('.app-application-card__secondary').text).to include('Hoth Teacher Training')
+      end
     end
   end
 end
