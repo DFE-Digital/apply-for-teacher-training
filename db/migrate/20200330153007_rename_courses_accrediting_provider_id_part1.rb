38f4836b5b05f6f60f1b747a789cb5503d2bf842
Steve Hook <steve.hook@gmail.com>
2020-03-31 12:13:32 +0100

    Revert add column
    This will be done in a separate PR

--- db/migrate/20200330153007_rename_courses_accrediting_provider_id_part1.rb
+++ db/migrate/20200330153007_rename_courses_accrediting_provider_id_part1.rb
@@ -1,10 +1,7 @@
 class RenameCoursesAccreditingProviderIdPart1 < ActiveRecord::Migration[6.0]
   def up
-    add_column :courses, :accredited_provider_id, :integer, null: true
     execute 'UPDATE courses SET accredited_provider_id = accrediting_provider_id'
   end
 
-  def down
-    remove_column :courses, :accredited_provider_id
-  end
+  def down; end
 end
