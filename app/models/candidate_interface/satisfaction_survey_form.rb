18e5b309ae10ca93b7f5f707033048693fcda271
David Gisbey <davidgisbey@gmail.com>
2020-04-01 12:24:12 +0100

    Adds logic for adding new values to the survey and editing previous responses
    If a user clicks back and inputs a new value,
    the model searches for the question in the keys
    and updates the value

--- app/models/candidate_interface/satisfaction_survey_form.rb
+++ app/models/candidate_interface/satisfaction_survey_form.rb
@@ -9,7 +9,24 @@ module CandidateInterface
     def save(application_form)
       return false unless valid?
 
-      application_form.update(satisfaction_survey: { @question => @answer })
+      if question_already_answered?(application_form)
+        application_form.satisfaction_survey[@question] = @answer
+        application_form.save
+      elsif application_form.satisfaction_survey.present?
+        application_form.update!(satisfaction_survey: merge_satisfaction_survey_and_answer(application_form))
+      else
+        application_form.update!(satisfaction_survey: { @question => @answer })
+      end
+    end
+
+  private
+
+    def question_already_answered?(application_form)
+      application_form.satisfaction_survey&.keys&.include?(@question)
+    end
+
+    def merge_satisfaction_survey_and_answer(application_form)
+      application_form.satisfaction_survey.merge!({ @question => @answer })
     end
   end
 end
--- spec/models/candidate_interface/satisfaction_survey_form_spec.rb
+++ spec/models/candidate_interface/satisfaction_survey_form_spec.rb
@@ -14,12 +14,59 @@ RSpec.describe CandidateInterface::SatisfactionSurveyForm, type: :model do
       end
     end
 
+<<<<<<< HEAD
     context 'when valid' do
       it 'updates the satisfaction_survey on the application_form with their answer' do
         application_form = create(:application_form)
         described_class.new(question: 'How was your experience', answer: '2').save(application_form)
+=======
+    context 'when the satisfcation_survey is nil and the user responds with strongly agree or strongly disagree' do
+      it 'only saves the number in the string and updates the satisfaction_survey on the application_form' do
+        application_form = create(:application_form)
+        described_class.new(question: 'How was your experience', response: '1 - strongly agree').save(application_form)
+
+        expect(application_form.satisfaction_survey).to eq({ 'How was your experience' => '1' })
+      end
+    end
+
+    context 'when the the satisfcation_survey is nil and user responds with any other response' do
+      it 'updates the satisfaction_survey on the application_form with their response' do
+        application_form = create(:application_form)
+        described_class.new(question: 'How was your experience', response: '2').save(application_form)
+>>>>>>> Adds logic for adding new values to the survey and editing previous responses
 
         expect(application_form.satisfaction_survey).to eq({ 'How was your experience' => '2' })
+      end
+    end
+
+    context 'when the the satisfcation_survey is present and the question exists as a key' do
+      it 'updates their response' do
+        application_form = create(:application_form, satisfaction_survey: {
+          'How was your experience' => '2',
+          'I found this service unnecessarily complex' => '4',
+          })
+        described_class.new(question: 'How was your experience', response: '3').save(application_form)
+
+        expect(application_form.satisfaction_survey).to eq(
+          {
+            'How was your experience' => '3',
+            'I found this service unnecessarily complex' => '4',
+          },
+        )
+      end
+    end
+
+    context 'when the the satisfcation_survey is present and the question does not exist as a key' do
+      it 'adds their response to the satisfaction survey' do
+        application_form = create(:application_form, satisfaction_survey: { 'How was your experience' => '2' })
+        described_class.new(question: 'I found this service unnecessarily complex', response: '3').save(application_form)
+
+        expect(application_form.satisfaction_survey).to eq(
+          {
+            'How was your experience' => '2',
+            'I found this service unnecessarily complex' => '3',
+          },
+        )
       end
     end
   end
