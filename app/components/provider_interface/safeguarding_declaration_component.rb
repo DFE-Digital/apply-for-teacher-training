345acc0b3dd0ceece1fae3dbe5e5286a77aa92fe
Steve Hook <steve.hook@gmail.com>
2020-04-06 15:51:37 +0100

    Handle the missing answer case in SafeguardingDeclarationComponent
    For applications that predate the safeguarding question the value is
    blank. We need to make sure we don't flag this as a false positive.

--- app/components/provider_interface/safeguarding_declaration_component.rb
+++ app/components/provider_interface/safeguarding_declaration_component.rb
@@ -8,8 +8,28 @@ module ProviderInterface
       @application_form = application_form
     end
 
+    HAS_DISCLOSED_MESSAGE = 'The candidate has shared information related to safeguarding. Please contact them directly for more details.'.freeze
+    NO_ANSWER_MESSAGE = 'Not answered'.freeze
+    NO_INFO_MESSAGE = 'No information shared'.freeze
+
+    def message
+      if has_no_answer?
+        NO_ANSWER_MESSAGE
+      elsif has_disclosed_safeguarding_issues?
+        HAS_DISCLOSED_MESSAGE
+      else
+        NO_INFO_MESSAGE
+      end
+    end
+
+  private
+
     def has_disclosed_safeguarding_issues?
       @application_form.safeguarding_issues != 'No'
+    end
+
+    def has_no_answer?
+      @application_form.safeguarding_issues.blank?
     end
   end
 end
--- spec/components/provider_interface/safeguarding_declaration_component_spec.rb
+++ spec/components/provider_interface/safeguarding_declaration_component_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe ProviderInterface::SafeguardingDeclarationComponent do
   let(:heading) { 'Criminal convictions and professional misconduct' }
   let(:has_shared_text) { 'The candidate has shared information related to safeguarding. Please contact them directly for more details.' }
   let(:has_not_shared_text) { 'No information shared' }
+  let(:has_not_answered_text) { 'Not answered' }
 
 
   context 'when the candidate has entered "Yes" as an answer to the safeguarding question' do
@@ -35,6 +36,17 @@ RSpec.describe ProviderInterface::SafeguardingDeclarationComponent do
 
       expect(result.text).to include(heading)
       expect(result.text).to include(has_not_shared_text)
+    end
+  end
+
+  context 'when the candidate has not answered the safeguarding question' do
+    it 'displays the corrent text' do
+      application_form = build_stubbed(:application_form, safeguarding_issues: nil)
+
+      result = render_inline(described_class.new(application_form: application_form))
+
+      expect(result.text).to include(heading)
+      expect(result.text).to include(has_not_answered_text)
     end
   end
 end
