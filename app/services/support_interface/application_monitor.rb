e505b77998b71e23ab2ff1f7164ccd06b094ebf1
Tijmen Brommet <tijmen@gmail.com>
2020-04-06 17:32:03 +0100

    Don't show finished applications in action required list
    We don't need to bother with these because no action can be required.

--- app/services/support_interface/application_monitor.rb
+++ app/services/support_interface/application_monitor.rb
@@ -1,21 +1,27 @@
 module SupportInterface
   class ApplicationMonitor
     def applications_to_disabled_courses
-      ApplicationChoice.where(
+      active_applications.where(
         course_option: CourseOption.joins(:course).where('courses.open_on_apply' => false),
       ).map(&:application_form)
     end
 
     def applications_to_hidden_courses
-      ApplicationChoice.where(
+      active_applications.where(
         course_option: CourseOption.joins(:course).where('courses.open_on_apply' => true, 'courses.exposed_in_find' => false),
       ).map(&:application_form)
     end
 
     def applications_to_full_courses
-      ApplicationChoice.where(
+      active_applications.where(
         course_option: CourseOption.where(invalidated_by_find: true),
       ).map(&:application_form)
+    end
+
+  private
+
+    def active_applications
+      ApplicationChoice.where('status NOT IN (?)', %w[rejected declined withdrawn enrolled conditions_not_met])
     end
   end
 end
--- spec/services/support_interface/application_monitor_spec.rb
+++ spec/services/support_interface/application_monitor_spec.rb
@@ -3,23 +3,26 @@ require 'rails_helper'
 RSpec.describe SupportInterface::ApplicationMonitor do
   describe '#applications_to_disabled_courses' do
     it 'returns applications to courses that have been disabled' do
-      application_to_open_course = create(:application_choice)
+      application_to_open_course = create(:application_choice, status: 'awaiting_provider_decision')
       application_to_open_course.course.update! open_on_apply: true
-      application_to_closed_course = create(:application_choice)
+      application_to_closed_course = create(:application_choice, status: 'awaiting_provider_decision')
       application_to_closed_course.course.update! open_on_apply: false
+      rejected_application_to_closed_course = create(:application_choice, status: 'rejected')
+      rejected_application_to_closed_course.course.update! open_on_apply: false
 
       applications = described_class.new.applications_to_disabled_courses
 
       expect(applications.map(&:id)).not_to include(application_to_open_course.application_form_id)
+      expect(applications.map(&:id)).not_to include(rejected_application_to_closed_course.application_form_id)
       expect(applications.map(&:id)).to include(application_to_closed_course.application_form_id)
     end
   end
 
   describe '#applications_to_hidden_courses' do
     it 'returns applications to courses that have been remove from Find' do
-      application_to_visible_course = create(:application_choice)
+      application_to_visible_course = create(:application_choice, status: 'awaiting_provider_decision')
       application_to_visible_course.course.update! open_on_apply: true, exposed_in_find: true
-      application_to_hidden_course = create(:application_choice)
+      application_to_hidden_course = create(:application_choice, status: 'awaiting_provider_decision')
       application_to_hidden_course.course.update! open_on_apply: true, exposed_in_find: false
 
       applications = described_class.new.applications_to_hidden_courses
@@ -31,8 +34,8 @@ RSpec.describe SupportInterface::ApplicationMonitor do
 
   describe '#applications_to_full_courses' do
     it 'returns applications to courses that have marked as no longer having vacancies' do
-      with_vacancies = create(:application_choice, course_option: create(:course_option, invalidated_by_find: false))
-      without_vacancies = create(:application_choice, course_option: create(:course_option, invalidated_by_find: true))
+      with_vacancies = create(:application_choice, status: 'awaiting_provider_decision', course_option: create(:course_option, invalidated_by_find: false))
+      without_vacancies = create(:application_choice, status: 'awaiting_provider_decision', course_option: create(:course_option, invalidated_by_find: true))
 
       applications = described_class.new.applications_to_full_courses
 
