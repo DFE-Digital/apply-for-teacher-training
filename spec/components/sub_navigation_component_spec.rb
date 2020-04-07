c9924cddbac84d677b60ad5e3fe48808dcc719c8
willmcb <william.mcbrien@digital.education.gov.uk>
2020-04-07 14:57:00 +0100

    Restyle SubNavigationComponent and add spec

---
+++ spec/components/sub_navigation_component_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe SubNavigationComponent do
+  let(:items) {
+    [{ name: 'Application', url: '#', current: true },
+     { name: 'Notes', url: '#' },
+     { name: 'Timeline', url: '#' }]
+  }
+
+  context 'nav tabs appearing as selected' do
+    it 'when the item is "current" then that tab is selected' do
+      result = render_inline(described_class.new(items: items))
+
+      expect(result.css('.govuk-tabs__list-item--selected').text).to include('Application')
+    end
+  end
+
+  context 'rendering tabs' do
+    it 'renders all of the nav tabs specified in the items hash passed to it' do
+      result = render_inline(described_class.new(items: items))
+
+      expect(result.text).to include('Application')
+      expect(result.text).to include('Notes')
+      expect(result.text).to include('Timeline')
+    end
+  end
+end
