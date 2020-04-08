fc5b8493b392086b71d89751d811a8c2ad027467
Steve Hook <steve.hook@gmail.com>
2020-04-08 14:39:45 +0100

    Allow out-of-order migrations to be run in either order
    `20200330153007_rename_courses_accrediting_provider_id_part1` depends on `20200331091319_add_courses_accredited_provider_id` but with the original timestamps they ran in the wrong order.

--- db/migrate/20200331091319_add_courses_accredited_provider_id.rb
+++ db/migrate/20200331091319_add_courses_accredited_provider_id.rb
@@ -1,5 +1,11 @@
 class AddCoursesAccreditedProviderId < ActiveRecord::Migration[6.0]
-  def change
+  def up
+    return if column_exists?(:courses, :accredited_provider_id)
+
     add_column :courses, :accredited_provider_id, :integer, null: true
+  end
+
+  def down
+    remove_column :courses, :accredited_provider_id
   end
 end
