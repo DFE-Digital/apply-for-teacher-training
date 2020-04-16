dc89c4865b8a77c2f6e779719e26226eeee20269
Al Davidson <apdavidson@gmail.com>
2020-04-16 12:29:54 +0100

    use bulk format for adding multiple columns

--- db/migrate/20200416094721_add_course_subject_codes_and_funding_type.rb
+++ db/migrate/20200416094721_add_course_subject_codes_and_funding_type.rb
@@ -1,6 +1,8 @@
 class AddCourseSubjectCodesAndFundingType < ActiveRecord::Migration[6.0]
   def change
-    add_column :courses, :subject_codes, :jsonb, null: true
-    add_column :courses, :funding_type, :string, null: true
+    change_table :courses, bulk: true do |t|
+      t.jsonb :subject_codes, null: true
+      t.string :funding_type, null: true
+    end
   end
 end
