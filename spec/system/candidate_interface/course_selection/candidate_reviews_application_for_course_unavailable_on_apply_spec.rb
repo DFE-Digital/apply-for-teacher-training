1419755e40b34aff32c32be632dcff9429b1474c
Steve Hook <steve.hook@gmail.com>
2020-04-16 12:56:51 +0100

    Extend system spec to include validations

--- spec/system/candidate_interface/course_selection/candidate_reviews_application_for_course_unavailable_on_apply_spec.rb
+++ spec/system/candidate_interface/course_selection/candidate_reviews_application_for_course_unavailable_on_apply_spec.rb
@@ -15,6 +15,9 @@ RSpec.feature 'Candidate reviews an application to a course that becomes unavail
 
     then_i_should_see_that_my_course_is_no_longer_on_apply
     and_i_should_see_a_link_to_ucas
+
+    when_i_submit_the_application
+    then_i_see_error_messages_for_the_things_i_was_warned_about
   end
 
   def given_i_am_signed_in
@@ -48,8 +51,7 @@ RSpec.feature 'Candidate reviews an application to a course that becomes unavail
   end
 
   def then_i_should_see_that_my_course_is_no_longer_on_apply
-    course = Course.last
-    expect(page).to have_content("'#{course.name_and_code}' at #{course.provider.name} is not available on Apply for teacher training anymore.")
+    expect(page).to have_content(course_closed_on_apply_message)
     expect(page).to have_content("You can still apply for '#{course.name_and_code}' on UCAS.")
   end
 
@@ -63,5 +65,25 @@ RSpec.feature 'Candidate reviews an application to a course that becomes unavail
 
   def when_i_click_on_check_your_answers
     click_link 'Check your answers before submitting'
+  end
+
+  def when_i_submit_the_application
+    click_link 'Continue'
+  end
+
+  def then_i_see_error_messages_for_the_things_i_was_warned_about
+    within('.govuk-error-summary') do
+      expect(page).to have_content course_closed_on_apply_message
+    end
+  end
+
+private
+
+  def course_closed_on_apply_message
+    "'#{course.name_and_code}' at #{course.provider.name} is not available on Apply for teacher training anymore"
+  end
+
+  def course
+    @course ||= Course.last
   end
 end
