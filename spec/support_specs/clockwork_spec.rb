a3f44f1d4c737623627cce98620943a102504911
Michael Nacos <m.nacos@gmail.com>
2020-04-16 20:53:46 +0100

    increase frequency of clockwork checks

--- spec/support_specs/clockwork_spec.rb
+++ spec/support_specs/clockwork_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe Clockwork do
       expect(Clockwork::Test.ran_job?('ClockworkCheck')).to be_truthy
     end
 
-    it 'runs the job every 5 minutes over the course of an hour' do
+    it 'runs the job every 1 minute over the course of an hour' do
       start_time = Time.zone.local(2019, 11, 7, 2, 0, 0)
       end_time = Time.zone.local(2019, 11, 7, 3, 0, 0)
       Clockwork::Test.run(
@@ -32,7 +32,7 @@ RSpec.describe Clockwork do
         tick_speed: 1.minute,
         file: './config/clock.rb',
       )
-      expect(Clockwork::Test.times_run('ClockworkCheck')).to eq 12
+      expect(Clockwork::Test.times_run('ClockworkCheck')).to eq 60
     end
 
     it 'enqueues a background ClockworkCheck job' do
