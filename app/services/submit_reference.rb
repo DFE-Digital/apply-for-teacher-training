862c0df90e3c5915d8a5cf5ff67e2b6effe62c09
Malcolm Baig <malcolm.baig@gmail.com>
2020-04-22 09:35:54 +0100

    Refactor SubmitReference
    In addition to making the business logic slightly clearer, this change
    simplifies the database changes that occur when enough references have
    been submitted. In this scenario, all record updates handled by
    SubmitReference occur within a single transaction, allowing them to
    roll back to a consistent state in the event of any errors.

--- app/services/submit_reference.rb
+++ app/services/submit_reference.rb
@@ -15,9 +15,11 @@ class SubmitReference
       raise 'Can\'t submit a reference without answers to all questions'
     end
 
-    @reference.update!(feedback_status: 'feedback_provided')
-
-    progress_application_if_enough_references_have_been_submitted
+    if enough_references_have_been_submitted?
+      progress_applications
+    else
+      reference_feedback_provided!
+    end
 
     CandidateMailer.reference_received(@reference).deliver_later
     RefereeMailer.reference_confirmation_email(application_form, reference).deliver_later
@@ -25,23 +27,28 @@ class SubmitReference
 
 private
 
-  def progress_application_if_enough_references_have_been_submitted
-    return unless there_are_now_enough_references_to_progress?
+  # Only progress the applications if the reference that is being submitted is
+  # the 2nd referee, since there might be more than 2 references per form. We
+  # don't want to send the references to the provider *again* when the 3rd or
+  # 4th reference is submitted.
+  def enough_references_have_been_submitted?
+    (
+      application_form.application_references.feedback_provided + [@reference]
+    ).uniq.count == ApplicationForm::MINIMUM_COMPLETE_REFERENCES
+  end
 
+  def reference_feedback_provided!
+    @reference.update!(feedback_status: 'feedback_provided')
+  end
+
+  def progress_applications
     ActiveRecord::Base.transaction do
+      reference_feedback_provided!
       application_form.application_choices.each do |application_choice|
         ApplicationStateChange.new(application_choice).references_complete!
       end
     end
 
     SendApplicationsToProvider.new.call
-  end
-
-  # Only progress the applications if the reference that is being submitted is
-  # the 2nd referee, since there might be more than 2 references per form. We
-  # don't want to send the references to the provider *again* when the 3rd or
-  # 4th reference is submitted.
-  def there_are_now_enough_references_to_progress?
-    application_form.application_references.feedback_provided.count == ApplicationForm::MINIMUM_COMPLETE_REFERENCES
   end
 end
