b136441a9b7916c85d6f31226731b26df6216863
Theodor Vararu <theo@vararu.org>
2020-04-25 13:20:31 +0100

    Add UCAS 1st of June recalculation to RecalculateDates worker
    To ensure that RBD/DBD dates are consistent with UCAS approach, we need
    to update our worker.
    
    All applications that had an RBD or DBD that fell between the 20th of
    April and 31st of May *before* we made that period a working day freeze
    should instead be recalculated for the end of the 1st of June.

--- app/workers/recalculate_dates.rb
+++ app/workers/recalculate_dates.rb
@@ -18,5 +18,43 @@ class RecalculateDates
         SetDeclineByDefault.new(application_form: application_form).call
       end
     end
+
+    ucas_1st_june_adjustment
+  end
+
+  def ucas_1st_june_adjustment
+    # All code related to the UCAS adjustment can be removed 2020-06-01.
+    #
+    # For the second freeze period, UCAS have introduced a manual adjustment to
+    # how RBDs and DBDs should be recalculated.
+    #
+    start_of_second_freeze = DateTime.parse('2020-04-20 00:00:00')
+    end_of_second_freeze = DateTime.parse('2020-05-31 23:59:59')
+    #
+    # Any RBDs and DBDs that were *initially* recalculated to fall between the
+    # 20th of April and the 31st of May must be manually adjusted to the
+    # 1st of June.
+    #
+    adjustment_date = DateTime.parse('2020-06-01 23:59:59')
+    #
+    # The choices that require adjustment should all have RBDs and DBDs that
+    # fall in the ~6 week period after the end of the second freeze.
+    #
+    duration_of_freeze = end_of_second_freeze - start_of_second_freeze # ~6.weeks
+    period_to_adjust = end_of_second_freeze..(end_of_second_freeze + duration_of_freeze)
+
+    Audited.audit_class.as_user('RecalculateDates worker UCAS 1st June adjustment') do
+      ApplicationChoice
+        .where(reject_by_default_at: period_to_adjust)
+        .find_each do |application_choice|
+          application_choice.update!(reject_by_default_at: adjustment_date)
+        end
+
+      ApplicationChoice
+        .where(decline_by_default_at: period_to_adjust)
+        .find_each do |application_choice|
+          application_choice.update!(decline_by_default_at: adjustment_date)
+        end
+    end
   end
 end
