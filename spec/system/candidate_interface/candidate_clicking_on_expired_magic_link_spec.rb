bed8d97de2de063f105eff34e03cbf20ba2069e5
Malcolm Baig <malcolm.baig@gmail.com>
2020-04-27 14:01:09 +0100

    Rework system specs around magic links
    - Update expired magic links system spec to reflect the fact that
      several CandidateMailer emails now contain an authenticating magic
      link rather than a link to the sign in page.
    - Make expired magic links system spec a bit more focused by removing
      unnecessary steps.
    - Add a step to the system spec for sending an application to a
      provider, making it clear that the candidate can sign into their
      account from the email they receive when this happens.

--- spec/system/candidate_interface/candidate_clicking_on_expired_magic_link_spec.rb
+++ spec/system/candidate_interface/candidate_clicking_on_expired_magic_link_spec.rb
@@ -1,27 +1,19 @@
 require 'rails_helper'
 
-RSpec.feature 'Candidate clicks on an expired link' do
+RSpec.feature 'Candidate clicks on an expired magic link' do
   scenario 'Candidate clicks on a link with an id and expired token link in an email' do
     given_the_pilot_is_open
     and_i_am_a_candidate_with_an_application
     and_i_received_the_submitted_application_email
 
-    when_i_click_on_the_sign_in_link_with_an_id
+    when_i_click_on_an_expired_magic_link
     then_i_am_redirected_to_the_expired_link_page
-
     when_i_click_the_button_to_send_me_a_sign_in_email
     then_i_receive_a_sign_in_email
     and_i_see_the_check_your_email_page
 
-    when_i_visit_the_sign_in_page_with_an_invalid_id_parameter
-    then_i_am_taken_to_the_sign_in_page
-
-    when_i_visit_the_expired_sign_in_page_without_u_parameter
-    then_i_am_taken_to_the_sign_in_page
-
     when_i_fill_in_the_sign_in_form
-
-    when_i_click_on_the_sign_in_link_with_token_after_one_hour
+    when_i_click_on_an_expired_magic_link
     then_i_am_redirected_to_the_expired_link_page
   end
 
@@ -38,9 +30,12 @@ RSpec.feature 'Candidate clicks on an expired link' do
     CandidateMailer.application_submitted(@application_form).deliver_now
   end
 
-  def when_i_click_on_the_sign_in_link_with_an_id
-    open_email(@candidate.email_address)
-    current_email.find_css('a').first.click
+  def when_i_click_on_an_expired_magic_link
+    Timecop.travel(Time.zone.now + 1.hour + 1.minute) do
+      open_email(@candidate.email_address)
+
+      current_email.find_css('a').first.click
+    end
   end
 
   def then_i_am_redirected_to_the_expired_link_page
@@ -60,28 +55,9 @@ RSpec.feature 'Candidate clicks on an expired link' do
     expect(page).to have_content('Check your email')
   end
 
-  def when_i_visit_the_sign_in_page_with_an_invalid_id_parameter
-    visit candidate_interface_sign_in_path(u: 'unencrypted_candidate_id')
-  end
-
-  def then_i_am_taken_to_the_sign_in_page
-    expect(page).to have_content(t('page_titles.sign_in'))
-  end
-
-  def when_i_visit_the_expired_sign_in_page_without_u_parameter
-    visit candidate_interface_expired_sign_in_path
-  end
-
   def when_i_fill_in_the_sign_in_form
+    visit candidate_interface_sign_in_path
     fill_in t('authentication.sign_up.email_address.label'), with: @candidate.email_address
     click_on 'Continue'
-  end
-
-  def when_i_click_on_the_sign_in_link_with_token_after_one_hour
-    Timecop.travel(Time.zone.now + 1.hour + 1.minute) do
-      open_email(@candidate.email_address)
-
-      current_email.find_css('a').first.click
-    end
   end
 end
