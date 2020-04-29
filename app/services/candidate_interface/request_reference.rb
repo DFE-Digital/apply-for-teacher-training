ea0a2cc872d8c556bef27a0d822979cbdbbd9637
Tijmen Brommet <tijmen@gmail.com>
2020-04-29 12:29:01 +0100

    Simplify the arguments to the reference request mailer
    We already know the application_form from the reference, so the first
    argument is redundant.

--- app/services/candidate_interface/request_reference.rb
+++ app/services/candidate_interface/request_reference.rb
@@ -1,7 +1,7 @@
 module CandidateInterface
   class RequestReference
     def self.call(reference)
-      RefereeMailer.reference_request_email(reference.application_form, reference).deliver_later
+      RefereeMailer.reference_request_email(reference).deliver_later
       reference.update!(feedback_status: 'feedback_requested', requested_at: Time.zone.now)
     end
   end
