3b3aee00ecdbf12c17c12faca936ed31862b48d6
Steve Hook <steve.hook@gmail.com>
2020-05-01 12:05:25 +0100

    Add submission steps to apply again system spec

--- spec/system/candidate_interface/candidate_with_unsuccessful_application_applies_again_spec.rb
+++ spec/system/candidate_interface/candidate_with_unsuccessful_application_applies_again_spec.rb
@@ -21,6 +21,11 @@ RSpec.feature 'Candidate with unsuccessful application' do
     and_i_click_on_start_now
 
     then_i_see_a_copy_of_my_application
+
+    when_i_select_a_course
+    and_submit_the_application
+
+    then_application_is_submitted
   end
 
   def given_the_pilot_is_open
@@ -37,7 +42,14 @@ RSpec.feature 'Candidate with unsuccessful application' do
   end
 
   def when_i_have_an_unsuccessful_application
-    @application_form = create(:completed_application_form, candidate: @candidate)
+    @application_form = create(
+      :completed_application_form,
+      :with_completed_references,
+      references_count: 2,
+      with_gces: true,
+      candidate: @candidate,
+      safeguarding_issues_status: :no_safeguarding_issues_to_declare,
+    )
     create(:application_choice, status: :rejected, application_form: @application_form)
   end
 
@@ -55,5 +67,27 @@ RSpec.feature 'Candidate with unsuccessful application' do
 
   def then_i_see_a_copy_of_my_application
     expect(page).to have_content('Your new application is ready for editing')
+  end
+
+  def when_i_select_a_course
+    given_courses_exist
+    and_the_suitability_to_work_with_children_feature_flag_is_on
+
+    click_link 'Course choices'
+    candidate_fills_in_course_choices
+  end
+
+  def and_submit_the_application
+    click_link 'Check and submit your application'
+    click_link 'Continue'
+    choose 'No' # "Is there anything else you would like to tell us?"
+
+    click_button 'Submit application'
+
+    @application = ApplicationForm.last
+  end
+
+  def then_application_is_submitted
+    expect(page).to have_content('Application successfully submitted')
   end
 end
