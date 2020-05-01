777eb7b6dfadde4f1e3443a97483950fff528fdf
Tijmen Brommet <tijmen@gmail.com>
2020-05-01 12:46:31 +0100

    Fix unrunnable migrations
    We fixed the API inflection in Rails, which means the migrations should
    be renamed.

--- db/migrate/20191016153741_add_provider_to_vendor_api_tokens.rb
+++ db/migrate/20191016153741_add_provider_to_vendor_api_tokens.rb
@@ -1,4 +1,4 @@
-class AddProviderToVendorApiTokens < ActiveRecord::Migration[6.0]
+class AddProviderToVendorAPITokens < ActiveRecord::Migration[6.0]
   def up
     execute 'TRUNCATE vendor_api_tokens'
     add_reference :vendor_api_tokens, :provider, null: false, foreign_key: { on_delete: :cascade }
