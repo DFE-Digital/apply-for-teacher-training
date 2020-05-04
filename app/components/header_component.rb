c23621a3509ca2da1ffe450d99d805d05ea0c021
Michael Nacos <m.nacos@gmail.com>
2020-05-04 15:07:43 +0100

    fix applications list max-width; remove wide/:page_width

--- app/components/header_component.rb
+++ app/components/header_component.rb
@@ -1,11 +1,10 @@
 class HeaderComponent < ViewComponent::Base
-  attr_reader :navigation_items, :service_url, :service_name, :classes, :wide
+  attr_reader :navigation_items, :service_url, :service_name, :classes
 
-  def initialize(navigation_items:, service_name:, service_url:, classes: '', wide: false)
+  def initialize(navigation_items:, service_name:, service_url:, classes: '')
     @navigation_items = navigation_items
     @service_name     = service_name
     @service_url      = service_url
     @classes          = classes
-    @wide             = wide
   end
 end
