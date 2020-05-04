b47043d19492f5add7815a71cf5b3da260a83785
David Gisbey <davidgisbey@gmail.com>
2020-05-04 21:50:07 +0100

    Refactor to use apply_again feature flag

--- app/services/feature_flag.rb
+++ app/services/feature_flag.rb
@@ -9,7 +9,7 @@ class FeatureFlag
   ].freeze
 
   TEMPORARY_FEATURE_FLAGS = %w[
-    apply_again_email_content
+    apply_again
     candidate_can_cancel_reference
     confirm_conditions
     download_dataset1_from_support_page
