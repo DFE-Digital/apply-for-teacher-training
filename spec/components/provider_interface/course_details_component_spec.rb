c1de84791106caeb4008a80b40a783ca16263e32
willmcb <william.mcbrien@digital.education.gov.uk>
2020-05-05 17:48:07 +0100

    Add method to make row reordering easier in future

--- spec/components/provider_interface/course_details_component_spec.rb
+++ spec/components/provider_interface/course_details_component_spec.rb
@@ -15,48 +15,77 @@ RSpec.describe ProviderInterface::CourseDetailsComponent do
 
   let(:render) { render_inline(described_class.new(application_choice: application_choice)) }
 
+  def row_text_selector(row_name, render)
+    rows = { provider: 0,
+             accredited_body: 1,
+             course: 2,
+             cycle: 3,
+             location: 4,
+             full_or_part_time: 5,
+             funding_type: 6 }
+
+    render.css('.govuk-summary-list__row')[rows[row_name]].text
+  end
+
   it 'renders the provider name and code' do
-    expect(render.css('.govuk-summary-list__row')[0].text).to include('Provider')
-    expect(render.css('.govuk-summary-list__row')[0].text).to include(application_choice.provider.name_and_code)
+    render_text = row_text_selector(:provider, render)
+
+    expect(render_text).to include('Provider')
+    expect(render_text).to include(application_choice.provider.name_and_code)
   end
 
   context 'when an accredited body is present' do
     it 'renders the accredited body name and code when it is present' do
       render = render_inline(described_class.new(application_choice: application_choice_with_accredited_body))
-      expect(render.css('.govuk-summary-list__row')[1].text).to include('Accredited body')
-      expect(render.css('.govuk-summary-list__row')[1].text).to include(application_choice_with_accredited_body.course.accredited_provider.name)
+
+      render_text = row_text_selector(:accredited_body, render)
+
+      expect(render_text).to include('Accredited body')
+      expect(render_text).to include(application_choice_with_accredited_body.course.accredited_provider.name)
     end
   end
 
   context 'when an accredited body is not present' do
     it 'renders the provider name and code in place of the accredited body name and code' do
-      expect(render.css('.govuk-summary-list__row')[1].text).to include('Accredited body')
-      expect(render.css('.govuk-summary-list__row')[1].text).to include(application_choice.provider.name_and_code)
+      render_text = row_text_selector(:accredited_body, render)
+
+      expect(render_text).to include('Accredited body')
+      expect(render_text).to include(application_choice.provider.name_and_code)
     end
   end
 
   it 'renders the course name and code' do
-    expect(render.css('.govuk-summary-list__row')[2].text).to include('Course')
-    expect(render.css('.govuk-summary-list__row')[2].text).to include(application_choice.course.name_and_code)
+    render_text = row_text_selector(:course, render)
+
+    expect(render_text).to include('Course')
+    expect(render_text).to include(application_choice.course.name_and_code)
   end
 
   it 'renders the recruitment cycle year' do
-    expect(render.css('.govuk-summary-list__row')[3].text).to include('Cycle')
-    expect(render.css('.govuk-summary-list__row')[3].text).to include(application_choice.course.recruitment_cycle_year.to_s)
+    render_text = row_text_selector(:cycle, render)
+
+    expect(render_text).to include('Cycle')
+    expect(render_text).to include(application_choice.course.recruitment_cycle_year.to_s)
   end
 
   it 'renders the preferred location' do
-    expect(render.css('.govuk-summary-list__row')[4].text).to include('Preferred location')
-    expect(render.css('.govuk-summary-list__row')[4].text).to include(application_choice.site.name_and_code)
+    render_text = row_text_selector(:location, render)
+
+    expect(render_text).to include('Preferred location')
+    expect(render_text).to include(application_choice.site.name_and_code)
   end
 
   it 'renders the study mode' do
-    expect(render.css('.govuk-summary-list__row')[5].text).to include('Full or part time')
-    expect(render.css('.govuk-summary-list__row')[5].text).to include(application_choice.course_option.study_mode.humanize)
+    render_text = row_text_selector(:full_or_part_time, render)
+
+    expect(render_text).to include('Full or part time')
+    expect(render_text).to include(application_choice.course_option.study_mode.humanize)
   end
 
   it 'renders financing funding type of a course' do
-    expect(render.css('.govuk-summary-list__row')[6].text).to include('Funding type')
-    expect(render.css('.govuk-summary-list__row')[6].text).to include('Fee paying')
+    render_text = row_text_selector(:funding_type, render)
+
+    expect(render_text).to include('Funding type')
+    expect(render_text).to include('Fee paying')
   end
 end
