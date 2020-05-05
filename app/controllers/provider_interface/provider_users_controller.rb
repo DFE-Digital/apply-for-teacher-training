d8eec3e46cf38b830b08e8187e9e054e0a3f0856
Steve Laing <steve.laing@gmail.com>
2020-05-05 09:39:47 +0100

    Add routing, controllers and views to show a provider user and update permissions
    Adds the wiring to hook up editing ProviderUser permissions, this also
    affects how support users administer provider users.
    Adds separate edit providers form specific to provider interface.

--- app/controllers/provider_interface/provider_users_controller.rb
+++ app/controllers/provider_interface/provider_users_controller.rb
@@ -1,9 +1,23 @@
 module ProviderInterface
   class ProviderUsersController < ProviderInterfaceController
-    before_action :requires_provider_add_provider_users_feature_flag, only: %i[new create]
+    before_action :requires_provider_add_provider_users_feature_flag
+    before_action :redirect_unless_permitted_to_manage_users
 
     def index
       @provider_users = ProviderUser.visible_to(current_provider_user)
+    end
+
+    def show
+      @provider_user = ProviderUser.visible_to(current_provider_user).find_by(id: params[:id])
+
+      redirect_to(action: :index) and return unless @provider_user
+
+      form = ProviderUserForm.new(
+        provider_user: @provider_user,
+        current_provider_user: current_provider_user,
+      )
+      @possible_permissions = form.possible_permissions
+      @provider_permissions = form.provider_permissions
     end
 
     def new
@@ -17,12 +31,18 @@ module ProviderInterface
 
     def create
       @form = ProviderUserForm.new(
-        provider_user_params.merge(current_provider_user: current_provider_user),
+        provider_user_params.merge(
+          current_provider_user: current_provider_user,
+          provider_permissions: provider_permissions_params,
+        ),
       )
       provider_user = @form.build
       service = SaveAndInviteProviderUser.new(
         form: @form,
-        save_service: SaveProviderUser.new(provider_user: provider_user),
+        save_service: SaveProviderUser.new(
+          provider_user: provider_user,
+          provider_permissions: @form.provider_permissions,
+        ),
         invite_service: InviteProviderUser.new(provider_user: provider_user),
         new_user: @form.existing_provider_user.blank?,
       )
@@ -33,15 +53,56 @@ module ProviderInterface
       redirect_to provider_interface_provider_users_path
     end
 
+    def edit_providers
+      provider_user = ProviderUser.visible_to(current_provider_user).find_by(id: params[:provider_user_id])
+      @form = ProviderUserForm.from_provider_user(provider_user)
+      @form.current_provider_user = current_provider_user
+    end
+
+    def update_providers
+      provider_user = ProviderUser
+        .visible_to(current_provider_user)
+        .find_by(id: params[:provider_user_id])
+
+      @form = ProviderUserForm.new(
+        provider_user: provider_user,
+        current_provider_user: current_provider_user,
+        provider_permissions: provider_permissions_params,
+      )
+
+      service = SaveProviderUser.new(
+        provider_user: provider_user,
+        provider_permissions: @form.provider_permissions,
+        deselected_provider_permissions: @form.deselected_provider_permissions,
+      )
+
+      render :edit_providers and return unless @form.valid? && service.call!
+
+      flash[:success] = 'Providers updated'
+      redirect_to provider_interface_provider_user_path(provider_user)
+    end
+
   private
 
     def provider_user_params
       params.require(:provider_interface_provider_user_form)
-            .permit(:email_address, :first_name, :last_name, provider_ids: [])
+            .permit(:email_address, :first_name, :last_name)
+    end
+
+    def provider_permissions_params
+      params.require(:provider_interface_provider_user_form)
+            .permit(provider_permissions_forms: {})
+            .fetch(:provider_permissions_forms, {})
+            .to_h
     end
 
     def requires_provider_add_provider_users_feature_flag
       raise unless FeatureFlag.active?('provider_add_provider_users')
+    end
+
+    def redirect_unless_permitted_to_manage_users
+      can_manage_users = ProviderPermissions.exists?(provider_user: current_provider_user, manage_users: true)
+      redirect_to root_path, warning: 'You do not have sufficient permissions to manage other users' unless can_manage_users
     end
   end
 end
---
+++ spec/system/provider_interface/manage_provider_user_permissions_spec.rb
@@ -0,0 +1,103 @@
+require 'rails_helper'
+
+RSpec.feature 'Managing provider user permissions' do
+  include DfESignInHelpers
+
+  scenario 'Provider manages permissions for users' do
+    given_i_am_a_provider_user_with_dfe_sign_in
+    and_the_provider_add_provider_users_feature_is_enabled
+    and_i_can_manage_applications_for_two_providers
+    and_i_can_manage_users_for_a_provider
+    and_i_sign_in_to_the_provider_interface
+
+    when_i_click_on_the_users_link
+    and_i_click_on_a_user
+    and_i_click_change_providers_and_permissions
+
+    then_i_see_providers_and_permissions
+    and_i_add_permission_to_manage_users_for_a_provider_user
+    then_i_can_see_the_new_permission_for_the_provider_user
+    and_i_click_change_providers_and_permissions
+
+    when_i_remove_a_permissions_from_a_provider_user
+    then_i_cant_see_the_permission_for_the_provider_user
+  end
+
+  def given_i_am_a_provider_user_with_dfe_sign_in
+    provider_exists_in_dfe_sign_in
+  end
+
+  def and_i_can_manage_applications_for_two_providers
+    provider_user_exists_in_apply_database
+    @managing_user = ProviderUser.find_by(dfe_sign_in_uid: 'DFE_SIGN_IN_UID')
+    @provider = Provider.find_by(code: 'ABC')
+    @another_provider = Provider.find_by(code: 'DEF')
+  end
+
+  def and_i_can_manage_users_for_a_provider
+    @managed_user = create(:provider_user, providers: [@provider])
+    @managing_user.provider_permissions.find_by(provider: @provider).update(manage_users: true)
+  end
+
+  def and_the_provider_add_provider_users_feature_is_enabled
+    FeatureFlag.activate('provider_add_provider_users')
+  end
+
+  def when_i_click_on_the_users_link
+    click_on('Users')
+  end
+
+  def and_i_click_on_a_user
+    click_on(@managed_user.full_name)
+  end
+
+  def and_i_click_change_providers_and_permissions
+    click_on('Change')
+  end
+
+  def then_i_see_providers_and_permissions
+    within(permissions_fields_id_for_provider(@provider)) do
+      expect(page).to have_unchecked_field 'Manage users'
+    end
+  end
+
+  def and_i_add_permission_to_manage_users_for_a_provider_user
+    expect(page).not_to have_checked_field 'Manage users'
+
+    within(permissions_fields_id_for_provider(@provider)) do
+      check 'Manage users'
+    end
+
+    click_on 'Update providers'
+  end
+
+  def then_i_can_see_the_new_permission_for_the_provider_user
+    expect(page).to have_content 'Providers updated'
+
+    within("#provider-#{@provider.id}-enabled-permissions") do
+      expect(page).to have_content 'Manage users'
+    end
+
+    expect(@managed_user.provider_permissions.first.manage_users).to be true
+  end
+
+  def when_i_remove_a_permissions_from_a_provider_user
+    within(permissions_fields_id_for_provider(@provider)) do
+      expect(page).to have_checked_field 'Manage users'
+      uncheck 'Manage users'
+    end
+
+    click_on 'Update providers'
+  end
+
+  def then_i_cant_see_the_permission_for_the_provider_user
+    expect(page).to have_content 'Providers updated'
+    expect(page).not_to have_content 'Manage users'
+
+    expect(@managed_user.provider_permissions.first.manage_users).to be false
+  end
+
+  def permissions_fields_id_for_provider(provider)
+    "#provider-interface-provider-user-form-provider-permissions-forms-#{provider.id}-active-true-conditional"
+  end
+end
