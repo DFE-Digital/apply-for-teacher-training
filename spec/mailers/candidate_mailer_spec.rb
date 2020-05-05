a5c65c1ba188806fb8fbbf34e03a2577250e55e1
David Gisbey <davidgisbey@gmail.com>
2020-05-05 09:38:19 +0100

    Add mailer tests for new emails

--- spec/mailers/candidate_mailer_spec.rb
+++ spec/mailers/candidate_mailer_spec.rb
@@ -178,11 +178,12 @@ RSpec.describe CandidateMailer, type: :mailer do
 
       it_behaves_like 'a mail with subject and content', :declined_by_default, 'Applications withdrawn automatically', {}
     end
+  end
 
-    context 'when the covid-19 feature flag and the apply_again flag are on' do
+  describe '#decline_by_default_without_rejections' do
+    context 'when the covid-19 feature flag' do
       before do
         FeatureFlag.activate('covid_19')
-        FeatureFlag.activate('apply_again')
         @application_form = build_stubbed(
           :application_form,
           application_choices: [build_stubbed(:application_choice, status: 'declined', declined_by_default: true, decline_by_default_days: 10)],
@@ -190,9 +191,10 @@ RSpec.describe CandidateMailer, type: :mailer do
       end
 
       it_behaves_like(
-        'a mail with subject and content', :declined_by_default,
-        'Application withdrawn automatically',
-        'Reason' => 'You did not respond in time so we declined your'
+        'a mail with subject and content',
+        :declined_by_default_without_rejections,
+        'You did not respond to your offer: next steps',
+        'Reason' => 'You did not respond in time so we declined your',
       )
     end
 
@@ -206,35 +208,14 @@ RSpec.describe CandidateMailer, type: :mailer do
       end
 
       it_behaves_like(
-        'a mail with subject and content', :declined_by_default,
-        'Application withdrawn automatically',
-        'Reason' => 'You did not respond within'
-      )
-    end
-
-    context 'when a candidate has 1 offer that was declined, is awaiting another decision and the apply_again flag is on' do
-      before do
-        FeatureFlag.activate('apply_again')
-        @application_form = build_stubbed(
-          :application_form,
-          first_name: 'Fred',
-          application_choices: [
-            build_stubbed(:application_choice, status: 'declined', declined_by_default: true, decline_by_default_days: 10),
-            build_stubbed(:application_choice, status: 'awaiting_provider_decision', declined_by_default: false, decline_by_default_days: 10),
-          ],
-        )
-      end
-
-      it_behaves_like(
         'a mail with subject and content',
-        :declined_by_default,
-        'Application withdrawn automatically',
-        'heading' => 'Dear Fred',
-        'days left to respond' => '10 working days',
+        :declined_by_default_without_rejections,
+        'You did not respond to your offer: next steps',
+        'Reason' => 'You did not respond within',
       )
     end
 
-    context 'when a candidate has 1 offer that was declined, has no rejections, is not awaiting further decisions and the apply_again flag is on' do
+    context 'when a candidate has 1 offer that was declined by default' do
       before do
         FeatureFlag.activate('apply_again')
         @application_form = build_stubbed(
@@ -248,15 +229,15 @@ RSpec.describe CandidateMailer, type: :mailer do
 
       it_behaves_like(
         'a mail with subject and content',
-        :declined_by_default,
-        'Application withdrawn automatically',
+        :declined_by_default_without_rejections,
+        'You did not respond to your offer: next steps',
         'heading' => 'Dear Fred',
-        'days left to respond' => '10 working days',
+        'DBD_days_they_had_to_respond' => '10 working days',
         'still_interested' => 'You didn’t pursue your teacher training application',
       )
     end
 
-    context 'when a candidate has 1 offer that was declined, has a rejection, is not awaiting further decisions and the apply_again flag is on' do
+    context 'when a candidate has 2 offers that were declined by default' do
       before do
         FeatureFlag.activate('apply_again')
         @application_form = build_stubbed(
@@ -264,17 +245,106 @@ RSpec.describe CandidateMailer, type: :mailer do
           first_name: 'Fred',
           application_choices: [
             build_stubbed(:application_choice, status: 'declined', declined_by_default: true, decline_by_default_days: 10),
-            build_stubbed(:application_choice, status: 'rejected', declined_by_default: false, decline_by_default_days: 10),
+            build_stubbed(:application_choice, status: 'declined', declined_by_default: true, decline_by_default_days: 10),
           ],
         )
       end
 
       it_behaves_like(
         'a mail with subject and content',
-        :declined_by_default,
-        'Application withdrawn automatically',
+        :declined_by_default_without_rejections,
+        'You did not respond to your offers: next steps',
         'heading' => 'Dear Fred',
-        'days left to respond' => '10 working days',
+        'DBD_days_they_had_to_respond' => '10 working days',
+        'still_interested' => 'You didn’t pursue your teacher training application',
+      )
+    end
+  end
+
+  describe '#decline_by_default_with_rejections' do
+    context 'when the covid-19 feature flag' do
+      before do
+        FeatureFlag.activate('covid_19')
+        @application_form = build_stubbed(
+          :application_form,
+          application_choices: [
+            build_stubbed(:application_choice, status: 'declined', declined_by_default: true, decline_by_default_days: 10),
+            build_stubbed(:application_choice, status: 'rejected'),
+          ],
+        )
+      end
+
+      it_behaves_like(
+        'a mail with subject and content',
+        :declined_by_default_with_rejections,
+        'You did not respond to your offer: next steps',
+        'Reason' => 'You did not respond in time so we declined your',
+      )
+    end
+
+    context 'when the covid-19 feature flag is off and the apply_again flag is on' do
+      before do
+        FeatureFlag.activate('apply_again')
+        @application_form = build_stubbed(
+          :application_form,
+          application_choices: [
+            build_stubbed(:application_choice, status: 'declined', declined_by_default: true, decline_by_default_days: 10),
+            build_stubbed(:application_choice, status: 'rejected'),
+            ],
+        )
+      end
+
+      it_behaves_like(
+        'a mail with subject and content',
+        :declined_by_default_with_rejections,
+        'You did not respond to your offer: next steps',
+        'Reason' => 'You did not respond within',
+      )
+    end
+
+    context 'when a candidate has 1 offer that was declined by default' do
+      before do
+        FeatureFlag.activate('apply_again')
+        @application_form = build_stubbed(
+          :application_form,
+          first_name: 'Fred',
+          application_choices: [
+            build_stubbed(:application_choice, status: 'declined', declined_by_default: true, decline_by_default_days: 10),
+            build_stubbed(:application_choice, status: 'rejected'),
+          ],
+        )
+      end
+
+      it_behaves_like(
+        'a mail with subject and content',
+        :declined_by_default_with_rejections,
+        'You did not respond to your offer: next steps',
+        'heading' => 'Dear Fred',
+        'DBD_days_they_had_to_respond' => '10 working days',
+        'still_interested' => 'If now’s the right time for you',
+      )
+    end
+
+    context 'when a candidate has 2 offers that were declined by default' do
+      before do
+        FeatureFlag.activate('apply_again')
+        @application_form = build_stubbed(
+          :application_form,
+          first_name: 'Fred',
+          application_choices: [
+            build_stubbed(:application_choice, status: 'declined', declined_by_default: true, decline_by_default_days: 10),
+            build_stubbed(:application_choice, status: 'declined', declined_by_default: true, decline_by_default_days: 10),
+            build_stubbed(:application_choice, status: 'rejected'),
+          ],
+        )
+      end
+
+      it_behaves_like(
+        'a mail with subject and content',
+        :declined_by_default_with_rejections,
+        'You did not respond to your offers: next steps',
+        'heading' => 'Dear Fred',
+        'DBD_days_they_had_to_respond' => '10 working days',
         'still_interested' => 'If now’s the right time for you',
       )
     end
