c7b3458503bc461512e340613b52ea94ab70daef
Steve Laing <steve.laing@gmail.com>
2020-05-06 11:45:54 +0100

    Make possible_permissions a class method on ProviderPermissions
    This method was previously part of ProviderUserForm but used in a non-form
    based action. To simplify, move the method to the owning model.

--- app/models/provider_permissions.rb
+++ app/models/provider_permissions.rb
@@ -9,4 +9,20 @@ class ProviderPermissions < ActiveRecord::Base
   audited associated_with: :provider_user
 
   scope :manage_users, -> { where(manage_users: true) }
+
+  def self.possible_permissions(current_provider_user:, provider_user:)
+    provider_ids = current_provider_user
+      .provider_permissions
+      .manage_users
+      .includes(:provider)
+      .order('providers.name')
+      .pluck(:provider_id)
+
+    provider_ids.map do |id|
+      find_or_initialize_by(
+        provider_id: id,
+        provider_user_id: provider_user&.id,
+      )
+    end
+  end
 end
