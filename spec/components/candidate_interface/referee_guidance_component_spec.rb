61df092d0183fb8b6891adcd35a9d4bda6674e55
David Gisbey <davidgisbey@gmail.com>
2020-05-11 12:13:01 +0100

    Add setup application method to spec

--- spec/components/candidate_interface/referee_guidance_component_spec.rb
+++ spec/components/candidate_interface/referee_guidance_component_spec.rb
@@ -5,14 +5,21 @@ RSpec.describe CandidateInterface::RefereeGuidanceComponent do
 
   let(:provider) { create(:provider) }
 
+  def setup_application
+    course_option_for_provider(provider: provider)
+    @application_form = create(:application_form)
+    create(:application_choice, application_form: @application_form, course_option: provider.courses.first.course_options.first)
+    create(:reference, :requested, application_form: @application_form)
+  end
+
+  before do
+    setup_application
+  end
+
   context 'when one reference is in the feedback_requested state' do
     context 'when the candidates courses all have the same provider' do
-      it 'renders the correct singular content for referees, references and providers' do
-        course_option_for_provider(provider: provider)
-        application_form = create(:application_form)
-        create(:application_choice, application_form: application_form, course_option: provider.courses.first.course_options.first)
-        create(:reference, :requested, application_form: application_form)
-        result = render_inline(described_class.new(application_form: application_form))
+      it 'renders the correct pluralization for referees, references and providerss' do
+        result = render_inline(described_class.new(application_form: @application_form))
 
         expect(result.css('.govuk-heading-m').text).to eq('Reference')
         expect(result.css('.govuk-body').text).to include('training provider')
@@ -21,15 +28,11 @@ RSpec.describe CandidateInterface::RefereeGuidanceComponent do
     end
 
     context 'when the candidates courses have different providers' do
-      it 'renders the correct singular content for referees, references and providers' do
+      it 'renders the correct pluralization for referees, references and providers' do
         provider2 = create(:provider)
-        course_option_for_provider(provider: provider)
         course_option_for_provider(provider: provider2)
-        application_form = create(:application_form)
-        create(:application_choice, application_form: application_form, course_option: provider.courses.first.course_options.first)
-        create(:application_choice, application_form: application_form, course_option: provider2.courses.first.course_options.first)
-        create(:reference, :requested, application_form: application_form)
-        result = render_inline(described_class.new(application_form: application_form))
+        create(:application_choice, application_form: @application_form, course_option: provider2.courses.first.course_options.first)
+        result = render_inline(described_class.new(application_form: @application_form))
 
         expect(result.css('.govuk-heading-m').text).to eq('Reference')
         expect(result.css('.govuk-body').text).to include('training providers')
@@ -39,12 +42,9 @@ RSpec.describe CandidateInterface::RefereeGuidanceComponent do
 
   context 'when two references are in the feedback_requested state' do
     context 'when the candidates courses all have the same provider' do
-      it 'renders the correct singular content for referees, references and providers' do
-        course_option_for_provider(provider: provider)
-        application_form = create(:application_form)
-        create(:application_choice, application_form: application_form, course_option: provider.courses.first.course_options.first)
-        create_list(:reference, 2, :requested, application_form: application_form)
-        result = render_inline(described_class.new(application_form: application_form))
+      it 'renders the correct pluralization for referees, references and providers' do
+        create(:reference, :requested, application_form: @application_form)
+        result = render_inline(described_class.new(application_form: @application_form))
 
         expect(result.css('.govuk-heading-m').text).to eq('References')
         expect(result.css('.govuk-body').text).to include('training provider')
@@ -53,15 +53,12 @@ RSpec.describe CandidateInterface::RefereeGuidanceComponent do
     end
 
     context 'when the candidates courses have different providers' do
-      it 'renders the correct singular content for referees, references and providers' do
+      it 'renders the correct pluralization for referees, references and providers' do
         provider2 = create(:provider)
-        course_option_for_provider(provider: provider)
         course_option_for_provider(provider: provider2)
-        application_form = create(:application_form)
-        create(:application_choice, application_form: application_form, course_option: provider.courses.first.course_options.first)
-        create(:application_choice, application_form: application_form, course_option: provider2.courses.first.course_options.first)
-        create_list(:reference, 2, :requested, application_form: application_form)
-        result = render_inline(described_class.new(application_form: application_form))
+        create(:application_choice, application_form: @application_form, course_option: provider2.courses.first.course_options.first)
+        create(:reference, :requested, application_form: @application_form)
+        result = render_inline(described_class.new(application_form: @application_form))
 
         expect(result.css('.govuk-heading-m').text).to eq('References')
         expect(result.css('.govuk-body').text).to include('training providers')
