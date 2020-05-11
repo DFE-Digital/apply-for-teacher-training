46021344d767c1b2a59d45c34b18f6c88ccb4caa
David Gisbey <davidgisbey@gmail.com>
2020-05-11 12:02:59 +0100

    Add in flow for when user is awaiting 2 references

--- spec/components/candidate_interface/referee_guidance_component_spec.rb
+++ spec/components/candidate_interface/referee_guidance_component_spec.rb
@@ -36,4 +36,36 @@ RSpec.describe CandidateInterface::RefereeGuidanceComponent do
       end
     end
   end
+
+  context 'when two references are in the feedback_requested state' do
+    context 'when the candidates courses all have the same provider' do
+      it 'renders the correct singular content for referees, references and providers' do
+        course_option_for_provider(provider: provider)
+        application_form = create(:application_form)
+        create(:application_choice, application_form: application_form, course_option: provider.courses.first.course_options.first)
+        create_list(:reference, 2, :requested, application_form: application_form)
+        result = render_inline(described_class.new(application_form: application_form))
+
+        expect(result.css('.govuk-heading-m').text).to eq('References')
+        expect(result.css('.govuk-body').text).to include('training provider')
+        expect(result.css('.govuk-body').text).not_to include('training providers')
+      end
+    end
+
+    context 'when the candidates courses have different providers' do
+      it 'renders the correct singular content for referees, references and providers' do
+        provider2 = create(:provider)
+        course_option_for_provider(provider: provider)
+        course_option_for_provider(provider: provider2)
+        application_form = create(:application_form)
+        create(:application_choice, application_form: application_form, course_option: provider.courses.first.course_options.first)
+        create(:application_choice, application_form: application_form, course_option: provider2.courses.first.course_options.first)
+        create_list(:reference, 2, :requested, application_form: application_form)
+        result = render_inline(described_class.new(application_form: application_form))
+
+        expect(result.css('.govuk-heading-m').text).to eq('References')
+        expect(result.css('.govuk-body').text).to include('training providers')
+      end
+    end
+  end
 end
