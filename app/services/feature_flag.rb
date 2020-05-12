e07879ebf6962778b2617d7546cc5455650f2e54
Steve Hook <steve.hook@gmail.com>
2020-05-12 16:48:51 +0100

    Add details about feature flags
    'Owner' is set to the developer who appears based on the git blame to
    have had most input to the development work. It's a best guess in other
    words.

--- app/services/feature_flag.rb
+++ app/services/feature_flag.rb
@@ -8,27 +8,27 @@ class FeatureFlag
   end
 
   PERMANENT_SETTINGS = [
-    [:banner_about_problems_with_dfe_sign_in, '', ''],
-    [:banner_for_ucas_downtime, '', ''],
-    [:covid_19, '', ''],
-    [:dfe_sign_in_fallback, '', ''],
-    [:force_ok_computer_to_fail, '', ''],
-    [:pilot_open, 'Enables the Apply for Teacher Training service', 'Theo'],
+    [:banner_about_problems_with_dfe_sign_in, 'Displays a banner to notify users that DfE-Sign is having problems', 'Jake Benilov'],
+    [:banner_for_ucas_downtime, 'Displays a banner to notify users that UCAS is having problems', 'Wen Ting Wang'],
+    [:covid_19, 'Alters deadlines and displays an information banner related to our response to Covid-19', 'Theodor Vararu'],
+    [:dfe_sign_in_fallback, 'Use this when DfE Sign-in is down', 'Tijmen Brommet'],
+    [:force_ok_computer_to_fail, 'OK Computer implements a health check endpoint, this flag forces it to fail for testing purposes', 'Michael Nacos'],
+    [:pilot_open, 'Enables the Apply for Teacher Training service', 'Tijmen Brommet'],
   ].freeze
 
   TEMPORARY_FEATURE_FLAGS = [
-    [:confirm_conditions, '', ''],
-    [:download_dataset1_from_support_page, '', ''],
-    [:notes, '', ''],
-    [:provider_add_provider_users, '', ''],
-    [:provider_application_filters, '', ''],
-    [:provider_change_response, '', ''],
-    [:provider_interface_work_breaks, '', ''],
-    [:provider_view_safeguarding, '', ''],
-    [:support_sign_in_confirmation_email, '', ''],
-    [:timeline, '', ''],
-    [:unavailable_course_option_warnings, '', ''],
-    [:track_validation_errors, '', ''],
+    [:confirm_conditions, 'Allows providers to confirm that conditions related to an offer have been met', 'Michael Nacos'],
+    [:download_dataset1_from_support_page, 'Enables the application CSV download from the support interface', 'Al Davidson'],
+    [:notes, 'Allows providers to add notes to applications', 'Michael Nacos'],
+    [:provider_add_provider_users, 'Allows provider users to invite other provider users', 'Steve Laing'],
+    [:provider_application_filters, 'Allows providers to filter applications returned by the main applications page by status etc.', 'Will McBrien'],
+    [:provider_change_response, 'Allows providers to change the course that they are offering to a candidate', 'Michael Nacos'],
+    [:provider_interface_work_breaks, 'Adds work break information to the provider interface', 'Steve Hook'],
+    [:provider_view_safeguarding, 'Allows providers to see whether a candidate has declared safeguarding issues', 'Will McBrien'],
+    [:support_sign_in_confirmation_email, 'Improves security of support interface with confirmation email to user on first login', 'Tijmen Brommet'],
+    [:timeline, 'Adds a timeline of status change events to the provider interface', 'Steve Hook'],
+    [:unavailable_course_option_warnings, 'Warns candidates at submission time if a course has become unavailable since they originally chose it', 'Malcolm Baig'],
+    [:track_validation_errors, 'Captures validation errors triggered by candidates so that they can be reviewed by support staff', 'Steve Hook'],
     [:apply_again, 'Enables unsuccessful candidates to reapply, AKA Apply 2', 'Steve Hook'],
   ].freeze
 
