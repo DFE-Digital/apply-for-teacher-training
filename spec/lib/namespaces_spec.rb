9e346f179b9145344517ac5faecbbf341e7683f3
Tijmen Brommet <tijmen@gmail.com>
2020-05-14 09:47:44 +0100

    Add a test for namespace isolation
    This test will fail if you use namespaced code in a different context,
    like using ProviderInterface code in the SupportInterface namespace.

---
+++ spec/lib/namespaces_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'Namespace' do
+  namespaces = %w[CandidateInterface ProviderInterface SupportInterface APIDocs RefereeInterface VendorAPI]
+
+  namespaces.each do |namespace|
+    describe namespace.constantize do
+      (namespaces - [namespace]).each do |other_namespace|
+        it "does not use code from the #{other_namespace} namespace" do
+          command = "grep -rnw app/**/#{namespace.underscore} -e '#{other_namespace}'"
+          usage_of_namespaced_code_in_other_namespace = `#{command}`
+          expect(usage_of_namespaced_code_in_other_namespace).to be_empty, usage_of_namespaced_code_in_other_namespace
+        end
+      end
+    end
+  end
+end
