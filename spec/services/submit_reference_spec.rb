e129c5be147f4bfe158546cf5c166034ced8edd6
Malcolm Baig <malcolm.baig@gmail.com>
2020-05-14 16:59:04 +0100

    Remove cool off period when receiving Apply Again references
    An Apply Again application may have removed previous references and
    added new ones. When the minimum number of required references are
    received (currently 2), we want to send the application to the Provider
    as soon as possible rather than wait out the cool off period as we do
    during a standard application.
    
    - SubmitReference already progresses the state of application choices if
      enough references have been received. Add to this logic by checking if
      the application form is apply_again, and if so set the
      application_choice edit_by to the current time.
    - Restructure the SubmitReference spec for clarity and add an example
      for the Apply Again logic.

--- spec/services/submit_reference_spec.rb
+++ spec/services/submit_reference_spec.rb
@@ -1,57 +1,77 @@
 require 'rails_helper'
 
 RSpec.describe SubmitReference do
-  it 'progresses the application choices to the "application complete" status once all references have been received' do
-    application_form = create(:completed_application_form)
-    active_application = create(:application_choice, application_form: application_form, status: 'awaiting_references', edit_by: 1.day.from_now)
-    cancelled_application = create(:application_choice, application_form: application_form, status: 'cancelled')
+  describe '#save!' do
+    let(:application_form) { create(:completed_application_form) }
 
-    create(:reference, :complete, application_form: application_form)
-    reference = create(:reference, :unsubmitted, application_form: application_form)
+    context 'minimum required references received' do
+      it 'progresses the application choices to the "application complete" status' do
+        active_application_choice = create(:application_choice, application_form: application_form, status: 'awaiting_references', edit_by: 1.day.from_now)
+        cancelled_application_choice = create(:application_choice, application_form: application_form, status: 'cancelled')
 
-    reference.update!(feedback: 'Trustworthy', relationship_correction: '', safeguarding_concerns: '')
+        create(:reference, :complete, application_form: application_form)
+        reference = create(:reference, :unsubmitted, application_form: application_form)
 
-    SubmitReference.new(
-      reference: reference,
-    ).save!
+        reference.update!(feedback: 'Trustworthy', relationship_correction: '', safeguarding_concerns: '')
 
-    expect(active_application.reload).to be_application_complete
-    expect(cancelled_application.reload).to be_cancelled
-  end
+        SubmitReference.new(
+          reference: reference,
+        ).save!
 
-  it 'progresses the application choices to the "awaiting_provider_decision" status once all references have been received if edit_by has elapsed' do
-    application_form = create(:completed_application_form)
-    create(:application_choice, application_form: application_form, status: 'awaiting_references', edit_by: 1.day.ago)
-    create(:reference, :complete, application_form: application_form)
-    reference = create(:reference, :unsubmitted, application_form: application_form)
+        expect(active_application_choice.reload).to be_application_complete
+        expect(cancelled_application_choice.reload).to be_cancelled
+      end
 
-    reference.update!(feedback: 'Trustworthy', relationship_correction: '', safeguarding_concerns: '')
+      it 'progresses the application choices to the "awaiting_provider_decision" if edit_by has elapsed' do
+        create(:application_choice, application_form: application_form, status: 'awaiting_references', edit_by: 1.day.ago)
+        create(:reference, :complete, application_form: application_form)
+        reference = create(:reference, :unsubmitted, application_form: application_form)
 
-    SubmitReference.new(
-      reference: reference,
-    ).save!
+        reference.update!(feedback: 'Trustworthy', relationship_correction: '', safeguarding_concerns: '')
 
-    expect(application_form.reload.application_choices).to all(be_awaiting_provider_decision)
-  end
+        SubmitReference.new(
+          reference: reference,
+        ).save!
 
-  it 'is okay with a 3rd reference being provided' do
-    application_form = create(:completed_application_form)
-    create(:application_choice, application_form: application_form, status: 'awaiting_references', edit_by: 1.day.ago)
-    create(:reference, :complete, application_form: application_form)
-    reference = create(:reference, :unsubmitted, application_form: application_form)
+        expect(application_form.reload.application_choices).to all(be_awaiting_provider_decision)
+      end
 
-    reference.update!(feedback: 'Trustworthy', relationship_correction: '', safeguarding_concerns: '')
+      it 'sets edit_by to current time if Apply Again application' do
+        application_form.apply_2!
+        create(:application_choice, application_form: application_form, status: 'awaiting_references', edit_by: 2.days.from_now)
+        create(:reference, :complete, application_form: application_form)
+        reference = create(:reference, :unsubmitted, application_form: application_form)
 
-    SubmitReference.new(
-      reference: reference,
-    ).save!
+        reference.update!(feedback: 'Trustworthy', relationship_correction: '', safeguarding_concerns: '')
 
-    another_reference = create(:reference, :unsubmitted, application_form: application_form)
+        Timecop.freeze(Time.utc(2020)) do
+          SubmitReference.new(
+            reference: reference,
+          ).save!
 
-    another_reference.update!(feedback: 'Trustworthy', relationship_correction: '', safeguarding_concerns: '')
+          expect(application_form.application_choices.first.edit_by).to eq Time.utc(2020)
+        end
+      end
 
-    SubmitReference.new(
-      reference: another_reference,
-    ).save!
+      it 'is okay with a 3rd reference being provided' do
+        create(:application_choice, application_form: application_form, status: 'awaiting_references', edit_by: 1.day.ago)
+        create(:reference, :complete, application_form: application_form)
+        reference = create(:reference, :unsubmitted, application_form: application_form)
+
+        reference.update!(feedback: 'Trustworthy', relationship_correction: '', safeguarding_concerns: '')
+
+        SubmitReference.new(
+          reference: reference,
+        ).save!
+
+        another_reference = create(:reference, :unsubmitted, application_form: application_form)
+
+        another_reference.update!(feedback: 'Trustworthy', relationship_correction: '', safeguarding_concerns: '')
+
+        SubmitReference.new(
+          reference: another_reference,
+        ).save!
+      end
+    end
   end
 end
