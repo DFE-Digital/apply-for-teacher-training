e87f5f7c698ae975270db95f8f65c20fe619e13f
David Gisbey <davidgisbey@gmail.com>
2020-05-17 16:14:09 +0100

    Pass request method into component

--- app/components/candidate_interface/complete_section_component.rb
+++ app/components/candidate_interface/complete_section_component.rb
@@ -1,10 +1,11 @@
 module CandidateInterface
   class CompleteSectionComponent < ViewComponent::Base
-    validates :application_form, :path, :field_name, presence: true
+    validates :application_form, :path, :request_method, :field_name, presence: true
 
-    def initialize(application_form:, path:, field_name:)
+    def initialize(application_form:, path:, request_method:, field_name:)
       @application_form = application_form
       @path = path
+      @request_method = request_method
       @field_name = field_name
     end
   end
