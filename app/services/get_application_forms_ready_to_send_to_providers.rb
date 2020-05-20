4c954c3f2a10a2d675c0d01436c95881e8a21c8e
David Gisbey <davidgisbey@gmail.com>
2020-05-20 14:10:48 +0100

    Change get_application_forms_ready_to_send_to_providers to use new value

--- app/services/get_application_forms_ready_to_send_to_providers.rb
+++ app/services/get_application_forms_ready_to_send_to_providers.rb
@@ -1,10 +1,17 @@
 class GetApplicationFormsReadyToSendToProviders
   def self.call
-    forms = ApplicationForm
-      .joins(:application_choices)
-      .where('"application_choices"."status" = ?', 'application_complete')
-      .having('max("application_choices"."edit_by") < ?', Time.zone.now)
-      .group(:id)
+    forms = if FeatureFlag.active?('move_edit_by_to_application_form')
+              ApplicationForm
+                .joins(:application_choices)
+                .where('"application_choices"."status" = ?', 'application_complete')
+                .where('"application_forms"."edit_by" < ?', Time.zone.now)
+            else
+              ApplicationForm
+               .joins(:application_choices)
+               .where('"application_choices"."status" = ?', 'application_complete')
+               .having('max("application_choices"."edit_by") < ?', Time.zone.now)
+               .group(:id)
+            end
 
     forms.select do |f|
       f.application_choices.all? { |application_choice| application_choice.status == 'application_complete' }
--- spec/services/get_application_forms_ready_to_send_to_providers_spec.rb
+++ spec/services/get_application_forms_ready_to_send_to_providers_spec.rb
@@ -3,52 +3,113 @@ require 'rails_helper'
 RSpec.describe GetApplicationFormsReadyToSendToProviders do
   subject(:returned_application_forms) { GetApplicationFormsReadyToSendToProviders.call }
 
-  let(:application_form) { create(:application_form) }
+  context 'with the move_edit_by_to_application_form flag off' do
+    let(:application_form) { create(:application_form) }
 
-  context 'when the edit_by dates have passed and the application_choices are application_complete' do
-    before do
-      create(:application_choice, application_form: application_form, status: :application_complete, edit_by: 1.day.ago)
-      create(:application_choice, application_form: application_form, status: :application_complete, edit_by: 1.day.ago)
+    context 'when the edit_by dates have passed and the application_choices are application_complete' do
+      before do
+        create(:application_choice, application_form: application_form, status: :application_complete, edit_by: 1.day.ago)
+        create(:application_choice, application_form: application_form, status: :application_complete, edit_by: 1.day.ago)
+      end
+
+      it 'returns the form' do
+        expect(returned_application_forms.first).to eq application_form
+      end
     end
 
-    it 'returns the form' do
-      expect(returned_application_forms.first).to eq application_form
+    context 'when the edit_by dates have not passed and the application_choices are application_complete' do
+      before do
+        create(:application_choice, application_form: application_form, status: :application_complete, edit_by: 1.day.from_now)
+        create(:application_choice, application_form: application_form, status: :application_complete, edit_by: 1.day.from_now)
+      end
+
+      it 'does not return the form' do
+        expect(returned_application_forms).to be_empty
+      end
+    end
+
+    context 'when the edit_by dates have passed and the application_choices are not application_complete' do
+      before do
+        create(:application_choice, application_form: application_form, status: :awaiting_references, edit_by: 1.day.from_now)
+        create(:application_choice, application_form: application_form, status: :awaiting_references, edit_by: 1.day.from_now)
+      end
+
+      it 'does not return the form' do
+        expect(returned_application_forms).to be_empty
+      end
+    end
+
+    context 'when the application_choices have a mixture of statuses' do
+      before do
+        # this should never happen. it is a possibility, though, because
+        # the fact that references have been received is currently sprinkled
+        # across all three application choices. this needs looking at.
+        create(:application_choice, application_form: application_form, status: :awaiting_references, edit_by: 1.day.ago)
+        create(:application_choice, application_form: application_form, status: :application_complete, edit_by: 1.day.ago)
+      end
+
+      it 'does not return the form' do
+        expect(returned_application_forms).to be_empty
+      end
     end
   end
 
-  context 'when the edit_by dates have not passed and the application_choices are application_complete' do
+  context 'with the move_edit_by_to_application_form flag on' do
+    let(:application_form) { create(:application_form, edit_by: 1.day.ago) }
+
     before do
-      create(:application_choice, application_form: application_form, status: :application_complete, edit_by: 1.day.from_now)
-      create(:application_choice, application_form: application_form, status: :application_complete, edit_by: 1.day.from_now)
+      FeatureFlag.activate('move_edit_by_to_application_form')
     end
 
-    it 'does not return the form' do
-      expect(returned_application_forms).to be_empty
-    end
-  end
+    context 'when the edit_by dates have passed and the application_choices are application_complete' do
+      before do
+        create(:application_choice, application_form: application_form, status: :application_complete)
+        create(:application_choice, application_form: application_form, status: :application_complete)
+      end
 
-  context 'when the edit_by dates have passed and the application_choices are not application_complete' do
-    before do
-      create(:application_choice, application_form: application_form, status: :awaiting_references, edit_by: 1.day.from_now)
-      create(:application_choice, application_form: application_form, status: :awaiting_references, edit_by: 1.day.from_now)
-    end
-
-    it 'does not return the form' do
-      expect(returned_application_forms).to be_empty
-    end
-  end
-
-  context 'when the application_choices have a mixture of statuses' do
-    before do
-      # this should never happen. it is a possibility, though, because
-      # the fact that references have been received is currently sprinkled
-      # across all three application choices. this needs looking at.
-      create(:application_choice, application_form: application_form, status: :awaiting_references, edit_by: 1.day.ago)
-      create(:application_choice, application_form: application_form, status: :application_complete, edit_by: 1.day.ago)
+      it 'returns the form' do
+        expect(returned_application_forms.first).to eq application_form
+      end
     end
 
-    it 'does not return the form' do
-      expect(returned_application_forms).to be_empty
+    context 'when the edit_by dates have not passed and the application_choices are application_complete' do
+      let(:application_form) { create(:application_form, edit_by: 1.day.from_now) }
+
+      before do
+        create(:application_choice, application_form: application_form, status: :application_complete)
+        create(:application_choice, application_form: application_form, status: :application_complete)
+      end
+
+      it 'does not return the form' do
+        expect(returned_application_forms).to be_empty
+      end
+    end
+
+    context 'when the edit_by dates have passed and the application_choices are not application_complete' do
+      let(:application_form) { create(:application_form, edit_by: 1.day.from_now) }
+
+      before do
+        create(:application_choice, application_form: application_form, status: :awaiting_references)
+        create(:application_choice, application_form: application_form, status: :awaiting_references)
+      end
+
+      it 'does not return the form' do
+        expect(returned_application_forms).to be_empty
+      end
+    end
+
+    context 'when the application_choices have a mixture of statuses' do
+      before do
+        # this should never happen. it is a possibility, though, because
+        # the fact that references have been received is currently sprinkled
+        # across all three application choices. this needs looking at.
+        create(:application_choice, application_form: application_form, status: :awaiting_references)
+        create(:application_choice, application_form: application_form, status: :application_complete)
+      end
+
+      it 'does not return the form' do
+        expect(returned_application_forms).to be_empty
+      end
     end
   end
 end
