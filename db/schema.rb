ffaf7ea7714d716801838f8d2d6b8762884f7eb2
David Gisbey <davidgisbey@gmail.com>
2020-05-21 12:27:21 +0100

    Backfill edit_by values

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_19_102317) do
+ActiveRecord::Schema.define(version: 2020_05_21_112623) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
