4e85d5bbb04632a5ce7c7cbdda94f8411ad08a25
David Gisbey <davidgisbey@gmail.com>
2020-05-21 11:27:59 +0100

    Fix link for contact details

--- app/components/candidate_interface/contact_details_review_component.rb
+++ app/components/candidate_interface/contact_details_review_component.rb
@@ -2,13 +2,14 @@ module CandidateInterface
   class ContactDetailsReviewComponent < ViewComponent::Base
     validates :application_form, presence: true
 
-    def initialize(application_form:, editable: true, missing_error: false)
+    def initialize(application_form:, editable: true, missing_error: false, submit_show: false)
       @application_form = application_form
       @contact_details_form = CandidateInterface::ContactDetailsForm.build_from_application(
         @application_form,
       )
       @editable = editable
       @missing_error = missing_error
+      @submit_show = submit_show
     end
 
     def contact_details_form_rows
@@ -16,7 +17,11 @@ module CandidateInterface
     end
 
     def show_missing_banner?
-      !@contact_details_form.valid?(:base) && !@contact_details_form.valid?(:address) && @editable
+      if @submit_show && FeatureFlag.active?('mark_every_section_complete')
+        !@application_form.contact_details_completed && @editable
+      else
+        !@contact_details_form.valid?(:base) && !@contact_details_form.valid?(:address) && @editable
+      end
     end
 
   private
