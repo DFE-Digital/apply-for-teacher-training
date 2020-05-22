eac079baabb77cb30658a1bd359267edf93189c1
David Gisbey <davidgisbey@gmail.com>
2020-05-22 08:12:36 +0100

    Check gcse checkbox if section is completed

--- app/controllers/candidate_interface/gcse/review_controller.rb
+++ app/controllers/candidate_interface/gcse/review_controller.rb
@@ -2,6 +2,7 @@ module CandidateInterface
   class Gcse::ReviewController < Gcse::DetailsController
     before_action :redirect_to_dashboard_if_submitted
     before_action :set_subject
+    before_action :set_field_name
 
     def show
       @application_form = current_application
@@ -9,9 +10,20 @@ module CandidateInterface
     end
 
     def complete
-      update_gcse_completed(params.dig('application_form', 'gcse_completed'))
+      current_application.update!(application_form_params)
 
       redirect_to candidate_interface_application_form_path
+    end
+
+  private
+
+    def set_field_name
+      @field_name = "#{@subject}_gcse_completed"
+    end
+
+    def application_form_params
+      params.require(:application_form).permit(@field_name)
+        .transform_values(&:strip)
     end
   end
 end
