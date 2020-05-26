2900363457d3019e779ab6de49b9db79b33bc87d
Malcolm Baig <malcolm.baig@gmail.com>
2020-05-26 13:35:55 +0100

    Remove empty CandidateInterface::CourseChoicesController
    All actions in this controller have been extracted to more specific
    places. Remove the now unnecessary file.

--- app/controllers/candidate_interface/course_choices_controller.rb
+++
@@ -1,6 +0,0 @@
-module CandidateInterface
-  class CourseChoicesController < CandidateInterfaceController
-    before_action :redirect_to_dashboard_if_submitted
-    rescue_from ActiveRecord::RecordNotFound, with: :render_404
-  end
-end
