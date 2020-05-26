8627d894e1735ef8bfd3329d6665b17675c26156
willmcb <william.mcbrien@digital.education.gov.uk>
2020-05-26 10:40:55 +0100

    Updates FilterComponent preview to work with refactored implementation

--- spec/components/previews/provider_interface/filter_component_preview.rb
+++ spec/components/previews/provider_interface/filter_component_preview.rb
@@ -1,145 +1,43 @@
 module ProviderInterface
   class FilterComponentPreview < ViewComponent::Preview
     def fully_selected
-      render_component_for(available_filters: available_filters,
-                           applied_filters: applied_filters_full,
-                           params_for_current_state: params_for_current_state)
-    end
-
-    def partially_selected
-      render_component_for(available_filters: available_filters,
-                           applied_filters: applied_filters_partial,
-                           params_for_current_state: params_for_current_state)
-    end
-
-    def empty
-      render_component_for(available_filters: available_filters,
-                           applied_filters: {},
-                           params_for_current_state: params_for_current_state)
+      render_component_for(page_state: page_state_mock(filters: with_name_status_provider_and_accredited_provider))
     end
 
   private
 
-    def render_component_for(available_filters:, applied_filters:, params_for_current_state:)
-      render ProviderInterface::FilterComponent.new(available_filters: available_filters,
-                                                    applied_filters: applied_filters,
-                                                    params_for_current_state: params_for_current_state)
+    def render_component_for(page_state:)
+      render ProviderInterface::FilterComponent.new(page_state: page_state)
     end
 
-    def applied_filters_partial
-      {
-        'status' => {
-          'pending-conditions' => 'pending_conditions',
-          'awaiting-provider-decision' =>
-          'awaiting_provider_decision',
-          'offer' => 'offer',
-          'rejected' => 'rejected',
-          'withdrawn' => 'withdrawn',
-        }, 'provider' => {
-          'the-beach-teaching-school' => '2',
-        }
-      }
+    def page_state_mock(filters:)
+      page_state = Struct.new(:filters)
+      page_state.new(filters)
     end
 
-    def applied_filters_full
-      {
-        'status' => {
-          'pending-conditions' => 'pending_conditions',
-          'recruited' => 'recruited',
-          'declined' => 'declined',
-          'awaiting-provider-decision' => 'awaiting_provider_decision',
-          'offer' => 'offer',
-          'rejected' => 'rejected',
-          'withdrawn' => 'withdrawn',
-          'offer-withdrawn' => 'offer_withdrawn',
-        }, 'provider' => {
-          'somerset-scitt-consortium' => '1',
-          'the-beach-teaching-school' => '2',
-        }
-      }
-    end
-
-    def available_filters
-      [
-        {
-          heading: 'status',
-          input_config: [
-            {
-              type: 'checkbox',
-              name: 'pending-conditions',
-              text: 'Accepted',
-              value: 'pending_conditions',
-            },
-            {
-              type: 'checkbox',
-              name: 'recruited',
-              text: 'Conditions met',
-              value: 'recruited',
-            },
-            {
-              type: 'checkbox',
-              name: 'declined',
-              text: 'Declined',
-              value: 'declined',
-            },
-            {
-              type: 'checkbox',
-              name: 'awaiting-provider-decision',
-              text: 'New',
-              value: 'awaiting_provider_decision',
-            },
-            {
-              type: 'checkbox',
-              name: 'offer',
-              text: 'Offered',
-              value: 'offer',
-            },
-            {
-              type: 'checkbox',
-              name: 'rejected',
-              text: 'Rejected',
-              value: 'rejected',
-            },
-            {
-              type: 'checkbox',
-              name: 'withdrawn',
-              text: 'Application withdrawn',
-              value: 'withdrawn',
-            },
-            {
-              type: 'checkbox',
-              name: 'offer-withdrawn',
-              text: 'Withdrawn by us',
-              value: 'offer_withdrawn',
-            },
-          ],
-        },
-        {
-          heading: 'provider',
-          input_config: [
-            {
-              type: 'checkbox',
-              name: 'somerset-scitt-consortium',
-              text: 'Somerset SCITT consortium',
-              value: '1',
-            },
-            {
-              type: 'checkbox',
-              name: 'the-beach-teaching-school',
-              text: 'The Beach Teaching School',
-              value: '2',
-            },
-
-          ],
-        },
-      ]
-    end
-
-    def params_for_current_state
-      {
-        sort_by: 'desc',
-        sort_order: 'last-updated',
-      }
+    def with_name_status_provider_and_accredited_provider
+      [{ type: :search, heading: 'Candidate’s name', value: '', name: 'candidate_name' },
+       { type: :checkboxes,
+         heading: 'Status',
+         name: 'status',
+         options: [{ value: 'awaiting_provider_decision', label: 'New', checked: false },
+                   { value: 'offer', label: 'Offered', checked: false },
+                   { value: 'pending_conditions', label: 'Accepted', checked: false },
+                   { value: 'recruited', label: 'Conditions met', checked: false },
+                   { value: 'enrolled', label: 'Enrolled', checked: false },
+                   { value: 'rejected', label: 'Rejected', checked: false },
+                   { value: 'declined', label: 'Declined', checked: true },
+                   { value: 'withdrawn', label: 'Application withdrawn', checked: true },
+                   { value: 'conditions_not_met', label: 'Conditions not met', checked: false },
+                   { value: 'offer_withdrawn', label: 'Withdrawn by us', checked: false }] },
+       { type: :checkboxes,
+         heading: 'Provider',
+         name: 'provider',
+         options: [{ value: 1, label: 'Gorse SCITT', checked: false }] },
+       { type: :checkboxes,
+         heading: 'Accredited provider',
+         name: 'accredited_provider',
+         options: [{ value: 5, label: 'Coventry University', checked: nil }] }]
     end
   end
 end
