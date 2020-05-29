1fea929086966e4bfb991b19d38ce4f6d7ad7bd5
Michael Nacos <m.nacos@gmail.com>
2020-05-29 12:59:50 +0100

    add preselect_same_site_if_available behaviour for study_mode changes

--- app/components/provider_interface/change_offer/change_location_component.rb
+++ app/components/provider_interface/change_offer/change_location_component.rb
@@ -12,6 +12,8 @@ module ProviderInterface
         if @change_offer_form.valid?
           @change_offer_form.step = @change_offer_form.next_step
         end
+
+        preselect_same_site_if_available
       end
 
       def course_options
@@ -20,6 +22,14 @@ module ProviderInterface
           study_mode: change_offer_form.study_mode,
           # TODO: check vacancy_status, e.g. 'B'
         ).includes(:site).order('sites.name')
+      end
+
+      def preselect_same_site_if_available
+        if change_offer_form.course_option_id
+          previous_option = CourseOption.find(change_offer_form.course_option_id)
+          new_option = course_options.find_by(site_id: previous_option.site.id)
+          change_offer_form.course_option_id = new_option.id if new_option
+        end
       end
 
       def page_title
