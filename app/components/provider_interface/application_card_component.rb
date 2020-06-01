992d6c52a3c5c9491762fdea08d39299f41d05f3
willmcb <william.mcbrien@digital.education.gov.uk>
2020-06-01 11:15:19 +0100

    Add 'changed at' date format to application card

--- app/components/provider_interface/application_card_component.rb
+++ app/components/provider_interface/application_card_component.rb
@@ -3,7 +3,7 @@ module ProviderInterface
     include ViewHelper
 
     attr_accessor :accredited_provider, :application_choice, :application_choice_path,
-                  :candidate_name, :course_name_and_code, :course_provider_name, :updated_at,
+                  :candidate_name, :course_name_and_code, :course_provider_name, :changed_at,
                   :most_recent_note, :site_name_and_code
 
     def initialize(application_choice:)
@@ -12,7 +12,7 @@ module ProviderInterface
       @candidate_name = application_choice.application_form.full_name
       @course_name_and_code = application_choice.offered_course.name_and_code
       @course_provider_name = application_choice.offered_course.provider.name
-      @updated_at = application_choice.updated_at.to_s(:govuk_date_short_month)
+      @changed_at = application_choice.updated_at.to_s(:govuk_date_and_time)
       @site_name_and_code = application_choice.site.name_and_code
       @most_recent_note = application_choice.notes.order('created_at DESC').first
     end
