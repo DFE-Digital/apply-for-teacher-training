c25141134d236c5425231a3cb4a0e9b314fa60b8
David Gisbey <davidgisbey@gmail.com>
2020-06-02 14:48:04 +0100

    Edit content on application edit page

--- spec/lib/time_limit_config_spec.rb
+++ spec/lib/time_limit_config_spec.rb
@@ -20,44 +20,23 @@ RSpec.describe TimeLimitConfig do
   end
 
   describe '.edit_by' do
-    context 'when the COVID-19 feature flag is on' do
-      before { FeatureFlag.activate('covid_19') }
 
-      it 'returns 7 days' do
-        expect(TimeLimitConfig.edit_by.count).to eq(7)
-      end
+    before { FeatureFlag.deactivate('covid_19') }
 
-      it 'returns type as calendar' do
-        expect(TimeLimitConfig.edit_by.type).to eq(:calendar)
-      end
-
-      it 'displays "7 calendar days"' do
-        expect(TimeLimitConfig.edit_by.to_s).to eq('7 calendar days')
-      end
-
-      it 'returns days as ActiveSupport:Duration object' do
-        expect(TimeLimitConfig.edit_by.to_days).to be_a_kind_of(ActiveSupport::Duration)
-      end
+    it 'returns 5 days' do
+      expect(TimeLimitConfig.edit_by.count).to eq(5)
     end
 
-    context 'when the COVID-19 feature flag is off' do
-      before { FeatureFlag.deactivate('covid_19') }
+    it 'returns type as working' do
+      expect(TimeLimitConfig.edit_by.type).to eq(:working)
+    end
 
-      it 'returns 5 days' do
-        expect(TimeLimitConfig.edit_by.count).to eq(5)
-      end
+    it 'displays "5 calendar days"' do
+      expect(TimeLimitConfig.edit_by.to_s).to eq('5 working days')
+    end
 
-      it 'returns type as working' do
-        expect(TimeLimitConfig.edit_by.type).to eq(:working)
-      end
-
-      it 'displays "5 calendar days"' do
-        expect(TimeLimitConfig.edit_by.to_s).to eq('5 working days')
-      end
-
-      it 'returns days as BusinessTime::BusinessDays object' do
-        expect(TimeLimitConfig.edit_by.to_days).to be_a_kind_of(BusinessTime::BusinessDays)
-      end
+    it 'returns days as BusinessTime::BusinessDays object' do
+      expect(TimeLimitConfig.edit_by.to_days).to be_a_kind_of(BusinessTime::BusinessDays)
     end
   end
 end
