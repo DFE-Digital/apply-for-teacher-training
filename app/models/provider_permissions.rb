e2753f4f86c80fe7833d147e05aeed39ece4b055
Steve Laing <steve.laing@gmail.com>
2020-06-04 09:55:23 +0100

    Assign 'manage organisations' permission via support interface

--- app/models/provider_permissions.rb
+++ app/models/provider_permissions.rb
@@ -1,5 +1,9 @@
 class ProviderPermissions < ActiveRecord::Base
-  VALID_PERMISSIONS = %i[manage_users view_safeguarding_information].freeze
+  VALID_PERMISSIONS = %i[
+    manage_users
+    manage_organisations
+    view_safeguarding_information
+  ].freeze
 
   self.table_name = 'provider_users_providers'
 
