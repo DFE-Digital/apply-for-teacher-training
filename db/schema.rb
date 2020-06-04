28bdd20cd1b9722b3f56d04c97b98c2789f27ef4
Steve Laing <steve.laing@gmail.com>
2020-06-04 16:16:50 +0100

    Add a setup_at timestamp field for ProviderRelationshipPermissions
    We will be directing a permitted user through a setup process for provider relationship
    permissions if they have not yet been set. Adds a timestamp to record the initial setup of
    these permissions as they are created from the data sync from Find we can't rely on this value.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_03_153852) do
+ActiveRecord::Schema.define(version: 2020_06_04_151228) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -285,6 +285,7 @@ ActiveRecord::Schema.define(version: 2020_06_03_153852) do
     t.boolean "view_safeguarding_information", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "setup_at"
     t.index ["type", "training_provider_id", "ratifying_provider_id"], name: "index_provider_relationship_permissions_provider_ids_and_type", unique: true
   end
 
