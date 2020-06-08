2d9e3ab5948ee92bc6877cb0be3def36a474eaa4
Steve Hook <steve.hook@gmail.com>
2020-06-08 15:49:35 +0100

    Pending spec to avoid repeated course unavailable notifications

--- spec/services/get_application_choices_with_newly_unavailable_courses_spec.rb
+++ spec/services/get_application_choices_with_newly_unavailable_courses_spec.rb
@@ -15,4 +15,14 @@ RSpec.describe GetApplicationChoicesWithNewlyUnavailableCourses do
     create :awaiting_references_application_choice
     expect(described_class.call.map(&:id)).to eq([application_choice_without_vacancies.id])
   end
+
+  it 'does not return application choices that have already received the notification email' do
+    pending 'need to merge with migration to add the `course_unavailable_notification_sent_at` column'
+    create(
+      :awaiting_references_application_choice,
+      course_option: create(:course_option, :no_vacancies),
+      course_unavailable_notification_sent_at: 1.day.ago,
+    )
+    expect(described_class.call.map(&:id)).to eq([])
+  end
 end
