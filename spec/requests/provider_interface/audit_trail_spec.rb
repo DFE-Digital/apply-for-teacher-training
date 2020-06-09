19a5a3aeb611fb012805bb9bcc972fd875bc51d2
Duncan Brown <duncanjbrown@gmail.com>
2020-06-09 16:06:40 +0100

    Update audit log spec
    MakeAnOffer now needs a course_option which is open_on_apply

--- spec/requests/provider_interface/audit_trail_spec.rb
+++ spec/requests/provider_interface/audit_trail_spec.rb
@@ -1,12 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Provider interface - audit trail', type: :request, with_audited: true do
-  def create_application
-    create(
-      :application_choice,
-      :awaiting_provider_decision,
-    )
-  end
+  include CourseOptionHelpers
 
   def set_provider_permission(application_choice)
     allow(ProviderUser).to receive(:load_from_session)
@@ -21,11 +16,16 @@ RSpec.describe 'Provider interface - audit trail', type: :request, with_audited:
   end
 
   it 'creates audit records attributed to the authenticated provider' do
-    application_choice = create_application
+    application_choice = create(
+      :application_choice,
+      :awaiting_provider_decision,
+    )
     set_provider_permission(application_choice)
+
     expect {
       post provider_interface_application_choice_create_offer_path(
         application_choice_id: application_choice.id,
+        course_option_id: course_option_for_provider(provider: application_choice.provider).id,
       ), params: { offer_conditions: '["must be clever"]' }
     }.to(change { application_choice.audits.count })
 
