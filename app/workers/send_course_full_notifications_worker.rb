986378d4f9de7eb59ab75313fae82de4fdf4d548
Steve Hook <steve.hook@gmail.com>
2020-06-09 09:46:49 +0100

    Add framework for evaluating reason course is unavailable

--- app/workers/send_course_full_notifications_worker.rb
+++ app/workers/send_course_full_notifications_worker.rb
@@ -3,7 +3,27 @@ class SendCourseFullNotificationsWorker
 
   def perform
     GetApplicationChoicesWithNewlyUnavailableCourses.call.each do |application_choice|
-      CandidateMailer.course_unavailable_notification(application_choice, :course_full).deliver_later
+      reason = reason_course_is_unavailable(application_choice)
+      CandidateMailer.course_unavailable_notification(application_choice, reason).deliver_later
     end
+  end
+
+  private
+
+  # TODO: Refactor this logic into a separate class?
+  def reason_course_is_unavailable(application_choice)
+    # all course options for the given course are full
+    return :course_full if application_choice.course_full?
+
+    # all course options for the given course are full at the selected location
+    return :location_full if application_choice.site_full?
+
+    # TODO: Handle other reasons...
+    # :location_withdrawn # n/a?
+    # :study_mode_full # all part/full-time course options are full for the given course
+    # :study_mode_withdrawn # n/a?
+    # :study_mode_location_full # the selected study mode is full at the selected location for the given course but (another study mode is available at the given location)
+    # :study_mode_location_withdrawn # n/a?
+    # :course_withdrawn # the course has been withdrawn (removing all course options)
   end
 end
--- spec/workers/send_course_full_notifications_worker_spec.rb
+++ spec/workers/send_course_full_notifications_worker_spec.rb
@@ -3,11 +3,23 @@ require 'rails_helper'
 RSpec.describe SendCourseFullNotificationsWorker do
   describe '#perform', sidekiq: true do
     it 'sends emails to candidates that applied to a course that is now full' do
-      application_choice = build :application_choice
-      allow(CandidateMailer).to receive(:course_unavailable_notification).and_return(double(deliver_later: true))
+      application_choice = create :application_choice
+      application_choice.course_option.update(vacancy_status: :no_vacancies)
+      allow(CandidateMailer).to receive(:course_unavailable_notification).and_call_original
       allow(GetApplicationChoicesWithNewlyUnavailableCourses).to receive(:call).and_return([application_choice])
       SendCourseFullNotificationsWorker.new.perform
-      expect(CandidateMailer).to have_received(:course_unavailable_notification)
+      expect(CandidateMailer).to have_received(:course_unavailable_notification).with(application_choice, :course_full).at_least(:once)
+    end
+
+    it 'sends emails to candidates that applied to a course that is now full at the selected location' do
+      pending 'need to implement the course_full template'
+      application_choice = create :application_choice
+      application_choice.course_option.update(vacancy_status: :no_vacancies)
+      create :course_option, course: application_choice.course
+      allow(CandidateMailer).to receive(:course_unavailable_notification).and_call_original
+      allow(GetApplicationChoicesWithNewlyUnavailableCourses).to receive(:call).and_return([application_choice])
+      SendCourseFullNotificationsWorker.new.perform
+      expect(CandidateMailer).to have_received(:course_unavailable_notification).with(application_choice, :location_full).at_least(:once)
     end
   end
 end
