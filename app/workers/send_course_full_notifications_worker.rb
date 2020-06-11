6ca3f214f9d7f91dca289b531571886f671e7c0c
Steve Hook <steve.hook@gmail.com>
2020-06-11 09:01:50 +0100

    Slack message instead of email when feature off

--- app/workers/send_course_full_notifications_worker.rb
+++ app/workers/send_course_full_notifications_worker.rb
@@ -2,15 +2,33 @@ class SendCourseFullNotificationsWorker
   include Sidekiq::Worker
 
   def perform
-    return unless FeatureFlag.active?(:unavailable_course_notifications)
-
     GetApplicationChoicesWithNewlyUnavailableCourses.call.each do |application_choice|
       reason = ReasonCourseNotAvailable.new(application_choice).call
-      ChaserSent.create!(
-        chased: application_choice,
-        chaser_type: :course_unavailable_notification,
-      )
-      CandidateMailer.course_unavailable_notification(application_choice, reason).deliver_later
+      if FeatureFlag.active?(:unavailable_course_notifications)
+        ChaserSent.create!(
+          chased: application_choice,
+          chaser_type: :course_unavailable_notification,
+        )
+        CandidateMailer.course_unavailable_notification(application_choice, reason).deliver_later
+      else
+        send_slack_message(application_choice, reason)
+      end
     end
+  end
+
+private
+
+  def send_slack_message(application_choice, reason)
+    message = I18n.t!(
+      "candidate_mailer.course_unavailable_notification.slack_message.#{reason}",
+      course_name: application_choice.course_option.course.name_and_code,
+      provider_name: application_choice.course_option.course.provider.name,
+      study_mode: application_choice.course_option.study_mode.humanize.downcase,
+      location: application_choice.course_option.site.name,
+      candidate_name: application_choice.application_form.first_name,
+    )
+    url = Rails.application.routes.url_helpers.support_interface_application_form_url(application_choice.application_form)
+
+    SlackNotificationWorker.perform_async(message, url)
   end
 end
--- spec/workers/send_course_full_notifications_worker_spec.rb
+++ spec/workers/send_course_full_notifications_worker_spec.rb
@@ -3,14 +3,19 @@ require 'rails_helper'
 RSpec.describe SendCourseFullNotificationsWorker do
   describe '#perform', sidekiq: true do
     context 'with feature flag inactive' do
-      it 'sends no emails' do
+      it 'sends a Slack notification but no email' do
         application_choice = create :application_choice
         application_choice.course_option.update(vacancy_status: :no_vacancies)
+        allow(SlackNotificationWorker).to receive(:perform_async)
         allow(CandidateMailer).to receive(:course_unavailable_notification)
         allow(GetApplicationChoicesWithNewlyUnavailableCourses).to receive(:call).and_return([application_choice])
         SendCourseFullNotificationsWorker.new.perform
         expect(CandidateMailer).not_to have_received(:course_unavailable_notification)
         expect(ChaserSent.where(chased: application_choice, chaser_type: :course_unavailable_notification)).not_to be_present
+        expect(SlackNotificationWorker).to have_received(:perform_async).with(
+          "#{application_choice.course.name_and_code} at #{application_choice.course.provider.name} became full while #{application_choice.application_form.first_name} was awaiting references",
+          Rails.application.routes.url_helpers.support_interface_application_form_url(application_choice.application_form),
+        )
       end
     end
 
