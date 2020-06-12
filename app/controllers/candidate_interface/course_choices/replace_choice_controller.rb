611320a98e05901910bee0f08d82d498ce8b8fa2
David Gisbey <davidgisbey@gmail.com>
2020-06-12 11:16:38 +0100

    add update_choice action to the controller

--- app/controllers/candidate_interface/course_choices/replace_choice_controller.rb
+++ app/controllers/candidate_interface/course_choices/replace_choice_controller.rb
@@ -89,6 +89,29 @@ module CandidateInterface
         @replacement_course_option = CourseOption.find(params['course_option_id'])
       end
 
+      def update_choice
+        @course_choice = current_application.application_choices.find(params['id'])
+        @replacement_course_option_id = params['course_option_id']
+
+        @pick_site = PickSiteForm.new(
+          application_form: current_application,
+          provider_id: @course_choice.provider.id,
+          course_id: @course_choice.course.id,
+          study_mode: @course_choice.course_option.study_mode,
+          course_option_id: @replacement_course_option_id,
+        )
+
+        if @pick_site.valid?
+          @course_choice.update!(course_option_id: @replacement_course_option_id)
+          flash[:success] = 'Your application has been updated'
+
+          redirect_to candidate_interface_application_complete_path
+        else
+          flash[:warning] = 'Please select a new location.'
+          redirect_to candidate_interface_replace_course_choice_location_path(@course_choice.id)
+        end
+      end
+
     private
 
       def render_404_if_flag_is_inactive
