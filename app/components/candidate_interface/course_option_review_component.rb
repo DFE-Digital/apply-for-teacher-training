ea4b6ad47e7d747209cab2d6872261b5a59322f4
David Gisbey <davidgisbey@gmail.com>
2020-06-12 11:03:36 +0100

    Add review course option component

---
+++ app/components/candidate_interface/course_option_review_component.rb
@@ -0,0 +1,42 @@
+module CandidateInterface
+  class CourseOptionReviewComponent < ViewComponent::Base
+    include ViewHelper
+
+    validates :course_option, presence: true
+
+    def initialize(course_option:)
+      @course_option = course_option
+    end
+
+    def course_option_rows
+      [
+        course_row,
+        location_row,
+        study_mode_row,
+      ]
+    end
+
+  private
+
+    def course_row
+      {
+        key: 'Course',
+        value: "#{@course_option.course.name} <br> #{@course_option.course.description}".html_safe,
+      }
+    end
+
+    def location_row
+      {
+        key: 'Location',
+        value: @course_option.site.name_and_address,
+      }
+    end
+
+    def study_mode_row
+      {
+        key: 'Study mode',
+        value: @course_option.study_mode.humanize,
+      }
+    end
+  end
+end
