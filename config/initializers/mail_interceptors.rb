0a10857a92881ae84d534278d1e1382079bd32a0
Duncan Brown <duncanjbrown@gmail.com>
2020-06-15 16:17:03 +0100

    Intercept provider emails in Sandbox
    - do not send these emails, as they're very noisy
    - still permit fallback_sign_in_email and account_created emails as
    these are never triggered automatically and are useful

--- config/initializers/mail_interceptors.rb
+++ config/initializers/mail_interceptors.rb
@@ -1,10 +1,12 @@
 require 'test_email_interceptor'
 require 'environment_flag_interceptor'
 require 'email_log_interceptor'
+require 'sandbox_interceptor'
 
 if Rails.env.production?
   ActionMailer::Base.register_interceptor(TestEmailInterceptor)
 end
 
+ActionMailer::Base.register_interceptor(SandboxInterceptor)
 ActionMailer::Base.register_interceptor(EnvironmentFlagInterceptor)
 ActionMailer::Base.register_interceptor(EmailLogInterceptor)
---
+++ spec/lib/sandbox_interceptor_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe SandboxInterceptor do
+  context 'In sandbox mode', sandbox: true do
+    context 'when rails_mailer is set to provider_mailer' do
+      it 'aborts delivery by default' do
+        message = email_with_mailer_and_template_headers('provider_mailer', 'anything')
+
+        SandboxInterceptor.delivering_email(message)
+
+        expect(message.perform_deliveries).to be false
+      end
+
+      it 'still permits fallback_sign_in_email' do
+        message = email_with_mailer_and_template_headers('provider_mailer', 'fallback_sign_in_email')
+
+        SandboxInterceptor.delivering_email(message)
+
+        expect(message.perform_deliveries).to be true
+      end
+
+      it 'still permits account_created' do
+        message = email_with_mailer_and_template_headers('provider_mailer', 'account_created')
+
+        SandboxInterceptor.delivering_email(message)
+
+        expect(message.perform_deliveries).to be true
+      end
+    end
+
+    it 'allows delivery when rails_mailer is not set to provider_mailer' do
+      message = email_with_mailer_and_template_headers('authentication_mailer', 'sign_in_email')
+
+      SandboxInterceptor.delivering_email(message)
+
+      expect(message.perform_deliveries).to be true
+    end
+  end
+
+  it 'does nothing outside sandbox mode' do
+    message = email_with_mailer_and_template_headers('provider_mailer', 'anything')
+
+    SandboxInterceptor.delivering_email(message)
+
+    expect(message.perform_deliveries).to be true
+  end
+
+  # this reflects the standard format emitted by ApplicationMailer
+  def email_with_mailer_and_template_headers(mailer, template)
+    Mail::Message.new(to: ['test@example.com'], subject: 'Foo', headers: { rails_mailer: mailer, rails_mail_template: template })
+  end
+end
