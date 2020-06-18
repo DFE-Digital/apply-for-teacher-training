0f7c61220a47799e33ff46ccc8ee654382769c35
Malcolm Baig <malcolm.baig@gmail.com>
2020-06-18 16:09:10 +0100

    Use consistent module nesting in degree controllers

--- app/controllers/candidate_interface/degrees/grade_controller.rb
+++ app/controllers/candidate_interface/degrees/grade_controller.rb
@@ -1,49 +1,51 @@
 module CandidateInterface
-  class Degrees::GradeController < CandidateInterfaceController
-    before_action :redirect_to_dashboard_if_submitted
+  module Degrees
+    class GradeController < CandidateInterfaceController
+      before_action :redirect_to_dashboard_if_submitted
 
-    def new
-      @degree_grade_form = DegreeGradeForm.new(degree: degree)
-    end
-
-    def create
-      @degree_grade_form = DegreeGradeForm.new(grade_params)
-
-      if @degree_grade_form.save
-        redirect_to candidate_interface_degree_year_path(degree)
-      else
-        render :new
+      def new
+        @degree_grade_form = DegreeGradeForm.new(degree: degree)
       end
-    end
 
-    def edit
-      @degree_grade_form = DegreeGradeForm.new(degree: degree).fill_form_values
-    end
+      def create
+        @degree_grade_form = DegreeGradeForm.new(grade_params)
 
-    def update
-      @degree_grade_form = DegreeGradeForm.new(grade_params)
-
-      if @degree_grade_form.save
-        current_application.update!(degrees_completed: false)
-        redirect_to candidate_interface_degrees_review_path
-      else
-        track_validation_error(@degree_grade_form)
-        render :new
+        if @degree_grade_form.save
+          redirect_to candidate_interface_degree_year_path(degree)
+        else
+          render :new
+        end
       end
-    end
 
-  private
+      def edit
+        @degree_grade_form = DegreeGradeForm.new(degree: degree).fill_form_values
+      end
 
-    def degree
-      @degree = ApplicationQualification.find(params[:id])
-    end
+      def update
+        @degree_grade_form = DegreeGradeForm.new(grade_params)
 
-    def grade_params
-      params
-        .require(:candidate_interface_degree_grade_form)
-        .permit(:grade, :other_grade, :predicted_grade)
-        .transform_values(&:strip)
-        .merge(degree: degree)
+        if @degree_grade_form.save
+          current_application.update!(degrees_completed: false)
+          redirect_to candidate_interface_degrees_review_path
+        else
+          track_validation_error(@degree_grade_form)
+          render :new
+        end
+      end
+
+    private
+
+      def degree
+        @degree = ApplicationQualification.find(params[:id])
+      end
+
+      def grade_params
+        params
+          .require(:candidate_interface_degree_grade_form)
+          .permit(:grade, :other_grade, :predicted_grade)
+          .transform_values(&:strip)
+          .merge(degree: degree)
+      end
     end
   end
 end
--- app/controllers/candidate_interface/degrees/year_controller.rb
+++ app/controllers/candidate_interface/degrees/year_controller.rb
@@ -1,49 +1,51 @@
 module CandidateInterface
-  class Degrees::YearController < CandidateInterfaceController
-    before_action :redirect_to_dashboard_if_submitted
+  module Degrees
+    class YearController < CandidateInterfaceController
+      before_action :redirect_to_dashboard_if_submitted
 
-    def new
-      @degree_year_form = DegreeYearForm.new(degree: degree)
-    end
-
-    def create
-      @degree_year_form = DegreeYearForm.new(degree_year_params)
-
-      if @degree_year_form.save
-        redirect_to candidate_interface_degrees_review_path
-      else
-        render :new
+      def new
+        @degree_year_form = DegreeYearForm.new(degree: degree)
       end
-    end
 
-    def edit
-      @degree_year_form = DegreeYearForm.new(degree: degree).fill_form_values
-    end
+      def create
+        @degree_year_form = DegreeYearForm.new(degree_year_params)
 
-    def update
-      @degree_year_form = DegreeYearForm.new(degree_year_params)
-
-      if @degree_year_form.save
-        current_application.update!(degrees_completed: false)
-        redirect_to candidate_interface_degrees_review_path
-      else
-        track_validation_error(@degree_year_form)
-        render :new
+        if @degree_year_form.save
+          redirect_to candidate_interface_degrees_review_path
+        else
+          render :new
+        end
       end
-    end
 
-  private
+      def edit
+        @degree_year_form = DegreeYearForm.new(degree: degree).fill_form_values
+      end
 
-    def degree
-      @degree = ApplicationQualification.find(params[:id])
-    end
+      def update
+        @degree_year_form = DegreeYearForm.new(degree_year_params)
 
-    def degree_year_params
-      params
-        .require(:candidate_interface_degree_year_form)
-        .permit(:start_year, :award_year)
-        .transform_values(&:strip)
-        .merge(degree: degree)
+        if @degree_year_form.save
+          current_application.update!(degrees_completed: false)
+          redirect_to candidate_interface_degrees_review_path
+        else
+          track_validation_error(@degree_year_form)
+          render :new
+        end
+      end
+
+    private
+
+      def degree
+        @degree = ApplicationQualification.find(params[:id])
+      end
+
+      def degree_year_params
+        params
+          .require(:candidate_interface_degree_year_form)
+          .permit(:start_year, :award_year)
+          .transform_values(&:strip)
+          .merge(degree: degree)
+      end
     end
   end
 end
