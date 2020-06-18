673a485a7f313359609b885a28b695129b26d907
Duncan Brown <duncanjbrown@gmail.com>
2020-06-18 10:16:55 +0100

    Add integration spec for Sandbox email suppression

---
+++ spec/system/provider_interface/sandbox_emails_spec.rb
@@ -0,0 +1,60 @@
+require 'rails_helper'
+
+RSpec.feature 'Emails are suppressed in Sandbox' do
+  include CourseOptionHelpers
+  include DfESignInHelpers
+
+  scenario 'when a candidate triggers a notification', sidekiq: true, sandbox: true do
+    given_i_am_a_provider_user_with_dfe_sign_in
+    and_i_am_permitted_to_see_applications_for_my_provider
+    and_an_application_choice_with_an_offer_exists_for_the_provider
+
+    when_a_user_accepts_the_offer
+    then_i_should_not_get_an_email
+
+    when_i_sign_in_to_the_provider_interface
+    and_i_visit_the_application
+    then_i_should_see_the_email_in_the_email_log
+  end
+
+  def given_i_am_a_provider_user_with_dfe_sign_in
+    provider_exists_in_dfe_sign_in
+  end
+
+  def and_i_am_permitted_to_see_applications_for_my_provider
+    provider_user_exists_in_apply_database
+  end
+
+  def and_an_application_choice_with_an_offer_exists_for_the_provider
+    course_option = course_option_for_provider_code(provider_code: 'ABC')
+    @application_choice = create(:application_choice, :with_offer, course_option: course_option)
+  end
+
+  def when_a_user_accepts_the_offer
+    # cheating as I don't want to touch the candidate UI
+    AcceptOffer.new(application_choice: @application_choice).save!
+  end
+
+  def then_i_should_not_get_an_email
+    open_email('provider@example.com')
+    expect(current_email).to be_nil
+  end
+
+  def when_i_sign_in_to_the_provider_interface
+    provider_signs_in_using_dfe_sign_in
+  end
+
+  def and_i_visit_the_application
+    visit provider_interface_application_choice_path(
+      @application_choice.id,
+    )
+  end
+
+  def then_i_should_see_the_email_in_the_email_log
+    within('[data-qa="sub-navigation"]') do
+      click_link 'Emails (Sandbox only)'
+    end
+
+    expect(page).to have_content('has accepted your offer')
+  end
+end
