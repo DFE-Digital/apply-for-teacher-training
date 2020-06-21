fae8a78900644272027c154114f0ef7b7529e06f
David Gisbey <davidgisbey@gmail.com>
2020-06-21 16:59:34 +0100

    Add view and controller logic for a course only on ucas

--- app/controllers/candidate_interface/course_choices/replace_choices/ucas_controller.rb
+++ app/controllers/candidate_interface/course_choices/replace_choices/ucas_controller.rb
@@ -6,6 +6,12 @@ module CandidateInterface
           @course_choice = current_application.application_choices.find(params[:id])
           @provider = Provider.find_by!(id: params[:provider_id])
         end
+
+        def with_course
+          @course_choice = current_application.application_choices.find(params[:id])
+          @provider = Provider.find_by!(id: params[:provider_id])
+          @course = Course.find_by!(id: params[:course_id])
+        end
       end
     end
   end
