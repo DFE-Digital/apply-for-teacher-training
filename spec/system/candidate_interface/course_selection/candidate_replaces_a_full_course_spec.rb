8852b6b2b7150e26f923f48f930eb9b9542a2934
David Gisbey <davidgisbey@gmail.com>
2020-06-21 16:55:57 +0100

    Extend system spec to cover adding a course

--- spec/system/candidate_interface/course_selection/candidate_replaces_a_full_course_spec.rb
+++ spec/system/candidate_interface/course_selection/candidate_replaces_a_full_course_spec.rb
@@ -28,6 +28,24 @@ RSpec.feature 'Selecting a course' do
 
     when_i_click_back
     then_i_see_the_pick_replacment_provider_page
+
+    when_i_choose_a_provider
+    then_i_should_see_a_course_and_its_description
+
+    when_submit_without_choosing_a_course
+    then_i_should_see_an_error
+
+    when_i_choose_a_course_on_ucas_but_not_on_apply
+    then_i_see_the_replace_course_choice_ucas_with_course_page
+
+    when_i_click_back
+    then_i_see_the_pick_replacment_course_page
+
+    when_i_choose_a_course_that_is_full
+    then_i_see_the_replace_course_choice_full_page
+
+    when_i_click_back
+    then_i_see_the_pick_replacment_course_page
   end
 
   def given_the_replace_full_or_withdrawn_application_choices_is_active
@@ -107,5 +125,47 @@ RSpec.feature 'Selecting a course' do
   def then_i_see_the_replace_course_choice_ucas_no_courses_page
     expect(page).to have_content t('page_titles.apply_to_provider_on_ucas')
     expect(page).to have_current_path candidate_interface_replace_course_choice_ucas_no_courses_path(@course_choice.id, @provider_with_no_courses.id)
+  end
+
+  def when_i_choose_a_provider
+    select @provider.name_and_code
+    click_button 'Continue'
+  end
+
+  def then_i_should_see_a_course_and_its_description
+    expect(page).to have_content(@course.name_and_code)
+    expect(page).to have_content(@course.description)
+  end
+
+  def when_submit_without_choosing_a_course
+    click_button 'Continue'
+  end
+
+  def then_i_should_see_an_error
+    expect(page).to have_content 'Select a course'
+  end
+
+  def when_i_choose_a_course_on_ucas_but_not_on_apply
+    choose @course_on_ucas.name_and_code
+    click_button 'Continue'
+  end
+
+  def then_i_see_the_replace_course_choice_ucas_with_course_page
+    expect(page).to have_content t('page_titles.apply_to_course_on_ucas')
+    expect(page).to have_current_path candidate_interface_replace_course_choice_ucas_with_course_path(@course_choice.id, @provider.id, @course_on_ucas.id)
+  end
+
+  def then_i_see_the_pick_replacment_course_page
+    expect(page).to have_current_path candidate_interface_replace_course_choice_course_path(@course_choice.id, @provider.id)
+  end
+
+  def when_i_choose_a_course_that_is_full
+    choose @full_course.name_and_code
+    click_button 'Continue'
+  end
+
+  def then_i_see_the_replace_course_choice_full_page
+    expect(page).to have_content t('page_titles.full_course')
+    expect(page).to have_current_path candidate_interface_replace_course_choice_full_path(@course_choice.id, @provider.id, @full_course.id)
   end
 end
