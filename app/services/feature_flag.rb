504581fcd5eae5ef8df6c42f7d085d1ef12eaf8a
Malcolm Baig <malcolm.baig@gmail.com>
2020-06-23 16:22:08 +0100

    Add feature flag for HESA degree data

--- app/services/feature_flag.rb
+++ app/services/feature_flag.rb
@@ -34,6 +34,7 @@ class FeatureFlag
     [:mark_every_section_complete, 'Each section of the application form should have to be explicitly completed', 'David Gisbey'],
     [:replace_full_or_withdrawn_application_choices, 'Allows candidates to replace full or withdrawn application choices post-submission', 'David Gisbey'],
     [:unavailable_course_notifications, 'Candidates with applications waiting for references receive an email notification if their course choice is withdrawn has no vacancies', 'Steve Hook'],
+    [:hesa_degree_data, 'Use structured HESA data to autocomplete certain parts of the add degree flow', 'Malcolm Baig'],
   ].freeze
 
   FEATURES = (PERMANENT_SETTINGS + TEMPORARY_FEATURE_FLAGS).map { |name, description, owner|
