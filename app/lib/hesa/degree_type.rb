4f3990b75a6a4ec258edeb8b41e8bd68ed73fd93
Malcolm Baig <malcolm.baig@gmail.com>
2020-06-24 14:17:42 +0100

    Add undergraduate filtering to Hesa::DegreeType model
    Update the `#abbreviations_and_names` method to accept the symbol
    `:undergraduate` as an argument and return only those degree types we
    want to make selectable when adding an undergraduate degree, ie -
    bachelors and masters.
    
    This will be used to populate the autocomplete on the frontend with
    different lists as required:
    
    - Show only bachelor and master types when adding an undergraduate degree.
    - Show all types when adding additional degrees.

--- app/lib/hesa/degree_type.rb
+++ app/lib/hesa/degree_type.rb
@@ -1,20 +1,27 @@
 module Hesa
   class DegreeType
     DegreeTypeStruct = Struct.new(:hesa_code, :abbreviation, :name, :level)
+    UNDERGRADUATE_LEVELS = %i[bachelor master].freeze
 
     class << self
       def all
         HESA_DEGREE_TYPES.map { |type_data| DegreeTypeStruct.new(*type_data) }
       end
 
-      def abbreviations_and_names
-        all.map do |degree_type|
-          "#{degree_type.abbreviation}|#{degree_type.name}"
+      def abbreviations_and_names(level: :all)
+        case level
+        when :all
+          all
+            .map { |type| "#{type.abbreviation}|#{type.name}" }
+        when :undergraduate
+          all
+            .select { |type| type.level.in? UNDERGRADUATE_LEVELS }
+            .map { |type| "#{type.abbreviation}|#{type.name}" }
         end
       end
 
       def find_by_name(name)
-        all.find { |degree_type| degree_type.name == name }
+        all.find { |type| type.name == name }
       end
     end
   end
