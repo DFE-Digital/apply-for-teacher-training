47ac24ea9607b5b4bf7028b0fff2e7fc682a4894
Aga Dufrat <aga.dufrat@digital.education.gov.uk>
2020-06-26 18:18:07 +0100

    Show `No information shared` when there is no disability disclosure
    In the providers interface, when viewing an application without the
    disability disclosure filled in, this section is empty. It should say
    "No information shared" so it's consistent with the candidate side.

--- spec/components/provider_interface/training_with_disability_component_spec.rb
+++ spec/components/provider_interface/training_with_disability_component_spec.rb
@@ -1,27 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe ProviderInterface::TrainingWithDisabilityComponent do
-  it 'renders nothing if `#disclose_disability` is false' do
+  it 'renders `No information shared` if `#disclose_disability` is false' do
     application_form = instance_double(
       ApplicationForm,
       disclose_disability?: false,
-      disability_disclosure: 'I am hard of hearing',
+      disability_disclosure: nil,
     )
     result = render_inline(described_class.new(application_form: application_form))
-    expect(result.text).to eq ''
+    expect(result.text).to include('No information shared.')
   end
 
-  it 'renders nothing if `#disclose_disability` is true but there is no disclosure' do
+  it 'renders `No information shared` if `#disclose_disability` is true but there is no disclosure' do
     application_form = instance_double(
       ApplicationForm,
       disclose_disability?: true,
       disability_disclosure: '',
     )
     result = render_inline(described_class.new(application_form: application_form))
-    expect(result.text).to eq ''
+    expect(result.text).to include('No information shared.')
   end
 
-  it 'renders heading and disclosure if `#disclose_disability` is true' do
+  it 'renders `No information shared` if `#disclose_disability` is nil' do
+    application_form = instance_double(
+      ApplicationForm,
+      disclose_disability?: nil,
+      disability_disclosure: nil,
+    )
+    result = render_inline(described_class.new(application_form: application_form))
+    expect(result.text).to include('No information shared.')
+  end
+
+  it 'renders disclosure if `#disclose_disability` is true' do
     application_form = instance_double(
       ApplicationForm,
       disclose_disability?: true,
