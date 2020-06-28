1eee74bf2679036bf50479ca4bfa9456275c9aaf
Steve Hook <steve.hook@gmail.com>
2020-06-29 00:18:17 +0100

    Refactor timestamp lookups into separate service class

---
+++ app/services/support_interface/candidate_journey_tracker.rb
@@ -0,0 +1,50 @@
+module SupportInterface
+  class CandidateJourneyTracker
+    def initialize(application_choice)
+      @application_choice = application_choice
+    end
+
+    def form_not_started
+      @application_choice.application_form.created_at
+    end
+
+    def form_started_and_not_submitted
+      [
+        @application_choice.application_form.audits.where(action: :update).minimum(:created_at),
+        @application_choice.created_at,
+      ].compact.min
+    end
+
+    def submitted_and_awaiting_references
+      @application_choice.application_form.submitted_at
+    end
+    # def reference_1_received: nil, # - from audit trail (reference status change)?
+    # def reference_2_recieved: nil, # - from audit trail (reference status change)?
+    # def reference_reminder_email_sent: nil, # - from chasers sent (chaser_type: :reference_request)
+    # def new_reference_request_email_sent: nil, # - emails?
+    # def new_reference_added: nil, # - ?
+    # def references_complete: nil, # - from audit trail when `ApplicationForm#references_completed` gets set to true? (can happen more than once)
+    # def waiting_to_be_sent_to_provider: nil, # - not sure what this one means
+    # def application_sent_to_provider: nil, # - `ApplicationChoice#sent_to_provider_at`
+    # def awaiting_decision: nil, # QUESTION - not sure what this means (isn't it the same as `sent_to_provider_at`?)
+    # def rbd_date: nil, # - `ApplicationChoice#sent_to_provider_at`
+    # def rbd_reminder_sent: nil, # - Is this the `chase_provider_decision` email? `ChaserSent#provider_decision_request` ?
+    # def application_rbd: nil, # - Combination of `ApplicationChoice#rejected_at` and `rejected_by_default`
+    # def provider_decision: nil, # (Reject/Offer) - `ApplicationChoice#rejected_at` or `ApplicationChoice#offered_at`
+    # def offer_made: nil, # awaiting decision from candidate - `ApplicationChoice#offered_at`
+    # def email_sent_to_candidate: nil, # - the offer email? also the reject email?
+    # def dbd_date: nil, # - `ApplicationChoice#decline_by_default_at`
+    # def dbd_reminder_email: nil, # - `ChaserSent#chaser_type`
+    # def candidate_decision: nil, # (accept/decline) - `ApplicationChoice#accepted_at` or `ApplicationChoice#declined_at`
+    # def offer_declined: nil, # - `ApplicationChoice#declined_at`
+    # def offer_accepted: nil, # - `ApplicationChoice#accepted_at`
+    # def email_sent_to_candidate_2: nil, # - the offer accepted email? QUESTION
+    # def pending_conditions: nil, # - from audit trail
+    # def conditions_outcome: nil, # - from audit trail
+    # def conditions_met: nil, # - from audit trail
+    # def conditions_not_met: nil, # - from audit trail
+    # def enrolled: nil, # - from audit trail
+    # def ended_without_success: nil, # - audit trail?
+    # def send_rejection_email: nil, # - emails?
+  end
+end
--- app/services/support_interface/candidate_journey_tracking_export.rb
+++ app/services/support_interface/candidate_journey_tracking_export.rb
@@ -15,39 +15,9 @@ module SupportInterface
   private
 
     def journey_items(application_choice)
-      {
-        form_not_started: application_choice.application_form.created_at, # - created_at?
-        form_started_and_not_submitted: form_started_and_not_submitted(application_choice), #  first application form audit trail update or application choice created_at?
-        submitted_and_awaiting_references: application_choice.application_form.submitted_at, # - submitted_at
-        reference_1_received: nil, # - from audit trail (reference status change)?
-        reference_2_recieved: nil, # - from audit trail (reference status change)?
-        reference_reminder_email_sent: nil, # - from chasers sent (chaser_type: :reference_request)
-        new_reference_request_email_sent: nil, # - emails?
-        new_reference_added: nil, # - ?
-        references_complete: nil, # - from audit trail when `ApplicationForm#references_completed` gets set to true? (can happen more than once)
-        waiting_to_be_sent_to_provider: nil, # - not sure what this one means
-        application_sent_to_provider: nil, # - `ApplicationChoice#sent_to_provider_at`
-        awaiting_decision: nil, # QUESTION - not sure what this means (isn't it the same as `sent_to_provider_at`?)
-        rbd_date: nil, # - `ApplicationChoice#sent_to_provider_at`
-        rbd_reminder_sent: nil, # - Is this the `chase_provider_decision` email? `ChaserSent#provider_decision_request` ?
-        application_rbd: nil, # - Combination of `ApplicationChoice#rejected_at` and `rejected_by_default`
-        provider_decision: nil, # (Reject/Offer) - `ApplicationChoice#rejected_at` or `ApplicationChoice#offered_at`
-        offer_made: nil, # awaiting decision from candidate - `ApplicationChoice#offered_at`
-        email_sent_to_candidate: nil, # - the offer email? also the reject email?
-        dbd_date: nil, # - `ApplicationChoice#decline_by_default_at`
-        dbd_reminder_email: nil, # - `ChaserSent#chaser_type`
-        candidate_decision: nil, # (accept/decline) - `ApplicationChoice#accepted_at` or `ApplicationChoice#declined_at`
-        offer_declined: nil, # - `ApplicationChoice#declined_at`
-        offer_accepted: nil, # - `ApplicationChoice#accepted_at`
-        email_sent_to_candidate_2: nil, # - the offer accepted email? QUESTION
-        pending_conditions: nil, # - from audit trail
-        conditions_outcome: nil, # - from audit trail
-        conditions_met: nil, # - from audit trail
-        conditions_not_met: nil, # - from audit trail
-        enrolled: nil, # - from audit trail
-        ended_without_success: nil, # - audit trail?
-        send_rejection_email: nil, # - emails?
-      }
+      tracker = CandidateJourneyTracker.new(application_choice)
+
+      tracker.public_methods(false).index_with { |item| [item, tracker.send(item)] }
     end
 
     def all_application_choices
@@ -57,13 +27,6 @@ module SupportInterface
         )
         .joins(:application_form)
         .order('application_forms.submitted_at asc, application_forms.id asc, id asc')
-    end
-
-    def form_started_and_not_submitted(application_choice)
-      [
-        application_choice.application_form.audits.where(action: :update).minimum(:created_at),
-        application_choice.created_at,
-      ].compact.min
     end
   end
 end
