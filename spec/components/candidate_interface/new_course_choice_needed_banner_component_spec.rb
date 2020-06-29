14c58297d0910e8ba134d6a2cc5db2340cf1bab7
David Gisbey <davidgisbey@gmail.com>
2020-06-29 09:44:23 +0100

    Update replace choice banner spec to check it renders correctly

--- spec/components/candidate_interface/new_course_choice_needed_banner_component_spec.rb
+++ spec/components/candidate_interface/new_course_choice_needed_banner_component_spec.rb
@@ -6,9 +6,19 @@ RSpec.describe CandidateInterface::NewCourseChoiceNeededBannerComponent do
 
     before { FeatureFlag.activate('replace_full_or_withdrawn_application_choices') }
 
+
     context 'when a course has been withdrawn and the candidate is awaiting their references' do
       it 'renders the component' do
-        course = create(:course, withdrawn: true)
+        course = create(:course, withdrawn: false, open_on_apply: false)
+        create(:application_choice, application_form: application_form, status: 'awaiting_references')
+
+        expect(described_class.new(application_form: application_form).render?).to be_truthy
+      end
+    end
+
+    context 'when a course has been withdrawn and the candidate is awaiting their references' do
+      it 'renders the component' do
+        course = create(:course, withdrawn: true, open_on_apply: true)
         course_option = create(:course_option, course: course)
         create(:application_choice, application_form: application_form, course_option: course_option, status: 'awaiting_references')
 
@@ -18,7 +28,7 @@ RSpec.describe CandidateInterface::NewCourseChoiceNeededBannerComponent do
 
     context 'when a course has been withdrawn and the candidates application is complete' do
       it 'does not render the componen' do
-        course = create(:course, withdrawn: true)
+        course = create(:course, withdrawn: true, open_on_apply: true)
         course_option = create(:course_option, course: course)
         create(:application_choice, application_form: application_form, course_option: course_option, status: 'application_complete')
 
@@ -28,7 +38,7 @@ RSpec.describe CandidateInterface::NewCourseChoiceNeededBannerComponent do
 
     context 'when a course has not been withdrawn' do
       it 'does not render the component' do
-        course = create(:course, withdrawn: false)
+        course = create(:course, withdrawn: false, open_on_apply: true)
         course_option = create(:course_option, course: course)
         create(:application_choice, application_form: application_form, course_option: course_option, status: 'awaiting_references')
 
@@ -38,7 +48,8 @@ RSpec.describe CandidateInterface::NewCourseChoiceNeededBannerComponent do
 
     context 'when a course choice has become full' do
       it 'renders the component' do
-        course_option = build(:course_option, vacancy_status: 'no_vacancies')
+        course = create(:course, withdrawn: false, open_on_apply: true)
+        course_option = build(:course_option, vacancy_status: 'no_vacancies', course: course)
         create(:application_choice, application_form: application_form, course_option: course_option, status: 'awaiting_references')
 
         expect(described_class.new(application_form: application_form).render?).to be_truthy
@@ -47,7 +58,8 @@ RSpec.describe CandidateInterface::NewCourseChoiceNeededBannerComponent do
 
     context 'when a course choice still has vacancies' do
       it 'does not render the component' do
-        course_option = build(:course_option, vacancy_status: 'vacancies')
+        course = create(:course, withdrawn: false, open_on_apply: true)
+        course_option = build(:course_option, vacancy_status: 'vacancies', course: course)
         create(:application_choice, application_form: application_form, course_option: course_option, status: 'awaiting_references')
 
         expect(described_class.new(application_form: application_form).render?).to be_falsey
