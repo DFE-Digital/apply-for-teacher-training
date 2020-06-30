52ee5d0517b056037c861388dedeface14327240
Malcolm Baig <malcolm.baig@gmail.com>
2020-06-30 15:16:12 +0100

    Use main HESA grades in degress grade form
    - Set the main grades in an instance variable.
    - Create the radio buttons on the form from these grades.

--- app/controllers/candidate_interface/degrees/grade_controller.rb
+++ app/controllers/candidate_interface/degrees/grade_controller.rb
@@ -2,6 +2,7 @@ module CandidateInterface
   module Degrees
     class GradeController < CandidateInterfaceController
       before_action :redirect_to_dashboard_if_submitted
+      before_action :set_main_grades
 
       def new
         @degree_grade_form = DegreeGradeForm.new(degree: degree)
@@ -37,6 +38,10 @@ module CandidateInterface
 
       def degree
         @degree = ApplicationQualification.find(params[:id])
+      end
+
+      def set_main_grades
+        @main_grades = Hesa::Grade.main.map(&:description)
       end
 
       def grade_params
