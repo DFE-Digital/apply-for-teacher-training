8a559d76313c78c6c9275fb5361c776637c990b6
Duncan Brown <duncanjbrown@gmail.com>
2020-07-01 09:05:13 +0100

    Revert "Silence Redis deprecation warnings in worker logs"
    This reverts commit 16d2ad7ae053247760cf3905cdbacfdf4ec4b7ea.

--- config/initializers/redis.rb
+++
@@ -1,5 +0,0 @@
-if Gem::Version.new(Sidekiq::VERSION) < Gem::Version.new('6.1')
-  Redis.exists_returns_integer = true
-else
-  raise 'Time to remove Redis.exists_returns_integer: https://github.com/mperham/sidekiq/issues/4591'
-end
