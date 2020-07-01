c810790e1b7e05605cf8e8002d85ae7cf69d34a9
Steve Laing <steve.laing@gmail.com>
2020-07-01 08:17:15 +0100

    Add an STI alias for TrainingProviderPermissions
    There are ProviderRelationshipPermissions records in the db using the STI name 'ProviderInterface::TrainingProviderPermissions'
    but this class has been demodulized. So add an alias until we have migrated existing records.

--- app/models/training_provider_permissions.rb
+++ app/models/training_provider_permissions.rb
@@ -1,2 +1,6 @@
 class TrainingProviderPermissions < ProviderRelationshipPermissions
 end
+
+# Backwards compatibility for any existing db records using the old STI class name.
+# This can be removed once records are migrated.
+ProviderInterface::TrainingProviderPermissions = TrainingProviderPermissions
