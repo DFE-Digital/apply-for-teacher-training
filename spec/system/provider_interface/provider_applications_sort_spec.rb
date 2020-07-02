5759901d9f7db0731a38607e3ed639becaa10959
Steve Laing <steve.laing@gmail.com>
2020-07-02 14:17:08 +0100

    Update sorting system spec with new behaviours
    Adds sorting by 'Days left to respond' steps.

--- spec/system/provider_interface/provider_applications_sort_spec.rb
+++ spec/system/provider_interface/provider_applications_sort_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature 'Providers should be able to sort applications' do
   include CourseOptionHelpers
   include DfESignInHelpers
 
-  scenario 'by column headings' do
+  scenario 'by sort options' do
     given_i_am_a_provider_user_with_dfe_sign_in
     and_i_am_permitted_to_see_applications_for_my_provider
     and_my_organisation_has_courses_with_applications
@@ -12,11 +12,14 @@ RSpec.feature 'Providers should be able to sort applications' do
 
     when_i_visit_the_provider_page
     then_i_should_see_the_applications_in_descending_date_order
-    and_the_sorted_by_explanation_line_should_be_present
+    and_the_sorted_by_option_should_be_present
+
+    when_i_sort_by_days_left_to_respond
+    then_i_should_see_the_applications_in_descending_reject_by_default_date_order
   end
 
-  def and_the_sorted_by_explanation_line_should_be_present
-    expect(page).to have_content('Sorted by: last changed')
+  def and_the_sorted_by_option_should_be_present
+    expect(page).to have_select(:sort_by, selected: 'Last changed')
   end
 
   def given_i_am_a_provider_user_with_dfe_sign_in
@@ -34,17 +37,45 @@ RSpec.feature 'Providers should be able to sort applications' do
     course_option_two = course_option_for_provider(provider: current_provider, course: create(:course, name: 'Divination', provider: current_provider))
     course_option_three = course_option_for_provider(provider: current_provider, course: create(:course, name: 'English', provider: current_provider))
 
-    create(:application_choice, :awaiting_provider_decision, course_option: course_option_one, status: 'withdrawn', application_form:
-           create(:application_form, first_name: 'Jim', last_name: 'James'), updated_at: 1.day.ago)
+    create(
+      :application_choice,
+      :awaiting_provider_decision,
+      course_option: course_option_one,
+      status: 'withdrawn',
+      application_form: create(:application_form, first_name: 'Jim', last_name: 'James'),
+      reject_by_default_at: 1.day.from_now,
+      updated_at: 1.day.ago,
+    )
 
-    create(:application_choice, :awaiting_provider_decision, course_option: course_option_two, status: 'offer', application_form:
-           create(:application_form, first_name: 'Adam', last_name: 'Jones'), updated_at: 2.days.ago)
+    create(
+      :application_choice,
+      :awaiting_provider_decision,
+      course_option: course_option_two,
+      status: 'offer',
+      application_form: create(:application_form, first_name: 'Adam', last_name: 'Jones'),
+      reject_by_default_at: 5.days.from_now,
+      updated_at: 2.days.ago,
+    )
 
-    create(:application_choice, :awaiting_provider_decision, course_option: course_option_two, status: 'offer', application_form:
-           create(:application_form, first_name: 'Tom', last_name: 'Jones'), updated_at: 2.days.ago)
+    create(
+      :application_choice,
+      :awaiting_provider_decision,
+      course_option: course_option_two,
+      status: 'offer',
+      application_form: create(:application_form, first_name: 'Tom', last_name: 'Jones'),
+      reject_by_default_at: 10.days.from_now,
+      updated_at: 2.days.ago,
+    )
 
-    create(:application_choice, :awaiting_provider_decision, course_option: course_option_three, status: 'declined', application_form:
-           create(:application_form, first_name: 'Bill', last_name: 'Bones'), updated_at: 3.days.ago)
+    create(
+      :application_choice,
+      :awaiting_provider_decision,
+      course_option: course_option_three,
+      status: 'declined',
+      application_form: create(:application_form, first_name: 'Bill', last_name: 'Bones'),
+      reject_by_default_at: 1.day.ago,
+      updated_at: 3.days.ago,
+    )
   end
 
   def when_i_visit_the_provider_page
@@ -57,4 +88,33 @@ RSpec.feature 'Providers should be able to sort applications' do
     expect('Tom Jones').to appear_before('Bill Bones')
   end
   # rubocop:enable RSpec/ExpectActual
+
+  def when_i_sort_by_days_left_to_respond
+    select 'Days left to respond', from: :sort_by
+    click_on 'Sort'
+  end
+
+  def then_i_should_see_the_applications_in_descending_reject_by_default_date_order
+    cards = all('.app-application-card')
+
+    within(cards[0]) do
+      expect(page).to have_content('Tom Jones')
+      expect(page).to have_content('10 days to respond')
+    end
+
+    within(cards[1]) do
+      expect(page).to have_content('Adam Jones')
+      expect(page).to have_content('5 days to respond')
+    end
+
+    within(cards[2]) do
+      expect(page).to have_content('Jim James')
+      expect(page).to have_content('1 day to respond')
+    end
+
+    within(cards[3]) do
+      expect(page).to have_content('Bill Bones')
+      expect(page).to have_content(3.days.ago.to_s(:govuk_date))
+    end
+  end
 end
