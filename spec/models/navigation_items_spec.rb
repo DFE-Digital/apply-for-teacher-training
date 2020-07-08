4b58dd72f6d836e73fe3ca638655996d14aa0c3d
Steve Laing <steve.laing@gmail.com>
2020-07-08 09:55:27 +0100

    Only show Organisation nav item when there are organisations to manage
    This should only link to the orgs index if the user level permissions can manage orgs is present
    and there are provider relationship permissions for the current user's providers.

---
+++ spec/models/navigation_items_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe NavigationItems do
+  describe '.for_provider_interface' do
+    let(:provider) { create(:provider) }
+    let(:provider_user) { create(:provider_user, providers: [provider]) }
+    let(:controller) { ProviderInterface::ProviderInterfaceController }
+
+    subject(:navigation_items) { NavigationItems.for_provider_interface(provider_user, controller) }
+
+    context 'when the user can manage organisations and there are no provider relationships to manage' do
+      before { provider_user.provider_permissions.find_by(provider: provider).update!(manage_organisations: true) }
+
+      it 'does not include a link to Organisations' do
+        expect(navigation_items.map(&:text)).not_to include('Organisations')
+      end
+    end
+
+    context 'when the user can manage organisations and there are provider relationships to manage' do
+      before do
+        provider_user.provider_permissions.find_by(provider: provider).update!(manage_organisations: true)
+        create(:training_provider_permissions, training_provider: provider)
+      end
+
+      it 'includes a link to Organisations' do
+        expect(navigation_items.map(&:text)).to include('Organisations')
+      end
+    end
+  end
+end
