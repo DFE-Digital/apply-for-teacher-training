7ba96c89873f5d681ee7357451d4117e4663d66a
Steve Hook <steve.hook@gmail.com>
2020-07-09 22:07:00 +0100

    Extend DegreeTypeForm to persist `international` attribute

--- app/forms/candidate_interface/degree_type_form.rb
+++ app/forms/candidate_interface/degree_type_form.rb
@@ -3,8 +3,10 @@ module CandidateInterface
     include ActiveModel::Model
 
     attr_accessor :type_description
+    attr_accessor :uk_degree
     attr_accessor :application_form, :degree
 
+    validates :uk_degree, presence: true, if: -> { FeatureFlag.active?(:international_degrees) }
     validates :type_description, presence: true
     validates :type_description, length: { maximum: 255 }
 
@@ -13,6 +15,7 @@ module CandidateInterface
       return false unless application_form_present?
 
       self.degree = application_form.application_qualifications.degree.create!(
+        international: international?,
         qualification_type: type_description,
         qualification_type_hesa_code: hesa_code,
       )
@@ -23,12 +26,14 @@ module CandidateInterface
       return false unless degree_present?
 
       degree.update!(
+        international: international?,
         qualification_type: type_description,
         qualification_type_hesa_code: hesa_code,
       )
     end
 
     def fill_form_values
+      self.uk_degree = !degree.international?
       self.type_description = degree.qualification_type
       self
     end
@@ -55,6 +60,10 @@ module CandidateInterface
         errors.add(:degree, 'is missing')
         false
       end
+    end
+
+    def international?
+      FeatureFlag.active?(:international_degrees) && uk_degree == 'no'
     end
   end
 end
