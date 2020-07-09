ae1785723c598008ea8f6a3bcee696aa98e3883d
Malcolm Baig <malcolm.baig@gmail.com>
2020-07-09 09:58:24 +0100

    Place feature flag in front of in-progress EFL feature

--- app/controllers/candidate_interface/english_language/start_controller.rb
+++ app/controllers/candidate_interface/english_language/start_controller.rb
@@ -1,7 +1,9 @@
 module CandidateInterface
   module EnglishLanguage
     class StartController < CandidateInterfaceController
-      def new; end
+      def new
+        render_404 unless FeatureFlag.active?(:efl_section)
+      end
     end
   end
 end
