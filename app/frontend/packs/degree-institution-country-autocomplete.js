2bc38cb70e0d2f4f67a45cb7332521cb2b8d9278
Steve Hook <steve.hook@gmail.com>
2020-07-11 23:53:56 +0100

    Make institution country autocomplete
    Also add validation error message

---
+++ app/frontend/packs/degree-institution-country-autocomplete.js
@@ -0,0 +1,32 @@
+import accessibleAutocomplete from "accessible-autocomplete";
+
+const initDegreeInstitutionCountryAutocomplete = () => {
+  try {
+    const inputIds = [
+      "#candidate-interface-degree-institution-form-institution-country-field",
+      "#candidate-interface-degree-institution-form-institution-country-field-error",
+    ];
+
+    inputIds.forEach(inputId => {
+      const select = document.querySelector(inputId);
+      console.log(inputId, select);
+
+      if (!select) return;
+
+      // Replace "Select a country" with empty string
+      select.querySelector("[value='']").innerHTML = "";
+
+      accessibleAutocomplete.enhanceSelectElement({
+        selectElement: select,
+        name: select.name,
+        showAllValues: true,
+        confirmOnBlur: false
+      });
+      select.name = "";
+    });
+  } catch (err) {
+    console.error("Could not enhance degree institution country select:", err);
+  }
+};
+
+export default initDegreeInstitutionCountryAutocomplete;
