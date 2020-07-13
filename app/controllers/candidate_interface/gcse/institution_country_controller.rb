43f9de3be338e7bf22c7a0ec08a7af7cd3e91370
David Gisbey <davidgisbey@gmail.com>
2020-07-13 15:32:21 +0100

    Add update action to the controller

--- app/controllers/candidate_interface/gcse/institution_country_controller.rb
+++ app/controllers/candidate_interface/gcse/institution_country_controller.rb
@@ -7,18 +7,44 @@ module CandidateInterface
       @institution_country = find_or_build_qualification_form
     end
 
+    def update
+      @institution_country = find_or_build_qualification_form
+
+      @current_qualification = params['institution_country']
+
+      if @institution_country.save(@current_qualification)
+        update_gcse_completed(false)
+
+        redirect_to next_gcse_path
+      else
+        track_validation_error(@institution_country)
+        render :edit
+      end
+    end
+
   private
 
     def find_or_build_qualification_form
-      current_qualification = current_application.qualification_in_subject(:gcse, subject_param)
+      @current_qualification = current_application.qualification_in_subject(:gcse, subject_param)
 
-      if current_qualification
-        GcseInstitutionCountryForm.build_from_qualification(current_qualification)
+      if @current_qualification
+        GcseInstitutionCountryForm.build_from_qualification(@current_qualification)
       else
         GcseInstitutionCountryForm.new(
           subject: subject_param,
           level: ApplicationQualification.levels[:gcse],
         )
+      end
+    end
+
+    def next_gcse_path
+      @details_form = GcseQualificationDetailsForm.build_from_qualification(
+        current_application.qualification_in_subject(:gcse, subject_param),
+      )
+      if @details_form.qualification.naric_reference.nil?
+        candidate_interface_gcse_details_edit_naric_reference_path
+      else
+        candidate_interface_gcse_review_path
       end
     end
   end
