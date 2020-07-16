8109342a305ffdfc7c92ae94b7bfb1f700e35d86
Duncan Brown <duncanjbrown@gmail.com>
2020-07-16 17:29:12 +0100

    Add the grey nav bar to the provider interface
    I've called it sub_nav as it's underneath the other one, even though
    it sort of has equal status with the rest of the navigation

--- app/components/provider_interface/header_component.rb
+++ app/components/provider_interface/header_component.rb
@@ -2,10 +2,11 @@ class ProviderInterface::HeaderComponent < ViewComponent::Base
   attr_reader :navigation_items, :service_url, :product_name, :classes
   include ViewHelper
 
-  def initialize(navigation_items:, product_name:, service_url:, classes: '')
-    @navigation_items = navigation_items
-    @product_name     = product_name
-    @service_url      = service_url
-    @classes          = classes
+  def initialize(navigation_items:, sub_navigation_items:, product_name:, service_url:, classes: '')
+    @navigation_items     = navigation_items
+    @sub_navigation_items = sub_navigation_items
+    @product_name         = product_name
+    @service_url          = service_url
+    @classes              = classes
   end
 end
--- app/models/navigation_items.rb
+++ app/models/navigation_items.rb
@@ -39,7 +39,7 @@ class NavigationItems
     def for_provider_interface(current_provider_user, current_controller)
       return [NavigationItem.new('Sign in', provider_interface_sign_in_path, false)] unless current_provider_user
 
-      items = [NavigationItem.new('Applications', provider_interface_applications_path, is_active(current_controller, %w[application_choices decisions offer_changes]))]
+      items = []
 
       if current_provider_user.can_manage_organisations? && Provider.with_permissions_visible_to(current_provider_user).exists?
         items << NavigationItem.new('Organisations', provider_interface_organisations_path, is_active(current_controller, %w[organisations provider_relationship_permissions]))
@@ -51,6 +51,12 @@ class NavigationItems
 
       items << NavigationItem.new('Account', provider_interface_account_path, is_active(current_controller, 'account'))
       items << NavigationItem.new('Sign out', provider_interface_sign_out_path, false)
+    end
+
+    def for_provider_interface_sub_nav(current_controller)
+      [
+        NavigationItem.new('Applications', provider_interface_applications_path, is_active(current_controller, %w[application_choices decisions offer_changes])),
+      ]
     end
 
     def for_api_docs(current_controller)
