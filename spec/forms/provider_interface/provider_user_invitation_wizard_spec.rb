4f581615ccaac08eb73754031b5bc688cfd69692
Steve Hook <steve.hook@gmail.com>
2020-07-16 15:55:44 +0100

    Refactor to makes specs more explicit (removed lets)

--- spec/forms/provider_interface/provider_user_invitation_wizard_spec.rb
+++ spec/forms/provider_interface/provider_user_invitation_wizard_spec.rb
@@ -1,19 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe ProviderInterface::ProviderUserInvitationWizard do
-  let(:form_params) { {} }
-  let(:initial_state) { {} }
-  let(:state_store) do
-    {
-      described_class::STATE_STORE_KEY => initial_state.to_json,
-    }
-  end
-
   def state_store_for(state)
     { described_class::STATE_STORE_KEY => state.to_json }
   end
-
-  subject(:wizard) { described_class.new(state_store, form_params) }
 
   describe 'next_step' do
     it 'returns the providers page from the basic details page for a new user' do
@@ -42,14 +32,11 @@ RSpec.describe ProviderInterface::ProviderUserInvitationWizard do
   end
 
   describe 'initializer' do
-    let(:initial_state) do
-      {
-        first_name: 'Bob',
-        email_address: 'bob@example.com',
-      }
-    end
-
     it 'deserializes state' do
+      state_store = state_store_for(first_name: 'Bob', email_address: 'bob@example.com')
+
+      wizard = described_class.new(state_store, current_step: 'details')
+
       expect(wizard.first_name).to eq 'Bob'
       expect(wizard.last_name).to be_nil
       expect(wizard.email_address).to eq 'bob@example.com'
@@ -57,17 +44,14 @@ RSpec.describe ProviderInterface::ProviderUserInvitationWizard do
   end
 
   describe '#save_state!' do
-    let(:initial_state) do
-      {
-        first_name: 'Bob',
-        email_address: 'bob@example.com',
-      }
-    end
-
     it 'serializes state to state store' do
+      state_store = state_store_for(first_name: 'Bob', email_address: 'bob@example.com')
+      wizard = described_class.new(state_store)
       wizard.last_name = 'Roberts'
       wizard.email_address = 'bob@roberts.com'
+
       wizard.save_state!
+
       expect(JSON.parse(state_store[described_class::STATE_STORE_KEY]).symbolize_keys).to eq({
         first_name: 'Bob',
         last_name: 'Roberts',
@@ -77,27 +61,23 @@ RSpec.describe ProviderInterface::ProviderUserInvitationWizard do
   end
 
   describe '#clear_state!' do
-    let(:initial_state) do
-      {
-        first_name: 'Bob',
-        email_address: 'bob@example.com',
-      }
-    end
-
     it 'purges all state' do
+      state_store = state_store_for(first_name: 'Bob', email_address: 'bob@example.com')
+      wizard = described_class.new(state_store, current_step: 'details')
       wizard.last_name = 'Roberts'
+
       wizard.clear_state!
+
       expect(state_store[described_class::STATE_STORE_KEY]).to be_nil
     end
   end
 
   describe 'validations' do
     context 'with missing name and email fields' do
-      let(:form_params) do
-        {}
-      end
-
       it 'first, last name and email address are required' do
+        state_store = state_store_for({})
+        wizard = described_class.new(state_store, {})
+
         wizard.valid?(:details)
 
         expect(wizard.errors[:first_name]).not_to be_empty
@@ -107,13 +87,13 @@ RSpec.describe ProviderInterface::ProviderUserInvitationWizard do
     end
 
     context 'with email address of an existing user' do
-      let(:email_address) { 'provider@example.com' }
-      let(:existing_user) { create(:provider_user, :with_provider, email_address: email_address) }
-
-      before { form_params[:email_address] = existing_user.email_address }
-
       it 'is valid' do
+        existing_user = create(:provider_user, :with_provider, email_address: 'provider@example.com')
+        state_store = state_store_for({})
+        wizard = described_class.new(state_store, email_address: existing_user.email_address)
+
         wizard.validate
+
         expect(wizard.errors[:email_address]).to be_empty
       end
     end
