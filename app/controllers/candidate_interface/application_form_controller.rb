6bdb0bf87e512ef67576b6c9a3a6393c22834e3b
George Holborn <georgeholborn5@gmail.com>
2020-07-21 15:40:26 +0100

    rubocop correction

--- app/controllers/candidate_interface/application_form_controller.rb
+++ app/controllers/candidate_interface/application_form_controller.rb
@@ -29,6 +29,7 @@ module CandidateInterface
 
     def edit
       redirect_to candidate_interface_application_complete_path and return unless current_application.can_edit_after_submission?
+
       @application_form = current_application
       render :edit_by_support
     end
