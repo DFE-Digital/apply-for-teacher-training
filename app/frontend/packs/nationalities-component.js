43aa0e063114ad9b29a31f7662196257345d7007
David Gisbey <davidgisbey@gmail.com>
2020-07-22 15:06:12 +0100

    Add JavaScript to the personal details nationalities page
    This should do a few things. It should have an add
    nationalities button. When clicked this should display
    an addtional select for candidates to pick an additional
    nationality. This button should not display once 3 selects
    are visible.
    
    It should have a remove link on the second and third select.
    When clicked it should hide the select and clear it's value.

---
+++ app/frontend/packs/nationalities-component.js
@@ -0,0 +1,116 @@
+const nationalitiesComponent = () => {
+  if (
+    document.querySelector(
+      "[for=candidate-interface-nationalities-form-other-nationality2-field-error]"
+    ) ||
+    document.querySelector(
+      "[for=candidate-interface-nationalities-form-other-nationality3-field-error]"
+    )
+  ) {
+    return;
+  }
+
+  addRemoveLink(
+    "candidate-interface-nationalities-form-other-nationality2-field"
+  );
+
+  addRemoveLink(
+    "candidate-interface-nationalities-form-other-nationality3-field"
+  );
+
+  addAddNationalityButton(
+    "#candidate-interface-nationalities-form-other-other-conditional"
+  );
+
+  hideSection(
+    "candidate-interface-nationalities-form-other-nationality2-field"
+  );
+
+  hideSection(
+    "candidate-interface-nationalities-form-other-nationality3-field"
+  );
+
+  function addRemoveLink(selector) {
+    const labelEl = document.querySelector(`[for=${selector}]`);
+    const removeLink = document.createElement("a");
+    removeLink.innerHTML = "Remove";
+    removeLink.classList.add("govuk-link", "remove-link");
+
+    // This has to be a link and not a button as the govuk-link class requires an
+    // href to apply  it's styling
+    removeLink.href = "#";
+    labelEl.appendChild(removeLink);
+
+    removeLink.addEventListener("click", function () {
+      addRemoveLinkEvent(labelEl, selector);
+    });
+  }
+
+  function addAddNationalityButton(parentSelector) {
+    const parent = document.querySelector(parentSelector);
+    const nationalityButton = document.createElement("button");
+    nationalityButton.innerHTML = "Add another nationality";
+    nationalityButton.id = "add-nationality-button";
+    nationalityButton.classList.add("govuk-button", "govuk-button--secondary");
+    parent.appendChild(nationalityButton);
+
+    const secondSelect = document.querySelector(
+      "[id=candidate-interface-nationalities-form-other-nationality2-field]"
+    );
+    const thirdSelect = document.querySelector(
+      "[id=candidate-interface-nationalities-form-other-nationality3-field]"
+    );
+
+    if (secondSelect.value && thirdSelect.value) {
+      nationalityButton.style.display = "none";
+    }
+
+    nationalityButton.addEventListener("click", function () {
+      event.preventDefault();
+      addNationalityEvent(nationalityButton);
+    });
+  }
+
+  function hideSection(selector) {
+    const select = document.getElementById(selector);
+    const labelEl = document.querySelector(`[for=${selector}]`);
+
+    if (select.value === "") {
+      labelEl.parentElement.style.display = "none";
+    }
+  }
+
+  function addRemoveLinkEvent(labelEl, selector) {
+    const addNationalityButton = document.getElementById(
+      "add-nationality-button"
+    );
+
+    addNationalityButton.style.display = "block";
+    labelEl.parentElement.style.display = "none";
+    document.getElementById(selector).value = "";
+  }
+
+  function addNationalityEvent(nationalityButton) {
+    const secondFormLabel = document.querySelector(
+      "[for=candidate-interface-nationalities-form-other-nationality2-field]"
+    );
+    const thirdFormLabel = document.querySelector(
+      "[for=candidate-interface-nationalities-form-other-nationality3-field]"
+    );
+
+    if (secondFormLabel.parentElement.style.display === "none") {
+      secondFormLabel.parentElement.style.display = "block";
+    } else {
+      thirdFormLabel.parentElement.style.display = "block";
+    }
+
+    if (
+      secondFormLabel.parentElement.style.display === "block" &&
+      thirdFormLabel.parentElement.style.display === "block"
+    ) {
+      nationalityButton.style.display = "none";
+    }
+  }
+};
+
+export default nationalitiesComponent;
