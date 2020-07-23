e9d69a07d0ab44bd7aecd96cb3c6eb6822628194
David Gisbey <davidgisbey@gmail.com>
2020-07-23 16:29:18 +0100

    Add event listener for add nationality button

--- app/frontend/packs/nationalities-component.js
+++ app/frontend/packs/nationalities-component.js
@@ -1,48 +1,4 @@
 const nationalitiesComponent = () => {
-  const nationalitySelects = [
-    "candidate-interface-nationalities-form-other-nationality1-field",
-    "candidate-interface-nationalities-form-other-nationality1-field-error",
-    "candidate-interface-nationalities-form-other-nationality2-field",
-    "candidate-interface-nationalities-form-other-nationality2-field-error",
-    "candidate-interface-nationalities-form-other-nationality3-field",
-    "candidate-interface-nationalities-form-other-nationality3-field-error",
-  ];
-
-  const firstNationalitySelect = [
-    "candidate-interface-nationalities-form-other-nationality1-field",
-    "candidate-interface-nationalities-form-other-nationality1-field-error",
-  ];
-
-  const thirdNationalitySelect = [
-    "candidate-interface-nationalities-form-other-nationality3-field",
-    "candidate-interface-nationalities-form-other-nationality3-field-error",
-  ];
-
-  const errorSelects = [
-    "candidate-interface-nationalities-form-other-nationality1-field-error",
-    "candidate-interface-nationalities-form-other-nationality2-field-error",
-    "candidate-interface-nationalities-form-other-nationality3-field-error",
-  ];
-
-  // nationalitySelects.forEach((id) => {
-  //   var selectNationality = document.getElementById(id);
-  //
-  //   if (selectNationality) {
-  //     var form = selectNationality.parentElement.parentElement;
-  //     addRemoveNationalityLink(form);
-  //     addNationalityButton(selectNationality, id);
-  //
-  //     // hide the form group if the select has no value or does not have an error. The first div always shows
-  //     if (
-  //       notOtherNationality1(id) &&
-  //       selectNationality.value === "" &&
-  //       !errorSelects.includes(id)
-  //     ) {
-  //       form.parentElement.style.display = "none";
-  //     }
-  //   }
-  // });
-
   if (
     document.querySelector(
       "[for=candidate-interface-nationalities-form-other-nationality2-field-error]"
@@ -64,6 +20,14 @@ const nationalitiesComponent = () => {
 
   addAddNationalityButton(
     "#candidate-interface-nationalities-form-other-other-conditional"
+  );
+
+  hideSection(
+    "[id=candidate-interface-nationalities-form-other-nationality2-field]"
+  );
+
+  hideSection(
+    "[id=candidate-interface-nationalities-form-other-nationality3-field]"
   );
 
   /*
@@ -99,92 +63,97 @@ const nationalitiesComponent = () => {
     nationalityButton.classList.add(
       "govuk-button",
       "govuk-button--secondary",
-      "govuk-!-margin-top-5",
-      "govuk-!-margin-bottom-2"
     );
     nationalityButton.href = "#";
     parent.appendChild(nationalityButton);
+
+    const secondSelect = document.querySelector("[id=candidate-interface-nationalities-form-other-nationality2-field]")
+    const thirdSelect = document.querySelector("[id=candidate-interface-nationalities-form-other-nationality3-field]")
+
+    if(secondSelect.value && thirdSelect.value) {
+      nationalityButton.style.display = 'none';
+    }
+
+    nationalityButton.addEventListener("click", function () {
+      addNationalityEvent();
+    })
   }
 
-  function addRemoveNationalityLink(form) {
-    var removeNationalityLink = document.createElement("a");
-    removeNationalityLink.innerHTML = "Remove";
-    removeNationalityLink.classList.add("govuk-link");
-    form.before(removeNationalityLink);
+  function hideSection(selector) {
+    const select = document.querySelector(selector)
+    if(select.value === "") {
+      select.parentElement.parentElement.parentElement.style.display = 'none';
+    }
   }
 
-  function addNationalityButton(selectNationality, id) {
-    if (!thirdNationalitySelect.includes(id)) {
-      var button = document.createElement("a");
-      button.innerHTML = "Add another nationality";
-      button.classList.add(
-        "govuk-button",
-        "govuk-button--secondary",
-        "govuk-!-margin-top-5",
-        "govuk-!-margin-bottom-2"
-      );
-      selectNationality.after(button);
+  function addNationalityEvent() {
+    const secondFormLabel = document.querySelector("[for=candidate-interface-nationalities-form-other-nationality2-field]");
+    const thirdFormLabel = document.querySelector("[for=candidate-interface-nationalities-form-other-nationality3-field]");
 
-      // the button shows if there is no value on the next select, with the exception of if there is an error as the section is already rendered
-      var secondError = document.getElementById(
-        "candidate-interface-nationalities-form-other-nationality2-field-error"
-      );
-      var thirdError = document.getElementById(
-        "candidate-interface-nationalities-form-other-nationality3-field-error"
-      );
-      if (
-        !notOtherNationality1(id) &&
-        (getSecondSelect().value !== "" || secondError)
-      ) {
-        button.style.display = "none";
-      } else if (getThirdSelect().value !== "" || thirdError) {
-        button.style.display = "none";
-      }
-      button.addEventListener("click", function () {
-        addNationalityEvent(id, button);
-      });
+    if(secondFormLabel.parentElement.style.display === "none") {
+      secondFormLabel.parentElement.style.display = "block";
+    }
+    else
+    {
+      thirdFormLabel.parentElement.style.display = "block";
     }
   }
-  function notOtherNationality1(id) {
-    if (firstNationalitySelect.includes(id)) {
-      return false;
-    } else {
-      return true;
-    }
-  }
-  function getSecondSelect() {
-    var select = document.getElementById(
-      "candidate-interface-nationalities-form-other-nationality2-field"
-    );
-    if (!select) {
-      select = document.getElementById(
-        "candidate-interface-nationalities-form-other-nationality2-field-error"
-      );
-    }
-    return select;
-  }
-  function getThirdSelect() {
-    var select = document.getElementById(
-      "candidate-interface-nationalities-form-other-nationality3-field"
-    );
-    if (!select) {
-      select = document.getElementById(
-        "candidate-interface-nationalities-form-other-nationality3-field-error"
-      );
-    }
-    return select;
-  }
-  function addNationalityEvent(id, addNationalityButton) {
-    if (!notOtherNationality1(id)) {
-      var secondSelect = getSecondSelect();
-      secondSelect.parentElement.parentElement.parentElement.style.display =
-        "block";
-    } else {
-      var thirdSelect = getThirdSelect();
-      thirdSelect.parentElement.parentElement.parentElement.style.display =
-        "block";
-    }
-    addNationalityButton.style.display = "none";
-  }
+
+
+  // function addNationalityButton(selectNationality, id) {
+  //     if (
+  //       !notOtherNationality1(id) &&
+  //       (getSecondSelect().value !== "" || secondError)
+  //     ) {
+  //       button.style.display = "none";
+  //     } else if (getThirdSelect().value !== "" || thirdError) {
+  //       button.style.display = "none";
+  //     }
+  //     button.addEventListener("click", function () {
+  //       addNationalityEvent(id, button);
+  //     });
+  //   }
+  // }
+  // function notOtherNationality1(id) {
+  //   if (firstNationalitySelect.includes(id)) {
+  //     return false;
+  //   } else {
+  //     return true;
+  //   }
+  // }
+  // function getSecondSelect() {
+  //   var select = document.getElementById(
+  //     "candidate-interface-nationalities-form-other-nationality2-field"
+  //   );
+  //   if (!select) {
+  //     select = document.getElementById(
+  //       "candidate-interface-nationalities-form-other-nationality2-field-error"
+  //     );
+  //   }
+  //   return select;
+  // }
+  // function getThirdSelect() {
+  //   var select = document.getElementById(
+  //     "candidate-interface-nationalities-form-other-nationality3-field"
+  //   );
+  //   if (!select) {
+  //     select = document.getElementById(
+  //       "candidate-interface-nationalities-form-other-nationality3-field-error"
+  //     );
+  //   }
+  //   return select;
+  // }
+  // function addNationalityEvent(id, addNationalityButton) {
+  //   if (!notOtherNationality1(id)) {
+  //     var secondSelect = getSecondSelect();
+  //     secondSelect.parentElement.parentElement.parentElement.style.display =
+  //       "block";
+  //   } else {
+  //     var thirdSelect = getThirdSelect();
+  //     thirdSelect.parentElement.parentElement.parentElement.style.display =
+  //       "block";
+  //   }
+  //   addNationalityButton.style.display = "none";
+  // }
 };
 export default nationalitiesComponent;
