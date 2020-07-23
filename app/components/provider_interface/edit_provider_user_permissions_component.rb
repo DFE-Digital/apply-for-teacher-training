06598f6f5e60ae64c964601cab889582aa4b47be
Aga Dufrat <aga.dufrat@digital.education.gov.uk>
2020-07-23 09:53:22 +0100

    Add provider-provider permissions to edit view

--- app/components/provider_interface/edit_provider_user_permissions_component.rb
+++ app/components/provider_interface/edit_provider_user_permissions_component.rb
@@ -7,5 +7,27 @@ module ProviderInterface
     def initialize(form:)
       @permissions_form = form
     end
+
+    def training_providers_that_can(permission)
+      permissions_as_ratifying_provider.map do |permission_relationship|
+        permission_relationship.training_provider if permission_relationship.send("training_provider_can_#{permission}?")
+      end
+    end
+
+    def ratifying_providers_that_can(permission)
+      permissions_as_training_provider.map do |permission_relationship|
+        permission_relationship.ratifying_provider if permission_relationship.send("ratifying_provider_can_#{permission}?")
+      end
+    end
+
+  private
+
+    def permissions_as_ratifying_provider
+      @permissions_as_ratifying_provider ||= ProviderRelationshipPermissions.where(ratifying_provider_id: permissions_form.provider.id)
+    end
+
+    def permissions_as_training_provider
+      @permissions_as_training_provider ||= ProviderRelationshipPermissions.where(training_provider_id: permissions_form.provider.id)
+    end
   end
 end
