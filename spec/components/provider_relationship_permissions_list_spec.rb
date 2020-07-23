d8cfdf3b32dd7850eb58a510d2eea215a5ebe35c
Steve Laing <steve.laing@gmail.com>
2020-07-23 11:29:42 +0100

    Update content when a provider can only view applications
    We will be validating that at least one of the two providers in a relationship
    will be authorised for each permission. If a provider has no authorisation for
    any permission we display this as a sentence as there would only ever be one
    provider for this case.
    
    There is a case where the permissions have not yet been set up, in this
    case we render a specific message explaining that the training provider
    should set up the permissions.

--- spec/components/provider_relationship_permissions_list_spec.rb
+++ spec/components/provider_relationship_permissions_list_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe ProviderRelationshipPermissionsList do
   let(:training_provider) { create(:provider) }
-  let(:ratifiying_provider) { create(:provider) }
+  let(:ratifying_provider) { create(:provider) }
   let(:provider_relationship_permissions) do
     create(:provider_relationship_permissions,
            training_provider: training_provider,
-           ratifying_provider: ratifiying_provider,
+           ratifying_provider: ratifying_provider,
            training_provider_can_make_decisions: true,
            training_provider_can_view_safeguarding_information: true)
   end
@@ -26,7 +26,7 @@ RSpec.describe ProviderRelationshipPermissionsList do
 
     expect(result.css('.govuk-body').first.text).to include('The following organisation(s) can make decisions:')
     expect(result.css('.govuk-list').first.text).to include(training_provider.name.to_s)
-    expect(result.css('.govuk-list').first.text).not_to include(ratifiying_provider.name.to_s)
+    expect(result.css('.govuk-list').first.text).not_to include(ratifying_provider.name.to_s)
   end
 
   it 'renders organisations who can view safeguarding information' do
@@ -34,14 +34,31 @@ RSpec.describe ProviderRelationshipPermissionsList do
 
     expect(result.css('.govuk-body')[1].text).to include('The following organisation(s) can see safeguarding information:')
     expect(result.css('.govuk-list')[1].text).to include(training_provider.name.to_s)
-    expect(result.css('.govuk-list')[1].text).not_to include(ratifiying_provider.name.to_s)
+    expect(result.css('.govuk-list')[1].text).not_to include(ratifying_provider.name.to_s)
   end
 
   it 'renders organisations who can only view applications' do
     result = render_inline(described_class.new(provider_relationship_permissions))
 
-    expect(result.css('.govuk-body')[2].text).to include('The following organisation(s) can only view applications:')
-    expect(result.css('.govuk-list')[2].text).not_to include(training_provider.name.to_s)
-    expect(result.css('.govuk-list')[2].text).to include(ratifiying_provider.name.to_s)
+    expect(result.css('.govuk-body')[2].text).to include("#{ratifying_provider.name} can only view applications.")
+  end
+
+  context 'when the permissions for a provider relationship have not been set up' do
+    let(:provider_relationship_permissions) do
+      create(
+        :provider_relationship_permissions,
+        training_provider: training_provider,
+        ratifying_provider: ratifying_provider,
+        training_provider_can_make_decisions: false,
+        training_provider_can_view_safeguarding_information: false,
+        setup_at: nil,
+      )
+    end
+
+    it 'renders a message about setup' do
+      result = render_inline(described_class.new(provider_relationship_permissions))
+
+      expect(result.css('.govuk-body')[0].text).to include("#{training_provider.name} has not set up permissions for this partnership yet.")
+    end
   end
 end
