afec1e6633c199ed5a007a19746e1f331f91b0a2
Malcolm Baig <malcolm.baig@gmail.com>
2020-07-28 12:06:30 +0100

    Change field check in LanguagesSectionPolicy
    Check the `english_main_language` database field directly, rather than
    using the overridden method.

--- app/services/languages_section_policy.rb
+++ app/services/languages_section_policy.rb
@@ -1,6 +1,6 @@
 class LanguagesSectionPolicy
   def self.hide?(application_form)
     FeatureFlag.active?(:efl_section) &&
-      application_form.english_main_language.nil?
+      application_form[:english_main_language].nil?
   end
 end
