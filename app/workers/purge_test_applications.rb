35c72e9c09fed2e6c009443b47e3c4c7a1236217
Steve Hook <steve.hook@gmail.com>
2020-07-30 16:33:53 +0100

    Get very basic spec working

--- app/workers/purge_test_applications.rb
+++ app/workers/purge_test_applications.rb
@@ -2,5 +2,14 @@ class PurgeTestApplications
   include Sidekiq::Worker
 
   def perform(*)
+    application_forms_to_purge.find_each do |application_form|
+      application_form.destroy
+    end
+  end
+
+private
+
+  def application_forms_to_purge
+    ApplicationForm.joins(:candidate).where("candidates.email_address ilike '%@example.com'")
   end
 end
