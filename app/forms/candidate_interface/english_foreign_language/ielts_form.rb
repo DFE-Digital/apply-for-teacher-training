e89c1107b96a9d206d316eab9f9bb5a38cf2dca9
Malcolm Baig <malcolm.baig@gmail.com>
2020-08-03 12:04:25 +0100

    Fix several bugs related to IELTS band score
    - Update the autocomplete Javascript to correctly enhance the select box
      when the form field is in an error state.
    - Add an empty select option so that the band score field is empty on a
      blank form. This allows all values to be visible when the candidate
      clicks on the enhanced select box.
    - Add validation that checks the submitted value is in the list of
      possible band scores. Not ordinarily needed, but useful in cases where
      the form HTML has been modified by the client.
    - Remove unnecessary length validation - no longer needed if we're
      validating that the submitted score is in the list of accepted values.

--- app/forms/candidate_interface/english_foreign_language/ielts_form.rb
+++ app/forms/candidate_interface/english_foreign_language/ielts_form.rb
@@ -9,12 +9,15 @@ module CandidateInterface
       attr_accessor :trf_number, :band_score, :award_year, :application_form
 
       validates :trf_number, presence: true, length: { maximum: 255 }
-      validates :band_score, presence: true, length: { maximum: 255 }
+      validates :band_score, presence: true
       validates :award_year, presence: true
       validate :award_year_is_a_valid_year
+      validate :band_score_is_a_valid_score
 
       def self.band_score_drop_down_options
-        IeltsQualification::VALID_SCORES.map { |s| BandScore.new(s, s) }
+        empty_option = [BandScore.new('', '')]
+        scores = IeltsQualification::VALID_SCORES.map { |s| BandScore.new(s, s) }
+        empty_option + scores
       end
 
       def save
@@ -46,6 +49,12 @@ module CandidateInterface
       def award_year_is_a_valid_year
         if !valid_year?(award_year)
           errors.add(:award_year, :invalid)
+        end
+      end
+
+      def band_score_is_a_valid_score
+        unless band_score.in? IeltsQualification::VALID_SCORES
+          errors.add(:band_score, :invalid)
         end
       end
 
