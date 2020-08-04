2aa7e11610ff6a856cd8542d023e4a66f92ca4df
Steve Laing <steve.laing@gmail.com>
2020-08-04 16:11:52 +0100

    Integrate wizard permissions validation with setup controller

--- app/controllers/provider_interface/provider_relationship_permissions_setup_controller.rb
+++ app/controllers/provider_interface/provider_relationship_permissions_setup_controller.rb
@@ -2,37 +2,57 @@ module ProviderInterface
   class ProviderRelationshipPermissionsSetupController < ProviderInterfaceController
     before_action :require_feature_flag!
     before_action :require_manage_organisations_permission!
+    before_action :require_access_to_manage_provider_relationship_permissions!, only: %i[setup_permissions save_permissions]
 
     def organisations
       @grouped_provider_names_from_relationships = grouped_provider_names_from_relationships
-      @wizard = wizard_for(current_step: 'provider_relationships')
+      @wizard = wizard_for(provider_relationships_attrs.merge(current_step: 'provider_relationships'))
       @wizard.save_state!
     end
 
     def info
+      @permissions_model = provider_relationship_permissions_needing_setup.first
       @wizard = wizard_for(current_step: 'info')
       @wizard.save_state!
     end
 
     def setup_permissions
-      @permissions_model = provider_relationship_permissions_needing_setup.first
+      @permissions_model = ProviderRelationshipPermissions.find(params[:id])
       @wizard = wizard_for(current_step: 'permissions')
       @wizard.save_state!
     end
 
     def save_permissions
-      @wizard = wizard_for(current_step: 'permissions')
-      @wizard.save_state!
+      @wizard = wizard_for(permissions_params.merge(current_step: 'permissions'))
+
+      if @wizard.valid?(:permissions)
+        @wizard.save_state!
+
+        next_step, id = @wizard.next_step
+
+        if next_step == :permissions
+          redirect_to provider_interface_setup_provider_relationship_permissions_path(id)
+        else
+          redirect_to provider_interface_check_provider_relationship_permissions_path
+        end
+      else
+        @permissions_model = ProviderRelationshipPermissions.find(params[:id])
+        render :setup_permissions
+      end
     end
 
     def check
       @wizard = wizard_for(current_step: 'check')
+      @permissions_models = ProviderRelationshipPermissions
+        .includes(:training_provider, :ratifying_provider)
+        .where(id: @wizard.provider_relationships)
       @wizard.save_state!
     end
 
     def commit
       @wizard = wizard_for({})
-      @wizard.save_state!
+      # TODO: Save permissions
+      @wizard.clear_state!
     end
 
   private
@@ -47,8 +67,13 @@ module ProviderInterface
 
     def provider_relationship_permissions_needing_setup
       current_provider_user.authorisation
-        .training_providers_that_actor_can_manage_organisations_for
+        .training_provider_relationships_that_actor_can_manage_organisations_for
         .where(setup_at: nil)
+        .order(created_at: :asc)
+    end
+
+    def provider_relationships_attrs
+      { provider_relationships: provider_relationship_permissions_needing_setup.pluck(:id) }
     end
 
     def wizard_for(options)
@@ -62,6 +87,21 @@ module ProviderInterface
 
     def require_manage_organisations_permission!
       render_404 unless current_provider_user.authorisation.can_manage_organisations_for_at_least_one_provider?
+    end
+
+    def require_access_to_manage_provider_relationship_permissions!
+      provider_relationship_permissions = ProviderRelationshipPermissions.find(params[:id])
+      permitted_relationship_permissions = current_provider_user.authorisation.training_provider_relationships_that_actor_can_manage_organisations_for
+
+      render_403 unless permitted_relationship_permissions.include?(provider_relationship_permissions)
+    end
+
+    def permissions_params
+      return {} unless params.key?(:provider_interface_provider_relationship_permissions_setup_wizard)
+
+      params.require(:provider_interface_provider_relationship_permissions_setup_wizard)
+        .permit(make_decisions: [], view_safeguarding_information: []).to_h
+        .merge(current_provider_relationship_id: params[:id])
     end
   end
 end
