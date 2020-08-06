4d19da4e7a0c963df9833f3b8d5b28a72e913493
Malcolm Baig <malcolm.baig@gmail.com>
2020-08-06 16:14:04 +0100

    Add deadline banners for Apply 1 and Apply 2 applications
    Show a banner on unsubmitted applications indicating the deadline for
    submission. This shows the deadline for either Apply 1 or Apply 2
    depending on the phase of the application being completed.

---
+++ app/components/candidate_interface/deadline_banner_component.rb
@@ -0,0 +1,47 @@
+class CandidateInterface::DeadlineBannerComponent < ViewComponent::Base
+  attr_accessor :phase, :flash_empty
+
+  def initialize(phase:, flash_empty:)
+    @phase = phase
+    @flash_empty = flash_empty
+  end
+
+  def show_deadline_banner?
+    flash_empty &&
+      (show_apply_1_deadline_banner? || show_apply_2_deadline_banner?)
+  end
+
+  def deadline
+    apply_1? ? apply_1_deadline : apply_2_deadline
+  end
+
+private
+
+  def show_apply_1_deadline_banner?
+    apply_1? &&
+      EndOfCycleTimetable.show_apply_1_deadline_banner? &&
+      FeatureFlag.active?(:deadline_notices)
+  end
+
+  def show_apply_2_deadline_banner?
+    apply_2? &&
+      EndOfCycleTimetable.show_apply_2_deadline_banner? &&
+      FeatureFlag.active?(:deadline_notices)
+  end
+
+  def apply_1?
+    phase == 'apply_1'
+  end
+
+  def apply_2?
+    phase == 'apply_2'
+  end
+
+  def apply_1_deadline
+    EndOfCycleTimetable.date(:apply_1_deadline).strftime('%d %B')
+  end
+
+  def apply_2_deadline
+    EndOfCycleTimetable.date(:apply_2_deadline).strftime('%d %B')
+  end
+end
---
+++ spec/components/candidate_interface/deadline_banner_component_spec.rb
@@ -0,0 +1,69 @@
+require 'rails_helper'
+
+RSpec.describe CandidateInterface::DeadlineBannerComponent, type: :component do
+  describe '#render' do
+    let(:application_form) { build(:application_form) }
+    let(:flash) { double }
+
+    def set_conditions_for_rendering_banner(phase)
+      application_form.phase = phase
+      FeatureFlag.activate(:deadline_notices)
+      allow(flash).to receive(:empty?).and_return true
+      allow(EndOfCycleTimetable).to receive(:show_apply_1_deadline_banner?).and_return(true)
+      allow(EndOfCycleTimetable).to receive(:show_apply_2_deadline_banner?).and_return(true)
+    end
+
+    it 'renders the Apply 1 banner when the right conditions are met' do
+      set_conditions_for_rendering_banner('apply_1')
+
+      result = render_inline(described_class.new(phase: application_form.phase, flash_empty: flash.empty?))
+
+      expect(result.text).to include('The deadline for applying to courses starting this academic year is 24 August')
+    end
+
+    it 'renders the Apply 2 banner when the right conditions are met' do
+      set_conditions_for_rendering_banner('apply_2')
+
+      result = render_inline(described_class.new(phase: application_form.phase, flash_empty: flash.empty?))
+
+      expect(result.text).to include('The deadline for applying to courses starting this academic year is 18 September')
+    end
+
+    it 'renders nothing if feature flag is inactive' do
+      set_conditions_for_rendering_banner('apply_1')
+      FeatureFlag.deactivate(:deadline_notices)
+
+      result = render_inline(described_class.new(phase: application_form.phase, flash_empty: flash.empty?))
+
+      expect(result.text).to be_blank
+    end
+
+    it 'renders nothing if the flash contains something' do
+      set_conditions_for_rendering_banner('apply_1')
+      allow(flash).to receive(:empty?).and_return false
+
+      result = render_inline(described_class.new(phase: application_form.phase, flash_empty: flash.empty?))
+
+      expect(result.text).to be_blank
+    end
+
+    it 'renders nothing if it\'s not the right time to show the banner' do
+      set_conditions_for_rendering_banner('apply_1')
+      allow(EndOfCycleTimetable).to receive(:show_apply_1_deadline_banner?).and_return(false)
+      allow(EndOfCycleTimetable).to receive(:show_apply_2_deadline_banner?).and_return(false)
+
+      result = render_inline(described_class.new(phase: application_form.phase, flash_empty: flash.empty?))
+
+      expect(result.text).to be_blank
+    end
+
+    it 'renders a banner if the timetable says only one of them should be shown' do
+      set_conditions_for_rendering_banner('apply_2')
+      allow(EndOfCycleTimetable).to receive(:show_apply_1_deadline_banner?).and_return(false)
+
+      result = render_inline(described_class.new(phase: application_form.phase, flash_empty: flash.empty?))
+
+      expect(result.text).to include('The deadline for applying to courses starting this academic year is 18 September')
+    end
+  end
+end
