21f083ac4d1c24a6dc0e3a307ed077ef1e8fbfaf
David Gisbey <davidgisbey@gmail.com>
2020-08-06 12:44:56 +0100

    Add service for end of cycle dates

--- app/services/end_of_cycle_timetable.rb
+++ app/services/end_of_cycle_timetable.rb
@@ -2,6 +2,8 @@ class EndOfCycleTimetable
   DATES = {
     apply_1_deadline: Date.new(2020, 8, 24),
     apply_2_deadline: Date.new(2020, 9, 18),
+    find_closes: Date.new(2020, 9, 19),
+    find_reopens: Date.new(2020, 10, 3),
   }.freeze
 
   def self.show_apply_1_deadline_banner?
@@ -10,6 +12,18 @@ class EndOfCycleTimetable
 
   def self.show_apply_2_deadline_banner?
     Time.zone.now < date(:apply_2_deadline)
+  end
+
+  def self.find_down?
+    Time.zone.today.between?(find_closes, find_reopens)
+  end
+
+  def self.find_closes
+    date(:find_closes)
+  end
+
+  def self.find_reopens
+    date(:find_reopens)
   end
 
   def self.date(name)
