093808352ea0a5033328a90f6ee4eb7d5bbeda7f
Malcolm Baig <malcolm.baig@gmail.com>
2020-08-12 11:34:37 +0100

    Expire magic links after single use
    Magic links currently last for one hour, with no limit on their use
    during that time. A recent pentest recommended that we make magic links
    single-use.
    
    - Change magic links so that they point to a sign in confirmation page.
    - When the user clicks 'Continue' from this page, expire their token and
      sign them in.
    - Amend the copy on the expired token page to reflect the new behaviour.

--- app/controllers/candidate_interface/sign_in_controller.rb
+++ app/controllers/candidate_interface/sign_in_controller.rb
@@ -1,7 +1,7 @@
 module CandidateInterface
   class SignInController < CandidateInterfaceController
     skip_before_action :authenticate_candidate!
-    before_action :redirect_to_application_if_signed_in, except: %i[authenticate]
+    before_action :redirect_to_application_if_signed_in, except: %i[confirm_authentication authenticate]
 
     def new
       candidate = Candidate.new
@@ -12,23 +12,27 @@ module CandidateInterface
       SignInCandidate.new(candidate_params[:email_address], self).call
     end
 
+    def confirm_authentication
+      candidate = retrieve_candidate_by_token || retrieve_candidate_by_user_id
+      redirect_to action: :new if candidate.nil?
+    end
+
     def authenticate
-      candidate = FindCandidateByToken.call(raw_token: params[:token])
+      candidate = retrieve_candidate_by_token
       token_not_expired = FindCandidateByToken.token_not_expired?(candidate)
 
-      if candidate.nil? && params[:u]
-        candidate_id = Encryptor.decrypt(params[:u])
-        candidate = Candidate.find(candidate_id) if candidate_id
+      if candidate.nil?
+        candidate = retrieve_candidate_by_user_id
       end
 
-      if candidate.nil?
-        redirect_to action: :new
-      elsif token_not_expired
+      redirect_to(action: :new) and return if candidate.nil?
+
+      if token_not_expired
         flash[:success] = t('apply_from_find.account_created_message') if candidate.last_signed_in_at.nil?
         sign_in(candidate, scope: :candidate)
         add_identity_to_log candidate.id
         candidate.update!(last_signed_in_at: Time.zone.now)
-
+        candidate.expire_magic_link_token!
         redirect_to candidate_interface_interstitial_path
       else
         encrypted_candidate_id = Encryptor.encrypt(candidate.id)
@@ -59,6 +63,15 @@ module CandidateInterface
 
     def candidate_params
       params.require(:candidate).permit(:email_address)
+    end
+
+    def retrieve_candidate_by_token
+      FindCandidateByToken.call(raw_token: params[:token])
+    end
+
+    def retrieve_candidate_by_user_id
+      candidate_id = Encryptor.decrypt(params[:u])
+      Candidate.find(candidate_id) if candidate_id
     end
   end
 end
