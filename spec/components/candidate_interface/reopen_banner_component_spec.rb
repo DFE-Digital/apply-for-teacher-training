509d33e28b89b2b01a9ff5037bac728ae9b95a44
Steve Hook <steve.hook@gmail.com>
2020-08-12 12:04:40 +0100

    Appease Rubocop

--- spec/components/candidate_interface/reopen_banner_component_spec.rb
+++ spec/components/candidate_interface/reopen_banner_component_spec.rb
@@ -16,10 +16,12 @@ RSpec.describe CandidateInterface::ReopenBannerComponent do
     it 'renders the banner for an Apply 1 app' do
       set_conditions_for_rendering_banner('apply_1')
 
-      result = render_inline(described_class.new(
-        phase: application_form.phase,
-        flash_empty: flash.empty?,
-      ))
+      result = render_inline(
+        described_class.new(
+          phase: application_form.phase,
+          flash_empty: flash.empty?,
+        ),
+      )
 
       expect(result.text).to include('Applications for courses starting this academic year have now closed')
       expect(result.text).to include('Submit your application from 13 October 2020 for courses starting in the next academic year.')
@@ -28,10 +30,12 @@ RSpec.describe CandidateInterface::ReopenBannerComponent do
     it 'renders the banner for an Apply 2 app' do
       set_conditions_for_rendering_banner('apply_2')
 
-      result = render_inline(described_class.new(
-        phase: application_form.phase,
-        flash_empty: flash.empty?,
-      ))
+      result = render_inline(
+        described_class.new(
+          phase: application_form.phase,
+          flash_empty: flash.empty?,
+        ),
+      )
 
       expect(result.text).to include('Applications for courses starting this academic year have now closed')
       expect(result.text).to include('Submit your application from 13 October 2020 for courses starting in the next academic year.')
@@ -41,10 +45,12 @@ RSpec.describe CandidateInterface::ReopenBannerComponent do
       set_conditions_for_rendering_banner('apply_1')
       allow(EndOfCycleTimetable).to receive(:show_apply_1_reopen_banner?).and_return(false)
 
-      result = render_inline(described_class.new(
-        phase: application_form.phase,
-        flash_empty: flash.empty?,
-      ))
+      result = render_inline(
+        described_class.new(
+          phase: application_form.phase,
+          flash_empty: flash.empty?,
+        ),
+      )
 
       expect(result.text).not_to include('Applications for courses starting this academic year have now closed')
     end
