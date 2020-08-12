7a5edbe69acac75c40ffd845a2cf0d57211d7bac
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2020-08-12 17:09:04 +0100

    Spike: Split up CSS and JS assets

---
+++ app/frontend/packs/application-candidate.js
@@ -0,0 +1,33 @@
+require.context("govuk-frontend/govuk/assets");
+
+import { initAll as govUKFrontendInitAll } from "govuk-frontend";
+import initWarnOnUnsavedChanges from "./warn-on-unsaved-changes";
+import initNationalityAutocomplete from "./nationality-autocomplete";
+import initCoursesAutocomplete from "./courses-autocomplete";
+import initCountryAutocomplete from "./country-autocomplete";
+import initProvidersAutocomplete from "./providers-autocomplete";
+import initDegreeGradeAutocomplete from "./degree-grade-autocomplete";
+import initDegreeInstitutionAutocomplete from "./degree-institution-autocomplete";
+import initDegreeInstitutionCountryAutocomplete from "./degree-institution-country-autocomplete";
+import initDegreeSubjectAutocomplete from "./degree-subject-autocomplete";
+import initDegreeTypeAutocomplete from "./degree-type-autocomplete";
+import initGcseGradeAutocomplete from "./gcse-grade-autocomplete";
+import initIeltsBandScoreAutocomplete from "./ielts-band-score-autocomplete";
+import nationalitiesComponent from "./nationalities-component";
+import "accessible-autocomplete/dist/accessible-autocomplete.min.css";
+import "../styles/application-candidate.scss";
+
+govUKFrontendInitAll();
+initNationalityAutocomplete();
+initProvidersAutocomplete();
+initCoursesAutocomplete();
+initCountryAutocomplete();
+initDegreeGradeAutocomplete();
+initDegreeInstitutionAutocomplete();
+initDegreeInstitutionCountryAutocomplete();
+initDegreeSubjectAutocomplete();
+initDegreeTypeAutocomplete();
+initGcseGradeAutocomplete();
+initIeltsBandScoreAutocomplete();
+initWarnOnUnsavedChanges();
+nationalitiesComponent();
