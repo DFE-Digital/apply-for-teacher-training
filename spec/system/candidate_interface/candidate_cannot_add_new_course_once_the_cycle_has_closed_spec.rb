a04d1a057bf32a3a4e2daf2deeb6657d441633dc
David Gisbey <davidgisbey@gmail.com>
2020-08-13 10:44:10 +0100

    Stop the replace course banner showing after the cycle ends

--- spec/system/candidate_interface/candidate_cannot_add_new_course_once_the_cycle_has_closed_spec.rb
+++ spec/system/candidate_interface/candidate_cannot_add_new_course_once_the_cycle_has_closed_spec.rb
@@ -54,6 +54,14 @@ RSpec.describe 'Candidate vists their applicatin form after the cycle has ended'
 
     when_i_visit_the_site
     then_there_is_a_link_to_the_course_choices_section
+
+    given_it_is_before_before_the_apply_1_deadline
+    and_i_have_submitted_my_application
+    and_the_apply_1_deadline_passes
+    and_one_of_my_courses_has_become_full
+
+    when_i_arrive_at_my_application_dashboard
+    then_i_see_do_not_see_the_banner_to_replace_my_course_choice
   end
 
   def given_i_am_signed_in
@@ -82,6 +90,10 @@ RSpec.describe 'Candidate vists their applicatin form after the cycle has ended'
 
   def and_there_is_not_a_link_to_the_course_choices_section
     expect(page).not_to have_link('Course choices')
+  end
+
+  def when_i_click_check_and_submit_my_application
+    click_link 'Check and submit your application'
   end
 
   def when_i_try_to_visit_the_pick_provider_page
@@ -114,5 +126,35 @@ RSpec.describe 'Candidate vists their applicatin form after the cycle has ended'
 
   def given_it_is_the_day_after_the_apply2_deadline
     Timecop.travel(EndOfCycleTimetable.apply_2_deadline + 1.day)
+  end
+
+  def given_it_is_the_day_before_the_apply_2_deadline
+    and_it_is_the_day_before_the_apply_2_deadline
+  end
+
+  def given_it_is_before_before_the_apply_1_deadline
+    Timecop.travel(EndOfCycleTimetable.apply_1_deadline)
+  end
+
+  def and_i_have_submitted_my_application
+    current_candidate.current_application.destroy!
+    candidate_completes_application_form
+    candidate_submits_application
+  end
+
+  def and_the_apply_1_deadline_passes
+    given_it_is_the_day_after_the_apply1_deadline
+  end
+
+  def and_one_of_my_courses_has_become_full
+    current_candidate.current_application.application_choices.first.course_option.no_vacancies!
+  end
+
+  def when_i_arrive_at_my_application_dashboard
+    visit candidate_interface_application_complete_path
+  end
+
+  def then_i_see_do_not_see_the_banner_to_replace_my_course_choice
+    expect(page).not_to have_content 'One of your choices is not available anymore.'
   end
 end
