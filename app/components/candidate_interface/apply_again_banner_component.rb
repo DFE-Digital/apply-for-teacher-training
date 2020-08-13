97a891f9d839d6adfd4a8ea1b420499ae3951d56
George Holborn <georgeholborn5@gmail.com>
2020-08-13 16:29:54 +0100

    Add unit tests for apply again banner and system spec. All passing.

--- app/components/candidate_interface/apply_again_banner_component.rb
+++ app/components/candidate_interface/apply_again_banner_component.rb
@@ -12,11 +12,7 @@ module CandidateInterface
     end
 
     def render?
-      if @application_form.apply_1?
-        !EndOfCycleTimetable.show_apply_1_reopen_banner?
-      else
         !EndOfCycleTimetable.show_apply_2_reopen_banner?
-      end
     end
   end
 end
--- spec/system/candidate_interface/candidate_views_unsuccessful_application_between_cycles_spec.rb
+++ spec/system/candidate_interface/candidate_views_unsuccessful_application_between_cycles_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature 'Candidate with unsuccessful application' do
   include CandidateHelper
 
   around do |example|
-    Timecop.freeze(Time.zone.local(2020, 8, 25, 8, 56, 0)) do
+    Timecop.freeze(Time.zone.local(2020, 9, 25, 8, 56, 0)) do
       example.run
     end
   end
@@ -14,6 +14,7 @@ RSpec.feature 'Candidate with unsuccessful application' do
     and_i_have_an_unsuccessful_application
     and_i_visit_the_application_dashboard
     then_i_do_not_see_an_apply_again_banner
+    and_i_do_see_a_reopen_banner
   end
 
   def given_i_am_signed_in_as_a_candidate
@@ -39,5 +40,9 @@ RSpec.feature 'Candidate with unsuccessful application' do
 
   def then_i_do_not_see_an_apply_again_banner
     expect(page).not_to have_content('Do you want to apply again?')
+  end
+
+  def and_i_do_see_a_reopen_banner
+    expect(page).to have_content('Applications for courses starting this academic year have now closed')
   end
 end
