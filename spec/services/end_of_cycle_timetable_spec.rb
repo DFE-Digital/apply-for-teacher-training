0372b7abcc550b0a3a28966ecfdc1f94fa65752d
Steve Hook <steve.hook@gmail.com>
2020-08-13 09:52:18 +0100

    Apply deadlines are upto end of deadline dates

--- spec/services/end_of_cycle_timetable_spec.rb
+++ spec/services/end_of_cycle_timetable_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 RSpec.describe EndOfCycleTimetable do
   describe '.show_apply_1_deadline_banner?' do
     it 'returns true before the configured date' do
-      Timecop.travel(Date.new(2020, 8, 23)) do
+      Timecop.travel(Time.zone.local(2020, 8, 24, 23, 0, 0)) do
         expect(EndOfCycleTimetable.show_apply_1_deadline_banner?).to be true
       end
     end
 
     it 'returns false after the configured date' do
-      Timecop.travel(Date.new(2020, 8, 24) + 1.hour) do
+      Timecop.travel(Time.zone.local(2020, 8, 25, 1, 0, 0)) do
         expect(EndOfCycleTimetable.show_apply_1_deadline_banner?).to be false
       end
     end
@@ -17,13 +17,13 @@ RSpec.describe EndOfCycleTimetable do
 
   describe '.show_apply_2_deadline_banner?' do
     it 'returns true before the configured date' do
-      Timecop.travel(Date.new(2020, 9, 17)) do
+      Timecop.travel(Time.zone.local(2020, 9, 18, 23, 0, 0)) do
         expect(EndOfCycleTimetable.show_apply_2_deadline_banner?).to be true
       end
     end
 
     it 'returns false after the configured date' do
-      Timecop.travel(Date.new(2020, 9, 18) + 1.hour) do
+      Timecop.travel(Time.zone.local(2020, 9, 19, 1, 0, 0)) do
         expect(EndOfCycleTimetable.show_apply_2_deadline_banner?).to be false
       end
     end
