3a677186e69ac27a5cc12b2ccd6d9a23b9d8d449
Steve Hook <steve.hook@gmail.com>
2020-08-16 23:39:06 +0100

    Add required copy to new review section

--- spec/system/candidate_interface/candidate_with_unsubmitted_application_between_cycles_spec.rb
+++ spec/system/candidate_interface/candidate_with_unsubmitted_application_between_cycles_spec.rb
@@ -16,6 +16,15 @@ RSpec.feature 'Candidate attempts to submit the application after the end-of-cyc
 
     then_i_can_only_review_my_application
     and_i_cannot_submit_my_application
+
+    when_i_return_after_new_cycle_opens
+    and_i_log_in_again
+    and_i_visit_the_application_form_page
+    then_i_can_see_the_submit_link
+  end
+
+  def and_i_visit_the_application_form_page
+    visit candidate_interface_application_form_path
   end
 
   def given_i_am_signed_in
@@ -36,16 +45,24 @@ RSpec.feature 'Candidate attempts to submit the application after the end-of-cyc
 
   def then_i_can_only_review_my_application
     expect(page).not_to have_link 'Check and submit your application'
+    expect(page).to have_content 'Applications for courses starting in 2021 open from 13 October'
     click_link 'Review your application'
   end
 
-  # def then_i_should_see_all_sections_are_complete
-  #   CandidateHelper::APPLICATION_FORM_SECTIONS.each do |section|
-  #     expect(page).not_to have_selector "[aria-describedby='missing-#{section}']"
-  #   end
-  # end
-
   def and_i_cannot_submit_my_application
     expect(page).not_to have_link 'Continue'
+  end
+
+  def when_i_return_after_new_cycle_opens
+    Timecop.travel(Time.zone.local(2020, 10, 13, 12, 0, 0))
+  end
+
+  def and_i_log_in_again
+    logout
+    create_and_sign_in_candidate
+  end
+
+  def then_i_can_see_the_submit_link
+    expect(page).to have_link 'Check and submit your application'
   end
 end
