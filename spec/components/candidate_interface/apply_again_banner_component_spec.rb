621230f9309bec20ec3cc7035710ce4464bb66c1
Steve Hook <steve.hook@gmail.com>
2020-08-25 01:04:36 +0100

    Add missing spec for ApplyAgainBannerComponent

--- spec/components/candidate_interface/apply_again_banner_component_spec.rb
+++ spec/components/candidate_interface/apply_again_banner_component_spec.rb
@@ -1,48 +1,93 @@
 require 'rails_helper'
 
-RSpec.describe CandidateInterface::CarryOverBannerComponent do
+RSpec.describe CandidateInterface::ApplyAgainBannerComponent do
   let(:application_form) { create(:completed_application_form) }
 
-  context 'when latest application is unsuccessful and in the current recruitment cycle' do
-    it 'renders nothing' do
-      Timecop.freeze(Time.zone.local(2020, 8, 1, 12, 0, 0)) do
-        create(:application_choice, application_form: application_form, status: 'cancelled')
-        result = render_inline(described_class.new(application_form: application_form))
+  context 'when all course choices have been cancelled' do
+    it 'renders component with correct values' do
+      create(:application_choice, application_form: application_form, status: 'cancelled')
+      create(:application_choice, application_form: application_form, status: 'cancelled')
+      result = render_inline(described_class.new(application_form: application_form))
 
-        expect(result.text).to be_blank
-      end
+      expect(result.text).to include('Your application has been withdrawn. Do you want to apply again?')
+      expect(result.css('a')[0].attr('href')).to include(Rails.application.routes.url_helpers.candidate_interface_start_apply_again_path)
+      expect(result.text).not_to include('The deadline when applying again is')
     end
   end
 
-  context 'when latest application is unsuccessful and in the previous recruitment cycle' do
+  context 'when some course choices were not cancelled' do
     it 'renders component with correct values' do
-      Timecop.freeze(Time.zone.local(2020, 8, 1, 12, 0, 0)) do
-        create(:application_choice, application_form: application_form, status: 'cancelled')
-        Timecop.freeze(Time.zone.local(2020, 10, 24, 12, 0, 0)) do
-          result = render_inline(described_class.new(application_form: application_form))
+      create(:application_choice, application_form: application_form, status: 'cancelled')
+      create(:application_choice, :with_rejection, application_form: application_form)
+      result = render_inline(described_class.new(application_form: application_form))
 
-          expect(result.text).to eq 'foo'
-        end
-      end
+      expect(result.text).to include('Do you want to apply again?')
+      expect(result.css('a')[0].attr('href')).to include(Rails.application.routes.url_helpers.candidate_interface_start_apply_again_path)
+      expect(result.text).not_to include('Your application has been withdrawn.')
+      expect(result.text).not_to include('The deadline when applying again is')
+    end
+  end
+
+  context 'when application is for an earlier cycle' do
+    it 'renders component with correct values' do
+      application_choice = create(:application_choice, :with_rejection, application_form: application_form)
+      application_choice.course.update(recruitment_cycle_year: RecruitmentCycle.current_year - 1)
+
+      result = render_inline(described_class.new(application_form: application_form))
+
+      expect(result.text).to include('Do you want to apply again?')
+      expect(result.css('a')[0].attr('href')).to include(Rails.application.routes.url_helpers.candidate_interface_start_carry_over_path)
+      expect(result.text).not_to include('Your application has been withdrawn.')
+      expect(result.text).not_to include('The deadline when applying again is')
     end
   end
 
   describe 'deadline copy' do
+    before do
+      # Set required conditions to display deadline copy
+      FeatureFlag.activate(:deadline_notices)
+      allow(EndOfCycleTimetable).to receive(:show_apply_2_deadline_banner?).and_return true
+    end
+
+    it 'is displayed when conditions are met' do
+      create(:application_choice, application_form: application_form, status: 'cancelled')
+      result = render_inline(described_class.new(application_form: application_form))
+
+      expect(result.text).to include 'The deadline when applying again is'
+    end
+
+    it 'is not displayed when the feature flag is off' do
+      FeatureFlag.deactivate(:deadline_notices)
+
+      create(:application_choice, application_form: application_form, status: 'cancelled')
+      result = render_inline(described_class.new(application_form: application_form))
+
+      expect(result.text).not_to include 'The deadline when applying again is'
+    end
+
+    it 'is not displayed when it\'s not the right time' do
+      allow(EndOfCycleTimetable).to receive(:show_apply_2_deadline_banner?).and_return false
+
+      create(:application_choice, application_form: application_form, status: 'cancelled')
+      result = render_inline(described_class.new(application_form: application_form))
+
+      expect(result.text).not_to include 'The deadline when applying again is'
+    end
   end
 
   describe 'visibility of banner between cycles' do
-    # it 'is rendered during cycle' do
-    #   Timecop.freeze(Time.zone.local(2020, 9, 17, 12, 0, 0)) do
-    #     result = render_inline(described_class.new(application_form: application_form))
-    #     expect(result.text).to include('Do you want to apply again?')
-    #   end
-    # end
+    it 'is rendered' do
+      Timecop.freeze(Time.zone.local(2020, 9, 17, 12, 0, 0)) do
+        result = render_inline(described_class.new(application_form: application_form))
+        expect(result.text).to include('Do you want to apply again?')
+      end
+    end
 
-    # it 'is not rendered between cycles' do
-    #   Timecop.freeze(Time.zone.local(2020, 9, 25, 12, 0, 0)) do
-    #     result = render_inline(described_class.new(application_form: application_form))
-    #     expect(result.text).to eq('')
-    #   end
-    # end
+    it 'is not rendered' do
+      Timecop.freeze(Time.zone.local(2020, 9, 25, 12, 0, 0)) do
+        result = render_inline(described_class.new(application_form: application_form))
+        expect(result.text).to eq('')
+      end
+    end
   end
 end
