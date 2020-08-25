1533f52ba038d92c2ab02b48cef59f6a7ed54a27
Steve Hook <steve.hook@gmail.com>
2020-08-25 14:15:23 +0100

    Use Timecop.travel rather than freeze
    Certain specs depend on time moving forward

--- spec/support/dates.rb
+++ spec/support/dates.rb
@@ -2,7 +2,7 @@ MID_CYCLE_DATE = Time.zone.local(RecruitmentCycle.current_year, 1, 1, 12, 0, 0)
 
 RSpec.configure do |config|
   config.around do |example|
-    Timecop.freeze(MID_CYCLE_DATE) do
+    Timecop.travel(MID_CYCLE_DATE) do
       example.run
     end
   end
