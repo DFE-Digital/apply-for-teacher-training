4d7bdffeda61deeb04c1f296a226035e3cb7e74b
Malcolm Baig <malcolm.baig@gmail.com>
2020-08-26 20:07:45 +0100

    Stop applications to unavailable courses towards the end of the cycle
    - When sending applications to providers, check with the
      EndOfCyclePolicy if we should stop the application.
    - If so, cancel the application and send an explanatory email. Send the
      version of this email that assumes there are no other active choices.

--- app/services/send_applications_to_provider.rb
+++ app/services/send_applications_to_provider.rb
@@ -1,11 +1,25 @@
 class SendApplicationsToProvider
   def call
     GetApplicationFormsReadyToSendToProviders.call.each do |application_form|
-      application_form.application_choices.each do |choice|
-        SendApplicationToProvider.new(application_choice: choice).call
+      application_choices = application_form.application_choices
+      cancelled_choices = []
+
+      application_choices.each do |choice|
+        if CandidateInterface::EndOfCyclePolicy.cancel_application_because_option_unavailable?(choice)
+          ApplicationStateChange.new(choice).cancel!
+          cancelled_choices << choice
+        else
+          SendApplicationToProvider.new(application_choice: choice).call
+        end
       end
 
-      CandidateMailer.application_sent_to_provider(application_form).deliver_later
+      if application_choices.any?(&:awaiting_provider_decision?)
+        CandidateMailer.application_sent_to_provider(application_form).deliver_later
+      else
+        cancelled_choices.each do |choice|
+          CandidateMailer.eoc_choice_unavailable_and_no_other_choices(choice).deliver_later
+        end
+      end
     end
   end
 end
