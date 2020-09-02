dff0fffb7140f65c4fcb7d4f9d3b9f5f705f4767
Tijmen Brommet <tijmen@gmail.com>
2020-09-02 12:25:10 +0100

    Allow navigation to same course in next/previous cycle

--- app/models/course.rb
+++ app/models/course.rb
@@ -110,6 +110,10 @@ class Course < ApplicationRecord
     Course.find_by(recruitment_cycle_year: recruitment_cycle_year - 1, provider_id: provider_id, code: code)
   end
 
+  def in_next_cycle
+    Course.find_by(recruitment_cycle_year: recruitment_cycle_year + 1, provider_id: provider_id, code: code)
+  end
+
   def application_forms
     ApplicationForm
       .includes(:candidate, :application_choices)
