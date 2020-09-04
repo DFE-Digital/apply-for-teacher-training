e62b94cf97e1c50dc526c53bd57afd293eeedbd9
Theodor Vararu <theo@vararu.org>
2020-09-04 14:27:23 +0200

    Add missing spec for RejectAwaitingReferencesCourseChoicesWorker
    Forgot to add this in my previous PR.

--- spec/workers/reject_awaiting_references_course_choices_worker_spec.rb
+++ spec/workers/reject_awaiting_references_course_choices_worker_spec.rb
@@ -2,11 +2,14 @@ require 'rails_helper'
 
 RSpec.describe RejectAwaitingReferencesCourseChoicesWorker do
   describe '#perform' do
-    it 'calls the `GetAwaitingReferencesCourseChoicesForPreviousCycle` service' do
-      allow(CandidateInterface::GetPreviousCyclesAwaitingReferencesApplications).to receive(:call).and_return([])
+    it 'calls the appropriate services' do
+      application_choice = create(:application_choice, :awaiting_references)
+      allow(CandidateInterface::GetPreviousCyclesAwaitingReferencesApplications).to receive(:call).and_return([application_choice.application_form])
+      allow(CandidateMailer).to receive(:application_on_pause).and_call_original
       RejectAwaitingReferencesCourseChoicesWorker.new.perform
 
       expect(CandidateInterface::GetPreviousCyclesAwaitingReferencesApplications).to have_received(:call)
+      expect(CandidateMailer).to have_received(:application_on_pause).with(application_choice.application_form)
     end
   end
 end
