4e8e6ad74bf116fc41f653a8dc8361fb90c72556
Tijmen Brommet <tijmen@gmail.com>
2020-09-07 12:03:33 +0100

    Automatically infer the current cycle

--- spec/models/recruitment_cycle_spec.rb
+++ spec/models/recruitment_cycle_spec.rb
@@ -2,13 +2,16 @@ require 'rails_helper'
 
 RSpec.describe RecruitmentCycle do
   describe '.current_year' do
-    it 'is 2020' do
-      expect(RecruitmentCycle.current_year).to eq(2020)
+    it 'is 2020 before the end of cycle' do
+      Timecop.travel(Time.zone.local(2020, 1, 1, 12, 0, 0)) do
+        expect(RecruitmentCycle.current_year).to eq(2020)
+      end
     end
 
-    it 'can be changed to the next cycle using a feature flag' do
-      FeatureFlag.activate('switch_to_next_recruitment_cycle')
-      expect(RecruitmentCycle.current_year).to eq(2021)
+    it 'is 2021 in the new cycle' do
+      Timecop.travel(Time.zone.local(2020, 11, 1, 12, 0, 0)) do
+        expect(RecruitmentCycle.current_year).to eq(2021)
+      end
     end
   end
 end
