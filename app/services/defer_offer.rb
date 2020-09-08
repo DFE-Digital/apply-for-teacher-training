d8f5407a7ea100565f77f74270c6a86c87d05027
Michael Nacos <m.nacos@gmail.com>
2020-09-08 14:13:16 +0100

    Support the ability to filter 'offer_deferred' applications
    - Add 'offer_deferred' to test data generation and factory bot traits
    - Add offer_deferred_at field to ApplicationChoice
    - Add DeferOffer service

---
+++ app/services/defer_offer.rb
@@ -0,0 +1,23 @@
+class DeferOffer
+  include ActiveModel::Validations
+
+  def initialize(actor:, application_choice:)
+    @auth = ProviderAuthorisation.new(actor: actor)
+    @application_choice = application_choice
+  end
+
+  def save
+    @auth.assert_can_make_decisions!(application_choice: @application_choice, course_option_id: @application_choice.offered_option.id)
+
+    ActiveRecord::Base.transaction do
+      ApplicationStateChange.new(@application_choice).defer_offer!
+      @application_choice.update(offer_deferred_at: Time.zone.now)
+    end
+  rescue Workflow::NoTransitionAllowed
+    errors.add(
+      :base,
+      I18n.t('activerecord.errors.models.application_choice.attributes.status.invalid_transition'),
+    )
+    false
+  end
+end
