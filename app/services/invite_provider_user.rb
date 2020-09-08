751078abf37dc115d22e17970dbb278b8be259c9
Steve Laing <steve.laing@gmail.com>
2020-09-08 14:22:26 +0100

    Link to edit provider user view in the support interface
    There isn't a show page for provider users in the support interface so link to the edit page.

--- app/services/invite_provider_user.rb
+++ app/services/invite_provider_user.rb
@@ -46,7 +46,7 @@ private
   def send_slack_notification
     providers = @provider_user.providers.map(&:name).to_sentence
     message = "#{@provider_user.first_name} has been invited to join #{providers}"
-    url = Rails.application.routes.url_helpers.support_interface_provider_user_url(@provider_user)
+    url = Rails.application.routes.url_helpers.edit_support_interface_provider_user_url(@provider_user)
 
     SlackNotificationWorker.perform_async(message, url)
   end
