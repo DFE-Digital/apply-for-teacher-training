08338f387197b4e5ea83f66b2572bc014d87c99c
Malcolm Baig <malcolm.baig@gmail.com>
2020-09-08 16:17:26 +0100

    Show right to work fields when viewing application in Provider interface

--- spec/components/personal_details_component_spec.rb
+++ spec/components/personal_details_component_spec.rb
@@ -68,4 +68,59 @@ RSpec.describe PersonalDetailsComponent do
 
     expect(result.css('.govuk-summary-list__value').text).to include(international_address)
   end
+
+  it 'does not render right to work fields if nationality is British or Irish' do
+    expect(result.text).not_to include('Has the right to work or study in the UK?')
+    expect(result.text).not_to include('Residency details')
+  end
+
+  context 'a candidate whose nationality is neither British or Irish' do
+    let(:application_form) do
+      build_stubbed(
+        :completed_application_form,
+        first_nationality: 'Pakistani',
+        second_nationality: 'Singaporean',
+        third_nationality: 'Spanish',
+      )
+    end
+
+    it 'renders their right to work or study status' do
+      PersonalDetailsComponent::RIGHT_TO_WORK_OR_STUDY_DISPLAY_VALUES.each do |key, value|
+        application_form.right_to_work_or_study = key
+        result = render_inline(PersonalDetailsComponent.new(application_form: application_form))
+        row_title = result.css('.govuk-summary-list__row')[5].css('dt').text
+        row_value = result.css('.govuk-summary-list__row')[5].css('dd').text
+        expect(row_title).to include 'Has the right to work or study in the UK?'
+        expect(row_value).to include value
+      end
+    end
+
+    context 'the right to work or study status is "yes"' do
+      before do
+        application_form.right_to_work_or_study = 'yes'
+        application_form.right_to_work_or_study_details = 'I have settled status'
+      end
+
+      it 'renders the residency_details_row' do
+        expect(result.css('.govuk-summary-list__key').text).to include('Residency details')
+        expect(result.css('.govuk-summary-list__value').text).to include('I have settled status')
+      end
+    end
+
+    context 'the right to work or study status is "no"' do
+      before { application_form.right_to_work_or_study = 'no' }
+
+      it 'does not render the residency_details_row' do
+        expect(result.css('.govuk-summary-list__key').text).not_to include('Residency details')
+      end
+    end
+
+    context 'the right to work or study status is "decide_later"' do
+      before { application_form.right_to_work_or_study = 'decide_later' }
+
+      it 'does not render the residency_details_row' do
+        expect(result.css('.govuk-summary-list__key').text).not_to include('Residency details')
+      end
+    end
+  end
 end
