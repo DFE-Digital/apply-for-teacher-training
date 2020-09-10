6baf98f4625dfd1639abb5695558776bf2c62920
Malcolm Baig <malcolm.baig@gmail.com>
2020-09-10 16:19:40 +0100

    Add EflQualificationCardComponent
    When viewing an application choice on the Provider interface, we want to
    display details of any EFL qualifications the candidate might hold.
    
    - Add a qualification card component that summarises the detail captured
      by the application's english_proficiency record.
    - Only render the component if an english_proficiency record is actually
      present, and the candidate nationality is not in our list of
      "English-speaking" nationalities.

---
+++ app/components/efl_qualification_card_component.rb
@@ -0,0 +1,32 @@
+class EflQualificationCardComponent < ViewComponent::Base
+  attr_reader :application_form
+
+  def initialize(application_form)
+    @application_form = application_form
+  end
+
+  def render?
+    !application_form.english_speaking_nationality? && english_proficiency.present?
+  end
+
+  def english_proficiency
+    @english_proficiency ||= application_form.english_proficiency
+  end
+
+  def efl_qualification
+    @efl_qualification ||= english_proficiency.efl_qualification
+  end
+
+  def qualification_status
+    if english_proficiency.has_qualification?
+      'Candidate has an English as a foreign language qualification.'
+    elsif english_proficiency.no_qualification?
+      'Candidate does not have an English as a foreign language qualification yet.'
+    else
+      'Candidate said that English is not a foreign language to them.'
+    end
+  end
+
+  delegate :name, :award_year, :grade, :unique_reference_number, to: :efl_qualification
+  delegate :no_qualification_details, to: :english_proficiency
+end
