9128e0bb463dbdf9773afa8d51660ddfb5caaae7
Steve Hook <steve.hook@gmail.com>
2020-09-11 10:34:42 +0100

    Fix spec regressions

--- spec/system/candidate_interface/candidate_cannot_add_new_course_once_the_cycle_has_closed_spec.rb
+++ spec/system/candidate_interface/candidate_cannot_add_new_course_once_the_cycle_has_closed_spec.rb
@@ -42,23 +42,7 @@ RSpec.describe 'Candidate vists their applicatin form after the cycle has ended'
     given_it_is_the_day_after_the_apply2_deadline
 
     when_i_visit_the_site
-    then_i_see_that_i_can_add_new_course_choices_in_october
-    and_there_is_not_a_link_to_the_course_choices_section
-
-    given_the_new_cycle_is_open
-    and_i_logout
-    and_i_am_signed_in
-
-    when_i_visit_the_site
-    then_there_is_a_link_to_the_carry_over_journey
-
-    given_it_is_before_before_the_apply_1_deadline
-    and_i_have_submitted_my_application
-    and_the_apply_1_deadline_passes
-    and_one_of_my_courses_has_become_full
-
-    when_i_arrive_at_my_application_dashboard
-    then_i_see_do_not_see_the_banner_to_replace_my_course_choice
+    then_i_see_that_i_can_carry_over_my_application
   end
 
   def given_i_am_signed_in
@@ -75,11 +59,6 @@ RSpec.describe 'Candidate vists their applicatin form after the cycle has ended'
 
   def then_there_is_a_link_to_the_course_choices_section
     expect(page).to have_link('Course choices')
-  end
-
-  def then_there_is_a_link_to_the_carry_over_journey
-    expect(page).to have_content('Do you want to continue applying?')
-    expect(page).to have_button('Apply again')
   end
 
   def given_it_is_the_day_after_the_apply1_deadline
@@ -130,33 +109,8 @@ RSpec.describe 'Candidate vists their applicatin form after the cycle has ended'
     Timecop.travel(EndOfCycleTimetable.apply_2_deadline + 1.day)
   end
 
-  def given_it_is_the_day_before_the_apply_2_deadline
-    and_it_is_the_day_before_the_apply_2_deadline
-  end
-
-  def given_it_is_before_before_the_apply_1_deadline
-    Timecop.travel(EndOfCycleTimetable.apply_1_deadline)
-  end
-
-  def and_i_have_submitted_my_application
-    current_candidate.current_application.destroy!
-    candidate_completes_application_form
-    candidate_submits_application
-  end
-
-  def and_the_apply_1_deadline_passes
-    given_it_is_the_day_after_the_apply1_deadline
-  end
-
-  def and_one_of_my_courses_has_become_full
-    current_candidate.current_application.application_choices.first.course_option.no_vacancies!
-  end
-
-  def when_i_arrive_at_my_application_dashboard
-    visit candidate_interface_application_complete_path
-  end
-
-  def then_i_see_do_not_see_the_banner_to_replace_my_course_choice
-    expect(page).not_to have_content 'One of your choices is not available anymore.'
+  def then_i_see_that_i_can_carry_over_my_application
+    expect(page).to have_content('Get your application ready to submit from 13 October 2020')
+    expect(page).to have_content('You will be able to choose courses from 3 October 2020 and submit from 13 October 2020.')
   end
 end
