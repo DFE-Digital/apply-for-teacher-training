4c1b5325aee675fd89c7b70c82cd1f6b8ee3e4c2
Malcolm Baig <malcolm.baig@gmail.com>
2020-09-15 21:08:39 +0100

    Change how we display GCSEs on the Manage application choice page
    Introduce a GcseQualificationCardsComponent that renders a set of
    three cards, one each for Maths, English, and Science.
    
    Each qualification card can render in one of four states:
    
    - A 'missing' qualification, ie - the candidate has declared that they
      don't have the qualification.
    - An international qualification
    - A UK qualification, picked from one of the standard types we list
      (GCSE, O Level, and Scottish National 5).
    - An 'other' UK qualification, specified by the candidate.
    
    International cards display a NARIC statement of comparability if the
    candidate has provided a NARIC reference.

---
+++ app/components/gcse_qualification_cards_component.rb
@@ -0,0 +1,47 @@
+class GcseQualificationCardsComponent < ViewComponent::Base
+  attr_reader :application_form
+
+  def initialize(application_form)
+    @application_form = application_form
+  end
+
+  def maths
+    application_form.maths_gcse
+  end
+
+  def english
+    application_form.english_gcse
+  end
+
+  def science
+    application_form.science_gcse
+  end
+
+  def candidate_does_not_have
+    'Candidate does not have this qualification yet'
+  end
+
+  def subject(qualification)
+    qualification.subject.capitalize
+  end
+
+  def institution_country(qualification)
+    COUNTRIES[qualification.institution_country]
+  end
+
+  def presentable_qualification_type(qualification)
+    if qualification.other_uk_qualification_type.present?
+      qualification.other_uk_qualification_type
+    elsif qualification.non_uk_qualification_type.present?
+      qualification.non_uk_qualification_type
+    else
+      I18n.t!("application_form.gcse.qualification_types.#{qualification.qualification_type}")
+    end
+  end
+
+  def naric_statement(qualification)
+    if qualification.naric_reference.present? && qualification.comparable_uk_qualification.present?
+      "NARIC statement #{qualification.naric_reference} says this is comparable to a #{qualification.comparable_uk_qualification}."
+    end
+  end
+end
