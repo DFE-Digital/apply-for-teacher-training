864e55382cb4765c7007af1ce37f9dc358eb42ed
Steve Hook <steve.hook@gmail.com>
2020-09-15 21:25:12 +0100

    Fix ApplicationStatusTagComponent preview
    This started failing on `qa` because the stubbed application choice
    created doesn't include a course or provider and the component makes a
    couple of (reasonable) assumptions about these being present.
    
    This is a compromise solution, I've stubbed out the 'missing' models but
    this requires the code under test to be a bit more explicit. e.g.
    replacing application_choice.course with
    application_choice.course_option.course because FactoryBot.build_stubbed
    doesn't stub implied `through` associations.

--- spec/components/previews/candidate_interface/application_status_tag_component_preview.rb
+++ spec/components/previews/candidate_interface/application_status_tag_component_preview.rb
@@ -2,7 +2,17 @@ module CandidateInterface
   class ApplicationStatusTagComponentPreview < ViewComponent::Preview
     ApplicationStateChange.valid_states.each do |state_name|
       define_method state_name do
-        render CandidateInterface::ApplicationStatusTagComponent.new(application_choice: FactoryBot.build_stubbed(:application_choice, status: state_name))
+        provider = FactoryBot.build_stubbed(:provider)
+        course = FactoryBot.build_stubbed(:course, provider: provider)
+        application_choice = FactoryBot.build_stubbed(
+          :application_choice,
+          status: state_name,
+          course_option: FactoryBot.build_stubbed(
+            :course_option,
+            course: course,
+          ),
+        )
+        render CandidateInterface::ApplicationStatusTagComponent.new(application_choice: application_choice)
       end
     end
   end
