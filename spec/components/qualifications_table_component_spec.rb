3e533f040905fd2004bb6bbba23802d004e5289d
Malcolm Baig <malcolm.baig@gmail.com>
2020-09-18 12:20:51 +0100

    Simplify QualificationTableComponent table header
    This is only used for Other Qualifications so doesn't need a dynamic
    table header. The component will be refactored to remove the type_label
    argument at a later time.

--- spec/components/qualifications_table_component_spec.rb
+++ spec/components/qualifications_table_component_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe QualificationsTableComponent do
   it 'renders nothing when no qualifications present' do
     result = render_inline(described_class.new(qualifications: [], type_label: 'My label'))
 
-    expect(result.to_s).to be_blank
+    expect(result.css('table')).to be_blank
   end
 
   it 'renders a qualifications table' do
@@ -13,6 +13,6 @@ RSpec.describe QualificationsTableComponent do
     ]
     result = render_inline(described_class.new(qualifications: qualifications, type_label: 'My label'))
 
-    expect(result.css('th').text).to include('My label')
+    expect(result.css('table').first['data-qa']).to eq 'qualifications-table-my-label'
   end
 end
