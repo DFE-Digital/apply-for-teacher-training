32fcc44a5cfbe3a78d78144e6895194610101a34
Michael Nacos <m.nacos@gmail.com>
2020-09-22 09:43:47 +0100

    Stop all scheduled background tasks (esp. Sync)

--- config/clock.rb
+++ config/clock.rb
@@ -5,34 +5,4 @@ require 'clockwork'
 
 class Clock
   include Clockwork
-
-  every(15.minutes, 'SyncAllFromFind') { SyncAllFromFind.perform_async }
-  every(1.hour, 'DetectInvariants') { DetectInvariants.perform_async }
-  every(1.hour, 'SendApplicationsToProvider', at: '**:05') { SendApplicationsToProviderWorker.perform_async }
-  every(1.hour, 'RejectApplicationsByDefault', at: '**:10') { RejectApplicationsByDefaultWorker.perform_async }
-  every(1.hour, 'DeclineOffersByDefault', at: '**:15') { DeclineOffersByDefaultWorker.perform_async }
-
-  every(1.hour, 'SendReferenceChaseEmailToBothParties', at: '**:20') { SendReferenceChaseEmailToBothPartiesWorker.perform_async }
-  every(1.hour, 'AskCandidatesForNewReferees', at: '**:25') { AskCandidatesForNewRefereesWorker.perform_async }
-  every(1.hour, 'SendAdditionalReferenceChaseEmailToCandidates', at: '**:30') { SendAdditionalReferenceChaseEmailToBothPartiesWorker.perform_async }
-
-  every(1.hour, 'SendChaseEmailToProviders', at: '**:35') { SendChaseEmailToProvidersWorker.perform_async }
-  every(1.hour, 'SendChaseEmailToCandidates', at: '**:40') { SendChaseEmailToCandidatesWorker.perform_async }
-  every(1.hour, 'SendCourseFullNotifications', at: '**:45') { SendCourseFullNotificationsWorker.perform_async }
-
-  every(1.day, 'UCASMatching::UploadMatchingData', at: '06:23') do
-    if Time.zone.today.weekday?
-      UCASMatching::UploadMatchingData.perform_async
-    end
-  end
-
-  every(1.day, 'UCASMatching::ProcessMatchingData', at: '9:00') do
-    if Time.zone.today.weekday?
-      if HostingEnvironment.qa?
-        UCASMatching::UploadTestFile.new.upload
-      end
-
-      UCASMatching::ProcessMatchingData.perform_async
-    end
-  end
 end
