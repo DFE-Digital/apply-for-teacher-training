e73ce36b32b6466e233b93c1aef9bc6ed5d1e7ac
Aga Dufrat <aga.dufrat@digital.education.gov.uk>
2020-09-23 16:39:09 +0100

    Handle when permissions are empty in save provider user service
    When the user decides not to give any permissions to the new user
    (view applications only) permissions are nil.

--- spec/services/provider_interface/save_provider_user_service_spec.rb
+++ spec/services/provider_interface/save_provider_user_service_spec.rb
@@ -78,4 +78,31 @@ RSpec.describe ProviderInterface::SaveProviderUserService do
     actor.provider_permissions.find_by(provider: @providers[1]).update(manage_users: false)
     expect { described_class.new(actor: actor, wizard: wizard).call! }.to raise_error(ProviderAuthorisation::NotAuthorisedError)
   end
+
+  context 'when no permissions are granted' do
+    it 'invokes a service to persist a new ProviderUser for a new user' do
+      @providers = create_list(:provider, 3)
+      wizard = instance_double(
+        ProviderInterface::ProviderUserInvitationWizard,
+        first_name: 'Ed',
+        last_name: 'Yewcater',
+        email_address: 'ed@example.com',
+        provider_permissions: {
+          @providers[0].id.to_s => { 'provider_id' => @providers[0].id.to_s },
+          @providers[1].id.to_s => { 'provider_id' => @providers[1].id.to_s },
+        },
+      )
+      actor = setup_actor
+      expect { described_class.new(actor: actor, wizard: wizard).call! }.to change { ProviderUser.count }.by(1)
+      new_provider_user = ProviderUser.last
+      expect(new_provider_user.provider_permissions.count).to eq 2
+
+      first_permission = new_provider_user.provider_permissions.find { |permission| permission.provider_id == @providers[0].id }
+      second_permission = new_provider_user.provider_permissions.find { |permission| permission.provider_id == @providers[1].id }
+      expect(first_permission.manage_users).to be false
+      expect(first_permission.make_decisions).to be false
+      expect(second_permission.manage_users).to be false
+      expect(second_permission.make_decisions).to be false
+    end
+  end
 end
