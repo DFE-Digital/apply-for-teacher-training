1775bb60d70eb7f70f64e90935ced44090410de0
Toby Retallick <toby.retallick@gmail.com>
2020-09-24 16:03:16 +0100

    Add definitions for new HESA codes
    - Definitions added for ethnicity, sex and disabilities

---
+++ config/initializers/hesa_disabilities.rb
@@ -0,0 +1,12 @@
+HESA_DISABILITIES = [
+  ['00', 'No known disability'],
+  ['08', 'Multiple disabilities'],
+  ['51', 'A specific learning difficulty such as dyslexia, dyspraxia or AD(H)D'],
+  ['53', "A social/communication impairment such as Asperger's syndrome/other autistic spectrum disorder"],
+  ['54', 'A long standing illness or health condition such as cancer, HIV, diabetes, chronic heart disease, or epilepsy'],
+  ['55', 'A mental health condition, such as depression, schizophrenia or anxiety disorder'],
+  ['56', 'A physical impairment or mobility issues, such as difficulty using arms or using a wheelchair or crutches'],
+  ['57', 'Deaf or a serious hearing impairment'],
+  ['58', 'Blind or a serious visual impairment uncorrected by glasses'],
+  ['96', 'A disability, impairment or medical condition that is not listed above'],
+].freeze
