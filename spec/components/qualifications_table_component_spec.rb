bd0cc535cef5eed3a93337a0222a4d2f38ef9738
Malcolm Baig <malcolm.baig@gmail.com>
2020-09-25 11:03:45 +0100

    Refactor QualificationsTableComponent
    This component was previously used to render a generic table populated
    with qualifications. Most qualifications are now displayed in cards
    (see, for example - `EflQualificationCardComponent`). This table
    component is now only used for rendering the 'other' qualification
    type. It's also in a slightly odd state - it still accepts a
    `type_label` argument while having a hard-coded 'Other qualifications'
    header in its markup.
    
    There are a couple of options to deal with this inconsistency -
    
    - make the component specific to 'other' qualifications, renaming it and
      removing the `type_label` argument
    - leave it as a generic component and use the argument to populate the
      header
    
    This commit opts for the latter. There isn't much of a gain in code clarity
    by making the component more specific, and it can be used to render
    similar tables if required later on.

--- spec/components/qualifications_table_component_spec.rb
+++ spec/components/qualifications_table_component_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe QualificationsTableComponent do
   it 'renders nothing when no qualifications present' do
-    result = render_inline(described_class.new(qualifications: [], type_label: 'My label'))
+    result = render_inline(described_class.new(qualifications: [], header: 'My header'))
 
     expect(result.css('table')).to be_blank
   end
@@ -11,8 +11,8 @@ RSpec.describe QualificationsTableComponent do
     qualifications = [
       build_stubbed(:application_qualification),
     ]
-    result = render_inline(described_class.new(qualifications: qualifications, type_label: 'My label'))
+    result = render_inline(described_class.new(qualifications: qualifications, header: 'My header'))
 
-    expect(result.css('table').first['data-qa']).to eq 'qualifications-table-my-label'
+    expect(result.css('table').first['data-qa']).to eq 'qualifications-table-my-header'
   end
 end
