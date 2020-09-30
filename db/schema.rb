59d3da6c9e1dc689f6fcf1e7aa501e01db92e8b1
David Gisbey <davidgisbey@gmail.com>
2020-09-30 10:11:15 +0100

    Remove not null constraint from the references email address table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_28_124834) do
+ActiveRecord::Schema.define(version: 2020_09_30_091030) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -407,7 +407,7 @@ ActiveRecord::Schema.define(version: 2020_09_28_124834) do
   end
 
   create_table "references", force: :cascade do |t|
-    t.string "email_address", null: false
+    t.string "email_address"
     t.string "feedback"
     t.bigint "application_form_id", null: false
     t.datetime "created_at", precision: 6, null: false
