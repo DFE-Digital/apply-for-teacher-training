effe7f8739fae5d1328def7d4f494617e2ebcc11
Steve Hook <steve.hook@gmail.com>
2020-10-02 00:20:51 +0100

    Add new message to RefereesReviewComponent

--- app/components/candidate_interface/referees_review_component.rb
+++ app/components/candidate_interface/referees_review_component.rb
@@ -26,7 +26,13 @@ module CandidateInterface
     end
 
     def show_missing_banner?
-      !@application_form.references_completed && @editable if @submitting_application
+      !@application_form.references_completed && @editable && @submitting_application
+    end
+
+    def show_references_not_provided?
+      return false unless FeatureFlag.active?(:decoupled_references)
+
+      !@application_form.enough_references_have_been_provided? && @editable && @submitting_application
     end
 
   private
