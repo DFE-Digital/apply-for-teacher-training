58e7e7a1fcd8e91ae776e260bf506518c433bbb9
David Gisbey <davidgisbey@gmail.com>
2020-10-01 13:10:53 +0100

    Add validations for email addresses to the model

--- app/forms/candidate_interface/reference/referee_email_address_form.rb
+++ app/forms/candidate_interface/reference/referee_email_address_form.rb
@@ -2,9 +2,15 @@ module CandidateInterface
   class Reference::RefereeEmailAddressForm
     include ActiveModel::Model
 
-    attr_accessor :email_address
+    attr_accessor :email_address, :application_form_id
 
-    validates :email_address, presence: true
+    validates :email_address, presence: true,
+                              email_address: true,
+                              length: { maximum: 100 }
+
+    validates :application_form_id, presence: true
+
+    validate :email_address_unique?
 
     def self.build_from_reference(reference)
       new(email_address: reference.email_address)
@@ -14,6 +20,16 @@ module CandidateInterface
       return false unless valid?
 
       reference.update!(email_address: email_address)
+    end
+
+  private
+
+    def email_address_unique?
+      application_form = ApplicationForm.find(application_form_id)
+      current_email_addresses = application_form.application_references.map(&:email_address).compact
+      return true if current_email_addresses.blank?
+
+      errors.add(:email_address, :duplicate) if current_email_addresses.map(&:downcase).include?(email_address)
     end
   end
 end
--- spec/forms/candidate_interface/reference/referee_email_address_form_spec.rb
+++ spec/forms/candidate_interface/reference/referee_email_address_form_spec.rb
@@ -1,8 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe CandidateInterface::Reference::RefereeEmailAddressForm, type: :model do
+  before do
+    FeatureFlag.activate('decoupled_references')
+  end
+
   describe 'validations' do
+    let(:form) { subject }
+
+    before do
+      allow(form).to receive(:email_address_unique?).and_return true
+    end
+
     it { is_expected.to validate_presence_of(:email_address) }
+    it { is_expected.to validate_presence_of(:application_form_id) }
+
+    one_hundred_character_email = "#{SecureRandom.hex(44)}@example.com"
+    one_hundred_and_two_character_email = "#{SecureRandom.hex(45)}@example.com"
+
+    it { is_expected.to allow_value(one_hundred_character_email).for(:email_address) }
+    it { is_expected.not_to allow_value(one_hundred_and_two_character_email).for(:email_address) }
+
+    context 'when a duplicate email is given' do
+      it 'is not valid' do
+        application_form = create(:application_form)
+        create(:reference, email_address: 'iamtheone@whoknocks.com', application_form: application_form)
+        application_reference = create(:reference, email_address: nil)
+
+        form = described_class.new(email_address: 'iamtheone@whoknocks.com', application_form_id: application_form.id)
+        expect(form.save(application_reference)).to be(false)
+      end
+    end
   end
 
   describe '.build_from_reference' do
@@ -17,21 +45,9 @@ RSpec.describe CandidateInterface::Reference::RefereeEmailAddressForm, type: :mo
   describe '#save' do
     let(:application_reference) { create(:reference) }
 
-    before do
-      FeatureFlag.activate('decoupled_references')
-    end
-
-    context 'when email_address is blank' do
-      it 'returns false' do
-        form = described_class.new
-
-        expect(form.save(application_reference)).to be(false)
-      end
-    end
-
     context 'when email_address has a value' do
       it 'creates the referee' do
-        form = described_class.new(email_address: 'iamtheone@whoknocks.com')
+        form = described_class.new(email_address: 'iamtheone@whoknocks.com', application_form_id: application_reference.application_form.id)
         form.save(application_reference)
 
         expect(application_reference.email_address).to eq('iamtheone@whoknocks.com')
