22e741ea0818bf121b6e3aa446462bec1ab1529d
David Gisbey <davidgisbey@gmail.com>
2020-10-01 13:11:34 +0100

    Add description controller and view to get the test passing

---
+++ app/controllers/candidate_interface/decoupled_references/description_controller.rb
@@ -0,0 +1,9 @@
+module CandidateInterface
+  module DecoupledReferences
+    class DescriptionController < BaseController
+      before_action :set_reference
+
+      def new; end
+    end
+  end
+end
