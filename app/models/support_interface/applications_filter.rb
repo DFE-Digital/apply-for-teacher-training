e7e620c33da68b6d0b9ebf1e2fd1b67713b87728
Tijmen Brommet <tijmen@gmail.com>
2020-10-06 09:56:17 +0100

    Refactor ApplicationsFilter

--- app/models/support_interface/applications_filter.rb
+++ app/models/support_interface/applications_filter.rb
@@ -25,31 +25,38 @@ module SupportInterface
     end
 
     def filters
-      [
-        {
-          type: :search,
-          heading: 'Name, email or reference',
-          value: applied_filters[:q],
-          name: 'q',
-        },
-        {
-          type: :checkboxes,
-          heading: 'Phase',
-          name: 'phase',
-          options: [
-            {
-              value: 'apply_1',
-              label: 'Apply 1',
-              checked: applied_filters[:phase]&.include?('apply_1'),
-            },
-            {
-              value: 'apply_2',
-              label: 'Apply 2',
-              checked: applied_filters[:phase]&.include?('apply_2'),
-            },
-          ],
-        },
-      ]
+      @filters ||= [search_filter] + [phase_filter]
+    end
+
+private
+
+    def search_filter
+      {
+        type: :search,
+        heading: 'Name, email or reference',
+        value: applied_filters[:q],
+        name: 'q',
+      }
+    end
+
+    def phase_filter
+      {
+        type: :checkboxes,
+        heading: 'Phase',
+        name: 'phase',
+        options: [
+          {
+            value: 'apply_1',
+            label: 'Apply 1',
+            checked: applied_filters[:phase]&.include?('apply_1'),
+          },
+          {
+            value: 'apply_2',
+            label: 'Apply 2',
+            checked: applied_filters[:phase]&.include?('apply_2'),
+          },
+        ],
+      }
     end
   end
 end
