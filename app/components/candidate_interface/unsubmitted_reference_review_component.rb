5cf351220f2afa025a158a8bb7566515f598d97f
David Gisbey <davidgisbey@gmail.com>
2020-10-08 17:07:15 +0100

    Add UnsubmittedReferenceReviewComponent

---
+++ app/components/candidate_interface/unsubmitted_reference_review_component.rb
@@ -0,0 +1,58 @@
+module CandidateInterface
+  class UnsubmittedReferenceReviewComponent < ViewComponent::Base
+    validates :reference, presence: true
+
+    def initialize(reference:)
+      @reference = reference
+    end
+
+    def reference_rows(reference)
+      [
+        name_row(reference),
+        email_address_row(reference),
+        type_row(reference),
+        relationship_row(reference),
+      ]
+    end
+
+  private
+
+    attr_reader :reference
+
+    def name_row(reference)
+      {
+        key: 'Name',
+        value: reference.name,
+        action: "name for #{reference.name}",
+        change_path: candidate_interface_decoupled_references_edit_name_path(reference.id),
+      }
+    end
+
+    def email_address_row(reference)
+      {
+        key: 'Email address',
+        value: reference.email_address,
+        action: "email address for #{reference.name}",
+        change_path: candidate_interface_decoupled_references_edit_email_address_path(reference.id),
+      }
+    end
+
+    def type_row(reference)
+      {
+        key: 'Reference type',
+        value: reference.referee_type ? reference.referee_type.capitalize.dasherize : '',
+        action: "reference type for #{reference.name}",
+        change_path: candidate_interface_decoupled_references_edit_type_path(reference.id),
+      }
+    end
+
+    def relationship_row(reference)
+      {
+        key: 'Relationship to referee',
+        value: reference.relationship,
+        action: "relationship for #{reference.name}",
+        change_path: candidate_interface_decoupled_references_edit_relationship_path(reference.id),
+      }
+    end
+  end
+end
---
+++ spec/components/candidate_interface/unsubmitted_reference_review_component_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+RSpec.describe CandidateInterface::UnsubmittedReferenceReviewComponent do
+  let(:reference) { create(:reference) }
+
+  it 'renders component with correct values for a references name' do
+    result = render_inline(described_class.new(reference: reference))
+
+    expect(result.css('.govuk-summary-list__key')[0].text).to include('Name')
+    expect(result.css('.govuk-summary-list__value')[0].to_html).to include(reference.name)
+    expect(result.css('.govuk-summary-list__actions a')[0].attr('href')).to include(
+      Rails.application.routes.url_helpers.candidate_interface_decoupled_references_edit_name_path(reference.id),
+    )
+    expect(result.css('.govuk-summary-list__actions')[0].text).to include("Change name for #{reference.name}")
+  end
+
+  it 'renders component with correct values for the references email address' do
+    result = render_inline(described_class.new(reference: reference))
+
+    expect(result.css('.govuk-summary-list__key')[1].text).to include('Email address')
+    expect(result.css('.govuk-summary-list__value')[1].to_html).to include(reference.email_address)
+    expect(result.css('.govuk-summary-list__actions a')[1].attr('href')).to include(
+      Rails.application.routes.url_helpers.candidate_interface_decoupled_references_edit_email_address_path(reference.id),
+    )
+    expect(result.css('.govuk-summary-list__actions')[1].text).to include("Change email address for #{reference.name}")
+  end
+
+  it 'renders component with correct values for the references type' do
+    result = render_inline(described_class.new(reference: reference))
+
+    expect(result.css('.govuk-summary-list__key')[2].text).to include('Reference type')
+    expect(result.css('.govuk-summary-list__value')[2].to_html).to include(reference.referee_type.capitalize.dasherize)
+    expect(result.css('.govuk-summary-list__actions a')[2].attr('href')).to include(
+      Rails.application.routes.url_helpers.candidate_interface_decoupled_references_edit_type_path(reference.id),
+    )
+    expect(result.css('.govuk-summary-list__actions')[2].text).to include("Change reference type for #{reference.name}")
+  end
+
+  it 'renders component with correct values for the references relationship' do
+    result = render_inline(described_class.new(reference: reference))
+
+    expect(result.css('.govuk-summary-list__key')[3].text).to include('Relationship to referee')
+    expect(result.css('.govuk-summary-list__value')[3].to_html).to include(reference.relationship)
+    expect(result.css('.govuk-summary-list__actions a')[3].attr('href')).to include(
+      Rails.application.routes.url_helpers.candidate_interface_decoupled_references_edit_relationship_path(reference.id),
+    )
+    expect(result.css('.govuk-summary-list__actions')[3].text).to include("Change relationship for #{reference.name}")
+  end
+end
