c8b91d418fead186fc0b878b16408e45524601b9
Steve Hook <steve.hook@gmail.com>
2020-10-08 14:51:34 +0100

    Don't prompt for name if candidate skips request now

--- app/controllers/candidate_interface/decoupled_references/request_controller.rb
+++ app/controllers/candidate_interface/decoupled_references/request_controller.rb
@@ -19,8 +19,9 @@ module CandidateInterface
     private
 
       def prompt_for_candidate_name_if_not_already_given
-        if @reference.application_form.first_name.blank? ||
-            @reference.application_form.last_name.blank?
+        if request_now? &&
+            (@reference.application_form.first_name.blank? ||
+             @reference.application_form.last_name.blank?)
           redirect_to candidate_interface_decoupled_references_new_candidate_name_path(@reference.id)
         end
       end
