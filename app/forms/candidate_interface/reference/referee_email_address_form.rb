ebd04a6560342d978dd2ba358647830ae57c5c4b
David Gisbey <davidgisbey@gmail.com>
2020-10-08 18:50:05 +0100

    Fix bug with email address validation
    A duplicate email address was throwing when a candidate clicked
    edit on their email address and tried to submit the same address.
    It now removes the current references email address
    from the array of email addresses before checking they are unique.

--- app/forms/candidate_interface/reference/referee_email_address_form.rb
+++ app/forms/candidate_interface/reference/referee_email_address_form.rb
@@ -2,13 +2,13 @@ module CandidateInterface
   class Reference::RefereeEmailAddressForm
     include ActiveModel::Model
 
-    attr_accessor :email_address, :application_form_id
+    attr_accessor :email_address, :reference_id
 
     validates :email_address, presence: true,
                               email_address: true,
                               length: { maximum: 100 }
 
-    validates :application_form_id, presence: true
+    validates :reference_id, presence: true
 
     validate :email_address_unique?
 
@@ -25,8 +25,8 @@ module CandidateInterface
   private
 
     def email_address_unique?
-      application_form = ApplicationForm.find(application_form_id)
-      current_email_addresses = application_form.application_references.map(&:email_address).compact
+      reference = ApplicationReference.find(reference_id)
+      current_email_addresses = (reference.application_form.application_references.map(&:email_address) - [reference.email_address]).compact
       return true if current_email_addresses.blank?
 
       errors.add(:email_address, :duplicate) if current_email_addresses.map(&:downcase).include?(email_address)
