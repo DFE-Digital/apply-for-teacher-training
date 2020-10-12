a364b6d333f6159027ccbb76b47f7f9699c39028
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2020-10-12 17:06:35 +0100

    Support: Refactor provider user views

--- spec/components/support_interface/provider_users_table_component_spec.rb
+++ spec/components/support_interface/provider_users_table_component_spec.rb
@@ -20,25 +20,6 @@ RSpec.describe SupportInterface::ProviderUsersTableComponent do
     it 'renders all the fields' do
       expect(rendered_component).to include('provider@example.com')
       expect(rendered_component).to include('The Provider')
-      expect(rendered_component).to include('1 December 2019 at 10:45am')
-    end
-  end
-
-  context 'when the provider user has never signed in and was created before 24/12/2019' do
-    let(:provider_users) { [create(:provider_user, last_signed_in_at: nil, created_at: Time.zone.local(2019, 12, 23))] }
-
-    it 'shows that we do not know whether they’ve signed in or not (they’ve never signed in, or they signed in before records began on 24/12/2019)' do
-      expect(rendered_component).to include('Unknown (records began 24 December 2019)')
-    end
-  end
-
-  context 'when the provider user has never signed in and was created after 24/12/2019' do
-    let(:provider_users) { [create(:provider_user, last_signed_in_at: nil, created_at: Time.zone.local(2019, 12, 25))] }
-
-    it 'shows that we do not know whether they’ve signed in or not (they’ve never signed in, or they signed in before records began on 24/12/2019)' do
-      pending 'broken due to time mismatch'
-
-      expect(rendered_component).to include('Never signed in')
     end
   end
 
