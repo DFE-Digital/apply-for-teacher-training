51ebd84df37fea2008f8681da05be12f4a6a3e2b
Tijmen Brommet <tijmen@gmail.com>
2020-10-12 13:08:36 +0100

    Add user agent to Find API requests
    This will make it easier for the Find API to know where requests are
    coming from.

--- app/models/find_api/resource.rb
+++ app/models/find_api/resource.rb
@@ -1,5 +1,6 @@
 module FindAPI
   class Resource < JsonApiClient::Resource
     self.site = ENV.fetch('FIND_BASE_URL')
+    self.connection_options = { headers: { user_agent: 'Apply for teacher training' } }
   end
 end
