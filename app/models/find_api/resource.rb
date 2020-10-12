298a19445aa6dc9cc43b0d065c73d317601f3c25
Tijmen Brommet <tijmen@gmail.com>
2020-10-12 13:11:37 +0100

    Add custom logging for API requests
    This will make sure that any HTTP from the JSON API Client are logged,
    with the URL, status and reported runtime. This will allow us to find
    slow requests to Find.

--- app/models/find_api/resource.rb
+++ app/models/find_api/resource.rb
@@ -4,3 +4,24 @@ module FindAPI
     self.connection_options = { headers: { user_agent: 'Apply for teacher training' } }
   end
 end
+
+require 'faraday/logging/formatter'
+
+class APIRequestLoggingFormatter < Faraday::Logging::Formatter
+  # Don't do logging when the request starts
+  def request(env); end
+
+  def response(env)
+    info('Response') do
+      {
+        api_request_url: "#{env.method.upcase} #{env.url}",
+        response_status: env.status,
+        runtime_ms: (env.response_headers['x-runtime'].to_f * 1000).to_i,
+      }
+    end
+  end
+end
+
+FindAPI::Resource.connection do |connection|
+  connection.use Faraday::Response::Logger, Rails.logger, formatter: APIRequestLoggingFormatter
+end
