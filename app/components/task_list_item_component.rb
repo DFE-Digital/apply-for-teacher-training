01f15fe5a528014e112eb36f30d62f96d718413a
Steve Hook <steve.hook@gmail.com>
2020-10-13 16:02:34 +0100

    Add `custom_status` option to TaskListItemComponent

--- app/components/task_list_item_component.rb
+++ app/components/task_list_item_component.rb
@@ -3,12 +3,20 @@ class TaskListItemComponent < ViewComponent::Base
 
   validates :path, presence: true
 
-  def initialize(completed:, path:, text:, show_incomplete: true, submitted: false)
+  def initialize(
+    completed:,
+    path:,
+    text:,
+    show_incomplete: true,
+    submitted: false,
+    custom_status: nil
+  )
     @completed = completed
     @path = path
     @text = text
     @show_incomplete = show_incomplete
     @submitted = submitted
+    @custom_status = custom_status
   end
 
   def tag_id
@@ -17,5 +25,5 @@ class TaskListItemComponent < ViewComponent::Base
 
 private
 
-  attr_reader :completed, :path, :text, :show_incomplete, :submitted
+  attr_reader :completed, :path, :text, :show_incomplete, :submitted, :custom_status
 end
