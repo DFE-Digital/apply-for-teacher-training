b0df698dd3deb63438f3305e53a3b03910cbcc51
David Gisbey <davidgisbey@gmail.com>
2020-10-20 15:12:35 +0100

    Update adding a ref on sandbox system spec

--- spec/system/candidate_interface/candidate_adding_referees_in_sandbox_spec.rb
+++ spec/system/candidate_interface/candidate_adding_referees_in_sandbox_spec.rb
@@ -3,101 +3,60 @@ require 'rails_helper'
 RSpec.feature 'Candidate adding referees in Sandbox', sandbox: true do
   include CandidateHelper
 
-  before { FeatureFlag.deactivate(:decoupled_references) }
+  before { FeatureFlag.activate(:decoupled_references) }
 
-  def candidate_provides_two_referees
-    visit candidate_interface_referees_type_path
+  scenario 'Candidate adds two auto-references' do
+    given_i_am_signed_in
+    and_i_have_provided_my_personal_details
+
+    when_i_provide_two_references
+    then_i_see_that_references_are_given
+  end
+
+  def given_i_am_signed_in
+    @candidate = create(:candidate)
+    login_as(@candidate)
+  end
+
+  def and_i_have_provided_my_personal_details
+    @candidate.current_application.update!(first_name: 'Mr', last_name: 'Bot')
+  end
+
+  def when_i_provide_two_references
+    visit candidate_interface_decoupled_references_start_path
+    click_link 'Continue'
     choose 'Academic'
-    click_button 'Continue'
+    click_button 'Save and continue'
 
     candidate_fills_in_referee(
       name: 'Refbot One',
       email_address: 'refbot1@example.com',
       relationship: 'First boss',
     )
+    choose 'Yes, send a reference request now'
     click_button 'Save and continue'
-    click_link 'Add another referee'
 
+    click_link 'Add another referee'
+    click_link 'Continue'
     choose 'Professional'
-    click_button 'Continue'
+    click_button 'Save and continue'
 
     candidate_fills_in_referee(
       name: 'Refbot Two',
       email_address: 'refbot2@example.com',
       relationship: 'Second boss',
     )
+    choose 'Yes, send a reference request now'
     click_button 'Save and continue'
-    check t('application_form.completed_checkbox')
-    click_button t('application_form.continue')
   end
 
-  scenario 'Candidate adds two auto-references' do
-    given_i_am_signed_in
-
-    when_i_have_completed_my_application
-    and_i_review_my_application
-
-    then_i_should_see_all_sections_are_complete
-
-    and_i_confirm_my_application
-
-    when_i_choose_not_to_fill_in_the_equality_and_diversity_survey
-    and_i_choose_not_to_add_further_information
-    and_i_submit_the_application
-
-    then_i_see_that_the_application_was_sent_to_provider
-    and_i_see_that_references_are_given
-  end
-
-  def given_i_am_signed_in
-    create_and_sign_in_candidate
-  end
-
-  def when_i_have_completed_my_application
-    candidate_completes_application_form
-  end
-
-  def and_i_review_my_application
-    and_i_visit_the_application_form_page
-    when_i_click_on_check_your_answers
-  end
-
-  def and_i_visit_the_application_form_page
-    visit candidate_interface_application_form_path
-  end
-
-  def when_i_click_on_check_your_answers
-    click_link 'Check and submit your application'
-  end
-
-  def then_i_should_see_all_sections_are_complete
-    CandidateHelper::APPLICATION_FORM_SECTIONS.each do |section|
-      expect(page).not_to have_selector "[data-qa='missing-#{section}']"
+  def then_i_see_that_references_are_given
+    within all('.app-summary-card')[0] do
+      expect(all('.govuk-summary-list__value')[4].text).to have_content('Reference given')
     end
-  end
 
-  def and_i_confirm_my_application
-    click_link 'Continue'
-  end
-
-  def when_i_choose_not_to_fill_in_the_equality_and_diversity_survey
-    click_link 'Continue without completing questionnaire'
-  end
-
-  def and_i_choose_not_to_add_further_information
-    choose 'No'
-  end
-
-  def and_i_submit_the_application
-    click_button(FeatureFlag.active?(:decoupled_references) ? 'Send application' : 'Submit application')
-  end
-
-  def then_i_see_that_the_application_was_sent_to_provider
-    visit candidate_interface_application_complete_path
-    expect(page).to have_content('Status Awaiting decision')
-  end
-
-  def and_i_see_that_references_are_given
-    expect(page).to have_content('Reference given')
+    within all('.app-summary-card')[1] do
+      expect(all('.govuk-summary-list__value')[4].text).to have_content('Reference given')
+    end
   end
 end
