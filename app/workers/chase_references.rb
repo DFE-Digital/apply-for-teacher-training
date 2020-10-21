22642a4cdd84b9acd6ee4dadd28681086b4766ac
Tijmen Brommet <tijmen@gmail.com>
2020-10-21 13:30:52 +0100

    Combine all reference chase code into ChaseReferences class
    This is partly a refactor, but it also solves an issue where the
    referees wouldn't get the last chase email in the new decoupled
    references feature.

--- app/workers/chase_references.rb
+++ app/workers/chase_references.rb
@@ -10,16 +10,25 @@ class ChaseReferences
 private
 
   def send_7_day_chaser!
-    GetReferencesToChase.call.each do |reference|
-      SendReferenceChaseEmailToRefereeAndCandidate.call(
-        application_form: reference.application_form,
-        reference: reference,
-      )
+    references = ApplicationReference
+      .feedback_requested
+      .where(['requested_at < ?', TimeLimitConfig.chase_referee_by.days.before(Time.zone.now)])
+      .where.not(id: ChaserSent.reference_request.select(:chased_id))
+
+    references.each do |reference|
+      RefereeMailer.reference_request_chaser_email(reference.application_form, reference).deliver_later
+      CandidateMailer.chase_reference(reference).deliver_later
+      ChaserSent.create!(chased: reference, chaser_type: :reference_request)
     end
   end
 
   def send_14_day_chaser!
-    GetRefereesThatNeedReplacing.call.each do |reference|
+    references = ApplicationReference
+      .feedback_requested
+      .where(['requested_at < ?', TimeLimitConfig.replace_referee_by.days.before(Time.zone.now)])
+      .where.not(id: ChaserSent.reference_replacement.select(:chased_id))
+
+    references.each do |reference|
       SendNewRefereeRequestEmail.call(
         reference: reference,
         reason: :not_responded,
@@ -28,43 +37,15 @@ private
   end
 
   def send_28_day_chaser!
-    applications_that_need_chasing_after_28_days
-    .each do |application_form|
-      referees_that_need_chasing = application_form.application_references.select { |reference| reference.feedback_overdue? && reference.requested_at < final_chaser_time_limit }
-      referees_that_need_chasing.each do |referee|
-        CandidateMailer.chase_reference_again(referee).deliver_later
-        RefereeMailer.reference_request_chase_again_email(referee).deliver_later
-      end
-
-      ChaserSent.create!(chased: application_form, chaser_type: :follow_up_missing_references)
-    end
-  end
-
-  def applications_that_need_chasing_after_28_days
-    outstanding_reference_requests =
-      ApplicationReference
+    references = ApplicationReference
       .feedback_requested
-      .where(['requested_at < ?', final_chaser_time_limit])
-
-    application_form_ids =
-      outstanding_reference_requests
-      .where(application_form_id: applications_waiting_on_reference_feedback.pluck(:id))
-      .distinct(:application_form_id)
-      .pluck(:application_form_id)
-
-    ApplicationForm
-      .where(id: application_form_ids)
+      .where(['requested_at < ?', TimeLimitConfig.additional_reference_chase_calendar_days.days.before(Time.zone.now)])
       .where.not(id: ChaserSent.follow_up_missing_references.select(:chased_id))
-  end
 
-  def applications_waiting_on_reference_feedback
-    ApplicationForm
-      .joins(:application_choices)
-      .group('application_forms.id, application_choices.status')
-      .having('application_choices.status = ?', :unsubmitted)
-  end
-
-  def final_chaser_time_limit
-    TimeLimitConfig.additional_reference_chase_calendar_days.days.before(Time.zone.now)
+    references.each do |reference|
+      RefereeMailer.reference_request_chase_again_email(reference).deliver_later
+      CandidateMailer.chase_reference_again(reference).deliver_later
+      ChaserSent.create!(chased: reference, chaser_type: :follow_up_missing_references)
+    end
   end
 end
