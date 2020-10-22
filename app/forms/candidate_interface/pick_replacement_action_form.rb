ecb675fcde59a6159a39d0c553caa60f74e1a8d7
David Gisbey <davidgisbey@gmail.com>
2020-10-22 11:30:06 +0100

    Remove forms and components are only used in the post submission flow

--- app/forms/candidate_interface/pick_replacement_action_form.rb
+++
@@ -1,9 +0,0 @@
-module CandidateInterface
-  class PickReplacementActionForm
-    include ActiveModel::Model
-
-    attr_accessor :replacement_action
-
-    validates :replacement_action, presence: true
-  end
-end
