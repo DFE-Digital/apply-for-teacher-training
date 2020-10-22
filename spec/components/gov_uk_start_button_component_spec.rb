b647560ff65f20040250eea8de9fd6b11fe1e6e5
Ben Swannack <ben.swannack@softwire.com>
2020-10-22 09:24:02 +0100

    Use govuk start button for sandbox page buttons, use rails paths rather than hard-coded strings
    Appease RuboCop

--- spec/components/gov_uk_start_button_component_spec.rb
+++ spec/components/gov_uk_start_button_component_spec.rb
@@ -1,8 +1,8 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe GovUkStartButtonComponent, type: :component do
   it 'renders with button with an icon and specified link and title' do
-    render_result = render_inline(described_class.new(title: "Button", href: "www.example.com"))
+    render_result = render_inline(described_class.new(title: 'Button', href: 'www.example.com'))
 
     expect(render_result.css('a').attr('href').value).to eq 'www.example.com'
     expect(render_result.text).to include('Button')
