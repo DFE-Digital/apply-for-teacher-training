e5cc7473a9e454ed659fc40214bf4ca1b1ef6576
David Gisbey <davidgisbey@gmail.com>
2020-10-25 16:43:51 +0000

    Redirect to application form if course_from_find_id is nil

--- app/controllers/candidate_interface/after_sign_in_controller.rb
+++ app/controllers/candidate_interface/after_sign_in_controller.rb
@@ -1,9 +1,11 @@
 module CandidateInterface
   class AfterSignInController < CandidateInterfaceController
-    def interstitial
-      course = current_candidate.course_from_find
+    before_action :redirect_to_application_form_unless_course_from_find_is_present
 
-      current_candidate.update!(course_from_find_id: nil) if course.present?
+    def interstitial
+      course = course_from_find
+
+      current_candidate.update!(course_from_find_id: nil)
 
       if current_application.contains_course?(course)
         flash[:warning] = "You have already selected #{course.name_and_code}."
@@ -11,13 +13,25 @@ module CandidateInterface
       elsif current_application.has_the_maximum_number_of_course_choices?
         flash[:warning] = I18n.t('errors.messages.too_many_course_choices', course_name_and_code: course.name_and_code)
         redirect_to candidate_interface_course_choices_review_path
-      elsif course.present?
+      else
         redirect_to candidate_interface_course_confirm_selection_path(course.id)
-      elsif current_application.blank_application?
+      end
+    end
+
+  private
+
+    def redirect_to_application_form_unless_course_from_find_is_present
+      return false unless course_from_find.nil?
+
+      if current_application.blank_application?
         redirect_to candidate_interface_before_you_start_path
       else
         redirect_to candidate_interface_application_form_path
       end
+    end
+
+    def course_from_find
+      current_candidate.course_from_find
     end
   end
 end
