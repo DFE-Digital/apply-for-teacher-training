3f37d8638e39db45d2f1f8d0020db1d26aa26b2b
David Gisbey <davidgisbey@gmail.com>
2020-10-25 15:17:01 +0000

    Clarify the logiic in the interstitual route selector

--- app/controllers/candidate_interface/after_sign_in_controller.rb
+++ app/controllers/candidate_interface/after_sign_in_controller.rb
@@ -6,24 +6,19 @@ module CandidateInterface
       service = InterstitialRouteSelector.new(candidate: current_candidate)
       service.execute
 
-      if service.candidate_does_not_have_a_course_from_find || service.candidate_has_submitted_application
-        if current_candidate.current_application.blank_application?
-          redirect_to candidate_interface_before_you_start_path
-        else
-          redirect_to candidate_interface_application_form_path
-        end
-      elsif service.candidate_has_already_selected_the_course
+
+      if service.candidate_has_already_selected_the_course
         flash[:warning] = "You have already selected #{course.name_and_code}."
         redirect_to candidate_interface_course_choices_review_path
       elsif service.candidate_already_has_3_courses
         flash[:warning] = I18n.t('errors.messages.too_many_course_choices', course_name_and_code: course.name_and_code)
         redirect_to candidate_interface_course_choices_review_path
-      elsif !service.candidate_does_not_have_a_course_from_find
-        redirect_to candidate_interface_course_confirm_selection_path(course_id: course.id)
-      elsif service.candidate_should_choose_site
-        redirect_to candidate_interface_course_choices_site_path(course.provider.id, course.id, course.study_mode)
-      elsif service.candidate_should_choose_study_mode
-        redirect_to candidate_interface_course_choices_study_mode_path(course.provider.id, course.id)
+      elsif course.present?
+        redirect_to candidate_interface_course_confirm_selection_path(course.id)
+      elsif current_application.blank_application?
+        redirect_to candidate_interface_before_you_start_path
+      else
+        redirect_to candidate_interface_application_form_path
       end
     end
   end
