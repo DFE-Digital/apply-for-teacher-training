08f928d578c53eb08bc6c71fa7d1f616bd36add8
Steve Hook <steve.hook@gmail.com>
2020-10-28 17:43:28 +0000

    Add new statistics as additional tiles

--- app/models/performance_statistics.rb
+++ app/models/performance_statistics.rb
@@ -13,6 +13,7 @@ class PerformanceStatistics
           SELECT
               c.id,
               f.id,
+              f.phase,
               COUNT(f.id) FILTER (WHERE f.id IS NOT NULL) application_forms,
               COUNT(ch.id) FILTER (WHERE f.id IS NOT NULL) application_choices,
               CASE
@@ -24,6 +25,7 @@ class PerformanceStatistics
                 WHEN 'recruited' = ANY(ARRAY_AGG(ch.status)) THEN ARRAY['8', 'recruited']
                 WHEN 'pending_conditions' = ANY(ARRAY_AGG(ch.status)) THEN ARRAY['7', 'pending_conditions']
                 WHEN 'offer_deferred' = ANY(ARRAY_AGG(ch.status)) THEN ARRAY['10', 'offer_deferred']
+                WHEN 'rejected' = ANY(ARRAY_AGG(ch.status)) AND true = ANY(ARRAY_AGG(ch.rejected_by_default)) THEN ARRAY['11', 'rejected_by_default']
                 WHEN #{ended_without_success_sql} = '{}' THEN ARRAY['5', 'ended_without_success']
                 ELSE ARRAY['10', 'unknown_state']
               END status
@@ -37,15 +39,16 @@ class PerformanceStatistics
               NOT c.hide_in_reporting
               #{year_clause}
           GROUP BY
-              c.id, f.id
+              c.id, f.id, f.phase
       )
       SELECT
           raw_data.status[2],
+          raw_data.phase,
           COUNT(*)
       FROM
           raw_data
       GROUP BY
-          raw_data.status
+          raw_data.status, raw_data.phase
       ORDER BY
           raw_data.status[1]
     SQL
@@ -65,12 +68,13 @@ class PerformanceStatistics
     candidate_status_counts.find { |x| x['status'] == key.to_s }&.[]('count')
   end
 
-  def total_candidate_count(only: nil, except: [])
+  def total_candidate_count(only: nil, except: [], phase: nil)
     candidate_status_counts
-     .select { |row| only.nil? || row['status'].to_sym.in?(only) }
-     .reject { |row| row['status'].to_sym.in?(except) }
-     .map { |row| row['count'] }
-     .sum
+      .select { |row| only.nil? || row['status'].to_sym.in?(only) }
+      .select { |row| phase.nil? ||  row['phase'].to_sym == phase.to_sym}
+      .reject { |row| row['status'].to_sym.in?(except) }
+      .map { |row| row['count'] }
+      .sum
   end
 
   def candidate_status_counts
@@ -84,11 +88,30 @@ class PerformanceStatistics
     total_candidate_count(except: %i[never_signed_in unsubmitted_not_started_form unsubmitted_in_progress])
   end
 
+  def apply_again_submitted_count
+    total_candidate_count(except: %i[never_signed_in unsubmitted_not_started_form unsubmitted_in_progress], phase: :apply_2)
+  end
+
   def ended_without_success_count
-    total_candidate_count(only: %i[ended_without_success])
+    total_candidate_count(only: %i[rejected_by_default ended_without_success])
   end
 
   def accepted_offer_count
     total_candidate_count(only: %i[pending_conditions recruited offer_deferred])
+  end
+
+  def apply_again_accepted_offer_count
+    total_candidate_count(only: %i[pending_conditions recruited offer_deferred], phase: :apply_2)
+  end
+
+  def rejected_by_default_count
+    total_candidate_count(only: %i[rejected_by_default])
+  end
+
+  def percentage_of_providers_onboarded
+    @percentage_of_providers_onboarded ||= begin
+      counts = Provider.group(:sync_courses).count
+      "#{(counts[true] * 100) / (counts[true] + counts[false])}%"
+    end
   end
 end
