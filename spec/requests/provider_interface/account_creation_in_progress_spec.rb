46b2edfbf29783f266ddc6374c88ca2e1902330f
Duncan Brown <duncanjbrown@gmail.com>
2020-11-02 13:42:05 +0000

    Don’t send MissingProvider exceptions to Sentry
    We now tell the user what’s up here and the notifications aren’t
    actionable from our end

--- spec/requests/provider_interface/account_creation_in_progress_spec.rb
+++ spec/requests/provider_interface/account_creation_in_progress_spec.rb
@@ -22,19 +22,5 @@ RSpec.describe 'GET /provider/applications' do
       expect(response).to have_http_status 403
       expect(response.body).to include('Your email address is not recognised')
     end
-
-    it 'reports the error to Sentry, appending the Sign-in UID' do
-      allow(Raven).to receive(:user_context)
-      allow(Raven).to receive(:capture_exception)
-
-      get '/provider/applications'
-
-      expect(Raven).to have_received(:user_context).with(
-        dfe_sign_in_uid: 'DFE_SIGN_IN_UID',
-        provider_user_admin_url: 'http://www.example.com/support/users/provider/12345',
-      )
-
-      expect(Raven).to have_received(:capture_exception)
-    end
   end
 end
