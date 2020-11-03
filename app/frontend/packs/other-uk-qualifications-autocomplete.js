0ade2fc4fd748520b77aa52b3d8fb68fe3a49822
Malcolm Baig <malcolm.baig@gmail.com>
2020-11-03 11:42:54 +0000

    Add autocomplete for Other UK qualifications
    There are several kinds of qualification that we want to provide as
    autocomplete options in this free text field.
    
    - Add the qualification names to a constant defined in an initializer
    - Make these names available in the markup via a data attribute
    - Add the Javascript to make the input field autocomplete

---
+++ app/frontend/packs/other-uk-qualifications-autocomplete.js
@@ -0,0 +1,26 @@
+import { accessibleAutocompleteFromSource } from "./helpers.js";
+
+const initOtherUKQualificationsAutocomplete = () => {
+  try {
+    const inputIds = [
+      "candidate-interface-other-qualification-type-form-other-uk-qualification-type-field",
+      "candidate-interface-other-qualification-type-form-other-uk-qualification-type-field-error"
+    ];
+
+    inputIds.forEach(inputId => {
+      const input = document.getElementById(inputId);
+      if (!input) return;
+
+      const containerId = "other-uk-qualifications-autocomplete";
+      const container = document.getElementById(containerId);
+      if (!container) return;
+
+      accessibleAutocompleteFromSource(input, container);
+    });
+
+  } catch (err) {
+    console.error("Could not enhance degree type input:", err);
+  }
+};
+
+export default initOtherUKQualificationsAutocomplete;
---
+++ config/initializers/qualifications/other_uk.rb
@@ -0,0 +1,10 @@
+OTHER_UK_QUALIFICATIONS = [
+  'BTEC',
+  'GCE O level',
+  'NVQ',
+  'SVQ',
+  'Scottish Advanced Higher',
+  'Scottish Higher',
+  'Scottish National 5',
+  'VRQ',
+].freeze
