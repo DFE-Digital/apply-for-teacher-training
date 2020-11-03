55cce7ecc7cbd6914c3e0b0ca06bd45eb811989c
Steve Laing <steve.laing@gmail.com>
2020-11-03 12:06:23 +0000

    Add a service to handle adding a course after application submission

---
+++ app/services/support_interface/add_course_choice_after_submission.rb
@@ -0,0 +1,22 @@
+module SupportInterface
+  class AddCourseChoiceAfterSubmission
+    attr_reader :application_form, :course_option
+
+    def initialize(application_form:, course_option:)
+      @application_form = application_form
+      @course_option = course_option
+    end
+
+    def call
+      application_choice = ApplicationChoice.create!(
+        application_form: application_form,
+        course_option: course_option,
+        status: 'unsubmitted',
+      )
+
+      SendApplicationToProvider.call(application_choice)
+
+      application_choice
+    end
+  end
+end
