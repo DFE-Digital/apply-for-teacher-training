75414f6306370bb09548d29aa7f9b772b18c477d
Tijmen Brommet <tijmen@gmail.com>
2020-11-09 12:50:07 +0000

    Pass in the application form model instead of an ID
    This works as well.

--- app/lib/state_change_notifier.rb
+++ app/lib/state_change_notifier.rb
@@ -23,7 +23,7 @@ class StateChangeNotifier
     provider_name = application_choice.course.provider.name
     course_name = application_choice.course.name_and_code
     applicant = application_choice.application_form.first_name
-    application_form_id = application_choice.application_form.id
+    application_form = application_choice.application_form
 
     case event
     when :make_an_offer
@@ -34,31 +34,31 @@ class StateChangeNotifier
       url = helpers.support_interface_application_form_url(application_form_id)
     when :reject_application
       text = ":broken_heart: #{provider_name} has just rejected #{applicant}’s application"
-      url = helpers.support_interface_application_form_url(application_form_id)
+      url = helpers.support_interface_application_form_url(application_form)
     when :reject_application_by_default
       text = ":broken_heart: #{applicant}’s application has just been rejected by default"
       url = helpers.support_interface_application_form_url(application_form_id)
     when :offer_accepted
       text = ":handshake: #{applicant} has accepted #{provider_name}’s offer"
-      url = helpers.support_interface_application_form_url(application_form_id)
+      url = helpers.support_interface_application_form_url(application_form)
     when :offer_declined
       text = ":no_good: #{applicant} has declined #{provider_name}’s offer"
-      url = helpers.support_interface_application_form_url(application_form_id)
+      url = helpers.support_interface_application_form_url(application_form)
     when :withdraw
       text = ":runner: #{applicant} has withdrawn their application for #{course_name} at #{provider_name}"
-      url = helpers.support_interface_application_form_url(application_form_id)
+      url = helpers.support_interface_application_form_url(application_form)
     when :withdraw_offer
       text = ":no_good: #{provider_name} has just withdrawn #{applicant}’s offer"
-      url = helpers.support_interface_application_form_url(application_form_id)
+      url = helpers.support_interface_application_form_url(application_form)
     when :defer_offer
       text = ":double_vertical_bar: #{provider_name} has just deferred #{applicant}’s offer"
-      url = helpers.support_interface_application_form_url(application_form_id)
+      url = helpers.support_interface_application_form_url(application_form)
     when :reinstate_offer_conditions_met
       text = ":arrow_forward: #{provider_name} has just reinstated their offer to #{applicant} (conditions met)"
-      url = helpers.support_interface_application_form_url(application_form_id)
+      url = helpers.support_interface_application_form_url(application_form)
     when :reinstate_offer_pending_conditions
       text = ":arrow_forward: #{provider_name} has just reinstated their offer to #{applicant} (pending conditions)"
-      url = helpers.support_interface_application_form_url(application_form_id)
+      url = helpers.support_interface_application_form_url(application_form)
     else
       raise 'StateChangeNotifier: unsupported state transition event'
     end
