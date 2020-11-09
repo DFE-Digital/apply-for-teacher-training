b89d2a305479a18a5535c84630f757333a93efee
Tijmen Brommet <tijmen@gmail.com>
2020-11-09 15:43:55 +0000

    Remove reference related Slack messages
    These messages were useful to see whether or not the new decoupled
    references features were working properly, but now take up too much
    space in the Slack channel.

--- app/services/candidate_interface/send_reference_reminder.rb
+++ app/services/candidate_interface/send_reference_reminder.rb
@@ -5,6 +5,7 @@ module CandidateInterface
     end
 
     attr_reader :reference, :flash
+    delegate :application_form, to: :reference
 
     def initialize(reference, flash)
       @reference = reference
@@ -15,34 +16,14 @@ module CandidateInterface
       if reference.can_send_reminder?
         RefereeMailer.reference_request_chaser_email(application_form, reference).deliver_later
         reference.update!(reminder_sent_at: Time.zone.now)
-
-        send_slack_message(message)
-
         flash[:success] = "Reminder sent to #{referee_name}"
       end
     end
 
   private
 
-    def message
-      "Candidate #{application_form.first_name} has sent a reminder to #{referee_first_name}"
-    end
-
     def referee_name
       reference.name
-    end
-
-    def referee_first_name
-      reference.name.split.first
-    end
-
-    def application_form
-      reference.application_form
-    end
-
-    def send_slack_message(message)
-      url = Rails.application.routes.url_helpers.support_interface_application_form_url(application_form)
-      SlackNotificationWorker.perform_async(message, url)
     end
   end
 end
