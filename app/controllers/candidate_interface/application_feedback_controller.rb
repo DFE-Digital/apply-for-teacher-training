f3e7b2f13229917633ce05e0eac2f0e94abc91ba
David Gisbey <davidgisbey@gmail.com>
2020-11-10 18:09:21 +0000

    Add update logic to the form and controller action

--- app/controllers/candidate_interface/application_feedback_controller.rb
+++ app/controllers/candidate_interface/application_feedback_controller.rb
@@ -19,9 +19,22 @@ module CandidateInterface
       end
     end
 
-    def edit; end
+    def edit
+      @application_feedback = current_application.application_feedback.find(params[:id])
+      @application_feedback_form = CandidateInterface::ApplicationFeedbackForm.new
+    end
 
-    def update; end
+    def update
+      @application_feedback = current_application.application_feedback.find(params[:id])
+      @application_feedback_form = CandidateInterface::ApplicationFeedbackForm.new(update_params)
+
+      if @application_feedback_form.update(@application_feedback)
+        redirect_to candidate_interface_application_feedback_thank_you_path
+      else
+        track_validation_error(@application_feedback_form)
+        render :edit
+      end
+    end
 
     def thank_you; end
 
@@ -34,6 +47,13 @@ module CandidateInterface
         page_title: params[:page_title],
         issues: params[:issues],
       }
+    end
+
+    def update_params
+      params.require(:candidate_interface_application_feedback_form).permit(
+        :does_not_understand_section, :need_more_information, :answer_does_not_fit_format,
+        :other_feedback, :consent_to_be_contacted
+      )
     end
 
     def previous_path
--- spec/forms/candidate_interface/application_feedback_form_spec.rb
+++ spec/forms/candidate_interface/application_feedback_form_spec.rb
@@ -14,10 +14,11 @@ RSpec.describe CandidateInterface::ApplicationFeedbackForm, type: :model do
   let(:application_form) { create(:application_form) }
 
   describe 'validations' do
-    it { is_expected.to validate_presence_of(:issues) }
-    it { is_expected.to validate_presence_of(:section) }
-    it { is_expected.to validate_presence_of(:path) }
-    it { is_expected.to validate_presence_of(:page_title) }
+    it { is_expected.to validate_presence_of(:issues).on(:save) }
+    it { is_expected.to validate_presence_of(:section).on(:save) }
+    it { is_expected.to validate_presence_of(:path).on(:save) }
+    it { is_expected.to validate_presence_of(:page_title).on(:save) }
+    it { is_expected.to validate_presence_of(:consent_to_be_contacted).on(:update) }
 
     describe '#path_is_valid' do
       it 'returns false if the path is not one of our endpoints' do
@@ -51,6 +52,34 @@ RSpec.describe CandidateInterface::ApplicationFeedbackForm, type: :model do
       expect(feedback.path).to eq form.path
       expect(feedback.page_title).to eq form.page_title
       expect(feedback.id_in_path).to eq 1
+    end
+  end
+
+  describe '#update' do
+    let(:form) do
+      described_class.new(
+        need_more_information: 'true',
+        answer_does_not_fit_format: 'true',
+        other_feedback: 'This would be easier if i could read.',
+        consent_to_be_contacted: 'false',
+      )
+    end
+
+    it 'returns false if not valid' do
+      application_feedback = double
+      expect(described_class.new.save(application_feedback)).to eq(false)
+    end
+
+    it 'updates the ApplicationFeedback object and ensures booleans with missing values are set to false' do
+      feedback = create(:application_feedback, other_feedback: nil)
+
+      form.update(feedback)
+
+      expect(feedback.does_not_understand_section).to eq false
+      expect(feedback.need_more_information).to eq true
+      expect(feedback.answer_does_not_fit_format).to eq true
+      expect(feedback.other_feedback).to eq 'This would be easier if i could read.'
+      expect(feedback.consent_to_be_contacted).to eq false
     end
   end
 
