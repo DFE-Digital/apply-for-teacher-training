61c2ed404a1f63d1b5d98c6128c4c074b2fac719
David Gisbey <davidgisbey@gmail.com>
2020-11-12 14:15:52 +0000

    Update ApplicationFeedbackComponment spec to use data in the shape of actual paramaters

--- spec/components/candidate_interface/application_feedback_component_spec.rb
+++ spec/components/candidate_interface/application_feedback_component_spec.rb
@@ -6,14 +6,14 @@ RSpec.describe CandidateInterface::ApplicationFeedbackComponent do
   it 'renders successfully' do
     result = render_inline(
       described_class.new(
-        section: 'application_references',
-        path: 'candidate_interface_references_start_path',
-        page_title: 'This is the references start page',
+        section: { 'controller' => 'candidate_interface/degrees/type', 'action' => 'new' },
+        path: '/candidate/application/degrees',
+        page_title: 'Add a degreeeeee',
       ),
     )
 
     expected_base_url = '/candidate/application/application-feedback'
-    expected_query_string = '?page_title=This+is+the+references+start+page&path=candidate_interface_references_start_path&section=application_references'
+    expected_query_string = '?page_title=Add+a+degreeeeee&path=%2Fcandidate%2Fapplication%2Fdegrees&section%5Baction%5D=new&section%5Bcontroller%5D=candidate_interface%2Fdegrees%2Ftype'
 
     expected_url = result.css('a').first.attributes['href'].value
 
