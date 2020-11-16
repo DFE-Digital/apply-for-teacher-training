b52ef1878acbf6766a0aae6192f2e6dc79b762c3
Tijmen Brommet <tijmen@gmail.com>
2020-11-16 14:51:05 +0000

    Remove duplication in ReferenceActionsPolicy

--- app/services/reference_actions_policy.rb
+++ app/services/reference_actions_policy.rb
@@ -5,32 +5,34 @@ class ReferenceActionsPolicy
 
   def can_send?
     reference.not_requested_yet? &&
-      !reference.application_form.enough_references_have_been_provided? &&
-      CandidateInterface::Reference::SubmitRefereeForm.new(
-        submit: 'yes',
-        reference_id: reference.id,
-      ).valid?
+      needs_more_references? &&
+      valid_reference?
   end
 
   def can_resend?
     (reference.cancelled? || reference.cancelled_at_end_of_cycle?) &&
-      !reference.application_form.enough_references_have_been_provided? &&
-      CandidateInterface::Reference::SubmitRefereeForm.new(
-        submit: 'yes',
-        reference_id: reference.id,
-      ).valid?
+      needs_more_references? &&
+      valid_reference?
   end
 
   def can_retry?
     reference.email_bounced? &&
-      !reference.application_form.enough_references_have_been_provided? &&
-      CandidateInterface::Reference::SubmitRefereeForm.new(
-        submit: 'yes',
-        reference_id: reference.id,
-      ).valid?
+      needs_more_references? &&
+      valid_reference?
   end
 
 private
 
   attr_reader :reference
+
+  def needs_more_references?
+    !reference.application_form.enough_references_have_been_provided?
+  end
+
+  def valid_reference?
+    CandidateInterface::Reference::SubmitRefereeForm.new(
+      submit: 'yes',
+      reference_id: reference.id,
+    ).valid?
+  end
 end
