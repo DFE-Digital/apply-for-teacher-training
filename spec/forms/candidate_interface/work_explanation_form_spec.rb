059f9724432d30ab61036c13444c40856ef032a9
Steve Hook <steve.hook@gmail.com>
2020-11-18 09:34:09 +0000

    Update content/textarea for work hist explanation

--- spec/forms/candidate_interface/work_explanation_form_spec.rb
+++ spec/forms/candidate_interface/work_explanation_form_spec.rb
@@ -16,8 +16,8 @@ RSpec.describe CandidateInterface::WorkExplanationForm, type: :model do
   describe 'validations' do
     it { is_expected.to validate_presence_of(:work_history_explanation) }
 
-    okay_text = Faker::Lorem.sentence(word_count: 600)
-    long_text = Faker::Lorem.sentence(word_count: 601)
+    okay_text = Faker::Lorem.sentence(word_count: 400)
+    long_text = Faker::Lorem.sentence(word_count: 401)
 
     it { is_expected.to allow_value(okay_text).for(:work_history_explanation) }
     it { is_expected.not_to allow_value(long_text).for(:work_history_explanation) }
