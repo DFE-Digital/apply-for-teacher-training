5c8d3398fed39ebcc19424e41fca4d5e5a523bcd
George Holborn <georgeholborn5@gmail.com>
2020-11-19 17:04:23 +0000

    Add Logic to IElTs form to transform single digit value to correct format
    Comments on PR request that band score field should transform single digits input into the band
     score field to "n.0" format. I have added a sanitize method to the form that is called during
    validation and saving band score. I have had to modify it slighlty to work with existing tests
    hence the nil? clause.

--- app/forms/candidate_interface/english_foreign_language/ielts_form.rb
+++ app/forms/candidate_interface/english_foreign_language/ielts_form.rb
@@ -21,7 +21,7 @@ module CandidateInterface
 
         ielts = IeltsQualification.new(
           trf_number: trf_number,
-          band_score: band_score,
+          band_score: sanitize(band_score),
           award_year: award_year,
         )
         UpdateEnglishProficiency.new(
@@ -47,8 +47,18 @@ module CandidateInterface
       end
 
       def band_score_is_a_valid_score
-        unless band_score.in? IeltsQualification::VALID_SCORES
+        unless sanitize(band_score).in? IeltsQualification::VALID_SCORES
           errors.add(:band_score, :invalid)
+        end
+      end
+
+      def sanitize(band_score)
+        if band_score.nil?
+          nil
+        elsif band_score.length == 1
+          "#{band_score}.0"
+        else
+          band_score
         end
       end
 
