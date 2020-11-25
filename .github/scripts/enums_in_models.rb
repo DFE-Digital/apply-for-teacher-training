c63446747f94a9990ef174ba44637fa6572c99a8
Michael Nacos <m.nacos@gmail.com>
2020-11-25 12:37:57 +0000

    Add github action for db-level enum changes; Update Ruby to 2.7.2

---
+++ .github/scripts/enums_in_models.rb
@@ -0,0 +1,24 @@
+require './config/environment'
+
+filename = ARGV[0]
+
+if filename
+  model_names = File.read(filename).split(/\n/).map do |line|
+    next unless line =~ /^<enum> /
+    line.split('<enum> ')[1].split('.')[0]
+  end
+  model_names.compact.uniq.map(&:constantize)
+else
+  diff_tree_output = `git diff-tree -r --name-only --no-commit-id origin/master HEAD app/models`
+  diff_tree_output.split(/\n/).map do |model_path|
+    require "./#{model_path}"
+  end
+end
+
+# Only includes classes already loaded via constantize or require above
+ApplicationRecord.descendants.each do |model|
+  enums = model.attribute_types.select { |k, v| v.is_a? ActiveRecord::Enum::EnumType }
+  enums.each do |k, v|
+    puts "<enum> #{model}.#{k}: #{v.send(:mapping).values}"
+  end
+end
