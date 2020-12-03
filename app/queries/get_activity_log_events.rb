20625a26e599eb761046c2bb5486ef53c29d9b57
Michael Nacos <m.nacos@gmail.com>
2020-12-03 12:39:40 +0000

    Add system spec; eager-load auditable for audited events

--- app/queries/get_activity_log_events.rb
+++ app/queries/get_activity_log_events.rb
@@ -1,8 +1,20 @@
 class GetActivityLogEvents
+  INCLUDES = {
+    auditable: %i[
+      application_form
+      course_option
+      course
+      site
+      provider
+      accredited_provider
+      offered_course_option
+    ],
+  }.freeze
+
   def self.call(application_choices:, since: nil)
     since ||= Time.zone.local(2018, 1, 1) # before the pilot began, i.e. all records
 
-    Audited::Audit.from <<~COMBINE_AUDITS_WITH_APPLICATION_CHOICES_SCOPE.squish
+    Audited::Audit.includes(INCLUDES).from <<~COMBINE_AUDITS_WITH_APPLICATION_CHOICES_SCOPE.squish
       (
         SELECT a.*
           FROM audits a
