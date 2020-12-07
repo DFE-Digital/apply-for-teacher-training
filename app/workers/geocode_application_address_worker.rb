7098bd2d5cc716f1cf8982695805a65b35c7eede
Steve Hook <steve.hook@gmail.com>
2020-12-07 14:58:30 +0000

    Avoid storing latitude/longitude for addresses outside UK
    In order to avoid storing geo-located coordinates for disguised
    international addresses we check to see whether the fall outside the
    bounding box for the UK.
    
    Data source:
    https://en.wikipedia.org/wiki/List_of_extreme_points_of_the_United_Kingdom

--- app/workers/geocode_application_address_worker.rb
+++ app/workers/geocode_application_address_worker.rb
@@ -7,8 +7,24 @@ class GeocodeApplicationAddressWorker
     return unless Geocoder.config.api_key
 
     application_form = ApplicationForm.find(application_form_id)
-    application_form.latitude, application_form.longitude = application_form.geocode
+    coordinates = application_form.geocode
+    application_form.latitude, application_form.longitude = outside_uk?(coordinates) ? [nil, nil] : coordinates
 
     application_form.save!
+  end
+
+private
+
+  SOUTHERLY_LIMIT = 49.51
+  NORTHERLY_LIMIT = 60.51
+  WESTERLY_LIMIT = -8.638
+  EASTERLY_LIMIT = 1.46
+
+  def outside_uk?(coordinates)
+    latitude, longitude = coordinates
+    latitude < SOUTHERLY_LIMIT ||
+      latitude > NORTHERLY_LIMIT ||
+      longitude < WESTERLY_LIMIT ||
+      longitude > EASTERLY_LIMIT
   end
 end
