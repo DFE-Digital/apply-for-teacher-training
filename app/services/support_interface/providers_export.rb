179b69ef4cc559051f6c9e74dd96de32a5df0d8e
David Gisbey <davidgisbey@gmail.com>
2020-12-07 19:02:35 +0000

    Add average distance to site to the provider export

--- app/services/support_interface/providers_export.rb
+++ app/services/support_interface/providers_export.rb
@@ -1,11 +1,14 @@
 module SupportInterface
   class ProvidersExport
+    include GeocodeHelper
+
     def providers
       relevant_providers.map do |provider|
         {
-          name: provider.name,
-          code: provider.code,
-          agreement_accepted_at: provider.provider_agreements.where.not(accepted_at: nil).first&.accepted_at,
+          'name' => provider.name,
+          'code' => provider.code,
+          'agreement_accepted_at' => provider.provider_agreements.where.not(accepted_at: nil).first&.accepted_at,
+          'Average distance to site' => average_distance_to_site(provider),
         }
       end
     end
@@ -18,8 +21,18 @@ module SupportInterface
       Provider
         .includes(
           :provider_agreements,
+          :sites,
         )
         .where(sync_courses: true)
+        .order(:name)
+    end
+
+    def average_distance_to_site(provider)
+      format_average_distance(
+        provider,
+        provider.sites,
+        with_units: false,
+      )
     end
   end
 end
