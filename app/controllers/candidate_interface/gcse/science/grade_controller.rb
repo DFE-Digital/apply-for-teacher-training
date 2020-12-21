3c15361f6025a9c016fcf1f5e0c2c70e6034b93a
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2020-12-21 13:58:01 +0000

    Replace occurances of nil legends with h1 headings

--- app/controllers/candidate_interface/gcse/science/grade_controller.rb
+++ app/controllers/candidate_interface/gcse/science/grade_controller.rb
@@ -1,6 +1,7 @@
 module CandidateInterface
   class Gcse::Science::GradeController < CandidateInterfaceController
     include Gcse::GradeControllerConcern
+
     before_action :redirect_to_dashboard_if_submitted
     before_action :set_subject
 
