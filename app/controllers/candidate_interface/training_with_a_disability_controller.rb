34d34cd366692bc2aff302029dab4be5aa4cac19
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2020-12-22 18:23:47 +0000

    Consistent implementaton for safeguarding and training with a disability questions

--- app/controllers/candidate_interface/training_with_a_disability_controller.rb
+++ app/controllers/candidate_interface/training_with_a_disability_controller.rb
@@ -2,17 +2,18 @@ module CandidateInterface
   class TrainingWithADisabilityController < CandidateInterfaceController
     before_action :redirect_to_dashboard_if_submitted
 
+    def show
+      @application_form = current_application
+    end
+
     def edit
-      @training_with_a_disability_form = TrainingWithADisabilityForm.build_from_application(
-        current_application,
-      )
+      @training_with_a_disability_form = TrainingWithADisabilityForm.build_from_application(current_application)
     end
 
     def update
       @training_with_a_disability_form = TrainingWithADisabilityForm.new(training_with_a_disability_params)
 
       if @training_with_a_disability_form.save(current_application)
-        @application_form = current_application
         current_application.update!(training_with_a_disability_completed: false)
 
         redirect_to candidate_interface_training_with_a_disability_show_path
@@ -20,13 +21,6 @@ module CandidateInterface
         track_validation_error(@training_with_a_disability_form)
         render :edit
       end
-    end
-
-    def show
-      @application_form = current_application
-      @training_with_a_disability_form = TrainingWithADisabilityForm.build_from_application(
-        current_application,
-      )
     end
 
     def complete
