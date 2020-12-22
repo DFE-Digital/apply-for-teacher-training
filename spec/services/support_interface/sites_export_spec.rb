edeb7e83585fdc630ceda82081249cd27fe77662
Toby Retallick <toby.retallick@gmail.com>
2020-12-22 15:15:27 +0000

    Improve the query we use for the Sites data export
     - We now filter out sites that are no longer available

--- spec/services/support_interface/sites_export_spec.rb
+++ spec/services/support_interface/sites_export_spec.rb
@@ -6,8 +6,20 @@ RSpec.describe SupportInterface::SitesExport do
       unsynced_provider = create(:provider, sync_courses: false, latitude: 20, longitude: 20)
       synced_provider = create(:provider, sync_courses: true, latitude: 20, longitude: 20)
 
-      create(:site, latitude: 20, longitude: 21, provider: unsynced_provider)
-      create(:site, latitude: 20, longitude: 21, provider: synced_provider)
+      create(:course_option,
+             site_still_valid: true,
+             course: create(:course, provider: synced_provider),
+             site: create(:site, latitude: 20, longitude: 21, provider: synced_provider))
+
+      create(:course_option,
+             site_still_valid: true,
+             course: create(:course, provider: unsynced_provider),
+             site: create(:site, latitude: 20, longitude: 21, provider: unsynced_provider))
+
+      create(:course_option,
+             site_still_valid: false,
+             course: create(:course, provider: synced_provider),
+             site: create(:site, latitude: 20, longitude: 21, provider: synced_provider))
 
       sites = described_class.new.sites
       expect(sites.size).to eq(1)
