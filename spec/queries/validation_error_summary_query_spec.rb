d1a5c2bd49ed4b1ea4c3c43093a7481674d3b069
Toby Retallick <toby.retallick@gmail.com>
2020-12-29 15:35:46 +0000

    Pass sort parameter to ValidationErrorSummaryQuery
    - This allows us to change the sorting options as required by the front
    end

--- spec/queries/validation_error_summary_query_spec.rb
+++ spec/queries/validation_error_summary_query_spec.rb
@@ -26,4 +26,104 @@ RSpec.describe ValidationErrorSummaryQuery do
       ])
     end
   end
+
+  describe 'sorting' do
+    it "returns results sorted by 'All time'" do
+      create :validation_error, form_object: 'Foo', created_at: 2.days.ago
+      create :validation_error, form_object: 'Bar', created_at: 6.days.ago
+      create :validation_error, form_object: 'Baz', created_at: 50.days.ago
+      create :validation_error, form_object: 'Baz', created_at: 60.days.ago
+
+      expect(described_class.new(described_class::ALL_TIME).call).to eq([
+        {
+          'form_object' => 'Baz',
+          'attribute' => 'feedback',
+          'incidents_last_week' => 0,
+          'unique_users_last_week' => 0,
+          'incidents_last_month' => 0,
+          'unique_users_last_month' => 0,
+          'incidents_all_time' => 2,
+          'unique_users_all_time' => 2,
+        },
+        {
+          'form_object' => 'Bar',
+          'attribute' => 'feedback',
+          'incidents_last_week' => 1,
+          'unique_users_last_week' => 1,
+          'incidents_last_month' => 1,
+          'unique_users_last_month' => 1,
+          'incidents_all_time' => 1,
+          'unique_users_all_time' => 1,
+        },
+        {
+          'form_object' => 'Foo',
+          'attribute' => 'feedback',
+          'incidents_last_week' => 1,
+          'unique_users_last_week' => 1,
+          'incidents_last_month' => 1,
+          'unique_users_last_month' => 1,
+          'incidents_all_time' => 1,
+          'unique_users_all_time' => 1,
+        },
+      ])
+    end
+
+    it "returns results sorted by 'Last week'" do
+      create :validation_error, form_object: 'Foo', created_at: 2.days.ago
+      create :validation_error, form_object: 'Bar', created_at: 6.days.ago
+      create :validation_error, form_object: 'Foo', created_at: 10.days.ago
+
+      expect(described_class.new(described_class::LAST_WEEK).call).to eq([
+        {
+          'form_object' => 'Bar',
+          'attribute' => 'feedback',
+          'incidents_last_week' => 1,
+          'unique_users_last_week' => 1,
+          'incidents_last_month' => 1,
+          'unique_users_last_month' => 1,
+          'incidents_all_time' => 1,
+          'unique_users_all_time' => 1,
+        },
+        {
+          'form_object' => 'Foo',
+          'attribute' => 'feedback',
+          'incidents_last_week' => 1,
+          'unique_users_last_week' => 1,
+          'incidents_last_month' => 2,
+          'unique_users_last_month' => 2,
+          'incidents_all_time' => 2,
+          'unique_users_all_time' => 2,
+        },
+      ])
+    end
+
+    it "returns resulted sorted by 'Last month'" do
+      create :validation_error, form_object: 'Foo', created_at: 2.days.ago
+      create :validation_error, form_object: 'Bar', created_at: 6.days.ago
+      create :validation_error, form_object: 'Foo', created_at: 10.days.ago
+
+      expect(described_class.new(described_class::LAST_MONTH).call).to eq([
+        {
+          'form_object' => 'Foo',
+          'attribute' => 'feedback',
+          'incidents_last_week' => 1,
+          'unique_users_last_week' => 1,
+          'incidents_last_month' => 2,
+          'unique_users_last_month' => 2,
+          'incidents_all_time' => 2,
+          'unique_users_all_time' => 2,
+        },
+        {
+          'form_object' => 'Bar',
+          'attribute' => 'feedback',
+          'incidents_last_week' => 1,
+          'unique_users_last_week' => 1,
+          'incidents_last_month' => 1,
+          'unique_users_last_month' => 1,
+          'incidents_all_time' => 1,
+          'unique_users_all_time' => 1,
+        },
+      ])
+    end
+  end
 end
