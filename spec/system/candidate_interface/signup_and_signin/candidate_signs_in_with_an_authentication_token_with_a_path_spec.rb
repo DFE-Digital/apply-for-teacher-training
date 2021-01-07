287790b29529d3a79a735bf376523b993505b856
David Gisbey <davidgisbey@gmail.com>
2021-01-07 17:21:41 +0000

    Extend system spec to ensure behaviour is as expected
    Checks that paths with id's and paths where the
    associated object has been deleted work as expected.

--- spec/system/candidate_interface/signup_and_signin/candidate_signs_in_with_an_authentication_token_with_a_path_spec.rb
+++ spec/system/candidate_interface/signup_and_signin/candidate_signs_in_with_an_authentication_token_with_a_path_spec.rb
@@ -23,6 +23,20 @@ RSpec.feature 'Candidates authenitcation token has the path attriute populated' 
     when_i_click_on_the_link_in_my_email
     and_i_confirm_the_sign_in
     then_i_am_redirected_to_the_personal_statement_page
+
+    given_i_have_a_reference_in_the_not_requested_yet_state
+    and_i_have_received_a_token_associated_with_the_review_unsubmitted_reference_path
+
+    when_i_sign_in_using_the_token
+    and_i_confirm_the_sign_in
+    then_i_am_redirected_to_the_review_unsubmitted_reference_page
+
+    given_i_have_received_a_token_associated_with_the_review_unsubmitted_reference_path
+    and_i_have_subsequently_deleted_the_reference
+
+    when_i_sign_in_using_the_token
+    and_i_confirm_the_sign_in
+    then_i_see_the_references_start_page
   end
 
   def given_the_pilot_is_open
@@ -82,5 +96,35 @@ RSpec.feature 'Candidates authenitcation token has the path attriute populated' 
   def then_i_receive_an_email_inviting_me_to_sign_in
     open_email(@candidate.email_address)
     expect(current_email.subject).to have_content t('authentication.sign_in.email.subject')
+  end
+
+  def given_i_have_a_reference_in_the_not_requested_yet_state
+    @reference = create(:reference, :not_requested_yet, application_form: @candidate.current_application)
+  end
+
+  def and_i_have_received_a_token_associated_with_the_review_unsubmitted_reference_path
+    @magic_link_token = MagicLinkToken.new
+    create(
+      :authentication_token,
+      user: @candidate,
+      hashed_token: @magic_link_token.encrypted,
+      path: "http://localhost:3000/candidate/application/references/review-unsubmitted/#{@reference.id}",
+    )
+  end
+
+  def then_i_am_redirected_to_the_review_unsubmitted_reference_page
+    expect(page).to have_current_path candidate_interface_references_review_unsubmitted_path(@reference.id)
+  end
+
+  def and_i_have_subsequently_deleted_the_reference
+    @reference.destroy
+  end
+
+  def given_i_have_received_a_token_associated_with_the_review_unsubmitted_reference_path
+    and_i_have_received_a_token_associated_with_the_review_unsubmitted_reference_path
+  end
+
+  def then_i_see_the_references_start_page
+    expect(page).to have_current_path candidate_interface_references_start_path
   end
 end
