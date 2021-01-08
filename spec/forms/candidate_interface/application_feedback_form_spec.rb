fea12f2c1459e6b9e42bf5e965b4086b95328e2a
Frankie Roberto <frankie@frankieroberto.com>
2021-01-08 09:17:28 +0000

    Tidy up unused code

--- spec/forms/candidate_interface/application_feedback_form_spec.rb
+++ spec/forms/candidate_interface/application_feedback_form_spec.rb
@@ -5,8 +5,6 @@ RSpec.describe CandidateInterface::ApplicationFeedbackForm, type: :model do
     described_class.new(
       path: 'candidate/application/references/type/edit/1',
       page_title: t('page_titles.references_type'),
-      need_more_information: 'true',
-      answer_does_not_fit_format: 'true',
       other_feedback: 'This would be easier if i could read.',
       consent_to_be_contacted: 'false',
     )
@@ -18,6 +16,7 @@ RSpec.describe CandidateInterface::ApplicationFeedbackForm, type: :model do
     it { is_expected.to validate_presence_of(:path) }
     it { is_expected.to validate_presence_of(:page_title) }
     it { is_expected.to validate_presence_of(:consent_to_be_contacted) }
+    it { is_expected.to validate_presence_of(:other_feedback) }
 
     describe '#path_is_valid' do
       it 'returns false if the path is not one of our endpoints' do
@@ -46,9 +45,6 @@ RSpec.describe CandidateInterface::ApplicationFeedbackForm, type: :model do
       expect(application_form.application_feedback.count).to eq 1
       expect(feedback.path).to eq form.path
       expect(feedback.page_title).to eq form.page_title
-      expect(feedback.does_not_understand_section).to eq false
-      expect(feedback.need_more_information).to eq true
-      expect(feedback.answer_does_not_fit_format).to eq true
       expect(feedback.other_feedback).to eq 'This would be easier if i could read.'
       expect(feedback.consent_to_be_contacted).to eq false
     end
