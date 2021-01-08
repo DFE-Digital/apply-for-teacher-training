55a1ac42e096b5fe19917151f49cc83a6f4364ac
Aga Dufrat <a.dufrat@gmail.com>
2021-01-08 09:16:00 +0000

    Only show status change events for statuses visible to providers
    Fixes:
    ```
    ActionView::Template::Error undefined method `match' for nil:NilClass
    ```
    in ProviderInterface::ApplicationTimelineComponent
    https://sentry.io/organizations/dfe-bat/issues/2132714011/events/5d13bf5b27cd4c9b9ec9760efcc9230e/?project=1765973
    
    The `title_for(status)` was nil for statuses not visible to providers because
    they are not defined in the TITLES constant.  Those type of events (e.g. when
    status changes from "awaiting_references" to "application_complete") should not
    be included in the timeline.

--- app/queries/get_activity_log_events.rb
+++ app/queries/get_activity_log_events.rb
@@ -15,8 +15,7 @@ class GetActivityLogEvents
     ],
   }.freeze
 
-  ONLY_CHANGES_TO = %w[
-    status
+  CHANGES_TO = %w[
     reject_by_default_feedback_sent_at
     offer_changed_at
   ].freeze
@@ -24,8 +23,11 @@ class GetActivityLogEvents
   def self.call(application_choices:, since: nil)
     since ||= Time.zone.local(2018, 1, 1) # before the pilot began, i.e. all records
 
-    filter = ONLY_CHANGES_TO.map { |f| "jsonb_exists(audited_changes, '#{f}')" }
-                            .join(' OR ')
+    filter = CHANGES_TO.map { |f| "jsonb_exists(audited_changes, '#{f}')" }
+                      .join(' OR ')
+
+    statuses_visible_to_providers = ApplicationStateChange::STATES_VISIBLE_TO_PROVIDER.map { |status| "'#{status}'" }.join(', ')
+    filter += " OR audited_changes::json#>>'{status, 1}' IN (#{statuses_visible_to_providers})"
 
     Audited::Audit.includes(INCLUDES)
                   .where(auditable: application_choices)
