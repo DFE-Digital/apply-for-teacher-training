df4ca4603d47e0f6213c8061a53d9b37da2fe36d
Malcolm Baig <malcolm.baig@gmail.com>
2021-01-11 22:50:10 +0000

    Add support for parallel tests
    - Add the parallel_tests gem
    - Update Postgres config so that we use a separate database for each
      process when running tests in parallel
    - Update Capybara config so that we use a separate port for each process
    - Single-process test executions behave as before

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,5 +1,10 @@
 require 'capybara/rails'
 
+# Use different Capybara ports when running tests in parallel
+if ENV['TEST_ENV_NUMBER']
+  Capybara.server_port = 9887 + ENV['TEST_ENV_NUMBER'].to_i
+end
+
 RSpec.configure do |config|
   config.before(:each, type: :system) do
     driven_by(:rack_test)
