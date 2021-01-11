63c0954a7f9e8eaa7ee12fb0b31faabc6fe27da6
Steve Hook <steve.hook@gmail.com>
2021-01-11 09:28:25 +0000

    Add 'Accessing the service' metrics to the dashboard

--- app/controllers/support_interface/feature_metrics_dashboard_controller.rb
+++ app/controllers/support_interface/feature_metrics_dashboard_controller.rb
@@ -3,6 +3,7 @@ module SupportInterface
     def dashboard
       @reference_statistics = ReferenceFeatureMetrics.new
       @work_history_statistics = WorkHistoryFeatureMetrics.new
+      @magic_link_statistics = MagicLinkFeatureMetrics.new
       @reasons_for_rejection_statistics = ReasonsForRejectionFeatureMetrics.new
     end
   end
