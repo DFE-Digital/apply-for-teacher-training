9db2a7379e82d451de871a5adaffc3ffcb1b6bb9
Steve Hook <steve.hook@gmail.com>
2021-01-11 18:04:59 +0000

    Ignore brakeman warning
    Also added some code to make sure that the offending SQL query can't be
    injected and cleaned up redundant brakeman rules.

--- app/models/reasons_for_rejection_feature_metrics.rb
+++ app/models/reasons_for_rejection_feature_metrics.rb
@@ -6,6 +6,8 @@ class ReasonsForRejectionFeatureMetrics
     start_time,
     end_time = Time.zone.now.beginning_of_day
   )
+    raise ArgumentError unless ProviderInterface::ReasonsForRejectionWizard::INITIAL_TOP_LEVEL_QUESTIONS.include?(reason)
+
     ApplicationChoice
       .rejected
       .where("structured_rejection_reasons->>'#{reason}' = 'Yes'")
