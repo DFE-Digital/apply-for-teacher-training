6a347cd18e61adeed1b2909179680835ef4b2e5e
Steve Laing <steve.laing@gmail.com>
2021-01-19 16:51:14 +0000

    Handle request method and more complex response_data param
    This commit allows the worker to populate the request_method and the response_headers fields on VendorApiRequests.

--- app/workers/vendor_api_request_worker.rb
+++ app/workers/vendor_api_request_worker.rb
@@ -4,14 +4,26 @@ class VendorAPIRequestWorker
 
   sidekiq_options retry: 3, queue: :low_priority
 
-  def perform(request_data, response_body, status_code, created_at)
+  def perform(request_data, response_data, status_code, created_at)
     request_headers = request_data['headers']
     provider_id = provider_id_from_auth_token(request_headers.delete('HTTP_AUTHORIZATION'))
+
+    # FIXME: Temporary measure to ensure any existing background jobs will populate records correctly.
+    # This can be simplified once middleware is enqueuing response headers and body in the response_data param.
+    if response_data.is_a?(Hash)
+      response_headers = response_data['headers']
+      response_body = response_data['body']
+    else
+      response_headers = nil
+      response_body = response_data
+    end
 
     VendorAPIRequest.create!(
       request_path: request_data['path'],
       request_headers: request_headers,
       request_body: request_data['params'],
+      request_method: request_data['method'],
+      response_headers: response_headers,
       response_body: response_hash(response_body, status_code),
       status_code: status_code,
       provider_id: provider_id,
--- spec/workers/vendor_api_request_worker_spec.rb
+++ spec/workers/vendor_api_request_worker_spec.rb
@@ -19,4 +19,24 @@ RSpec.describe VendorAPIRequestWorker do
       expect(VendorAPIRequest.find_by(provider_id: provider.id)).not_to be nil
     end
   end
+
+  it 'accepts headers and body in response_data' do
+    described_class.new.perform(
+      { 'headers' => {}, 'path' => '/api/v1/foo' },
+      { 'headers' => { 'this' => 'that' }, 'body' => { 'that' => 'this' }.to_json },
+      500,
+      Time.zone.now,
+    )
+
+    vendor_api_request = VendorAPIRequest.find_by(request_path: '/api/v1/foo')
+
+    expect(vendor_api_request.response_headers).to eq({ 'this' => 'that' })
+    expect(vendor_api_request.response_body).to eq({ 'that' => 'this' })
+  end
+
+  it 'saves the request method on the vendor api request' do
+    described_class.new.perform({ 'headers' => {}, 'path' => '/api/v1/bar', 'method' => 'GET' }, {}.to_json, 500, Time.zone.now)
+
+    expect(VendorAPIRequest.find_by(request_path: '/api/v1/bar').request_method).to eq('GET')
+  end
 end
