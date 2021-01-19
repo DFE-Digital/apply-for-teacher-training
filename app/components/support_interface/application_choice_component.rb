f326dec4a5c66a9b03b5cf307dcf3bfc55c45637
Duncan Brown <duncanjbrown@gmail.com>
2021-01-19 20:55:42 +0000

    Show date of offer and DBD date on offered applications in support

--- app/components/support_interface/application_choice_component.rb
+++ app/components/support_interface/application_choice_component.rb
@@ -13,6 +13,11 @@ module SupportInterface
         { key: 'Status', value: render(SupportInterface::ApplicationStatusTagComponent.new(status: application_choice.status)) },
       ]
 
+      if application_choice.offer?
+        rows << { key: 'Offer made at', value: application_choice.offered_at.to_s(:govuk_date_and_time) }
+        rows << { key: 'Decline by default at', value: application_choice.decline_by_default_at.to_s(:govuk_date_and_time) }
+      end
+
       if application_choice.different_offer?
         rows << [
           { key: 'Course candidate applied for', value: render(CourseOptionDetailsComponent.new(course_option: application_choice.course_option)) },
