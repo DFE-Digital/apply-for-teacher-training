444118b4c44b5ba7a6f57df05a2d78e2bf1012a5
Aga Dufrat <a.dufrat@gmail.com>
2021-01-20 14:09:52 +0000

    Update UCASMatch reminder email duplicate applications candidate mailer

--- app/mailers/candidate_mailer.rb
+++ app/mailers/candidate_mailer.rb
@@ -303,12 +303,11 @@ class CandidateMailer < ApplicationMailer
   def ucas_match_reminder_email_duplicate_applications(application_choice, ucas_match)
     @course_name_and_code = application_choice.course_option.course.name_and_code
     @provider_name = application_choice.course_option.course.provider.name
-    @initial_email_date = ucas_match.candidate_last_contacted_at.to_s(:govuk_date)
-    @request_ucas_withdrawal_date = ucas_match.calculate_action_date(:ucas_match_ucas_withdrawal_request, Time.zone.today).to_s(:govuk_date)
+    @withdraw_by_date = ucas_match.calculate_action_date(:ucas_match_ucas_withdrawal_request, Time.zone.today).to_s(:govuk_date)
 
     email_for_candidate(
       application_choice.application_form,
-      subject: I18n.t!('candidate_mailer.ucas_match_reminder_email.duplicate_applications.subject'),
+      subject: I18n.t!('candidate_mailer.ucas_match.duplicate_applications.subject', withdraw_by_date: @withdraw_by_date),
     )
   end
 
--- spec/services/ucas_matches/send_ucas_match_emails_spec.rb
+++ spec/services/ucas_matches/send_ucas_match_emails_spec.rb
@@ -29,7 +29,7 @@ RSpec.describe UCASMatches::SendUCASMatchEmails, sidekiq: true do
     it 'sends reminder emails for a UCAS match that needs action' do
       reminder_email = email_for_candidate(ucas_match_to_send_remider_emails.candidate)
       expect(reminder_email).to be_present
-      expect(reminder_email.subject).to include 'Action required: please withdraw one of your duplicate applications'
+      expect(reminder_email.subject).to include 'Withdraw your duplicate application by'
     end
 
     it 'records when the reminder emails were sent' do
