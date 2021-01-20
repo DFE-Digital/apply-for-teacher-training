7d36a01a87b4865d7b50ad6a2a8745ba887a21fb
Duncan Brown <duncanjbrown@gmail.com>
2021-01-20 10:30:07 +0000

    Fix an unrealistic system spec
    We reached into models and changed fields instead of using services to
    change states. This made the spec fantastical.

--- spec/system/support_interface/audit_trail_spec.rb
+++ spec/system/support_interface/audit_trail_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.feature 'See application history', with_audited: true do
   include DfESignInHelpers
+  include ProviderUserPermissionsHelper
+  include CourseOptionHelpers
 
   around do |example|
     Timecop.freeze(Time.zone.local(2019, 10, 1, 12, 0, 0)) do
@@ -27,6 +29,7 @@ RSpec.feature 'See application history', with_audited: true do
 
   def and_there_is_an_application_in_the_system_logged_by_a_candidate
     candidate = create :candidate, email_address: 'alice@example.com'
+    @provider = create(:provider)
 
     Audited.audit_class.as_user(candidate) do
       application_form = create(
@@ -38,8 +41,9 @@ RSpec.feature 'See application history', with_audited: true do
       Timecop.freeze(Time.zone.local(2019, 10, 1, 12, 0, 1)) do
         @application_choice = create(
           :application_choice,
+          :awaiting_provider_decision,
           application_form: application_form,
-          status: 'awaiting_provider_decision',
+          course_option: course_option_for_provider(provider: @provider),
         )
       end
     end
@@ -47,20 +51,22 @@ RSpec.feature 'See application history', with_audited: true do
 
   def and_a_vendor_updates_the_application_status
     vendor_api_user = create :vendor_api_user, email_address: 'bob@example.com'
+    vendor_api_user.vendor_api_token.update(provider_id: @provider.id)
 
     Timecop.freeze(Time.zone.local(2019, 10, 2, 12, 0, 0)) do
       Audited.audit_class.as_user(vendor_api_user) do
-        @application_choice.update(status: 'rejected')
+        RejectApplication.new(actor: vendor_api_user, application_choice: @application_choice, rejection_reason: 'BAD BAD BAD!').save
       end
     end
   end
 
   def and_a_provider_updates_the_application_status
-    provider_user = create :provider_user, email_address: 'derek@example.com'
+    provider_user = create :provider_user, email_address: 'derek@example.com', dfe_sign_in_uid: '123', providers: [@provider]
+    permit_make_decisions!(dfe_sign_in_uid: '123')
 
     Timecop.freeze(Time.zone.local(2019, 10, 3, 9, 0, 0)) do
       Audited.audit_class.as_user(provider_user) do
-        @application_choice.update(status: 'offer')
+        MakeAnOffer.new(actor: provider_user, application_choice: @application_choice, course_option: @application_choice.course_option).save
       end
     end
   end
@@ -78,28 +84,28 @@ RSpec.feature 'See application history', with_audited: true do
   end
 
   def then_i_should_be_able_to_see_history_events
-    within('tbody tr:eq(1)') do
+    within('tbody tr:eq(2)') do
       expect(page).to have_content '3 October 2019'
       expect(page).to have_content '09:00'
       expect(page).to have_content 'Update Application Choice'
       expect(page).to have_content 'derek@example.com (Provider user)'
       expect(page).to have_content 'status rejected → offer'
     end
-    within('tbody tr:eq(2)') do
+    within('tbody tr:eq(4)') do
       expect(page).to have_content '2 October 2019'
       expect(page).to have_content '12:00'
       expect(page).to have_content 'Update Application Choice'
       expect(page).to have_content 'bob@example.com (Vendor API)'
       expect(page).to have_content 'status awaiting_provider_decision → rejected'
     end
-    within('tbody tr:eq(3)') do
+    within('tbody tr:eq(5)') do
       expect(page).to have_content '1 October 2019'
       expect(page).to have_content '12:00'
       expect(page).to have_content 'Create Application Choice'
       expect(page).to have_content 'alice@example.com (Candidate)'
       expect(page).to have_content 'status awaiting_provider_decision'
     end
-    within('tbody tr:eq(4)') do
+    within('tbody tr:eq(6)') do
       expect(page).to have_content '1 October 2019'
       expect(page).to have_content '12:00'
       expect(page).to have_content 'Create Application Form'
