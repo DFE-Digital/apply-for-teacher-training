1c3bc28ebf2f3930f49e1728cda2089ff4432db3
David Gisbey <davidgisbey@gmail.com>
2021-01-21 18:49:44 +0000

    Backfill values for the new datetime columns

---
+++ db/migrate/20210125142659_backfill_new_application_reference_datetime_columns.rb
@@ -0,0 +1,36 @@
+class BackfillNewApplicationReferenceDatetimeColumns < ActiveRecord::Migration[6.0]
+  def up
+    ApplicationReference
+    .all
+    .each do |reference|
+      feedback_provided_at = reference.audits.where("audited_changes#>>'{feedback_status, 1}' = 'feedback_provided'").last&.created_at
+      feedback_refused_at = reference.audits.where("audited_changes#>>'{feedback_status, 1}' = 'feedback_refused'").last&.created_at
+      cancelled_at = reference.audits.where("audited_changes#>>'{feedback_status, 1}' = 'cancelled'").last&.created_at
+      cancelled_at_eoc_at = reference.audits.where("audited_changes#>>'{feedback_status, 1}' = 'cancelled_at_end_of_cycle'").last&.created_at
+      email_bounced_at = reference.audits.where("audited_changes#>>'{feedback_status, 1}' = 'email_bounced'").last&.created_at
+
+      reference.update!(
+        feeback_provided_at: feedback_provided_at,
+        feedback_refused_at: feedback_refused_at,
+        cancelled_at: cancelled_at,
+        cancelled_at_the_end_of_cycle_at: cancelled_at_eoc_at,
+        email_bounced_at: email_bounced_at,
+        audit_comment: 'Backfilled after adding new datetime columns https://github.com/DFE-Digital/apply-for-teacher-training/pull/3901',
+      )
+    end
+  end
+
+  def down
+    ApplicationReference
+    .all
+    .each do |reference|
+      reference.update!(
+        feedback_provided_at: nil,
+        feedback_refused_at: nil,
+        cancelled_at: nil,
+        cancelled_at_the_end_of_cycle_at: nil,
+        email_bounced_at: nil,
+      )
+    end
+  end
+end
