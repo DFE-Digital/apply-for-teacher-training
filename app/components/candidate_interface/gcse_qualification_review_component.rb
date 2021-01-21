48e155456f58d317efe581b5abb4baf4494f8984
JRG <47917431+JR-G@users.noreply.github.com>
2021-01-21 09:40:54 +0000

    Added a flash message to warn candidates of incomplete GCSEs

--- app/components/candidate_interface/gcse_qualification_review_component.rb
+++ app/components/candidate_interface/gcse_qualification_review_component.rb
@@ -34,15 +34,19 @@ module CandidateInterface
     end
 
     def show_values_missing_banner?
-      if @submitting_application && @application_qualification.grade.nil? || @submitting_application && @application_qualification.award_year.nil?
-        gcse_completed = "#{@subject}_gcse_completed"
-        @application_form.send(gcse_completed) && @editable
+      if @submitting_application
+        section_or_gcse_incomplete? && @editable
       end
     end
 
   private
 
     attr_reader :application_qualification, :subject
+
+    def section_or_gcse_incomplete?
+      gcse_completed = "#{@subject}_gcse_completed"
+      (!@application_form.send(gcse_completed) || @application_form.send("#{@subject}_gcse").incomplete_gcse_information?) && !@application_qualification.missing_qualification?
+    end
 
     def qualification_row
       {
--- spec/components/candidate_interface/gcse_qualification_review_component_spec.rb
+++ spec/components/candidate_interface/gcse_qualification_review_component_spec.rb
@@ -137,22 +137,22 @@ RSpec.describe CandidateInterface::GcseQualificationReviewComponent do
   describe '#show_values_missing_banner?' do
     context 'when they have an incomplete qualification and are submitting their application' do
       it 'returns true' do
-        application_form = build :application_form
+        application_form = create(:application_form, maths_gcse_completed: true)
 
-        application_qualification = build(
-        :application_qualification,
-        application_form: application_form,
-        qualification_type: 'gcse',
-        level: 'gcse',
-        grade: nil,
-        award_year: nil,
-        subject: 'english',
-      )
+        application_qualification = create(
+          :application_qualification,
+          application_form: application_form,
+          qualification_type: 'gcse',
+          level: 'gcse',
+          grade: nil,
+          award_year: nil,
+          subject: 'maths',
+        )
         result = described_class.new(
           application_form: application_form,
           application_qualification: application_qualification,
-          subject: 'english',
-          submitting_application: true
+          subject: 'maths',
+          submitting_application: true,
         )
 
         expect(result.show_values_missing_banner?).to eq true
