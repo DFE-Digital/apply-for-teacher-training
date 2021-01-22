2fe2dbc40039aa9f4d51416359a4a8cd56a1ae72
Raam <raam.chauhan@softwire.com>
2021-01-22 09:24:48 +0000

    Set up create_or_update course method

--- app/services/teacher_training_public_api/sync_courses.rb
+++ app/services/teacher_training_public_api/sync_courses.rb
@@ -12,7 +12,8 @@ module TeacherTrainingPublicAPI
         year: recruitment_cycle_year,
         provider_code: @provider.code,
       ).paginate(per_page: 500).each do |course_from_api|
-        update_course(course_from_api, recruitment_cycle_year)
+        create_or_update_course(course_from_api, recruitment_cycle_year)
+        # update_course(course_from_api, recruitment_cycle_year)
       end
     rescue JsonApiClient::Errors::ApiError
       raise TeacherTrainingPublicAPI::SyncError
@@ -35,5 +36,37 @@ module TeacherTrainingPublicAPI
         )
       end
     end
-  end
-end
+
+    def create_or_update_course(course_from_api, recruitment_cycle_year)
+      course = provider.courses.find_or_create_by(
+          code: course_from_api.code,
+          recruitment_cycle_year: recruitment_cycle_year,
+          ) do |new_course|
+        new_course.open_on_apply = !!new_course.in_previous_cycle&.open_on_apply
+      end
+
+      assign_course_attributes(course, course_from_api, recruitment_cycle_year)
+      course.save!
+    def assign_course_attributes(course, course_from_api, recruitment_cycle_year)
+      course.name = course_from_api.name
+      course.level = course_from_api.level
+      course.study_mode = course_from_api.study_mode
+      course.description = course_from_api.summary
+      course.start_date = course_from_api.start_date
+      course.course_length = course_from_api.course_length
+      course.recruitment_cycle_year = recruitment_cycle_year
+      course.exposed_in_find = course_from_api.findable
+      course.funding_type = course_from_api.funding_type
+      course.program_type = course_from_api.program_type
+      course.age_range = age_range_in_years(course_from_api)
+      course.withdrawn = course_from_api.state == 'withdrawn'
+      course.qualifications = course_from_api.qualifications
+    end
+
+    def age_range_in_years(course_from_api)
+      if course_from_api.age_minimum.blank? || course_from_api.age_maximum.blank?
+        nil
+      else
+        "#{course_from_api.age_minimum} to #{course_from_api.age_maximum}"
+      end
+    end
