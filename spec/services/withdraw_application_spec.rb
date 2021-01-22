81e342ac07cdfa04aad90b4232ad673e27234d25
Aga Dufrat <a.dufrat@gmail.com>
2021-01-22 11:12:46 +0000

    Email ratifying providers when an application is withdrawn
    In keeping with the principle that providers are assumed to be interested in
    all applications they can see in Manage, we should also send emails to users of
    the accredited body (also called ratifying provider).

--- spec/services/withdraw_application_spec.rb
+++ spec/services/withdraw_application_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe WithdrawApplication do
+  include CourseOptionHelpers
+
   describe '#save!' do
     it 'changes the state of the application_choice to "withdrawn"' do
       choice = create(:application_choice, status: :awaiting_provider_decision)
@@ -18,6 +20,25 @@ RSpec.describe WithdrawApplication do
       WithdrawApplication.new(application_choice: withdrawing_application).save!
 
       expect(SetDeclineByDefault).to have_received(:new).with(application_form: withdrawing_application.application_form)
+    end
+
+    it 'sends emails to the training provider and ratyfing provider', sidekiq: true do
+      training_provider = create(:provider)
+      training_provider_user = create(:provider_user, send_notifications: true, providers: [training_provider])
+
+      ratifying_provider = create(:provider)
+      ratifying_provider_user = create(:provider_user, send_notifications: true, providers: [ratifying_provider])
+
+      course_option = course_option_for_accredited_provider(provider: training_provider, accredited_provider: ratifying_provider)
+      application_choice = create(:submitted_application_choice, course_option: course_option)
+
+      WithdrawApplication.new(application_choice: application_choice).save!
+
+      training_provider_email = ActionMailer::Base.deliveries.find { |e| e.header['to'].value == training_provider_user.email_address }
+      ratifying_provider_email = ActionMailer::Base.deliveries.find { |e| e.header['to'].value == ratifying_provider_user.email_address }
+
+      expect(training_provider_email['rails-mail-template'].value).to eq('application_withdrawn')
+      expect(ratifying_provider_email['rails-mail-template'].value).to eq('application_withdrawn')
     end
 
     describe 'retrieving a UCASMatch' do
