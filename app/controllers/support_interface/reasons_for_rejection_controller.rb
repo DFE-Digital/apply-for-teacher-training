08459b6465a52895129e60d8d6f6ac523a9a454f
Steve Hook <steve.hook@gmail.com>
2021-01-23 01:32:31 +0000

    Use human readable labels for the reasons/sub-reasons

--- app/controllers/support_interface/reasons_for_rejection_controller.rb
+++ app/controllers/support_interface/reasons_for_rejection_controller.rb
@@ -1,5 +1,14 @@
 module SupportInterface
   class ReasonsForRejectionController < SupportInterfaceController
+    # TODO: move this
+    MAPPING = {
+      candidate_behaviour_what_did_the_candidate_do: :candidate_behaviour_what_did_the_candidate_do,
+      quality_of_application_which_parts_needed_improvement: :quality_of_application,
+      qualifications_which_qualifications: :qualifications_which_qualifications,
+      honesty_and_professionalism_concerns: :honesty_and_professionalism,
+      safeguarding_concerns: :safeguarding_issues,
+    }.with_indifferent_access
+    
     def sub_reasons
       @reasons = ReasonsForRejectionCountQuery.new.sub_reason_counts
     end
