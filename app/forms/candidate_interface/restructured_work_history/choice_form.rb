dc7b8954bc33873e17305bb093eaa0d281875ae5
David Gisbey <davidgisbey@gmail.com>
2021-01-25 10:30:14 +0000

    Add the RestructedWorkHistory::ChoiceForm
    This follows the established pattern of using .save and
    build from application methods.

---
+++ app/forms/candidate_interface/restructured_work_history/choice_form.rb
@@ -0,0 +1,36 @@
+module CandidateInterface
+  module RestructuredWorkHistory
+    class ChoiceForm
+      include ActiveModel::Model
+
+      attr_accessor :choice, :explanation
+
+      validates :choice, presence: true
+      validates :explanation, presence: true, if: -> { can_not_complete_work_history? }
+
+      def save(application_form)
+        return false unless valid?
+
+        application_form.update!(
+          work_history_status: choice,
+          work_history_explanation: explanation,
+        )
+      end
+
+      def self.build_from_application(application_form)
+        new(
+          choice: application_form.work_history_status,
+          explanation: application_form.work_history_explanation,
+        )
+      end
+
+      def can_complete_work_history?
+        choice == 'can_complete'
+      end
+
+      def can_not_complete_work_history?
+        choice == 'can_not_complete'
+      end
+    end
+  end
+end
