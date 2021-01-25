5cad651d0c32360ca7408de8b205ccc2f29e513a
George Holborn <george.holborn@digtal.education.gov.uk>
2021-01-25 12:38:13 +0000

    Add Percentage value to Dashboard
    After discussion with stakeholders it was clear that before_this_month wqs not useful and that instead
    a percentage of total reasons would be more helpful. This commit adds logic to the view component to
    calculate this value and then rejigs the front end based on styling comments. The spec has also been updated to reflect the changes to the front end, to include a test for the percentage value.

--- app/components/support_interface/reasons_for_rejection_dashboard_component.rb
+++ app/components/support_interface/reasons_for_rejection_dashboard_component.rb
@@ -11,6 +11,18 @@ module SupportInterface
       values['count']
     end
 
+    def total_rejection_count(reason)
+      current_month_rejection_count(reason) + previous_rejection_count(reason)
+    end
+
+    def percentage_rejected_for_reason(reason)
+      count = current_month_rejection_count(reason) + previous_rejection_count(reason)
+      total = @rejection_reasons.inject(0) { |sum, hash| sum + hash['count'] }
+      formatted_percentage(count, total)
+    end
+
+  private
+
     def previous_rejection_count(reason)
       values = @rejection_reasons.find { |h| h['key'] == reason && h['time_period'] == 'before_this_month' }
       return 0 if values.nil?
@@ -18,8 +30,16 @@ module SupportInterface
       values['count']
     end
 
-    def total_rejection_count(reason)
-      current_month_rejection_count(reason) + previous_rejection_count(reason)
+    def formatted_percentage(count, total)
+      percentage = count.percent_of(total)
+      precision = (percentage % 1).zero? ? 0 : 2
+      number_to_percentage(percentage, precision: precision)
     end
+  end
+end
+
+class Numeric
+  def percent_of(number)
+    to_f / number * 100.0
   end
 end
