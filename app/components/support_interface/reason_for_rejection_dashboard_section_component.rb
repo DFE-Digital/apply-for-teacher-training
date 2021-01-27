c8e6ed95a3160c16c7f51afd2c6666f90f69adba
David Gisbey <davidgisbey@gmail.com>
2021-01-27 22:38:06 +0000

    Use the total structured rejection count to calculate the % shown
    Previously, we were calculating the total number of reasons for rejection
    then doing reason/total_number_of_rejection_reasons_given * 100.
    
    This doesn't really work for a couple of reasons. Firstly,
    while is indicates rejection reasons are more common, this can
    be ascerntained by just looking at the total number of rejection
    for each section.
    
    Secondly, the interested in future applications and other advice or
    feedback sections will distort the percentages for the actual rejection
    reasons.
    
    These two sections are not actually rejection reasons, so if we were to
    use the previous implementation, should not be included.

--- app/components/support_interface/reason_for_rejection_dashboard_section_component.rb
+++ app/components/support_interface/reason_for_rejection_dashboard_section_component.rb
@@ -1,10 +1,21 @@
 module SupportInterface
   class ReasonForRejectionDashboardSectionComponent < ViewComponent::Base
-    def initialize(heading:, total_count:, this_month:, percentage_rejected:)
+    def initialize(heading:, total_count:, this_month:, percentage_rejected:, total_structured_rejection_reasons_count:)
       @heading = heading
       @total_count = total_count
       @this_month = this_month
       @percentage_rejected = percentage_rejected
+      @total_structured_rejection_reasons_count = total_structured_rejection_reasons_count
+    end
+
+  private
+
+    def set_div_id_from_heading
+      @heading.parameterize.underscore
+    end
+
+    def number_of_rejections_out_of_total_rejections
+      "#{@total_count} of #{@total_structured_rejection_reasons_count} application choices"
     end
   end
 end
