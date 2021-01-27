dc6f0c679cd71f680fbba8e7cbfed376aefa3c11
Aga Dufrat <a.dufrat@gmail.com>
2021-01-27 17:39:08 +0000

    Conditionally send resolved on UCAS at our request email

--- app/services/ucas_matches/resolve_on_ucas.rb
+++ app/services/ucas_matches/resolve_on_ucas.rb
@@ -8,7 +8,10 @@ module UCASMatches
 
     def call
       UCASMatches::RecordActionTaken.new(ucas_match, :resolved_on_ucas).call
-      UCASMatches::SendResolvedOnUCASEmails.new(ucas_match).call
+      UCASMatches::SendResolvedOnUCASEmails.new(
+        ucas_match,
+        at_our_request: ucas_match.action_taken_before_last_save == 'ucas_withdrawal_requested',
+      ).call
     end
   end
 end
