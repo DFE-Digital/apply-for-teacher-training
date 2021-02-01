a112ef831ecfc4df3226638e3ce89a0ca811019a
Duncan Brown <duncanjbrown@gmail.com>
2021-02-01 21:11:58 +0000

    Ignore query params in the tab navigation component
    This lets us set default query options for filters etc under each tab
    and keep the tab highlighted when those options change.
    
    What if the tab title doesn't apply when the query parameters change,
    though?  I don’t think this is a problem because if query values would
    cause the content to be in a different tab - ie under a different
    heading altogether - then that ought to be a different URL anyway.

--- app/components/tab_navigation_component.rb
+++ app/components/tab_navigation_component.rb
@@ -5,4 +5,10 @@ class TabNavigationComponent < ViewComponent::Base
   def initialize(items:)
     @items = items
   end
+
+  def strip_query(url)
+    url = Addressable::URI.parse(url)
+    url.query_values = nil
+    url.to_s
+  end
 end
