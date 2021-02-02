53679bc397aa5db1b024e844e5834014faab7a8d
David Gisbey <davidgisbey@gmail.com>
2021-02-02 12:43:04 +0000

    Update E&D export to include rejection reasons

--- app/services/support_interface/equality_and_diversity_export.rb
+++ app/services/support_interface/equality_and_diversity_export.rb
@@ -1,7 +1,8 @@
 module SupportInterface
   class EqualityAndDiversityExport
     def data_for_export
-      data_for_export = application_forms.map do |application_form|
+      data_for_export = application_forms.includes(:application_choices).map do |application_form|
+        rejected_application_choices = application_form.application_choices.rejected
         output = {
           'Month' => application_form.submitted_at&.strftime('%B'),
           'Recruitment cycle year' => application_form.recruitment_cycle_year,
@@ -9,6 +10,12 @@ module SupportInterface
           'Ethnic group' => application_form.equality_and_diversity['ethnic_group'],
           'Ethnic background' => application_form.equality_and_diversity['ethnic_background'],
           'Application status' => I18n.t!("candidate_flow_application_states.#{ProcessState.new(application_form).state}.name"),
+          'First rejection reason' => rejected_application_choices[0]&.rejection_reason,
+          'Second rejection reason' => rejected_application_choices[1]&.rejection_reason,
+          'Third rejection reason' => rejected_application_choices[2]&.rejection_reason,
+          'First structured rejection reasons' => format_structured_rejection_reasons(rejected_application_choices[0]&.structured_rejection_reasons),
+          'Second structured rejection reasons' => format_structured_rejection_reasons(rejected_application_choices[1]&.structured_rejection_reasons),
+          'Third structured rejection reasons' => format_structured_rejection_reasons(rejected_application_choices[2]&.structured_rejection_reasons),
         }
 
         disabilities = application_form.equality_and_diversity['disabilities']
@@ -31,6 +38,25 @@ module SupportInterface
       ApplicationForm
         .includes(:application_choices)
         .where.not(equality_and_diversity: nil)
+    end
+
+    def format_structured_rejection_reasons(structured_rejection_reasons)
+      return nil if structured_rejection_reasons.blank?
+
+      select_high_level_rejection_reasons(structured_rejection_reasons)
+      .keys
+      .map { |reason| format_reason(reason) }
+      .join("\n")
+    end
+
+    def select_high_level_rejection_reasons(structured_rejection_reasons)
+      structured_rejection_reasons.select { |reason, value| value == 'Yes' && reason.include?('_y_n') }
+    end
+
+    def format_reason(reason)
+      reason
+      .delete_suffix('_y_n')
+      .humanize
     end
   end
 end
