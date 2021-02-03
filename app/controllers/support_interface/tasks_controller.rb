a854c7cd57bcc897273e00337f9da025b55a3ef2
Raam <raam.chauhan@softwire.com>
2021-02-03 11:35:50 +0000

    Update support sync button

--- app/controllers/support_interface/tasks_controller.rb
+++ app/controllers/support_interface/tasks_controller.rb
@@ -9,6 +9,7 @@ module SupportInterface
         flash[:success] = 'Scheduled job to generate test applications - this might take a while!'
         redirect_to support_interface_tasks_path
       when 'sync_providers'
+        TeacherTrainingPublicAPI::SyncAllProvidersAndCoursesWorker.perform_async
         SyncAllFromFind.perform_async
         flash[:success] = 'Scheduled job to sync providers - this might take a while!'
         redirect_to support_interface_tasks_path
