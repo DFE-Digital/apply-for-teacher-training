c6067a6ca0dd5e08fe214b80455af366c7a88398
Duncan Brown <duncanjbrown@gmail.com>
2021-02-04 22:44:27 +0000

    Refactor SupportInterface::ProvidersFilter spec
    Remove let!, make the specs a bit shorter and be clearer about the
    default behaviour

--- spec/models/support_interface/providers_filter_spec.rb
+++ spec/models/support_interface/providers_filter_spec.rb
@@ -1,13 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe SupportInterface::ProvidersFilter do
-  let!(:provider_with_synced_courses) { create(:provider, sync_courses: true) }
-  let!(:provider_without_synced_courses) { create(:provider, sync_courses: false) }
-  let!(:provider_with_signed_dsa) { create(:provider, :with_signed_agreement) }
-  let!(:provider_with_nothing) { create(:provider) }
-
   describe '#filter_records' do
     it 'filters by synced courses' do
+      provider_with_synced_courses = create(:provider, sync_courses: true)
+      create(:provider, sync_courses: false)
+
       providers = Provider.all
       filter = described_class.new(params: { onboarding_stages: %w[synced] })
 
@@ -15,17 +13,31 @@ RSpec.describe SupportInterface::ProvidersFilter do
     end
 
     it 'filters by DSA signed' do
+      provider_with_signed_dsa = create(:provider, :with_signed_agreement)
+      create(:provider)
+
       providers = Provider.all
       filter = described_class.new(params: { onboarding_stages: %w[dsa_signed] })
 
       expect(filter.filter_records(providers)).to eq [provider_with_signed_dsa]
     end
 
-    it 'excludes providers when the boxes aren’t ticked' do
+    it 'defaults to showing providers with synced courses and DSAs' do
+      synced_and_signed = create(:provider, :with_signed_agreement, sync_courses: true)
+      neither_synced_not_signed = create(:provider)
+      create(:provider, sync_courses: true) # only synced
+      create(:provider, :with_signed_agreement) # only signed
+
       providers = Provider.all
+      filter = described_class.new(params: {})
+
+      expect(filter.filter_records(providers)).to match_array([
+        synced_and_signed,
+      ])
+
       filter = described_class.new(params: { remove: true })
 
-      expect(filter.filter_records(providers)).to match_array [provider_with_nothing, provider_without_synced_courses]
+      expect(filter.filter_records(providers)).to eq [neither_synced_not_signed]
     end
   end
 end
