944f4b7315d089cb2f86a3d34aca7edeaa35bb9a
Steve Laing <steve.laing@gmail.com>
2021-02-04 09:30:16 +0000

    Add service to update interview

---
+++ app/services/update_interview.rb
@@ -0,0 +1,33 @@
+class UpdateInterview
+  attr_reader :auth, :interview, :provider, :date_and_time, :location, :additional_details
+
+  def initialize(
+    actor:,
+    interview:,
+    provider:,
+    date_and_time:,
+    location:,
+    additional_details:
+  )
+    @auth = ProviderAuthorisation.new(actor: actor)
+    @interview = interview
+    @provider = provider
+    @date_and_time = date_and_time
+    @location = location
+    @additional_details = additional_details
+  end
+
+  def save!
+    auth.assert_can_make_decisions!(
+      application_choice: interview.application_choice,
+      course_option_id: interview.application_choice.offered_option.id,
+    )
+
+    interview.update!(
+      provider: provider,
+      date_and_time: date_and_time,
+      location: location,
+      additional_details: additional_details,
+    )
+  end
+end
