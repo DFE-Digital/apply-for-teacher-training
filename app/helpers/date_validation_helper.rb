e77664c39a2fb8bae74f5667a337a1bc995a1750
Despo Pentara <despo@extractmethod.com>
2021-02-05 01:14:45 +0000

    Remove obsolete DateValidationHelper methods

--- app/helpers/date_validation_helper.rb
+++ app/helpers/date_validation_helper.rb
@@ -16,50 +16,9 @@ module DateValidationHelper
       Struct.new(:day, :month, :year).new(1, month, year)
     end
   end
-
-  def end_date_blank?
-    end_date_year.blank? && end_date_month.blank?
-  end
-
-  def end_date_valid
-    errors.add(:end_date, :invalid) unless
-      end_date.is_a?(Date) &&
-        end_date.year.positive?
-  end
-
-  def start_date_valid
-    errors.add(:start_date, :invalid) unless
-      start_date.is_a?(Date) &&
-        start_date.year.positive?
-  end
-
   def start_date_before_end_date
     if start_date_and_end_date_valid?
       errors.add(:start_date, :before) unless start_date <= end_date
     end
-  end
-
-  def start_date_before_current_year_and_month
-    if start_date > Time.zone.today
-      errors.add(:start_date, :in_the_future)
-    end
-  end
-
-  def end_date_before_current_year_and_month
-    if end_date > Time.zone.today
-      errors.add(:end_date, :in_the_future)
-    end
-  end
-
-  def start_date_and_end_date_valid?
-    end_date.is_a?(Date) && start_date.is_a?(Date)
-  end
-
-  def start_date_valid?
-    start_date.is_a?(Date)
-  end
-
-  def end_date_valid?
-    end_date.is_a?(Date)
   end
 end
