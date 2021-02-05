2e0d8cc8d242a504a947511fcae4f7eab12800f8
Malcolm Baig <malcolm.baig@gmail.com>
2021-02-05 11:33:20 +0000

    Update render_content_page helper to support ERB tags
    This helper is used to turn content defined in Markdown files into HTML.
    
    Use an `ApplicationController::Renderer` to render the files just prior
    to processing the Markdown. This:
    
    - Processes ERB tags
    - Gives access to the `t` translation helper that's available in views,
      which will respect the `raise_on_missing_translation` config.

--- app/helpers/content_helper.rb
+++ app/helpers/content_helper.rb
@@ -1,6 +1,8 @@
 module ContentHelper
   def render_content_page(page_name)
-    @converted_markdown = GovukMarkdown.render(File.read("app/views/content/#{page_name}.md")).html_safe
+    raw_content = File.read("app/views/content/#{page_name}.md")
+    content_with_erb_tags_replaced = ApplicationController.renderer.render(inline: raw_content)
+    @converted_markdown = GovukMarkdown.render(content_with_erb_tags_replaced).html_safe
     @page_name = page_name
     render 'rendered_markdown_template'
   end
