53a871b732d1370340943b7861809ed73daca7ac
Duncan Brown <duncanjbrown@gmail.com>
2021-02-05 21:39:55 +0000

    Add @rails/ujs and add it to Support

--- app/frontend/packs/application-support.js
+++ app/frontend/packs/application-support.js
@@ -1,5 +1,6 @@
 require.context("govuk-frontend/govuk/assets");
 
+import Rails from '@rails/ujs';
 import { initAll as govUKFrontendInitAll } from "govuk-frontend";
 import initApiTokenProviderAutocomplete from "./autocompletes/api-token-autocomplete";
 import "../styles/application-support.scss";
@@ -7,7 +8,7 @@ import filter from "./components/paginated_filter";
 import "accessible-autocomplete/dist/accessible-autocomplete.min.css";
 import initCountryAutocomplete from "./autocompletes/country-autocomplete";
 
-
+Rails.start();
 govUKFrontendInitAll();
 initApiTokenProviderAutocomplete();
 filter();
