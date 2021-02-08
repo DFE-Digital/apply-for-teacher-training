d892966b4ad7e4da448f176acb8ed800c6e83f2d
Duncan Brown <duncanjbrown@gmail.com>
2021-02-08 20:33:53 +0000

    Link to application choices from API log entries

--- spec/components/support_interface/vendor_api_request_details_component_spec.rb
+++ spec/components/support_interface/vendor_api_request_details_component_spec.rb
@@ -8,4 +8,22 @@ RSpec.describe SupportInterface::VendorAPIRequestDetailsComponent do
 
     expect(component.text).to include('Unknown')
   end
+
+  it 'includes a link to the application if the request identifies a single application' do
+    req = build(:vendor_api_request, request_path: '/api/v1/applications/11/offer')
+
+    component = render_inline(described_class.new(req))
+    link_to_application = component.at_css('a:contains("View in support")')
+
+    expect(link_to_application).to be_present
+    expect(link_to_application['href']).to match(%r{/support/application-choices/11})
+  end
+
+  it 'does not includes a link to the application if the request does not identify a single application' do
+    req = build(:vendor_api_request, request_path: '/api/v1/applications')
+
+    component = render_inline(described_class.new(req))
+
+    expect(component.at_css('a:contains("View in support")')).not_to be_present
+  end
 end
