a978de6dd27076192fe8b92d2ee4bd36b8d5c08d
Despo Pentara <despo@extractmethod.com>
2021-02-08 22:09:11 +0000

    Add pagination to interview schedule pages
    - prevents access to interview schedule unless feature is active

--- app/controllers/provider_interface/interview_schedules_controller.rb
+++ app/controllers/provider_interface/interview_schedules_controller.rb
@@ -1,19 +1,27 @@
 module ProviderInterface
   class InterviewSchedulesController < ProviderInterfaceController
+    before_action :interview_flag_enabled?
+
     def show
-      @grouped_interviews = Interview.for_application_choices(application_choices_for_user_providers)
+      @interviews = Interview.for_application_choices(application_choices_for_user_providers)
         .undiscarded
         .includes([:provider, application_choice: [:course_option, application_form: [:candidate]]])
         .where('date_and_time >= ?', Time.zone.now)
-        .order(:date_and_time).group_by(&:date)
+        .order(:date_and_time)
+        .page(params[:page] || 1).per(50)
+
+      @grouped_interviews = @interviews.group_by(&:date)
     end
 
     def past
-      @grouped_interviews = Interview.for_application_choices(application_choices_for_user_providers)
+      @interviews = Interview.for_application_choices(application_choices_for_user_providers)
         .undiscarded
         .includes([:provider, application_choice: [:course_option, application_form: [:candidate]]])
         .where('date_and_time < ?', Time.zone.now)
-        .order(date_and_time: :desc).group_by(&:date)
+        .order(date_and_time: :desc)
+        .page(params[:page] || 1).per(50)
+
+      @grouped_interviews = @interviews.group_by(&:date)
     end
 
   private
@@ -22,6 +30,13 @@ module ProviderInterface
       GetApplicationChoicesForProviders.call(
         providers: current_provider_user.providers,
       )
+    end
+
+    def interview_flag_enabled?
+      unless FeatureFlag.active?(:interviews)
+        fallback_path = provider_interface_application_choice_path(@application_choice)
+        redirect_back(fallback_location: fallback_path)
+      end
     end
   end
 end
