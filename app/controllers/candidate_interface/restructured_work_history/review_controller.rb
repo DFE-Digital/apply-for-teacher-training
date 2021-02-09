343db4162d736ec1cc07dbfe170b6e753e2dd6ae
JRG <47917431+JR-G@users.noreply.github.com>
2021-02-09 20:48:14 +0000

    Added a flash message for unexplained work history breaks
    As the application review page only checks if the work history
    section has been completed or not, validation was required to ensure
    that all gaps have either been explained with a break or that a job
    has been added to cover that period of time. A flash message has been
    added to the work history review page that will appear if the candidate
    tries to mark the section as completed with unexplained gaps.

--- app/controllers/candidate_interface/restructured_work_history/review_controller.rb
+++ app/controllers/candidate_interface/restructured_work_history/review_controller.rb
@@ -5,9 +5,32 @@ module CandidateInterface
     end
 
     def complete
-      current_application.update!(application_form_params)
+      @application_form = current_application
 
-      redirect_to candidate_interface_application_form_path
+      if section_marked_as_complete? && unexplained_gaps?
+        flash[:warning] = 'You cannot mark this section complete with unexplained work breaks.'
+
+        render :show
+      else
+        current_application.update!(application_form_params)
+
+        redirect_to candidate_interface_application_form_path
+      end
+    end
+
+  private
+
+    def unexplained_gaps?
+      breaks = WorkHistoryWithBreaks.new(current_application).timeline
+      breaks.any? { |entry| entry.is_a?(WorkHistoryWithBreaks::BreakPlaceholder) }
+    end
+
+    def application_form_params
+      strip_whitespace params.require(:application_form).permit(:work_history_completed)
+    end
+
+    def section_marked_as_complete?
+      application_form_params[:work_history_completed] == 'true'
     end
   end
 end
