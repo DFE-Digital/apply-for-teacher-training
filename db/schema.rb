6c06e4fa7886c6c80f6e36094f723ac68e6c1827
Richard Pattinson <richie.pattinson@googlemail.com>
2021-02-10 11:18:57 +0000

    remove line

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_02_08_105018) do
 
   create_sequence "application_choices_id_seq"
