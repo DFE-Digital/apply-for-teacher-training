e4fcf4a0faef92c642851594aa7b00ab8a0f4a10
JRG <47917431+JR-G@users.noreply.github.com>
2021-02-10 15:34:45 +0000

    Removed validation

--- app/controllers/candidate_interface/restructured_work_history/review_controller.rb
+++ app/controllers/candidate_interface/restructured_work_history/review_controller.rb
@@ -5,32 +5,15 @@ module CandidateInterface
     end
 
     def complete
-      @application_form = current_application
+      current_application.update!(application_form_params)
 
-      if section_marked_as_complete? && unexplained_gaps?
-        flash[:warning] = 'You cannot mark this section complete with unexplained work breaks.'
-
-        render :show
-      else
-        current_application.update!(application_form_params)
-
-        redirect_to candidate_interface_application_form_path
-      end
+      redirect_to candidate_interface_application_form_path
     end
 
   private
 
-    def unexplained_gaps?
-      breaks = WorkHistoryWithBreaks.new(current_application).timeline
-      breaks.any? { |entry| entry.is_a?(WorkHistoryWithBreaks::BreakPlaceholder) }
-    end
-
     def application_form_params
       strip_whitespace params.require(:application_form).permit(:work_history_completed)
-    end
-
-    def section_marked_as_complete?
-      application_form_params[:work_history_completed] == 'true'
     end
   end
 end
