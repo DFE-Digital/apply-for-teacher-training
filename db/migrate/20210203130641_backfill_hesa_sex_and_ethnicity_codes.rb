cc71ff5483dfe231058f388fd90e7e696370b851
Richard Pattinson <richie.pattinson@googlemail.com>
2021-02-10 14:42:58 +0000

    rename backfill_hesa_sex_and_ethnicity_codes.rb -> backfill_application_form_hesa_sex_and_ethnicity_code.rb as this class name has already been used and causes conflict on migration

--- db/migrate/20210203130641_backfill_hesa_sex_and_ethnicity_codes.rb
+++ db/migrate/20210203130641_backfill_hesa_sex_and_ethnicity_codes.rb
@@ -2,7 +2,7 @@ class BackfillHesaSexAndEthnicityCodes < ActiveRecord::Migration[6.0]
   def change
     application_forms_with_equality_and_diversity_data = ApplicationForm.where.not(equality_and_diversity: nil)
     application_forms_with_equality_and_diversity_data.each do |application_form|
-      BackfillHesaSexAndEthnicityCodes.call(application_form)
+      BackfillApplicationFormHesaSexAndEthnicityCode.call(application_form)
     end
   end
 end
