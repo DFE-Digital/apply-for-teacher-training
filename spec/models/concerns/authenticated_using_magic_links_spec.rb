bcec1f55a60f1c61cfaea502c3e2c9faf4f0301d
Tijmen Brommet <tijmen@gmail.com>
2021-02-10 11:12:17 +0100

    Allow provider/support auth tokens to be used only once

--- spec/models/concerns/authenticated_using_magic_links_spec.rb
+++ spec/models/concerns/authenticated_using_magic_links_spec.rb
@@ -2,12 +2,13 @@ require 'rails_helper'
 
 RSpec.describe AuthenticatedUsingMagicLinks do
   describe '.authenticate!' do
-    it 'returns the user if the token is valid' do
+    it 'returns the user if the token is valid, but only once' do
       user = create(:provider_user)
 
       token = user.create_magic_link_token!
 
       expect(ProviderUser.authenticate!(token)).to eql(user)
+      expect(ProviderUser.authenticate!(token)).to be_falsey
     end
 
     it 'returns nil if the token does not exist' do
