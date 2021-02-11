c6bed62aa0f26fda4dce815410963853178f3f52
Michael Nacos <m.nacos@gmail.com>
2021-02-11 15:35:25 +0000

    Use raw SQL only for the application choices join

--- app/queries/get_activity_log_events.rb
+++ app/queries/get_activity_log_events.rb
@@ -25,6 +25,26 @@ class GetActivityLogEvents
   def self.call(application_choices:, since: nil)
     since ||= Time.zone.local(2018, 1, 1) # before the pilot began, i.e. all records
 
+    application_choices_join_sql = <<~COMBINE_AUDITS_WITH_APPLICATION_CHOICES_SCOPE_AND_FILTER.squish
+      INNER JOIN (#{application_choices.to_sql}) ac
+        ON (
+          auditable_type = 'ApplicationChoice'
+          AND auditable_id = ac.id
+          AND action = 'update'
+          AND ( #{application_choice_audits_filter_sql} )
+        ) OR (
+          associated_type = 'ApplicationChoice'
+          AND associated_id = ac.id
+        )
+    COMBINE_AUDITS_WITH_APPLICATION_CHOICES_SCOPE_AND_FILTER
+
+    Audited::Audit.includes(INCLUDES)
+                  .joins(application_choices_join_sql)
+                  .where('audits.created_at >= ?', since)
+                  .order('audits.created_at DESC')
+  end
+
+  def self.application_choice_audits_filter_sql
     application_choice_audits_filter = INCLUDE_APPLICATION_CHOICE_CHANGES_TO.map { |f|
       "jsonb_exists(audited_changes, '#{f}')"
     }.join(' OR ')
@@ -32,27 +52,6 @@ class GetActivityLogEvents
     filtered_statuses = ApplicationStateChange::STATES_VISIBLE_TO_PROVIDER - IGNORE_STATUS
     status_transitions_to_include = filtered_statuses.map { |status| "'#{status}'" }.join(', ')
 
-    application_choice_audits_filter += " OR audited_changes::json#>>'{status, 1}' IN (#{status_transitions_to_include})"
-
-    Audited::Audit.includes(INCLUDES).from <<~COMBINE_AUDITS_WITH_APPLICATION_CHOICES_SCOPE_AND_FILTER.squish
-      (
-        SELECT a.*
-          FROM audits a
-
-          INNER JOIN (#{application_choices.to_sql}) ac
-            ON (
-              auditable_type = 'ApplicationChoice'
-              AND auditable_id = ac.id
-              AND action = 'update'
-              AND ( #{application_choice_audits_filter} )
-            ) OR (
-              associated_type = 'ApplicationChoice'
-              AND associated_id = ac.id
-            )
-
-          WHERE a.created_at >= '#{since.iso8601}'::TIMESTAMPTZ
-          ORDER BY a.created_at DESC
-      ) AS audits
-    COMBINE_AUDITS_WITH_APPLICATION_CHOICES_SCOPE_AND_FILTER
+    application_choice_audits_filter + " OR audited_changes::json#>>'{status, 1}' IN (#{status_transitions_to_include})"
   end
 end
