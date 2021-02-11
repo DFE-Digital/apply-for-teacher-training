6e8534d36f0d8aabc8ddbb4ad9e446f7b5bb3f48
benswannack <ben.swannack@softwire.com>
2021-02-11 10:36:06 +0000

    Refactor interview and course details UI into component

---
+++ app/components/provider_interface/interview_and_course_summary_component.rb
@@ -0,0 +1,35 @@
+module ProviderInterface
+  class InterviewAndCourseSummaryComponent < ViewComponent::Base
+    attr_reader :interview, :application_choice
+
+    def initialize(interview:)
+      @application_choice = interview.application_choice
+      @interview = interview
+    end
+
+    def rows
+      [
+        {
+          key: 'Course',
+          value: interview.offered_course.name,
+        },
+        {
+          key: 'Funding type',
+          value: interview.offered_course.funding_type,
+        },
+        {
+          key: 'Organisation carrying out interview',
+          value: interview.provider.name,
+        },
+        {
+          key: 'Address or online meeting details',
+          value: interview.location,
+        },
+        {
+          key: 'Additional details',
+          value: interview.additional_details,
+        },
+      ]
+    end
+  end
+end
---
+++ spec/components/provider_interface/interview_and_course_summary_component_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe ProviderInterface::InterviewAndCourseSummaryComponent do
+  let(:interview) { create(:interview) }
+  let(:component) { render_inline(described_class.new(interview: interview)).text }
+
+  it 'displays interview preferences' do
+    expect(component).to include(interview.application_choice.application_form.interview_preferences)
+  end
+
+  it 'displays the provider name' do
+    expect(component).to include(interview.application_choice.course.provider.name)
+  end
+
+  it 'displays the course name' do
+    expect(component).to include(interview.application_choice.course.name)
+  end
+
+  it 'displays interview location' do
+    expect(component).to include(interview.location)
+  end
+end
