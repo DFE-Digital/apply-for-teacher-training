c92a543a90f6ba9b3af02c9cc12e4c2210461bae
David Gisbey <davidgisbey@gmail.com>
2021-02-18 15:12:34 +0000

    Use the new form in the controller and view

--- app/controllers/candidate_interface/gcse/year_controller.rb
+++ app/controllers/candidate_interface/gcse/year_controller.rb
@@ -1,25 +1,34 @@
 module CandidateInterface
   class Gcse::YearController < Gcse::BaseController
     def edit
-      @application_qualification = details_form
-      @qualification_type = details_form.qualification.qualification_type
+      @year_form = CandidateInterface::GcseYearForm.build_from_qualification(current_qualification)
     end
 
     def update
-      @application_qualification = details_form
-      @qualification_type = details_form.qualification.qualification_type
+      @year_form = CandidateInterface::GcseYearForm.new(year_params)
 
-      details_form.award_year = details_params[:award_year]
-
-      if @application_qualification.save_year
+      if @year_form.save(current_qualification)
         update_gcse_completed(false)
 
         redirect_to candidate_interface_gcse_review_path
       else
-        track_validation_error(@application_qualification)
+        track_validation_error(@year_form)
 
         render :edit
       end
+    end
+
+  private
+
+    def year_params
+      strip_whitespace params
+        .require(:candidate_interface_gcse_year_form)
+        .permit(:award_year)
+        .merge!(qualification_type: current_qualification.qualification_type)
+    end
+
+    def current_qualification
+      current_application.qualification_in_subject(:gcse, subject_param)
     end
   end
 end
