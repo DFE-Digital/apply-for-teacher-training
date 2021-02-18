f59b489d50668d438239fd9b2c0f29fa4fd5bb10
Despo Pentara <despo@extractmethod.com>
2021-02-18 16:16:05 +0000

    Refactor ApplyAgainBannerComponent
    and use new Notification banner

--- app/components/candidate_interface/apply_again_banner_component.rb
+++ app/components/candidate_interface/apply_again_banner_component.rb
@@ -1,23 +1,41 @@
 module CandidateInterface
   class ApplyAgainBannerComponent < ViewComponent::Base
     include ViewHelper
+    attr_reader :application_form
+
     validates :application_form, presence: true
 
     def initialize(application_form:)
       @application_form = application_form
     end
 
-    def render?
-      !EndOfCycleTimetable.between_cycles_apply_2? &&
-        @application_form.recruitment_cycle_year == RecruitmentCycle.current_year
+    def all_withdrawn_copy
+      'Your application has been withdrawn.' if all_applications_withdrawn?
     end
 
-    def show_deadline_copy?
-      EndOfCycleTimetable.show_apply_2_deadline_banner? && FeatureFlag.active?(:deadline_notices)
+    def deadline_copy
+      return nil unless show_deadline_copy?
+
+      "The deadline when applying again is #{apply_2_deadline_date} for courses starting this academic year."
+    end
+
+    def render?
+      !EndOfCycleTimetable.between_cycles_apply_2? &&
+        application_form.recruitment_cycle_year == RecruitmentCycle.current_year
     end
 
     def start_path
       candidate_interface_start_apply_again_path
+    end
+
+  private
+
+    def all_applications_withdrawn?
+      application_form.application_choices.all?(&:cancelled?)
+    end
+
+    def show_deadline_copy?
+      EndOfCycleTimetable.show_apply_2_deadline_banner? && FeatureFlag.active?(:deadline_notices)
     end
 
     def apply_2_deadline_date
