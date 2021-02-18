b735b39d8dd230731d0ff8f852c611d8a938d882
benswannack <ben.swannack@softwire.com>
2021-02-18 13:10:07 +0000

    Refactor applications controller to use its own includes and commonise the call to GetApplicationChoicesForProviders

--- app/controllers/vendor_api/applications_controller.rb
+++ app/controllers/vendor_api/applications_controller.rb
@@ -9,16 +9,28 @@ module VendorAPI
     end
 
     def show
-      application_choice = GetApplicationChoicesForProviders.call(providers: current_provider, vendor_api: true)
-        .find(params[:application_id])
+      application_choice = application_choices_visible_to_provider.find(params[:application_id])
 
       render json: { data: SingleApplicationPresenter.new(application_choice).as_json }
     end
 
   private
 
+    def application_choices_visible_to_provider
+      GetApplicationChoicesForProviders
+        .call(
+          providers: current_provider,
+          vendor_api: true,
+          includes: [
+            application_form: %i[candidate application_qualifications application_work_experiences application_work_history_breaks application_volunteering_experiences english_proficiency],
+            course_option: [{ course: %i[provider] }, :site],
+            offered_course_option: [{ course: %i[provider] }, :site],
+          ],
+        )
+    end
+
     def get_application_choices_for_provider_since(since:)
-      GetApplicationChoicesForProviders.call(providers: current_provider, vendor_api: true)
+      application_choices_visible_to_provider
         .where('application_choices.updated_at > ?', since)
     end
 
