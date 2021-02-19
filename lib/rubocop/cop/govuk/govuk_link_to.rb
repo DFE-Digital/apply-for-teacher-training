f185cc785308b7b47bbcc1ed92750f382e79e3e6
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2021-02-19 13:25:13 +0000

    Remove helper for footer links

--- lib/rubocop/cop/govuk/govuk_link_to.rb
+++ lib/rubocop/cop/govuk/govuk_link_to.rb
@@ -5,7 +5,7 @@ module RuboCop
         def on_send(node)
           return unless node.method_name == :link_to
 
-          add_offense(node, message: 'Use govuk_link_to, govuk_footer_link_to, govuk_back_link_to or govuk_button_link_to instead of link_to')
+          add_offense(node, message: 'Use govuk_link_to, govuk_back_link_to or govuk_button_link_to instead of link_to')
         end
       end
     end
