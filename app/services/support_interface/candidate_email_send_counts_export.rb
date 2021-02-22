4f9e89b60e5b28383e427aba8cf3eecba450e70b
Malcolm Baig <malcolm.baig@gmail.com>
2021-02-22 12:35:09 +0000

    Simplify queries in CandidateEmailSendCountsExport
    Consolidate the separate queries by writing better SQL.

--- app/services/support_interface/candidate_email_send_counts_export.rb
+++ app/services/support_interface/candidate_email_send_counts_export.rb
@@ -2,35 +2,21 @@ module SupportInterface
   class CandidateEmailSendCountsExport
     def data_for_export
       emails_with_total_send_counts.map do |send_count_record|
-        unique_recipients = unique_recipients_hash[send_count_record.mail_template]
         {
           'Email' => send_count_record.mail_template,
           'Send count' => send_count_record.send_count,
           'Last sent at' => send_count_record.last_sent_at,
-          'Unique recipients' => unique_recipients,
+          'Unique recipients' => send_count_record.unique_recipients,
         }
       end
     end
 
   private
 
-    def unique_recipients_per_email
-      distinct_mail_templates_and_recipients = Email.select('DISTINCT "to", "mail_template"').to_sql
-      Email
-        .select('mail_template, COUNT(mail_template) AS unique_recipients')
-        .from("(#{distinct_mail_templates_and_recipients}) AS template_recipient_combos GROUP BY template_recipient_combos.mail_template")
-    end
-
-    def unique_recipients_hash
-      unique_recipients_per_email.each_with_object({}) do |recipient_count_record, hash|
-        hash[recipient_count_record.mail_template] = recipient_count_record.unique_recipients
-      end
-    end
-
     def emails_with_total_send_counts
       Email
         .select(
-          'mail_template, COUNT(mail_template) AS send_count, MAX(created_at) AS last_sent_at',
+          'mail_template, COUNT(mail_template) AS send_count, COUNT(DISTINCT "to") AS unique_recipients, MAX(created_at) AS last_sent_at',
         )
         .group('mail_template')
         .order('mail_template ASC')
--- spec/services/support_interface/candidate_email_send_counts_export_spec.rb
+++ spec/services/support_interface/candidate_email_send_counts_export_spec.rb
@@ -3,26 +3,32 @@ require 'rails_helper'
 RSpec.describe SupportInterface::CandidateEmailSendCountsExport do
   describe '#data_for_export' do
     it 'returns a hash of email counts from the emails table' do
-      yesterday = Time.zone.yesterday
-      two_days_ago = Time.zone.today - 2.days
-      most_recent_app_submitted_email = create(:email, mail_template: 'application_submitted', created_at: yesterday)
-      create(:email, mail_template: 'application_submitted', created_at: two_days_ago)
-      most_recent_conditions_met_email = create(:email, mail_template: 'conditions_met', created_at: two_days_ago)
+      Timecop.freeze(Time.zone.now.round) do
+        yesterday = Time.zone.now - 1.day
+        two_days_ago = Time.zone.now - 2.days
 
-      expect(described_class.new.data_for_export).to match_array([
-        {
-          'Email' => 'application_submitted',
-          'Send count' => 2,
-          'Last sent at' => most_recent_app_submitted_email.created_at,
-          'Unique recipients' => 1,
-        },
-        {
-          'Email' => 'conditions_met',
-          'Send count' => 1,
-          'Last sent at' => most_recent_conditions_met_email.created_at,
-          'Unique recipients' => 1,
-        },
-      ])
+        'application_submitted'.tap do |mail_template|
+          create(:email, mail_template: mail_template, created_at: yesterday)
+          create(:email, mail_template: mail_template, created_at: two_days_ago)
+          create(:email, mail_template: mail_template, created_at: two_days_ago, to: 'another_recipient@email.com')
+        end
+        create(:email, mail_template: 'conditions_met', created_at: two_days_ago)
+
+        expect(described_class.new.data_for_export).to match_array([
+          {
+            'Email' => 'application_submitted',
+            'Send count' => 3,
+            'Last sent at' => yesterday.utc,
+            'Unique recipients' => 2,
+          },
+          {
+            'Email' => 'conditions_met',
+            'Send count' => 1,
+            'Last sent at' => two_days_ago.utc,
+            'Unique recipients' => 1,
+          },
+        ])
+      end
     end
   end
 end
