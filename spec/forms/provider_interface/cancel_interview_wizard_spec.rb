1cc2fa43ec9d4ce06dd7b16aa69cc536fc0925c8
Aga Dufrat <a.dufrat@gmail.com>
2021-02-22 14:32:09 +0000

    Add a word limit to interview free text fields

--- spec/forms/provider_interface/cancel_interview_wizard_spec.rb
+++ spec/forms/provider_interface/cancel_interview_wizard_spec.rb
@@ -5,7 +5,11 @@ RSpec.describe ProviderInterface::CancelInterviewWizard do
   let(:subject) { described_class.new(store) }
 
   describe '.validations' do
+    valid_text = Faker::Lorem.sentence(word_count: 2000)
+    invalid_text = Faker::Lorem.sentence(word_count: 2001)
+
     it { is_expected.to validate_presence_of(:cancellation_reason) }
-    it { is_expected.to validate_length_of(:cancellation_reason).is_at_most(10240) }
+    it { is_expected.to allow_value(valid_text).for(:cancellation_reason) }
+    it { is_expected.not_to allow_value(invalid_text).for(:cancellation_reason) }
   end
 end
