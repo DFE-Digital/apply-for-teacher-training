9818d71764d8fd8766661ad1a159c076f9e7c2d7
Despo Pentara <despo@extractmethod.com>
2021-03-01 15:14:46 +0000

    Raise errors if service fails
    - if the user does not have correct permissions and
    - if the application choice cannot transition to the offer state
    
    Check that all expected service calls take place when no failure

--- app/services/make_offer.rb
+++ app/services/make_offer.rb
@@ -12,9 +12,14 @@ class MakeOffer
   end
 
   def save!
-    MakeAnOffer.new(actor: actor,
-                    application_choice: application_choice,
-                    course_option: course_option,
-                    offer_conditions: conditions)
+    make_an_offer = MakeAnOffer.new(actor: actor,
+                                    application_choice: application_choice,
+                                    course_option: course_option,
+                                    offer_conditions: conditions)
+    make_an_offer.save
+
+    if make_an_offer.errors[:base].include?(I18n.t('activerecord.errors.models.application_choice.attributes.status.invalid_transition'))
+      ApplicationStateChange.new(application_choice).make_offer!
+    end
   end
 end
--- spec/services/make_offer_spec.rb
+++ spec/services/make_offer_spec.rb
@@ -14,12 +14,54 @@ RSpec.describe MakeOffer do
     let(:application_choice) { create(:application_choice) }
     let(:course_option) { course_option_for_provider(provider: application_choice.course_option.provider) }
     let(:provider_user) { create(:provider_user, providers: [build(:provider)]) }
-    let(:conditions) { [ Faker::Lorem.sentence ] }
+    let(:conditions) { [Faker::Lorem.sentence] }
 
-    it 'throws an exception if the actor is not authorised to perform this action' do
-      expect {
+    describe 'if the actor is not authorised to perform this action' do
+      it 'throws an exception' do
+        expect {
+          make_offer.save!
+        }.to raise_error(ProviderAuthorisation::NotAuthorisedError, 'You are not allowed to make decisions')
+      end
+    end
+
+    describe 'if the application choice cannot transition to the offer state' do
+      let(:application_choice) { create(:application_choice, status: :pending_conditions) }
+      let(:provider_user) do
+        create(:provider_user,
+               :with_make_decisions,
+               providers: [application_choice.course_option.provider])
+      end
+
+      it 'throws an exception' do
+        expect {
+          make_offer.save!
+        }.to raise_error(Workflow::NoTransitionAllowed, 'There is no event make_offer defined for the pending_conditions state')
+      end
+    end
+
+    describe 'if the provided details are correct' do
+      let(:application_choice) { create(:application_choice, status: :awaiting_provider_decision) }
+      let(:provider_user) do
+        create(:provider_user,
+               :with_make_decisions,
+               providers: [application_choice.course_option.provider])
+      end
+
+      it 'then it executes the service without errors ' do
+        set_declined_by_default = instance_double(SetDeclineByDefault, call: true)
+        send_new_offer_email_to_candidate = instance_double(SendNewOfferEmailToCandidate, call: true)
+        allow(SetDeclineByDefault)
+          .to receive(:new).with(application_form: application_choice.application_form)
+          .and_return(set_declined_by_default)
+        allow(SendNewOfferEmailToCandidate)
+          .to receive(:new).with(application_choice: application_choice)
+          .and_return(send_new_offer_email_to_candidate)
+
         make_offer.save!
-      }.to raise_error(ProviderAuthorisation::NotAuthorisedError, 'You are not allowed to make decisions')
+
+        expect(SetDeclineByDefault).to have_received(:new).with(application_form: application_choice.application_form)
+        expect(SendNewOfferEmailToCandidate).to have_received(:new).with(application_choice: application_choice)
+      end
     end
   end
 end
