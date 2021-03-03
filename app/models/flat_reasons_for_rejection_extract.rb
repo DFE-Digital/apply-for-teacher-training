497e9526c753da308deba844574be13b43cf2c74
JRG <47917431+JR-G@users.noreply.github.com>
2021-03-03 16:04:40 +0000

    Finalised flat r4r model and built spec
    Finishing model and adding Unit spec to test functionality.

--- app/models/flat_reasons_for_rejection_extract.rb
+++ app/models/flat_reasons_for_rejection_extract.rb
@@ -1,98 +1,80 @@
 class FlatReasonsForRejectionExtract
   include ActiveModel::Model
 
-  attr_accessor :structured_rejection_reasons_hash
+  attr_accessor :structured_rejection_reasons_hash, :candidate_behaviour, :didnt_reply_to_interview_offer, :didnt_attend_interview,
+                :candidate_behaviour_other_details, :candidate_behaviour_what_to_improve_details, :quality_of_application, :personal_statement,
+                :quality_of_application_personal_statement_what_to_improve, :subject_knowledge, :quality_of_application_subject_knowledge_what_to_improve_details,
+                :quality_of_application_other_what_to_improve_details, :quality_of_application_other_details, :qualifications, :no_maths_gcse,
+                :no_english_gcse, :no_science_gcse, :no_degree, :qualifications_other_details, :performance_at_interview,
+                :performance_at_interview_what_to_improve_details, :course_full, :offered_on_another_course, :honesty_and_professionalism,
+                :information_false_or_inaccurate, :honesty_and_professionalism_concerns_information_false_or_inaccurate_details,
+                :plagiarism, :honesty_and_professionalism_concerns_plagiarism_details, :references, :honesty_and_professionalism_concerns_references_details,
+                :honesty_and_professionalism_concerns_other_details, :safeguarding, :candidate_disclosed_information,
+                :safeguarding_concerns_candidate_disclosed_information_details, :vetting_disclosed_information, :safeguarding_concerns_vetting_disclosed_information_details,
+                :safeguarding_concerns_other_details, :other_advice_or_feedback, :interested_in_future_applications, :why_are_you_rejecting_this_application_details
 
-  def initialize(structured_rejection_reasons)
-    @structured_rejection_reasons = structured_rejection_reasons
+  def self.build_from_hash(structured_rejection_reasons)
+    new(
+      candidate_behaviour: word_to_boolean(structured_rejection_reasons['candidate_behaviour_y_n']),
+      didnt_reply_to_interview_offer: subreason_exists?(structured_rejection_reasons['candidate_behaviour_what_did_the_candidate_do'], 'didnt_reply_to_interview_offer'),
+      didnt_attend_interview: subreason_exists?(structured_rejection_reasons['candidate_behaviour_what_did_the_candidate_do'], 'didnt_attend_interview'),
+      candidate_behaviour_other_details: structured_rejection_reasons['candidate_behaviour_other'],
+      candidate_behaviour_what_to_improve_details: structured_rejection_reasons['candidate_behaviour_what_to_improve'],
+      quality_of_application: word_to_boolean(structured_rejection_reasons['quality_of_application_y_n']),
+      personal_statement: subreason_exists?(structured_rejection_reasons['quality_of_application_which_parts_needed_improvement'], 'personal_statement'),
+      quality_of_application_personal_statement_what_to_improve: structured_rejection_reasons['quality_of_application_personal_statement_what_to_improve'],
+      subject_knowledge: subreason_exists?(structured_rejection_reasons['quality_of_application_which_parts_needed_improvement'], 'subject_knowledge'),
+      quality_of_application_subject_knowledge_what_to_improve_details: structured_rejection_reasons['quality_of_application_subject_knowledge_what_to_improve'],
+      quality_of_application_other_what_to_improve_details: structured_rejection_reasons['quality_of_application_other_what_to_improve'],
+      quality_of_application_other_details: structured_rejection_reasons['quality_of_application_other_details'],
+      qualifications: word_to_boolean(structured_rejection_reasons['qualifications_y_n']),
+      no_maths_gcse: subreason_exists?(structured_rejection_reasons['qualifications_which_qualifications'], 'no_maths_gcse'),
+      no_english_gcse: subreason_exists?(structured_rejection_reasons['qualifications_which_qualifications'], 'no_english_gcse'),
+      no_science_gcse: subreason_exists?(structured_rejection_reasons['qualifications_which_qualifications'], 'no_science_gcse'),
+      no_degree: subreason_exists?(structured_rejection_reasons['qualifications_which_qualifications'], 'no_degree'),
+      qualifications_other_details: structured_rejection_reasons['qualifications_other_details'],
+      performance_at_interview: word_to_boolean(structured_rejection_reasons['performance_at_interview_y_n']),
+      performance_at_interview_what_to_improve_details: structured_rejection_reasons['performance_at_interview_what_to_improve'],
+      course_full: word_to_boolean(structured_rejection_reasons['course_full_y_n']),
+      offered_on_another_course: word_to_boolean(structured_rejection_reasons['offered_on_another_course_y_n']),
+      honesty_and_professionalism: word_to_boolean(structured_rejection_reasons['honesty_and_professionalism_y_n']),
+      information_false_or_inaccurate: subreason_exists?(structured_rejection_reasons['honesty_and_professionalism_concerns'], 'information_false_or_inaccurate'),
+      honesty_and_professionalism_concerns_information_false_or_inaccurate_details: structured_rejection_reasons['honesty_and_professionalism_concerns_information_false_or_inaccurate_details'],
+      plagiarism: subreason_exists?(structured_rejection_reasons['honesty_and_professionalism_concerns'], 'plagiarism'),
+      honesty_and_professionalism_concerns_plagiarism_details: structured_rejection_reasons['honesty_and_professionalism_concerns_plagiarism_details'],
+      references: subreason_exists?(structured_rejection_reasons['honesty_and_professionalism_concerns'], 'references'),
+      honesty_and_professionalism_concerns_references_details: structured_rejection_reasons['honesty_and_professionalism_concerns_references_details'],
+      honesty_and_professionalism_concerns_other_details: structured_rejection_reasons['honesty_and_professionalism_concerns_other_details'],
+      safeguarding: word_to_boolean(structured_rejection_reasons['safeguarding_y_n']),
+      candidate_disclosed_information: subreason_exists?(structured_rejection_reasons['safeguarding_concerns'], 'candidate_disclosed_information'),
+      safeguarding_concerns_candidate_disclosed_information_details: structured_rejection_reasons['safeguarding_concerns_candidate_disclosed_information_details'],
+      vetting_disclosed_information: subreason_exists?(structured_rejection_reasons['safeguarding_concerns'], 'vetting_disclosed_information'),
+      safeguarding_concerns_vetting_disclosed_information_details: structured_rejection_reasons['safeguarding_concerns_vetting_disclosed_information_details'],
+      safeguarding_concerns_other_details: structured_rejection_reasons['safeguarding_concerns_other_details'],
+      other_advice_or_feedback: word_to_boolean(structured_rejection_reasons['other_advice_or_feedback_y_n']),
+      interested_in_future_applications: word_to_boolean(structured_rejection_reasons['interested_in_future_applications_y_n']),
+      why_are_you_rejecting_this_application_details: structured_rejection_reasons['why_are_you_rejecting_this_application'],
+    )
   end
 
-  def build_hash
-    @structured_rejection_reasons_hash = @structured_rejection_reasons.merge(mapped_rejection_reasons)
-    @structured_rejection_reasons_hash.each do |key, value|
-      self.class.send :define_method, key do value end
-    end
-  end
+  def self.build_high_level(structured_rejection_reasons)
+    return nil if structured_rejection_reasons.blank?
 
-  def mapped_rejection_reasons
-    { 'candidate_behaviour?' => top_level_present?('candidate_behaviour_y_n'),
-      'didnt_reply_to_interview_offer?' => sub_level_present?('candidate_behaviour_what_did_the_candidate_do', 'didnt_reply_to_interview_offer'),
-      'didnt_attend_interview?' => sub_level_present?('candidate_behaviour_what_did_the_candidate_do', 'didnt_attend_interview'),
-      'candidate_behaviour_other_details' => sub_level_details_present?('candidate_behaviour_other'),
-      'candidate_behaviour_what_to_improve_details' => sub_level_details_present?('candidate_behaviour_what_to_improve'),
-      'quality_of_application?' => top_level_present?('quality_of_application_y_n'),
-      'personal_statement?' => sub_level_present?('quality_of_application_which_parts_needed_improvement', 'personal_statement'),
-      'quality_of_application_personal_statement_what_to_improve' => sub_level_details_present?('quality_of_application_personal_statement_what_to_improve'),
-      'subject_knowledge?' => sub_level_present?('quality_of_application_which_parts_needed_improvement', 'subject_knowledge'),
-      'quality_of_application_subject_knowledge_what_to_improve_details' => sub_level_details_present?('quality_of_application_subject_knowledge_what_to_improve'),
-      'quality_of_application_other_what_to_improve_details' => sub_level_details_present?('quality_of_application_other_what_to_improve'),
-      'quality_of_application_other_details' => sub_level_details_present?('quality_of_application_other_details'),
-      'qualifications?' => top_level_present?('qualifications_y_n'),
-      'no_maths_gcse?' => sub_level_present?('qualifications_which_qualifications', 'no_maths_gcse'),
-      'no_english_gcse?' => sub_level_present?('qualifications_which_qualifications', 'no_english_gcse'),
-      'no_science_gcse?' => sub_level_present?('qualifications_which_qualifications', 'no_science_gcse'),
-      'no_degree?' => sub_level_present?('qualifications_which_qualifications', 'no_degree'),
-      'qualifications_other_details' => sub_level_details_present?('qualifications_other_details'),
-      'performance_at_interview?' => top_level_present?('performance_at_interview_y_n'),
-      'performance_at_interview_what_to_improve_details' => sub_level_details_present?('performance_at_interview_what_to_improve'),
-      'course_full?' => top_level_present?('course_full_y_n'),
-      'offered_on_another_course?' => top_level_present?('offered_on_another_course_y_n'),
-      'honesty_and_professionalism?' => top_level_present?('honesty_and_professionalism_y_n'),
-      'information_false_or_inaccurate?' => sub_level_present?('honesty_and_professionalism_concerns', 'information_false_or_inaccurate'),
-      'honesty_and_professionalism_concerns_information_false_or_inaccurate_details' => sub_level_details_present?('honesty_and_professionalism_concerns_information_false_or_inaccurate_details'),
-      'plagiarism?' => sub_level_present?('honesty_and_professionalism_concerns', 'plagiarism'),
-      'honesty_and_professionalism_concerns_plagiarism_details' => sub_level_details_present?('honesty_and_professionalism_concerns_plagiarism_details'),
-      'references?' => sub_level_present?('honesty_and_professionalism_concerns', 'references'),
-      'honesty_and_professionalism_concerns_references_details' => sub_level_details_present?('honesty_and_professionalism_concerns_references_details'),
-      'honesty_and_professionalism_concerns_other_details' => sub_level_details_present?('honesty_and_professionalism_concerns_other_details'),
-      'safeguarding?' => top_level_present?('safeguarding_y_n'),
-      'candidate_disclosed_information?' => sub_level_present?('safeguarding_concerns', 'candidate_disclosed_information'),
-      'safeguarding_concerns_candidate_disclosed_information_details' => sub_level_details_present?('safeguarding_concerns_candidate_disclosed_information_details'),
-      'vetting_disclosed_information?' => sub_level_present?('safeguarding_concerns', 'vetting_disclosed_information'),
-      'safeguarding_concerns_vetting_disclosed_information_details' => sub_level_details_present?('safeguarding_concerns_vetting_disclosed_information_details'),
-      'safeguarding_concerns_other_details' => sub_level_details_present?('safeguarding_concerns_other_details'),
-      'other_advice_or_feedback?' => top_level_present?('other_advice_or_feedback_y_n'),
-      'interested_in_future_applications?' => top_level_present?('interested_in_future_applications_y_n'),
-      'why_are_you_rejecting_this_application_details' => sub_level_details_present?('why_are_you_rejecting_this_application') }
-  end
-
-  # These three methods are copied from the application_choices_export.rb & equality_and_diversity_export.rb - will be refactored later
-  def formatted
-    return nil if @structured_rejection_reasons.blank?
-
-    select_top_level_rejection_reasons(@structured_rejection_reasons)
+    structured_rejection_reasons.select { |reason, value| value == 'Yes' && reason.include?('_y_n') }
     .keys
-    .map { |reason| format_reason(reason) }
+    .map { |reason| reason.delete_suffix('_y_n').humanize }
     .join("\n")
   end
 
-  def select_top_level_rejection_reasons(structured_rejection_reasons)
-    structured_rejection_reasons.select { |reason, value| value == 'Yes' && reason.include?('_y_n') }
+  def self.word_to_boolean(word)
+    return false if word.nil?
+
+    word == 'Yes'
   end
 
-  def format_reason(reason)
-    reason
-    .delete_suffix('_y_n')
-    .humanize
-  end
+  def self.subreason_exists?(reason, subreason)
+    return false if reason.nil?
 
-private
-
-  def sub_level_present?(key, value)
-    return false if @structured_rejection_reasons[key.to_s].blank?
-
-    @structured_rejection_reasons[key.to_s].include?(value.to_s)
-  end
-
-  def top_level_present?(key)
-    return false if @structured_rejection_reasons[key.to_s].blank?
-
-    @structured_rejection_reasons.select { |reason, value| value == 'Yes' && reason == key.to_s }.present?
-  end
-
-  def sub_level_details_present?(key)
-    return false if @structured_rejection_reasons[key.to_s].blank?
-
-    @structured_rejection_reasons[key.to_s]
+    reason.include?(subreason)
   end
 end
---
+++ spec/models/flat_reasons_for_rejection_extract_spec.rb
@@ -0,0 +1,68 @@
+require 'rails_helper'
+
+RSpec.describe FlatReasonsForRejectionExtract, type: :model do
+  let(:application_choice) do
+    create(
+      :application_choice,
+      :with_structured_rejection_reasons,
+    )
+  end
+
+  describe '.build_from_hash' do
+    it 'creates an object based on the provided rejected ApplicationChoice' do
+      rejection_export = FlatReasonsForRejectionExtract.build_from_hash(application_choice.structured_rejection_reasons)
+
+      expect(rejection_export).to have_attributes(
+        candidate_behaviour: true,
+        didnt_reply_to_interview_offer: true,
+        didnt_attend_interview: true,
+        candidate_behaviour_other_details: 'Persistent scratching',
+        candidate_behaviour_what_to_improve_details: 'Not scratch so much',
+        quality_of_application: true,
+        personal_statement: true,
+        quality_of_application_personal_statement_what_to_improve: 'Use a spellchecker',
+        subject_knowledge: true,
+        quality_of_application_subject_knowledge_what_to_improve_details: 'Claiming to be the \'world\'s leading expert\' seemed a bit strong',
+        quality_of_application_other_what_to_improve_details: 'Study harder',
+        quality_of_application_other_details: 'Lights on but nobody home',
+        qualifications: true,
+        no_maths_gcse: false,
+        no_english_gcse: true,
+        no_science_gcse: false,
+        no_degree: false,
+        qualifications_other_details: 'All the other stuff',
+        performance_at_interview: true,
+        performance_at_interview_what_to_improve_details: 'Be fully dressed',
+        course_full: false,
+        offered_on_another_course: false,
+        honesty_and_professionalism: true,
+        information_false_or_inaccurate: true,
+        honesty_and_professionalism_concerns_information_false_or_inaccurate_details: 'Fake news',
+        plagiarism: false,
+        honesty_and_professionalism_concerns_plagiarism_details: nil,
+        references: true,
+        honesty_and_professionalism_concerns_references_details: 'Clearly not a popular student',
+        honesty_and_professionalism_concerns_other_details: nil,
+        safeguarding: true,
+        candidate_disclosed_information: false,
+        safeguarding_concerns_candidate_disclosed_information_details: nil,
+        vetting_disclosed_information: false,
+        safeguarding_concerns_vetting_disclosed_information_details: nil,
+        safeguarding_concerns_other_details: 'We need to run further checks',
+        other_advice_or_feedback: false,
+        interested_in_future_applications: false,
+        why_are_you_rejecting_this_application_details: nil,
+      )
+    end
+  end
+
+  describe '.build_high_level' do
+    it 'creates a string containing the rejection reasons' do
+      rejection_export_line = FlatReasonsForRejectionExtract.build_high_level(application_choice.structured_rejection_reasons)
+
+      expect(rejection_export_line).to eq(
+        "Candidate behaviour\nHonesty and professionalism\nPerformance at interview\nQualifications\nQuality of application\nSafeguarding",
+      )
+    end
+  end
+end
