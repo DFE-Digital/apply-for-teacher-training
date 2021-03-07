205ea038219b36d8420745839c28d5bf5c5f2776
Steve Hook <steve.hook@gmail.com>
2021-03-07 22:07:35 +0000

    Simplify comparision logic
    We can just use `as_json` to extract the attributes and associations
    that are considered significant when changed. This makes the definition
    of those 'significant things' more declarative and easier to change.

--- app/services/candidate_interface/find_changed_apply_again_applications.rb
+++ app/services/candidate_interface/find_changed_apply_again_applications.rb
@@ -18,35 +18,19 @@ module CandidateInterface
     end
 
     def changed?(application_form)
-      personal_statement_changed?(application_form) ||
-        subject_knowledge_changed?(application_form) ||
-        qualifications_changed?(application_form)
+      significant_values(application_form) !=
+        significant_values(application_form.previous_application_form)
     end
 
-    def subject_knowledge_changed?(application_form)
-      application_form.subject_knowledge != application_form.previous_application_form.subject_knowledge
-    end
-
-    def personal_statement_changed?(application_form)
-      application_form.becoming_a_teacher != application_form.previous_application_form.becoming_a_teacher
-    end
-
-    def qualifications_changed?(application_form)
-      original_qualifications = application_form.previous_application_form.application_qualifications.order(:id)
-      new_qualifications = application_form.application_qualifications.order(:id)
-      return true if original_qualifications.size != new_qualifications.size
-
-      original_qualifications.any? { |qualification| any_qualification_differ?(qualification, new_qualifications) }
-    end
-
-    def any_qualification_differ?(original_qualification, new_qualifications)
-      new_qualifications.all? { |new_qualification| !qualifications_match?(original_qualification, new_qualification) }
-    end
-
-    def qualifications_match?(original_qualification, new_qualifications)
-      original_attributes = original_qualification.attributes.reject { |k, _| DuplicateApplication::IGNORED_CHILD_ATTRIBUTES.include?(k) }
-      new_attributes = new_qualifications.attributes.reject { |k, _| DuplicateApplication::IGNORED_CHILD_ATTRIBUTES.include?(k) }
-      original_attributes == new_attributes
+    def significant_values(application_form)
+      application_form.as_json(
+        only: %i[becoming_a_teacher subject_knowledge],
+        include: {
+          application_qualifications: {
+            except: DuplicateApplication::IGNORED_CHILD_ATTRIBUTES,
+          },
+        },
+      )
     end
   end
 end
