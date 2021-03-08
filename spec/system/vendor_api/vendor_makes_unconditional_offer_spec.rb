cdd5d56a836bdb8ea881422297ab4a5a02240453
cwrw <abeer.salameh@gmail.com>
2021-03-08 21:19:46 +0200

    Unset headers when making a POST request in tests
    Setting a page driver header in this test was causing other tests to fail as
    all forms were expected to send requests with the content type `application/json`.
    
    Unsetting the content type header seems to resolve the issue at the end of the test.
    I also removed setting the Capybara session as it didn't seem required.

--- spec/system/vendor_api/vendor_makes_unconditional_offer_spec.rb
+++ spec/system/vendor_api/vendor_makes_unconditional_offer_spec.rb
@@ -5,6 +5,7 @@ RSpec.feature 'Vendor makes unconditional offer' do
 
   scenario 'A vendor makes an unconditional offer and this is accepted by the candidate' do
     FeatureFlag.activate(:unconditional_offers_via_api)
+
     given_a_candidate_has_submitted_their_application
     when_i_make_an_unconditional_offer_for_the_application_over_the_api
     then_i_can_see_the_offer_was_made_successfully
@@ -14,26 +15,25 @@ RSpec.feature 'Vendor makes unconditional offer' do
   end
 
   def given_a_candidate_has_submitted_their_application
-    Capybara.session_name = 'candidate_interaction'
-    Capybara.using_session('candidate_interaction') do
-      candidate_completes_application_form
-      candidate_submits_application
-    end
+    candidate_completes_application_form
+    candidate_submits_application
   end
 
   def when_i_make_an_unconditional_offer_for_the_application_over_the_api
-    Capybara.session_name = 'api_request'
-    Capybara.using_session('api_request') do
-      api_token = VendorAPIToken.create_with_random_token!(provider: @provider)
-      page.driver.header 'Authorization', "Bearer #{api_token}"
-      page.driver.header 'Content-Type', 'application/json'
-      @application_choice = @application.application_choices.first
-      @course_option = @application_choice.course_option
-      @provider_user = create(:provider_user, send_notifications: true, providers: [@provider])
-      uri = "/api/v1/applications/#{@application_choice.id}/offer"
+    api_token = VendorAPIToken.create_with_random_token!(provider: @provider)
+    Capybara.current_session.driver.header('Authorization', "Bearer #{api_token}")
+    Capybara.current_session.driver.header('Content-Type', 'application/json')
 
-      @api_response = page.driver.post(uri, unconditional_offer_payload)
-    end
+    @application_choice = @application.application_choices.first
+    @course_option = @application_choice.course_option
+    @provider_user = create(:provider_user, send_notifications: true, providers: [@provider])
+    uri = "/api/v1/applications/#{@application_choice.id}/offer"
+
+    @api_response = page.driver.post(uri, unconditional_offer_payload)
+
+    # Unset session headers
+    Capybara.current_session.driver.header('Authorization', nil)
+    Capybara.current_session.driver.header('Content-Type', nil)
   end
 
   def then_i_can_see_the_offer_was_made_successfully
@@ -46,20 +46,16 @@ RSpec.feature 'Vendor makes unconditional offer' do
   end
 
   def when_the_candidate_accepts_the_unconditional_offer
-    Capybara.using_session('candidate_interaction') do
-      visit candidate_interface_offer_path(@application_choice)
+    visit candidate_interface_offer_path(@application_choice)
 
-      choose 'Accept offer and conditions'
-      click_button 'Continue'
+    choose 'Accept offer and conditions'
+    click_button 'Continue'
 
-      click_button 'Accept offer'
-    end
+    click_button 'Accept offer'
   end
 
   def then_the_candidate_sees_that_they_have_accepted_the_offer
-    Capybara.using_session('candidate_interaction') do
-      expect(page).to have_content "You have accepted your offer for #{@application_choice.course.name_and_code} at #{@application_choice.provider.name}"
-    end
+    expect(page).to have_content "You have accepted your offer for #{@application_choice.course.name_and_code} at #{@application_choice.provider.name}"
   end
 
   def unconditional_offer_payload
