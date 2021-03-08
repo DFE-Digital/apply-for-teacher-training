07cd1dc2748e956fdf5fab7fbc68db313303f706
Steve Laing <steve.laing@gmail.com>
2021-03-08 12:47:01 +0000

    Add unconditional offers feature flag (specific to API)

--- app/services/feature_flag.rb
+++ app/services/feature_flag.rb
@@ -29,6 +29,7 @@ class FeatureFlag
     [:restructured_work_history, 'Candidates use the new design for the Work History section', 'David Gisbey'],
     [:structured_reasons_for_rejection_on_rbd, 'Allows providers to give specific feedback for applications rejected by default', 'Aga Dufrat'],
     [:updated_offer_flow, 'Activates the new make offer flow for providers', 'Despo Pentara'],
+    [:unconditional_offers_via_api, 'Activates the ability to accept unconditional offers via the API', 'Steve Laing'],
   ].freeze
 
   FEATURES = (PERMANENT_SETTINGS + TEMPORARY_FEATURE_FLAGS).map { |name, description, owner|
