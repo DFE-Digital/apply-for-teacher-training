uk-gov-mirror/DFE-Digital.apply-for-teacher-training

Move VendorAPI docs under APIDocs namespace
As we add other APIDocs we can put them under this namespace too

app/helpers/navigation_items.rb
@@ -86,14 +86,14 @@ class NavigationItems
       end
     end
 
-    def for_api_docs(current_controller)
+    def for_vendor_api_docs(current_controller)
       [
         NavigationItem.new('Home', api_docs_home_path, is_active_action(current_controller, 'home')),
-        NavigationItem.new(t('page_titles.api_docs.usage'), api_docs_usage_path, is_active_action(current_controller, 'usage')),
-        NavigationItem.new(t('page_titles.api_docs.reference'), api_docs_reference_path, is_active_action(current_controller, 'reference')),
-        NavigationItem.new(t('page_titles.api_docs.release_notes'), api_docs_release_notes_path, is_active_action(current_controller, 'release_notes')),
-        NavigationItem.new(t('page_titles.api_docs.lifecycle'), api_docs_lifecycle_path, is_active_action(current_controller, 'lifecycle')),
-        NavigationItem.new(t('page_titles.api_docs.help'), api_docs_help_path, is_active_action(current_controller, 'help')),
+        NavigationItem.new(t('page_titles.api_docs.vendor_api.usage'), api_docs_usage_path, is_active_action(current_controller, 'usage')),
+        NavigationItem.new(t('page_titles.api_docs.vendor_api.reference'), api_docs_reference_path, is_active_action(current_controller, 'reference')),
+        NavigationItem.new(t('page_titles.api_docs.vendor_api.release_notes'), api_docs_release_notes_path, is_active_action(current_controller, 'release_notes')),
+        NavigationItem.new(t('page_titles.api_docs.vendor_api.lifecycle'), api_docs_lifecycle_path, is_active_action(current_controller, 'lifecycle')),
+        NavigationItem.new(t('page_titles.api_docs.vendor_api.help'), api_docs_help_path, is_active_action(current_controller, 'help')),
       ]
     end
 

config/routes.rb
@@ -957,16 +957,18 @@ Rails.application.routes.draw do
     get '*path', to: 'errors#not_found'
   end
 
-  namespace :api_docs, path: '/api-docs' do
-    get '/' => 'pages#home', as: :home
-    get '/usage-scenarios' => 'pages#usage', as: :usage
-    get '/reference' => 'reference#reference', as: :reference
-    get '/release-notes' => 'pages#release_notes', as: :release_notes
-    get '/alpha-release-notes' => 'pages#alpha_release_notes'
-    get '/lifecycle' => 'pages#lifecycle'
-    get '/when-emails-are-sent' => 'pages#when_emails_are_sent'
-    get '/help' => 'pages#help', as: :help
-    get '/spec.yml' => 'openapi#spec', as: :spec
+  namespace :api_docs, path: nil do
+    scope module: :vendor_api, path: '/api-docs' do
+      get '/' => 'pages#home', as: :home
+      get '/usage-scenarios' => 'pages#usage', as: :usage
+      get '/reference' => 'reference#reference', as: :reference
+      get '/release-notes' => 'pages#release_notes', as: :release_notes
+      get '/alpha-release-notes' => 'pages#alpha_release_notes'
+      get '/lifecycle' => 'pages#lifecycle'
+      get '/when-emails-are-sent' => 'pages#when_emails_are_sent'
+      get '/help' => 'pages#help', as: :help
+      get '/spec.yml' => 'openapi#spec', as: :spec
+    end
   end
 
   get '/check', to: 'healthcheck#show'

DFE-Digital/apply-for-teacher-training

app/helpers/navigation_items.rb
@@ -86,14 +86,14 @@ class NavigationItems
       end
     end
 
-    def for_api_docs(current_controller)
+    def for_vendor_api_docs(current_controller)
       [
         NavigationItem.new('Home', api_docs_home_path, is_active_action(current_controller, 'home')),
-        NavigationItem.new(t('page_titles.api_docs.usage'), api_docs_usage_path, is_active_action(current_controller, 'usage')),
-        NavigationItem.new(t('page_titles.api_docs.reference'), api_docs_reference_path, is_active_action(current_controller, 'reference')),
-        NavigationItem.new(t('page_titles.api_docs.release_notes'), api_docs_release_notes_path, is_active_action(current_controller, 'release_notes')),
-        NavigationItem.new(t('page_titles.api_docs.lifecycle'), api_docs_lifecycle_path, is_active_action(current_controller, 'lifecycle')),
-        NavigationItem.new(t('page_titles.api_docs.help'), api_docs_help_path, is_active_action(current_controller, 'help')),
+        NavigationItem.new(t('page_titles.api_docs.vendor_api_docs.usage'), api_docs_usage_path, is_active_action(current_controller, 'usage')),
+        NavigationItem.new(t('page_titles.api_docs.vendor_api_docs.reference'), api_docs_reference_path, is_active_action(current_controller, 'reference')),
+        NavigationItem.new(t('page_titles.api_docs.vendor_api_docs.release_notes'), api_docs_release_notes_path, is_active_action(current_controller, 'release_notes')),
+        NavigationItem.new(t('page_titles.api_docs.vendor_api_docs.lifecycle'), api_docs_lifecycle_path, is_active_action(current_controller, 'lifecycle')),
+        NavigationItem.new(t('page_titles.api_docs.vendor_api_docs.help'), api_docs_help_path, is_active_action(current_controller, 'help')),
       ]
     end
 

config/routes.rb
@@ -957,16 +957,18 @@ Rails.application.routes.draw do
     get '*path', to: 'errors#not_found'
   end
 
-  namespace :api_docs, path: '/api-docs' do
-    get '/' => 'pages#home', as: :home
-    get '/usage-scenarios' => 'pages#usage', as: :usage
-    get '/reference' => 'reference#reference', as: :reference
-    get '/release-notes' => 'pages#release_notes', as: :release_notes
-    get '/alpha-release-notes' => 'pages#alpha_release_notes'
-    get '/lifecycle' => 'pages#lifecycle'
-    get '/when-emails-are-sent' => 'pages#when_emails_are_sent'
-    get '/help' => 'pages#help', as: :help
-    get '/spec.yml' => 'openapi#spec', as: :spec
+  namespace :api_docs, path: nil do
+    scope module: :vendor_api_docs, path: '/api-docs' do
+      get '/' => 'pages#home', as: :home
+      get '/usage-scenarios' => 'pages#usage', as: :usage
+      get '/reference' => 'reference#reference', as: :reference
+      get '/release-notes' => 'pages#release_notes', as: :release_notes
+      get '/alpha-release-notes' => 'pages#alpha_release_notes'
+      get '/lifecycle' => 'pages#lifecycle'
+      get '/when-emails-are-sent' => 'pages#when_emails_are_sent'
+      get '/help' => 'pages#help', as: :help
+      get '/spec.yml' => 'openapi#spec', as: :spec
+    end
   end
 
   get '/check', to: 'healthcheck#show'