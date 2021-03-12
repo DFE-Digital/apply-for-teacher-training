7c0c6eb104f5f2bfdba3146333058c4f116d2a9f
Aga Dufrat <a.dufrat@gmail.com>
2021-03-12 18:08:31 +0000

    Allow provider users to manage granular notification preferences

--- app/controllers/provider_interface/notifications_controller.rb
+++ app/controllers/provider_interface/notifications_controller.rb
@@ -1,11 +1,15 @@
 module ProviderInterface
   class NotificationsController < ProviderInterfaceController
     def update
-      current_provider_user.update!(
-        send_notifications: notification_params[:send_notifications],
-      )
-
       if FeatureFlag.active?(:configurable_provider_notifications)
+        notification_preferences = current_provider_user.notification_preferences
+        notification_preferences.assign_attributes(notification_preferences_params)
+
+        notification_preferences.save! if notification_preferences.valid?
+      else
+        current_provider_user.update!(
+          send_notifications: notification_params[:send_notifications],
+        )
         current_provider_user.notification_preferences
           .update_all_preferences(notification_params[:send_notifications])
       end
@@ -17,7 +21,16 @@ module ProviderInterface
   private
 
     def notification_params
+      return {} unless params.key?(:provider_user)
+
       params.require(:provider_user).permit(:send_notifications)
+    end
+
+    def notification_preferences_params
+      return {} unless params.key?(:provider_user_notification_preferences)
+
+      params.require(:provider_user_notification_preferences)
+        .permit(ProviderUserNotificationPreferences::NOTIFICATION_PREFERENCES)
     end
   end
 end
