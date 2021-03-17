b7a947c39c9b5da65e124ab4b24518b8fb8e137f
Raam <raam.chauhan@softwire.com>
2021-03-17 14:53:32 +0000

    Add equality and diversity doc

--- app/services/support_interface/equality_and_diversity_export.rb
+++ app/services/support_interface/equality_and_diversity_export.rb
@@ -5,24 +5,24 @@ module SupportInterface
         rejected_application_choices = application_form.application_choices.rejected
 
         output = {
-          'Month' => application_form.submitted_at&.strftime('%B'),
-          'Recruitment cycle year' => application_form.recruitment_cycle_year,
-          'Sex' => application_form.equality_and_diversity['sex'],
-          'Ethnic group' => application_form.equality_and_diversity['ethnic_group'],
-          'Ethnic background' => application_form.equality_and_diversity['ethnic_background'],
-          'Application status' => I18n.t!("candidate_flow_application_states.#{ProcessState.new(application_form).state}.name"),
-          'Application choice 1 unstructured rejection reasons' => rejected_application_choices[0]&.rejection_reason,
-          'Application choice 2 unstructured rejection reasons' => rejected_application_choices[1]&.rejection_reason,
-          'Application choice 3 unstructured rejection reasons' => rejected_application_choices[2]&.rejection_reason,
-          'Application choice 1 structured rejection reasons' => FlatReasonsForRejectionPresenter.build_top_level_reasons(rejected_application_choices[0]&.structured_rejection_reasons),
-          'Application choice 2 structured rejection reasons' => FlatReasonsForRejectionPresenter.build_top_level_reasons(rejected_application_choices[1]&.structured_rejection_reasons),
-          'Application choice 3 structured rejection reasons' => FlatReasonsForRejectionPresenter.build_top_level_reasons(rejected_application_choices[2]&.structured_rejection_reasons),
+          month: application_form.submitted_at&.strftime('%B'),
+          recruitment_cycle_year: application_form.recruitment_cycle_year,
+          sex: application_form.equality_and_diversity['sex'],
+          ethnic_group: application_form.equality_and_diversity['ethnic_group'],
+          ethnic_background: application_form.equality_and_diversity['ethnic_background'],
+          application_status: I18n.t!("candidate_flow_application_states.#{ProcessState.new(application_form).state}.name"),
+          application_choice_1_unstructured_rejection_reasons: rejected_application_choices[0]&.rejection_reason,
+          application_choice_2_unstructured_rejection_reasons: rejected_application_choices[1]&.rejection_reason,
+          application_choice_3_unstructured_rejection_reasons: rejected_application_choices[2]&.rejection_reason,
+          application_choice_1_structured_rejection_reasons: FlatReasonsForRejectionPresenter.build_top_level_reasons(rejected_application_choices[0]&.structured_rejection_reasons),
+          application_choice_2_structured_rejection_reasons: FlatReasonsForRejectionPresenter.build_top_level_reasons(rejected_application_choices[1]&.structured_rejection_reasons),
+          application_choice_3_structured_rejection_reasons: FlatReasonsForRejectionPresenter.build_top_level_reasons(rejected_application_choices[2]&.structured_rejection_reasons),
         }
 
         disabilities = application_form.equality_and_diversity['disabilities'].to_a
 
         disabilities.map.with_index(1) do |disability, index|
-          output["Disability #{index}"] = disability
+          output["disability_#{index}".to_sym] = disability
         end
 
         output
--- spec/services/support_interface/equality_and_diversity_export_spec.rb
+++ spec/services/support_interface/equality_and_diversity_export_spec.rb
@@ -1,6 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe SupportInterface::EqualityAndDiversityExport do
+  describe 'documentation' do
+    before do
+      three_disabilities = {
+        sex: 'female',
+        ethnic_background: 'Kiwi',
+        ethnic_group: 'Cantabrian',
+        disabilities: %w[unexplained amnesia blind],
+      }
+      application_form = create(:completed_application_form, equality_and_diversity: three_disabilities)
+      create(
+        :application_choice,
+        :with_structured_rejection_reasons,
+        structured_rejection_reasons: {
+          course_full_y_n: 'No',
+          candidate_behaviour_y_n: 'Yes',
+          candidate_behaviour_other: 'Persistent scratching',
+          honesty_and_professionalism_y_n: 'Yes',
+          honesty_and_professionalism_concerns: %w[references],
+        },
+        application_form: application_form,
+      )
+      create(:application_choice, :with_rejection, rejection_reason: 'Absence of English GCSE.', application_form: application_form)
+    end
+
+    it_behaves_like 'a data export'
+  end
+
   describe '#data_for_export' do
     it 'returns an array of hashes containing equality and diversity data' do
       one_disability = {
@@ -39,56 +66,56 @@ RSpec.describe SupportInterface::EqualityAndDiversityExport do
         application_form: application_form_two,
       )
 
-      create(:application_choice, :with_rejection, rejection_reason: 'Abscence of English GCSE.', application_form: application_form_three)
+      create(:application_choice, :with_rejection, rejection_reason: 'Absence of English GCSE.', application_form: application_form_three)
 
       expect(described_class.new.data_for_export).to contain_exactly(
         {
-          'Month' => application_form_three.submitted_at&.strftime('%B'),
-          'Recruitment cycle year' => application_form_three.recruitment_cycle_year,
-          'Sex' => application_form_three.equality_and_diversity['sex'],
-          'Ethnic group' => application_form_three.equality_and_diversity['ethnic_group'],
-          'Ethnic background' => application_form_three.equality_and_diversity['ethnic_background'],
-          'Application status' => 'Ended without success',
-          'Application choice 1 unstructured rejection reasons' => 'Abscence of English GCSE.',
-          'Application choice 2 unstructured rejection reasons' => nil,
-          'Application choice 3 unstructured rejection reasons' => nil,
-          'Application choice 1 structured rejection reasons' => nil,
-          'Application choice 2 structured rejection reasons' => nil,
-          'Application choice 3 structured rejection reasons' => nil,
-          'Disability 1' => application_form_three.equality_and_diversity['disabilities'].first,
-          'Disability 2' => application_form_three.equality_and_diversity['disabilities'].second,
-          'Disability 3' => application_form_three.equality_and_diversity['disabilities'].last,
+          month: application_form_three.submitted_at&.strftime('%B'),
+          recruitment_cycle_year: application_form_three.recruitment_cycle_year,
+          sex: application_form_three.equality_and_diversity['sex'],
+          ethnic_group: application_form_three.equality_and_diversity['ethnic_group'],
+          ethnic_background: application_form_three.equality_and_diversity['ethnic_background'],
+          application_status: 'Ended without success',
+          application_choice_1_unstructured_rejection_reasons: 'Absence of English GCSE.',
+          application_choice_2_unstructured_rejection_reasons: nil,
+          application_choice_3_unstructured_rejection_reasons: nil,
+          application_choice_1_structured_rejection_reasons: nil,
+          application_choice_2_structured_rejection_reasons: nil,
+          application_choice_3_structured_rejection_reasons: nil,
+          disability_1: application_form_three.equality_and_diversity['disabilities'].first,
+          disability_2: application_form_three.equality_and_diversity['disabilities'].second,
+          disability_3: application_form_three.equality_and_diversity['disabilities'].last,
         },
         {
-          'Month' => application_form_one.submitted_at&.strftime('%B'),
-          'Recruitment cycle year' => application_form_one.recruitment_cycle_year,
-          'Sex' => application_form_one.equality_and_diversity['sex'],
-          'Ethnic group' => application_form_one.equality_and_diversity['ethnic_group'],
-          'Ethnic background' => application_form_one.equality_and_diversity['ethnic_background'],
-          'Application status' => 'Have not started form',
-          'Application choice 1 unstructured rejection reasons' => nil,
-          'Application choice 2 unstructured rejection reasons' => nil,
-          'Application choice 3 unstructured rejection reasons' => nil,
-          'Application choice 1 structured rejection reasons' => nil,
-          'Application choice 2 structured rejection reasons' => nil,
-          'Application choice 3 structured rejection reasons' => nil,
-          'Disability 1' => application_form_one.equality_and_diversity['disabilities'].first,
-          'Disability 2' => application_form_one.equality_and_diversity['disabilities'].last,
+          month: application_form_one.submitted_at&.strftime('%B'),
+          recruitment_cycle_year: application_form_one.recruitment_cycle_year,
+          sex: application_form_one.equality_and_diversity['sex'],
+          ethnic_group: application_form_one.equality_and_diversity['ethnic_group'],
+          ethnic_background: application_form_one.equality_and_diversity['ethnic_background'],
+          application_status: 'Have not started form',
+          application_choice_1_unstructured_rejection_reasons: nil,
+          application_choice_2_unstructured_rejection_reasons: nil,
+          application_choice_3_unstructured_rejection_reasons: nil,
+          application_choice_1_structured_rejection_reasons: nil,
+          application_choice_2_structured_rejection_reasons: nil,
+          application_choice_3_structured_rejection_reasons: nil,
+          disability_1: application_form_one.equality_and_diversity['disabilities'].first,
+          disability_2: application_form_one.equality_and_diversity['disabilities'].last,
         },
         {
-          'Month' => application_form_two.submitted_at&.strftime('%B'),
-          'Recruitment cycle year' => application_form_two.recruitment_cycle_year,
-          'Sex' => application_form_two.equality_and_diversity['sex'],
-          'Ethnic group' => application_form_two.equality_and_diversity['ethnic_group'],
-          'Ethnic background' => application_form_two.equality_and_diversity['ethnic_background'],
-          'Application status' => 'Ended without success',
-          'Application choice 1 unstructured rejection reasons' => nil,
-          'Application choice 2 unstructured rejection reasons' => nil,
-          'Application choice 3 unstructured rejection reasons' => nil,
-          'Application choice 1 structured rejection reasons' => "Something you did\nHonesty and professionalism",
-          'Application choice 2 structured rejection reasons' => nil,
-          'Application choice 3 structured rejection reasons' => nil,
-          'Disability 1' => application_form_two.equality_and_diversity['disabilities'].first,
+          month: application_form_two.submitted_at&.strftime('%B'),
+          recruitment_cycle_year: application_form_two.recruitment_cycle_year,
+          sex: application_form_two.equality_and_diversity['sex'],
+          ethnic_group: application_form_two.equality_and_diversity['ethnic_group'],
+          ethnic_background: application_form_two.equality_and_diversity['ethnic_background'],
+          application_status: 'Ended without success',
+          application_choice_1_unstructured_rejection_reasons: nil,
+          application_choice_2_unstructured_rejection_reasons: nil,
+          application_choice_3_unstructured_rejection_reasons: nil,
+          application_choice_1_structured_rejection_reasons: "Something you did\nHonesty and professionalism",
+          application_choice_2_structured_rejection_reasons: nil,
+          application_choice_3_structured_rejection_reasons: nil,
+          disability_1: application_form_two.equality_and_diversity['disabilities'].first,
         },
       )
     end
