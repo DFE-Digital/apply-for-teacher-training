553dd3d32d02e3bd844e50256b3de8e6973c0624
Raam <raam.chauhan@softwire.com>
2021-03-19 10:37:44 +0000

    Add test for checking no shadowed common columns

--- app/services/data_set_documentation.rb
+++ app/services/data_set_documentation.rb
@@ -12,4 +12,19 @@ class DataSetDocumentation
     used_common_columns = common_columns.slice(*spec['common_columns'])
     used_common_columns.merge(spec['custom_columns'] || {})
   end
+
+  def self.shadowed_common_columns(klass)
+    name = klass.name.demodulize.underscore
+
+    begin
+      spec = YAML.load_file(Rails.root.join("app/exports/#{name}.yml"))
+    rescue Errno::ENOENT
+      return false
+    end
+
+    common_columns = YAML.load_file(Rails.root.join('app/exports/_common_columns.yml')).keys.map(&:to_sym)
+    custom_columns = spec['custom_columns'].keys.map(&:to_sym)
+
+    custom_columns & common_columns
+  end
 end
