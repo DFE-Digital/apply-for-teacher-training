d704b3500c5141f9e39b18aad21ce7c94076e1cb
Duncan Brown <duncanjbrown@gmail.com>
2021-03-20 05:53:41 +0000

    Decouple JSON schema validation from Vendor API schema
    We'll be able to use the same tools to validate other APIs by defining
    be_valid_against_openapi_schema in their respective helpers
    
    Matcher class created according to the matcher protocol set out in the
    RSpec docs:
    
    https://www.rubydoc.info/gems/rspec-expectations/RSpec/Matchers

---
+++ spec/support/json_schema_validator.rb
@@ -0,0 +1,41 @@
+class JSONSchemaValidator
+  attr_reader :schema, :item
+
+  def initialize(schema, item)
+    @schema = schema
+    @item = item
+  end
+
+  def valid?
+    formatted_validation_errors.blank?
+  end
+
+  def failure_message
+    <<~ERROR
+      Expected the item to be valid against schema:
+
+      #{formatted_item}
+
+      But I got these validation errors:
+
+      #{formatted_validation_errors}
+    ERROR
+  end
+
+private
+
+  def formatted_validation_errors
+    validator = JSON::Validator.fully_validate(schema, item)
+    validator.map { |message| '- ' + humanized_error(message) }.join("\n")
+  end
+
+  def formatted_item
+    return item if item.is_a?(String)
+
+    JSON.pretty_generate(item)
+  end
+
+  def humanized_error(message)
+    message.gsub("The property '#/'", 'The item')
+  end
+end
--- spec/support/vendor_api/vendor_api_spec_helpers.rb
+++ spec/support/vendor_api/vendor_api_spec_helpers.rb
@@ -62,65 +62,7 @@ module VendorAPISpecHelpers
     parsed_response['errors'].first
   end
 
-  RSpec::Matchers.define :be_valid_against_openapi_schema do |schema_name|
-    match do |item|
-      spec = OpenAPI3Specification.new(VendorAPISpecification.as_hash)
-
-      JSONSchemaValidator.new(
-        spec.as_json_schema(schema_name),
-        item,
-      ).valid?
-    end
-
-    failure_message do |item|
-      spec = OpenAPI3Specification.new(VendorAPISpecification.as_hash)
-
-      JSONSchemaValidator.new(
-        spec.as_json_schema(schema_name),
-        item,
-      ).failure_message
-    end
-  end
-
-  class JSONSchemaValidator
-    attr_reader :schema, :item
-
-    def initialize(schema, item)
-      @schema = schema
-      @item = item
-    end
-
-    def valid?
-      formatted_validation_errors.blank?
-    end
-
-    def failure_message
-      <<~ERROR
-        Expected the item to be valid against schema:
-
-        #{formatted_item}
-
-        But I got these validation errors:
-
-        #{formatted_validation_errors}
-      ERROR
-    end
-
-  private
-
-    def formatted_validation_errors
-      validator = JSON::Validator.fully_validate(schema, item)
-      validator.map { |message| '- ' + humanized_error(message) }.join("\n")
-    end
-
-    def formatted_item
-      return item if item.is_a?(String)
-
-      JSON.pretty_generate(item)
-    end
-
-    def humanized_error(message)
-      message.gsub("The property '#/'", 'The item')
-    end
+  def be_valid_against_openapi_schema(expected)
+    ValidAgainstOpenAPISchemaMatcher.new(expected, VendorAPISpecification.as_hash)
   end
 end
