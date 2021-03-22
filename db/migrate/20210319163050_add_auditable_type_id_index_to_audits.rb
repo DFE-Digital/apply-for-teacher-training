814798e92862f7ba58d1e8e55c3efb744a7e55e2
benswannack <ben.swannack@softwire.com>
2021-03-22 11:52:01 +0000

    Account for index already existing when running migration

--- db/migrate/20210319163050_add_auditable_type_id_index_to_audits.rb
+++ db/migrate/20210319163050_add_auditable_type_id_index_to_audits.rb
@@ -1,7 +1,15 @@
 class AddAuditableTypeIdIndexToAudits < ActiveRecord::Migration[6.0]
   disable_ddl_transaction!
 
-  def change
+  def up
+    return if index_exists?(:audits, %i[auditable_type id])
+
     add_index :audits, %i[auditable_type id], algorithm: :concurrently
+  end
+
+  def down
+    return unless index_exists?(:audits, %i[auditable_type id])
+
+    remove_index :audits, column: %i[auditable_type id], algorithm: :concurrently
   end
 end
