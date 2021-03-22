67bd8fb0dbb660da1c02539f1934e96f92860f47
David Gisbey <davidgisbey@gmail.com>
2021-03-22 14:23:03 +0000

    Update struct to use keyword args during initialisation

--- app/forms/support_interface/application_forms/pick_course_form.rb
+++ app/forms/support_interface/application_forms/pick_course_form.rb
@@ -10,7 +10,16 @@ module SupportInterface
       validates :course_code, presence: true
       validate :course_is_open_on_apply, on: :save
 
-      RadioOption = Struct.new(:course_option_id, :provider_name, :provider_code, :course_name, :course_code, :site_name, :study_mode)
+      RadioOption = Struct.new(
+        :course_option_id,
+        :provider_name,
+        :provider_code,
+        :course_name,
+        :course_code,
+        :site_name,
+        :study_mode,
+        keyword_init: true,
+      )
 
       def course_options
         return @course_options if @course_options
@@ -21,13 +30,13 @@ module SupportInterface
                 .reject { |course_option| existing_course_ids.include?(course_option.course_id) }
                 .map do |course_option|
                   RadioOption.new(
-                    course_option.id,
-                    course.provider.name,
-                    course.provider.code,
-                    course.name,
-                    course.code,
-                    course_option.site.name,
-                    course_option.study_mode.humanize,
+                    course_option_id: course_option.id,
+                    provider_name: course.provider.name,
+                    provider_code: course.provider.code,
+                    course_name: course.name,
+                    course_code: course.code,
+                    site_name: course_option.site.name,
+                    study_mode: course_option.study_mode.humanize,
                   )
                 end
         }.flatten
