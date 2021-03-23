09b4ba3f05112eb7a9c3e7554033e571f3ddc122
Raam <raam.chauhan@softwire.com>
2021-03-23 16:09:11 +0000

    Refactor user permissions export

--- app/services/support_interface/user_permissions_export.rb
+++ app/services/support_interface/user_permissions_export.rb
@@ -1,50 +1,24 @@
 module SupportInterface
   class UserPermissionsExport
     def data_for_export
-      data = Audited::Audit
-        .joins('JOIN provider_users_providers pup1 ON audits.auditable_id = pup1.id')
-        .joins('JOIN provider_users ON pup1.provider_user_id = provider_users.id')
-        .joins('LEFT JOIN provider_users_providers pup2 ON audits.user_id = pup2.provider_user_id')
-        .joins('LEFT JOIN providers ON pup2.provider_id = providers.id')
-        .where(auditable_type: 'ProviderPermissions')
-        .where(action: %w[create update])
-        .where(permissions_clauses)
-        .pluck(
-          'audits.created_at',
-          'audits.user_id',
-          Arel.sql(
-            "CASE audits.user_type
-              WHEN 'ProviderUser' THEN (SELECT CONCAT(first_name, ' ', last_name) FROM provider_users WHERE id = audits.user_id)
-              WHEN 'SupportUser' THEN (SELECT CONCAT(first_name, ' ', last_name) FROM support_users WHERE id = audits.user_id)
-            END".squish,
-          ),
-          'providers.code',
-          'providers.name',
-          Arel.sql("CONCAT(provider_users.first_name, ' ' , provider_users.last_name)"),
-          'audits.audited_changes',
-        )
+      raw_data.map do |row|
+        audited_changes = row[6]
 
-      data.map do |row|
-        audited_changes = row.pop
-        row << permissions_changes(audited_changes)
-        row << permissions_changes(audited_changes, false)
-        Hash[labels.zip(row)]
+        {
+          date: row[0],
+          user_id: row[1],
+          user_making_change: row[2],
+          provider_code: row[3],
+          provider_name: row[4],
+          user_whose_permissions_have_changed: row[5],
+          permissions_added: permissions_changes(audited_changes),
+          permissions_removed: permissions_changes(audited_changes, false),
+
+        }
       end
     end
 
   private
-
-    def labels
-      [
-        'Date',
-        'User ID',
-        'User making change',
-        'Provider code', 'Provider',
-        'User whose permission(s) have changed',
-        'Permissions added',
-        'Permissions removed'
-      ]
-    end
 
     def permissions_changes(audited_changes, enabled = true)
       changes = audited_changes.select do |k, v|
@@ -61,6 +35,31 @@ module SupportInterface
       end
 
       clauses.join(' OR ')
+    end
+
+    def raw_data
+      Audited::Audit
+          .joins('JOIN provider_users_providers pup1 ON audits.auditable_id = pup1.id')
+          .joins('JOIN provider_users ON pup1.provider_user_id = provider_users.id')
+          .joins('LEFT JOIN provider_users_providers pup2 ON audits.user_id = pup2.provider_user_id')
+          .joins('LEFT JOIN providers ON pup2.provider_id = providers.id')
+          .where(auditable_type: 'ProviderPermissions')
+          .where(action: %w[create update])
+          .where(permissions_clauses)
+          .pluck(
+            'audits.created_at',
+            'audits.user_id',
+            Arel.sql(
+              "CASE audits.user_type
+              WHEN 'ProviderUser' THEN (SELECT CONCAT(first_name, ' ', last_name) FROM provider_users WHERE id = audits.user_id)
+              WHEN 'SupportUser' THEN (SELECT CONCAT(first_name, ' ', last_name) FROM support_users WHERE id = audits.user_id)
+            END".squish,
+            ),
+            'providers.code',
+            'providers.name',
+            Arel.sql("CONCAT(provider_users.first_name, ' ' , provider_users.last_name)"),
+            'audits.audited_changes',
+          )
     end
   end
 end
