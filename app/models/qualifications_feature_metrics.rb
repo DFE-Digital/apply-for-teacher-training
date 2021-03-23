809ead4cf46f2f86b17ee46ebeea158e12595214
Steve Hook <steve.hook@gmail.com>
2021-03-23 16:26:56 +0000

    Attempt to make the QualificationsFeatureMetrics a little clearer

--- app/models/qualifications_feature_metrics.rb
+++ app/models/qualifications_feature_metrics.rb
@@ -6,11 +6,11 @@ class QualificationsFeatureMetrics
     start_time,
     end_time = Time.zone.now.end_of_day
   )
-    applications_with_a_levels_count = applications_with_a_levels_grouped(
+    applications_with_a_levels_count = applications_with_a_levels_grouped_counts(
       minimum_count,
       start_time,
       end_time,
-    ).count.keys.count
+    ).keys.count
     all_applications_count = all_applications(start_time, end_time).count
 
     return 'n/a' if all_applications_count.zero?
@@ -29,13 +29,14 @@ private
       .where(recruitment_cycle_year: RecruitmentCycle.current_year)
   end
 
-  def applications_with_a_levels_grouped(minimum_count, start_time, end_time)
+  def applications_with_a_levels_grouped_counts(minimum_count, start_time, end_time)
     all_applications(start_time, end_time)
       .joins(:application_qualifications)
       .where('application_qualifications.level': 'other')
       .where("application_qualifications.qualification_type IN ('A level', 'A/S level')")
       .having('count(application_qualifications.id) >= ?', minimum_count)
       .group('application_forms.id')
+      .count
   end
 
   def format_as_percentage(ratio)
