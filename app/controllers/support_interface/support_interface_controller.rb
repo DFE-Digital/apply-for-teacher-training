a7fc941bc6865bef9727f5b31fd341b626c19e49
Duncan Brown <duncanjbrown@gmail.com>
2021-03-23 11:13:22 +0000

    Simplify current_support_user
    It's either nil or it's present — don't set to false because then
    current_support_user&.method doesn't work

--- app/controllers/support_interface/support_interface_controller.rb
+++ app/controllers/support_interface/support_interface_controller.rb
@@ -9,7 +9,7 @@ module SupportInterface
     helper_method :current_support_user, :dfe_sign_in_user
 
     def current_support_user
-      !@current_support_user.nil? ? @current_support_user : @current_support_user = (SupportUser.load_from_session(session) || false)
+      @current_support_user ||= SupportUser.load_from_session(session)
     end
 
     def dfe_sign_in_user
