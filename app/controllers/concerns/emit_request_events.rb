f600a2cb7b19dfaef3bc9ce993bb7351aa4792aa
Duncan Brown <duncanjbrown@gmail.com>
2021-03-23 11:16:11 +0000

    Log an event on every request that hits the app

---
+++ app/controllers/concerns/emit_request_events.rb
@@ -0,0 +1,17 @@
+module EmitRequestEvents
+  extend ActiveSupport::Concern
+  include ApplicationHelper
+
+  included do
+    after_action :trigger_request_event
+  end
+
+  def trigger_request_event
+    request_event = Events::Event.new
+      .with_request_details(request)
+      .with_user_and_namespace(current_user, current_namespace)
+
+    # send event
+    Rails.logger.info(request_event.as_json)
+  end
+end
