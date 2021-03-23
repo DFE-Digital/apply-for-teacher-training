9346738c05e5eaac376a6157a35edc789d600016
Steve Hook <steve.hook@gmail.com>
2021-03-23 22:57:44 +0000

    New apply again call to action
    Replace ApplyAgainBannerComponent with ApplyAgainCallToActionComponent
    using inset component rather than a notification banner.

---
+++ app/components/candidate_interface/apply_again_call_to_action_component.rb
@@ -0,0 +1,55 @@
+module CandidateInterface
+  class ApplyAgainCallToActionComponent < ViewComponent::Base
+    include ViewHelper
+    attr_reader :application_form
+
+    def initialize(application_form:)
+      @application_form = application_form
+    end
+
+    def title
+      if offers_declined?
+        "You’ve declined #{ multiple_offers_declined? ? 'all of your offers' : 'your offer' }."
+      elsif applications_withdrawn?
+        "You’ve withdrawn your #{ multiple_offers_withdrawn? ? 'applications' : 'application' }"
+      else
+        "Your #{ multiple_applications? ? 'applications were' : 'application was' } unsuccessful"
+      end
+    end
+
+    def render?
+      !EndOfCycleTimetable.between_cycles_apply_2? &&
+        application_form.recruitment_cycle_year == RecruitmentCycle.current_year
+    end
+
+  private
+
+    def offers_declined?
+      statuses.include?('declined')
+    end
+
+    def multiple_offers_declined?
+      multiple_choices_with_status?('declined')
+    end
+
+    def applications_withdrawn?
+      statuses.include?('withdrawn')
+    end
+
+    def multiple_offers_withdrawn?
+      multiple_choices_with_status?('withdrawn')
+    end
+
+    def multiple_applications?
+      statuses.count > 1
+    end
+
+    def multiple_choices_with_status?(status)
+      statuses.select { |s| s == status }.count > 1
+    end
+
+    def statuses
+      @statuses ||= application_form.application_choices.map(&:status)
+    end
+  end
+end
