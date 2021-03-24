279f9d13f802debcca44b5cf6909c88561888b7a
David Gisbey <davidgisbey@gmail.com>
2021-03-24 11:58:39 +0000

    Add a cycle timetable helper
    This passes randomised dates to use in tests for each discreet phase
    of the cycle.

---
+++ spec/support/test_helpers/cycle_timetable_helper.rb
@@ -0,0 +1,39 @@
+module CycleTimetableHelper
+  def mid_cycle
+    rand(previous_end_of_cycle_timetable[:apply_reopens]..current_end_of_cycle_timetable[:apply_1_deadline])
+  end
+
+  def after_apply_1_deadline
+    rand((current_end_of_cycle_timetable[:apply_1_deadline] + 1.day)..current_end_of_cycle_timetable[:stop_applications_to_unavailable_course_options])
+  end
+
+  def after_full_course_deadline
+    rand((current_end_of_cycle_timetable[:stop_applications_to_unavailable_course_options] + 1.day)..current_end_of_cycle_timetable[:apply_2_deadline])
+  end
+
+  def after_apply_2_deadline
+    rand((current_end_of_cycle_timetable[:apply_2_deadline] + 1.day)..current_end_of_cycle_timetable[:find_closes])
+  end
+
+  def after_find_closes
+    rand((current_end_of_cycle_timetable[:find_closes])..current_end_of_cycle_timetable[:find_reopens])
+  end
+
+  def after_find_reopens
+    rand((current_end_of_cycle_timetable[:find_reopens])..current_end_of_cycle_timetable[:apply_reopens])
+  end
+
+  def after_apply_reopens
+    rand((current_end_of_cycle_timetable[:find_reopens])..Date.new(EndOfCycleTimetable::CURRENT_YEAR_FOR_SCHEDULE, 12, 31))
+  end
+
+private
+
+  def previous_end_of_cycle_timetable
+    EndOfCycleTimetable::CYCLE_DATES[EndOfCycleTimetable::CURRENT_YEAR_FOR_SCHEDULE - 1]
+  end
+
+  def current_end_of_cycle_timetable
+    EndOfCycleTimetable::CYCLE_DATES[EndOfCycleTimetable::CURRENT_YEAR_FOR_SCHEDULE]
+  end
+end
