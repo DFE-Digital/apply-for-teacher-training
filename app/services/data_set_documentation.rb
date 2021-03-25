b4eb89f350be66d26c1bcf74c17adb8bf3a10390
Raam <raam.chauhan@softwire.com>
2021-03-25 17:13:27 +0000

    Refactor DataSetDocumentation class

--- app/services/data_set_documentation.rb
+++ app/services/data_set_documentation.rb
@@ -2,35 +2,27 @@ class DataSetDocumentation
   def self.for(klass)
     name = klass.name.demodulize.underscore
 
-    begin
-      spec = YAML.load_file(Rails.root.join("app/exports/#{name}.yml"))
-    rescue Errno::ENOENT
-      return false
-    end
+    spec = YAML.load_file(Rails.root.join("app/exports/#{name}.yml"))
+    common_columns = load_common_columns
+    custom_columns = spec['custom_columns']
 
-    common_columns = Dir[Rails.root.join('app/exports/common_columns/*')]
-                         .map { |file| YAML.load_file(file) }
-                         .reduce({}, :merge)
+    check_for_shadowed_columns(common_columns, custom_columns)
 
     used_common_columns = common_columns.slice(*spec['common_columns'])
-    used_common_columns.merge(spec['custom_columns'] || {})
+    used_common_columns.merge(custom_columns || {})
   end
 
-  def self.shadowed_common_columns(klass)
-    name = klass.name.demodulize.underscore
-
-    begin
-      spec = YAML.load_file(Rails.root.join("app/exports/#{name}.yml"))
-    rescue Errno::ENOENT
-      return false
+  def self.check_for_shadowed_columns(common_columns, custom_columns)
+    if (custom_columns.keys.map(&:to_sym) & common_columns.keys.map(&:to_sym)).any?
+      raise 'There are columns in the export documentation that shadow the shared common columns'
     end
-
-    common_columns = Dir[Rails.root.join('app/exports/common_columns/*')]
-                         .map { |file| YAML.load_file(file) }
-                         .reduce({}, :merge).keys.map(&:to_sym)
-
-    custom_columns = spec['custom_columns'].keys.map(&:to_sym)
-
-    custom_columns & common_columns
   end
+
+  def self.load_common_columns
+    Dir[Rails.root.join('app/exports/common_columns/*')]
+        .map { |file| YAML.load_file(file) }
+        .reduce({}, :merge)
+  end
+
+  private_class_method :load_common_columns, :check_for_shadowed_columns
 end
