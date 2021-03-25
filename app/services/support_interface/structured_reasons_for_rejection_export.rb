a3b8b63b33327bbd87711d628d7da8322ca04cef
Raam <raam.chauhan@softwire.com>
2021-03-25 16:14:06 +0000

    Add structured reasons for rejection doc

--- app/services/support_interface/structured_reasons_for_rejection_export.rb
+++ app/services/support_interface/structured_reasons_for_rejection_export.rb
@@ -3,13 +3,13 @@ module SupportInterface
     def data_for_export
       data_for_export = application_choices.order(:id).map do |application_choice|
         output = {
-          'Candidate ID' => application_choice.application_form_id,
-          'Application ID' => application_choice.id,
-          'Recruitment cycle year' => application_choice.course.recruitment_cycle_year,
-          'Phase (apply1/apply2)' => application_choice.application_form.phase,
-          'Provider code' => application_choice.provider.code,
-          'Course code' => application_choice.course.code,
-          'Date rejected' => application_choice.rejected_at.strftime('%d/%m/%Y'),
+          candidate_id: application_choice.application_form_id,
+          application_choice_id: application_choice.id,
+          recruitment_cycle_year: application_choice.course.recruitment_cycle_year,
+          phase: application_choice.application_form.phase,
+          provider_code: application_choice.provider.code,
+          course_code: application_choice.course.code,
+          rejected_at: application_choice.rejected_at.strftime('%d/%m/%Y'),
         }.merge!(FlatReasonsForRejectionPresenter.build_from_structured_rejection_reasons(ReasonsForRejection.new(application_choice.structured_rejection_reasons)))
 
         output
