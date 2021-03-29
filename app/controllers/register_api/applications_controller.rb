70cd32ab12cbe7b1f704af96a5f2156090295847
cwrw <abeer.salameh@gmail.com>
2021-03-29 23:44:36 +0300

    3499 Plug in the recruited query service into the register api controller
    trello-ticket: https://trello.com/c/yo8jZhLa/3499-spike-bare-bones-api-for-register
    
    * Use the new query service in the controller
    * Remove old register states constant as it is no longer used
    * Remove extra rescue as will never be called as there is no
    parsing on the attribute

--- app/controllers/register_api/applications_controller.rb
+++ app/controllers/register_api/applications_controller.rb
@@ -3,28 +3,20 @@ module RegisterAPI
     include ServiceAPIUserAuthentication
 
     def index
-      application_choices = get_recruited_applications(
+      application_choices = GetRecruitedApplicationChoices.call(
         recruitment_cycle_year: recruitment_cycle_year_param,
       )
+
       render json: { data: MultipleApplicationsPresenter.new(application_choices, api: RegisterAPI).as_json }
     end
 
   private
-
-    def get_recruited_applications(recruitment_cycle_year:)
-      ApplicationChoice.includes([:application_form])
-        .where(application_forms: { recruitment_cycle_year: recruitment_cycle_year }).references(:application_forms)
-        .where(status: ApplicationStateChange::STATES_VISIBLE_TO_REGISTER)
-        .where.not(recruited_at: nil)
-    end
 
     def recruitment_cycle_year_param
       year = params.fetch(:recruitment_cycle_year)
       raise ParameterInvalid, 'Parameter is invalid: recruitment_cycle_year' unless year.in?(RecruitmentCycle.years_visible_to_providers.map(&:to_s))
 
       year
-    rescue ArgumentError
-      raise ParameterInvalid, 'Parameter is invalid: recruitment_cycle_year'
     end
   end
 end
