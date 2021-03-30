uk-gov-mirror/DFE-Digital.apply-for-teacher-training

Add DataAPI docs to APIDocs namespace

app/controllers/api_docs/data_api/open_api_controller.rb
@@ -1,0 +1,9 @@
+module APIDocs
+  module DataAPI
+    class OpenAPIController < APIDocsController
+      def spec
+        render plain: DataAPISpecification.as_yaml, content_type: 'text/yaml'
+      end
+    end
+  end
+end

app/controllers/api_docs/data_api/reference_controller.rb
@@ -1,0 +1,11 @@
+module APIDocs
+  module DataAPI
+    class ReferenceController < APIDocsController
+      def reference
+        spec = DataAPISpecification.as_hash
+
+        @api_reference = APIReference.new(spec)
+      end
+    end
+  end
+end

DFE-Digital/apply-for-teacher-training

app/controllers/api_docs/data_api_docs/open_api_controller.rb
@@ -1,0 +1,9 @@
+module APIDocs
+  module DataAPIDocs
+    class OpenAPIController < APIDocsController
+      def spec
+        render plain: DataAPISpecification.as_yaml, content_type: 'text/yaml'
+      end
+    end
+  end
+end

app/controllers/api_docs/data_api_docs/reference_controller.rb
@@ -1,0 +1,11 @@
+module APIDocs
+  module DataAPIDocs
+    class ReferenceController < APIDocsController
+      def reference
+        spec = DataAPISpecification.as_hash
+
+        @api_reference = APIReference.new(spec)
+      end
+    end
+  end
+end