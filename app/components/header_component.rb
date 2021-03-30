967408f130a8dbaf54e124225ab156f4de5160fd
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2021-03-30 21:02:47 +0100

    Use GovukComponent::Header

--- app/components/header_component.rb
+++ app/components/header_component.rb
@@ -1,10 +1,12 @@
 class HeaderComponent < ViewComponent::Base
-  attr_reader :navigation_items, :service_url, :service_name, :classes
+  attr_reader :classes, :product_name, :service_name, :service_name_href, :phase_tag, :navigation_items
 
-  def initialize(navigation_items:, service_name:, service_url:, classes: '')
-    @navigation_items = navigation_items
-    @service_name     = service_name
-    @service_url      = service_url
-    @classes          = classes
+  def initialize(classes: '', product_name: nil, service_name: nil, service_name_href:, phase_tag: false, navigation_items:)
+    @classes           = classes
+    @product_name      = product_name
+    @service_name      = service_name
+    @service_name_href = service_name_href
+    @phase_tag         = phase_tag
+    @navigation_items  = navigation_items
   end
 end
