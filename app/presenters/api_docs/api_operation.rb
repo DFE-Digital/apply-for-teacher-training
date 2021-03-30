uk-gov-mirror/DFE-Digital.apply-for-teacher-training

Support CSV responses in API documentation
TAD get their data as CSV, so render that instead of JSON.

It's necessary to bring the syntax-highlighting CSS into the
data_api_docs namespace so the example is nicely formatted

app/presenters/api_docs/api_operation.rb
@@ -35,6 +35,8 @@ module APIDocs
   class Response
     attr_reader :code, :response
 
+    RESPONSES_WITH_SCHEMAS = ['application/json', 'text/csv'].freeze
+
     delegate :description, :content, to: :response
 
     def initialize(code, response)
@@ -43,20 +45,25 @@ module APIDocs
     end
 
     def example
-      return unless response.content['application/json']
+      return unless RESPONSES_WITH_SCHEMAS.include?(mime_type)
 
-      response.content['application/json']['example'] || SchemaExample.new(schema).as_json
+      response.content[mime_type]['example'] || SchemaExample.new(schema).as_json
     end
 
     def schema
-      return unless response.content['application/json']
 
-      response.content['application/json'].schema
+      return unless RESPONSES_WITH_SCHEMAS.include?(mime_type)
+
+      response.content.values.first.schema
     end
 
     def schema_name
       location = schema.node_context.source_location.to_s
       location.gsub(/#\/components\/schemas\//, '')
+    end
+
+    def mime_type
+      response.content.keys.first
     end
   end
 

DFE-Digital/apply-for-teacher-training

app/presenters/api_docs/api_operation.rb
@@ -32,7 +32,6 @@ module APIDocs
 
   class DescriptionAndSchema
     attr_reader :definition
-
     delegate :description, to: :definition
 
     def initialize(definition)
@@ -40,7 +39,11 @@ module APIDocs
     end
 
     def schema
-      APIDocs::APISchema.new(definition.content['application/json'].schema)
+      APIDocs::APISchema.new(definition.content[mime_type].schema)
+    end
+
+    def mime_type
+      definition.content.keys.first
     end
   end
 end