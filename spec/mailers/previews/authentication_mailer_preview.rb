170e65087367de103c77101846e2e0463bf4e12d
Raam <raam.chauhan@softwire.com>
2021-04-07 15:23:06 +0100

    Add preview for sign_in_email_without_account_email

--- spec/mailers/previews/authentication_mailer_preview.rb
+++ spec/mailers/previews/authentication_mailer_preview.rb
@@ -3,6 +3,10 @@ class AuthenticationMailerPreview < ActionMailer::Preview
     AuthenticationMailer.sign_up_email(candidate: FactoryBot.build_stubbed(:candidate), token: SecureRandom.hex)
   end
 
+  def sign_in_email_without_account_email
+    AuthenticationMailer.sign_in_without_account_email(to: FactoryBot.build_stubbed(:candidate).email_address)
+  end
+
   def sign_in_email
     AuthenticationMailer.sign_in_email(candidate: FactoryBot.build_stubbed(:candidate), token: SecureRandom.hex)
   end
