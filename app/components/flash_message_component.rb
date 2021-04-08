576f904c4cb9c62fc71cfb0f409bd873ff4c4516
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2021-04-08 21:47:59 +0100

    Replace NotificationMessageComponent with GovukComponent::NotificationBanner

--- app/components/flash_message_component.rb
+++ app/components/flash_message_component.rb
@@ -9,12 +9,36 @@ class FlashMessageComponent < ViewComponent::Base
     flash.keys.detect { |key| ALLOWED_PRIMARY_KEYS.include?(key.to_sym) }
   end
 
-  def message
+  def title
+    I18n.t(message_key, scope: :notification_banner)
+  end
+
+  def classes
+    "govuk-notification-banner--#{message_key}"
+  end
+
+  def role
+    %i[warning success].include?(message_key) ? 'alert' : 'region'
+  end
+
+  def disable_auto_focus
+    message_key == 'info'
+  end
+
+  def heading
     messages.is_a?(Array) ? messages[0] : messages
   end
 
-  def secondary_message
-    messages.is_a?(Array) && messages.count >= 2 ? messages[1] : nil
+  def body
+    capture do
+      if messages.is_a?(Array) && messages.count >= 2
+        concat tag.p(messages[1], class: 'govuk-body')
+      end
+
+      if link.present?
+        concat tag.p(govuk_link_to(link['text'], link['url'], class: 'govuk-notification-banner__link'), class: 'govuk-body')
+      end
+    end
   end
 
   def render?
@@ -25,6 +49,10 @@ private
 
   def messages
     flash[message_key]
+  end
+
+  def link
+    flash[:link]
   end
 
   attr_reader :flash
