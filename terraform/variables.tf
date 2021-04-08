44d339b3c01dbaf622d3edac4675e8a6a9e2d0e0
Vignesh.N <vignesh.natraj@digital.education.gov.uk>
2021-04-08 16:48:41 +0100

    Use az cli credentials for non CI use
    Use az cli credentials to connect to keyVault when running on local dev machines

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -24,7 +24,7 @@ variable "paas_clock_app_instances" { default = 1 }
 variable "paas_worker_app_instances" { default = 1 }
 
 # Key Vault variables
-variable "azure_credentials" {}
+variable "azure_credentials" { default = null }
 
 variable "key_vault_name" {}
 
@@ -36,7 +36,7 @@ variable "key_vault_app_secret_name" {}
 
 locals {
   cf_api_url        = "https://api.london.cloud.service.gov.uk"
-  azure_credentials = jsondecode(var.azure_credentials)
+  azure_credentials = try(jsondecode(var.azure_credentials), null)
   app_secrets       = yamldecode(data.azurerm_key_vault_secret.app_secrets.value)
   infra_secrets     = yamldecode(data.azurerm_key_vault_secret.infra_secrets.value)
 
