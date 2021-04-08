ae42ff6f3fd0f160dc2ef152128c8c2cde6a229d
Malcolm Baig <malcolm.baig@gmail.com>
2021-04-08 13:19:55 +0100

    Rails 6.1 upgrade - handle deprecation warning for i18n config
    Change the config namespace for the setting that raises errors on
    missing translations.

--- config/application.rb
+++ config/application.rb
@@ -52,7 +52,7 @@ module ApplyForPostgraduateTeacherTraining
     config.action_view.default_form_builder = GOVUKDesignSystemFormBuilder::FormBuilder
 
     config.i18n.exception_handler = Proc.new { |exception| raise exception.to_exception }
-    config.action_view.raise_on_missing_translations = true
+    config.i18n.raise_on_missing_translations = true
     config.action_view.form_with_generates_remote_forms = false
 
     config.active_job.queue_adapter = :sidekiq
