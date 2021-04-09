75a3b2aa5727fbc3ceb3d16722a1776ca17509e8
Duncan Brown <duncanjbrown@gmail.com>
2021-04-09 17:16:16 +0100

    Remove ApplicationAPIRepresentationComponent
    This is simpler inlined straight into the support view which uses it

--- app/components/support_interface/application_choice_component.rb
+++ app/components/support_interface/application_choice_component.rb
@@ -1,6 +1,7 @@
 module SupportInterface
   class ApplicationChoiceComponent < ViewComponent::Base
     include ViewHelper
+    include APIDocsHelper
 
     attr_reader :application_choice
 
@@ -40,10 +41,16 @@ module SupportInterface
       rows << { key: 'Reject by default at', value: application_choice.reject_by_default_at.to_s(:govuk_date_and_time) } if application_choice.reject_by_default_at && application_choice.awaiting_provider_decision?
       rows << { key: 'Decline by default at', value: application_choice.decline_by_default_at.to_s(:govuk_date_and_time) } if application_choice.decline_by_default_at && application_choice.offer?
 
-      if ApplicationStateChange::STATES_VISIBLE_TO_PROVIDER.include?(application_choice.status.to_sym)
-        rows << { key: 'API', value: render(SupportInterface::ApplicationAPIRepresentationComponent.new(application_choice: application_choice)) }
+      if visible_over_vendor_api?
+        application_json = AllowedCrossNamespaceUsage::VendorAPISingleApplicationPresenter.new(application_choice).as_json
+        rows << {
+          key: 'Vendor API',
+          value: govuk_details(summary: 'See this application choice as it appears over the Vendor API') do
+            json_code_sample(application_json)
+          end,
+        }
       else
-        rows << { key: 'API', value: 'This application hasn’t been sent to the provider yet, so it isn’t available over the API' }
+        rows << { key: 'Vendor API', value: 'This application hasn’t been sent to the provider yet, so it isn’t available over the Vendor API' }
       end
 
       if application_choice.interviews.present?
@@ -73,6 +80,10 @@ module SupportInterface
           application_choice.rejection_reason
         end
       end
+    end
+
+    def visible_over_vendor_api?
+      ApplicationStateChange::STATES_VISIBLE_TO_PROVIDER.include?(application_choice.status.to_sym)
     end
   end
 end
