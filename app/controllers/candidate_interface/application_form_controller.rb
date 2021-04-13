bfe64fd1268d7b641ac0ffac4bd1dbb5140d4ae7
Richard Pattinson <richie.pattinson@googlemail.com>
2021-04-13 13:03:59 +0100

    add view to handle unsubmitted previous applications

--- app/controllers/candidate_interface/application_form_controller.rb
+++ app/controllers/candidate_interface/application_form_controller.rb
@@ -2,9 +2,6 @@ module CandidateInterface
   class ApplicationFormController < CandidateInterfaceController
     def review_previous_application
       @application_form = current_candidate.application_forms.find(params[:id])
-      @review_previous_application = true
-
-      render 'candidate_interface/submitted_application_form/review_submitted'
     rescue ActiveRecord::RecordNotFound
       render_404
     end
