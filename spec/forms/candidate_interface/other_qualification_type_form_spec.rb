70960e0f0c4daa338eb409a1ba8eac2f04860477
David Gisbey <davidgisbey@gmail.com>
2021-04-14 13:49:24 +0100

    Add tests for save_no_other_qualifications method

--- spec/forms/candidate_interface/other_qualification_type_form_spec.rb
+++ spec/forms/candidate_interface/other_qualification_type_form_spec.rb
@@ -5,6 +5,19 @@ RSpec.describe CandidateInterface::OtherQualificationTypeForm do
     'activemodel.errors.models.candidate_interface/other_qualification_type_form.attributes.'
   end
 
+  let(:current_application) { create(:application_form) }
+  let(:intermediate_data_service) do
+    Class.new {
+      def read
+        {
+          'qualification_type' => 'non_uk',
+          'institution_country' => 'New Zealand',
+          'non_uk_qualification_type' => 'German diploma',
+        }
+      end
+    }.new
+  end
+
   describe 'validations' do
     it { is_expected.to validate_presence_of(:qualification_type) }
     it { is_expected.to validate_length_of(:other_uk_qualification_type).is_at_most(100) }
@@ -59,19 +72,6 @@ RSpec.describe CandidateInterface::OtherQualificationTypeForm do
   end
 
   describe '#initialize' do
-    let(:current_application) { create(:application_form) }
-    let(:intermediate_data_service) do
-      Class.new {
-        def read
-          {
-            'qualification_type' => 'non_uk',
-            'institution_country' => 'New Zealand',
-            'non_uk_qualification_type' => 'German diploma',
-          }
-        end
-      }.new
-    end
-
     context 'the qualification type is being updated from a non-uk qualification to a uk qualification' do
       it 'assigns an empty string to the non_uk attributes' do
         form = CandidateInterface::OtherQualificationTypeForm.new(
@@ -85,6 +85,25 @@ RSpec.describe CandidateInterface::OtherQualificationTypeForm do
         expect(form.non_uk_qualification_type).to be_nil
         expect(form.institution_country).to be_nil
       end
+    end
+  end
+
+  describe '#save_no_other_qualifications' do
+    it 'does not update no_other_qualifications when invalid' do
+      form = described_class.new
+      expect(form.save_no_other_qualifications).to eq false
+      expect(current_application.reload.no_other_qualifications).to eq false
+    end
+
+    it 'updates the application forms no_other_qualifications to true' do
+      form = CandidateInterface::OtherQualificationTypeForm.new(
+        current_application,
+        intermediate_data_service,
+        'qualification_type' => 'no_other_qualifications',
+      )
+
+      expect(form.save_no_other_qualifications).to eq true
+      expect(current_application.reload.no_other_qualifications).to eq true
     end
   end
 end
