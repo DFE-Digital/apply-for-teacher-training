4041bf2a180fdffbdddd4531f1f598b1516f7330
benswannack <ben.swannack@softwire.com>
2021-04-14 08:59:12 +0100

    Ignore rejected and withdrawn applications when checking to see if the same course has been applied to more than once. This will allow for some existing applications to be in valid states

--- spec/workers/detect_invariants_spec.rb
+++ spec/workers/detect_invariants_spec.rb
@@ -147,5 +147,21 @@ RSpec.describe DetectInvariants do
         ),
       )
     end
+
+    it 'ignores withdrawn and rejected application choices submitted with the same course' do
+      course = create(:course)
+      course_option1 = create(:course_option, course: course)
+      course_option2 = create(:course_option, course: course)
+      course_option3 = create(:course_option, course: course)
+      application_form = create(:completed_application_form)
+
+      create(:submitted_application_choice, status: :withdrawn, application_form: application_form, course_option: course_option1)
+      create(:submitted_application_choice, status: :rejected, application_form: application_form, course_option: course_option2)
+      create(:submitted_application_choice, application_form: application_form, course_option: course_option3)
+
+      DetectInvariants.new.perform
+
+      expect(Raven).not_to have_received(:capture_exception)
+    end
   end
 end
