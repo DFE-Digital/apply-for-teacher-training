7166795f3806c2f646d7f385165c84f811393457
Steve Hook <steve.hook@gmail.com>
2021-04-15 00:53:58 +0100

    Additional features

--- config/application.rb
+++ config/application.rb
@@ -71,7 +71,7 @@ module ApplyForPostgraduateTeacherTraining
     end
 
     config.action_dispatch.default_headers = {
-      'Feature-Policy' => "accelerometer 'none'; camera 'none'; geolocation 'none'; gyroscope 'none'; magnetometer 'none'; microphone 'none'; payment 'none'; usb 'none'",
+      'Feature-Policy' => "accelerometer 'none'; ambient-light-sensor: 'none', autoplay: 'none', battery: 'none', camera 'none'; display-capture: 'none', document-domain: 'none', fullscreen: 'none', geolocation 'none'; gyroscope 'none'; magnetometer 'none'; microphone 'none'; midi: 'none', payment 'none'; publickey-credentials-get: 'none', usb 'none', wake-lock: 'none', screen-wake-lock: 'none', web-share: 'none'",
     }
   end
 end
