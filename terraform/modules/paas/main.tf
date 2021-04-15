78cbdb2341525d8e46411d7e90829a4dc8ed561c
Vignesh.N <vignesh.natraj@digital.education.gov.uk>
2021-04-15 15:35:55 +0100

    Use http health check for web app

--- terraform/modules/paas/main.tf
+++ terraform/modules/paas/main.tf
@@ -17,8 +17,9 @@ provider "cloudfoundry" {
 resource "cloudfoundry_app" "web_app" {
   name                       = local.web_app_name
   docker_image               = var.app_docker_image
-  health_check_type          = "process"
+  health_check_type          = "http"
   health_check_http_endpoint = "/check"
+  health_check_timeout       = 180
   instances                  = var.web_app_instances
   memory                     = var.web_app_memory
   space                      = data.cloudfoundry_space.space.id
