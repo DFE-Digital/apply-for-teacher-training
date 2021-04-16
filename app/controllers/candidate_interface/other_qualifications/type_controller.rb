2d750640322d4737c95b43a4cfc6738a7116dd22
JRG <47917431+JR-G@users.noreply.github.com>
2021-04-16 11:15:13 +0100

    Updated back links

--- app/controllers/candidate_interface/other_qualifications/type_controller.rb
+++ app/controllers/candidate_interface/other_qualifications/type_controller.rb
@@ -8,7 +8,7 @@ module CandidateInterface
       @form = OtherQualificationTypeForm.new(
         current_application,
         intermediate_data_service,
-        { current_step: :type }.merge!(qualification_type: current_application.no_other_qualifications ? 'no_other_qualifications' : nil),
+        current_step: :type,
       )
     end
 
