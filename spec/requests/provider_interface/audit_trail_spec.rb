32a61c7c62c5cd12a980e78131d4df329249410e
Despo Pentara <despo@extractmethod.com>
2021-04-19 23:21:48 +0100

    Update audit trail example
    (Current example is quite complex to use because it relies on data being setup on wizard)

--- spec/requests/provider_interface/audit_trail_spec.rb
+++ spec/requests/provider_interface/audit_trail_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Provider interface - audit trail', type: :request, with_audited:
   it 'creates audit records attributed to the authenticated provider' do
     provider_user = create(:provider_user, :with_provider, :with_make_decisions)
     course_option = course_option_for_provider(provider: provider_user.providers.first)
-    application_choice = create(:application_choice, :awaiting_provider_decision, course_option: course_option)
+    application_choice = create(:application_choice, :with_offer, course_option: course_option)
 
     allow(ProviderUser).to receive(:load_from_session)
       .and_return(
@@ -17,10 +17,10 @@ RSpec.describe 'Provider interface - audit trail', type: :request, with_audited:
       )
 
     expect {
-      post provider_interface_application_choice_create_offer_path(
+      post provider_interface_application_choice_withdraw_offer_path(
         application_choice_id: application_choice.id,
         course_option_id: course_option.id,
-      ), params: { offer_conditions: ['must be clever'] }
+      ), params: { withdraw_offer: { offer_withdrawal_reason: 'All spots taken' } }
     }.to(change { application_choice.audits.count })
 
     expect(application_choice.audits.last.user_id).to eq provider_user.id
