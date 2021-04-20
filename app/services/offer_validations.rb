b7ba849202f4d5a3ed5d5d9ff9804819c30bf31e
Despo Pentara <despo@extractmethod.com>
2021-04-20 16:07:30 +0100

    Offer validations: course_option open on apply

--- app/services/offer_validations.rb
+++ app/services/offer_validations.rb
@@ -4,4 +4,9 @@ class OfferValidations
   attr_accessor :course_option
 
   validates :course_option, presence: true
+  validate :course_option_open_on_apply, if: :course_option
+
+  def course_option_open_on_apply
+    errors.add(:course_option, :not_open_on_apply) unless course_option.course.open_on_apply?
+  end
 end
--- spec/services/offer_validations_spec.rb
+++ spec/services/offer_validations_spec.rb
@@ -1,6 +1,33 @@
 require 'rails_helper'
 RSpec.describe OfferValidations, type: :model do
+  subject(:offer) { OfferValidations.new(course_option: course_option) }
+
+  let(:course_option) { create(:course_option, course: course) }
+  let(:course) { create(:course, :open_on_apply) }
+
   context 'validations' do
     it { is_expected.to validate_presence_of(:course_option) }
+
+    describe '#course_option_open_on_apply' do
+      context 'when no course_option' do
+        let(:course_option) { nil }
+
+        it 'does not add a :not_open_on_apply error' do
+          offer.valid?
+
+          expect(offer.errors[:course_option]).not_to contain_exactly('is not open for applications via the Apply service')
+        end
+      end
+
+      context 'when not open on apply' do
+        let(:course) { create(:course, :ucas_only) }
+
+        it 'adds a :not_open_on_apply error' do
+          expect(offer).to be_invalid
+
+          expect(offer.errors[:course_option]).to contain_exactly('The requested course is not open for applications via the Apply service')
+        end
+      end
+    end
   end
 end
