6c114b0d754a4184f1f1b647d8e9e8d8de3bc046
JRG <47917431+JR-G@users.noreply.github.com>
2021-04-21 17:21:09 +0100

    Reinstate offer form and controller
    As part of the new reinstate offer flow a controller and a new
    form have been added to handle loading and updating the status of
    the candidates course choice. The form will also validate that the
    support user has confirmed that they've read the guidance and that
    they have also provided a valid zendesk ticket link.

---
+++ spec/forms/support_interface/application_forms/reinstate_declined_offer_form_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+
+RSpec.describe SupportInterface::ApplicationForms::ReinstateDeclinedOfferForm, type: :model, with_audited: true do
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:accept_guidance) }
+    it { is_expected.to validate_presence_of(:audit_comment_ticket) }
+
+    context 'for an invalid zendesk link' do
+      invalid_link = 'nonsense'
+      it { is_expected.not_to allow_value(invalid_link).for(:audit_comment_ticket) }
+    end
+
+    context 'for an valid zendesk link' do
+      valid_link = 'www.becomingateacher.zendesk.com/agent/tickets/example'
+      it { is_expected.to allow_value(valid_link).for(:audit_comment_ticket) }
+    end
+  end
+
+  describe '.build_from_course_choice' do
+    it 'creates an object based on the provided ApplicationChoice' do
+    end
+  end
+
+  describe '#save' do
+    let(:zendesk_ticket) { 'www.becomingateacher.zendesk.com/agent/tickets/example' }
+
+    it 'returns false if not valid' do
+      course_choice = create(:application_choice, status: :offer)
+      declined_offer_form = SupportInterface::ApplicationForms::ReinstateDeclinedOfferForm.new
+
+      expect(declined_offer_form.save(course_choice)).to eq(false)
+    end
+
+    it 'updates the provided ApplicationChoice with the "offer made" status if valid' do
+      Timecop.freeze do
+        course_choice = create(:application_choice, :with_declined_offer)
+
+        declined_offer_form = SupportInterface::ApplicationForms::ReinstateDeclinedOfferForm.new(
+          { status: :declined,
+            audit_comment_ticket: zendesk_ticket,
+            accept_guidance: true },
+        )
+
+        expect(declined_offer_form.save(course_choice)).to eq(true)
+
+        expect(course_choice).to have_attributes({
+          status: 'offer',
+          declined_at: nil,
+          declined_by_default: false,
+          decline_by_default_at: 10.business_days.from_now.end_of_day,
+        })
+
+        expect(course_choice.audits.last.comment).to include(zendesk_ticket)
+      end
+    end
+  end
+end
