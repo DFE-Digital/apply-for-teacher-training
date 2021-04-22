fed38b97f1638c6f8a23949695f43b346d6ab1f1
Malcolm Baig <malcolm.baig@gmail.com>
2021-04-22 12:51:07 +0100

    Add additional specs for review components
    Update specs for both BecomingATeacherReviewComponent and
    SubjectKnowledgeReviewComponent to test the messaging the candidate sees
    depending on whether or not there are rejection reasons for them to
    review.

--- spec/components/candidate_interface/becoming_a_teacher_review_component_spec.rb
+++ spec/components/candidate_interface/becoming_a_teacher_review_component_spec.rb
@@ -19,4 +19,28 @@ RSpec.describe CandidateInterface::BecomingATeacherReviewComponent do
       expect(result.css('.govuk-summary-list__actions').text).not_to include("Change #{t('application_form.personal_statement.becoming_a_teacher.change_action')}")
     end
   end
+
+  describe 'submitting app' do
+    let(:result) { render_inline(described_class.new(application_form: application_form, submitting_application: true)) }
+
+    context 'section not complete' do
+      before { application_form.becoming_a_teacher_completed = false }
+
+      context 'when review is pending' do
+        before { allow(application_form).to receive(:review_pending?).with(:becoming_a_teacher).and_return(true) }
+
+        it 'shows review-related messaging' do
+          expect(result.text).to include(t('review_application.becoming_a_teacher.not_reviewed'))
+        end
+      end
+
+      context 'when review is not pending' do
+        before { allow(application_form).to receive(:review_pending?).with(:becoming_a_teacher).and_return(false) }
+
+        it 'shows section missing messaging' do
+          expect(result.text).to include(t('review_application.becoming_a_teacher.incomplete'))
+        end
+      end
+    end
+  end
 end
--- spec/components/candidate_interface/subject_knowledge_review_component_spec.rb
+++ spec/components/candidate_interface/subject_knowledge_review_component_spec.rb
@@ -19,4 +19,28 @@ RSpec.describe CandidateInterface::SubjectKnowledgeReviewComponent do
       expect(result.css('.govuk-summary-list__actions').text).not_to include("Change #{t('application_form.personal_statement.subject_knowledge.change_action')}")
     end
   end
+
+  describe 'submitting app' do
+    let(:result) { render_inline(described_class.new(application_form: application_form, submitting_application: true)) }
+
+    context 'section not complete' do
+      before { application_form.subject_knowledge_completed = false }
+
+      context 'when review is pending' do
+        before { allow(application_form).to receive(:review_pending?).with(:subject_knowledge).and_return(true) }
+
+        it 'shows review-related messaging' do
+          expect(result.text).to include(t('review_application.subject_knowledge.not_reviewed'))
+        end
+      end
+
+      context 'when review is not pending' do
+        before { allow(application_form).to receive(:review_pending?).with(:subject_knowledge).and_return(false) }
+
+        it 'shows section missing messaging' do
+          expect(result.text).to include(t('review_application.subject_knowledge.incomplete'))
+        end
+      end
+    end
+  end
 end
