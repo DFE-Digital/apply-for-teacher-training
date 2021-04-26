fd990c5c7d85c77d1797cd0efd8af8fc4c434e7b
Steve Hook <steve.hook@gmail.com>
2021-04-26 14:48:42 +0100

    Trim trail/leading whitespace autosuggest inputs
    This can be done by providing a custom `source` option.
    
    We probably would want to make this an option for our own wrapper. So
    this would need to be generalised a bit.
    Also would want to avoid the `JSON.parse` everytime we suggest.

--- app/frontend/packs/autosuggests/init-autosuggest.js
+++ app/frontend/packs/autosuggests/init-autosuggest.js
@@ -1,6 +1,6 @@
 import { accessibleAutosuggestFromSource } from './helpers'
 
-export const initAutosuggest = ({ inputIds, containerId, templates = {}, styles = () => {} }) => {
+export const initAutosuggest = ({ inputIds, containerId, templates = {}, styles = () => {}, stripWhitespace = true }) => {
   try {
     inputIds.forEach(inputId => {
       const input = document.getElementById(inputId)
@@ -9,15 +9,25 @@ export const initAutosuggest = ({ inputIds, containerId, templates = {}, styles 
       const container = document.getElementById(containerId)
       if (!container) return
 
+      let options = {
+        templates: {
+          inputValue: templates.inputTemplate,
+          suggestion: templates.suggestionTemplate
+        },
+      };
+
+      if (stripWhitespace) {
+        const source = JSON.parse(container.dataset.source)
+        options.source = (query, populateResults) => {
+          const matches = source.filter(r => r.toLowerCase().indexOf(query.toLowerCase().trim()) !== -1)
+          populateResults(matches)
+        }
+      }
+
       accessibleAutosuggestFromSource(
         input,
         container,
-        {
-          templates: {
-            inputValue: templates.inputTemplate,
-            suggestion: templates.suggestionTemplate
-          }
-        }
+        options,
       )
 
       styles(containerId)
