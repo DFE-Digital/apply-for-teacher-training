e35da067b51bb1c291a0118e36ae4a0d58e44c72
Steve Hook <steve.hook@gmail.com>
2021-04-26 22:07:24 +0100

    Add data migration to cleanup qualification types

---
+++ app/services/data_migrations/trim_qualification_degree_types.rb
@@ -0,0 +1,17 @@
+module DataMigrations
+  class TrimQualificationDegreeTypes
+    TIMESTAMP = 20210426215031
+    MANUAL_RUN = false
+
+    def change
+      ApplicationQualification.where(level: 'degree').find_each do |application_qualification|
+        if /^\s+/ =~ application_qualification.qualification_type ||
+            /\s+$/ =~ application_qualification.qualification_type
+          application_qualification.update!(
+            qualification_type: application_qualification.qualification_type.strip,
+          )
+        end
+      end
+    end
+  end
+end
