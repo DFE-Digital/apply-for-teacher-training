7b52a0a2e62a290392df1ba25caba4bb0a002184
Steve Laing <steve.laing@gmail.com>
2021-04-27 11:38:26 +0100

    Add Sidekiq worker to insert request events in BQ table

---
+++ app/workers/send_request_events_to_bigquery.rb
@@ -0,0 +1,14 @@
+class SendRequestEventsToBigquery
+  include Sidekiq::Worker
+
+  sidekiq_options retry: 3, queue: :low_priority
+
+  TABLE_NAME = 'bat_apply_request_events'.freeze
+
+  def perform(request_event_json)
+    bq = Google::Cloud::Bigquery.new(project: ENV.fetch('BIG_QUERY_PROJECT_ID'))
+    dataset = bq.dataset(ENV.fetch('BIG_QUERY_DATASET'), skip_lookup: true)
+    bq_table = dataset.table(TABLE_NAME, skip_lookup: true)
+    bq_table.insert([request_event_json])
+  end
+end
