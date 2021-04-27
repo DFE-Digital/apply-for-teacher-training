cd7e65929c88ae5ecb4dd976a351dc5a48af6979
Steve Laing <steve.laing@gmail.com>
2021-04-27 18:46:05 +0100

    Configure project ID and JSON credentials
    Credentials are the contents of the service user key file saved as a
    JSON string ENV var.

--- config/initializers/bigquery.rb
+++ config/initializers/bigquery.rb
@@ -4,7 +4,7 @@ BIG_QUERY_API_JSON_KEY = ENV['BIG_QUERY_API_JSON_KEY']
 
 if BIG_QUERY_API_JSON_KEY.present?
   Google::Cloud::Bigquery.configure do |config|
-    config.project_id  = 'becoming-a-teacher-apply'
+    config.project_id  = ENV.fetch('BIG_QUERY_PROJECT_ID')
     config.credentials = JSON.parse(BIG_QUERY_API_JSON_KEY)
   end
 
