bf47cdfefcc73cee19c49aefb109bb5d6a0de4fa
Steve Hook <steve.hook@gmail.com>
2021-05-05 20:40:21 +0100

    Add SupportInterface::ConditionsForm#audit_comment

--- app/forms/support_interface/conditions_form.rb
+++ app/forms/support_interface/conditions_form.rb
@@ -2,30 +2,35 @@ module SupportInterface
   class ConditionsForm
     include ActiveModel::Model
 
-    attr_accessor :application_choice, :standard_conditions, :further_conditions
+    attr_accessor :application_choice, :standard_conditions, :further_conditions, :audit_comment
 
     NUMBER_OF_FURTHER_CONDITIONS = 4
-    validates :application_choice, presence: true
 
-    def self.build_from_application_choice(application_choice)
+    validates :application_choice, presence: true
+    validates :audit_comment, presence: true
+
+    def self.build_from_application_choice(application_choice, attrs = {})
       attrs = {
         application_choice: application_choice,
         standard_conditions: standard_conditions_from(application_choice.offer),
         further_conditions: further_conditions_from(application_choice.offer),
-      }
+      }.merge(attrs)
+
       new(attrs).tap do |form|
         form.setup_further_conditions
       end
     end
 
     def self.build_from_params(application_choice, params)
-      form = build_from_application_choice(application_choice)
+      attrs = {
+        standard_conditions: params['standard_conditions'],
+        audit_comment: params['audit_comment'],
+        further_conditions: params['further_conditions']&.values&.map { |v| v['text'] },
+      }
 
-      form.tap do |form|
-        form.standard_conditions = params['standard_conditions']
-        form.further_conditions = params['further_conditions'].values.map { |v| v['text'] }
-        form.setup_further_conditions
-      end
+      form = build_from_application_choice(application_choice, attrs)
+      form.setup_further_conditions
+      form
     end
 
     def self.standard_conditions_from(offer)
@@ -56,8 +61,11 @@ module SupportInterface
     end
 
     def save
+      return false unless valid?
+
       application_choice.update(
-        offer: { conditions: (standard_conditions + further_conditions).reject(&:blank?) }
+        offer: { conditions: (standard_conditions + further_conditions).reject(&:blank?) },
+        audit_comment: audit_comment,
       )
     end
   end
--- spec/forms/support_interface/conditions_form_spec.rb
+++ spec/forms/support_interface/conditions_form_spec.rb
@@ -2,6 +2,29 @@ require 'rails_helper'
 
 RSpec.describe SupportInterface::ConditionsForm do
   describe '#save' do
+    it 'returns false with a validation error if audit_comment is missing' do
+      application_choice = create(
+        :application_choice,
+        offer: { 'conditions' => ['Fitness to train to teach check', 'Get a haircut'] },
+      )
+      form = described_class.build_from_params(
+        application_choice,
+        'standard_conditions' => [
+          'Fitness to train to teach check',
+          'Disclosure and Barring Service (DBS) check',
+        ],
+        'further_conditions' => {
+          '0' => { 'text' => 'Get a haircut' },
+          '1' => { 'text' => 'Wear a tie' },
+        },
+      )
+      expect(form.save).to be(false)
+      expect(application_choice.reload.offer['conditions']).to eq([
+        'Fitness to train to teach check',
+        'Get a haircut',
+      ])
+    end
+
     it 'adds an additional further and standard condition' do
       application_choice = create(
         :application_choice,
@@ -17,6 +40,7 @@ RSpec.describe SupportInterface::ConditionsForm do
           '0' => { 'text' => 'Get a haircut' },
           '1' => { 'text' => 'Wear a tie' },
         },
+        'audit_comment' => 'See support ticket #123',
       )
       form.save
       expect(application_choice.reload.offer).to eq(
@@ -42,6 +66,7 @@ RSpec.describe SupportInterface::ConditionsForm do
         'further_conditions' => {
           '0' => { 'text' => 'Wear a tie' },
         },
+        'audit_comment' => 'See support ticket #123',
       )
       form.save
       expect(application_choice.reload.offer).to eq(
@@ -50,6 +75,25 @@ RSpec.describe SupportInterface::ConditionsForm do
           'Wear a tie',
         ],
       )
+    end
+
+    it 'includes an audit comment', with_audited: true do
+      application_choice = create(
+        :application_choice,
+        offer: { 'conditions' => ['Fitness to train to teach check'] },
+      )
+      form = described_class.build_from_params(
+        application_choice,
+        'standard_conditions' => [
+          'Disclosure and Barring Service (DBS) check',
+        ],
+        'audit_comment' => 'See support ticket #123',
+      )
+      form.save
+      expect(application_choice.audits.where(
+        action: 'update',
+        comment: 'See support ticket #123',
+      )).to be_present
     end
   end
 
