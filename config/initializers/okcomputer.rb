e086d162af44d5c41ff04c1a96c481167fee6e07
cwrw <abeer.salameh@gmail.com>
2021-05-07 11:31:51 +0100

    Move sidekiq retries into invariant check
    This is causing statuscake checks to fail, move to invariant
    check instead to be alerted.

--- config/initializers/okcomputer.rb
+++ config/initializers/okcomputer.rb
@@ -11,26 +11,12 @@ class SimulatedFailureCheck < OkComputer::Check
   end
 end
 
-class SidekiqRetriesCheck < OkComputer::Check
-  def check
-    retries_queue_length = Sidekiq::RetrySet.new.size
-    queue_length_text = " (#{retries_queue_length})"
-    if retries_queue_length > 50
-      mark_failure
-      mark_message "Sidekiq pending retries depth is high #{queue_length_text}. Suggests high error rate"
-    else
-      mark_message 'Sidekiq pending retries depth at reasonable level' + queue_length_text
-    end
-  end
-end
-
 OkComputer.mount_at = 'integrations/monitoring'
 
 OkComputer::Registry.register 'postgres', OkComputer::ActiveRecordCheck.new
 OkComputer::Registry.register 'redis', OkComputer::RedisCheck.new(url: ENV['REDIS_URL'])
 OkComputer::Registry.register 'sidekiq_default_queue', OkComputer::SidekiqLatencyCheck.new(queue: 'default', threshold: 100) # threshold in seconds
 OkComputer::Registry.register 'sidekiq_mailers_queue', OkComputer::SidekiqLatencyCheck.new(queue: 'mailers', threshold: 100) # threshold in seconds
-OkComputer::Registry.register 'sidekiq_retries_count', SidekiqRetriesCheck.new
 OkComputer::Registry.register 'simulated_failure', SimulatedFailureCheck.new
 OkComputer::Registry.register 'version', OkComputer::AppVersionCheck.new
 
