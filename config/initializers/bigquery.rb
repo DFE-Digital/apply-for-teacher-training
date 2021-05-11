1cd158005df209b055a20bd3c46f4b0b74349161
Steve Laing <steve.laing@gmail.com>
2021-05-11 17:40:42 +0100

    Don't test for feature flag in app initializer
    This seems to be breaking the build as a connection can't be established, the env var check will ensure we're insulated from initializing BQ.

--- config/initializers/bigquery.rb
+++ config/initializers/bigquery.rb
@@ -1,14 +1,12 @@
 require 'google/cloud/bigquery'
 
-if FeatureFlag.active?(:send_request_data_to_bigquery)
-  BIG_QUERY_API_JSON_KEY = ENV['BIG_QUERY_API_JSON_KEY']
+BIG_QUERY_API_JSON_KEY = ENV['BIG_QUERY_API_JSON_KEY']
 
-  if BIG_QUERY_API_JSON_KEY.present?
-    Google::Cloud::Bigquery.configure do |config|
-      config.project_id  = ENV.fetch('BIG_QUERY_PROJECT_ID')
-      config.credentials = JSON.parse(BIG_QUERY_API_JSON_KEY)
-    end
-
-    Google::Cloud::Bigquery.new
+if BIG_QUERY_API_JSON_KEY.present?
+  Google::Cloud::Bigquery.configure do |config|
+    config.project_id  = ENV.fetch('BIG_QUERY_PROJECT_ID')
+    config.credentials = JSON.parse(BIG_QUERY_API_JSON_KEY)
   end
+
+  Google::Cloud::Bigquery.new
 end
