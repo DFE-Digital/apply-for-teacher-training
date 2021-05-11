bcf47f00fdbdac20343a62d7c0a31dd08d5e3fbc
Steve Laing <steve.laing@gmail.com>
2021-05-11 11:03:59 +0100

    Ensure duplicate provider users aren't orphaned
    If there are provider users who are solely associated with the duplicate provider, do not destroy the provider record.
    We should address any cases like this in support. No users currently are in this state.

--- app/services/data_migrations/remove_duplicate_provider.rb
+++ app/services/data_migrations/remove_duplicate_provider.rb
@@ -12,16 +12,23 @@ module DataMigrations
       if permissions.any?
         permissions.each do |permission|
           ActiveRecord::Base.transaction do
-            courses = permission.training_provider.courses.where(accredited_provider: permission.ratifying_provider)
-
             next if permission.ratifying_provider.courses.any?
+
+            courses = permission.training_provider.courses.where(accredited_provider: permission.ratifying_provider)
 
             courses.each do |course|
               course.update!(accredited_provider: nil, audit_comment: 'Deduplicating accredited provider, this course is self-ratified')
             end
 
             permission.destroy!
-            permission.ratifying_provider.destroy!
+
+            # Ensure we don't orphan any users who were only associated with the duplicate provider
+            if permission.ratifying_provider.provider_users.select { |user| user.providers.size == 1 }.any?
+              Rails.logger.warn "Skipping deletion of #{permission.ratifying_provider.name}. This organisation has users which do not belong to any other provider."
+            else
+              permission.destroy!
+              permission.ratifying_provider.destroy!
+            end
           end
         end
       end
--- spec/services/data_migrations/remove_duplicate_provider_spec.rb
+++ spec/services/data_migrations/remove_duplicate_provider_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe DataMigrations::RemoveDuplicateProvider do
   let(:provider) { create(:provider, name: 'Test Provider 001') }
   let(:duplicate_provider) { create(:provider, name: 'Test Provider 001') }
+  let!(:provider_user) { create(:provider_user, providers: [provider, duplicate_provider]) }
   let!(:provider_relationship_permissions) { create(:provider_relationship_permissions, training_provider: provider, ratifying_provider: duplicate_provider) }
   let!(:self_ratified_course) { create(:course, provider: provider, accredited_provider: duplicate_provider) }
   let!(:other_course) { create(:course, provider: provider, accredited_provider: create(:provider)) }
@@ -18,7 +19,29 @@ RSpec.describe DataMigrations::RemoveDuplicateProvider do
     expect { DataMigrations::RemoveDuplicateProvider.new.change }.to change(ProviderRelationshipPermissions, :count).by(-1)
   end
 
-  it 'destroys the duplicate provider' do
-    expect { DataMigrations::RemoveDuplicateProvider.new.change }.to change(Provider, :count).by(-1)
+  context 'with unrelated courses ratified by the ratifying provider' do
+    it 'does nothing' do
+      provider = create(:provider, name: 'Test Provider 002')
+      another_provider = create(:provider, name: 'Another Test Provider')
+      duplicate_provider = create(:provider, name: 'Test Provider 002')
+      create(:course, provider: provider, accredited_provider: duplicate_provider)
+      ratified_course = create(:course, provider: another_provider, accredited_provider: duplicate_provider)
+      create(:provider_relationship_permissions, training_provider: provider, ratifying_provider: duplicate_provider)
+      create(:provider_relationship_permissions, training_provider: another_provider, ratifying_provider: duplicate_provider)
+
+      expect { described_class.new.change }.not_to change(ProviderRelationshipPermissions, :count)
+      expect(ratified_course.reload.accredited_provider).not_to be_nil
+    end
+  end
+
+  context 'with users who only belong to the duplicate provider' do
+    it 'does not remove the provider' do
+      provider = create(:provider, name: 'Test Provider 002')
+      duplicate_provider = create(:provider, name: 'Test Provider 002')
+      create(:provider_user, providers: [duplicate_provider])
+      create(:provider_relationship_permissions, training_provider: provider, ratifying_provider: duplicate_provider)
+
+      expect { described_class.new.change }.not_to change(Provider, :count)
+    end
   end
 end
