46ef6e03be79767fc71e0e8287ce7bc001b83169
Malcolm Baig <malcolm.baig@gmail.com>
2021-05-12 13:54:21 +0100

    Add an expanded version of the Qualifications export
    The Qualifications export currently only contains a subset of each
    candidate's qualifications. It needs to cover all qualifications in the
    system so that a more complete analysis can take place.
    
    This commit adds a new export class that:
    
    - Contains details about every qualification for any given candidate
    - Maintains the relationship between application form and course choices
      that was present in the previous export as well. Note that this means
      that each set of qualification data per candidate is repeated in the
      data table for every course choice.

---
+++ app/services/support_interface/expanded_qualifications_export.rb
@@ -0,0 +1,48 @@
+module SupportInterface
+  class ExpandedQualificationsExport
+    def data_for_export
+      application_choices = ApplicationChoice
+        .select(:id, :application_form_id, :status, :course_option_id, :rejection_reason, :structured_rejection_reasons)
+        .includes(:course_option, :course, :provider, application_form: [:application_qualifications])
+        .order(:application_form_id)
+
+      application_choices.find_each(batch_size: 100).lazy.flat_map do |application_choice|
+        application_form = application_choice.application_form
+        course = application_choice.course
+        qualifications = application_form.application_qualifications
+
+        qualifications.map do |qualification|
+          {
+            application_form_id: application_form.id,
+            choice_id: application_choice.id,
+            qualification_id: qualification.id,
+            candidate_id: application_form.candidate_id,
+            support_reference: application_form.support_reference,
+            phase: application_form.phase,
+            recruitment_cycle_year: application_form.recruitment_cycle_year,
+
+            choice_status: application_choice.status,
+            rejection_reason: application_choice.structured_rejection_reasons || application_choice.rejection_reason,
+            course_code: course.code,
+            provider_code: course.provider.code,
+
+            level: qualification.level,
+            qualification_type: qualification.qualification_type,
+            other_uk_qualification_type: qualification.other_uk_qualification_type,
+            award_year: qualification.award_year,
+            subject: qualification.subject,
+            predicted_grade: qualification.predicted_grade,
+            grade: qualification.grade,
+            institution_name: qualification.institution_name,
+            institution_country: qualification.institution_country,
+            international: qualification.international,
+            comparable_uk_degree: qualification.comparable_uk_degree,
+            non_uk_qualification_type: qualification.non_uk_qualification_type,
+            comparable_uk_qualification: qualification.comparable_uk_qualification,
+            constituent_grades: qualification.constituent_grades,
+          }
+        end
+      end
+    end
+  end
+end
