ef01f8beed6a8bb5b85cf2f657941f656ffb3569
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2021-05-13 09:55:21 +0100

    Refactor SummaryListComponent to use govuk_summary_list component

--- app/components/summary_list_component.rb
+++ app/components/summary_list_component.rb
@@ -6,8 +6,24 @@ class SummaryListComponent < ViewComponent::Base
     @rows = rows
   end
 
-  def any_row_has_action_span?
-    rows.select { |row| row.key?(:action) }.any?
+  def value(row)
+    if row[:value].is_a?(Array)
+      row[:value].join('<br>').html_safe
+    elsif row[:value].html_safe?
+      row[:value]
+    else
+      simple_format(row[:value], class: 'govuk-body')
+    end
+  end
+
+  def action(row)
+    if row[:change_path]
+      govuk_link_to(row[:change_path], class: 'govuk-!-display-none-print') do
+        "Change<span class=\"govuk-visually-hidden\">#{row[:action]}</span>".html_safe
+      end
+    elsif row[:action_path]
+      govuk_link_to(row[:action], row[:action_path], class: 'govuk-!-display-none-print')
+    end
   end
 
 private
