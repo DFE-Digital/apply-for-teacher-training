292a45c8d95140fcd99b9498c7131523e385937a
benswannack <ben.swannack@softwire.com>
2021-05-13 15:42:26 +0100

    Add Javascript specs for make offer
    Also add chromedriver to support running js system specs

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -5,8 +5,26 @@ if ENV['TEST_ENV_NUMBER']
   Capybara.server_port = 9887 + ENV['TEST_ENV_NUMBER'].to_i
 end
 
+Capybara.register_driver :chrome_headless do |app|
+  options = ::Selenium::WebDriver::Chrome::Options.new
+
+  options.add_argument('--headless')
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-dev-shm-usage')
+  options.add_argument('--disable-gpu')
+  options.add_argument('--window-size=1400,1400')
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+
+Capybara.javascript_driver = :chrome_headless
+
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    driven_by(:rack_test)
+    if self.class.metadata[:js] == true
+      driven_by(:chrome_headless)
+    else
+      driven_by(:rack_test)
+    end
   end
 end
