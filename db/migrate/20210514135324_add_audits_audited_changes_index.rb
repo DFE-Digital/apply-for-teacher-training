84368991d1e038568e628f353252d8b79cc2f025
Steve Hook <steve.hook@gmail.com>
2021-05-14 17:08:57 +0100

    Dodge query timeouts in migration

--- db/migrate/20210514135324_add_audits_audited_changes_index.rb
+++ db/migrate/20210514135324_add_audits_audited_changes_index.rb
@@ -1,7 +1,17 @@
 class AddAuditsAuditedChangesIndex < ActiveRecord::Migration[6.1]
   disable_ddl_transaction!
 
-  def change
+  def up
+    return if index_exists?(:audits, :audited_changes)
+
+    safety_assured { execute 'SET statement_timeout = 0' }
     add_index :audits, :audited_changes, using: :gin, algorithm: :concurrently
+  end
+
+  def down
+    return unless index_exists?(:audits, :audited_changes)
+
+    safety_assured { execute 'SET statement_timeout = 0' }
+    remove_index :audits, column: :audited_changes, algorithm: :concurrently
   end
 end
