dfb4ec5718920bbe4a661ecaf236cce78c0805eb
benswannack <ben.swannack@softwire.com>
2021-05-18 14:55:28 +0100

    Rename base css class to add-another-condition to be more specific

--- app/frontend/packs/further_conditions/constants.js
+++ app/frontend/packs/further_conditions/constants.js
@@ -1,4 +1,4 @@
-const ROOT_CSS_CLASS = 'app-add-another'
+const ROOT_CSS_CLASS = 'app-add-condition'
 export const ITEM_CSS_CLASS = `${ROOT_CSS_CLASS}__item`
 export const REMOVE_BUTTON_CSS_CLASS = `${ROOT_CSS_CLASS}__remove-button`
 export const ADD_BUTTON_CSS_CLASS = `${ROOT_CSS_CLASS}__add-button`
