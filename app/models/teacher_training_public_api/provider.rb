679d6c44081a2faee91a8ca3f65f89b8fc4c131b
Duncan Brown <duncanjbrown@gmail.com>
2021-05-21 13:37:24 +0100

    Add Provider.fetch to TTAPI interface

--- app/models/teacher_training_public_api/provider.rb
+++ app/models/teacher_training_public_api/provider.rb
@@ -2,5 +2,15 @@ module TeacherTrainingPublicAPI
   class Provider < TeacherTrainingPublicAPI::Resource
     belongs_to :recruitment_cycle, param: :year
     has_many :courses
+
+    def self.fetch(provider_code)
+      where(year: ::RecruitmentCycle.current_year)
+        .find(provider_code).first
+    rescue JsonApiClient::Errors::NotFound
+      nil
+    rescue JsonApiClient::Errors::ServerError, JsonApiClient::Errors::ConnectionError => e
+      Raven.capture_exception(e)
+      nil
+    end
   end
 end
