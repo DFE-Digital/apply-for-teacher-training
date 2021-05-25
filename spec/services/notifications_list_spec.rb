904b3835a624f2833dc9b491b944ef26e70b61a3
Steve Hook <steve.hook@gmail.com>
2021-05-25 09:54:50 +0100

    De-dup provider users returned by `NotificationsList`

--- spec/services/notifications_list_spec.rb
+++ spec/services/notifications_list_spec.rb
@@ -35,6 +35,26 @@ RSpec.describe NotificationsList do
 
         expect(NotificationsList.for(application_choice, include_ratifying_provider: true, event: :offer_accepted).to_a).to match_array([training_provider_user, ratifying_provider_user])
       end
+
+      it 'returns a provider user who is a member of the training and ratifying providers without duplicates' do
+        ratifying_provider = create(:provider)
+        training_provider = create(:provider)
+        provider_user = create(:provider_user, send_notifications: true, providers: [training_provider, ratifying_provider])
+        application_choice = create(:application_choice, course_option: create(:course_option, course: create(:course, provider: training_provider, accredited_provider: ratifying_provider)))
+
+        create(
+          :provider_user_notification_preferences,
+          :all_off,
+          provider_user: create(:provider_user, send_notifications: false, providers: [training_provider, ratifying_provider]),
+        )
+        create(:provider_user_notification_preferences)
+
+        expect(NotificationsList.for(
+          application_choice,
+          include_ratifying_provider: true,
+          event: :offer_accepted,
+        ).to_a).to match_array([provider_user])
+      end
     end
 
     context 'when the configurable provider notifications feature flag is off' do
@@ -60,6 +80,26 @@ RSpec.describe NotificationsList do
         training_provider_user = create(:provider_user, send_notifications: true, providers: [application_choice.course.provider])
 
         expect(NotificationsList.for(application_choice, include_ratifying_provider: true).to_a).to match_array([training_provider_user, ratifying_provider_user])
+      end
+
+      it 'returns a provider user who is a member of the training and ratifying providers without duplicates' do
+        ratifying_provider = create(:provider)
+        training_provider = create(:provider)
+        provider_user = create(:provider_user, send_notifications: true, providers: [training_provider, ratifying_provider])
+        application_choice = create(:application_choice, course_option: create(:course_option, course: create(:course, provider: training_provider, accredited_provider: ratifying_provider)))
+
+        create(
+          :provider_user_notification_preferences,
+          :all_off,
+          provider_user: create(:provider_user, send_notifications: false, providers: [training_provider, ratifying_provider]),
+        )
+        create(:provider_user_notification_preferences)
+
+        expect(NotificationsList.for(
+          application_choice,
+          include_ratifying_provider: true,
+          event: :offer_accepted,
+        ).to_a).to match_array([provider_user])
       end
     end
   end
