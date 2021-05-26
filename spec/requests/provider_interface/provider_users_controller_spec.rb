df78e19c19ba6c8f3bf4101b3839ea4898e2f0cb
Steve Laing <steve.laing@gmail.com>
2021-05-26 16:11:03 +0100

    Track validation errors from provider users controller

---
+++ spec/requests/provider_interface/provider_users_controller_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe ProviderInterface::ProviderUsersController, type: :request do
+  include DfESignInHelpers
+
+  describe 'validation errors' do
+    let(:managing_user) { create(:provider_user, :with_manage_organisations, :with_manage_users, providers: [provider]) }
+    let(:provider_user) { create(:provider_user, providers: [provider]) }
+    let(:provider) { create(:provider, :with_signed_agreement) }
+
+    before do
+      allow(DfESignInUser).to receive(:load_from_session).and_return(managing_user)
+
+      user_exists_in_dfe_sign_in(email_address: managing_user.email_address)
+    end
+
+    # rubocop:disable RSpec/AnyInstance
+    it 'tracks validation errors for GET to edit_permissions' do
+      allow_any_instance_of(ProviderInterface::ProviderUserEditPermissionsForm).to receive(:invalid?).and_return(true)
+
+      expect { get provider_interface_provider_user_edit_permissions_path(provider_user, provider) }.to change(ValidationError, :count).by(1)
+    end
+
+    it 'tracks validation errors for PATCH to update_permissions' do
+      allow_any_instance_of(ProviderInterface::ProviderUserEditPermissionsForm).to receive(:save).and_return(false)
+      expect {
+        patch provider_interface_provider_user_edit_permissions_path(provider_user, provider),
+              params: { provider_interface_provider_user_edit_permissions_form: { provider_id: provider.id } }
+      }.to change(ValidationError, :count).by(1)
+    end
+    # rubocop:enable RSpec/AnyInstance
+
+    it 'tracks validation errors for PUT to update_providers' do
+      expect { patch provider_interface_provider_user_edit_providers_path(provider_user) }.to change(ValidationError, :count).by(1)
+    end
+  end
+end
