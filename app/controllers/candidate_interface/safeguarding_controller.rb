fdef460664c83cc125c7eaa88a187319da8a4eb2
David Gisbey <davidgisbey@Davids-MacBook-Pro.local>
2021-05-27 14:55:46 +0100

    Update the safeguarding section to use explicit backlinks

--- app/controllers/candidate_interface/safeguarding_controller.rb
+++ app/controllers/candidate_interface/safeguarding_controller.rb
@@ -7,6 +7,21 @@ module CandidateInterface
       @section_complete_form = SectionCompleteForm.new(completed: current_application.safeguarding_issues_completed)
     end
 
+    def new
+      @safeguarding_form = SafeguardingIssuesDeclarationForm.build_from_application(current_application)
+    end
+
+    def create
+      @safeguarding_form = SafeguardingIssuesDeclarationForm.new(safeguarding_params)
+
+      if @safeguarding_form.save(current_application)
+        redirect_to candidate_interface_review_safeguarding_path
+      else
+        track_validation_error(@safeguarding_form)
+        render :new
+      end
+    end
+
     def edit
       @safeguarding_form = SafeguardingIssuesDeclarationForm.build_from_application(current_application)
     end
