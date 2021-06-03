372e2fc8c700d04fe53b9573fa6f3a25a5b94d2e
benswannack <ben.swannack@softwire.com>
2021-06-03 11:55:23 +0100

    Move ethnicity constants to initializers file and make structure more useful

--- app/helpers/ethnic_background_helper.rb
+++ app/helpers/ethnic_background_helper.rb
@@ -1,26 +1,10 @@
 module EthnicBackgroundHelper
-  ETHNIC_GROUPS = [
-    'Asian or Asian British',
-    'Black, African, Black British or Caribbean',
-    'Mixed or multiple ethnic groups',
-    'White',
-    'Another ethnic group',
-  ].freeze
-
-  ETHNIC_BACKGROUNDS = {
-    'Asian or Asian British' => %w[Bangladeshi Chinese Indian Pakistani],
-    'Black, African, Black British or Caribbean' => %w[African Caribbean],
-    'Mixed or multiple ethnic groups' => ['Asian and White', 'Black African and White', 'Black Caribbean and White'],
-    'White' => ['British, English, Northern Irish, Scottish, or Welsh', 'Irish', 'Irish Traveller or Gypsy'],
-    'Another ethnic group' => %w[Arab],
-  }.freeze
-
-  OTHER_ETHNIC_BACKGROUNDS = {
-    'Asian or Asian British' => ['Another Asian background', 'Your Asian background (optional)'],
-    'Black, African, Black British or Caribbean' => ['Another Black background', 'Your Black background (optional)'],
-    'Mixed or multiple ethnic groups' => ['Another Mixed background', 'Your Mixed background (optional)'],
-    'White' => ['Another White background', 'Your White background (optional)'],
-    'Another ethnic group' => ['Another ethnic background', 'Describe your ethnic background (optional)'],
+  ETHNIC_BACKGROUND_TEXTFIELD_LABELS = {
+    EthnicGroup::ASIAN => 'Your Asian background (optional)',
+    EthnicGroup::BLACK => 'Your Black background (optional)',
+    EthnicGroup::MIXED => 'Your Mixed background (optional)',
+    EthnicGroup::WHITE => 'Your White background (optional)',
+    EthnicGroup::OTHER => 'Describe your ethnic background (optional)',
   }.freeze
 
   def ethnic_backgrounds(group)
@@ -31,7 +15,8 @@ module EthnicBackgroundHelper
       )
     end
 
-    button_label, textfield_label = OTHER_ETHNIC_BACKGROUNDS[group]
+    button_label = OTHER_ETHNIC_BACKGROUNDS[group]
+    textfield_label = ETHNIC_BACKGROUND_TEXTFIELD_LABELS[group]
 
     ethnic_backgrounds << OpenStruct.new(
       label: button_label,
@@ -49,7 +34,7 @@ module EthnicBackgroundHelper
   # used in the factory for generating sensible combinations in test data
   def all_combinations
     combos = []
-    ETHNIC_GROUPS.each do |group|
+    EthnicGroup.all.each do |group|
       ETHNIC_BACKGROUNDS[group].each do |bg|
         combos << [group, bg]
       end
