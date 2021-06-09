27b924d4f919801be344c23de746dbb800bcb98d
Vignesh.N <vignesh.natraj@digital.education.gov.uk>
2021-06-09 17:07:22 +0100

    Add custom tags to indentiy in Grafana dashboard

---
+++ config/initializers/yabeda.rb
@@ -0,0 +1,16 @@
+if ENV.key?('VCAP_APPLICATION')
+  vcap_config = JSON.parse(ENV['VCAP_APPLICATION'])
+  app_name = vcap_config['name']
+  app_guid = vcap_config['application_id']
+  org_name = vcap_config['organization_name']
+  space_name = vcap_config['space_name']
+  app_instance = ENV['CF_INSTANCE_INDEX']
+
+  Yabeda.configure do
+    default_tag :app, app_name
+    default_tag :guid, app_guid
+    default_tag :exported_instance, app_instance
+    default_tag :organisation, org_name
+    default_tag :space, space_name
+  end
+end
