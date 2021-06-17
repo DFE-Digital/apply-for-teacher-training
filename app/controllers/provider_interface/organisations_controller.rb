5a1247dbefa1cce356529b8266f92a2beaed165f
SamJamCul <samuel.culley@digital.education.gov.uk>
2021-06-17 16:00:35 +0100

    Only show details of organisations for set up permissions
    When navigating to the details of permissions a provider
    can manage, only surface ones that have been set up.
    
    If the provider user is associated to a provider that is
    also ratifying provider they cannot manage surface the
    permission as it is read only.

--- app/controllers/provider_interface/organisations_controller.rb
+++ app/controllers/provider_interface/organisations_controller.rb
@@ -3,12 +3,13 @@ module ProviderInterface
     before_action :render_403_unless_organisation_valid_for_user, only: :show
 
     def index
-      @manageable_providers = current_provider_user.authorisation.providers_that_actor_can_manage_organisations_for(with_set_up_permissions: true)
+      @manageable_providers = manageable_providers
     end
 
     def show
       @training_permissions = ProviderRelationshipPermissions.where(training_provider_id: params[:id])
         .or(ProviderRelationshipPermissions.where(training_provider_id: manageable_providers, ratifying_provider_id: params[:id]))
+        .where.not(setup_at: nil)
         .includes(:training_provider)
 
       @ratifying_permissions = ProviderRelationshipPermissions.where(ratifying_provider_id: params[:id])
@@ -18,7 +19,7 @@ module ProviderInterface
   private
 
     def manageable_providers
-      @_manageable_providers ||= current_provider_user.authorisation.providers_that_actor_can_manage_organisations_for
+      @_manageable_providers ||= current_provider_user.authorisation.providers_that_actor_can_manage_organisations_for(with_set_up_permissions: true)
     end
 
     def render_403_unless_organisation_valid_for_user
--- spec/system/provider_interface/viewing_organisational_permissions_spec.rb
+++ spec/system/provider_interface/viewing_organisational_permissions_spec.rb
@@ -19,11 +19,25 @@ RSpec.feature 'Viewing organisational permissions' do
 
     when_i_go_to_organisational_permissions
     then_i_can_see_organisations_with_setup_permissions
+
+    when_i_go_to_the_training_provider_permissions
+    then_i_can_only_see_permissions_that_have_been_set_up
   end
 
   def given_i_am_a_provider_user_with_dfe_sign_in
     provider_exists_in_dfe_sign_in
-    provider_user_exists_in_apply_database
+    provider_user_is_associated_with_a_training_provider
+  end
+
+  def provider_user_is_associated_with_a_training_provider
+    @training_provider = create(:provider, :with_signed_agreement, code: 'ABC')
+    @ratifying_provider = create(:provider, :with_signed_agreement, code: 'DEF')
+    @another_ratifying_provider = create(:provider, :with_signed_agreement, code: 'GHI')
+    create(:provider_user,
+           :with_notifications_enabled,
+           providers: [@training_provider],
+           dfe_sign_in_uid: 'DFE_SIGN_IN_UID',
+           email_address: 'email@provider.ac.uk')
   end
 
   def then_i_cannot_see_organisational_permissions
@@ -38,32 +52,25 @@ RSpec.feature 'Viewing organisational permissions' do
   def given_i_can_manage_multiple_organisations_that_do_not_have_permissions_set_up
     @provider_user = ProviderUser.last
 
-    @training_provider = Provider.find_by(code: 'ABC')
-    @ratifying_provider = Provider.find_by(code: 'DEF')
-    @another_ratifying_provider = create(:provider, :with_signed_agreement, code: 'GHI', name: 'Another Example Provider')
-    @provider_user.providers << @another_ratifying_provider
-
     @permission_one = create(
       :provider_relationship_permissions,
       training_provider: @training_provider,
       ratifying_provider: @ratifying_provider,
       setup_at: nil,
-      )
+    )
 
     @permission_two = create(
       :provider_relationship_permissions,
       training_provider: @training_provider,
       ratifying_provider: @another_ratifying_provider,
       setup_at: nil,
-      )
+    )
 
     @provider_user.provider_permissions.update_all(manage_organisations: true)
   end
 
   def then_i_can_see_organisations_with_setup_permissions
-    expect(page).to have_content("#{@training_provider.name}")
-    expect(page).to have_content("#{@ratifying_provider.name}")
-    expect(page).not_to have_content("#{@another_ratifying_provider.name}")
+    expect(page).to have_content(@training_provider.name.to_s)
   end
 
   def when_i_set_up_permissions_for_a_provider
@@ -76,6 +83,15 @@ RSpec.feature 'Viewing organisational permissions' do
 
   def when_i_go_to_organisational_permissions
     click_on 'Organisational permissions'
+  end
+
+  def when_i_go_to_the_training_provider_permissions
+    click_on @training_provider.name.to_s
+  end
+
+  def then_i_can_only_see_permissions_that_have_been_set_up
+    expect(page).to have_content("#{@training_provider.name} and #{@ratifying_provider.name}")
+    expect(page).not_to have_content("#{@training_provider.name} and #{@another_ratifying_provider.name}")
   end
 
   alias_method :and_i_go_to_my_account, :when_i_go_to_my_account
