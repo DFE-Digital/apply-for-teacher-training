74c969c394e1c6a40fd7fa1ffa7d2f40f93f2561
benswannack <ben.swannack@softwire.com>
2021-06-17 14:47:01 +0100

    Add ConditionsListComponent for rendering conditions

---
+++ app/components/provider_interface/conditions_list_component.rb
@@ -0,0 +1,9 @@
+module ProviderInterface
+  class ConditionsListComponent < ViewComponent::Base
+    attr_reader :conditions
+
+    def initialize(conditions)
+      @conditions = conditions || []
+    end
+  end
+end
