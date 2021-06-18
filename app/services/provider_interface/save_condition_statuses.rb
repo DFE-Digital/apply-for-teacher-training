14da6f4eb15d76b0184a2c6c970190e579404770
benswannack <ben.swannack@softwire.com>
2021-06-18 15:50:23 +0100

    Add service to save conditions and deal with consequences

---
+++ app/services/provider_interface/save_condition_statuses.rb
@@ -0,0 +1,61 @@
+module ProviderInterface
+  class SaveConditionStatuses
+    include ImpersonationAuditHelper
+
+    def initialize(actor:, application_choice:)
+      @auth = ProviderAuthorisation.new(actor: actor)
+      @application_choice = application_choice
+    end
+
+    def save!
+      auth.assert_can_make_decisions!(application_choice: application_choice, course_option: application_choice.current_course_option)
+
+      audit(auth.actor) do
+        ActiveRecord::Base.transaction do
+          save_conditions_and_update_application!
+        end
+      end
+    end
+
+  private
+
+    attr_reader :auth, :application_choice
+
+    def save_conditions_and_update_application!
+      if conditions_met?
+        transition_to_conditions_met!
+      elsif conditions_not_met?
+        transition_to_conditions_not_met!
+      else
+        save_conditions
+      end
+    end
+
+    def conditions_met?
+      application_choice.offer.conditions.all?(&:met?)
+    end
+
+    def conditions_not_met?
+      application_choice.offer.conditions.any?(&:unmet?)
+    end
+
+    def transition_to_conditions_met!
+      ApplicationStateChange.new(application_choice).confirm_conditions_met!
+      application_choice.update!(recruited_at: Time.zone.now)
+      save_conditions
+      CandidateMailer.conditions_met(application_choice).deliver_later
+      StateChangeNotifier.new(:recruited, application_choice).application_outcome_notification
+    end
+
+    def transition_to_conditions_not_met!
+      ApplicationStateChange.new(application_choice).conditions_not_met!
+      application_choice.update!(conditions_not_met_at: Time.zone.now)
+      save_conditions
+      CandidateMailer.conditions_not_met(application_choice).deliver_later
+    end
+
+    def save_conditions
+      application_choice.offer.conditions.each(&:save!)
+    end
+  end
+end
---
+++ spec/services/provider_interface/save_condition_statuses_spec.rb
@@ -0,0 +1,103 @@
+require 'rails_helper'
+
+RSpec.describe ProviderInterface::SaveConditionStatuses do
+  let(:application_choice) { create(:application_choice, :with_accepted_offer, offer: offer) }
+  let(:offer) { create(:offer, conditions: conditions) }
+  let(:conditions) { create_list(:offer_condition, 3, status: :pending) }
+
+  let(:provider_user) { create(:provider_user, :with_make_decisions, providers: [application_choice.current_course.provider]) }
+
+  let(:service) { described_class.new(actor: provider_user, application_choice: application_choice) }
+
+  describe 'save!' do
+    context 'provider user does not have make_decisions' do
+      let(:provider_user) { create(:provider_user, providers: [application_choice.current_course.provider]) }
+
+      it 'raises an error' do
+        expect { service.save! }.to raise_error(ProviderAuthorisation::NotAuthorisedError)
+      end
+    end
+
+    context 'when a condition status is changed' do
+      before { application_choice.offer.conditions.each { |condition| condition.status = 'met' } }
+
+      it 'attributes audits to the actor', with_audited: true do
+        expect { service.save! }.to change(offer.conditions.last.audits, :count).by(1)
+        expect(offer.conditions.last.audits.last.user).to eq(provider_user)
+      end
+    end
+
+    context 'when none of the conditions are marked as unmet' do
+      context 'when all conditions are met' do
+        before { application_choice.offer.conditions.each { |condition| condition.status = 'met' } }
+
+        it 'transitions the application to the recruited state' do
+          Timecop.freeze do
+            expect { service.save! }.to change(application_choice, :status).from('pending_conditions').to('recruited')
+                                    .and change(application_choice, :recruited_at).to(Time.zone.now)
+          end
+        end
+
+        it 'sends an email to the candidate', sidekiq: true do
+          service.save!
+          expect(ActionMailer::Base.deliveries.first['rails-mail-template'].value).to eq('conditions_met')
+        end
+
+        # rubocop:disable RSpec/NestedGroups
+        context 'when the application is not in the pending_conditions state' do
+          let(:application_choice) { create(:application_choice, :with_recruited, offer: offer) }
+
+          it 'raises a Workflow::NoTransitionAllowed error' do
+            expect { service.save! }.to raise_error(Workflow::NoTransitionAllowed)
+          end
+        end
+        # rubocop:enable RSpec/NestedGroups
+      end
+
+      context 'when one conditions is still pending' do
+        before do
+          application_choice.offer.conditions.each_with_index do |condition, index|
+            condition.status = index.zero? ? 'pending' : 'met'
+          end
+        end
+
+        it 'keeps the application in the pending_conditions state' do
+          expect { service.save! }.not_to change(application_choice, :status)
+        end
+
+        it 'does not send an email to the candidate', sidekiq: true do
+          service.save!
+          expect(ActionMailer::Base.deliveries).to be_empty
+        end
+      end
+    end
+
+    context 'when one of the conditions is marked as unmet' do
+      before do
+        application_choice.offer.conditions.each_with_index do |condition, index|
+          condition.status = index.zero? ? 'unmet' : 'pending'
+        end
+      end
+
+      it 'transitions the application to the conditions_not_met state' do
+        Timecop.freeze do
+          expect { service.save! }.to change(application_choice, :status).from('pending_conditions').to('conditions_not_met')
+                                  .and change(application_choice, :conditions_not_met_at).to(Time.zone.now)
+        end
+      end
+
+      it 'sends an email to the candidate', sidekiq: true do
+        service.save!
+        expect(ActionMailer::Base.deliveries.first['rails-mail-template'].value).to eq('conditions_not_met')
+      end
+
+      context 'when the application is not in the pending_conditions state' do
+        let(:application_choice) { create(:application_choice, :with_conditions_not_met, offer: offer) }
+
+        it 'raises a Workflow::NoTransitionAllowed error' do
+          expect { service.save! }.to raise_error(Workflow::NoTransitionAllowed)
+        end
+      end
+    end
+  end
+end
