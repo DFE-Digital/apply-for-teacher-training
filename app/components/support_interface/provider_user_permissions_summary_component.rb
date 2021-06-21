70ff098221832456626afdee05271122353abdf3
Malcolm Baig <malcolm.baig@gmail.com>
2021-06-21 20:51:50 +0100

    Add reworked permissions to provider user support page
    - Render permissions in a PermissionsListComponent, rendered as part of
      a ProviderUserPermissionsSummaryComponent
    - Implement PermissionsListComponent as a slightly stripped down version
      of the equivalent component in the provider interface

---
+++ app/components/support_interface/provider_user_permissions_summary_component.rb
@@ -0,0 +1,35 @@
+module SupportInterface
+  class ProviderUserPermissionsSummaryComponent < SummaryListComponent
+    include ViewHelper
+
+    attr_reader :provider_user
+
+    def initialize(provider_user)
+      @provider_user = provider_user
+    end
+
+    def rows
+      provider_user
+        .provider_permissions
+        .includes(provider: %i[ratifying_provider_permissions training_provider_permissions])
+        .map do |permission|
+        {
+          key: permission.provider.name_and_code,
+          value: render(SupportInterface::PermissionsListComponent.new(permission)),
+          action: 'permissions',
+          change_path: change_path,
+        }
+      end
+    end
+
+  private
+
+    def change_path
+      if FeatureFlag.active?(:new_provider_user_flow)
+        support_interface_edit_permissions_path(provider_user)
+      else
+        edit_support_interface_provider_user_path(provider_user)
+      end
+    end
+  end
+end
