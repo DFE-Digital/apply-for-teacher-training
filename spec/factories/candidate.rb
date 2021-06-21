98a7256b1b5f2a960b32841c060af352ce6017d1
James Glenn <47917431+JR-G@users.noreply.github.com>
2021-06-21 11:07:18 +0100

    Update candidate_api migration and candidate factory
    The backfill needed access to additional columns to update the date.
    The test didn't reveal this as the candidate_api_updated_at value
    was not actually getting set as nil. The candidate factory has also
    been updated so this attribute can optional be set as nil.

--- spec/factories/candidate.rb
+++ spec/factories/candidate.rb
@@ -2,5 +2,16 @@ FactoryBot.define do
   factory :candidate do
     email_address { "#{SecureRandom.hex(5)}@example.com" }
     sign_up_email_bounced { false }
+
+    transient do
+      skip_candidate_api_updated_at { false }
+    end
+
+    callback(:after_create) do |candidate, evaluator|
+      if evaluator.skip_candidate_api_updated_at
+        candidate.candidate_api_updated_at = nil
+        candidate.save
+      end
+    end
   end
 end
