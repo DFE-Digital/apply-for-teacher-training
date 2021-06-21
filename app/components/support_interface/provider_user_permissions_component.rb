e0f31d9d8edde5223465bd3e02a3a5bb79af095c
Malcolm Baig <malcolm.baig@gmail.com>
2021-06-21 20:51:55 +0100

    Remove SupportInterface::ProviderUserPermissionsComponent
    The provider user support page has been reworked to match new designs,
    and this component is no longer used.

--- app/components/support_interface/provider_user_permissions_component.rb
+++
@@ -1,9 +0,0 @@
-module SupportInterface
-  class ProviderUserPermissionsComponent < ViewComponent::Base
-    include ViewHelper
-
-    def initialize(provider_user:)
-      @provider_user = provider_user
-    end
-  end
-end
