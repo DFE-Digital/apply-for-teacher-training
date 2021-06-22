7e252b30c5a2592a14dae406a05eec7c901b2768
Steve Hook <steve.hook@gmail.com>
2021-06-23 00:01:40 +0100

    Only carry over required work history attributes

--- app/services/duplicate_application.rb
+++ app/services/duplicate_application.rb
@@ -9,6 +9,7 @@ class DuplicateApplication
 
   IGNORED_ATTRIBUTES = %w[id created_at updated_at submitted_at course_choices_completed phase support_reference].freeze
   IGNORED_CHILD_ATTRIBUTES = %w[id created_at updated_at application_form_id public_id].freeze
+  IGNORED_LEGACY_WORK_HISTORY_ATTRIBUTES = %w[working_with_children working_pattern].freeze
 
   def duplicate
     attrs = original_application_form.attributes.except(
@@ -23,7 +24,7 @@ class DuplicateApplication
 
     original_application_form.application_work_experiences.each do |w|
       new_application_form.application_work_experiences.create!(
-        w.attributes.except(*IGNORED_CHILD_ATTRIBUTES),
+        w.attributes.except(*ignored_work_experience_attributes),
       )
     end
     if original_application_form.feature_restructured_work_history == false &&
@@ -65,5 +66,16 @@ class DuplicateApplication
     end
 
     new_application_form
+  end
+
+private
+
+  def ignored_work_experience_attributes
+    if original_application_form.feature_restructured_work_history == false &&
+        FeatureFlag.active?(:restructured_work_history)
+      IGNORED_CHILD_ATTRIBUTES + IGNORED_LEGACY_WORK_HISTORY_ATTRIBUTES
+    else
+      IGNORED_CHILD_ATTRIBUTES
+    end
   end
 end
