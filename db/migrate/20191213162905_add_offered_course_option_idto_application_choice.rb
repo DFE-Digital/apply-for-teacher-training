fe8d22ef4e5fd2fed8726d0e4aee995d8edd34c9
cwrw <abeer.salameh@gmail.com>
2021-06-22 08:21:23 +0100

    Remove old unused index key as this column has now been removed anyway
    To address the cop: `Rails/AddColumnIndex` where `add_column` does not
    accept an index key.

--- db/migrate/20191213162905_add_offered_course_option_idto_application_choice.rb
+++ db/migrate/20191213162905_add_offered_course_option_idto_application_choice.rb
@@ -1,6 +1,6 @@
 class AddOfferedCourseOptionIdtoApplicationChoice < ActiveRecord::Migration[6.0]
   def change
-    add_column :application_choices, :offered_course_option_id, :integer, index: true
+    add_column :application_choices, :offered_course_option_id, :integer
     add_foreign_key :application_choices, :course_options, column: :offered_course_option_id
   end
 end
