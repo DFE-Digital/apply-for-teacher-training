b0d395acba5e65e4bbc71c8aff738f9857aac85d
Duncan Brown <duncanjbrown@gmail.com>
2021-06-28 20:24:55 +0100

    Use an ENV var for the BigQuery table name
    This will enable us to migrate to the new schema
    
    To make this work, stub ENV with ClimateControl in the BigQuery spec.
    This means that if ENV gets another call then it doesn't bail out
    because the argument isn't stubbed

--- app/workers/send_request_events_to_bigquery.rb
+++ app/workers/send_request_events_to_bigquery.rb
@@ -3,12 +3,11 @@ class SendRequestEventsToBigquery
 
   sidekiq_options retry: 3, queue: :low_priority
 
-  TABLE_NAME = 'events'.freeze
-
   def perform(request_event_json)
+    table_name = ENV.fetch('BIG_QUERY_TABLE_NAME', 'events')
     bq = Google::Cloud::Bigquery.new(project: ENV.fetch('BIG_QUERY_PROJECT_ID'))
     dataset = bq.dataset(ENV.fetch('BIG_QUERY_DATASET'), skip_lookup: true)
-    bq_table = dataset.table(TABLE_NAME, skip_lookup: true)
+    bq_table = dataset.table(table_name, skip_lookup: true)
     bq_table.insert([request_event_json])
   end
 end
