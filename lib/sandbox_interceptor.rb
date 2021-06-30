b05df0485b219c7d0176230c67577c94b466b3cf
Despo Pentara <despo@extractmethod.com>
2021-06-30 11:35:26 +0100

    Configure Naming/InclusiveLanguage cop
    Ignore strings and symbols as we can’t control content

--- lib/sandbox_interceptor.rb
+++ lib/sandbox_interceptor.rb
@@ -1,11 +1,11 @@
 class SandboxInterceptor
-  PROVIDER_EMAIL_WHITELIST = %w[fallback_sign_in_email account_created].freeze
+  PROVIDER_EMAIL_ALLOWLIST = %w[fallback_sign_in_email account_created].freeze
 
   def self.delivering_email(message)
     return unless HostingEnvironment.sandbox_mode?
 
     if message.header['rails_mailer'].value == 'provider_mailer' &&
-        PROVIDER_EMAIL_WHITELIST.exclude?(message.header['rails_mail_template'].value)
+        PROVIDER_EMAIL_ALLOWLIST.exclude?(message.header['rails_mail_template'].value)
       message.perform_deliveries = false
     end
   end
