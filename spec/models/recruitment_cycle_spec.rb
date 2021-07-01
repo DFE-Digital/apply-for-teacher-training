5cdf2968ff1522e0dbe767c664223bc3b69f2f87
Toby Retallick <toby.retallick@gmail.com>
2021-07-01 13:33:08 +0100

    Stub 'current_year' and update spec assertions

--- spec/models/recruitment_cycle_spec.rb
+++ spec/models/recruitment_cycle_spec.rb
@@ -12,29 +12,29 @@ RSpec.describe RecruitmentCycle do
   end
 
   describe '.next_year' do
-    it 'is 2021' do
-      Timecop.travel(Time.zone.local(2020, 1, 1, 12, 0, 0)) do
-        expect(RecruitmentCycle.next_year).to eq(2021)
-      end
+    it 'is 2021 if the current year is 2020' do
+      allow(EndOfCycleTimetable).to receive(:current_year).and_return(2020)
+
+      expect(RecruitmentCycle.next_year).to eq(2021)
     end
   end
 
   describe '.previous_year' do
-    it 'is 2019' do
-      Timecop.travel(Time.zone.local(2020, 1, 1, 12, 0, 0)) do
-        expect(RecruitmentCycle.previous_year).to eq(2019)
-      end
+    it 'is 2019 if the current year is 2020' do
+      allow(EndOfCycleTimetable).to receive(:current_year).and_return(2020)
+
+      expect(RecruitmentCycle.previous_year).to eq(2019)
     end
   end
 
   describe '.cycle_name' do
-    it 'defaults to current year to the following year' do
-      Timecop.travel(Time.zone.local(2020, 1, 1, 1, 0, 0)) do
-        expect(RecruitmentCycle.cycle_name).to eq('2019 to 2020')
-      end
+    it 'defaults from current year to the following year' do
+      allow(EndOfCycleTimetable).to receive(:current_year).and_return(2020)
+
+      expect(RecruitmentCycle.cycle_name).to eq('2019 to 2020')
     end
 
-    it 'is the argument year to the following year' do
+    it 'is from argument(year) to the following year' do
       expect(RecruitmentCycle.cycle_name(2021)).to eq('2020 to 2021')
     end
   end
