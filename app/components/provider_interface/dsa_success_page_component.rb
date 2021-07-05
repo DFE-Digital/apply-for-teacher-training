9a378a346d28cc12f9323a46ebd97d3a54843a34
benswannack <ben.swannack@softwire.com>
2021-07-05 16:26:39 +0100

    Add new next steps content for DSA signed page behind feature flag

--- app/components/provider_interface/dsa_success_page_component.rb
+++ app/components/provider_interface/dsa_success_page_component.rb
@@ -6,5 +6,9 @@ module ProviderInterface
       @provider_user = provider_user
       @provider_permission_setup_pending = provider_permission_setup_pending
     end
+
+    def user_can_manage_users?
+      provider_user.authorisation.can_manage_users_for_at_least_one_provider?
+    end
   end
 end
