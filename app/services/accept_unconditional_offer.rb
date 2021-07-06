2bf1c5bb9596ad855f818ef295c339a8caf64f5a
Steve Laing <steve.laing@gmail.com>
2021-07-06 12:18:04 +0100

    Withdraw associated applications when accepting unconditional offers
    When a candidate accepts an unconditional offer their other application choices should be withdrawn/declined appropriate to their current state.

--- app/services/accept_unconditional_offer.rb
+++ app/services/accept_unconditional_offer.rb
@@ -1,12 +1,10 @@
-class AcceptUnconditionalOffer
-  def initialize(application_choice:)
-    @application_choice = application_choice
-  end
-
+class AcceptUnconditionalOffer < AcceptOffer
   def save!
     ActiveRecord::Base.transaction do
       ApplicationStateChange.new(@application_choice).accept_unconditional_offer!
       @application_choice.update!(accepted_at: Time.zone.now, recruited_at: Time.zone.now)
+
+      withdraw_and_decline_associated_application_choices!
     end
 
     NotificationsList.for(@application_choice, event: :unconditional_offer_accepted, include_ratifying_provider: true).each do |provider_user|
