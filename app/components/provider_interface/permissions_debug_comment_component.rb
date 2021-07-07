d137a809401bb1b441e8867db737699aa853be64
Despo Pentara <despo@extractmethod.com>
2021-07-08 00:24:53 +0100

    Refactoring to reduce Metrics/PerceivedComplexity cop errors

--- app/components/provider_interface/permissions_debug_comment_component.rb
+++ app/components/provider_interface/permissions_debug_comment_component.rb
@@ -2,28 +2,36 @@ module ProviderInterface
   class PermissionsDebugCommentComponent < ViewComponent::Base
     include ViewHelper
 
-    attr_reader :f
+    attr_reader :analysis
 
     def initialize(auth_analysis:)
       @analysis = auth_analysis
+    end
 
-      @f = [
-        (@analysis.ratified_course? ? '✔' : '✗'),
-        (@analysis.provider_user_associated_with_training_provider? ? 'Trains' : 'Ratifies'),
-        (@analysis.provider_user_has_user_level_access? ? '✔' : '✗'),
-        (@analysis.provider_relationship_allows_access? ? '✔' : '✗'),
-        (@analysis.provider_relationship_has_been_set_up? ? '✔' : '✗'),
-        (@analysis.provider_user_can_manage_users? ? '✔' : '✗'),
-        (@analysis.provider_user_can_manage_organisations? ? '✔' : '✗'),
-      ].map { |s| s.ljust(9, ' ') }
+    def formatted_details
+      @formatted_details ||= details.map { |s| s.ljust(9, ' ') }
     end
 
     def permission_name
-      @analysis.permission.to_s.ljust(37, ' ')
+      analysis.permission.to_s.ljust(37, ' ')
     end
 
     def ids_block
-      "[provider user #{@analysis.provider_user.id} vs. course #{@analysis.course.id}]".rjust(45, ' ')
+      "[provider user #{analysis.provider_user.id} vs. course #{analysis.course.id}]".rjust(45, ' ')
+    end
+
+  private
+
+    def details
+      [
+        (analysis.ratified_course? ? '✔' : '✗'),
+        (analysis.provider_user_associated_with_training_provider? ? 'Trains' : 'Ratifies'),
+        (analysis.provider_user_has_user_level_access? ? '✔' : '✗'),
+        (analysis.provider_relationship_allows_access? ? '✔' : '✗'),
+        (analysis.provider_relationship_has_been_set_up? ? '✔' : '✗'),
+        (analysis.provider_user_can_manage_users? ? '✔' : '✗'),
+        (analysis.provider_user_can_manage_organisations? ? '✔' : '✗'),
+      ]
     end
   end
 end
--- app/components/qualification_title_component.rb
+++ app/components/qualification_title_component.rb
@@ -1,37 +1,25 @@
 # NOTE: This component is used by both provider and support UIs
 class QualificationTitleComponent < ViewComponent::Base
+  attr_accessor :qualification
+
   def initialize(qualification:)
     @qualification = qualification
   end
 
   def qualification_type
-    if @qualification.level == 'gcse'
-      return type_for_missing_qualification if @qualification.missing_qualification?
-      return type_for_other_uk_qualification if @qualification.other_uk_qualification_type.present?
-      return type_for_non_uk_qualification if @qualification.non_uk_qualification_type.present?
+    text = if qualification.gcse?
+             gcse_qualification_type
+           elsif qualification.degree?
+             degree_qualification_type
+           elsif qualification.other?
+             other_qualification_type
+           end
 
-      return type_for_gcse
-    elsif @qualification.degree?
-      return type_for_degree
-    elsif @qualification.level == 'other'
-      return type_for_other_uk_qualification if @qualification.other_uk_qualification_type.present?
-      return type_for_non_uk_qualification if @qualification.non_uk_qualification_type.present?
-    end
-
-    @qualification.qualification_type
-  end
-
-  def type_for_degree
-    hesa_degree_type = Hesa::DegreeType.find_by_hesa_code(hesa_code)
-    if hesa_degree_type
-      hesa_degree_type.shortest_display_name
-    else
-      @qualification.qualification_type
-    end
+    text || qualification.qualification_type
   end
 
   def hesa_code
-    @qualification.qualification_type_hesa_code
+    qualification.qualification_type_hesa_code
   end
 
 private
@@ -41,17 +29,40 @@ private
   end
 
   def type_for_other_uk_qualification
-    @qualification.other_uk_qualification_type
+    qualification.other_uk_qualification_type
   end
 
   def type_for_non_uk_qualification
-    @qualification.non_uk_qualification_type
+    qualification.non_uk_qualification_type
   end
 
   def type_for_gcse
     ApplicationQualification.human_attribute_name(
-      "qualification_type.#{@qualification.qualification_type}",
-      default: @qualification.qualification_type,
+      "qualification_type.#{qualification.qualification_type}",
+      default: qualification.qualification_type,
     )
+  end
+
+  def gcse_qualification_type
+    return type_for_missing_qualification if qualification.missing_qualification?
+    return type_for_other_uk_qualification if qualification.other_uk_qualification_type.present?
+    return type_for_non_uk_qualification if qualification.non_uk_qualification_type.present?
+
+    type_for_gcse
+  end
+
+  def other_qualification_type
+    return type_for_other_uk_qualification if qualification.other_uk_qualification_type.present?
+
+    type_for_non_uk_qualification if qualification.non_uk_qualification_type.present?
+  end
+
+  def degree_qualification_type
+    hesa_degree_type = Hesa::DegreeType.find_by_hesa_code(hesa_code)
+    if hesa_degree_type
+      hesa_degree_type.shortest_display_name
+    else
+      qualification.qualification_type
+    end
   end
 end
