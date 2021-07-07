afd96f7523c300c0e902a65c1ce9d92baeab25e4
george-bit <george.holborn@digital.education.gov.uk>
2021-07-07 19:03:14 +0100

    Add positive feedback metrics to feature metrics dashboard
    Adding methods to the pre-existing `SatisfactionSurveyMetrics` to
    measure number of positive responses. Have reused existing method
    for all surveys with feedback and using this to measure against
    `feedback_level`s of ver_ satisfied and satisfied.

--- app/models/satisfaction_survey_feature_metrics.rb
+++ app/models/satisfaction_survey_feature_metrics.rb
@@ -12,11 +12,29 @@ class SatisfactionSurveyFeatureMetrics
     success_count / (fail_count + success_count)
   end
 
+  def positive_feedback_rate(
+    start_time,
+    end_time = Time.zone.now.end_of_day
+  )
+    feedback_count = application_forms_with_feedback(start_time, end_time).count.to_f
+    positive_feedback_count = application_forms_with_positive_feedback(start_time, end_time).count.to_f
+    return nil if feedback_count.zero?
+
+    positive_feedback_count / feedback_count
+  end
+
   def formatted_response_rate(
     start_time,
     end_time = Time.zone.now.end_of_day
   )
     format_as_percentage(response_rate(start_time, end_time))
+  end
+
+  def formatted_positive_feedback_rate(
+    start_time,
+    end_time = Time.zone.now.end_of_day
+  )
+    format_as_percentage(positive_feedback_rate(start_time, end_time))
   end
 
 private
@@ -39,6 +57,16 @@ private
       )
       .where('application_forms.submitted_at BETWEEN ? AND ?', start_time, end_time)
       .where.not(feedback_satisfaction_level: '')
+      .where(recruitment_cycle_year: RecruitmentCycle.current_year)
+  end
+
+  def application_forms_with_positive_feedback(start_time, end_time)
+    ApplicationForm
+      .where(
+        recruitment_cycle_year: RecruitmentCycle.current_year,
+      )
+      .where('application_forms.submitted_at BETWEEN ? AND ?', start_time, end_time)
+      .where(feedback_satisfaction_level: 'very_satisfied').or(ApplicationForm.where(feedback_satisfaction_level: 'satisfied'))
       .where(recruitment_cycle_year: RecruitmentCycle.current_year)
   end
 
