f056334e73f0355a4af182450a06dbd4fc968c0e
James Glenn <47917431+JR-G@users.noreply.github.com>
2021-07-12 13:24:33 +0100

    Add deliver_later to EoC deadline mailer

--- app/services/send_eoc_deadline_reminder_email_to_candidate.rb
+++ app/services/send_eoc_deadline_reminder_email_to_candidate.rb
@@ -1,6 +1,6 @@
 class SendEocDeadlineReminderEmailToCandidate
   def self.call(application_form:)
-    CandidateMailer.eoc_deadline_reminder(application_form)
+    CandidateMailer.eoc_deadline_reminder(application_form).deliver_later
     ChaserSent.create!(chased: application_form, chaser_type: :eoc_deadline_reminder)
   end
 end
