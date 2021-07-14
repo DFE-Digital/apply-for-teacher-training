4ab6198bc736649faed3caa0587f1fbe8d2d57d8
Malcolm Baig <malcolm.baig@gmail.com>
2021-07-14 21:45:38 +0100

    Refine Apply JMeter test plan
    A series of fixes and improvements that put this test into a runnable
    state.
    
    - Fix form submissions by using a multi-part POST, which ensures
      authenticity tokens are correctly parsed
    - Refactor out `think` method (slightly clearer, also helps when
      manually testing locally to have a single place to set think time)
    - For now, set the total duration of the test to 15 minutes
    - Commenting and layout changes for clarity

--- jmeter/plans/apply.rb
+++ jmeter/plans/apply.rb
@@ -6,87 +6,108 @@ def url(path)
   BASEURL + path
 end
 
-# 975 concurrent users, ~7 minutes per session
+def think(milliseconds = 500)
+  think_time milliseconds
+end
+
+# 975 concurrent users, 15 minute run time
 test do
   cookies clear_each_iteration: true
+  view_results_tree
   thread_count = 975
 
-  threads count: thread_count, continue_forever: true, duration: 420 do
+  threads count: thread_count, continue_forever: true, duration: 900 do
     #-> Sign up
     visit name: 'Account page', url: url('/candidate/account') do
       extract name: 'authenticity_token', regex: 'name="authenticity_token" value="(.+?)"'
     end
-    think_time 2000
+
+    think
+
     submit(
-      name: 'Account page - create an account', url: url('/candidate/account'),
+      'DO_MULTIPART_POST': 'true',
+      name: 'Account page - choose no existing account', url: url('/candidate/account'),
       fill_in: {
         'candidate_interface_create_account_or_sign_in_form[existing_account]' => 'false',
         'authenticity_token' => '${authenticity_token}',
       }
     )
-    think_time 1000
+
+    think
+
     visit name: 'Sign up page', url: url('/candidate/sign-up') do
       extract name: 'authenticity_token', regex: 'name="authenticity_token" value="(.+?)"'
     end
-    think_time 2000
+
+    think
+
     submit(
-      name: 'Sign up page - submit email', url: url('/candidate/account'),
+      'DO_MULTIPART_POST': 'true',
+      name: 'Sign up page - submit email', url: url('/candidate/sign-up'),
       fill_in: {
-        'authenticity_token': '${authenticity_token}',
         'candidate_interface_sign_up_form[email_address]': '${__threadNum}' + '@loadtest.example.com',
         'candidate_interface_sign_up_form[accept_ts_and_cs][]': '',
         'candidate_interface_sign_up_form[accept_ts_and_cs]': 'true',
+        'authenticity_token': '${authenticity_token}',
+        'commit': 'Continue',
+      }
+    ) do
+      # Assume that an authentication bypass is present which redirects to the magic link page at this point
+      extract name: 'sign_in_token', xpath: '//input[@type="hidden" and @name="token"]/@value', tolerant: true
+      extract name: 'authenticity_token', regex: 'name="authenticity_token" value="(.+?)"'
+    end
+
+    think
+
+    #-> Sign in
+    submit(
+      'DO_MULTIPART_POST': 'true',
+      name: 'Confirm authentication - continue', url: url('/candidate/sign-in/confirm'),
+      fill_in: {
+        'authenticity_token': '${authenticity_token}',
+        'token': '${sign_in_token}',
         'commit': 'Continue',
       }
     )
-    think_time 1000
 
-    #-> Sign in
-    visit name: 'Account page', url: url('/candidate/account') do
-      extract name: 'authenticity_token', regex: 'name="authenticity_token" value="(.+?)"'
-    end
-    think_time 2000
-    submit(
-      name: 'Account page - sign in', url: url('/candidate/account'),
-      fill_in: {
-        'candidate-interface-create-account-or-sign-in-form-email-field' => '${__threadNum}' + '@loadtest.example.com',
-        'authenticity_token' => '${authenticity_token}',
-      }
-    )
-    think_time 1000
-    # TODO: add a bypass to our auth logic that immediately signs in @loadtest.example.com email addresses
+    think
 
     #-> Partially navigate through course choice flow
     visit name: 'Do you know which course?', url: url('/candidate/application/courses/choose') do
       extract name: 'authenticity_token', regex: 'name="authenticity_token" value="(.+?)"'
     end
-    think_time 2000
+
+    think
+
     submit(
-      name: 'Do you know which course? - say yes', url: url('/candidate/account'),
+      'DO_MULTIPART_POST': 'true',
+      name: 'Do you know which course? - say yes', url: url('/candidate/application/courses/choose'),
       fill_in: {
         'candidate_interface_course_chosen_form[choice]' => 'yes',
         'authenticity_token' => '${authenticity_token}',
         'commit' => 'Continue',
       }
     )
-    think_time 1000
+
+    think
+
     visit name: 'Which training provider?', url: url('/candidate/application/courses/provider')
-    think_time 2000
+
+    think
 
     #-> View several other sections of the form
     visit name: 'References', url: url('/candidate/application/references/start')
-    think_time 2000
+    think
     visit name: 'Back to application', url: url('/candidate/application')
-    think_time 2000
+    think
     visit name: 'GCSE English', url: url('/candidate/application/gcse/english')
-    think_time 2000
+    think
     visit name: 'Back to application', url: url('/candidate/application')
-    think_time 2000
+    think
     visit name: 'Work history', url: url('/candidate/application/restructured-work-history')
-    think_time 2000
+    think
     visit name: 'Back to application', url: url('/candidate/application')
-    think_time 2000
+    think
     visit name: 'Personal statement', url: url('/candidate/application/personal-statement')
-    think_time 2000
   end
-end
+end.jmx
