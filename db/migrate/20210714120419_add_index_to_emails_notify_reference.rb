4044cfac06d592deacadbc5a85bdbc51b0e3eb04
davidgisbey <davidgisbey@gmail.com>
2021-07-14 13:55:05 +0100

    Add index to notify_reference for emails

--- db/migrate/20210714120419_add_index_to_emails_notify_reference.rb
+++ db/migrate/20210714120419_add_index_to_emails_notify_reference.rb
@@ -2,6 +2,6 @@ class AddIndexToEmailsNotifyReference < ActiveRecord::Migration[6.1]
   disable_ddl_transaction!
 
   def change
-    add_index :emails, :notify_reference, algorithm: :concurrently
+    add_index :emails, :notify_reference, unique: true, algorithm: :concurrently
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -450,7 +450,7 @@ ActiveRecord::Schema.define(version: 2021_07_14_120419) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "delivery_status", default: "unknown", null: false
     t.index ["application_form_id"], name: "index_emails_on_application_form_id"
-    t.index ["notify_reference"], name: "index_emails_on_notify_reference"
+    t.index ["notify_reference"], name: "index_emails_on_notify_reference", unique: true
   end
 
   create_table "english_proficiencies", force: :cascade do |t|
