3d9cd0f6c7985e94568494006862f6ef0436b9a9
Frankie Roberto <frankie@frankieroberto.com>
2021-07-14 16:33:12 +0100

    Don’t zero-pad day in the "short month and year" format

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -8,8 +8,8 @@ Date::DATE_FORMATS[:month_and_year] = '%B %Y'
 Time::DATE_FORMATS[:short_month_and_year] = '%b %Y'
 Date::DATE_FORMATS[:short_month_and_year] = '%b %Y'
 
-Time::DATE_FORMATS[:day_and_month] = '%d %B'
-Date::DATE_FORMATS[:day_and_month] = '%d %B'
+Time::DATE_FORMATS[:day_and_month] = '%-d %B'
+Date::DATE_FORMATS[:day_and_month] = '%-d %B'
 
 Time::DATE_FORMATS[:govuk_date_and_time] = lambda do |time|
   format = if time >= time.midday && time <= time.midday.end_of_minute
