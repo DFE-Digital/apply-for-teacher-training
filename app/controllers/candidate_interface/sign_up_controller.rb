81698ac91b0810df04e963d852aa5bec25dad18f
Malcolm Baig <malcolm.baig@gmail.com>
2021-07-15 12:19:00 +0100

    Add an authentication shortcut for load testing accounts
    When load testing Apply, test users driven by JMeter will create
    accounts. We need some way to sign in these users via the magic link
    that gets emailed out.
    
    Introduce logic to the SignUpController that checks for a load testing
    email address, and if found redirects the candidate to the magic link.

--- app/controllers/candidate_interface/sign_up_controller.rb
+++ app/controllers/candidate_interface/sign_up_controller.rb
@@ -11,15 +11,15 @@ module CandidateInterface
       @sign_up_form = CandidateInterface::SignUpForm.new(candidate_sign_up_form_params)
 
       if @sign_up_form.existing_candidate?
-        CandidateInterface::RequestMagicLink.for_sign_in(candidate: @sign_up_form.candidate)
+        magic_link_token = CandidateInterface::RequestMagicLink.for_sign_in(candidate: @sign_up_form.candidate)
         set_user_context @sign_up_form.candidate.id
         candidate = @sign_up_form.candidate
         candidate.update!(course_from_find_id: @sign_up_form.course_from_find_id)
-        redirect_to candidate_interface_check_email_sign_up_path
+        redirect_after_signup(candidate, magic_link_token)
       elsif @sign_up_form.save
-        CandidateInterface::RequestMagicLink.for_sign_up(candidate: @sign_up_form.candidate)
+        magic_link_token = CandidateInterface::RequestMagicLink.for_sign_up(candidate: @sign_up_form.candidate)
         set_user_context @sign_up_form.candidate.id
-        redirect_to candidate_interface_check_email_sign_up_path
+        redirect_after_signup(@sign_up_form.candidate, magic_link_token)
       else
         track_validation_error(@sign_up_form)
         redirect_to candidate_interface_external_sign_up_forbidden_path and return if external_sign_up_forbidden?
@@ -33,6 +33,14 @@ module CandidateInterface
     def external_sign_up_forbidden; end
 
   private
+
+    def redirect_after_signup(candidate, magic_link_token)
+      if candidate.load_tester?
+        redirect_to candidate_interface_authenticate_url(token: magic_link_token)
+      else
+        redirect_to candidate_interface_check_email_sign_up_path
+      end
+    end
 
     def external_sign_up_forbidden?
       @sign_up_form.errors.details[:email_address].include?(error: :dfe_signup_only)
