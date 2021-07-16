e66afc0b72f5bf00f74db68d9037f705170a4a4b
Toby Retallick <toby.retallick@gmail.com>
2021-07-16 14:48:01 +0100

    Update DeadlineBannerComponent
    - Make changes to copy and date formatting
    - Use 'important' banner style
    - Add calendar date for when deadline banners should appear

--- app/components/candidate_interface/deadline_banner_component.rb
+++ app/components/candidate_interface/deadline_banner_component.rb
@@ -1,13 +1,17 @@
 class CandidateInterface::DeadlineBannerComponent < ViewComponent::Base
   attr_accessor :phase, :flash_empty
 
-  def initialize(phase:, flash_empty:)
-    @phase = phase
+  def initialize(application_form:, flash_empty:)
+    @application_form = application_form
     @flash_empty = flash_empty
   end
 
   def deadline
     apply_1? ? apply_1_deadline : apply_2_deadline
+  end
+
+  def academic_year
+    "#{application_form_recruitment_cycle_year} to #{application_form_recruitment_cycle_year + 1}"
   end
 
   def render?
@@ -18,30 +22,30 @@ class CandidateInterface::DeadlineBannerComponent < ViewComponent::Base
 private
 
   def show_apply_1_deadline_banner?
-    apply_1? &&
-      CycleTimetable.show_apply_1_deadline_banner? &&
-      FeatureFlag.active?(:deadline_notices)
+    apply_1? && CycleTimetable.show_apply_1_deadline_banner?
   end
 
   def show_apply_2_deadline_banner?
-    apply_2? &&
-      CycleTimetable.show_apply_2_deadline_banner? &&
-      FeatureFlag.active?(:deadline_notices)
+    apply_2? && CycleTimetable.show_apply_2_deadline_banner?
   end
 
   def apply_1?
-    phase == 'apply_1'
+    @application_form.phase == 'apply_1'
   end
 
   def apply_2?
-    phase == 'apply_2'
+    @application_form.phase == 'apply_2'
   end
 
   def apply_1_deadline
-    CycleTimetable.date(:apply_1_deadline).to_s(:day_and_month)
+    CycleTimetable.date(:apply_1_deadline).to_s(:govuk_date)
   end
 
   def apply_2_deadline
-    CycleTimetable.date(:apply_2_deadline).to_s(:day_and_month)
+    CycleTimetable.date(:apply_2_deadline).to_s(:govuk_date)
+  end
+
+  def application_form_recruitment_cycle_year
+    @application_form.recruitment_cycle_year
   end
 end
