d26eb47fad9690eb42fd0e3cb241833d400d1b98
Duncan Brown <duncanjbrown@gmail.com>
2021-07-16 16:04:46 +0100

    Suppress Google::Cloud::Errors

--- config/initializers/okcomputer.rb
+++ config/initializers/okcomputer.rb
@@ -15,6 +15,7 @@ OkComputer.mount_at = 'integrations/monitoring'
 
 OkComputer::Registry.register 'postgres', OkComputer::ActiveRecordCheck.new
 OkComputer::Registry.register 'redis', OkComputer::RedisCheck.new(url: ENV['REDIS_URL'])
+OkComputer::Registry.register 'sidekiq_low_priority_queue', OkComputer::SidekiqLatencyCheck.new(queue: 'low_priority', threshold: 100) # threshold in seconds
 OkComputer::Registry.register 'sidekiq_default_queue', OkComputer::SidekiqLatencyCheck.new(queue: 'default', threshold: 100) # threshold in seconds
 OkComputer::Registry.register 'sidekiq_mailers_queue', OkComputer::SidekiqLatencyCheck.new(queue: 'mailers', threshold: 100) # threshold in seconds
 OkComputer::Registry.register 'simulated_failure', SimulatedFailureCheck.new
