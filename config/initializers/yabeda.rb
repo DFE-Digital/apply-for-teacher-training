b623c527a6a6b53fbf28c8483d9694f47a2cd824
Michael Nacos <m.nacos@gmail.com>
2021-07-20 12:47:48 +0100

    Add number of candidates metric

--- config/initializers/yabeda.rb
+++ config/initializers/yabeda.rb
@@ -19,9 +19,11 @@ end
 Yabeda.configure do
   group :apply_db do
     gauge :application_choices,  comment: "Number of application choices in database"
+    gauge :candidates,  comment: "Number of candidates in database"
   end
 
   collect do
     apply_db.application_choices.set({}, ApplicationChoice.count)
+    apply_db.candidates.set({}, Candidate.count)
   end
 end
