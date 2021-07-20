de1965ceb62f7b8eba025dcf31085e473f4227ad
benswannack <ben.swannack@softwire.com>
2021-07-20 09:42:01 +0100

    Add organisation permissions setup wizard
    This includes basic functionality for storing attributes and returning relationships. Still todo is the next_step logic

---
+++ app/forms/provider_interface/organisation_permissions_setup_wizard.rb
@@ -0,0 +1,61 @@
+module ProviderInterface
+  class OrganisationPermissionsSetupWizard
+    include ActiveModel::Model
+    include ProviderRelationshipPermissionsParamsHelper
+
+    attr_accessor :relationship_ids, :current_relationship_id
+    attr_writer :state_store, :provider_relationship_attrs
+
+    def initialize(state_store, attrs = {})
+      @state_store = state_store
+
+      super(last_saved_state.deep_merge(attrs))
+    end
+
+    def current_relationship
+      relationship = ProviderRelationshipPermissions.find(current_relationship_id)
+      assign_wizard_attrs_to_relationship(relationship)
+      relationship
+    end
+
+    def save_state!
+      @state_store.write(state)
+    end
+
+    def clear_state!
+      @state_store.delete
+    end
+
+  private
+
+    def provider_relationship_attrs
+      @provider_relationship_attrs.presence || {}
+    end
+
+    def state
+      as_json(except: %w[state_store errors validation_context]).to_json
+    end
+
+    def last_saved_state
+      state = @state_store.read
+
+      if state
+        JSON.parse(state).with_indifferent_access
+      else
+        {}
+      end
+    end
+
+    def assign_wizard_attrs_to_relationship(relationship)
+      relationship.assign_attributes(wizard_attrs_for_relationship(relationship))
+      relationship
+    end
+
+    def wizard_attrs_for_relationship(relationship)
+      permission_attrs = provider_relationship_attrs[relationship.id.to_s]
+      return {} if permission_attrs.blank?
+
+      translate_params_for_model(permission_attrs)
+    end
+  end
+end
