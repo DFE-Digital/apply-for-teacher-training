f570ba2dbcfe5a78a9b9c5acfe009507467f7b5e
benswannack <ben.swannack@softwire.com>
2021-07-20 09:57:13 +0100

    Add edit endpoint to setup org permissions controller

--- app/controllers/provider_interface/organisation_permissions_setup_controller.rb
+++ app/controllers/provider_interface/organisation_permissions_setup_controller.rb
@@ -9,7 +9,14 @@ module ProviderInterface
         current_provider_user,
       )
       @sorted_and_grouped_provider_names = permission_setup_presenter.grouped_provider_names
-      @sorted_permission_ids = permission_setup_presenter.sorted_provider_permission_ids
+      @sorted_relationship_ids = permission_setup_presenter.sorted_provider_permission_ids
+      OrganisationPermissionsSetupWizard.new(organisation_permissions_wizard_store, relationship_ids: @sorted_relationship_ids).save_state!
+    end
+
+    def edit
+      wizard = OrganisationPermissionsSetupWizard.new(organisation_permissions_wizard_store, current_relationship_id: params[:id])
+      @current_relationship = wizard.current_relationship
+      @current_relationship_description = ProviderRelationshipPermissionAsProviderUserPresenter.new(@current_relationship, current_provider_user).provider_relationship_description
     end
 
   private
@@ -31,6 +38,11 @@ module ProviderInterface
 
     def require_manage_organisations_permission!
       render_404 unless current_provider_user.authorisation.can_manage_organisations_for_at_least_one_provider?
+    end
+
+    def organisation_permissions_wizard_store
+      key = "organisation_permissions_wizard_store_#{current_provider_user.id}"
+      WizardStateStores::RedisStore.new(key: key)
     end
   end
 end
