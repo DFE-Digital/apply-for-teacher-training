01f66dc4fdaa403a6cafed9ecdbe8a6a9ba35642
davidgisbey <davidgisbey@gmail.com>
2021-07-22 17:02:32 +0100

    Address pr feedback

--- app/components/support_interface/duplicate_candidate_matches_table_component.rb
+++ app/components/support_interface/duplicate_candidate_matches_table_component.rb
@@ -9,16 +9,7 @@ module SupportInterface
     end
 
     def table_rows
-      matches.map do |match|
-        {
-          candidate_id: match['candidate_id'],
-          first_name: match['first_name'],
-          last_name: match['last_name'],
-          date_of_birth: match['date_of_birth'],
-          postcode: match['btrim'],
-          email_address: govuk_link_to(match['email_address'], support_interface_candidate_path(match['candidate_id'])),
-        }
-      end
+      matches.map(&:symbolize_keys)
     end
   end
 end
