c2c0a2370eae8f045a8b8a9959ac1e7eb51fa5b7
davidgisbey <davidgisbey@gmail.com>
2021-07-22 17:02:32 +0100

    Address pr feedback

--- app/queries/get_duplicate_candidate_matches.rb
+++ app/queries/get_duplicate_candidate_matches.rb
@@ -1,7 +1,7 @@
 class GetDuplicateCandidateMatches < ApplicationRecord
   def self.call
     ActiveRecord::Base.connection.exec_query(
-      "SELECT application_details.candidate_id, application_details.first_name, application_details.last_name, TRIM(UPPER(application_details.postcode)), application_details.date_of_birth, email_address
+      "SELECT DISTINCT application_details.candidate_id, application_details.first_name, application_details.last_name, TRIM(UPPER(application_details.postcode)) postcode, application_details.date_of_birth, email_address
     FROM application_forms application_details
     JOIN(
       SELECT application_forms.last_name, application_forms.date_of_birth, application_forms.postcode
--- spec/queries/get_duplicate_candidate_matches_spec.rb
+++ spec/queries/get_duplicate_candidate_matches_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe GetDuplicateCandidateMatches do
   before do
     create(:application_form, candidate: candidate1, first_name: 'Jeffrey', last_name: 'Thompson', date_of_birth: '1998-08-08', postcode: 'w6 9bh ', submitted_at: Time.zone.now - 7.days)
     create(:application_form, candidate: candidate2, first_name: 'Joffrey', last_name: 'Thompson', date_of_birth: '1998-08-08', postcode: 'w6 9bh ', submitted_at: Time.zone.now - 7.days)
+    create(:application_form, candidate: candidate1, first_name: 'Jeffrey', last_name: 'Thompson', date_of_birth: '1998-08-08', postcode: 'w6 9bh ', submitted_at: Time.zone.now - 1.day)
   end
 
   describe '#call' do
@@ -15,7 +16,7 @@ RSpec.describe GetDuplicateCandidateMatches do
     it 'returns an array of hashes with the correct keys' do
       expect(returned_array_of_hashes.count).to eq(2)
       expect(returned_array_of_hashes.first.keys).to \
-        eq %w[candidate_id first_name last_name btrim date_of_birth email_address]
+        eq %w[candidate_id first_name last_name postcode date_of_birth email_address]
     end
 
     it 'returns an array of hashes the correct values' do
@@ -23,14 +24,14 @@ RSpec.describe GetDuplicateCandidateMatches do
       expect(returned_array_of_hashes.first['first_name']).to eq('Jeffrey')
       expect(returned_array_of_hashes.first['last_name']).to eq('Thompson')
       expect(returned_array_of_hashes.first['date_of_birth']).to eq('1998-08-08')
-      expect(returned_array_of_hashes.first['btrim']).to eq('W6 9BH')
+      expect(returned_array_of_hashes.first['postcode']).to eq('W6 9BH')
       expect(returned_array_of_hashes.first['email_address']).to eq('exemplar1@example.com')
 
       expect(returned_array_of_hashes.second['candidate_id']).to eq(2)
       expect(returned_array_of_hashes.second['first_name']).to eq('Joffrey')
       expect(returned_array_of_hashes.second['last_name']).to eq('Thompson')
       expect(returned_array_of_hashes.second['date_of_birth']).to eq('1998-08-08')
-      expect(returned_array_of_hashes.second['btrim']).to eq('W6 9BH')
+      expect(returned_array_of_hashes.second['postcode']).to eq('W6 9BH')
       expect(returned_array_of_hashes.second['email_address']).to eq('exemplar2@example.com')
     end
   end
