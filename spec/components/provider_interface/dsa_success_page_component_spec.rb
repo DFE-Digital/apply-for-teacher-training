f33a482d7c859345c29b38885489c02a15844bbf
benswannack <ben.swannack@softwire.com>
2021-07-26 12:00:34 +0100

    Refactor next steps list into separate component

--- spec/components/provider_interface/dsa_success_page_component_spec.rb
+++ spec/components/provider_interface/dsa_success_page_component_spec.rb
@@ -28,29 +28,11 @@ RSpec.describe ProviderInterface::DsaSuccessPageComponent do
   context 'when permissions have been set up' do
     let(:permissions_require_setup) { false }
 
-    it 'shows a link to the applications page' do
-      expect(render.css('a')[0].text).to eq('view applications')
-      expect(render.css('a')[0].attributes['href'].value).to eq('/provider/applications')
-    end
+    before { allow(ProviderInterface::SetupCompleteNextStepsComponent).to receive(:new).with(provider_user: provider_user).and_call_original }
 
-    context 'when the provider can manage users' do
-      let(:provider_user) { create(:provider_user, :with_provider, :with_manage_users) }
-
-      it 'shows a link to the users page' do
-        expect(render.css('a')[1].text).to eq('invite or manage users')
-        expect(render.css('a')[1].attributes['href'].value).to eq('/provider/account/users')
-      end
-    end
-
-    context 'when the provider can not manage users' do
-      it 'does not show a link to the users page' do
-        expect(render.css('a').text).not_to include('invite or manage users')
-      end
-    end
-
-    it 'shows a link to the notifications page' do
-      expect(render.css('a').last.text).to eq('manage your email notifications')
-      expect(render.css('a').last.attributes['href'].value).to eq('/provider/account/notification-settings')
+    it 'renders the next steps component' do
+      render
+      expect(ProviderInterface::SetupCompleteNextStepsComponent).to have_received(:new)
     end
   end
 
