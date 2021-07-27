4d38254842cb21a17b2d652d6906a630f0650d6d
Steve Hook <steve.hook@gmail.com>
2021-07-28 00:32:06 +0100

    Remove reference_selection FF from services

--- app/services/delete_reference.rb
+++ app/services/delete_reference.rb
@@ -2,7 +2,7 @@ class DeleteReference
   def call(reference:)
     raise 'Application has been sent to providers' if reference.application_form.submitted?
 
-    if FeatureFlag.active?(:reference_selection) && reference.selected
+    if reference.selected
       reference.application_form.update!(references_completed: false)
     end
     reference.destroy!
