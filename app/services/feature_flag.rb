c7d477b1a4b4896317bf5520989de0daedf579d9
Steve Hook <steve.hook@gmail.com>
2021-07-28 10:33:39 +0100

    Remove select_references feature flag

--- app/services/feature_flag.rb
+++ app/services/feature_flag.rb
@@ -34,7 +34,6 @@ class FeatureFlag
     [:support_user_reinstate_offer, 'Allows a support users to reinstate a declined course choice offer', 'James Glenn'],
     [:expanded_quals_export, 'Rework the Qualifications export to contain all candidate qualifications', 'Malcolm Baig'],
     [:support_user_change_offered_course, 'Allows support users to offer a different course option for an application choice', 'David Gisbey'],
-    [:reference_selection, 'Allow candidates to receive multiple references and then select which two are added to their application', 'Malcolm Baig'],
     [:withdraw_at_candidates_request, "Allows providers to withdraw an application at the candidate's request", 'Steve Laing'],
     [:summer_recruitment_banner, 'Show a banner to indicate a shorter recruitment timeframe during summer', 'Richard Pattinson'],
     [:accredited_provider_setting_permissions, 'Allow accredited providers to set org permissions', 'Michael Nacos'],
