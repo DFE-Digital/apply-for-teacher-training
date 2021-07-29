1d0c6488811ac5852f23265d21d7e23f4273bbbf
Despo Pentara <despo@extractmethod.com>
2021-07-29 15:36:40 +0300

    New feature flag for `Your account and Organisation settings epic`

--- app/services/feature_flag.rb
+++ app/services/feature_flag.rb
@@ -38,6 +38,7 @@ class FeatureFlag
     [:withdraw_at_candidates_request, "Allows providers to withdraw an application at the candidate's request", 'Steve Laing'],
     [:summer_recruitment_banner, 'Show a banner to indicate a shorter recruitment timeframe during summer', 'Richard Pattinson'],
     [:accredited_provider_setting_permissions, 'Allow accredited providers to set org permissions', 'Michael Nacos'],
+    [:account_and_org_settings_changes, 'Allows new account and org setting changes', 'Despo Pentara'],
   ].freeze
 
   FEATURES = (PERMANENT_SETTINGS + TEMPORARY_FEATURE_FLAGS).map do |name, description, owner|
