02c468d910009ab98b6e8e49cfaeedd38bbd3936
benswannack <ben.swannack@softwire.com>
2021-07-29 09:42:04 +0100

    Refactor organisation permissions spec.
    This now tests that permissions that have not been set up are not visible

--- spec/system/provider_interface/provider_views_organisation_permissions_spec.rb
+++ spec/system/provider_interface/provider_views_organisation_permissions_spec.rb
@@ -1,30 +1,25 @@
 require 'rails_helper'
 
-RSpec.feature 'Viewing organisational permissions' do
+RSpec.feature 'Viewing organisation permissions' do
   include DfESignInHelpers
 
-  scenario 'Provider user uses their account page with various permissions' do
+  scenario 'Provider user views their organisation permissions page with various permissions' do
     given_i_am_a_provider_user_with_dfe_sign_in
-    and_the_accredited_provider_setting_permissions_flag_is_inactive
-
+    and_i_can_manage_multiple_organisations_that_do_not_have_permissions_set_up
     and_i_sign_in_to_the_provider_interface
 
-    given_i_can_manage_multiple_organisations_that_do_not_have_permissions_set_up
-
-    when_i_go_to_my_account
+    when_i_go_to_organisation_settings
     then_i_cannot_see_organisational_permissions
 
     when_i_set_up_permissions_for_a_provider
-    and_i_go_to_my_account
-    then_i_can_see_organisational_permissions
-
-    when_i_go_to_organisational_permissions
+    and_i_go_to_organisation_settings
+    and_i_click_organisation_permissions
     then_i_can_see_organisations_with_setup_permissions
 
     when_i_go_to_the_training_provider_permissions
     then_i_can_only_see_permissions_that_have_been_set_up
 
-    when_i_go_to_organisational_permissions
+    when_i_click_organisation_permissions
     then_i_go_to_the_ratifying_provider_permissions
     then_i_can_only_see_permissions_that_have_been_set_up
   end
@@ -39,22 +34,14 @@ RSpec.feature 'Viewing organisational permissions' do
     @ratifying_provider = create(:provider, :with_signed_agreement, code: 'DEF')
     @another_ratifying_provider = create(:provider, :with_signed_agreement, code: 'GHI')
     create(:provider_user,
+           :with_manage_users,
            :with_notifications_enabled,
            providers: [@training_provider, @ratifying_provider],
            dfe_sign_in_uid: 'DFE_SIGN_IN_UID',
            email_address: 'email@provider.ac.uk')
   end
 
-  def then_i_cannot_see_organisational_permissions
-    expect(page).to have_content(t('page_titles.provider.notifications'))
-    expect(page).not_to have_content(t('page_titles.provider.org_permissions'))
-  end
-
-  def then_i_can_see_organisational_permissions
-    expect(page).to have_content(t('page_titles.provider.org_permissions'))
-  end
-
-  def given_i_can_manage_multiple_organisations_that_do_not_have_permissions_set_up
+  def and_i_can_manage_multiple_organisations_that_do_not_have_permissions_set_up
     @provider_user = ProviderUser.last
 
     @permission_one = create(
@@ -74,22 +61,31 @@ RSpec.feature 'Viewing organisational permissions' do
     @provider_user.provider_permissions.update_all(manage_organisations: true)
   end
 
-  def then_i_can_see_organisations_with_setup_permissions
-    expect(page).to have_content(@training_provider.name.to_s)
-    expect(page).to have_content(@ratifying_provider.name.to_s)
+  def when_i_go_to_organisation_settings
+    click_on t('page_titles.provider.organisation_settings')
+  end
+
+  def then_i_cannot_see_organisational_permissions
+    expect(page).to have_content(t('page_titles.provider.users'))
+    expect(page).not_to have_content(t('page_titles.provider.organisation_permissions'))
   end
 
   def when_i_set_up_permissions_for_a_provider
     @permission_one.update(setup_at: Time.zone.now)
   end
 
-  def when_i_go_to_my_account
-    click_on t('page_titles.provider.account')
+  alias_method :and_i_go_to_organisation_settings, :when_i_go_to_organisation_settings
+
+  def and_i_click_organisation_permissions
+    click_on t('page_titles.provider.organisation_permissions')
   end
 
-  def when_i_go_to_organisational_permissions
-    click_on 'Organisational permissions'
+  def then_i_can_see_organisations_with_setup_permissions
+    expect(page).to have_content(@training_provider.name.to_s)
+    expect(page).to have_content(@ratifying_provider.name.to_s)
   end
+
+  alias_method :when_i_click_organisation_permissions, :and_i_click_organisation_permissions
 
   def when_i_go_to_the_training_provider_permissions
     click_on @training_provider.name.to_s
@@ -103,10 +99,4 @@ RSpec.feature 'Viewing organisational permissions' do
     expect(page).to have_content("#{@training_provider.name} and #{@ratifying_provider.name}")
     expect(page).not_to have_content("#{@training_provider.name} and #{@another_ratifying_provider.name}")
   end
-
-  def and_the_accredited_provider_setting_permissions_flag_is_inactive
-    FeatureFlag.deactivate(:accredited_provider_setting_permissions)
-  end
-
-  alias_method :and_i_go_to_my_account, :when_i_go_to_my_account
 end
