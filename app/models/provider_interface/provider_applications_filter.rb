5ffc28559f6ed904cb1cddc1762c86f6b31ee66c
Despo Pentara <despo@extractmethod.com>
2021-07-29 15:26:11 +0300

    Change filter label and label on application details to "Year received"

--- app/models/provider_interface/provider_applications_filter.rb
+++ app/models/provider_interface/provider_applications_filter.rb
@@ -73,7 +73,7 @@ module ProviderInterface
 
       {
         type: :checkboxes,
-        heading: 'Cycle',
+        heading: 'Year received',
         name: 'recruitment_cycle_year',
         options: cycle_options,
       }
