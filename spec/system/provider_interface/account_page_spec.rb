50e8faf675753bfb08c6219c73c4f3b39afdedf8
benswannack <ben.swannack@softwire.com>
2021-08-02 16:23:53 +0100

    Add placeholder links to your account page

--- spec/system/provider_interface/account_page_spec.rb
+++ spec/system/provider_interface/account_page_spec.rb
@@ -3,12 +3,14 @@ require 'rails_helper'
 RSpec.feature 'Viewing the provider user account page' do
   include DfESignInHelpers
 
-  scenario 'Provider user visits their account page with various permissions' do
+  before { FeatureFlag.activate(:account_and_org_settings_changes) }
+
+  scenario 'Provider user visits their account page' do
     given_i_am_a_provider_user_with_dfe_sign_in
     and_i_sign_in_to_the_provider_interface
 
     when_i_go_to_my_account
-    then_i_can_see_links_to_my_profile_and_notifications
+    then_i_can_see_links_to_my_settings_and_details
   end
 
   def given_i_am_a_provider_user_with_dfe_sign_in
@@ -20,8 +22,9 @@ RSpec.feature 'Viewing the provider user account page' do
     click_on t('page_titles.provider.account')
   end
 
-  def then_i_can_see_links_to_my_profile_and_notifications
-    expect(page).to have_content(t('page_titles.provider.profile'))
-    expect(page).to have_content(t('page_titles.provider.notifications'))
+  def then_i_can_see_links_to_my_settings_and_details
+    expect(page).to have_content(t('page_titles.provider.personal_details'))
+    expect(page).to have_content(t('page_titles.provider.user_permissions'))
+    expect(page).to have_content(t('page_titles.provider.email_notifications'))
   end
 end
