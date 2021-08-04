44597264f7fa918d34980f9f2dbf10039839df06
davidgisbey <davidgisbey@gmail.com>
2021-08-04 09:51:01 +0100

    Remove redundant application choices from specs

--- spec/workers/send_eoc_deadline_reminder_email_to_candidates_worker_spec.rb
+++ spec/workers/send_eoc_deadline_reminder_email_to_candidates_worker_spec.rb
@@ -1,15 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe SendEocDeadlineReminderEmailToCandidatesWorker, sidekiq: true do
-  let(:application_choice) { build(:application_choice) }
-  let(:application_choices) { [application_choice] }
   let(:send_reminder) { described_class.new.applications_to_send_reminders_to }
 
   describe '#perform' do
     context 'when the candidate is in Apply 1' do
       it 'returns an application when the deadline is 2 months away' do
         Timecop.travel(CycleTimetable.apply_1_deadline_first_reminder) do
-          application_form = create(:application_form, application_choices: application_choices, phase: 'apply_1')
+          application_form = create(:application_form)
 
           expect(send_reminder).to include application_form
         end
@@ -17,7 +15,7 @@ RSpec.describe SendEocDeadlineReminderEmailToCandidatesWorker, sidekiq: true do
 
       it 'returns an application when the deadline is 1 month away' do
         Timecop.travel(CycleTimetable.apply_1_deadline_second_reminder) do
-          application_form = create(:application_form, application_choices: application_choices, phase: 'apply_1')
+          application_form = create(:application_form)
 
           expect(send_reminder).to include application_form
         end
@@ -25,7 +23,7 @@ RSpec.describe SendEocDeadlineReminderEmailToCandidatesWorker, sidekiq: true do
 
       it 'does not return an application when the deadline is 3 months away' do
         Timecop.travel(CycleTimetable.apply_1_deadline_first_reminder - 1.month) do
-          create(:application_form, application_choices: application_choices, phase: 'apply_1')
+          create(:application_form)
 
           expect(send_reminder).to be_empty
         end
@@ -33,7 +31,15 @@ RSpec.describe SendEocDeadlineReminderEmailToCandidatesWorker, sidekiq: true do
 
       it 'does not return an application when the deadline has passed' do
         Timecop.travel(CycleTimetable.apply_1_deadline + 1.day) do
-          create(:application_form, application_choices: application_choices, phase: 'apply_1')
+          create(:application_form)
+
+          expect(send_reminder).to be_empty
+        end
+      end
+
+      it 'does not return an application form from the previous cycle' do
+        Timecop.travel(CycleTimetable.apply_1_deadline_first_reminder) do
+          application_form_from_previous_cycle = create(:application_form, recruitment_cycle_year: RecruitmentCycle.previous_year)
 
           expect(send_reminder).to be_empty
         end
@@ -43,7 +49,7 @@ RSpec.describe SendEocDeadlineReminderEmailToCandidatesWorker, sidekiq: true do
     context 'when the candidate is in Apply 2' do
       it 'returns an application when the deadline is 2 months away' do
         Timecop.travel(CycleTimetable.apply_2_deadline_first_reminder) do
-          application_form = create(:application_form, application_choices: application_choices, phase: 'apply_2')
+          application_form = create(:application_form, phase: 'apply_2')
 
           expect(send_reminder).to include application_form
         end
@@ -51,7 +57,7 @@ RSpec.describe SendEocDeadlineReminderEmailToCandidatesWorker, sidekiq: true do
 
       it 'returns an application when the deadline is 1 month away' do
         Timecop.travel(CycleTimetable.apply_2_deadline_second_reminder) do
-          application_form = create(:application_form, application_choices: application_choices, phase: 'apply_2')
+          application_form = create(:application_form, phase: 'apply_2')
 
           expect(send_reminder).to include application_form
         end
@@ -59,7 +65,7 @@ RSpec.describe SendEocDeadlineReminderEmailToCandidatesWorker, sidekiq: true do
 
       it 'does not return an application when the deadline is 3 months away' do
         Timecop.travel(CycleTimetable.apply_2_deadline_first_reminder - 1.month) do
-          create(:application_form, application_choices: application_choices, phase: 'apply_2')
+          create(:application_form, phase: 'apply_2')
 
           expect(send_reminder).to be_empty
         end
@@ -67,7 +73,15 @@ RSpec.describe SendEocDeadlineReminderEmailToCandidatesWorker, sidekiq: true do
 
       it 'does not return an application when the deadline has passed' do
         Timecop.travel(CycleTimetable.apply_2_deadline + 1.day) do
-          create(:application_form, application_choices: application_choices, phase: 'apply_2')
+          create(:application_form, phase: 'apply_2')
+
+          expect(send_reminder).to be_empty
+        end
+      end
+
+      it 'does not return an application form from the previous cycle' do
+        Timecop.travel(CycleTimetable.apply_1_deadline_first_reminder) do
+          application_form_from_previous_cycle = create(:application_form, phase: 'apply_2', recruitment_cycle_year: RecruitmentCycle.previous_year)
 
           expect(send_reminder).to be_empty
         end
