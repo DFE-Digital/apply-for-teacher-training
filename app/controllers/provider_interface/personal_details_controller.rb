34ee579f3c70fd7fe9095fd7bcf522a6a487209e
benswannack <ben.swannack@softwire.com>
2021-08-05 15:25:02 +0100

    Reinstate DSI profile link on personal details page

--- app/controllers/provider_interface/personal_details_controller.rb
+++ app/controllers/provider_interface/personal_details_controller.rb
@@ -2,7 +2,9 @@ module ProviderInterface
   class PersonalDetailsController < ProviderInterfaceController
     before_action :redirect_to_profile_unless_feature_flag_on
 
-    def show; end
+    def show
+      @dsi_profile_url = dsi_profile_url
+    end
 
   private
 
@@ -10,6 +12,12 @@ module ProviderInterface
       unless FeatureFlag.active?(:account_and_org_settings_changes)
         redirect_to provider_interface_profile_path
       end
+    end
+
+    def dsi_profile_url
+      return 'https://test-profile.signin.education.gov.uk' if HostingEnvironment.qa?
+
+      'https://profile.signin.education.gov.uk'
     end
   end
 end
