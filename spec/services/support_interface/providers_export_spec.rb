5fcc8a399fe42343d4ac590b44a83938d2c9dbda
Richard Pattinson <you@example.com>
2021-08-06 12:55:35 +0100

    add organisation_type column to provider data export

--- spec/services/support_interface/providers_export_spec.rb
+++ spec/services/support_interface/providers_export_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe SupportInterface::ProvidersExport, with_audited: true do
   describe 'documentation' do
     before do
-      provider = create(:provider, sync_courses: true, name: 'B', latitude: 51.498161, longitude: 0.129900)
+      provider = create(:provider, name: 'B', latitude: 51.498161, longitude: 0.129900)
       create(:site, latitude: 51.482578, longitude: -0.007659, provider: provider)
     end
 
@@ -11,9 +11,9 @@ RSpec.describe SupportInterface::ProvidersExport, with_audited: true do
   end
 
   describe '#providers' do
-    it 'returns synced providers and the date they signed the data sharing agreement' do
-      create(:provider, sync_courses: false)
-      provider_without_signed_dsa = create(:provider, sync_courses: true, name: 'B', latitude: 51.498161, longitude: 0.129900)
+    it 'returns providers and the date they signed the data sharing agreement' do
+      first_provider = create(:provider)
+      provider_without_signed_dsa = create(:provider, name: 'B', latitude: 51.498161, longitude: 0.129900, provider_type: 'string')
       create(:site, latitude: 51.482578, longitude: -0.007659, provider: provider_without_signed_dsa)
       create(:site, latitude: 52.246868, longitude: 0.711190, provider: provider_without_signed_dsa)
 
@@ -22,13 +22,13 @@ RSpec.describe SupportInterface::ProvidersExport, with_audited: true do
         provider_with_signed_dsa = create(
           :provider,
           :with_signed_agreement,
-          sync_courses: true,
           name: 'A',
+          provider_type: 'string',
         )
       end
 
       providers = described_class.new.providers
-      expect(providers.size).to eq(2)
+      expect(providers.size).to eq(3)
 
       expect(providers).to contain_exactly(
         {
@@ -36,12 +36,21 @@ RSpec.describe SupportInterface::ProvidersExport, with_audited: true do
           provider_code: provider_with_signed_dsa.code,
           agreement_accepted_at: Time.zone.local(2019, 10, 1, 12, 0, 0),
           average_distance_to_site: '',
+          organisation_type: provider_with_signed_dsa.provider_type,
         },
         {
           provider_name: provider_without_signed_dsa.name,
           provider_code: provider_without_signed_dsa.code,
           agreement_accepted_at: nil,
           average_distance_to_site: '31.7',
+          organisation_type: provider_without_signed_dsa.provider_type,
+        },
+        {
+          provider_name: first_provider.name,
+          provider_code: first_provider.code,
+          agreement_accepted_at: nil,
+          average_distance_to_site: '',
+          organisation_type: first_provider.provider_type,
         },
       )
     end
