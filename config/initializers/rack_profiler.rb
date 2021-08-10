3de7e1ac288412c193490c968efce5e1c4d04487
Abeer Salameh <cwrw@users.noreply.github.com>
2021-08-10 16:35:13 +0100

    Revert "Add rack-mini-profiler to development and QA"

--- config/initializers/rack_profiler.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-if HostingEnvironment.test_environment? && HostingEnvironment.environment_name != 'test'
-  require 'rack-mini-profiler'
-
-  # initialization is skipped so trigger it
-  Rack::MiniProfilerRails.initialize!(Rails.application)
-end
