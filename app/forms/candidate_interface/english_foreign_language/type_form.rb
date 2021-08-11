2e011dd23eadd4b2d07ffacd4f0e69a2256fc13e
Steve Hook <steve.hook@gmail.com>
2021-08-11 15:28:23 +0100

    Update forms to handle `return_to` param

--- app/forms/candidate_interface/english_foreign_language/type_form.rb
+++ app/forms/candidate_interface/english_foreign_language/type_form.rb
@@ -4,7 +4,7 @@ module CandidateInterface
       include ActiveModel::Model
       include Rails.application.routes.url_helpers
 
-      attr_accessor :type
+      attr_accessor :type, :return_to
 
       validates :type, presence: true
 
@@ -17,12 +17,18 @@ module CandidateInterface
       def next_form_path
         case type
         when 'ielts'
-          candidate_interface_ielts_path
+          candidate_interface_ielts_path(return_to_params)
         when 'toefl'
-          candidate_interface_toefl_path
+          candidate_interface_toefl_path(return_to_params)
         when 'other'
-          candidate_interface_other_efl_qualification_path
+          candidate_interface_other_efl_qualification_path(return_to_params)
         end
+      end
+
+      private
+
+      def return_to_params
+        return_to == 'application-review' ? { 'return-to' => 'application-review' } : {}
       end
     end
   end
