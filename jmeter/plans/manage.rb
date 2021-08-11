8e14fef7002c81f0078c62592c79e4dd300aa1ed
Michael Nacos <m.nacos@gmail.com>
2021-08-11 11:27:09 +0100

    Add JMETER_THREAD_CONFIG to specify threads per scenario

--- jmeter/plans/manage.rb
+++ jmeter/plans/manage.rb
@@ -13,6 +13,9 @@ def log_in(user_id)
     fill_in: { 'uid' => user_id }
 end
 
+THREAD_CONFIG = ENV.fetch('JMETER_THREAD_CONFIG', '1').split(',').map(&:to_i)
+THREADS_PER_SCENARIO = (0..5).map { |i| THREAD_CONFIG[i] || 1 }
+
 test do
   cookies clear_each_iteration: true
 
@@ -32,7 +35,7 @@ test do
     # The total number of sessions for each uid (below) should be 6
 
     # See interviewing application and interview information
-    threads count: 1, rampup: RAMPUP, continue_forever: true, duration: 3600 do
+    threads count: THREADS_PER_SCENARIO[0], rampup: RAMPUP, continue_forever: true, duration: 3600 do
       log_in uid
       visit name: 'Filter by interviewing', url: BASEURL + '/provider/applications?commit=Apply+filters&status%5B%5D=interviewing' do
         extract name: 'application_id', regex: 'href="/provider/applications/(\d+)"', match_number: 0
@@ -42,7 +45,7 @@ test do
     end
 
     # Start making an offer
-    threads count: 1, rampup: RAMPUP, continue_forever: true, duration: 3600 do
+    threads count: THREADS_PER_SCENARIO[1], rampup: RAMPUP, continue_forever: true, duration: 3600 do
       log_in uid
       visit name: 'Filter by awaiting_provider_decision', url: BASEURL + '/provider/applications?commit=Apply+filters&status%5B%5D=awaiting_provider_decision' do
         extract name: 'application_id', regex: 'href="/provider/applications/(\d+)"', match_number: 0
@@ -61,7 +64,7 @@ test do
     end
 
     # See rejected application
-    threads count: 1, rampup: RAMPUP, continue_forever: true, duration: 3600 do
+    threads count: THREADS_PER_SCENARIO[2], rampup: RAMPUP, continue_forever: true, duration: 3600 do
       log_in uid
       visit name: 'Filter by rejected', url: BASEURL + '/provider/applications?commit=Apply+filters&status%5B%5D=rejected' do
         extract name: 'application_id', regex: 'href="/provider/applications/(\d+)"', match_number: 0
@@ -72,20 +75,20 @@ test do
     end
 
     # See provider interview schedule
-    threads count: 1, rampup: RAMPUP, continue_forever: true, duration: 3600 do
+    threads count: THREADS_PER_SCENARIO[3], rampup: RAMPUP, continue_forever: true, duration: 3600 do
       log_in uid
       visit name: 'See interview schedule', url: BASEURL + '/provider/interview-schedule'
       visit name: 'See past interview schedule', url: BASEURL + '/provider/interview-schedule/past'
     end
 
     # See provider activity log
-    threads count: 1, rampup: RAMPUP, continue_forever: true, duration: 3600 do
+    threads count: THREADS_PER_SCENARIO[4], rampup: RAMPUP, continue_forever: true, duration: 3600 do
       log_in uid
       visit name: 'Load activity log', url: BASEURL + '/provider/activity'
     end
 
     # Data export
-     threads count: 1, rampup: RAMPUP, continue_forever: true, duration: 3600 do
+     threads count: THREADS_PER_SCENARIO[5], rampup: RAMPUP, continue_forever: true, duration: 3600 do
       log_in uid
       visit name: 'Load provider data export form', url: BASEURL + '/provider/applications/data-export/new'
       visit name: 'Download data export', url: BASEURL + '/provider/applications/data-export?provider_interface_application_data_export_form[recruitment_cycle_years][]=&provider_interface_application_data_export_form[recruitment_cycle_years][]=2021&provider_interface_application_data_export_form[recruitment_cycle_years][]=2020&provider_interface_application_data_export_form[application_status_choice]=all&provider_interface_application_data_export_form[statuses][]=' do
