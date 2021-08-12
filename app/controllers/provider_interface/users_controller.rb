4168caab5e0ade38c7dd4ac03302a08b1a3daef7
benswannack <ben.swannack@softwire.com>
2021-08-12 15:31:40 +0100

    Add provider user show page

--- app/controllers/provider_interface/users_controller.rb
+++ app/controllers/provider_interface/users_controller.rb
@@ -1,7 +1,22 @@
 module ProviderInterface
   class UsersController < ProviderInterfaceController
-    def index
+    before_action :set_provider
+
+    def index; end
+
+    def show
+      @provider_user = @provider.provider_users.find(params[:id])
+      @current_user_can_manage_users = current_user_can_manage_users
+    end
+
+  private
+
+    def set_provider
       @provider = current_provider_user.providers.find(params[:organisation_id])
+    end
+
+    def current_user_can_manage_users
+      current_provider_user.authorisation.can_manage_users_for?(provider: @provider)
     end
   end
 end
