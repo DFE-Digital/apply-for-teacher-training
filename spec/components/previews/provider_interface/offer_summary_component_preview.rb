93082aa8a681790219600cb65360aee4ccfc4bc1
cwrw <abeer.salameh@gmail.com>
2021-08-13 11:01:45 +0100

    Update offer summary component preview
    Update component to use the new fields that are required
    including an application choice course and offer conditions
    model

--- spec/components/previews/provider_interface/offer_summary_component_preview.rb
+++ spec/components/previews/provider_interface/offer_summary_component_preview.rb
@@ -3,11 +3,12 @@ module ProviderInterface
     layout 'previews/provider'
 
     def offer_summary
-      application_choice = ApplicationChoice.limit(5).sample
-      course_option = CourseOption.limit(10).sample
-      conditions =  OfferCondition::STANDARD_CONDITIONS + ['Driving license']
+      application_choice = ApplicationChoice.where(status: :offer).limit(5).sample
+      course_option = application_choice.current_course_option
+      conditions =  application_choice.offer.conditions
 
       render ProviderInterface::OfferSummaryComponent.new(application_choice: application_choice,
+                                                          course: course_option.course,
                                                           course_option: course_option,
                                                           conditions: conditions)
     end
