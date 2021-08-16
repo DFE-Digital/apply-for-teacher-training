9f7c4673d2c8bb1b429a8295e93c07b826ab3d3c
Richard Pattinson <you@example.com>
2021-08-16 16:42:01 +0100

    Generate service for provider application statuses
    New service will take a provider and make an
    array of applications in each state by count. This will be used to render
    report data through the report table.

---
+++ app/queries/get_application_progress_data_by_course.rb
@@ -0,0 +1,21 @@
+class GetApplicationProgressDataByCourse
+  attr_reader :provider
+
+  def initialize(provider:)
+    @provider = provider
+  end
+
+  def call
+    Course.joins(:application_choices).merge(provider_application_choices).left_joins(:accredited_provider)
+      .group('courses.id', 'courses.name', 'courses.code', 'application_choices.status', 'providers.name')
+      .select('providers.name as provider_name', 'courses.name', 'courses.code', "count('application_choices.status') as count", 'application_choices.status as status', 'courses.id')
+  end
+
+  def provider_application_choices
+    ApplicationChoice.joins(:course)
+      .where(status: %i[awaiting_provider_decision interviewing offer pending_conditions recruited])
+      .where(course: { provider_id: @provider.id })
+      .or(ApplicationChoice.joins(:course).where(course: { accredited_provider_id: @provider.id }))
+      .where(course: { recruitment_cycle_year: RecruitmentCycle.current_year })
+  end
+end
