b4441351c7f547365cdc0af299e34310eddec2be
Richard Pattinson <you@example.com>
2021-08-16 17:08:12 +0100

    Extract course status and provider name to their own methods

--- app/queries/get_application_progress_data_by_course.rb
+++ app/queries/get_application_progress_data_by_course.rb
@@ -8,14 +8,14 @@ class GetApplicationProgressDataByCourse
   def call
     Course.joins(:application_choices).merge(provider_application_choices).left_joins(:accredited_provider)
       .group('courses.id', 'courses.name', 'courses.code', 'application_choices.status', 'providers.name')
-      .select('providers.name as provider_name', 'courses.name', 'courses.code', "count('application_choices.status') as count", 'application_choices.status as status', 'courses.id')
+      .select('courses.provider_id', 'courses.accredited_provider_id', 'providers.name as provider_name', 'courses.name', 'courses.code', "count('application_choices.status') as count", 'application_choices.status as status', 'courses.id')
   end
 
   def provider_application_choices
     ApplicationChoice.joins(:course)
       .where(status: %i[awaiting_provider_decision interviewing offer pending_conditions recruited])
-      .where(course: { provider_id: @provider.id })
-      .or(ApplicationChoice.joins(:course).where(course: { accredited_provider_id: @provider.id }))
+      .where(course: { provider_id: provider.id })
+      .or(ApplicationChoice.joins(:course).where(course: { accredited_provider_id: provider.id }))
       .where(course: { recruitment_cycle_year: RecruitmentCycle.current_year })
   end
 end
--- app/services/provider_interface/active_application_statuses_by_provider.rb
+++ app/services/provider_interface/active_application_statuses_by_provider.rb
@@ -8,15 +8,15 @@ module ProviderInterface
 
     def call
       grouped_course_data.map do |course_data|
-        course = course_data.last
+        courses = course_data.last
         {
-          header: course.first.name_and_code.to_s,
-          subheader: course.first.provider_name || provider.name,
-          values: [course.find { |c| c.status == 'awaiting_provider_decision' }&.count || 0,
-                   course.find { |c| c.status == 'interviewing' }&.count || 0,
-                   course.find { |c| c.status == 'offer' }&.count || 0,
-                   course.find { |c| c.status == 'pending_conditions' }&.count || 0,
-                   course.find { |c| c.status == 'recruited' }&.count || 0],
+          header: courses.first.name_and_code,
+          subheader: provider_name(courses.first),
+          values: [status_count(courses, :awaiting_provider_decision),
+                   status_count(courses, :interviewing),
+                   status_count(courses, :offer),
+                   status_count(courses, :pending_conditions),
+                   status_count(courses, :recruited)],
         }
       end
     end
@@ -25,6 +25,16 @@ module ProviderInterface
 
     def grouped_course_data
       @course_data ||= GetApplicationProgressDataByCourse.new(provider: provider).call.group_by(&:id)
+    end
+
+    def status_count(courses, status)
+      courses.find { |course| course.status == status.to_s }&.count || 0
+    end
+
+    def provider_name(course)
+      provider_name = course.provider_name || provider.name
+      provider_name = Provider.find(course.accredited_provider_id).name if course.provider_id != course.accredited_provider_id
+      provider_name
     end
   end
 end
