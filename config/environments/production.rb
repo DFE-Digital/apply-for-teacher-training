8ce5405ca4174ee2b2e20d1827fbe95b9bb9c0a4
Steve Laing <steve.laing@gmail.com>
2021-08-17 15:47:15 +0100

    Temporarily enable memory store caching
    We'd like to understand whether using Redis as a cache store is producing slower response times.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,8 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  config.cache_store = :redis_cache_store, { url: ENV.fetch('REDIS_CACHE_URL') }
+  config.cache_store = :memory_store, { size: 512.megabytes }
+  # config.cache_store = :redis_cache_store, { url: ENV.fetch('REDIS_CACHE_URL') }
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
