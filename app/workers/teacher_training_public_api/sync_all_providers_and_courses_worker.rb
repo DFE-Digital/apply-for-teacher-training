f2215e8ee1dce9eb7a267ff3a0b1e8c89fe48a3e
cwrw <abeer.salameh@gmail.com>
2021-08-17 15:57:00 +0100

    Stop syncing TTAPI when in heroku review apps env
    This is causing spikes on the TTAPI, and sycing every night.
    Since a sync isn't required and can be done manually, skip the sync
    if on a review app.

--- app/workers/teacher_training_public_api/sync_all_providers_and_courses_worker.rb
+++ app/workers/teacher_training_public_api/sync_all_providers_and_courses_worker.rb
@@ -4,6 +4,8 @@ module TeacherTrainingPublicAPI
     sidekiq_options retry: 3, queue: :low_priority
 
     def perform(incremental = true, year = ::RecruitmentCycle.current_year, suppress_sync_update_errors = false)
+      return if HostingEnvironment.review?
+
       SyncSubjects.new.perform
       SyncAllProvidersAndCourses.call(recruitment_cycle_year: year, incremental_sync: incremental, suppress_sync_update_errors: suppress_sync_update_errors)
     end
