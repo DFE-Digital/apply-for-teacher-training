513a20ffb9f3fa1a35e03fb991477fb448f284bb
Richard Pattinson <richie.pattinson@googlemail.com>
2021-08-18 15:25:39 +0100

    add totals row to export

--- app/services/provider_interface/status_of_active_applications_export.rb
+++ app/services/provider_interface/status_of_active_applications_export.rb
@@ -22,7 +22,38 @@ module ProviderInterface
           pending_conditions: status_count(courses, :recruited),
         }
       end
+      data << totals_row(data)
+
       SafeCSV.generate(data.map(&:values), data.first.keys)
+    end
+
+  private
+
+    def totals_row(rows)
+      {
+        name: 'All courses',
+        code: 'TOTAL',
+        partner_organisation: '',
+        received: totals_count(rows)[0],
+        interviewing: totals_count(rows)[1],
+        offered: totals_count(rows)[2],
+        awaiting_conditions: totals_count(rows)[3],
+        pending_conditions: totals_count(rows)[4],
+      }
+    end
+
+    def totals_count(rows)
+      return @totals_count if @totals_count
+
+      @totals_count = Array.new(5) { 0 }
+      rows.each do |row|
+        @totals_count[0] += row[:received]
+        @totals_count[1] += row[:interviewing]
+        @totals_count[2] += row[:offered]
+        @totals_count[3] += row[:awaiting_conditions]
+        @totals_count[4] += row[:pending_conditions]
+      end
+      @totals_count
     end
   end
 end
