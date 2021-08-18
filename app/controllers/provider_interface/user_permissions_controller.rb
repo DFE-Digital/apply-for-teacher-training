5b90ccfb44fdde59dc0ab088ecda32f0396646f9
SamJamCul <samuel.culley@digital.education.gov.uk>
2021-08-18 17:07:26 +0100

    Change back button for edit permissions flow

--- app/controllers/provider_interface/user_permissions_controller.rb
+++ app/controllers/provider_interface/user_permissions_controller.rb
@@ -1,7 +1,7 @@
 module ProviderInterface
   class UserPermissionsController < UsersController
     def edit
-      @previous_page_path = provider_interface_organisation_settings_organisation_user_path(@provider, @provider_user)
+      @previous_page_path = previous_page_path
       provider_permissions = @provider_user.provider_permissions.find_by!(provider: @provider)
       @wizard = EditUserPermissionsWizard.from_model(edit_user_permissions_store, provider_permissions)
     end
@@ -26,6 +26,14 @@ module ProviderInterface
 
     def provider_permissions_params
       params.require(:provider_interface_edit_user_permissions_wizard).permit(permissions: [])
+    end
+
+    def previous_page_path
+      if params[:checking_answers] == 'true'
+        check_provider_interface_organisation_settings_organisation_user_permissions_path(@provider, @provider_user)
+      else
+        provider_interface_organisation_settings_organisation_user_path(@provider, @provider_user)
+      end
     end
   end
 end
