a0339d5f7c26666b1e12b21be2eebc3cfa6413fc
Richard Pattinson <richie.pattinson@googlemail.com>
2021-08-20 15:09:07 +0100

    Add csv export to status of active applications report page

--- app/controllers/provider_interface/reports/status_of_active_applications_controller.rb
+++ app/controllers/provider_interface/reports/status_of_active_applications_controller.rb
@@ -4,7 +4,19 @@ module ProviderInterface
       def show
         provider_id = params[:provider_id]
         @provider = current_user.providers.find(provider_id)
-        @active_application_status_data = ActiveApplicationStatusesByProvider.new(@provider).call
+        respond_to do |format|
+          format.csv do
+            csv_data = ProviderInterface::StatusOfActiveApplicationsExport.new(provider: @provider).call
+            send_data csv_data, disposition: 'attachment', filename: csv_filename(@provider)
+          end
+          format.html do
+            @active_application_status_data = ActiveApplicationStatusesByProvider.new(@provider).call
+          end
+        end
+      end
+
+      def csv_filename(provider)
+        "#{Time.zone.now},#{provider.name}.status_of_active_applications.csv"
       end
     end
   end
