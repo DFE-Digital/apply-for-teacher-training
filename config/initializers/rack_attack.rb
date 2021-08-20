4c5a66cf702a36cb5f7fdf6cf3ec67ada36b260f
Duncan Brown <duncanjbrown@gmail.com>
2021-08-20 10:40:16 +0100

    Use remote_ip for Rack::Attack client identification
    Using #ip will always just return the IP of the load balancer in prod,
    which is very bad as all clients will share a limit.

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,7 +1,19 @@
 VENDOR_API_MAX_REQS_PER_MINUTE = 120
 
 class Rack::Attack
+  class Request < ::Rack::Request
+    def remote_ip
+      # This will always be present because the ActionDispatch::RemoteIp
+      # middleware runs long before this middleware. Use #fetch here
+      # so that we bail quickly if that middleware goes away or changes
+      # the field name. Have preferred this to instantiating a whole
+      # ActionDispatch::Request as that's a whole lot of work and this happens
+      # on every request
+      @remote_ip ||= env.fetch('action_dispatch.remote_ip')
+    end
+  end
+
   throttle('vendor_api/ip', limit: VENDOR_API_MAX_REQS_PER_MINUTE, period: 1.minute) do |req|
-    req.ip if req.path.match(/api\/v1/)
+    req.remote_ip if req.path.match(/api\/v1/)
   end
 end
