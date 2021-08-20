4716cdbe314f0df13b43e503a913cfca0e8c362c
cwrw <abeer.salameh@gmail.com>
2021-08-20 16:42:58 +0100

    Reset devise login after each test
    We are maintaining current user state between scenarios,
    ensure we tear those down after each scenario to avoid
    flakey tests which look for a none existant current user

--- spec/support/warden.rb
+++ spec/support/warden.rb
@@ -1,6 +1,9 @@
 RSpec.configure do |config|
   config.include Warden::Test::Helpers
   config.include Devise::Test::ControllerHelpers, type: :controller
+  config.after do
+    Warden.test_reset!
+  end
 end
 
 # The Devise test helpers rely on an ApplicationController being present
