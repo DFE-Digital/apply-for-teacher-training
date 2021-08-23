7a8b2d81344ef4e142c843c562c58461f7bfda3c
Steve Laing <steve.laing@gmail.com>
2021-08-23 15:46:25 +0100

    Stream applications export data
    Uses the module StreamableDataExport to stream the response data in
    applications export.

--- app/services/provider_interface/application_data_export.rb
+++ app/services/provider_interface/application_data_export.rb
@@ -1,65 +1,70 @@
 module ProviderInterface
   class ApplicationDataExport
-    def self.call(application_choices:)
-      rows = []
-      applications = Array.wrap(application_choices)
+    CSV_HEADINGS = %w[
+      application_choice_id candidate_id support_reference status submitted_at updated_at recruited_at
+      rejection_reason rejected_at reject_by_default_at first_name last_name date_of_birth nationality
+      domicile uk_residency_status english_main_language english_language_qualifications email phone_number
+      address_line1 address_line2 address_line3 address_line4 postcode country recruitment_cycle_year
+      provider_code accredited_provider_name accredited_provider_code course_code site_code study_mode
+      start_date FIRSTDEG qualification_type non_uk_qualification_type subject grade start_year award_year
+      institution_details equivalency_details awarding_body gcse_qualifications_summary missing_gcses_explanation
+      disability_disclosure
+    ].freeze
 
-      applications.each do |application_choice|
-        application = ApplicationChoiceExportDecorator.new(application_choice)
+    def self.export_row(application_choice)
+      return [] if application_choice.blank?
 
-        rows << {
-          'application_choice_id' => application.id,
-          'candidate_id' => application.application_form.candidate.public_id,
-          'support_reference' => application.application_form.support_reference,
-          'status' => application.status,
-          'submitted_at' => application.application_form.submitted_at,
-          'updated_at' => application.updated_at,
-          'recruited_at' => application.recruited_at,
-          'rejection_reason' => application.rejection_reason,
-          'rejected_at' => application.rejected_at,
-          'reject_by_default_at' => application.reject_by_default_at,
-          'first_name' => application.application_form.first_name,
-          'last_name' => application.application_form.last_name,
-          'date_of_birth' => application.application_form.date_of_birth,
-          'nationality' => application.nationalities.join(' '),
-          'domicile' => application.application_form.domicile,
-          'uk_residency_status' => application.application_form.uk_residency_status,
-          'english_main_language' => application.application_form.english_main_language,
-          'english_language_qualifications' => replace_smart_quotes(application.application_form.english_language_details),
-          'email' => application.application_form.candidate.email_address,
-          'phone_number' => application.application_form.phone_number,
-          'address_line1' => application.application_form.address_line1,
-          'address_line2' => application.application_form.address_line2,
-          'address_line3' => application.application_form.address_line3,
-          'address_line4' => application.application_form.address_line4,
-          'postcode' => application.application_form.postcode,
-          'country' => application.application_form.country,
-          'recruitment_cycle_year' => application.application_form.recruitment_cycle_year,
-          'provider_code' => application.current_provider.code,
-          'accredited_provider_name' => application.current_accredited_provider&.name,
-          'accredited_provider_code' => application.current_accredited_provider&.code,
-          'course_code' => application.current_course.code,
-          'site_code' => application.current_site.code,
-          'study_mode' => application.current_course_option.study_mode,
-          'start_date' => application.current_course.start_date,
-          'FIRSTDEG' => application.degrees_completed_flag,
-          'qualification_type' => application.first_degree&.qualification_type,
-          'non_uk_qualification_type' => application.first_degree&.non_uk_qualification_type,
-          'subject' => application.first_degree&.subject,
-          'grade' => application.first_degree&.grade,
-          'start_year' => application.first_degree&.start_year,
-          'award_year' => application.first_degree&.award_year,
-          'institution_details' => application.first_degree&.institution_name,
-          'equivalency_details' => replace_smart_quotes(application.first_degree&.composite_equivalency_details),
-          'awarding_body' => nil, # included for backwards compatibility. This column is always blank
-          'gcse_qualifications_summary' => replace_smart_quotes(application.gcse_qualifications_summary),
-          'missing_gcses_explanation' => replace_smart_quotes(application.missing_gcses_explanation),
-          'disability_disclosure' => application.application_form.disability_disclosure,
-        }
-      end
+      application = ApplicationChoiceExportDecorator.new(application_choice)
 
-      header_row ||= rows.first&.keys
-      SafeCSV.generate(rows.map(&:values), header_row)
+      [
+        application.id,
+        application.application_form.candidate.public_id,
+        application.application_form.support_reference,
+        application.status,
+        application.application_form.submitted_at,
+        application.updated_at,
+        application.recruited_at,
+        application.rejection_reason,
+        application.rejected_at,
+        application.reject_by_default_at,
+        application.application_form.first_name,
+        application.application_form.last_name,
+        application.application_form.date_of_birth,
+        application.nationalities.join(' '),
+        application.application_form.domicile,
+        application.application_form.uk_residency_status,
+        application.application_form.english_main_language,
+        replace_smart_quotes(application.application_form.english_language_details),
+        application.application_form.candidate.email_address,
+        application.application_form.phone_number,
+        application.application_form.address_line1,
+        application.application_form.address_line2,
+        application.application_form.address_line3,
+        application.application_form.address_line4,
+        application.application_form.postcode,
+        application.application_form.country,
+        application.application_form.recruitment_cycle_year,
+        application.current_provider.code,
+        application.current_accredited_provider&.name,
+        application.current_accredited_provider&.code,
+        application.current_course.code,
+        application.current_site.code,
+        application.current_course_option.study_mode,
+        application.current_course.start_date,
+        application.degrees_completed_flag,
+        application.first_degree&.qualification_type,
+        application.first_degree&.non_uk_qualification_type,
+        application.first_degree&.subject,
+        application.first_degree&.grade,
+        application.first_degree&.start_year,
+        application.first_degree&.award_year,
+        application.first_degree&.institution_name,
+        replace_smart_quotes(application.first_degree&.composite_equivalency_details),
+        nil, # included for backwards compatibility. This column is always blank
+        replace_smart_quotes(application.gcse_qualifications_summary),
+        replace_smart_quotes(application.missing_gcses_explanation),
+        application.application_form.disability_disclosure,
+      ]
     end
 
     def self.replace_smart_quotes(text)
--- spec/services/provider_interface/application_data_export_spec.rb
+++ spec/services/provider_interface/application_data_export_spec.rb
@@ -7,36 +7,32 @@ RSpec.describe ProviderInterface::ApplicationDataExport do
     end
   end
 
-  describe '#call' do
-    let(:data_export) { CSV.parse(described_class.call(application_choices: application_choices), headers: true) }
+  describe '.export_row' do
+    let(:exported_row) { described_class.export_row(application_choice) }
 
     context 'when there are no application choices' do
-      let(:application_choices) { [] }
+      let(:application_choice) { nil }
 
       it 'returns no rows' do
-        expect(data_export).to be_empty
+        expect(exported_row).to be_empty
       end
     end
 
     context 'when there are application choices with a completed form and a degree' do
       let(:application_form) { create(:completed_application_form, :with_degree) }
-      let(:application_choices) { create_list(:application_choice, 1, :with_modified_offer, application_form: application_form) }
+      let(:application_choice) { create(:application_choice, :with_modified_offer, application_form: application_form) }
 
       it 'returns the correct data' do
-        row = data_export.first
-
-        expect_row_to_match_application_choice(row, application_choices.first)
+        expect_row_to_match_application_choice(exported_row, application_choice)
       end
     end
 
     context 'when there are application choices without a degree' do
       let(:application_form) { create(:completed_application_form, degrees_completed: false) }
-      let(:application_choices) { create_list(:application_choice, 1, :with_modified_offer, application_form: application_form) }
+      let(:application_choice) { create(:application_choice, :with_modified_offer, application_form: application_form) }
 
       it 'returns the correct data' do
-        row = data_export.first
-
-        expect_row_to_match_application_choice(row, application_choices.first)
+        expect_row_to_match_application_choice(exported_row, application_choice)
       end
     end
 
@@ -45,58 +41,58 @@ RSpec.describe ProviderInterface::ApplicationDataExport do
                        .order(created_at: :asc)
                        .find_by(level: 'degree')
 
-      expected = {
-        'application_choice_id' => application_choice.id.to_s,
-        'candidate_id' => application_choice.application_form.candidate.public_id,
-        'support_reference' => application_choice.application_form.support_reference,
-        'status' => application_choice.status,
-        'submitted_at' => application_choice.application_form.submitted_at&.to_s,
-        'updated_at' => application_choice.updated_at&.to_s,
-        'recruited_at' => application_choice.recruited_at&.to_s,
-        'rejection_reason' => application_choice.rejection_reason,
-        'rejected_at' => application_choice.rejected_at&.to_s,
-        'reject_by_default_at' => application_choice.reject_by_default_at&.to_s,
-        'first_name' => application_choice.application_form.first_name,
-        'last_name' => application_choice.application_form.last_name,
-        'date_of_birth' => application_choice.application_form.date_of_birth&.to_s,
-        'nationality' => 'GB US',
-        'domicile' => application_choice.application_form.domicile,
-        'uk_residency_status' => application_choice.application_form.uk_residency_status,
-        'english_main_language' => application_choice.application_form.english_main_language&.to_s,
-        'english_language_qualifications' => application_choice.application_form.english_language_details,
-        'email' => application_choice.application_form.candidate.email_address,
-        'phone_number' => application_choice.application_form.phone_number,
-        'address_line1' => application_choice.application_form.address_line1,
-        'address_line2' => application_choice.application_form.address_line2,
-        'address_line3' => application_choice.application_form.address_line3,
-        'address_line4' => application_choice.application_form.address_line4,
-        'postcode' => application_choice.application_form.postcode,
-        'country' => application_choice.application_form.country,
-        'recruitment_cycle_year' => application_choice.application_form.recruitment_cycle_year&.to_s,
-        'provider_code' => application_choice.current_provider.code,
-        'accredited_provider_name' => application_choice.current_accredited_provider&.name,
-        'accredited_provider_code' => application_choice.current_accredited_provider&.code,
-        'course_code' => application_choice.current_course.code,
-        'site_code' => application_choice.current_site.code,
-        'study_mode' => application_choice.current_course.study_mode,
-        'start_date' => application_choice.current_course.start_date&.to_s,
-        'FIRSTDEG' => application_choice.application_form.degrees_completed ? '1' : '0',
-        'qualification_type' => first_degree&.qualification_type,
-        'non_uk_qualification_type' => first_degree&.non_uk_qualification_type,
-        'subject' => first_degree&.subject,
-        'grade' => first_degree&.grade,
-        'start_year' => first_degree&.start_year,
-        'award_year' => first_degree&.award_year,
-        'institution_details' => first_degree&.institution_name,
-        'equivalency_details' => first_degree&.equivalency_details,
-        'awarding_body' => nil,
-        'gcse_qualifications_summary' => nil,
-        'missing_gcses_explanation' => nil,
-        'disability_disclosure' => application_choice.application_form.disability_disclosure,
-      }
+      expected = [
+        application_choice.id,
+        application_choice.application_form.candidate.public_id,
+        application_choice.application_form.support_reference,
+        application_choice.status,
+        application_choice.application_form.submitted_at,
+        application_choice.updated_at,
+        application_choice.recruited_at,
+        application_choice.rejection_reason,
+        application_choice.rejected_at,
+        application_choice.reject_by_default_at,
+        application_choice.application_form.first_name,
+        application_choice.application_form.last_name,
+        application_choice.application_form.date_of_birth,
+        'GB US',
+        application_choice.application_form.domicile,
+        application_choice.application_form.uk_residency_status,
+        application_choice.application_form.english_main_language,
+        application_choice.application_form.english_language_details,
+        application_choice.application_form.candidate.email_address,
+        application_choice.application_form.phone_number,
+        application_choice.application_form.address_line1,
+        application_choice.application_form.address_line2,
+        application_choice.application_form.address_line3,
+        application_choice.application_form.address_line4,
+        application_choice.application_form.postcode,
+        application_choice.application_form.country,
+        application_choice.application_form.recruitment_cycle_year,
+        application_choice.current_provider.code,
+        application_choice.current_accredited_provider&.name,
+        application_choice.current_accredited_provider&.code,
+        application_choice.current_course.code,
+        application_choice.current_site.code,
+        application_choice.current_course_option.study_mode,
+        application_choice.current_course.start_date,
+        application_choice.application_form.degrees_completed ? 1 : 0,
+        first_degree&.qualification_type,
+        first_degree&.non_uk_qualification_type,
+        first_degree&.subject,
+        first_degree&.grade,
+        first_degree&.start_year,
+        first_degree&.award_year,
+        first_degree&.institution_name,
+        first_degree&.composite_equivalency_details,
+        nil, # included for backwards compatibility. This column is always blank
+        nil,
+        nil,
+        application_choice.application_form.disability_disclosure,
+      ]
 
-      expected.each do |key, expected_value|
-        expect(row[key]).to eq(expected_value), "Expected #{key} to eq (#{expected_value.class}) #{expected_value}, got (#{row[key].class}) #{row[key]} instead"
+      expected.each_with_index do |expected_value, index|
+        expect(row[index]).to eq(expected_value), "Expected value at #{index} to eq (#{expected_value.class}) #{expected_value}, got (#{row[index].class}) #{row[index]} instead"
       end
     end
   end
