2e959d63c54ebe60966ec1b1fd1f05023cb7cae4
davidgisbey <davidgisbey@gmail.com>
2021-08-23 19:48:35 +0100

    Update the task list component to accept a blank path

--- spec/components/utility/task_list_item_component_spec.rb
+++ spec/components/utility/task_list_item_component_spec.rb
@@ -1,14 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe TaskListItemComponent do
-  def render_component(completed:, custom_status: nil, custom_color: nil)
-    render_inline(TaskListItemComponent.new(text: 'Personal details', path: '/personal-details', completed: completed, custom_status: custom_status, custom_color: custom_color))
+  def render_component(completed:, custom_status: nil, custom_color: nil, path: '/personal-details')
+    render_inline(TaskListItemComponent.new(text: 'Personal details', path: path, completed: completed, custom_status: custom_status, custom_color: custom_color))
   end
 
-  it 'renders the correct text, href' do
+  it 'renders the correct text, href when a path is passed in' do
     result = render_component(completed: true)
     expect(result.text).to include('Personal details')
     expect(result.css('a').first.attr(:href)).to eq('/personal-details')
+  end
+
+  it 'renders the correct text when a path is not passed in' do
+    result = render_component(completed: true, path: nil)
+    expect(result.text).to include('Personal details')
+    expect(result.css('a')).to be_empty
   end
 
   it 'renders with a completed badge' do
