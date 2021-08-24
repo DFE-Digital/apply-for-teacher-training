f2f016ccf563aeae299be0556a7d3df940bafdf1
Steve Laing <steve.laing@gmail.com>
2021-08-24 15:18:17 +0100

    Add a module to provider convenient streamable methods
    This module allows us to modify response headers and return a response body suitable for a chunked/streamed data export response

---
+++ app/controllers/concerns/streamable_data_export.rb
@@ -0,0 +1,25 @@
+module StreamableDataExport
+  def streamable_response(filename:, export_data:, export_headings: nil, item_yielder: proc { |item| item })
+    streamable_response_headers(filename)
+    response.status = 200
+    streamable_response_body(export_headings: export_headings, export_data: export_data, item_yielder: item_yielder)
+  end
+
+private
+
+  def streamable_response_headers(filename)
+    headers.delete('Content-Length')
+    headers['Cache-Control'] = 'no-cache'
+    headers['Content-Type'] = 'text/csv; charset=utf-8'
+    headers['Content-Disposition'] = %(attachment; filename="#{filename}")
+    headers['X-Accel-Buffering'] = 'no'
+    headers['Last-Modified'] = Time.zone.now.ctime.to_s
+  end
+
+  def streamable_response_body(export_data:, export_headings:, item_yielder:)
+    Enumerator.new do |yielder|
+      yielder << SafeCSV.generate_line(export_headings) if export_headings.present?
+      export_data.map { |item| yielder << SafeCSV.generate_line(item_yielder.call(item)) }
+    end
+  end
+end
