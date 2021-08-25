b7dc7d21ea572003588c817e2ccfe1795f25f5e2
george-bit <george.holborn@digital.education.gov.uk>
2021-08-26 00:24:00 +0100

    Update controllers to pass params

--- app/controllers/candidate_interface/course_choices/ucas_controller.rb
+++ app/controllers/candidate_interface/course_choices/ucas_controller.rb
@@ -8,6 +8,15 @@ module CandidateInterface
       def with_course
         @provider = Provider.find(params[:provider_id])
         @course = Course.find(params[:course_id])
+
+        @return_to_path = if params[:return_to].nil?
+                            candidate_interface_course_choices_course_path(@provider.id)
+                          else
+                            candidate_interface_edit_course_choices_course_path(
+                              course_choice_id: params[:previous_course_choice_id],
+                              return_to: params[:return_to],
+                            )
+                          end
       end
     end
   end
