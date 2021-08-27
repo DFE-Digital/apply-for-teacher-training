547dbba8707613d436bd33b97a47c07adf3e8b98
Vignesh.N <vignesh.natraj@digital.education.gov.uk>
2021-08-27 15:00:01 +0100

    Cache course/provider counts

--- app/controllers/provider_interface/start_page_controller.rb
+++ app/controllers/provider_interface/start_page_controller.rb
@@ -1,16 +1,20 @@
 module ProviderInterface
   class StartPageController < ProviderInterfaceController
+    COURSES_COUNT_CACHE_KEY = 'provider_interface_courses_count'.freeze
+    PROVIDERS_COUNT_CACHE_KEY = 'provider_interface_providers_count'.freeze
     before_action :redirect_authenticated_provider_user
     skip_before_action :authenticate_provider_user!
 
     layout 'base'
 
     def show
-      courses = Course.current_cycle.includes(:provider).open_on_apply
-      @course_count = courses.count
-      @provider_count = courses.map(&:provider).uniq.count
+      @course_count = Rails.cache.fetch(COURSES_COUNT_CACHE_KEY, expires_in: 24.hours) do
+        Course.current_cycle.open_on_apply.count
+      end
+      @provider_count = Rails.cache.fetch(PROVIDERS_COUNT_CACHE_KEY, expires_in: 24.hours) do
+        Course.current_cycle.includes(:provider).open_on_apply.map(&:provider).uniq.count
+      end
 
-      @pilot_enrolment_form_url = 'https://forms.gle/D7GQ5XCoJnsneniH9'
       session['post_dfe_sign_in_path'] = provider_interface_applications_path
     end
 
