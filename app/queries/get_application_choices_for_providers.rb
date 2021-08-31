1688870d8aef0c1798127165d318ba269efcdb2c
Michael Nacos <m.nacos@gmail.com>
2021-08-31 16:10:19 +0100

    Start reading provider_ids and current_recruitment_cycle_year

--- app/queries/get_application_choices_for_providers.rb
+++ app/queries/get_application_choices_for_providers.rb
@@ -12,40 +12,32 @@ class GetApplicationChoicesForProviders
   ].freeze
 
   def self.call(providers:, vendor_api: false, includes: DEFAULT_INCLUDES, recruitment_cycle_year: RecruitmentCycle.years_visible_to_providers)
-    providers = Array.wrap(providers).select(&:present?)
+    raise MissingProvider if providers.blank? # super important!
 
-    raise MissingProvider if providers.none?
-
+    provider_ids = providers.map(&:id)
     statuses = vendor_api ? ApplicationStateChange.states_visible_to_provider_without_deferred : ApplicationStateChange.states_visible_to_provider
 
-    with_course_joins = ApplicationChoice
-      .joins('INNER JOIN course_options AS current_course_option ON current_course_option_id = current_course_option.id')
-      .joins('INNER JOIN course_options AS original_option ON course_option_id = original_option.id')
-      .joins('INNER JOIN courses AS current_course ON current_course_option.course_id = current_course.id')
-      .joins('INNER JOIN courses AS original_course ON original_option.course_id = original_course.id')
-
-    applications =
-      with_course_joins.where(
-        'original_course.provider_id' => providers,
-        'original_course.recruitment_cycle_year' => recruitment_cycle_year,
-      ).or(
-        with_course_joins.where(
-          'original_course.accredited_provider_id' => providers,
-          'original_course.recruitment_cycle_year' => recruitment_cycle_year,
-        ),
-      ).or(
-        with_course_joins.where(
-          'current_course.provider_id' => providers,
-          'current_course.recruitment_cycle_year' => recruitment_cycle_year,
-        ),
-      ).or(
-        with_course_joins.where(
-          'current_course.accredited_provider_id' => providers,
-          'current_course.recruitment_cycle_year' => recruitment_cycle_year,
-        ),
-      )
+    ApplicationChoice
+      .where(provider_ids_check(provider_ids))
+      .where(current_recruitment_cycle_year: recruitment_cycle_year)
       .where(status: statuses)
+      .includes(*includes)
+  end
 
-    applications.includes(*includes)
+  def self.provider_ids_check(provider_ids)
+    combine_with_or(
+      provider_ids.map { |id| id_in_provider_ids(id) },
+    )
+  end
+
+  def self.id_in_provider_ids(provider_id)
+    Arel::Nodes::Contains.new(
+      ApplicationChoice.arel_table[:provider_ids],
+      Arel::Nodes.build_quoted("{#{provider_id}}"),
+    )
+  end
+
+  def self.combine_with_or(conditions)
+    conditions.drop(1).inject(conditions[0]) { |a, b| a.or(b) }
   end
 end
