2faf044c22df7dcc442db2233d25eb1a02175c2d
Toby Retallick <toby.retallick@gmail.com>
2021-09-01 11:06:22 +0100

    Delegate methods to the application form instance

--- app/controllers/candidate_interface/unsubmitted_application_form_controller.rb
+++ app/controllers/candidate_interface/unsubmitted_application_form_controller.rb
@@ -8,8 +8,7 @@ module CandidateInterface
 
     def show
       @application_form_presenter = CandidateInterface::ApplicationFormPresenter.new(current_application)
-      # @application_form = current_application
-      @application_cache_key = CacheKey.generate(@application_form_presenter.application_form.cache_key_with_version)
+      @application_cache_key = CacheKey.generate(@application_form_presenter.cache_key_with_version)
     end
 
     def review
--- app/presenters/candidate_interface/application_form_presenter.rb
+++ app/presenters/candidate_interface/application_form_presenter.rb
@@ -1,14 +1,24 @@
 module CandidateInterface
   class ApplicationFormPresenter
-
     attr_reader :application_form
+
+    delegate :apply_2?,
+             :cache_key_with_version,
+             :candidate_has_previously_applied?,
+             :english_main_language,
+             :first_name,
+             :first_nationality,
+             :previous_application_form,
+             :phase,
+             :personal_details_completed,
+             :support_reference, to: :application_form
 
     def initialize(application_form)
       @application_form = application_form
     end
 
     def updated_at
-      "Last saved on #{@application_form.updated_at.to_s(:govuk_date_and_time)}"
+      "Last saved on #{application_form.updated_at.to_s(:govuk_date_and_time)}"
     end
 
     def sections_with_completion
@@ -28,7 +38,7 @@ module CandidateInterface
         [:degrees, degrees_completed?],
         [:maths_gcse, maths_gcse_completed?],
         [:english_gcse, english_gcse_completed?],
-        ([:science_gcse, science_gcse_completed?] if @application_form.science_gcse_needed?),
+        ([:science_gcse, science_gcse_completed?] if application_form.science_gcse_needed?),
         [:other_qualifications, other_qualifications_completed?],
         ([:efl, english_as_a_foreign_language_completed?] if display_efl_link?),
 
@@ -64,7 +74,7 @@ module CandidateInterface
 
     def application_choice_errors
       [].tap do |error_list|
-        @application_form.application_choices.each do |choice|
+        application_form.application_choices.each do |choice|
           if choice.course_not_available?
             error_list << ApplicationChoiceError.new(
               choice.course_not_available_error, choice.id
@@ -111,7 +121,7 @@ module CandidateInterface
     def reference_section_errors
       [].tap do |errors|
         # A defensive check, in case the candidate somehow ends up in this state
-        if @application_form.references_completed? && @application_form.selected_incorrect_number_of_references?
+        if application_form.references_completed? && application_form.selected_incorrect_number_of_references?
           errors << OpenStruct.new(message: I18n.t('application_form.references.review.incorrect_number_selected'), anchor: '#references')
         end
       end
@@ -124,28 +134,26 @@ module CandidateInterface
     end
 
     def application_choices_added?
-      @application_form.application_choices.present?
+      application_form.application_choices.present?
     end
 
-    def personal_details_completed?
-      @application_form.personal_details_completed?
-    end
+    delegate :personal_details_completed?, to: :application_form
 
     def contact_details_completed?
-      @application_form.contact_details_completed
+      application_form.contact_details_completed
     end
 
     def contact_details_valid?
-      form = ContactDetailsForm.build_from_application(@application_form)
+      form = ContactDetailsForm.build_from_application(application_form)
       form.valid?(:base) && form.valid?(:address) && form.valid?(:address_type)
     end
 
     def work_experience_completed?
-      @application_form.work_history_completed
+      application_form.work_history_completed
     end
 
     def references_link_text
-      if @application_form.application_references.present?
+      if application_form.application_references.present?
         I18n.t('section_items.manage_references')
       else
         I18n.t('section_items.add_references')
@@ -153,7 +161,7 @@ module CandidateInterface
     end
 
     def references_selection_path
-      if @application_form.application_references.includes(:application_form).selected.count >= 2
+      if application_form.application_references.includes(:application_form).selected.count >= 2
         Rails.application.routes.url_helpers.candidate_interface_review_selected_references_path
       else
         Rails.application.routes.url_helpers.candidate_interface_select_references_path
@@ -161,13 +169,13 @@ module CandidateInterface
     end
 
     def work_experience_path
-      if @application_form.feature_restructured_work_history && FeatureFlag.active?(:restructured_work_history)
-        if @application_form.application_work_experiences.any? || @application_form.work_history_explanation.present?
+      if application_form.feature_restructured_work_history && FeatureFlag.active?(:restructured_work_history)
+        if application_form.application_work_experiences.any? || application_form.work_history_explanation.present?
           Rails.application.routes.url_helpers.candidate_interface_restructured_work_history_review_path('return-to' => 'application-review')
         else
           Rails.application.routes.url_helpers.candidate_interface_restructured_work_history_path
         end
-      elsif @application_form.application_work_experiences.any? || @application_form.work_history_explanation.present?
+      elsif application_form.application_work_experiences.any? || application_form.work_history_explanation.present?
         Rails.application.routes.url_helpers.candidate_interface_work_history_show_path
       else
         Rails.application.routes.url_helpers.candidate_interface_work_history_length_path
@@ -183,7 +191,7 @@ module CandidateInterface
     end
 
     def other_qualification_path
-      if other_qualifications_completed? || other_qualifications_added? || @application_form.no_other_qualifications
+      if other_qualifications_completed? || other_qualifications_added? || application_form.no_other_qualifications
         Rails.application.routes.url_helpers.candidate_interface_review_other_qualifications_path
       else
         Rails.application.routes.url_helpers.candidate_interface_other_qualification_type_path
@@ -191,7 +199,7 @@ module CandidateInterface
     end
 
     def english_as_a_foreign_language_path
-      if @application_form.english_proficiency.present?
+      if application_form.english_proficiency.present?
         Rails.application.routes.url_helpers.candidate_interface_english_foreign_language_review_path
       else
         Rails.application.routes.url_helpers.candidate_interface_english_foreign_language_start_path
@@ -207,55 +215,55 @@ module CandidateInterface
     end
 
     def degrees_completed?
-      @application_form.degrees_completed
+      application_form.degrees_completed
     end
 
     def degrees_added?
-      @application_form.application_qualifications.degrees.any?
+      application_form.application_qualifications.degrees.any?
     end
 
     def maths_gcse_completed?
-      @application_form.maths_gcse_completed
+      application_form.maths_gcse_completed
     end
 
     def maths_gcse_added?
-      @application_form.maths_gcse.present?
+      application_form.maths_gcse.present?
     end
 
     def english_gcse_completed?
-      @application_form.english_gcse_completed
+      application_form.english_gcse_completed
     end
 
     def english_gcse_added?
-      @application_form.english_gcse.present?
+      application_form.english_gcse.present?
     end
 
     def science_gcse_completed?
-      @application_form.science_gcse_completed
+      application_form.science_gcse_completed
     end
 
     def science_gcse_added?
-      @application_form.science_gcse.present?
+      application_form.science_gcse.present?
     end
 
     def other_qualifications_completed?
-      @application_form.other_qualifications_completed && no_incomplete_qualifications?
+      application_form.other_qualifications_completed && no_incomplete_qualifications?
     end
 
     def other_qualifications_added?
-      @application_form.application_qualifications.other.any?
+      application_form.application_qualifications.other.any?
     end
 
     def english_as_a_foreign_language_completed?
-      @application_form.efl_completed?
+      application_form.efl_completed?
     end
 
     def becoming_a_teacher_completed?
-      @application_form.becoming_a_teacher_completed
+      application_form.becoming_a_teacher_completed
     end
 
     def becoming_a_teacher_valid?
-      BecomingATeacherForm.build_from_application(@application_form).valid?
+      BecomingATeacherForm.build_from_application(application_form).valid?
     end
 
     def becoming_a_teacher_path
@@ -267,15 +275,15 @@ module CandidateInterface
     end
 
     def becoming_a_teacher_review_pending?
-      @application_form.review_pending?(:becoming_a_teacher)
+      application_form.review_pending?(:becoming_a_teacher)
     end
 
     def subject_knowledge_completed?
-      @application_form.subject_knowledge_completed
+      application_form.subject_knowledge_completed
     end
 
     def subject_knowledge_valid?
-      SubjectKnowledgeForm.build_from_application(@application_form).valid?
+      SubjectKnowledgeForm.build_from_application(application_form).valid?
     end
 
     def subject_knowledge_path
@@ -287,65 +295,73 @@ module CandidateInterface
     end
 
     def subject_knowledge_review_pending?
-      @application_form.review_pending?(:subject_knowledge)
+      application_form.review_pending?(:subject_knowledge)
     end
 
     def interview_preferences_completed?
-      @application_form.interview_preferences_completed
+      application_form.interview_preferences_completed
     end
 
     def interview_preferences_valid?
-      InterviewPreferencesForm.build_from_application(@application_form).valid?
+      InterviewPreferencesForm.build_from_application(application_form).valid?
     end
 
     def training_with_a_disability_completed?
-      @application_form.training_with_a_disability_completed
+      application_form.training_with_a_disability_completed
     end
 
     def training_with_a_disability_valid?
-      TrainingWithADisabilityForm.build_from_application(@application_form).valid?
+      TrainingWithADisabilityForm.build_from_application(application_form).valid?
     end
 
     def course_choices_completed?
-      @application_form.course_choices_completed
+      application_form.course_choices_completed
     end
 
     def volunteering_completed?
-      @application_form.volunteering_completed
+      application_form.volunteering_completed
     end
 
     def volunteering_added?
-      @application_form.application_volunteering_experiences.any?
+      application_form.application_volunteering_experiences.any?
     end
 
     def references_completed?
-      @application_form.references_completed
+      application_form.references_completed
     end
 
     def safeguarding_completed?
-      @application_form.safeguarding_issues_completed
+      application_form.safeguarding_issues_completed
     end
 
     def safeguarding_valid?
-      SafeguardingIssuesDeclarationForm.build_from_application(@application_form).valid?
+      SafeguardingIssuesDeclarationForm.build_from_application(application_form).valid?
     end
 
     def no_incomplete_qualifications?
-      @application_form.application_qualifications.other.select(&:incomplete_other_qualification?).blank?
+      application_form.application_qualifications.other.select(&:incomplete_other_qualification?).blank?
     end
 
     def display_efl_link?
-      @application_form.international_applicant?
+      application_form.international_applicant?
     end
 
     def references
-      @application_form.application_references.includes(:application_form)
+      application_form.application_references.includes(:application_form)
+    end
+
+    def previous_application_choices_rejected?
+      application_form.previous_application_form.application_choices.rejected.any?
+    end
+
+    def right_to_work_or_study_present?
+      application_form.right_to_work_or_study.present?
     end
 
   private
 
     def show_review_volunteering?
-      no_volunteering_confirmed = @application_form.volunteering_experience == false && @application_form.application_volunteering_experiences.empty?
+      no_volunteering_confirmed = application_form.volunteering_experience == false && application_form.application_volunteering_experiences.empty?
 
       volunteering_completed? || volunteering_added? || no_volunteering_confirmed
     end
