a4489cbff76d93cbc3c3e27f063b2a95561b06ac
James Glenn <47917431+JR-G@users.noreply.github.com>
2021-09-02 16:37:02 +0100

    Add new routes for the updated missing GCSE flow

--- config/routes.rb
+++ config/routes.rb
@@ -210,6 +210,16 @@ Rails.application.routes.draw do
         get '/edit' => 'gcse/type#edit', as: :gcse_details_edit_type
         patch '/edit' => 'gcse/type#update'
 
+        get '/not-yet-completed' => 'gcse/not_completed_qualification#new', as: :gcse_not_yet_completed
+        post '/not-yet-completed' => 'gcse/not_completed_qualification#create'
+        get 'not-yet-completed/edit' => 'gcse/gcse/not_completed_qualification#edit', as: :gcse_edit_not_yet_completed
+        patch 'no-yet-completed/edit' => 'gcse/gcse/not_completed_qualification#update'
+
+        get '/equivalency' => 'gcse/missing_qualification#new', as: :gcse_missing
+        post '/equivalency' => 'gcse/missing_qualification#create'
+        get 'equivalency/edit' => 'gcse/gcse/missing_qualification#edit', as: :gcse_edit_missing
+        patch 'equivalency/edit' => 'gcse/gcse/missing_qualification#update'
+
         get '/country' => 'gcse/institution_country#new', as: :gcse_details_new_institution_country
         patch '/country' => 'gcse/institution_country#create'
         get '/country/edit' => 'gcse/institution_country#edit', as: :gcse_details_edit_institution_country
