f34e722bb76101e449fa7246e84c4b928f8a3fd0
Steve Hook <steve.hook@gmail.com>
2021-09-02 15:30:52 +0100

    Refactor CourseChoicesReviewVisaStatusComponent spec

--- spec/components/candidate_interface/course_choices_review_visa_status_component_spec.rb
+++ spec/components/candidate_interface/course_choices_review_visa_status_component_spec.rb
@@ -4,21 +4,10 @@ RSpec.describe CandidateInterface::CourseChoicesReviewVisaStatusComponent do
   context 'when course is salaried' do
     context 'and provider does NOT sponsor Skilled Worker visas' do
       it 'renders component with warning' do
-        course_option = create(
-          :course_option,
-          course: create(
-            :course,
-            provider: create(
-              :provider,
-              can_sponsor_skilled_worker_visa: false,
-            ),
-            funding_type: :salary,
-          ),
-        )
-        application_choice = create(
-          :application_choice,
-          :with_completed_application_form,
-          course_option: course_option,
+        application_choice = setup_application(
+          funding_type: :salary,
+          can_sponsor_skilled_worker_visa: false,
+          can_sponsor_student_visa: false,
         )
         result = render_inline(described_class.new(application_choice: application_choice))
 
@@ -28,21 +17,10 @@ RSpec.describe CandidateInterface::CourseChoicesReviewVisaStatusComponent do
 
     context 'and provider does sponsor Skilled Worker visas' do
       it 'renders component with message that visas are sponsored' do
-        course_option = create(
-          :course_option,
-          course: create(
-            :course,
-            provider: create(
-              :provider,
-              can_sponsor_skilled_worker_visa: true,
-            ),
-            funding_type: :salary,
-          ),
-        )
-        application_choice = create(
-          :application_choice,
-          :with_completed_application_form,
-          course_option: course_option,
+        application_choice = setup_application(
+          funding_type: :salary,
+          can_sponsor_skilled_worker_visa: true,
+          can_sponsor_student_visa: false,
         )
         result = render_inline(described_class.new(application_choice: application_choice))
 
@@ -54,21 +32,10 @@ RSpec.describe CandidateInterface::CourseChoicesReviewVisaStatusComponent do
   context 'when course has a fee' do
     context 'and provider does NOT sponsor Student visas' do
       it 'renders component with warning' do
-        course_option = create(
-          :course_option,
-          course: create(
-            :course,
-            provider: create(
-              :provider,
-              can_sponsor_student_visa: false,
-            ),
-            funding_type: :fee,
-          ),
-        )
-        application_choice = create(
-          :application_choice,
-          :with_completed_application_form,
-          course_option: course_option,
+        application_choice = setup_application(
+          funding_type: :fee,
+          can_sponsor_skilled_worker_visa: false,
+          can_sponsor_student_visa: false,
         )
         result = render_inline(described_class.new(application_choice: application_choice))
 
@@ -78,26 +45,39 @@ RSpec.describe CandidateInterface::CourseChoicesReviewVisaStatusComponent do
 
     context 'and provider does sponsor Student visas' do
       it 'renders component with message that visas are sponsored' do
-        course_option = create(
-          :course_option,
-          course: create(
-            :course,
-            provider: create(
-              :provider,
-              can_sponsor_student_visa: true,
-            ),
-            funding_type: :fee,
-          ),
-        )
-        application_choice = create(
-          :application_choice,
-          :with_completed_application_form,
-          course_option: course_option,
+        application_choice = setup_application(
+          funding_type: :fee,
+          can_sponsor_skilled_worker_visa: false,
+          can_sponsor_student_visa: true,
         )
         result = render_inline(described_class.new(application_choice: application_choice))
 
         expect(result.text).to include('Visas can be sponsored')
       end
     end
+  end
+
+  def setup_application(
+    funding_type:,
+    can_sponsor_skilled_worker_visa:,
+    can_sponsor_student_visa:
+  )
+    course_option = create(
+      :course_option,
+      course: create(
+        :course,
+        provider: create(
+          :provider,
+          can_sponsor_skilled_worker_visa: can_sponsor_skilled_worker_visa,
+          can_sponsor_student_visa: can_sponsor_student_visa,
+        ),
+        funding_type: funding_type,
+      ),
+    )
+    create(
+      :application_choice,
+      :with_completed_application_form,
+      course_option: course_option,
+    )
   end
 end
