306c0bd98f7f1f84b59e3a5ee59f9908e73f9e00
Duncan Brown <duncanjbrown@gmail.com>
2021-09-03 15:59:52 +0100

    Remove stale fields from analytics.yml, update specs
    - accomodate irregularities between table/class names in analytics spec
    - only consider relevant bigquery data in web request spec

--- spec/requests/emit_request_events_spec.rb
+++ spec/requests/emit_request_events_spec.rb
@@ -32,7 +32,9 @@ RSpec.describe EmitRequestEvents, type: :request, with_bigquery: true do
             headers: { 'HTTP_USER_AGENT' => 'Test agent' })
       }.to change(SendEventsToBigquery.jobs, :size).by(1)
 
-      payload = SendEventsToBigquery.jobs.first['args'].first
+      payload = SendEventsToBigquery.jobs
+        .map { |j| j['args'].first }
+        .find { |bigquery_data| bigquery_data['event_type'] == 'web_request' }
 
       expect(payload['request_method']).to eq('GET')
       expect(payload['request_user_agent']).to eq('Test agent')
