86f53e82a43488fcef762ee95f7783bce14559c9
Steve Hook <steve.hook@gmail.com>
2021-09-06 10:22:22 +0100

    Failing test for new immigration status flow

---
+++ spec/system/candidate_interface/entering_details/candidate_entering_personal_details_when_requiring_visa_2022_spec.rb
@@ -0,0 +1,63 @@
+require 'rails_helper'
+
+RSpec.describe 'Entering personal details' do
+  include CandidateHelper
+
+  scenario 'I can specify that I need to apply for right to work or study in the UK' do
+    given_i_am_apply_during_the_2022_recruitment_cycle
+    and_i_am_signed_in
+    and_i_can_complete_personal_information_with_non_british_or_irish_nationality
+    and_i_can_mark_the_section_complete
+  end
+
+  def given_i_am_apply_during_the_2022_recruitment_cycle
+    allow(RecruitmentCycle).to receive(:current_year).and_return(2022)  
+  end
+
+  def and_i_am_signed_in
+    create_and_sign_in_candidate
+    visit candidate_interface_application_form_path
+  end
+
+  def and_i_can_complete_personal_information_with_non_british_or_irish_nationality
+    click_link t('page_titles.personal_information')
+
+    # Basic details
+    scope = 'application_form.personal_details'
+    fill_in t('first_name.label', scope: scope), with: 'Lando'
+    fill_in t('last_name.label', scope: scope), with: 'Calrissian'
+    fill_in 'Day', with: '6'
+    fill_in 'Month', with: '4'
+    fill_in 'Year', with: '1937'
+    click_button t('save_and_continue')
+
+    # Nationality
+    check 'Citizen of a different country'
+    within all('.govuk-form-group')[1] do
+      select 'Pakistani'
+    end
+    click_button t('save_and_continue')
+
+    # Right to work or study
+    expect(page).to have_content 'Do you have the right to work or study in the UK for the length of the teacher training course?'
+    choose 'Not yet'
+    click_button t('save_and_continue')
+    expect(page).to have_content 'How will you get the right to work or study in the UK?'
+    choose 'A visa sponsored by a course provider'
+    click_button t('save_and_continue')
+
+    # Review
+    expect(page).to have_current_path candidate_interface_personal_details_show_path
+    expect(page).to have_content 'Name'
+    expect(page).to have_content 'Lando Calrissian'
+    expect(page).to have_content 'Pakistani'
+    expect(page).to have_content 'How will you get the right to work or study in the UK?\nA visa sponsored by a course provider.'
+  end
+
+  def and_i_can_mark_the_section_complete
+    choose t('application_form.completed_radio')
+    click_button t('continue')
+
+    expect(page).to have_css('#personal-information-badge-id', text: 'Completed')
+  end
+end
