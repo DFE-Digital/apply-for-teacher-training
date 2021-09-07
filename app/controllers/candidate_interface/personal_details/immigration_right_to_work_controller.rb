d8cda88c138ec4eefe613d757e44eed7bf889bc1
Steve Hook <steve.hook@gmail.com>
2021-09-07 01:18:24 +0100

    Add new forms for immigration status and route

--- app/controllers/candidate_interface/personal_details/immigration_right_to_work_controller.rb
+++ app/controllers/candidate_interface/personal_details/immigration_right_to_work_controller.rb
@@ -11,8 +11,8 @@ module CandidateInterface
         @form = ImmigrationRightToWorkForm.new(right_to_work_params)
 
         if @form.save(current_application)
-          if @form.immigration_right_to_work == false
-            redirect_to candidate_interface_immigration_route_path
+          if ActiveModel::Type::Boolean.new.cast(@form.immigration_right_to_work)
+            redirect_to candidate_interface_immigration_status_path
           else
             redirect_to candidate_interface_immigration_route_path
           end
@@ -21,25 +21,6 @@ module CandidateInterface
           render :new
         end
       end
-
-      # def edit
-      #   @form = RightToWorkOrStudyForm.build_from_application(current_application)
-      #   @return_to = return_to_after_edit(default: candidate_interface_personal_details_complete_path)
-      # end
-
-      # def update
-      #   @form = RightToWorkOrStudyForm.new(right_to_work_params)
-      #   @return_to = return_to_after_edit(default: candidate_interface_personal_details_complete_path)
-
-      #   if @form.save(current_application)
-      #     return redirect_to candidate_interface_application_review_path if redirect_back_to_application_review_page?
-
-      #     redirect_to candidate_interface_personal_details_show_path
-      #   else
-      #     track_validation_error(@form)
-      #     render :edit
-      #   end
-      # end
 
     private
 
