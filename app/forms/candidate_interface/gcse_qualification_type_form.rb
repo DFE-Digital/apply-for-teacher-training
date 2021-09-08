c888de67126c31a75f6efb9120040cf5427ba2a2
James Glenn <47917431+JR-G@users.noreply.github.com>
2021-09-08 12:58:20 +0100

    Update existing type controller for the new GCSE flow
    Also clear GCSE attributes if the candidate changes the type to
    missing GCSE

--- app/forms/candidate_interface/gcse_qualification_type_form.rb
+++ app/forms/candidate_interface/gcse_qualification_type_form.rb
@@ -5,7 +5,9 @@ module CandidateInterface
     include ActiveModel::Model
 
     attr_accessor :subject, :level, :qualification_type, :other_uk_qualification_type,
-                  :missing_explanation, :qualification_id, :non_uk_qualification_type
+                  :missing_explanation, :qualification_id, :non_uk_qualification_type,
+                  :not_completed_explanation, :grade, :constituent_grades, :award_year,
+                  :institution_name, :institution_country, :start_year
 
     validates :subject, :level, :qualification_type, presence: true
 
@@ -31,7 +33,6 @@ module CandidateInterface
         qualification_type: qualification_type,
         other_uk_qualification_type: other_uk_qualification_type,
         non_uk_qualification_type: non_uk_qualification_type,
-        missing_explanation: missing_explanation,
       )
     end
 
@@ -40,14 +41,22 @@ module CandidateInterface
 
       reset_other_uk_qualification_type
       reset_non_uk_qualification_type
+      reset_missing_information
+      reset_qualification_information
 
       qualification.update(
         level: level,
         subject: subject,
+        grade: grade,
+        constituent_grades: constituent_grades,
+        award_year: award_year,
+        institution_name: institution_name,
+        institution_country: institution_country,
         qualification_type: qualification_type,
         other_uk_qualification_type: other_uk_qualification_type,
         non_uk_qualification_type: non_uk_qualification_type,
         missing_explanation: missing_explanation,
+        not_completed_explanation: not_completed_explanation,
       )
     end
 
@@ -63,7 +72,6 @@ module CandidateInterface
         other_uk_qualification_type: qualification.other_uk_qualification_type,
         non_uk_qualification_type: qualification.non_uk_qualification_type,
         qualification_id: qualification.id,
-        missing_explanation: qualification.missing_explanation,
       )
     end
 
@@ -78,6 +86,23 @@ module CandidateInterface
     def reset_non_uk_qualification_type
       if qualification_type != NON_UK_QUALIFICATION_TYPE
         @non_uk_qualification_type = nil
+      end
+    end
+
+    def reset_missing_information
+      if !missing_qualification?
+        @missing_explanation = nil
+        @not_completed_explanation = nil
+      end
+    end
+
+    def reset_qualification_information
+      if missing_qualification?
+        @grade = nil
+        @constituent_grades = nil
+        @award_year = nil
+        @institution_name = nil
+        @institution_country = nil
       end
     end
   end
