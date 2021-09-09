805740175dd75814e1a798242dd2bc0dce3bdfae
SamJamCul <samuel.culley@digital.education.gov.uk>
2021-09-09 15:55:24 +0100

    Remove permission list component

--- app/components/shared/permissions_list_component.rb
+++
@@ -1,8 +0,0 @@
-class PermissionsListComponent < ViewComponent::Base
-  attr_reader :user_is_viewing_their_own_permissions, :permission_model
-
-  def initialize(permission_model, user_is_viewing_their_own_permissions: false)
-    @permission_model = permission_model
-    @user_is_viewing_their_own_permissions = user_is_viewing_their_own_permissions
-  end
-end
