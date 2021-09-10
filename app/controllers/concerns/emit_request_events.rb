7f5a9ebd84d24121aed0f45bedba29c5386bd993
Vignesh.N <vignesh.natraj@digital.education.gov.uk>
2021-09-10 15:22:41 +0100

    Move SendEventsToBigquery call to background
    using EventMachine

--- app/controllers/concerns/emit_request_events.rb
+++ app/controllers/concerns/emit_request_events.rb
@@ -14,7 +14,19 @@ module EmitRequestEvents
         .with_response_details(response)
         .with_user_and_namespace(current_user, current_namespace)
 
-      SendEventsToBigquery.perform_async(request_event.as_json)
+      EM.next_tick do
+        attempt_count = 0
+        begin
+          attempt_count += 1
+          SendEventsToBigquery.perform_async(request_event.as_json)
+        rescue Redis::CommandError
+          abort if attempt_count > 5
+          wait_time = 2 ** (attempt_count + 1)
+          puts "Waiting #{wait_time} seconds to SendEventsToBigquery..."
+          sleep wait_time
+          retry
+        end
+      end
     end
   end
 end
