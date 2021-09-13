d05fee9a8f1bfaa43de4f55b79aee442342d5fc5
James Glenn <47917431+JR-G@users.noreply.github.com>
2021-09-13 08:44:36 +0100

    Add custom validation messages

--- app/forms/candidate_interface/gcse_grade_explanation_form.rb
+++ app/forms/candidate_interface/gcse_grade_explanation_form.rb
@@ -4,6 +4,8 @@ module CandidateInterface
 
     attr_accessor :missing_explanation, :not_completed_explanation
 
+    validate :validates_not_completed_explanation
+
     def self.build_from_qualification(qualification)
       new(
         missing_explanation: qualification.missing_explanation,
@@ -12,12 +14,19 @@ module CandidateInterface
     end
 
     def save(qualification)
+      @subject_name = qualification.subject == 'english' ? qualification.subject.capitalize : qualification.subject
       return false unless valid?
 
       qualification.update!(
         missing_explanation: missing_explanation,
         not_completed_explanation: not_completed_explanation,
       )
+    end
+
+  private
+
+    def validates_not_completed_explanation
+      errors.add(:not_completed_explanation, :blank, subject: @subject_name) if not_completed_explanation.blank?
     end
   end
 end
