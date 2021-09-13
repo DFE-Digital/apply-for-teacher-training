28e7a0f6110f8cb52c29626d7c8ab1bc9ff3adf6
James Glenn <47917431+JR-G@users.noreply.github.com>
2021-09-13 16:09:34 +0100

    Modify the Grade Explanation params
    Use dig instead of require & permit as it's unable to pass the
    form through the params and as such results in an error

--- app/controllers/candidate_interface/gcse/grade_explanation_controller.rb
+++ app/controllers/candidate_interface/gcse/grade_explanation_controller.rb
@@ -55,9 +55,9 @@ module CandidateInterface
     end
 
     def update_params
-      strip_whitespace params
-        .require(:candidate_interface_gcse_grade_explanation_form)
-        .permit(:missing_explanation, :not_completed_explanation)
+      {
+        not_completed_explanation: params.dig(:candidate_interface_gcse_grade_explanation_form, :not_completed_explanation),
+      }
     end
   end
 end
