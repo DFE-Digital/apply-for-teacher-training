e5c31bc9d41a024ec0c1dfd2c820fff23e563b9c
benswannack <ben.swannack@softwire.com>
2021-09-14 12:35:47 +0100

    Only show personas on test environments

---
+++ app/components/support_interface/providers_navigation_component.rb
@@ -0,0 +1,21 @@
+module SupportInterface
+  class ProvidersNavigationComponent < ViewComponent::Base
+    include ViewHelper
+
+    def initialize; end
+
+  private
+
+    def items
+      items = [
+        { name: 'Providers', url: support_interface_providers_path },
+        { name: 'API tokens', url: support_interface_api_tokens_path },
+        { name: 'Provider users', url: support_interface_provider_users_path },
+      ]
+
+      items << { name: 'Personas', url: support_interface_personas_path } if HostingEnvironment.test_environment?
+
+      items
+    end
+  end
+end
