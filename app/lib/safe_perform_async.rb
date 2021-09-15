4ba61d74ab80d52461664a776e34cc60f55a005e
Michael Nacos <m.nacos@gmail.com>
2021-09-15 15:17:00 +0100

    Restore self here, because mixin; ignore rubocop

--- app/lib/safe_perform_async.rb
+++ app/lib/safe_perform_async.rb
@@ -3,12 +3,14 @@ module SafePerformAsync
     def perform_async(*args)
       super(*args)
     rescue Redis::BaseError => e
+      # rubocop:disable Style/RedundantSelf
       Sentry.with_scope do |scope|
-        scope.set_tags(class: to_s) # e.g. 'SendEventsToBigquery'
+        scope.set_tags(class: self.to_s) # e.g. 'SendEventsToBigquery'
         scope.set_tags(args: args.to_s) # e.g. ['{}']
         scope.set_tags(action: 'perform_async')
         Sentry.capture_exception(e)
       end
+      # rubocop:enable Style/RedundantSelf
     end
   end
 
