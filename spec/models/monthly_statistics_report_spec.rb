cbb61c95ca697e71773037c233443dda6bca8ae7
Toby Retallick <toby.retallick@gmail.com>
2021-09-17 09:20:54 +0100

    Generate monthly statistics report

---
+++ spec/models/monthly_statistics_report_spec.rb
@@ -0,0 +1,94 @@
+require 'rails_helper'
+
+RSpec.describe MonthlyStatisticsReport do
+  describe '#write_metric' do
+    context 'the #statistics attribute is nil' do
+      let(:report) { described_class.new }
+
+      it 'writes the given key/value pair' do
+        report.write_statistic(:test, :value)
+        expect(report.statistics).to eq('test' => 'value')
+      end
+    end
+
+    context 'the #statistics attribute already contains a hash' do
+      let(:report) { described_class.new(statistics: { existing: :value }) }
+
+      it 'adds new values' do
+        report.write_statistic(:new, :value)
+        expect(report.statistics).to eq(
+          'existing' => 'value',
+          'new' => 'value',
+        )
+      end
+
+      it 'overrides existing values' do
+        report.write_statistic(:existing, :changed_value)
+        expect(report.statistics).to eq('existing' => 'changed_value')
+      end
+    end
+  end
+
+  describe '#read_metric' do
+    let(:report) { described_class.new(statistics: { 'test' => 'value' }) }
+
+    it 'returns the matching value' do
+      expect(report.read_statistic('test')).to eq 'value'
+    end
+
+    it 'accepts key names as both strings and symbols' do
+      expect(report.read_statistic(:test)).to eq 'value'
+    end
+
+    it 'returns a placeholder "missing value" if the key is not found' do
+      expect(report.read_statistic(:testttt)).to eq 'n/a'
+    end
+  end
+
+  describe '#load_updated_statistics' do
+    it 'retrieves all required statistics and current version' do
+      course_age_group_monthly_statistics_double = instance_double(CourseAgeGroupMonthlyStatistics)
+
+      allow(CourseAgeGroupMonthlyStatistics).to receive(:new).and_return(course_age_group_monthly_statistics_double)
+
+      allow(course_age_group_monthly_statistics_double).to receive(:call).and_return({
+        'Primary' => {
+          'recruited' => 1,
+          'rejected' => 1,
+          'total' => 2,
+        },
+        'Secondary' => {
+          'conditions_not_met' => 1,
+          'offer' => 1,
+          'total' => 2,
+        },
+        'Further education' => {
+          'conditions_not_met' => 1,
+          'total' => 1,
+        },
+      })
+
+      report = described_class.new
+      report.load_updated_statistics
+
+      expect(report.statistics).to eq({
+        'by_course_age_group' => {
+          'Primary' => {
+            'recruited' => 1,
+            'rejected' => 1,
+            'total' => 2,
+          },
+          'Secondary' => {
+            'conditions_not_met' => 1,
+            'offer' => 1,
+            'total' => 2,
+          },
+          'Further education' => {
+            'conditions_not_met' => 1,
+            'total' => 1,
+          },
+        },
+      })
+    end
+  end
+end
