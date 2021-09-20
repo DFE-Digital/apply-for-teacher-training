57c0bb02d37ac6d24e8c82ee5e3883003bd0f1b0
Steve Hook <steve.hook@gmail.com>
2021-09-20 11:34:45 +0100

    Correction to migration - only reset non-British/Irish
    We only need to prompt non-British/Irish candidates to fill in the new
    immigration status flow.

--- app/services/data_migrations/backfill_restructured_immigration_status.rb
+++ app/services/data_migrations/backfill_restructured_immigration_status.rb
@@ -8,7 +8,7 @@ module DataMigrations
         recruitment_cycle_year: 2022,
         submitted_at: nil,
       ).find_each do |application_form|
-        application_form.update!(personal_details_completed: false)
+        application_form.update!(personal_details_completed: false) unless application_form.british_or_irish?
       end
     end
   end
