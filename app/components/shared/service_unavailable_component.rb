cca200ecbc0f016f2782d47e0b764aab6eaff1d6
cwrw <abeer.salameh@gmail.com>
2021-09-22 19:57:05 +0100

    Remove specific downtime content on service page
    We'd like a generic maintenance page to put up if
    required. THe current one is very specific, remove the
    downtime text for now.

--- app/components/shared/service_unavailable_component.rb
+++ app/components/shared/service_unavailable_component.rb
@@ -4,8 +4,4 @@ class ServiceUnavailableComponent < ViewComponent::Base
   def title_content
     HostingEnvironment.sandbox_mode? ? t('service_unavailable.sandbox_title') : t('service_unavailable.title')
   end
-
-  def downtime_content
-    HostingEnvironment.sandbox_mode? ? t('service_unavailable.sandbox_downtime') : t('service_unavailable.downtime')
-  end
 end
--- spec/components/utility/service_unavailable_component_spec.rb
+++ spec/components/utility/service_unavailable_component_spec.rb
@@ -7,17 +7,9 @@ RSpec.describe ServiceUnavailableComponent do
     expect(result.text).to include('Sorry, this service is unavailable')
   end
 
-  it 'renders the page downtime' do
-    expect(result.text).to include('use this service')
-  end
-
   context 'when the hosting environment is sandbox', sandbox: true do
     it 'renders the page title' do
       expect(result.text).to include('Sorry, the sandbox is unavailable')
-    end
-
-    it 'renders the page downtime', sandbox: true do
-      expect(result.text).to include('use the sandbox')
     end
   end
 end
