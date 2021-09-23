941fae9e211988699bd971e87c0b3228055d3308
davidgisbey <davidgisbey@gmail.com>
2021-09-23 11:09:23 +0100

    Ensure we send Apply open email to new candidates
    Currently, we won't send an email to candidates who have signed
    up after the apply1 deadline. This adds some logic to check for
    applicants with an application form in the current cycle.

---
+++ spec/queries/get_unsuccessful_and_unsubmitted_candidates_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe GetUnsuccessfulAndUnsubmittedCandidates do
+  describe '.call' do
+    it 'returns unsuccessful and unsubmitted applications from the previous cycle' do
+      rejected_application_form_from_previous_cycle = create(
+        :application_form,
+        :minimum_info,
+        recruitment_cycle_year: RecruitmentCycle.previous_year,
+      )
+
+      create(:application_choice, :with_rejection, application_form: rejected_application_form_from_previous_cycle)
+
+      carried_over_application_form = create(
+        :application_form,
+        :minimum_info,
+        recruitment_cycle_year: RecruitmentCycle.current_year,
+        candidate: rejected_application_form_from_previous_cycle.candidate,
+      )
+
+      application_form_current_year = create(:application_form, :minimum_info, recruitment_cycle_year: RecruitmentCycle.current_year)
+
+      application_form_previous_year = create(:application_form, submitted_at: 1.year.ago, recruitment_cycle_year: RecruitmentCycle.previous_year)
+
+      create(:application_choice, :with_rejection, application_form: application_form_previous_year)
+      create(:application_choice, :with_recruited, application_form: application_form_previous_year)
+
+      rejected_application_choice_from_previous_cycle = create(:application_choice, :with_rejection, :previous_year)
+      unsubmitted_application_from_previous_cycle = create(:application_form, submitted_at: nil, recruitment_cycle_year: RecruitmentCycle.previous_year)
+
+      unsuccessful_and_unsubmitted_applications = described_class.call
+
+      expect(unsuccessful_and_unsubmitted_applications.count).to eq(4)
+      expect(unsuccessful_and_unsubmitted_applications.map(&:id))
+        .to match_array(
+          [
+            carried_over_application_form.candidate.id,
+            application_form_current_year.candidate.id,
+            unsubmitted_application_from_previous_cycle.candidate.id,
+            rejected_application_choice_from_previous_cycle.application_form.candidate.id,
+          ],
+        )
+    end
+  end
+end
