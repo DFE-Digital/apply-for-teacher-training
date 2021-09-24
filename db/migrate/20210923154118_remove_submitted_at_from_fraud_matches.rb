b852b8b2f39998b43d2f7707187744fb553f66bc
cwrw <abeer.salameh@gmail.com>
2021-09-24 17:43:19 +0100

    Check if column exists when trying to remove it
    Somehow this column does not exist on the table although
    it seems fine when running all the migrations locally.
    To not block deploy check if it exists before removing.

--- db/migrate/20210923154118_remove_submitted_at_from_fraud_matches.rb
+++ db/migrate/20210923154118_remove_submitted_at_from_fraud_matches.rb
@@ -1,5 +1,5 @@
 class RemoveSubmittedAtFromFraudMatches < ActiveRecord::Migration[6.1]
   def change
-    safety_assured { remove_column :fraud_matches, :submitted_at, :datetime }
+    safety_assured { remove_column :fraud_matches, :submitted_at, :datetime, if_exists: true }
   end
 end
