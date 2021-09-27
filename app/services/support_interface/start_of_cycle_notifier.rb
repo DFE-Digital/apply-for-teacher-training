d14a8b7d4b0cf549c91ea3f5117b2cdc418c25f3
Steve Laing <steve.laing@gmail.com>
2021-09-27 14:00:20 +0100

    Make tests and notifier relative to recruitment cycle

--- app/services/support_interface/start_of_cycle_notifier.rb
+++ app/services/support_interface/start_of_cycle_notifier.rb
@@ -1,18 +1,9 @@
 module SupportInterface
   class StartOfCycleNotifier
-    def call(service:, batch_size: 500, year: 2022)
-      return unless service_opens_today?(service, year)
+    def call(service:, batch_size: 500, year: RecruitmentCycle.current_year)
+      return unless CycleTimetable.service_opens_today?(service, year: year)
 
       StartOfCycleNotificationWorker.perform_async(service, batch_size)
-    end
-
-  private
-
-    def service_opens_today?(service, year)
-      current_cycles = CycleTimetable::CYCLE_DATES[year]
-      service_opening_date = current_cycles[:"#{service}_opens"]
-
-      Time.zone.now.between?(service_opening_date, service_opening_date.change(hour: 16, min: 1))
     end
   end
 end
--- spec/services/support_interface/start_of_cycle_notifier_spec.rb
+++ spec/services/support_interface/start_of_cycle_notifier_spec.rb
@@ -2,53 +2,55 @@ require 'rails_helper'
 
 RSpec.describe SupportInterface::StartOfCycleNotifier do
   describe '#call' do
+    let(:year) { RecruitmentCycle.current_year }
+
     it 'does nothing when called on a day that is not the start of the cycle for the service' do
-      Timecop.freeze(2021, 9, 7) do
+      Timecop.freeze(6.months.before(CycleTimetable.find_opens(year))) do
         expect {
-          described_class.new.call(service: :apply)
-          described_class.new.call(service: :find)
+          described_class.new.call(service: :apply, year: year)
+          described_class.new.call(service: :find, year: year)
         }.not_to change(StartOfCycleNotificationWorker.jobs, :size)
       end
     end
 
     it 'does nothing when called out of business hours for the service opening' do
-      Timecop.freeze(2021, 10, 5, 8, 59) do
+      Timecop.freeze(CycleTimetable.find_opens(year).change(hour: 8, min: 59)) do
         expect {
-          described_class.new.call(service: :find)
+          described_class.new.call(service: :find, year: year)
         }.not_to change(StartOfCycleNotificationWorker.jobs, :size)
       end
 
-      Timecop.freeze(2021, 10, 5, 16, 2) do
+      Timecop.freeze(CycleTimetable.find_opens(year).change(hour: 16, min: 2)) do
         expect {
-          described_class.new.call(service: :find)
+          described_class.new.call(service: :find, year: year)
         }.not_to change(StartOfCycleNotificationWorker.jobs, :size)
       end
 
-      Timecop.freeze(2021, 10, 12, 8, 59) do
+      Timecop.freeze(CycleTimetable.apply_opens(year).change(hour: 8, min: 59)) do
         expect {
-          described_class.new.call(service: :apply)
+          described_class.new.call(service: :apply, year: year)
         }.not_to change(StartOfCycleNotificationWorker.jobs, :size)
       end
 
-      Timecop.freeze(2021, 10, 12, 16, 2) do
+      Timecop.freeze(CycleTimetable.apply_opens(year).change(hour: 16, min: 2)) do
         expect {
-          described_class.new.call(service: :apply)
+          described_class.new.call(service: :apply, year: year)
         }.not_to change(StartOfCycleNotificationWorker.jobs, :size)
       end
     end
 
     it 'enqueues a StartOfCycleNotificationWorker job when called on the day Apply starts' do
-      Timecop.freeze(2021, 10, 12, 9, 1) do
+      Timecop.freeze(1.minute.since(CycleTimetable.apply_opens(year))) do
         expect {
-          described_class.new.call(service: :apply)
+          described_class.new.call(service: :apply, year: year)
         }.to change(StartOfCycleNotificationWorker.jobs, :size).by(1)
       end
     end
 
     it 'enqueues a StartOfCycleNotificationWorker job when called on the day Find starts' do
-      Timecop.freeze(2021, 10, 5, 9, 1) do
+      Timecop.freeze(1.minute.since(CycleTimetable.find_opens(year))) do
         expect {
-          described_class.new.call(service: :find)
+          described_class.new.call(service: :find, year: year)
         }.to change(StartOfCycleNotificationWorker.jobs, :size).by(1)
       end
     end
