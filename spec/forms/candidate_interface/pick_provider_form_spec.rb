d00a4783447a9ddfd94467d3dc3c49e44d4ab3ae
davidgisbey <davidgisbey@gmail.com>
2021-09-29 14:40:15 +0100

    Only expose providers with courses in the provider drop down

--- spec/forms/candidate_interface/pick_provider_form_spec.rb
+++ spec/forms/candidate_interface/pick_provider_form_spec.rb
@@ -2,35 +2,15 @@ require 'rails_helper'
 
 RSpec.describe CandidateInterface::PickProviderForm do
   describe '#available_providers' do
-    it 'returns all providers' do
+    it 'returns providers with a course exposed in find in the current cycle' do
       create(:provider, name: 'School without courses')
-      create(:course, open_on_apply: false, exposed_in_find: false, provider: create(:provider, name: 'School with disabled courses'))
+      create(:course, open_on_apply: true, exposed_in_find: false, provider: create(:provider, name: 'School with disabled courses'))
       create(:course, open_on_apply: true, exposed_in_find: true, provider: create(:provider, name: 'School with courses'))
+      create(:course, open_on_apply: true, exposed_in_find: true, recruitment_cycle_year: RecruitmentCycle.previous_year)
 
       form = described_class.new({})
 
-      expect(form.available_providers.map(&:name)).to eql(['School with courses', 'School with disabled courses', 'School without courses'])
-    end
-  end
-
-  describe '#courses_available?' do
-    it 'returns false if there are no exposed courses matched by provider id in the current cycle' do
-      unexposed_course = create(:course, exposed_in_find: false)
-      provider = unexposed_course.provider
-      create(:course, exposed_in_find: true, recruitment_cycle_year: RecruitmentCycle.previous_year, provider: unexposed_course.provider)
-
-      form = described_class.new(provider_id: provider.id)
-
-      expect(form.courses_available?).to eq false
-    end
-
-    it 'returns true if there are exposed courses matched by provider id' do
-      exposed_course = create(:course, exposed_in_find: true)
-      provider = exposed_course.provider
-
-      form = described_class.new(provider_id: provider.id)
-
-      expect(form.courses_available?).to eq true
+      expect(form.available_providers.map(&:name)).to eq(['School with courses'])
     end
   end
 end
