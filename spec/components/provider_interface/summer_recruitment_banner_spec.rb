713783bf992aee113a61d1868e5f6696444137d5
benswannack <ben.swannack@softwire.com>
2021-09-29 14:42:19 +0100

    Remove old body text of summer recruitment banner

--- spec/components/provider_interface/summer_recruitment_banner_spec.rb
+++ spec/components/provider_interface/summer_recruitment_banner_spec.rb
@@ -13,30 +13,6 @@ RSpec.describe ProviderInterface::SummerRecruitmentBanner do
     it 'renders the banner header' do
       expect(result.text).to include(t('summer_recruitment_banner.header'))
     end
-
-    describe 'rendering the banner content' do
-      around do |example|
-        Timecop.freeze(time) { example.run }
-      end
-
-      context 'when the current time is before the apply 2 deadline' do
-        let(:time) { CycleTimetable.apply_2_deadline(RecruitmentCycle.current_year) - 1.day }
-
-        it 'renders the banner content' do
-          expect(result.text).to include(t('summer_recruitment_banner.body'))
-          expect(page).to have_selector('.govuk-body')
-        end
-      end
-
-      context 'when the current time is after the apply 2 deadline' do
-        let(:time) { CycleTimetable.apply_2_deadline(RecruitmentCycle.current_year) + 1.day }
-
-        it 'does not render the banner content' do
-          expect(result.text).not_to include(t('summer_recruitment_banner.body'))
-          expect(page).not_to have_css('.govuk-body')
-        end
-      end
-    end
   end
 
   context 'when the provider information banner feature flag is off' do
