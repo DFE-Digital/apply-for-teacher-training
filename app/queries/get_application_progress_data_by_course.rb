627f5b51a75074b841e1dadfb6fdf524787bb59a
Richard Pattinson <richie.pattinson@googlemail.com>
2021-09-29 13:59:51 +0100

    filter accredited provider report by current cycle

--- app/queries/get_application_progress_data_by_course.rb
+++ app/queries/get_application_progress_data_by_course.rb
@@ -10,7 +10,7 @@ class GetApplicationProgressDataByCourse
       .joins(:provider)
       .left_joins(:accredited_provider)
       .where(recruitment_cycle_year: RecruitmentCycle.current_year, provider_id: provider.id)
-      .or(Course.where(accredited_provider_id: provider.id))
+      .or(Course.where(accredited_provider_id: provider.id, recruitment_cycle_year: RecruitmentCycle.current_year))
       .group('courses.id', 'courses.name', 'courses.code', 'application_choices.status', 'providers.name', 'accredited_providers_courses.name')
       .select('courses.provider_id', 'courses.accredited_provider_id', 'providers.name as provider_name',
               'accredited_providers_courses.name as accredited_provider_name', 'courses.name', 'courses.code',
