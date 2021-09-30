dcd7f97debd18d20e1cae3bf3868e898970f9db8
Despo Pentara <despo@extractmethod.com>
2021-09-30 14:32:09 +0100

    Increase BigQuery timeout and set retries to ensure that any requests that timeout are executed

--- app/workers/send_events_to_bigquery.rb
+++ app/workers/send_events_to_bigquery.rb
@@ -6,7 +6,7 @@ class SendEventsToBigquery
 
   def perform(request_event_json)
     table_name = ENV.fetch('BIG_QUERY_TABLE_NAME', 'events')
-    bq = Google::Cloud::Bigquery.new(project: ENV.fetch('BIG_QUERY_PROJECT_ID'))
+    bq = Google::Cloud::Bigquery.new(project: ENV.fetch('BIG_QUERY_PROJECT_ID'), retries: 3, timeout: 120)
     dataset = bq.dataset(ENV.fetch('BIG_QUERY_DATASET'), skip_lookup: true)
     bq_table = dataset.table(table_name, skip_lookup: true)
     bq_table.insert([request_event_json])
