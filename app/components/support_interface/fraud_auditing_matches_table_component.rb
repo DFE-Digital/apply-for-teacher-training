79325d8110b2a72c21f0a3b1dc2c1a80b08a67c8
george-bit <george.holborn@digital.education.gov.uk>
2021-10-04 10:35:49 +0100

    Update Fraud auditing dashboard to use model
    Now that we have a table for fraud matches the front end needs to use this
    rather than just loading the raw query data. This PR removes the postcode
    and date of birth columns as they are superfluous. Adds in columns to
    accomodate future features.

--- app/components/support_interface/fraud_auditing_matches_table_component.rb
+++ app/components/support_interface/fraud_auditing_matches_table_component.rb
@@ -9,7 +9,34 @@ module SupportInterface
     end
 
     def table_rows
-      matches.map(&:symbolize_keys)
+      matches.map do |match|
+        {
+          first_names: match.candidates.map { |candidate| candidate.application_forms.first.first_name },
+          last_name: match.last_name,
+          fraudulent: marked_as_fraudulent?(match),
+          candidate_last_contacted_at: match.candidate_last_contacted_at&.to_s(:govuk_date_and_time),
+          email_addresses:  match.candidates,
+          submitted_at: match.candidates.map { |candidate| submitted?(candidate) },
+        }
+      end
+    end
+
+    private
+
+    def submitted?(candidate)
+      if candidate.current_application.submitted_at?
+        'Yes'
+      else
+        'No'
+      end
+    end
+
+    def marked_as_fraudulent?(match)
+      if match.fraudulent?
+        'Yes'
+      else
+        'No'
+      end
     end
   end
 end
