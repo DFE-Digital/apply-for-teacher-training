81b7e0b12219c4fd3308e6732373bbd9e682ac24
Despo Pentara <despo@extractmethod.com>
2021-10-06 11:41:04 +0100

    Rename variable names and extract reusable method

--- app/services/save_provider_user.rb
+++ app/services/save_provider_user.rb
@@ -1,4 +1,6 @@
 class SaveProviderUser
+  attr_reader :provider_user, :provider_permissions, :deselected_provider_permissions
+
   def initialize(provider_user:, provider_permissions: [], deselected_provider_permissions: [])
     @provider_user = provider_user
     @provider_permissions = provider_permissions
@@ -6,18 +8,18 @@ class SaveProviderUser
   end
 
   def call!
-    @provider_user.save!
+    provider_user.save!
     save_notification_preferences!
     update_provider_permissions!
-    @provider_user.reload
+    provider_user.reload
   end
 
 private
 
   def save_notification_preferences!
-    return if ProviderUserNotificationPreferences.exists?(provider_user: @provider_user)
+    return if ProviderUserNotificationPreferences.exists?(provider_user: provider_user)
 
-    ProviderUserNotificationPreferences.create!(provider_user: @provider_user)
+    ProviderUserNotificationPreferences.create!(provider_user: provider_user)
   end
 
   def update_provider_permissions!
@@ -29,19 +31,24 @@ private
   end
 
   def destroy_deselected_provider_permissions!
-    @deselected_provider_permissions.each(&:destroy!)
+    deselected_provider_permissions.each(&:destroy!)
   end
 
   def add_new_provider_permissions!
-    new_provider_permissions = @provider_permissions - @provider_user.provider_permissions
-    new_provider_permissions.each do |pp|
-      pp.provider_user_id = @provider_user.id if pp.provider_user_id.blank?
-      pp.save!
+    new_provider_permissions.each do |provider_permissions|
+      provider_permissions.provider_user_id ||= provider_user.id
+      provider_permissions.save!
     end
   end
 
   def updated_existing_provider_permissions!
-    existing_provider_permissions = @provider_permissions & @provider_user.provider_permissions
-    existing_provider_permissions.each { |p| p.save! if p.changed? }
+    existing_provider_permissions = provider_permissions & provider_user.provider_permissions
+    existing_provider_permissions.each do |provider_permissions|
+      provider_permissions.save! if provider_permissions.changed?
+    end
+  end
+
+  def new_provider_permissions
+    @new_provider_permissions ||= provider_permissions - provider_user.provider_permissions
   end
 end
