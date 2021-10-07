4814dcc2b058adfe354beefbb6726588d5a18aec
Despo Pentara <despo@extractmethod.com>
2021-10-07 14:37:00 +0100

    Configure secondary sidekiq worker

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -31,26 +31,27 @@ provider "azurerm" {
 module "paas" {
   source = "./modules/paas"
 
-  cf_api_url                = local.cf_api_url
-  cf_user                   = var.paas_sso_code == "" ? local.infra_secrets.CF_USER : null
-  cf_user_password          = var.paas_sso_code == "" ? local.infra_secrets.CF_PASSWORD : null
-  cf_sso_passcode           = var.paas_sso_code
-  cf_space                  = var.paas_cf_space
-  prometheus_app            = var.prometheus_app
-  docker_credentials        = local.docker_credentials
-  web_app_instances         = var.paas_web_app_instances
-  web_app_memory            = var.paas_web_app_memory
-  app_docker_image          = var.paas_docker_image
-  app_environment           = var.paas_app_environment
-  app_environment_variables = local.paas_app_environment_variables
-  logstash_url              = local.infra_secrets.LOGSTASH_URL
-  postgres_service_plan     = var.paas_postgres_service_plan
-  worker_redis_service_plan = var.paas_worker_redis_service_plan
-  cache_redis_service_plan  = var.paas_cache_redis_service_plan
-  clock_app_memory          = var.paas_clock_app_memory
-  worker_app_memory         = var.paas_worker_app_memory
-  clock_app_instances       = var.paas_clock_app_instances
-  worker_app_instances      = var.paas_worker_app_instances
+  cf_api_url                      = local.cf_api_url
+  cf_user                         = var.paas_sso_code == "" ? local.infra_secrets.CF_USER : null
+  cf_user_password                = var.paas_sso_code == "" ? local.infra_secrets.CF_PASSWORD : null
+  cf_sso_passcode                 = var.paas_sso_code
+  cf_space                        = var.paas_cf_space
+  prometheus_app                  = var.prometheus_app
+  docker_credentials              = local.docker_credentials
+  web_app_instances               = var.paas_web_app_instances
+  web_app_memory                  = var.paas_web_app_memory
+  app_docker_image                = var.paas_docker_image
+  app_environment                 = var.paas_app_environment
+  app_environment_variables       = local.paas_app_environment_variables
+  logstash_url                    = local.infra_secrets.LOGSTASH_URL
+  postgres_service_plan           = var.paas_postgres_service_plan
+  worker_redis_service_plan       = var.paas_worker_redis_service_plan
+  cache_redis_service_plan        = var.paas_cache_redis_service_plan
+  clock_app_memory                = var.paas_clock_app_memory
+  worker_app_memory               = var.paas_worker_app_memory
+  clock_app_instances             = var.paas_clock_app_instances
+  worker_app_instances            = var.paas_worker_app_instances
+  worker_secondary_app_instances  = var.paas_worker_secondary_app_instances
 
   restore_db_from_db_instance          = var.paas_restore_db_from_db_instance
   restore_db_from_point_in_time_before = var.paas_restore_db_from_point_in_time_before
