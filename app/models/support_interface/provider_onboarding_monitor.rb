34866058355b264066c6bb1a5010f871ba693b03
benswannack <ben.swannack@softwire.com>
2021-10-08 13:11:48 +0100

    Memoize methods in provider onboarding monitor

--- app/models/support_interface/provider_onboarding_monitor.rb
+++ app/models/support_interface/provider_onboarding_monitor.rb
@@ -1,39 +1,40 @@
 module SupportInterface
   class ProviderOnboardingMonitor
     def providers_with_no_users
-      Provider.where.not(id: providers_with_users.select(:id))
+      @providers_with_no_users ||= Provider.where.not(id: providers_with_users.select(:id))
     end
 
     def providers_where_no_user_has_logged_in
-      providers_with_users
-        .select('providers.*, MAX(provider_users.last_signed_in_at)')
-        .group('providers.id')
-        .having('MAX(provider_users.last_signed_in_at) IS NULL')
+      @providers_where_no_user_has_logged_in ||= providers_with_users
+                                                   .select('providers.*, MAX(provider_users.last_signed_in_at)')
+                                                   .group('providers.id')
+                                                   .having('MAX(provider_users.last_signed_in_at) IS NULL')
     end
 
     def permissions_not_set_up
-      providers_that_need_to_set_up_permissions = ProviderRelationshipPermissions
-                                                    .providers_have_open_course
-                                                    .where(setup_at: nil)
-                                                    .pluck(:ratifying_provider_id, :training_provider_id)
-                                                    .flatten
-                                                    .uniq
-
-      providers_with_users.where(id: providers_that_need_to_set_up_permissions)
+      @permissions_not_set_up ||= begin
+        providers_that_need_to_set_up_permissions = ProviderRelationshipPermissions
+                                                      .providers_have_open_course
+                                                      .where(setup_at: nil)
+                                                      .pluck(:ratifying_provider_id, :training_provider_id)
+                                                      .flatten
+                                                      .uniq
+        providers_with_users.where(id: providers_that_need_to_set_up_permissions)
+      end
     end
 
     def no_decisions_in_last_7_days
-      Provider
-        .select('providers.*, MAX(last_decisions.last_decision) as last_decision')
-        .joins("INNER JOIN (#{applications_with_last_decision_sql}) as last_decisions ON providers.id = ANY(last_decisions.provider_ids)")
-        .group('providers.id')
-        .having("MAX(last_decisions.last_decision) < now() - interval '7 days' OR MAX(last_decisions.last_decision) IS NULL")
+      @no_decisions_in_last_7_days ||= Provider
+                                         .select('providers.*, MAX(last_decisions.last_decision) as last_decision')
+                                         .joins("INNER JOIN (#{applications_with_last_decision_sql}) as last_decisions ON providers.id = ANY(last_decisions.provider_ids)")
+                                         .group('providers.id')
+                                         .having("MAX(last_decisions.last_decision) < now() - interval '7 days' OR MAX(last_decisions.last_decision) IS NULL")
     end
 
   private
 
     def providers_with_users
-      Provider.joins(:provider_users).distinct
+      @providers_with_users ||= Provider.joins(:provider_users).distinct
     end
 
     def applications_with_last_decision_sql
