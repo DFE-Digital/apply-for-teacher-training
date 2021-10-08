55b62587fe7bf08ddecd18fd9e51e9e528971c99
James Glenn <47917431+JR-G@users.noreply.github.com>
2021-10-08 13:53:52 +0100

    Add blocked to the fraud_matches table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_07_103121) do
+ActiveRecord::Schema.define(version: 2021_10_08_124803) do
 
   create_sequence "qualifications_public_id_seq", start: 120000
 
@@ -472,6 +472,7 @@ ActiveRecord::Schema.define(version: 2021_10_07_103121) do
     t.boolean "fraudulent?"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "blocked", default: false
   end
 
   create_table "ielts_qualifications", force: :cascade do |t|
