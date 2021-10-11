e839e7c111276ffbef85f6d986d7a207a8bb939d
SamJamCul <samuel.culley@digital.education.gov.uk>
2021-10-11 15:18:56 +0100

    Remove bold and add ratifying provider to offer course change

--- app/components/utility/collection_select_component.rb
+++ app/components/utility/collection_select_component.rb
@@ -4,9 +4,9 @@ class CollectionSelectComponent < ViewComponent::Base
   attr_reader :attribute, :collection,
               :value_method, :text_method, :hint_method,
               :form_object, :form_path, :form_method,
-              :page_title, :caption
+              :page_title, :caption, :bold_labels
 
-  def initialize(attribute:, collection:, value_method:, text_method:, hint_method:, form_object:, form_path:, page_title:, caption:, form_method: :post)
+  def initialize(attribute:, collection:, value_method:, text_method:, hint_method:, form_object:, form_path:, page_title:, caption:, form_method: :post, bold_labels: true)
     @attribute = attribute
     @collection = collection
     @value_method = value_method
@@ -17,5 +17,6 @@ class CollectionSelectComponent < ViewComponent::Base
     @form_method = form_method
     @page_title = page_title
     @caption = caption
+    @bold_labels = hint_method.present? ? bold_labels : false
   end
 end
