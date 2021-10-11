8a5c424189564cbd93a1e3cb97b09fae3821cf21
davidgisbey <davidgisbey@gmail.com>
2021-10-11 21:42:52 +0100

    Add a slack update to the UpdateFraudMatches service

--- app/services/update_fraud_matches.rb
+++ app/services/update_fraud_matches.rb
@@ -19,5 +19,24 @@ class UpdateFraudMatches
         )
       end
     end
+
+    message = <<~MSG
+      :face_with_monocle: There’s #{new_match_count} new fraud #{'match'.pluralize(new_match_count)} today :face_with_monocle:
+      :gavel: #{fraudulent_match_count} #{'match'.pluralize(fraudulent_match_count)} #{fraudulent_match_count == 1 ? 'has' : 'have'} been marked as fraudulent :gavel:
+      :female-detective: In total there’s #{FraudMatch.count} #{'match'.pluralize(FraudMatch.count)} :male-detective:
+    MSG
+
+    url = Rails.application.routes.url_helpers.support_interface_fraud_auditing_matches_url
+    SlackNotificationWorker.perform_async(message, url)
+  end
+
+private
+
+  def new_match_count
+    FraudMatch.where('created_at > ?', 1.day.ago).count
+  end
+
+  def fraudulent_match_count
+    FraudMatch.where(fraudulent?: true).count
   end
 end
--- spec/services/update_fraud_matches_spec.rb
+++ spec/services/update_fraud_matches_spec.rb
@@ -3,11 +3,21 @@ require 'rails_helper'
 RSpec.describe UpdateFraudMatches do
   let(:candidate1) { create(:candidate, email_address: 'exemplar1@example.com') }
   let(:candidate2) { create(:candidate, email_address: 'exemplar2@example.com') }
+  let(:expected_message) do
+    <<~MSG
+      :face_with_monocle: There’s 1 new fraud match today :face_with_monocle:
+      :gavel: 1 match has been marked as fraudulent :gavel:
+      :female-detective: In total there’s 2 matches :male-detective:
+    MSG
+  end
+
+  let(:expected_url) { Rails.application.routes.url_helpers.support_interface_fraud_auditing_matches_url }
 
   before do
     Timecop.freeze(Time.zone.local(2020, 8, 23, 12, 0o0, 0o0)) do
       create(:application_form, candidate: candidate1, first_name: 'Jeffrey', last_name: 'Thompson', date_of_birth: '1998-08-08', postcode: 'W6 9BH', submitted_at: Time.zone.now)
       create(:application_form, candidate: candidate2, first_name: 'Joffrey', last_name: 'Thompson', date_of_birth: '1998-08-08', postcode: 'W6 9BH')
+      allow(SlackNotificationWorker).to receive(:perform_async)
     end
   end
 
@@ -42,6 +52,23 @@ RSpec.describe UpdateFraudMatches do
       expect(match.date_of_birth).to eq(candidate1.application_forms.first.date_of_birth)
       expect(match.last_name).to eq('Thompson')
       expect(match.recruitment_cycle_year).to eq(RecruitmentCycle.current_year)
+    end
+
+    it 'sends a slack message' do
+      application_form1 = create(:application_form, first_name: 'Jeffrey', last_name: 'Thompsun', date_of_birth: '1998-08-08', postcode: 'W6 9BH', submitted_at: Time.zone.now)
+      application_form2 = create(:application_form, first_name: 'Joffrey', last_name: 'Thompsun', date_of_birth: '1998-08-08', postcode: 'W6 9BH')
+
+      create(:fraud_match,
+             candidates: [application_form1.candidate, application_form2.candidate],
+             last_name: 'Thompsun',
+             date_of_birth: '1998-08-08',
+             postcode: 'W6 9BH',
+             fraudulent?: true,
+             created_at: 2.days.ago)
+
+      described_class.new.save!
+
+      expect(SlackNotificationWorker).to have_received(:perform_async).with(expected_message, expected_url)
     end
   end
 end
