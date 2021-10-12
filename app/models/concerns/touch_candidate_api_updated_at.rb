c4ae0ec4babfd10b1812469f10046280a7f55c6d
davidgisbey <davidgisbey@gmail.com>
2021-10-12 14:51:58 +0100

    Fix issue with the callback mutating the object

--- app/models/concerns/touch_candidate_api_updated_at.rb
+++ app/models/concerns/touch_candidate_api_updated_at.rb
@@ -5,9 +5,12 @@ module TouchCandidateAPIUpdatedAt
     around_save do |_object, block|
       application_form = is_a?(ApplicationForm) ? self : self.application_form
 
-      previous_application_form_status = ProcessState.new(application_form).state
+      duped_form = application_form.id.nil? ? application_form.dup : ApplicationForm.find(application_form.id)
+      previous_application_form_status = ProcessState.new(duped_form).state
+
       block.call
-      current_application_form_status = ProcessState.new(application_form).state
+
+      current_application_form_status = ProcessState.new(ApplicationForm.find(application_form.id)).state
 
       candidate.update!(candidate_api_updated_at: Time.zone.now) if previous_application_form_status != current_application_form_status
     end
