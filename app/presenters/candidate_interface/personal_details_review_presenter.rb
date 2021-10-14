3052a4149cfd3fbc5c675b092ccb413d62ee12fe
davidgisbey <davidgisbey@gmail.com>
2021-10-14 11:28:53 +0100

    Fix bug with personal details review component

--- app/presenters/candidate_interface/personal_details_review_presenter.rb
+++ app/presenters/candidate_interface/personal_details_review_presenter.rb
@@ -53,7 +53,7 @@ module CandidateInterface
     def date_of_birth_row
       {
         key: I18n.t('application_form.personal_details.date_of_birth.label'),
-        value: @personal_details_form.date_of_birth.to_s(:govuk_date),
+        value: @personal_details_form.date_of_birth&.to_s(:govuk_date),
         action: (if @editable
                    {
                      href: candidate_interface_edit_name_and_dob_path(return_to_params),
