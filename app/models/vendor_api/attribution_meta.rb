41e37738497041454888fd433349173bbbe85128
Despo Pentara <despo@extractmethod.com>
2021-10-15 09:57:20 +0100

    Add character limit for attribution_meta properties

--- app/models/vendor_api/attribution_meta.rb
+++ app/models/vendor_api/attribution_meta.rb
@@ -5,6 +5,8 @@ module VendorAPI
     attr_accessor :full_name, :email, :user_id
 
     validates :full_name, :email, :user_id, presence: true
+    validates :full_name, length: { maximum: 120 }
+    validates :email, :user_id, length: { maximum: 100 }
 
     def initialize(attrs = {})
       @full_name = attrs[:full_name]
--- spec/models/vendor_api/attribution_meta_spec.rb
+++ spec/models/vendor_api/attribution_meta_spec.rb
@@ -6,4 +6,8 @@ RSpec.describe VendorAPI::AttributionMeta do
   it { is_expected.to validate_presence_of :full_name }
   it { is_expected.to validate_presence_of :email }
   it { is_expected.to validate_presence_of :user_id }
+
+  it { is_expected.to validate_length_of(:full_name).is_at_most(120) }
+  it { is_expected.to validate_length_of(:email).is_at_most(100) }
+  it { is_expected.to validate_length_of(:user_id).is_at_most(100) }
 end
