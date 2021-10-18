162bc8184e7dfd9661f59ca1e44c918343f62db9
Toby Retallick <toby.retallick@gmail.com>
2021-10-18 15:24:24 +0100

    Create 'by primary specialist subject' monthly stats table

--- spec/models/monthly_statistics_report_spec.rb
+++ spec/models/monthly_statistics_report_spec.rb
@@ -46,22 +46,25 @@ RSpec.describe MonthlyStatisticsReport do
   end
 
   describe '#load_updated_statistics' do
-    it 'retrieves all required statistics and current version' do
+    it 'retrieves all required statistics' do
       course_age_group_monthly_statistics_double = instance_double(MonthlyStatistics::ByCourseAgeGroup)
       sex_group_monthly_statistics_double = instance_double(MonthlyStatistics::BySex)
       applications_by_status_monthly_statistics_double = instance_double(MonthlyStatistics::ByStatus)
       candidates_by_status_monthly_statistics_double = instance_double(MonthlyStatistics::ByStatus)
+      applications_by_primary_specialist_subject_double = instance_double(MonthlyStatistics::ByPrimarySpecialistSubject)
       table_data = [{ 'foo' => 'bar' }]
 
       allow(MonthlyStatistics::ByCourseAgeGroup).to receive(:new).and_return(course_age_group_monthly_statistics_double)
       allow(MonthlyStatistics::BySex).to receive(:new).and_return(sex_group_monthly_statistics_double)
       allow(MonthlyStatistics::ByStatus).to receive(:new).and_return(applications_by_status_monthly_statistics_double)
       allow(MonthlyStatistics::ByStatus).to receive(:new).with(by_candidate: true).and_return(candidates_by_status_monthly_statistics_double)
+      allow(MonthlyStatistics::ByPrimarySpecialistSubject).to receive(:new).and_return(applications_by_primary_specialist_subject_double)
 
       allow(course_age_group_monthly_statistics_double).to receive(:table_data).and_return(table_data)
       allow(sex_group_monthly_statistics_double).to receive(:table_data).and_return(table_data)
       allow(applications_by_status_monthly_statistics_double).to receive(:table_data).and_return(table_data)
       allow(candidates_by_status_monthly_statistics_double).to receive(:table_data).and_return(table_data)
+      allow(applications_by_primary_specialist_subject_double).to receive(:table_data).and_return(table_data)
 
       report = described_class.new
       report.load_table_data
@@ -71,6 +74,7 @@ RSpec.describe MonthlyStatisticsReport do
         'by_sex' => table_data,
         'applications_by_status' => table_data,
         'candidates_by_status' => table_data,
+        'by_primary_specialist_subject' => table_data,
       )
     end
   end
