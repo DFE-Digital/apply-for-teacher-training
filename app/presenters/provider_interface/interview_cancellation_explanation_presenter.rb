19d23f29405be42381edba393d8291aa3aa5ff0b
Richard Pattinson <richie.pattinson@googlemail.com>
2021-10-18 15:46:59 +0100

    add presenter to display interview cancellation explanation text

---
+++ app/presenters/provider_interface/interview_cancellation_explanation_presenter.rb
@@ -0,0 +1,23 @@
+module ProviderInterface
+  class InterviewCancellationExplanationPresenter
+    attr_reader :application_choice
+
+    def initialize(application_choice)
+      @application_choice = application_choice
+    end
+
+    def render?
+      number_of_interviews_to_be_cancelled.positive?
+    end
+
+    def text
+      I18n.t(:interview_cancellation_explanation, count: number_of_interviews_to_be_cancelled)
+    end
+
+  private
+
+    def number_of_interviews_to_be_cancelled
+      application_choice.interviews.kept.upcoming_not_today.count
+    end
+  end
+end
