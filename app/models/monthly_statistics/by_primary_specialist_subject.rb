082c29659472060b06b9fa6bb3a8b8a95efea6f9
Toby Retallick <toby.retallick@gmail.com>
2021-10-18 15:26:43 +0100

    Refactor - inherit from MonthlyStatistics::Base

--- app/models/monthly_statistics/by_primary_specialist_subject.rb
+++ app/models/monthly_statistics/by_primary_specialist_subject.rb
@@ -1,5 +1,5 @@
 module MonthlyStatistics
-  class ByPrimarySpecialistSubject
+  class ByPrimarySpecialistSubject < MonthlyStatistics::Base
     def table_data
       {
         rows: rows,
@@ -92,44 +92,6 @@ module MonthlyStatistics
       ApplicationChoice
         .joins(course: :subjects)
         .where(current_recruitment_cycle_year: recruitment_cycle_year)
-    end
-
-    def current_year_group_query
-      ApplicationChoice
-        .joins(course: :subjects)
-        .where(current_recruitment_cycle_year: RecruitmentCycle.current_year)
-        .where.not(status: 'offer_deferred')
-        .where(course: { level: 'primary' })
-        .group('status', 'subjects.name')
-        .count
-    end
-
-    def recruited_count(statuses)
-      statuses['recruited'] || 0
-    end
-
-    def pending_count(statuses)
-      statuses['pending_conditions'] || 0
-    end
-
-    def offer_count(statuses)
-      statuses['offer'] || 0
-    end
-
-    def awaiting_decision_count(statuses)
-      (statuses['awaiting_provider_decision'] || 0) + (statuses['interviewing'] || 0)
-    end
-
-    def unsuccessful_count(statuses)
-      (statuses['declined'] || 0) + (statuses['rejected'] || 0)
-    end
-
-    def statuses_count(statuses)
-      recruited_count(statuses) +
-        pending_count(statuses) +
-        offer_count(statuses) +
-        awaiting_decision_count(statuses) +
-        unsuccessful_count(statuses)
     end
   end
 end
