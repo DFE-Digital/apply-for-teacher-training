da9ef1d2494a74be9bec68212acc06c28153a5de
benswannack <ben.swannack@softwire.com>
2021-10-20 13:17:11 +0100

    Add data migration to ensure candidates enter degree information before submitting

--- lib/tasks/data.rake
+++ lib/tasks/data.rake
@@ -1,5 +1,6 @@
 DATA_MIGRATION_SERVICES = [
   # do not delete or edit this line - services added below by generator
+  'DataMigrations::BackfillCarriedOverApplicationsDegreesComplete',
   'DataMigrations::BackfillWorkHistoryStatusForCurrentCycle',
   'DataMigrations::BackfillWorkHistoryStatus',
   'DataMigrations::AddVendorsToProvidersForFirstTime',
---
+++ spec/services/data_migrations/backfill_carried_over_applications_degrees_complete_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+RSpec.describe DataMigrations::BackfillCarriedOverApplicationsDegreesComplete do
+  let!(:form_with_missing_start_year_degree) do
+    form = create(:application_form, degrees_completed: true)
+    create(:degree_qualification, application_form: form, start_year: nil)
+    form
+  end
+  let!(:form_with_missing_award_year_degree) do
+    form = create(:application_form, degrees_completed: true)
+    create(:degree_qualification, application_form: form, award_year: nil)
+    form
+  end
+  let!(:incomplete_form_with_invalid_degree) do
+    form = create(:application_form, degrees_completed: false)
+    create(:degree_qualification, application_form: form, start_year: nil)
+    form
+  end
+  let!(:submitted_form_with_invalid_degree) do
+    form = create(:application_form, degrees_completed: true, submitted_at: 1.day.ago)
+    create(:degree_qualification, application_form: form, award_year: nil)
+    form
+  end
+  let!(:form_with_valid_degree) do
+    form = create(:application_form, degrees_completed: true)
+    create(:degree_qualification, application_form: form)
+    form
+  end
+  let!(:previous_year_form_with_invalid_degree) do
+    form = create(:application_form, recruitment_cycle_year: RecruitmentCycle.previous_year, degrees_completed: true)
+    create(:degree_qualification, application_form: form, start_year: nil)
+    form
+  end
+
+  it 'sets degrees_complete to false on unsubmitted current cycle applications with degrees with no start_year' do
+    expect { described_class.new.change }.to change { form_with_missing_start_year_degree.reload.degrees_completed }.from(true).to(false)
+  end
+
+  it 'sets degrees_complete to false on unsubmitted current cycle applications with degrees with no award_year' do
+    expect { described_class.new.change }.to change { form_with_missing_award_year_degree.reload.degrees_completed }.from(true).to(false)
+  end
+
+  it 'does not change unsubmitted current cycle applications with degrees with no award_year but degrees_complete already false' do
+    expect { described_class.new.change }.not_to(change { incomplete_form_with_invalid_degree.reload.updated_at })
+  end
+
+  it 'does not change submitted current cycle applications with invalid degrees' do
+    expect { described_class.new.change }.not_to(change { submitted_form_with_invalid_degree.reload.updated_at })
+  end
+
+  it 'does not change unsubmitted current cycle applications with valid degrees' do
+    expect { described_class.new.change }.not_to(change { form_with_valid_degree.reload.updated_at })
+  end
+
+  it 'does not change previous cycle applications with invalid degrees' do
+    expect { described_class.new.change }.not_to(change { previous_year_form_with_invalid_degree.reload.updated_at })
+  end
+end
