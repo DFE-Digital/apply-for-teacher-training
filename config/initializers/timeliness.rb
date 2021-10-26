71382b1c95ff8dd8acc0a11cb1e506b3e72436d3
benswannack <ben.swannack@softwire.com>
2021-10-26 16:50:23 +0100

    Add additional allowed time formats for interviews, and add new specs for allowed times

---
+++ config/initializers/timeliness.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+Timeliness.add_formats('time', 'h[.]?')             # Allow 12/24 hour time with no minutes e.g. 14 = 2:00pm, 5 = 5am
+Timeliness.add_formats('time', 'h_nn')              # Allow 12/24 hour format without punctuation or am/pm
+Timeliness.add_formats('time', 'h_nn_ampm')         # Allow 12 hour format without punctuation between hours and minutes
+Timeliness.add_formats('time', 'h[:.]_ampm')        # Allow 12 hour format with am/pm separated by .
+Timeliness.add_formats('time', 'h[:.]?_nn_._ampm')  # Allow hours and minutes separated with : or . and with am/pm separated by .
