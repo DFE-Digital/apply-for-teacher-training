87a33be5c7061ab38c8a27bd2ad1a25481f40b85
davidgisbey <davidgisbey@gmail.com>
2021-10-28 09:19:02 +0100

    Add download link to the monthly statistics view

--- app/controllers/monthly_statistics_controller.rb
+++ app/controllers/monthly_statistics_controller.rb
@@ -1,5 +1,6 @@
 class MonthlyStatisticsController < ApplicationController
   def show
     @statistics = MonthlyStatisticsReport.last.statistics
+    @applications_export = DataExport.where(export_type: 'external_report_applications').order(:created_at).last
   end
 end
