7de23d0494c92d036c5505990665c74164b05454
benswannack <ben.swannack@softwire.com>
2021-10-28 08:45:31 +0100

    Link to timeliness docs for configuring formats

--- config/initializers/timeliness.rb
+++ config/initializers/timeliness.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# Time formats for timeliness are defined here: https://github.com/adzap/timeliness/blob/master/lib/timeliness/definitions.rb
+
 Timeliness.add_formats('time', 'h[.]?')             # Allow 12/24 hour time with no minutes e.g. 14 = 2:00pm, 5 = 5am
 Timeliness.add_formats('time', 'h_nn')              # Allow 12/24 hour format without punctuation or am/pm
 Timeliness.add_formats('time', 'h_nn_ampm')         # Allow 12 hour format without punctuation between hours and minutes
