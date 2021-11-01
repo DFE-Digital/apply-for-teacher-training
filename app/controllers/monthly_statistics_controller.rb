68b97aecc08692d67ca8876d5852c9f2f1f439a2
davidgisbey <davidgisbey@gmail.com>
2021-11-01 16:00:01 +0000

    Add download link on to the latest download of the export

--- app/controllers/monthly_statistics_controller.rb
+++ app/controllers/monthly_statistics_controller.rb
@@ -1,6 +1,7 @@
 class MonthlyStatisticsController < ApplicationController
   def show
     @statistics = MonthlyStatisticsReport.last.statistics
+    @candidates_export = DataExport.where(export_type: 'external_report_candidates').order(:created_at).last
     @applications_export = DataExport.where(export_type: 'external_report_applications').order(:created_at).last
   end
 end
