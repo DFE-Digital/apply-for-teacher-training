33680f031a806ad9cb6f5e55a14168599b55e933
Steve Laing <steve.laing@gmail.com>
2021-11-02 16:30:53 +0000

    Pass recruitment cycle year on to details component
    This allows us to pass the year to the applications query for a given
    sub-reason

--- spec/system/support_interface/reasons_for_rejection_dashboard_spec.rb
+++ spec/system/support_interface/reasons_for_rejection_dashboard_spec.rb
@@ -287,6 +287,7 @@ private
     expect(page).to have_current_path(
       support_interface_reasons_for_rejection_application_choices_path(
         structured_rejection_reasons: { candidate_behaviour_y_n: 'Yes' },
+        recruitment_cycle_year: RecruitmentCycle.current_year,
       ),
     )
     expect(page).to have_content('Showing application choices with rejection reason Something you did')
@@ -328,6 +329,7 @@ private
     expect(page).to have_current_path(
       support_interface_reasons_for_rejection_application_choices_path(
         structured_rejection_reasons: { candidate_behaviour_what_did_the_candidate_do: 'didnt_attend_interview' },
+        recruitment_cycle_year: RecruitmentCycle.current_year,
       ),
     )
 
