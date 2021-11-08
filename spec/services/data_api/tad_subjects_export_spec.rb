c0f1b0c7fd43f8d28b46f4b8b2239d0e8d29ec1a
Steve Hook <steve.hook@gmail.com>
2021-11-08 09:56:05 +0000

    Add specs to cover the apply_1/2 rules
    Only include the original Apply 1 and the latest Apply 2
    applications.

--- spec/services/data_api/tad_subjects_export_spec.rb
+++ spec/services/data_api/tad_subjects_export_spec.rb
@@ -63,6 +63,59 @@ RSpec.describe DataAPI::TADSubjectsExport do
       ])
     end
 
+    it 'returns statistics for apply 1 and latest apply 2 application only' do
+      apply_1_application_form = create_application(
+        nationality: 'French',
+        domicile: 'GB',
+        application_choice_attrs: [
+          { status: :rejected, subjects: ['Mathematics'] },
+          { status: :rejected, subjects: ['Physics'] },
+          { status: :withdrawn, subjects: ['Mathematics'] },
+        ],
+      )
+      first_apply_2_application_form = create_application(
+        nationality: 'French',
+        domicile: 'GB',
+        application_choice_attrs: [
+          { status: :rejected, subjects: ['Mathematics'] },
+        ],
+        phase: 'apply_2',
+        previous_application_form: apply_1_application_form,
+      )
+      create_application(
+        nationality: 'French',
+        domicile: 'GB',
+        application_choice_attrs: [
+          { status: :offer, subjects: ['Mathematics'] },
+        ],
+        phase: 'apply_2',
+        previous_application_form: first_apply_2_application_form,
+      )
+
+      result = described_class.new.data_for_export
+
+      expect(result).to match_array([
+        {
+          subject: 'Mathematics',
+          candidate_domicile: 'UK',
+          candidate_nationality: 'EU',
+          adjusted_applications: 1,
+          adjusted_offers: 1,
+          pending_conditions: 0,
+          recruited: 0,
+        },
+        {
+          subject: 'Physics',
+          candidate_domicile: 'UK',
+          candidate_nationality: 'EU',
+          adjusted_applications: 1,
+          adjusted_offers: 0,
+          pending_conditions: 0,
+          recruited: 0,
+        },
+      ])
+    end
+
     it 'returns statistics for multiple applications' do
       create_application(
         nationality: 'French',
@@ -145,7 +198,9 @@ RSpec.describe DataAPI::TADSubjectsExport do
     def create_application(
       nationality:,
       domicile:,
-      application_choice_attrs:
+      application_choice_attrs:,
+      phase: 'apply_1',
+      previous_application_form: nil
     )
       application_choices = application_choice_attrs.map do |attrs|
         create(
@@ -163,12 +218,15 @@ RSpec.describe DataAPI::TADSubjectsExport do
         )
       end
 
-      create(
-        :completed_application_form,
+      application_form_attrs = {
         first_nationality: nationality,
         country: domicile,
         application_choices: application_choices,
-      )
+        phase: phase,
+        previous_application_form_id: previous_application_form&.id,
+      }
+      application_form_attrs.merge!(candidate_id: previous_application_form&.candidate_id) if previous_application_form
+      create(:completed_application_form, application_form_attrs)
     end
   end
 end
