fff6206f9f5a0d0d789bee6805698879a75ce930
davidgisbey <davidgisbey@gmail.com>
2021-11-08 18:12:36 +0000

    Extract common logic into a helper

---
+++ app/helpers/external_report_export_helper.rb
@@ -0,0 +1,8 @@
+module ExternalReportExportHelper
+  def merge_rows_and_totals(data)
+    column_names = data[:rows].first.drop(1).map(&:first)
+    totals = data[:column_totals]
+
+    data[:rows] + [{ data[:rows].first.keys.first => 'Total' }.merge!(column_names.zip(totals).to_h)]
+  end
+end
--- app/services/support_interface/external_report_exports/candidates_by_status_export.rb
+++ app/services/support_interface/external_report_exports/candidates_by_status_export.rb
@@ -1,22 +1,11 @@
 module SupportInterface
   module ExternalReportExports
     class CandidatesByStatusExport
+      include ExternalReportExportHelper
+
       def data_for_export(*)
         data = MonthlyStatistics::ByStatus.new(by_candidate: true).table_data
         merge_rows_and_totals(data)
-      end
-
-      def merge_rows_and_totals(data)
-        keys = data[:rows].first.reject { |key, _value| key == 'Status' }
-        totals = data[:column_totals]
-
-        data[:rows] + [ { 'Status' => 'Total' }.merge!(merge_totals(keys, totals).to_h)]
-      end
-
-      def merge_totals(hash, totals)
-        hash.map do |key, _value|
-          [key, totals[hash.find_index { |k, _v| k == key }]]
-        end
       end
     end
   end
