074daae68613c775820662c55c3c4d51a9a9d1f2
george-bit <george.holborn@digital.education.gov.uk>
2021-11-12 11:41:06 +0000

    Add spec for data export
    ApplicationsByDemographicDomicileAndDegreeClassExport

---
+++ spec/services/support_interface/applications_by_demographic_domicile_and_degree_class_export_spec.rb
@@ -0,0 +1,115 @@
+require 'rails_helper'
+
+RSpec.describe SupportInterface::ApplicationsByDemographicDomicileAndDegreeClassExport do
+  describe '#call' do
+    before do
+      create(:completed_application_form,
+             date_of_birth: '1999-09-05',
+             country: 'GB',
+             equality_and_diversity: {
+               'sex' => 'male',
+               'hesa_sex' => '1',
+               'disabilities' => ['Learning difficulty', 'Social or communication impairment', 'Blind'],
+               'ethnic_group' => 'Another ethnic group',
+               'hesa_ethnicity' => '50',
+               'ethnic_background' => 'Arab',
+               'hesa_disabilities' => %w[51 53 58],
+             },
+             application_choices: [
+               create(:application_choice, status: :pending_conditions),
+             ],
+             application_qualifications: [
+               create(:application_qualification, level: 'degree', grade: 'Lower second-class honours (2:2)'),
+             ])
+    end
+
+    it 'returns counts for a single application' do
+      result = described_class.new.data_for_export
+
+      expect(result).to match_array([
+        {
+          age_group: 'Under 25',
+          sex: 'Male',
+          ethnicity: '50',
+          disability: 'Two or more impairments and/or disabling medical conditions',
+          degree_class: 'Lower second-class honours (2:2)',
+          domicile: 'UK',
+          adjusted_applications: 0,
+          adjusted_offers: 0,
+          pending_conditions: 1,
+          recruited: 0,
+          total: 1,
+        },
+      ])
+    end
+
+    it 'returns combined and distinct counts for a multiple applications' do
+      create(:completed_application_form,
+             date_of_birth: '1986-09-05',
+             country: 'DE',
+             equality_and_diversity: {
+               'sex' => 'female',
+               'hesa_sex' => '2',
+               'disabilities' => ['Blind'],
+               'ethnic_group' => 'Another ethnic group',
+               'hesa_ethnicity' => '90',
+               'ethnic_background' => 'Not known',
+               'hesa_disabilities' => ['58'],
+             },
+             application_choices: [
+               create(:application_choice, status: :rejected),
+             ],
+             application_qualifications: [
+               create(:application_qualification, level: 'degree', grade: 'Upper second-class honours (2:1)'),
+             ])
+      create(:completed_application_form,
+             date_of_birth: '1986-09-20',
+             country: 'DE',
+             equality_and_diversity: {
+               'sex' => 'female',
+               'hesa_sex' => '2',
+               'disabilities' => ['Blind'],
+               'ethnic_group' => 'Another ethnic group',
+               'hesa_ethnicity' => '90',
+               'ethnic_background' => 'Not known',
+               'hesa_disabilities' => ['58'],
+             },
+             application_choices: [
+               create(:application_choice, status: :recruited),
+             ],
+             application_qualifications: [
+               create(:application_qualification, level: 'degree', grade: 'Upper second-class honours (2:1)'),
+             ])
+      result = described_class.new.data_for_export
+
+      expect(result).to match_array([
+        {
+          age_group: 'Under 25',
+          sex: 'Male',
+          ethnicity: '50',
+          disability: 'Two or more impairments and/or disabling medical conditions',
+          degree_class: 'Lower second-class honours (2:2)',
+          domicile: 'UK',
+          adjusted_applications: 0,
+          adjusted_offers: 0,
+          pending_conditions: 1,
+          recruited: 0,
+          total: 1,
+        },
+        {
+          age_group: '35 to 39',
+          sex: 'Female',
+          ethnicity: '90',
+          disability: '58',
+          degree_class: 'Upper second-class honours (2:1)',
+          domicile: 'EU',
+          adjusted_applications: 1,
+          adjusted_offers: 0,
+          pending_conditions: 0,
+          recruited: 1,
+          total: 2,
+        },
+      ])
+    end
+  end
+end
