3f6485b513687a08a300233bf91b812e034f1b54
Steve Laing <steve.laing@gmail.com>
2021-11-16 12:19:01 +0000

    Rely on FactoryBot list ordering for API results

--- spec/requests/candidate_api/get_candidates_spec.rb
+++ spec/requests/candidate_api/get_candidates_spec.rb
@@ -87,7 +87,6 @@ RSpec.describe 'GET /candidate-api/candidates', type: :request do
       2,
       candidate: candidate,
     )
-    application_forms.second.update(created_at: 1.minute.ago)
 
     get_api_request "/candidate-api/candidates?updated_since=#{CGI.escape((Time.zone.now - 1.day).iso8601)}", token: candidate_api_token
 
@@ -95,16 +94,17 @@ RSpec.describe 'GET /candidate-api/candidates', type: :request do
 
     expect(response_data.size).to eq(2)
 
-    expect(response_data.first['id']).to eq(application_forms.second.id)
-    expect(response_data.first['application_phase']).to eq(application_forms.second.phase)
-    expect(response_data.first['application_status']).to eq(ProcessState.new(application_forms.second).state.to_s)
-    expect(response_data.first['recruitment_cycle_year']).to eq(application_forms.second.recruitment_cycle_year)
-    expect(response_data.first['submitted_at']).to eq(application_forms.second.submitted_at.iso8601)
-    expect(response_data.second['id']).to eq(application_forms.first.id)
-    expect(response_data.second['application_phase']).to eq(application_forms.first.phase)
-    expect(response_data.second['application_status']).to eq(ProcessState.new(application_forms.first).state.to_s)
-    expect(response_data.first['recruitment_cycle_year']).to eq(application_forms.second.recruitment_cycle_year)
-    expect(response_data.first['submitted_at']).to eq(application_forms.second.submitted_at.iso8601)
+    expect(response_data.first['id']).to eq(application_forms.first.id)
+    expect(response_data.first['application_phase']).to eq(application_forms.first.phase)
+    expect(response_data.first['application_status']).to eq(ProcessState.new(application_forms.first).state.to_s)
+    expect(response_data.first['recruitment_cycle_year']).to eq(application_forms.first.recruitment_cycle_year)
+    expect(response_data.first['submitted_at']).to eq(application_forms.first.submitted_at.iso8601)
+
+    expect(response_data.second['id']).to eq(application_forms.second.id)
+    expect(response_data.second['application_phase']).to eq(application_forms.second.phase)
+    expect(response_data.second['application_status']).to eq(ProcessState.new(application_forms.second).state.to_s)
+    expect(response_data.second['recruitment_cycle_year']).to eq(application_forms.second.recruitment_cycle_year)
+    expect(response_data.second['submitted_at']).to eq(application_forms.second.submitted_at.iso8601)
   end
 
   it 'returns the correct page and the default page items' do
