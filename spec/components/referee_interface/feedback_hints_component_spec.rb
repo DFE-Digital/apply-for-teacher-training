cbaac579d55c2a85e4e05a0448d3ee75abebbc22
thatrandomadmin <thatrandomdev@gmail.com>
2021-11-16 15:00:52 +0000

    Changed wording on feedback hints view

--- spec/components/referee_interface/feedback_hints_component_spec.rb
+++ spec/components/referee_interface/feedback_hints_component_spec.rb
@@ -1,23 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe RefereeInterface::FeedbackHintsComponent do
-  context 'when it is an academic reference' do
-    let(:reference) { build_stubbed(:reference, referee_type: :academic) }
+  let(:reference) { build_stubbed(:reference, referee_type: :academic) }
 
-    it 'displays the academic skills bullet point' do
-      result = render_inline(described_class.new(reference: reference))
+  it 'displays the academic skills bullet point' do
+    result = render_inline(described_class.new(reference: reference))
 
-      expect(result.text).to include('academic skills')
-    end
-  end
-
-  context 'when it is non-academic reference' do
-    let(:reference) { build_stubbed(:reference, referee_type: :school_based) }
-
-    it 'does not display that the academic skills bullet point' do
-      result = render_inline(described_class.new(reference: reference))
-
-      expect(result.text).not_to include('academic skills')
-    end
+    expect(result.text).to include('academic skills')
   end
 end
