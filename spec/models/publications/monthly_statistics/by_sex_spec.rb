f387c9cfecfcab191dfcce1a0ada5084d213cc5a
Steve Hook <steve.hook@gmail.com>
2021-11-25 11:52:40 +0000

    Refactor specs to cover multiple choices per form

--- spec/models/publications/monthly_statistics/by_sex_spec.rb
+++ spec/models/publications/monthly_statistics/by_sex_spec.rb
@@ -5,17 +5,54 @@ RSpec.describe Publications::MonthlyStatistics::BySex do
 
   it "returns table data for 'by course age group'" do
     5.times do
-      create_application_choice(status: :with_rejection, sex: 'female')
-      create_application_choice(status: :awaiting_provider_decision, sex: 'Prefer not to say')
-      create_application_choice(status: :with_recruited, sex: 'Prefer not to say')
-      create_application_choice(status: :with_offer, sex: 'intersex')
-      create_application_choice(status: :with_deferred_offer, sex: 'male', status_before_deferral: 'offer', recruitment_cycle_year: RecruitmentCycle.previous_year)
-      create_application_choice(status: :with_deferred_offer, sex: 'female', status_before_deferral: 'offer', recruitment_cycle_year: RecruitmentCycle.current_year)
-      create_application_choice(status: :with_conditions_not_met, sex: 'intersex')
-      create_application_choice(status: :with_offer, sex: 'female')
-      create_application_choice(status: :with_withdrawn_offer, sex: 'female')
-      create_application_choice(status: :withdrawn, sex: 'male')
-      create_application_choice_with_previous_application(status: :with_rejection, sex: 'male')
+      create_application_choice(
+        statuses: %i[with_rejection with_rejection],
+        sex: 'female',
+      )
+      create_application_choice(
+        statuses: %i[awaiting_provider_decision],
+        sex: 'Prefer not to say',
+      )
+      create_application_choice(
+        statuses: %i[awaiting_provider_decision with_rejection with_recruited],
+        sex: 'Prefer not to say',
+      )
+      create_application_choice(
+        statuses: %i[awaiting_provider_decision with_rejection with_offer],
+        sex: 'intersex',
+      )
+      create_application_choice(
+        statuses: %i[with_deferred_offer],
+        sex: 'male',
+        status_before_deferral: 'offer',
+        recruitment_cycle_year: RecruitmentCycle.previous_year,
+      )
+      create_application_choice(
+        statuses: %i[with_deferred_offer],
+        sex: 'female',
+        status_before_deferral: 'offer',
+        recruitment_cycle_year: RecruitmentCycle.current_year,
+      )
+      create_application_choice(
+        statuses: %i[with_conditions_not_met],
+        sex: 'intersex',
+      )
+      create_application_choice(
+        statuses: %i[with_offer],
+        sex: 'female',
+      )
+      create_application_choice(
+        statuses: %i[with_withdrawn_offer],
+        sex: 'female',
+      )
+      create_application_choice(
+        statuses: %i[withdrawn],
+        sex: 'male',
+      )
+      create_application_choice_with_previous_application(
+        status: :with_rejection,
+        sex: 'male',
+      )
     end
 
     expect(statistics).to eq(
@@ -63,23 +100,28 @@ RSpec.describe Publications::MonthlyStatistics::BySex do
   end
 
   def create_application_choice(
-    status:,
+    statuses:,
     sex:,
     recruitment_cycle_year: RecruitmentCycle.current_year,
     previous_application_form: nil,
     status_before_deferral: nil
   )
-    create(
-      :application_choice,
-      status,
-      status_before_deferral: status_before_deferral,
-      application_form: create(
-        :application_form,
-        previous_application_form: previous_application_form,
-        recruitment_cycle_year: recruitment_cycle_year,
-        equality_and_diversity: { 'sex' => sex },
-      ),
+    application_form = create(
+      :application_form,
+      previous_application_form: previous_application_form,
+      recruitment_cycle_year: recruitment_cycle_year,
+      equality_and_diversity: { 'sex' => sex },
     )
+    statuses.each do |status|
+      create(
+        :application_choice,
+        status,
+        application_form: application_form,
+        status_before_deferral: status_before_deferral,
+      )
+    end
+
+    application_form
   end
 
   def create_application_choice_with_previous_application(
@@ -87,16 +129,16 @@ RSpec.describe Publications::MonthlyStatistics::BySex do
     sex:,
     recruitment_cycle_year: RecruitmentCycle.current_year
   )
-    previous_application_choice = create_application_choice(
-      status: status,
+    previous_application_form = create_application_choice(
+      statuses: [status],
       sex: sex,
       recruitment_cycle_year: recruitment_cycle_year,
     )
     create_application_choice(
-      status: status,
+      statuses: [status],
       sex: sex,
       recruitment_cycle_year: recruitment_cycle_year,
-      previous_application_form: previous_application_choice.application_form,
+      previous_application_form: previous_application_form,
     )
   end
 end
