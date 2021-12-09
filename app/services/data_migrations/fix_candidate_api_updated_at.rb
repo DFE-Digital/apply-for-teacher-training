845b370a3d088a796ff6c503da404850d29171c8
Steve Hook <steve.hook@gmail.com>
2021-12-09 09:24:44 +0000

    Add migration to fix Candidate#candidate_api_updated_at

---
+++ app/services/data_migrations/fix_candidate_api_updated_at.rb
@@ -0,0 +1,28 @@
+module DataMigrations
+  class FixCandidateAPIUpdatedAt
+    TIMESTAMP = 20211208172459
+    MANUAL_RUN = false
+
+    # Ensure that all the candidates have a `candidate_api_updated_at` that is
+    # no earlier than the `created_at` of the most recent application form.
+    def change
+      candidates = Candidate
+        .joins('inner join (
+            select candidates.id id,
+              max(application_forms.created_at) max_form_created_at
+            from candidates
+            inner join application_forms on application_forms.candidate_id = candidates.id
+            group by candidates.id
+          ) candidates_with_latest_application_form on candidates_with_latest_application_form.id = candidates.id'
+        )
+        .select('candidates.*, candidates_with_latest_application_form.max_form_created_at')
+        .where('candidate_api_updated_at < max_form_created_at')
+
+      candidates.find_in_batches(batch_size: 10) do |batch|
+        batch.each do |candidate|
+          candidate.update!(candidate_api_updated_at: candidate.max_form_created_at)
+        end
+      end
+    end
+  end
+end
---
+++ spec/services/data_migrations/fix_candidate_api_updated_at_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+RSpec.describe DataMigrations::FixCandidateAPIUpdatedAt do
+  around do |example|
+    Timecop.freeze { example.run }
+  end
+
+  it 'does not change valid `candidate_api_updated_at` values' do
+    valid_candidate = create(
+      :candidate,
+      candidate_api_updated_at: 5.days.ago,
+    )
+    create(
+      :application_form,
+      candidate: valid_candidate,
+      created_at: 3.weeks.ago,
+    )
+    create(
+      :application_form,
+      candidate: valid_candidate,
+      created_at: 2.weeks.ago,
+    )
+    valid_candidate.update(candidate_api_updated_at: 5.days.ago)
+
+    expect { described_class.new.change }.not_to(
+      change { valid_candidate.reload.candidate_api_updated_at },
+    )
+  end
+
+  it 'does not change `candidate_api_updated_at` values for candidates with no applications' do
+    candidate_with_no_applications = create(:candidate)
+    candidate_with_no_applications.update(candidate_api_updated_at: 5.days.ago)
+
+    expect { described_class.new.change }.not_to(
+      change { candidate_with_no_applications.reload.candidate_api_updated_at },
+    )
+  end
+
+  it 'changes invalid `candidate_api_updated_at` values' do
+    invalid_candidate = create(
+      :candidate,
+    )
+    create(
+      :application_form,
+      candidate: invalid_candidate,
+      created_at: 3.weeks.ago,
+    )
+    create(
+      :application_form,
+      candidate: invalid_candidate,
+      created_at: 2.weeks.ago,
+    )
+    invalid_candidate.update(candidate_api_updated_at: 5.weeks.ago)
+
+    expect { described_class.new.change }.to(
+      change { invalid_candidate.reload.candidate_api_updated_at }.to(2.weeks.ago),
+    )
+  end
+end
