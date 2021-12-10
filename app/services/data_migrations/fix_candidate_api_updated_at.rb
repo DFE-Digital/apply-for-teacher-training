903befad92a55c8fa84abe0df0e035df464244cd
Steve Hook <steve.hook@gmail.com>
2021-12-10 16:16:13 +0000

    Update query to include earliest update logic
    Also removed `application_forms.id` from the `group by` of the joined
    psuedo tables because it joins to a row per application rather than per
    candidate (so doesn't find the max or min across all applications
    per candidate).

--- app/services/data_migrations/fix_candidate_api_updated_at.rb
+++ app/services/data_migrations/fix_candidate_api_updated_at.rb
@@ -16,14 +16,33 @@ module DataMigrations
             group by candidates.id
           ) candidates_with_latest_application_form on candidates_with_latest_application_form.id = candidates.id',
         )
-        .select('candidates.*, candidates_with_latest_application_form.max_form_created_at')
-        .where('candidate_api_updated_at < max_form_created_at')
+        .joins(
+          "left outer join (
+            select application_forms.candidate_id candidate_id,
+              min(audits.created_at) earliest_update
+            from audits
+            inner join application_forms on application_forms.id = audits.auditable_id
+            WHERE audits.action = 'update'
+            AND audits.auditable_type = 'ApplicationForm'
+            AND audits.user_id IS NOT NULL
+            group by application_forms.candidate_id
+          ) forms_with_earliest_audit ON forms_with_earliest_audit.candidate_id = candidates.id",
+        )
+        .select('candidates.*, candidates_with_latest_application_form.max_form_created_at, forms_with_earliest_audit.earliest_update')
+        .where('candidate_api_updated_at < max_form_created_at OR candidate_api_updated_at < forms_with_earliest_audit.earliest_update')
+        .where("created_at > '2021-01-01'")
 
       candidates.find_in_batches(batch_size: 10) do |batch|
         batch.each do |candidate|
-          candidate.update!(candidate_api_updated_at: candidate.max_form_created_at)
+          candidate.update!(candidate_api_updated_at: calculate_candidate_api_updated_at(candidate))
         end
       end
+    end
+
+    private
+
+    def calculate_candidate_api_updated_at(candidate)
+      candidate.max_form_created_at
     end
   end
 end
