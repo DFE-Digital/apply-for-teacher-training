cade32c575c936c492d3d426452f3451fb9200b0
Steve Laing <steve.laing@gmail.com>
2021-12-13 14:22:00 +0000

    Reduce test coverage to prove diff

--- spec/models/application_choice_spec.rb
+++ spec/models/application_choice_spec.rb
@@ -1,28 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationChoice, type: :model do
-  describe 'auditing', with_audited: true do
-    it 'creates audit entries' do
-      application_choice = create :application_choice, status: 'unsubmitted'
-      expect(application_choice.audits.count).to eq 1
-      application_choice.update!(status: 'awaiting_provider_decision')
-      expect(application_choice.audits.count).to eq 2
-    end
-
-    it 'creates an associated object in each audit record' do
-      application_choice = create :application_choice
-      expect(application_choice.audits.last.associated).to eq application_choice.application_form
-    end
-
-    it 'audit record can be attributed to a candidate' do
-      candidate = create :candidate
-      application_choice = Audited.audit_class.as_user(candidate) do
-        create :application_choice
-      end
-      expect(application_choice.audits.last.user).to eq candidate
-    end
-  end
-
   describe '.decision_pending' do
     it 'returns nothing when there are no awaiting_provider_decision or interviewing application choices' do
       create(:application_choice, :with_offer)
