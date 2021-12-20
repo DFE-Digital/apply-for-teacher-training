e4dd19daa3e93a2fb11059e449f508cfff97c0be
Despo Pentara <despo@extractmethod.com>
2021-12-20 19:39:31 +0000

    Refactor vendor api route validator

--- config/initializers/route_validators.rb
+++ config/initializers/route_validators.rb
@@ -5,20 +5,42 @@ class ValidRecruitmentCycleYear
 end
 
 class ValidVendorApiRoute
-  extend VersioningHelpers
-
   def self.matches?(request)
-    api_version = request.params[:api_version]
-    controller_class = request.controller_class
-    action = request.params[:action]
-
-    version = extract_version(api_version)
-
-    VendorAPI::VERSIONS[version_number(version)].each do |change_module|
-      return true if change_module.new.actions[controller_class]&.include?(action.to_sym)
-    end
-    false
+    VersionMatcher.new(request).match?
   rescue ArgumentError, NoMethodError
     false
+  end
+
+  class VersionMatcher
+    include VersioningHelpers
+
+    attr_reader :request
+
+    def initialize(request)
+      @request = request
+    end
+
+    def match?
+      VendorAPI::VERSIONS[version_number(version)].each do |change_class|
+        return true if change_class.new.actions[controller_class]&.include?(action.to_sym)
+      end
+      false
+    end
+
+  private
+
+    delegate :controller_class, to: :request
+
+    def api_version
+      request.params[:api_version]
+    end
+
+    def action
+      request.params[:action]
+    end
+
+    def version
+      extract_version(api_version)
+    end
   end
 end
