283ac03ea42075d6c167ceedd11575d8ea2b398d
Steve Laing <steve.laing@gmail.com>
2021-12-22 16:46:40 +0000

    Merge minor versions into base major version
    Given the version number 1.2 we build the OpenAPI spec up from version
    1.0 incrementing through the minor versions and including any
    experimental definitions if they exist.

--- app/lib/vendor_api_specification.rb
+++ app/lib/vendor_api_specification.rb
@@ -1,6 +1,10 @@
 class VendorAPISpecification
+  include VersioningHelpers
+
   CURRENT_VERSION = '1.0'.freeze
-  DRAFT_VERSION = '1.1'.freeze
+  DRAFT_VERSION = '1.2'.freeze
+
+  YAML_FILE_PATH = 'config/vendor_api'.freeze
 
   def initialize(version: nil)
     @version = version || CURRENT_VERSION
@@ -15,8 +19,22 @@ class VendorAPISpecification
   end
 
   def spec
-    YAML
-      .load_file("config/vendor_api/v#{@version}.yml")
-      .deep_merge(YAML.load_file("config/vendor_api/experimental-v#{@version}.yml"))
+    (0..minor_version_number(@version)).inject({}) do |versions, minor_version|
+      version_to_load = "#{major_version_number(@version)}.#{minor_version}"
+      yaml_file_for_version = "#{YAML_FILE_PATH}/v#{version_to_load}.yml"
+
+      if File.exist?(yaml_file_for_version)
+        yaml_for_version = YAML.load_file(yaml_file_for_version)
+        experimental_yaml_file_for_version = "#{YAML_FILE_PATH}/experimental-v#{version_to_load}.yml"
+
+        if File.exist?(experimental_yaml_file_for_version)
+          yaml_for_version = yaml_for_version.deep_merge(YAML.load_file(experimental_yaml_file_for_version))
+        end
+
+        versions = versions.deep_merge(yaml_for_version)
+      end
+
+      versions
+    end
   end
 end
