bf7bdf4e11c9e06dd15f5f7248ba251789888b9e
Despo Pentara <despo@extractmethod.com>
2021-12-22 14:53:48 +0000

    Introduces provider permissions_updated emails

--- spec/mailers/provider_mailer_spec.rb
+++ spec/mailers/provider_mailer_spec.rb
@@ -278,6 +278,64 @@ RSpec.describe ProviderMailer, type: :mailer do
     )
   end
 
+  describe 'permissions_updated' do
+    let(:provider) { FactoryBot.create(:provider, name: 'Hogwards University') }
+    let(:permissions_updated_by_user) { FactoryBot.create(:provider_user, first_name: 'Jane', last_name: 'Doe') }
+    let(:provider_user) { FactoryBot.create(:provider_user, first_name: 'Princess', last_name: 'Fiona', providers: [provider]) }
+    let(:permissions) { %i[view_safeguarding_information view_diversity_information] }
+
+    let(:email) do
+      described_class.permissions_updated(provider_user, provider, permissions, permissions_updated_by_user)
+    end
+
+    it_behaves_like(
+      'a mail with subject and content',
+      'Jane Doe updated your permissions for Hogwards University - manage teacher training applications',
+      'salutation' => 'Dear Princess Fiona',
+      'heading' => 'Jane Doe updated your permissions for Hogwards University.',
+      'view safeguarding' => 'view criminal convictions and professional misconduct',
+      'view diversity' => 'view sex, disability and ethnicity information',
+    )
+  end
+
+  describe 'permissions_updated with all permissions removed' do
+    let(:provider) { FactoryBot.create(:provider, name: 'Hogwards University') }
+    let(:permissions_updated_by_user) { FactoryBot.create(:provider_user, first_name: 'Jane', last_name: 'Doe') }
+    let(:provider_user) { FactoryBot.create(:provider_user, first_name: 'Princess', last_name: 'Fiona', providers: [provider]) }
+    let(:permissions) { %i[] }
+
+    let(:email) do
+      described_class.permissions_updated(provider_user, provider, permissions, permissions_updated_by_user)
+    end
+
+    it_behaves_like(
+      'a mail with subject and content',
+      'Jane Doe updated your permissions for Hogwards University - manage teacher training applications',
+      'salutation' => 'Dear Princess Fiona',
+      'heading' => 'Jane Doe updated your permissions for Hogwards University.',
+      'permissiosn' => 'You only have permission to view applications.',
+    )
+  end
+
+  describe 'permissions_updated_by_support' do
+    let(:provider) { FactoryBot.create(:provider, name: 'Hogwards University') }
+    let(:provider_user) { FactoryBot.create(:provider_user, first_name: 'Princess', last_name: 'Fiona', providers: [provider]) }
+    let(:permissions) { %i[make_decisions view_safeguarding_information] }
+
+    let(:email) do
+      described_class.permissions_updated(provider_user, provider, permissions, nil)
+    end
+
+    it_behaves_like(
+      'a mail with subject and content',
+      'Your permissions have been updated for Hogwards University - manage teacher training applications',
+      'salutation' => 'Dear Princess Fiona',
+      'heading' => 'Your permissions have been updated for Hogwards University.',
+      'make decisions' => 'make offers and reject application',
+      'view safeguarding' => 'view criminal convictions and professional misconduct',
+    )
+  end
+
   describe 'permissions_removed' do
     let(:provider) { FactoryBot.create(:provider, name: 'Hogwards University') }
     let(:permissions_removed_by_user) { FactoryBot.create(:provider_user, first_name: 'Jane', last_name: 'Doe') }
