e99341332658fb575cbc5f7c7c81bc3a63215c22
James Glenn <47917431+JR-G@users.noreply.github.com>
2022-01-04 13:54:26 +0000

    Update deferred offers confirmation header

--- app/helpers/task_view_helper.rb
+++ app/helpers/task_view_helper.rb
@@ -1,7 +1,7 @@
 module TaskViewHelper
   def task_view_header(choice)
     case choice&.task_view_group
-    when 1 then 'Deferred offers: review and confirm'
+    when 1 then 'Confirm deferred offers'
     when 2 then 'Deadline approaching: make decision about application'
     when 3 then 'Give feedback: you did not make a decision in time'
     when 4 then 'Ready for review'
