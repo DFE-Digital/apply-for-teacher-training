d9e9a69c73e3cd31e3d19d86362bfd40247b24ef
Duncan Brown <duncanjbrown@gmail.com>
2022-01-05 17:23:28 +0000

    Tidy up MonthlyStatisticsController

--- app/controllers/publications/monthly_statistics_controller.rb
+++ app/controllers/publications/monthly_statistics_controller.rb
@@ -12,9 +12,8 @@ module Publications
                        MonthlyStatisticsTimetable.report_for_current_period,
                      )
                    end
-      @monthly_statistics_report = MonthlyStatisticsTimetable.report_for_current_period
-      @statistics = @monthly_statistics_report.statistics
-      @report = calculate_download_size
+
+      @csv_export_types_and_sizes = calculate_download_sizes(@presenter)
       @academic_year_name = RecruitmentCycle.cycle_name(CycleTimetable.next_year)
       @current_cycle_name = RecruitmentCycle.verbose_cycle_name
     end
@@ -38,8 +37,8 @@ module Publications
       params[:date] == '2021-11'
     end
 
-    def calculate_download_size
-      @monthly_statistics_report.statistics.map do |k, raw_data|
+    def calculate_download_sizes(report)
+      report.statistics.map do |k, raw_data|
         header_row = raw_data['rows'].first.keys
         data = SafeCSV.generate(raw_data['rows'].map(&:values), header_row)
         [k, data.size]
