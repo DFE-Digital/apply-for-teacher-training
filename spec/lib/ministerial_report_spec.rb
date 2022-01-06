f14b38d3b96561790799c7e111911fdc6befe2ac
Duncan Brown <duncanjbrown@gmail.com>
2022-01-06 20:11:48 +0000

    Move testing of dominant subject onto MinisterialReport
    This is app code, not initialization — we can probably find a better
    place for it to live than lib/ but it's ok for the time being. The
    ApplicationExport spec is now completely focussed on the data being
    output.

---
+++ spec/lib/ministerial_report_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe MinisterialReport do
+  describe '.determine_dominant_course_subject_for_report' do
+    let(:course_level) { 'secondary' }
+
+    subject(:dominant_subject) do
+      described_class.determine_dominant_course_subject_for_report(course_name,
+                                                                   course_level,
+                                                                   subject_names_and_codes)
+    end
+
+    context 'when the course name contains two words' do
+      let(:course_name) { 'Business studies with History' }
+      let(:subject_names_and_codes) { { 'Business studies' => '08', 'History' => 'V1' } }
+
+      it { is_expected.to eq(:business_studies) }
+    end
+
+    context 'when the associated subjects are in brackets' do
+      let(:course_name) { 'Modern Languages (French with Spanish)' }
+      let(:subject_names_and_codes) { { 'Spanish' => '22', 'French' => '15' } }
+
+      it { is_expected.to eq(:modern_foreign_languages) }
+    end
+
+    context 'when the course name contains no information' do
+      let(:course_name) { 'Nonsense course' }
+      let(:subject_names_and_codes) { { 'History' => 'V1', 'Business studies' => '08' } }
+
+      it { is_expected.to eq(:history) }
+    end
+
+    context 'when the course is titled PE and PE is not the first subject' do
+      let(:course_name) { 'PE with EBacc' }
+      let(:subject_names_and_codes) { { 'Biology' => 'C1', 'Physical education' => 'C6' } }
+
+      it { is_expected.to eq(:physical_education) }
+    end
+  end
+end
