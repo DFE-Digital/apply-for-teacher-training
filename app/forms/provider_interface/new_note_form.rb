8249725003e8cb4c408c2cc2d48273133d7b7d32
Steve Laing <steve.laing@gmail.com>
2022-01-11 15:09:53 +0000

    Amend NewNoteForm to accept and populate User association
    We continue to populate provider_user_id until it's safe to remove this column and db index in another PR.

--- app/forms/provider_interface/new_note_form.rb
+++ app/forms/provider_interface/new_note_form.rb
@@ -1,16 +1,17 @@
 module ProviderInterface
   class NewNoteForm
     include ActiveModel::Model
-    attr_accessor :application_choice, :message, :provider_user, :referer
+    attr_accessor :application_choice, :message, :referer, :user
 
-    validates :application_choice, :provider_user, presence: true
+    validates :application_choice, :user, presence: true
     validates :message, length: { maximum: 500 }, presence: true
 
     def save
       if valid?
         Note.new(
           application_choice: application_choice,
-          provider_user: provider_user,
+          provider_user: user,
+          user: user,
           message: message,
         ).save
       end
