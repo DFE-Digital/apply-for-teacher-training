51484ef464ffbc253efb1f3e279753db7597a7f6
Steve Laing <steve.laing@gmail.com>
2022-01-11 15:08:21 +0000

    Add polymorphic User association with Notes model

--- spec/factories/note.rb
+++ spec/factories/note.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :note do
     application_choice
     provider_user
+    user { create(:provider_user) }
 
     message { Faker::Quote.most_interesting_man_in_the_world }
   end
