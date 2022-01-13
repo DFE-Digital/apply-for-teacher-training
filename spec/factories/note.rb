620e3e42ccbb9be4b7b4ce5ead5a5bb8feeb571a
Steve Laing <steve.laing@gmail.com>
2022-01-13 16:13:03 +0000

    Randomly associate different user types to notes

--- spec/factories/note.rb
+++ spec/factories/note.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :note do
     application_choice
     provider_user
-    user { create(:provider_user) }
+    user { [0, 1].sample.zero? ? create(:vendor_api_user) : create(:provider_user) }
 
     message { Faker::Quote.most_interesting_man_in_the_world }
   end
