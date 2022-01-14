0c91061c90a0155c485db359591ee2631d5fe37f
Despo Pentara <despo@extractmethod.com>
2022-01-14 13:24:11 +0000

    Update header component homepage_url to be configurable

--- spec/components/utility/header_component_spec.rb
+++ spec/components/utility/header_component_spec.rb
@@ -2,9 +2,28 @@ require 'rails_helper'
 
 RSpec.describe HeaderComponent do
   let(:navigation_items) { [] }
+  let(:homepage_url) { nil }
 
   subject(:rendered_component) do
-    render_inline(described_class.new(navigation_items: navigation_items, service_name: '', service_link: '#'))
+    render_inline(described_class.new(navigation_items: navigation_items, homepage_url: homepage_url, service_name: '', service_link: '#'))
+  end
+
+  describe 'rendering the homepage_url' do
+    let(:header_link_xpath) { './/a[@class="govuk-header__link govuk-header__link--homepage"]/@href' }
+
+    context 'when no homepage_url is not set' do
+      it 'defauls to the govuk website' do
+        expect(rendered_component.xpath(header_link_xpath).first.value).to eq('https://www.gov.uk')
+      end
+    end
+
+    context 'when no homepage_url is set' do
+      let(:homepage_url) { '/provider' }
+
+      it 'links to the specified url' do
+        expect(rendered_component.xpath(header_link_xpath).first.value).to eq(homepage_url)
+      end
+    end
   end
 
   describe 'rendering NavigationItems' do
