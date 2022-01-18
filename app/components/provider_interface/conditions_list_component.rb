12da2fc32d9a8259e649b65c508ca87c841e2a7d
Samuel Culley <samuel.culley@digital.education.gov.uk>
2022-01-18 14:45:28 +0000

    Add standardised sorting to conditions list component

--- app/components/provider_interface/conditions_list_component.rb
+++ app/components/provider_interface/conditions_list_component.rb
@@ -3,7 +3,13 @@ module ProviderInterface
     attr_reader :conditions
 
     def initialize(conditions)
-      @conditions = conditions
+      @conditions = sorted_conditions(conditions)
+    end
+
+  private
+
+    def sorted_conditions(conditions)
+      conditions.sort_by { |c| [OfferCondition::STANDARD_CONDITIONS.index(c.text) || Float::INFINITY, c.created_at] }
     end
   end
 end
