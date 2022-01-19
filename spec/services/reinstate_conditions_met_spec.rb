806630fef89250462ad7b7e2d5e0e42e3d7d7d07
Despo Pentara <despo@extractmethod.com>
2022-01-19 08:59:02 +0000

    Refactor reinstate offer service and introduce a validation service to be used for deferred offer confirmations
    Raise exceptions when using `save!` so that a similar pattern to the one
    used in the vendor_api/decisions_controller can be used to handle and
    display errors.
    
    Maintain existing `save` method for usage by provider controllers

--- spec/services/reinstate_conditions_met_spec.rb
+++ spec/services/reinstate_conditions_met_spec.rb
@@ -1,16 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe ReinstateConditionsMet do
+  subject(:service) do
+    described_class.new(actor: provider_user,
+                        application_choice: application_choice,
+                        course_option: new_course_option)
+  end
+
   let(:provider_user) { create(:provider_user, :with_provider, :with_make_decisions) }
   let(:provider) { provider_user.providers.first }
   let(:original_course) { create(:course, :open_on_apply, :previous_year_but_still_available, provider: provider) }
   let(:previous_course_option) { create(:course_option, course: original_course) }
   let(:new_course_option) { create(:course_option, course: original_course.in_next_cycle) }
   let(:application_choice) { create(:application_choice, :with_deferred_offer, course_option: previous_course_option) }
-
-  def service
-    described_class.new(actor: provider_user, application_choice: application_choice, course_option: new_course_option)
-  end
 
   it 'changes application status to \'recruited\'' do
     expect { service.save }.to change(application_choice, :status).to('recruited')
@@ -27,10 +29,8 @@ RSpec.describe ReinstateConditionsMet do
   end
 
   it 'does not modify recruited_at if conditions had already been met before' do
-    application_choice.update(
-      status_before_deferral: 'recruited',
-      recruited_at: application_choice.accepted_at + 7.days,
-    )
+    application_choice.update(status_before_deferral: 'recruited',
+                              recruited_at: application_choice.accepted_at + 7.days)
 
     Timecop.freeze do
       expect { service.save }.not_to change(application_choice, :recruited_at)
@@ -63,30 +63,64 @@ RSpec.describe ReinstateConditionsMet do
     end
   end
 
-  describe 'course option validation' do
-    it 'checks the course option is present' do
-      reinstate = described_class.new(actor: provider_user, application_choice: application_choice, course_option: nil)
+  describe 'validations' do
+    context 'checks the course option is present' do
+      let(:new_course_option) { nil }
 
-      expect(reinstate).not_to be_valid
+      it 'save! raises a ValidationException' do
+        expect { service.save! }
+          .to raise_error(ValidationException, 'Please provide a course_option or course_option_id')
+      end
 
-      expect(reinstate.errors[:course_option]).to include('could not be found')
+      it 'save returns false' do
+        expect(service.save).to be false
+      end
     end
 
-    it 'checks the course is open on apply' do
-      new_course_option = create(:course_option, course: create(:course, provider: provider, open_on_apply: false))
-      reinstate = described_class.new(actor: provider_user, application_choice: application_choice, course_option: new_course_option)
+    context 'checks the course is open on apply' do
+      let(:new_course_option) do
+        create(:course_option,
+               course: create(:course, provider: provider, open_on_apply: false))
+      end
 
-      expect(reinstate).not_to be_valid
+      it 'save! raises a ValidationException' do
+        expect { service.save! }
+          .to raise_error(ValidationException, 'The requested course is not open for applications via the Apply service')
+      end
 
-      expect(reinstate.errors[:course_option]).to include('is not open for applications via the Apply service')
+      it 'save returns false' do
+        expect(service.save).to be false
+      end
     end
 
-    it 'checks course option matches the current RecruitmentCycle' do
-      reinstate = described_class.new(actor: provider_user, application_choice: application_choice, course_option: previous_course_option)
+    context 'checks course option matches the current RecruitmentCycle' do
+      let(:new_course_option) { previous_course_option }
 
-      expect(reinstate).not_to be_valid
+      it 'save! raises a ValidationException' do
+        expect { service.save! }
+          .to raise_error(ValidationException, 'The requested course does not exist in the current cycle')
+      end
 
-      expect(reinstate.errors[:course_option]).to include('does not belong to the current cycle')
+      it 'save returns false' do
+        expect(service.save).to be false
+      end
+    end
+
+    context 'when the application is not in a state allowing to reinstate conditions met' do
+      let(:application_choice) do
+        create(:application_choice, :awaiting_provider_decision,
+               course_option: previous_course_option)
+      end
+
+      it 'save! raises a Workflow::NoTransitionAllowed error' do
+        expect { service.save!  }
+          .to raise_error(Workflow::NoTransitionAllowed,
+                          'There is no event reinstate_conditions_met defined for the awaiting_provider_decision state')
+      end
+
+      it 'save returns false' do
+        expect(service.save).to be false
+      end
     end
   end
 
