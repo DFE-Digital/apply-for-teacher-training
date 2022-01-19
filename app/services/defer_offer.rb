d44c47e6a1214a31566af43c1a700556b4afb6e8
Despo Pentara <despo@extractmethod.com>
2022-01-19 02:01:49 +0000

    Minor refactoring to DeferOffer service

--- app/services/defer_offer.rb
+++ app/services/defer_offer.rb
@@ -2,27 +2,36 @@ class DeferOffer
   include ActiveModel::Model
   include ImpersonationAuditHelper
 
+  attr_reader :actor, :application_choice, :course_option
+
   def initialize(actor:, application_choice:)
-    @auth = ProviderAuthorisation.new(actor: actor)
+    @actor = actor
     @application_choice = application_choice
+    @course_option = application_choice.current_course_option
   end
 
   def save!
-    @auth.assert_can_make_decisions!(application_choice: @application_choice, course_option_id: @application_choice.current_course_option.id)
+    auth.assert_can_make_decisions!(application_choice: application_choice, course_option: course_option)
 
-    audit(@auth.actor) do
-      prior_status = @application_choice.status
+    audit(actor) do
+      previous_status = application_choice.status
 
       ActiveRecord::Base.transaction do
-        ApplicationStateChange.new(@application_choice).defer_offer!
-        @application_choice.update!(
-          status_before_deferral: prior_status,
+        ApplicationStateChange.new(application_choice).defer_offer!
+        application_choice.update!(
+          status_before_deferral: previous_status,
           offer_deferred_at: Time.zone.now,
         )
       end
 
-      CandidateMailer.deferred_offer(@application_choice).deliver_later
-      StateChangeNotifier.call(:defer_offer, application_choice: @application_choice)
+      CandidateMailer.deferred_offer(application_choice).deliver_later
+      StateChangeNotifier.call(:defer_offer, application_choice: application_choice)
     end
+  end
+
+private
+
+  def auth
+    @auth ||= ProviderAuthorisation.new(actor: actor)
   end
 end
