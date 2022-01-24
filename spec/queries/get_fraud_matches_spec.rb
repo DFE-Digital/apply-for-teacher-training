1a0bb55e095cb5aaf7b1241840c367b605e4e1d0
Tomas D'Stefano <tomas_stefano@successoft.com>
2022-01-24 08:20:18 -0300

    Omit arguments in get fraud matches spec
    The zero hour is not needed

--- spec/queries/get_fraud_matches_spec.rb
+++ spec/queries/get_fraud_matches_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe GetFraudMatches do
 
     context 'matches two identical names in identical casing' do
       before do
-        Timecop.freeze(Time.zone.local(2020, 8, 23, 12, 0o0, 0o0)) do
+        Timecop.freeze(Time.zone.local(2020, 8, 23, 12)) do
           create(:application_form, candidate: candidate1, first_name: 'Jeffrey', last_name: 'Thompson', date_of_birth: '1998-08-08', postcode: 'w6 9bh ', submitted_at: Time.zone.now)
           create(:application_form, candidate: candidate2, first_name: 'Joffrey', last_name: 'Thompson', date_of_birth: '1998-08-08', postcode: 'w6 9bh ', submitted_at: Time.zone.now)
         end
@@ -80,7 +80,7 @@ RSpec.describe GetFraudMatches do
       let(:candidate_ids) { returned_array_of_hashes.map { |element| element['candidate_id'] } }
 
       before do
-        Timecop.freeze(Time.zone.local(2020, 8, 23, 12, 0o0, 0o0)) do
+        Timecop.freeze(Time.zone.local(2020, 8, 23, 12)) do
           create(:application_form, candidate: candidate1, first_name: 'Jeffrey', last_name: 'Thompson', date_of_birth: '1998-08-08', postcode: 'w6 9bh ', submitted_at: nil)
           create(:application_form, candidate: candidate2, first_name: 'Joffrey', last_name: 'Thompson', date_of_birth: '1998-08-08', postcode: 'w6 9bh ', submitted_at: nil)
         end
