b63f7d1150bf799e6796c7351a462bf91988bafc
Tomas D'Stefano <tomas_stefano@successoft.com>
2022-01-25 13:49:56 -0300

    Unblock submissions if they weren't blocked deliberately
    Because of the new duplicate matching issues we had to turn off the
    feature flag and endup having many candidates with submission blocked
    that should be unblocked.

---
+++ spec/services/data_migrations/fixup_candidates_submission_blocked_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe DataMigrations::FixupCandidatesSubmissionBlocked do
+  before do
+    @candidate_one = create(:candidate, submission_blocked: true)
+    @candidate_two = create(:candidate, submission_blocked: true)
+
+    @fraud_match_one = create(:fraud_match, candidates: [@candidate_one, @candidate_two], blocked: true)
+
+    @candidate_third = create(:candidate, submission_blocked: true)
+    @candidate_fourth = create(:candidate, submission_blocked: true)
+
+    @fraud_match_two = create(:fraud_match, candidates: [@candidate_third, @candidate_fourth], blocked: false)
+  end
+
+  it 'updates blocked submissions as false if they were not blocked deliberately' do
+    described_class.new.change
+    expect(@candidate_one.reload).to be_submission_blocked
+    expect(@candidate_two.reload).to be_submission_blocked
+
+    expect(@candidate_third.reload).not_to be_submission_blocked
+    expect(@candidate_fourth.reload).not_to be_submission_blocked
+  end
+end
