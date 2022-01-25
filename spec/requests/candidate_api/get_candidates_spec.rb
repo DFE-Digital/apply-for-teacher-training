e24fccbf5772e9a394ef2cd5950dfb541b5dcb9a
Jonathan Filar <jonathanfilar@hotmail.com>
2022-01-25 17:12:55 +0000

    Refactor duplicate request specs

--- spec/requests/candidate_api/get_candidates_spec.rb
+++ spec/requests/candidate_api/get_candidates_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe 'GET /candidate-api/candidates', type: :request do
   include CandidateAPISpecHelper
 
+  it_behaves_like 'an index API endpoint', '/candidate-api/candidates', 'updated_since', ServiceAPIUser.candidate_user.create_magic_link_token!
+
   it 'does not allow access to the API from other data users' do
     api_token = ServiceAPIUser.test_data_user.create_magic_link_token!
     get_api_request "/candidate-api/candidates?updated_since=#{CGI.escape(1.month.ago.iso8601)}", token: api_token
@@ -145,44 +147,6 @@ RSpec.describe 'GET /candidate-api/candidates', type: :request do
 
     expect(response).to have_http_status(:ok)
     expect(response.headers['page-items']).to eq '20'
-  end
-
-  it 'returns an error if the token is incorrect' do
-    get "/candidate-api/candidates?updated_since=#{CGI.escape(1.day.ago.iso8601)}", headers: { Authorization: 'invalid-token' }
-
-    expect(response).to have_http_status(:unauthorized)
-    expect(parsed_response).to be_valid_against_openapi_schema('UnauthorizedResponse')
-  end
-
-  it 'returns an error if no API token is present' do
-    get "/candidate-api/candidates?updated_since=#{CGI.escape(1.day.ago.iso8601)}", headers: { Authorization: nil }
-
-    expect(response).to have_http_status(:unauthorized)
-    expect(parsed_response).to be_valid_against_openapi_schema('UnauthorizedResponse')
-  end
-
-  it 'returns HTTP status 422 given an unparseable `updated_since` date value' do
-    get_api_request '/candidate-api/candidates?updated_since=17/07/2020T12:00:42Z', token: candidate_api_token
-
-    expect(response).to have_http_status(:unprocessable_entity)
-    expect(error_response['message']).to eql('Parameter is invalid (should be ISO8601): updated_since')
-    expect(parsed_response).to be_valid_against_openapi_schema('ParameterInvalidResponse')
-  end
-
-  it 'returns HTTP status 422 when encountering a KeyError from ActiveSupport::TimeZone' do
-    get_api_request '/candidate-api/candidates?updated_since=12936', token: candidate_api_token
-
-    expect(response).to have_http_status(:unprocessable_entity)
-    expect(error_response['message']).to eql('Parameter is invalid (should be ISO8601): updated_since')
-    expect(parsed_response).to be_valid_against_openapi_schema('ParameterInvalidResponse')
-  end
-
-  it 'returns HTTP status 422 given a parseable but nonsensensical `updated_since` date value' do
-    get_api_request '/candidate-api/candidates?updated_since=-004713-03-23T11:52:19.448Z', token: candidate_api_token
-
-    expect(response).to have_http_status(:unprocessable_entity)
-    expect(error_response['message']).to eql('Parameter is invalid (date is nonsense): updated_since')
-    expect(parsed_response).to be_valid_against_openapi_schema('ParameterInvalidResponse')
   end
 
   it 'returns HTTP status 422 when given a parseable page value that exceeds the range' do
---
+++ spec/support/shared_examples.rb
@@ -0,0 +1,39 @@
+RSpec.shared_examples 'an index API endpoint' do |path, date_param, api_token|
+  it 'returns an error if the token is incorrect' do
+    get "#{path}?#{date_param}=#{CGI.escape(1.day.ago.iso8601)}", headers: { Authorization: 'invalid-token' }
+
+    expect(response).to have_http_status(:unauthorized)
+    expect(parsed_response).to be_valid_against_openapi_schema('UnauthorizedResponse')
+  end
+
+  it 'returns an error if no API token is present' do
+    get "#{path}?#{date_param}=#{CGI.escape(1.day.ago.iso8601)}", headers: { Authorization: nil }
+
+    expect(response).to have_http_status(:unauthorized)
+    expect(parsed_response).to be_valid_against_openapi_schema('UnauthorizedResponse')
+  end
+
+  it "returns an error if the #{date_param} parameter is missing" do
+    get_api_request path, token: api_token
+
+    expect(response).to have_http_status(:unprocessable_entity)
+    expect(error_response['message']).to eql('param is missing or the value is empty: updated_since')
+    expect(parsed_response).to be_valid_against_openapi_schema('ParameterInvalidResponse')
+  end
+
+  it "returns HTTP status 422 given an unparseable #{date_param} date value" do
+    get_api_request "#{path}?#{date_param}=17/07/2020T12:00:42Z", token: api_token
+
+    expect(response).to have_http_status(:unprocessable_entity)
+    expect(error_response['message']).to eql('Parameter is invalid (should be ISO8601): updated_since')
+    expect(parsed_response).to be_valid_against_openapi_schema('ParameterInvalidResponse')
+  end
+
+  it 'returns HTTP status 422 when encountering a KeyError from ActiveSupport::TimeZone' do
+    get_api_request "#{path}?#{date_param}=12936", token: api_token
+
+    expect(response).to have_http_status(:unprocessable_entity)
+    expect(error_response['message']).to eql('Parameter is invalid (should be ISO8601): updated_since')
+    expect(parsed_response).to be_valid_against_openapi_schema('ParameterInvalidResponse')
+  end
+end
