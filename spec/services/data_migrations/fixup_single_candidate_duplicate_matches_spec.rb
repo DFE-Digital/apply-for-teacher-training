88d6edfaf06c0e1650da4649a07c4029a83fccab
Steve Hook <steve.hook@gmail.com>
2022-01-26 11:21:01 +0000

    Add test case to assert that we don't alter valid fraud matches

--- spec/services/data_migrations/fixup_single_candidate_duplicate_matches_spec.rb
+++ spec/services/data_migrations/fixup_single_candidate_duplicate_matches_spec.rb
@@ -37,4 +37,33 @@ RSpec.describe DataMigrations::FixupSingleCandidateDuplicateMatches do
     expect(FraudMatch.count).to be(1)
     expect(FraudMatch.first.candidates).to match_array([bob, alice])
   end
+
+  it 'ignores fraud matches with two candidates' do
+    alice = create(:candidate, email_address: 'alice@example.com')
+    alices_application_form = create(
+      :application_form,
+      :duplicate_candidates,
+      candidate: alice,
+    )
+    bob = create(:candidate, email_address: 'bob@example.com')
+    bobs_application_form = create(
+      :application_form,
+      :duplicate_candidates,
+      last_name: " #{ApplicationForm.last.last_name.upcase} ",
+      postcode: "#{ApplicationForm.last.postcode.downcase} ",
+      candidate: bob,
+    )
+    fraud_match = create(
+      :fraud_match,
+      last_name: bobs_application_form.last_name,
+      postcode: bobs_application_form.postcode,
+      date_of_birth: bobs_application_form.date_of_birth,
+      candidates: [alice, bob],
+    )
+
+    described_class.new.change
+
+    expect(FraudMatch.count).to be(1)
+    expect(fraud_match.reload.candidates).to match_array([bob, alice])
+  end
 end
