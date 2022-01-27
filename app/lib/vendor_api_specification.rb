921db6ae7f458b0eec8d5f4be1b4c46f841b6c50
Steve Laing <steve.laing@gmail.com>
2022-01-27 12:04:54 +0000

    Refactor schema merging method
    Simplify how we merge schemas into a few different methods.

--- app/lib/vendor_api_specification.rb
+++ app/lib/vendor_api_specification.rb
@@ -1,8 +1,8 @@
 class VendorAPISpecification
   include VersioningHelpers
 
-  YAML_FILE_PATH = 'config/vendor_api'.freeze
-  DRAFT_YAML_FILE_PATH = "#{YAML_FILE_PATH}/draft.yml".freeze
+  SPEC_FILE_DIR = 'config/vendor_api'.freeze
+  DRAFT_YAML_FILE_PATH = "#{SPEC_FILE_DIR}/draft.yml".freeze
 
   def initialize(version: nil, draft: false)
     @version = version || VendorAPI::VERSION
@@ -18,30 +18,43 @@ class VendorAPISpecification
   end
 
   def spec
-    (0..minor_version_number(@version)).inject({}) do |versions, minor_version|
+    (0..minor_version_number(@version)).each_with_object({}) do |minor_version, versions|
       version_to_load = "#{major_version_number(@version)}.#{minor_version}"
 
-      yaml_file_for_version = "#{YAML_FILE_PATH}/v#{version_to_load}.yml"
+      if File.exist?(yaml_file_path(version_to_load))
+        yaml_for_version = YAML.load_file(yaml_file_path(version_to_load))
 
-      if File.exist?(yaml_file_for_version)
-        yaml_for_version = YAML.load_file(yaml_file_for_version)
-
-        experimental_yaml_file_for_version = "#{YAML_FILE_PATH}/experimental-v#{version_to_load}.yml"
-
-        if File.exist?(experimental_yaml_file_for_version)
-          yaml_for_version = yaml_for_version.deep_merge(YAML.load_file(experimental_yaml_file_for_version))
-        end
-
+        merge_experimental_spec!(yaml_for_version, version_to_load)
       end
 
-      if draft? && File.exist?(DRAFT_YAML_FILE_PATH)
-        yaml_for_version = yaml_for_version.deep_merge(YAML.load_file(DRAFT_YAML_FILE_PATH))
-      end
+      merge_draft_spec!(yaml_for_version)
 
-      versions = versions.deep_merge(yaml_for_version) if yaml_for_version.present?
-
-      versions
+      versions.deep_merge!(yaml_for_version) if yaml_for_version.present?
     end
+  end
+
+private
+
+  def yaml_file_path(version_to_load)
+    "#{SPEC_FILE_DIR}/v#{version_to_load}.yml"
+  end
+
+  def merge_experimental_spec!(yaml_for_version, version_to_load)
+    experimental_yaml_file_for_version = "#{SPEC_FILE_DIR}/experimental-v#{version_to_load}.yml"
+
+    if File.exist?(experimental_yaml_file_for_version)
+      yaml_for_version.deep_merge!(YAML.load_file(experimental_yaml_file_for_version))
+    end
+
+    yaml_for_version
+  end
+
+  def merge_draft_spec!(yaml_for_version)
+    if draft? && File.exist?(DRAFT_YAML_FILE_PATH)
+      yaml_for_version.deep_merge!(YAML.load_file(DRAFT_YAML_FILE_PATH))
+    end
+
+    yaml_for_version
   end
 
   def draft?
