4304e7a30e8f975c8a8f6ace0a958ead6239b14d
Michael Nacos <m.nacos@gmail.com>
2022-02-01 17:16:08 +0000

    Add more validations

--- app/services/interview_validations.rb
+++ app/services/interview_validations.rb
@@ -3,35 +3,47 @@ class InterviewValidations
 
   attr_reader :interview, :today
 
-  validate :interview_is_still_pending, if: -> { interview.changed? }
-  validate :date_and_time_changes, if: -> { interview.date_and_time_change }
+  delegate :date_and_time, to: :interview
+  delegate :application_choice, to: :interview
+
+  validates :date_and_time, presence: true
+  validates :application_choice, presence: true
+
+  validate :stop_changes_if_interview_already_passed, if: -> { interview.changed? }
+  validate :updates_to_date_and_time, if: -> { interview.date_and_time_change }
+  validate :keep_date_and_time_before_rbd, if: -> { interview.date_and_time_change }
 
   def initialize(interview:)
     @interview = interview
     @today = Time.zone.now.beginning_of_day
   end
 
-  def interview_is_still_pending
-    if interview.date_and_time < today
+  def rbd_date
+    @rbd_date ||= application_choice.reject_by_default_at
+  end
+
+  def stop_changes_if_interview_already_passed
+    if date_and_time && date_and_time < today
       errors.add :base, 'Changing a past interview'
     end
   end
 
-  def date_and_time_changes
+  def updates_to_date_and_time
     old_date = interview.date_and_time_change&.first
     new_date = interview.date_and_time_change&.second
-    rbd_date = interview.application_choice.reject_by_default_at
 
-    if old_date.present? && new_date.present? # date_and_time update
+    if old_date.present? && new_date.present?
       if old_date < today
         errors.add :base, 'Changing a past interview'
       elsif new_date < today
         errors.add :date_and_time, 'Moving an interview to the past'
       end
+    end
+  end
 
-      if new_date > rbd_date
-        errors.add :date_and_time, 'Moving an interview past RBD'
-      end
+  def keep_date_and_time_before_rbd
+    if date_and_time > rbd_date
+      errors.add :date_and_time, 'Scheduling an interview past RBD'
     end
   end
 end
--- spec/services/interview_validations_spec.rb
+++ spec/services/interview_validations_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe InterviewValidations do
   let(:provider) { course.provider }
   let(:course_option) { create(:course_option, course: course) }
 
-  context 'no changes' do
+  context 'existing interview with no changes' do
     let(:interview) { create(:interview, :past_date_and_time, application_choice: application_choice) }
 
     it 'is valid even if date_and_time in the past' do
@@ -17,60 +17,176 @@ RSpec.describe InterviewValidations do
     end
   end
 
-  context 'date_and_time updates' do
-    context 'changes from future to past' do
-      it 'is not valid' do
-        interview.date_and_time = 5.days.ago
+  context 'application_choice past interviewing stage' do
+    let(:application_choice) { create(:application_choice, :with_scheduled_interview, :with_offer, course_option: course_option) }
 
-        expect(interview_validations).not_to be_valid
-      end
+    it 'makes interview still valid if there are no changes' do
+      expect(interview_validations).to be_valid
     end
 
-    context 'changes from past to future' do
-      let(:interview) { create(:interview, :past_date_and_time, application_choice: application_choice) }
-
-      it 'is not valid' do
-        interview.date_and_time = 2.days.from_now
-
-        expect(interview_validations).not_to be_valid
-      end
+    it 'makes new interviews not valid' do
+      skip
+      expect(interview_validations).not_to be_valid
     end
 
-    context 'changes from past to past' do
-      let(:interview) { create(:interview, :past_date_and_time, application_choice: application_choice) }
-
-      it 'is not valid' do
-        interview.date_and_time -= 1.day
-
-        expect(interview_validations).not_to be_valid
-      end
+    it 'makes interview updates not valid' do
+      skip
+      expect(interview_validations).not_to be_valid
     end
 
-    context 'changes from future to future' do
-      it 'is valid' do
-        interview.date_and_time += 1.day
-
-        expect(interview_validations).to be_valid
-      end
-    end
-
-    context 'changes from future to after RBD date' do
-      it 'is not valid' do
-        rbd_date = application_choice.reject_by_default_at
-        interview.date_and_time = rbd_date + 1.second
-
-        expect(interview_validations).not_to be_valid
-      end
+    it 'makes interview cancellations not valid' do
+      skip
+      expect(interview_validations).not_to be_valid
     end
   end
 
-  context 'any change to an interview that has passed' do
-    let(:interview) { create(:interview, :past_date_and_time, application_choice: application_choice) }
+  context 'new interview' do
+    let(:interview) do
+      build(
+        :interview,
+        application_choice: application_choice,
+        date_and_time: nil,
+      )
+    end
 
-    it 'is not valid' do
-      interview.additional_details = 'Changed value'
+    it 'needs a date_and_time' do
+      expect(interview_validations).not_to be_valid
+    end
+
+    it 'needs an application_choice' do
+      interview.application_choice = nil
 
       expect(interview_validations).not_to be_valid
+    end
+
+    it 'needs a provider' do
+      interview.date_and_time = 3.days.from_now
+      interview.provider = nil
+
+      expect(interview_validations).not_to be_valid
+    end
+
+    it 'needs an appropriate provider' do
+      interview.date_and_time = 3.days.from_now
+
+      interview.provider = course.accredited_provider
+      expect(interview_validations).to be_valid
+
+      interview.provider = create(:provider)
+      expect(interview_validations).not_to be_valid
+    end
+
+    it 'with a date_and_time in the past is not valid' do
+      interview.date_and_time = 3.days.ago
+
+      expect(interview_validations).not_to be_valid
+    end
+
+    it 'with a date_and_time in the future is valid' do
+      interview.date_and_time = 3.days.from_now
+
+      expect(interview_validations).to be_valid
+    end
+
+    it 'with a date_and_time past the RBD date' do
+      rbd_date = application_choice.reject_by_default_at
+      interview.date_and_time = rbd_date + 1.second
+
+      expect(interview_validations).not_to be_valid
+    end
+
+    it 'with a date_and_time past the RBD date' do
+      rbd_date = application_choice.reject_by_default_at
+      interview.date_and_time = rbd_date + 1.second
+
+      expect(interview_validations).not_to be_valid
+    end
+  end
+
+  context 'update interview' do
+    context 'any change to an interview that has passed' do
+      let(:interview) { create(:interview, :past_date_and_time, application_choice: application_choice) }
+
+      it 'is not valid' do
+        interview.additional_details = 'Changed value'
+
+        expect(interview_validations).not_to be_valid
+      end
+    end
+
+    context 'provider' do
+      context 'becomes the training provider' do
+        let(:provider) { course.accredited_provider }
+
+        it 'is valid' do
+          interview.provider = course.provider
+
+          expect(interview_validations).to be_valid
+        end
+      end
+
+      context 'becomes the ratifying provider' do
+        it 'is valid' do
+          interview.provider = course.accredited_provider
+
+          expect(interview_validations).to be_valid
+        end
+      end
+
+      context 'becomes any other provider' do
+        it 'is not valid' do
+          interview.provider = create(:provider)
+
+          expect(interview_validations).not_to be_valid
+        end
+      end
+    end
+
+    context 'date_and_time' do
+      context 'changes from future to past' do
+        it 'is not valid' do
+          interview.date_and_time = 5.days.ago
+
+          expect(interview_validations).not_to be_valid
+        end
+      end
+
+      context 'changes from past to future' do
+        let(:interview) { create(:interview, :past_date_and_time, application_choice: application_choice) }
+
+        it 'is not valid' do
+          interview.date_and_time = 2.days.from_now
+
+          expect(interview_validations).not_to be_valid
+        end
+      end
+
+      context 'changes from past to past' do
+        let(:interview) { create(:interview, :past_date_and_time, application_choice: application_choice) }
+
+        it 'is not valid' do
+          interview.date_and_time -= 1.day
+
+          expect(interview_validations).not_to be_valid
+        end
+      end
+
+      context 'changes from future to future' do
+        it 'is valid' do
+          interview.date_and_time += 1.day
+
+          expect(interview_validations).to be_valid
+        end
+      end
+
+      context 'changes from future to after RBD date' do
+        it 'is not valid' do
+          rbd_date = application_choice.reject_by_default_at
+          interview.date_and_time = rbd_date + 1.second
+
+          expect(interview_validations).not_to be_valid
+        end
+      end
     end
   end
 end
