186d1f5c594078c223cd4cc4ecba2cb4519d9806
Despo Pentara <despo@extractmethod.com>
2022-02-11 23:33:51 +0000

    Minor fixes in version change descriptions

--- app/lib/vendor_api/changes/defer_an_offer.rb
+++ app/lib/vendor_api/changes/defer_an_offer.rb
@@ -1,7 +1,7 @@
 module VendorAPI
   module Changes
     class DeferAnOffer < VersionChange
-      description 'Defers an offer to the next cycle.' \
+      description 'Defers an offer to the next cycle. ' \
                   'The application will transition to the offer_deferred status and the fields ' \
                   'offer_deferred_at and status_before_deferral will be populated.'
 
--- app/lib/vendor_api/changes/withdraw_or_decline_application.rb
+++ app/lib/vendor_api/changes/withdraw_or_decline_application.rb
@@ -2,7 +2,7 @@ module VendorAPI
   module Changes
     class WithdrawOrDeclineApplication < VersionChange
       description \
-        'Withdraw an application\n' \
+        "Withdraw an application\n" \
         'Withdraws an application or declines an offer at the candidate‘s request' \
 
       action WithdrawOrDeclineOfferController, :create
