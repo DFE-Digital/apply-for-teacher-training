35cfc356e4b081195f94840852e1ae495a814c97
Despo Pentara <despo@extractmethod.com>
2022-02-11 23:31:19 +0000

    Move interview presenter concerns to presenter concerns directory

---
+++ app/presenters/concerns/add_interviews_to_application_api_data.rb
@@ -0,0 +1,13 @@
+module AddInterviewsToApplicationAPIData
+  def schema
+    super.deep_merge!({
+      attributes: {
+        interviews: interviews.map { |interview| VendorAPI::InterviewPresenter.new(active_version, interview).schema },
+      },
+    })
+  end
+
+  def interviews
+    application_choice.interviews.order(updated_at: :desc)
+  end
+end
