78e57a5de055d6a30dbd7951bfeb98c3773de4c1
Tomas D'Stefano <tomas_stefano@successoft.com>
2022-02-16 13:06:36 -0300

    Rename variables in the spec to use lets on reference data

--- app/lib/dfe/reference_data/hardcoded_reference_list.rb
+++ app/lib/dfe/reference_data/hardcoded_reference_list.rb
@@ -5,6 +5,35 @@ module DfE
     # A +HardcodedReferenceList+ is an implementation of +ReferenceList+ that uses a
     # hardcoded hash as the data source.
     class HardcodedReferenceList < ReferenceList
+      class Record
+        attr_reader :data
+        delegate :[], :[]=, :merge, :key?, :values, :keys, to: :data
+
+        def initialize(data)
+          @data = data
+        end
+
+        def ==(other)
+          if other.is_a?(Hash)
+            data == other
+          else
+            data == other.data
+          end
+        end
+
+        def respond_to_missing?(method_name, *args)
+          data.has_key?(method_name)
+        end
+
+        def method_missing(method_name, *args, &block)
+          if data.has_key?(method_name)
+            data[method_name]
+          else
+            super
+          end
+        end
+      end
+
       ##
       # +HardcodedReferenceList+ constructor. +data+ is a hash from IDs to records;
       # the records do not need to contain an +id+ field as those are provided
@@ -12,9 +41,15 @@ module DfE
       def initialize(data)
         super()
         @data = {}
+#        @record_type = Struct.new(*data.values.map(&:keys).push(:id).flatten.uniq.map(&:to_sym), keyword_init: true)
+
         data.each_entry do |id, record|
-          @data[id] = record.merge({ id: id })
+          @data[id] = Record.new(record.merge({ id: id }))
         end
+    #DuplicatedMatch = Struct.new(
+    #  :email_address, :candidate_id, :created_at, :name, :date_of_birth, :address, :application_status, :account_status,
+    #  keyword_init: true
+    #)
       end
 
       def all
--- spec/lib/dfe/reference_data/hardcoded_reference_list_spec.rb
+++ spec/lib/dfe/reference_data/hardcoded_reference_list_spec.rb
@@ -1,14 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe DfE::ReferenceData::HardcodedReferenceList do
-  hrl = DfE::ReferenceData::HardcodedReferenceList.new(
-    {
-      '1' => { name: 'Alaric', child: false },
-      '2' => { name: 'Sarah', child: false },
-      '3' => { name: 'Jean', child: true },
-      '4' => { name: 'Mary', child: true }
-    }
-  )
+  let(:hardcoded_reference_list) do
+    DfE::ReferenceData::HardcodedReferenceList.new(
+      {
+        '1' => { name: 'Alaric', child: false },
+        '2' => { name: 'Sarah', child: false },
+        '3' => { name: 'Jean', child: true },
+        '4' => { name: 'Mary', child: true }
+      }
+    )
+  end
+
+  context 'when accessing the attributes of each record' do
+    let(:records) { hardcoded_reference_list.all }
+
+    it 'returns the values for each attribute' do
+      expect(records.first.name).to eq('Alaric')
+      expect(records.first.child).to be(false)
+    end
+  end
 
   # NB: These particular tests also make a potentially fragile assumption that
   # the implementation of some preserves the order of entries, it would be
@@ -16,42 +27,42 @@ RSpec.describe DfE::ReferenceData::HardcodedReferenceList do
   # comparator
 
   it 'returns correct data from low-level methods' do
-    expect(hrl.all).to eq([
+    expect(hardcoded_reference_list.all).to eq([
                             { id: '1', name: 'Alaric', child: false },
                             { id: '2', name: 'Sarah', child: false },
                             { id: '3', name: 'Jean', child: true },
                             { id: '4', name: 'Mary', child: true }
                           ])
-    expect(hrl.all_as_hash).to eq({
+    expect(hardcoded_reference_list.all_as_hash).to eq({
                                     '1' => { id: '1', name: 'Alaric', child: false },
                                     '2' => { id: '2', name: 'Sarah', child: false },
                                     '3' => { id: '3', name: 'Jean', child: true },
                                     '4' => { id: '4', name: 'Mary', child: true }
                                   })
 
-    expect(hrl.one('1')).to eq({ id: '1', name: 'Alaric', child: false })
-    expect(hrl.one('nonexistant')).to eq(nil)
+    expect(hardcoded_reference_list.one('1')).to eq({ id: '1', name: 'Alaric', child: false })
+    expect(hardcoded_reference_list.one('nonexistant')).to eq(nil)
   end
 
   it 'returns correct data when filtered with some' do
-    expect(hrl.some({ child: true })).to eq([
+    expect(hardcoded_reference_list.some({ child: true })).to eq([
                                               { id: '3', name: 'Jean', child: true },
                                               { id: '4', name: 'Mary', child: true }
                                             ])
 
-    expect(hrl.some({ child: false })).to eq([
+    expect(hardcoded_reference_list.some({ child: false })).to eq([
                                                { id: '1', name: 'Alaric', child: false },
                                                { id: '2', name: 'Sarah', child: false }
                                              ])
 
-    expect(hrl.some({})).to eq([
+    expect(hardcoded_reference_list.some({})).to eq([
                                  { id: '1', name: 'Alaric', child: false },
                                  { id: '2', name: 'Sarah', child: false },
                                  { id: '3', name: 'Jean', child: true },
                                  { id: '4', name: 'Mary', child: true }
                                ])
 
-    expect(hrl.some(nil)).to eq([
+    expect(hardcoded_reference_list.some(nil)).to eq([
                                   { id: '1', name: 'Alaric', child: false },
                                   { id: '2', name: 'Sarah', child: false },
                                   { id: '3', name: 'Jean', child: true },
@@ -60,7 +71,7 @@ RSpec.describe DfE::ReferenceData::HardcodedReferenceList do
   end
 
   it 'returns correct data reformatted by some_by_field' do
-    expect(hrl.some_by_field(:child)).to eq({
+    expect(hardcoded_reference_list.some_by_field(:child)).to eq({
                                               true => [
                                                 { id: '3', name: 'Jean', child: true },
                                                 { id: '4', name: 'Mary', child: true }
@@ -71,12 +82,12 @@ RSpec.describe DfE::ReferenceData::HardcodedReferenceList do
                                               ]
                                             })
 
-    expect(hrl.some_by_field(:child, { name: 'Alaric' })).to eq({
+    expect(hardcoded_reference_list.some_by_field(:child, { name: 'Alaric' })).to eq({
                                                                   false => [
                                                                     { id: '1', name: 'Alaric', child: false }
                                                                   ]
                                                                 })
 
-    expect(hrl.some_by_field(:nonexistant)).to eq({})
+    expect(hardcoded_reference_list.some_by_field(:nonexistant)).to eq({})
   end
 end
