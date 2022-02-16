13f45ed8e71c835760335a7fdff72b675231ec85
Steve Laing <steve.laing@gmail.com>
2022-02-16 14:47:58 +0000

    Record dynamically defined attribute names
    We need to build parameter lists and likely need to distinguish 'normal' attributes from dynamic ones.

--- app/lib/dynamic_rejection_reasons.rb
+++ app/lib/dynamic_rejection_reasons.rb
@@ -1,7 +1,12 @@
 module DynamicRejectionReasons
   CONFIG_FILE_PATH = 'config/rejection_reasons.yml'.freeze
 
+  attr_accessor :attribute_names, :array_attribute_names
+
   def initialize_dynamic_rejection_reasons
+    @attribute_names = []
+    @array_attribute_names = []
+
     init_attrs
   end
 
@@ -11,7 +16,7 @@ module DynamicRejectionReasons
 
   def init_attrs
     questions.each do |question_key, question_config|
-      attr_accessor question_key
+      define_attr(question_key.to_sym, details: question_config.key?('details'))
 
       next unless question_config.key?('reasons')
 
@@ -21,10 +26,22 @@ module DynamicRejectionReasons
     end
   end
 
+  def define_attr(attr_name, details: false)
+    attr_accessor attr_name
+    @attribute_names << attr_name
+
+    if details
+      details_attr_name = :"#{attr_name}_details"
+      attr_accessor details_attr_name
+      @attribute_names << details_attr_name
+    end
+  end
+
   def define_reasons_collection_methods(question_key)
     reasons_collection_attr = :"#{question_key}_reasons"
 
     attr_writer reasons_collection_attr
+    @array_attribute_names << reasons_collection_attr
 
     define_method reasons_collection_attr do
       instance_variable_get("@#{reasons_collection_attr}") || []
@@ -33,10 +50,14 @@ module DynamicRejectionReasons
 
   def define_reasons_attr_accessors(reasons)
     reasons.each do |reason_key, reason_config|
-      attr_accessor reason_key
-      if reason_config.key?('details')
-        attr_accessor :"#{reason_key}_details"
-      end
+      attr_accessor reason_key.to_sym
+      @attribute_names << reason_key.to_sym
+
+      next unless reason_config.key?('details')
+
+      details_attr_name = :"#{reason_key}_details"
+      attr_accessor details_attr_name
+      @attribute_names << details_attr_name
     end
   end
 
