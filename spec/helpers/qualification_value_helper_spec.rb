56a232df200940945302242b17b99c666da98776
James Glenn <47917431+JR-G@users.noreply.github.com>
2022-02-17 12:31:15 +0000

    Create a qualification value helper

---
+++ spec/helpers/qualification_value_helper_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe QualificationValueHelper, type: :helper do
+  describe '#qualification_text' do
+    let(:course_option) { build_stubbed(:course_option, course: @course) }
+
+    context 'when there are multiple values' do
+      it 'capitalises and concatenates the values' do
+        @course = build_stubbed(:course, qualifications: %w[qts pgce])
+
+        expect(qualification_text(course_option)).to eq 'PGCE with QTS'
+      end
+    end
+
+    context 'when there is a single value' do
+      it 'capitalises the value' do
+        @course = build_stubbed(:course, qualifications: %w[qts])
+
+        expect(qualification_text(course_option)).to eq 'QTS'
+      end
+    end
+
+    context 'when there is no value' do
+      it 'returns nil' do
+        @course = build_stubbed(:course, qualifications: nil)
+
+        expect(qualification_text(course_option)).to eq nil
+      end
+    end
+  end
+end
