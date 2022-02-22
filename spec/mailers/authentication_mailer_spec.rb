db1677c703f0ebb7a01c9052e0e61c4b0af35648
EmmaFrith <56349171+EmmaFrith@users.noreply.github.com>
2022-02-22 17:13:37 +0000

    fix tests

--- spec/mailers/authentication_mailer_spec.rb
+++ spec/mailers/authentication_mailer_spec.rb
@@ -16,6 +16,7 @@ RSpec.describe AuthenticationMailer, type: :mailer do
     it_behaves_like(
       'a mail with subject and content',
       I18n.t('authentication.sign_up.email.subject'),
+      'heading' => I18n.t('authentication.sign_up.email.subject'),
       'magic_link' => 'http://localhost:3000/candidate/sign-in/confirm?token=blub',
     )
 
