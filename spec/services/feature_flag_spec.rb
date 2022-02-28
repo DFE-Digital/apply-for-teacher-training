3f4dd30d963fdd6c3609f17f60ccd778d5e9b456
Avin Hurry <avin.hurry@digital.education.gov.uk>
2022-02-28 17:06:45 +0000

    Replace the feature flag used for the feature flag unit tests with dfe_sign_in_fallback

--- spec/services/feature_flag_spec.rb
+++ spec/services/feature_flag_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 RSpec.describe FeatureFlag do
   describe '.activate' do
     it 'activates a feature' do
-      expect { described_class.activate('pilot_open') }.to(
-        change { described_class.active?('pilot_open') }.from(false).to(true),
+      expect { described_class.activate('dfe_sign_in_fallback') }.to(
+        change { described_class.active?('dfe_sign_in_fallback') }.from(false).to(true),
       )
     end
 
     it 'records the change in the database' do
-      feature = Feature.create_or_find_by(name: 'pilot_open')
+      feature = Feature.create_or_find_by(name: 'dfe_sign_in_fallback')
       feature.update!(active: false)
-      expect { described_class.activate('pilot_open') }.to(
+      expect { described_class.activate('dfe_sign_in_fallback') }.to(
         change { feature.reload.active }.from(false).to(true),
       )
     end
@@ -20,16 +20,16 @@ RSpec.describe FeatureFlag do
   describe '.deactivate' do
     it 'deactivates a feature' do
       # To avoid flakey tests where activation/deactivation happens at the same time
-      Timecop.travel(5.minutes.ago) { described_class.activate('pilot_open') }
-      expect { described_class.deactivate('pilot_open') }.to(
-        change { described_class.active?('pilot_open') }.from(true).to(false),
+      Timecop.travel(5.minutes.ago) { described_class.activate('dfe_sign_in_fallback') }
+      expect { described_class.deactivate('dfe_sign_in_fallback') }.to(
+        change { described_class.active?('dfe_sign_in_fallback') }.from(true).to(false),
       )
     end
 
     it 'records the change in the database' do
-      feature = Feature.create_or_find_by(name: 'pilot_open')
+      feature = Feature.create_or_find_by(name: 'dfe_sign_in_fallback')
       feature.update!(active: true)
-      expect { described_class.deactivate('pilot_open') }.to(
+      expect { described_class.deactivate('dfe_sign_in_fallback') }.to(
         change { feature.reload.active }.from(true).to(false),
       )
     end
