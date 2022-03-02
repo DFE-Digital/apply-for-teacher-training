1e0f609faf6c114354f5a1cba0413ef44a419837
Avin Hurry <avin.hurry@digital.education.gov.uk>
2022-03-02 17:19:13 +0000

    Add back link to complaints page

--- spec/system/candidate_interface/candidate_content_spec.rb
+++ spec/system/candidate_interface/candidate_content_spec.rb
@@ -20,6 +20,8 @@ RSpec.feature 'Candidate content' do
 
     when_i_click_on_complaints
     then_i_can_see_the_complaints_page
+    and_i_click_back
+    then_i_can_see_the_cookies_page
 
     when_i_click_on_the_privacy_policy
     then_i_can_see_the_privacy_policy
