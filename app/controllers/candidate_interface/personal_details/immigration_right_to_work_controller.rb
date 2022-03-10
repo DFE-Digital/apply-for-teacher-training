57a1f9862bba840ee45717c65f95eb90d15c6db4
Avin Hurry <avin.hurry@digital.education.gov.uk>
2022-03-10 23:02:32 +0000

    Use right_to_work_or_study column instead

--- app/controllers/candidate_interface/personal_details/immigration_right_to_work_controller.rb
+++ app/controllers/candidate_interface/personal_details/immigration_right_to_work_controller.rb
@@ -25,12 +25,9 @@ module CandidateInterface
     private
 
       def right_to_work_params
-        {
-          immigration_right_to_work: params.dig(
-            :candidate_interface_immigration_right_to_work_form,
-            :immigration_right_to_work,
-          ),
-        }
+        strip_whitespace params.require(:candidate_interface_immigration_right_to_work_form).permit(
+          :right_to_work_or_study
+        )
       end
     end
   end
--- app/presenters/candidate_interface/personal_details_review_presenter.rb
+++ app/presenters/candidate_interface/personal_details_review_presenter.rb
@@ -225,9 +225,7 @@ module CandidateInterface
     end
 
     def formatted_immigration_right_to_work
-      if immigration_right_to_work_form.immigration_right_to_work.nil?
-        ApplicationController.render(CandidateInterface::MissingRightToWorkInsetComponent.new(application_form: @application_form), layout: nil)
-      elsif immigration_right_to_work_form.right_to_work_or_study?
+      if immigration_right_to_work_form.right_to_work_or_study?
         'Yes'
       else
         'Not yet'
