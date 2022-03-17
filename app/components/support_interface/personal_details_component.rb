39031dbc05d1e4ded1adfcb86802b4dcba5e1212
Frankie Roberto <frankie@frankieroberto.com>
2022-03-17 10:55:50 +0000

    Fix bad merge

--- app/components/support_interface/personal_details_component.rb
+++ app/components/support_interface/personal_details_component.rb
@@ -148,7 +148,7 @@ module SupportInterface
 
       row.merge(
         action: {
-          href: support_interface_application_form_edit_right_to_work_or_study_path(application_form),
+          href: support_interface_application_form_edit_immigration_status_path(application_form),
           visually_hidden_text: 'immigration status',
         },
       )
