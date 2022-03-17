5cd405b32e456229b7b4f058269e232cf5f993b4
Steve Hook <steve.hook@gmail.com>
2022-03-17 09:50:51 +0000

    Fix spec regressions

--- app/lib/candidate_api_specification.rb
+++ app/lib/candidate_api_specification.rb
@@ -2,15 +2,15 @@ class CandidateAPISpecification
   CURRENT_VERSION = 'v1.1'.freeze
   VERSIONS = ['v1.1', 'v1.2'].freeze
 
-  def self.as_yaml(version)
+  def self.as_yaml(version = CURRENT_VERSION)
     spec(version).to_yaml
   end
 
-  def self.as_hash(version)
+  def self.as_hash(version = CURRENT_VERSION)
     spec(version)
   end
 
-  def self.spec(version)
+  def self.spec(version = CURRENT_VERSION)
     YAML.load_file("config/candidate_api/#{version}.yml")
   end
 end
