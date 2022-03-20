365035c3a5a5a083d109bccfc3e54ac7c46b8ef9
Tomas D'Stefano <tomas_stefano@successoft.com>
2022-03-20 20:43:50 -0300

    Replace old data with degree institutions reference data

--- app/lib/hesa/institution.rb
+++ app/lib/hesa/institution.rb
@@ -1,10 +1,12 @@
 module Hesa
   class Institution
-    InstitutionStruct = Struct.new(:hesa_code, :name)
+    include ActiveModel::Model
+    attr_accessor :id, :name, :hesa_code, :suggestion_synonyms, :match_synonyms, :dttp_id, :ukprn, :comment, :closed, :has_never_awarded_degrees
+    alias hesa_itt_code= hesa_code=
 
     class << self
       def all
-        HESA_DEGREE_INSTITUTIONS.map { |institution_data| InstitutionStruct.new(*institution_data) }
+        DfE::ReferenceData::Degrees::INSTITUTIONS.all.map { |institution_data| new(institution_data.to_h) }
       end
 
       def names
@@ -12,7 +14,7 @@ module Hesa
       end
 
       def find_by_name(name)
-        all.find { |institution| institution.name == name }
+        all.find { |institution| institution.name == name || name.in?(institution.match_synonyms) }
       end
     end
   end
