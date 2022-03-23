e634df7dc6f3d8d18de279b91178aedcac5b82bd
Steve Hook <steve.hook@gmail.com>
2022-03-23 21:17:15 +0000

    Wire up `NudgeUnsubmittedCandidatesWorker`
    Integrate the worker with the `GetUnsubmittedApplicationsReadyToNudge`
    query and `CandidateMailer`

--- app/workers/nudge_unsubmitted_candidates_worker.rb
+++ app/workers/nudge_unsubmitted_candidates_worker.rb
@@ -3,7 +3,9 @@ class NudgeUnsubmittedCandidatesWorker
   include Sidekiq::Worker
 
   def perform
-    GetUnsubmittedApplicationsReadyToNudge.call.each do |application_form|
+    return unless FeatureFlag.active?(:candidate_nudge_emails)
+
+    GetUnsubmittedApplicationsReadyToNudge.new.call.each do |application_form|
       send_nudge(application_form)
     end
   end
@@ -11,5 +13,6 @@ class NudgeUnsubmittedCandidatesWorker
 private
 
   def send_nudge(application_form)
+    CandidateMailer.nudge_unsubmitted(application_form).deliver_later
   end
 end
