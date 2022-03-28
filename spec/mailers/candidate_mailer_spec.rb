d618f45d7c2571f75bcb1cf243839265504e71ed
James Glenn <47917431+JR-G@users.noreply.github.com>
2022-03-28 12:46:04 +0100

    Add the change course mailer

--- spec/mailers/candidate_mailer_spec.rb
+++ spec/mailers/candidate_mailer_spec.rb
@@ -410,6 +410,71 @@ RSpec.describe CandidateMailer, type: :mailer do
     end
   end
 
+  describe '.change_course' do
+    # TODO: swap out course_option with original_course_option once implemented.
+    let(:application_choice) do
+      create(
+        :application_choice,
+        course_option: course_option,
+        current_course_option: current_course_option,
+        site: site,
+        application_form: create(:application_form, first_name: 'Fred'),
+      )
+    end
+    let(:email) { mailer.change_course(application_choice) }
+    let(:course_option) do
+      create(
+        :course_option,
+        course: create(
+          :course,
+          name: 'Mathematics',
+          code: 'M101',
+        ),
+      )
+    end
+    let(:current_course_option) do
+      create(
+        :course_option,
+        course: create(
+          :course,
+          :part_time,
+          name: 'Geography',
+          code: 'H234',
+          provider: provider,
+        ),
+        site: site,
+      )
+    end
+    let(:site) do
+      create(:site,
+             name: 'First Road',
+             code: 'F34',
+             address_line1: 'Fountain Street',
+             address_line2: 'Morley',
+             address_line3: 'Leeds',
+             postcode: 'LS27 OPD',
+             provider: provider)
+    end
+
+    let(:provider) do
+      create(:provider,
+             name: 'Best Training',
+             code: 'B54')
+    end
+
+    it_behaves_like(
+      'a mail with subject and content',
+      'Course details changed for Mathematics (M101)',
+      'greeting' => 'Dear Fred',
+      'old course' => 'The details of your application to study Mathematics (M101) have been changed',
+      'new details' => 'The new details are:',
+      'provider' => 'Training provider: Best Training',
+      'course' => 'Course: Geography',
+      'location' => 'Location: First Road',
+      'study mode' => 'Full time or part time: Part time',
+    )
+  end
+
   describe '.deadline_reminder' do
     context 'when a candidate is in Apply 1' do
       let(:email) { mailer.eoc_deadline_reminder(application_form) }
