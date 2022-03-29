1bec49aa94baf1024d4764bfc116bd0bf3aefdc7
Steve Hook <steve.hook@gmail.com>
2022-03-29 13:46:50 +0100

    Add dry_run method so that we can test query on prod
    After deploying this change but before activating the feature flag we
    can run this command in the Rails console on production to get a list of
    applications for which the nudge email will be sent.

--- app/workers/nudge_unsubmitted_candidates_worker.rb
+++ app/workers/nudge_unsubmitted_candidates_worker.rb
@@ -5,8 +5,19 @@ class NudgeUnsubmittedCandidatesWorker
   def perform
     return unless FeatureFlag.active?(:candidate_nudge_emails)
 
-    GetUnsubmittedApplicationsReadyToNudge.new.call.each do |application_form|
+    GetUnsubmittedApplicationsReadyToNudge.new.call.find_each do |application_form|
       send_nudge(application_form)
+    end
+  end
+
+  # This method can be run manually via a Rails console to perform a dry-run to 
+  # see who will be sent a nudge email without actually sending anything.
+  # It can be deleted when the `candidate_nudge_emails` feature flag is deleted.
+  def dry_run
+    GetUnsubmittedApplicationsReadyToNudge.new.call.find_each do |application_form|
+      # rubocop:disable Rails/Output
+      puts "Sending email for application form #{Rails.application.routes.url_helpers.support_interface_application_form_url(application_form.id)}"
+      # rubocop:enable Rails/Output
     end
   end
 
