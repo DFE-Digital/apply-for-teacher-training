8bb52727b8e51dd7bbffb1b754a0ea4721279145
Steve Hook <steve.hook@gmail.com>
2022-04-07 14:16:41 +0100

    Refactor out version specific part of candidate API

--- app/controllers/candidate_api/candidates_controller.rb
+++ app/controllers/candidate_api/candidates_controller.rb
@@ -19,7 +19,9 @@ module CandidateAPI
     MAX_PER_PAGE = 500
 
     def index
-      render json: { data: serialized_candidates }
+      render json: {
+        data: serializer.serialize(paginate(serializer.query))
+      }
     end
 
     def parameter_missing(e)
@@ -78,41 +80,109 @@ module CandidateAPI
       (params[:page] || 1).to_i
     end
 
-    def serialized_candidates
-      paginate(candidates).map do |candidate|
-        {
-          id: candidate.public_id,
-          type: 'candidate',
-          attributes: {
-            created_at: candidate.created_at.iso8601,
-            updated_at: candidate.candidate_api_updated_at,
-            email_address: candidate.email_address,
-            application_forms:
-              candidate.application_forms.order(:created_at).map do |application|
-                {
-                  id: application.id,
-                  created_at: application.created_at.iso8601,
-                  updated_at: application.updated_at.iso8601,
-                  application_status: ProcessState.new(application).state,
-                  application_phase: application.phase,
-                  recruitment_cycle_year: application.recruitment_cycle_year,
-                  submitted_at: application.submitted_at&.iso8601,
-                }
-              end,
-          },
-        }
+    def serializer
+      if version_param == 'v1.2'
+        V1_2.new(updated_since: updated_since_params)
+      else
+        V1_1.new(updated_since: updated_since_params)
       end
     end
 
-    def candidates
-      Candidate
-      .left_outer_joins(:application_forms)
-      .where(application_forms: { recruitment_cycle_year: RecruitmentCycle.current_year })
-      .or(Candidate.where('candidates.created_at > ? ', CycleTimetable.apply_1_deadline(RecruitmentCycle.previous_year)))
-      .distinct
-      .includes(application_forms: :application_choices)
-      .where('candidate_api_updated_at > ?', updated_since_params)
-      .order('candidates.candidate_api_updated_at DESC')
+    class V1_1
+      attr_reader :updated_since
+
+      def initialize(updated_since:)
+        @updated_since = updated_since
+      end
+
+      def serialize(candidates)
+        candidates.map do |candidate|
+          {
+            id: candidate.public_id,
+            type: 'candidate',
+            attributes: {
+              created_at: candidate.created_at.iso8601,
+              updated_at: candidate.candidate_api_updated_at,
+              email_address: candidate.email_address,
+              application_forms:
+                candidate.application_forms.order(:created_at).map do |application|
+                  {
+                    id: application.id,
+                    created_at: application.created_at.iso8601,
+                    updated_at: application.updated_at.iso8601,
+                    application_status: ProcessState.new(application).state,
+                    application_phase: application.phase,
+                    recruitment_cycle_year: application.recruitment_cycle_year,
+                    submitted_at: application.submitted_at&.iso8601,
+                  }
+                end,
+            },
+          }
+        end
+      end
+
+      def query
+        Candidate
+        .left_outer_joins(:application_forms)
+        .where(application_forms: { recruitment_cycle_year: RecruitmentCycle.current_year })
+        .or(Candidate.where('candidates.created_at > ? ', CycleTimetable.apply_1_deadline(RecruitmentCycle.previous_year)))
+        .distinct
+        .includes(application_forms: :application_choices)
+        .where('candidate_api_updated_at > ?', updated_since)
+        .order('candidates.candidate_api_updated_at DESC')
+      end
+    end
+
+    class V1_2
+      attr_reader :updated_since
+
+      def initialize(updated_since:)
+        @updated_since = updated_since
+      end
+
+      def serialize(candidates)
+        candidates.map do |candidate|
+          {
+            id: candidate.public_id,
+            type: 'candidate',
+            attributes: {
+              created_at: candidate.created_at.iso8601,
+              updated_at: candidate.candidate_api_updated_at,
+              email_address: candidate.email_address,
+              application_forms:
+                candidate.application_forms.order(:created_at).map do |application|
+                  {
+                    id: application.id,
+                    created_at: application.created_at.iso8601,
+                    updated_at: application.updated_at.iso8601,
+                    application_status: ProcessState.new(application).state,
+                    application_phase: application.phase,
+                    recruitment_cycle_year: application.recruitment_cycle_year,
+                    submitted_at: application.submitted_at&.iso8601,
+                  }
+                end,
+            },
+          }
+        end
+      end
+
+      def query
+        Candidate
+        .left_outer_joins(:application_forms)
+        .where(application_forms: { recruitment_cycle_year: RecruitmentCycle.current_year })
+        .or(Candidate.where('candidates.created_at > ? ', CycleTimetable.apply_1_deadline(RecruitmentCycle.previous_year)))
+        .distinct
+        .includes(application_forms: :application_choices)
+        .where('candidate_api_updated_at > ?', updated_since)
+        .order('candidates.candidate_api_updated_at DESC')
+      end
+    end
+
+    def paginate(scope)
+      pagy, paginated_records = pagy(scope, items: per_page, page: page)
+      pagy_headers_merge(pagy)
+
+      paginated_records
     end
 
     def updated_since_params
@@ -126,6 +196,10 @@ module CandidateAPI
       rescue ArgumentError, KeyError
         raise ParameterInvalid, 'Parameter is invalid (should be ISO8601): updated_since'
       end
+    end
+
+    def version_param
+      params[:api_version] || CandidateAPISpecification::CURRENT_VERSION
     end
   end
 end
