5110ee343c9edda00bbe9a8168cb9fa006227efd
Samuel Culley <samuel.culley@digital.education.gov.uk>
2022-04-08 16:54:14 +0100

    Add support personal information component

---
+++ app/components/support_interface/personal_information_component.rb
@@ -0,0 +1,142 @@
+module SupportInterface
+  class PersonalInformationComponent < ViewComponent::Base
+    MISSING = '<em>Not provided</em>'.html_safe
+    RIGHT_TO_WORK_OR_STUDY_DISPLAY_VALUES = {
+      'yes' => 'Yes',
+      'no' => 'Not yet',
+      'decide_later' => 'Candidate does not know',
+    }.freeze
+
+    include ViewHelper
+
+    delegate :first_name,
+             :last_name,
+             :candidate,
+             to: :application_form
+
+    def initialize(application_form:)
+      @application_form = application_form
+    end
+
+    def rows
+      [
+        first_name_row,
+        last_name_row,
+        date_of_birth_row,
+        nationality_row,
+        domicile_row,
+        right_to_work_or_study_row,
+        residency_details_row,
+      ].compact
+    end
+
+  private
+
+    def first_name_row
+      row = {
+        key: 'First name',
+        value: first_name,
+      }
+      return row unless editable?
+
+      row.merge(
+        action: {
+          href: support_interface_application_form_edit_applicant_details_path(application_form),
+          visually_hidden_text: 'first name',
+        },
+      )
+    end
+
+    def last_name_row
+      row = {
+        key: 'Last name',
+        value: last_name,
+      }
+      return row unless editable?
+
+      row.merge(
+        action: {
+          href: support_interface_application_form_edit_applicant_details_path(application_form),
+          visually_hidden_text: 'last name',
+        },
+      )
+    end
+
+    def nationality_row
+      row = {
+        key: 'Nationality',
+        value: application_form.nationalities.to_sentence(last_word_connector: ' and '),
+      }
+      return row unless editable?
+
+      row.merge(
+        action: {
+          href: support_interface_application_form_edit_nationalities_path(application_form),
+          visually_hidden_text: 'nationality',
+        },
+      )
+    end
+
+    def domicile_row
+      {
+        key: 'Domicile',
+        value: application_form.domicile,
+      }
+    end
+
+    def right_to_work_or_study_row
+      return if application_form.right_to_work_or_study.blank?
+
+      row = {
+        key: 'Has the right to work or study in the UK?',
+        value: RIGHT_TO_WORK_OR_STUDY_DISPLAY_VALUES.fetch(application_form.right_to_work_or_study),
+      }
+      return row unless editable?
+
+      row.merge(
+        action: {
+          href: support_interface_application_form_edit_immigration_right_to_work_path(application_form),
+          visually_hidden_text: 'right to work or study',
+        },
+      )
+    end
+
+    def residency_details_row
+      return unless application_form.right_to_work_or_study == 'yes'
+
+      row = {
+        key: 'Immigration status',
+        value: FormatResidencyDetailsService.new(application_form: application_form).residency_details_value,
+      }
+      return row unless editable?
+
+      row.merge(
+        action: {
+          href: support_interface_application_form_edit_immigration_status_path(application_form),
+          visually_hidden_text: 'immigration status',
+        },
+      )
+    end
+
+    def date_of_birth_row
+      row = {
+        key: 'Date of birth',
+        value: application_form.date_of_birth ? application_form.date_of_birth.to_fs(:govuk_date) : MISSING,
+      }
+      return row unless editable?
+
+      row.merge(
+        action: {
+          href: support_interface_application_form_edit_applicant_details_path(application_form),
+          visually_hidden_text: 'date of birth',
+        },
+      )
+    end
+
+    attr_reader :application_form
+
+    def editable?
+      application_form.editable?
+    end
+  end
+end
---
+++ spec/components/support_interface/personal_information_component_spec.rb
@@ -0,0 +1,133 @@
+require 'rails_helper'
+
+RSpec.describe SupportInterface::PersonalInformationComponent do
+  let(:application_form) do
+    build_stubbed(
+      :completed_application_form,
+      support_reference: 'AB123',
+      date_of_birth: Date.new(2000, 1, 1),
+      first_nationality: 'British',
+      second_nationality: 'Irish',
+      third_nationality: 'Spanish',
+    )
+  end
+
+  subject(:result) { render_inline(described_class.new(application_form: application_form)) }
+
+  it 'renders component with correct labels' do
+    ['First name', 'Last name', 'Date of birth', 'Nationality'].each do |key|
+      expect(result.css('.govuk-summary-list__key').text).to include(key)
+    end
+  end
+
+  it 'renders the candidate first name' do
+    expect(result.css('.govuk-summary-list__value').text).to include(application_form.first_name)
+  end
+
+  it 'renders the candidate last name' do
+    expect(result.css('.govuk-summary-list__value').text).to include(application_form.last_name)
+  end
+
+  it 'renders the candidate date of birth' do
+    expect(result.css('.govuk-summary-list__value').text).to include('1 January 2000')
+  end
+
+  it 'renders the candidates nationalities' do
+    expect(result.css('.govuk-summary-list__value').text).to include('British, Irish and Spanish')
+  end
+
+  it 'renders their HESA domicile code' do
+    expect(result.css('.govuk-summary-list__value').text).to include(application_form.domicile)
+  end
+
+  it 'does not render right to work fields if nationality is British or Irish' do
+    expect(result.text).not_to include('Has the right to work or study in the UK?')
+    expect(result.text).not_to include('Immigration status')
+  end
+
+  it 'shows change links' do
+    expect(result.css('a').first.text).to eq('Change first name')
+  end
+
+  context 'when the application form has a subsequent application' do
+    let(:application_form) do
+      create(
+        :completed_application_form,
+        support_reference: 'AB123',
+        date_of_birth: Date.new(2000, 1, 1),
+        first_nationality: 'British',
+        second_nationality: 'Irish',
+        third_nationality: 'Spanish',
+      )
+    end
+    let!(:subsequent_application_form) { create(:application_form, previous_application_form: application_form) }
+
+    it 'does not shows change links' do
+      expect(result.css('a').text).not_to include('Change')
+    end
+  end
+
+  context 'a candidate whose nationality is neither British or Irish' do
+    let(:application_form) do
+      build_stubbed(
+        :completed_application_form,
+        first_nationality: 'Pakistani',
+        second_nationality: 'Singaporean',
+        third_nationality: 'Spanish',
+      )
+    end
+
+    it 'renders their right to work or study status' do
+      SupportInterface::PersonalInformationComponent::RIGHT_TO_WORK_OR_STUDY_DISPLAY_VALUES.each do |key, value|
+        application_form.right_to_work_or_study = key
+        result = render_inline(described_class.new(application_form: application_form))
+        row_title = result.css('.govuk-summary-list__row')[5].css('dt').text
+        row_value = result.css('.govuk-summary-list__row')[5].css('dd').text
+        expect(row_title).to include 'Has the right to work or study in the UK?'
+        expect(row_value).to include value
+      end
+    end
+
+    context 'the right to work or study status is "yes" and a radio answer was selected' do
+      before do
+        application_form.right_to_work_or_study = 'yes'
+        application_form.immigration_status = 'eu_settled'
+        application_form.right_to_work_or_study_details = nil
+      end
+
+      it 'renders the immigration status row' do
+        expect(result.css('.govuk-summary-list__key').text).to include('Immigration status')
+        expect(result.css('.govuk-summary-list__value').text).to include('EU settled status')
+      end
+    end
+
+    context 'the right to work or study status is "yes" and another immigration status was given' do
+      before do
+        application_form.right_to_work_or_study = 'yes'
+        application_form.immigration_status = 'other'
+        application_form.right_to_work_or_study_details = 'Indefinite leave to remain'
+      end
+
+      it 'renders the residency_details_row' do
+        expect(result.css('.govuk-summary-list__key').text).to include('Immigration status')
+        expect(result.css('.govuk-summary-list__value').text).to include('Indefinite leave to remain')
+      end
+    end
+
+    context 'the right to work or study status is "no"' do
+      before { application_form.right_to_work_or_study = 'no' }
+
+      it 'does not render the immigration status row' do
+        expect(result.css('.govuk-summary-list__key').text).not_to include('Immigration status')
+      end
+    end
+
+    context 'the right to work or study status is "decide_later"' do
+      before { application_form.right_to_work_or_study = 'decide_later' }
+
+      it 'does not render the immigration status row' do
+        expect(result.css('.govuk-summary-list__key').text).not_to include('Immigration status')
+      end
+    end
+  end
+end
