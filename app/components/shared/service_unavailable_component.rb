de4e70c8a88a60610c1cb9be178f67fe8cefa29d
cwrw <abeer.salameh@gmail.com>
2022-04-11 07:02:15 +0100

    Resolve rubocop Style/RedundantInitialize cop

--- app/components/shared/service_unavailable_component.rb
+++ app/components/shared/service_unavailable_component.rb
@@ -1,6 +1,4 @@
 class ServiceUnavailableComponent < ViewComponent::Base
-  def initialize; end
-
   def title_content
     HostingEnvironment.sandbox_mode? ? t('service_unavailable.sandbox_title') : t('service_unavailable.title')
   end
--- app/components/shared/when_emails_are_sent_component.rb
+++ app/components/shared/when_emails_are_sent_component.rb
@@ -1,5 +1,3 @@
 class WhenEmailsAreSentComponent < ViewComponent::Base
   include ViewHelper
-
-  def initialize; end
 end
