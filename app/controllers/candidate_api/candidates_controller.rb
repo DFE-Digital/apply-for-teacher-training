4af1ff6c3570805c132a44feb2499c285601c8cc
Steve Hook <steve.hook@gmail.com>
2022-04-11 09:53:17 +0100

    Refactor extract version-specific serializers

--- app/controllers/candidate_api/candidates_controller.rb
+++ app/controllers/candidate_api/candidates_controller.rb
@@ -81,101 +81,12 @@ module CandidateAPI
     end
 
     def serializer
-      if version_param == 'v1.2'
-        V1_2.new(updated_since: updated_since_params)
-      else
-        V1_1.new(updated_since: updated_since_params)
-      end
-    end
-
-    class V1_1
-      attr_reader :updated_since
-
-      def initialize(updated_since:)
-        @updated_since = updated_since
-      end
-
-      def serialize(candidates)
-        candidates.map do |candidate|
-          {
-            id: candidate.public_id,
-            type: 'candidate',
-            attributes: {
-              created_at: candidate.created_at.iso8601,
-              updated_at: candidate.candidate_api_updated_at,
-              email_address: candidate.email_address,
-              application_forms:
-                candidate.application_forms.order(:created_at).map do |application|
-                  {
-                    id: application.id,
-                    created_at: application.created_at.iso8601,
-                    updated_at: application.updated_at.iso8601,
-                    application_status: ProcessState.new(application).state,
-                    application_phase: application.phase,
-                    recruitment_cycle_year: application.recruitment_cycle_year,
-                    submitted_at: application.submitted_at&.iso8601,
-                  }
-                end,
-            },
-          }
+      @serializer ||=
+        if version_param == 'v1.2'
+          CandidateAPI::Serializers::V12.new(updated_since: updated_since_params)
+        else
+          CandidateAPI::Serializers::V11.new(updated_since: updated_since_params)
         end
-      end
-
-      def query
-        Candidate
-        .left_outer_joins(:application_forms)
-        .where(application_forms: { recruitment_cycle_year: RecruitmentCycle.current_year })
-        .or(Candidate.where('candidates.created_at > ? ', CycleTimetable.apply_1_deadline(RecruitmentCycle.previous_year)))
-        .distinct
-        .includes(application_forms: :application_choices)
-        .where('candidate_api_updated_at > ?', updated_since)
-        .order('candidates.candidate_api_updated_at DESC')
-      end
-    end
-
-    class V1_2
-      attr_reader :updated_since
-
-      def initialize(updated_since:)
-        @updated_since = updated_since
-      end
-
-      def serialize(candidates)
-        candidates.map do |candidate|
-          {
-            id: candidate.public_id,
-            type: 'candidate',
-            attributes: {
-              created_at: candidate.created_at.iso8601,
-              updated_at: candidate.candidate_api_updated_at,
-              email_address: candidate.email_address,
-              application_forms:
-                candidate.application_forms.order(:created_at).map do |application|
-                  {
-                    id: application.id,
-                    created_at: application.created_at.iso8601,
-                    updated_at: application.updated_at.iso8601,
-                    application_status: ProcessState.new(application).state,
-                    application_phase: application.phase,
-                    recruitment_cycle_year: application.recruitment_cycle_year,
-                    submitted_at: application.submitted_at&.iso8601,
-                  }
-                end,
-            },
-          }
-        end
-      end
-
-      def query
-        Candidate
-        .left_outer_joins(:application_forms)
-        .where(application_forms: { recruitment_cycle_year: RecruitmentCycle.current_year })
-        .or(Candidate.where('candidates.created_at > ? ', CycleTimetable.apply_1_deadline(RecruitmentCycle.previous_year)))
-        .distinct
-        .includes(application_forms: :application_choices)
-        .where('candidate_api_updated_at > ?', updated_since)
-        .order('candidates.candidate_api_updated_at DESC')
-      end
     end
 
     def paginate(scope)
--- spec/requests/candidate_api/get_candidates_v1_2_spec.rb
+++ spec/requests/candidate_api/get_candidates_v1_2_spec.rb
@@ -16,6 +16,7 @@ RSpec.describe 'GET /candidate-api/v1.2/candidates', type: :request do
       :completed_application_form,
       2,
       candidate: candidate,
+      application_choices_count: 3,
     )
 
     get_api_request "#{root_path}?updated_since=#{CGI.escape(1.day.ago.iso8601)}", token: candidate_api_token
@@ -29,11 +30,13 @@ RSpec.describe 'GET /candidate-api/v1.2/candidates', type: :request do
     expect(response_data.first['application_status']).to eq(ProcessState.new(application_forms.first).state.to_s)
     expect(response_data.first['recruitment_cycle_year']).to eq(application_forms.first.recruitment_cycle_year)
     expect(response_data.first['submitted_at']).to eq(application_forms.first.submitted_at.iso8601)
+    expect(response_data.first['application_choices'].count).to eq(3)
 
     expect(response_data.second['id']).to eq(application_forms.second.id)
     expect(response_data.second['application_phase']).to eq(application_forms.second.phase)
     expect(response_data.second['application_status']).to eq(ProcessState.new(application_forms.second).state.to_s)
     expect(response_data.second['recruitment_cycle_year']).to eq(application_forms.second.recruitment_cycle_year)
     expect(response_data.second['submitted_at']).to eq(application_forms.second.submitted_at.iso8601)
+    expect(response_data.second['application_choices'].count).to eq(3)
   end
 end
