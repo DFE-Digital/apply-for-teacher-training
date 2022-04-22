bdfeaac29333540ce6cf8ee3ab71f593b2d2e887
Tomas D'Stefano <tomas_stefano@successoft.com>
2022-04-22 12:03:10 -0300

    Ignore the postcode if postcode is NULL when get dup matches
    Change approach to our duplicate matching when candidate is international.
    
    There is an international candidate (no postcode) so it didn’t appear on the dup match list.

--- app/queries/get_duplicate_matches.rb
+++ app/queries/get_duplicate_matches.rb
@@ -10,21 +10,21 @@ class GetDuplicateMatches
           submitted_at
         FROM application_forms application_details
         JOIN(
-          SELECT TRIM(UPPER(application_forms.last_name)) last_name, application_forms.date_of_birth, REPLACE(UPPER(application_forms.postcode), ' ', '') postcode
+          SELECT TRIM(UPPER(application_forms.last_name)) last_name, application_forms.date_of_birth, COALESCE(REPLACE(UPPER(application_forms.postcode), ' ', ''), '') postcode
           FROM application_forms
           WHERE application_forms.previous_application_form_id IS NULL
           GROUP BY TRIM(UPPER(application_forms.last_name)), application_forms.date_of_birth, REPLACE(UPPER(application_forms.postcode), ' ', '')
           HAVING (count(*) > 1)
         ) duplicate_attributes
-        ON REPLACE(UPPER(application_details.postcode), ' ', '') = duplicate_attributes.postcode
+        ON COALESCE(REPLACE(UPPER(application_details.postcode), ' ', ''), '') = duplicate_attributes.postcode
         AND application_details.date_of_birth = duplicate_attributes.date_of_birth
         AND TRIM(UPPER(application_details.last_name)) = duplicate_attributes.last_name
         JOIN(
-          SELECT TRIM(UPPER(application_forms.last_name)) last_name, application_forms.date_of_birth, REPLACE(UPPER(application_forms.postcode), ' ', '') postcode
+          SELECT TRIM(UPPER(application_forms.last_name)) last_name, application_forms.date_of_birth, COALESCE(REPLACE(UPPER(application_forms.postcode), ' ', ''), '') postcode
           FROM application_forms
           WHERE application_forms.previous_application_form_id IS NULL
         ) duplicate_submitted_attributes
-        ON REPLACE(UPPER(application_details.postcode), ' ', '') = duplicate_submitted_attributes.postcode
+        ON COALESCE(REPLACE(UPPER(application_details.postcode), ' ', ''), '') = duplicate_submitted_attributes.postcode
         AND application_details.date_of_birth = duplicate_submitted_attributes.date_of_birth
         AND TRIM(UPPER(application_details.last_name)) = duplicate_submitted_attributes.last_name
         JOIN(
