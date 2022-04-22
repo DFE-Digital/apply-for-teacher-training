639d5d5b4351cb504978f141d8599561fb3d8a17
Steve Hook <steve.hook@gmail.com>
2022-04-22 15:43:40 +0100

    Generalise the implementation of NudgeUnsubmittedCandidatesWorker
    Make it possible to add further nudges defined by a query object, mailer
    action and option feature flag.

--- app/workers/nudge_unsubmitted_candidates_worker.rb
+++ app/workers/nudge_unsubmitted_candidates_worker.rb
@@ -2,19 +2,29 @@
 class NudgeUnsubmittedCandidatesWorker
   include Sidekiq::Worker
 
-  def perform
-    return unless FeatureFlag.active?(:candidate_nudge_emails)
+  Nudge = Struct.new(:query_class, :mailer_action, :feature_flag)
+  NUDGES = [
+    Nudge.new(
+      GetUnsubmittedApplicationsReadyToNudge,
+      :nudge_unsubmitted,
+      :candidate_nudge_emails,
+    ),
+  ].freeze
 
-    GetUnsubmittedApplicationsReadyToNudge.new.call.each do |application_form|
-      send_nudge(application_form)
+  def perform
+    NUDGES.each do |nudge|
+      next unless nudge.feature_flag.nil? || FeatureFlag.active?(nudge.feature_flag)
+
+      nudge.query_class.new.call.each do |application_form|
+        send_nudge(nudge.mailer_action, application_form)
+      end
     end
   end
 
   # This method can be run manually via a Rails console to perform a dry-run to
   # see who will be sent a nudge email without actually sending anything.
-  # It can be deleted when the `candidate_nudge_emails` feature flag is deleted.
-  def dry_run
-    GetUnsubmittedApplicationsReadyToNudge.new.call.find_each do |application_form|
+  def dry_run(nudge)
+    nudge.query_class.new.call.find_each do |application_form|
       # rubocop:disable Rails/Output
       puts "Sending email for application form #{Rails.application.routes.url_helpers.support_interface_application_form_url(application_form.id)}"
       # rubocop:enable Rails/Output
@@ -23,7 +33,7 @@ class NudgeUnsubmittedCandidatesWorker
 
 private
 
-  def send_nudge(application_form)
-    CandidateMailer.nudge_unsubmitted(application_form).deliver_later
+  def send_nudge(mailer_action, application_form)
+    CandidateMailer.send(mailer_action, application_form).deliver_later
   end
 end
