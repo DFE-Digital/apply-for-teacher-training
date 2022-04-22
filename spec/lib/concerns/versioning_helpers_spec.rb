ad24d241fb8868893725f7560b563519cb0f283b
Carlos Martinez <carlosjmtz@gmail.com>
2022-04-22 16:37:33 +0100

    Remove unnecessary env-specific tests

--- spec/lib/concerns/versioning_helpers_spec.rb
+++ spec/lib/concerns/versioning_helpers_spec.rb
@@ -18,34 +18,8 @@ RSpec.describe VersioningHelpers do
     end
 
     context 'no minor version specified' do
-      context 'production env' do
-        before { allow(HostingEnvironment).to receive(:production?).and_return(true) }
-
-        it 'returns the latest released minor version' do
-          expect(minor_version_number('1')).to eq 1
-        end
-      end
-
-      context 'sandbox env' do
-        before do
-          allow(HostingEnvironment).to receive(:production?).and_return(false)
-          allow(HostingEnvironment).to receive(:sandbox_mode?).and_return(true)
-        end
-
-        it 'returns the default minor version' do
-          expect(minor_version_number('1')).to eq 2
-        end
-      end
-
-      context 'development env' do
-        before do
-          allow(HostingEnvironment).to receive(:production?).and_return(false)
-          allow(HostingEnvironment).to receive(:sandbox_mode?).and_return(false)
-        end
-
-        it 'returns the latest minor version' do
-          expect(minor_version_number('1')).to eq 3
-        end
+      it 'returns the latest released minor version' do
+        expect(minor_version_number('1')).to eq 1
       end
     end
   end
