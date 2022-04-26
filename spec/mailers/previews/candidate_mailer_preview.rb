23a5ab39acb0dd1ece4d96f1a878dffa231d0fc1
Steve Hook <steve.hook@gmail.com>
2022-04-26 14:29:14 +0100

    Add previews for nudge emails

--- spec/mailers/previews/candidate_mailer_preview.rb
+++ spec/mailers/previews/candidate_mailer_preview.rb
@@ -842,6 +842,16 @@ class CandidateMailerPreview < ActionMailer::Preview
     CandidateMailer.nudge_unsubmitted(application_form)
   end
 
+  def nudge_unsubmitted_with_incomplete_courses
+    application_form = FactoryBot.create(:completed_application_form)
+    CandidateMailer.nudge_unsubmitted_with_incomplete_courses(application_form)
+  end
+
+  def nudge_unsubmitted_with_incomplete_personal_statement
+    application_form = FactoryBot.create(:completed_application_form)
+    CandidateMailer.nudge_unsubmitted_with_incomplete_personal_statement(application_form)
+  end
+
 private
 
   def candidate
