747ddd4700eac87c8c1f266313bf92b6894b893e
Steve Laing <steve.laing@gmail.com>
2022-04-28 15:01:08 +0100

    Remove subheading tag logic
    Legacy reasons for rejection are no longer editable, so remove ancilliary logic around tags we display for legacy headings.
    The current R4R presentation doesn't use this logic.

--- app/components/shared/rejection_reasons/structured_rejection_reasons_component.rb
+++ app/components/shared/rejection_reasons/structured_rejection_reasons_component.rb
@@ -20,10 +20,6 @@ class RejectionReasons::StructuredRejectionReasonsComponent < ViewComponent::Bas
     input.split("\r\n")
   end
 
-  def subheading_tag_name
-    editable? ? :h2 : :h3
-  end
-
   def link_to_find_when_rejected_on_qualifications(application_choice)
     link = govuk_link_to(
       'Find postgraduate teacher training courses',
