84ff732154071f8f67036bd2055046800e4b0851
Steve Laing <steve.laing@gmail.com>
2022-05-03 11:26:02 +0100

    Restrict editable function to current SR4R component
    The only place we edit reasons for rejection is when checking the values on the last wizard step, we do not need to support this in other R4R components so remove and restrict to the current R4R wizard implementation.

--- app/components/shared/rejection_reasons/reasons_for_rejection_component.rb
+++ app/components/shared/rejection_reasons/reasons_for_rejection_component.rb
@@ -2,8 +2,4 @@
 # This component class supports the rendering of rejection reasons from the initial iteration of structured rejection reasons.
 # See https://github.com/DFE-Digital/apply-for-teacher-training/blob/main/docs/reasons-for-rejection.md
 #
-class RejectionReasons::ReasonsForRejectionComponent < RejectionReasons::StructuredRejectionReasonsComponent
-  def editable?
-    false
-  end
-end
+class RejectionReasons::ReasonsForRejectionComponent < RejectionReasons::StructuredRejectionReasonsComponent; end
--- app/components/shared/rejection_reasons/rejection_reasons_component.rb
+++ app/components/shared/rejection_reasons/rejection_reasons_component.rb
@@ -3,6 +3,22 @@
 # See https://github.com/DFE-Digital/apply-for-teacher-training/blob/main/docs/reasons-for-rejection.md
 #
 class RejectionReasons::RejectionReasonsComponent < RejectionReasons::StructuredRejectionReasonsComponent
+  attr_reader :editable
+
+  def initialize(application_choice:, reasons:, editable: false, render_link_to_find_when_rejected_on_qualifications: false)
+    super(
+      application_choice: application_choice,
+      reasons: reasons,
+      render_link_to_find_when_rejected_on_qualifications: render_link_to_find_when_rejected_on_qualifications,
+    )
+
+    @editable = editable
+  end
+
+  def editable?
+    editable
+  end
+
   def render_link_to_find?(reason)
     reason.id == 'qualifications' && @render_link_to_find_when_rejected_on_qualifications
   end
