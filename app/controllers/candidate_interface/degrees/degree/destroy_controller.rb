a3d41a5dec837c82975094aa5275ad314abb508b
Jonathan Filar <jonathanfilar@hotmail.com>
2022-05-06 15:56:41 +0100

    Clear redis after deleting final degree

--- app/controllers/candidate_interface/degrees/degree/destroy_controller.rb
+++ app/controllers/candidate_interface/degrees/degree/destroy_controller.rb
@@ -11,9 +11,11 @@ module CandidateInterface
 
         def destroy
           current_degree.destroy!
+          @wizard = DegreeWizard.new(degree_store)
 
           if current_application.application_qualifications.degrees.blank?
             current_application.update!(degrees_completed: nil)
+            @wizard.clear_state!
           end
           redirect_to candidate_interface_new_degree_review_path
         end
@@ -22,6 +24,11 @@ module CandidateInterface
 
         def redirect_to_old_degree_flow_unless_feature_flag_is_active
           redirect_to candidate_interface_new_degree_path unless FeatureFlag.active?(:new_degree_flow)
+        end
+
+        def degree_store
+          key = "degree_wizard_store_#{current_user.id}_#{current_application.id}"
+          WizardStateStores::RedisStore.new(key: key)
         end
       end
     end
