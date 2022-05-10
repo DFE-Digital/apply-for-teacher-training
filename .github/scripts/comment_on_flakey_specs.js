d2456bf28d9d17399bed7b586405a942e82e2d61
Steve Laing <steve.laing@gmail.com>
2022-05-10 16:22:34 +0100

    Parse JSON data from flakey spec log

--- .github/scripts/comment_on_flakey_specs.js
+++ .github/scripts/comment_on_flakey_specs.js
@@ -11,24 +11,14 @@ module.exports = ({github, context}) => {
     const { issue: { number: issue_number }, repo: { owner, repo } } = context;
     const heading = 'You have one or more flakey tests on this branch!';
     let commentBody = `<h2>${heading} :snowflake: :snowflake: :snowflake:</h2>`;
-    let dataStr, dataAry, attempts, retryCount, errorLocation, errorPath, errorMessages, errorLink;
     let branchName = GITHUB_HEAD_REF.split('/').pop();
     let createComment = false;
-    let rows = FLAKEY_TEST_DATA.split("\n");
-    let rowsLength = rows.length;
-    for (var idx = 0; idx < rowsLength; idx++) {
-      dataStr = rows[idx];
-      if (dataStr.length) {
-        dataAry = dataStr.split(',');
-        if (dataAry.length >= 4) {
-          [attempts, retryCount, errorLocation, ...errorMessages] = dataAry;
-          errorPath = `/${owner}/${repo}/blob/${branchName}/${errorLocation.replace(':', '#L')}`;
-          errorLink = `<a href="${errorPath}">${errorLocation}</a>`;
-          commentBody += `Failed ${attempts} out of ${retryCount} times at ${errorLink}: :warning: ${errorMessages.toString()}<br>`;
-          createComment = true;
-        }
-      }
-    }
+    JSON.parse(FLAKEY_TEST_DATA).forEach(function(error) {
+      let errorPath = `/${owner}/${repo}/blob/${branchName}/${error['location'].replace(':', '#L')}`;
+      let errorLink = `<a href="${errorPath}">${error['location']}</a>`;
+      commentBody += `Failed ${error['attempts']} out of ${error['retry_count']} times at ${errorLink}: :warning: ${error['messages'].toString()}<br>`;
+      createComment = true;
+    })
     if (createComment) {
       github.issues.createComment({ issue_number, owner, repo, body: commentBody });
       throw new FlakeySpecError(heading);
