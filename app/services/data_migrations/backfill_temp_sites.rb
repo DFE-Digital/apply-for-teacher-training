8c9c05fc78a5c507ad23f9b132766c905b3ef2f4
Carlos Martinez <carlosjmtz@gmail.com>
2022-05-23 11:59:57 +0100

    Move initialize_with_generated_uuid logic into BackfillTempSites migration
    So that we don't have it running on an ongoing basis as part of the
    sites sync

--- app/services/data_migrations/backfill_temp_sites.rb
+++ app/services/data_migrations/backfill_temp_sites.rb
@@ -34,11 +34,33 @@ module DataMigrations
       ).includes(:location_status).paginate(per_page: 500)
 
       sites_from_api.each do |site_from_api|
-        temp_site = TeacherTrainingPublicAPI::AssignTempSiteAttributes.new(site_from_api, provider).call
+        temp_site = if site_from_api.uuid.present?
+                      TeacherTrainingPublicAPI::AssignTempSiteAttributes.new(site_from_api, provider).call
+                    else
+                      initialize_with_generated_uuid(site_from_api, provider)
+                    end
         temp_site.save!
 
         attach_to_course_options(site_from_api.code, temp_site, Course.find_by(code: course_from_api.code))
       end
+    end
+
+    def initialize_with_generated_uuid(site_from_api, provider)
+      TempSite.new(
+        uuid: SecureRandom.uuid,
+        uuid_generated_by_apply: true,
+        name: site_from_api.name,
+        code: site_from_api.code,
+        provider: provider,
+        address_line1: site_from_api.street_address_1&.strip,
+        address_line2: site_from_api.street_address_2&.strip,
+        address_line3: site_from_api.city&.strip,
+        address_line4: site_from_api.county&.strip,
+        postcode: site_from_api.postcode&.strip,
+        region: site_from_api.region_code&.strip,
+        latitude: site_from_api.latitude,
+        longitude: site_from_api.longitude,
+      )
     end
 
     def attach_to_course_options(site_code, temp_site, course)
