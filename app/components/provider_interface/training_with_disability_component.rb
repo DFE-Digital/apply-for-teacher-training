09d1cc7c39db0e6fc7610af1d7cd87d27aca0f97
Tomas D'Stefano <tomas_stefano@successoft.com>
2022-05-26 01:59:57 -0300

    Add new disability support section

--- app/components/provider_interface/training_with_disability_component.rb
+++ app/components/provider_interface/training_with_disability_component.rb
@@ -3,15 +3,34 @@ module ProviderInterface
     include ViewHelper
 
     attr_reader :application_form
+    delegate :disclose_disability?, :disability_disclosure, to: :application_form
 
     def initialize(application_form:)
       @application_form = application_form
     end
 
-    def disability_disclosure
-      return application_form.disability_disclosure if application_form.disclose_disability? && application_form.disability_disclosure.present?
+    def rows
+      rows = [{ key: 'Do you want to ask for help to become a teacher?', value: disability_disclosure_support }]
 
-      'No information shared.'
+      if disclose_information?
+        rows << { key: 'Give any relevant information', value: disability_disclosure }
+      end
+
+      rows
+    end
+
+  private
+
+    def disclose_information?
+      disclose_disability? && disability_disclosure.present?
+    end
+
+    def disability_disclosure_support
+      if disclose_information?
+        'Yes, I want to share information about myself so my provider can take steps to support me'
+      else
+        'No'
+      end
     end
   end
 end
--- spec/components/provider_interface/training_with_disability_component_spec.rb
+++ spec/components/provider_interface/training_with_disability_component_spec.rb
@@ -1,43 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe ProviderInterface::TrainingWithDisabilityComponent do
-  it 'renders `No information shared` if `#disclose_disability` is false' do
-    application_form = instance_double(
-      ApplicationForm,
-      disclose_disability?: false,
-      disability_disclosure: nil,
-    )
-    result = render_inline(described_class.new(application_form: application_form))
-    expect(result.text).to include('No information shared.')
+  context 'when the candidate has not disclose disability support' do
+    it 'renders that no help is required' do
+      application_form = instance_double(
+        ApplicationForm,
+        disclose_disability?: false,
+        disability_disclosure: nil,
+      )
+      result = render_inline(described_class.new(application_form: application_form))
+      expect(result.text).to include('Do you want to ask for help to become a teacher?No')
+      expect(result.text).not_to include('Give any relevant information')
+    end
   end
 
-  it 'renders `No information shared` if `#disclose_disability` is true but there is no disclosure' do
-    application_form = instance_double(
-      ApplicationForm,
-      disclose_disability?: true,
-      disability_disclosure: '',
-    )
-    result = render_inline(described_class.new(application_form: application_form))
-    expect(result.text).to include('No information shared.')
+  context 'when the candidate has disclose disability support' do
+    it 'renders the disability disclosure' do
+      application_form = instance_double(
+        ApplicationForm,
+        disclose_disability?: true,
+        disability_disclosure: 'I am hard of hearing',
+      )
+      result = render_inline(described_class.new(application_form: application_form))
+      expect(result.text).to include('Do you want to ask for help to become a teacher?Yes, I want to share information about myself so my provider can take steps to support me')
+      expect(result.text).to include('Give any relevant informationI am hard of hearing')
+    end
   end
 
-  it 'renders `No information shared` if `#disclose_disability` is nil' do
-    application_form = instance_double(
-      ApplicationForm,
-      disclose_disability?: nil,
-      disability_disclosure: nil,
-    )
-    result = render_inline(described_class.new(application_form: application_form))
-    expect(result.text).to include('No information shared.')
-  end
-
-  it 'renders disclosure if `#disclose_disability` is true' do
-    application_form = instance_double(
-      ApplicationForm,
-      disclose_disability?: true,
-      disability_disclosure: 'I am hard of hearing',
-    )
-    result = render_inline(described_class.new(application_form: application_form))
-    expect(result.text).to include('I am hard of hearing')
+  context 'when the candidate has an empty disclose disability support' do
+    it 'renders that no help is required' do
+      application_form = instance_double(
+        ApplicationForm,
+        disclose_disability?: true,
+        disability_disclosure: '',
+      )
+      result = render_inline(described_class.new(application_form: application_form))
+      expect(result.text).to include('Do you want to ask for help to become a teacher?No')
+      expect(result.text).not_to include('Give any relevant information')
+    end
   end
 end
