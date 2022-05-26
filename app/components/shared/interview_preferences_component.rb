9631f8470c5f4e2cea46a5415d978ce8e96b1712
Tomas D'Stefano <tomas_stefano@successoft.com>
2022-05-26 02:02:19 -0300

    Add new interview preferences section

--- app/components/shared/interview_preferences_component.rb
+++ app/components/shared/interview_preferences_component.rb
@@ -1,14 +1,29 @@
 # NOTE: This component is used by both provider and support UIs
 class InterviewPreferencesComponent < ViewComponent::Base
   attr_reader :application_form
+  delegate :interview_preferences, to: :application_form
 
   def initialize(application_form:)
     @application_form = application_form
   end
 
-  def interview_preferences
-    return application_form.interview_preferences if application_form.interview_preferences.present?
+  def rows
+    rows = [{ key: 'Do you have any interview needs?', value: interview_needs_message }]
 
-    'None given.'
+    if application_form.interview_preferences.present?
+      rows << { key: 'What are your interview needs?', value: interview_preferences }
+    end
+
+    rows
+  end
+
+private
+
+  def interview_needs_message
+    if application_form.interview_preferences.present?
+      'Yes'
+    else
+      'No'
+    end
   end
 end
