8d6555f31940feb29ce749c913e6b5b55ae9818c
Tomas D'Stefano <tomas_stefano@successoft.com>
2022-05-30 15:35:16 -0300

    Do not set selected reference as false when we update feedback

---
+++ spec/requests/support_interface/update_reference_feedback_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe 'Support interface - POST /support/applications/:application_id/references/:reference_id/feedback', type: :request do
+  def support_user
+    @support_user ||= SupportUser.new(
+      email_address: 'alice@example.com',
+      dfe_sign_in_uid: 'ABC',
+    )
+  end
+
+  def set_support_user_permission
+    allow(SupportUser).to receive(:load_from_session).and_return(support_user)
+  end
+
+  it 'does not change the selected attribute' do
+    set_support_user_permission
+
+    application_form = create(:application_form)
+    reference = create(:reference, application_form: application_form, selected: true)
+
+    post(
+      support_interface_application_form_update_reference_feedback_path(
+        support_interface_application_forms_edit_reference_feedback_form: {
+          feedback: 'some feedback',
+          audit_comment: 'ticket',
+          send_emails: 'false',
+        },
+        application_form_id: application_form.id,
+        reference_id: reference.id,
+      ),
+    )
+
+    expect(reference.reload.selected).to be(true)
+    expect(response).to redirect_to(
+      support_interface_application_form_path(application_form),
+    )
+  end
+end
