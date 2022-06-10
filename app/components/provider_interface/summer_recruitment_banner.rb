23d5b9f089f9ebe7e2ce265bdf84edefd4cd6175
Tomas D'Stefano <tomas_stefano@successoft.com>
2022-06-10 15:45:08 +0100

    Use summer recruitment banner date from cycle timetable

--- app/components/provider_interface/summer_recruitment_banner.rb
+++ app/components/provider_interface/summer_recruitment_banner.rb
@@ -1,7 +1,7 @@
 module ProviderInterface
   class SummerRecruitmentBanner < ViewComponent::Base
     def render?
-      Time.zone.now <= CycleTimetable.apply_1_deadline && Time.zone.now >= Time.zone.local(Date.current.year, 7, 1)
+      CycleTimetable.show_summer_recruitment_banner?
     end
 
     def end_date
