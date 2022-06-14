e5c029c008e534000f80c2d484c8aaa20b7a7a05
James Glenn <47917431+JR-G@users.noreply.github.com>
2022-06-14 15:01:06 +0100

    Offers should never have a previous year value
    Refactor the validations to capture this

--- app/queries/get_course_option_from_codes.rb
+++ app/queries/get_course_option_from_codes.rb
@@ -12,6 +12,12 @@ class GetCourseOptionFromCodes
   # The act of validation also performs the relevant lookups and memoizes the results, which are then used
   # in subsequent validations.
 
+  validates_each :recruitment_cycle_year do |record, attr|
+    next if record.recruitment_cycle_year.blank?
+
+    record.errors.add(attr, 'The recruitment cycle year must be from the current year') unless record.recruitment_cycle_year == RecruitmentCycle.current_year
+  end
+
   validates_each :provider_code do |record, attr, value|
     next if record.provider_code.blank?
 
@@ -20,7 +26,7 @@ class GetCourseOptionFromCodes
   end
 
   validates_each :course_code do |record, attr, value|
-    next if record.course_code.blank? || record.recruitment_cycle_year.blank?
+    next if record.course_code.blank?
 
     if record.provider
       record.course ||= record.provider.courses.find_by(
@@ -53,7 +59,7 @@ class GetCourseOptionFromCodes
     course_code:,
     study_mode:,
     site_code:,
-    recruitment_cycle_year:
+    recruitment_cycle_year: RecruitmentCycle.current_year
   )
     @provider_code = provider_code
     @course_code = course_code
