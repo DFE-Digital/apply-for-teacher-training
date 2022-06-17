0d5a86deb7143bbda463084cda0e89780d308744
Jonathan Filar <jonathanfilar@hotmail.com>
2022-06-17 16:18:57 +0100

    Catch error and present message
    There is an error before the token is created as provider id is nil.
    There is now a warning message to support user letting them know they did
    not select a provider

---
+++ app/errors/missing_provider_error.rb
@@ -0,0 +1,1 @@
+class MissingProviderError < StandardError; end
--- spec/system/support_interface/api_tokens_spec.rb
+++ spec/system/support_interface/api_tokens_spec.rb
@@ -7,7 +7,11 @@ RSpec.feature 'Manage API tokens' do
     given_i_am_signed_in
     and_providers_exist
     when_i_visit_the_tokens_page
-    and_i_select_a_provider
+    and_i_click_on_create_a_token_without_entering_a_provider
+    then_i_am_redirected_to_the_support_page
+    and_a_warning_message_is_showing
+
+    when_i_select_a_provider
     and_i_click_on_create_a_token
     then_i_should_see_a_new_token
     and_i_am_able_to_connect_to_the_api_using_the_token
@@ -22,18 +26,27 @@ RSpec.feature 'Manage API tokens' do
     visit support_interface_api_tokens_path
   end
 
+  def then_i_am_redirected_to_the_support_page
+    expect(page).to have_current_path(support_interface_api_tokens_path)
+  end
+
+  def and_a_warning_message_is_showing
+    expect(page).to have_content 'Did not select a provider'
+  end
+
   def and_providers_exist
     create(:provider, name: 'Some Provider')
     create(:provider, name: 'Super Provider')
   end
 
-  def and_i_select_a_provider
+  def when_i_select_a_provider
     select 'Super Provider'
   end
 
   def and_i_click_on_create_a_token
     click_on 'Create new token'
   end
+  alias_method :and_i_click_on_create_a_token_without_entering_a_provider, :and_i_click_on_create_a_token
 
   def then_i_should_see_a_new_token
     expect(page).to have_content 'Your token is'
