6e384717a01a2888134b2485921d029062d5a353
Carlos Martinez <carlosjmtz@gmail.com>
2022-06-20 17:21:45 +0100

    unpause sync job

--- config/clock.rb
+++ config/clock.rb
@@ -10,9 +10,9 @@ class Clock
 
   # More-than-hourly jobs
 
-  # every(10.minutes, 'IncrementalSyncAllFromTeacherTrainingPublicAPI', skip_first_run: true) do
-  #   TeacherTrainingPublicAPI::SyncAllProvidersAndCoursesWorker.perform_async(true)
-  # end
+  every(10.minutes, 'IncrementalSyncAllFromTeacherTrainingPublicAPI', skip_first_run: true) do
+    TeacherTrainingPublicAPI::SyncAllProvidersAndCoursesWorker.perform_async(true)
+  end
 
   # Hourly jobs
 
