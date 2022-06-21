3ef75572cb716b6ad0393295b6ec07bc0acb6e3d
cwrw <abeer.salameh@gmail.com>
2022-06-21 16:39:52 +0100

    Add safety assured to making not null validations

--- db/migrate/20220621132939_validate_make_sites_uuid_not_nullable.rb
+++ db/migrate/20220621132939_validate_make_sites_uuid_not_nullable.rb
@@ -1,7 +1,7 @@
 class ValidateMakeSitesUuidNotNullable < ActiveRecord::Migration[7.0]
   def change
     validate_check_constraint :sites, name: 'sites_uuid_null'
-    change_column_null :sites, :uuid, false
+    safety_assured { change_column_null :sites, :uuid, false }
     remove_check_constraint :sites, name: 'sites_uuid_null'
   end
 end
--- db/migrate/20220621133542_validate_make_course_options_site_id_not_nullable.rb
+++ db/migrate/20220621133542_validate_make_course_options_site_id_not_nullable.rb
@@ -1,7 +1,7 @@
 class ValidateMakeCourseOptionsSiteIdNotNullable < ActiveRecord::Migration[7.0]
   def change
     validate_check_constraint :course_options, name: 'course_options_site_id_null'
-    change_column_null :course_options, :site_id, false
+    safety_assured { change_column_null :course_options, :site_id, false }
     remove_check_constraint :course_options, name: 'course_options_site_id_null'
   end
 end
