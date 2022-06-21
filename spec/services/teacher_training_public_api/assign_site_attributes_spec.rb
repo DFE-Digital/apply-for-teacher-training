17a5d603985c36928a21cafe14227cf7041c5bb2
James Glenn <47917431+JR-G@users.noreply.github.com>
2022-06-21 09:27:49 +0100

    Remove old site test

--- spec/services/teacher_training_public_api/assign_site_attributes_spec.rb
+++ spec/services/teacher_training_public_api/assign_site_attributes_spec.rb
@@ -28,14 +28,6 @@ RSpec.describe TeacherTrainingPublicAPI::AssignSiteAttributes do
     expect(site.longitude).to eq site_from_api.longitude.to_f
   end
 
-  context 'when the provider has the site' do
-    let!(:site) { create(:site, provider: provider, uuid: uuid) }
-
-    it 'finds a Site' do
-      expect(site.uuid).to eq site.uuid
-    end
-  end
-
   context 'when the provider does not have the site' do
     it 'creates a Site' do
       expect(site.uuid).to eq site_from_api.uuid
