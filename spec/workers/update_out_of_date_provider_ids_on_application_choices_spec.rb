50b6b098d1f476bb9b2809ccade92969b892930a
cwrw <abeer.salameh@gmail.com>
2022-06-23 16:12:58 +0100

    Add worker to update out of date provider ids on applications
    If an accredited provider changes in publish, existing applications
    to courses will hold stale provider id data. Add a worker which finds
    out of date applications and updates the provider ids appropriately.
    
    We do not want to trigger any callbacks on application choices not from
    this cycle, add an extra check and use update_columns to avoid callbacks
    which will amend the `updated_at` timestamp on an application choice.

---
+++ spec/workers/update_out_of_date_provider_ids_on_application_choices_spec.rb
@@ -0,0 +1,67 @@
+require 'rails_helper'
+
+RSpec.describe UpdateOutOfDateProviderIdsOnApplicationChoices, sidekiq: true, with_audited: true do
+  describe '#perform' do
+    let(:wrong_provider) { create(:provider) }
+    let(:accredited_course) { create(:course, :with_accredited_provider) }
+    let(:accredited_course_option) { create(:course_option, course: accredited_course) }
+    let!(:application_choice) do
+      create(:application_choice, course_option: accredited_course_option)
+    end
+
+    context 'when application choice provider ids are up to date' do
+      it 'does nothing if no application choices provider ids are out of date' do
+        expect { described_class.new.perform }.not_to change(application_choice, :provider_ids)
+      end
+    end
+
+    context 'when application choice in current cycle provider ids are out of date' do
+      before { application_choice.update!(provider_ids: [wrong_provider.id]) }
+
+      it 'updates application provider ids' do
+        expect { described_class.new.perform }
+          .to change { application_choice.reload.provider_ids }
+          .from([wrong_provider.id])
+          .to([accredited_course.provider.id, accredited_course.accredited_provider.id])
+      end
+
+      it 'amends updated_at timestamp on the application' do
+        updated_at_time = 1.day.from_now.change(usec: 0)
+
+        Timecop.freeze(updated_at_time) do
+          expect { described_class.new.perform }.to change { application_choice.reload.updated_at }.to(updated_at_time)
+        end
+      end
+
+      it 'adds an audit comment to application choice' do
+        described_class.new.perform
+        audit = application_choice.audits.last
+
+        expect(audit.comment)
+          .to eq('Update out of date providers on application choice due to provider change')
+      end
+    end
+
+    context 'when application choice in previous cycle provider ids are out of date' do
+      let(:accredited_course) { create(:course, :with_accredited_provider, :previous_year) }
+
+      before { application_choice.update!(provider_ids: [wrong_provider.id]) }
+
+      it 'updates application provider ids' do
+        expect { described_class.new.perform }
+          .to change { application_choice.reload.provider_ids }
+          .from([wrong_provider.id])
+          .to([accredited_course.provider.id, accredited_course.accredited_provider.id])
+      end
+
+      it 'does not amend updated_at timestamp on the application' do
+        updated_at_time = 1.day.before
+        application_choice.update!(updated_at: updated_at_time)
+
+        described_class.new.perform
+
+        expect(application_choice.reload.updated_at).to be_within(1.second).of(updated_at_time)
+      end
+    end
+  end
+end
