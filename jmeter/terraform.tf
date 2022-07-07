0f9f5a2f0d22e876be33decb53f22c0f1045791d
Stephen Neal <smneal@users.noreply.github.com>
2022-07-07 20:49:54 +0100

    Update azurerm provider 3.12.0
    Update the azurerm provider so we can maintain
    compatibility and include latest bug fixes.

--- jmeter/terraform.tf
+++ jmeter/terraform.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.53.0"
+      version = "3.12.0"
     }
     cloudfoundry = {
       source  = "cloudfoundry-community/cloudfoundry"
