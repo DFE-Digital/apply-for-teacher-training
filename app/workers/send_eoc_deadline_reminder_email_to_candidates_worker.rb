31ac4fd8d9dd6d430134f6cfafb05721890b24ba
Tomas D'Stefano <tomas_stefano@successoft.com>
2022-07-07 16:04:19 +0100

    Create batch delivery so it can stagger batches over a period of time
    This can help the workers that need to process between 40k-100k records
    and send email to each of them.

--- app/workers/send_eoc_deadline_reminder_email_to_candidates_worker.rb
+++ app/workers/send_eoc_deadline_reminder_email_to_candidates_worker.rb
@@ -1,30 +1,16 @@
 class SendEocDeadlineReminderEmailToCandidatesWorker
   include Sidekiq::Worker
 
-  STAGGER_OVER = 5.hours
   BATCH_SIZE = 120
 
   def perform
     return unless CycleTimetable.need_to_send_deadline_reminder?
 
-    next_batch_time = Time.zone.now
-
-    GetApplicationsToSendDeadlineRemindersTo.call.find_in_batches(batch_size: BATCH_SIZE) do |applications|
+    BatchDelivery.new(relation: GetApplicationsToSendDeadlineRemindersTo.call, batch_size: BATCH_SIZE).each do |batch_time, records|
       SendEocDeadlineReminderEmailToCandidatesBatchWorker.perform_at(
-        next_batch_time,
-        applications.pluck(:id),
+        batch_time,
+        records.pluck(:id),
       )
-
-      next_batch_time += interval_between_batches
-    end
-  end
-
-private
-
-  def interval_between_batches
-    @interval_between_batches ||= begin
-      number_of_batches = (GetApplicationsToSendDeadlineRemindersTo.call.count.to_f / BATCH_SIZE).ceil
-      number_of_batches < 2 ? STAGGER_OVER : STAGGER_OVER / (number_of_batches - 1).to_f
     end
   end
 end
---
+++ spec/services/batch_delivery_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+RSpec.describe BatchDelivery do
+  describe 'Staggered email sending' do
+    around do |example|
+      Timecop.freeze do
+        example.run
+      end
+    end
+
+    let(:stagger_over) { 5.hours }
+
+    before do
+      @candidates = double
+
+      allow(@candidates).to receive(:count).and_return(300)
+      allow(@candidates).to receive(:find_in_batches).and_yield(
+        (1..120).map { |id| Candidate.new(id: id) },
+      ).and_yield(
+        (121..240).map { |id| Candidate.new(id: id) },
+      ).and_yield(
+        (241..300).map { |id| Candidate.new(id: id) },
+      )
+      allow(SendFindHasOpenedEmailToCandidatesBatchWorker).to receive(:perform_at)
+    end
+
+    it 'queues three staggered SendFindHasOpenedEmailToCandidatesBatchWorker jobs' do
+      described_class.new(relation: @candidates).each do |batch_time, candidates|
+        SendFindHasOpenedEmailToCandidatesBatchWorker.perform_at(
+          batch_time,
+          candidates.pluck(:id),
+        )
+      end
+
+      expect(SendFindHasOpenedEmailToCandidatesBatchWorker).to(
+        have_received(:perform_at).with(Time.zone.now, (1..120).to_a),
+      )
+      expect(SendFindHasOpenedEmailToCandidatesBatchWorker).to(
+        have_received(:perform_at).with(Time.zone.now + (stagger_over / 2.0), (121..240).to_a),
+      )
+      expect(SendFindHasOpenedEmailToCandidatesBatchWorker).to(
+        have_received(:perform_at).with(Time.zone.now + stagger_over, (241..300).to_a),
+      )
+    end
+  end
+end
