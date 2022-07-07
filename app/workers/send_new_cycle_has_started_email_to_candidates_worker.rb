c89b4152a0747ad4fdac1162b8a6f325dbebdf35
James Glenn <47917431+JR-G@users.noreply.github.com>
2022-07-07 16:39:51 +0100

    Use BatchDelivery service in the new cycle has started worker

--- app/workers/send_new_cycle_has_started_email_to_candidates_worker.rb
+++ app/workers/send_new_cycle_has_started_email_to_candidates_worker.rb
@@ -1,29 +1,16 @@
 class SendNewCycleHasStartedEmailToCandidatesWorker
   include Sidekiq::Worker
 
-  STAGGER_OVER = 5.hours
   BATCH_SIZE = 120
 
   def perform
-    if CycleTimetable.send_new_cycle_has_started_email?
-      next_batch_time = Time.zone.now
+    return unless CycleTimetable.send_new_cycle_has_started_email?
 
-      GetUnsuccessfulAndUnsubmittedCandidates.call.find_in_batches(batch_size: BATCH_SIZE) do |candidates|
-        SendNewCycleHasStartedEmailToCandidatesBatchWorker.perform_at(
-          next_batch_time,
-          candidates.pluck(:id),
-        )
-        next_batch_time += interval_between_batches
-      end
-    end
-  end
-
-private
-
-  def interval_between_batches
-    @interval_between_batches ||= begin
-      number_of_batches = (GetUnsuccessfulAndUnsubmittedCandidates.call.count.to_f / BATCH_SIZE).ceil
-      number_of_batches < 2 ? STAGGER_OVER : STAGGER_OVER / (number_of_batches - 1).to_f
+    BatchDelivery.new(relation: GetUnsuccessfulAndUnsubmittedCandidates.call, batch_size: BATCH_SIZE).each do |batch_time, records|
+      SendNewCycleHasStartedEmailToCandidatesBatchWorker.perform_at(
+        batch_time,
+        records.pluck(:id),
+      )
     end
   end
 end
