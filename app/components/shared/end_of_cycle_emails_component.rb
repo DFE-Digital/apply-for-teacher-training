9caff79d3c8bbff79fc1de6ef0cd466b0665c550
Tomas D'Stefano <tomas_stefano@successoft.com>
2022-07-13 15:27:01 +0100

    Move the providers scope to notify about find to its own class

--- app/components/shared/end_of_cycle_emails_component.rb
+++ app/components/shared/end_of_cycle_emails_component.rb
@@ -39,17 +39,11 @@ class EndOfCycleEmailsComponent < ViewComponent::Base
   end
 
   def apply_1_candidates
-    ApplicationForm
-    .joins(:candidate)
-    .where(submitted_at: nil, phase: 'apply_1', recruitment_cycle_year: RecruitmentCycle.current_year)
-    .where.not(candidate: { unsubscribed_from_emails: true }).count
+    GetApplicationsToSendDeadlineRemindersTo.deadline_reminder_candidates_apply_1.count
   end
 
   def apply_2_candidates
-    ApplicationForm
-    .joins(:candidate)
-    .where(submitted_at: nil, phase: 'apply_2', recruitment_cycle_year: RecruitmentCycle.current_year)
-    .where.not(candidate: { unsubscribed_from_emails: true }).count
+    GetApplicationsToSendDeadlineRemindersTo.deadline_reminder_candidates_apply_2.count
   end
 
   def candidates_to_notify_about_find_and_apply
@@ -57,24 +51,6 @@ class EndOfCycleEmailsComponent < ViewComponent::Base
   end
 
   def providers_to_notify_about_find_and_apply
-    Provider
-      .joins('INNER JOIN provider_users_providers ON providers.id = provider_users_providers.provider_id')
-      .joins('INNER JOIN provider_users ON provider_users.id = provider_users_providers.provider_user_id')
-      .where.not(Arel.sql(providers_whose_users_have_been_chased))
-      .order('providers.name')
-      .distinct
-      .count
-  end
-
-  def providers_whose_users_have_been_chased
-    <<-SQL.squish
-      EXISTS(
-        SELECT 1
-        FROM chasers_sent
-        WHERE chased_type = 'Provider'
-        AND chased_id = providers.id
-        AND chaser_type = 'find_service_open_organisation_notification'
-      )
-    SQL
+    GetProvidersToNotifyAboutFindAndApply.call.count
   end
 end
