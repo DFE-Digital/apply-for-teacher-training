f49d31952e8cad36a634af0cc7d33753c42cd948
Tomas D'Stefano <tomas_stefano@successoft.com>
2022-07-14 11:29:32 +0100

    Use locales to show cycle timeline event description

--- app/components/shared/end_of_cycle_timeline_component.rb
+++ app/components/shared/end_of_cycle_timeline_component.rb
@@ -5,18 +5,6 @@ class EndOfCycleTimelineComponent < ViewComponent::Base
     show_summer_recruitment_banner: 'Summer recruitment period starts',
     reject_by_default: 'Applications are automatically rejected',
   }.freeze
-  DESCRIPTION = {
-    find_opens: 'Candidates can browse and add courses (current cycle)',
-    apply_opens: 'Candidates can now submit their applications (current cycle)',
-    show_deadline_banner: 'Candidates are alerted of the upcoming deadlines',
-    show_summer_recruitment_banner: 'Providers are alerted that applications will be automatically rejected if they don‘t make a decision within 20 working days',
-    apply_1_deadline: 'Candidates who have an Apply 1 application can no longer continue with it but can start preparing it for the new cycle',
-    apply_2_deadline: 'Candidates who have an Apply 2 application can no longer continue with it but can start preparing it for the new cycle',
-    reject_by_default: 'All applications are automatically rejected',
-    find_closes: 'Candidates can no longer browse or add courses',
-    find_reopens: 'Candidates can browse and add courses from the new cycle',
-    apply_reopens: 'Candidates can now submit their applications for the new cycle',
-  }.freeze
 
   def initialize
     @cycle_timetable = CycleTimetable::CYCLE_DATES[CycleTimetable.current_year].merge(
@@ -34,7 +22,7 @@ class EndOfCycleTimelineComponent < ViewComponent::Base
       Timetable.new(
         name: ALTERNATIVE_NAMES[key] || key.to_s.humanize,
         date: value.to_fs(:govuk_date_and_time),
-        description: DESCRIPTION[key],
+        description: I18n.t("cycle_timeline.#{key}"),
       )
     end.compact
   end
