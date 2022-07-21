33a294b135db958d9027fb29998d538ea57f3e52
Tomas D'Stefano <tomas_stefano@successoft.com>
2022-07-21 18:32:47 +0100

    Add dry run to the provider migration
    The provider ask in detail about this migration so I created this dry
    run so I can pass directly to them.

--- app/services/data_migrations/provider_interview_data_fix.rb
+++ app/services/data_migrations/provider_interview_data_fix.rb
@@ -4,11 +4,7 @@ module DataMigrations
     MANUAL_RUN = false
 
     def change
-      provider = Provider.find_by(name: 'The Manchester Metropolitan University')
-
-      Interview
-        .where(provider_id: provider.id)
-        .where("location LIKE 'An email%'").each do |interview|
+      interviews.each do |interview|
         interview.update_columns(
           location: '',
           additional_details: additional_details_for(interview),
@@ -16,13 +12,45 @@ module DataMigrations
       end
     end
 
+    # rubocop:disable Rails/Output
+    def dry_run
+      interviews.each do |interview|
+        puts '-' * 80
+        puts "Application number #{interview.application_choice.id}"
+        puts '-' * 80
+        puts
+
+        if interview.additional_details.present?
+          puts
+          puts 'Move location value below AMENDING/ADDING to the additional details column:'
+          puts
+          puts "'#{additional_details_for(interview)}'"
+        else
+          puts 'Moving location value below to additional details column:'
+          puts
+          puts "'#{interview.location}'"
+        end
+
+        puts
+      end
+    end
+  # rubocop:enable Rails/Output
+
   private
+
+    def provider
+      Provider.find_by(name: 'The Manchester Metropolitan University')
+    end
+
+    def interviews
+      Interview.includes(:application_choice).where(provider_id: provider.id).where("location LIKE 'An email%'")
+    end
 
     def additional_details_for(interview)
       if interview.additional_details.blank?
         interview.location
       else
-        "#{interview.additional_details} #{interview.location}"
+        "#{interview.additional_details}\n\n#{interview.location}"
       end
     end
   end
