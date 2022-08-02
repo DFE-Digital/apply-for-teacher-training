87c31f81cfcf31f8266502260f9d326584631707
James Glenn <47917431+JR-G@users.noreply.github.com>
2022-08-02 09:05:41 +0100

    Add the new references section on the Application review page
    Co-authored-by: Tomas D'Stefano <tomas_stefano@successoft.com>

--- spec/components/candidate_interface/new_references_review_component_spec.rb
+++ spec/components/candidate_interface/new_references_review_component_spec.rb
@@ -1,65 +1,106 @@
 require 'rails_helper'
 
 RSpec.describe CandidateInterface::NewReferencesReviewComponent, type: :component do
-  it 'renders the referee name and email' do
-    reference = create(:reference, :not_requested_yet)
-    result = render_inline(described_class.new(references: [reference]))
+  let(:application_form) { create(:application_form) }
 
-    name_row = result.css('.govuk-summary-list__row')[1].text
-    email_row = result.css('.govuk-summary-list__row')[2].text
-    expect(name_row).to include 'Name'
-    expect(name_row).to include reference.name
-    expect(email_row).to include 'Email'
-    expect(email_row).to include reference.email_address
-  end
+  context 'when on application review page' do
+    context 'when application has zero references' do
+      it 'renders add references message' do
+        result = render_inline(described_class.new(application_form: application_form, references: []))
 
-  it 'renders the reference type' do
-    reference = create(:reference, :not_requested_yet, referee_type: :school_based)
-    result = render_inline(described_class.new(references: [reference]))
-
-    type_row = result.css('.govuk-summary-list__row')[0].text
-    expect(type_row).to include 'Type'
-    expect(type_row).to include 'School-based'
-  end
-
-  it 'renders the relationship' do
-    reference = create(:reference, :not_requested_yet)
-    result = render_inline(described_class.new(references: [reference]))
-
-    relationship_row = result.css('.govuk-summary-list__row')[3].text
-    expect(relationship_row).to include 'Relationship to you'
-    expect(relationship_row).to include reference.relationship
-  end
-
-  it 'renders all references passed in' do
-    reference_one = create(:reference)
-    reference_two = create(:reference)
-
-    result = render_inline(described_class.new(references: [reference_one, reference_two]))
-    expect(result.text).to include reference_one.email_address
-    expect(result.text).to include reference_two.email_address
-  end
-
-  context 'when a reference is carried over' do
-    context 'when the state is feedback_provided' do
-      it 'renders a status row' do
-        reference = create(:reference, :feedback_provided)
-        result = render_inline(described_class.new(references: [reference]))
-
-        status_row = result.css('.govuk-summary-list__row')[4].text
-        expect(status_row).to include 'Status'
-        expect(status_row).to include 'Reference received'
-        expect(status_row).to include "#{reference.name} will not be asked to give you another reference."
+        expect(result.text).to include('References not entered')
+        expect(result.css('a').map(&:text)).to include('Add references')
       end
     end
 
-    context 'when the state is not feedback_provided' do
-      it 'does not render a status row' do
-        reference = create(:reference, :feedback_requested)
-        result = render_inline(described_class.new(references: [reference]))
+    context 'when application has one references' do
+      it 'renders add references message' do
+        reference = create(:reference, :not_requested_yet, application_form: application_form)
+        result = render_inline(described_class.new(application_form: application_form, references: [reference]))
 
-        status_row = result.css('.govuk-summary-list__row')[4]
-        expect(status_row).to be_nil
+        expect(result.text).to include('You need to add 2 references')
+        expect(result.css('a').map(&:text)).to include('Add references')
+      end
+    end
+
+    context 'when application has two references but references section is incomplete' do
+      it 'renders incomplete message' do
+        application_form = create(:application_form, references_completed: false)
+        reference_one = create(:reference, :not_requested_yet, application_form: application_form)
+        reference_two = create(:reference, :not_requested_yet, application_form: application_form)
+
+        result = render_inline(described_class.new(application_form: application_form, references: [reference_one, reference_two]))
+
+        expect(result.text).to include('References section not marked as complete')
+        expect(result.css('a').map(&:text)).to include('Complete references section')
+      end
+    end
+
+    context 'when references section is complete' do
+    end
+  end
+
+  context 'when on review page' do
+    it 'renders the referee name and email' do
+      reference = create(:reference, :not_requested_yet, application_form: application_form)
+      result = render_inline(described_class.new(application_form: application_form, references: [reference]))
+
+      name_row = result.css('.govuk-summary-list__row')[1].text
+      email_row = result.css('.govuk-summary-list__row')[2].text
+      expect(name_row).to include 'Name'
+      expect(name_row).to include reference.name
+      expect(email_row).to include 'Email'
+      expect(email_row).to include reference.email_address
+    end
+
+    it 'renders the reference type' do
+      reference = create(:reference, :not_requested_yet, referee_type: :school_based, application_form: application_form)
+      result = render_inline(described_class.new(references: [reference], application_form: application_form))
+
+      type_row = result.css('.govuk-summary-list__row')[0].text
+      expect(type_row).to include 'Type'
+      expect(type_row).to include 'School-based'
+    end
+
+    it 'renders the relationship' do
+      reference = create(:reference, :not_requested_yet, application_form: application_form)
+      result = render_inline(described_class.new(references: [reference], application_form: application_form))
+
+      relationship_row = result.css('.govuk-summary-list__row')[3].text
+      expect(relationship_row).to include 'Relationship to you'
+      expect(relationship_row).to include reference.relationship
+    end
+
+    it 'renders all references passed in' do
+      reference_one = create(:reference, application_form: application_form)
+      reference_two = create(:reference, application_form: application_form)
+
+      result = render_inline(described_class.new(references: [reference_one, reference_two], application_form: application_form))
+      expect(result.text).to include reference_one.email_address
+      expect(result.text).to include reference_two.email_address
+    end
+
+    context 'when a reference is carried over' do
+      context 'when the state is feedback_provided' do
+        it 'renders a status row' do
+          reference = create(:reference, :feedback_provided, application_form: application_form)
+          result = render_inline(described_class.new(references: [reference], application_form: application_form))
+
+          status_row = result.css('.govuk-summary-list__row')[4].text
+          expect(status_row).to include 'Status'
+          expect(status_row).to include 'Reference received'
+          expect(status_row).to include "#{reference.name} will not be asked to give you another reference."
+        end
+      end
+
+      context 'when the state is not feedback_provided' do
+        it 'does not render a status row' do
+          reference = create(:reference, :feedback_requested, application_form: application_form)
+          result = render_inline(described_class.new(references: [reference], application_form: application_form))
+
+          status_row = result.css('.govuk-summary-list__row')[4]
+          expect(status_row).to be_nil
+        end
       end
     end
   end
