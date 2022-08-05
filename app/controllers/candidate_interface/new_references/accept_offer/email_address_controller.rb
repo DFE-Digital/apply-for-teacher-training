58ec99294501e24ce67bfd27cd3b489cdb0e91dd
Tomas D'Stefano <tomas_stefano@successoft.com>
2022-08-05 11:56:29 +0100

    Create or edit references email address on accept offer page
    Co-authored-by: James Glenn <47917431+JR-G@users.noreply.github.com>

---
+++ app/controllers/candidate_interface/new_references/accept_offer/email_address_controller.rb
@@ -0,0 +1,30 @@
+module CandidateInterface
+  module NewReferences
+    class AcceptOffer::EmailAddressController < EmailAddressController
+      include AcceptOfferConfirmReferences
+
+      def references_email_address_path
+        candidate_interface_accept_offer_new_references_email_address_path(
+          application_choice,
+          @reference.id
+        )
+      end
+
+      def edit_email_address_path
+        candidate_interface_accept_offer_new_references_edit_email_address_path(
+          application_choice,
+          @reference.id,
+          return_to: params[:return_to]
+        )
+      end
+      helper_method :edit_email_address_path
+
+      def next_path
+        candidate_interface_accept_offer_new_references_relationship_path(
+          application_choice,
+          @reference.id
+        )
+      end
+    end
+  end
+end
