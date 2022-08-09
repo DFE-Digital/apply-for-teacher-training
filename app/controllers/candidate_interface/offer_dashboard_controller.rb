7e6db66aa74c411225a09006d74859358d836f1c
Tomas D'Stefano <tomas_stefano@successoft.com>
2022-08-09 16:51:55 +0100

    Showing the right statuses and history on new references post offer

--- app/controllers/candidate_interface/offer_dashboard_controller.rb
+++ app/controllers/candidate_interface/offer_dashboard_controller.rb
@@ -2,10 +2,12 @@ module CandidateInterface
   class OfferDashboardController < CandidateInterfaceController
     def show
       @application_form = current_application
+      @application_choice_with_offer = current_application.application_choices.pending_conditions.first
+      @accepted_offer_provider_name = @application_choice_with_offer.provider.name
     end
 
     def view_reference
-      @reference = params[:id]
+      @reference = current_application.application_references.find(params[:id])
     end
   end
 end
