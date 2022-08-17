9b2cce8f4fb8fc0ee16299ab5e62a082eddbfa15
Jonathan Filar <jonathanfilar@hotmail.com>
2022-08-17 18:32:45 +0100

    Surface error in courses controller

--- app/controllers/support_interface/application_forms/courses_controller.rb
+++ app/controllers/support_interface/application_forms/courses_controller.rb
@@ -59,7 +59,7 @@ module SupportInterface
           else
             render :edit
           end
-        rescue CourseChoiceError, ProviderInterviewError, FundingTypeError => e
+        rescue CourseChoiceError, ProviderInterviewError, FundingTypeError, CourseFullError => e
           flash[:warning] = e.message
           render :edit
         end
@@ -78,7 +78,7 @@ module SupportInterface
 
       def change_course_choice_params
         params.require(:support_interface_application_forms_change_course_choice_form)
-              .permit(:application_choice_id, :provider_code, :course_code, :study_mode, :site_code, :audit_comment_ticket, :accept_guidance)
+              .permit(:application_choice_id, :provider_code, :course_code, :study_mode, :site_code, :audit_comment_ticket, :accept_guidance, :confirm_course_change, :checkbox_rendered)
       end
 
       def application_form_id
