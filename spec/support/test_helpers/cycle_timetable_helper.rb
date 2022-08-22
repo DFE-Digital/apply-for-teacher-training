467b1c34c0921f85deac31944f9b0798adc5de7f
Steve Laing <steve.laing@gmail.com>
2022-08-22 15:29:46 +0100

    Use default cycle timetable args

--- spec/support/test_helpers/cycle_timetable_helper.rb
+++ spec/support/test_helpers/cycle_timetable_helper.rb
@@ -1,18 +1,18 @@
 module CycleTimetableHelper
   def mid_cycle
-    CycleTimetable.apply_opens(2020) + 1.day
+    CycleTimetable.apply_opens + 1.day
   end
 
   def after_apply_1_deadline
-    CycleTimetable.apply_1_deadline(2020) + 1.day
+    CycleTimetable.apply_1_deadline + 1.day
   end
 
   def after_apply_2_deadline
-    CycleTimetable.apply_2_deadline(2020) + 1.day
+    CycleTimetable.apply_2_deadline + 1.day
   end
 
   def after_apply_reopens
-    CycleTimetable.apply_reopens(2021) + 1.day
+    CycleTimetable.apply_reopens + 1.day
   end
 end
 
