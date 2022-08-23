456e5606c1f1489e74f4265c6371505b2621a7c6
Steve Laing <steve.laing@gmail.com>
2022-08-23 14:59:39 +0100

    Remove RecruitmentCycle monkey patch from test helper

--- spec/support/test_helpers/cycle_timetable_helper.rb
+++ spec/support/test_helpers/cycle_timetable_helper.rb
@@ -15,13 +15,3 @@ module CycleTimetableHelper
     CycleTimetable.apply_reopens(year) + 1.day
   end
 end
-
-module RecruitmentCycle
-  CYCLES = {
-    '2023' => '2022 to 2023',
-    '2022' => '2021 to 2022',
-    '2021' => '2020 to 2021',
-    '2020' => '2019 to 2020',
-    '2019' => '2018 to 2019',
-  }.freeze
-end
