4fecfb775549d65739091bca2f9ede8b988135b5
Steve Laing <steve.laing@gmail.com>
2022-08-23 10:58:07 +0100

    Allow specific year to be passed to helper methods

--- spec/support/test_helpers/cycle_timetable_helper.rb
+++ spec/support/test_helpers/cycle_timetable_helper.rb
@@ -1,6 +1,6 @@
 module CycleTimetableHelper
-  def mid_cycle
-    CycleTimetable.apply_opens + 1.day
+  def mid_cycle(year = CycleTimetable.current_year)
+    CycleTimetable.apply_opens(year) + 1.day
   end
 
   def after_apply_1_deadline
@@ -11,8 +11,8 @@ module CycleTimetableHelper
     CycleTimetable.apply_2_deadline + 1.day
   end
 
-  def after_apply_reopens
-    CycleTimetable.apply_reopens + 1.day
+  def after_apply_reopens(year = CycleTimetable.next_year)
+    CycleTimetable.apply_reopens(year) + 1.day
   end
 end
 
