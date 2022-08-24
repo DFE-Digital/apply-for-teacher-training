20fa28ab7e56d8c5c779984b989c7061904a892b
Tomas D'Stefano <tomas_stefano@successoft.com>
2022-08-24 18:21:20 +0100

    Use submit referee form to validate the referee data
    We can use an existing class to do that for us

--- app/validators/application_form_with_complete_references_validator.rb
+++ app/validators/application_form_with_complete_references_validator.rb
@@ -1,25 +1,16 @@
 class ApplicationFormWithCompleteReferencesValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    return unless record.completed?
-
-    application_form = value
+    application_form = value.is_a?(ApplicationForm) ? value : value.application_form
 
     record.errors.add(attribute, :incomplete_references) if invalid_references?(application_form)
   end
 
   def invalid_references?(application_form)
-    !application_form.complete_references_information? ||
-      application_form.application_references.any? do |application_reference|
-        referee_email_address_form = CandidateInterface::Reference::RefereeEmailAddressForm.new(
-          email_address: application_reference.email_address,
-          reference_id: application_reference.id,
-        )
-
-        referee_relationship_form = CandidateInterface::Reference::RefereeRelationshipForm.new(
-          relationship: application_reference.relationship,
-        )
-
-        referee_email_address_form.invalid? || referee_relationship_form.invalid?
-      end
+    application_form.application_references.any? do |application_reference|
+      !CandidateInterface::Reference::SubmitRefereeForm.new(
+        submit: 'yes',
+        reference_id: application_reference.id,
+      ).valid?
+    end
   end
 end
