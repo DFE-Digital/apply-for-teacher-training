0fc3c17b9ce4253e6c3a5ced1a8edebdf43edb6e
Tomas D'Stefano <tomas_stefano@successoft.com>
2022-09-01 17:46:14 +0100

    Change the chase reference days
    So we decide to stagger the chase references between the referee and the
    candidate:
    
    * to referees at 7 days, 14 days and 28 days
    * to candidates at 9 days, 16 days and 30 days
    
    So we’d chase the referees a week earlier for the second reminder.
    I don’t know what the rationale was for the original spacing.

--- app/workers/chase_references.rb
+++ app/workers/chase_references.rb
@@ -4,64 +4,84 @@
 class ChaseReferences
   include Sidekiq::Worker
 
+  REFEREE_CHASERS = [
+    {
+      after: 7.days,
+      email: :reference_request_chaser_email,
+      type: :referee_reference_request,
+      old_type: :reference_request,
+      include_application_form: true,
+    },
+    {
+      after: 14.days,
+      email: :reference_request_chaser_email,
+      type: :reminder_reference_nudge,
+      include_application_form: true,
+    },
+    {
+      after: 28.days,
+      email: :reference_request_chase_again_email,
+      type: :referee_follow_up_missing_references,
+      old_type: :follow_up_missing_references,
+    },
+  ].freeze
+
+  CANDIDATE_CHASERS = [
+    {
+      after: 9.days,
+      email: :chase_reference,
+      type: :candidate_reference_request,
+      old_type: :reference_request,
+    },
+    {
+      after: 16.days,
+      email: :new_referee_request,
+      type: :reference_replacement,
+      extra_params: { reason: :not_responded },
+    },
+    {
+      after: 30.days,
+      email: :chase_reference_again,
+      type: :candidate_follow_up_missing_references,
+      old_type: :follow_up_missing_references,
+    },
+  ].freeze
+
   def perform
-    send_7_day_chaser!
-    send_14_day_chaser!
-    send_21_day_chaser!
-    send_28_day_chaser!
+    REFEREE_CHASERS.each { |chaser| send_chaser(chaser, mailer: RefereeMailer) }
+    CANDIDATE_CHASERS.each { |chaser| send_chaser(chaser, mailer: CandidateMailer) }
   end
 
 private
 
-  def send_7_day_chaser!
-    references = ApplicationReference.referees_to_chase(
-      chase_referee_by: TimeLimitConfig.chase_referee_by.days.before(Time.zone.now),
-      rejected_chased_ids: ChaserSent.reference_request.select(:chased_id),
-    )
+  def send_chaser(chaser, mailer:)
+    chase_referee_by = chaser[:after].before(Time.zone.now)
+    rejected_chased_ids = ChaserSent.send(chaser[:type]).select(:chased_id)
+
+    # Backwards compatibility so we don't re-send emails that we already sent
+    #
+    if chaser[:old_type]
+      rejected_chased_ids = rejected_chased_ids.or(ChaserSent.send(chaser[:old_type]).select(:chased_id))
+    end
+
+    references = ApplicationReference.referees_to_chase(chase_referee_by: chase_referee_by, rejected_chased_ids: rejected_chased_ids)
 
     references.each do |reference|
-      RefereeMailer.reference_request_chaser_email(reference.application_form, reference).deliver_later
-      CandidateMailer.chase_reference(reference).deliver_later
-      ChaserSent.create!(chased: reference, chaser_type: :reference_request)
+      deliver_email(reference: reference, chaser: chaser, mailer: mailer)
+
+      ChaserSent.create!(chased: reference, chaser_type: chaser[:type])
     end
   end
 
-  def send_14_day_chaser!
-    references = ApplicationReference.referees_to_chase(
-      chase_referee_by: TimeLimitConfig.replace_referee_by.days.before(Time.zone.now),
-      rejected_chased_ids: ChaserSent.reference_replacement.select(:chased_id),
-    )
+  def deliver_email(reference:, chaser:, mailer:)
+    email_method = mailer.method(chaser[:email])
 
-    references.each do |reference|
-      SendNewRefereeRequestEmail.call(
-        reference:,
-        reason: :not_responded,
-      )
-    end
-  end
+    return email_method.call(reference, chaser[:extra_params]).deliver_later if chaser[:extra_params].present?
 
-  def send_21_day_chaser!
-    references = ApplicationReference.referees_to_chase(
-      chase_referee_by: TimeLimitConfig.second_chase_referee_by.days.before(Time.zone.now),
-      rejected_chased_ids: ChaserSent.reminder_reference_nudge.select(:chased_id),
-    )
-
-    references.each do |reference|
-      RefereeMailer.reference_request_chaser_email(reference.application_form, reference).deliver_later
-      ChaserSent.create!(chased: reference, chaser_type: :reminder_reference_nudge)
-    end
-  end
-
-  def send_28_day_chaser!
-    references = ApplicationReference.referees_to_chase(
-      chase_referee_by: TimeLimitConfig.additional_reference_chase_calendar_days.days.before(Time.zone.now),
-      rejected_chased_ids: ChaserSent.follow_up_missing_references.select(:chased_id),
-    )
-
-    references.each do |reference|
-      RefereeMailer.reference_request_chase_again_email(reference).deliver_later
-      CandidateMailer.chase_reference_again(reference).deliver_later
-      ChaserSent.create!(chased: reference, chaser_type: :follow_up_missing_references)
+    if chaser[:include_application_form].present?
+      email_method.call(reference.application_form, reference).deliver_later
+    else
+      email_method.call(reference).deliver_later
     end
   end
 end
