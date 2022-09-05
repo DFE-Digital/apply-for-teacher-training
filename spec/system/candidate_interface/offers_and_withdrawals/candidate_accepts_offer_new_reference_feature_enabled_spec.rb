f243f65d3cf9beda67b1e8776cec06e0116bec7e
Frankie Roberto <frankie@frankieroberto.com>
2022-09-05 15:10:34 +0100

    Update test

--- spec/system/candidate_interface/offers_and_withdrawals/candidate_accepts_offer_new_reference_feature_enabled_spec.rb
+++ spec/system/candidate_interface/offers_and_withdrawals/candidate_accepts_offer_new_reference_feature_enabled_spec.rb
@@ -456,7 +456,7 @@ RSpec.feature 'Candidate accepts an offer' do
   end
 
   def when_i_add_reference_relationship
-    click_on 'Enter relationship to referee'
+    click_on 'Enter how you know them and for how long'
     fill_in 'How do you know Aragorn and how long have you known them?', with: 'Middle earth'
     and_i_click_save_and_continue
   end
