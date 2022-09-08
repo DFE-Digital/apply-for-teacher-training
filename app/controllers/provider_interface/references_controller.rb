f14c6ec38678c0d2badfa91405abc9fa580f9f58
James Glenn <47917431+JR-G@users.noreply.github.com>
2022-09-08 16:16:37 +0100

    Pass provider_can_make_decisions into the references tab

--- app/controllers/provider_interface/references_controller.rb
+++ app/controllers/provider_interface/references_controller.rb
@@ -4,6 +4,10 @@ module ProviderInterface
 
     def index
       @references = @application_choice.application_form.application_references
+
+      @provider_can_make_decisions =
+        current_provider_user.authorisation.can_make_decisions?(application_choice: @application_choice,
+                                                                course_option: @application_choice.current_course_option)
     end
   end
 end
