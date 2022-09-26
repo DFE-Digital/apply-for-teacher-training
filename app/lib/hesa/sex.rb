d3ebd2fe1da2a97091cd87d1a7739011879f66a6
Tomas D'Stefano <tomas_stefano@successoft.com>
2022-09-26 08:10:07 +0100

    Change hesa data to lookup for recruitment cycle year

--- app/lib/hesa/sex.rb
+++ app/lib/hesa/sex.rb
@@ -2,16 +2,19 @@ module Hesa
   class Sex
     SexStruct = Struct.new(:hesa_code, :type)
 
-    def self.all
-      HESA_SEX.map { |sex| SexStruct.new(*sex) }
+    def self.all(cycle_year)
+      collection_name = "HESA_SEX_#{cycle_year - 1}_#{cycle_year}"
+      HesaSexCollections.const_get(collection_name).map { |sex| SexStruct.new(*sex) }
+    rescue NameError
+      raise ArgumentError, "Do not know Hesa Sex codes for #{cycle_year}"
     end
 
-    def self.find(sex)
+    def self.find(sex, cycle_year = RecruitmentCycle.current_year)
       if sex == 'intersex'
         sex = 'other'
       end
 
-      all.find { |hesa_sex| hesa_sex.type == sex }
+      all(cycle_year).find { |hesa_sex| hesa_sex.type == sex }
     end
   end
 end
--- spec/lib/hesa/sex_spec.rb
+++ spec/lib/hesa/sex_spec.rb
@@ -2,25 +2,51 @@ require 'rails_helper'
 
 RSpec.describe Hesa::Sex do
   describe '.all' do
-    it 'returns a list of HESA sex structs' do
-      sex_types = described_class.all
+    context 'when 2022' do
+      it 'returns a list of HESA sex structs' do
+        sex_types = described_class.all(2022)
 
-      expect(sex_types.size).to eq 3
+        expect(sex_types.size).to eq 3
 
-      female = sex_types.find { |s| s.hesa_code == '2' }
+        female = sex_types.find { |s| s.hesa_code == '2' }
 
-      expect(female.hesa_code).to eq '2'
-      expect(female.type).to eq 'female'
+        expect(female.hesa_code).to eq '2'
+        expect(female.type).to eq 'female'
+      end
+    end
+
+    context 'when 2023' do
+      it 'returns a list of HESA sex structs' do
+        sex_types = described_class.all(2023)
+
+        expect(sex_types.size).to eq 3
+
+        female = sex_types.find { |s| s.hesa_code == '10' }
+
+        expect(female.hesa_code).to eq '10'
+        expect(female.type).to eq 'female'
+      end
     end
   end
 
   describe '.find' do
     context 'given a valid type' do
       it 'returns the matching struct' do
+        allow(RecruitmentCycle).to receive(:current_year).and_return(2022)
         result = described_class.find('female')
 
         expect(result.type).to eq 'female'
         expect(result.hesa_code).to eq '2'
+      end
+    end
+
+    context 'when 2023' do
+      it 'returns a list of HESA sex structs' do
+        allow(RecruitmentCycle).to receive(:current_year).and_return(2023)
+        result = described_class.find('female')
+
+        expect(result.type).to eq 'female'
+        expect(result.hesa_code).to eq '10'
       end
     end
 
