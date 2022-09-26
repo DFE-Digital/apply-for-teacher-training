8758e2f6faa1387bb443a48f67767298d01665e7
Tomas D'Stefano <tomas_stefano@successoft.com>
2022-09-26 08:09:48 +0100

    Make hesa data to be using cycle year

--- config/initializers/hesa_sex.rb
+++ config/initializers/hesa_sex.rb
@@ -1,5 +1,17 @@
-HESA_SEX = [
-  %w[1 male],
-  %w[2 female],
-  %w[3 other],
-].freeze
+module HesaSexCollections
+  HESA_SEX_2019_2020 = [
+    %w[1 male],
+    %w[2 female],
+    %w[3 other],
+  ].freeze
+
+  HESA_SEX_2020_2021 = HESA_SEX_2019_2020
+
+  HESA_SEX_2021_2022 = HESA_SEX_2020_2021
+
+  HESA_SEX_2022_2023 = [
+    %w[10 female],
+    %w[11 male],
+    %w[12 other],
+  ].freeze
+end
