4e68175c9498673302c07aa853590d897a7f28ab
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2022-10-04 11:26:24 +0100

    Extract the SimulatedFailure healthcheck

--- config/initializers/okcomputer.rb
+++ config/initializers/okcomputer.rb
@@ -1,21 +1,11 @@
 require 'teacher_training_public_api/sync_check'
-
-class SimulatedFailureCheck < OkComputer::Check
-  def check
-    if FeatureFlag.active?('force_ok_computer_to_fail')
-      mark_failure
-      mark_message 'force_ok_computer_to_fail is on'
-    else
-      mark_message 'force_ok_computer_to_fail is off'
-    end
-  end
-end
+require 'healthchecks'
 
 OkComputer.mount_at = 'integrations/monitoring'
 
 OkComputer::Registry.register 'postgres', OkComputer::ActiveRecordCheck.new
 OkComputer::Registry.register 'redis', OkComputer::RedisCheck.new(url: ENV['REDIS_URL'])
-OkComputer::Registry.register 'simulated_failure', SimulatedFailureCheck.new
+OkComputer::Registry.register 'simulated_failure', Healthchecks::SimulatedFailureCheck.new
 OkComputer::Registry.register 'version', OkComputer::AppVersionCheck.new
 
 OkComputer.make_optional %w[version]
---
+++ lib/healthchecks.rb
@@ -0,0 +1,3 @@
+module Healthchecks
+  autoload :SimulatedFailureCheck, 'healthchecks/simulated_failure_check'
+end
