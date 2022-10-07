b84bb834c7f065ceaefe6b7d3420ac3a06f5764c
Tomas D'Stefano <tomas_stefano@successoft.com>
2022-10-07 14:55:02 +0100

    Don't show the send reminder link if the reference recently requested

--- app/components/candidate_interface/new_reference_status_line_component.rb
+++ app/components/candidate_interface/new_reference_status_line_component.rb
@@ -11,7 +11,7 @@ module CandidateInterface
     end
 
     def can_send_reminder?
-      ReferenceActionsPolicy.new(reference).can_send_reminder?
+      ReferenceActionsPolicy.new(reference).can_send_reminder? && request_sent_at && request_sent_at <= TimeLimitConfig.minimum_hours_between_chaser_emails.hours.ago
     end
   end
 end
