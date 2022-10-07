74fb602536e432ede46425aaf323595e0121980e
Tomas D'Stefano <tomas_stefano@successoft.com>
2022-10-07 17:10:31 +0100

    Remove new ethnicities if we are in different recruitment cycle

--- spec/factories/application_form.rb
+++ spec/factories/application_form.rb
@@ -59,12 +59,16 @@ FactoryBot.define do
     trait :with_equality_and_diversity_data do
       equality_and_diversity do
         all_ethnicities = Class.new.extend(EthnicBackgroundHelper).all_combinations
+        if RecruitmentCycle.current_year < 2023
+          all_ethnicities -= [['White', 'Irish'], ['White', 'Roma']]
+        end
         ethnicity = all_ethnicities.sample
         other_disability = 'Acquired brain injury'
         all_disabilities = DisabilityHelper::STANDARD_DISABILITIES.map(&:second) << other_disability
         disabilities = rand < 0.85 ? all_disabilities.sample([*0..3].sample) : ['Prefer not to say']
         hesa_sex = sex == 'Prefer not to say' ? nil : Hesa::Sex.find(sex, RecruitmentCycle.current_year)['hesa_code']
         hesa_disabilities = disabilities == ['Prefer not to say'] ? %w[00] : disabilities.map { |disability| Hesa::Disability.find(disability)['hesa_code'] }
+        binding.pry if Hesa::Ethnicity.find(ethnicity.last, RecruitmentCycle.current_year).blank?
         hesa_ethnicity = Hesa::Ethnicity.find(ethnicity.last, RecruitmentCycle.current_year)['hesa_code']
 
         {
