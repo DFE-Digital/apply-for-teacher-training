26dc0c4fa5e9b091262abd761f827556033cb19f
Tomas D'Stefano <tomas_stefano@successoft.com>
2022-10-07 09:20:55 +0100

    Make disabilities form to accept all answers
    The input for candidates to add disabilities or choose no disability
    or prefer not to say

--- spec/forms/candidate_interface/equality_and_diversity/disabilities_form_spec.rb
+++ spec/forms/candidate_interface/equality_and_diversity/disabilities_form_spec.rb
@@ -1,27 +1,60 @@
 require 'rails_helper'
 
 RSpec.describe CandidateInterface::EqualityAndDiversity::DisabilitiesForm, type: :model do
+  let(:disabilities) do
+    [
+      I18n.t('equality_and_diversity.disabilities.blind.label'),
+      I18n.t('equality_and_diversity.disabilities.deaf.label')
+    ]
+  end
+  let(:disabilities_with_other) do
+    [
+      disabilities,
+      described_class::OTHER
+    ].flatten
+  end
+  let(:no_know_disability) do
+    I18n.t('equality_and_diversity.disabilities.no.label')
+  end
+  let(:opt_out) do
+    I18n.t('equality_and_diversity.disabilities.opt_out.label')
+  end
+
   describe '.build_from_application' do
     it 'creates an object based on the application form' do
-      application_form = build_stubbed(:application_form, equality_and_diversity: { 'disabilities' => %w[Blind Deaf] })
+      application_form = build_stubbed(:application_form, equality_and_diversity: { 'disabilities' => disabilities })
       form = described_class.build_from_application(application_form)
 
-      expect(form.disabilities).to eq(%w[Blind Deaf])
+      expect(form.disabilities).to eq(disabilities)
     end
 
     it 'creates an object with other disability based on the application form' do
-      application_form = build_stubbed(:application_form, equality_and_diversity: { 'disabilities' => ['Blind', 'Deaf', 'Other disability'] })
+      application_form = build_stubbed(:application_form, equality_and_diversity: { 'disabilities' => [disabilities, 'Other disability'].flatten })
       form = described_class.build_from_application(application_form)
 
-      expect(form.disabilities).to eq(%w[Blind Deaf Other])
+      expect(form.disabilities).to eq(disabilities_with_other)
       expect(form.other_disability).to eq('Other disability')
     end
 
-    it 'allows other disability to be undisclosed' do
-      application_form = build_stubbed(:application_form, equality_and_diversity: { 'disabilities' => %w[Blind Deaf Other] })
+    it 'creates an object with no know disability based on the application form' do
+      application_form = build_stubbed(:application_form, equality_and_diversity: { 'disabilities' => [no_know_disability] })
       form = described_class.build_from_application(application_form)
 
-      expect(form.disabilities).to eq(%w[Blind Deaf Other])
+      expect(form.disabilities).to eq([no_know_disability])
+    end
+
+    it 'creates an object with no know disability based on the application form' do
+      application_form = build_stubbed(:application_form, equality_and_diversity: { 'disabilities' => [opt_out] })
+      form = described_class.build_from_application(application_form)
+
+      expect(form.disabilities).to eq([opt_out])
+    end
+
+    it 'allows other disability to be undisclosed' do
+      application_form = build_stubbed(:application_form, equality_and_diversity: { 'disabilities' => disabilities_with_other })
+      form = described_class.build_from_application(application_form)
+
+      expect(form.disabilities).to eq(disabilities_with_other)
       expect(form.other_disability).to be_nil
     end
 
@@ -46,48 +79,75 @@ RSpec.describe CandidateInterface::EqualityAndDiversity::DisabilitiesForm, type:
 
     context 'when disabilities field has a value' do
       it 'returns true' do
-        form = described_class.new(disabilities: %w[Blind])
+        form = described_class.new(disabilities: disabilities)
 
         expect(form.save(application_form)).to be(true)
       end
 
-      it 'updates the equality and diversity information on the application form' do
-        form = described_class.new(disabilities: %w[Blind Other], other_disability: 'Other disability')
+      it 'saves no know disability' do
+        disabilities = [no_know_disability]
+        form = described_class.new(disabilities: disabilities)
         form.save(application_form)
 
         expect(application_form.equality_and_diversity).to eq(
-          'disabilities' => ['Blind', 'Other disability'],
-          'hesa_disabilities' => %w[58 96],
+          'disabilities' => disabilities,
+          'hesa_disabilities' => %w[95],
+        )
+      end
+
+      it 'saves prefer not to say' do
+        disabilities = [I18n.t('equality_and_diversity.disabilities.opt_out.label')]
+        form = described_class.new(disabilities: disabilities)
+        form.save(application_form)
+
+        expect(application_form.equality_and_diversity).to eq(
+          'disabilities' => disabilities,
+          'hesa_disabilities' => %w[98],
+        )
+      end
+
+      it 'updates the equality and diversity information on the application form' do
+        form = described_class.new(disabilities: disabilities_with_other, other_disability: 'Other disability')
+        form.save(application_form)
+
+        expect(application_form.equality_and_diversity).to eq(
+          'disabilities' => [disabilities, 'Other disability'].flatten,
+          'hesa_disabilities' => %w[58 57 96],
         )
       end
 
       it 'allows other_disability field to be optional' do
-        form = described_class.new(disabilities: %w[Blind Other], other_disability: '')
+        form = described_class.new(
+          disabilities: disabilities_with_other,
+          other_disability: ''
+        )
         form.save(application_form)
 
         expect(application_form.equality_and_diversity).to eq(
-          'disabilities' => %w[Blind Other],
-          'hesa_disabilities' => %w[58 96],
+          'disabilities' => disabilities_with_other,
+          'hesa_disabilities' => %w[58 57 96],
         )
       end
 
       it 'updates the existing record of equality and diversity information' do
         application_form = create(:application_form, equality_and_diversity: { 'sex' => 'male' })
-        form = described_class.new(disabilities: %w[Blind])
+        form = described_class.new(
+          disabilities: [disabilities, 'Other disability'].flatten
+        )
         form.save(application_form)
 
         expect(application_form.equality_and_diversity).to eq(
-          'sex' => 'male', 'hesa_disabilities' => %w[58], 'disabilities' => %w[Blind],
+          'sex' => 'male', 'hesa_disabilities' => %w[58 57 96], 'disabilities' => [disabilities, 'Other disability'].flatten,
         )
       end
 
       it 'does not update disabilities with other disability if Other is not selected' do
         application_form = create(:application_form, equality_and_diversity: { 'sex' => 'male' })
-        form = described_class.new(disabilities: %w[Blind], other_disability: 'Other disability')
+        form = described_class.new(disabilities: disabilities, other_disability: 'Other disability')
         form.save(application_form)
 
         expect(application_form.equality_and_diversity).to eq(
-          'sex' => 'male', 'hesa_disabilities' => %w[58], 'disabilities' => %w[Blind],
+          'sex' => 'male', 'hesa_disabilities' => %w[58 57], 'disabilities' => disabilities,
         )
       end
     end
