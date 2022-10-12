519c7b63a51bbc137bef2e7710926c57617b44d9
James Glenn <47917431+JR-G@users.noreply.github.com>
2022-10-12 17:01:12 +0100

    Change cancel unsubmitted task to be previous cycle

---
+++ app/workers/cancel_previous_cycle_unsubmitted_applications_worker.rb
@@ -0,0 +1,21 @@
+class CancelPreviousCycleUnsubmittedApplicationsWorker
+  include Sidekiq::Worker
+
+  def perform
+    unsubmitted_applications_from_earlier_cycle.each do |application_form|
+      CandidateInterface::CancelUnsubmittedApplicationAtEndOfCycle.new(application_form).call
+    end
+  end
+
+private
+
+  def unsubmitted_applications_from_earlier_cycle
+    ApplicationForm
+      .unsubmitted
+      .where(recruitment_cycle_year: RecruitmentCycle.previous_year)
+      .where.not(application_forms: { candidate_id: Candidate.where(hide_in_reporting: true).select(:id) })
+      .where(application_forms: { id: ApplicationChoice.unsubmitted.select(:application_form_id) })
+      .includes(:application_choices)
+      .distinct
+  end
+end
--- spec/system/support_interface/cancel_previous_cycle_unsubmitted_at_eoc_task_spec.rb
+++ spec/system/support_interface/cancel_previous_cycle_unsubmitted_at_eoc_task_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature 'Cancel unsubmitted applications support task', sidekiq: true do
+RSpec.feature 'Cancel previous cycle unsubmitted applications support task', sidekiq: true do
   include DfESignInHelpers
   include CycleTimetableHelper
 
@@ -8,7 +8,7 @@ RSpec.feature 'Cancel unsubmitted applications support task', sidekiq: true do
     Timecop.freeze(after_apply_2_deadline) { example.run }
   end
 
-  scenario 'Support user performs the cancel unsubmitted applications at EoC task' do
+  scenario 'Support user performs the cancel previous cycle unsubmitted applications at EoC task' do
     given_i_have_a_candidate_with_an_unsubmitted_application
 
     when_i_am_a_support_user
@@ -26,7 +26,7 @@ RSpec.feature 'Cancel unsubmitted applications support task', sidekiq: true do
   end
 
   def given_i_have_a_candidate_with_an_unsubmitted_application
-    @application_form = create :completed_application_form, submitted_at: nil
+    @application_form = create :completed_application_form, submitted_at: nil, recruitment_cycle_year: RecruitmentCycle.previous_year
     create :application_choice, application_form: @application_form, status: :unsubmitted
   end
 
@@ -43,7 +43,7 @@ RSpec.feature 'Cancel unsubmitted applications support task', sidekiq: true do
   end
 
   def then_i_see_that_the_job_has_been_scheduled
-    expect(page).to have_content 'Scheduled job to cancel unsubmitted applications that reached end-of-cycle'
+    expect(page).to have_content 'Scheduled job to cancel unsubmitted applications, from the previous cycle, that reached end-of-cycle'
   end
 
   def when_i_lookup_the_candidates_application
