154508a0884e3258a72d9d2e855a5006af813292
Colin Saliceti <colin.saliceti@digital.education.gov.uk>
2022-10-18 11:11:28 +0100

    redis

---
+++ terraform/modules/kubernetes/redis.tf
@@ -0,0 +1,58 @@
+resource "kubernetes_deployment" "redis" {
+  metadata {
+    name      = local.redis_service_name
+    namespace = var.namespace
+  }
+  spec {
+    replicas = 1
+    selector {
+      match_labels = {
+        app = local.redis_service_name
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = local.redis_service_name
+        }
+      }
+      spec {
+        node_selector = {
+            "kubernetes.io/os": "linux"
+        }
+        container {
+          name  = local.redis_service_name
+          image = "redis:5-alpine"
+          resources {
+            requests = {
+              cpu = "100m"
+              memory = "256Mi"
+            }
+            limits = {
+              cpu = "250m"
+              memory = "1Gi"
+            }
+          }
+          port {
+            container_port = 6379
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "redis" {
+  metadata {
+    name = local.redis_service_name
+    namespace = var.namespace
+  }
+  spec {
+    port {
+      port = 6379
+    }
+    selector = {
+      app = local.redis_service_name
+    }
+  }
+}
--- terraform/modules/kubernetes/variables.tf
+++ terraform/modules/kubernetes/variables.tf
@@ -11,6 +11,7 @@ locals {
   app_secrets_name       = "apply-secrets-${var.app_environment}"
   app_config_name        = "apply-config-${var.app_environment}"
   postgres_service_name = "apply-postgres-${var.app_environment}"
+  redis_service_name = "apply-redis-${var.app_environment}"
 
   hostname = "${local.webapp_name}.paas.teaching-identity.education.gov.uk"
   web_app_env_variables = merge(
@@ -24,12 +25,14 @@ locals {
   web_app_env_variables_hash = sha1(join("-", [for k, v in local.web_app_env_variables : "${k}:${v}"]))
 
   database_url = "postgres://postgres:password@${local.postgres_service_name}:5432/${local.postgres_service_name}"
+  redis_url="redis://${local.redis_service_name}:6379/0"
 
   app_secrets = merge(
     var.app_secrets,
     {
       DATABASE_URL        = local.database_url
       BLAZER_DATABASE_URL = local.database_url
+      REDIS_URL           = local.redis_url
     }
   )
   app_secrets_hash = sha1(join("-", [for k, v in local.app_secrets : "${k}:${v}" if v != null]))
