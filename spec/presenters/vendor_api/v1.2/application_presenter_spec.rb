2f609fd6c3b237a80083f22c1bf05a22ecec543b
James Glenn <47917431+JR-G@users.noreply.github.com>
2022-11-02 19:25:23 +0000

    Do not return 2023 HESA codes for 2022 applications

--- spec/presenters/vendor_api/v1.2/application_presenter_spec.rb
+++ spec/presenters/vendor_api/v1.2/application_presenter_spec.rb
@@ -5,23 +5,37 @@ RSpec.describe VendorAPI::ApplicationPresenter do
 
   let(:version) { '1.2' }
   let(:attributes) { application_json[:attributes] }
-  let(:application_form) { create(:completed_application_form, :with_equality_and_diversity_data) }
+  let(:application_form) { create(:completed_application_form, :with_equality_and_diversity_data, recruitment_cycle_year:) }
   let(:application_choice) { create(:application_choice, :with_recruited, application_form: application_form) }
 
-  describe 'HESA data' do
-    it 'returns the HESA codes associated with that application' do
-      equality_and_diversity_data = application_form[:equality_and_diversity]
-      expect(attributes).to include(
-        {
-          equality_and_diversity: {
-            sex: equality_and_diversity_data['hesa_sex'],
-            disability: equality_and_diversity_data['hesa_disabilities'],
-            ethnicity: equality_and_diversity_data['hesa_ethnicity'],
-            other_disability_details: equality_and_diversity_data['other_disability_details'],
-            other_ethnicity_details: equality_and_diversity_data['other_ethnicity_details'],
+  describe 'Equality and diversity data' do
+    context 'when it is a current cycle application' do
+      let(:recruitment_cycle_year) { RecruitmentCycle.current_year }
+
+      it 'returns the 2023 HESA codes associated with that application' do
+        equality_and_diversity_data = application_form[:equality_and_diversity]
+        expect(attributes).to include(
+          {
+            equality_and_diversity: {
+              sex: equality_and_diversity_data['hesa_sex'],
+              disability: equality_and_diversity_data['hesa_disabilities'],
+              ethnicity: equality_and_diversity_data['hesa_ethnicity'],
+              other_disability_details: equality_and_diversity_data['other_disability_details'],
+              other_ethnicity_details: equality_and_diversity_data['other_ethnicity_details'],
+            },
           },
-        },
-      )
+        )
+      end
+    end
+
+    context 'when it is a previous cycle application' do
+      let(:recruitment_cycle_year) { 2022 }
+
+      it 'does not return the 2023 HESA codes associated with that applications' do
+        expect(attributes).to include({
+          equality_and_diversity: nil,
+        })
+      end
     end
   end
 end
