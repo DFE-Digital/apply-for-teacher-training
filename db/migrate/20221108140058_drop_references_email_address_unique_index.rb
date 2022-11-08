fd95be26617eb05f689f4933a1834bdbf2960f81
Tomas D'Stefano <tomas_stefano@successoft.com>
2022-11-08 16:19:29 +0000

    Appease rubocop

--- db/migrate/20221108140058_drop_references_email_address_unique_index.rb
+++ db/migrate/20221108140058_drop_references_email_address_unique_index.rb
@@ -1,5 +1,5 @@
 class DropReferencesEmailAddressUniqueIndex < ActiveRecord::Migration[7.0]
   def change
-    remove_index :references, column: [:application_form_id, :email_address], name: 'index_references_on_application_form_id_and_email_address'
+    remove_index :references, column: %i[application_form_id email_address], name: 'index_references_on_application_form_id_and_email_address'
   end
 end
