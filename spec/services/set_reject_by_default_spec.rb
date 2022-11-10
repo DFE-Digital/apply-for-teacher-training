c09f7b9aa78600af8a1748f888a2efc0f898038c
Carlos Martinez <carlosjmtz@gmail.com>
2022-11-10 17:13:45 +0000

    Update test to match cycle timetable fix

--- spec/services/set_reject_by_default_spec.rb
+++ spec/services/set_reject_by_default_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe SetRejectByDefault do
 
   describe 'RBD calculation' do
     submitted_vs_rbd_dates = [
-      ['2 Sept 2019 9:00:00 AM BST', '01 Oct 2019 0:00:00 AM BST', 'near the BST/GMT boundary'],
+      ['2 Sept 2019 9:00:00 AM BST', '28 Sep 2019 0:00:00 AM BST', 'near the BST/GMT boundary'],
       ['1 Jul 2019 9:00:00 AM BST',  '30 Jul 2019 0:00:00 AM BST', 'safely within BST'],
       ['4 Jan 2019 11:00:00 PM GMT', '2 Mar 2019 0:00:00 AM GMT',  'safely within GMT'],
       ['1 Jul 2020 11:00:00 PM BST', '30 Jul 2020 0:00:00 AM BST', 'during the 20-day summer period'],
