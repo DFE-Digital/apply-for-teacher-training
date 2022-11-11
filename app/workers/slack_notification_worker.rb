077222412a3638a68a00dc4fad83846595eb99fb
James Glenn <47917431+JR-G@users.noreply.github.com>
2022-11-11 15:10:24 +0000

    Update slack worker to take a channel argument

--- app/workers/slack_notification_worker.rb
+++ app/workers/slack_notification_worker.rb
@@ -3,12 +3,12 @@ require 'http'
 class SlackNotificationWorker
   include Sidekiq::Worker
 
-  def perform(text, url = nil)
+  def perform(text, url = nil, channel = nil)
     @webhook_url = ENV['STATE_CHANGE_SLACK_URL']
 
     if @webhook_url.present?
       message = url.present? ? hyperlink(text, url) : text
-      post_to_slack message
+      post_to_slack message, channel
     end
   end
 
@@ -18,10 +18,10 @@ private
     "<#{url}|#{text}>"
   end
 
-  def post_to_slack(text)
+  def post_to_slack(text, channel)
     if HostingEnvironment.production?
       slack_message = text
-      slack_channel = '#twd_apply_support'
+      slack_channel = channel || '#twd_apply_support'
     else
       slack_message = "[#{HostingEnvironment.environment_name.upcase}] #{text}"
       slack_channel = '#twd_apply_test'
