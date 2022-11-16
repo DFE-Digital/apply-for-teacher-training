8eef1eb412ab82404c480ab4e2b3910d54bec5e3
James Glenn <47917431+JR-G@users.noreply.github.com>
2022-11-16 15:17:38 +0000

    Include last cycle numbers in stat summary slack post

--- app/services/stats_summary.rb
+++ app/services/stats_summary.rb
@@ -5,47 +5,51 @@ class StatsSummary
     <<~MARKDOWN
       *Today on Apply*
 
-      :wave: #{pluralize(candidate_signups, 'candidate signup')}
-      :pencil: #{pluralize(applications_begun, 'application')} begun
-      :#{mailbox_emoji(applications_submitted)}: #{pluralize(applications_submitted, 'application')} submitted
-      :#{rand(2) == 1 ? 'wo' : nil}man-tipping-hand: #{pluralize(offers_made, 'offer')} made
-      :#{rand(2) == 1 ? 'wo' : nil}man-gesturing-no: #{pluralize(rejections_issued, 'rejection')} issued#{rejections_issued.positive? ? ", of which #{pluralize(rbd_count, 'was')} RBD" : nil}
-      :handshake: #{pluralize(candidates_recruited, 'candidate')} recruited
+      :wave: #{pluralize(candidate_signups(today), 'candidate signup')} | #{candidate_signups(this_day_last_year)} last cycle
+      :pencil: #{pluralize(applications_begun(today), 'application')} begun | #{applications_begun(this_day_last_year)} last cycle
+      :#{mailbox_emoji(applications_submitted(today))}: #{pluralize(applications_submitted(today), 'application')} submitted | #{applications_submitted(this_day_last_year)} last cycle
+      :#{rand(2) == 1 ? 'wo' : nil}man-tipping-hand: #{pluralize(offers_made(today), 'offer')} made | #{offers_made(this_day_last_year)} last cycle
+      :#{rand(2) == 1 ? 'wo' : nil}man-gesturing-no: #{pluralize(rejections_issued(today), 'rejection')} issued#{rejections_issued(today).positive? ? ", of which #{pluralize(rbd_count(today), 'was')} RBD" : nil} | #{rejections_issued(this_day_last_year)} last cycle
+      :handshake: #{pluralize(candidates_recruited(today), 'candidate')} recruited | #{candidates_recruited(this_day_last_year)} last cycle
     MARKDOWN
   end
 
-  def candidate_signups
-    Candidate.where('created_at > ?', beginning_of_period).count
+  def candidate_signups(period)
+    Candidate.where(created_at: period).count
   end
 
-  def applications_begun
-    ApplicationForm.where('created_at > ?', beginning_of_period).count
+  def applications_begun(period)
+    ApplicationForm.where(created_at: period).count
   end
 
-  def applications_submitted
-    ApplicationForm.where('submitted_at > ?', beginning_of_period).count
+  def applications_submitted(period)
+    ApplicationForm.where(submitted_at: period).count
   end
 
-  def offers_made
-    Offer.where('created_at > ?', beginning_of_period).count
+  def offers_made(period)
+    Offer.where(created_at: period).count
   end
 
-  def candidates_recruited
-    ApplicationChoice.where('recruited_at > ?', beginning_of_period).count
+  def candidates_recruited(period)
+    ApplicationChoice.where(recruited_at: period).count
   end
 
-  def rejections_issued
-    ApplicationChoice.where('rejected_at > ?', beginning_of_period).count
+  def rejections_issued(period)
+    ApplicationChoice.where(rejected_at: period).count
   end
 
-  def rbd_count
-    ApplicationChoice.where('rejected_at > ?', beginning_of_period).where(rejected_by_default: true).count
+  def rbd_count(period)
+    ApplicationChoice.where(rejected_at: period).where(rejected_by_default: true).count
   end
 
 private
 
-  def beginning_of_period
-    Time.zone.now.beginning_of_day
+  def today
+    Time.zone.now.beginning_of_day..Time.zone.now
+  end
+
+  def this_day_last_year
+    CycleTimetable.this_working_day_last_cycle.beginning_of_day..CycleTimetable.this_working_day_last_cycle
   end
 
   def mailbox_emoji(message_count)
