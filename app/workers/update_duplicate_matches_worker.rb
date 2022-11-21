82bbc9d42e2a88cd41008b98d751bf722b8a97a0
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2022-11-21 16:51:59 +0000

    Fix matcher bug again
    Because Sidekiq is serialising the arguments to JSON, the
    keys are coming back as strings.
    
    I didn't know this before, but splatting a hash as keyword
    arguments only works if the keys are symbols.

--- app/workers/update_duplicate_matches_worker.rb
+++ app/workers/update_duplicate_matches_worker.rb
@@ -4,6 +4,7 @@ class UpdateDuplicateMatchesWorker
   sidekiq_options retry: 0, queue: :low_priority
 
   def perform(options = {})
+    options.symbolize_keys!
     UpdateDuplicateMatches.new(**options).save!
   end
 end
