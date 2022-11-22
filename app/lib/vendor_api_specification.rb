1a4abb1f3fa73bcceada4693183da6d00db06233
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2022-11-22 15:49:18 +0000

    Tidy up some of the API YAML file loading
    We don't need to merge the draft version in every time, just once at the
    end.

--- app/lib/vendor_api_specification.rb
+++ app/lib/vendor_api_specification.rb
@@ -18,19 +18,22 @@ class VendorAPISpecification
   end
 
   def spec
-    yaml_for_version = nil
-    (0..minor_version_number(@version)).each_with_object({}) do |minor_version, versions|
+    yaml_for_version = (0..minor_version_number(@version)).each_with_object({}) do |minor_version, versions|
       version_to_load = "#{major_version_number(@version)}.#{minor_version}"
 
+      yaml_for_version = nil
       if File.exist?(yaml_file_path(version_to_load))
         yaml_for_version = YAML.load_file(yaml_file_path(version_to_load), permitted_classes: [Time, Date])
-
-        merge_experimental_spec!(yaml_for_version, version_to_load)
+        yaml_for_version = merge_experimental_spec(yaml_for_version, version_to_load)
       end
 
-      merge_draft_spec!(yaml_for_version)
-
       versions.deep_merge!(yaml_for_version) if yaml_for_version.present?
+    end
+
+    if draft?
+      merge_draft_spec(yaml_for_version)
+    else
+      yaml_for_version
     end
   end
 
@@ -40,22 +43,24 @@ private
     "#{SPEC_FILE_DIR}/v#{version_to_load}.yml"
   end
 
-  def merge_experimental_spec!(yaml_for_version, version_to_load)
+  def merge_experimental_spec(yaml_for_version, version_to_load)
     experimental_yaml_file_for_version = "#{SPEC_FILE_DIR}/experimental-v#{version_to_load}.yml"
 
     if File.exist?(experimental_yaml_file_for_version)
-      yaml_for_version.deep_merge!(YAML.load_file(experimental_yaml_file_for_version))
+      yaml_for_version.deep_merge(YAML.load_file(experimental_yaml_file_for_version))
+    else
+      yaml_for_version
     end
-
-    yaml_for_version
   end
 
-  def merge_draft_spec!(yaml_for_version)
-    if draft? && File.exist?(DRAFT_YAML_FILE_PATH)
-      yaml_for_version.deep_merge!(YAML.load_file(DRAFT_YAML_FILE_PATH))
-    end
+  def merge_draft_spec(yaml_for_version)
+    return nil if yaml_for_version.blank?
 
-    yaml_for_version
+    if File.exist?(DRAFT_YAML_FILE_PATH)
+      yaml_for_version.deep_merge(YAML.load_file(DRAFT_YAML_FILE_PATH))
+    else
+      yaml_for_version
+    end
   end
 
   def draft?
