ca2d85b647f7938d0f442684bf0a25d270ad5028
Tomas D'Stefano <tomas_stefano@successoft.com>
2022-11-28 09:15:44 +0000

    Add system spec back for monthly statistics

--- spec/system/monthly_statistics/monthly_statistics_page_spec.rb
+++ spec/system/monthly_statistics/monthly_statistics_page_spec.rb
@@ -12,6 +12,9 @@ RSpec.feature 'Monthly statistics page', mid_cycle: false do
 
   scenario 'User can download a CSV from the monthly statistics page' do
     given_i_visit_the_monthly_statistics_page
+    and_i_see_the_monthly_statistics
+    when_i_click_a_link
+    then_a_csv_downloads
   end
 
   def create_monthly_stats_report
@@ -20,5 +23,17 @@ RSpec.feature 'Monthly statistics page', mid_cycle: false do
 
   def given_i_visit_the_monthly_statistics_page
     visit '/publications/monthly-statistics'
+  end
+
+  def and_i_see_the_monthly_statistics
+    expect(page).to have_content "Initial teacher training applications for courses starting in the #{RecruitmentCycle.cycle_name(CycleTimetable.next_year)} academic year"
+  end
+
+  def when_i_click_a_link
+    click_link 'Applications by status (CSV)'
+  end
+
+  def then_a_csv_downloads
+    expect(page).to(have_text('Status,First application,Apply again,Total'))
   end
 end
