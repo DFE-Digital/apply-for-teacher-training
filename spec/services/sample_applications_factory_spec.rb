94566a2c64d2ec8fcc9f7b1ec54e9826aa0be146
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2022-12-19 09:00:27 +0000

    Expand functionality in `SampleApplicationsFactory`

--- spec/services/sample_applications_factory_spec.rb
+++ spec/services/sample_applications_factory_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe SampleApplicationsFactory do
-  describe '.generate_applications(count, provider:)' do
-    subject(:applications) { described_class.generate_applications(count, provider:) }
+  describe '.generate_basic_applications(count, provider:)' do
+    subject(:applications) { described_class.generate_basic_applications(count, provider:) }
 
     let(:count) { 3 }
     let(:provider) { create(:provider) }
@@ -24,6 +24,51 @@ RSpec.describe SampleApplicationsFactory do
         expect(application.application_choices.count).to eq(1)
         expect(application.application_choices.first.course.provider).to eq(provider)
         expect(application.application_choices.first).to be_awaiting_provider_decision
+      end
+    end
+  end
+
+  describe '.generate_applications(**options)' do
+    subject(:applications) do
+      described_class.generate_applications(**options)
+    end
+
+    let(:options) do
+      {
+        provider:,
+        application_form_count:,
+        application_choice_count:,
+      }
+    end
+
+    let(:provider) { create(:provider) }
+
+    before do
+      2.times do
+        course = create(:course, :open_on_apply, provider:)
+        create(:course_option, course:)
+      end
+    end
+
+    context 'when a specific number of applications are requested' do
+      let(:application_form_count) { 3 }
+      let(:application_choice_count) { 2 }
+
+      it 'returns the requested set of `ApplicationForm`s' do
+        expect(applications.count).to eq(application_form_count)
+        expect(applications).to all(be_an(ApplicationForm))
+        applications.each do |application|
+          expect(application.application_choices.count).to eq(application_choice_count)
+        end
+      end
+    end
+
+    context 'when more than 3 application choices are requested' do
+      let(:application_form_count) { 3 }
+      let(:application_choice_count) { 4 }
+
+      it 'raises an error' do
+        expect { applications }.to raise_error(ArgumentError, 'appplication_choice_count cannot be greater than 3')
       end
     end
   end
