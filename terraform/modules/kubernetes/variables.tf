0dcffff322b1883ead0e35bf23c8d77bf3eb2769
Stephen Neal <smneal@users.noreply.github.com>
2023-01-25 17:00:13 +0000

    Add the AKS review app to the main workflow
    Add the deploy_v2 workflow to the main build and deploy workflow
    so it deploys the review app to AKS

--- terraform/modules/kubernetes/variables.tf
+++ terraform/modules/kubernetes/variables.tf
@@ -9,14 +9,14 @@ variable "webapp_startup_command" {}
 locals {
   app_config_name        = "apply-config-${var.app_environment}"
   app_secrets_name       = "apply-secrets-${var.app_environment}"
-  database_url           = "postgres://postgres:password@${local.postgres_service_name}:5432/${local.postgres_service_name}"
-  hostname               = "${local.webapp_name}.${var.cluster.dns_suffix}"
   postgres_service_name  = "apply-postgres-${var.app_environment}"
+  database_url           = "postgres://postgres:password@${local.postgres_service_name}:5432/${local.postgres_service_name}"
+  webapp_name            = "apply-${var.app_environment}"
+  hostname               = "${local.webapp_name}.${var.cluster.dns_suffix}"
   redis_service_name     = "apply-redis-${var.app_environment}"
   redis_url              = "redis://${local.redis_service_name}:6379/0"
   secondary_worker_name  = "apply-secondary-worker-${var.app_environment}"
   webapp_startup_command = var.webapp_startup_command == null ? null : ["/bin/sh", "-c", var.webapp_startup_command]
-  webapp_name            = "apply-${var.app_environment}"
   worker_name            = "apply-worker-${var.app_environment}"
 
   webapp_env_variables = merge(
@@ -27,6 +27,7 @@ locals {
       AUTHORISED_HOSTS = local.hostname
     }
   )
+  # Create a unique name based on the values to force recreation when they change
   webapp_env_variables_hash = sha1(join("-", [for k, v in local.webapp_env_variables : "${k}:${v}"]))
 
   app_secrets = merge(
@@ -37,5 +38,6 @@ locals {
       REDIS_URL           = local.redis_url
     }
   )
+  # Create a unique name based on the values to force recreation when they change
   app_secrets_hash = sha1(join("-", [for k, v in local.app_secrets : "${k}:${v}" if v != null]))
 }
