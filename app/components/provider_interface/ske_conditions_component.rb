9bc66e95c37b35228be4428793ac03f18ebdcaf7
Tomas D'Stefano <tomas_stefano@successoft.com>
2023-02-02 13:54:36 +0000

    Add right links to change reason and length on CYA page on SKE

--- app/components/provider_interface/ske_conditions_component.rb
+++ app/components/provider_interface/ske_conditions_component.rb
@@ -11,9 +11,13 @@ module ProviderInterface
     def summary_list_rows
       [
         { key: 'Subject', value: subject },
-        { key: 'Length', value: "#{length} weeks", action: { visually_hidden_text: 'change ske length', href: provider_interface_application_choice_offer_ske_length_path } },
-        { key: 'Reason', value: reason, action: { visually_hidden_text: 'change ske reason', href: provider_interface_application_choice_offer_ske_reason_path } },
+        { key: 'Length', value: "#{length} weeks", action: { visually_hidden_text: 'change ske length', href: new_provider_interface_application_choice_offer_ske_length_path } },
+        { key: 'Reason', value: reason, action: { visually_hidden_text: 'change ske reason', href: new_provider_interface_application_choice_offer_ske_reason_path } },
       ]
+    end
+
+    def remove_condition_path
+      provider_interface_application_choice_offer_ske_standard_flow_path
     end
   end
 end
