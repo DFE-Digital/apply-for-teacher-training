de13c40f7be55f2f9654c55ead94279b2fe395e3
Elliot Crosby-McCullough <elliot.cm@gmail.com>
2023-02-02 11:32:31 +0000

    Fix redis DB assignment for 16 core machines
    This was indexed one off, so the 16th core would try to use a 17th DB.
    Redis defaults to having 16 DBs.

--- spec/support/sidekiq.rb
+++ spec/support/sidekiq.rb
@@ -3,9 +3,24 @@ ActiveJob::Base.queue_adapter = :test
 RSpec.configure do |config|
   include ActiveJob::TestHelper
 
-  # Show the Redis connections being used when running tests in parallel
-  if ENV['TEST_ENV_NUMBER'] && ENV['DEBUG_REDIS_CONNECTIONS']
-    Sidekiq.redis { |c| p c }
+  # If running tests in parallel, use a unique Redis database per test process.
+  # See https://github.com/grosser/parallel_tests#add-to-configdatabaseyml
+  if ENV.key?('TEST_ENV_NUMBER')
+    redis_url_without_database = ENV['REDIS_URL']&.gsub(/\/\d+$/, '') || 'redis://localhost:6379'
+    redis_db_index = (ENV['TEST_ENV_NUMBER'].presence || 1).to_i - 1
+    redis_url_with_database = "#{redis_url_without_database}/#{redis_db_index}"
+
+    if ENV['DEBUG_REDIS_CONNECTIONS']
+      # Show the Redis connections being used when running tests in parallel
+      puts "Using Redis database URL `#{redis_url_with_database}`"
+      config.before { Sidekiq.redis { |c| p c } }
+    end
+
+    config.around do |example|
+      ClimateControl.modify(REDIS_URL: redis_url_with_database) do
+        example.run
+      end
+    end
   end
 
   # Turn Sidekiq on automatically in system tests. Use `sidekiq: false` in
