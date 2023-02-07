88a7f38e2d0f2215bb2d0f964abd969aae42db77
Ross Oliver <ross.oliver36@gmail.com>
2023-02-07 16:08:47 +0000

    Add job to sign up for an adviser
    The job accepts a pre-validated `application_form` and
    `preferred_teaching_subject_id`. It is responsible for constructing a
    suitable/valid request payload from the provided details and submitting
    it to the Get into Teaching API.
    
    It is possible that the `ApplicationForm` associated with the job could
    mutate into a state that is no longer applicable for an adviser if the
    candidate updates the details while the job is in the queue. I would
    expect this to be very edge-case as most jobs will execute fairly
    instantaneously and the candidate would likely have to undo a part of
    their application form. If we find that we do see errors from the sign up
    submission we could expand the job to check for eligibility and notify
    the candidate in this scenario.
    
    There is no matchback logic implemented yet; all submissions will be
    treated as new candidates in the CRM.

---
+++ app/workers/adviser_sign_up_worker.rb
@@ -0,0 +1,132 @@
+class AdviserSignUpWorker
+  include Sidekiq::Worker
+
+  attr_reader :application_form, :preferred_teaching_subject_id
+
+  CHANNELS = {
+    apply: 222_750_049,
+  }.freeze
+  DEGREE_STATUS = {
+    graduated: 222_750_000,
+    studying: 222_750_001,
+  }.freeze
+  UK_DEGREE_GRADES = {
+    'First-class honours' => 222_750_001,
+    'Upper second-class honours (2:1)' => 222_750_002,
+    'Lower second-class honours (2:2)' => 222_750_003,
+  }.freeze
+  DEGREE_TYPES = {
+    domestic: 222_750_000,
+    international: 222_750_005,
+  }.freeze
+  TYPES = {
+    interested_in_teacher_training: 222_750_000,
+  }.freeze
+  GCSE = {
+    yes: 222_750_000,
+    no: 222_750_001,
+  }.freeze
+  SUBJECTS = {
+    primary: 'b02655a1-2afa-e811-a981-000d3a276620',
+  }.freeze
+  EDUCATION_PHASES = {
+    primary: 222_750_000,
+    secondary: 222_750_001,
+  }.freeze
+  COUNTRIES = {
+    unknown: '76f5c2e6-74f9-e811-a97a-000d3a2760f2',
+  }.freeze
+
+  def perform(application_form_id, preferred_teaching_subject_id)
+    @application_form = Adviser::ApplicationFormValidations.new(
+      ApplicationForm.find(application_form_id),
+    )
+    @preferred_teaching_subject_id = preferred_teaching_subject_id
+
+    request = GetIntoTeachingApiClient::TeacherTrainingAdviserSignUp.new(attributes)
+    api.sign_up_teacher_training_adviser_candidate(request)
+  end
+
+private
+
+  def attributes
+    {
+      email: application_form.email_address,
+      first_name: application_form.first_name,
+      last_name: application_form.last_name,
+      date_of_birth: application_form.date_of_birth,
+      address_telephone: application_form.phone_number,
+      address_postcode: application_form.postcode,
+      country_id: country_id,
+      degree_subject: degree.subject,
+      uk_degree_grade_id: UK_DEGREE_GRADES[degree.grade],
+      degree_status_id: degree.completed? ? DEGREE_STATUS[:graduated] : DEGREE_STATUS[:studying],
+      degree_type_id: degree.international? ? DEGREE_TYPES[:international] : DEGREE_TYPES[:domestic],
+      has_gcse_maths_and_english_id: pass_gcse_maths_and_english? ? GCSE[:yes] : GCSE[:no],
+      planning_to_retake_gcse_maths_and_english_id: retaking_gcse_maths_and_english? ? GCSE[:yes] : GCSE[:no],
+      has_gcse_science_id: pass_gcse_science? ? GCSE[:yes] : GCSE[:no],
+      planning_to_retake_gcse_science_id: retaking_gcse_science? ? GCSE[:yes] : GCSE[:no],
+      preferred_teaching_subject_id: preferred_teaching_subject_id,
+      preferred_education_phase_id: preferred_education_phase_id,
+      initial_teacher_training_year_id: current_itt_year.id,
+      accepted_policy_id: latest_privacy_policy.id,
+      type_id: TYPES[:interested_in_teacher_training],
+      channel_id: CHANNELS[:apply],
+    }
+  end
+
+  def degree
+    application_form.applicable_degree
+  end
+
+  def pass_gcse_maths_and_english?
+    application_form.maths_gcse&.pass_gcse? && application_form.english_gcse&.pass_gcse?
+  end
+
+  def retaking_gcse_maths_and_english?
+    application_form.maths_gcse&.currently_completing_qualification? &&
+      application_form.english_gcse&.currently_completing_qualification?
+  end
+
+  def pass_gcse_science?
+    application_form.science_gcse&.pass_gcse?
+  end
+
+  def retaking_gcse_science?
+    application_form.science_gcse&.currently_completing_qualification?
+  end
+
+  def preferred_education_phase_id
+    preferred_teaching_subject_id == SUBJECTS[:primary] ? EDUCATION_PHASES[:primary] : EDUCATION_PHASES[:secondary]
+  end
+
+  def current_itt_year
+    today = Time.zone.today
+    cutoff_date = Date.new(today.year, 9, 7)
+    current_year = today < cutoff_date ? today.year : today.year + 1
+
+    itt_years.find { |itt_year| itt_year.value == current_year.to_s }
+  end
+
+  def itt_years
+    @itt_years ||= GetIntoTeachingApiClient::PickListItemsApi.new.get_candidate_initial_teacher_training_years
+  end
+
+  def countries
+    @countries ||= GetIntoTeachingApiClient::LookupItemsApi.new.get_countries
+  end
+
+  def latest_privacy_policy
+    @latest_privacy_policy ||= GetIntoTeachingApiClient::PrivacyPoliciesApi.new.get_latest_privacy_policy
+  end
+
+  def api
+    @api ||= GetIntoTeachingApiClient::TeacherTrainingAdviserApi.new
+  end
+
+  def country_id
+    matching_country = countries.find { |country| country.iso_code == application_form.country }
+
+    matching_country&.id || COUNTRIES[:unknown]
+  end
+end
---
+++ spec/workers/adviser_sign_up_worker_spec.rb
@@ -0,0 +1,181 @@
+require 'rails_helper'
+
+RSpec.describe AdviserSignUpWorker do
+  include_context 'get into teaching api stubbed endpoints'
+
+  before { TestSuiteTimeMachine.travel_permanently_to(date) }
+
+  let(:date) { Date.new(Time.zone.today.year, 9, 6) }
+  let(:application_form) { create(:application_form_eligible_for_adviser) }
+  let(:degree) { application_form.application_qualifications.degrees.last }
+
+  subject(:perform) { described_class.new.perform(application_form.id, preferred_teaching_subject.id) }
+
+  describe '#perform' do
+    after { perform }
+
+    it 'sends a request to sign up for an adviser' do
+      expect_sign_up do |attributes|
+        expect(attributes.values).to all(be_present)
+      end
+    end
+
+    it 'sends graduated for degree_status_id if the degree has been completed' do
+      degree.update(predicted_grade: false)
+      expect_sign_up({
+        degree_status_id: described_class::DEGREE_STATUS[:graduated],
+      })
+    end
+
+    it 'sends unknown for country_id when the country ISO code is not matched' do
+      application_form.update(country: 'UNMATCHED')
+      expect_sign_up({
+        country_id: described_class::COUNTRIES[:unknown],
+      })
+    end
+
+    context 'when the preferred_teaching_subject_id is primary' do
+      let(:primary_subject_id) { described_class::SUBJECTS[:primary] }
+      let(:preferred_teaching_subject) { GetIntoTeachingApiClient::TeachingSubject.new(id: primary_subject_id) }
+
+      it 'sends primary for preferred_education_phase_id' do
+        expect_sign_up({
+          preferred_education_phase_id: described_class::EDUCATION_PHASES[:primary],
+        })
+      end
+    end
+
+    context "when it's after the current ITT year cutoff point" do
+      let(:date) { Date.new(Time.zone.today.year, 9, 7) }
+
+      it 'sends next year as the initial_teacher_training_year_id' do
+        expect_sign_up({
+          initial_teacher_training_year_id: next_year.id,
+        })
+      end
+    end
+
+    context 'when the applicable degree is international' do
+      let(:application_form) { create(:completed_application_form, :with_international_adviser_qualifications) }
+
+      it 'sends the international degree type' do
+        expect_sign_up({
+          degree_type_id: described_class::DEGREE_TYPES[:international],
+        })
+      end
+
+      it 'sends nil for uk_degree_grade_id if the grade is not recognised' do
+        degree.update(grade: '100%')
+        expect_sign_up({
+          degree_type_id: anything,
+          uk_degree_grade_id: nil,
+        })
+      end
+    end
+
+    describe 'sending GCSE qualification details' do
+      it "sends no for has_gcse_maths_and_english_id if they haven't got Maths or English GCSEs" do
+        application_form.maths_gcse.destroy
+        application_form.english_gcse.destroy
+
+        expect_sign_up({
+          has_gcse_maths_and_english_id: described_class::GCSE[:no],
+        })
+      end
+
+      it 'sends no for has_gcse_maths_and_english_id if they have passed Maths but not English GCSEs' do
+        application_form.english_gcse.update(grade: 'Z')
+
+        expect_sign_up({
+          has_gcse_maths_and_english_id: described_class::GCSE[:no],
+        })
+      end
+
+      it 'sends no for has_gcse_maths_and_english_id if they have passed English but not Maths GCSEs' do
+        application_form.maths_gcse.update(grade: 'Z')
+
+        expect_sign_up({
+          has_gcse_maths_and_english_id: described_class::GCSE[:no],
+        })
+      end
+
+      it "sends no for has_gcse_science_id if they haven't got a Science GCSE" do
+        application_form.science_gcse.destroy
+
+        expect_sign_up({
+          has_gcse_science_id: described_class::GCSE[:no],
+        })
+      end
+
+      it 'sends no for has_gcse_science_id if they have not passed their Science GCSE' do
+        application_form.science_gcse.update(grade: 'Z')
+
+        expect_sign_up({
+          has_gcse_science_id: described_class::GCSE[:no],
+        })
+      end
+
+      it 'sends yes for planning_to_retake_gcse_maths_and_english_id if they are completing both Maths and English GCSEs' do
+        application_form.maths_gcse.update(currently_completing_qualification: true)
+        application_form.english_gcse.update(currently_completing_qualification: true)
+
+        expect_sign_up({
+          planning_to_retake_gcse_maths_and_english_id: described_class::GCSE[:yes],
+        })
+      end
+
+      it 'sends yes for planning_to_retake_gcse_science_id if they are completing their Science GCSE' do
+        application_form.science_gcse.update(currently_completing_qualification: true)
+
+        expect_sign_up({
+          planning_to_retake_gcse_science_id: described_class::GCSE[:yes],
+        })
+      end
+    end
+  end
+
+  def expect_request_attribtues(attributes, expected_attributes)
+    expect(attributes).to include(expected_attributes)
+  end
+
+  def expect_sign_up(expected_attribute_overrides = {})
+    expect_any_instance_of(GetIntoTeachingApiClient::TeacherTrainingAdviserApi).to \
+      receive(:sign_up_teacher_training_adviser_candidate) do |_, request|
+        request_attributes = extract_attributes(request)
+        expect_request_attribtues(request_attributes, baseline_attributes.merge(expected_attribute_overrides))
+        yield(request_attributes) if block_given?
+      end
+  end
+
+  def extract_attributes(request)
+    request.to_hash.transform_keys do |key|
+      request.class.attribute_map.invert[key]
+    end
+  end
+
+  def baseline_attributes
+    {
+      email: application_form.candidate.email_address,
+      first_name: application_form.first_name,
+      last_name: application_form.last_name,
+      date_of_birth: application_form.date_of_birth,
+      address_telephone: application_form.phone_number,
+      address_postcode: application_form.postcode,
+      country_id: country.id,
+      degree_subject: degree.subject,
+      uk_degree_grade_id: described_class::UK_DEGREE_GRADES[degree.grade],
+      degree_status_id: described_class::DEGREE_STATUS[:studying],
+      degree_type_id: described_class::DEGREE_TYPES[:domestic],
+      has_gcse_maths_and_english_id: described_class::GCSE[:yes],
+      planning_to_retake_gcse_maths_and_english_id: described_class::GCSE[:no],
+      has_gcse_science_id: described_class::GCSE[:yes],
+      planning_to_retake_gcse_science_id: described_class::GCSE[:no],
+      preferred_teaching_subject_id: preferred_teaching_subject.id,
+      preferred_education_phase_id: described_class::EDUCATION_PHASES[:secondary],
+      initial_teacher_training_year_id: this_year.id,
+      accepted_policy_id: privacy_policy.id,
+      type_id: described_class::TYPES[:interested_in_teacher_training],
+      channel_id: described_class::CHANNELS[:apply],
+    }
+  end
+end
