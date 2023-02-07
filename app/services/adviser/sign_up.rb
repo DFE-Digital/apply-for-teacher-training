86cebc605efa03406222428bf777ac82744b9e5d
Ross Oliver <ross.oliver36@gmail.com>
2023-02-07 13:09:43 +0000

    Add model to sign up for an adviser
    Add a model to sign up for an adviser; in order to sign up the
    `ApplicationForm` must be in an applicable state and the relevant feature
    must be enabled.
    
    As the standard `ApplicationForm` does not ask the user what their
    preferred teaching subject is we need to collect this information as part
    of the submission. We verify it against the list of subject items in the
    GiT API/CRM.

---
+++ app/services/adviser/sign_up.rb
@@ -0,0 +1,49 @@
+class Adviser::SignUp
+  class AdviserSignUpUnavailableError < RuntimeError; end
+
+  include ActiveModel::Model
+  include ActiveModel::Attributes
+
+  attr_reader :application_form
+  attribute :preferred_teaching_subject
+
+  validates :preferred_teaching_subject, inclusion: { in: :teaching_subject_names, allow_blank: false }
+
+  def initialize(application_form, *args)
+    @application_form = Adviser::ApplicationFormValidations.new(application_form)
+
+    super(*args)
+  end
+
+  def save
+    raise AdviserSignUpUnavailableError unless available?
+
+    return false if invalid?
+
+    AdviserSignUpWorker.perform_async(application_form.id, preferred_teaching_subject_id)
+
+    true
+  end
+
+  def available?
+    feature_active? && application_form.valid?
+  end
+
+  def teaching_subjects
+    @teaching_subjects ||= GetIntoTeachingApiClient::LookupItemsApi.new.get_teaching_subjects
+  end
+
+private
+
+  def preferred_teaching_subject_id
+    teaching_subjects.find { |subject| subject.value == preferred_teaching_subject }&.id
+  end
+
+  def teaching_subject_names
+    teaching_subjects.map(&:value)
+  end
+
+  def feature_active?
+    FeatureFlag.active?(:adviser_sign_up)
+  end
+end
