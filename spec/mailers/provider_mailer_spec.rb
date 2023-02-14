937e7dbec0a7ccb5716737f64cdc6b4382661f8d
Carlos Martinez <carlosjmtz@gmail.com>
2023-02-14 16:46:00 +0000

    Provider mailer: conditional text if eligible for IRP
    When a candidate submits an application that fulfils certain criteria
    (non-British, lives outside UK/Ireland, right to work in UK, and wishing
    to teach classics, modern languages or physics), the provider mailer
    application submitted emails should include some text about the
    candidate's eligibility for international relocation payment (IRP).

--- spec/mailers/provider_mailer_spec.rb
+++ spec/mailers/provider_mailer_spec.rb
@@ -37,6 +37,18 @@ RSpec.describe ProviderMailer do
                     'link to application' => /http:\/\/localhost:3000\/provider\/applications\/\d+/,
                     'notification settings' => 'You can change your email notification settings',
                     'footer' => 'Get help, report a problem or give feedback')
+
+    context 'eligible for international relocation payment' do
+      before do
+        allow(IsEligibleForInternationalRelocationPayment)
+          .to receive(:new)
+          .and_return(instance_double(IsEligibleForInternationalRelocationPayment, call: true))
+      end
+
+      it_behaves_like('a mail with subject and content',
+                      'Harry Potter submitted an application for Computer Science - manage teacher training applications',
+                      'international relocation' => 'help with the financial costs of moving')
+    end
   end
 
   describe 'Send application submitted with safeguarding issues email' do
@@ -53,6 +65,18 @@ RSpec.describe ProviderMailer do
                     'link to application' => /http:\/\/localhost:3000\/provider\/applications\/\d+/,
                     'notification settings' => 'You can change your email notification settings',
                     'footer' => 'Get help, report a problem or give feedback')
+
+    context 'eligible for international relocation payment' do
+      before do
+        allow(IsEligibleForInternationalRelocationPayment)
+          .to receive(:new)
+          .and_return(instance_double(IsEligibleForInternationalRelocationPayment, call: true))
+      end
+
+      it_behaves_like('a mail with subject and content',
+                      'Safeguarding issues - Harry Potter submitted an application for Computer Science - manage teacher training applications',
+                      'international relocation' => 'help with the financial costs of moving')
+    end
   end
 
   describe 'Send application rejected by default email' do
