6ff597d9617fa6524cae191e7b2ba8421d64a665
Tomas D'Stefano <tomas_stefano@successoft.com>
2023-02-14 00:45:15 +0000

    Show ske condition on offer summary

--- app/components/provider_interface/ske_conditions_component.rb
+++ app/components/provider_interface/ske_conditions_component.rb
@@ -1,18 +1,27 @@
 module ProviderInterface
   class SkeConditionsComponent < ViewComponent::Base
-    attr_reader :application_choice, :ske_condition
+    attr_reader :application_choice, :ske_condition, :editable
     delegate :reason, :length, to: :ske_condition
 
-    def initialize(application_choice:, ske_condition:)
+    def initialize(application_choice:, ske_condition:, editable:)
       @application_choice = application_choice
       @ske_condition = ske_condition
+      @editable = editable
     end
 
     def summary_list_rows
       [
         { key: 'Subject', value: subject },
-        { key: 'Length', value: "#{length} weeks", action: { visually_hidden_text: 'change ske length', href: new_provider_interface_application_choice_offer_ske_length_path } },
-        { key: 'Reason', value: reason, action: { visually_hidden_text: 'change ske reason', href: new_provider_interface_application_choice_offer_ske_reason_path } },
+        {
+          key: 'Length',
+          value: "#{length} weeks",
+          action: editable ? { visually_hidden_text: 'change ske length', href: new_provider_interface_application_choice_offer_ske_length_path } : {}
+        },
+        {
+          key: 'Reason',
+          value: reason,
+          action: editable ? { visually_hidden_text: 'change ske reason', href: new_provider_interface_application_choice_offer_ske_reason_path } : {}
+        },
       ]
     end
 
