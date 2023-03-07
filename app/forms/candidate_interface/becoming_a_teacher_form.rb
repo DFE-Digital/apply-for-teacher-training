a2f871e270fdb93432ee1d990507b783aae1e647
James Glenn <47917431+JR-G@users.noreply.github.com>
2023-03-07 11:46:08 +0000

    Dynamic word count

--- app/forms/candidate_interface/becoming_a_teacher_form.rb
+++ app/forms/candidate_interface/becoming_a_teacher_form.rb
@@ -6,14 +6,21 @@ module CandidateInterface
 
     alias single_personal_statement? single_personal_statement
 
-    validates :becoming_a_teacher,
-              word_count: { maximum: 600 },
-              presence: true
+    validates :becoming_a_teacher, presence: true
+    validates :becoming_a_teacher, word_count: { maximum: 1000 }, if: :single_personal_statement?
+    validates :becoming_a_teacher, word_count: { maximum: 600 }, unless: :single_personal_statement?
 
     def self.build_from_application(application_form)
       new(
         single_personal_statement: application_form.single_personal_statement?,
         becoming_a_teacher: application_form.becoming_a_teacher,
+      )
+    end
+
+    def self.build_from_params(params)
+      new(
+        single_personal_statement: params[:single_personal_statement],
+        becoming_a_teacher: params[:becoming_a_teacher],
       )
     end
 
