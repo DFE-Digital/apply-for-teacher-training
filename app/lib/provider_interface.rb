d477c7c2e5df330e4d8efcd15b6f3815d5fff28d
Frankie Roberto <frankie@frankieroberto.com>
2023-03-24 14:14:27 +0000

    Replace feedback form link with email link
    The Google form will no longer work once we have transitioned away from GSuite.

--- app/lib/provider_interface.rb
+++ app/lib/provider_interface.rb
@@ -1,3 +1,3 @@
 module ProviderInterface
-  FEEDBACK_LINK = 'https://docs.google.com/forms/d/e/1FAIpQLSeJfcE9cITcICGIbwnBn-xYGT24bn_Us_AUYVuA2GtJOZmIoA/viewform?usp=sf_link'.freeze
+  FEEDBACK_LINK = 'mailto:becomingateacher@digital.education.gov.uk'.freeze
 end
