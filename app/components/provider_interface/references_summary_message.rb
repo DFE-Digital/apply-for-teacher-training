4c9e998729b664269e0d84123f9cba596f56fb18
Maeve Roseveare <68232608+MaeveRoseveare@users.noreply.github.com>
2023-03-24 13:11:12 +0000

    Adding line to references page for providers

--- app/components/provider_interface/references_summary_message.rb
+++ app/components/provider_interface/references_summary_message.rb
@@ -1,18 +1,19 @@
 module ProviderInterface
   class ReferencesSummaryMessage < ViewComponent::Base
-    attr_reader :references
+    attr_reader :references, :full_name
 
     delegate :feedback_provided, :feedback_requested, to: :references
 
-    def initialize(references)
+    def initialize(references, full_name)
       @references = references
+      @full_name = full_name
     end
 
     def message
       if feedback_provided.blank?
-        "The candidate #{I18n.t('provider_interface.references.requested_message', count: feedback_requested.count)}."
+        "The candidate #{I18n.t('provider_interface.references.requested_message', count: feedback_requested.count)}. #{I18n.t('provider_interface.references.do_not_share_message', name: full_name)}"
       else
-        "The candidate #{I18n.t('provider_interface.references.received_message', count: feedback_provided.count)}#{other_requested_reference}."
+        "The candidate #{I18n.t('provider_interface.references.received_message', count: feedback_provided.count)}#{other_requested_reference}. #{I18n.t('provider_interface.references.do_not_share_message', name: full_name)}"
       end
     end
 
