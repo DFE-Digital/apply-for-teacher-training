f71c5ab07018194a3b27c38109919b44dc173e20
Colin Saliceti <colin.saliceti@digital.education.gov.uk>
2023-03-28 22:08:40 +0100

    Refactor Azure backing services
    - Deploy postgres, redis to the main app resource group
    - Split azure-resources.tf file
    - Use existing variables from global config instead of recreating them

--- terraform/aks/data.tf
+++ terraform/aks/data.tf
@@ -1,6 +1,6 @@
 data "azurerm_key_vault" "key_vault" {
   name                = var.key_vault_name
-  resource_group_name = var.key_vault_resource_group
+  resource_group_name = local.app_resource_group_name
 }
 
 data "azurerm_key_vault_secret" "app_secrets" {
--- terraform/modules/kubernetes/azure_redis.tf
+++ terraform/modules/kubernetes/azure_redis.tf
@@ -1,56 +1,9 @@
-resource "azurerm_postgresql_flexible_server" "postgres-server" {
-  count = var.deploy_azure_backing_services ? 1 : 0
-
-  name                   = local.postgres_server_name
-  location               = data.azurerm_resource_group.backing-service-resource-group[0].location
-  resource_group_name    = data.azurerm_resource_group.backing-service-resource-group[0].name
-  version                = var.postgres_version
-  administrator_login    = var.postgres_admin_username
-  administrator_password = var.postgres_admin_password
-  create_mode            = "Default"
-  storage_mb             = var.postgres_flexible_server_storage_mb
-  sku_name               = var.postgres_flexible_server_sku
-  delegated_subnet_id    = data.azurerm_subnet.postgres-subnet[0].id
-  private_dns_zone_id    = data.azurerm_private_dns_zone.postgres-dns[0].id
-  dynamic "high_availability" {
-    for_each = var.postgres_enable_high_availability ? [1] : []
-    content {
-      mode = "ZoneRedundant"
-    }
-  }
-  lifecycle {
-    ignore_changes = [
-      tags,
-      # Allow Azure to manage deployment zone. Ignore changes.
-      zone,
-      # Allow Azure to manage primary and standby server on fail-over. Ignore changes.
-      high_availability[0].standby_availability_zone
-    ]
-  }
-}
-
-resource "azurerm_postgresql_flexible_server_configuration" "postgres-extensions" {
-  count = var.deploy_azure_backing_services ? 1 : 0
-
-  name      = "azure.extensions"
-  server_id = azurerm_postgresql_flexible_server.postgres-server[0].id
-  value     = "PG_BUFFERCACHE,PG_STAT_STATEMENTS,PGCRYPTO"
-}
-
-resource "azurerm_postgresql_flexible_server_configuration" "max-connections" {
-  count = var.deploy_azure_backing_services ? 1 : 0
-
-  name      = "max_connections"
-  server_id = azurerm_postgresql_flexible_server.postgres-server[0].id
-  value     = 856 # Maximum on GP_Standard_D2ds_v4. See: https://learn.microsoft.com/en-us/azure/postgresql/flexible-server/concepts-limits#maximum-connections
-}
-
 resource "azurerm_redis_cache" "redis-cache" {
   count = var.deploy_azure_backing_services ? 1 : 0
 
   name                          = local.redis_cache_name
-  location                      = data.azurerm_resource_group.backing-service-resource-group[0].location
-  resource_group_name           = data.azurerm_resource_group.backing-service-resource-group[0].name
+  location                      = data.azurerm_resource_group.app-resource-group.location
+  resource_group_name           = data.azurerm_resource_group.app-resource-group.name
   capacity                      = var.redis_capacity
   family                        = var.redis_family
   sku_name                      = var.redis_sku_name
@@ -78,8 +31,8 @@ resource "azurerm_private_endpoint" "redis-cache-private-endpoint" {
   count = var.deploy_azure_backing_services ? 1 : 0
 
   name                = local.redis_cache_private_endpoint_name
-  location            = data.azurerm_resource_group.backing-service-resource-group[0].location
-  resource_group_name = data.azurerm_resource_group.backing-service-resource-group[0].name
+  location            = data.azurerm_resource_group.app-resource-group.location
+  resource_group_name = data.azurerm_resource_group.app-resource-group.name
   subnet_id           = data.azurerm_subnet.redis-subnet[0].id
 
   private_dns_zone_group {
@@ -105,8 +58,8 @@ resource "azurerm_redis_cache" "redis-queue" {
   count = var.deploy_azure_backing_services ? 1 : 0
 
   name                          = local.redis_queue_name
-  location                      = data.azurerm_resource_group.backing-service-resource-group[0].location
-  resource_group_name           = data.azurerm_resource_group.backing-service-resource-group[0].name
+  location                      = data.azurerm_resource_group.app-resource-group.location
+  resource_group_name           = data.azurerm_resource_group.app-resource-group.name
   capacity                      = var.redis_capacity
   family                        = var.redis_family
   sku_name                      = var.redis_sku_name
@@ -133,8 +86,8 @@ resource "azurerm_private_endpoint" "redis-queue-private-endpoint" {
   count = var.deploy_azure_backing_services ? 1 : 0
 
   name                = local.redis_queue_private_endpoint_name
-  location            = data.azurerm_resource_group.backing-service-resource-group[0].location
-  resource_group_name = data.azurerm_resource_group.backing-service-resource-group[0].name
+  location            = data.azurerm_resource_group.app-resource-group.location
+  resource_group_name = data.azurerm_resource_group.app-resource-group.name
   subnet_id           = data.azurerm_subnet.redis-subnet[0].id
 
   private_dns_zone_group {
