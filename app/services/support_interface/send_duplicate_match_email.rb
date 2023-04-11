7cd7d79d67e4fcc00372556452fe39ccb850bc34
MylesJarvis <myles.jarvis@education.gov.uk>
2023-04-11 14:35:06 +0100

    Testing updating the duplicate matching

--- app/services/support_interface/send_duplicate_match_email.rb
+++ app/services/support_interface/send_duplicate_match_email.rb
@@ -10,8 +10,5 @@ module SupportInterface
       CandidateMailer.duplicate_match_email(@candidate.current_application, submitted).deliver_later
     end
 
-    def submitted
-      @candidate.application_forms.map(&:submitted_at).any?
-    end
   end
 end
