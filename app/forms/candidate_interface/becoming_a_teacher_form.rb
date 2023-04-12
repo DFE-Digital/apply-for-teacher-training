46092baea29361d9f6b05522898b1a1f93c89153
Frankie Roberto <frankie@frankieroberto.com>
2023-04-12 13:24:10 +0100

    Fix conflict

--- app/forms/candidate_interface/becoming_a_teacher_form.rb
+++ app/forms/candidate_interface/becoming_a_teacher_form.rb
@@ -26,9 +26,6 @@ module CandidateInterface
     end
 
     def save(application_form)
-      return false unless valid?
-
-    def save(application_form)
       application_form.update(
         becoming_a_teacher:,
       )
