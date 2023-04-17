c12ae0ed17365840a8bb465ed7b8431e3d155ff2
Steve Hook <steve.hook@gmail.com>
2023-04-17 22:57:47 +0100

    Complete specs for `DeleteApplicationForm`

--- spec/forms/support_interface/application_forms/delete_application_form_spec.rb
+++ spec/forms/support_interface/application_forms/delete_application_form_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe SupportInterface::ApplicationForms::ChangeCourseChoiceForm, type: :model, with_audited: true do
+RSpec.describe SupportInterface::ApplicationForms::DeleteApplicationForm, type: :model, with_audited: true do
   include CourseOptionHelpers
 
   describe 'validations' do
@@ -18,14 +18,33 @@ RSpec.describe SupportInterface::ApplicationForms::ChangeCourseChoiceForm, type:
     end
   end
 
-  describe '#save!' do
+  describe '#save' do
     context 'if the application has already been submitted' do
+      let(:application_form) { create(:application_form, :completed, submitted_application_choices_count: 1) }
+      let(:actor) { create(:support_user) }
+
       it 'raises an error' do
+        form = described_class.new(
+          accept_guidance: true,
+          audit_comment_ticket: 'https://becomingateacher.zendesk.com/agent/tickets/12345',
+        )
+        expect { form.save(actor:, application_form:) }.to raise_error(RuntimeError)
+        expect(application_form.reload.date_of_birth).to be_present
       end
     end
 
     context 'if the application has not been submitted' do
+      let(:application_form) { create(:application_form, :minimum_info) }
+      let(:actor) { create(:support_user) }
+
       it 'deletes the application' do
+        form = described_class.new(
+          accept_guidance: true,
+          audit_comment_ticket: 'https://becomingateacher.zendesk.com/agent/tickets/12345',
+        )
+        form.save(actor:, application_form:)
+        expect(application_form.reload.first_name).not_to be_present
+        expect(application_form.date_of_birth).not_to be_present
       end
     end
   end
