6babd62dad6ae63418e88b30c7aeea27edd5536e
James Glenn <47917431+JR-G@users.noreply.github.com>
2023-04-18 12:02:21 +0100

    Export data as ZIP file

--- app/controllers/provider_interface/reports/diversity_reports_controller.rb
+++ app/controllers/provider_interface/reports/diversity_reports_controller.rb
@@ -1,21 +1,34 @@
 module ProviderInterface
   module Reports
     class DiversityReportsController < ProviderInterfaceController
-      include CSVNameHelper
       attr_reader :diversity_data, :provider
 
       def show
         @provider = current_user.providers.find(provider_id)
-        @diversity_data = DiversityDataByProvider.new(provider: provider)
-        @diversity_report_sex_data = diversity_data.sex_data
-        @diversity_report_disability_data = diversity_data.disability_data
-        @diversity_report_ethnicity_data = diversity_data.ethnicity_data
-        @diversity_report_age_data = diversity_data.age_data
-        @completed_e_and_d_survey_count = diversity_data.completed_e_and_d_survey_count
-        @total_submitted_applications = diversity_data.total_submitted_applications
+        zip_filename = ProviderInterface::DiversityReportExport.new(provider: provider).call
+
+        respond_to do |format|
+          format.zip do
+            send_file(
+              zip_filename,
+              filename: "#{provider.name.parameterize}-diversity-report-#{Time.zone.today}.zip",
+              type: 'application/zip'
+            )
+          end
+
+          format.html do
+            @diversity_data = DiversityDataByProvider.new(provider: provider)
+            @diversity_report_sex_data = diversity_data.sex_data
+            @diversity_report_disability_data = diversity_data.disability_data
+            @diversity_report_ethnicity_data = diversity_data.ethnicity_data
+            @diversity_report_age_data = diversity_data.age_data
+            @completed_e_and_d_survey_count = diversity_data.completed_e_and_d_survey_count
+            @total_submitted_applications = diversity_data.total_submitted_applications
+          end
+        end
       end
 
-    private
+      private
 
       def provider_id
         params.permit(:provider_id)[:provider_id]
@@ -23,3 +36,4 @@ module ProviderInterface
     end
   end
 end
+
