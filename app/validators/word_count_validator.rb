43d38462e0e17250b26b68a2e997f3a18cb3e69a
Frankie Roberto <frankie@frankieroberto.com>
2023-04-20 12:24:00 +0100

    Update word count validator
    This now includes the number of words that you've gone over, as well as the maximum.

--- app/validators/word_count_validator.rb
+++ app/validators/word_count_validator.rb
@@ -4,8 +4,11 @@ class WordCountValidator < ActiveModel::EachValidator
 
     # This RegExp should match the implementation in govuk-frontend:
     # https://github.com/alphagov/govuk-frontend/blob/aa30ee76a5f84e230a323bb92d341285a6da3a10/src/govuk/components/character-count/character-count.js#L82
-    if value.scan(/\S+/).size > options[:maximum]
-      record.errors.add(attribute, :too_many_words, count: options[:maximum])
+
+    word_count = value.scan(/\S+/).size
+
+    if word_count > options[:maximum]
+      record.errors.add(attribute, :too_many_words, maximum: options[:maximum], count: (word_count - options[:maximum]))
     end
   end
 end
