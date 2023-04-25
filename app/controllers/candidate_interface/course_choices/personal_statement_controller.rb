4b635ebe4bcf848c4c7d2b999997e2630d7be32b
James Glenn <47917431+JR-G@users.noreply.github.com>
2023-04-25 12:15:02 +0100

    personal statement routes and controllers

---
+++ app/controllers/candidate_interface/course_choices/personal_statement_controller.rb
@@ -0,0 +1,13 @@
+module CandidateInterface
+  module CourseChoices
+    class PersonalStatementController < BaseController
+      def new; end
+
+      def edit; end
+
+      def create; end
+
+      def update; end
+    end
+  end
+end
