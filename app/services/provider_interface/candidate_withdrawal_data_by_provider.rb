8edf0d1b57c4a5c74eb15213c5d54c2f7268ed21
James Glenn <47917431+JR-G@users.noreply.github.com>
2023-04-27 09:23:46 +0100

    Define constant for provider reports

--- app/services/provider_interface/candidate_withdrawal_data_by_provider.rb
+++ app/services/provider_interface/candidate_withdrawal_data_by_provider.rb
@@ -1,7 +1,6 @@
 module ProviderInterface
   class CandidateWithdrawalDataByProvider
     CONFIG_PATH = 'config/withdrawal_reasons.yml'.freeze
-    MIN_SUBMITTED_WITHDRAWAL_REASONS_TO_SHOW = 10
 
     attr_reader :provider
 
@@ -17,35 +16,35 @@ module ProviderInterface
 
     def withdrawal_data
       selectable_reasons.map do |reason|
-        before_acceptance_count = withdrawal_query[["withdrawn_before_acceptance", reason[:id]]] || 0
-        after_acceptance_count = withdrawal_query[["withdrawn_after_acceptance", reason[:id]]] || 0
+        before_acceptance_count = withdrawal_query[['withdrawn_before_acceptance', reason[:id]]] || 0
+        after_acceptance_count = withdrawal_query[['withdrawn_after_acceptance', reason[:id]]] || 0
         total_count = before_acceptance_count + after_acceptance_count
-    
+
         {
           header: reason[:label],
           values: [
             before_acceptance_count,
             after_acceptance_count,
-            total_count
-          ]
+            total_count,
+          ],
         }
       end
     end
 
   private
 
-  def withdrawal_query
-    current_cycle_applications_visible_to_provider
-      .pluck(Arel.sql('CASE WHEN accepted_at IS NULL THEN \'withdrawn_before_acceptance\' ELSE \'withdrawn_after_acceptance\' END AS withdrawal_status, unnest(structured_withdrawal_reasons) as reason'))
-      .group_by(&:itself)
-      .transform_values(&:count)
-  end
+    def withdrawal_query
+      current_cycle_applications_visible_to_provider
+        .pluck(Arel.sql('CASE WHEN accepted_at IS NULL THEN \'withdrawn_before_acceptance\' ELSE \'withdrawn_after_acceptance\' END AS withdrawal_status, unnest(structured_withdrawal_reasons) as reason'))
+        .group_by(&:itself)
+        .transform_values(&:count)
+    end
 
-  def current_cycle_applications_visible_to_provider
-    ApplicationChoice
-      .where('provider_ids @> ARRAY[?]::bigint[]', provider)
-      .where(current_recruitment_cycle_year: RecruitmentCycle.current_year)
-  end
+    def current_cycle_applications_visible_to_provider
+      ApplicationChoice
+        .where('provider_ids @> ARRAY[?]::bigint[]', provider)
+        .where(current_recruitment_cycle_year: RecruitmentCycle.current_year)
+    end
 
     def selectable_reasons
       YAML.load_file(CONFIG_PATH)
