6b8445d7e5f00dcf8510850e748a0447cffef4aa
James Glenn <47917431+JR-G@users.noreply.github.com>
2023-05-11 09:00:19 +0100

    Controller, routes and view for mid cycle report

---
+++ app/controllers/publications/mid_cycle_report_controller.rb
@@ -0,0 +1,7 @@
+module Publications
+  class MidCycleReportController < ApplicationController
+    def show
+      @national_data = Publications::NationalMidCycleReport.last.statistics
+    end
+  end
+end
--- config/routes/publications.rb
+++ config/routes/publications.rb
@@ -2,4 +2,5 @@ namespace :publications, path: '/publications' do
   get '/monthly-statistics/ITT(:year)' => 'monthly_statistics#show', as: :monthly_report_itt
   get '/monthly-statistics(/:month)' => 'monthly_statistics#show', as: :monthly_report
   get '/monthly-statistics/:month/:export_type' => 'monthly_statistics#download', as: :monthly_report_download
+  get '/mid-cycle-report' => 'mid_cycle_report#show', as: :mid_cycle_report
 end
