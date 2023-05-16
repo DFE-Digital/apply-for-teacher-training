c04eee01abddf82ef5760503b2cd1f3ea579a0a0
James Glenn <47917431+JR-G@users.noreply.github.com>
2023-05-16 08:38:35 +0100

    Do not render the mid cycle report if before the publication date

--- app/controllers/publications/mid_cycle_report_controller.rb
+++ app/controllers/publications/mid_cycle_report_controller.rb
@@ -2,6 +2,7 @@ module Publications
   class MidCycleReportController < ApplicationController
     def show
       @national_data = Publications::NationalMidCycleReport.last&.statistics
+      @publication_date = Publications::NationalMidCycleReport.last&.publication_date
     end
   end
 end
