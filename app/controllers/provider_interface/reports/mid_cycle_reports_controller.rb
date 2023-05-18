504e47a2be860daf33c5c2d50f2d2e0e3cf5217f
James Glenn <47917431+JR-G@users.noreply.github.com>
2023-05-18 09:01:36 +0100

    Final content changes and guard against no data

--- app/controllers/provider_interface/reports/mid_cycle_reports_controller.rb
+++ app/controllers/provider_interface/reports/mid_cycle_reports_controller.rb
@@ -3,8 +3,8 @@ module ProviderInterface
     class MidCycleReportsController < ProviderInterfaceController
       def show
         @provider = current_user.providers.find(provider_id)
-        @provider_data = Publications::ProviderMidCycleReport.where(provider_id: provider_id).last.statistics
-        @national_data = Publications::NationalMidCycleReport.last.statistics
+        @provider_data = Publications::ProviderMidCycleReport.where(provider_id: provider_id).last&.statistics
+        @national_data = Publications::NationalMidCycleReport.last&.statistics
       end
 
     private
