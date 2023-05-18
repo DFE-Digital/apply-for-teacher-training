7ed1a521ff59993b612ba78a411053a355260385
Tomas D'Stefano <tomas_stefano@successoft.com>
2023-05-18 16:14:00 +0100

    Show reference condition message when user accepting an offer
    And the reference condition exists

--- app/components/candidate_interface/reference_condition_header_component.rb
+++ app/components/candidate_interface/reference_condition_header_component.rb
@@ -1,15 +1,20 @@
 module CandidateInterface
   class ReferenceConditionHeaderComponent < ViewComponent::Base
-    attr_accessor :reference_condition, :provider_name
+    attr_accessor :reference_condition, :provider_name, :show_extra_content
     delegate :description, :met?, to: :reference_condition, allow_nil: true
 
-    def initialize(reference_condition:, provider_name:)
+    def initialize(reference_condition:, provider_name:, show_extra_content: true)
       @reference_condition = reference_condition
       @provider_name = provider_name
+      @show_extra_content = show_extra_content
     end
 
     def render?
       !met?
+    end
+
+    def show_extra_content?
+      @show_extra_content.present?
     end
   end
 end
