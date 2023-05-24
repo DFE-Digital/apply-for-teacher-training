126790c5a3d994a04f57ddf9329d2d4d1356b27e
Steve Hook <steve.hook@gmail.com>
2023-05-24 10:03:04 +0100

    Remove redundant references to `OfferCondition#text`
    Various factories were setting this attribute to `nil`. We don't really
    need this anyway and it breaks the factories because we now ignore that
    column.
    
    Also remove the MigrateToStructuredOfferConditions data migration
    because this is has run and can't be re-run against the updated schema.

--- spec/factories/reference_condition.rb
+++ spec/factories/reference_condition.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
   factory :reference_condition, class: 'ReferenceCondition' do
     offer
 
-    text { nil }
     required { true }
     description { 'Provide 2 references' }
     status { 'pending' }
--- spec/factories/text_condition.rb
+++ spec/factories/text_condition.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
   factory :text_condition, class: 'TextCondition' do
     offer
 
-    text { nil }
     description { Faker::Lorem.sentence }
     status { 'pending' }
   end
