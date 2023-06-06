8237a432285a8ffa5a0ae5f46da5eedec205cd92
Steve Hook <steve.hook@gmail.com>
2023-06-06 14:39:26 +0100

    Remove mid_cycle_report feature flag
    Replace this with some conditional logic that only shows the links to
    the mid-cycle reports when they are available.

--- app/controllers/provider_interface/reports_controller.rb
+++ app/controllers/provider_interface/reports_controller.rb
@@ -3,5 +3,13 @@ module ProviderInterface
     def index
       @providers = current_user.providers
     end
+
+  private
+
+    def mid_cycle_report_present_for?(provider)
+      Publications::ProviderMidCycleReport.exists?(provider: provider)
+    end
+
+    helper_method :mid_cycle_report_present_for?
   end
 end
--- app/services/feature_flag.rb
+++ app/services/feature_flag.rb
@@ -36,7 +36,6 @@ class FeatureFlag
     [:adviser_sign_up, 'Allow candidates to sign up for a teacher training adviser', 'Ross Oliver'],
     [:one_personal_statement, 'Combining the 2 personal statements into 1 for new applications', 'Frankie Roberto + Maeve Roseveare'],
     [:structured_reference_condition, 'Structured reference condition that can be added as a condition to an offer', 'Tomas Destefi'],
-    [:mid_cycle_report, 'The provider mid cycle report', 'James Glenn'],
     [:continuous_applications, 'The new continuous applications flow', 'James Glenn'],
   ].freeze
 
