edfb993f2df3a286d38fe7eea39427ffc33e4445
Steve Hook <steve.hook@gmail.com>
2023-06-07 13:25:37 +0100

    Data migration to clean up `mid_cycle_reports` FF

--- spec/factories/provider_mid_cycle_report.rb
+++ spec/factories/provider_mid_cycle_report.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :provider_mid_cycle_report, class: Publications::ProviderMidCycleReport do
+  factory :provider_mid_cycle_report, class: 'Publications::ProviderMidCycleReport' do
     provider
     statistics { [] }
     publication_date { 2.weeks.ago }
