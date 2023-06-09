996f03db330a160dd4617668a804fdebdf606f6e
Tomas D'Stefano <tomas_stefano@successoft.com>
2023-06-09 10:21:59 +0100

    Make sure the state diagram docs are flexible for ignore states
    I need to ignore inactive for vendor API (the API won't surface
        the inactive status)

--- app/components/shared/state_diagram_component.rb
+++ app/components/shared/state_diagram_component.rb
@@ -1,5 +1,8 @@
 class StateDiagramComponent < ViewComponent::Base
-  def initialize(machine:)
+  attr_accessor :machine, :ignore_states
+
+  def initialize(machine:, ignore_states: [])
     @machine = machine
+    @ignore_states = ignore_states
   end
 end
