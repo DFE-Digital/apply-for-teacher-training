ad5afb3b890e9f6c2b6004bed2c1e7263441a892
Iain McNulty <iain.mcnulty@education.gov.uk>
2023-06-15 12:29:30 +0100

    Update personal statement controller
      If there is an issue in saving the personal statement in the
      transaction then an error will be shown to the candidate

---
+++ spec/requests/candidate_interface/personal_statement_spec.rb
@@ -0,0 +1,163 @@
+require 'rails_helper'
+
+RSpec.describe 'PUT candidate/application/equality-and-diversity', type: :request do
+  include Devise::Test::IntegrationHelpers
+  let(:candidate) { create(:candidate) }
+  let(:application_form) { create(:application_form, candidate: candidate) }
+
+  before do
+    sign_in candidate
+  end
+
+  describe 'GET /candidate/application/personal-statement' do
+    it 'responds with 200' do
+      get candidate_interface_new_becoming_a_teacher_path
+
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe 'PATCH /candidate/application/personal-statement/edit' do
+    context 'when becoming_a_teacher is blank' do
+      let(:params) do
+        {
+          candidate_interface_becoming_a_teacher_form: {
+            becoming_a_teacher: '',
+          },
+        }
+      end
+
+      it 'redirects to application form page' do
+        patch candidate_interface_new_becoming_a_teacher_path, params: params
+
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(candidate_interface_application_form_path)
+      end
+    end
+
+    context 'when becoming_a_teacher is invalid' do
+      let(:params) do
+        {
+          candidate_interface_becoming_a_teacher_form: {
+            becoming_a_teacher: 'Some ' * 1001,
+          },
+        }
+      end
+
+      it 'redirects to review page' do
+        patch candidate_interface_edit_becoming_a_teacher_path, params: params
+
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(candidate_interface_becoming_a_teacher_show_path)
+      end
+    end
+
+    context 'when becoming_a_teacher with content' do
+      let(:params) do
+        {
+          candidate_interface_becoming_a_teacher_form: {
+            becoming_a_teacher: 'Some content',
+          },
+        }
+      end
+
+      it 'redirects to review page' do
+        patch candidate_interface_edit_becoming_a_teacher_path, params: params
+
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(candidate_interface_becoming_a_teacher_show_path)
+      end
+    end
+
+    context 'when saving to db fails' do
+      let(:params) do
+        {
+          candidate_interface_becoming_a_teacher_form: {
+            becoming_a_teacher: 'content',
+          },
+        }
+      end
+
+      it 'renders the error message' do
+        allow_any_instance_of(ApplicationForm).to receive(:update).and_return(false)
+
+        patch candidate_interface_edit_becoming_a_teacher_path, params: params
+
+        expect(response).to have_http_status(:internal_server_error)
+        expect(response.body).to include('The record could not be saved. Please try again.')
+      end
+    end
+  end
+
+  describe 'PATCH /candidate/application/personal-statement' do
+    context 'when becoming_a_teacher with content' do
+      let(:params) do
+        {
+          candidate_interface_becoming_a_teacher_form: {
+            becoming_a_teacher: 'Some content',
+          },
+        }
+      end
+
+      it 'redirects to review page' do
+        patch candidate_interface_new_becoming_a_teacher_path, params: params
+
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(candidate_interface_becoming_a_teacher_show_path)
+      end
+    end
+
+    context 'when becoming_a_teacher is blank' do
+      let(:params) do
+        {
+          candidate_interface_becoming_a_teacher_form: {
+            becoming_a_teacher: '',
+          },
+        }
+      end
+
+      it 'redirects to application form page' do
+        patch candidate_interface_new_becoming_a_teacher_path, params: params
+
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(candidate_interface_application_form_path)
+      end
+    end
+
+    context 'when becoming_a_teacher is invalid' do
+      let(:params) do
+        {
+          candidate_interface_becoming_a_teacher_form: {
+            becoming_a_teacher: Faker::Lorem.paragraph_by_chars(number: 1001),
+          },
+        }
+      end
+
+      it 'redirects to review page' do
+        patch candidate_interface_new_becoming_a_teacher_path, params: params
+
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(candidate_interface_becoming_a_teacher_show_path)
+      end
+    end
+
+    context 'when saving to db fails' do
+      let(:params) do
+        {
+          candidate_interface_becoming_a_teacher_form: {
+            becoming_a_teacher: 'content',
+          },
+        }
+      end
+
+      it 'renders the error message' do
+        allow_any_instance_of(ApplicationForm).to receive(:update).and_return(false)
+
+        patch candidate_interface_edit_becoming_a_teacher_path, params: params
+
+        expect(response).to have_http_status(:internal_server_error)
+        expect(response.body).to include('The record could not be saved. Please try again.')
+      end
+    end
+  end
+end
