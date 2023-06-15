0877c0bc3316bdcd2a159782c910bf64f3f09e84
montychristo <chrisroseuk@gmail.com>
2023-06-15 12:11:15 +0100

    New Feedback Survey [Backfill Data Migration] #3 (#8336)
    ## Wider Context
    
    Only present Feedback form the first time the application is submitted
    
    ## Changes in this PR (2 of 3 PRs)
    
    Data migration to backfill feedback survey complete flag on existing
    application_forms
    
    ## Link to Trello card
    
    https://trello.com/c/ZibcQeay/1514-ca-implement-the-new-feedback-survey

---
+++ app/services/data_migrations/backfill_feedback_form_complete.rb
@@ -0,0 +1,20 @@
+module DataMigrations
+  class BackfillFeedbackFormComplete
+    TIMESTAMP  = 20230612071351
+    MANUAL_RUN = false
+
+    def change
+      applications.find_each(batch_size: 500) do |batch|
+        batch.update_columns(feedback_form_complete: true)
+      end
+    end
+
+  private
+
+    def applications
+      ApplicationForm.where(feedback_form_complete: false)
+                     .where.not(feedback_satisfaction_level: nil)
+                     .or(ApplicationForm.where.not(feedback_suggestions: nil))
+    end
+  end
+end
