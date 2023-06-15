009369d4bb4a51a2d0abfd17191a49935c5a95c8
Iain McNulty <iain.mcnulty@education.gov.uk>
2023-06-15 11:59:00 +0100

    Remove parentheses in migration method call

--- db/migrate/20230608145455_add_personal_statement_to_application_choice.rb
+++ db/migrate/20230608145455_add_personal_statement_to_application_choice.rb
@@ -1,5 +1,5 @@
 class AddPersonalStatementToApplicationChoice < ActiveRecord::Migration[7.0]
   def change
-    add_column(:application_choices, :personal_statement, :text)
+    add_column :application_choices, :personal_statement, :text
   end
 end
