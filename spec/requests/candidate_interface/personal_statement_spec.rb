8537dbd0b8e89b6e45bf3f049b39abb040436d54
Iain McNulty <iain.mcnulty@education.gov.uk>
2023-06-16 15:26:27 +0100

    Improve PersonalStatement request spec file   Correct the top level description   Clean up the RSpec lets

--- spec/requests/candidate_interface/personal_statement_spec.rb
+++ spec/requests/candidate_interface/personal_statement_spec.rb
@@ -1,9 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe 'PUT candidate/application/equality-and-diversity' do
+RSpec.describe 'CandidateInterface::BecomingATeacherController' do
   include Devise::Test::IntegrationHelpers
   let(:candidate) { create(:candidate) }
   let(:application_form) { create(:application_form, candidate: candidate) }
+  let(:params) do
+    {
+      candidate_interface_becoming_a_teacher_form: {
+        becoming_a_teacher: becoming_a_teacher,
+      },
+    }
+  end
 
   before do
     sign_in candidate
@@ -17,15 +24,20 @@ RSpec.describe 'PUT candidate/application/equality-and-diversity' do
     end
   end
 
-  describe 'PATCH /candidate/application/personal-statement/edit' do
-    context 'when becoming_a_teacher is blank' do
-      let(:params) do
-        {
-          candidate_interface_becoming_a_teacher_form: {
-            becoming_a_teacher: '',
-          },
-        }
+  describe 'PATCH /candidate/application/personal-statement' do
+    context 'when becoming_a_teacher with content' do
+      let(:becoming_a_teacher) { 'Valid content' }
+
+      it 'redirects to review page' do
+        patch candidate_interface_new_becoming_a_teacher_path, params: params
+
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(candidate_interface_becoming_a_teacher_show_path)
       end
+    end
+
+    context 'when becoming_a_teacher is blank' do
+      let(:becoming_a_teacher) { '' }
 
       it 'redirects to application form page' do
         patch candidate_interface_new_becoming_a_teacher_path, params: params
@@ -36,33 +48,10 @@ RSpec.describe 'PUT candidate/application/equality-and-diversity' do
     end
 
     context 'when becoming_a_teacher is invalid' do
-      let(:params) do
-        {
-          candidate_interface_becoming_a_teacher_form: {
-            becoming_a_teacher: 'Some ' * 1001,
-          },
-        }
-      end
+      let(:becoming_a_teacher) { 'Some ' * 1001 }
 
       it 'redirects to review page' do
-        patch candidate_interface_edit_becoming_a_teacher_path, params: params
-
-        expect(response).to have_http_status(:redirect)
-        expect(response).to redirect_to(candidate_interface_becoming_a_teacher_show_path)
-      end
-    end
-
-    context 'when becoming_a_teacher with content' do
-      let(:params) do
-        {
-          candidate_interface_becoming_a_teacher_form: {
-            becoming_a_teacher: 'Some content',
-          },
-        }
-      end
-
-      it 'redirects to review page' do
-        patch candidate_interface_edit_becoming_a_teacher_path, params: params
+        patch candidate_interface_new_becoming_a_teacher_path, params: params
 
         expect(response).to have_http_status(:redirect)
         expect(response).to redirect_to(candidate_interface_becoming_a_teacher_show_path)
@@ -70,13 +59,7 @@ RSpec.describe 'PUT candidate/application/equality-and-diversity' do
     end
 
     context 'when saving to db fails' do
-      let(:params) do
-        {
-          candidate_interface_becoming_a_teacher_form: {
-            becoming_a_teacher: 'content',
-          },
-        }
-      end
+      let(:becoming_a_teacher) { 'Valid content' }
 
       it 'renders the error message' do
         allow_any_instance_of(ApplicationForm).to receive(:update!).and_raise(ActiveRecord::StatementTimeout) # rubocop:disable RSpec/AnyInstance
@@ -89,32 +72,9 @@ RSpec.describe 'PUT candidate/application/equality-and-diversity' do
     end
   end
 
-  describe 'PATCH /candidate/application/personal-statement' do
-    context 'when becoming_a_teacher with content' do
-      let(:params) do
-        {
-          candidate_interface_becoming_a_teacher_form: {
-            becoming_a_teacher: 'Some content',
-          },
-        }
-      end
-
-      it 'redirects to review page' do
-        patch candidate_interface_new_becoming_a_teacher_path, params: params
-
-        expect(response).to have_http_status(:redirect)
-        expect(response).to redirect_to(candidate_interface_becoming_a_teacher_show_path)
-      end
-    end
-
+  describe 'PATCH /candidate/application/personal-statement/edit' do
     context 'when becoming_a_teacher is blank' do
-      let(:params) do
-        {
-          candidate_interface_becoming_a_teacher_form: {
-            becoming_a_teacher: '',
-          },
-        }
-      end
+      let(:becoming_a_teacher) { '' }
 
       it 'redirects to application form page' do
         patch candidate_interface_new_becoming_a_teacher_path, params: params
@@ -125,16 +85,21 @@ RSpec.describe 'PUT candidate/application/equality-and-diversity' do
     end
 
     context 'when becoming_a_teacher is invalid' do
-      let(:params) do
-        {
-          candidate_interface_becoming_a_teacher_form: {
-            becoming_a_teacher: Faker::Lorem.paragraph_by_chars(number: 1001),
-          },
-        }
-      end
+      let(:becoming_a_teacher) { 'Some ' * 1001 }
 
       it 'redirects to review page' do
-        patch candidate_interface_new_becoming_a_teacher_path, params: params
+        patch candidate_interface_edit_becoming_a_teacher_path, params: params
+
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(candidate_interface_becoming_a_teacher_show_path)
+      end
+    end
+
+    context 'when becoming_a_teacher with content' do
+      let(:becoming_a_teacher) { 'Valid content' }
+
+      it 'redirects to review page' do
+        patch candidate_interface_edit_becoming_a_teacher_path, params: params
 
         expect(response).to have_http_status(:redirect)
         expect(response).to redirect_to(candidate_interface_becoming_a_teacher_show_path)
@@ -142,13 +107,7 @@ RSpec.describe 'PUT candidate/application/equality-and-diversity' do
     end
 
     context 'when saving to db fails' do
-      let(:params) do
-        {
-          candidate_interface_becoming_a_teacher_form: {
-            becoming_a_teacher: 'content',
-          },
-        }
-      end
+      let(:becoming_a_teacher) { 'Valid content' }
 
       it 'renders the error message' do
         allow_any_instance_of(ApplicationForm).to receive(:update!).and_raise(ActiveRecord::StatementTimeout) # rubocop:disable RSpec/AnyInstance
