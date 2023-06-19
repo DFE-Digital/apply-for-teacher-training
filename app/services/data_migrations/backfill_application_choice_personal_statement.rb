121f4cd1f6db762124af5b098a7b41947446e86d
Iain McNulty <iain.mcnulty@education.gov.uk>
2023-06-19 11:48:09 +0100

    Batch personal statement update migration

--- app/services/data_migrations/backfill_application_choice_personal_statement.rb
+++ app/services/data_migrations/backfill_application_choice_personal_statement.rb
@@ -4,15 +4,21 @@ module DataMigrations
     MANUAL_RUN = true
 
     def change
-      ActiveRecord::Base.connection.execute(<<~SQL)
-        UPDATE "application_choices" c
-        SET personal_statement = (
-          SELECT becoming_a_teacher
-          FROM application_forms
-          WHERE c.application_form_id = id
-          AND becoming_a_teacher IS NOT NULL
-        )
-      SQL
+      ApplicationForm.select(:id).in_batches(of: 100) do |relation|
+        ids = relation.ids.join(',')
+
+        ActiveRecord::Base.connection.execute(<<~SQL)
+          UPDATE "application_choices" c
+          SET personal_statement = (
+            SELECT becoming_a_teacher
+            FROM application_forms
+            WHERE c.application_form_id = id
+            AND id in (#{ids})
+            AND becoming_a_teacher IS NOT NULL
+          )
+          where c.application_form_id in (#{ids})
+        SQL
+      end
     end
   end
 end
