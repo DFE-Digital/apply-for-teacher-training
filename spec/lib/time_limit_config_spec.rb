958bc28b95610cdd5f77cf1cf392ed0821bbe3c4
Tomas D'Stefano <tomas_stefano@successoft.com>
2023-06-22 16:45:54 +0100

    Change inactive applications for 30 days (capps only)

--- spec/lib/time_limit_config_spec.rb
+++ spec/lib/time_limit_config_spec.rb
@@ -2,10 +2,6 @@ require 'rails_helper'
 
 RSpec.describe TimeLimitConfig do
   describe '#limits_for' do
-    it ':reject_by_default returns a default limit of 40 days' do
-      expect(described_class.limits_for(:reject_by_default).first.limit).to eq(40)
-    end
-
     it ':decline_by_default returns a default limit of 10 days' do
       expect(described_class.limits_for(:decline_by_default).first.limit).to eq(10)
     end
@@ -16,6 +12,26 @@ RSpec.describe TimeLimitConfig do
 
     it ':chase_candidate_before_dbd returns a default limit of 5 days' do
       expect(described_class.limits_for(:chase_candidate_before_dbd).first.limit).to eq(5)
+    end
+
+    context 'when continuous applications', time: Time.zone.local(2023, 11, 11, 11, 11, 11) do
+      before do
+        FeatureFlag.activate(:continuous_applications)
+      end
+
+      it ':reject_by_default returns a default limit of 30 days' do
+        expect(described_class.limits_for(:reject_by_default).first.limit).to eq(30)
+      end
+    end
+
+    context 'when not continuous applications', time: Time.zone.local(2023, 11, 11, 11, 11, 11) do
+      before do
+        FeatureFlag.deactivate(:continuous_applications)
+      end
+
+      it ':reject_by_default returns a default limit of 40 days' do
+        expect(described_class.limits_for(:reject_by_default).first.limit).to eq(40)
+      end
     end
   end
 end
