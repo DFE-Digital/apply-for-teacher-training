9cfc386ba5447beefb90bb093424fc3e5a67a64b
Iain McNulty <iain.mcnulty@education.gov.uk>
2023-06-27 12:36:43 +0100

    Add SetDeclineByDefaultToEndOfCycle
      This class will set the ApplicationChoice Decline By Default date to
      the end of the cycle in which the offer is made.
    
      This is an alternative to the SetDeclineByDefault which sets the
      Decline By Default date a set number of days from a calculated date
      based on other attributes on the ApplicationForm and ApplicationChoice.

---
+++ app/services/set_decline_by_default_to_end_of_cycle.rb
@@ -0,0 +1,37 @@
+class SetDeclineByDefaultToEndOfCycle
+  def initialize(application_form:)
+    @application_form = application_form
+  end
+
+  def call
+    # Only set Decline by Default (DBD) date if all applications have been acted upon.
+    return if pending_decisions?
+
+    # If there are not any offers we do not need to set a DBD date
+    return if no_offers?
+
+    application_choices.offer.each do |application_choice|
+      next if application_choice.decline_by_default_at == CycleTimetable.next_apply_deadline
+
+      application_choice.update!(
+        decline_by_default_at: CycleTimetable.next_apply_deadline,
+      )
+    end
+  end
+
+private
+
+  attr_reader :application_form
+
+  def application_choices
+    application_form.application_choices
+  end
+
+  def pending_decisions?
+    application_form.awaiting_provider_decisions?
+  end
+
+  def no_offers?
+    application_choices.offer.none?
+  end
+end
