ad5f12ae9edad766a6547c3d40f284435c70c73a
Iain McNulty <iain.mcnulty@education.gov.uk>
2023-06-28 12:36:44 +0100

    Move filtering of application_choices to SQL query

--- app/services/set_decline_by_default_to_end_of_cycle.rb
+++ app/services/set_decline_by_default_to_end_of_cycle.rb
@@ -7,12 +7,12 @@ class SetDeclineByDefaultToEndOfCycle
     # Only set Decline by Default (DBD) date if all applications have been acted upon.
     return if pending_decisions?
 
-    application_choices.offer.each do |application_choice|
-      next if application_choice.decline_by_default_at == CycleTimetable.next_apply_deadline
-
-      application_choice.update!(
-        decline_by_default_at: CycleTimetable.next_apply_deadline,
-      )
+    ActiveRecord::Base.transaction do
+      filtered_application_choices.each do |application_choice|
+        application_choice.update!(
+          decline_by_default_at: CycleTimetable.next_apply_deadline,
+        )
+      end
     end
   end
 
@@ -20,8 +20,12 @@ private
 
   attr_reader :application_form
 
-  def application_choices
-    application_form.application_choices
+  def filtered_application_choices
+    application_form
+      .application_choices
+      .offer
+      .where(decline_by_default_at: nil)
+      .or(ApplicationChoice.where.not(decline_by_default_at: CycleTimetable.next_apply_deadline))
   end
 
   def pending_decisions?
