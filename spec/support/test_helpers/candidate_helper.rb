6579cf5d2108fd19adb835b8e3ac33b39c71bbfa
Iain McNulty <iain.mcnulty@education.gov.uk>
2023-07-03 12:17:07 +0100

    Extend click_change_link in system tests CandidateHelper
      Accept kw option :first to specify that we accept the first instance
      of a matching change link

--- spec/support/test_helpers/candidate_helper.rb
+++ spec/support/test_helpers/candidate_helper.rb
@@ -585,11 +585,11 @@ module CandidateHelper
     email.click_link(@reference_feedback_url)
   end
 
-  def click_change_link(row_description)
+  def click_change_link(row_description, first: false)
     link_text = "Change #{row_description}"
     matches = page.all('.govuk-summary-list__actions').select { |row| row.has_link?(link_text) }
 
-    if matches.count > 1
+    if matches.count > 1 && first == false
       raise "More than one '#{link_text}' link found. Use 'within' to scope this action to a more specific node in the document."
     else
       matches.pop.click_link(link_text)
