0836a10006d02432d6b4125d1a8a828e4004940c
Duncan Brown <duncanjbrown@gmail.com>
2023-07-07 17:18:27 +0100

    Redirect to Blazer after sign-in
    Wrap the engines in a middleware which does the auth and redirect. This
    way the routes always exist (which is good) and the post-sign-in
    redirect can happen correctly for each requested engine.

--- spec/system/support_interface/blazer_sql_spec.rb
+++ spec/system/support_interface/blazer_sql_spec.rb
@@ -3,18 +3,15 @@ require 'rails_helper'
 RSpec.feature 'Blazer SQL tool' do
   include DfESignInHelpers
 
-  scenario 'Support user views the Blazer' do
+  scenario 'logging in to use Blazer' do
     when_i_visit_the_blazer_interface
     then_i_should_be_redirected_to_login_page
-
     when_i_sign_in_via_dfe_sign_in
-    when_i_visit_the_blazer_interface
-
     then_i_should_see_the_blazer_admin_interface
   end
 
   def when_i_visit_the_blazer_interface
-    visit '/support/blazer'
+    visit support_interface_blazer_path
   end
 
   def then_i_should_be_redirected_to_login_page
@@ -27,6 +24,6 @@ RSpec.feature 'Blazer SQL tool' do
 
   def then_i_should_see_the_blazer_admin_interface
     expect(page).to have_content 'New Query'
-    expect(page).to have_current_path '/support/blazer'
+    expect(page).to have_current_path "#{support_interface_blazer_path}/" # engine paths end with /
   end
 end
