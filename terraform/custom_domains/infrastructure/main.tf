9af28a887b1c45998314af9b784b05714277b484
mrzta <zeshan@azuredevops.co.uk>
2023-07-17 13:56:02 +0100

    Changed git source to testing

--- terraform/custom_domains/infrastructure/main.tf
+++ terraform/custom_domains/infrastructure/main.tf
@@ -1,5 +1,5 @@
 module "domains_infrastructure" {
-  source                  = "git::https://github.com/DFE-Digital/terraform-modules.git//domains/infrastructure?ref=0.5.1"
+  source                  = "git::https://github.com/DFE-Digital/terraform-modules.git//domains/infrastructure?ref=testing"
   hosted_zone             = var.hosted_zone
   tags                    = var.tags
   deploy_default_records  = var.deploy_default_records
