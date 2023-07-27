48c7ae70e9184cb4448e0896dad92e7fb7eb80a8
Tomas D'Stefano <tomas_stefano@successoft.com>
2023-07-27 10:30:36 +0100

    Remove comment for vacancies query
    This will be done in a future PR

--- app/queries/get_available_courses_for_provider.rb
+++ app/queries/get_available_courses_for_provider.rb
@@ -5,14 +5,6 @@ class GetAvailableCoursesForProvider
     @provider = provider
   end
 
-  # provider
-  #  .courses
-  #  .current_cycle
-  #  .exposed_in_find
-  #  .left_outer_joins(:course_options)
-  #  .where(:'course_options.site_still_valid' => true)
-  #  .where(:'course_options.vacancy_status' => 'vacancies')
-  #  .order(:name)
   def call
     courses_for_current_cycle
       .exposed_in_find
