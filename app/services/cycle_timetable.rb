238d10c45c7df7a4722b3923df3e19fd9f6a5ee3
Iain McNulty <iain.mcnulty@education.gov.uk>
2023-08-03 13:23:04 +0100

    CycleTimetable.current_year respects CycleSwitcher
      If the cycle switcher is engaged, and it's set to find has reopened or
      apply has reopened, then we have technically travelled to the next
      recruitment cycle. That means the current_year method should return
      the year of the next recruitment cycle.
    
      There is a check for ActivRecord::Base.connected? becuase current_year
      is called in many places, but importantly, it's called in
      initializers. By checking for a DB connection we avoid triggering an
      exception when the application doesn't have access to the DB yet.

--- app/services/cycle_timetable.rb
+++ app/services/cycle_timetable.rb
@@ -95,7 +95,10 @@ class CycleTimetable
   }.freeze
 
   def self.current_year(now = Time.zone.now)
-    now += 1.year if ActiveRecord::Base.connected? && current_cycle_schedule == :today_is_after_apply_opens
+    if ActiveRecord::Base.connected? && current_cycle_schedule.in?(%i[today_is_after_find_opens today_is_after_apply_opens])
+      now += 1.year
+    end
+
     CYCLE_DATES.keys.detect do |year|
       return year if last_recruitment_cycle_year?(year)
 
