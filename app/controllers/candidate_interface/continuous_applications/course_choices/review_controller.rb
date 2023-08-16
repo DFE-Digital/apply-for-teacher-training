47611135a24eba77640eabea39f476f86578f88e
Iain McNulty <iain.mcnulty@education.gov.uk>
2023-08-16 10:01:32 +0100

    Set back links for continuous applications review
      Backlinks are set in three different way the candidate arrives on the
      review page:
    
      1. From the final step in the wizard
      2. From the Choices page having clicked "View application"
      3. Directly without any referer
    
      The backlink has different target and different link text based on the
      context.

--- app/controllers/candidate_interface/continuous_applications/course_choices/review_controller.rb
+++ app/controllers/candidate_interface/continuous_applications/course_choices/review_controller.rb
@@ -2,11 +2,40 @@ module CandidateInterface
   module ContinuousApplications
     module CourseChoices
       class ReviewController < BaseController
+        before_action :set_back_link
+
         def show
           @application_choice = current_application.application_choices.find(params[:application_choice_id])
           @submit_application_form = CandidateInterface::ContinuousApplications::SubmitApplicationForm.new(
             application_choice: @application_choice,
           )
+        end
+
+      private
+
+        def set_back_link
+          @return_to = if referrer_blank?
+                         [candidate_interface_continuous_applications_choices_path]
+                       elsif referrer_step?
+                         [request.referer]
+                       elsif referrer_view?
+                         [request.referer, 'Back to your applications']
+                       end
+        end
+
+        # User arrives from the View Application link
+        def referrer_view?
+          request.referer.match?(Regexp.compile('/candidate/application/choices\Z'))
+        end
+
+        # User arrives on the review page from the last wizard step
+        def referrer_step?
+          request.referer.match?(Regexp.compile('/candidate/application/continuous-applications'))
+        end
+
+        # User does not visit the page from another page, probably bookmarked
+        def referrer_blank?
+          request.referer.blank?
         end
       end
     end
