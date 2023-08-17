dfd65b5fa7db7b19b94f119a46e31b0523df77d2
Iain McNulty <iain.mcnulty@education.gov.uk>
2023-08-17 11:48:16 +0100

    Set back link for guidance page
      If candidate visits the guidance page from Your details then the link
      targets Your details. If the candidate visits from your applications
      then the target is your applications. If there is no referer then it
      targets your details.

--- app/controllers/candidate_interface/guidance_controller.rb
+++ app/controllers/candidate_interface/guidance_controller.rb
@@ -1,9 +1,16 @@
 module CandidateInterface
   class GuidanceController < CandidateInterfaceController
     skip_before_action :authenticate_candidate!
+    before_action :set_back_link
 
     def index
       @recruitment_cycle_year = CycleTimetable.current_year
+    end
+
+  private
+
+    def set_back_link
+      @back_link = request.referer || application_form_path
     end
   end
 end
