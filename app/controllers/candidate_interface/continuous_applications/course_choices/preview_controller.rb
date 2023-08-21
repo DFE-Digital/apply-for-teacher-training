467fea72a9ac9e9a5b8f66f75e6c890a924160ef
Iain McNulty <iain.mcnulty@education.gov.uk>
2023-08-21 15:37:07 +0100

    Add Preview controller for continuous applications choice

---
+++ app/controllers/candidate_interface/continuous_applications/course_choices/preview_controller.rb
@@ -0,0 +1,10 @@
+module CandidateInterface
+  module ContinuousApplications
+    module CourseChoices
+      class PreviewController < BaseController
+        def show
+        end
+      end
+    end
+  end
+end
