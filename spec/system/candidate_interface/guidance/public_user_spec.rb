127d673dc3c501c64c5342261697b8d1bb06f167
Iain McNulty <iain.mcnulty@education.gov.uk>
2023-09-08 11:49:35 +0100

    Add specs for different phases of the cycle for guidance page

--- spec/system/candidate_interface/guidance/public_user_spec.rb
+++ spec/system/candidate_interface/guidance/public_user_spec.rb
@@ -1,8 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe 'Non logged in user can visit guidance' do
-  scenario 'User can visit the guidance page without session' do
-    visit(candidate_interface_guidance_path)
-    expect(page).to have_content('Start applying for courses')
+  context 'when legacy applications', continuous_applications: false do
+    context 'when mid cycle', time: mid_cycle do
+      scenario 'User can visit the guidance page without session' do
+        visit(candidate_interface_guidance_path)
+        expect(page).to have_content('Apply for teacher training')
+      end
+    end
+
+    context 'when after_apply_reopens', time: after_apply_reopens do
+      scenario 'User can visit the guidance page without session' do
+        visit(candidate_interface_guidance_path)
+        expect(page).to have_content('Apply for teacher training')
+      end
+    end
+  end
+
+  context 'when continuous applications mid cycle', continuous_applications: true do
+    context 'when mid cycle', time: mid_cycle do
+      scenario 'User can visit the guidance page without session' do
+        visit(candidate_interface_guidance_path)
+        expect(page).to have_content('Apply for teacher training')
+      end
+    end
+
+    context 'when after_apply_reopens', time: after_apply_reopens do
+      scenario 'User can visit the guidance page without session' do
+        visit(candidate_interface_guidance_path)
+        expect(page).to have_content('Apply for teacher training')
+      end
+    end
   end
 end
