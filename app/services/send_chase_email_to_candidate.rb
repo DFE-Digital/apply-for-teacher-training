2bfbe61517788cf7f949121d31f3d1cc6a4e5e0c
avinhurry <avin.hurry@digital.education.gov.uk>
2023-09-13 16:06:37 +0100

    [470] Disable candidate chaser emails
    - When `continuous_applications` flow active

--- app/services/send_chase_email_to_candidate.rb
+++ app/services/send_chase_email_to_candidate.rb
@@ -1,6 +1,8 @@
 class SendChaseEmailToCandidate
   def self.call(application_form:)
-    CandidateMailer.chase_candidate_decision(application_form).deliver_later
-    ChaserSent.create!(chased: application_form, chaser_type: :candidate_decision_request)
+    unless application_form.continuous_applications?
+      CandidateMailer.chase_candidate_decision(application_form).deliver_later
+      ChaserSent.create!(chased: application_form, chaser_type: :candidate_decision_request)
+    end
   end
 end
