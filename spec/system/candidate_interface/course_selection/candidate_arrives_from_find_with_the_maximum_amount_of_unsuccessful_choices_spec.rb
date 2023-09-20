4f82f9b515255255755a6035b60dec861e65b5c2
James Glenn <47917431+JR-G@users.noreply.github.com>
2023-09-20 11:44:29 +0100

    Update the journey from find spec

--- spec/system/candidate_interface/course_selection/candidate_arrives_from_find_with_the_maximum_amount_of_unsuccessful_choices_spec.rb
+++ spec/system/candidate_interface/course_selection/candidate_arrives_from_find_with_the_maximum_amount_of_unsuccessful_choices_spec.rb
@@ -9,10 +9,10 @@ RSpec.feature 'Candidate arrives from Find with provider and course params', :co
     when_i_follow_a_link_from_find
     then_i_am_redirected_to_the_create_account_or_sign_in_path
 
-    given_i_am_signed_in
+    given_i_am_at_the_maximum_limit_of_unsuccessful_applications
 
     when_i_follow_a_link_from_find
-    then_i_am_redirected_to_the_course_confirm_selection_page
+    then_i_am_presented_with_an_error_message
   end
 
   def given_there_is_a_provider_with_a_course_that_is_only_accepting_applications_on_apply
@@ -28,13 +28,13 @@ RSpec.feature 'Candidate arrives from Find with provider and course params', :co
     expect(page).to have_current_path candidate_interface_create_account_or_sign_in_path(providerCode: @provider.code, courseCode: @course.code)
   end
 
-  def given_i_am_signed_in
+  def given_i_am_at_the_maximum_limit_of_unsuccessful_applications
     create_and_sign_in_candidate
     current_candidate.application_forms << build(:application_form, :completed)
     current_candidate.current_application.application_choices << build_list(:application_choice, 20, :withdrawn)
   end
 
-  def then_i_am_redirected_to_the_course_confirm_selection_page
+  def then_i_am_presented_with_an_error_message
     expect(page).to have_content I18n.t('errors.messages.too_many_unsuccessful_choices', max_unsuccessful_applications: ApplicationForm::MAXIMUM_NUMBER_OF_UNSUCCESSFUL_APPLICATIONS)
   end
 end
