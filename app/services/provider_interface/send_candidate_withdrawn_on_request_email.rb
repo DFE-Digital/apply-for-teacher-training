24f90c07ae0c11e8a1e52184a776b4f49c4add37
Avin Hurry <avinhurry@icloud.com>
2023-09-20 18:56:43 +0100

    [480] Disable withdrawn emails
    When continuous applications flow active

--- app/services/provider_interface/send_candidate_withdrawn_on_request_email.rb
+++ app/services/provider_interface/send_candidate_withdrawn_on_request_email.rb
@@ -9,14 +9,16 @@ module ProviderInterface
     end
 
     def call
-      if candidate_applications.all?(&:withdrawn?)
-        CandidateMailer.application_withdrawn_on_request_all_applications_withdrawn(application_choice).deliver_later
-      elsif applications_with_offer_and_awaiting_decision?
-        CandidateMailer.application_withdrawn_on_request_one_offer_one_awaiting_decision(application_choice).deliver_later
-      elsif applications_awaiting_decision_only?
-        CandidateMailer.application_withdrawn_on_request_awaiting_decision_only(application_choice).deliver_later
-      elsif applications_with_offers_only?
-        CandidateMailer.application_withdrawn_on_request_offers_only(application_choice).deliver_later
+      unless application_choice.continuous_applications?
+        if candidate_applications.all?(&:withdrawn?)
+          CandidateMailer.application_withdrawn_on_request_all_applications_withdrawn(application_choice).deliver_later
+        elsif applications_with_offer_and_awaiting_decision?
+          CandidateMailer.application_withdrawn_on_request_one_offer_one_awaiting_decision(application_choice).deliver_later
+        elsif applications_awaiting_decision_only?
+          CandidateMailer.application_withdrawn_on_request_awaiting_decision_only(application_choice).deliver_later
+        elsif applications_with_offers_only?
+          CandidateMailer.application_withdrawn_on_request_offers_only(application_choice).deliver_later
+        end
       end
     end
   end
--- spec/services/provider_interface/send_candidate_withdrawn_on_request_email_spec.rb
+++ spec/services/provider_interface/send_candidate_withdrawn_on_request_email_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe ProviderInterface::SendCandidateWithdrawnOnRequestEmail do
+RSpec.describe ProviderInterface::SendCandidateWithdrawnOnRequestEmail, continuous_applications: false do
   describe '#call' do
     let(:mailer) { instance_double(ActionMailer::MessageDelivery, deliver_later: true) }
 
@@ -36,6 +36,19 @@ RSpec.describe ProviderInterface::SendCandidateWithdrawnOnRequestEmail do
       described_class.new(application_choice: offered).call
 
       expect(CandidateMailer).to have_received(:application_withdrawn_on_request_one_offer_one_awaiting_decision)
+    end
+
+    context 'when continuous_applications flag is turned on', :continuous_applications do
+      it 'does not send any emails' do
+        allow(CandidateMailer).to receive_messages(application_withdrawn_on_request_all_applications_withdrawn: mailer, application_withdrawn_on_request_awaiting_decision_only: mailer, application_withdrawn_on_request_offers_only: mailer, application_withdrawn_on_request_one_offer_one_awaiting_decision: mailer)
+
+        described_class.new(application_choice: create(:application_choice, :withdrawn)).call
+
+        expect(CandidateMailer).not_to have_received(:application_withdrawn_on_request_all_applications_withdrawn)
+        expect(CandidateMailer).not_to have_received(:application_withdrawn_on_request_awaiting_decision_only)
+        expect(CandidateMailer).not_to have_received(:application_withdrawn_on_request_offers_only)
+        expect(CandidateMailer).not_to have_received(:application_withdrawn_on_request_one_offer_one_awaiting_decision)
+      end
     end
   end
 end
