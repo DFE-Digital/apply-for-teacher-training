5664f68e23c19f63837160c35ce05c587349ccf9
Tomas D'Stefano <tomas_stefano@successoft.com>
2023-09-22 00:39:12 +0100

    Add entire module definition
    Rubocop fixed in the wrong way before I commited

--- spec/components/candidate_interface/editable_section_warning_spec.rb
+++ spec/components/candidate_interface/editable_section_warning_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe CandidateInterface::EditableSectionWarning do
     let(:current_application) { create(:application_form, :completed, submitted_application_choices_count: 1) }
 
     context 'when candidate can edit the section' do
-      let(:editable_section) { instance_double(EditableSection, can_edit?: true) }
+      let(:editable_section) { instance_double(CandidateInterface::EditableSection, can_edit?: true) }
 
       it 'renders message' do
         expect(result.text).to include(
@@ -21,7 +21,7 @@ RSpec.describe CandidateInterface::EditableSectionWarning do
     end
 
     context 'when candidate can not edit the section' do
-      let(:editable_section) { instance_double(EditableSection, can_edit?: false) }
+      let(:editable_section) { instance_double(CandidateInterface::EditableSection, can_edit?: false) }
 
       it 'renders nothing' do
         expect(result.text).to be_blank
@@ -30,7 +30,7 @@ RSpec.describe CandidateInterface::EditableSectionWarning do
   end
 
   context 'when candidate did not submitted yet' do
-    let(:editable_section) { instance_double(EditableSection, can_edit?: true) }
+    let(:editable_section) { instance_double(CandidateInterface::EditableSection, can_edit?: true) }
     let(:current_application) { create(:application_form) }
 
     it 'renders nothing' do
