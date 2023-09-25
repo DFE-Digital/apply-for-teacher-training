56f7d03524fea530d7e58fe8ab120c5f8f6251eb
Avin Hurry <avinhurry@icloud.com>
2023-09-25 14:06:17 +0100

    [480] Linter fix

--- app/services/provider_interface/send_candidate_withdrawn_on_request_email.rb
+++ app/services/provider_interface/send_candidate_withdrawn_on_request_email.rb
@@ -11,16 +11,14 @@ module ProviderInterface
     def call
       if application_choice.continuous_applications?
         CandidateMailer.application_withdrawn_on_request(application_choice).deliver_later
-      else
-        if candidate_applications.all?(&:withdrawn?)
-          CandidateMailer.application_withdrawn_on_request_all_applications_withdrawn(application_choice).deliver_later
-        elsif applications_with_offer_and_awaiting_decision?
-          CandidateMailer.application_withdrawn_on_request_one_offer_one_awaiting_decision(application_choice).deliver_later
-        elsif applications_awaiting_decision_only?
-          CandidateMailer.application_withdrawn_on_request_awaiting_decision_only(application_choice).deliver_later
-        elsif applications_with_offers_only?
-          CandidateMailer.application_withdrawn_on_request_offers_only(application_choice).deliver_later
-        end
+      elsif candidate_applications.all?(&:withdrawn?)
+        CandidateMailer.application_withdrawn_on_request_all_applications_withdrawn(application_choice).deliver_later
+      elsif applications_with_offer_and_awaiting_decision?
+        CandidateMailer.application_withdrawn_on_request_one_offer_one_awaiting_decision(application_choice).deliver_later
+      elsif applications_awaiting_decision_only?
+        CandidateMailer.application_withdrawn_on_request_awaiting_decision_only(application_choice).deliver_later
+      elsif applications_with_offers_only?
+        CandidateMailer.application_withdrawn_on_request_offers_only(application_choice).deliver_later
       end
     end
   end
