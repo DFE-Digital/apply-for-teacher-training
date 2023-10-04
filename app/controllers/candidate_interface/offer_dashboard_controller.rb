31b22e987e0353c4ee0a527dc456b86f5c545544
Iain McNulty <iain.mcnulty@education.gov.uk>
2023-10-04 09:15:10 +0100

    Don't use instance var for status

--- app/controllers/candidate_interface/offer_dashboard_controller.rb
+++ app/controllers/candidate_interface/offer_dashboard_controller.rb
@@ -7,7 +7,6 @@ module CandidateInterface
     def show
       @application_form = current_application
       @application_choice_with_offer = current_application.application_choices.pending_conditions.first || current_application.application_choices.recruited.first || current_application.application_choices.offer_deferred.first
-      @status = @application_choice_with_offer.status
       @accepted_offer_provider_name = @application_choice_with_offer.current_provider.name
       @accepted_offer_course_name_and_code = @application_choice_with_offer.current_course.name_and_code
     end
