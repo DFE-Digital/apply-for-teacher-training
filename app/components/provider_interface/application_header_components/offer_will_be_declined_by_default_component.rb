f33140e68ab3580ac2df317d4ece637e48893e13
Sebastian Zaremba <1636476+vassyz@users.noreply.github.com>
2023-10-09 12:10:42 +0100

    Add back the non continuous application text for DBD component
    This is for candidates with offers for 2023, they have till ~12th oct
    until declined by default.

--- app/components/provider_interface/application_header_components/offer_will_be_declined_by_default_component.rb
+++ app/components/provider_interface/application_header_components/offer_will_be_declined_by_default_component.rb
@@ -1,10 +1,21 @@
 module ProviderInterface
   module ApplicationHeaderComponents
     class OfferWillBeDeclinedByDefaultComponent < ApplicationChoiceHeaderComponent
-      def offer_text
+      def continuous_applications_offer_text
         days = application_choice.days_since_offered
 
         "You made this offer #{days_since(days)}. Most candidates respond to offers within 15 working days. The candidate will receive reminders to respond."
+      end
+
+      def decline_by_default_text
+        return unless offer_will_be_declined_by_default?
+
+        if time_is_today_or_tomorrow?(application_choice.decline_by_default_at)
+          "at the end of #{date_and_time_today_or_tomorrow(application_choice.decline_by_default_at)}"
+        else
+          days_remaining = days_until(application_choice.decline_by_default_at.to_date)
+          "in #{days_remaining} (#{application_choice.decline_by_default_at.to_fs(:govuk_date_and_time)})"
+        end
       end
     end
   end
