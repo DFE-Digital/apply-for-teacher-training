8ff79f32a9251e4ea6f5ce1a9c85d0438ac867f8
Iain McNulty <iain.mcnulty@education.gov.uk>
2023-10-23 13:17:33 +0100

    Section is an instance variable of ActivityLog ApplicationForm

--- app/components/provider_interface/activity_log/application_form.rb
+++ app/components/provider_interface/activity_log/application_form.rb
@@ -1,14 +1,13 @@
 module ProviderInterface
   module ActivityLog
     class ApplicationForm
-      attr_reader :event, :application_choice
+      attr_reader :event, :section
       def initialize(event:)
         @event = event
+        @section = ::ApplicationForm::ColumnSectionMapping.by_column(event.audit.audited_changes.keys.first)
       end
 
       def event_description
-        section = ::ApplicationForm::ColumnSectionMapping.by_column(event.audit.audited_changes.keys.first)
-
         "#{section.humanize} edited"
       end
     end
