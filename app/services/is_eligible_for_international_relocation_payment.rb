705cff3cb051f3fc6c775bd05221824f1e276737
MylesJarvis <myles.jarvis@education.gov.uk>
2023-10-24 13:02:59 +0100

    attempt to amend subject mapping

--- app/services/is_eligible_for_international_relocation_payment.rb
+++ app/services/is_eligible_for_international_relocation_payment.rb
@@ -1,7 +1,7 @@
 class IsEligibleForInternationalRelocationPayment
   delegate :application_form, to: :application_choice
 
-  ELIGIBLE_SUBJECTS = %i[classics physics modern_foreign_languages].freeze
+  ELIGIBLE_SUBJECTS = %i[classics physics french german spanish].freeze
 
   def initialize(application_choice)
     @application_choice = application_choice
