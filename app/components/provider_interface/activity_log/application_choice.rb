9645ae89406e1f8e33afd6a3ca5c35747910fca3
Iain McNulty <iain.mcnulty@education.gov.uk>
2023-10-24 09:44:25 +0100

    Refactor ActivityLog ApplicationChoice to use translations

--- app/components/provider_interface/activity_log/application_choice.rb
+++ app/components/provider_interface/activity_log/application_choice.rb
@@ -3,6 +3,7 @@ module ProviderInterface
     class ApplicationChoice
       attr_reader :event, :application_choice
       delegate :changes, to: :event
+      delegate :t, to: :I18n
 
       def initialize(event:)
         @event = event
@@ -10,47 +11,60 @@ module ProviderInterface
       end
 
       def event_description
-        user = event.user_full_name
-        candidate = event.candidate_full_name
+        match_description || rejected_description || declined_description || other_description
+      end
 
-        case event.application_status_at_event
-        when 'awaiting_provider_decision'
-          "Application received from #{candidate}"
-        when 'withdrawn'
-          "#{candidate} withdrew their application"
-        when 'rejected'
-          if application_choice.rejected_by_default
-            "#{candidate}’s application was automatically rejected"
-          else
-            "#{user} rejected #{candidate}’s application"
-          end
-        when 'offer'
-          "#{user} made an offer to #{candidate}"
-        when 'offer_withdrawn'
-          "#{user} withdrew #{candidate}’s offer"
-        when 'declined'
-          if application_choice.declined_by_default
-            "#{candidate}’s offer was automatically declined"
-          else
-            "#{candidate} declined an offer"
-          end
-        when 'pending_conditions'
-          "#{candidate} accepted an offer"
-        when 'conditions_not_met'
-          "#{user} marked #{candidate}’s offer conditions as not met"
-        when 'recruited'
-          "#{user} marked #{candidate}’s offer conditions as all met"
-        when 'offer_deferred'
-          "#{user} deferred #{candidate}’s offer"
-        else
-          if changes['reject_by_default_feedback_sent_at'].present?
-            "#{user} sent feedback to #{candidate}"
-          elsif changes['offer_changed_at'].present?
-            "#{user} changed the offer made to #{candidate}"
-          elsif changes['course_changed_at'].present?
-            "#{user} updated #{candidate}’s course"
-          end
+    private
+
+      def other_description
+        if changes['reject_by_default_feedback_sent_at'].present?
+          t('provider_interface.activity_log.application_choice.reject_by_default_feedback_sent', candidate:, user:)
+        elsif changes['offer_changed_at'].present?
+          t('provider_interface.activity_log.application_choice.offer_changed_at', candidate:, user:)
+        elsif changes['course_changed_at'].present?
+          t('provider_interface.activity_log.application_choice.course_changed_at', candidate:, user:)
         end
+      end
+
+      def declined_description
+        return unless status == 'declined'
+
+        if application_choice.declined_by_default
+          t('provider_interface.activity_log.application_choice.declined_by_default', candidate:)
+        else
+          t('provider_interface.activity_log.application_choice.declined', candidate:)
+        end
+      end
+
+      def rejected_description
+        return unless status == 'rejected'
+
+        if application_choice.rejected_by_default
+          t('provider_interface.activity_log.application_choice.rejected_by_default', candidate:)
+        else
+          t('provider_interface.activity_log.application_choice.rejected', candidate:, user:)
+        end
+      end
+
+      def match_description
+        case status
+        when 'awaiting_provider_decision', 'withdrawn', 'pending_conditions'
+          t("provider_interface.activity_log.application_choice.#{status}", candidate:)
+        when 'offer', 'offer_withdrawn', 'recruited', 'offer_deferred', 'conditions_not_met'
+          t("provider_interface.activity_log.application_choice.#{status}", candidate:, user:)
+        end
+      end
+
+      def user
+        @event.user_full_name
+      end
+
+      def candidate
+        @event.candidate_full_name
+      end
+
+      def status
+        @event.application_status_at_event
       end
     end
   end
