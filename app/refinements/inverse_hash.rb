db1f57ff513b52116058f3c093d95cc880cabc5f
Iain McNulty <iain.mcnulty@education.gov.uk>
2023-10-31 15:58:51 +0000

    Add inverse_hash refinement

--- app/refinements/inverse_hash.rb
+++ app/refinements/inverse_hash.rb
@@ -1,11 +1,26 @@
 module InverseHash
   refine Hash do
+    Hash::UninversableHashError = Class.new(StandardError)
+
     def inverse
       each_with_object({}) do |(key, value), object|
-        value.each do |val|
-          object[val] ||= []
-          object[val] << key unless object[val].include?(key)
-          object[val].sort!
+        if value.is_a?(Array)
+          value.each do |val|
+            if object[val].is_a?(Array)
+              object[val] << key
+            elsif object[val].nil?
+              object[val] = [key]
+            else
+              mid = object[val]
+              object[val] = []
+              object[val] << mid << key
+            end
+            object[val].sort!
+          end
+        elsif value.is_a?(Hash)
+          raise Hash::UninversableHashError, 'Cannot inverse a nested hash'
+        else
+          object[value] = key
         end
       end
     end
---
+++ spec/refinements/inverse_hash_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe InverseHash do
+  before do
+    stub_const('Klass', Class.new do
+      using InverseHash
+
+      def config(hsh)
+        hsh.inverse
+      end
+    end)
+  end
+
+  subject(:run) { Klass.new.config(arg) }
+
+  context 'when the hash is simple' do
+    let(:arg) { { a: 1, b: 2 } }
+
+    it { is_expected.to eq({ 1 => :a, 2 => :b }) }
+  end
+
+  context 'when the hash value is an array' do
+    let(:arg) { { a: 1, b: [1, 2] } }
+
+    it { is_expected.to eq({ 1 => %i[a b], 2 => [:b] }) }
+  end
+
+  context 'when the hash value is an hash' do
+    let(:arg) { { a: 1, b: { c: 2 } } }
+
+    it { expect { run }.to raise_error(Hash::UninversableHashError) }
+  end
+end
