ad303302d7008e315bcc2d0522e77e389b54daf1
Tomas D'Stefano <tomas_stefano@successoft.com>
2023-11-08 10:36:38 +0000

    Move client code to bigquery module

--- app/lib/dfe/bigquery.rb
+++ app/lib/dfe/bigquery.rb
@@ -23,5 +23,18 @@ module DfE
     def self.missing_config
       CONFIGURABLES.select { |value| config.send(value).nil? }
     end
+
+    def self.client
+      @client ||= begin
+        raise(ConfigurationError, "DfE::Bigquery: missing required config values: #{missing_config}") unless valid_config?
+
+        Google::Cloud::Bigquery.new(
+          project: config.bigquery_project_id,
+          credentials: JSON.parse(config.bigquery_api_json_key),
+          retries: config.bigquery_retries,
+          timeout: config.bigquery_timeout,
+        )
+      end
+    end
   end
 end
--- app/lib/dfe/bigquery/table.rb
+++ app/lib/dfe/bigquery/table.rb
@@ -4,18 +4,7 @@ module DfE
   module Bigquery
     class Table
       def self.client
-        @client ||= begin
-          raise(ConfigurationError, "DfE::Bigquery: missing required config values: #{::DfE::Bigquery.missing_config}") unless ::DfE::Bigquery.valid_config?
-
-          config = ::DfE::Bigquery.config
-
-          Google::Cloud::Bigquery.new(
-            project: config.bigquery_project_id,
-            credentials: JSON.parse(config.bigquery_api_json_key),
-            retries: config.bigquery_retries,
-            timeout: config.bigquery_timeout,
-          )
-        end
+        ::DfE::Bigquery.client
       end
     end
   end
