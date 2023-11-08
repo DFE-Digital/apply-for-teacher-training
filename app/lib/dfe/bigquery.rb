d9c65e9b1809588974f59066624f1ff3e5f46418
Tomas D'Stefano <tomas_stefano@successoft.com>
2023-11-08 10:34:03 +0000

    Move config check to bigquery module

--- app/lib/dfe/bigquery.rb
+++ app/lib/dfe/bigquery.rb
@@ -1,20 +1,27 @@
 module DfE
   module Bigquery
     class ConfigurationError < StandardError; end
+    CONFIGURABLES = %i[
+      bigquery_retries
+      bigquery_timeout
+      bigquery_api_json_key
+      bigquery_project_id
+    ].freeze
 
     def self.config
-      configurables = %i[
-        bigquery_retries
-        bigquery_timeout
-        bigquery_api_json_key
-        bigquery_project_id
-      ]
-
-      @config ||= Struct.new(*configurables).new
+      @config ||= Struct.new(*CONFIGURABLES).new
     end
 
     def self.configure
       yield(config)
+    end
+
+    def self.valid_config?
+      missing_config.none?
+    end
+
+    def self.missing_config
+      CONFIGURABLES.select { |value| config.send(value).nil? }
     end
   end
 end
--- app/lib/dfe/bigquery/table.rb
+++ app/lib/dfe/bigquery/table.rb
@@ -1,26 +1,21 @@
+require 'google/cloud/bigquery'
+
 module DfE
   module Bigquery
     class Table
       def self.client
-        require 'google/cloud/bigquery'
+        @client ||= begin
+          raise(ConfigurationError, "DfE::Bigquery: missing required config values: #{::DfE::Bigquery.missing_config}") unless ::DfE::Bigquery.valid_config?
 
-        config = ::DfE::Bigquery.config
+          config = ::DfE::Bigquery.config
 
-        missing_config = %i[
-          bigquery_project_id
-          bigquery_api_json_key
-          bigquery_retries
-          bigquery_timeout
-        ].select { |val| config.send(val).nil? }
-
-        raise(ConfigurationError, "DfE::Bigquery: missing required config values: #{missing_config.join(', ')}") if missing_config.any?
-
-        Google::Cloud::Bigquery.new(
-          project: config.bigquery_project_id,
-          credentials: JSON.parse(config.bigquery_api_json_key),
-          retries: config.bigquery_retries,
-          timeout: config.bigquery_timeout,
-        )
+          Google::Cloud::Bigquery.new(
+            project: config.bigquery_project_id,
+            credentials: JSON.parse(config.bigquery_api_json_key),
+            retries: config.bigquery_retries,
+            timeout: config.bigquery_timeout,
+          )
+        end
       end
     end
   end
