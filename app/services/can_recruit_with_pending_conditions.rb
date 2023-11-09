21977bd12f98ab2837f468679e9d79c70f81bfd1
Carlos Martinez <carlosjmtz@gmail.com>
2023-11-09 16:43:20 +0000

    Allow recruit with pending conditions when accredited body is a SCITT
    Following on from #8650, this allows recruitment when the accredited
    body is a SCITT and the training provider is a lead school.

--- app/services/can_recruit_with_pending_conditions.rb
+++ app/services/can_recruit_with_pending_conditions.rb
@@ -2,14 +2,14 @@ class CanRecruitWithPendingConditions < HasPendingSkeConditionsOnly
   def call
     application_choice.pending_conditions? &&
       pending_ske_conditions_only? &&
-      provider_is_scitt? &&
+      provider_or_accredited_provider_is_scitt? &&
       course_is_within_time_limit?
   end
 
 private
 
-  def provider_is_scitt?
-    application_choice.provider&.scitt?
+  def provider_or_accredited_provider_is_scitt?
+    application_choice.provider&.scitt? || application_choice.accredited_provider&.scitt?
   end
 
   def course_is_within_time_limit?
