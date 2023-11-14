8b452d51345f374518b055d575959845d7bae197
Carlos Martinez <carlosjmtz@gmail.com>
2023-11-14 10:42:30 +0000

    Remove feature flag for recruiting with pending conditions
    The functionality is live and the feature flag is no longer needed.

--- spec/services/recruited_with_pending_conditions_spec.rb
+++ spec/services/recruited_with_pending_conditions_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe RecruitedWithPendingConditions do
 
   subject(:service) { described_class.new(application_choice: offer.application_choice) }
 
-  context 'when feature flag is off' do
+  context 'when there is a pending SKE condition and other met conditions' do
     let(:offer) { create(:offer, :with_ske_conditions) }
 
     before do
@@ -14,31 +14,13 @@ RSpec.describe RecruitedWithPendingConditions do
       end
     end
 
-    it 'returns false' do
+    it 'returns false if the status is `pending_conditions`' do
       expect(service.call).to be(false)
     end
-  end
 
-  context 'when feature flag is on' do
-    before { FeatureFlag.activate(:recruit_with_pending_conditions) }
-
-    context 'when there is a pending SKE condition and other met conditions' do
-      let(:offer) { create(:offer, :with_ske_conditions) }
-
-      before do
-        offer.conditions.reject { |condition| condition.is_a?(SkeCondition) }.each do |condition|
-          condition.update!(status: :met)
-        end
-      end
-
-      it 'returns false if the status is `pending_conditions`' do
-        expect(service.call).to be(false)
-      end
-
-      it 'returns true if the status is `recruited`' do
-        offer.application_choice.recruited!
-        expect(service.call).to be(true)
-      end
+    it 'returns true if the status is `recruited`' do
+      offer.application_choice.recruited!
+      expect(service.call).to be(true)
     end
   end
 end
