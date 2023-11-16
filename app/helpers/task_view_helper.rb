72bdf2c0e2525c58c99aba83896d74ebb0d70b47
Iain McNulty <iain.mcnulty@education.gov.uk>
2023-11-16 09:31:04 +0000

    Add inactive group to UI

--- app/helpers/task_view_helper.rb
+++ app/helpers/task_view_helper.rb
@@ -1,16 +1,17 @@
 module TaskViewHelper
   def task_view_header(choice)
     case choice&.task_view_group
-    when 1 then 'Confirm deferred offers'
-    when 2 then 'Deadline approaching: make decision about application'
-    when 3 then 'Give feedback: you did not make a decision in time'
-    when 4 then 'Received – make a decision'
-    when 5 then 'Interviewing'
-    when 6 then 'Offers pending conditions (previous cycle)'
-    when 7 then 'Waiting for candidate to respond to offer'
-    when 8 then 'Offers pending conditions (current cycle)'
-    when 9 then 'Successful candidates'
-    when 10 then 'Deferred offers'
+    when 1 then 'Received over 30 days ago - make a decision now'
+    when 2 then 'Confirm deferred offers'
+    when 3 then 'Deadline approaching: make decision about application'
+    when 4 then 'Give feedback: you did not make a decision in time'
+    when 5 then 'Received – make a decision'
+    when 6 then 'Interviewing'
+    when 7 then 'Offers pending conditions (previous cycle)'
+    when 8 then 'Waiting for candidate to respond to offer'
+    when 9 then 'Offers pending conditions (current cycle)'
+    when 10 then 'Successful candidates'
+    when 11 then 'Deferred offers'
     else
       'No action needed'
     end
