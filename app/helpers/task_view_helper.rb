0f7b5003dad3b894ee4f95472eb2e4bfef8ad8af
Iain McNulty <iain.mcnulty@education.gov.uk>
2023-11-17 11:31:14 +0000

    Move header display logic to helper

--- app/helpers/task_view_helper.rb
+++ app/helpers/task_view_helper.rb
@@ -1,19 +1,25 @@
 module TaskViewHelper
   def task_view_header(choice)
-    case choice&.task_view_group
-    when 1 then 'Received over 30 days ago - make a decision now'
-    when 2 then 'Received – make a decision'
-    when 3 then 'Confirm deferred offers'
-    when 5 then 'Give feedback: you did not make a decision in time'
-    when 6 then 'Interviewing'
-    when 7 then 'Offers pending conditions (previous cycle)'
-    when 8 then 'Waiting for candidate to respond to offer'
-    when 9 then 'Offers pending conditions (current cycle)'
-    when 10 then 'Successful candidates'
-    when 11 then 'Deferred offers'
-    else
-      'No action needed'
-    end
+    yield (case choice&.task_view_group
+           when 1 then 'Received over 30 days ago - make a decision now'
+           when 2 then 'Received – make a decision'
+           when 3 then 'Confirm deferred offers'
+           when 5 then 'Give feedback: you did not make a decision in time'
+           when 6 then 'Interviewing'
+           when 7 then 'Offers pending conditions (previous cycle)'
+           when 8 then 'Waiting for candidate to respond to offer'
+           when 9 then 'Offers pending conditions (current cycle)'
+           when 10 then 'Successful candidates'
+           when 11 then 'Deferred offers'
+           else
+             'No action needed'
+           end)
+  end
+
+  def display_header?(application_choices, choice)
+    return true if (index = application_choices.index(choice)).zero?
+
+    choice.task_view_group != application_choices[index - 1].task_view_group
   end
 
   def relative_date_text_color(choice)
@@ -29,8 +35,8 @@ module TaskViewHelper
   def task_view_subheader(choice)
     return unless choice.respond_to?(:task_view_group)
 
-    text = { 1 => 'You received these applications over 30 working days ago. You need to make a decision as soon as possible or the candidate may choose to withdraw and apply to another provider.' }.delete(choice.task_view_group)
-
-    tag.p(text, class: 'govuk-caption-s govuk-!-font-size-16 govuk-hint') if text
+    yield (case choice.task_view_group
+           when 1 then 'You received these applications over 30 working days ago. You need to make a decision as soon as possible or the candidate may choose to withdraw and apply to another provider.'
+           end)
   end
 end
