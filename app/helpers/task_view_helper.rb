ee4f19c3964cf5d5443aaf040dda39c646d8be2b
Iain McNulty <iain.mcnulty@education.gov.uk>
2023-11-17 09:19:33 +0000

    Fix ordering of groups in view helper

--- app/helpers/task_view_helper.rb
+++ app/helpers/task_view_helper.rb
@@ -2,9 +2,9 @@ module TaskViewHelper
   def task_view_header(choice)
     case choice&.task_view_group
     when 1 then 'Received over 30 days ago - make a decision now'
-    when 2 then 'Confirm deferred offers'
-    when 4 then 'Give feedback: you did not make a decision in time'
-    when 5 then 'Received – make a decision'
+    when 2 then 'Received – make a decision'
+    when 3 then 'Confirm deferred offers'
+    when 5 then 'Give feedback: you did not make a decision in time'
     when 6 then 'Interviewing'
     when 7 then 'Offers pending conditions (previous cycle)'
     when 8 then 'Waiting for candidate to respond to offer'
