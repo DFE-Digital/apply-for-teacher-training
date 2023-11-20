3a6b50cdfe594408badc56662713b3c54ec48495
Sebastian Zaremba <1636476+vassyz@users.noreply.github.com>
2023-11-20 13:48:54 +0000

    Update worker batching attributes

--- app/workers/send_apply_to_another_course_when_inactive_email_to_candidates_worker.rb
+++ app/workers/send_apply_to_another_course_when_inactive_email_to_candidates_worker.rb
@@ -1,10 +1,11 @@
 class SendApplyToAnotherCourseWhenInactiveEmailToCandidatesWorker
   include Sidekiq::Worker
 
-  BATCH_SIZE = 120
+  STAGGER_OVER = 3.hours
+  BATCH_SIZE = 150
 
   def perform
-    BatchDelivery.new(relation: GetInactiveApplicationsFromPastDay.call, batch_size: BATCH_SIZE).each do |batch_time, records|
+    BatchDelivery.new(relation: GetInactiveApplicationsFromPastDay.call, stagger_over: STAGGER_OVER, batch_size: BATCH_SIZE).each do |batch_time, records|
       SendApplyToAnotherCourseWhenInactiveEmailToCandidatesBatchWorker.perform_at(
         batch_time,
         records.pluck(:id),
--- app/workers/send_apply_to_multiple_courses_when_inactive_email_to_candidates_worker.rb
+++ app/workers/send_apply_to_multiple_courses_when_inactive_email_to_candidates_worker.rb
@@ -1,8 +1,11 @@
 class SendApplyToMultipleCoursesWhenInactiveEmailToCandidatesWorker
   include Sidekiq::Worker
 
+  STAGGER_OVER = 3.hours
+  BATCH_SIZE = 150
+
   def perform
-    BatchDelivery.new(relation: GetInactiveApplicationsFromPastDay.call(single: false)).each do |batch_time, records|
+    BatchDelivery.new(relation: GetInactiveApplicationsFromPastDay.call(single: false), stagger_over: STAGGER_OVER, batch_size: BATCH_SIZE).each do |batch_time, records|
       SendApplyToMultipleCoursesWhenInactiveEmailToCandidatesBatchWorker.perform_at(
         batch_time,
         records.pluck(:id),
