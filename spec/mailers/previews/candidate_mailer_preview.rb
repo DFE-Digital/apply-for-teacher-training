719d35d399a112100520d256597e34d6ecae3231
Iain McNulty <iain.mcnulty@education.gov.uk>
2023-12-01 11:08:24 +0000

    Add offer_10_day mailer

--- spec/mailers/previews/candidate_mailer_preview.rb
+++ spec/mailers/previews/candidate_mailer_preview.rb
@@ -116,6 +116,12 @@ class CandidateMailerPreview < ActionMailer::Preview
     CandidateMailer.interview_cancelled(application_choice, interview, 'You contacted us to say you didn’t want to apply for this course any more.')
   end
 
+  def offer_10_day
+    application_form_with_course_choices([application_choice_with_offer])
+
+    CandidateMailer.offer_10_day(application_choice_with_offer)
+  end
+
   def new_offer_made
     application_form_with_name = FactoryBot.build_stubbed(
       :application_form,
