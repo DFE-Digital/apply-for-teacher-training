28d5618ca537c630e913ebc5e89ccbef3ce1019b
Avin Hurry <avinhurry@icloud.com>
2023-12-05 12:03:39 +0000

    [1035] Remove sending logic and related specs

--- app/services/send_new_offer_email_to_candidate.rb
+++ app/services/send_new_offer_email_to_candidate.rb
@@ -6,33 +6,6 @@ class SendNewOfferEmailToCandidate
   end
 
   def call
-    if application_choice.continuous_applications?
-      CandidateMailer.new_offer_made(application_choice).deliver_later
-    else
-      pre_continuous_applications_withdrawn_mailers
-    end
-  end
-
-private
-
-  def pre_continuous_applications_withdrawn_mailers
-    CandidateMailer.send(
-      :"new_offer_#{mail_type(application_choice)}",
-      application_choice,
-    ).deliver_later
-  end
-
-  def mail_type(application_choice)
-    candidate_application_choices = application_choice.self_and_siblings
-    number_of_pending_decisions = candidate_application_choices.decision_pending.count
-    number_of_offers = candidate_application_choices.select(&:offer?).count
-
-    if number_of_pending_decisions.positive?
-      :decisions_pending
-    elsif number_of_offers > 1
-      :multiple_offers
-    else
-      :single_offer
-    end
+    CandidateMailer.new_offer_made(application_choice).deliver_later
   end
 end
