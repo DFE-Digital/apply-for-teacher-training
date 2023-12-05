efa19aa45c66fc26400f9d3aa450952b7226c153
Tomas D'Stefano <tomas_stefano@successoft.com>
2023-12-05 14:16:10 +0000

    Remove RBD call from process stale applications service

--- app/services/process_stale_applications.rb
+++ app/services/process_stale_applications.rb
@@ -5,15 +5,7 @@ class ProcessStaleApplications
 
   def call
     @application_choices.each do |application_choice|
-      stale_application_processor(application_choice:).new(application_choice:).call
-    end
-  end
-
-  def stale_application_processor(application_choice:)
-    if application_choice.continuous_applications?
-      MarkInactiveApplication
-    else
-      RejectApplicationByDefault
+      MarkInactiveApplication.new(application_choice:).call
     end
   end
 end
