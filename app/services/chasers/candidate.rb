6527aff1480290609819b28fedf19630deee9a6a
Tomas D'Stefano <tomas_stefano@successoft.com>
2023-12-11 16:21:13 +0000

    Fix unecessary interval implementation
    We can use range day in the chasers

--- app/services/chasers/candidate.rb
+++ app/services/chasers/candidate.rb
@@ -1,21 +1,21 @@
 module Chasers
   module Candidate
-    # chaser: [days_ago, days_ago]
-    OFFER_CHASERS_TO_INTERVALS = {
-      offer_10_day: [19, 10],
-      offer_20_day: [29, 20],
-      offer_30_day: [39, 30],
-      offer_40_day: [49, 40],
-      offer_50_day: [59, 50],
+    # chaser: days_ago
+    OFFER_CHASERS = {
+      offer_10_day: 10,
+      offer_20_day: 20,
+      offer_30_day: 30,
+      offer_40_day: 40,
+      offer_50_day: 50,
     }.freeze
 
     def self.chaser_types
-      OFFER_CHASERS_TO_INTERVALS.keys
+      OFFER_CHASERS.keys
     end
 
     def self.chaser_to_date_range
-      OFFER_CHASERS_TO_INTERVALS.each_with_object({}) do |(chaser_type, (start, ending)), object|
-        object[chaser_type] = (start.days.ago..ending.days.ago)
+      OFFER_CHASERS.transform_values do |day|
+        day.days.ago.all_day
       end
     end
   end
--- spec/services/chasers/candidate_spec.rb
+++ spec/services/chasers/candidate_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Chasers::Candidate do
   describe '.chaser_to_date_range' do
     it 'returns a chaser_type with respective date range' do
-      expected = { offer_10_day: (20.days.ago..10.days.ago) }
+      expected = { offer_10_day: 10.days.ago.all_day }
       expect(described_class.chaser_to_date_range).to include(expected)
     end
   end
