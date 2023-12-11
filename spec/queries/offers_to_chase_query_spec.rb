728ea6132dc9b1e87f84b0ed269b2f9801012c96
Iain McNulty <iain.mcnulty@education.gov.uk>
2023-12-11 16:36:18 +0000

    Update OffersToChaseQuery
      OffersToChaseQuery now uses static days implemented by
      Chasers::Candidate

--- spec/queries/offers_to_chase_query_spec.rb
+++ spec/queries/offers_to_chase_query_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe OffersToChaseQuery do
   let(:application_choice_with_offer_not_in_offer_status) { create(:application_choice, :recruited, offer: create(:offer, created_at: inside_range)) }
   let(:application_choice_offer_from_last_cycle) { create(:application_choice, current_recruitment_cycle_year: CycleTimetable.previous_year, offer: create(:offer, created_at: inside_range)) }
 
-  let(:application_choice_with_chaser) { create(:application_choice, :offer, chasers_sent: [create(:chaser_sent, chaser_type: "offer_#{spread[1]}_day")]) }
+  let(:application_choice_with_chaser) { create(:application_choice, :offer, chasers_sent: [create(:chaser_sent, chaser_type: "offer_#{days}_day")]) }
 
   let(:application_choice_without_chaser) { create(:application_choice, :offer, current_recruitment_cycle_year:) }
   let(:offer_without_chaser) { application_choice_without_chaser.offer }
@@ -13,13 +13,13 @@ RSpec.describe OffersToChaseQuery do
 
   let(:chaser_type) { :offer_10_day }
 
-  let(:spread) { [20, 10] }
-  let(:date_range) { (spread[0].days.ago..spread[1].days.ago) }
-  let(:inside_range) { date_range.max - 1.day }
-  let(:offset) { 0 }
+  let(:days) { 10 }
+  let(:date_range) { days.days.ago.all_day }
+  let(:inside_range) { date_range.max - 1.hour }
+  let(:outside_range) { date_range.max + 1.hour }
 
   before do
-    TestSuiteTimeMachine.travel_temporarily_to(spread[1].days.ago + offset) do
+    TestSuiteTimeMachine.travel_temporarily_to(offset) do
       application_choice_without_offer
       application_choice_with_chaser
       application_choice_without_chaser
@@ -27,7 +27,7 @@ RSpec.describe OffersToChaseQuery do
   end
 
   context 'when offers are made before the chaser sent range for offer_10_day' do
-    let(:offset) { 10.seconds }
+    let(:offset) { outside_range }
 
     it 'returns empty collection' do
       expect(offer_without_chaser.created_at).to be > 10.days.ago
@@ -36,8 +36,8 @@ RSpec.describe OffersToChaseQuery do
   end
 
   context 'when offers are made inside the range 20 to 10 days ago and chaser_type is offer_10_day' do
-    let(:spread) { [20, 10] }
-    let(:offset) { -10.seconds }
+    let(:days) { 10 }
+    let(:offset) { inside_range }
 
     it 'returns the application choice without a chaser' do
       expect(date_range).to cover(offer_without_chaser.created_at)
@@ -47,10 +47,10 @@ RSpec.describe OffersToChaseQuery do
   end
 
   describe 'when the range is 20 to 30 days ago and offers are made inside the range' do
-    let(:spread) { [30, 20] }
+    let(:days) { 20 }
 
     context 'and chaser_type is offer_20_day and chaser has been sent for offer_20_day' do
-      let(:offset) { -10.seconds }
+      let(:offset) { inside_range }
       let(:chaser_type) { :offer_20_day }
 
       it 'returns the application choice without a chaser sent' do
@@ -61,7 +61,7 @@ RSpec.describe OffersToChaseQuery do
     end
 
     context 'and chaser_type is offer_20_day and chaser only sent for offer_10_day' do
-      let(:offset) { -10.seconds }
+      let(:offset) { inside_range }
       let(:chaser_type) { :offer_10_day }
 
       it 'returns the application choice no chaser sent and one with old chaser sent' do
@@ -73,10 +73,10 @@ RSpec.describe OffersToChaseQuery do
   end
 
   describe 'when the range is 10 to 20 days ago and offers are made inside the range' do
-    let(:spread) { [20, 10] }
+    let(:days) { 10 }
 
     context 'and the application is from the previous recruitment cycle' do
-      let(:offset) { -10.seconds }
+      let(:offset) { inside_range }
       let(:chaser_type) { :offer_10_day }
       let(:current_recruitment_cycle_year) { CycleTimetable.previous_year }
 
