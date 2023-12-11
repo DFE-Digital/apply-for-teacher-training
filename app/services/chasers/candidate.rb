f2131d5b6721c53f42713fc3a16851bb22089b62
Tomas D'Stefano <tomas_stefano@successoft.com>
2023-12-11 16:06:27 +0000

    Fix a bug on chaser offer intervals
    On the 19, 29, 39, 49 we shouldn't send the emails only in the following
    day

--- app/services/chasers/candidate.rb
+++ app/services/chasers/candidate.rb
@@ -2,11 +2,11 @@ module Chasers
   module Candidate
     # chaser: [days_ago, days_ago]
     OFFER_CHASERS_TO_INTERVALS = {
-      offer_10_day: [20, 10],
-      offer_20_day: [30, 20],
-      offer_30_day: [40, 30],
-      offer_40_day: [50, 40],
-      offer_50_day: [60, 50],
+      offer_10_day: [19, 10],
+      offer_20_day: [29, 20],
+      offer_30_day: [39, 30],
+      offer_40_day: [49, 40],
+      offer_50_day: [59, 50],
     }.freeze
 
     def self.chaser_types
