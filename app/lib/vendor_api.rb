e62000d5b541024bbb8ab722d5b050f343cbe2ad
Sebastian Zaremba <1636476+vassyz@users.noreply.github.com>
2023-12-14 15:14:18 +0000

    Update VendorAPI version

--- app/lib/vendor_api.rb
+++ app/lib/vendor_api.rb
@@ -5,7 +5,8 @@ module VendorAPI
   VERSION_1_1 = '1.1'.freeze
   VERSION_1_2 = '1.2'.freeze
   VERSION_1_3 = '1.3'.freeze
-  VERSION = VERSION_1_3
+  VERSION_1_4 = '1.4pre'.freeze
+  VERSION = VERSION_1_4
 
   VERSIONS = {
     '1.0' => [
