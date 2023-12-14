9d387d4a04203ff46fb3c4ee738dc06bda59aeea
Iain McNulty <iain.mcnulty@education.gov.uk>
2023-12-14 15:04:20 +0000

    Delete SiteSelectionController#create

--- config/routes/candidate.rb
+++ config/routes/candidate.rb
@@ -404,7 +404,6 @@ namespace :candidate_interface, path: '/candidate' do
 
       get '/provider/:provider_id/courses/:course_id/full' => 'course_choices/course_selection#full', as: :course_choices_full
 
-      post '/provider/:provider_id/courses/:course_id/:study_mode' => 'course_choices/site_selection#create'
       patch '/provider/:provider_id/courses/:course_id/:study_mode/edit' => 'course_choices/site_selection#update'
 
       get '/another' => 'course_choices/add_another_course#ask', as: :course_choices_add_another_course
