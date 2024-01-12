035c60ad0e7b7983691202c8ac36796170d3d2e7
Tomas D'Stefano <tomas_stefano@successoft.com>
2024-01-12 17:28:48 +0000

    Remove specs from pages that doesn't exist anymore
    We don't link them in the system and we don't render those
    pages anymore so we can delete this specs

--- spec/requests/candidate_interface/continuous_applications_choose_course_spec.rb
+++ spec/requests/candidate_interface/continuous_applications_choose_course_spec.rb
@@ -8,58 +8,15 @@ RSpec.describe 'continuous applications redirects' do
     sign_in candidate
   end
 
-  context 'when continuous applications', :continuous_applications do
-    context 'when cycle is over' do
-      it 'redirects the user when trying to add a course from find' do
-        TestSuiteTimeMachine.travel_permanently_to(after_apply_1_deadline + 1.day)
-        provider = create(:provider, code: '8N5', name: 'Snape University')
-        course = create(:course, :open_on_apply, name: 'Potions', provider:)
+  context 'when cycle is over' do
+    it 'redirects the user when trying to add a course from find' do
+      TestSuiteTimeMachine.travel_permanently_to(after_apply_1_deadline + 1.day)
+      provider = create(:provider, code: '8N5', name: 'Snape University')
+      course = create(:course, :open_on_apply, name: 'Potions', provider:)
 
-        get candidate_interface_continuous_applications_course_confirm_selection_path(course.id)
+      get candidate_interface_continuous_applications_course_confirm_selection_path(course.id)
 
-        expect(response).to redirect_to(candidate_interface_continuous_applications_choices_path)
-      end
-    end
-
-    describe 'choose' do
-      it 'redirects choose to continuous applications' do
-        get candidate_interface_course_choices_choose_path
-
-        expect(response).to redirect_to(candidate_interface_continuous_applications_do_you_know_the_course_path)
-      end
-    end
-
-    describe 'find' do
-      it 'redirects find to continuous applications' do
-        get candidate_interface_go_to_find_path
-
-        expect(response).to redirect_to(candidate_interface_continuous_applications_go_to_find_explanation_path)
-      end
-    end
-
-    describe 'provider' do
-      it 'redirects to provider continuous applications' do
-        get candidate_interface_course_choices_provider_path
-        expect(response).to redirect_to(candidate_interface_continuous_applications_provider_selection_path)
-      end
-    end
-  end
-
-  context 'when not continuous applications', continuous_applications: false do
-    describe 'find' do
-      it 'redirects find to continuous applications' do
-        get candidate_interface_go_to_find_path
-
-        expect(response).not_to redirect_to(candidate_interface_continuous_applications_go_to_find_explanation_path)
-      end
-    end
-
-    describe 'provider' do
-      it 'redirects to provider continuous applications' do
-        get candidate_interface_course_choices_provider_path
-
-        expect(response).not_to redirect_to(candidate_interface_continuous_applications_provider_selection_path)
-      end
+      expect(response).to redirect_to(candidate_interface_continuous_applications_choices_path)
     end
   end
 end
