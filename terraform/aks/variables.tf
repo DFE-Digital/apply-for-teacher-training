b119c6b78eb23a3a05ac8db0c8cc4cf5aa0ca156
RMcVelia <rick.mcvelia@gmail.com>
2024-01-16 16:05:29 +0000

    Increase prod postgres cpu alert window to 15m

--- terraform/aks/variables.tf
+++ terraform/aks/variables.tf
@@ -75,6 +75,12 @@ variable "pdb_min_available" { default = null }
 variable "config_short" {}
 variable "service_short" {}
 variable "azure_maintenance_window" { default = null }
+variable "alert_window_size" {
+  type        = string
+  nullable    = false
+  default     = "PT5M"
+  description = "The period of time that is used to monitor alert activity e.g PT1M, PT5M, PT15M, PT30M, PT1H, PT6H or PT12H"
+}
 
 locals {
   app_name_suffix = var.app_name_suffix != null ? var.app_name_suffix : var.paas_app_environment
