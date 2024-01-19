da6522204daf9951d56c2ca1f1aec1afa93101f0
Tomas D'Stefano <tomas_stefano@successoft.com>
2024-01-19 09:39:55 +0000

    Change spec description and add one more assertion

--- spec/system/candidate_interface/submitting/candidate_submits_a_duplicate_application_spec.rb
+++ spec/system/candidate_interface/submitting/candidate_submits_a_duplicate_application_spec.rb
@@ -5,7 +5,7 @@ RSpec.feature 'Submitting a duplicate application' do
 
   before { given_courses_exist }
 
-  scenario 'Candidate submits complete application' do
+  scenario 'Candidate tries to submit an application and the submission is blocked' do
     given_i_have_an_account_submission_blocked
     and_i_am_signed_in
     when_i_try_to_submit_an_application
@@ -28,6 +28,7 @@ RSpec.feature 'Submitting a duplicate application' do
 
   def then_i_can_see_an_interstitial_page_that_i_cant_submit
     expect(page).to have_content('You’ve created more than one account.')
+    expect(page).to have_content('You can apply for up to 4 courses at a time.')
     expect(page).to have_no_button('Check and submit')
   end
 end
