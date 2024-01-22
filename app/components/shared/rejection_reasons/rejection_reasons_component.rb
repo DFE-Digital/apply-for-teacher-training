cc976634a1094ddca6a422a5b97ddf0472815b6b
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-01-22 17:25:23 +0000

    Remove StructuredRejectionReasonsComponent
      This parent class offered very little reason for existing and just
      added to the number of Rejection classes in the code base

--- app/components/shared/rejection_reasons/rejection_reasons_component.rb
+++ app/components/shared/rejection_reasons/rejection_reasons_component.rb
@@ -2,17 +2,31 @@
 # This component class supports the rendering of rejection reasons from the current iteration of structured rejection reasons.
 # See https://github.com/DFE-Digital/apply-for-teacher-training/blob/main/docs/reasons-for-rejection.md
 #
-class RejectionReasons::RejectionReasonsComponent < RejectionReasons::StructuredRejectionReasonsComponent
-  attr_reader :editable
+class RejectionReasons::RejectionReasonsComponent < ViewComponent::Base
+  include ViewHelper
 
-  def initialize(application_choice:, reasons:, editable: false, render_link_to_find_when_rejected_on_qualifications: false)
-    super(
-      application_choice:,
-      reasons:,
-      render_link_to_find_when_rejected_on_qualifications:,
+  attr_reader :application_choice, :reasons, :editable
+
+  def initialize(application_choice:, reasons:, render_link_to_find_when_rejected_on_qualifications: false, editable: false)
+    @application_choice = application_choice
+    @reasons = reasons
+    @render_link_to_find_when_rejected_on_qualifications = render_link_to_find_when_rejected_on_qualifications
+    @editable = editable
+  end
+
+  def paragraphs(input)
+    return [] if input.blank?
+
+    input.split("\r\n")
+  end
+
+  def link_to_find_when_rejected_on_qualifications(application_choice)
+    link = govuk_link_to(
+      'Find postgraduate teacher training courses',
+      "#{I18n.t('find_postgraduate_teacher_training.production_url')}course/#{application_choice.provider.code}/#{application_choice.course.code}#section-entry",
     )
 
-    @editable = editable
+    "View the course requirements on #{link}.".html_safe
   end
 
   def editable?
