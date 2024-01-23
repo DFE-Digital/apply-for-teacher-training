fdba92b7956ab0319c0ddc8169bee9a8f0fef958
avinhurry <avin.hurry@digital.education.gov.uk>
2024-01-23 15:20:47 +0000

    [1066] Add dynamic label
    This will be different depending on whether the country is EU or not and if the user has the right to work or study in the UK.

--- app/presenters/candidate_interface/personal_details_review_presenter.rb
+++ app/presenters/candidate_interface/personal_details_review_presenter.rb
@@ -101,21 +101,39 @@ module CandidateInterface
       ]
 
       if @application_form.immigration_status
-        rows << {
-          key: I18n.t('application_form.personal_details.immigration_status.label'),
-          value: formatted_immigration_status,
-          action: (if @editable && !@application_form.submitted_applications?
-                     {
-                       href: candidate_interface_edit_immigration_status_path(return_to_params),
-                       visually_hidden_text: I18n.t('application_form.personal_details.immigration_status.change_action'),
-                     }
-                   end),
-          html_attributes: {
-            data: {
-              qa: 'personal_details_immigration_status',
-            },
-          },
-        }
+        rows << if EU_EEA_SWISS_COUNTRY_CODES.intersect?(@application_form.nationalities.map { |name| NATIONALITIES_BY_NAME[name] })
+                  {
+                    key: I18n.t('application_form.personal_details.immigration_status.label'),
+                    value: formatted_immigration_status,
+                    action: (if @editable && !@application_form.submitted_applications?
+                               {
+                                 href: candidate_interface_edit_immigration_status_path(return_to_params),
+                                 visually_hidden_text: I18n.t('application_form.personal_details.immigration_status.change_action'),
+                               }
+                             end),
+                    html_attributes: {
+                      data: {
+                        qa: 'personal_details_immigration_status',
+                      },
+                    },
+                  }
+                else
+                  {
+                    key: 'Visa or Immigration status',
+                    value: formatted_immigration_status,
+                    action: (if @editable && !@application_form.submitted_applications?
+                               {
+                                 href: candidate_interface_edit_immigration_status_path(return_to_params),
+                                 visually_hidden_text: I18n.t('application_form.personal_details.visa_or_immigration_status.change_action'),
+                               }
+                             end),
+                    html_attributes: {
+                      data: {
+                        qa: 'personal_details_visa_or_immigration_status',
+                      },
+                    },
+                  }
+                end
       end
       rows
     end
