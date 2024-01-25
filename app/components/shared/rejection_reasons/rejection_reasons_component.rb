431d05821808aa731ada87b6a078cd52f286e5e1
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-01-25 15:16:18 +0000

    Use simple_format instead of paragraphs method

--- app/components/shared/rejection_reasons/rejection_reasons_component.rb
+++ app/components/shared/rejection_reasons/rejection_reasons_component.rb
@@ -13,12 +13,6 @@ class RejectionReasons::RejectionReasonsComponent < ViewComponent::Base
     @editable = editable
   end
 
-  def paragraphs(input)
-    return [] if input.blank?
-
-    input.split("\r\n")
-  end
-
   def link_to_find_when_rejected_on_qualifications(application_choice)
     link = govuk_link_to(
       'Find postgraduate teacher training courses',
