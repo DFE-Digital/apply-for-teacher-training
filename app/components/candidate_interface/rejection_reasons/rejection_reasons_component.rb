9d39600ac29ccbfd58b9353d5cbb56c283f4c874
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-01-26 10:15:38 +0000

    Extract CI RejectionReasonsComponent logic
      Separate shared and CandidateInterface RejectionReasons component.
      Update the CI template to render the updated design of
      RejectionReasons with inset text for the reason details

--- app/components/candidate_interface/rejection_reasons/rejection_reasons_component.rb
+++ app/components/candidate_interface/rejection_reasons/rejection_reasons_component.rb
@@ -1,6 +1,31 @@
 module CandidateInterface
   module RejectionReasons
-    class RejectionReasonsComponent < ::RejectionReasons::RejectionReasonsComponent
+    class RejectionReasonsComponent < ViewComponent::Base
+      include ViewHelper
+
+      attr_reader :application_choice
+
+      def initialize(application_choice:, render_link_to_find_when_rejected_on_qualifications: false)
+        @application_choice = application_choice
+        @render_link_to_find_when_rejected_on_qualifications = render_link_to_find_when_rejected_on_qualifications
+      end
+
+      def link_to_find_when_rejected_on_qualifications(application_choice)
+        link = govuk_link_to(
+          'Find postgraduate teacher training courses',
+          "#{application_choice.course.find_url}#section-entry",
+        )
+
+        "View the course requirements on #{link}.".html_safe
+      end
+
+      def reasons
+        ::RejectionReasons.new(application_choice.structured_rejection_reasons)
+      end
+
+      def render_link_to_find?(reason)
+        reason.id == 'qualifications' && @render_link_to_find_when_rejected_on_qualifications
+      end
     end
   end
 end
