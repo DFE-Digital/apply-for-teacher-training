159e59627ba3bcd2abf8cc5e557b9e7cbff772f5
Tomas D'Stefano <tomas_stefano@successoft.com>
2024-01-30 15:58:36 +0000

    Do not render continuous apps routes to 2023 application forms

--- app/filters/unsuccessful_carry_over_filter.rb
+++ app/filters/unsuccessful_carry_over_filter.rb
@@ -2,8 +2,8 @@ class UnsuccessfulCarryOverFilter < ApplicationFilter
   delegate :candidate_interface_application_complete_path, to: :controller
 
   def call
-    return if CycleTimetable.can_add_course_choice?(current_application)
+    return if CycleTimetable.can_add_course_choice?(current_application) || current_application.carry_over?
 
-    redirect_to candidate_interface_application_complete_path if current_application.carry_over?
+    redirect_to candidate_interface_application_complete_path unless current_application.continuous_applications?
   end
 end
--- spec/requests/candidate_interface/continuous_applications_spec.rb
+++ spec/requests/candidate_interface/continuous_applications_spec.rb
@@ -12,7 +12,9 @@ RSpec.describe 'continuous applications' do
 
   before { sign_in candidate }
 
-  context 'when continuous applications', :continuous_applications do
+  context 'when continuous applications' do
+    before { create(:application_form, :continuous_applications, candidate:) }
+
     it 'be successful' do
       continuous_applications_routes.each do |path|
         get path
@@ -21,11 +23,26 @@ RSpec.describe 'continuous applications' do
     end
   end
 
-  context 'when not continuous applications', continuous_applications: false do
-    it 'be not found' do
-      continuous_applications_routes.each do |path|
-        get path
-        expect(response).to have_http_status(:not_found)
+  context 'when not continuous applications' do
+    context 'when submitted' do
+      before { create(:application_form, :completed, :pre_continuous_applications, candidate:) }
+
+      it 'redirects to the application complete page' do
+        continuous_applications_routes.each do |path|
+          get path
+          expect(response).to redirect_to(candidate_interface_application_complete_path)
+        end
+      end
+    end
+
+    context 'when unsubmitted' do
+      before { create(:application_form, :minimum_info, :pre_continuous_applications, submitted_at: nil, candidate:) }
+
+      it 'redirects to the carry over page' do
+        continuous_applications_routes.each do |path|
+          get path
+          expect(response).to redirect_to(candidate_interface_start_carry_over_path)
+        end
       end
     end
   end
