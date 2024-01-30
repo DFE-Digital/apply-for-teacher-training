fc0047a4a564e5f67269080357abc6fe1b0efb30
Tomas D'Stefano <tomas_stefano@successoft.com>
2024-01-30 15:31:52 +0000

    Add specs for after signin redirects controllers
    We need to increase coverage on the redirects

---
+++ spec/requests/candidate_interface/after_sign_in_redirects_spec.rb
@@ -0,0 +1,68 @@
+require 'rails_helper'
+
+RSpec.describe 'After sign in redirects' do
+  include Devise::Test::IntegrationHelpers
+  let(:candidate) { create(:candidate, course_from_find:) }
+  let(:application_form) { create(:application_form, :completed, candidate:) }
+  let(:course_from_find) { create(:course) }
+
+  before do
+    sign_in candidate
+  end
+
+  context 'when course from find is not present' do
+    context 'when application is pre continuous applications' do
+    end
+
+    context 'when application is continuous applications' do
+    end
+  end
+
+  #    before_action :redirect_to_path_if_path_params_are_present
+
+  context 'when application submitted and non continuous applications' do
+    let!(:application_form) { create(:application_form, :completed, :pre_continuous_applications, candidate: candidate) }
+
+    it 'redirects to application complete path' do
+      get candidate_interface_interstitial_path
+      expect(response).to redirect_to(candidate_interface_application_complete_path)
+    end
+  end
+
+  context 'when application already contains course from find' do
+    it 'redirects to your applications and shows a message to the candidate' do
+      create(:application_choice, :awaiting_provider_decision, application_form:, course_option: create(:course_option, course: course_from_find))
+      get candidate_interface_interstitial_path
+      expect(response).to redirect_to(candidate_interface_continuous_applications_choices_path)
+      follow_redirect!
+      expect(response.body).to include("You have already added an application for #{course_from_find.name}")
+    end
+  end
+
+  context 'when reaching maximum number of choices' do
+    it 'redirects to your applications and shows a message to the candidate' do
+      create_list(:application_choice, ApplicationForm::MAXIMUM_NUMBER_OF_COURSE_CHOICES, :continuous_applications, :awaiting_provider_decision, application_form:)
+      get candidate_interface_interstitial_path
+      expect(response).to redirect_to(candidate_interface_continuous_applications_choices_path)
+      follow_redirect!
+      expect(response.body).to include(I18n.t('errors.messages.too_many_course_choices', max_applications: ApplicationForm::MAXIMUM_NUMBER_OF_COURSE_CHOICES, course_name: course_from_find.name))
+    end
+  end
+
+  context 'when reaching maximum unsuccessful number of choices' do
+    it 'redirects to your applications and shows a message to the candidate' do
+      create_list(:application_choice, ApplicationForm::MAXIMUM_NUMBER_OF_UNSUCCESSFUL_APPLICATIONS, :continuous_applications, :rejected, application_form:)
+      get candidate_interface_interstitial_path
+      expect(response).to redirect_to(candidate_interface_continuous_applications_choices_path)
+      follow_redirect!
+      expect(response.body).to include(I18n.t('errors.messages.too_many_unsuccessful_choices', max_unsuccessful_applications: ApplicationForm::MAXIMUM_NUMBER_OF_UNSUCCESSFUL_APPLICATIONS))
+    end
+  end
+
+  context 'when candidate can confirm the course selected' do
+    it 'redirects to confirm selection page' do
+      get candidate_interface_interstitial_path
+      expect(response).to redirect_to(candidate_interface_continuous_applications_course_confirm_selection_path(course_from_find.id))
+    end
+  end
+end
