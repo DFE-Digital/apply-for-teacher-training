da25e3b4afb8cba20f306d330f299fda7141904b
Tomas D'Stefano <tomas_stefano@successoft.com>
2024-01-31 14:11:37 +0000

    Make sure non continuous apps (2023 cycle) can't access 2024
    Even if somehow they bypass the carry over filters they shouldn't
    see the continuous apps. I added as a line of defense if somehow
    the carry over filters has any bugs we should stop the worst from
    happening.

--- app/controllers/candidate_interface/continuous_applications_controller.rb
+++ app/controllers/candidate_interface/continuous_applications_controller.rb
@@ -3,11 +3,16 @@ module CandidateInterface
     before_action :redirect_to_post_offer_dashboard_if_accepted_deferred_or_recruited
     before_action UnsuccessfulCarryOverFilter
     before_action CarryOverFilter
+    before_action :verify_continuous_applications
 
   private
 
     def redirect_to_your_applications_if_submitted
       redirect_to candidate_interface_continuous_applications_choices_path unless application_choice.unsubmitted?
+    end
+
+    def verify_continuous_applications
+      render_404 unless current_application&.continuous_applications?
     end
   end
 end
