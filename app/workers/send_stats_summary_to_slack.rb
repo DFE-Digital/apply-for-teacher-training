13398705553094f417f3af46c7344b4d088eb7e4
James Glenn <47917431+JR-G@users.noreply.github.com>
2024-02-02 13:47:06 +0000

    Take the slack webhook URL as an argument
    This will allow us to use the worker for additional apps.

--- app/workers/send_stats_summary_to_slack.rb
+++ app/workers/send_stats_summary_to_slack.rb
@@ -2,6 +2,6 @@ class SendStatsSummaryToSlack
   include Sidekiq::Worker
 
   def perform
-    SlackNotificationWorker.perform_async(StatsSummary.new.as_slack_message, nil, '#twd_find_and_apply')
+    SlackNotificationWorker.perform_async(StatsSummary.new.as_slack_message, nil, '#twd_find_and_apply', ENV['FIND_AND_APPLY_SLACK_URL'])
   end
 end
--- app/workers/send_weekly_stats_summary_to_slack.rb
+++ app/workers/send_weekly_stats_summary_to_slack.rb
@@ -2,6 +2,6 @@ class SendWeeklyStatsSummaryToSlack
   include Sidekiq::Worker
 
   def perform
-    SlackNotificationWorker.perform_async(WeeklyStatsSummary.new.as_slack_message, nil, '#twd_find_and_apply')
+    SlackNotificationWorker.perform_async(WeeklyStatsSummary.new.as_slack_message, nil, '#twd_find_and_apply', ENV['FIND_AND_APPLY_SLACK_URL'])
   end
 end
