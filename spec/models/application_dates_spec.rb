2a364529a2308a2d3bb18921fa09220ba4462673
Tomas D'Stefano <tomas_stefano@successoft.com>
2024-02-06 12:17:08 +0000

    Update specs submitted dates

--- spec/models/application_dates_spec.rb
+++ spec/models/application_dates_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationDates do
-  let(:submitted_at) { Time.zone.local(2019, 5, 1, 12, 0, 0).end_of_day }
+  let(:submitted_at) { Time.zone.local(CycleTimetable.current_year, 5, 1, 12, 0, 0).end_of_day }
 
   let(:application_form) do
     create(:application_form, submitted_at:, application_choices: [application_choice])
@@ -14,14 +14,8 @@ RSpec.describe ApplicationDates do
   end
 
   describe '#submitted_at' do
-    context 'when not continuous applications' do
-      it 'returns date that application was submitted on' do
-        expect(application_dates.submitted_at).to eql(submitted_at)
-      end
-    end
-
     context 'when before continuous applications' do
-      let(:submitted_at) { 1.month.ago }
+      let(:submitted_at) { Time.zone.local(2023, 5, 1, 12, 0, 0).end_of_day }
 
       let(:application_form) do
         create(:application_form, :pre_continuous_applications, submitted_at:, application_choices: [application_choice])
@@ -65,7 +59,7 @@ RSpec.describe ApplicationDates do
     end
 
     it 'returns date that providers will respond by when reject_by_default_at is set' do
-      reject_by_default_at = Time.zone.local(2019, 6, 28, 23, 59, 59)
+      reject_by_default_at = Time.zone.local(CycleTimetable.current_year, 6, 28, 23, 59, 59)
       application_form.application_choices.each do |application_choice|
         application_choice.update(reject_by_default_at:)
       end
