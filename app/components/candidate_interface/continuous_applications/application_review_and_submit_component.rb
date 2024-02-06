c857b1bcaa767c4c9a2ec3b917f92a4ed3b36668
Sebastian Zaremba <1636476+vassyz@users.noreply.github.com>
2024-02-06 09:25:19 +0000

    Add changes after design review

--- app/components/candidate_interface/continuous_applications/application_review_and_submit_component.rb
+++ app/components/candidate_interface/continuous_applications/application_review_and_submit_component.rb
@@ -3,13 +3,14 @@ module CandidateInterface
     class ApplicationReviewAndSubmitComponent < ViewComponent::Base
       include ApplicationHelper
 
-      attr_reader :application_choice, :application_choice_submission
+      attr_reader :application_choice, :application_choice_submission, :show_delete_application
       delegate :errors, to: :application_choice_submission
       delegate :unsubmitted?, :current_course, :current_course_option, to: :application_choice
 
-      def initialize(application_choice:)
+      def initialize(application_choice:, show_delete_application: false)
         @application_choice = application_choice
         @application_choice_submission = CandidateInterface::ContinuousApplications::ApplicationChoiceSubmission.new(application_choice:)
+        @show_delete_application = show_delete_application
       end
 
       def render?
--- app/controllers/candidate_interface/continuous_applications/course_choices/review_controller.rb
+++ app/controllers/candidate_interface/continuous_applications/course_choices/review_controller.rb
@@ -3,7 +3,15 @@ module CandidateInterface
     module CourseChoices
       class ReviewController < BaseController
         def show
+          @show_delete_application = !courses_path_request?
+
           @application_choice = current_application.application_choices.find(params[:application_choice_id])
+        end
+
+      private
+
+        def courses_path_request?
+          request.referer&.match?('courses')
         end
       end
     end
