e7bdee184ecb5484877ed244ab2a6eb9e5e2d8d1
Tomas D'Stefano <tomas_stefano@successoft.com>
2024-02-06 14:58:05 +0000

    Make spec pass for prefill in sandbox

--- app/controllers/candidate_interface/prefill_application_form_controller.rb
+++ app/controllers/candidate_interface/prefill_application_form_controller.rb
@@ -13,9 +13,10 @@ module CandidateInterface
         if @prefill_application_or_not_form.prefill?
           prefill_candidate_application_form
           flash[:info] = 'This application has been prefilled with example data'
+          return redirect_to candidate_interface_continuous_applications_choices_path
         end
 
-        redirect_to candidate_interface_continuous_applications_choices_path
+        redirect_to candidate_interface_continuous_applications_details_path
       else
         track_validation_error(@prefill_application_or_not_form)
         render :new
