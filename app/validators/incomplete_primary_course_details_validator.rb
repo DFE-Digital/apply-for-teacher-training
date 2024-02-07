766e171094c8ec406d6ecc5099077011b22782f1
Tomas D'Stefano <tomas_stefano@successoft.com>
2024-02-07 12:54:29 +0000

    Join primary course details validators on application choice submission
    One small check and one link difference from two scenarios on primary
    course which I think it makes sense to group then together.

--- app/validators/incomplete_primary_course_details_validator.rb
+++ app/validators/incomplete_primary_course_details_validator.rb
@@ -4,19 +4,30 @@ class IncompletePrimaryCourseDetailsValidator < ActiveModel::EachValidator
       application_choice:,
       application_form: application_choice.application_form,
     )
-    return unless application_form_sections.only_science_gcse_incomplete?
 
-    record.errors.add(
-      attribute,
-      :incomplete_primary_course_details,
-      link_to_science:,
-    )
+    if application_form_sections.only_science_gcse_incomplete?
+      record.errors.add(
+        attribute,
+        :incomplete_primary_course_details,
+        link_to_science:,
+      )
+    elsif application_form_sections.science_gcse_incomplete_and_others?
+      record.errors.add(
+        attribute,
+        :incomplete_details_including_primary_course_details,
+        link_to_details:,
+      )
+    end
   end
 
 private
 
   def link_to_science
     view.govuk_link_to('Add your science GCSE grade (or equivalent)', Rails.application.routes.url_helpers.candidate_interface_gcse_details_new_type_path('science'))
+  end
+
+  def link_to_details
+    view.govuk_link_to('Add your science GCSE grade (or equivalent)', Rails.application.routes.url_helpers.candidate_interface_continuous_applications_details_path)
   end
 
   def view
