a1cb33913d811c265197c0ea945879ba4b7c6805
Tomas D'Stefano <tomas_stefano@successoft.com>
2024-02-07 12:55:18 +0000

    Move specs to one file and add more scenario
    I decided to group all submission specs into the only service is using
    them.
    Also I added more scenarios around max limit on course choices

---
+++ spec/services/candidate_interface/continuous_applications/application_choice_submission_spec.rb
@@ -0,0 +1,347 @@
+require 'rails_helper'
+
+RSpec.describe CandidateInterface::ContinuousApplications::ApplicationChoiceSubmission do
+  subject(:application_choice_submission) { described_class.new(application_choice:) }
+
+  let(:routes) { Rails.application.routes.url_helpers }
+  let(:view) do
+    Class.new do
+      include ActionView::Helpers::UrlHelper
+      include GovukLinkHelper
+    end.new
+  end
+
+  describe 'validations' do
+    context 'immigration_status validation' do
+      let(:course) { create(:course, :with_course_options, :open_on_apply, level: 'secondary') }
+      let(:application_form) { create(:application_form, :completed) }
+      let(:application_choice) { create(:application_choice, :unsubmitted, course:, application_form:) }
+      let(:link_to_find) do
+        view.govuk_link_to(
+          'Find a course that has visa sponsorship',
+          routes.find_url,
+          target: '_blank',
+          rel: 'nofollow',
+        )
+      end
+
+      context 'when candidate is uk or irish national' do
+        it 'is valid' do
+          expect(application_choice_submission).to be_valid
+        end
+      end
+
+      context 'when candidate is American without right to work but course is salary and sponsored' do
+        let(:application_form) do
+          create(
+            :application_form,
+            :completed,
+            first_nationality: 'American',
+            right_to_work_or_study: 'no',
+            efl_completed: true,
+          )
+        end
+        let(:course) do
+          create(
+            :course,
+            :with_course_options,
+            :open_on_apply,
+            funding_type: 'fee',
+            can_sponsor_student_visa: true,
+            level: 'secondary',
+          )
+        end
+
+        it 'is valid' do
+          expect(application_choice_submission).to be_valid
+        end
+      end
+
+      context 'when candidate nationality is not British or Irish' do
+        let(:application_form) do
+          create(
+            :application_form,
+            :completed,
+            first_nationality: 'American',
+            right_to_work_or_study: 'no',
+            efl_completed: true,
+          )
+        end
+
+        it 'adds error to application choice' do
+          expect(application_choice_submission).not_to be_valid
+          expect(application_choice_submission.errors[:application_choice]).to include(
+            t('activemodel.errors.models.candidate_interface/continuous_applications/application_choice_submission.attributes.application_choice.immigration_status', link_to_find:),
+          )
+        end
+      end
+
+      context 'when candidate has right to work but course does not sponsor visa' do
+        let(:application_form) { create(:application_form, :completed, first_nationality: 'American', right_to_work_or_study: 'no', efl_completed: true) }
+        let(:course) { create(:course, :with_course_options, :open_on_apply, funding_type: 'fee', can_sponsor_student_visa: false, level: 'secondary') }
+
+        it 'adds error to application choice' do
+          expect(application_choice_submission).not_to be_valid
+          expect(application_choice_submission.errors[:application_choice]).to include(
+            t('activemodel.errors.models.candidate_interface/continuous_applications/application_choice_submission.attributes.application_choice.immigration_status', link_to_find:),
+          )
+        end
+      end
+    end
+
+    context 'applications_closed validation' do
+      let(:course) { create(:course, :with_course_options, :open_on_apply, level: 'secondary') }
+      let(:application_form) { create(:application_form, :completed) }
+      let(:application_choice) { create(:application_choice, :unsubmitted, course:, application_form:) }
+
+      context 'when apply is open and course is open for applications' do
+        it 'is valid' do
+          expect(application_choice_submission).to be_valid
+        end
+      end
+
+      context 'when apply is open but course not open for applications' do
+        it 'adds error to application choice' do
+          application_choice.course.update(applications_open_from: 1.day.from_now)
+
+          expect(application_choice_submission).not_to be_valid
+          expect(application_choice_submission.errors[:application_choice]).to include(
+            "This course is not yet open to applications. You’ll be able to submit your application on #{1.day.from_now.to_fs(:govuk_date)}.",
+          )
+        end
+      end
+
+      context 'when apply is closed and course open for applications same day' do
+        it 'adds error to application choice', time: CycleTimetableHelper.after_find_opens do
+          expect(application_choice_submission).not_to be_valid
+          expect(application_choice_submission.errors[:application_choice]).to include(
+            "This course is not yet open to applications. You’ll be able to submit your application on #{CycleTimetable.apply_opens.to_fs(:govuk_date)}.",
+          )
+        end
+      end
+
+      context 'when apply is open and course open for applications next day' do
+        let(:applications_open_from) { 1.day.from_now }
+        let(:course) { create(:course, :with_course_options, level: 'secondary', applications_open_from:) }
+
+        it 'adds error to application choice' do
+          expect(application_choice_submission).not_to be_valid
+          expect(application_choice_submission.errors[:application_choice]).to include(
+            "This course is not yet open to applications. You’ll be able to submit your application on #{course.applications_open_from.to_fs(:govuk_date)}.",
+          )
+        end
+      end
+    end
+
+    context 'course_unavailable validation' do
+      let(:course) { build(:course, :open_on_apply, course_options: []) }
+      let(:course_option) { create(:course_option, course: course) }
+      let(:application_form) { create(:application_form, :completed) }
+      let(:application_choice) { create(:application_choice, :unsubmitted, application_form:) }
+
+      context 'all validations pass' do
+        let(:course) { build(:course, :open_on_apply, :with_course_options) }
+
+        it 'adds no errors to application choice submission' do
+          expect(application_choice_submission).to be_valid
+        end
+      end
+
+      context 'course is full' do
+        let(:course_option) { create(:course_option, :no_vacancies, course: course) }
+        let(:application_choice) { create(:application_choice, course_option: course_option, application_form:) }
+
+        it 'adds error to application choice submission' do
+          expect(application_choice_submission).not_to be_valid
+          expect(application_choice_submission.errors[:application_choice]).to include(course_unavailable_error_message(application_choice))
+        end
+      end
+
+      context 'course not exposed in find' do
+        let(:course) { build(:course, :open_on_apply, exposed_in_find: false, course_options: []) }
+        let(:application_form) { create(:application_form, :minimum_info) }
+        let(:application_choice) { create(:application_choice, course_option: course_option, application_form:) }
+
+        it 'adds error to application choice submission' do
+          expect(application_choice_submission).not_to be_valid
+          expect(application_choice_submission.errors[:application_choice]).to include(course_unavailable_error_message(application_choice))
+        end
+      end
+
+      context 'course site not still valid' do
+        let(:course_option) { create(:course_option, site_still_valid: false, course: course) }
+        let(:application_form) { create(:application_form, :minimum_info) }
+        let(:application_choice) { create(:application_choice, course_option: course_option, application_form:) }
+
+        it 'adds error to application choice submission' do
+          expect(application_choice_submission).not_to be_valid
+          expect(application_choice_submission.errors[:application_choice]).to include(course_unavailable_error_message(application_choice))
+        end
+      end
+
+      def course_unavailable_error_message(application_choice)
+        <<~MSG.chomp
+          You cannot submit this application as the course is no longer available.
+
+          #{view.govuk_link_to('Remove this application', Rails.application.routes.url_helpers.candidate_interface_continuous_applications_confirm_destroy_course_choice_path(application_choice.id))} and search for other courses.
+        MSG
+      end
+    end
+
+    context 'incomplete_primary_course_details validation' do
+      let(:course_option) { create(:course_option, :open_on_apply, course:) }
+      let(:course) { create(:course, :open_on_apply, :primary, :with_course_options) }
+      let(:application_form) { create(:application_form, :completed) }
+      let(:application_choice) { create(:application_choice, :unsubmitted, application_form:, course:) }
+
+      context 'when all sections are completed' do
+        it 'is valid' do
+          expect(application_choice_submission).to be_valid
+        end
+      end
+
+      context 'primary course with only science gcse section incomplete' do
+        let(:course_option) { create(:course_option, course:) }
+        let(:application_form) { create(:application_form, :completed, science_gcse_completed: false) }
+        let(:application_choice) { create(:application_choice, :unsubmitted, course_option:, application_form:) }
+        let(:course) { create(:course, :open_on_apply, :primary) }
+
+        it 'adds error to application choice' do
+          expect(application_choice_submission).not_to be_valid
+          expect(application_choice_submission.errors[:application_choice]).to include(link_to_gcse_error_message)
+        end
+      end
+
+      context 'when secondary course choice with only science gcse section incomplete' do
+        let(:course) { create(:course, :open_on_apply, :secondary) }
+        let(:course_option) { create(:course_option, course:) }
+        let(:application_form) { create(:application_form, :completed, science_gcse_completed: false) }
+        let(:application_choice) { create(:application_choice, :unsubmitted, course_option:, application_form:) }
+
+        it 'is valid' do
+          application_choice_submission.valid?
+
+          expect(application_choice_submission.errors).to be_empty
+        end
+      end
+
+      context 'primary courses with science gcse section incomplete and other details incomplete' do
+        let(:application_form) { create(:application_form, :completed, degrees_completed: false, science_gcse_completed: false) }
+        let(:application_choice) { create(:application_choice, :unsubmitted, course_option:, application_form:) }
+
+        it 'adds error to application choice' do
+          expect(application_choice_submission).not_to be_valid
+          expect(application_choice_submission.errors[:application_choice]).to include(link_to_details_error_message)
+        end
+      end
+
+      context 'when secondary courses with science gcse section incomplete and other details incomplete' do
+        let(:course) { create(:course, :open_on_apply, :secondary) }
+        let(:course_option) { create(:course_option, course:) }
+        let(:application_form) { create(:application_form, :completed, degrees_completed: false, science_gcse_completed: false) }
+        let(:application_choice) { create(:application_choice, :unsubmitted, course_option:, application_form:) }
+
+        it 'does not add an error to incomplete science GCSE' do
+          application_choice_submission.valid?
+
+          expect(application_choice_submission.errors.map(&:type)).to eq([:incomplete_details])
+        end
+      end
+
+      def link_to_details_error_message
+        link_to_details = view.govuk_link_to('Add your science GCSE grade (or equivalent)', Rails.application.routes.url_helpers.candidate_interface_continuous_applications_details_path)
+        t('activemodel.errors.models.candidate_interface/continuous_applications/application_choice_submission.attributes.application_choice.incomplete_details_including_primary_course_details', link_to_details:)
+      end
+
+      def link_to_gcse_error_message
+        link_to_science = view.govuk_link_to('Add your science GCSE grade (or equivalent)', Rails.application.routes.url_helpers.candidate_interface_gcse_details_new_type_path('science'))
+
+        t('activemodel.errors.models.candidate_interface/continuous_applications/application_choice_submission.attributes.application_choice.incomplete_primary_course_details', link_to_science:)
+      end
+    end
+
+    context 'incomplete_details validation' do
+      let(:application_form) { create(:application_form, :completed) }
+      let(:application_choice) { create(:application_choice, :unsubmitted, application_form:) }
+
+      context 'valid' do
+        it 'is valid' do
+          expect(application_choice_submission).to be_valid
+        end
+      end
+
+      context 'sections incomplete' do
+        let(:application_form) { create(:application_form, :completed, degrees_completed: false) }
+        let(:application_choice) { create(:application_choice, application_form:) }
+
+        it 'adds error to application choice' do
+          expect(application_choice_submission).not_to be_valid
+          expect(application_choice_submission.errors[:application_choice]).to include(incomplete_details_error_message)
+        end
+      end
+
+      def incomplete_details_error_message
+        <<~MSG.chomp
+          You cannot submit this application until you #{view.govuk_link_to('complete your details', Rails.application.routes.url_helpers.candidate_interface_continuous_applications_details_path)}.
+
+          Your application will be saved as a draft while you finish adding your details.
+        MSG
+      end
+    end
+
+    context 'add more course choices validation' do
+      context 'when candidate can submit further applications' do
+        let(:application_form) { create(:completed_application_form, submitted_application_choices_count: 3) }
+        let(:application_choice) { create(:application_choice, :unsubmitted, application_form:) }
+
+        it 'is valid' do
+          expect(application_choice_submission).to be_valid
+        end
+      end
+
+      context 'when candidate can not submit further applications' do
+        let(:application_form) { create(:completed_application_form, submitted_application_choices_count: 4) }
+        let(:application_choice) { create(:application_choice, :unsubmitted, application_form:) }
+
+        it 'adds error to application choice' do
+          expect(application_choice_submission).not_to be_valid
+          expect(application_choice_submission.errors[:application_choice]).to include(
+            'You cannot submit this application because you have already submitted the maximum number of applications',
+          )
+        end
+      end
+
+      context 'when candidate does not reach the maximum unsuccessful choices' do
+        let(:application_form) { create(:completed_application_form, submitted_application_choices_count: 3) }
+        let(:application_choice) { create(:application_choice, :unsubmitted, application_form:) }
+
+        it 'is valid' do
+          application_form.application_choices << build_list(
+            :application_choice,
+            ApplicationForm::MAXIMUM_NUMBER_OF_UNSUCCESSFUL_APPLICATIONS - 1,
+            :rejected,
+          )
+          expect(application_choice_submission).to be_valid
+        end
+      end
+
+      context 'when candidate reaches the maximum unsuccessful choices' do
+        let(:application_form) { create(:completed_application_form) }
+        let(:application_choice) { create(:application_choice, :unsubmitted, application_form:) }
+
+        it 'adds error to application choice' do
+          application_form.application_choices << build_list(
+            :application_choice,
+            ApplicationForm::MAXIMUM_NUMBER_OF_UNSUCCESSFUL_APPLICATIONS,
+            :rejected,
+          )
+          expect(application_choice_submission).not_to be_valid
+
+          expect(application_choice_submission.errors[:application_choice]).to include(
+            "You cannot submit this application because you have #{ApplicationForm::MAXIMUM_NUMBER_OF_UNSUCCESSFUL_APPLICATIONS} unsuccessful applications",
+          )
+        end
+      end
+    end
+  end
+end
