4e4cbfba13ed890a9d274447faaed86fea193975
avinhurry <avin.hurry@digital.education.gov.uk>
2024-02-13 09:48:23 +0000

    [1128] Add govuk-frontend assets
    Required for govuk-components version 5.0

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -1,6 +1,6 @@
 import { initAll as govUKFrontendInitAll } from 'govuk-frontend'
 import '../styles/application.scss'
 
-require.context('govuk-frontend/dist/govuk')
+require.context('govuk-frontend/govuk/assets')
 
 govUKFrontendInitAll()
