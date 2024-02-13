be34c990cbf000cda6c6950e195d01768ee67f56
Tomas D'Stefano <tomas_stefano@successoft.com>
2024-02-13 07:48:45 +0000

    Organising the grouped tabs into the application dashboard

--- app/components/candidate_interface/continuous_applications/dashboard_component.rb
+++ app/components/candidate_interface/continuous_applications/dashboard_component.rb
@@ -1,24 +1,24 @@
 module CandidateInterface
   module ContinuousApplications
     class DashboardComponent < ViewComponent::Base
-      attr_reader :application_form
+      attr_reader :application_form, :application_choices
 
-      def initialize(application_form:)
+      ApplicationType = Struct.new(:text, :link, :active?, keyword_init: true)
+
+      def initialize(application_form:, application_choices:)
         @application_form = application_form
+        @application_choices = application_choices
+        @application_types = %w[all offers_received draft unsuccessful in_progress withdraw declined]
       end
 
-      def application_choices
-        CandidateInterface::SortApplicationChoices.call(
-          application_choices:
-            @application_form
-              .application_choices
-              .includes(:course, :site, :provider, :current_course, :current_course_option, :interviews)
-              .includes(offer: :conditions),
-        )
+      def tabs
+        @application_types.map do |application_type|
+          ApplicationType.new(text: I18n.t("candidate_interface.application_tabs.#{application_type}"), link: candidate_interface_continuous_applications_choices_path(application_type:), active?: active_tab?(application_type))
+        end
       end
 
-      def group_header(application_choice)
-        t("candidate_interface.sort_application_choices.#{application_choice.application_choices_group.humanize}")
+      def active_tab?(application_type)
+        application_type == params[:application_type]
       end
     end
   end
--- app/controllers/candidate_interface/continuous_applications_choices_controller.rb
+++ app/controllers/candidate_interface/continuous_applications_choices_controller.rb
@@ -4,6 +4,20 @@ module CandidateInterface
 
     def index
       @application_form_presenter = CandidateInterface::ApplicationFormPresenter.new(current_application)
+      @application_choices = CandidateInterface::SortApplicationChoices.call(
+        application_choices:
+          current_application
+            .application_choices
+            .includes(:course, :site, :provider, :current_course, :current_course_option, :interviews)
+            .includes(offer: :conditions),
+      )
+
+      @application_types = %w[all offers_received draft unsuccessful in_progress withdraw declined]
+      # ask Laura about order of withdrawn / declined
+      # todo: see when user tries to force non existent application type
+      if params[:application_type] && params[:application_type] != 'all'
+        @application_choices = @application_choices.select { |ac| ac.application_choices_group == @application_types.index(params[:application_type]) }
+      end
     end
   end
 end
