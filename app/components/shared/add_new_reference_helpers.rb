0629224833778d36d65a4fabcfd33c07ef772148
Avin Hurry <avinhurry@icloud.com>
2024-02-24 18:33:29 +0000

    [1128] Fix incorrect argument error in `govuk_link_to`
    This resolves an incorrect argument error by using the double splat operator to pass the `extra_options` hash as keyword arguments to `govuk_link_to`.
    
    This ensures compatibility with `govuk_link_to` in version 5.0, which expects options to be explicitly passed as keyword arguments.

--- app/components/shared/add_new_reference_helpers.rb
+++ app/components/shared/add_new_reference_helpers.rb
@@ -4,6 +4,8 @@ module AddNewReferenceHelpers
   def options_for_add_reference_link
     if application_form.complete_references_information?
       { secondary: true }
+    else
+      {}
     end
   end
 end
