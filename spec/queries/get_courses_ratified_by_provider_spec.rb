5e2d3a5336b39adfe5d37b694aefc735cb5f971f
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-02-29 10:42:02 +0000

    open_on_apply has no bearing on GetCoursesRatifiedByProvider

--- spec/queries/get_courses_ratified_by_provider_spec.rb
+++ spec/queries/get_courses_ratified_by_provider_spec.rb
@@ -5,32 +5,25 @@ RSpec.describe GetCoursesRatifiedByProvider do
   let(:previous_cycle) { false }
 
   it 'returns courses in the current year' do
-    course_current_year = create(:course, :open, accredited_provider: provider)
+    course_current_year = create(:course, accredited_provider: provider)
     create_list(:course_option, 3, course: course_current_year)
 
-    create(:course_option, course: create(:course, :open, :previous_year, accredited_provider: provider))
+    create(:course_option, course: create(:course, :previous_year, accredited_provider: provider))
 
     result = described_class.call(provider:, previous_cycle:)
     expect(result).to contain_exactly(course_current_year)
   end
 
   it 'only returns courses with a course option' do
-    create(:course, :open, accredited_provider: provider)
+    create(:course, accredited_provider: provider)
 
     result = described_class.call(provider:, previous_cycle:)
     expect(result).to be_empty
   end
 
   it 'excludes courses a provider runs' do
-    create(:course, :open, provider:, accredited_provider: provider)
-    create(:course, :open, provider:)
-
-    result = described_class.call(provider:, previous_cycle:)
-    expect(result).to be_empty
-  end
-
-  it 'only returns courses that are open on apply' do
-    create(:course, accredited_provider: provider)
+    create(:course, provider:, accredited_provider: provider)
+    create(:course, provider:)
 
     result = described_class.call(provider:, previous_cycle:)
     expect(result).to be_empty
@@ -41,7 +34,7 @@ RSpec.describe GetCoursesRatifiedByProvider do
     course_previous_year = create(:course, :previous_year, accredited_provider: provider)
     create_list(:course_option, 3, course: course_previous_year)
 
-    create(:course_option, course: create(:course, :open, accredited_provider: provider))
+    create(:course_option, course: create(:course, accredited_provider: provider))
 
     result = described_class.call(provider:, previous_cycle:)
     expect(result).to contain_exactly(course_previous_year)
