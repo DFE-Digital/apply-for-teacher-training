dc974cb73bc044616a6b44a504809abb9ea151ba
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-03-05 11:10:44 +0000

    Upgrade Rails configs to v7.1

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,11 +2,7 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 
-# Add the GOVUK Frontend fonts path
-Rails.application.config.assets.paths << Rails.root.join('node_modules/govuk-frontend/dist/govuk/assets/images')
-Rails.application.config.assets.paths << Rails.root.join('node_modules/govuk-frontend/dist/govuk/assets/fonts')
-
-Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
@@ -15,3 +11,7 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+# Add the GOVUK Frontend fonts path
+Rails.application.config.assets.paths << Rails.root.join("node_modules/govuk-frontend/dist/govuk/assets/images")
+Rails.application.config.assets.paths << Rails.root.join("node_modules/govuk-frontend/dist/govuk/assets/fonts")
