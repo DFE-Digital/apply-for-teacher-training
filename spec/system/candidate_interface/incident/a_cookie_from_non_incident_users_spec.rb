0c239ab24f87603d5093216be0215e74fbcca1a8
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-03-08 12:50:09 +0000

    Spec ensuring non incident users are unaffected by eviction

---
+++ spec/system/candidate_interface/incident/a_cookie_from_non_incident_users_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+require_relative 'incident_helper'
+
+RSpec.feature 'User unaffected by incident' do
+  include SignInHelper
+  include CandidateHelper
+  include IncidentHelper
+
+  scenario 'user id > 46 behaves normally' do
+    given_i_am_a_candidate_with_a_non_incident_id
+    when_i_am_signed_in
+    and_i_visit_my_details
+    then_i_am_on_the_my_details_page
+  end
+
+  def given_i_am_a_candidate_with_a_non_incident_id
+    @candidate = create(:candidate, id: 47)
+    create(:application_form, candidate: @candidate)
+  end
+end
