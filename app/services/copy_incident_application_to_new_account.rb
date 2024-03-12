6c02769fefb2f89d57f77ffea848d17c4c549a02
Tomas D'Stefano <tomas_stefano@successoft.com>
2024-03-12 14:45:05 +0000

    Add logging to copy incident application

--- app/services/copy_incident_application_to_new_account.rb
+++ app/services/copy_incident_application_to_new_account.rb
@@ -9,11 +9,13 @@ class CopyIncidentApplicationToNewAccount
   def call!
     # Create a new Candidate
     # this will not create a duplicate if they already exist
+    log('Creating a new candidate if they do not exist')
     CandidateInterface::SignUpForm.new(email_address: candidate_email_address).save
     candidate = Candidate.find_by!(email_address: candidate_email_address)
 
     # Copy the Application Form to the new Candidate
     # `target_phase` is always `"apply_1"`
+    log("Copying application form #{@original_application_form.id} from candidate #{@original_application_form.candidate_id} to candidate #{candidate.id}")
     new_application_form = DuplicateApplication.new(
       @original_application_form,
       target_phase: 'apply_1',
@@ -22,12 +24,14 @@ class CopyIncidentApplicationToNewAccount
 
     # Update the Reference section
     # This is only needed if the original Application Form had the Reference Section completed
+    log('Mark references section as complete')
     CandidateInterface::ReferenceSectionCompleteForm.new(
       application_form: new_application_form,
       completed: original_application_form.references_completed?.to_s, # section complete form expect a string
     ).save(new_application_form, :references_completed) #  need to pass the new application_form again
 
     # Create copies of the original Application Choices all in Draft
+    log("Copying #{original_application_form.application_choices.count} application choices to application form #{new_application_form.id}")
     original_application_form.application_choices.each do |original_application_choice|
       course_option = original_application_choice.course_option
       new_application_form.application_choices.new.configure_initial_course_choice!(course_option)
@@ -35,6 +39,7 @@ class CopyIncidentApplicationToNewAccount
 
     # Submit application choices - only from awaiting provider decisions - from
     # original application form
+    log("Submit #{original_application_form.application_choices.awaiting_provider_decision.count} application choices to application form #{new_application_form.id}")
     new_application_form.application_choices.each do |application_choice|
       original_application_choice = original_application_form.application_choices.find_by(course_option: application_choice.course_option)
       next unless original_application_choice.awaiting_provider_decision?
@@ -45,5 +50,11 @@ class CopyIncidentApplicationToNewAccount
     end
 
     new_application_form
+  end
+
+  def log(message)
+    Rails.logger.info('=' * 80)
+    Rails.logger.info(message)
+    Rails.logger.info('=' * 80)
   end
 end
