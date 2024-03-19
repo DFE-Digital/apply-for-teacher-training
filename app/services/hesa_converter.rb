d1bfb91d1925db40e4ba9ca5f3ee6664dd5179c8
Tomas D'Stefano <tomas_stefano@successoft.com>
2024-03-19 17:27:36 +0000

    Refactor HESA converter to have more grnaular methods

--- app/services/hesa_converter.rb
+++ app/services/hesa_converter.rb
@@ -1,12 +1,9 @@
 class HesaConverter
-  attr_reader :application_form, :recruitment_cycle_year, :hesa_sex_data
+  attr_reader :application_form, :recruitment_cycle_year
 
   def initialize(application_form:, recruitment_cycle_year:)
     @application_form = application_form
     @recruitment_cycle_year = recruitment_cycle_year
-    @application_form_sex = application_form.equality_and_diversity['sex'].to_s
-    @application_form_disabilities = @application_form.equality_and_diversity['disabilities']
-    @hesa_sex_data = Hesa::Sex.find(@application_form_sex.downcase, @recruitment_cycle_year)
   end
 
   def hesa_sex
@@ -14,7 +11,7 @@ class HesaConverter
   end
 
   def sex
-    hesa_sex_data&.type || @application_form_sex
+    hesa_sex_data&.type || application_form_sex
   end
 
   def hesa_disabilities
@@ -26,37 +23,35 @@ class HesaConverter
   end
 
   def hesa_ethnicity
-    if @application_form.equality_and_diversity['ethnic_background'].present?
-      hesa_code = Hesa::Ethnicity.find_without_conversion(
-        @application_form.equality_and_diversity['ethnic_background'],
-        @recruitment_cycle_year,
-      )&.hesa_code
-
-      if hesa_code.blank? && @application_form.equality_and_diversity['hesa_ethnicity'].present?
-        ethnic_value = HesaEthnicityCollections::HESA_ETHNICITIES_2019_2020.to_h[@application_form.equality_and_diversity['hesa_ethnicity']]
-        hesa_code = Hesa::Ethnicity.find_without_conversion(ethnic_value, @recruitment_cycle_year)&.hesa_code
-      end
-
-      hesa_code || @application_form.equality_and_diversity['hesa_ethnicity']
-    end
+    ethnicity_data[:hesa_ethnicity]
   end
 
-private
+  private
 
-  NOT_KNOW_DISABILITY_UUID = 'b14e142a-adfe-4646-af5d-8236b6a5b48d'.freeze
+  def application_form_sex
+    @application_form_sex ||= application_form.equality_and_diversity['sex'].to_s
+  end
+
+  def application_form_disabilities
+    @application_form_disabilities ||= @application_form.equality_and_diversity['disabilities']
+  end
+
+  def hesa_sex_data
+    @hesa_sex_data ||= Hesa::Sex.find(application_form_sex.downcase, recruitment_cycle_year)
+  end
 
   def hesa_disabilities_data
-    return {} unless candidate_chosen_no_disabilities? || @application_form_disabilities.present?
+    return {} unless candidate_chosen_no_disabilities? || application_form_disabilities.present?
 
     if candidate_chosen_no_disabilities?
-      no_disability = Hesa::Disability.no_disability(recruitment_cycle_year: @recruitment_cycle_year)
+      no_disability = Hesa::Disability.no_disability(recruitment_cycle_year: recruitment_cycle_year)
 
       {
         hesa_disabilities: [no_disability.hesa_code],
-        disabilities: [I18n.t('equality_and_diversity.disabilities.no.label')],
+        disabilities: [I18n.t('equality_and_diversity.disabilities.no.label')]
       }
     else
-      disabilities = Hesa::Disability.convert_disabilities(@application_form_disabilities)
+      disabilities = Hesa::Disability.convert_disabilities(application_form_disabilities)
       converted_hesa_data = disabilities.map { |disability| Hesa::Disability.find(disability) }
       hesa_disabilities = converted_hesa_data.map(&:hesa_code)
 
@@ -67,7 +62,26 @@ private
     end
   end
 
+  def ethnicity_data
+    return {} unless @application_form.equality_and_diversity['ethnic_background'].present?
+
+    hesa_code = Hesa::Ethnicity.find_without_conversion(
+      @application_form.equality_and_diversity['ethnic_background'],
+      recruitment_cycle_year
+    )&.hesa_code
+
+    if hesa_code.blank? && @application_form.equality_and_diversity['hesa_ethnicity'].present?
+      ethnic_value = HesaEthnicityCollections::HESA_ETHNICITIES_2019_2020.to_h[@application_form.equality_and_diversity['hesa_ethnicity']]
+      hesa_code = Hesa::Ethnicity.find_without_conversion(ethnic_value, recruitment_cycle_year)&.hesa_code
+    end
+
+    {
+      hesa_ethnicity: hesa_code || @application_form.equality_and_diversity['hesa_ethnicity']
+    }
+  end
+
   def candidate_chosen_no_disabilities?
     Array(@application_form.equality_and_diversity['hesa_disabilities']) == ['00']
   end
 end
+
