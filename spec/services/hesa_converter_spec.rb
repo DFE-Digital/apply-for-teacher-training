93347e5b304f23440a31937331f2087faa1dc707
Tomas D'Stefano <tomas_stefano@successoft.com>
2024-03-19 17:27:54 +0000

    Add shared example for ethnicities

--- spec/services/hesa_converter_spec.rb
+++ spec/services/hesa_converter_spec.rb
@@ -147,90 +147,46 @@ RSpec.describe HesaConverter do
   it_behaves_like 'convert disabilities field', { recruitment_cycle_year: 2023, disabilities: ['Some other disability'], hesa_disabilities: ['96'], expected_hesa_disabilities: ['96'], expected_disabilities: ['Some other disability'] }
   it_behaves_like 'convert disabilities field', { recruitment_cycle_year: 2023, disabilities: ['Learning difficulty', 'Some other disability'], hesa_disabilities: %w[51 96], expected_hesa_disabilities: %w[51 96], expected_disabilities: ['Dyslexia, dyspraxia or attention deficit hyperactivity disorder (ADHD) or another learning difference', 'Some other disability'] }
 
-  #    context 'when field is HESA ethnicities' do
-  #      context 'when ethnic background is nil' do
-  #        it 'carries over as the same ethnic data for' do
-  #          @original_application_form.update!(
-  #            recruitment_cycle_year: 2023,
-  #            equality_and_diversity: @original_application_form.equality_and_diversity.merge(
-  #              hesa_ethnicity: nil,
-  #              ethnic_background: nil,
-  #            ),
-  #          )
-  #
-  #          expect(duplicate_application_form.equality_and_diversity['hesa_ethnicity']).to be_nil
-  #          expect(duplicate_application_form.equality_and_diversity['ethnic_background']).to be_nil
-  #        end
-  #      end
-  #
-  #      context 'when 2022 cycle application' do
-  #        [
-  #          { hesa_code: '10', expected_conversion: '160', ethnic_background: 'White' },
-  #          { hesa_code: '15', expected_conversion: '163', ethnic_background: 'Gypsy or Traveller' },
-  #          { hesa_code: '21', expected_conversion: '121', ethnic_background: 'Black or Black British - Caribbean' },
-  #          { hesa_code: '22', expected_conversion: '120', ethnic_background: 'Black or Black British - African' },
-  #          { hesa_code: '29', expected_conversion: '139', ethnic_background: 'Other Black background' },
-  #          { hesa_code: '31', expected_conversion: '103', ethnic_background: 'Asian or Asian British - Indian' },
-  #          { hesa_code: '32', expected_conversion: '104', ethnic_background: 'Asian or Asian British - Pakistani' },
-  #          { hesa_code: '33', expected_conversion: '100', ethnic_background: 'Asian or Asian British - Bangladeshi' },
-  #          { hesa_code: '34', expected_conversion: '101', ethnic_background: 'Chinese' },
-  #          { hesa_code: '39', expected_conversion: '119', ethnic_background: 'Other Asian background' },
-  #          { hesa_code: '41', expected_conversion: '142', ethnic_background: 'Mixed - White and Black Caribbean' },
-  #          { hesa_code: '42', expected_conversion: '141', ethnic_background: 'Mixed - White and Black African' },
-  #          { hesa_code: '43', expected_conversion: '140', ethnic_background: 'Mixed - White and Asian' },
-  #          { hesa_code: '49', expected_conversion: '159', ethnic_background: 'Other Mixed background' },
-  #          { hesa_code: '50', expected_conversion: '180', ethnic_background: 'Arab' },
-  #          { hesa_code: '80', expected_conversion: '899', ethnic_background: 'Other Ethnic background' },
-  #          { hesa_code: '90', expected_conversion: '997', ethnic_background: 'Not known' },
-  #          { hesa_code: '98', expected_conversion: '998', ethnic_background: 'Prefer not to say' },
-  #        ].each do |ethnic_data|
-  #          it "carries over #{ethnic_data[:ethnic_background]} to HESA code '#{ethnic_data[:expected_conversion]}'" do
-  #            @original_application_form.update!(
-  #              recruitment_cycle_year: 2022,
-  #              equality_and_diversity: @original_application_form.equality_and_diversity.merge(
-  #                hesa_ethnicity: ethnic_data[:hesa_code],
-  #                ethnic_background: ethnic_data[:ethnic_background],
-  #              ),
-  #            )
-  #
-  #            expect(duplicate_application_form.equality_and_diversity['hesa_ethnicity']).to eq(ethnic_data[:expected_conversion])
-  #          end
-  #        end
-  #      end
-  #
-  #      context 'when new added fields between cycles like "Roma"' do
-  #        [
-  #          { hesa_code: '168', expected_conversion: '168', ethnic_background: 'Roma' },
-  #        ].each do |ethnic_data|
-  #          it "carries over #{ethnic_data[:ethnic_background]} to HESA code '#{ethnic_data[:expected_conversion]}'" do
-  #            @original_application_form.update!(
-  #              recruitment_cycle_year: 2023,
-  #              equality_and_diversity: @original_application_form.equality_and_diversity.merge(
-  #                hesa_ethnicity: ethnic_data[:hesa_code],
-  #                ethnic_background: ethnic_data[:ethnic_background],
-  #              ),
-  #            )
-  #
-  #            expect(duplicate_application_form.equality_and_diversity['hesa_ethnicity']).to eq(ethnic_data[:expected_conversion])
-  #          end
-  #        end
-  #      end
-  #
-  #      context 'when 2023 cycle application' do
-  #        HesaEthnicityCollections::HESA_ETHNICITIES_2023_2024.to_h.each do |hesa_code, ethnic_background|
-  #          it "carries over as the same ethnic data for '#{ethnic_background}'" do
-  #            @original_application_form.update!(
-  #              recruitment_cycle_year: 2023,
-  #              equality_and_diversity: @original_application_form.equality_and_diversity.merge(
-  #                hesa_ethnicity: hesa_code,
-  #                ethnic_background: ethnic_background,
-  #              ),
-  #            )
-  #
-  #            expect(duplicate_application_form.equality_and_diversity['hesa_ethnicity']).to eq(hesa_code)
-  #            expect(duplicate_application_form.equality_and_diversity['ethnic_background']).to eq(ethnic_background)
-  #          end
-  #        end
-  #      end
-  #    end
+  shared_examples 'convert ethnicity field' do |data|
+    it "converts old HESA codes from #{data[:recruitment_cycle_year]} cycle for '#{data[:ethnic_background]}' into the most up to date HESA codes" do
+      recruitment_cycle_year = data[:recruitment_cycle_year]
+      application_form = create(:application_form, :completed)
+      application_form.update!(
+        recruitment_cycle_year:,
+        equality_and_diversity: application_form.equality_and_diversity.merge(
+          hesa_ethnicity: data[:hesa_ethnicity],
+          ethnic_background: data[:ethnic_background],
+        ),
+      )
+      hesa_converter = described_class.new(application_form:, recruitment_cycle_year: RecruitmentCycle.current_year)
+      expect(hesa_converter.hesa_ethnicity).to eq(data[:expected_hesa_ethnicity])
+    end
+  end
+
+  it_behaves_like 'convert ethnicity field', { recruitment_cycle_year: 2022, hesa_ethnicity: '10', ethnic_background: 'White', expected_hesa_ethnicity: '160' }
+  it_behaves_like 'convert ethnicity field', { recruitment_cycle_year: 2022, hesa_ethnicity: '15', expected_hesa_ethnicity: '163', ethnic_background: 'Gypsy or Traveller' }
+  it_behaves_like 'convert ethnicity field', { recruitment_cycle_year: 2022, hesa_ethnicity: '21', expected_hesa_ethnicity: '121', ethnic_background: 'Black or Black British - Caribbean' }
+  it_behaves_like 'convert ethnicity field', { recruitment_cycle_year: 2022, hesa_ethnicity: '22', expected_hesa_ethnicity: '120', ethnic_background: 'Black or Black British - African' }
+  it_behaves_like 'convert ethnicity field', { recruitment_cycle_year: 2022, hesa_ethnicity: '29', expected_hesa_ethnicity: '139', ethnic_background: 'Other Black background' }
+  it_behaves_like 'convert ethnicity field', { recruitment_cycle_year: 2022, hesa_ethnicity: '31', expected_hesa_ethnicity: '103', ethnic_background: 'Asian or Asian British - Indian' }
+  it_behaves_like 'convert ethnicity field', { recruitment_cycle_year: 2022, hesa_ethnicity: '32', expected_hesa_ethnicity: '104', ethnic_background: 'Asian or Asian British - Pakistani' }
+  it_behaves_like 'convert ethnicity field', { recruitment_cycle_year: 2022, hesa_ethnicity: '33', expected_hesa_ethnicity: '100', ethnic_background: 'Asian or Asian British - Bangladeshi' }
+  it_behaves_like 'convert ethnicity field', { recruitment_cycle_year: 2022, hesa_ethnicity: '34', expected_hesa_ethnicity: '101', ethnic_background: 'Chinese' }
+  it_behaves_like 'convert ethnicity field', { recruitment_cycle_year: 2022, hesa_ethnicity: '39', expected_hesa_ethnicity: '119', ethnic_background: 'Other Asian background' }
+  it_behaves_like 'convert ethnicity field', { recruitment_cycle_year: 2022, hesa_ethnicity: '41', expected_hesa_ethnicity: '142', ethnic_background: 'Mixed - White and Black Caribbean' }
+  it_behaves_like 'convert ethnicity field', { recruitment_cycle_year: 2022, hesa_ethnicity: '42', expected_hesa_ethnicity: '141', ethnic_background: 'Mixed - White and Black African' }
+  it_behaves_like 'convert ethnicity field', { recruitment_cycle_year: 2022, hesa_ethnicity: '43', expected_hesa_ethnicity: '140', ethnic_background: 'Mixed - White and Asian' }
+  it_behaves_like 'convert ethnicity field', { recruitment_cycle_year: 2022, hesa_ethnicity: '49', expected_hesa_ethnicity: '159', ethnic_background: 'Other Mixed background' }
+  it_behaves_like 'convert ethnicity field', { recruitment_cycle_year: 2022, hesa_ethnicity: '50', expected_hesa_ethnicity: '180', ethnic_background: 'Arab' }
+  it_behaves_like 'convert ethnicity field', { recruitment_cycle_year: 2022, hesa_ethnicity: '80', expected_hesa_ethnicity: '899', ethnic_background: 'Other Ethnic background' }
+  it_behaves_like 'convert ethnicity field', { recruitment_cycle_year: 2022, hesa_ethnicity: '90', expected_hesa_ethnicity: '997', ethnic_background: 'Not known' }
+  it_behaves_like 'convert ethnicity field', { recruitment_cycle_year: 2022, hesa_ethnicity: '98', expected_hesa_ethnicity: '998', ethnic_background: 'Prefer not to say' }
+  it_behaves_like 'convert ethnicity field', { recruitment_cycle_year: 2023, hesa_ethnicity: nil, ethnic_background: nil, expected_hesa_ethnicity: nil }
+
+  # New ethnicity was added in 2023 cycle
+  it_behaves_like 'convert ethnicity field', { recruitment_cycle_year: 2023, hesa_ethnicity: '168', expected_hesa_ethnicity: '168', ethnic_background: 'Roma' }
+
+  HesaEthnicityCollections::HESA_ETHNICITIES_2023_2024.to_h.each do |hesa_code, ethnic_background|
+    it_behaves_like 'convert ethnicity field', { recruitment_cycle_year: 2023, hesa_ethnicity: hesa_code, expected_hesa_ethnicity: hesa_code, ethnic_background: ethnic_background }
+  end
 end
