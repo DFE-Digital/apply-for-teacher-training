ef2325b4469ba3f08f2e48b816c562f73d2b4e2e
Avin Hurry <avinhurry@icloud.com>
2024-03-21 00:44:22 +0000

    Address PR comments and reword test methods
    https://github.com/DFE-Digital/apply-for-teacher-training/pull/9222#discussion_r1532485405
    
    https://github.com/DFE-Digital/apply-for-teacher-training/pull/9222#discussion_r1532485926
    
    https://github.com/DFE-Digital/apply-for-teacher-training/pull/9222#discussion_r1532493234

--- spec/system/candidate_interface/continuous_applications/course_selection/candidate_changes_course_and_reaches_limit_spec.rb
+++ spec/system/candidate_interface/continuous_applications/course_selection/candidate_changes_course_and_reaches_limit_spec.rb
@@ -6,16 +6,16 @@ RSpec.feature 'Changing a course' do
   it 'Candidate changes course to one they have already been rejected from twice' do
     given_i_am_signed_in
     and_there_are_course_options
-    and_i_have_two_rejected_application_to_a_course
+    and_i_have_two_rejected_applications_to_a_course
 
-    when_i_visit_the_site
-    and_i_click_on_course_choices
+    when_i_visit_my_details_page
+    and_i_add_another_application
     and_i_choose_that_i_know_where_i_want_to_apply
     and_i_choose_the_same_provider
     then_i_see_the_courses_and_their_descriptions
 
     when_i_choose_a_different_course
-    and_i_return_to_the_course_selection_for_application
+    and_i_return_to_the_course_selection_page
     and_i_choose_the_rejected_course
     then_i_am_on_the_reached_reapplication_limit_page
   end
@@ -33,7 +33,7 @@ RSpec.feature 'Changing a course' do
     create(:course_option, course: @another_course)
   end
 
-  def and_i_have_two_rejected_application_to_a_course
+  def and_i_have_two_rejected_applications_to_a_course
     create(:application_choice, :rejected, course_option: @course.course_options.first, application_form: @candidate.current_application)
     create(:application_choice, :rejected, course_option: @course.course_options.first, application_form: @candidate.current_application)
   end
@@ -43,7 +43,7 @@ RSpec.feature 'Changing a course' do
     click_link_or_button t('continue')
   end
 
-  def and_i_click_on_course_choices
+  def and_i_add_another_application
     click_link_or_button 'Your application'
     click_link_or_button 'Add application'
   end
@@ -65,7 +65,7 @@ RSpec.feature 'Changing a course' do
     click_link_or_button t('continue')
   end
 
-  def and_i_return_to_the_course_selection_for_application
+  def and_i_return_to_the_course_selection_page
     click_on 'Change'
   end
 
@@ -78,7 +78,7 @@ RSpec.feature 'Changing a course' do
     expect(page.current_url).to end_with(candidate_interface_continuous_applications_reached_reapplication_limit_path(provider_id: @provider.id, course_id: @course.id))
   end
 
-  def when_i_visit_the_site
+  def when_i_visit_my_details_page
     visit candidate_interface_continuous_applications_details_path
   end
 end
