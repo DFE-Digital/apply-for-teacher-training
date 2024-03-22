28118338a0b628c366b12f408a66cdb5081a0ada
Tomas D'Stefano <tomas_stefano@successoft.com>
2024-03-22 16:26:57 +0000

    Break the migration into contexts

--- spec/services/data_migrations/incorrect_equality_and_diversity_migration_spec.rb
+++ spec/services/data_migrations/incorrect_equality_and_diversity_migration_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe DataMigrations::IncorrectEqualityAndDiversityMigration do
         @male = create(:application_form, equality_and_diversity: { hesa_sex: '1', sex: 'male' })
         @female = create(:application_form, equality_and_diversity: { hesa_sex: '2', sex: 'female' })
         @intersex = create(:application_form, equality_and_diversity: { hesa_sex: '3', sex: 'intersex' })
-        #        @prefer_not_to_say = create(:application_form, equality_and_diversity: { hesa_sex: nil, sex: 'Prefer not to say'})
+        @prefer_not_to_say = create(:application_form, equality_and_diversity: { hesa_sex: nil, sex: 'Prefer not to say' })
       end
 
       it 'returns the expected records' do
@@ -20,7 +20,7 @@ RSpec.describe DataMigrations::IncorrectEqualityAndDiversityMigration do
         expect(@female.reload.equality_and_diversity['hesa_sex']).to eq('10')
         expect(@male.reload.equality_and_diversity['hesa_sex']).to eq('11')
         expect(@intersex.reload.equality_and_diversity['hesa_sex']).to eq('12')
-        #        expect(@prefer_not_to_say.reload.equality_and_diversity['hesa_sex']).to eq(nil)
+        expect(@prefer_not_to_say.reload.equality_and_diversity['hesa_sex']).to be_nil
       end
     end
 
@@ -145,81 +145,105 @@ RSpec.describe DataMigrations::IncorrectEqualityAndDiversityMigration do
     end
 
     context 'when outdated hesa ethnicity' do
-      before do
-        @white = create(:application_form, equality_and_diversity: { ethnic_background: 'White', hesa_ethnicity: '10' })
-        # Some free text values in DB
-        @american = create(:application_form, equality_and_diversity: { ethnic_background: 'American', hesa_ethnicity: '10' })
-        @albaninan = create(:application_form, equality_and_diversity: { ethnic_background: 'Albaninan', hesa_ethnicity: '10' })
-        @another_white_background = create(:application_form, equality_and_diversity: { ethnic_background: 'Albaninan', hesa_ethnicity: '10' })
-        @uk_white = create(:application_form, equality_and_diversity: { ethnic_background: 'British, English, Northern Irish, Scottish, or Welsh', hesa_ethnicity: '10' })
+      context 'when white ethnicity old HESA values' do
+        it 'migrates to the most up to date value' do
+          white = create(:application_form, equality_and_diversity: { ethnic_background: 'White', hesa_ethnicity: '10' })
+          american = create(:application_form, equality_and_diversity: { ethnic_background: 'American', hesa_ethnicity: '10' })
+          albaninan = create(:application_form, equality_and_diversity: { ethnic_background: 'Albaninan', hesa_ethnicity: '10' })
+          another_white_background = create(:application_form, equality_and_diversity: { ethnic_background: 'Albaninan', hesa_ethnicity: '10' })
+          uk_white = create(:application_form, equality_and_diversity: { ethnic_background: 'British, English, Northern Irish, Scottish, or Welsh', hesa_ethnicity: '10' })
 
-        @gypsy_or_traveler = create(:application_form, equality_and_diversity: { ethnic_background: 'Gypsy or Traveller', hesa_ethnicity: '15' })
-        @black_british_caribbean = create(:application_form, equality_and_diversity: { ethnic_background: 'Black or Black British - Caribbean', hesa_ethnicity: '21' })
-        @black_british_african = create(:application_form, equality_and_diversity: { ethnic_background: 'Black or Black British - African', hesa_ethnicity: '22' })
-        # More free text values in DB
-        @black_british_african_free_text = create(:application_form, equality_and_diversity: { ethnic_background: 'African', hesa_ethnicity: '22' })
-        @other_black_background = create(:application_form, equality_and_diversity: { ethnic_background: 'Other Black background', hesa_ethnicity: '29' })
-        @another_black_background = create(:application_form, equality_and_diversity: { ethnic_background: 'Another Black background', hesa_ethnicity: '29' })
-        @asian_british_indian = create(:application_form, equality_and_diversity: { ethnic_background: 'Asian or Asian British - Indian', hesa_ethnicity: '31' })
-        # More free text values in DB
-        @asian_british_indian_free_text = create(:application_form, equality_and_diversity: { ethnic_background: 'Indian', hesa_ethnicity: '31' })
-        @asian_british_pakistani = create(:application_form, equality_and_diversity: { ethnic_background: 'Asian or Asian British - Pakistani', hesa_ethnicity: '32' })
-        @asian_british_bangladeshi = create(:application_form, equality_and_diversity: { ethnic_background: 'Asian or Asian British - Bangladeshi', hesa_ethnicity: '33' })
-        @chinese = create(:application_form, equality_and_diversity: { ethnic_background: 'Chinese', hesa_ethnicity: '34' })
-        @other_asian_background = create(:application_form, equality_and_diversity: { ethnic_background: 'Other Asian background', hesa_ethnicity: '39' })
-        @another_asian_background = create(:application_form, equality_and_diversity: { ethnic_background: 'Another Asian background', hesa_ethnicity: '39' })
-        @mixed_white_black_caribbean = create(:application_form, equality_and_diversity: { ethnic_background: 'Mixed - White and Black Caribbean', hesa_ethnicity: '41' })
-        # More free text values in DB
-        @mixed_white_black_caribbean_free_text = create(:application_form, equality_and_diversity: { ethnic_background: 'Black Caribbean and White', hesa_ethnicity: '41' })
-        @mixed_white_black_african = create(:application_form, equality_and_diversity: { ethnic_background: 'Mixed - White and Black African', hesa_ethnicity: '42' })
-        @mixed_white_asian = create(:application_form, equality_and_diversity: { ethnic_background: 'Mixed - White and Asian', hesa_ethnicity: '43' })
-        # More free text values in DB
-        @mixed_white_asian_free_text = create(:application_form, equality_and_diversity: { ethnic_background: 'Asian and White', hesa_ethnicity: '43' })
-        @other_mixed_background = create(:application_form, equality_and_diversity: { ethnic_background: 'Other Mixed background', hesa_ethnicity: '49' })
-        @another_mixed_background = create(:application_form, equality_and_diversity: { ethnic_background: 'Another Mixed background', hesa_ethnicity: '49' })
-        @arab = create(:application_form, equality_and_diversity: { ethnic_background: 'Arab', hesa_ethnicity: '50' })
-        @other_ethnic_background = create(:application_form, equality_and_diversity: { ethnic_background: 'Other Ethnic background', hesa_ethnicity: '80' })
-        @prefer_not_to_say = create(:application_form, equality_and_diversity: { ethnic_background: 'Prefer not to say', hesa_ethnicity: '98' })
+          described_class.new.change
+
+          expect(white.reload.equality_and_diversity['hesa_ethnicity']).to eq('160')
+          expect(american.reload.equality_and_diversity['hesa_ethnicity']).to eq('160')
+          expect(american.reload.equality_and_diversity['ethnic_background']).to eq('American')
+          expect(albaninan.reload.equality_and_diversity['hesa_ethnicity']).to eq('160')
+          expect(another_white_background.reload.equality_and_diversity['hesa_ethnicity']).to eq('160')
+          expect(uk_white.reload.equality_and_diversity['hesa_ethnicity']).to eq('160')
+        end
       end
 
-      it 'converts to the uptodate hesa values' do
-        described_class.new.change
+      context 'when black ethnicity old HESA values' do
+        it 'migrates to the most up to date value' do
+          black_british_caribbean = create(:application_form, equality_and_diversity: { ethnic_background: 'Black or Black British - Caribbean', hesa_ethnicity: '21' })
+          black_british_african = create(:application_form, equality_and_diversity: { ethnic_background: 'Black or Black British - African', hesa_ethnicity: '22' })
+          black_british_african_free_text = create(:application_form, equality_and_diversity: { ethnic_background: 'African', hesa_ethnicity: '22' })
+          other_black_background = create(:application_form, equality_and_diversity: { ethnic_background: 'Other Black background', hesa_ethnicity: '29' })
+          another_black_background = create(:application_form, equality_and_diversity: { ethnic_background: 'Another Black background', hesa_ethnicity: '29' })
 
-        expect(@white.reload.equality_and_diversity['hesa_ethnicity']).to eq('160')
-        expect(@american.reload.equality_and_diversity['hesa_ethnicity']).to eq('160')
-        expect(@american.reload.equality_and_diversity['ethnic_background']).to eq('American')
-        expect(@albaninan.reload.equality_and_diversity['hesa_ethnicity']).to eq('160')
-        expect(@another_white_background.reload.equality_and_diversity['hesa_ethnicity']).to eq('160')
-        expect(@uk_white.reload.equality_and_diversity['hesa_ethnicity']).to eq('160')
+          described_class.new.change
 
-        expect(@gypsy_or_traveler.reload.equality_and_diversity['hesa_ethnicity']).to eq('163')
-        expect(@black_british_caribbean.reload.equality_and_diversity['hesa_ethnicity']).to eq('121')
-        expect(@black_british_african.reload.equality_and_diversity['hesa_ethnicity']).to eq('120')
-        expect(@black_british_african_free_text.reload.equality_and_diversity['hesa_ethnicity']).to eq('120')
+          expect(black_british_caribbean.reload.equality_and_diversity['hesa_ethnicity']).to eq('121')
+          expect(black_british_african.reload.equality_and_diversity['hesa_ethnicity']).to eq('120')
+          expect(black_british_african_free_text.reload.equality_and_diversity['hesa_ethnicity']).to eq('120')
+          expect(other_black_background.reload.equality_and_diversity['hesa_ethnicity']).to eq('139')
+          expect(another_black_background.reload.equality_and_diversity['hesa_ethnicity']).to eq('139')
+        end
+      end
 
-        expect(@other_black_background.reload.equality_and_diversity['hesa_ethnicity']).to eq('139')
-        expect(@another_black_background.reload.equality_and_diversity['hesa_ethnicity']).to eq('139')
+      context 'when asian ethnicity old HESA values' do
+        it 'migrates to the most up to date value' do
+          asian_british_indian = create(:application_form, equality_and_diversity: { ethnic_background: 'Asian or Asian British - Indian', hesa_ethnicity: '31' })
+          asian_british_indian_free_text = create(:application_form, equality_and_diversity: { ethnic_background: 'Indian', hesa_ethnicity: '31' })
+          asian_british_pakistani = create(:application_form, equality_and_diversity: { ethnic_background: 'Asian or Asian British - Pakistani', hesa_ethnicity: '32' })
+          asian_british_bangladeshi = create(:application_form, equality_and_diversity: { ethnic_background: 'Asian or Asian British - Bangladeshi', hesa_ethnicity: '33' })
+          chinese = create(:application_form, equality_and_diversity: { ethnic_background: 'Chinese', hesa_ethnicity: '34' })
+          other_asian_background = create(:application_form, equality_and_diversity: { ethnic_background: 'Other Asian background', hesa_ethnicity: '39' })
+          another_asian_background = create(:application_form, equality_and_diversity: { ethnic_background: 'Another Asian background', hesa_ethnicity: '39' })
 
-        expect(@asian_british_indian.reload.equality_and_diversity['hesa_ethnicity']).to eq('103')
-        expect(@asian_british_indian_free_text.reload.equality_and_diversity['hesa_ethnicity']).to eq('103')
-        expect(@asian_british_pakistani.reload.equality_and_diversity['hesa_ethnicity']).to eq('104')
-        expect(@asian_british_bangladeshi.reload.equality_and_diversity['hesa_ethnicity']).to eq('100')
+          described_class.new.change
 
-        expect(@chinese.reload.equality_and_diversity['hesa_ethnicity']).to eq('101')
-        expect(@other_asian_background.reload.equality_and_diversity['hesa_ethnicity']).to eq('119')
-        expect(@another_asian_background.reload.equality_and_diversity['hesa_ethnicity']).to eq('119')
-        expect(@mixed_white_black_caribbean.reload.equality_and_diversity['hesa_ethnicity']).to eq('142')
-        expect(@mixed_white_black_caribbean_free_text.reload.equality_and_diversity['hesa_ethnicity']).to eq('142')
+          expect(asian_british_indian.reload.equality_and_diversity['hesa_ethnicity']).to eq('103')
+          expect(asian_british_indian_free_text.reload.equality_and_diversity['hesa_ethnicity']).to eq('103')
+          expect(asian_british_pakistani.reload.equality_and_diversity['hesa_ethnicity']).to eq('104')
+          expect(asian_british_bangladeshi.reload.equality_and_diversity['hesa_ethnicity']).to eq('100')
+          expect(chinese.reload.equality_and_diversity['hesa_ethnicity']).to eq('101')
+          expect(other_asian_background.reload.equality_and_diversity['hesa_ethnicity']).to eq('119')
+          expect(another_asian_background.reload.equality_and_diversity['hesa_ethnicity']).to eq('119')
+        end
+      end
 
-        expect(@mixed_white_black_african.reload.equality_and_diversity['hesa_ethnicity']).to eq('141')
-        expect(@mixed_white_asian.reload.equality_and_diversity['hesa_ethnicity']).to eq('140')
-        expect(@mixed_white_asian_free_text.reload.equality_and_diversity['hesa_ethnicity']).to eq('140')
+      context 'when mixed or other ethnicities' do
+        it 'converts to the uptodate hesa values' do
+          gypsy_or_traveler = create(:application_form, equality_and_diversity: { ethnic_background: 'Gypsy or Traveller', hesa_ethnicity: '15' })
+          arab = create(:application_form, equality_and_diversity: { ethnic_background: 'Arab', hesa_ethnicity: '50' })
+          other_ethnic_background = create(:application_form, equality_and_diversity: { ethnic_background: 'Other Ethnic background', hesa_ethnicity: '80' })
+          mixed_white_black_caribbean = create(:application_form, equality_and_diversity: { ethnic_background: 'Mixed - White and Black Caribbean', hesa_ethnicity: '41' })
+          mixed_white_black_caribbean_free_text = create(:application_form, equality_and_diversity: { ethnic_background: 'Black Caribbean and White', hesa_ethnicity: '41' })
+          mixed_white_black_african = create(:application_form, equality_and_diversity: { ethnic_background: 'Mixed - White and Black African', hesa_ethnicity: '42' })
+          mixed_white_asian = create(:application_form, equality_and_diversity: { ethnic_background: 'Mixed - White and Asian', hesa_ethnicity: '43' })
+          mixed_white_asian_free_text = create(:application_form, equality_and_diversity: { ethnic_background: 'Asian and White', hesa_ethnicity: '43' })
+          other_mixed_background = create(:application_form, equality_and_diversity: { ethnic_background: 'Other Mixed background', hesa_ethnicity: '49' })
+          another_mixed_background = create(:application_form, equality_and_diversity: { ethnic_background: 'Another Mixed background', hesa_ethnicity: '49' })
+          latin = create(:application_form, equality_and_diversity: { ethnic_background: 'Latin American', hesa_ethnicity: '80' })
 
-        expect(@other_mixed_background.reload.equality_and_diversity['hesa_ethnicity']).to eq('159')
-        expect(@another_mixed_background.reload.equality_and_diversity['hesa_ethnicity']).to eq('159')
-        expect(@arab.reload.equality_and_diversity['hesa_ethnicity']).to eq('180')
-        expect(@other_ethnic_background.reload.equality_and_diversity['hesa_ethnicity']).to eq('899')
-        expect(@prefer_not_to_say.reload.equality_and_diversity['hesa_ethnicity']).to eq('998')
+          described_class.new.change
+
+          expect(gypsy_or_traveler.reload.equality_and_diversity['hesa_ethnicity']).to eq('163')
+          expect(mixed_white_black_caribbean.reload.equality_and_diversity['hesa_ethnicity']).to eq('142')
+          expect(mixed_white_black_caribbean_free_text.reload.equality_and_diversity['hesa_ethnicity']).to eq('142')
+
+          expect(mixed_white_black_african.reload.equality_and_diversity['hesa_ethnicity']).to eq('141')
+          expect(mixed_white_asian.reload.equality_and_diversity['hesa_ethnicity']).to eq('140')
+          expect(mixed_white_asian_free_text.reload.equality_and_diversity['hesa_ethnicity']).to eq('140')
+
+          expect(other_mixed_background.reload.equality_and_diversity['hesa_ethnicity']).to eq('159')
+          expect(another_mixed_background.reload.equality_and_diversity['hesa_ethnicity']).to eq('159')
+          expect(arab.reload.equality_and_diversity['hesa_ethnicity']).to eq('180')
+          expect(other_ethnic_background.reload.equality_and_diversity['hesa_ethnicity']).to eq('899')
+          expect(latin.reload.equality_and_diversity['hesa_ethnicity']).to eq('899')
+        end
+      end
+
+      context 'when prefer not to say ethnicities' do
+        it 'converts to the uptodate hesa values' do
+          prefer_not_to_say = create(:application_form, equality_and_diversity: { ethnic_background: 'Prefer not to say', hesa_ethnicity: '98' })
+
+          described_class.new.change
+
+          expect(prefer_not_to_say.reload.equality_and_diversity['hesa_ethnicity']).to eq('998')
+        end
       end
     end
   end
