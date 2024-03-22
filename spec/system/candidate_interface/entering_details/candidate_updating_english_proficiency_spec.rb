9e87556d6d62385cba4a4808d3de343f8db9b98f
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2024-03-22 09:48:56 +0000

    Initialise english_proficiency if doesn't exist on edit

---
+++ spec/system/candidate_interface/entering_details/candidate_updating_english_proficiency_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.feature 'Candidate updating english proficiency' do
+  include CandidateHelper
+
+  scenario 'When application form does not have an english proficiency associations' do
+    given_i_am_signed_in_as_a_candidate
+    and_i_do_not_have_an_english_proficiency_attached_to_my_application_form
+    when_i_visit_the_english_proficiency_edit_path
+    then_i_am_able_to_create_an_english_proficiency_record
+  end
+
+  def given_i_am_signed_in_as_a_candidate
+    create_and_sign_in_candidate
+  end
+
+  def and_i_do_not_have_an_english_proficiency_attached_to_my_application_form
+    create(:completed_application_form, candidate: current_candidate)
+    expect(current_candidate.current_application.english_proficiency).to be_nil
+  end
+
+  def when_i_visit_the_english_proficiency_edit_path
+    visit candidate_interface_english_foreign_language_edit_start_path(current_candidate)
+  end
+
+  def then_i_am_able_to_create_an_english_proficiency_record
+    choose 'Yes'
+    complete_efl_form_with_ielts_details
+    expect(current_candidate.current_application.english_proficiency).not_to be_nil
+  end
+
+  def complete_efl_form_with_ielts_details
+    choose 'Yes'
+    click_on 'Continue'
+    choose 'International English Language Testing System (IELTS)'
+    click_on 'Continue'
+    fill_in 'Test report form (TRF) number', with: '02GB0674SOOM599A'
+    fill_in 'Overall band score', with: '7.5'
+    fill_in 'When did you complete the assessment?', with: '2020'
+    click_on 'Save and continue'
+    choose 'Yes, I have completed this section'
+    click_on 'Continue'
+  end
+end
