fe14a689dabab6d1afeab9d28d1aacf5b25fd39b
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-03-27 11:16:50 +0000

    Remove open! from course model

--- spec/factory_specs/course_factory_spec.rb
+++ spec/factory_specs/course_factory_spec.rb
@@ -41,13 +41,6 @@ RSpec.describe 'Course factory' do
       field :exposed_in_find?, value: false
     end
 
-    trait :open_on_apply do
-      field :open_on_apply?, value: true
-      field :exposed_in_find?, value: true
-      field :opened_on_apply_at, presence: true
-      field :applications_open_from, presence: true
-    end
-
     trait :with_accredited_provider do
       field :accredited_provider, presence: true
 
@@ -73,7 +66,6 @@ RSpec.describe 'Course factory' do
     end
 
     trait :ucas_only do
-      field :open_on_apply?, value: false
       field :exposed_in_find?, value: true
     end
 
