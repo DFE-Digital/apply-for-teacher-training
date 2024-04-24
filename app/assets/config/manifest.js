1d4749651de942cf6ff1df454647da6cfaf4d7af
David Young <david@dcyoung.dev>
2024-04-24 09:28:15 +0100

    Fonts and Images loaded from `govuk-frontend`

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,6 @@
 // This is just here for Blazer.
 //= link_tree ../builds
+
+// govuk-frontend assets
+//= link_directory ../../../node_modules/govuk-frontend/dist/govuk/assets/images
+//= link_directory ../../../node_modules/govuk-frontend/dist/govuk/assets/fonts
