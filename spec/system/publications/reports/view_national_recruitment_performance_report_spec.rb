9120ba23e58352afe22a0043460026427e822c6f
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2024-04-24 10:46:12 +0100

    Add redirect from national report when feature flag is inactive

--- spec/system/publications/reports/view_national_recruitment_performance_report_spec.rb
+++ spec/system/publications/reports/view_national_recruitment_performance_report_spec.rb
@@ -1,17 +1,29 @@
 require 'rails_helper'
 
 RSpec.feature 'Visit national recruitment performance report page' do
-  before { FeatureFlag.activate(:recruitment_performance_report) }
+  describe 'Feature flag is active' do
+    before { FeatureFlag.activate(:recruitment_performance_report) }
 
-  scenario 'national report has been generated' do
-    given_a_national_recruitment_performance_report_has_been_generated
-    and_i_visit_the_national_recruitment_report_page
-    then_i_see_the_report
+    scenario 'national report has been generated' do
+      given_a_national_recruitment_performance_report_has_been_generated
+      and_i_visit_the_national_recruitment_report_page
+      then_i_see_the_report
+    end
+
+    scenario 'national report has not been generated' do
+      given_i_visit_the_national_recruitment_report_page
+      then_i_see_no_report_message
+    end
   end
 
-  scenario 'national report has not been generated' do
-    given_i_visit_the_national_recruitment_report_page
-    then_i_see_no_report_message
+  describe 'Feature flag is inactive' do
+    before { FeatureFlag.deactivate(:recruitment_performance_report) }
+
+    scenario 'national report has been generated' do
+      given_a_national_recruitment_performance_report_has_been_generated
+      and_i_visit_the_national_recruitment_report_page
+      then_i_am_redirected_to_mid_cycle_report
+    end
   end
 
 private
@@ -33,5 +45,9 @@ private
   def then_i_see_no_report_message
     expect(page).to have_content('National recruitment performance weekly report 2023 to 2024')
     expect(page).to have_content('This report is not ready to view.')
+  end
+
+  def then_i_am_redirected_to_mid_cycle_report
+    expect(page).to have_current_path publications_mid_cycle_report_path
   end
 end
