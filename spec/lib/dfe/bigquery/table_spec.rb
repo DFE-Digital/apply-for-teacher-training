036e3ef45c3304f1d0a10fdc8ca307dc49a6ae57
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-04-26 11:16:24 +0100

    Add #select to DfE::Bigquery::Table

--- spec/lib/dfe/bigquery/table_spec.rb
+++ spec/lib/dfe/bigquery/table_spec.rb
@@ -4,6 +4,45 @@ RSpec.describe DfE::Bigquery::Table do
   subject(:table) { described_class.new(name: 'datapoint.magic_tricks') }
 
   describe '#to_sql' do
+    context 'when using select string' do
+      it 'returns select with exact string' do
+        expect(
+          table.select('one, two').to_sql,
+        ).to eq(
+          <<~SQL,
+            SELECT one, two
+            FROM datapoint.magic_tricks
+          SQL
+        )
+      end
+    end
+
+    context 'when using select array' do
+      it 'returns select with specified columns' do
+        expect(
+          table.select(%w[one two]).to_sql,
+        ).to eq(
+          <<~SQL,
+            SELECT one, two
+            FROM datapoint.magic_tricks
+          SQL
+        )
+      end
+    end
+
+    context 'when using no select' do
+      it 'returns select with star' do
+        expect(
+          table.to_sql,
+        ).to eq(
+          <<~SQL,
+            SELECT *
+            FROM datapoint.magic_tricks
+          SQL
+        )
+      end
+    end
+
     context 'when using where' do
       it 'returns where conditions using "AND"' do
         expect(
