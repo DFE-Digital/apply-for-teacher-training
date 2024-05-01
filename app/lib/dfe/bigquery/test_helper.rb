c4a13939d082c77624b44921223d15b329a58a56
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-05-01 16:23:15 +0100

    Add stubs for application_metrics_by_provider

--- app/lib/dfe/bigquery/test_helper.rb
+++ app/lib/dfe/bigquery/test_helper.rb
@@ -12,6 +12,17 @@ module DfE
         end
       end
 
+      def stub_bigquery_application_metrics_by_provider_request(stub_results = [])
+        bigquery_client = instance_double(Google::Cloud::Bigquery::Project)
+        allow(DfE::Bigquery).to receive(:client).and_return(bigquery_client)
+
+        if stub_results.present?
+          allow(bigquery_client).to receive(:query).and_return(stub_results)
+        else
+          allow(bigquery_client).to receive(:query).and_return(application_metrics_results)
+        end
+      end
+
       def application_metrics_results(options = {})
         [
           {
@@ -35,6 +46,37 @@ module DfE
             number_of_candidates_with_reconfirmed_offers_deferred_from_previous_cycle_to_same_date_previous_cycle: 213,
             number_of_candidates_accepted_to_date: 20,
             number_of_candidates_accepted_to_same_date_previous_cycle: 10,
+          }.merge(options.slice(:attributes)),
+        ]
+      end
+
+      def application_metrics_by_provider_results(options = {})
+        [
+          {
+            id: 1337,
+            cycle_week: 18,
+            recruitment_cycle_year: 2024,
+            nonprovider_filter: 'Level',
+            nonprovider_filter_category: 'Primary',
+            number_of_candidates_submitted_to_date: 100,
+            number_of_candidates_submitted_to_same_date_previous_cycle: 200,
+            number_of_candidates_submitted_to_date_as_proportion_of_last_cycle: 0.5,
+            number_of_candidates_with_offers_to_date: 10,
+            number_of_candidates_with_offers_to_same_date_previous_cycle: 5,
+            number_of_candidates_with_offers_to_date_as_proportion_of_last_cycle: 2.0,
+            offer_rate_to_date: 1.2,
+            offer_rate_to_same_date_previous_cycle: 1.5,
+            number_of_candidates_accepted_to_date: 1,
+            number_of_candidates_accepted_to_same_date_previous_cycle: 10,
+            number_of_candidates_accepted_to_date_as_proportion_of_last_cycle: 0.1,
+            number_of_candidates_with_reconfirmed_offers_deferred_from_previous_cycle_to_date: 12,
+            number_of_candidates_with_reconfirmed_offers_deferred_from_previous_cycle_to_same_date_previous_cycle: 12,
+            number_of_candidates_with_reconfirmed_offers_deferred_from_previous_cycle_to_date_as_proportion_of_last_cycle: 0,
+            number_of_candidates_who_had_all_applications_rejected_this_cycle_to_date: 12,
+            number_of_candidates_who_had_all_applications_rejected_this_cycle_to_same_date_previous_cycle: 12,
+            number_of_candidates_who_had_all_applications_rejected_this_cycle_to_date_as_proportion_of_last_cycle: 0,
+            number_of_candidates_who_had_an_inactive_application_this_cycle_to_date: 12,
+            number_of_candidates_who_had_an_inactive_application_this_cycle_to_date_as_proportion_of_submitted_candidates: 12,
           }.merge(options.slice(:attributes)),
         ]
       end
