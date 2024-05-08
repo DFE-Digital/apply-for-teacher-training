026bc6c19b9c6094930795e1295539591e052cd3
Tomas D'Stefano <tomas_stefano@successoft.com>
2024-05-08 15:34:02 +0100

    If the candidate doesn't have A-levels on TDA validates it

---
+++ app/validators/incomplete_tda_course_details_validator.rb
@@ -0,0 +1,26 @@
+class IncompleteTdaCourseDetailsValidator < ActiveModel::EachValidator
+  include ActionView::Helpers::UrlHelper
+  include GovukLinkHelper
+  include GovukVisuallyHiddenHelper
+
+  def validate_each(record, attribute, application_choice)
+    return unless application_choice.degree_apprenticeship?
+
+    application_form = application_choice.application_form
+    course_level = application_choice.current_course.level
+
+    if application_form.application_qualifications.a_levels.blank?
+      record.errors.add(
+        attribute,
+        :incomplete_details_a_levels,
+        link_to_details:,
+        course_level:,
+        course_subject: '[subject]',
+      )
+    end
+  end
+
+  def link_to_details
+    govuk_link_to('Add your A level grade (or equivalent)', Rails.application.routes.url_helpers.candidate_interface_other_qualification_type_path)
+  end
+end
