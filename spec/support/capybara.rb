0154aaa06d93a7a649d42850bd52e83799043bcf
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-05-16 16:55:19 +0100

    Register Selenium driver for chrome (browser)
      We now have three options in the metadata for a system spec
      - js: false (or absent :js) => rack_test
      - :js => :chrome_headless
      - :js_browser => :chrome

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -5,27 +5,33 @@ if ENV['TEST_ENV_NUMBER']
   Capybara.server_port = 9887 + ENV['TEST_ENV_NUMBER'].to_i
 end
 
-Capybara.register_driver :chrome_headless do |app|
-  options = Selenium::WebDriver::Chrome::Options.new
+options = Selenium::WebDriver::Chrome::Options.new.tap do |opts|
+  opts.add_argument('--no-sandbox')
+  opts.add_argument('--disable-dev-shm-usage')
+  opts.add_argument('--disable-gpu')
+  opts.add_argument('--window-size=1400,1400')
+end
 
-  options.add_argument('--headless') unless ENV['HEADLESS'] == 'false'
-  options.add_argument('--no-sandbox')
-  options.add_argument('--disable-dev-shm-usage')
-  options.add_argument('--disable-gpu')
-  options.add_argument('--window-size=1400,1400')
-
+Capybara.register_driver :chrome do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome, options:)
 end
 
-Capybara.javascript_driver = :chrome_headless
+Capybara.register_driver :chrome_headless do |app|
+  options.add_argument('--headless')
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options:)
+end
 
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    if self.class.metadata[:js] == true
-      driven_by(:chrome_headless)
-    else
-      driven_by(:rack_test)
-    end
+    driven_by(:rack_test)
+  end
+
+  config.before(:each, :js, type: :system) do
+    driven_by(:chrome_headless)
+  end
+
+  config.before(:each, :js_browser, type: :system) do
+    driven_by(:chrome)
   end
 
   config.before(:each, :smoke) do
