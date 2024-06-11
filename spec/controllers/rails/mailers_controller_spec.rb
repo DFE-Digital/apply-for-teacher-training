03af87fe65d50e2540d9d1631919b0c002cb4ee8
CatalinVoineag <11318084+CatalinVoineag@users.noreply.github.com>
2024-06-11 12:20:55 +0100

    fix specs

--- spec/controllers/rails/mailers_controller_spec.rb
+++ spec/controllers/rails/mailers_controller_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 # These tests are designed to verify that DB changes from previews are rolled back
-RSpec.describe Rails::MailersController do
+RSpec.describe Rails::MailersController, type: :request do
   ActionMailer::Preview.all.each do |preview|
     preview.emails.each do |email|
       it "/rails/mailers/#{preview.preview_name}/#{email} does not pollute the database" do
-        expect { get(:preview, params: { path: "#{preview.preview_name}/#{email}" }) }.not_to(
+        expect { get('/rails/mailers', params: { path: "#{preview.preview_name}/#{email}" }) }.not_to(
           change { ApplicationForm.count },
         )
       end
