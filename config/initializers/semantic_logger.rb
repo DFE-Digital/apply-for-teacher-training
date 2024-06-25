a9f24d164d0e4eb3e87c3a02a877fbb360a3268f
Tomas D'Stefano <tomas_stefano@successoft.com>
2024-06-25 12:14:57 +0100

    Make sure action mailer don't log subject and to attributes

--- config/initializers/semantic_logger.rb
+++ config/initializers/semantic_logger.rb
@@ -6,7 +6,16 @@ class CustomLogFormatter < SemanticLogger::Formatters::Raw
   def call(log, logger)
     super
 
-    # Add custom fields
+    add_custom_fields
+    sanitize_payload_fields
+    remove_post_params
+
+    hash.to_json
+  end
+
+private
+
+  def add_custom_fields
     hash['domain'] = HostingEnvironment.hostname
     hash['environment'] = HostingEnvironment.environment_name
     hash['hosting_environment'] = HostingEnvironment.environment_name
@@ -17,26 +26,37 @@ class CustomLogFormatter < SemanticLogger::Formatters::Raw
     if (job_queue = Thread.current[:job_queue])
       hash['job_queue'] = job_queue
     end
+
     tid = Thread.current['sidekiq_tid']
     if tid.present?
       ctx = Sidekiq::Context.current
       hash['tid'] = tid
       hash['ctx'] = ctx
     end
-
-    if hash['payload'].present?
-      hash['payload'].reject! { |key, _| SANITIZED_REQUEST_PARAMS.map(&:to_s).include?(key) }
-    end
-
-    # Remove post parameters if it's a PUT, POST, or PATCH request
-    if method_is_post_or_put_or_patch? && hash.dig(:payload, :params).present?
-      hash[:payload][:params].clear
-    end
-
-    hash.to_json
   end
 
-private
+  def sanitize_payload_fields
+    if hash['payload'].present?
+      hash['payload'].reject! { |key, _| SANITIZED_REQUEST_PARAMS.map(&:to_s).include?(key) }
+      sanitize_mailer_subject_and_to
+    end
+  end
+
+  def sanitize_mailer_subject_and_to
+    if hash.dig(:payload, :subject).present?
+      hash[:payload][:subject] = '[REDACTED]'
+    end
+
+    if hash.dig(:payload, :to).present?
+      hash[:payload][:to] = '[REDACTED]'
+    end
+  end
+
+  def remove_post_params
+    return unless method_is_post_or_put_or_patch? && hash.dig(:payload, :params).present?
+
+    hash[:payload][:params].clear
+  end
 
   def method_is_post_or_put_or_patch?
     hash.dig(:payload, :method).in? %w[PUT POST PATCH]
