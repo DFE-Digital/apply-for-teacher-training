772a37d3b83830cfff97cdd6eb695cc2e97462d9
CatalinVoineag <11318084+CatalinVoineag@users.noreply.github.com>
2024-06-26 13:12:48 +0100

    removed files

--- app/controllers/candidate_interface/references/request_reference/start_controller.rb
+++
@@ -1,9 +0,0 @@
-module CandidateInterface
-  module References
-    class RequestReference::StartController < BaseController
-      include RequestReferenceOfferDashboard
-
-      def new; end
-    end
-  end
-end
