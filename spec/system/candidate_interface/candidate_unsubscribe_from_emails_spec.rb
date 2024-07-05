67a48fabc8055b885580def20553b12f9f3acc38
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2024-07-05 11:09:46 +0100

    Another spec for when multiple tokens exist

--- spec/system/candidate_interface/candidate_unsubscribe_from_emails_spec.rb
+++ spec/system/candidate_interface/candidate_unsubscribe_from_emails_spec.rb
@@ -23,6 +23,17 @@ RSpec.describe 'Candidate clicks unsubscribe link' do
     then_i_see_a_404
   end
 
+  scenario 'Candidate has multiple tokens generated' do
+    given_i_am_signed_in
+    and_multiple_tokens_have_been_generated
+    when_i_click_on_a_link_with_any_of_the_tokens
+    then_i_am_unsubscribed
+    and_i_see_unsubscribe_confirmation
+
+    given_i_click_on_another_link
+    then_i_see_unsubscribe_confirmation
+  end
+
 private
 
   def given_i_am_signed_in
@@ -32,6 +43,16 @@ private
   def given_i_am_not_signed_in
     current_candidate
   end
+
+  def and_multiple_tokens_have_been_generated
+    @tokens = (1..2).map { current_candidate.generate_token_for :unsubscribe_link }
+  end
+
+  def when_i_click_on_a_link_with_any_of_the_tokens
+    token = @tokens.sample
+    visit candidate_interface_unsubscribe_from_emails_path(token:)
+  end
+  alias_method :given_i_click_on_another_link, :when_i_click_on_a_link_with_any_of_the_tokens
 
   def and_i_visit_the_unsubscribe_link_with_valid_token
     token = current_candidate.generate_token_for :unsubscribe_link
@@ -50,7 +71,7 @@ private
   def and_i_see_unsubscribe_confirmation
     expect(page).to have_content 'You have unsubscribed from emails'
   end
-
+  alias_method :then_i_see_unsubscribe_confirmation, :and_i_see_unsubscribe_confirmation
   def and_i_am_on_application_details_page
     expect(page).to have_current_path candidate_interface_continuous_applications_details_path, ignore_query: true
   end
