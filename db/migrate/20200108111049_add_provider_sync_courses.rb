f75d193280d3316919779bcbe8abfbe2f2ae668e
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-07-11 13:13:56 +0100

    Reset column information in migration

--- db/migrate/20200108111049_add_provider_sync_courses.rb
+++ db/migrate/20200108111049_add_provider_sync_courses.rb
@@ -1,6 +1,8 @@
 class AddProviderSyncCourses < ActiveRecord::Migration[6.0]
   def change
     add_column :providers, :sync_courses, :boolean, null: false, default: false
+
+    Provider.reset_column_information
     Provider.find_each do |provider|
       provider.update(sync_courses: true) if provider.courses.count.positive?
     end
