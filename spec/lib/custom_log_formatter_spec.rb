0e0a44a4844eda3ab044887c6fd2ea40476fe918
Tomas D'Stefano <tomas_stefano@successoft.com>
2024-07-18 18:06:40 +0100

    Move semantic logger to your file
    So we can start to add BDD on it and see where is not working

---
+++ spec/lib/custom_log_formatter_spec.rb
@@ -0,0 +1,63 @@
+require 'rails_helper'
+
+RSpec.describe CustomLogFormatter do
+  subject(:log_hash) { JSON.parse(described_class.new.call(log, logger), symbolize_names: true) }
+
+  let(:log) { SemanticLogger::Log.new('Rack', :debug) }
+  let(:logger) do
+    SemanticLogger::Appender::File.new(
+      'test.log',
+      retry_count: 1,
+      append: true,
+      reopen_period: nil,
+      reopen_count: 0,
+      reopen_size: 0,
+      encoding: Encoding::BINARY,
+      exclusive_lock: false,
+    )
+  end
+
+  before do
+    allow(HostingEnvironment).to receive_messages(hostname: 'test_host', environment_name: 'test_env')
+  end
+
+  it 'adds custom fields to the log hash' do
+    Thread.current[:job_id] = 'test_job_id'
+    Thread.current[:job_queue] = 'test_queue'
+    Thread.current['sidekiq_tid'] = 'test_tid'
+    allow(Sidekiq::Context).to receive(:current).and_return('test_context')
+
+    log.message = 'Started'
+    log.payload = {
+      method: 'GET',
+      path: '/support/applications',
+      ip: '::1',
+      subject: 'Test Subject',
+      to: 'test@example.com',
+      params: { key: 'value' },
+    }
+
+    expect(log_hash[:domain]).to eq('test_host')
+    expect(log_hash[:environment]).to eq('test_env')
+    expect(log_hash[:hosting_environment]).to eq('test_env')
+    expect(log_hash[:job_id]).to eq('test_job_id')
+    expect(log_hash[:job_queue]).to eq('test_queue')
+    expect(log_hash[:tid]).to eq('test_tid')
+    expect(log_hash[:ctx]).to eq('test_context')
+  end
+
+  it 'sanitizes the mailer subject and to fields' do
+    log.message = 'Started'
+    log.payload = {
+      method: 'GET',
+      path: '/support/applications',
+      ip: '::1',
+      subject: 'Test Subject',
+      to: 'test@example.com',
+      params: { key: 'value' },
+    }
+
+    expect(log_hash[:payload][:subject]).to eq('[REDACTED]')
+    expect(log_hash[:payload][:to]).to eq('[REDACTED]')
+  end
+end
