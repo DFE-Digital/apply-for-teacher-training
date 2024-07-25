54faf2395a014aed66aa8d7ff5bc766b3a6b36a2
David Young <david@dcyoung.dev>
2024-07-25 16:21:02 +0100

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_05_151153) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_24_104015) do
   create_sequence "qualifications_public_id_seq", start: 120000
 
   # These are extensions that must be enabled in order to support this database
