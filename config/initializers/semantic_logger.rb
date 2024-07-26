d7a6a22d7522c1d1b0878676283a6d7869e1f09c
Tomas D'Stefano <tomas_stefano@successoft.com>
2024-07-26 15:39:23 +0100

    Attempt to monkey patch the rails semantic logger gem

--- config/initializers/semantic_logger.rb
+++ config/initializers/semantic_logger.rb
@@ -13,3 +13,31 @@ unless Rails.env.local?
   )
   Rails.logger.info('Application logging to STDOUT')
 end
+
+## To avoid logging sensitive data on "subjects" and "to":
+## See more details on: https://github.com/reidmorrison/rails_semantic_logger/issues/230
+module RailsSemanticLogger
+  module ActionMailer
+    class LogSubscriber < ::ActiveSupport::LogSubscriber
+      class EventFormatter
+        def payload
+          {}.tap do |h|
+            h[:event_name]         = event.name
+            h[:mailer]             = mailer
+            h[:action]             = action
+            h[:message_id]         = event.payload[:message_id]
+            h[:perform_deliveries] = event.payload[:perform_deliveries]
+            h[:subject]            = '[FILTERED]'
+            h[:to]                 = '[FILTERED]'
+            h[:from]               = event.payload[:from]
+            h[:bcc]                = event.payload[:bcc]
+            h[:cc]                 = event.payload[:cc]
+            h[:date]               = date
+            h[:duration]           = event.duration.round(2) if log_duration?
+            h[:args]               = '[FILTERED]'
+          end
+        end
+      end
+    end
+  end
+end
