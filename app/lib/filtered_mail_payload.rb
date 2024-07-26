b42bc13b1cf4f4eb8bcdf62df0ac0f5593e9d6c7
Tomas D'Stefano <tomas_stefano@successoft.com>
2024-07-26 16:43:01 +0100

    Move filtering the mail payload from logging to a separate class
    For more details see:
    https://github.com/reidmorrison/rails_semantic_logger/issues/230

---
+++ app/lib/filtered_mail_payload.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+class FilteredMailPayload
+  attr_reader :formatter
+  delegate :mailer, :action, :date, :log_duration?, to: :formatter
+
+  def initialize(formatter, event)
+    @formatter = formatter
+    @event = event
+    @filtered_params = Rails.application.config.filter_parameters
+    @parameter_filter = ActiveSupport::ParameterFilter.new(@filtered_params)
+  end
+
+  def filtered_payload
+    {}.tap do |h|
+      h[:event_name]         = @parameter_filter.filter_param('mailer.event_name', @event.name)
+      h[:mailer]             = @parameter_filter.filter_param('mailer.mailer', mailer)
+      h[:action]             = @parameter_filter.filter_param('mailer.action', action)
+      h[:message_id]         = @parameter_filter.filter_param('mailer.message_id', @event.payload[:message_id])
+      h[:perform_deliveries] = @parameter_filter.filter_param('mailer.perform_deliveries', @event.payload[:perform_deliveries])
+      h[:subject]            = @parameter_filter.filter_param('mailer.subject', @event.payload[:subject])
+      h[:to]                 = @parameter_filter.filter_param('mailer.to', @event.payload[:to])
+      h[:from]               = @parameter_filter.filter_param('mailer.from', @event.payload[:from])
+      h[:bcc]                = @parameter_filter.filter_param('mailer.bcc', @event.payload[:bcc])
+      h[:cc]                 = @parameter_filter.filter_param('mailer.cc', @event.payload[:cc])
+      h[:date]               = @parameter_filter.filter_param('mailer.date', date)
+      h[:duration]           = @parameter_filter.filter_param('mailer.duration', @event.duration.round(2)) if log_duration?
+      h[:args]               = @parameter_filter.filter_param('mailer.args', @event.payload[:args])
+    end
+  end
+end
--- config/initializers/semantic_logger.rb
+++ config/initializers/semantic_logger.rb
@@ -21,24 +21,7 @@ module RailsSemanticLogger
     class LogSubscriber < ::ActiveSupport::LogSubscriber
       class EventFormatter
         def payload
-          filtered_params = Rails.application.config.filter_parameters
-          parameter_filter = ActiveSupport::ParameterFilter.new(filtered_params)
-
-          {}.tap do |h|
-            h[:event_name]         = parameter_filter.filter_param('mailer.event_name', event.name)
-            h[:mailer]             = parameter_filter.filter_param('mailer.mailer', mailer)
-            h[:action]             = parameter_filter.filter_param('mailer.action', action)
-            h[:message_id]         = parameter_filter.filter_param('mailer.message_id', event.payload[:message_id])
-            h[:perform_deliveries] = parameter_filter.filter_param('mailer.perform_deliveries', event.payload[:perform_deliveries])
-            h[:subject]            = parameter_filter.filter_param('mailer.subject', event.payload[:subject])
-            h[:to]                 = parameter_filter.filter_param('mailer.to', event.payload[:to])
-            h[:from]               = parameter_filter.filter_param('mailer.from', event.payload[:from])
-            h[:bcc]                = parameter_filter.filter_param('mailer.bcc', event.payload[:bcc])
-            h[:cc]                 = parameter_filter.filter_param('mailer.cc', event.payload[:cc])
-            h[:date]               = parameter_filter.filter_param('mailer.date', date)
-            h[:duration]           = parameter_filter.filter_param('mailer.duration', event.duration.round(2)) if log_duration?
-            h[:args]               = parameter_filter.filter_param('mailer.args', event.payload[:args])
-          end
+          FilteredMailPayload.new(self, event).filtered_payload
         end
       end
     end
