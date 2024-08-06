6d7eb01b5d41c326bc0821316fb9ff687e88b7a5
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-08-06 13:17:09 +0100

    Add more detail to Azure Errors

--- lib/azure.rb
+++ lib/azure.rb
@@ -1,6 +1,22 @@
 module Azure
   include ActiveSupport::Configurable
 
-  AzureTokenFilePathError = Class.new(StandardError)
-  AzureAPIError = Class.new(StandardError)
+  AzureError = Class.new(StandardError) do
+    def initialize(status:, body:)
+      @status = status
+      @body = body
+    end
+
+    def message
+      "\r\n\tstatus:\t#{@status}\r\n\tbody:\t#{@body}"
+    end
+  end
+  AzureAPIError = Class.new(AzureError)
+  STSAPIError = Class.new(AzureError)
+
+  AzureTokenFilePathError = Class.new(StandardError) do
+    def message
+      'Azure token file could not be found'
+    end
+  end
 end
