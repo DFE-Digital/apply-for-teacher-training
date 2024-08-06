0c80fa80f3f894e46b772c625e2b9808aa28cc14
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-08-06 13:36:47 +0100

    Add Google STS client for Azure token exchange

---
+++ lib/azure/google_token_exchange.rb
@@ -0,0 +1,42 @@
+module Azure
+  class GoogleTokenExchange
+    def initialize(azure_token)
+      @azure_token = azure_token
+      @conn = Faraday.new do |b|
+        b.response :json
+        b.request :url_encoded
+      end
+    end
+
+    def call
+      url = Azure.config.google_cloud_credentials[:token_url]
+
+      token_exchange_response = @conn.post(url, request_body)
+
+      if token_exchange_response.success?
+        token_exchange_response.body['access_token']
+      else
+        raise_error(token_exchange_response.status, token_exchange_response.body)
+      end
+    end
+
+    def request_body
+      {
+        grant_type: 'urn:ietf:params:oauth:grant-type:token-exchange',
+        audience: Azure.config.google_cloud_credentials[:audience],
+        scope: Azure.config.gcp_scope,
+        requested_token_type: 'urn:ietf:params:oauth:token-type:access_token',
+        subject_token: @azure_token,
+        subject_token_type: Azure.config.google_cloud_credentials[:subject_token_type],
+      }
+    end
+
+    def raise_error(status, body)
+      error = STSAPIError.new(status:, body:)
+
+      Rails.logger.error error.detailed_message
+
+      raise error
+    end
+  end
+end
