DFE-Digital/apply-for-teacher-training

commit bbe00f6ca4da231c3924abe1a470c64ab6211aaf

Move Continuous Applications controllers (#9632)
## Context

We are trying to pay down tech debt left over from Continuous
Applications

## Changes proposed in this pull request

- Removes the ContinuousApplication module name from Controllers and
Concerns
- Moves views out of the `continuous_applications/` directory

app/controllers/candidate_interface/course_choices/go_to_find_controller.rb
@@ -1,0 +1,16 @@
+module CandidateInterface
+  module CourseChoices
+    class GoToFindController < ::CandidateInterface::ContinuousApplicationsController
+      def new
+        @wizard = CourseSelectionWizard.new(current_step:)
+        @adviser_sign_up = Adviser::SignUp.new(current_application)
+      end
+
+    private
+
+      def current_step
+        :go_to_find_explanation
+      end
+    end
+  end
+end

app/controllers/candidate_interface/continuous_applications/course_choices/go_to_find_controller.rb
@@ -1,18 +1,0 @@
-module CandidateInterface
-  module ContinuousApplications
-    module CourseChoices
-      class GoToFindController < ::CandidateInterface::ContinuousApplicationsController
-        def new
-          @wizard = CourseSelectionWizard.new(current_step:)
-          @adviser_sign_up = Adviser::SignUp.new(current_application)
-        end
-
-      private
-
-        def current_step
-          :go_to_find_explanation
-        end
-      end
-    end
-  end
-end

commit 863ef6bfa23567ce196e2de43270bba4147c23b2

Updated model name from Controller (#9647)
## Context


[Error](https://dfe-teacher-services.sentry.io/issues/5682395737/?referrer=slack&notification_uuid=3066b64f-0690-4fd2-820f-07cdd85aeef2&environment=production&alert_rule_id=4434951&alert_type=issue)
introduced by #9632

## Changes proposed in this pull request

- Updated model naming
- Added request spec

app/controllers/candidate_interface/course_choices/go_to_find_controller.rb
@@ -2,7 +2,7 @@ module CandidateInterface
   module CourseChoices
     class GoToFindController < ::CandidateInterface::ContinuousApplicationsController
       def new
-        @wizard = CourseSelectionWizard.new(current_step:)
+        @wizard = CandidateInterface::ContinuousApplications::CourseSelectionWizard.new(current_step:)
         @adviser_sign_up = Adviser::SignUp.new(current_application)
       end
 

spec/requests/candidate_interface/get_go_to_find_spec.rb
@@ -1,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'GET course_choices/go_to_find' do
+  include Devise::Test::IntegrationHelpers
+  let(:candidate) { create(:candidate) }
+
+  before do
+    sign_in candidate
+  end
+
+  it 'is a successful request' do
+    get candidate_interface_continuous_applications_go_to_find_explanation_path
+
+    expect(response).to have_http_status(:success)
+  end
+end