e6e76831d03ec861b99141ff9c08333166c22809
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-08-07 17:22:01 +0100

    Update ApplicationMetrics to use Bigquery with OAuth

--- app/lib/dfe/bigquery/relation.rb
+++ app/lib/dfe/bigquery/relation.rb
@@ -8,7 +8,15 @@ module DfE
       delegate :select, :where, :order, to: :table
 
       def query(sql_query)
-        ::DfE::Bigquery.client.query(sql_query, dataset:).map { |result| result_class.new(result) }
+        query_object = Google::Apis::BigqueryV2::QueryRequest.new(
+          query: sql_query,
+          timeout_ms: DfE::Bigquery.config.bigquery_timeout,
+        )
+
+        ::DfE::Bigquery.client.query_job(
+          DfE::Bigquery.config.bigquery_project_id,
+          query_object,
+        ).map { |result| result_class.new(result) }
       end
 
       def table_name
