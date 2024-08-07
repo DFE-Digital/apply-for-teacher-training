2c04dfd4b19ac206aff334e70bbb6d7b65f08819
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2024-08-07 15:27:54 +0100

    Render application choices list.

--- app/components/candidate_interface/carry_over_interstitial_component.rb
+++ app/components/candidate_interface/carry_over_interstitial_component.rb
@@ -19,7 +19,7 @@ module CandidateInterface
         application_choices: @application_form.application_choices
                                               .includes(:course, :site, :provider, :current_course, :current_course_option, :interviews)
                                               .includes(offer: :conditions),
-        )
+      )
     end
 
   private
--- app/controllers/candidate_interface/course_choices/review_controller.rb
+++ app/controllers/candidate_interface/course_choices/review_controller.rb
@@ -1,6 +1,8 @@
 module CandidateInterface
   module CourseChoices
     class ReviewController < CandidateInterface::CourseChoices::BaseController
+      skip_before_action :redirect_to_your_applications_if_cycle_is_over
+      skip_before_action CarryOverFilter
       def show
         @application_choice = current_application.application_choices.find(params[:application_choice_id])
       end
