686ad0b8759edc7124ddec9bb2429e4570cb3541
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-08-08 11:57:25 +0100

    Azure federated file variable must match pods

--- config/initializers/azure.rb
+++ config/initializers/azure.rb
@@ -7,7 +7,7 @@ Rails.application.config.to_prepare do
     end
 
     config.azure_client_id = ENV['AZURE_CLIENT_ID']
-    config.azure_token_file_path = ENV['AZURE_TOKEN_FILE_PATH']
+    config.azure_token_file_path = ENV['AZURE_FEDERATED_TOKEN_FILE']
     config.google_cloud_credentials = google_cloud_credentials
     config.azure_scope = 'api://AzureADTokenExchange/.default'
     config.gcp_scope = 'https://www.googleapis.com/auth/cloud-platform'
