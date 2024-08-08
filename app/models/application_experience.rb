95b79ce24e77cf7c2758a4978bed53309ccc082a
CatalinVoineag <11318084+CatalinVoineag@users.noreply.github.com>
2024-08-08 14:12:45 +0100

    Ignore ApplicationExperience#experienceable
    These columns are part of the work to allow the user to edit their work
    experiences after submitting an application.
    
    This needs to be deployed first to not break the deploy pipeline

--- app/models/application_experience.rb
+++ app/models/application_experience.rb
@@ -1,3 +1,5 @@
 class ApplicationExperience < ApplicationRecord
+  self.ignored_columns += %w[experienceable_type experienceable_id]
+
   validates :role, :organisation, :start_date, presence: true
 end
