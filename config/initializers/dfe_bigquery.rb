d0df19e449c5a30fc105a3414b5e19ef6d096c92
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-08-08 10:59:58 +0100

    Use default value for bigquery_project_id
      It's not easy to test the absence of an environment variable when it's
      used in initializers. The value is loaded before the test is run.

--- config/initializers/dfe_bigquery.rb
+++ config/initializers/dfe_bigquery.rb
@@ -1,7 +1,7 @@
 require_dependency Rails.root.join('app/lib/dfe/bigquery')
 
 DfE::Bigquery.configure do |config|
-  config.bigquery_project_id = ENV['BIG_QUERY_PROJECT_ID']
+  config.bigquery_project_id = ENV.fetch('BIG_QUERY_PROJECT_ID', 'fake_project_id')
 
   # Retries and timeout (in seconds). See
   # https://github.com/googleapis/google-cloud-ruby/blob/main/google-cloud-bigquery/OVERVIEW.md#configuring-retries-and-timeout
