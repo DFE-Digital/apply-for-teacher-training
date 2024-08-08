de57ced4fb6b16984342044ef6784759747fdab1
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-08-08 10:19:08 +0100

    Stop modifying global configurations in tests

--- spec/lib/dfe/bigquery_spec.rb
+++ spec/lib/dfe/bigquery_spec.rb
@@ -11,9 +11,12 @@ RSpec.describe DfE::Bigquery do
   describe '#client' do
     context 'when missing configuration' do
       it 'raises a configuration error' do
-        with_config(bigquery_project_id: nil) do
-          expect { described_class.client }.to raise_error(DfE::Bigquery::ConfigurationError)
-        end
+        config = Struct.new(:bigquery_retries,
+                            :bigquery_timeout,
+                            :bigquery_api_json_key,
+                            :bigquery_project_id).new(1, 2, 3, nil)
+        allow(described_class).to receive(:config).and_return(config)
+        expect { described_class.client }.to raise_error(DfE::Bigquery::ConfigurationError)
       end
     end
 
@@ -25,18 +28,5 @@ RSpec.describe DfE::Bigquery do
         expect(described_class.client).to eq(bigquery)
       end
     end
-  end
-
-  def with_config(options)
-    old_config = DfE::Bigquery.config.dup
-    DfE::Bigquery.instance_variable_set(:@client, nil)
-
-    DfE::Bigquery.configure do |config|
-      options.each { |option, value| config[option] = value }
-    end
-
-    yield
-  ensure
-    DfE::Bigquery.instance_variable_set(:@config, old_config)
   end
 end
