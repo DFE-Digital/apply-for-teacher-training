5f6dc858dd5091b2c4469170d738bbb83ce6bc59
David Young <david@dcyoung.dev>
2024-08-09 16:01:17 +0100

    Named Routes: Renamed `/provider/:provider_id/courses/:course_id/closed` route
    Renamed from `continuous_applications_closed_course_selection` to `course_choices_closed_course_selection`

--- spec/forms/candidate_interface/course_selection/closed_course_selection_step_spec.rb
+++ spec/forms/candidate_interface/course_selection/closed_course_selection_step_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe CandidateInterface::CourseSelection::ClosedCourseSelectionStep do
   subject { described_class.new.class.route_name }
 
   describe '.route_name' do
-    it { is_expected.to eq('candidate_interface_continuous_applications_closed_course_selection') }
+    it { is_expected.to eq('candidate_interface_course_choices_closed_course_selection') }
   end
 end
