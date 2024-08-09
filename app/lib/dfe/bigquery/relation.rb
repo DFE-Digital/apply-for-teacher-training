462b55896b46909bd8f5dd5288c8ee6728cc3052
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-08-09 13:53:21 +0100

    Combine Bigquery TableRow and TableSchema for better results
      Combining the values from the Tables Row/Cells and the TableSchema
      allows us to give the column names of the response to the relevant
      Result class

--- app/lib/dfe/bigquery/relation.rb
+++ app/lib/dfe/bigquery/relation.rb
@@ -14,17 +14,24 @@ module DfE
           use_legacy_sql: false,
         )
 
-        ::DfE::Bigquery.client.query_job(
+        result = ::DfE::Bigquery.client.query_job(
           DfE::Bigquery.config.bigquery_project_id,
           query_object,
-        ).map { |result| result_class.new(result) }
+        )
+
+        processed_results = result.rows.map do |row|
+          result.schema.fields.each_with_object({}).with_index do |(field, memo), index|
+            field_name = field.name
+            raw_value = row.f[index].v
+
+            memo[field_name] = raw_value
+          end.symbolize_keys
+        end
+
+        processed_results.map { |row| result_class.new(row) }
       end
 
       def table_name
-        raise NotImplementedError
-      end
-
-      def dataset
         raise NotImplementedError
       end
     end
