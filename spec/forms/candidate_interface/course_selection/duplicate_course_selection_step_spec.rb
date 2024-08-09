1d4f8057a3461307fab8844084f9bf297061165b
David Young <david@dcyoung.dev>
2024-08-09 15:55:52 +0100

    Named Routes: Renamed `/provider/:provider_id/courses/:course_id/duplicate` route
    Renamed from `continuous_applications_duplicate_course_selection` to `course_choices_duplicate_course_selection`

--- spec/forms/candidate_interface/course_selection/duplicate_course_selection_step_spec.rb
+++ spec/forms/candidate_interface/course_selection/duplicate_course_selection_step_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe CandidateInterface::CourseSelection::DuplicateCourseSelectionStep
   subject { described_class.new.class.route_name }
 
   describe '.route_name' do
-    it { is_expected.to eq('candidate_interface_continuous_applications_duplicate_course_selection') }
+    it { is_expected.to eq('candidate_interface_course_choices_duplicate_course_selection') }
   end
 end
