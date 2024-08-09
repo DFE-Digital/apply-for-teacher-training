358e4c2d6aab73cf98417f756e8d6c5f58e23ea8
David Young <david@dcyoung.dev>
2024-08-09 16:01:17 +0100

    Named Routes: Renamed `/provider/:provider_id/courses/:course_id/closed` route
    Renamed from `continuous_applications_closed_course_selection` to `course_choices_closed_course_selection`

--- config/routes/candidate.rb
+++ config/routes/candidate.rb
@@ -357,7 +357,7 @@ namespace :candidate_interface, path: '/candidate' do
       get '/provider/:provider_id/courses/:course_id/reached-reapplication-limit' => 'course_choices/reached_reapplication_limit#new', as: :course_choices_reached_reapplication_limit
       get '/provider/:provider_id/courses/:course_id/duplicate' => 'course_choices/duplicate_course_selection#new', as: :course_choices_duplicate_course_selection
       get '/provider/:provider_id/courses/:course_id/full' => 'course_choices/full_course_selection#new', as: :course_choices_full_course_selection
-      get '/provider/:provider_id/courses/:course_id/closed' => 'course_choices/closed_course_selection#new', as: :continuous_applications_closed_course_selection
+      get '/provider/:provider_id/courses/:course_id/closed' => 'course_choices/closed_course_selection#new', as: :course_choices_closed_course_selection
 
       get '/provider/:provider_id/courses/:course_id' => 'course_choices/course_study_mode#new', as: :continuous_applications_course_study_mode
       post '/provider/:provider_id/courses/:course_id' => 'course_choices/course_study_mode#create'
