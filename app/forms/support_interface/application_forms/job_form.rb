f3cef7d2d807a84bba438a8712a508ef2814962e
CatalinVoineag <11318084+CatalinVoineag@users.noreply.github.com>
2024-08-09 13:43:43 +0100

    Update application choice experience in line with support changes

--- app/forms/support_interface/application_forms/job_form.rb
+++ app/forms/support_interface/application_forms/job_form.rb
@@ -22,6 +22,28 @@ module SupportInterface
           details: set_details_field,
           audit_comment:,
         )
+
+        application_form = job.application_form
+        ApplicationWorkExperience.where(
+          application_form_id: application_form.id,
+          experienceable_id: [application_form.application_choices.pluck(:id)],
+          experienceable_type: 'ApplicationChoice',
+        ).each do |work_experience|
+          # Update 1 by 1 because of the audit comment AR column, update_all does not have it
+          work_experience.update!(
+            role:,
+            organisation:,
+            commitment:,
+            start_date:,
+            end_date: not_currently_employed_in_this_role? ? end_date : nil,
+            start_date_unknown:,
+            end_date_unknown:,
+            currently_working:,
+            relevant_skills:,
+            details: set_details_field,
+            audit_comment:,
+          )
+        end
       end
     end
   end
--- app/forms/support_interface/application_forms/volunteering_role_form.rb
+++ app/forms/support_interface/application_forms/volunteering_role_form.rb
@@ -9,6 +9,19 @@ module SupportInterface
       def map_attributes
         super.merge(audit_comment: audit_comment)
       end
+
+      def update(application_form)
+        super
+
+        ApplicationVolunteeringExperience.where(
+          application_form_id: application_form.id,
+          experienceable_id: [application_form.application_choices.pluck(:id)],
+          experienceable_type: 'ApplicationChoice',
+        ).each do |volunteering_experience|
+          # Update 1 by 1 because of the audit comment AR column, update_all does not have it
+          volunteering_experience.update!(map_attributes)
+        end
+      end
     end
   end
 end
