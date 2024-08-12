9b60a633a0820bd233e0e4c403fd0231fc2e4b39
David Young <david4.young@education.gov.uk>
2024-08-12 11:19:19 +0100

    Added database indexing on the emails table (#9654)
    ## Context
    
    The current emails table has no indexing on the `id` column. This is
    causing page load errors when trying to view the Email Logs.
    
    ## Changes proposed in this pull request
    
    Added index on emails.id
    
    Adding this index locally reduced the count on the emails
    table from ~11s down to ~200ms based on 10 million rows

--- db/schema.rb
+++ db/schema.rb
@@ -501,6 +501,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_08_124535) do
     t.datetime "updated_at", null: false
     t.string "delivery_status", default: "unknown", null: false
     t.index ["application_form_id"], name: "index_emails_on_application_form_id"
+    t.index ["created_at"], name: "index_emails_on_created_at"
     t.index ["notify_reference"], name: "index_emails_on_notify_reference"
   end
 
