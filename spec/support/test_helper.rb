911916e6e75985ab68d13f87407f273acccf9aae
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-08-13 12:55:02 +0100

    Distinguish between application metrics and by provider in stubs

--- spec/support/test_helper.rb
+++ spec/support/test_helper.rb
@@ -12,34 +12,7 @@ module DfE
       def stub_bigquery_application_metrics_by_provider_request(rows: nil)
         bigquery_client = instance_double(Google::Apis::BigqueryV2::BigqueryService)
         allow(DfE::Bigquery).to receive(:client).and_return(bigquery_client)
-        allow(bigquery_client).to receive(:query_job).and_return(stubbed_bigquery_application_metrics_response(rows:))
-      end
-
-      def application_metrics_results(options = {})
-        [
-          {
-            cycle_week: '7',
-            first_date_in_week: Date.new(2023, 11, 13).iso8601,
-            last_date_in_week: Date.new(2023, 11, 19).iso8601,
-            nonsubject_filter: '21',
-            number_of_candidates_submitted_to_date: '400',
-            number_of_candidates_submitted_to_same_date_previous_cycle: '200',
-            number_of_candidates_who_did_not_meet_any_offer_conditions_this_cycle_to_date: '30',
-            number_of_candidates_who_did_not_meet_any_offer_conditions_this_cycle_to_same_date_previous_cycle: '15',
-            number_of_candidates_who_had_all_applications_rejected_this_cycle_to_date: '100',
-            number_of_candidates_who_had_all_applications_rejected_this_cycle_to_same_date_previous_cycle: '50',
-            number_of_candidates_with_all_accepted_offers_withdrawn_this_cycle_to_date: '200',
-            number_of_candidates_with_all_accepted_offers_withdrawn_this_cycle_to_same_date_previous_cycle: '100',
-            number_of_candidates_with_deferred_offers_from_this_cycle_to_date: '0',
-            number_of_candidates_with_deferred_offers_from_this_cycle_to_same_date_previous_cycle: '0',
-            number_of_candidates_with_offers_to_date: '598',
-            number_of_candidates_with_offers_to_same_date_previous_cycle: '567',
-            number_of_candidates_with_reconfirmed_offers_deferred_from_previous_cycle_to_date: '285',
-            number_of_candidates_with_reconfirmed_offers_deferred_from_previous_cycle_to_same_date_previous_cycle: '213',
-            number_of_candidates_accepted_to_date: '20',
-            number_of_candidates_accepted_to_same_date_previous_cycle: '10',
-          }.merge(options.slice(:attributes)),
-        ]
+        allow(bigquery_client).to receive(:query_job).and_return(stubbed_bigquery_application_metrics_by_provider_response(rows:))
       end
 
       def stubbed_bigquery_application_metrics_response(rows: nil)
@@ -69,8 +42,8 @@ module DfE
         ]])
       end
 
-      def application_metrics_by_provider_results(rows = [])
-        return stub_response(rows:) if rows.present?
+      def stubbed_bigquery_application_metrics_by_provider_response(rows: nil)
+        return stub_response(rows:) if rows
 
         stub_response(rows: [[
           { name: 'id', type: 'INTEGER', value: '1337' },
