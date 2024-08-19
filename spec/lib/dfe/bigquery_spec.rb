218532cc72258e04a1b5f411c3162f0704a358e7
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-08-19 17:20:12 +0100

    Exchange Google::Cloude::Bigquery for Google::Apis::BigqueryV2
      Our application now uses the lower level Google library to access
      Bigquery. The "modern client" is not capable of utilizing our Workload
      Identity Federation Service Account Impersonation Token.
    
      We are removing the JSON API key from the environment. Now, we use the
      WorkflowIdentityFederation module to get authorization tokens for Bigquery.

--- spec/lib/dfe/bigquery_spec.rb
+++ spec/lib/dfe/bigquery_spec.rb
@@ -1,37 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe DfE::Bigquery do
+  before do
+    stub_azure_access_token
+    stub_token_exchange
+    stub_google_access_token
+    stub_wif_config
+  end
+
   describe '#client' do
     context 'when missing configuration' do
       it 'raises a configuration error' do
-        with_config(bigquery_project_id: nil) do
-          expect { described_class.client }.to raise_error(DfE::Bigquery::ConfigurationError)
-        end
+        config = Struct.new(:bigquery_retries,
+                            :bigquery_timeout,
+                            :bigquery_project_id).new(1, 2, nil)
+        allow(described_class).to receive(:config).and_return(config)
+        expect { described_class.client }.to raise_error(DfE::Bigquery::ConfigurationError)
       end
     end
 
     context 'when valid configuration' do
-      let(:bigquery) { instance_double(Google::Cloud::Bigquery) }
+      let(:bigquery) { Google::Apis::BigqueryV2::BigqueryService.new }
 
       it 'instantiates bigquery object' do
-        with_config(bigquery_project_id: 'some-project', bigquery_api_json_key: '{}', bigquery_retries: 2, bigquery_timeout: 10) do
-          allow(Google::Cloud::Bigquery).to receive(:new).and_return(bigquery)
-          expect(described_class.client).to eq(bigquery)
-        end
+        allow(Google::Apis::BigqueryV2::BigqueryService).to receive(:new).and_return(bigquery)
+        expect(described_class.client).to eq(bigquery)
       end
     end
-  end
-
-  def with_config(options)
-    old_config = DfE::Bigquery.config.dup
-    DfE::Bigquery.instance_variable_set(:@client, nil)
-
-    DfE::Bigquery.configure do |config|
-      options.each { |option, value| config[option] = value }
-    end
-
-    yield
-  ensure
-    DfE::Bigquery.instance_variable_set(:@config, old_config)
   end
 end
