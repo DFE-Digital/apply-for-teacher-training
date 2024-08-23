c9ea8c85e17ae00d6bd682fab0624ce57cea8e0b
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2024-08-23 13:29:01 +0100

    Test relation method implicitly

--- spec/workers/end_of_cycle/send_reject_by_default_reminder_to_providers_worker_spec.rb
+++ spec/workers/end_of_cycle/send_reject_by_default_reminder_to_providers_worker_spec.rb
@@ -1,25 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe EndOfCycle::SendRejectByDefaultReminderToProvidersWorker do
-  describe '#relation' do
-    it 'includes providers who have rejectable choices only' do
-      inactive_application = create(:application_choice, :inactive)
-      interview_application = create(:application_choice, :interviewing)
-      awaiting_application = create(:application_choice, :awaiting_provider_decision)
-
-      create(:application_choice, :rejected)
-
-      results = described_class.new.relation
-
-      expect(results)
-        .to contain_exactly(
-          inactive_application.provider,
-          interview_application.provider,
-          awaiting_application.provider,
-        )
-    end
-  end
-
   describe '#perform' do
     context 'is before the date for sending the reminder', time: reject_by_default_reminder_run_date - 1.day do
       it 'does not send any emails' do
@@ -44,13 +25,24 @@ RSpec.describe EndOfCycle::SendRejectByDefaultReminderToProvidersWorker do
     end
 
     context 'it is on the reminder date', time: reject_by_default_reminder_run_date do
-      it 'sends emails and creates chaser sent record' do
-        application_choices = create(:application_choice, :awaiting_provider_decision)
-        create(:provider_permissions, provider: application_choices.provider)
+      it 'calls batch worker with application choices' do
+        inactive_application = create(:application_choice, :inactive)
+        interview_application = create(:application_choice, :interviewing)
+        awaiting_application = create(:application_choice, :awaiting_provider_decision)
+
+        # These two application choices should not be included
+        create(:application_choice, :rejected)
+        create(:application_choice, :unsubmitted)
 
         allow(EndOfCycle::SendRejectByDefaultReminderToProvidersBatchWorker).to receive(:perform_at)
         described_class.new.perform
-        expect(EndOfCycle::SendRejectByDefaultReminderToProvidersBatchWorker).to have_received(:perform_at)
+
+        expect(EndOfCycle::SendRejectByDefaultReminderToProvidersBatchWorker)
+          .to have_received(:perform_at).with(kind_of(Time), [
+            inactive_application.provider.id,
+            interview_application.provider.id,
+            awaiting_application.provider.id,
+          ])
       end
     end
   end
