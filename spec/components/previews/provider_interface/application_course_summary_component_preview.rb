bdb5747518bcea0be4c2c2b6ca93257e638aa51d
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-09-02 16:29:42 +0100

    Rename CourseSummaryComponent to include Application
      This component renders the Course Summary in the the context of a
      Course selected by a candidate.
    
      The Location key reflects whether the Location was auto selected or
      chosen by the candidate.

---
+++ spec/components/previews/provider_interface/application_course_summary_component_preview.rb
@@ -0,0 +1,10 @@
+module ProviderInterface
+  class ApplicationCourseSummaryComponentPreview < ViewComponent::Preview
+    layout 'previews/provider'
+
+    def course_details
+      application_choice = build_stubbed(:application_choice)
+      render ApplicationCourseSummaryComponent.new(application_choice:)
+    end
+  end
+end
