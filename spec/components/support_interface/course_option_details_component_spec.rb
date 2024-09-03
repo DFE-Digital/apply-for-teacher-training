9c2c5a71842188783198c747d172af30ded3fc3e
Tomas D'Stefano <tomas_stefano@successoft.com>
2024-09-03 14:46:45 +0100

    Change variables to explain what they mean

--- spec/components/support_interface/course_option_details_component_spec.rb
+++ spec/components/support_interface/course_option_details_component_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 RSpec.describe SupportInterface::CourseOptionDetailsComponent do
   it 'renders postgraduate course' do
-    course_option = create(:course_option)
-    result = render_inline(described_class.new(course_option:))
+    postgraduate_course_option = create(:course_option)
+    result = render_inline(described_class.new(course_option: postgraduate_course_option))
     expect(result.css('.govuk-summary-list__key').text).to include('Course type')
     expect(result.css('.govuk-summary-list__value').text).to include('Postgraduate')
   end
 
   it 'renders undergraduate course' do
-    course_option = create(:course_option, course: create(:course, :teacher_degree_apprenticeship))
-    result = render_inline(described_class.new(course_option:))
+    undergraduate_course_option = create(:course_option, course: create(:course, :teacher_degree_apprenticeship))
+    result = render_inline(described_class.new(course_option: undergraduate_course_option))
     expect(result.css('.govuk-summary-list__key').text).to include('Course type')
     expect(result.css('.govuk-summary-list__value').text).to include('Undergraduate')
   end
