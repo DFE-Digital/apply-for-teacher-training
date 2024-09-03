8f07d23661fb5e62ce56571aa5d8cf5575d001f7
David Young <david@dcyoung.dev>
2024-09-03 16:56:43 +0100

    Moved `redirect_to_your_applications_if_submitted` to CandidateInterfaceController

--- app/controllers/concerns/candidate_interface/continuous_applications.rb
+++ app/controllers/concerns/candidate_interface/continuous_applications.rb
@@ -8,12 +8,6 @@ module CandidateInterface
       # CandidateInterfaceController
       before_action :redirect_to_post_offer_dashboard_if_accepted_deferred_or_recruited
       before_action CarryOverFilter
-
-    private
-
-      def redirect_to_your_applications_if_submitted
-        redirect_to candidate_interface_application_choices_path unless application_choice.unsubmitted?
-      end
     end
   end
 end
