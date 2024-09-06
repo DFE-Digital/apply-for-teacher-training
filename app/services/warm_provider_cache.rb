6d79ccd64b2fd0d090014e1127a39ed76fc55d9f
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-09-06 11:32:50 +0100

    Cache in the worker

--- app/services/warm_provider_cache.rb
+++ app/services/warm_provider_cache.rb
@@ -6,19 +6,31 @@ class WarmProviderCache
   def call(api_version, provider_id)
     @provider = Provider.find(provider_id)
     @version_number = api_version
-    r = {@provider.name => []}
+    r = { @provider.name => [] }
     # For each of the application choices generate a cache entry
     application_choices_visible_to_provider
       .where('application_choices.updated_at between (?) and (?)', Date.new(2024, 9, 2), Date.new(2024, 9, 5))
-      .where.not(application_choices: { status: ['unsubmitted'] })
       .find_each do |application_choice|
       presenter = VendorAPI::ApplicationPresenter.new(api_version, application_choice)
-      presenter.as_json
-      presenter.serialized_json
+      Rails.cache.write(
+        presenter.send(:cache_key, application_choice, api_version),
+        schema(presenter),
+        expires_in: VendorAPI::ApplicationPresenter::CACHE_EXPIRES_IN,
+      )
+      Rails.cache.write(
+        presenter.send(:cache_key, application_choice, active_version, method: :as_json),
+        schema(presenter),
+        expires_in: VendorAPI::ApplicationPresenter::CACHE_EXPIRES_IN,
+      )
+
       Rails.logger.tagged('WarmProviderCache').info "ApplicationChoice cached: #{application_choice.id}"
       r[@provider.name] << application_choice.id
     end
     r
+  end
+
+  def schema(presenter)
+    @schema ||= presenter.send(:schema)
   end
 
   def current_provider
