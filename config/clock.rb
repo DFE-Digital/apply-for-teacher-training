dd872c4500beaedb46b87b5019645709ff9c023c
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-09-06 12:06:00 +0100

    Fix WarmCache call in clock

--- config/clock.rb
+++ config/clock.rb
@@ -26,7 +26,7 @@ class Clock
   every(1.hour, 'DetectInvariantsHourlyCheck', at: '**:30') { DetectInvariantsHourlyCheck.perform_async }
 
   # Warm the Vendor API cache for ApplicationChoices
-  every(23.hours, 'WarmCacheWorker') { ::WarmCache.call }
+  every(23.hours, 'WarmCacheWorker') { ::WarmCache.new.call }
 
   # Daily jobs
   every(1.day, 'Chasers::Candidate::OfferWorker', at: '10:30') { Chasers::Candidate::OfferWorker.perform_async }
