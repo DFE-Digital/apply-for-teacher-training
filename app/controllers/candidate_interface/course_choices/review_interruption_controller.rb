3913890c5329d06d8c44dd9a8132acdb235cf3ce
Tomas D'Stefano <tomas_stefano@successoft.com>
2024-09-09 14:08:56 +0100

    Transform helper method into a component

--- app/controllers/candidate_interface/course_choices/review_interruption_controller.rb
+++ app/controllers/candidate_interface/course_choices/review_interruption_controller.rb
@@ -7,17 +7,6 @@ module CandidateInterface
         @application_choice = current_application.application_choices.find(params[:application_choice_id])
         @word_count = current_application.becoming_a_teacher.scan(/\S+/).size
       end
-
-      def x_continue_without_editing_path
-        if application_choice.undergraduate_course_and_application_form_with_degree?
-          candidate_interface_course_choices_course_review_undergraduate_interruption_path(application_choice.id)
-        elsif current_application.qualifications_enic_reasons_waiting_or_maybe? || current_application.any_qualification_enic_reason_not_needed?
-          candidate_interface_course_choices_course_review_enic_interruption_path(@application_choice.id)
-        else
-          candidate_interface_course_choices_course_review_and_submit_path(@application_choice.id)
-        end
-      end
-      helper_method :x_continue_without_editing_path
     end
   end
 end
