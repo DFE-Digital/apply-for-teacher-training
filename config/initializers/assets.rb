2fa09addcd618e4215acf3a3c4bb4d808e929cea
Avin Hurry <avinhurry@icloud.com>
2024-09-11 14:12:22 +0100

    Remove space

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-
 Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
