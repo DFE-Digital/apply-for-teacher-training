4666cf53882770281010b2ad467eb03a07b62f66
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2024-09-18 14:40:03 +0100

    Improvements to decline by default worker
    Batch jobs and allow job to be forced to run

--- spec/workers/end_of_cycle/decline_by_default_worker_spec.rb
+++ spec/workers/end_of_cycle/decline_by_default_worker_spec.rb
@@ -2,56 +2,57 @@ require 'rails_helper'
 
 RSpec.describe EndOfCycle::DeclineByDefaultWorker do
   describe '#perform' do
+    context 'where force is true' do
+      it 'enqueues secondary worker for offered application choices', time: mid_cycle do
+        declineable = create(:application_choice, :offer)
+
+        allow(EndOfCycle::DeclineByDefaultSecondaryWorker).to receive(:perform_at)
+        described_class.new.perform(force: true)
+        expect(EndOfCycle::DeclineByDefaultSecondaryWorker)
+          .to have_received(:perform_at).with(kind_of(Time), [declineable.application_form.id])
+      end
+    end
+
     context 'for previous cycle, current cycle, next cycle' do
       [RecruitmentCycle.previous_year, RecruitmentCycle.current_year, RecruitmentCycle.next_year].each do |year|
         context 'after the decline by default date', time: decline_by_default_run_date(year) do
-          it 'calls DeclineByDefaultService' do
+          it 'enqueues secondary worker for offered application choices' do
             declineable = create(:application_choice, :offer)
-            decline_service = spy
-            allow(EndOfCycle::DeclineByDefaultService)
-              .to receive(:new)
-              .with(declineable.application_form)
-              .and_return(decline_service)
-            described_class.new.perform
-            expect(decline_service).to have_received(:call)
-          end
 
-          it 'changes inactive application to declined by default' do
-            declineable = create(:application_choice, :offer)
+            allow(EndOfCycle::DeclineByDefaultSecondaryWorker).to receive(:perform_at)
             described_class.new.perform
-
-            expect(declineable.reload.status).to eq 'declined'
-            expect(declineable.declined_by_default).to be true
+            expect(EndOfCycle::DeclineByDefaultSecondaryWorker)
+              .to have_received(:perform_at).with(kind_of(Time), contain_exactly(declineable.application_form.id))
           end
         end
 
         context 'between cycles, but not on decline by default date', time: after_apply_deadline(year) do
-          it 'does not run DeclineByDefaultService' do
-            declineable = create(:application_choice, :offer)
+          it 'does not enqueues secondary worker' do
+            create(:application_choice, :offer)
 
+            allow(EndOfCycle::DeclineByDefaultSecondaryWorker).to receive(:perform_at)
             described_class.new.perform
-
-            expect(declineable.reload.status).to eq 'offer'
+            expect(EndOfCycle::DeclineByDefaultSecondaryWorker).not_to have_received(:perform_at)
           end
         end
 
         context 'in mid-cycle', time: mid_cycle(year) do
-          it 'does not run DeclineByDefaultService' do
-            declineable = create(:application_choice, :offer)
+          it 'does not enqueues secondary worker' do
+            create(:application_choice, :offer)
 
+            allow(EndOfCycle::DeclineByDefaultSecondaryWorker).to receive(:perform_at)
             described_class.new.perform
-
-            expect(declineable.reload.status).to eq 'offer'
+            expect(EndOfCycle::DeclineByDefaultSecondaryWorker).not_to have_received(:perform_at)
           end
         end
 
         context 'after_apply_reopens', time: after_apply_reopens(year) do
-          it 'does not run once the new cycle starts' do
-            declineable = create(:application_choice, :offer)
+          it 'does not enqueues secondary worker' do
+            create(:application_choice, :offer)
 
+            allow(EndOfCycle::DeclineByDefaultSecondaryWorker).to receive(:perform_at)
             described_class.new.perform
-
-            expect(declineable.reload.status).to eq 'offer'
+            expect(EndOfCycle::DeclineByDefaultSecondaryWorker).not_to have_received(:perform_at)
           end
         end
       end
