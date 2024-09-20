d296ae365ec23488006bc0d9ba249ea024ba8914
Iain McNulty <iain.mcnulty@education.gov.uk>
2024-09-20 11:34:39 +0100

    Add auto selected school variant in preview

--- spec/components/previews/provider_interface/application_course_summary_component_preview.rb
+++ spec/components/previews/provider_interface/application_course_summary_component_preview.rb
@@ -2,7 +2,12 @@ module ProviderInterface
   class ApplicationCourseSummaryComponentPreview < ViewComponent::Preview
     layout 'previews/provider'
 
-    def course_details
+    def course_details_school_auto_selected
+      application_choice = FactoryBot.build_stubbed(:application_choice, school_placement_auto_selected: true)
+      render ApplicationCourseSummaryComponent.new(application_choice:)
+    end
+
+    def course_details_school_candidate_selected
       application_choice = FactoryBot.build_stubbed(:application_choice)
       render ApplicationCourseSummaryComponent.new(application_choice:)
     end
