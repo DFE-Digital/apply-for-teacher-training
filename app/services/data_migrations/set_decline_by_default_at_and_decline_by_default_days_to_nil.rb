9b8095545b28f053b3ce2e9c4052b4bc67eeb978
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2024-09-25 14:51:31 +0100

    Data migration to remove the decline_by_default data from 2024 application choices

---
+++ app/services/data_migrations/set_decline_by_default_at_and_decline_by_default_days_to_nil.rb
@@ -0,0 +1,28 @@
+module DataMigrations
+  class SetDeclineByDefaultAtAndDeclineByDefaultDaysToNil
+    TIMESTAMP = 20240925092609
+    MANUAL_RUN = false
+
+    def change
+      application_choices.in_batches(of: 4000) do |batch|
+        batch.update_all(decline_by_default_at: nil, decline_by_default_days: nil)
+      end
+    end
+
+  private
+
+    def application_choices
+      choices_from_2024
+        .where.not(decline_by_default_at: nil)
+        .or(choices_from_2024.where.not(decline_by_default_days: nil))
+        .distinct
+    end
+
+    def choices_from_2024
+      @choices_from_2024 ||= ApplicationChoice
+                               .joins(:application_form)
+                               .where('application_form.recruitment_cycle_year': 2024)
+                               .distinct
+    end
+  end
+end
