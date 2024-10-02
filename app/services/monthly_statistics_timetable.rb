398a908ab4400e4f3341ecb1669da3848e05f5d2
David Young <david@dcyoung.dev>
2024-10-02 16:50:29 +0100

    WIP

--- app/services/monthly_statistics_timetable.rb
+++ app/services/monthly_statistics_timetable.rb
@@ -1,40 +1,116 @@
 module MonthlyStatisticsTimetable
+  # Should the GenerateMonthlyStatistics generate a new report today?
   def self.generate_monthly_statistics?
-    return false if Time.zone.today.month == 10
-
-    Time.zone.today == current_generation_date
+    # Yes if there is an occurrence today
+    Timetable.new.generate_monthly_statistics?
   end
 
+  # Has the report generator run?
+  # DetectInvariantsDailyCheck#detect_if_the_monthly_statistics_has_not_run
+  # latest_monthly_report.generation_date >= MonthlyStatisticsTimetable.current_generation_date
+  #
+  # Generation date to use for the new report
   def self.current_generation_date
-    third_monday_of_the_month(Date.current.beginning_of_month)
+    # Generation date for most recent occurrence
+    Timetable.new.current_month_generation_date
   end
 
+  # Publication date to use for the new report
   def self.current_publication_date
-    current_generation_date + 1.week
+    # Publication date for most recent occurrence
+    Timetable.new.current_month_publication_date
   end
 
+  # Not used
   def self.last_generation_date
-    third_monday_of_the_month(Date.current.beginning_of_month - 1.month)
+    Timetable.new.previous_month_generation_date
   end
 
+  # MonthlyStatisticsReport#current_period used to get the previous month if current month is not published
+  # DetectInvariantsDailyCheck#detect_if_the_monthly_statistics_has_not_run
   def self.last_publication_date
     return current_publication_date if current_publication_date <= Time.zone.today
 
-    last_generation_date + 1.week
+    Timetable.new.previous_month_publication_date
   end
 
+  # Not used
   def self.next_generation_date
-    third_monday_of_the_month(Date.current.beginning_of_month + 1.month)
+    Timetable.new.next_month_generation_date
   end
 
+  # MonthlyStatisticsReport#current_period
+  # Used to handle non-published reports
+  #
+  # MonthlyStatisticsPresenter#next_publication_date
+  # Used to display the date of the next planned report
   def self.next_publication_date
-    return current_publication_date if current_publication_date > Time.zone.today
+    return current_publication_date if current_publication_date.past?
 
-    next_generation_date + 1.week
+    Timetable.new.next_month_publication_date
   end
 
   def self.third_monday_of_the_month(start_date)
-    first_monday = start_date.upto(start_date.next_week).find(&:monday?)
+    beginning_of_month = start_date.beginning_of_month
+    first_monday = beginning_of_month.upto(beginning_of_month.next_week).find(&:monday?)
     first_monday + 2.weeks
+  end
+
+  class Timetable
+    attr_reader :base_date
+
+    def initialize(base_date = Time.zone.today)
+      @base_date = base_date
+    end
+
+    def generate_monthly_statistics?
+      base_date == current_month_generation_date
+    end
+
+    def current_month_generation_date
+      base_date_to_use = base_date
+
+      if base_date.month == 10
+        base_date_to_use = base_date.prev_month
+      end
+
+      MonthlyStatisticsTimetable.third_monday_of_the_month(base_date_to_use)
+    end
+
+    def current_publication_date
+      if current_month_publication_date > base_date
+        previous_month_publication_date
+      else
+        current_month_publication_date
+      end
+    end
+
+    def current_month_publication_date
+      current_month_generation_date + 1.week
+    end
+
+    def previous_month_generation_date
+      Timetable.new(base_date.prev_month).current_month_generation_date
+    end
+
+    def previous_month_publication_date
+      Timetable.new(base_date.prev_month).current_month_publication_date
+    end
+
+    def next_month_generation_date
+      if base_date.month == 9
+        return Timetable.new(base_date.next_month(2)).current_month_generation_date
+      end
+
+      Timetable.new(base_date.next_month).current_month_generation_date
+    end
+
+    def next_month_publication_date
+      if base_date.month == 9
+        return Timetable.new(base_date.next_month(2)).current_month_publication_date
+      end
+
+      Timetable.new(base_date.next_month).current_month_publication_date
+    end
   end
 end
--- spec/services/monthly_statistics_timetable_spec.rb
+++ spec/services/monthly_statistics_timetable_spec.rb
@@ -51,3 +51,218 @@ RSpec.describe MonthlyStatisticsTimetable do
     end
   end
 end
+
+RSpec.describe MonthlyStatisticsTimetable::Timetable do
+  describe '#generate_monthly_statistics?' do
+    subject { described_class.new(base_date).generate_monthly_statistics? }
+
+    context 'when base date is the third Monday of the month' do
+      months_except_october = (1..12).reject { |month| month == 10 }
+
+      months_except_october.each do |month|
+        month_name = Date::MONTHNAMES[month]
+        let(:base_date) { MonthlyStatisticsTimetable.third_monday_of_the_month(Date.new(RecruitmentCycle.current_year, month, 1)) }
+
+        context "in #{month_name}" do
+          it { is_expected.to be_truthy }
+        end
+      end
+
+      context 'in October' do
+        let(:base_date) { MonthlyStatisticsTimetable.third_monday_of_the_month(Date.new(RecruitmentCycle.current_year, 10, 1)) }
+
+        it { is_expected.to be_falsey }
+      end
+    end
+
+    context 'when base date is the first day of the month' do
+      (1..12).each do |month|
+        month_name = Date::MONTHNAMES[month]
+
+        context "in #{month_name}" do
+          let(:base_date) { Date.new(RecruitmentCycle.current_year, month, 1) }
+
+          it { is_expected.to be_falsey }
+        end
+      end
+    end
+  end
+
+  describe '#current_month_generation_date' do
+    subject(:current_month_generation_date) { described_class.new(base_date).current_month_generation_date }
+
+    context 'when the base date is the first day of the month' do
+      let(:base_date) { Date.new(2021, 12, 1) }
+      let(:third_monday_of_the_month) { Date.new(2021, 12, 20) }
+
+      it { is_expected.to eq(third_monday_of_the_month) }
+    end
+
+    context 'when the base date is the third Monday of the month' do
+      let(:base_date) { Date.new(2021, 12, 20) }
+      let(:third_monday_of_the_month) { Date.new(2021, 12, 20) }
+
+      it { is_expected.to eq(third_monday_of_the_month) }
+    end
+
+    context 'when the base date is the last day of the month' do
+      let(:base_date) { Date.new(2021, 12, 31) }
+      let(:third_monday_of_the_month) { Date.new(2021, 12, 20) }
+
+      it { is_expected.to eq(third_monday_of_the_month) }
+    end
+
+    context 'when the base date is in October' do
+      let(:base_date) { Date.new(2021, 10, 1) }
+
+      it 'is expected to be the third Monday of September' do
+        third_monday_of_september = Date.new(2021, 9, 20)
+
+        expect(current_month_generation_date).to eq(third_monday_of_september)
+      end
+    end
+  end
+
+  describe '#current_month_publication_date' do
+    subject(:current_month_publication_date) { timetable.current_month_publication_date }
+
+    let(:timetable) { described_class.new }
+
+    before do
+      allow(timetable).to receive(:current_month_generation_date)
+                            .and_return(Date.new(2021, 1, 1))
+    end
+
+    it 'a date one week after the current generation date' do
+      expect(current_month_publication_date).to eq(Date.new(2021, 1, 8))
+    end
+  end
+
+  describe '#previous_month_generation_date' do
+    subject(:previous_month_generation_date) { described_class.new(base_date).previous_month_generation_date }
+
+    let(:base_date) { Date.new(2021, 12, 1) }
+
+    it 'is the third Monday of the previous month' do
+      expect(previous_month_generation_date).to eq(Date.new(2021, 11, 15))
+    end
+
+    context 'when the base date is November' do
+      let(:base_date) { Date.new(2021, 11, 1) }
+
+      # We skip October see #current_generation_date
+      it 'is the third Monday of September' do
+        expect(previous_month_generation_date).to eq(Date.new(2021, 9, 20))
+      end
+    end
+  end
+
+  describe '#previous_month_publication_date' do
+    subject(:previous_month_publication_date) { described_class.new(base_date).previous_month_publication_date }
+
+    let(:base_date) { Date.new(2021, 1, 1) }
+
+    it 'is a week after the third Monday of the previous month' do
+      expect(previous_month_publication_date).to eq(Date.new(2020, 12, 28))
+    end
+
+    context 'when the base date is November' do
+      let(:base_date) { Date.new(2021, 11, 1) }
+
+      # We skip October see #current_generation_date
+      it 'is a week after the third Monday of September' do
+        expect(previous_month_publication_date).to eq(Date.new(2021, 9, 27))
+      end
+    end
+  end
+
+  describe '#next_month_generation_date' do
+    subject(:next_month_generation_date) { described_class.new(base_date).next_month_generation_date }
+
+    let(:base_date) { Date.new(2021, 1, 1) }
+
+    it 'is the third Monday of the next month' do
+      expect(next_month_generation_date).to eq(Date.new(2021, 2, 15))
+    end
+
+    context 'when the base date is September' do
+      let(:base_date) { Date.new(2021, 9, 1) }
+
+      # We skip October see #current_generation_date
+      it 'is the third Monday of November' do
+        expect(next_month_generation_date).to eq(Date.new(2021, 11, 15))
+      end
+    end
+  end
+
+  describe '#next_month_publication_date' do
+    subject(:next_month_publication_date) { described_class.new(base_date).next_month_publication_date }
+
+    let(:base_date) { Date.new(2021, 1, 1) }
+
+    it 'is a week after the third Monday of the next month' do
+      expect(next_month_publication_date).to eq(Date.new(2021, 2, 22))
+    end
+
+    context 'when the base date is September' do
+      let(:base_date) { Date.new(2021, 9, 1) }
+
+      # We skip October see #current_generation_date
+      it 'is a week after the third Monday of November' do
+        expect(next_month_publication_date).to eq(Date.new(2021, 11, 22))
+      end
+    end
+  end
+
+  describe '#current_publication_date' do
+    subject(:current_publication_date) { described_class.new(base_date).current_publication_date }
+
+    context 'beginning of September' do
+      let(:base_date) { Date.new(2021, 9, 1) }
+
+      it 'returns the publication for August' do
+        expect(current_publication_date).to eq(Date.new(2021, 8, 23))
+      end
+    end
+
+    context 'end of September' do
+      let(:base_date) { Date.new(2021, 9, 30) }
+
+      it 'returns the publication for September' do
+        expect(current_publication_date).to eq(Date.new(2021, 9, 27))
+      end
+    end
+
+    context 'beginning of October' do
+      let(:base_date) { Date.new(2021, 10, 1) }
+
+      it 'returns the publication for September' do
+        expect(current_publication_date).to eq(Date.new(2021, 9, 27))
+      end
+    end
+
+    context 'end of October' do
+      let(:base_date) { Date.new(2021, 10, 30) }
+
+      it 'returns the publication for September' do
+        expect(current_publication_date).to eq(Date.new(2021, 9, 27))
+      end
+    end
+
+    context 'beginning of November' do
+      let(:base_date) { Date.new(2021, 11, 1) }
+
+      it 'returns the publication for September' do
+        expect(current_publication_date).to eq(Date.new(2021, 9, 27))
+      end
+    end
+
+    context 'end of November' do
+      let(:base_date) { Date.new(2021, 11, 30) }
+
+      it 'returns the publication for November' do
+        expect(current_publication_date).to eq(Date.new(2021, 11, 22))
+      end
+    end
+  end
+end
