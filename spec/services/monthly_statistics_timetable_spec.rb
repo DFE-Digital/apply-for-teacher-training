e384c60335c8c026c4127bd44f46800cc67e59a1
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2024-10-15 16:02:08 +0100

    Stop generating monthly statistics during the first month of cycle

--- spec/services/monthly_statistics_timetable_spec.rb
+++ spec/services/monthly_statistics_timetable_spec.rb
@@ -2,16 +2,25 @@ require 'rails_helper'
 
 RSpec.describe MonthlyStatisticsTimetable do
   describe '#generate_monthly_statistics?' do
+    let(:report_months) { (1..12).to_a - [CycleTimetable.find_opens.to_date.month] }
+
     it 'returns true if the monthly report is scheduled to run on the current date' do
-      1.upto(12).each do |month|
+      report_months.each do |month|
         travel_temporarily_to(described_class.third_monday_of_the_month(Date.new(RecruitmentCycle.current_year, month, 1))) do
           expect(described_class.generate_monthly_statistics?).to be true
         end
       end
     end
 
+    it 'returns false if within the first month of the cycle opening' do
+      find_opens_month = CycleTimetable.find_opens.to_date.month
+      travel_temporarily_to(described_class.third_monday_of_the_month(Date.new(RecruitmentCycle.current_year, find_opens_month, 1))) do
+        expect(described_class.generate_monthly_statistics?).to be false
+      end
+    end
+
     it 'returns false if the monthly report is not scheduled to run on the current date' do
-      1.upto(12).each do |month|
+      report_months.each do |month|
         travel_temporarily_to(RecruitmentCycle.current_year, month, 1) do
           expect(described_class.generate_monthly_statistics?).to be false
         end
@@ -20,13 +29,6 @@ RSpec.describe MonthlyStatisticsTimetable do
   end
 
   describe '.next_publication_date' do
-    let!(:current_report) do
-      create(:monthly_statistics_report, :v1, month: '2021-12')
-    end
-    let!(:previous_report) do
-      create(:monthly_statistics_report, :v1, month: '2021-11')
-    end
-
     context 'when today is before the publishing date in the current month' do
       it 'returns the date of this month’s report' do
         travel_temporarily_to(Date.new(2021, 12, 21)) do
@@ -39,6 +41,32 @@ RSpec.describe MonthlyStatisticsTimetable do
       it 'returns the date of next month’s report' do
         travel_temporarily_to(Date.new(2021, 12, 28)) do
           expect(described_class.next_publication_date).to eq(Date.new(2022, 1, 24))
+        end
+      end
+    end
+
+    context 'when today is before find opens' do
+      it 'returns 4th monday in the month after find opens' do
+        travel_temporarily_to(Date.new(2024, 9, 28)) do
+          expect(described_class.next_publication_date).to eq(Date.new(2024, 11, 25))
+        end
+      end
+    end
+  end
+
+  describe '#last_generation_date' do
+    context 'when it in the month after find opens' do
+      it 'returns date two months ago' do
+        travel_temporarily_to(Date.new(2024, 11, 1)) do
+          expect(described_class.last_generation_date).to eq(Date.new(2024, 9, 16))
+        end
+      end
+    end
+
+    context 'when it is three months after find_opens' do
+      it 'returns date one month ago' do
+        travel_temporarily_to(Date.new(2024, 12, 1)) do
+          expect(described_class.last_generation_date).to eq(Date.new(2024, 11, 18))
         end
       end
     end
