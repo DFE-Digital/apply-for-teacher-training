9688bd0d688afe088e67af847a70c2734999c4bc
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2024-10-22 11:19:55 +0100

    Only send inactive prompts when candidates can add applications

--- spec/workers/send_apply_to_another_course_when_inactive_email_to_candidates_worker_spec.rb
+++ spec/workers/send_apply_to_another_course_when_inactive_email_to_candidates_worker_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe SendApplyToAnotherCourseWhenInactiveEmailToCandidatesWorker, :sidekiq do
-  describe '#perform' do
+  describe '#perform', time: mid_cycle do
     let(:application_forms) { create_list(:completed_application_form, 2) }
 
     subject(:perform) { described_class.new.perform }
@@ -28,6 +28,29 @@ RSpec.describe SendApplyToAnotherCourseWhenInactiveEmailToCandidatesWorker, :sid
       }.to not_change(ActionMailer::Base.deliveries, :count)
      .and not_change(application_forms.first.chasers_sent.apply_to_another_course_after_30_working_days, :count)
      .and not_change(application_forms.second.chasers_sent.apply_to_another_course_after_30_working_days, :count)
+    end
+  end
+
+  describe '#perform after the apply deadline', time: after_apply_deadline do
+    let(:application_forms) { create_list(:completed_application_form, 2) }
+
+    subject(:perform) { described_class.new.perform }
+
+    before do
+      stub_const('SendApplyToAnotherCourseWhenInactiveEmailToCandidatesWorker::BATCH_SIZE', 3) # to make sure it loops through all the applications
+
+      create(:application_choice, :inactive, application_form: application_forms.first)
+      create(:application_choice, :inactive, application_form: application_forms.second)
+      create_list(:application_choice, 5, :inactive)
+      perform
+    end
+
+    it 'does not send any emails' do
+      expect {
+        perform
+      }.to not_change(ActionMailer::Base.deliveries, :count)
+             .and not_change(application_forms.first.chasers_sent.apply_to_another_course_after_30_working_days, :count)
+                    .and not_change(application_forms.second.chasers_sent.apply_to_another_course_after_30_working_days, :count)
     end
   end
 
