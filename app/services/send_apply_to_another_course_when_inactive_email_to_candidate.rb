5e289a81eb783c217771a901bc6b607996defae4
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2024-10-22 13:07:01 +0100

    Do not send email to candidates who have too many choices already

--- app/services/send_apply_to_another_course_when_inactive_email_to_candidate.rb
+++ app/services/send_apply_to_another_course_when_inactive_email_to_candidate.rb
@@ -1,5 +1,6 @@
 class SendApplyToAnotherCourseWhenInactiveEmailToCandidate
   def self.call(application_form)
+    return if application_form.maximum_number_of_choices_reached?
     return if already_sent_to?(application_form)
 
     CandidateMailer.apply_to_another_course_after_30_working_days(application_form).deliver_later
