295bb393aefa2a9500d83523c9b805e5300e4fc8
David Young <david@dcyoung.dev>
2024-10-24 09:27:02 +0100

    DfE Analytics

--- config/initializers/def_analytics.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.configure do
-  config.analytics = config_for(:analytics)
-  config.analytics_pii = config_for(:analytics_pii)
-  config.analytics_blocklist = config_for(:analytics_blocklist)
-end
