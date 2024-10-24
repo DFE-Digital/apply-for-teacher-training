919ca7ecc5bce80a54ef5571d1cb2c6e3a65487f
CatalinVoineag <11318084+CatalinVoineag@users.noreply.github.com>
2024-10-24 15:55:49 +0100

    More specs

--- spec/services/data_migrations/correct_hesa_ethnicity_spec.rb
+++ spec/services/data_migrations/correct_hesa_ethnicity_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe DataMigrations::CorrectHesaEthnicity do
   describe '#change' do
-    it 'sets the hesa_ethnicity to 179' do
+    it 'sets the hesa_ethnicity to 179 without chaning any other keys' do
       form_2023 = create(
         :application_form,
         recruitment_cycle_year: 2023,
@@ -10,6 +10,11 @@ RSpec.describe DataMigrations::CorrectHesaEthnicity do
           ethnic_group: 'White',
           ethnic_background: 'Another White background',
           hesa_ethnicity: '160',
+          sex: 'male',
+          hesa_sex: 11,
+          disabilities: ['I do not have any of these disabilities or health conditions'],
+          free_school_meals: 'Prefer not to say',
+          hesa_disabilities: ['95'],
         },
       )
 
@@ -47,6 +52,11 @@ RSpec.describe DataMigrations::CorrectHesaEthnicity do
         described_class.new.change
       }.to change { form_2023.reload.equality_and_diversity['hesa_ethnicity'] }.from('160').to('179')
         .and change { form_2024.reload.equality_and_diversity['hesa_ethnicity'] }.from('160').to('179')
+        .and not_change { form_2023.reload.equality_and_diversity['sex'] }.from('male')
+        .and not_change { form_2023.reload.equality_and_diversity['hesa_sex'] }.from(11)
+        .and not_change { form_2023.reload.equality_and_diversity['disabilities'] }.from(['I do not have any of these disabilities or health conditions'])
+        .and not_change { form_2023.reload.equality_and_diversity['free_school_meals'] }.from('Prefer not to say')
+        .and not_change { form_2023.reload.equality_and_diversity['hesa_disabilities'] }.from(['95'])
         .and not_change { form_2025.reload.equality_and_diversity['hesa_ethnicity'] }.from('160')
         .and not_change { correct_hesa_ethnicity.reload.equality_and_diversity['hesa_ethnicity'] }.from('160')
     end
