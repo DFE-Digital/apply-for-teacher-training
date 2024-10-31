0dfb42d9bd24d488734e626bb6461ac03fb716b1
David Young <david@dcyoung.dev>
2024-10-31 12:35:49 +0000

    Added migration to delete all Audits before 4 October 2022

---
+++ spec/services/data_migrations/destroy_all_old_audit_records_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe DataMigrations::DestroyAllOldAuditRecords do
+  it 'destroys all old audit records' do
+    # An audit is created on startup with `auditable_type: "SiteSetting"`
+    destroyable_audit_1 = create(:audit, created_at: Time.zone.local(2020, 10, 3))
+    destroyable_audit_2 = create(:audit, created_at: Time.zone.local(2020, 10, 3))
+    destroyable_audit_3 = create(:audit, created_at: Time.zone.local(2021, 10, 3))
+    destroyable_audit_4 = create(:audit, created_at: Time.zone.local(2022, 10, 3))
+    destroyable_audit_5 = create(:audit, created_at: Time.zone.local(2022, 10, 3, 23, 59, 59)) # 1 second before the timestamp
+    safe_audit = create(:audit, created_at: Time.zone.local(2022, 10, 4, 0, 0, 1)) # 1 second after the timestamp
+
+    described_class.new.change
+
+    expect(Audited::Audit.all).to include(safe_audit)
+    expect(Audited::Audit.all).not_to include(
+      destroyable_audit_1,
+      destroyable_audit_2,
+      destroyable_audit_3,
+      destroyable_audit_4,
+      destroyable_audit_5,
+    )
+  end
+end
