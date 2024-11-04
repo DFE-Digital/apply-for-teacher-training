608ff973ea9f37dc61d7d8e8f74ccf80ee22b97b
David Young <david@dcyoung.dev>
2024-11-04 16:57:46 +0000

    Removed conditional causing nil or false value

--- app/lib/dsi_profile.rb
+++ app/lib/dsi_profile.rb
@@ -7,6 +7,6 @@ class DsiProfile
     fields_to_update[:first_name] = dfe_user.first_name if dfe_user.first_name.present?
     fields_to_update[:last_name] = dfe_user.last_name if dfe_user.last_name.present?
 
-    local_user.update(fields_to_update) if fields_to_update.present?
+    local_user.update(fields_to_update)
   end
 end
