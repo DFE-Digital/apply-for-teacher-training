DFE-Digital/apply-for-teacher-training

commit f9683cda42ed0bf01f8ff6ffbc4c36a74fde8653

Check validity of User when using DfE Sign in callback (#10020)
## Context

We have `ActiveRecord::RecordNotUnique` errors appearing in Sentry
related to the email address of a DfE Sign in user already being used.
The current implementation does not handle this error.  

## Changes proposed in this pull request

- We have added email uniqueness and presence validation to SupportUser
and ProviderUser.
- We have updated the `DfESignInController#callback` to check for a
successful `update` before redirecting appropriately

app/lib/dsi_profile.rb
@@ -6,6 +6,7 @@ class DsiProfile
     end
     fields_to_update[:first_name] = dfe_user.first_name if dfe_user.first_name.present?
     fields_to_update[:last_name] = dfe_user.last_name if dfe_user.last_name.present?
+
     local_user.update(fields_to_update) if fields_to_update.present?
   end
 end

app/controllers/dfe_sign_in_controller.rb
@@ -10,8 +10,7 @@ class DfESignInController < ActionController::Base
     @target_path = session['post_dfe_sign_in_path']
     @local_user = local_user
 
-    if @local_user
-      DsiProfile.update_profile_from_dfe_sign_in(dfe_user: @dfe_sign_in_user, local_user: @local_user)
+    if @local_user && DsiProfile.update_profile_from_dfe_sign_in(dfe_user: @dfe_sign_in_user, local_user: @local_user)
       @local_user.update!(last_signed_in_at: Time.zone.now)
 
       if @local_user.is_a?(SupportUser)

app/models/dfe_sign_in_user.rb
@@ -28,12 +28,12 @@ class DfESignInUser
 
   def self.begin_session!(session, omniauth_payload)
     session['dfe_sign_in_user'] = {
-      'email_address' => omniauth_payload['info']['email'],
+      'email_address' => omniauth_payload.dig('info', 'email'),
       'dfe_sign_in_uid' => omniauth_payload['uid'],
-      'first_name' => omniauth_payload['info']['first_name'],
-      'last_name' => omniauth_payload['info']['last_name'],
+      'first_name' => omniauth_payload.dig('info', 'first_name'),
+      'last_name' => omniauth_payload.dig('info', 'last_name'),
       'last_active_at' => Time.zone.now,
-      'id_token' => omniauth_payload['credentials']['id_token'],
+      'id_token' => omniauth_payload.dig('credentials', 'id_token'),
     }
   end
 

app/models/provider_user.rb
@@ -8,8 +8,12 @@ class ProviderUser < ApplicationRecord
   attr_accessor :impersonator
 
   validates :dfe_sign_in_uid, uniqueness: true, allow_nil: true
+  validates :email_address,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            format: { with: URI::MailTo::EMAIL_REGEXP }
 
-  before_save :downcase_email_address
+  normalizes :email_address, with: ->(email) { email.downcase.strip }
 
   audited except: [:last_signed_in_at]
   has_associated_audits
@@ -59,11 +63,5 @@ class ProviderUser < ApplicationRecord
 
   def can_manage_organisations?
     provider_permissions.exists?(manage_organisations: true)
-  end
-
-private
-
-  def downcase_email_address
-    self.email_address = email_address.downcase
   end
 end

app/models/support_user.rb
@@ -4,9 +4,12 @@ class SupportUser < ApplicationRecord
 
   attr_accessor :impersonated_provider_user
   validates :dfe_sign_in_uid, presence: true
-  validates :email_address, presence: true, uniqueness: true
+  validates :email_address,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            format: { with: URI::MailTo::EMAIL_REGEXP }
 
-  before_validation :downcase_email_address
+  normalizes :email_address, with: ->(email) { email.downcase.strip }
 
   audited except: [:last_signed_in_at]
 
@@ -23,11 +26,5 @@ class SupportUser < ApplicationRecord
 
   def display_name
     [first_name, last_name].join(' ').presence || email_address
-  end
-
-private
-
-  def downcase_email_address
-    self.email_address = email_address.downcase
   end
 end

spec/lib/dsi_profile_spec.rb
@@ -2,9 +2,8 @@ require 'rails_helper'
 
 RSpec.describe DsiProfile do
   describe '#update_profile_from_dfe_sign_in' do
-    let(:provider_user) { create(:provider_user) }
-    let(:support_user) { create(:provider_user) }
-    let(:email_address) { Faker::Internet.email }
+    let(:provider_user) { create(:provider_user, email_address: 'original_provider@email.address') }
+    let(:email_address) { 'provider_user@email.address' }
     let(:dfe_user) do
       DfESignInUser.new(
         email_address:,
@@ -14,11 +13,12 @@ RSpec.describe DsiProfile do
       )
     end
 
-    context 'local_user\'s email_address' do
+    context "local_user's email_address" do
       it 'is updated if uid is previously known' do
-        expect {
-          described_class.update_profile_from_dfe_sign_in dfe_user:, local_user: provider_user
-        }.to change(provider_user, :email_address).to(email_address)
+        result = described_class.update_profile_from_dfe_sign_in dfe_user:, local_user: provider_user
+
+        expect(result).to be_truthy
+        expect(provider_user.reload.email_address).to eq(email_address)
       end
 
       it 'is not updated if uid is not yet established' do
@@ -40,6 +40,17 @@ RSpec.describe DsiProfile do
         expect {
           described_class.update_profile_from_dfe_sign_in dfe_user: dfe_user_no_email, local_user: provider_user
         }.not_to change(provider_user, :email_address)
+      end
+
+      context 'the email is already used by another user' do
+        it 'is not updated' do
+          _other_provider_user = create(:provider_user, email_address: email_address)
+
+          result = described_class.update_profile_from_dfe_sign_in(dfe_user: dfe_user, local_user: provider_user)
+
+          expect(result).to be_falsey
+          expect(provider_user.reload.email_address).not_to eq(email_address)
+        end
       end
     end
 

spec/models/provider_user_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe ProviderUser do
+  describe 'validations' do
+    let!(:existing_provider_user) { create(:provider_user) }
+
+    it { is_expected.to validate_presence_of(:email_address) }
+    it { is_expected.to validate_uniqueness_of(:email_address).case_insensitive }
+  end
+
   describe '#downcase_email_address' do
     it 'saves email_address in lower case' do
       provider_user = create(:provider_user, email_address: 'Bob.Roberts@example.com')

spec/models/support_user_spec.rb
@@ -2,11 +2,10 @@ require 'rails_helper'
 
 RSpec.describe SupportUser do
   describe 'validations' do
-    it 'flags email addresses that differ only by case as duplicates' do
-      create(:support_user, email_address: 'bob@example.com')
-      duplicate_support_user = build(:support_user, email_address: 'Bob@example.com')
-      expect(duplicate_support_user).not_to be_valid
-    end
+    let!(:existing_support_user) { create(:support_user) }
+
+    it { is_expected.to validate_presence_of(:email_address) }
+    it { is_expected.to validate_uniqueness_of(:email_address).case_insensitive }
   end
 
   describe '#downcase_email_address' do

commit 523b78ef11624f2fbf694df58bb0d85b7caadc76

Updated DSI Profile logic (#10023)
## Context

A bug was introduced in #10020 which stops the sign in by-pass from
working locally or on review apps.

## Changes proposed in this pull request

- Removed conditional that would return `nil` if there are no updates to
be done

app/lib/dsi_profile.rb
@@ -7,6 +7,6 @@ class DsiProfile
     fields_to_update[:first_name] = dfe_user.first_name if dfe_user.first_name.present?
     fields_to_update[:last_name] = dfe_user.last_name if dfe_user.last_name.present?
 
-    local_user.update(fields_to_update) if fields_to_update.present?
+    local_user.update(fields_to_update)
   end
 end