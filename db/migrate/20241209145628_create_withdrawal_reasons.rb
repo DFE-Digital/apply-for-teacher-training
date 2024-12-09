5f0b532216474ecc07dfe1ba2dd4c21fe93d9132
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2024-12-09 15:31:14 +0000

    migrations for withdrawal reasons

---
+++ db/migrate/20241209145628_create_withdrawal_reasons.rb
@@ -0,0 +1,11 @@
+class CreateWithdrawalReasons < ActiveRecord::Migration[8.0]
+  def change
+    create_table :withdrawal_reasons do |t|
+      t.string :reason, index: true
+      t.text :comment
+      t.references :application_choice, null: false, foreign_key: { on_delete: :cascade }
+
+      t.timestamps
+    end
+  end
+end
