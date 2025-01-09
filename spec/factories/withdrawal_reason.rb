63bf7c13e8f76a90df8ec23b41891ab290101d84
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2025-01-09 10:31:59 +0000

    Add job to remove orphan records, withdrawal reasons

--- spec/factories/withdrawal_reason.rb
+++ spec/factories/withdrawal_reason.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :withdrawal_reason, class: 'WithdrawalReason' do
+    application_choice
     reason { 'applying-to-another-provider.accepted_another-offer' }
+
+    trait :published do
+      status { 'published' }
+    end
+
+    trait :draft do
+      status { 'draft' }
+    end
   end
 end
