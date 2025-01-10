28d13df3ab1eac3ebefd757bd48ee23869649727
CatalinVoineag <11318084+CatalinVoineag@users.noreply.github.com>
2025-01-10 16:14:03 +0000

    Add button_to_as_link component

---
+++ app/components/utility/button_to_as_link_component.rb
@@ -0,0 +1,19 @@
+class ButtonToAsLinkComponent < ViewComponent::Base
+  def initialize(name:, path:, classes: [])
+    @name = name
+    @path = path
+    @classes = classes
+  end
+
+private
+
+  def styling_classes
+    default = 'govuk-link govuk-link-button'
+
+    @classes.each do |css_class|
+      default << " #{css_class}"
+    end
+
+    default
+  end
+end
