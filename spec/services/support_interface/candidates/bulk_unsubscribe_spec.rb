5a122adb9108be4215f42755c8e069b0a4e05991
David Young <david@dcyoung.dev>
2025-01-10 09:21:11 +0000

    Added spec to show that unknown email addresses are handled gracefully

--- spec/services/support_interface/candidates/bulk_unsubscribe_spec.rb
+++ spec/services/support_interface/candidates/bulk_unsubscribe_spec.rb
@@ -23,5 +23,16 @@ RSpec.describe SupportInterface::Candidates::BulkUnsubscribe do
       expect(candidate1.reload).to be_unsubscribed_from_emails
       expect(candidate2.reload).to be_unsubscribed_from_emails
     end
+
+    it 'can handle unknown email addresses' do
+      candidate1 = create(:candidate, email_address: 'candidate_1@email.address')
+      candidate2 = create(:candidate, email_address: 'candidate_2@email.address')
+
+      described_class.bulk_unsubscribe(['unknown@email.address',
+                                        'this is not an email address',
+                                        'candidate_1@email.address'])
+      expect(candidate1.reload).to be_unsubscribed_from_emails
+      expect(candidate2.reload).not_to be_unsubscribed_from_emails
+    end
   end
 end
