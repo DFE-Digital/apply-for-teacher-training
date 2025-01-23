751bb32f6b13c83d44c53103b7168edae4cd301b
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2025-01-23 11:32:58 +0000

    Add recruitment cycles to database

---
+++ app/services/data_migrations/add_all_recruitment_cycle_timetables_to_database.rb
@@ -0,0 +1,30 @@
+module DataMigrations
+  class AddAllRecruitmentCycleTimetablesToDatabase
+    TIMESTAMP = 20250123092938
+    MANUAL_RUN = false
+
+    def change
+      CYCLE_DATES.each do |recruitment_cycle_year, attributes|
+        dates = attributes.filter do |date|
+          %i[
+            apply_1_deadline
+            apply_2_deadline
+            show_deadline_banner
+            show_summer_recruitment_banner
+          ].exclude? date
+        end
+
+        dates[:christmas_holiday] = dates.dig(:holidays, :christmas)
+        dates[:easter_holiday] = dates.dig(:holidays, :easter)
+        dates[:decline_by_default] = dates[:find_closes] - 1.day
+        dates = dates.filter { |date| date != :holidays }
+
+        RecruitmentCycleTimetable.create(
+          recruitment_cycle_year:,
+          real_timetable: true,
+          **dates,
+        )
+      end
+    end
+  end
+end
