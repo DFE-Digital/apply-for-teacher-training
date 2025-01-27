52db9de03dd58c8849737917892f80f4e6ecc61a
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2025-01-27 17:01:53 +0000

    Data migration to add timetables

---
+++ spec/services/data_migrations/add_all_recruitment_cycle_timetables_to_database_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe DataMigrations::AddAllRecruitmentCycleTimetablesToDatabase do
+  it 'adds all recruitment cycles to database' do
+    described_class.new.change
+    expect(RealCycleTimetable.pluck(:recruitment_cycle_year))
+      .to contain_exactly(2019, 2020, 2021, 2022, 2023, 2024, 2025, 2026, 2027)
+  end
+
+  it 'classifies the timetables as real' do
+    described_class.new.change
+    expect(RealCycleTimetable.where(type: 'RealCycleTimetable').pluck(:recruitment_cycle_year))
+      .to contain_exactly(2019, 2020, 2021, 2022, 2023, 2024, 2025, 2026, 2027)
+  end
+
+  it 'handles holidays as expected' do
+    described_class.new.change
+    year_without_holidays = RealCycleTimetable.find_by(recruitment_cycle_year: 2020)
+    year_with_holidays = RealCycleTimetable.find_by(recruitment_cycle_year: 2021)
+
+    expect(year_without_holidays.easter_holiday).to be_nil
+    expect(year_without_holidays.christmas_holiday).to be_nil
+
+    expect(year_with_holidays.easter_holiday.begin).to eq Date.new(2021, 4, 2)
+    expect(year_with_holidays.easter_holiday.end).to eq Date.new(2021, 4, 17)
+
+    expect(year_with_holidays.christmas_holiday.begin).to eq Date.new(2020, 12, 20)
+    expect(year_with_holidays.christmas_holiday.end).to eq Date.new(2021, 1, 2)
+  end
+end
