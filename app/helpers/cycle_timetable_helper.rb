2f972f92273a9c26f0a943e241e2d1d4945ee04d
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2025-01-30 16:11:57 +0000

    Update test helpers to use db backed timetables

--- app/helpers/cycle_timetable_helper.rb
+++ app/helpers/cycle_timetable_helper.rb
@@ -1,51 +1,71 @@
 module CycleTimetableHelper
 module_function
 
-  def after_find_opens(year = CycleTimetable.current_year)
-    CycleTimetable.find_opens(year) + 1.day
+  def after_find_opens(year = nil)
+    timetable = get_timetable(year)
+    timetable.find_opens_at + 1.day
   end
 
-  def after_find_reopens(year = CycleTimetable.current_year)
-    CycleTimetable.find_reopens(year) + 1.day
+  def after_find_reopens(year = nil)
+    timetable = get_timetable(year + 1)
+    timetable.find_opens_at + 1.day
   end
 
-  def mid_cycle(year = CycleTimetable.current_year)
-    CycleTimetable.apply_opens(year) + 1.day
+  def mid_cycle(year = nil)
+    timetable = get_timetable(year)
+    timetable.apply_opens_at + 1.day
   end
 
-  def after_apply_deadline(year = CycleTimetable.current_year)
-    CycleTimetable.apply_deadline(year) + 1.day
+  def after_apply_deadline(year = nil)
+    timetable = get_timetable(year)
+    timetable.apply_deadline_at + 1.day
   end
 
-  def cancel_application_deadline(year = CycleTimetable.current_year)
-    CycleTimetable.apply_deadline(year)
+  def cancel_application_deadline(year = nil)
+    timetable = get_timetable(year)
+    timetable.apply_deadline_at
   end
 
-  def before_apply_deadline(year = CycleTimetable.current_year)
-    CycleTimetable.apply_deadline(year) - 1.day
+  def before_apply_deadline(year = nil)
+    timetable = get_timetable(year)
+    timetable.apply_deadline_at - 1.day
   end
 
-  def after_apply_reopens(year = CycleTimetable.next_year)
-    CycleTimetable.apply_reopens(year) + 1.day
+  def after_apply_reopens(year = nil)
+    timetable = get_timetable(year + 1)
+    timetable.apply_opens_at + 1.day
   end
 
-  def after_reject_by_default(year = CycleTimetable.current_year)
-    CycleTimetable.reject_by_default(year) + 1.day
+  def after_reject_by_default(year = nil)
+    timetable = get_timetable(year)
+    timetable.reject_by_default_at(year) + 1.day
   end
 
-  def reject_by_default_run_date(year = CycleTimetable.current_year)
-    CycleTimetable.reject_by_default(year) + 1.second
+  def reject_by_default_run_date(year = nil)
+    timetable = get_timetable(year)
+    timetable.reject_by_default_at + 1.second
   end
 
-  def decline_by_default_run_date(year = CycleTimetable.current_year)
-    CycleTimetable.decline_by_default_date(year) + 1.second
+  def decline_by_default_run_date(year = nil)
+    timetable = get_timetable(year)
+    timetable.decline_by_default_at + 1.second
   end
 
-  def application_deadline_has_passed_email_date(year = RecruitmentCycleTimetable.current_year)
-    RecruitmentCycleTimetable.find_by(recruitment_cycle_year: year).apply_deadline_at + 1.day
+  def application_deadline_has_passed_email_date(year = nil)
+    timetable = get_timetable(year)
+    timetable.apply_deadline_at + 1.day
   end
 
-  def reject_by_default_explainer_date(year = RecruitmentCycleTimetable.current_year)
-    RecruitmentCycleTimetable.find_by(recruitment_cycle_year: year).reject_by_default_at + 1.day
+  def reject_by_default_explainer_date(year = nil)
+    timetable = get_timetable(year)
+    timetable.reject_by_default_at + 1.day
+  end
+
+  def get_timetable(year = nil)
+    if year.present?
+      RecruitmentCycleTimetable.find_by(recruitment_cycle_year: year)
+    else
+      RecruitmentCycleTimetable.current_timetable
+    end
   end
 end
