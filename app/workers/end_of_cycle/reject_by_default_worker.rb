b0690d06fb1098f46ae3f31cdd17158152186414
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2025-02-03 12:12:11 +0000

    Use new timetables model to schedule EOC jobs

--- app/workers/end_of_cycle/reject_by_default_worker.rb
+++ app/workers/end_of_cycle/reject_by_default_worker.rb
@@ -5,7 +5,7 @@ module EndOfCycle
     BATCH_SIZE = 120
 
     def perform(force: false)
-      return unless CycleTimetable.run_reject_by_default? || force
+      return unless EndOfCycle::JobTimetabler.new.run_reject_by_default? || force
 
       BatchDelivery.new(relation:, batch_size: BATCH_SIZE).each do |batch_time, applications|
         RejectByDefaultSecondaryWorker.perform_at(batch_time, applications.pluck(:id))
