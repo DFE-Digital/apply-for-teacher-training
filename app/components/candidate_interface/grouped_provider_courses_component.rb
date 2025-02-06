5bf447851182a5c02bdad512224eedb1defa031d
Avin Hurry <avinhurry@icloud.com>
2025-02-06 15:16:14 +0000

    Remove provider regions translations

--- app/components/candidate_interface/grouped_provider_courses_component.rb
+++ app/components/candidate_interface/grouped_provider_courses_component.rb
@@ -3,7 +3,7 @@ module CandidateInterface
     include ViewHelper
 
     def label_for(region_code)
-      region_code.present? ? I18n.t("provider_regions.#{region_code}") : I18n.t('provider_regions.no_region_specified')
+  
     end
 
     def courses_grouped_by_provider_and_region
