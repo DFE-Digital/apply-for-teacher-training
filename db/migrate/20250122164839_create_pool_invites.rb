da4e6818da9c2b53179dbd16723bb8d1d96e082b
CatalinVoineag <11318084+CatalinVoineag@users.noreply.github.com>
2025-02-18 11:09:00 +0000

    Remove cascade delete on provider user for pool invites and dismissals

--- db/migrate/20250122164839_create_pool_invites.rb
+++ db/migrate/20250122164839_create_pool_invites.rb
@@ -3,7 +3,7 @@ class CreatePoolInvites < ActiveRecord::Migration[8.0]
     create_table :pool_invites do |t|
       t.references :candidate, null: false, foreign_key: { on_delete: :cascade }
       t.references :provider, null: false, foreign_key: { on_delete: :cascade }
-      t.references :invited_by, null: false, foreign_key: { to_table: :provider_users, on_delete: :cascade }
+      t.references :invited_by, null: false, foreign_key: { to_table: :provider_users }
       t.references :course, null: false, foreign_key: { on_delete: :cascade }
 
       t.timestamps
