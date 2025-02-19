4e37e184263e764310509c38742e5a4432da9755
Avin Hurry <avinhurry@icloud.com>
2025-02-19 17:43:27 +0000

    [653] Ensure the completed question is unselected
    Prior to explicitly changing it.
    
    The fix is for the following sections:
    
    - Contact details

--- app/forms/candidate_interface/contact_details_form.rb
+++ app/forms/candidate_interface/contact_details_form.rb
@@ -110,7 +110,7 @@ module CandidateInterface
       attributes[:postcode] = nil if international?
 
       unless valid_for_submission?
-        attributes = attributes.merge(contact_details_completed: false)
+        attributes = attributes.merge(contact_details_completed: nil)
       end
       application_form.update(attributes)
     end
