6b90bca6e9ed78dafa44fb1cde55a5d2e83ee0b0
CatalinVoineag <11318084+CatalinVoineag@users.noreply.github.com>
2025-03-03 13:46:06 +0000

    Candidate pool filters geolocation filter
    This commit adds 2 filters to the candidate pool index page.
    
    Location based filter and ordering and visa sponsorship.
    
    I've used the geokit rails gem to calculate the distance from the
    filtered location to the actual site location.
    
    The location filter also makes calls to Google maps autocomplete api to
    help the user find postcodes cities, etc. Sadly this endpoint doesn't
    return lat and long of each location.
    
    So another api call will be done when you apply the filters to the
    google maps places details endpoint to get the lat and long which are
    used to get all the sites within a range of a location.
    
    Once you select a location filter, we order the results based on the
    proximity of the sites to that location.
    
    These api calls are cached.
    
    I've also changed the Pool::Candidate to return application forms. This
    way is easier to query and read the queries, I think and also all the
    information for the index page is on the application form.

--- spec/system/provider_interface/candidate_pool/provider_views_candidate_pool_list_spec.rb
+++ spec/system/provider_interface/candidate_pool/provider_views_candidate_pool_list_spec.rb
@@ -6,22 +6,34 @@ RSpec.describe 'Providers views candidate pool list' do
 
   let(:current_provider) { create(:provider) }
 
+  before do
+    set_rejected_candidate_form
+    set_declined_candidate_form
+    set_visa_sponsorship_candidate_form
+  end
+
   scenario 'View candidates' do
     given_i_am_a_provider_user_with_dfe_sign_in
     and_provider_user_exists
-    and_there_are_candidates_for_candidate_pool
     and_provider_is_opted_in_to_candidate_pool
     and_i_sign_in_to_the_provider_interface
 
     when_i_visit_the_find_candidates_page
 
     then_i_expect_to_see_eligible_candidates_order_by_application_form_submitted_at
+
+    when_i_filter_by_location
+    then_i_expect_to_see_filtered_candidates([@rejected_candidate_form, @visa_sponsorship_form])
+    when_i_filter_by_visa_sponsorship
+    then_i_expect_to_see_filtered_candidates([@visa_sponsorship_form])
+
+    when_i_click('Clear filters')
+    then_i_expect_to_see_eligible_candidates_order_by_application_form_submitted_at
   end
 
   scenario 'Provider cannot view candidates if not invited' do
     given_i_am_a_provider_user_with_dfe_sign_in
     and_provider_user_exists
-    and_there_are_candidates_for_candidate_pool
     and_i_sign_in_to_the_provider_interface
 
     when_i_visit_the_find_candidates_page
@@ -38,16 +50,7 @@ RSpec.describe 'Providers views candidate pool list' do
     provider_user_exists_in_apply_database(provider_code: current_provider.code)
   end
 
-  def and_there_are_candidates_for_candidate_pool
-    rejected_candidate = create(:candidate, pool_status: 'opt_in')
-    @rejected_candidate_form = create(
-      :application_form,
-      :completed,
-      candidate: rejected_candidate,
-      submitted_at: 1.day.ago,
-    )
-    create(:application_choice, :rejected, application_form: @rejected_candidate_form)
-
+  def set_declined_candidate_form
     declined_candidate = create(:candidate, pool_status: 'opt_in')
     @declined_candidate_form = create(
       :application_form,
@@ -69,6 +72,61 @@ RSpec.describe 'Providers views candidate pool list' do
     create(:application_choice, :declined, application_form: previous_cycle_form)
   end
 
+  def set_rejected_candidate_form
+    rejected_candidate = create(:candidate, pool_status: 'opt_in')
+    @rejected_candidate_form = create(
+      :application_form,
+      :completed,
+      candidate: rejected_candidate,
+      submitted_at: 1.day.ago,
+    )
+    aa_teamworks = create(
+      :site,
+      latitude: 51.4524877,
+      longitude: -0.1204749,
+      provider: current_provider,
+    )
+    course_option = create(
+      :course_option,
+      site: aa_teamworks,
+      course: create(:course, provider: current_provider),
+    )
+    create(
+      :application_choice,
+      :rejected,
+      application_form: @rejected_candidate_form,
+      course_option:,
+    )
+  end
+
+  def set_visa_sponsorship_candidate_form
+    visa_sponsorship_candidate = create(:candidate, pool_status: 'opt_in')
+    @visa_sponsorship_form = create(
+      :application_form,
+      :completed,
+      candidate: visa_sponsorship_candidate,
+      submitted_at: 6.hours.ago,
+      right_to_work_or_study: :no,
+    )
+    aa_teamworks = create(
+      :site,
+      latitude: 51.4524880,
+      longitude: -0.1204752,
+      provider: current_provider,
+    )
+    course_option = create(
+      :course_option,
+      site: aa_teamworks,
+      course: create(:course, provider: current_provider),
+    )
+    create(
+      :application_choice,
+      :rejected,
+      application_form: @visa_sponsorship_form,
+      course_option:,
+    )
+  end
+
   def and_provider_is_opted_in_to_candidate_pool
     create(:candidate_pool_provider_opt_in, provider: current_provider)
   end
@@ -83,6 +141,7 @@ RSpec.describe 'Providers views candidate pool list' do
     expect(candidates).to eq([
       @rejected_candidate_form.redacted_full_name,
       @declined_candidate_form.redacted_full_name,
+      @visa_sponsorship_form.redacted_full_name,
     ])
   end
 
@@ -94,5 +153,25 @@ RSpec.describe 'Providers views candidate pool list' do
     within('#service-navigation') do
       expect(page).to have_no_css('li', text: 'Find candidates')
     end
+  end
+
+  def when_i_filter_by_location
+    fill_in('original_location', with: 'Manchester')
+    click_link_or_button('Apply filters')
+  end
+
+  def when_i_filter_by_visa_sponsorship
+    check('visa_sponsorship-required')
+    click_link_or_button('Apply filters')
+  end
+
+  def then_i_expect_to_see_filtered_candidates(application_forms)
+    candidates = page.all('.govuk-table__body .govuk-table__row td:first-child').map(&:text)
+
+    expect(candidates).to eq(application_forms.map(&:redacted_full_name))
+  end
+
+  def when_i_click(button)
+    click_link_or_button button
   end
 end
