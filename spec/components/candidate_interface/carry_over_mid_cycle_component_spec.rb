8097f1233bd754a752f2f87e8278be5d8f48c8b4
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2025-03-22 10:31:56 +0000

    Remove unused methods in CycleTimetables and RecruitmentCycle classes

--- spec/components/candidate_interface/carry_over_mid_cycle_component_spec.rb
+++ spec/components/candidate_interface/carry_over_mid_cycle_component_spec.rb
@@ -8,23 +8,27 @@ RSpec.describe CandidateInterface::CarryOverMidCycleComponent do
     expect(result.text).to include('Continue your application')
   end
 
-  [RecruitmentCycleTimetable.previous_timetable, RecruitmentCycleTimetable.current_timetable].each do |timetable|
-    context "after the new recruitment cycle begins year: #{timetable.recruitment_cycle_year}", time: timetable.apply_opens_at do
-      it 'renders the correct academic years' do
-        current_year = timetable.recruitment_cycle_year
-        application_year = current_year - 1
-        application_form = build(:completed_application_form, recruitment_cycle_year: application_year)
-        result = render_inline(described_class.new(application_form:))
+  context 'after the new recruitment cycle begins year' do
+    let(:timetable) { RecruitmentCycleTimetable.current_timetable }
 
-        expect(result.text).to include("You started an application for courses starting in the #{application_year} to #{application_year + 1} academic year, which have now closed.")
-        expect(result.text).to include("Continue your application to apply for courses starting in the #{current_year} to #{current_year + 1} academic year instead.")
-      end
+    before do
+      TestSuiteTimeMachine.travel_permanently_to(timetable.apply_opens_at)
     end
 
-    context 'the application was started two years ago', time: timetable.apply_opens_at do
+    it 'renders the correct academic years' do
+      current_year = timetable.recruitment_cycle_year
+      application_year = current_year - 1
+      application_form = build(:completed_application_form, recruitment_cycle_year: application_year)
+      result = render_inline(described_class.new(application_form:))
+
+      expect(result.text).to include("You started an application for courses starting in the #{application_year} to #{application_year + 1} academic year, which have now closed.")
+      expect(result.text).to include("Continue your application to apply for courses starting in the #{current_year} to #{current_year + 1} academic year instead.")
+    end
+
+    context 'the application was started two years ago' do
       it 'renders the correct academic years' do
-        application_year = timetable.recruitment_cycle_year - 2
-        current_year = RecruitmentCycleTimetable.current_year
+        current_year = timetable.recruitment_cycle_year
+        application_year = current_year - 2
 
         application_form = build(:completed_application_form, recruitment_cycle_year: application_year)
         result = render_inline(described_class.new(application_form:))
