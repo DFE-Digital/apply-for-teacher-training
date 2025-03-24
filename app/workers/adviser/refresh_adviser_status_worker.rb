d570604993cb465c8954cac8b40f8d36edfd5a3d
David Young <david@dcyoung.dev>
2025-03-24 15:09:18 +0000

    Handle API errors when refreshing adviser status

--- app/workers/adviser/refresh_adviser_status_worker.rb
+++ app/workers/adviser/refresh_adviser_status_worker.rb
@@ -6,8 +6,7 @@ class Adviser::RefreshAdviserStatusWorker
 
     application_form = ApplicationForm.find(application_form_id)
 
-    candidate_matchback = Adviser::CandidateMatchback.new(application_form)
-    candidate_matchback_adviser_status = candidate_matchback.teacher_training_adviser_sign_up.adviser_status
+    candidate_matchback_adviser_status = candidate_matchback_adviser_status(application_form)
 
     # The Application Form is in this state by default,
     # We may have only just sent the sign-up to the GIT API and manually updated the Application Form's adviser_status to 'waiting_to_be_assigned'
@@ -15,5 +14,15 @@ class Adviser::RefreshAdviserStatusWorker
     return if candidate_matchback_adviser_status == 'unassigned'
 
     application_form.update!(adviser_status: candidate_matchback_adviser_status)
+  end
+
+private
+
+  def candidate_matchback_adviser_status(application_form)
+    candidate_matchback = Adviser::CandidateMatchback.new(application_form)
+    candidate_matchback.teacher_training_adviser_sign_up.adviser_status
+  rescue GetIntoTeachingApiClient::ApiError
+    # This API often falls over - return `unassigned` we can try again later
+    'unassigned'
   end
 end
