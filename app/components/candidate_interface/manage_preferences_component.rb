5e217a356c39687f6ab28bc563e45d1a84f087c0
CatalinVoineag <11318084+CatalinVoineag@users.noreply.github.com>
2025-03-28 16:43:25 +0000

    specs

--- app/components/candidate_interface/manage_preferences_component.rb
+++ app/components/candidate_interface/manage_preferences_component.rb
@@ -1,33 +1,26 @@
 class CandidateInterface::ManagePreferencesComponent < ViewComponent::Base
-  attr_reader :current_candidate
+  attr_reader :current_candidate, :application_form
 
-  def initialize(current_candidate)
+  def initialize(current_candidate:, application_form:)
     @current_candidate = current_candidate
+    @application_form = application_form
   end
 
   def render?
-    FeatureFlag.active?(:candidate_preferences)
+    FeatureFlag.active?(:candidate_preferences) && application_form.submitted_applications?
+  end
+
+  def pool_opt_in?
+    current_candidate.published_preferences&.last&.opt_in?
   end
 
 private
 
-  def links_list
-    list = [
-      govuk_link_to(
-        'Read about how sharing your application details works',
-        candidate_interface_share_details_path,
-      ),
-    ]
-
+  def path_to_change_preferences
     if current_candidate.published_preferences.any?
-      list.unshift(
-        govuk_link_to(
-          'Change your sharing and location settings',
-          candidate_interface_draft_preference_publish_preferences_path(current_candidate.published_preferences.last),
-        ),
-      )
+      candidate_interface_draft_preference_publish_preferences_path(current_candidate.published_preferences.last)
+    else
+      new_candidate_interface_pool_opt_in_path
     end
-
-    list
   end
 end
