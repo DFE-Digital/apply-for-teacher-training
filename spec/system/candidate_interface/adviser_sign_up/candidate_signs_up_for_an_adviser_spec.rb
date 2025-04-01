0f43226da42bf246f9d013fa0d10e271913e9588
dp-daly <danpatdaly@gmail.com>
2025-04-01 20:59:34 +0100

    Change create action so that it is triggered by review form

--- spec/system/candidate_interface/adviser_sign_up/candidate_signs_up_for_an_adviser_spec.rb
+++ spec/system/candidate_interface/adviser_sign_up/candidate_signs_up_for_an_adviser_spec.rb
@@ -20,17 +20,31 @@ RSpec.describe 'Candidate signs up for an adviser', :js do
     when_i_click_on_the_adviser_cta
     then_i_am_on_the_adviser_sign_up_page
 
-    when_i_click_the_sign_up_button
+    when_i_click_the_continue_button
     then_i_see_validation_errors_for_preferred_teaching_subject
     and_the_validation_error_is_tracked
 
     when_i_select_a_preferred_teaching_subject
-    when_i_click_the_sign_up_button
-    then_i_am_redirected_to_your_details_page
-    and_i_see_the_success_message
-    and_the_adviser_cta_be_replaced_with_the_waiting_to_be_assigned_message
-    and_an_adviser_sign_up_job_is_enqueued
-    and_the_sign_up_is_tracked
+    and_i_click_the_continue_button
+    then_i_am_redirected_to_the_review_page
+    # and_my_adviser_status_is_waiting_to_be_assigned
+
+    # when_i_click_the_change_link
+    # then_i_am_returned_to_the_adviser_sign_up_change
+    # and_my_adviser_status_is_unassigned
+
+    # when_i_change_my_subject_selection
+    # and_click_the_continue_button
+    # then_my_choice_is_updated
+    # and_my_adviser_status_is_waiting_to_be_assigned
+
+    # when_i_click_request_adviser
+    # ! already existing -------
+    # then_i_am_redirected_to_your_details_page
+    # and_i_see_the_success_message
+    # and_the_adviser_cta_be_replaced_with_the_waiting_to_be_assigned_message
+    # and_an_adviser_sign_up_job_is_enqueued
+    # and_the_sign_up_is_tracked
   end
 
   def and_adviser_teaching_subjects_exist
@@ -80,9 +94,10 @@ RSpec.describe 'Candidate signs up for an adviser', :js do
     expect(page).to have_current_path(new_candidate_interface_adviser_sign_up_path)
   end
 
-  def when_i_click_the_sign_up_button
+  def when_i_click_the_continue_button
     click_link_or_button t('application_form.adviser_sign_up.submit_text')
   end
+  alias_method :and_i_click_the_continue_button, :when_i_click_the_continue_button
 
   def then_i_see_validation_errors_for_preferred_teaching_subject
     expect(page).to have_content(
@@ -123,4 +138,11 @@ RSpec.describe 'Candidate signs up for an adviser', :js do
   def and_the_sign_up_is_tracked
     expect(:candidate_signed_up_for_adviser).to have_been_enqueued_as_analytics_events
   end
+
+  def then_i_am_redirected_to_the_review_page
+    expect(page).to have_link(t('application_form.adviser_sign_up.change'))
+    expect(page).to have_button(t('application_form.adviser_sign_up.request'))
+  end
+
+  def and_my_adviser_status_is_waiting_to_be_assigned; end
 end
