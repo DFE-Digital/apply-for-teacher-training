b6f6ee1a12114d343ee8816b1814a1600e8783ba
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2025-04-04 18:03:39 +0100

    Block submission from candidates with student and skilled worker visas

--- app/validators/immigration_status_validator.rb
+++ app/validators/immigration_status_validator.rb
@@ -3,17 +3,24 @@ class ImmigrationStatusValidator < ActiveModel::EachValidator
   include GovukLinkHelper
   include GovukVisuallyHiddenHelper
 
+  VISAS_REQUIRING_SPONSORSHIP = %w[student_visa skilled_worker_visa].freeze
+
   def validate_each(record, attribute, application_choice)
     return if did_not_add_nationality_yet?(application_choice) ||
-              immigration_right_to_work?(application_choice) ||
+              british_or_irish?(application_choice) ||
+              not_requiring_sponsorship?(application_choice) ||
               course_can_sponsor_visa?(application_choice)
 
     record.errors.add(attribute, :immigration_status, link_to_find:)
   end
 
-  def immigration_right_to_work?(application_choice)
-    application_choice.application_form.british_or_irish? ||
-      application_choice.application_form.right_to_work_or_study_yes?
+  def british_or_irish?(application_choice)
+    application_choice.application_form.british_or_irish?
+  end
+
+  def not_requiring_sponsorship?(application_choice)
+    application_choice.application_form.right_to_work_or_study_yes? &&
+      !application_choice.application_form.immigration_status.in?(VISAS_REQUIRING_SPONSORSHIP)
   end
 
   def course_can_sponsor_visa?(application_choice)
