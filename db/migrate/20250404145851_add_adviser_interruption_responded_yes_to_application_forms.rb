5b49af18cb3e8b989319bd2fc1aca798bcbfa8df
dp-daly <danpatdaly@gmail.com>
2025-04-08 15:48:01 +0100

    Change column name to adviser_interruption_response

--- db/migrate/20250404145851_add_adviser_interruption_responded_yes_to_application_forms.rb
+++
@@ -1,5 +0,0 @@
-class AddAdviserInterruptionRespondedYesToApplicationForms < ActiveRecord::Migration[8.0]
-  def change
-    add_column :application_forms, :adviser_interruption_responded_yes, :boolean, default: nil, null: true
-  end
-end
