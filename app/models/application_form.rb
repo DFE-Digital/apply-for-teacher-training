647e54ac0a7f54ac928aa00ec73bc2d3f34b2945
dp-daly <danpatdaly@gmail.com>
2025-04-09 14:56:51 +0100

    Create initial matching logic - Return Adviser::TeachingSubject external identifier if subject match - Currently string must be an exact match with a teaching subject in db - Some safety in place for in case the value is nil

--- app/models/application_form.rb
+++ app/models/application_form.rb
@@ -761,4 +761,14 @@ private
   def prevent_unsave_touches?
     !RequestStore.store[:allow_unsafe_application_choice_touches]
   end
+
+  def degree_matches_with_adviser_teaching_subject?
+    return false unless degrees?
+
+    Adviser::TeachingSubject.find_by(title: recent_degree_subject)
+  end
+
+  def recent_degree_subject
+    application_qualifications.degrees.order('award_year').last.subject.titleize
+  end
 end
