9416e2129b0e96632f63255de4794abf569c2e8c
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2025-04-16 14:17:55 +0100

    Remove CycleTimetable from Helper methods

--- app/services/cycle_timetable.rb
+++ app/services/cycle_timetable.rb
@@ -12,58 +12,6 @@ class CycleTimetable
     end
   end
 
-  def self.next_year
-    current_year + 1
-  end
-
-  def self.previous_year
-    current_year - 1
-  end
-
-  def self.current_date
-    Time.zone.now
-  end
-
-  def self.apply_deadline(year = current_year)
-    date(:apply_deadline, year)
-  end
-
-  def self.reject_by_default(year = current_year)
-    date(:reject_by_default, year)
-  end
-
-  def self.decline_by_default_date(year = current_year)
-    find_closes(year) - 1.day
-  end
-
-  def self.find_closes(year = current_year)
-    date(:find_closes, year)
-  end
-
-  def self.find_opens(year = current_year)
-    date(:find_opens, year)
-  end
-
-  def self.find_reopens(year = next_year)
-    if CYCLE_DATES[year].present?
-      date(:find_opens, year)
-    else
-      date(:find_closes, year - 1) + 8.hours
-    end
-  end
-
-  def self.apply_opens(year = current_year)
-    date(:apply_opens, year)
-  end
-
-  def self.apply_reopens(year = next_year)
-    if CYCLE_DATES[year].present?
-      date(:apply_opens, year)
-    else
-      find_reopens(year) + 1.week
-    end
-  end
-
   def self.holidays(year = current_year)
     # do not support the cycle switcher via #date as:
     #
@@ -74,46 +22,12 @@ class CycleTimetable
     real_schedule_for(year).fetch(:holidays)
   end
 
-  def self.date(name, year = current_year)
-    schedule = real_schedule_for(year)
-
-    schedule.fetch(name)
-  end
-
-  #
-  # cycle_week methods
-  #
-
-  def self.current_cycle_week(time = Time.zone.now)
-    weeks = (time.to_date - find_opens(current_year(time)).beginning_of_week.to_date).to_i / 7
-    (weeks % 52).succ
-  end
-
-  #
-  # cycle_schedule methods
-  #
-
   def self.real_schedule_for(year = current_year)
     CYCLE_DATES[year]
   end
 
-  def self.before_apply_opens?
-    current_date < date(:apply_opens)
-  end
-
   def self.last_recruitment_cycle_year?(year)
     year == CYCLE_DATES.keys.last
-  end
-
-  def self.cycle_year_range(year = current_year)
-    "#{year} to #{year + 1}"
-  end
-
-  def self.this_day_last_cycle
-    days_since_cycle_started = (current_date.to_date - CycleTimetable.apply_opens.to_date).round
-    last_cycle_opening_date = apply_opens(previous_year).to_date
-    last_cycle_date = days_since_cycle_started.days.after(last_cycle_opening_date)
-    DateTime.new(last_cycle_date.year, last_cycle_date.month, last_cycle_date.day, Time.current.hour, Time.current.min, Time.current.sec)
   end
 
   private_class_method :last_recruitment_cycle_year?
