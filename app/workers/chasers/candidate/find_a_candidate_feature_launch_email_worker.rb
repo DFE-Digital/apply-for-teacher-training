ddd4d6d6f986004f17c3a9b76304b3af2a01653e
David Young <david@dcyoung.dev>
2025-04-25 12:28:03 +0100

    Staggers the FAC Launch Email Worker
    This will send the emails in batches of 500 spread over 1 hour
    
    On Monday the rest of tier 1 and all of tier 2 ~=2,000 - we'll send 4 lots of 500 at 15min intervals
    Tier 3 ~15000 - we'll send 500 emails every 2 mins for 1 hour

--- app/workers/chasers/candidate/find_a_candidate_feature_launch_email_worker.rb
+++ app/workers/chasers/candidate/find_a_candidate_feature_launch_email_worker.rb
@@ -1,29 +1,46 @@
 module Chasers
   module Candidate
+    class StaggeredFindACandidateFeatureLaunchEmailWorker
+      include Sidekiq::Worker
+
+      def perform(application_form_ids)
+        application_forms = FindACandidateFeatureLaunchEmailWorker.application_forms_to_send(application_form_ids)
+
+        BatchDelivery.new(relation: application_forms, stagger_over: 1.hour, batch_size: 500).each do |batch_time, applications|
+          FindACandidateFeatureLaunchEmailWorker.perform_at(batch_time, applications.pluck)
+        end
+      end
+    end
+
     class FindACandidateFeatureLaunchEmailWorker
       include Sidekiq::Worker
 
-      def perform(application_form_ids, limit = 1000)
-        return unless FeatureFlag.active?(:candidate_preferences)
-
+      def self.application_forms_to_send(application_form_ids)
         application_form_ids_with_sent_chaser = ChaserSent
                                                   .where(chaser_type: 'find_a_candidate_feature_launch',
                                                          chased_type: 'ApplicationForm')
                                                   .select(:chased_id)
 
-        application_forms_to_send = ApplicationForm
-                                      .where(id: application_form_ids)
-                                      # Tier 1 & 2 application forms
-                                      .where(id: Pool::Candidates.application_forms_eligible_for_pool)
-                                      # Filter out candidates who should not receive nudge-like emails
-                                      # Filter out blocked and locked candidates
-                                      .joins(:candidate)
-                                      .merge(::Candidate.for_marketing_or_nudge_emails)
-                                      # Filter out application forms that have already received the chaser
-                                      .where.not(id: application_form_ids_with_sent_chaser)
-                                      # Filter out application forms without submitted application choices
-                                      .where.not(submitted_at: nil)
-                                      .distinct
+        ApplicationForm
+          .where(id: application_form_ids)
+          # Tier 1 & 2 application forms
+          .where(id: Pool::Candidates.application_forms_eligible_for_pool)
+          # Filter out candidates who should not receive nudge-like emails
+          # Filter out blocked and locked candidates
+          .joins(:candidate)
+          .merge(::Candidate.for_marketing_or_nudge_emails)
+          # Filter out application forms that have already received the chaser
+          .where.not(id: application_form_ids_with_sent_chaser)
+          # Filter out application forms without submitted application choices
+          .where.not(submitted_at: nil)
+          .distinct
+      end
+
+      def perform(application_form_ids, limit = 1000)
+        return unless FeatureFlag.active?(:candidate_preferences)
+
+        application_forms_to_send = FindACandidateFeatureLaunchEmailWorker
+                                      .application_forms_to_send(application_form_ids)
                                       .limit(limit)
 
         application_forms_to_send.in_batches do |application_forms|
