73c2ffad80aceb86c2f34a2395383781794618db
David Young <david4.young@education.gov.uk>
2025-05-02 11:13:14 +0100

    Updated location filtering on FAC (#10668)
    ## Context
    
    The current implementation causes SQL queries to "run away" crashing the
    application
    
    ## Changes proposed in this pull request
    
    - Uses CTEs and geocoder to calculate nearby locations
    
    ## Guidance to review
    
    This has been built against a sanitised database backup to prove
    performance

--- app/models/candidate_location_preference.rb
+++ app/models/candidate_location_preference.rb
@@ -3,4 +3,6 @@ class CandidateLocationPreference < ApplicationRecord
   belongs_to :provider, optional: true
 
   acts_as_mappable lat_column_name: :latitude, lng_column_name: :longitude
+
+  geocoded_by :name
 end
