15d5efaef1852b08b77b2929409d5cbc23f3de01
David Young <david4.young@education.gov.uk>
2025-05-23 15:07:45 +0100

    Added worker to populate Find A Candidate table (#10749)
    ## Context
    
    We want to periodically populate the CandidatePoolApplication table with
    the most recent eligible Application Forms to appear in the Pool
    
    ## Changes proposed in this pull request
    
    - Adds a worker to find all the eligible Application Forms and adds them
    to the CandidatePoolApplication table
    - Worker set to run every 15 minutes
    
    Co-authored-by: Catalin Voineag <11318084+CatalinVoineag@users.noreply.github.com>

---
+++ app/workers/find_a_candidate/populate_pool_worker.rb
@@ -0,0 +1,20 @@
+class FindACandidate::PopulatePoolWorker
+  include Sidekiq::Worker
+
+  sidekiq_options queue: :default
+
+  def perform
+    application_forms_eligible_for_pool = Pool::Candidates.new.application_forms_in_the_pool
+                                            .select('application_forms.id as application_form_id, application_forms.candidate_id, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP')
+
+    insert_all_from_eligible_sql = <<~SQL
+      INSERT INTO candidate_pool_applications (application_form_id, candidate_id, created_at, updated_at)
+      #{application_forms_eligible_for_pool.to_sql}
+    SQL
+
+    CandidatePoolApplication.transaction do
+      CandidatePoolApplication.delete_all
+      ActiveRecord::Base.connection.execute(insert_all_from_eligible_sql)
+    end
+  end
+end
