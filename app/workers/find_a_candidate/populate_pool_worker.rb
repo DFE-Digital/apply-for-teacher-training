3ce3a707c6fd249a9117ef14ae5f50bf29b2a2c9
David Young <david@dcyoung.dev>
2025-05-27 15:02:12 +0100

    Tidy up attributes and variable names

--- app/workers/find_a_candidate/populate_pool_worker.rb
+++ app/workers/find_a_candidate/populate_pool_worker.rb
@@ -7,14 +7,15 @@ class FindACandidate::PopulatePoolWorker
     application_forms_eligible_for_pool = Pool::Candidates.new.application_forms_in_the_pool
 
     applications = application_forms_eligible_for_pool.map do |application_form|
-      full_time = application_form.course_options.any? { |course_option| course_option.study_mode == 'full_time' }
-      part_time = application_form.course_options.any? { |course_option| course_option.study_mode == 'part_time' }
+      study_mode_full_time = application_form.course_options.any? { |course_option| course_option.study_mode == 'full_time' }
+      study_mode_part_time = application_form.course_options.any? { |course_option| course_option.study_mode == 'part_time' }
 
       undergraduate_program_type = 'teacher_degree_apprenticeship'
       postgraduate_program_types = Course.program_types.except('teacher_degree_apprenticeship').keys
 
-      postgraduate = application_form.course_options.any? { |course_option| postgraduate_program_types.include?(course_option.course.program_type) }
-      undergraduate = application_form.course_options.any? { |course_option| course_option.course.program_type == undergraduate_program_type }
+      course_type_postgraduate = application_form.course_options.any? { |course_option| postgraduate_program_types.include?(course_option.course.program_type) }
+      course_type_undergraduate = application_form.course_options.any? { |course_option| course_option.course.program_type == undergraduate_program_type }
+
       subject_ids = application_form.courses.flat_map do |course|
         course.subjects.pluck(:id)
       end.uniq
@@ -24,10 +25,10 @@ class FindACandidate::PopulatePoolWorker
       {
         application_form_id: application_form.id,
         candidate_id: application_form.candidate_id,
-        study_mode_full_time: full_time,
-        study_mode_part_time: part_time,
-        course_type_postgraduate: postgraduate,
-        course_type_undergraduate: undergraduate,
+        study_mode_full_time: study_mode_full_time,
+        study_mode_part_time: study_mode_part_time,
+        course_type_postgraduate: course_type_postgraduate,
+        course_type_undergraduate: course_type_undergraduate,
         subject_ids: subject_ids,
         needs_visa: needs_visa,
       }
