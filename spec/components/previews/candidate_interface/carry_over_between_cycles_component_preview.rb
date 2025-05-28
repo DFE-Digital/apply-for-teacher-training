8e471730baca38860947ab383d0790dd8c4e7629
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2025-05-28 16:19:53 +0100

    Carry over component previews

---
+++ spec/components/previews/candidate_interface/carry_over_between_cycles_component_preview.rb
@@ -0,0 +1,19 @@
+module CandidateInterface
+  class CarryOverBetweenCyclesComponentPreview < ViewComponent::Preview
+    def with_application_choices
+      application_form = FactoryBot.create(
+        :application_form,
+        recruitment_cycle_year: RecruitmentCycleTimetable.previous_year,
+      )
+      FactoryBot.create(:application_choice, status: 'cancelled', application_form:)
+      render CarryOverBetweenCyclesComponent.new(application_form:)
+    end
+
+    def without_application_choices
+      recruitment_cycle_year = RecruitmentCycleTimetable.previous_year
+      render CarryOverBetweenCyclesComponent.new(
+        application_form: FactoryBot.build_stubbed(:application_form, recruitment_cycle_year:),
+      )
+    end
+  end
+end
