687efef92f3c69daf917fbd8ac1d64499e2790a8
David Young <david@dcyoung.dev>
2025-06-09 11:48:02 +0100

    Set a default filter value
    This set the default `year` filter to the current cycle, reducing the number of Applications from being fetched

--- app/controllers/support_interface/application_forms_controller.rb
+++ app/controllers/support_interface/application_forms_controller.rb
@@ -1,7 +1,7 @@
 module SupportInterface
   class ApplicationFormsController < SupportInterfaceController
     def index
-      @filter = SupportInterface::ApplicationsFilter.new(params:)
+      @filter = SupportInterface::ApplicationsFilter.new(params: filter_params)
       @pagy, @application_forms = @filter.filter_records(ApplicationForm)
     end
 
@@ -17,6 +17,14 @@ module SupportInterface
 
     def application_form
       @_application_form ||= ApplicationForm.find(params[:application_form_id])
+    end
+
+    def filter_params
+      if params[:remove] && !params[:year]
+        params
+      else
+        params.with_defaults(year: [RecruitmentCycleTimetable.current_year.to_s])
+      end
     end
   end
 end
