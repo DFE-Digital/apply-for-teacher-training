a85f6ae69a6883b1f0e8bf15d2854d82ae683a9e
CatalinVoineag <11318084+CatalinVoineag@users.noreply.github.com>
2025-06-10 12:30:34 +0100

    Remove one login sign up banner
    We don't need this anymore. We also added a bit of guidance to sandbox
    users when they create their candidate accounts for testing

--- app/components/candidate_interface/one_login_post_release_sign_in_banner_component.rb
+++
@@ -1,7 +0,0 @@
-module CandidateInterface
-  class OneLoginPostReleaseSignInBannerComponent < ViewComponent::Base
-    def render?
-      FeatureFlag.active?(:one_login_candidate_sign_in)
-    end
-  end
-end
