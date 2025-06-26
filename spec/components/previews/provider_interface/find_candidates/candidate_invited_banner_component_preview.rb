101f082b5f8dc5c4df5b587adb8e03efaf2a4174
Dan Daly <dan.daly@education.gov.uk>
2025-06-26 08:18:16 +0100

    Add component preview

---
+++ spec/components/previews/provider_interface/find_candidates/candidate_invited_banner_component_preview.rb
@@ -0,0 +1,14 @@
+class ProviderInterface::FindCandidates::CandidateInvitedBannerComponentPreview < ViewComponent::Preview
+  def candidate_invited_but_not_applied_yet_view
+    candidate = FactoryBot.create(:candidate)
+    application_form = FactoryBot.create(:application_form, :completed, candidate:, submitted_at: 1.day.ago)
+    pool_invite = FactoryBot.create(:pool_invite, :published, candidate:)
+    provider = pool_invite.provider
+    current_provider_user = FactoryBot.create(:provider_user, providers: [provider])
+
+    render ProviderInterface::FindCandidates::CandidateInvitedBannerComponent.new(
+      application_form:,
+      current_provider_user:,
+    )
+  end
+end
--- spec/components/provider_interface/find_candidates/candidate_invited_banner_component_spec.rb
+++ spec/components/provider_interface/find_candidates/candidate_invited_banner_component_spec.rb
@@ -3,20 +3,34 @@ require 'rails_helper'
 RSpec.describe ProviderInterface::FindCandidates::CandidateInvitedBannerComponent, type: :component do
   describe '#render' do
     let(:candidate) { create(:candidate) }
-    let(:rejected_candidate_form) do
+    let(:application_form) do
       create(:application_form, :completed, candidate:, submitted_at: 1.day.ago)
     end
     let(:pool_invite) { create(:pool_invite, :published, candidate:) }
     let(:provider) { pool_invite.provider }
-    let(:current_provider_user) { create(:provider_user, providers: [provider]) }
     let(:course) { Course.find(pool_invite.course_id) }
     let(:date) { pool_invite.created_at.to_fs(:govuk_date) }
 
-    it 'renders the banner when a published pool invite exists with the current provider' do
-      result = render_inline(described_class.new(application_form: rejected_candidate_form, current_provider_user:))
+    context 'when a published pool invite exists with the current provider' do
+      let(:current_provider_user) { create(:provider_user, providers: [provider]) }
 
-      expect(result.text).to include('Important')
-      expect(result.text).to include("This candidate was invited to #{course.name_and_code} at #{provider.name} on #{date}")
+      it 'renders the banner' do
+        result = render_inline(described_class.new(application_form:, current_provider_user:))
+
+        expect(result.text).to include('Important')
+        expect(result.text).to include("This candidate was invited to #{course.name_and_code} at #{provider.name} on #{date}")
+      end
+    end
+
+    context 'when the current provider is not the one who invited the candidate' do
+      let(:different_provider) { create(:provider) }
+      let(:current_provider_user) { create(:provider_user, providers: [different_provider]) }
+
+      it 'does not render the banner' do
+        result = render_inline(described_class.new(application_form:, current_provider_user:))
+
+        expect(result.to_html).to be_blank
+      end
     end
   end
 end
