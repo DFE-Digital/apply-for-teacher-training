15522f686a817ee8d27bfccf5a11122d7b573bee
Dan Daly <dan.daly@education.gov.uk>
2025-06-27 17:42:53 +0100

    Add previews to encompass single invite with application

--- spec/components/provider_interface/find_candidates/already_invited_to_multiple_courses_banner_component_spec.rb
+++ spec/components/provider_interface/find_candidates/already_invited_to_multiple_courses_banner_component_spec.rb
@@ -18,18 +18,15 @@ RSpec.describe ProviderInterface::FindCandidates::AlreadyInvitedToMultipleCourse
     let(:course1) { invite1.course }
     let(:course2) { invite2.course }
 
-    # TODO: Context blocks for when the candidate has not yet been invited to one or either of the courses (displays "They have not applied yet")
-    # TODO: Context blocks for when the candidate has been invited to one or both of the courses (shows view application link(s))
-
     context 'when multiple invites exist' do
-      it 'renders the banner' do
+      it 'renders the banner with both course names' do
         result = render_inline(described_class.new(
                                  application_form:,
                                  current_provider_user:,
                                  show_provider_name: false,
                                ))
 
-        expect(result.text).to include(I18n.t('provider_interface.find_candidates.already_invited_to_multiple_courses_banner_component.heading'))
+        expect(result.text).to include('This candidate was invited to apply')
         expect(result.text).to include(course1.name_and_code)
         expect(result.text).to include(course2.name_and_code)
       end
@@ -45,12 +42,12 @@ RSpec.describe ProviderInterface::FindCandidates::AlreadyInvitedToMultipleCourse
                                  show_provider_name: true,
                                ))
 
-        expect(result.to_html).to be_blank
+        expect(result.text).to be_blank
       end
     end
 
     context 'when show_provider_name is false' do
-      it 'renders banner text without the provider name' do
+      it 'renders invite details without provider name' do
         result = render_inline(described_class.new(
                                  application_form:,
                                  current_provider_user:,
@@ -62,7 +59,42 @@ RSpec.describe ProviderInterface::FindCandidates::AlreadyInvitedToMultipleCourse
       end
     end
 
-    context 'when no matching invites for the current provider user exist' do
+    context 'when show_provider_name is true' do
+      it 'renders invite details with provider name' do
+        result = render_inline(described_class.new(
+                                 application_form:,
+                                 current_provider_user:,
+                                 show_provider_name: true,
+                               ))
+
+        expect(result.text).to include(provider.name)
+        expect(result.text).to include(course1.name_and_code)
+      end
+    end
+
+    context 'when an invite matches an application choice' do
+      before do
+        create(
+          :application_choice,
+          application_form: application_form,
+          course_option: create(:course_option, course: course1),
+          provider_ids: [provider.id],
+        )
+      end
+
+      it 'renders link to view application for that course' do
+        result = render_inline(described_class.new(
+                                 application_form:,
+                                 current_provider_user:,
+                                 show_provider_name: true,
+                               ))
+
+        expect(result.text).to include(course1.name_and_code)
+        expect(result.text).to include('View application')
+      end
+    end
+
+    context 'when no invites belong to the current provider user`s providers' do
       let(:other_provider) { create(:provider) }
       let(:current_provider_user) { create(:provider_user, providers: [other_provider]) }
 
@@ -73,7 +105,7 @@ RSpec.describe ProviderInterface::FindCandidates::AlreadyInvitedToMultipleCourse
                                  show_provider_name: true,
                                ))
 
-        expect(result.to_html).to be_blank
+        expect(result.text).to be_blank
       end
     end
   end
