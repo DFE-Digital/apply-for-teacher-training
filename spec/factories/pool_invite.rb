efc4a3df79c648884108ad1315c997a77089ea74
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2025-06-27 07:31:00 +0100

    Implement front end for showing providers invited candidates

--- spec/factories/pool_invite.rb
+++ spec/factories/pool_invite.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :pool_invite, class: 'Pool::Invite' do
     candidate factory: %i[candidate]
-    provider factory: %i[provider]
     invited_by factory: %i[provider_user]
     course factory: %i[course]
+    provider { course.provider }
     recruitment_cycle_year { CycleTimetableHelper.current_year }
 
     trait :sent_to_candidate do
