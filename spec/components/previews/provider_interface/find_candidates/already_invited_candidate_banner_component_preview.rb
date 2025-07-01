c20c8f6ef4e5a849387412d6554f5687eacefb70
Dan Daly <dan.daly@education.gov.uk>
2025-07-01 16:05:42 +0100

    Add previews

--- spec/components/previews/provider_interface/find_candidates/already_invited_candidate_banner_component_preview.rb
+++ spec/components/previews/provider_interface/find_candidates/already_invited_candidate_banner_component_preview.rb
@@ -4,12 +4,12 @@ class ProviderInterface::FindCandidates::AlreadyInvitedCandidateBannerComponentP
     application_form = FactoryBot.create(:application_form, :completed, candidate:, submitted_at: 1.day.ago)
     pool_invite = FactoryBot.create(:pool_invite, :published, candidate:)
     provider = pool_invite.provider
-    current_provider_user = FactoryBot.create(:provider_user, providers: [provider])
+    provider2 = FactoryBot.create(:provider)
+    current_provider_user = FactoryBot.create(:provider_user, providers: [provider, provider2])
 
     render ProviderInterface::FindCandidates::AlreadyInvitedCandidateBannerComponent.new(
       application_form: application_form,
       current_provider_user: current_provider_user,
-      show_provider_name: true,
     )
   end
 
@@ -23,15 +23,15 @@ class ProviderInterface::FindCandidates::AlreadyInvitedCandidateBannerComponentP
     render ProviderInterface::FindCandidates::AlreadyInvitedCandidateBannerComponent.new(
       application_form: application_form,
       current_provider_user: current_provider_user,
-      show_provider_name: false,
     )
   end
 
-  def fac_status_banner_where_candidate_has_applied
+  def fac_status_banner_for_single_invite_where_candidate_has_applied
     candidate = FactoryBot.create(:candidate)
     application_form = FactoryBot.create(:application_form, :completed, candidate:, submitted_at: 1.day.ago)
     pool_invite = FactoryBot.create(:pool_invite, :published, candidate:)
     provider = pool_invite.provider
+    provider2 = FactoryBot.create(:provider)
     course = pool_invite.course
 
     FactoryBot.create(
@@ -41,35 +41,11 @@ class ProviderInterface::FindCandidates::AlreadyInvitedCandidateBannerComponentP
       provider_ids: [provider.id],
     )
 
-    current_provider_user = FactoryBot.create(:provider_user, providers: [provider])
+    current_provider_user = FactoryBot.create(:provider_user, providers: [provider, provider2])
 
     render ProviderInterface::FindCandidates::AlreadyInvitedCandidateBannerComponent.new(
       application_form: application_form,
       current_provider_user: current_provider_user,
-      show_provider_name: true,
-    )
-  end
-
-  def fac_status_banner_where_candidate_has_applied_without_provider_name
-    candidate = FactoryBot.create(:candidate)
-    application_form = FactoryBot.create(:application_form, :completed, candidate:, submitted_at: 1.day.ago)
-    pool_invite = FactoryBot.create(:pool_invite, :published, candidate:)
-    provider = pool_invite.provider
-    course = pool_invite.course
-
-    FactoryBot.create(
-      :application_choice,
-      application_form: application_form,
-      course_option: FactoryBot.create(:course_option, course: course),
-      provider_ids: [provider.id],
-    )
-
-    current_provider_user = FactoryBot.create(:provider_user, providers: [provider])
-
-    render ProviderInterface::FindCandidates::AlreadyInvitedCandidateBannerComponent.new(
-      application_form: application_form,
-      current_provider_user: current_provider_user,
-      show_provider_name: false,
     )
   end
 end
--- spec/components/previews/provider_interface/find_candidates/already_invited_to_multiple_courses_banner_component_preview.rb
+++ spec/components/previews/provider_interface/find_candidates/already_invited_to_multiple_courses_banner_component_preview.rb
@@ -1,25 +1,25 @@
 class ProviderInterface::FindCandidates::AlreadyInvitedToMultipleCoursesBannerComponentPreview < ViewComponent::Preview
-  def with_provider_name
+  def fac_status_banner_for_multiple_invite_with_provider_name
     candidate = FactoryBot.create(:candidate)
     application_form = FactoryBot.create(:application_form, :completed, candidate:, submitted_at: 1.day.ago)
 
     provider = Provider.find_or_create_by!(code: 'PV001')
+    provider2 = Provider.find_or_create_by!(code: 'PV991')
     course1 = Course.find_or_create_by!(code: 'COURSE1', provider:)
     course2 = Course.find_or_create_by!(code: 'COURSE2', provider:)
 
     FactoryBot.create(:pool_invite, :published, candidate:, provider:, course: course1)
     FactoryBot.create(:pool_invite, :published, candidate:, provider:, course: course2)
 
-    current_provider_user = FactoryBot.create(:provider_user, providers: [provider])
+    current_provider_user = FactoryBot.create(:provider_user, providers: [provider, provider2])
 
     render ProviderInterface::FindCandidates::AlreadyInvitedToMultipleCoursesBannerComponent.new(
       application_form:,
       current_provider_user:,
-      show_provider_name: true,
     )
   end
 
-  def without_provider_name
+  def fac_status_banner_for_multiple_invite_without_provider_name
     candidate = FactoryBot.create(:candidate)
     application_form = FactoryBot.create(:application_form, :completed, candidate:, submitted_at: 1.day.ago)
 
@@ -36,7 +36,33 @@ class ProviderInterface::FindCandidates::AlreadyInvitedToMultipleCoursesBannerCo
     render ProviderInterface::FindCandidates::AlreadyInvitedToMultipleCoursesBannerComponent.new(
       application_form:,
       current_provider_user:,
-      show_provider_name: false,
+    )
+  end
+
+  def fac_status_banner_for_multiple_invite_where_candidate_has_applied
+    candidate = FactoryBot.create(:candidate)
+    application_form = FactoryBot.create(:application_form, :completed, candidate:, submitted_at: 1.day.ago)
+
+    provider = Provider.find_or_create_by!(code: 'PV999')
+
+    course5 = Course.find_by(code: 'COURSE5', provider:) || FactoryBot.create(:course, code: 'COURSE5', provider:)
+    course6 = Course.find_by(code: 'COURSE6', provider:) || FactoryBot.create(:course, code: 'COURSE6', provider:)
+
+    FactoryBot.create(:pool_invite, :published, candidate:, provider:, course: course5)
+    FactoryBot.create(:pool_invite, :published, candidate:, provider:, course: course6)
+
+    FactoryBot.create(
+      :application_choice,
+      application_form: application_form,
+      course_option: FactoryBot.create(:course_option, course: course5),
+      provider_ids: [provider.id],
+    )
+
+    current_provider_user = FactoryBot.create(:provider_user, providers: [provider])
+
+    render ProviderInterface::FindCandidates::AlreadyInvitedToMultipleCoursesBannerComponent.new(
+      application_form:,
+      current_provider_user:,
     )
   end
 end
