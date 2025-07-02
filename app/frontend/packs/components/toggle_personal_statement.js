b93eedbc258fb41c7ac2257e238781986ed9456f
Dan Daly <dan.daly@education.gov.uk>
2025-07-02 15:39:00 +0100

    Tweak js to allow for correct rendering when js disabled

--- app/frontend/packs/components/toggle_personal_statement.js
+++ app/frontend/packs/components/toggle_personal_statement.js
@@ -1,27 +1,31 @@
 export default function personalStatementToggle () {
-  document.addEventListener('DOMContentLoaded', function () {
+  document.addEventListener('DOMContentLoaded', () => {
     const showMoreLink = document.querySelector('.app-show-more-toggle')
     const showLessLink = document.querySelector('.app-show-less-toggle')
     const truncated = document.getElementById('app-truncated-personal-statement')
     const full = document.getElementById('app-full-personal-statement')
 
-    if (showMoreLink && showLessLink && truncated && full) {
-      showMoreLink.addEventListener('click', function (e) {
-        e.preventDefault()
-        truncated.classList.add('govuk-visually-hidden')
-        full.classList.remove('govuk-visually-hidden')
-        showMoreLink.classList.add('govuk-visually-hidden')
-        showLessLink.classList.remove('govuk-visually-hidden')
-      })
+    if (!showMoreLink || !showLessLink || !truncated || !full) return
 
-      showLessLink.addEventListener('click', function (e) {
-        e.preventDefault()
-        full.classList.add('govuk-visually-hidden')
-        truncated.classList.remove('govuk-visually-hidden')
-        showLessLink.classList.add('govuk-visually-hidden')
-        showMoreLink.classList.remove('govuk-visually-hidden')
-        truncated.scrollIntoView({ block: 'center' })
-      })
-    }
+    truncated.classList.remove('govuk-visually-hidden')
+    showMoreLink.classList.remove('govuk-visually-hidden')
+
+    showMoreLink.addEventListener('click', e => {
+      e.preventDefault()
+      truncated.classList.add('govuk-visually-hidden')
+      full.classList.remove('govuk-visually-hidden')
+      showMoreLink.classList.add('govuk-visually-hidden')
+      showLessLink.classList.remove('govuk-visually-hidden')
+      full.focus()
+    })
+
+    showLessLink.addEventListener('click', e => {
+      e.preventDefault()
+      full.classList.add('govuk-visually-hidden')
+      truncated.classList.remove('govuk-visually-hidden')
+      showLessLink.classList.add('govuk-visually-hidden')
+      showMoreLink.classList.remove('govuk-visually-hidden')
+      truncated.scrollIntoView({ block: 'center' })
+    })
   })
 }
