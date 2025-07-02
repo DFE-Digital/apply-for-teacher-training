9806808972cc25506e1793a00385b446e4d16179
David Young <david@dcyoung.dev>
2025-07-02 12:47:30 +0100

    Recommendation controllers redirect to Find
    These controllers will redirect to the Find service if there is an available Recommendation

--- spec/requests/support_interface/candidates/course_recommendation_spec.rb
+++ spec/requests/support_interface/candidates/course_recommendation_spec.rb
@@ -11,4 +11,15 @@ RSpec.describe 'Support interface - Candidates, Course Recommendations' do
     expect(response).to redirect_to(support_interface_root_path)
     expect(request.flash[:notice]).to eq('We are unable to recommend a course for this candidate.')
   end
+
+  context 'when a course recommendation is available' do
+    it 'redirects to the url from the course recommendation service' do
+      url = URI.join(I18n.t('find_teacher_training.production_url'), 'results').to_s
+      allow(CandidateCoursesRecommender).to receive(:recommended_courses_url).and_return(url)
+
+      get support_interface_candidate_course_recommendation_path(create(:candidate))
+
+      expect(response).to redirect_to(url)
+    end
+  end
 end
