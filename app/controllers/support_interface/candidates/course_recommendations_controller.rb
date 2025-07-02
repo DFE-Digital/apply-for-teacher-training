4e97c8b29aebb5f90927eaa0b55a062622549526
David Young <david@dcyoung.dev>
2025-07-02 12:47:30 +0100

    Recommendation controllers redirect to Find
    These controllers will redirect to the Find service if there is an available Recommendation

--- app/controllers/support_interface/candidates/course_recommendations_controller.rb
+++ app/controllers/support_interface/candidates/course_recommendations_controller.rb
@@ -1,5 +1,13 @@
 class SupportInterface::Candidates::CourseRecommendationsController < SupportInterface::SupportInterfaceController
   def show
-    redirect_to support_interface_root_path, notice: 'We are unable to recommend a course for this candidate.'
+    candidate = Candidate.find(params.expect(:candidate_id))
+    application_form = candidate.current_application
+    recommendation = CandidateCoursesRecommender.recommended_courses_url(candidate:, locatable: application_form)
+
+    if recommendation.present?
+      redirect_to recommendation, allow_other_host: true
+    else
+      redirect_to support_interface_root_path, notice: 'We are unable to recommend a course for this candidate.'
+    end
   end
 end
--- spec/requests/support_interface/application_choices/course_recommendation_spec.rb
+++ spec/requests/support_interface/application_choices/course_recommendation_spec.rb
@@ -11,4 +11,15 @@ RSpec.describe 'Support interface - Application Choice, Course Recommendations' 
     expect(response).to redirect_to(support_interface_root_path)
     expect(request.flash[:notice]).to eq('We are unable to recommend a course for this application choice.')
   end
+
+  context 'when a course recommendation is available' do
+    it 'redirects to the url from the course recommendation service' do
+      url = URI.join(I18n.t('find_teacher_training.production_url'), 'results').to_s
+      allow(CandidateCoursesRecommender).to receive(:recommended_courses_url).and_return(url)
+
+      get support_interface_application_choice_course_recommendation_path(create(:application_choice))
+
+      expect(response).to redirect_to(url)
+    end
+  end
 end
