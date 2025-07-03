df7f8478aa18bd839d3eddff4484d1fc299572da
David Young <david4.young@education.gov.uk>
2025-07-03 13:17:12 +0100

    Update app/services/candidate_courses_recommender.rb
    Co-authored-by: Lori Bailey <44073106+elceebee@users.noreply.github.com>

--- app/services/candidate_courses_recommender.rb
+++ app/services/candidate_courses_recommender.rb
@@ -153,10 +153,7 @@ private
 
     # What Courses has the Candidate applied for?
     # course codes & provider codes
-    candidate.application_choices
-                       .joins(:application_form)
-                       .where(application_form: { recruitment_cycle_year: current_year })
-                       .where(status: ApplicationStateChange::STATES_VISIBLE_TO_PROVIDER)
+    candidate.current_application.application_choices.visible_to_provider
                        .joins(course_option: { course: :provider })
                        .pluck('course.code', 'provider.code')
                        .compact_blank
