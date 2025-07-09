49079e84a5c2fbf6615d77f0511e1ce952f72b77
David Young <david@dcyoung.dev>
2025-07-09 12:17:11 +0100

    Fix after rebase

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -90,44 +90,6 @@ class Candidate < ApplicationRecord
     ).exists?
   end
 
-  def self.with_safeguarding_concerns
-    safeguarding_on_application_forms = joins(:application_forms).where(application_forms: { safeguarding_issues_status: :has_safeguarding_issues_to_declare })
-    safeguarding_on_references = joins(application_forms: :application_references).where(application_references: { safeguarding_concerns_status: :has_safeguarding_concerns_to_declare })
-    safeguarding_on_rejection_reasons = joins(:application_choices).where(
-      "application_choices.structured_rejection_reasons->'selected_reasons' @> '[{\"id\":\"safeguarding\"}]'::jsonb",
-    )
-
-    with(
-      safeguarding_on_application_forms: safeguarding_on_application_forms,
-      safeguarding_on_references: safeguarding_on_references,
-      safeguarding_on_rejection_reasons: safeguarding_on_rejection_reasons,
-    ).where(id: safeguarding_on_application_forms.select('candidates.id'))
-     .or(where(id: safeguarding_on_references.select('candidates.id')))
-      .or(where(id: safeguarding_on_rejection_reasons.select('candidates.id')))
-  end
-
-  def self.without_safeguarding_concerns
-    where.not(id: with_safeguarding_concerns)
-  end
-
-  def safeguarding_concerns?
-    @has_safeguarding_concerns ||= application_forms_with_safeguarding_concerns? || application_references_with_safeguarding_concerns? || application_choices_rejected_with_safeguarding_concerns?
-  end
-
-  def application_forms_with_safeguarding_concerns?
-    application_forms.exists?(safeguarding_issues_status: :has_safeguarding_issues_to_declare)
-  end
-
-  def application_references_with_safeguarding_concerns?
-    application_references.exists?(safeguarding_concerns_status: :has_safeguarding_concerns_to_declare)
-  end
-
-  def application_choices_rejected_with_safeguarding_concerns?
-    application_choices.where(
-      "application_choices.structured_rejection_reasons->'selected_reasons' @> '[{\"id\":\"safeguarding\"}]'::jsonb",
-    ).exists?
-  end
-
   def current_application
     application_form = application_forms.order(:created_at, :id).last
     application_form || if RecruitmentCycleTimetable.current_timetable.after_apply_deadline?
