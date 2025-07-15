2e41ef1a7b03be51617464ebcae07701e8a09a17
David Young <david@dcyoung.dev>
2025-07-15 17:44:36 +0100

    Fix spec

--- spec/services/data_migrations/drop_grouped_invite_email_feature_flag_spec.rb
+++ spec/services/data_migrations/drop_grouped_invite_email_feature_flag_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe DataMigrations::DropGroupedInviteEmailFeatureFlag do
   context 'when the feature flag exists' do
     it 'removes the feature flag' do
-      create(:feature, name: 'grouped_invite_email')
+      Feature.find_or_create_by(name: 'grouped_invite_email')
+
       expect { described_class.new.change }.to change { Feature.count }.by(-1)
       expect(Feature.where(name: 'grouped_invite_email')).to be_blank
     end
