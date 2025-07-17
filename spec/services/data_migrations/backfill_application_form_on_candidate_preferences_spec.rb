5327e34249d1818e42178f7e7e82828c32aa3952
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2025-07-17 12:50:30 +0100

    Application form id is now required because backfill is complete

--- spec/services/data_migrations/backfill_application_form_on_candidate_preferences_spec.rb
+++ spec/services/data_migrations/backfill_application_form_on_candidate_preferences_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe DataMigrations::BackfillApplicationFormOnCandidatePreferences do
   it 'adds an application form to the candidate preference where it is missing' do
     application_form = create(:application_form)
-    without_application_form = create(:candidate_preference, application_form: nil, candidate: application_form.candidate)
+    without_application_form = create(:candidate_preference, application_form:)
+    without_application_form.application_form_id = nil
+    without_application_form.save(validate: false)
     described_class.new.change
 
     expect(without_application_form.reload.application_form_id).to eq application_form.id
