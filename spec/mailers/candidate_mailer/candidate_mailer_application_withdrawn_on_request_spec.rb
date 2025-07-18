76ea18b036cfe1f6189fb656991c56c4abf5f55a
David Young <david@dcyoung.dev>
2025-07-18 16:06:35 +0100

    Updated application_withdrawn_on_request to include recommendation link

--- spec/mailers/candidate_mailer/candidate_mailer_application_withdrawn_on_request_spec.rb
+++ spec/mailers/candidate_mailer/candidate_mailer_application_withdrawn_on_request_spec.rb
@@ -4,10 +4,23 @@ RSpec.describe CandidateMailer do
   include TestHelpers::MailerSetupHelper
 
   describe '.application_withdrawn_on_request' do
-    context 'when the candidate has withdrawn or asked to be withdrawn from an application choice', time: mid_cycle do
-      let(:email) { described_class.application_withdrawn_on_request(application_form.application_choices.first) }
+    let(:email) { described_class.application_withdrawn_on_request(application_form.application_choices.first) }
 
-      context 'mid cycle', time: mid_cycle do
+    context 'mid cycle', time: mid_cycle do
+      it_behaves_like(
+        'a mail with subject and content',
+        'Update on your application',
+        'greeting' => 'Hello Fred',
+        'details' => 'has withdrawn your application for',
+        'still interested' => 'If now’s the right time for you, you can still apply for teacher training again this year.',
+        'content' => 'If now’s the right time for you, you can still apply for teacher training again this year.',
+        'realistic job preview' => 'Try the realistic job preview tool',
+        'realistic job preview link' => /https:\/\/platform\.teachersuccess\.co\.uk\/p\/.*\?id=\w{64}&utm_source/,
+      )
+
+      context 'with a course recommendation url' do
+        let(:email) { described_class.application_withdrawn_on_request(application_form.application_choices.first, 'https://www.find-postgraduate-teacher-training.service.gov.uk/results') }
+
         it_behaves_like(
           'a mail with subject and content',
           'Update on your application',
@@ -17,28 +30,30 @@ RSpec.describe CandidateMailer do
           'content' => 'If now’s the right time for you, you can still apply for teacher training again this year.',
           'realistic job preview' => 'Try the realistic job preview tool',
           'realistic job preview link' => /https:\/\/platform\.teachersuccess\.co\.uk\/p\/.*\?id=\w{64}&utm_source/,
+          'course recommendation' => 'Based on the details in your previous application, you could be suitable for other teacher training courses.',
+          'course recommendation link' => 'https://www.find-postgraduate-teacher-training.service.gov.uk/results',
         )
       end
+    end
 
-      context 'between cycles, before find reopens', time: after_apply_deadline(2024) do
-        it_behaves_like(
-          'a mail with subject and content',
-          'Update on your application',
-          'greeting' => 'Hello Fred',
-          'still interested' => 'You can apply again for courses starting in the 2025 to 2026 academic year.',
-          'when to apply again' => 'submit from 9am UK time on 8 October 2024',
-        )
-      end
+    context 'between cycles, before find reopens', time: after_apply_deadline(2024) do
+      it_behaves_like(
+        'a mail with subject and content',
+        'Update on your application',
+        'greeting' => 'Hello Fred',
+        'still interested' => 'You can apply again for courses starting in the 2025 to 2026 academic year.',
+        'when to apply again' => 'submit from 9am UK time on 8 October 2024',
+      )
+    end
 
-      context 'between cycles, before find reopens', time: after_find_opens(2025) do
-        it_behaves_like(
-          'a mail with subject and content',
-          'Update on your application',
-          'greeting' => 'Hello Fred',
-          'still interested' => 'You can apply again for courses starting in the 2025 to 2026 academic year.',
-          'when to apply again' => 'submit from 9am UK time on 8 October 2024',
-        )
-      end
+    context 'between cycles, before find reopens', time: after_find_opens(2025) do
+      it_behaves_like(
+        'a mail with subject and content',
+        'Update on your application',
+        'greeting' => 'Hello Fred',
+        'still interested' => 'You can apply again for courses starting in the 2025 to 2026 academic year.',
+        'when to apply again' => 'submit from 9am UK time on 8 October 2024',
+      )
     end
   end
 end
