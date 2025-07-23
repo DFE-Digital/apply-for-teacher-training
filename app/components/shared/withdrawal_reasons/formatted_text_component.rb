3c40a7a24dabc74da8bcbc05caac587f228a1c9c
Dan Daly <dan.daly@education.gov.uk>
2025-07-23 11:10:05 +0100

    Add backward compatibility for withdrawal reasons

--- app/components/shared/withdrawal_reasons/formatted_text_component.rb
+++ app/components/shared/withdrawal_reasons/formatted_text_component.rb
@@ -1,4 +1,6 @@
 class WithdrawalReasons::FormattedTextComponent < ViewComponent::Base
+  OLD_REASONS_PATH = 'config/withdrawal_reasons.yml'.freeze
+
   def initialize(application_choice:)
     @application_choice = application_choice
   end
@@ -14,18 +16,42 @@ class WithdrawalReasons::FormattedTextComponent < ViewComponent::Base
 private
 
   def reasons
-    @application_choice.published_withdrawal_reasons.flat_map do |reason|
-      reason_key = reason.reason
-      comment = reason.comment
+    # No reason given if withdrawn or declined by provider
+    if @application_choice.withdrawn_or_declined_for_candidate_by_provider?
+      [t('withdrawal_reasons.formatted_text_component.withdrawn_by_provider_on_behalf_of_candidate')]
+    # New reason model
+    elsif @application_choice.published_withdrawal_reasons.present?
+      @application_choice.published_withdrawal_reasons.flat_map do |reason|
+        reason_key = reason.reason
+        comment = reason.comment
 
-      normalized_reason_key = reason_key.tr('-', '_')
-      key = "withdrawal_reasons.formatted_text_component.reasons.#{normalized_reason_key}"
+        normalized_reason_key = reason_key.tr('-', '_')
+        key = "withdrawal_reasons.formatted_text_component.reasons.#{normalized_reason_key}"
 
-      translation = t(key, default: nil, comment: comment)
+        translation = t(key, default: nil, comment: comment)
 
-      next unless translation
+        next unless translation
 
-      [translation, (comment.present? ? %("#{comment}") : nil)]
-    end.compact
+        [translation, (comment.present? ? %("#{comment}") : nil)]
+      end.compact
+    # Old reason model for backward compatibility
+    elsif @application_choice.structured_withdrawal_reasons.present?
+      @application_choice.structured_withdrawal_reasons.map do |reason_id|
+        find_reason_label(reason_id)
+      end.compact
+    # In the old model, reasons were optional
+    else
+      [t('withdrawal_reasons.formatted_text_component.no_reason_given')]
+    end
+  end
+
+  def find_reason_label(reason_id)
+    old_reasons.find do |reason|
+      reason[:id] == reason_id
+    end&.fetch(:label, nil)
+  end
+
+  def old_reasons
+    YAML.load_file(OLD_REASONS_PATH)
   end
 end
