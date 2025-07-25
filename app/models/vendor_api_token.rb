5e22abdc0d7beeb5178b5c66f14667409bc6c0ad
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2025-07-25 15:30:42 +0100

    Allow provider users to add a description to api tokens

--- app/models/vendor_api_token.rb
+++ app/models/vendor_api_token.rb
@@ -5,9 +5,9 @@ class VendorAPIToken < ApplicationRecord
 
   scope :used_in_last_3_months, -> { where('last_used_at >= ?', 3.months.ago) }
 
-  def self.create_with_random_token!(provider:)
+  def self.create_with_random_token!(attributes = {}, provider:)
     unhashed_token, hashed_token = Devise.token_generator.generate(VendorAPIToken, :hashed_token)
-    create!(hashed_token:, provider:)
+    create!(attributes.merge({ hashed_token:, provider: }))
     unhashed_token
   end
 
