d5dbb79bd38a6506fbda9ae064b70e8b220f3bac
CatalinVoineag <11318084+CatalinVoineag@users.noreply.github.com>
2025-07-28 15:57:06 +0100

    Add declined candidate decision to pool invites
    This adds the declined status to the pool invite and changes the invite
    component to display the correct status and link on the invite.
    
    This is not the last version of the candidate invites index.

--- app/components/candidate_interface/invites_component.rb
+++ app/components/candidate_interface/invites_component.rb
@@ -1,9 +1,8 @@
 module CandidateInterface
   class InvitesComponent < ViewComponent::Base
-    attr_reader :application_form, :invites
+    attr_reader :invites
 
-    def initialize(application_form:, invites:)
-      @application_form = application_form
+    def initialize(invites:)
       @invites = invites
     end
 
@@ -12,6 +11,26 @@ module CandidateInterface
         candidate_interface_offer_path(invite.application_choice, return_to: 'invites')
       else
         candidate_interface_course_choices_course_review_path(invite.application_choice, return_to: 'invites')
+      end
+    end
+
+    def status_tag(invite)
+      if invite.applied? && invite.application_choice.present?
+        govuk_tag text: invite.candidate_decision.capitalize, colour: 'green'
+      elsif invite.declined?
+        govuk_tag text: invite.candidate_decision.capitalize, colour: 'red'
+      elsif invite.course_closed?
+        govuk_tag text: t('.closed'), colour: 'grey'
+      end
+    end
+
+    def action_link(invite)
+      if invite.applied? && invite.application_choice.present?
+        govuk_link_to t('.view_application'), application_choice_link(invite)
+      elsif invite.declined? || !invite.course_open?
+        govuk_link_to t('.view_course'), invite.course.find_url
+      else
+        govuk_link_to t('.view_invite'), candidate_interface_invite_path(invite)
       end
     end
   end
