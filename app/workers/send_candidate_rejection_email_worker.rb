607f6fa105a6328203c499ed25a841a00d273a2e
David Young <david@dcyoung.dev>
2025-08-04 11:34:15 +0100

    Removed unused worker

--- app/workers/send_candidate_rejection_email_worker.rb
+++
@@ -1,9 +0,0 @@
-class SendCandidateRejectionEmailWorker
-  include Sidekiq::Worker
-
-  def perform(application_choice_id)
-    SendCandidateRejectionEmail.new(
-      application_choice: ApplicationChoice.find(application_choice_id),
-    ).call
-  end
-end
