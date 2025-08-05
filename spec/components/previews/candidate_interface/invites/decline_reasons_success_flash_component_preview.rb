71e32314dfa2eafcada85bd2fafa046d61389a72
David Young <david@dcyoung.dev>
2025-08-05 14:07:17 +0100

    Added small components for each state
    The main decline_reasons_success_flash_component.rb renders the relevant message based on the logic.
    
    Each small component has no logic involved

--- spec/components/previews/candidate_interface/invites/decline_reasons_success_flash_component_preview.rb
+++ spec/components/previews/candidate_interface/invites/decline_reasons_success_flash_component_preview.rb
@@ -1,20 +1,33 @@
 # frozen_string_literal: true
 
 class CandidateInterface::Invites::DeclineReasonsSuccessFlashComponentPreview < ViewComponent::Preview
-  include ViewComponent::TestHelpers
-
   def default
     invite = FactoryBot.build_stubbed(:pool_invite)
 
-    component_html = render_inline(CandidateInterface::Invites::DeclineReasonsSuccessFlashComponent.new(invite:)).to_s
+    render(CandidateInterface::Invites::DeclineReasonsSuccessFlashComponent.new(invite:))
+  end
 
-    render(::FlashMessageComponent.new(flash: {
-      success: [
-        I18n.t('candidate_interface.decline_reasons.create.header',
-               course: invite.course.name_and_code,
-               provider: invite.provider_name),
-        component_html,
-      ],
-    }))
+  def no_longer_interested
+    invite = FactoryBot.build_stubbed(:pool_invite)
+
+    render(CandidateInterface::Invites::DeclineReasonsSuccessFlashComponent::NoLongerInterestedComponent.new(invite:))
+  end
+
+  def update_location_and_funding_preferences
+    invite = FactoryBot.build_stubbed(:pool_invite)
+
+    render(CandidateInterface::Invites::DeclineReasonsSuccessFlashComponent::UpdateLocationAndFundingPreferencesComponent.new(invite:))
+  end
+
+  def change_funding_preferences
+    invite = FactoryBot.build_stubbed(:pool_invite)
+
+    render(CandidateInterface::Invites::DeclineReasonsSuccessFlashComponent::ChangeFundingPreferencesComponent.new(invite:))
+  end
+
+  def change_location_preferences
+    invite = FactoryBot.build_stubbed(:pool_invite)
+
+    render(CandidateInterface::Invites::DeclineReasonsSuccessFlashComponent::ChangeLocationPreferencesComponent.new(invite:))
   end
 end
