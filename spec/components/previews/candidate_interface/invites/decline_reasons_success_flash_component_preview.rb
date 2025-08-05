f1273c6c908f4d0f0a49d61804f504147b37406f
David Young <david@dcyoung.dev>
2025-08-05 14:24:09 +0100

    Wrap previews in a flash success component

--- spec/components/previews/candidate_interface/invites/decline_reasons_success_flash_component_preview.rb
+++ spec/components/previews/candidate_interface/invites/decline_reasons_success_flash_component_preview.rb
@@ -1,33 +1,80 @@
 # frozen_string_literal: true
 
 class CandidateInterface::Invites::DeclineReasonsSuccessFlashComponentPreview < ViewComponent::Preview
+  include ViewComponent::TestHelpers
+
   def default
     invite = FactoryBot.build_stubbed(:pool_invite)
 
-    render(CandidateInterface::Invites::DeclineReasonsSuccessFlashComponent.new(invite:))
+    component_html = render_inline(CandidateInterface::Invites::DeclineReasonsSuccessFlashComponent.new(invite:)).to_s
+
+    render(::FlashMessageComponent.new(flash: {
+      success: [
+        I18n.t('candidate_interface.decline_reasons.create.header',
+               course: invite.course.name_and_code,
+               provider: invite.provider_name),
+        component_html,
+      ],
+    }))
   end
 
   def no_longer_interested
     invite = FactoryBot.build_stubbed(:pool_invite)
 
-    render(CandidateInterface::Invites::DeclineReasonsSuccessFlashComponent::NoLongerInterestedComponent.new(invite:))
+    component_html = render_inline(CandidateInterface::Invites::DeclineReasonsSuccessFlashComponent::NoLongerInterestedComponent.new(invite:)).to_s
+
+    render(::FlashMessageComponent.new(flash: {
+      success: [
+        I18n.t('candidate_interface.decline_reasons.create.header',
+               course: invite.course.name_and_code,
+               provider: invite.provider_name),
+        component_html,
+      ],
+    }))
   end
 
   def update_location_and_funding_preferences
     invite = FactoryBot.build_stubbed(:pool_invite)
 
-    render(CandidateInterface::Invites::DeclineReasonsSuccessFlashComponent::UpdateLocationAndFundingPreferencesComponent.new(invite:))
+    component_html = render_inline(CandidateInterface::Invites::DeclineReasonsSuccessFlashComponent::UpdateLocationAndFundingPreferencesComponent.new(invite:)).to_s
+
+    render(::FlashMessageComponent.new(flash: {
+      success: [
+        I18n.t('candidate_interface.decline_reasons.create.header',
+               course: invite.course.name_and_code,
+               provider: invite.provider_name),
+        component_html,
+      ],
+    }))
   end
 
   def change_funding_preferences
     invite = FactoryBot.build_stubbed(:pool_invite)
 
-    render(CandidateInterface::Invites::DeclineReasonsSuccessFlashComponent::ChangeFundingPreferencesComponent.new(invite:))
+    component_html = render_inline(CandidateInterface::Invites::DeclineReasonsSuccessFlashComponent::ChangeFundingPreferencesComponent.new(invite:)).to_s
+
+    render(::FlashMessageComponent.new(flash: {
+      success: [
+        I18n.t('candidate_interface.decline_reasons.create.header',
+               course: invite.course.name_and_code,
+               provider: invite.provider_name),
+        component_html,
+      ],
+    }))
   end
 
   def change_location_preferences
     invite = FactoryBot.build_stubbed(:pool_invite)
 
-    render(CandidateInterface::Invites::DeclineReasonsSuccessFlashComponent::ChangeLocationPreferencesComponent.new(invite:))
+    component_html = render_inline(CandidateInterface::Invites::DeclineReasonsSuccessFlashComponent::ChangeLocationPreferencesComponent.new(invite:)).to_s
+
+    render(::FlashMessageComponent.new(flash: {
+      success: [
+        I18n.t('candidate_interface.decline_reasons.create.header',
+               course: invite.course.name_and_code,
+               provider: invite.provider_name),
+        component_html,
+      ],
+    }))
   end
 end
