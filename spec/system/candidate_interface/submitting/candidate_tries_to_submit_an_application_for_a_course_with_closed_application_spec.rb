13b7a698b9924eabd82a3aad89221a9c10ccc748
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2025-08-07 15:17:59 +0100

    Application closed validator updates

--- spec/system/candidate_interface/submitting/candidate_tries_to_submit_an_application_for_a_course_with_closed_application_spec.rb
+++ spec/system/candidate_interface/submitting/candidate_tries_to_submit_an_application_for_a_course_with_closed_application_spec.rb
@@ -9,44 +9,15 @@ RSpec.describe 'Candidate tries to submit an application choice when the course 
     and_i_have_one_application_in_draft
   end
 
-  scenario 'Apply is open but course not open for applications', time: mid_cycle do
-    and_my_course_choice_is_not_open_for_applications
-    when_i_continue_my_draft_application
-    then_i_see_the_course_closed_error_message
-  end
-
-  scenario 'Apply is closed and course open for applications before apply opens', time: after_find_opens do
-    and_my_course_choice_is_not_open_for_applications
+  scenario 'Apply is closed', time: after_find_opens do
     when_i_continue_my_draft_application
     then_i_see_the_course_closed_error_message_until_apply_opens
   end
 
-  scenario 'Apply is closed and course open for applications same day', time: after_find_opens do
-    and_my_course_choice_is_open_at_the_same_time_apply_opens
-    when_i_continue_my_draft_application
-    then_i_see_the_course_closed_error_message_until_apply_opens
-  end
-
-  scenario 'Apply is open and course is open', time: mid_cycle do
-    and_my_course_choice_is_open_at_the_same_time_apply_opens
+  scenario 'Apply is open', time: mid_cycle do
     when_i_continue_my_draft_application
     then_i_do_not_see_any_error_message
     and_i_can_review_my_application
-  end
-
-  def and_my_course_choice_is_not_open_for_applications
-    @application_choice.current_course.update!(applications_open_from: 2.days.from_now)
-  end
-
-  def and_my_course_choice_is_open_at_the_same_time_apply_opens
-    @application_choice.current_course.update!(applications_open_from: apply_opens_at)
-  end
-
-  def then_i_see_the_course_closed_error_message
-    expect(page).to have_content(
-      "This course is not yet open to applications. You will be able to submit your application on #{@application_choice.current_course.applications_open_from.to_fs(:govuk_date)}.",
-    )
-    expect(page).to have_no_content('Review application')
   end
 
   def then_i_see_the_course_closed_error_message_until_apply_opens
