1f8aabd3f830127e39322dfd10a5c64ff2eec7e3
David Young <david4.young@education.gov.uk>
2025-08-07 13:39:49 +0100

    Added helper methods to Invite and Invite Decline Reason (#11062)
    ## Context
    
    These helpers make it easy to query whether an Invite has been declined
    for a given reason
    
    Extracted from
    https://github.com/DFE-Digital/apply-for-teacher-training/pull/11047
    
    ## Changes proposed in this pull request
    
    Added helpers for reasons

--- app/models/pool/invite_decline_reason.rb
+++ app/models/pool/invite_decline_reason.rb
@@ -5,4 +5,22 @@ class Pool::InviteDeclineReason < ApplicationRecord
     draft: 'draft',
     published: 'published',
   }, default: :draft
+
+  def reason_only_salaried?
+    reason_inquiry.only_salaried?
+  end
+
+  def reason_location_not_convenient?
+    reason_inquiry.location_not_convenient?
+  end
+
+  def reason_no_longer_interested?
+    reason_inquiry.no_longer_interested?
+  end
+
+private
+
+  def reason_inquiry
+    (reason || '').inquiry
+  end
 end
