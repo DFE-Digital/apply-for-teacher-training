42702c92c78fd7322a73380b9f31624879da64d8
David Young <david@dcyoung.dev>
2025-08-08 15:02:59 +0100

    Extracted smaller components to separate files

---
+++ app/components/candidate_interface/invites/decline_reasons_success_flash/change_funding_preferences_component.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class CandidateInterface::Invites::DeclineReasonsSuccessFlash::ChangeFundingPreferencesComponent < CandidateInterface::Invites::DeclineReasonsSuccessFlashComponent
+  def call
+    tag.div do
+      concat(
+        tag.p do
+          concat govuk_link_to('Change your funding preferences', candidate_interface_candidate_preferences_review_path, class: 'govuk-notification-banner__link')
+          concat ' to receive invitations to more relevant courses'
+        end,
+      )
+
+      concat(
+        tag.p do
+          concat 'If you have changed your mind you can still '
+          concat govuk_link_to('apply to this course', candidate_interface_course_choices_course_confirm_selection_path(course), class: 'govuk-notification-banner__link')
+        end,
+      )
+    end
+  end
+end
