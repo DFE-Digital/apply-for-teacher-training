a010a15e6884b88a10877e7670319f9c70d794a2
CatalinVoineag <11318084+CatalinVoineag@users.noreply.github.com>
2025-08-08 17:10:16 +0100

    Add accepted_at on the application_choice for Candidate API 1.4
    This will help the The GIT Marketing to sent their emails when the offer
    has been accepted.
    
    This is not a braking change, so don't think it's necessary to use a new
    version.

--- app/services/candidate_api/serializers/v1_4.rb
+++ app/services/candidate_api/serializers/v1_4.rb
@@ -17,6 +17,25 @@ module CandidateAPI
           program_type: course.program_type,
         )
       end
+
+      def serialize_application_choices(application_form)
+        {
+          data:
+          application_form.application_choices.sort_by(&:id).map do |application_choice|
+            {
+              id: application_choice.id,
+              created_at: application_choice.created_at&.iso8601,
+              updated_at: application_choice.updated_at&.iso8601,
+              sent_to_provider_at: application_choice.sent_to_provider_at&.iso8601,
+              accepted_at: application_choice.accepted_at&.iso8601,
+              status: application_choice.status,
+              provider: serialize_provider(application_choice.provider),
+              course: serialize_course(application_choice.course),
+              interviews: serialize_interviews(application_choice),
+            }
+          end,
+        }
+      end
     end
   end
 end
