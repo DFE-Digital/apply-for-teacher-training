b4248785140be00b974f0ffaf8afdd6b74b69060
David Young <david@dcyoung.dev>
2025-08-18 16:41:57 +0100

    Another flaky test

--- spec/system/provider_interface/provider_removes_conditions_from_an_offer_spec.rb
+++ spec/system/provider_interface/provider_removes_conditions_from_an_offer_spec.rb
@@ -108,6 +108,7 @@ RSpec.describe 'Remove offer conditions' do
 
   def when_i_send_the_new_offer
     click_link_or_button 'Send new offer'
+    expect(page).to have_current_path provider_interface_application_choice_offer_path(@application_choice)
   end
 
   def then_the_candidate_has_the_new_conditions
