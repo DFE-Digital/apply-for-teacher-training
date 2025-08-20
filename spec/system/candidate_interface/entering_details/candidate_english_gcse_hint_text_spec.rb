b52ad8d8af610bdf999044afc67752a37d5fd44c
David Young <david@dcyoung.dev>
2025-08-20 14:00:13 +0100

    Fixed spec

--- spec/system/candidate_interface/entering_details/candidate_english_gcse_hint_text_spec.rb
+++ spec/system/candidate_interface/entering_details/candidate_english_gcse_hint_text_spec.rb
@@ -5,29 +5,33 @@ RSpec.describe 'English GCSE qualification form hint text' do
 
   scenario 'Candidate sees correct hint text for English non-UK qualification' do
     given_i_am_signed_in_with_one_login
-    and_i_wish_to_apply_to_a_course_that_requires_gcse_english
 
     when_i_visit_the_english_gcse_type_page
+    and_i_select_qualification_from_outside_the_uk
     then_i_see_the_correct_hint_text_for_english_non_uk_qualification
     and_i_do_not_see_the_old_hint_text_at_the_top
   end
 
   scenario 'Candidate sees original hint text for Maths non-UK qualification' do
     given_i_am_signed_in_with_one_login
-    and_i_wish_to_apply_to_a_course_that_requires_gcse_maths
 
     when_i_visit_the_maths_gcse_type_page
+    and_i_select_qualification_from_outside_the_uk
     then_i_see_the_original_hint_text_for_maths_non_uk_qualification
   end
 
-  private
+private
 
   def when_i_visit_the_english_gcse_type_page
-    visit candidate_interface_gcse_details_edit_type_path(subject: 'english')
+    click_on 'English GCSE or equivalent'
   end
 
   def when_i_visit_the_maths_gcse_type_page
-    visit candidate_interface_gcse_details_edit_type_path(subject: 'maths')
+    click_on 'Maths GCSE or equivalent'
+  end
+
+  def and_i_select_qualification_from_outside_the_uk
+    choose 'Qualification from outside the UK'
   end
 
   def then_i_see_the_correct_hint_text_for_english_non_uk_qualification
@@ -39,20 +43,6 @@ RSpec.describe 'English GCSE qualification form hint text' do
   end
 
   def and_i_do_not_see_the_old_hint_text_at_the_top
-    expect(page).not_to have_content('This should not be a qualification showing you speak English as a foreign language.')
-  end
-
-  def and_i_wish_to_apply_to_a_course_that_requires_gcse_english
-    @application_form = create_application_form(candidate: current_candidate)
-    @course = build(:course, funding_type: 'fee')
-    @course_option = build(:course_option, course: @course)
-    create(:application_choice, course_option: @course_option, application_form: @application_form)
-  end
-
-  def and_i_wish_to_apply_to_a_course_that_requires_gcse_maths
-    @application_form = create_application_form(candidate: current_candidate)
-    @course = build(:course, funding_type: 'fee')
-    @course_option = build(:course_option, course: @course)
-    create(:application_choice, course_option: @course_option, application_form: @application_form)
+    expect(page).to have_no_content('This should not be a qualification showing you speak English as a foreign language.')
   end
 end
