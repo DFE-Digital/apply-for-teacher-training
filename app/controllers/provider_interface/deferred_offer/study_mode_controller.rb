8c3f81d5c8c5eb08aef1ffc266ea3b7aef2ea4ff
David Young <david4.young@education.gov.uk>
2025-09-08 14:49:19 +0100

    Added question for changing the Study Type for a Deferred Offer (#11160)
    ## Context
    We want to be able to change the Course details of a Deferred Offer.
    
    ## Changes proposed in this pull request
    This PR introduces the Course select page, allowing a Provider User to
    set the Course that they want to defer the offer to.
    
    ## Guidance to review
    
    1. Sign in to the Support console
    2. then sign in as a Provider User "Susan Upport"
    3. then visit
    https://apply-review-11160.test.teacherservices.cloud/provider/applications/5/deferred-offer/check
    4. click "Change" next to the study mode details
    5. When you return to the "Check" page, Course and Location details will
    be missing - this is to be expected and rectified in coming PRs

---
+++ app/controllers/provider_interface/deferred_offer/study_mode_controller.rb
@@ -0,0 +1,37 @@
+class ProviderInterface::DeferredOffer::StudyModeController < ProviderInterface::ProviderInterfaceController
+  def edit
+    @study_mode_form = DeferredOfferConfirmation::StudyModeForm.find_or_initialize_by(
+      provider_user: current_provider_user,
+      offer: offer,
+    )
+  end
+
+  def update
+    @study_mode_form = DeferredOfferConfirmation::StudyModeForm.find_or_initialize_by(
+      provider_user: current_provider_user,
+      offer: offer,
+    )
+
+    if @study_mode_form.update(study_mode_form_params)
+      redirect_to provider_interface_deferred_offer_check_path(application_choice)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+private
+
+  def study_mode_form_params
+    params.expect(deferred_offer_confirmation_study_mode_form: [:study_mode])
+  end
+
+  def offer
+    application_choice.offer
+  end
+
+  def application_choice
+    @application_choice ||= GetApplicationChoicesForProviders.call(
+      providers: current_provider_user.providers,
+    ).find(params.require(:application_choice_id))
+  end
+end
