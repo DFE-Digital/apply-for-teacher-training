d91d654fafc54a5be7b7f5280e76a15a6fd49fbc
CatalinVoineag <11318084+CatalinVoineag@users.noreply.github.com>
2025-09-11 10:33:30 +0100

    Add PreviousTeacherTraining table
    This adds the migrations for the new form capturing if the candidate had
    undergone a previous teacher training
    
    - PreviousTeacherTraining for the form
    - The columns on the application_form follow the pattern we already have
    for most sections

---
+++ db/migrate/20250904140343_create_previous_teacher_trainings.rb
@@ -0,0 +1,16 @@
+class CreatePreviousTeacherTrainings < ActiveRecord::Migration[8.0]
+  def change
+    create_table :previous_teacher_trainings do |t|
+      t.references :application_form, null: false, foreign_key: { on_delete: :cascade }
+      t.references :provider, null: true, foreign_key: true
+      t.string :status, null: false, default: 'draft'
+      t.string :choice
+      t.string :provider_name
+      t.datetime :started_at
+      t.datetime :ended_at
+      t.text :details
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20250910102132_add_previous_teacher_training_to_application_form.rb
@@ -0,0 +1,6 @@
+class AddPreviousTeacherTrainingToApplicationForm < ActiveRecord::Migration[8.0]
+  def change
+    add_column(:application_forms, :previous_teacher_training_completed, :boolean)
+    add_column(:application_forms, :previous_teacher_training_completed_at, :datetime)
+  end
+end
