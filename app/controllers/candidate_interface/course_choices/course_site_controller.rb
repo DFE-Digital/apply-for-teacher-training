fc842d0501c5fb87044f4a40061e7adff20e0b96
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2025-09-15 15:21:07 +0100

    Match new course option when changing course

--- app/controllers/candidate_interface/course_choices/course_site_controller.rb
+++ app/controllers/candidate_interface/course_choices/course_site_controller.rb
@@ -3,6 +3,17 @@ module CandidateInterface
     class CourseSiteController < CandidateInterface::CourseChoices::BaseController
       include CandidateInterface::CourseChoices::Concerns::DuplicateCourseRedirect
 
+      def edit
+        @wizard = CandidateInterface::CourseChoices::CourseSelectionWizard.new(
+          current_step:,
+          step_params: update_params,
+          current_application:,
+          application_choice:,
+          edit: true,
+        )
+        @wizard.current_step.set_course_option_id
+      end
+
     private
 
       def current_step
