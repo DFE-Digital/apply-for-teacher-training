082b7cecc47107050bfc1b9723436340d15fcc63
Catalin Voineag <11318084+CatalinVoineag@users.noreply.github.com>
2025-09-26 09:53:11 +0100

    Fix invite status bug (#11231)
    There is a bug in the way we set the candidate_decision when a candidate
    declined an invite and then creates an application choice for the invited course.
    
    This sets the candidate_decision to accepted. But when they delete
    their that application choice or they change the course. We need to set
    the candidate_decision back to declined if they declined it in the
    first place.
    
    We know they declined it because we still have a declined reason
    attached to the invite, even if the candidate decides to create an
    application choice for the invited course, triggering a change in
    candidate_decision to accepted
    
    Previously we would set the candidate_decision back to not_responded.
    
    This was a problem as the candidate already responded to the invite, by
    declining. So they will not get any chasers or banners prompting them to
    respond to the invite.

--- app/services/candidate_interface/invite_application.rb
+++ app/services/candidate_interface/invite_application.rb
@@ -21,10 +21,9 @@ module CandidateInterface
       )
 
       if invite.present? && application_choice.persisted?
-        invite.update!(
-          application_choice_id: application_choice.id,
-          candidate_decision: 'accepted',
-        )
+        invite.application_choice_id = application_choice.id
+        invite.candidate_decision = calculate_candidate_decision(invite)
+        invite.save!
       end
     end
 
@@ -33,10 +32,9 @@ module CandidateInterface
     end
 
     def accept_and_link_to_choice!
-      @invite.update!(
-        application_choice:,
-        candidate_decision: 'accepted',
-      )
+      @invite.application_choice_id = application_choice.id
+      @invite.candidate_decision = calculate_candidate_decision(@invite)
+      @invite.save!
     end
 
     def self.unlink_invites_from_choice(application_choice:)
@@ -44,10 +42,23 @@ module CandidateInterface
     end
 
     def unlink_invites_from_choice
-      application_choice.published_invites.update_all(
-        application_choice_id: nil,
-        candidate_decision: 'not_responded',
-      )
+      ActiveRecord::Base.transaction do
+        application_choice.published_invites.each do |invite|
+          invite.application_choice_id = nil
+          invite.candidate_decision = calculate_candidate_decision(invite)
+          invite.save!
+        end
+      end
+    end
+
+    def calculate_candidate_decision(invite)
+      if invite.application_choice_id.nil? && invite.invite_decline_reasons.any?
+        'declined'
+      elsif invite.application_choice_id.nil?
+        'not_responded'
+      elsif invite.application_choice.present?
+        'accepted'
+      end
     end
 
   private
@@ -56,12 +67,18 @@ module CandidateInterface
       # When a candidate creates a draft choice for invited course and changes course to a non invited course
       # We then need to remove the link between the choice and the invite
       if application_choice.current_course == application_choice.original_course
-        application_choice.published_invites.where.not(
-          course_id: application_choice.current_course.id,
-        ).update_all(
-          application_choice_id: nil,
-          candidate_decision: 'not_responded',
-        )
+
+        ActiveRecord::Base.transaction do
+          invites = application_choice.published_invites.where.not(
+            course_id: application_choice.current_course.id,
+          )
+
+          invites.each do |invite|
+            invite.application_choice_id = nil
+            invite.candidate_decision = calculate_candidate_decision(invite)
+            invite.save!
+          end
+        end
       end
     end
   end
