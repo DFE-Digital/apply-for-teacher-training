0abed0150e391b91476ab5f149ae22ef88265b50
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2025-10-02 14:38:36 +0100

    Remove hourly checks that are no longer necessary

--- app/workers/detect_invariants_hourly_check.rb
+++ app/workers/detect_invariants_hourly_check.rb
@@ -1,36 +1,9 @@
-# Detect state that *should* be impossible in the system and report them to Sentry
 class DetectInvariantsHourlyCheck
   include Sidekiq::Worker
 
-  SIDEKIQ_QUEUE_NAMES = %w[low_priority default mailers big_query].freeze
-  SIDEKIQ_LATENCY_THRESHOLD = 120
-
   def perform
     detect_course_sync_not_succeeded_for_an_hour unless HostingEnvironment.review?
-    detect_high_sidekiq_retries_queue_length
-    detect_high_sidekiq_latency
     detect_unauthorised_application_form_edits
-    detect_application_choices_in_old_states
-  end
-
-  def detect_application_choices_in_old_states
-    choices_in_wrong_state =
-      ApplicationChoice
-      .where("status IN ('awaiting_references', 'application_complete')")
-      .map(&:id).sort
-
-    if choices_in_wrong_state.any?
-      urls = choices_in_wrong_state.map { |application_choice_id| helpers.support_interface_application_choice_url(application_choice_id) }
-
-      message = <<~MSG
-        One or more application choices are still in `awaiting_references` or
-        `application_complete` state, but all these states have been removed:
-
-        #{urls.join("\n")}
-      MSG
-
-      Sentry.capture_exception(ApplicationInRemovedState.new(message))
-    end
   end
 
   def detect_unauthorised_application_form_edits
@@ -66,36 +39,8 @@ class DetectInvariantsHourlyCheck
     end
   end
 
-  def detect_high_sidekiq_retries_queue_length
-    retries_queue_length = Sidekiq::RetrySet.new.size
-    if retries_queue_length > 50
-      Sentry.capture_exception(
-        SidekiqRetriesQueueHigh.new(
-          "Sidekiq pending retries depth is high (#{retries_queue_length}). Suggests high error rate",
-        ),
-      )
-    end
-  end
-
-  def detect_high_sidekiq_latency
-    SIDEKIQ_QUEUE_NAMES.each do |queue_name|
-      latency = Sidekiq::Queue.new(queue_name).latency
-
-      next unless latency >= SIDEKIQ_LATENCY_THRESHOLD
-
-      Sentry.capture_exception(
-        SidekiqHighLatency.new(
-          "Sidekiq queue #{queue_name} latency is high (#{latency}).",
-        ),
-      )
-    end
-  end
-
-  class ApplicationInRemovedState < StandardError; end
   class ApplicationEditedByWrongCandidate < StandardError; end
   class CourseSyncNotSucceededForAnHour < StandardError; end
-  class SidekiqRetriesQueueHigh < StandardError; end
-  class SidekiqHighLatency < StandardError; end
 
 private
 
