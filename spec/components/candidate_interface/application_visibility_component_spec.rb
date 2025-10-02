c9d3b943587d27f4a9f31e0d56a24b3cdfe8a738
Dan Daly <dan.daly@education.gov.uk>
2025-10-02 12:08:49 +0100

    Add offer method and robust visible to providers method

--- spec/components/candidate_interface/application_visibility_component_spec.rb
+++ spec/components/candidate_interface/application_visibility_component_spec.rb
@@ -67,8 +67,49 @@ RSpec.describe CandidateInterface::ApplicationVisibilityComponent, type: :compon
     end
   end
 
-  describe 'invisible?' do
-    it 'displays opted in but not visible if the application form is opted in and has choices awaiting decision' do
+  describe '#pool_opt_out_or_no_preference?' do
+    it 'returns true when candidate has opted out' do
+      application_form = create(:application_form)
+      _preference = create(
+        :candidate_preference,
+        pool_status: 'opt_out',
+        status: 'published',
+        application_form:,
+      )
+
+      component = described_class.new(application_form:)
+      expect(component.pool_opt_out_or_no_preference?).to be true
+    end
+
+    it 'returns true when candidate has no published preference' do
+      application_form = create(:application_form)
+      _preference = create(
+        :candidate_preference,
+        pool_status: 'opt_in',
+        status: 'draft',
+        application_form:,
+      )
+
+      component = described_class.new(application_form:)
+      expect(component.pool_opt_out_or_no_preference?).to be true
+    end
+
+    it 'returns false when candidate has opted in' do
+      application_form = create(:application_form)
+      _preference = create(
+        :candidate_preference,
+        pool_status: 'opt_in',
+        status: 'published',
+        application_form:,
+      )
+
+      component = described_class.new(application_form:)
+      expect(component.pool_opt_out_or_no_preference?).to be false
+    end
+  end
+
+  describe 'waiting_for_provider_decision?' do
+    it 'displays opted in but not visible to providers if the application form is opted in and has choices awaiting decision' do
       FeatureFlag.activate(:candidate_preferences)
       application_form = create(:application_form)
       _preference = create(
@@ -85,7 +126,7 @@ RSpec.describe CandidateInterface::ApplicationVisibilityComponent, type: :compon
       expect(page).to have_content('When all your applications are rejected, withdrawn or inactive, providers will be able to view your application details and invite you to apply. However, you should continue to apply to courses yourself. Do not wait to be invited.')
     end
 
-    it 'displays opted in but not visible if the application form is opted in and has choices with a status of interviewing' do
+    it 'displays opted in but not visible to providers if the application form is opted in and has choices with a status of interviewing' do
       FeatureFlag.activate(:candidate_preferences)
       application_form = create(:application_form)
       _preference = create(
@@ -101,8 +142,10 @@ RSpec.describe CandidateInterface::ApplicationVisibilityComponent, type: :compon
       expect(page).to have_content('Your application details are not currently visible to other providers. You have submitted applications that are waiting for a decision.')
       expect(page).to have_content('When all your applications are rejected, withdrawn or inactive, providers will be able to view your application details and invite you to apply. However, you should continue to apply to courses yourself. Do not wait to be invited.')
     end
+  end
 
-    it 'displays opted in and visible if the application form is opted in and has no applications awaiting decisions' do
+  describe 'offer?' do
+    it 'displays opted in but not visible to providers if the application form is opted in and has an offer' do
       FeatureFlag.activate(:candidate_preferences)
       application_form = create(:application_form)
       _preference = create(
@@ -111,28 +154,32 @@ RSpec.describe CandidateInterface::ApplicationVisibilityComponent, type: :compon
         status: 'published',
         application_form:,
       )
-      _withdrawn_application_choice = create(:application_choice, :withdrawn, application_form:)
+      _application_choice_awaiting_provider_decision = create(:application_choice, :offered, application_form:)
+
+      render_inline(described_class.new(application_form:))
+
+      expect(page).to have_content('Your application details are not currently visible to other providers. You have offers that you need to respond to.')
+      expect(page).to have_content('When all your applications are rejected, withdrawn or inactive, providers will be able to view your application details and invite you to apply. However, you should continue to apply to courses yourself. Do not wait to be invited.')
+    end
+  end
+
+  describe 'visible_to_providers?' do
+    it 'displays opted in and visible if the candidate is in the pool' do
+      FeatureFlag.activate(:candidate_preferences)
+      application_form = create(:application_form)
+      _preference = create(
+        :candidate_preference,
+        pool_status: 'opt_in',
+        status: 'published',
+        application_form:,
+      )
+      _application_choice = create(:application_choice, :withdrawn, application_form:)
+      create(:candidate_pool_application, application_form:, candidate: application_form.candidate)
 
       render_inline(described_class.new(application_form:))
 
       expect(page).to have_content('Your application details are currently visible to other providers. You have no submitted applications that are waiting for a decision.')
       expect(page).to have_content('Because all your applications are rejected, withdrawn or inactive, providers can view your application details and invite you to apply. However, you should continue to apply to courses yourself. Do not wait to be invited.')
-    end
-
-    it 'displays opted out if the application form is opted out' do
-      FeatureFlag.activate(:candidate_preferences)
-      application_form = create(:application_form)
-      _preference = create(
-        :candidate_preference,
-        pool_status: 'opt_out',
-        status: 'published',
-        application_form:,
-      )
-      _any_application_choice = create(:application_choice, :awaiting_provider_decision, application_form:)
-
-      render_inline(described_class.new(application_form:))
-
-      expect(page).to have_content('Your application details are not currently visible to other providers. You are not sharing your application details with other providers.')
     end
   end
 end
