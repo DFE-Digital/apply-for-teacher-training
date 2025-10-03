4655996efe7c0ce0640303be8b6cd993298c7e87
Dan Daly <dan.daly@education.gov.uk>
2025-10-03 17:02:55 +0100

    Add logic for only rendering component if conditions are met

--- app/components/candidate_interface/application_visibility_component.rb
+++ app/components/candidate_interface/application_visibility_component.rb
@@ -6,7 +6,15 @@ class CandidateInterface::ApplicationVisibilityComponent < ViewComponent::Base
   end
 
   def render?
-    FeatureFlag.active?(:candidate_preferences) && application_form.submitted_applications?
+    FeatureFlag.active?(:candidate_preferences) &&
+      application_form.submitted_applications? &&
+      (
+        pool_opt_out_or_no_preference? ||
+        visible_to_providers? ||
+        waiting_for_provider_decision? ||
+        application_form.withdrawn_no_longer_training? ||
+        offer?
+      )
   end
 
   def pool_opt_in?
--- spec/components/candidate_interface/application_visibility_component_spec.rb
+++ spec/components/candidate_interface/application_visibility_component_spec.rb
@@ -1,40 +1,96 @@
 require 'rails_helper'
 
 RSpec.describe CandidateInterface::ApplicationVisibilityComponent, type: :component do
-  describe '#render' do
-    context 'when candidate preference feature flag is enabled' do
-      it 'renders the component' do
-        FeatureFlag.activate(:candidate_preferences)
-        application_form = create(:application_form, :with_accepted_offer)
+  describe '#render?' do
+    before { FeatureFlag.activate(:candidate_preferences) }
 
-        component = described_class.new(application_form:)
-        result = render_inline(component)
+    let(:application_form) { create(:application_form) }
 
-        expect(result.to_html).not_to be_blank
+    subject(:component) { described_class.new(application_form:) }
+
+    context 'when feature flag is off' do
+      before { FeatureFlag.deactivate(:candidate_preferences) }
+
+      it 'returns false' do
+        expect(component.render?).to be false
       end
     end
 
-    context 'when candidate preference feature flag is not enabled' do
-      it 'renders the component' do
-        FeatureFlag.deactivate(:candidate_preferences)
-        application_form = create(:application_form, :with_accepted_offer)
+    context 'when application has not been submitted' do
+      before { allow(application_form).to receive(:submitted_applications?).and_return(false) }
 
-        component = described_class.new(application_form:)
-        result = render_inline(component)
-
-        expect(result.to_html).to be_blank
+      it 'returns false' do
+        expect(component.render?).to be false
       end
     end
 
-    context 'when candidate preference feature flag is enabled but no sent applications' do
-      it 'renders the component' do
-        FeatureFlag.activate(:candidate_preferences)
-        application_form = create(:application_form)
+    context 'when pool_opt_out_or_no_preference is true' do
+      before do
+        create(:candidate_preference, pool_status: 'opt_out', status: 'published', application_form:)
+        allow(application_form).to receive(:submitted_applications?).and_return(true)
+      end
 
-        component = described_class.new(application_form:)
-        result = render_inline(component)
+      it 'returns true' do
+        expect(component.render?).to be true
+      end
+    end
 
-        expect(result.to_html).to be_blank
+    context 'when visible_to_providers is true' do
+      before do
+        create(:candidate_pool_application, application_form:, candidate: application_form.candidate)
+        allow(application_form).to receive(:submitted_applications?).and_return(true)
+      end
+
+      it 'returns true' do
+        expect(component.render?).to be true
+      end
+    end
+
+    context 'when waiting_for_provider_decision is true' do
+      before do
+        create(:application_choice, :awaiting_provider_decision, application_form:)
+        create(:candidate_preference, pool_status: 'opt_in', status: 'published', application_form:)
+        allow(application_form).to receive(:submitted_applications?).and_return(true)
+      end
+
+      it 'returns true' do
+        expect(component.render?).to be true
+      end
+    end
+
+    context 'when withdrawn_no_longer_training is true' do
+      before do
+        withdrawn_choice = create(:application_choice, :withdrawn, application_form:)
+        create(:withdrawal_reason, application_choice: withdrawn_choice, reason: 'do-not-want-to-train-anymore.another_career_path_or_accepted_a_job_offer')
+        create(:candidate_preference, pool_status: 'opt_in', status: 'published', application_form:)
+        allow(application_form).to receive(:submitted_applications?).and_return(true)
+      end
+
+      it 'returns true' do
+        expect(component.render?).to be true
+      end
+    end
+
+    context 'when offer is true' do
+      before do
+        create(:application_choice, :offered, application_form:)
+        create(:candidate_preference, pool_status: 'opt_in', status: 'published', application_form:)
+        allow(application_form).to receive(:submitted_applications?).and_return(true)
+      end
+
+      it 'returns true' do
+        expect(component.render?).to be true
+      end
+    end
+
+    context 'when none of the visibility conditions are met' do
+      before do
+        create(:candidate_preference, pool_status: 'opt_in', status: 'published', application_form:)
+        allow(application_form).to receive(:submitted_applications?).and_return(true)
+      end
+
+      it 'returns false' do
+        expect(component.render?).to be false
       end
     end
   end
@@ -108,7 +164,7 @@ RSpec.describe CandidateInterface::ApplicationVisibilityComponent, type: :compon
     end
   end
 
-  describe 'waiting_for_provider_decision?' do
+  describe '#waiting_for_provider_decision?' do
     it 'displays opted in but not visible to providers if the application form is opted in and has choices awaiting decision' do
       FeatureFlag.activate(:candidate_preferences)
       application_form = create(:application_form)
@@ -144,7 +200,7 @@ RSpec.describe CandidateInterface::ApplicationVisibilityComponent, type: :compon
     end
   end
 
-  describe 'offer?' do
+  describe '#offer?' do
     it 'displays opted in but not visible to providers if the application form is opted in and has an offer' do
       FeatureFlag.activate(:candidate_preferences)
       application_form = create(:application_form)
@@ -163,7 +219,7 @@ RSpec.describe CandidateInterface::ApplicationVisibilityComponent, type: :compon
     end
   end
 
-  describe 'visible_to_providers?' do
+  describe '#visible_to_providers?' do
     it 'displays opted in and visible if the candidate is in the pool' do
       FeatureFlag.activate(:candidate_preferences)
       application_form = create(:application_form)
@@ -180,6 +236,29 @@ RSpec.describe CandidateInterface::ApplicationVisibilityComponent, type: :compon
 
       expect(page).to have_content('Your application details are currently visible to other providers. You have no submitted applications that are waiting for a decision.')
       expect(page).to have_content('Because all your applications are rejected, withdrawn or inactive, providers can view your application details and invite you to apply. However, you should continue to apply to courses yourself. Do not wait to be invited.')
+    end
+  end
+
+  describe '#application_form.withdrawn_no_longer_training?' do
+    context 'when application has a withdrawn choice with reason of not training to teach anymore' do
+      it 'renders the withdrawn_no_longer_training content' do
+        FeatureFlag.activate(:candidate_preferences)
+        application_form = create(:application_form)
+        _preference = create(
+          :candidate_preference,
+          pool_status: 'opt_in',
+          status: 'published',
+          application_form:,
+        )
+        withdrawn_offer = create(:application_choice, :withdrawn, application_form:)
+        create(:withdrawal_reason, application_choice: withdrawn_offer, reason: 'do-not-want-to-train-anymore.another_career_path_or_accepted_a_job_offer')
+
+        render_inline(described_class.new(application_form:))
+
+        expect(page).to have_content('Your application details are not currently visible to other providers.')
+        expect(page).to have_content("You have withdrawn an application with the reason 'I do not want to train to teach anymore'.")
+        expect(page).to have_link('contact support', href: 'mailto:becomingateacher@digital.education.gov.uk')
+      end
     end
   end
 end
