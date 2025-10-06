59faed9757e528dfccce02b913506b322ba631e2
CatalinVoineag <11318084+CatalinVoineag@users.noreply.github.com>
2025-10-06 10:01:00 +0100

    remove comments

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -35,13 +35,6 @@ class Candidate < ApplicationRecord
     class_name: 'Pool::Invite',
   )
 
-  # has_many :preferences, dependent: :destroy, class_name: 'CandidatePreference'
-  # has_many :published_preferences, -> { where(status: 'published') }, dependent: :destroy, class_name: 'CandidatePreference'
-  # has_many :duplicated_preferences, -> { where(status: 'duplicated') }, dependent: :destroy, class_name: 'CandidatePreference'
-  # has_many :published_opt_in_preferences, -> { where(status: 'published', pool_status: :opt_in) }, dependent: :destroy, class_name: 'CandidatePreference'
-  # has_many :published_location_preferences, class_name: 'CandidateLocationPreference', through: :published_preferences, source: :location_preferences
-  # has_many :published_opt_in_location_preferences, class_name: 'CandidateLocationPreference', through: :published_opt_in_preferences, source: :location_preferences
-
   PUBLISHED_FIELDS = %w[email_address].freeze
 
   enum :account_recovery_status, {
