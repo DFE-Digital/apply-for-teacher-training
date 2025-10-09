7dac9da59256059a997c0a29a578999b5442620e
CatalinVoineag <11318084+CatalinVoineag@users.noreply.github.com>
2025-10-09 13:02:58 +0100

    Consider study mode and better update course option method

--- app/workers/provider/change_choices_to_main_site_worker.rb
+++ app/workers/provider/change_choices_to_main_site_worker.rb
@@ -10,17 +10,33 @@ class Provider::ChangeChoicesToMainSiteWorker
     if choices.present? && main_site.present?
       choices.each do |choice|
         ActiveRecord::Base.transaction do
+          study_mode = choice.current_course_option.study_mode
           new_course_option = choice.current_course.course_options.joins(:site)
-            .find_by(site: { id: main_site.id })
+            .find_by(study_mode:, site: { id: main_site.id })
 
           if new_course_option.present?
-            choice.update_course_option_and_associated_fields!(
+            update_all_course_option_attributes!(
               new_course_option,
+              application_choice: choice,
               audit_comment: "Worker change_choices_to_main_site ran at #{Time.zone.now}",
             )
           end
         end
       end
     end
+  end
+
+private
+
+  def update_all_course_option_attributes!(new_course_option, application_choice:, audit_comment: nil)
+    attrs = {
+      current_course_option: new_course_option,
+      course_option: new_course_option,
+      original_course_option: new_course_option,
+      current_recruitment_cycle_year: new_course_option.course.recruitment_cycle_year,
+    }
+    attrs.merge!(audit_comment:) if audit_comment.present?
+
+    application_choice.update!(attrs)
   end
 end
--- spec/workers/provider/change_choices_to_main_site_worker_spec.rb
+++ spec/workers/provider/change_choices_to_main_site_worker_spec.rb
@@ -13,16 +13,20 @@ RSpec.describe Provider::ChangeChoicesToMainSiteWorker do
         choice = create(
           :application_choice,
           current_course_option: old_course_option,
+          course_option: old_course_option,
+          original_course_option: old_course_option,
         )
 
         expect {
           described_class.new.perform([choice.id], main_site.id)
         }.to change { choice.reload.current_course_option }.from(old_course_option).to(new_course_option)
+        .and change { choice.course_option }.from(old_course_option).to(new_course_option)
+        .and change { choice.original_course_option }.from(old_course_option).to(new_course_option)
       end
     end
 
     context 'when there is not main site for the course' do
-      it 'sets the choices to the main site' do
+      it 'does not set the choices to the main site' do
         provider = create(:provider)
         course = create(:course, provider:)
         main_site = create(:site, provider:)
@@ -35,7 +39,30 @@ RSpec.describe Provider::ChangeChoicesToMainSiteWorker do
 
         expect {
           described_class.new.perform([choice.id], main_site.id)
-        }.not_to(change { choice.reload.current_course_option })
+        }.to not_change(choice.reload, :current_course_option)
+        .and not_change(choice, :course_option)
+        .and not_change(choice, :original_course_option)
+      end
+    end
+
+    context 'when there is main site but there is not the same study mode' do
+      it 'does not set the choices to the main site' do
+        provider = create(:provider)
+        course = create(:course, provider:)
+        main_site = create(:site, provider:)
+
+        old_course_option = create(:course_option, :part_time, course:)
+        _new_course_option = create(:course_option, :full_time, course:, site: main_site)
+        choice = create(
+          :application_choice,
+          current_course_option: old_course_option,
+        )
+
+        expect {
+          described_class.new.perform([choice.id], main_site.id)
+        }.to not_change(choice.reload, :current_course_option)
+        .and not_change(choice, :course_option)
+        .and not_change(choice, :original_course_option)
       end
     end
   end
