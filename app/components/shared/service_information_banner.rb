663e6296155d21bbb6a0493055b1328c51c1963b
Dan Daly <dan.daly@education.gov.uk>
2025-10-15 08:41:50 +0100

    Configure service banner

--- app/components/shared/service_information_banner.rb
+++ app/components/shared/service_information_banner.rb
@@ -8,10 +8,20 @@ class ServiceInformationBanner < ViewComponent::Base
   end
 
   def body_content
-    HostingEnvironment.sandbox_mode? ? t("service_information_banner.#{@namespace}.sandbox_body") : t("service_information_banner.#{@namespace}.body")
+    HostingEnvironment.sandbox_mode? ? t("service_information_banner.#{@namespace}.sandbox_body") : t("service_information_banner.#{@namespace}.body_html", contact_us_link:, email_link:)
   end
 
   def render?
     FeatureFlag.active?('service_information_banner')
+  end
+
+private
+
+  def contact_us_link
+    view_context.govuk_link_to('contact us', 'https://getintoteaching.education.gov.uk/help-and-support', target: '_blank')
+  end
+
+  def email_link
+    view_context.mail_to('becomingateacher@digital.education.gov.uk')
   end
 end
