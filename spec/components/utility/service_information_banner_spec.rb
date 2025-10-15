4d2a353528d0110a3e43d3c586d1b4dc1d716270
Dan Daly <166535825+dp-daly@users.noreply.github.com>
2025-10-15 12:03:13 +0100

    Configure service banner (#11297)
    ## Context
    
    We have planned downtime on Thursday 16 October.
    
    ## Changes proposed in this pull request
    
    - Update service banner with service unavailable information for
    Thursday 16 October.
    
    ## Guidance to review
    
    <img width="772" height="125" alt="Screenshot 2025-10-15 at 11 27 20"
    src="https://github.com/user-attachments/assets/499e4f0b-4e52-4af0-8a28-9b39252bdeb9"
    />
    
    ## Things to check
    
    - [ ] If the code removes any existing feature flags, a data migration
    has also been added to delete the entry from the database
    - [ ] API release notes have been updated if necessary
    - [ ] If it adds a significant user-facing change, is it documented in
    the [CHANGELOG](CHANGELOG.md)?
    - [x] Attach the PR to the Trello card
    - [ ] This code adds a column or table to the database
      - [ ] This code does not rely on migrations in the same Pull Request
    - [ ] decide whether it needs to be in analytics yml file or analytics
    blocklist
    - [ ] data insights team has been informed of the change and have
    updated the pipeline
    - [ ] the sanitise.sql script and
    0025-protecting-personal-data-in-production-dump.md ADR have been
    updated
      - [ ] does the code safely backfill existing records for consistency

--- spec/components/utility/service_information_banner_spec.rb
+++ spec/components/utility/service_information_banner_spec.rb
@@ -9,12 +9,12 @@ RSpec.describe ServiceInformationBanner do
 
   context 'in the provider namespace' do
     it 'renders the banner' do
-      expect(result.text).to include('disruption')
+      expect(result.text).to include('unavailable')
     end
 
     context 'when the hosting environment is sandbox' do
       it 'renders the banner header', :sandbox do
-        expect(result.text).to include('disruption')
+        expect(result.text).to include('sandbox')
       end
     end
   end
@@ -23,12 +23,12 @@ RSpec.describe ServiceInformationBanner do
     let(:namespace) { :candidate }
 
     it 'renders the banner' do
-      expect(result.text).to include('disruption')
+      expect(result.text).to include('unavailable')
     end
 
     context 'when the hosting environment is sandbox' do
       it 'renders the banner header', :sandbox do
-        expect(result.text).to include('disruption')
+        expect(result.text).to include('unavailable')
       end
     end
   end
