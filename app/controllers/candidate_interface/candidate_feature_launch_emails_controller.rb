90c8aa7e89b42457590fa5002899c8dbd2bf22c8
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2025-10-20 11:18:14 +0100

    [lb/1407] Sentry re candidate preferences association (#11312)
    ## Context
    
    Sentry error related to candidate preferences being associated with the
    candidate instead of the application form.
    
    ## Changes proposed in this pull request
    
    <!-- If there are UI changes, please include Before and After
    screenshots. -->
    
    ## Guidance to review
    
    <!-- How could someone else check this work? Which parts do you want
    more feedback on? -->
    
    
    ## Things to check
    
    - [ ] If the code removes any existing feature flags, a data migration
    has also been added to delete the entry from the database
    - [ ] API release notes have been updated if necessary
    - [ ] If it adds a significant user-facing change, is it documented in
    the [CHANGELOG](CHANGELOG.md)?
    - [x] Attach the PR to the Trello card
    - [ ] This code adds a column or table to the database
      - [ ] This code does not rely on migrations in the same Pull Request
    - [ ] decide whether it needs to be in analytics yml file or analytics
    blocklist
    - [ ] data insights team has been informed of the change and have
    updated the pipeline
    - [ ] the sanitise.sql script and
    0025-protecting-personal-data-in-production-dump.md ADR have been
    updated
      - [ ] does the code safely backfill existing records for consistency

--- app/controllers/candidate_interface/candidate_feature_launch_emails_controller.rb
+++ app/controllers/candidate_interface/candidate_feature_launch_emails_controller.rb
@@ -6,12 +6,12 @@ class CandidateInterface::CandidateFeatureLaunchEmailsController < CandidateInte
 private
 
   def change_preferences_path
-    if current_candidate.published_preferences.last&.opt_out?
-      edit_candidate_interface_pool_opt_in_path(current_candidate.published_preferences.last)
-    elsif current_candidate.published_preferences.blank?
+    if current_application.published_preference&.opt_out?
+      edit_candidate_interface_pool_opt_in_path(current_application.published_preference)
+    elsif current_application.published_preference.blank?
       new_candidate_interface_pool_opt_in_path
     else
-      candidate_interface_draft_preference_publish_preferences_path(current_candidate.published_preferences.last)
+      candidate_interface_draft_preference_publish_preferences_path(current_application.published_preference)
     end
   end
 end
