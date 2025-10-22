a18e74d89a63b689e5a5d5f30b7ed98e2ff65a8d
CatalinVoineag <11318084+CatalinVoineag@users.noreply.github.com>
2025-10-22 14:13:58 +0100

    dont need this spec

--- spec/system/provider_interface/provider_marks_conditions_as_met_spec.rb
+++ spec/system/provider_interface/provider_marks_conditions_as_met_spec.rb
@@ -12,7 +12,6 @@ RSpec.describe 'Confirm conditions met' do
 
     when_i_navigate_to_an_offer_accepted_by_the_candidate
     and_i_navigate_to_the_offer_tab
-    then_i_see_change_offer_text
 
     when_i_click_on_confirm_conditions
     then_i_see_a_summary_of_the_standard_conditions
@@ -27,7 +26,6 @@ RSpec.describe 'Confirm conditions met' do
     and_the_candidate_is_recruited
     and_the_candidate_receives_an_email_notification
     and_i_navigate_to_the_offer_tab
-    then_i_see_change_offer_text
   end
 
   def given_i_am_a_provider_user_with_dfe_sign_in
@@ -67,10 +65,6 @@ RSpec.describe 'Confirm conditions met' do
 
   def and_i_navigate_to_the_offer_tab
     click_link_or_button 'Offer'
-  end
-
-  def then_i_see_change_offer_text
-    expect(page).to have_content 'To change this offer, send proof that the candidate agrees and details of the changes to becomingateacher@digital.education.gov.uk.'
   end
 
   def when_i_click_on_confirm_conditions
