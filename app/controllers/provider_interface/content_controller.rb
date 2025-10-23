01ecf12dffe1c7fdd5e8400abc362748dc0c5489
Catalin Voineag <11318084+CatalinVoineag@users.noreply.github.com>
2025-10-23 15:53:21 +0100

    New privacy content (#11326)
    We want our own privacy pages back!
    
    This PR updates the content of our privacy pages in apply and manage, mostly with links to somewhere else.

--- app/controllers/provider_interface/content_controller.rb
+++ app/controllers/provider_interface/content_controller.rb
@@ -14,18 +14,6 @@ module ProviderInterface
 
     def privacy; end
 
-    def service_privacy_notice
-      render_deprecated_privacy_notice_pages :service_privacy_notice,
-                                             breadcrumb_title: 'privacy_notices',
-                                             breadcrumb_path: provider_interface_privacy_path
-    end
-
-    def online_chat_privacy_notice
-      render_deprecated_privacy_notice_pages :online_chat_privacy_notice,
-                                             breadcrumb_title: 'privacy_notices',
-                                             breadcrumb_path: provider_interface_privacy_path
-    end
-
     def cookies_page
       @application = :manage
       @cookie_ga_code = ENV.fetch('GOOGLE_ANALYTICS_MANAGE', '').gsub('-', '_')
