d33a4ab78c7cdc8efcfc26ce4fa1028c93ff7668
CatalinVoineag <11318084+CatalinVoineag@users.noreply.github.com>
2025-11-05 15:59:37 +0000

    need this

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_11_05_155512) do
-  create_sequence "qualifications_public_id_seq", start: 114522
+  create_sequence "qualifications_public_id_seq", start: 120000
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
