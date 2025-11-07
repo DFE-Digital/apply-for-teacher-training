75bf34d9f445bf411f078c369279d8b60afc3dd4
CatalinVoineag <11318084+CatalinVoineag@users.noreply.github.com>
2025-11-07 15:12:53 +0000

    fix specs

--- spec/system/provider_interface/make_offer_ske_standard_flow_spec.rb
+++ spec/system/provider_interface/make_offer_ske_standard_flow_spec.rb
@@ -188,4 +188,51 @@ RSpec.describe 'Provider makes an offer with SKE enabled in standard courses' do
   def subject_name
     application_choice.course_option.course.subjects.first&.name
   end
+
+  def and_i_can_confirm_my_answers
+    expect(page).to have_content('A* on Maths A Level')
+  end
+
+  def when_i_click_change_course
+    @selected_course = @provider_available_course
+    @selected_course_option = @provider_available_course_option
+
+    within(all('.govuk-summary-list__row')[2]) do
+      click_link_or_button 'Change'
+    end
+  end
+
+  def and_i_can_confirm_the_new_course_selection
+    within(all('.govuk-summary-list__row')[2]) do
+      expect(page).to have_content(@selected_course.name_and_code)
+    end
+  end
+
+  def and_i_can_confirm_the_new_study_mode_selection
+    within(all('.govuk-summary-list__row')[3]) do
+      expect(page).to have_content(@selected_course_option.study_mode.humanize)
+    end
+  end
+
+  def and_i_can_confirm_the_new_location_selection
+    within(all('.govuk-summary-list__row')[5]) do
+      expect(page).to have_content(@selected_course_option.site.name_and_address(' '))
+    end
+  end
+
+  def when_i_click_change_provider
+    @selected_provider = @available_provider
+    @selected_course = @selected_provider_available_course
+    @selected_course_option = @selected_provider_available_course_option
+
+    within(all('.govuk-summary-list__row')[1]) do
+      click_link_or_button 'Change'
+    end
+  end
+
+  def and_i_can_confirm_the_new_provider_selection
+    within(all('.govuk-summary-list__row')[1]) do
+      expect(page).to have_content(@selected_provider.name_and_code)
+    end
+  end
 end
