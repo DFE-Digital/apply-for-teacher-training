00e558af22763b7995794c70662108ce4d83f01c
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2025-11-10 14:44:43 +0000

    wip

--- app/workers/find_a_candidate/populate_pool_worker.rb
+++ app/workers/find_a_candidate/populate_pool_worker.rb
@@ -7,26 +7,26 @@ class FindACandidate::PopulatePoolWorker
     # if CandidatePoolApplication.closed?
     #   CandidatePoolApplication.delete_all
     # else
-      application_forms_eligible_for_pool = Pool::Candidates.new.application_forms_in_the_pool
+    application_forms_eligible_for_pool = Pool::Candidates.new.application_forms_in_the_pool
 
-      applications = application_forms_eligible_for_pool
-                       .joins(application_choices: { course_option: { course: :subjects } })
-                       .joins(:published_opt_in_preferences)
-                       .left_joins(:not_responded_published_invites)
-                       .where.not(application_choices: { status: 'unsubmitted' })
-                       .select(
-                         'application_forms.id AS application_form_id',
-                         'application_forms.candidate_id AS candidate_id',
-                         "BOOL_OR(course_options.study_mode = 'full_time') AS study_mode_full_time",
-                         "BOOL_OR(course_options.study_mode = 'part_time') AS study_mode_part_time",
-                         "BOOL_OR(courses.program_type != 'TDA') AS course_type_postgraduate",
-                         "BOOL_OR(courses.program_type = 'TDA') AS course_type_undergraduate",
-                         'ARRAY_AGG(DISTINCT subjects.id) AS subject_ids',
-                         "COALESCE(ARRAY_AGG(DISTINCT courses.provider_id) FILTER (WHERE application_choices.status = 'rejected'), '{}') AS rejected_provider_ids",
-                         "MAX(CASE WHEN application_forms.right_to_work_or_study = 'no' OR (application_forms.right_to_work_or_study = 'yes' AND application_forms.immigration_status IN ('student_visa', 'skilled_worker_visa')) THEN 1 ELSE 0 END) = 1 AS needs_visa",
-                         'CURRENT_TIMESTAMP as created_at',
-                         'CURRENT_TIMESTAMP as updated_at',
-                         "MAX(CASE WHEN candidate_preferences.funding_type = 'fee'
+    applications = application_forms_eligible_for_pool
+                     .joins(application_choices: { course_option: { course: :subjects } })
+                     .joins(:published_opt_in_preferences)
+                     .left_joins(:not_responded_published_invites)
+                     .where.not(application_choices: { status: 'unsubmitted' })
+                     .select(
+                       'application_forms.id AS application_form_id',
+                       'application_forms.candidate_id AS candidate_id',
+                       "BOOL_OR(course_options.study_mode = 'full_time') AS study_mode_full_time",
+                       "BOOL_OR(course_options.study_mode = 'part_time') AS study_mode_part_time",
+                       "BOOL_OR(courses.program_type != 'TDA') AS course_type_postgraduate",
+                       "BOOL_OR(courses.program_type = 'TDA') AS course_type_undergraduate",
+                       'ARRAY_AGG(DISTINCT subjects.id) AS subject_ids',
+                       "COALESCE(ARRAY_AGG(DISTINCT courses.provider_id) FILTER (WHERE application_choices.status = 'rejected'), '{}') AS rejected_provider_ids",
+                       "MAX(CASE WHEN application_forms.right_to_work_or_study = 'no' OR (application_forms.right_to_work_or_study = 'yes' AND application_forms.immigration_status IN ('student_visa', 'skilled_worker_visa')) THEN 1 ELSE 0 END) = 1 AS needs_visa",
+                       'CURRENT_TIMESTAMP as created_at',
+                       'CURRENT_TIMESTAMP as updated_at',
+                       "MAX(CASE WHEN candidate_preferences.funding_type = 'fee'
                           OR EXISTS (
                                SELECT 1
                                FROM   application_forms
@@ -37,43 +37,43 @@ class FindACandidate::PopulatePoolWorker
                          THEN 1
                          ELSE 0
                        END) = 1 AS course_funding_type_fee",
-                       )
-                       .group(:id)
-                       .having("COUNT(DISTINCT pool_invites.id) < #{Pool::Invite::NUMBER_OF_INVITES_TO_REMOVE_FROM_POOL}")
+                     )
+                     .group(:id)
+                     .having("COUNT(DISTINCT pool_invites.id) < #{Pool::Invite::NUMBER_OF_INVITES_TO_REMOVE_FROM_POOL}")
 
-      insert_all_from_eligible_sql = <<~SQL
-        INSERT INTO candidate_pool_applications (
-          application_form_id,
-          candidate_id,
-          study_mode_full_time,
-          study_mode_part_time,
-          course_type_postgraduate,
-          course_type_undergraduate,
-          subject_ids,
-          rejected_provider_ids,
-          needs_visa,
-          created_at,
-          updated_at,
-          course_funding_type_fee
-        )
-        #{applications.to_sql}
-        ON CONFLICT(application_form_id)
-        DO UPDATE SET
-          candidate_id = EXCLUDED.candidate_id,
-          study_mode_full_time = EXCLUDED.study_mode_full_time,
-          study_mode_part_time = EXCLUDED.study_mode_part_time,
-          course_type_postgraduate = EXCLUDED.course_type_postgraduate,
-          course_type_undergraduate = EXCLUDED.course_type_undergraduate,
-          subject_ids = EXCLUDED.subject_ids,
-          rejected_provider_ids = EXCLUDED.rejected_provider_ids,
-          needs_visa = EXCLUDED.needs_visa,
-          updated_at = EXCLUDED.updated_at,
-          course_funding_type_fee = EXCLUDED.course_funding_type_fee
-      SQL
+    insert_all_from_eligible_sql = <<~SQL
+      INSERT INTO candidate_pool_applications (
+        application_form_id,
+        candidate_id,
+        study_mode_full_time,
+        study_mode_part_time,
+        course_type_postgraduate,
+        course_type_undergraduate,
+        subject_ids,
+        rejected_provider_ids,
+        needs_visa,
+        created_at,
+        updated_at,
+        course_funding_type_fee
+      )
+      #{applications.to_sql}
+      ON CONFLICT(application_form_id)
+      DO UPDATE SET
+        candidate_id = EXCLUDED.candidate_id,
+        study_mode_full_time = EXCLUDED.study_mode_full_time,
+        study_mode_part_time = EXCLUDED.study_mode_part_time,
+        course_type_postgraduate = EXCLUDED.course_type_postgraduate,
+        course_type_undergraduate = EXCLUDED.course_type_undergraduate,
+        subject_ids = EXCLUDED.subject_ids,
+        rejected_provider_ids = EXCLUDED.rejected_provider_ids,
+        needs_visa = EXCLUDED.needs_visa,
+        updated_at = EXCLUDED.updated_at,
+        course_funding_type_fee = EXCLUDED.course_funding_type_fee
+    SQL
 
-      CandidatePoolApplication.transaction do
-        CandidatePoolApplication.where.not(application_form_id: applications.ids).delete_all
-        ActiveRecord::Base.connection.execute(insert_all_from_eligible_sql)
+    CandidatePoolApplication.transaction do
+      CandidatePoolApplication.where.not(application_form_id: applications.ids).delete_all
+      ActiveRecord::Base.connection.execute(insert_all_from_eligible_sql)
       # end
     end
   end
