acafbea4e7ff05ee7bf196f71a8661a7b5deaaba
CatalinVoineag <11318084+CatalinVoineag@users.noreply.github.com>
2025-11-10 12:14:38 +0000

    don't need this

--- app/components/utility/summary_card_component.rb
+++ app/components/utility/summary_card_component.rb
@@ -1,26 +1,13 @@
 class SummaryCardComponent < ViewComponent::Base
-  def initialize(rows:, border: true, editable: true, ignore_editable: [], warning_text: nil, classes: [])
+  def initialize(rows:, border: true, editable: true, ignore_editable: [], warning_text: nil)
     rows = transform_hash(rows) if rows.is_a?(Hash)
     @rows = rows_including_actions_if_editable(rows, editable, ignore_editable)
     @border = border
     @warning_text = warning_text
-    @classes = classes
   end
 
   def border_css_class
     @border ? '' : 'no-border'
-  end
-
-  def additional_classes
-    if @classes
-      if @classes.any? { |style_class| style_class.include?('margin-bottom') }
-        @classes.join(' ')
-      else
-        @classes.push('govuk-!-margin-bottom-6').join(' ')
-      end
-    else
-      ''
-    end
   end
 
 private
