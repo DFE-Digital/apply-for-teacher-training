57b0afb76823fb3a2c53adc583c2c2406022e6fe
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2025-11-13 15:14:40 +0000

    Scope on course to search site codes in the API

--- spec/system/vendor_api/vendor_makes_an_offer_for_course_in_past_cycle_spec.rb
+++ spec/system/vendor_api/vendor_makes_an_offer_for_course_in_past_cycle_spec.rb
@@ -37,7 +37,7 @@ RSpec.describe 'Vendor makes an offer for a course in the past recruitment cycle
     validation_errors = parsed_response_body['errors']
 
     expect(@api_response.status).to eq 422
-    expect(validation_errors.first['message']).to eq("Site #{@course_option.site.code} does not exist for provider #{@provider.code} in #{current_year}")
+    expect(validation_errors.first['message']).to eq("Course must be in #{current_year} recruitment cycle")
   end
 
   def when_a_vendor_makes_an_offer_for_a_course_in_the_current_cycle
