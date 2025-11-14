2909523466a4d90b0e63b651387268a4197e912f
Jamie <jamiecleare2525@hotmail.com>
2025-11-14 16:51:32 +0000

    Change application visibility if the candidate makes an application

--- spec/components/candidate_interface/application_visibility_component_spec.rb
+++ spec/components/candidate_interface/application_visibility_component_spec.rb
@@ -223,6 +223,23 @@ RSpec.describe CandidateInterface::ApplicationVisibilityComponent, type: :compon
       expect(page).to have_content('Your application details are currently visible to other providers. You have no submitted applications that are waiting for a decision.')
       expect(page).to have_content('Because all your applications are rejected, withdrawn or inactive, providers can view your application details and invite you to apply. However, you should continue to apply to courses yourself. Do not wait to be invited.')
     end
+
+    context "when the candidate makes an application since being added to the pool" do
+      it "returns false" do
+        application_form = create(:application_form)
+        _preference = create(
+          :candidate_preference,
+          pool_status: 'opt_in',
+          status: 'published',
+          application_form:,
+          )
+        _application_choice = create(:application_choice, :withdrawn, application_form:)
+        create(:candidate_pool_application, application_form:, candidate: application_form.candidate)
+        create(:application_choice, :awaiting_provider_decision, application_form:)
+
+        expect(described_class.new(application_form:).visible_to_providers?).to be false
+      end
+    end
   end
 
   describe '#application_form.withdrawn_no_longer_training?' do
