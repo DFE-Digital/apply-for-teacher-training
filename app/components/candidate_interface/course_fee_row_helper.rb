e4f823b75fcdefc59c90d21cc1f288dba0654885
Jamie Cleare <jamiecleare2525@hotmail.com>
2025-12-01 13:59:26 +0000

    Updates to review application content (#11460)
    ## Context
    
    - Add warning text above qualifications on the "Review your application"
    page
    - Add Financial support guidance to the "course fee" on the "Review your
    application" page, unless:
    - The candidate is an international student and selected a course
    containing a language or physics subject.
      
    ## Changes proposed in this pull request
    
    - Add warning text above qualifications on the "Review your application"
    page
    - Add method to the `CandidateInterface::CourseFeeRowHelper`, to display
    financial support guidance conditionally.
    
    ## Guidance to review
    
    - Visit https://apply-review-11460.test.teacherservices.cloud/support
    - Sign in as a support user
    ------------
    _International candidate_
    - Visit
    https://apply-review-11460.test.teacherservices.cloud/support/applications/69
    - Sign in as the candidate
    - Click on the applications for "Physics" and "Modern languages"
    - Check that the funding guidance DOES NOT appear
    - Check that the warning text above "Qualifications" DOES appear
    - Return to applications
    - Click on the applications for "Computing" or "Geography"
    - Check that the funding guidance DOES appear
    --------------
    _Domestic candidate_
    - Visit
    https://apply-review-11460.test.teacherservices.cloud/support/applications/70
    - Sign in as the candidate
    - Click on the application
    - Check that the funding guidance DOES NOT appear
    - Check that the warning text above "Qualifications" DOES appear
    
    ## Things to check
    
    - [ ] If the code removes any existing feature flags, a data migration
    has also been added to delete the entry from the database
    - [ ] API release notes have been updated if necessary
    - [ ] If it adds a significant user-facing change, is it documented in
    the [CHANGELOG](CHANGELOG.md)?
    - [x] Attach the PR to the Trello card
    - [ ] This code adds a column or table to the database
      - [ ] This code does not rely on migrations in the same Pull Request
    - [ ] decide whether it needs to be in analytics yml file or analytics
    blocklist
    - [ ] data insights team has been informed of the change and have
    updated the pipeline
    - [ ] the sanitise.sql script and
    0025-protecting-personal-data-in-production-dump.md ADR have been
    updated
      - [ ] does the code safely backfill existing records for consistency
    
    ## Trello
    
    
    https://trello.com/c/gs8l1J8z/1204-update-the-review-application-summary-cards-international-fees-and-degree-grade
    
    ## Screenshots
    
    <img width="379" height="706" alt="Screenshot 2025-11-27 at 16 28 13"
    src="https://github.com/user-attachments/assets/23afb19a-8b9a-4e41-ac9b-89c03fbf67a9"
    />

--- app/components/candidate_interface/course_fee_row_helper.rb
+++ app/components/candidate_interface/course_fee_row_helper.rb
@@ -1,11 +1,12 @@
 module CandidateInterface
   module CourseFeeRowHelper
-    def course_fee_row(course)
+    def course_fee_row(application_choice, course)
       return unless course.fee?
 
       {
         key: I18n.t('course_fee_row_helper.course_fee'),
-        value: domestic_fee(course) + international_fee(course),
+        value: domestic_fee(course) + international_fee(course) +
+          funding_advise(application_choice, course),
       }
     end
 
@@ -31,6 +32,28 @@ module CandidateInterface
         ),
         class: 'govuk-body',
       )
+    end
+
+    def funding_advise(application_choice, course)
+      return '' unless application_choice.application_form.international_applicant? &&
+                       !(Subject.languages.intersect?(course.subjects) || Subject.physics.intersect?(course.subjects))
+
+      content_tag :p, class: 'govuk-body secondary-text' do
+        concat(
+          content_tag(:span, I18n.t('course_fee_row_helper.non_uk_citizens_are_unlikely_to_get_help_funding')),
+        )
+        concat(
+          govuk_link_to(
+            I18n.t(
+              'course_fee_row_helper.funding_for_non_uk_citizens',
+            ),
+            'https://getintoteaching.education.gov.uk/non-uk-teachers/fees-and-funding-for-non-uk-trainees',
+            no_visited_state: true,
+            new_tab: true,
+            target: '_blank',
+          ),
+        )
+      end
     end
   end
 end
