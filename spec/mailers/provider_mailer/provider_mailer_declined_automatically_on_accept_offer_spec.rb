9ad27c3db7f41f4ba36da1433f3af0fb918658a6
Dan Daly <166535825+dp-daly@users.noreply.github.com>
2025-12-02 08:18:05 +0000

    Auto decline offer on acceptance of another offer email  (#11465)
    ## Context
    
    We created a new email to alert providers when a candidate’s application
    has been withdrawn because they have accepted an offer elsewhere (PR
    #11455).
    
    There is another scenario where any offers they have can be declined
    automatically when they accept another offer. This card is to create
    that email.
    
    ## Changes proposed in this pull request
    
    - Add a new email to describe an auto-decline upon accepting another
    offer
    - Update the DeclineApplication service to accept an accepted_offer
    boolean value
    - Set the accepted_offer value in the AcceptOffer service
    - Add conditional logic to send the auto-decline email when
    accepted_offer is set to true
    
    ## Guidance to review
    
    - In the review console, set Susan Upport's email address to yours:
    `ProviderUser.find(2).update(email_address:
    'your.name@education.gov.uk')`
    - Login in as Roland Kozely in the review app:
    https://apply-review-11465.test.teacherservices.cloud/support/applications/76
    - Accept the UCL offer
    - You should receive the new auto decline email
    
    - Now find any candidate with an offer from UCL, NIoT@Harris Initial
    Teacher Training, or Keele
    - Decline the offer
    - You should receive the standard decline email
    
    ## Preview
    
    -
    https://apply-review-11465.test.teacherservices.cloud/rails/mailers/provider/applications_mailer/declined_automatically_on_accept_offer
    
    ## Things to check
    
    - [ ] If the code removes any existing feature flags, a data migration
    has also been added to delete the entry from the database
    - [ ] API release notes have been updated if necessary
    - [ ] If it adds a significant user-facing change, is it documented in
    the [CHANGELOG](CHANGELOG.md)?
    - [x] Attach the PR to the Trello card
    - [ ] This code adds a column or table to the database
      - [ ] This code does not rely on migrations in the same Pull Request
    - [ ] decide whether it needs to be in analytics yml file or analytics
    blocklist
    - [ ] data insights team has been informed of the change and have
    updated the pipeline
    - [ ] the sanitise.sql script and
    0025-protecting-personal-data-in-production-dump.md ADR have been
    updated
      - [ ] does the code safely backfill existing records for consistency

---
+++ spec/mailers/provider_mailer/provider_mailer_declined_automatically_on_accept_offer_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe ProviderMailer do
+  include TestHelpers::MailerSetupHelper
+
+  let(:email) { described_class.declined_automatically_on_accept_offer(provider_user, application_choices.first) }
+
+  describe '.declined_automatically_on_accept_offer' do
+    let(:provider_user) { build_stubbed(:provider_user, first_name: 'Johny', last_name: 'English') }
+    let(:application_choices) { [build_stubbed(:application_choice, :awaiting_provider_decision)] }
+
+    before { application_form }
+
+    it_behaves_like('a mail with subject and content',
+                    'Fred ’s offer has been automatically declined - manage teacher training applications',
+                    'provider name' => 'Dear Johny English',
+                    'candidate name' => 'Fred ',
+                    'offer link' => /http:\/\/localhost:3000\/provider\/applications\/\d+\/offers/,
+                    'notification settings' => 'You can change your email notification settings',
+                    'footer' => 'Get help, report a problem or give feedback')
+  end
+end
