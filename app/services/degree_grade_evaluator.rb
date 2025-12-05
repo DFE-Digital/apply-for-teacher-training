a04170db8b17e1bda246300583185058fc753ffe
Dan Daly <dan.daly@education.gov.uk>
2025-12-05 12:31:42 +0000

    Remove degree_required_component and extract into a service

--- app/services/degree_grade_evaluator.rb
+++ app/services/degree_grade_evaluator.rb
@@ -1,4 +1,4 @@
-class CandidateInterface::DegreeRequiredComponent < ViewComponent::Base
+class DegreeGradeEvaluator
   CANDIDATE_GRADES = {
     'First class honours' => 4,
     'First-class honours' => 4,
@@ -28,29 +28,46 @@ class CandidateInterface::DegreeRequiredComponent < ViewComponent::Base
     'Pass' => 'an Ordinary degree (pass).',
   }.freeze
 
-  attr_accessor :application_choice, :course_degree_requirements, :uk_bachelor_degrees
+  attr_accessor :application_choice
 
   def initialize(application_choice)
     @application_choice = application_choice
-    @course_degree_requirements = application_choice.current_course.degree_grade
-    @uk_bachelor_degrees = find_uk_and_compatible_bachelor_degrees(application_choice)
   end
 
-  def candidate_degree_grade_below_required_grade?
-    return false if @uk_bachelor_degrees.empty? || degrees_with_other_grades_only? || bachelors_and_masters_degree?
+  def degree_grade_below_required_grade?
+    return false if uk_bachelor_degrees.empty? || degrees_with_other_grades_only? || bachelors_and_masters_degree?
 
-    COURSE_REQUIRED_GRADES[@course_degree_requirements] > CANDIDATE_GRADES[highest_degree_grade]
+    COURSE_REQUIRED_GRADES[required_course_degree_grade] > CANDIDATE_GRADES[highest_degree_grade]
   end
-
-private
 
   def highest_degree_grade
     @highest_degree_grade ||= return_highest_valid_degree_grade
   end
 
+  def required_course_degree_text
+    COURSE_REQUIRED_GRADE_TEXT[required_course_degree_grade]
+  end
+
+private
+
+  def uk_bachelor_degrees
+    @uk_bachelor_degrees ||= find_uk_and_compatible_bachelor_degrees
+  end
+
+  def find_uk_and_compatible_bachelor_degrees
+    possible_institution_countries = [nil, 'GB'] + ApplicationQualification::COUNTRIES_WITH_COMPATIBLE_DEGREES.keys
+    application_choice.application_form.application_qualifications
+      .where(level: 'degree', other_uk_qualification_type: nil, institution_country: possible_institution_countries)
+      .where(qualification_type_hesa_code: Hesa::DegreeType.bachelor_hesa_codes)
+  end
+
   def return_highest_valid_degree_grade
     grades = uk_bachelor_degrees.where(grade: CANDIDATE_GRADES.keys).map(&:grade)
     grades.max_by { |grade| CANDIDATE_GRADES[grade] }
+  end
+
+  def required_course_degree_grade
+    application_choice.current_course.degree_grade
   end
 
   def degrees_with_other_grades_only?
@@ -65,13 +82,7 @@ private
   def candidate_has_masters_degree?
     application_choice.application_form.application_qualifications
       .where(level: 'degree', other_uk_qualification_type: nil, institution_country: [nil, 'GB'])
-      .where(qualification_type_hesa_code: Hesa::DegreeType.master_hesa_codes).any?
-  end
-
-  def find_uk_and_compatible_bachelor_degrees(application_choice)
-    possible_institution_countries = [nil, 'GB'] + ApplicationQualification::COUNTRIES_WITH_COMPATIBLE_DEGREES.keys
-    application_choice.application_form.application_qualifications
-      .where(level: 'degree', other_uk_qualification_type: nil, institution_country: possible_institution_countries)
-      .where(qualification_type_hesa_code: Hesa::DegreeType.bachelor_hesa_codes)
+      .where(qualification_type_hesa_code: Hesa::DegreeType.master_hesa_codes)
+      .any?
   end
 end
