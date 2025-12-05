16ea92f0ad77c5c373f3bc6c93b651a9acef327c
Dan Daly <dan.daly@education.gov.uk>
2025-12-05 14:55:12 +0000

    Create interruption page

---
+++ app/controllers/candidate_interface/course_choices/review_degree_grade_interruption_controller.rb
@@ -0,0 +1,27 @@
+module CandidateInterface
+  module CourseChoices
+    class ReviewDegreeGradeInterruptionController < CandidateInterface::CourseChoices::BaseController
+      before_action :redirect_to_your_applications_if_submitted
+
+      def show
+        @application_choice = current_application.application_choices.find(params[:application_choice_id])
+        @required_grade = required_course_degree_text
+        @candidate_highest_grade = highest_degree_grade
+      end
+
+    private
+
+      def required_course_degree_text
+        grade_evaluator.required_course_degree_text
+      end
+
+      def highest_degree_grade
+        grade_evaluator.highest_degree_grade
+      end
+
+      def grade_evaluator
+        DegreeGradeEvaluator.new(@application_choice)
+      end
+    end
+  end
+end
