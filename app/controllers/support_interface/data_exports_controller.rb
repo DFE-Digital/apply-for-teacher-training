77de4336cce24932f2cad380799ba2ab91b418fb
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2026-01-11 15:29:00 +0000

    WIP

--- app/controllers/support_interface/data_exports_controller.rb
+++ app/controllers/support_interface/data_exports_controller.rb
@@ -20,7 +20,7 @@ module SupportInterface
     end
 
     def view_export_information
-      @data_export = DataExport::EXPORT_TYPES[params[:data_export_type].to_sym]
+      @data_export_type = DataExport::EXPORT_TYPES[params[:data_export_type].to_sym]
     end
 
     def view_history
@@ -34,23 +34,28 @@ module SupportInterface
       @data_exports = @data_exports.select(DataExport.column_names - %w[data])
     end
 
-    def new
-      @export_types = DataExport::EXPORT_TYPES
-    end
-
     def create
       export_type = DataExport::EXPORT_TYPES.fetch(params.fetch(:export_type_id).to_sym)
-      data_export = DataExport.create!(name: export_type.fetch(:name), initiator: current_support_user, export_type: export_type.fetch(:export_type))
-      DataExporter.perform_async(export_type.fetch(:class).to_s, data_export.id, export_options)
-
-      redirect_to support_interface_data_export_path(data_export)
+      if export_type.fetch(:deprecated)
+        flash[:warning] = 'This export type has been deprecated and cannot be generated'
+        redirect_to support_interface_data_directory_path
+      else
+        data_export = DataExport.create!(name: export_type.fetch(:name), initiator: current_support_user, export_type: export_type.fetch(:export_type))
+        DataExporter.perform_async(export_type.fetch(:class).to_s, data_export.id, export_options)
+        redirect_to support_interface_data_export_path(data_export)
+      end
     end
 
     def download
       data_export = DataExport.where.associated(:file_attachment).find(params[:id])
-      data_export.update!(audit_comment: 'File downloaded')
+      if data_export.export_type_deprecated?
+        flash[:warning] = 'This export type has been deprecated and cannot be generated'
+        redirect_to support_interface_data_directory_path
+      else
+        data_export.update!(audit_comment: 'File downloaded')
 
-      redirect_to rails_blob_path(data_export.file, disposition: 'attachment')
+        redirect_to rails_blob_path(data_export.file, disposition: 'attachment')
+      end
     end
 
     def data_set_documentation
