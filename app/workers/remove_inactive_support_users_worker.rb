0b5fa1a1d4a1916786efd84a1835757d829037d5
CatalinVoineag <11318084+CatalinVoineag@users.noreply.github.com>
2026-01-13 14:47:04 +0000

    Account for support users that never logged in

--- app/workers/remove_inactive_support_users_worker.rb
+++ app/workers/remove_inactive_support_users_worker.rb
@@ -4,6 +4,8 @@ class RemoveInactiveSupportUsersWorker
   sidekiq_options queue: :low_priority
 
   def perform
-    SupportUser.where('last_signed_in_at < ?', 9.months.ago)&.discard_all
+    SupportUser.where('last_signed_in_at < ?', 9.months.ago)
+    .or(SupportUser.where('last_signed_in_at IS NULL AND created_at < ?', 9.months.ago))
+    &.discard_all
   end
 end
--- spec/workers/remove_inactive_support_users_worker_spec.rb
+++ spec/workers/remove_inactive_support_users_worker_spec.rb
@@ -4,10 +4,12 @@ RSpec.describe RemoveInactiveSupportUsersWorker do
   describe '#perform' do
     it 'discards inactive support users' do
       should_discard = create(:support_user, last_signed_in_at: 10.months.ago)
+      account_never_used = create(:support_user, last_signed_in_at: nil, created_at: 10.months.ago)
       should_not_discard = create(:support_user, last_signed_in_at: 8.months.ago)
 
-      expect { described_class.new.perform }.to change { SupportUser.kept.count }.by(-1)
+      expect { described_class.new.perform }.to change { SupportUser.kept.count }.by(-2)
       expect(should_discard.reload.discarded_at.present?).to be(true)
+      expect(account_never_used.reload.discarded_at.present?).to be(true)
       expect(should_not_discard.reload.discarded_at).to be_nil
     end
   end
