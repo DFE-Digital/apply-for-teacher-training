5d36b0ab67b5ced5086233ecfd7e74071d19a5ac
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2026-01-13 09:52:47 +0000

    Remove access to old data reports (#11554)
    ## Context
    
    The Data directory in the support console is largely unused. It was
    conceived of before we were using Blazer or BigQuery to create
    dashboards for monitoring features. Many of these reports would contain
    personal data that can end up on a person's machine. If you have a look
    in production, most of these exports have not been generated since 2022.
    It feels like a potential data breach to have them sitting around and
    available for download.
    
    ## Changes proposed in this pull request
    
    I've marked all but a handful of exports as 'deprecated'. 
    
    We can still view the history of these reports and see who generated
    them, but they will no longer be available to view. And you cannot
    generate more.
    
    These are the only active reports now: 
    
    - active_provider_user_permissions,
    - active_provider_users,
    - ministerial_report_applications_export,
    - ministerial_report_candidates_export,
    - tad_applications,
    - tad_provider_performance,
    - tad_subject_domicile_nationality,
    - who_ran_which_export
    
    
    https://github.com/user-attachments/assets/79eab7d2-1479-4497-850a-c143974fd3a8
    
    ## Guidance to review
    
    1. In the review app, navigate to the data directory (performance ->
    data directory)
    2. Click one one of the above reports. You should be able to generate a
    new one and then download it
    3. Go back to the data directory
    4. Click on a report that is deprecated (eg,
    structured_reasons_for_rejection)
    5. You shouldn't see the green button to download. 
    
    ## Things to check
    
    - [ ] If the code removes any existing feature flags, a data migration
    has also been added to delete the entry from the database
    - [ ] API release notes have been updated if necessary
    - [ ] If it adds a significant user-facing change, is it documented in
    the [CHANGELOG](CHANGELOG.md)?
    - [x] Attach the PR to the Trello card
    - [ ] This code adds a column or table to the database
      - [ ] This code does not rely on migrations in the same Pull Request
    - [ ] decide whether it needs to be in analytics yml file or analytics
    blocklist
    - [ ] data insights team has been informed of the change and have
    updated the pipeline
    - [ ] the sanitise.sql script and
    0025-protecting-personal-data-in-production-dump.md ADR have been
    updated
      - [ ] does the code safely backfill existing records for consistency

--- app/controllers/support_interface/data_exports_controller.rb
+++ app/controllers/support_interface/data_exports_controller.rb
@@ -20,7 +20,7 @@ module SupportInterface
     end
 
     def view_export_information
-      @data_export = DataExport::EXPORT_TYPES[params[:data_export_type].to_sym]
+      @data_export_type = DataExport::EXPORT_TYPES[params[:data_export_type].to_sym]
     end
 
     def view_history
@@ -35,22 +35,31 @@ module SupportInterface
     end
 
     def new
-      @export_types = DataExport::EXPORT_TYPES
+      @export_types = DataExport.active_export_types
     end
 
     def create
       export_type = DataExport::EXPORT_TYPES.fetch(params.fetch(:export_type_id).to_sym)
-      data_export = DataExport.create!(name: export_type.fetch(:name), initiator: current_support_user, export_type: export_type.fetch(:export_type))
-      DataExporter.perform_async(export_type.fetch(:class).to_s, data_export.id, export_options)
-
-      redirect_to support_interface_data_export_path(data_export)
+      if export_type.fetch(:deprecated)
+        flash[:warning] = 'This export type has been deprecated and cannot be generated'
+        redirect_to support_interface_data_directory_path
+      else
+        data_export = DataExport.create!(name: export_type.fetch(:name), initiator: current_support_user, export_type: export_type.fetch(:export_type))
+        DataExporter.perform_async(export_type.fetch(:class).to_s, data_export.id, export_options)
+        redirect_to support_interface_data_export_path(data_export)
+      end
     end
 
     def download
       data_export = DataExport.where.associated(:file_attachment).find(params[:id])
-      data_export.update!(audit_comment: 'File downloaded')
+      if data_export.export_type_deprecated?
+        flash[:warning] = 'This export type has been deprecated and is no longer available to download'
+        redirect_to support_interface_data_directory_path
+      else
+        data_export.update!(audit_comment: 'File downloaded')
 
-      redirect_to rails_blob_path(data_export.file, disposition: 'attachment')
+        redirect_to rails_blob_path(data_export.file, disposition: 'attachment')
+      end
     end
 
     def data_set_documentation
