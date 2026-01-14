0bad7cdb647b7a78b851b05e53895896b22ed349
Dan Daly <dan.daly@education.gov.uk>
2026-01-14 12:12:22 +0000

    Remove unnecessary rescues

--- app/controllers/provider_interface/account_controller.rb
+++ app/controllers/provider_interface/account_controller.rb
@@ -2,14 +2,6 @@ module ProviderInterface
   class AccountController < ProviderInterfaceController
     skip_before_action :redirect_unless_user_associated_with_an_organisation
 
-    rescue_from ProviderUserWithoutOrganisationError, with: :redirect_to_your_account
-
     def show; end
-
-  private
-
-    def redirect_to_your_account
-      redirect_to provider_interface_account_path
-    end
   end
 end
--- app/controllers/provider_interface/personal_details_controller.rb
+++ app/controllers/provider_interface/personal_details_controller.rb
@@ -2,8 +2,6 @@ module ProviderInterface
   class PersonalDetailsController < ProviderInterfaceController
     skip_before_action :redirect_unless_user_associated_with_an_organisation
 
-    rescue_from ProviderUserWithoutOrganisationError, with: :redirect_to_your_personal_details
-
     def show
       @dsi_profile_url = dsi_profile_url
     end
@@ -14,10 +12,6 @@ module ProviderInterface
       return 'https://test-profile.signin.education.gov.uk' if HostingEnvironment.qa?
 
       'https://profile.signin.education.gov.uk'
-    end
-
-    def redirect_to_your_personal_details
-      redirect_to provider_interface_personal_details_path
     end
   end
 end
