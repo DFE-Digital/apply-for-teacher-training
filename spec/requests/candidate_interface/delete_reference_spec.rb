27c35ff998ebe2b60af5899af246d4394eb18739
Jamie <jamiecleare2525@hotmail.com>
2026-01-14 12:41:02 +0000

    Changes to tests to allow reference editing on not sent references

--- spec/requests/candidate_interface/delete_reference_spec.rb
+++ spec/requests/candidate_interface/delete_reference_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe 'Candidate Interface - Redirects acepted offer' do
   context 'when deleting a reference from references review' do
     it 'destroys the reference and redirect to references review' do
       application_form = create(:application_form, recruitment_cycle_year: 2023, candidate:)
-      reference = create(:reference, :feedback_requested, application_form:)
+      reference = create(:reference, :not_requested_yet, application_form:)
 
       delete candidate_interface_destroy_new_reference_path(reference)
       expect(response).to redirect_to(candidate_interface_references_review_path)
@@ -22,7 +22,7 @@ RSpec.describe 'Candidate Interface - Redirects acepted offer' do
   context 'when deleting a nil reference' do
     it 'redirects to review page' do
       application_form = create(:application_form, recruitment_cycle_year: 2023, candidate:)
-      reference = create(:reference, :feedback_requested, application_form:)
+      reference = create(:reference, :not_requested_yet, application_form:)
       reference.destroy
 
       delete candidate_interface_destroy_new_reference_path(reference)
