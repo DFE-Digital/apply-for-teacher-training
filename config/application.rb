9770f21cd71cb0a0e63755c5ef449c06b25bd61a
David Young <david@dcyoung.dev>
2026-01-16 09:37:05 +0000

    Switched from sprockets to propshaft

--- config/application.rb
+++ config/application.rb
@@ -13,8 +13,6 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 # require "action_cable/engine"
 
-require "sprockets/railtie"
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
