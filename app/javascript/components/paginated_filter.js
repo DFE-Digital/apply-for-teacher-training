d128ae834cb3289dd575f5be0f0d9f0490a31ad8
David Young <david@dcyoung.dev>
2026-01-16 11:09:40 +0000

    Removed jquery

--- app/javascript/components/paginated_filter.js
+++ app/javascript/components/paginated_filter.js
@@ -6,17 +6,17 @@ const filter = () => {
     bigModeMediaQuery: '(min-width: 48.063em)',
     startHidden: false,
     toggleButton: {
-      container: $('.filter-toggle-button'),
+      container: document.querySelector('.filter-toggle-button'),
       showText: 'Show filters',
       hideText: 'Hide filters',
       classes: 'govuk-button--secondary'
     },
     closeButton: {
-      container: $('.moj-filter__header-action'),
+      container: document.querySelector('.moj-filter__header-action'),
       text: 'Close'
     },
     filter: {
-      container: $('.moj-filter-layout__filter')
+      container: document.querySelector('.moj-filter-layout__filter')
     }
   })
 }
