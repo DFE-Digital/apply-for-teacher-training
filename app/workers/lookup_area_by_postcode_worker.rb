87db7dc56d442f9027968bcf63b40fa7933d74ce
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2026-01-19 10:30:32 +0000

    Correct postcode formatting when assigning region (#11577)
    ## Context
    
    Badly formatted postcodes can cause problems for API users, we have had
    a couple of support requests related to for instance additional white
    space.
    
    We currently user `Postcodes::IO` to look up and assign regions to
    applications forms when a postcode changes. `Postcodes::IO` can handle
    the badly formatted postcodes and it returns a correctly formatted
    postcode.
    
    We can correct formatting issues by assigning the correctly formatted
    postcode from Postcodes::IO to the application form at the same time we
    assign the region.
    
    ## Changes proposed in this pull request
    
    - Additional specs for the postcode worker
    - Update postcode as well as region with `Postcode::IO` result. 
    
    ## Guidance to review
    
    - On the review app, or locally. change the postcode on an application
    form to something legit, but badly formatted, for example: 'ha1fw', or
    'ha1 2FW'.
    - Wait 15 seconds for the worker to run 
    - Refresh the page. 
    - The postcode should be correctly formatted, for example 'HA1 2FW'
    
    
    ## Things to check
    
    - [ ] If the code removes any existing feature flags, a data migration
    has also been added to delete the entry from the database
    - [ ] API release notes have been updated if necessary
    - [ ] If it adds a significant user-facing change, is it documented in
    the [CHANGELOG](CHANGELOG.md)?
    - [x] Attach the PR to the Trello card
    - [ ] This code adds a column or table to the database
      - [ ] This code does not rely on migrations in the same Pull Request
    - [ ] decide whether it needs to be in analytics yml file or analytics
    blocklist
    - [ ] data insights team has been informed of the change and have
    updated the pipeline
    - [ ] the sanitise.sql script and
    0025-protecting-personal-data-in-production-dump.md ADR have been
    updated
      - [ ] does the code safely backfill existing records for consistency

--- app/workers/lookup_area_by_postcode_worker.rb
+++ app/workers/lookup_area_by_postcode_worker.rb
@@ -3,29 +3,30 @@ class LookupAreaByPostcodeWorker
 
   sidekiq_options queue: :low_priority, retry: 5
 
+  attr_reader :application_form
+
   def perform(application_form_id)
-    application_form = ApplicationForm.find(application_form_id)
+    @application_form = ApplicationForm.find(application_form_id)
     return if application_form&.postcode.blank?
 
-    application_form.update!(
-      region_code: region_code_for(
-        lookup_area(application_form.postcode),
-      ),
-    )
+    application_form.update!(region_code:, postcode:)
   end
 
 private
 
-  def lookup_area(postcode)
-    api = Postcodes::IO.new
-    api.lookup(postcode)
-  end
-
-  def region_code_for(result)
+  def region_code
     if result&.region.present?
       REGION_CODES[result.region.downcase]
     elsif result&.country.present?
       REGION_CODES[result.country.downcase]
     end
+  end
+
+  def postcode
+    result&.postcode || application_form.postcode
+  end
+
+  def result
+    @result ||= Postcodes::IO.new.lookup(application_form.postcode)
   end
 end
--- spec/workers/lookup_area_by_postcode_worker_spec.rb
+++ spec/workers/lookup_area_by_postcode_worker_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe LookupAreaByPostcodeWorker do
   let(:api) { instance_double(Postcodes::IO) }
   let(:result) { double }
-  let(:application_form) { create(:application_form, postcode: 'SW1A 1AA') }
 
   before do
     allow(Postcodes::IO).to receive(:new).and_return(api)
@@ -11,9 +10,11 @@ RSpec.describe LookupAreaByPostcodeWorker do
   end
 
   describe 'region from postcode' do
+    let(:application_form) { create(:application_form, postcode: 'SW1A 1AA') }
+
     context 'when the API returns an English region' do
       before do
-        allow(result).to receive_messages(region: 'South West', country: 'England')
+        allow(result).to receive_messages(postcode: 'SW1A 1AA', region: 'South West', country: 'England')
       end
 
       it 'updates the region code' do
@@ -24,13 +25,49 @@ RSpec.describe LookupAreaByPostcodeWorker do
 
     context 'when the API returns Scotland' do
       before do
-        allow(result).to receive_messages(region: nil, country: 'Scotland')
+        allow(result).to receive_messages(postcode: 'SW1A 1AA', region: nil, country: 'Scotland')
       end
 
       it 'updates the region code' do
         described_class.new.perform(application_form.id)
         expect(application_form.reload.region_code).to eq('scotland')
       end
+    end
+  end
+
+  describe 'when application has badly formatted postcode' do
+    let(:application_form) { create(:application_form, postcode: 'sw1A  1aa') }
+
+    before do
+      allow(result).to receive_messages(postcode: 'SW1A 1AA', region: 'South West', country: 'England')
+    end
+
+    it 'corrects postcode' do
+      described_class.new.perform(application_form.id)
+      expect(application_form.reload.postcode).to eq('SW1A 1AA')
+    end
+  end
+
+  describe 'when no results are returned' do
+    let(:application_form) { create(:application_form, postcode: 'sw1A  1aa') }
+
+    before do
+      allow(result).to receive_messages(postcode: nil, region: nil, country: nil)
+    end
+
+    it 'does not update region or postcode' do
+      described_class.new.perform(application_form.id)
+      application_form.reload
+      expect(application_form.postcode).to eq('sw1A  1aa')
+      expect(application_form.region_code).to be_nil
+    end
+  end
+
+  describe 'when postcode is not present on application form' do
+    let(:application_form) { create(:application_form, postcode: 'sw1A  1aa') }
+
+    it 'does not call postcode IO' do
+      expect(Postcodes::IO).not_to have_received(:new)
     end
   end
 end
