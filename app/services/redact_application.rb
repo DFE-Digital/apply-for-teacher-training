1723c689072545cd3c6a60deb88e126964fb1a5a
Jamie <jamiecleare2525@hotmail.com>
2026-01-30 13:52:38 +0000

    Add RedactApplication service

---
+++ app/services/redact_application.rb
@@ -0,0 +1,34 @@
+class RedactApplication < DeleteApplication
+  def call!
+
+    if !@force && in_flight_applications?
+      raise 'Application has inflight applications. You will need to withdrawn the applications first!'
+    end
+
+    audit(actor) do
+      ActiveRecord::Base.transaction do
+        APPLICATION_FORM_FIELDS_TO_REDACT.each { |attr| application_form.send("#{attr}=", nil) }
+        application_form.save!
+
+        reference = application_form.support_reference
+        application_form.candidate.update!(email_address: "deleted-application-#{reference}@example.com")
+        application_form.candidate.one_login_auth&.delete
+        application_form.candidate.sessions&.destroy_all
+
+        application_form.own_and_associated_audits.destroy_all
+        add_audit_event_for_deletion!
+      end
+    end
+  end
+
+private
+
+  def in_flight_applications?
+    application_form
+      .application_choices
+      .joins(:current_course)
+      .where('current_course.recruitment_cycle_year' => RecruitmentCycleTimetable.current_year)
+      .where(status: ApplicationStateChange::DECISION_PENDING_AND_INACTIVE_STATUSES + ApplicationStateChange::ACCEPTED_STATES)
+      .exists?
+  end
+end
---
+++ spec/services/redact_application_spec.rb
@@ -0,0 +1,117 @@
+require 'rails_helper'
+
+RSpec.describe RedactApplication do
+  let(:support_user) { create(:support_user) }
+  let(:application_form) do
+    create(
+      :completed_application_form,
+      application_choices_count: 1,
+      work_experiences_count: 2,
+      volunteering_experiences_count: 2,
+      references_count: 2,
+      full_work_history: true,
+      application_qualifications: [build(:application_qualification)]
+    )
+  end
+  let(:zendesk_url) { 'https://becomingateacher.zendesk.com/agent/tickets/1234' }
+  let(:force) { false }
+  let(:service) do
+    described_class.new(
+      actor: support_user,
+      application_form:,
+      zendesk_url:,
+      force:,
+      )
+  end
+
+  describe '#call!', :with_audited do
+    context 'when the application form has inflight application choices' do
+      %i[awaiting_provider_decision interviewing inactive pending_conditions conditions_not_met recruited offer_deferred].each do |status|
+        it "raises an error if application has been an application choice with the status #{status}" do
+          create(:application_choice, status: status, application_form:)
+          expect { service.call! }.to raise_error(
+            'Application has inflight applications. You will need to withdrawn the applications first!',
+          )
+        end
+      end
+    end
+
+    context 'when the application form has no inflight application choices' do
+      %i[unsubmitted cancelled offer rejected application_not_sent offer_withdrawn declined withdrawn].each do |status|
+        it "does not raise an error if application has been an application choice with the status #{status}" do
+          create(:application_choice, status: status, application_form:)
+          expect { service.call! }.not_to raise_error
+        end
+      end
+    end
+
+    it 'deletes all personal information from the application form' do
+      service.call!
+
+      application_form.reload
+      DeleteApplication::APPLICATION_FORM_FIELDS_TO_REDACT.each do |attr|
+        expect(application_form.send(attr)).to be_blank
+      end
+    end
+
+    it 'does not remove their application choices' do
+      service.call!
+
+      application_form.reload
+      expect(application_form.application_choices.count).to eq(1)
+    end
+
+    it 'does not removes associations' do
+      service.call!
+
+      application_form.reload
+      DeleteApplication::ASSOCIATIONS_TO_DESTROY.each do |assoc|
+        expect(application_form.send(assoc).count).not_to be_zero
+      end
+    end
+
+    it 'deletes one login auth if present' do
+      one_login_auth = create(:one_login_auth, candidate: application_form.candidate)
+      service.call!
+
+      expect { one_login_auth.reload }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+
+    it 'destroys all sessions associated with user' do
+      session = create(:session, candidate: application_form.candidate)
+
+      service.call!
+
+      expect { session.reload }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+
+    it 'not to delete the candidate_pool_application' do
+      candidate_pool_application = create(:candidate_pool_application, application_form:)
+
+      service.call!
+
+      expect(application_form.candidate_pool_application).to eq(candidate_pool_application)
+    end
+
+    it 'replaces all audits with a single entry documenting the deletion' do
+      service.call!
+
+      application_form.reload
+      expect(application_form.own_and_associated_audits.count).to eq(1)
+
+      audit = application_form.own_and_associated_audits.first
+      expect(audit.user).to eq(support_user)
+      expect(audit.comment).to eq("Data deletion request: #{zendesk_url}")
+    end
+
+    context 'when force option is provided' do
+      let(:force) { true }
+
+      it 'allows delete if application has been submitted to providers' do
+        application_choice = application_form.application_choices.first
+        CandidateInterface::SubmitApplicationChoice.new(application_choice).call
+        expect { service.call! }.not_to raise_error
+      end
+    end
+  end
+end
