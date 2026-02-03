65a02f3d65b9571fcc5b2ef0dcc4ff01cbd4d216
Jamie <jamiecleare2525@hotmail.com>
2026-02-03 10:15:43 +0000

    Linting

--- spec/components/previews/provider_interface/possible_previous_teacher_training_component_preview.rb
+++ spec/components/previews/provider_interface/possible_previous_teacher_training_component_preview.rb
@@ -4,15 +4,15 @@ module ProviderInterface
     def one_possible_previous_teacher_trainings
       render PossiblePreviousTeacherTrainingComponent.new(
         possible_previous_teacher_trainings: FactoryBot.build_list(
-          :possible_previous_teacher_training, 1,
-          ),
-        )
+          :possible_previous_teacher_training, 1
+        ),
+      )
     end
 
     def many_possible_previous_teacher_trainings
       render PossiblePreviousTeacherTrainingComponent.new(
         possible_previous_teacher_trainings: FactoryBot.build_list(
-          :possible_previous_teacher_training, 3,
+          :possible_previous_teacher_training, 3
         ),
       )
     end
