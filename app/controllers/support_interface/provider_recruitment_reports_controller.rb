69496acda1094241815da7f05de571629d1b1868
CatalinVoineag <11318084+CatalinVoineag@users.noreply.github.com>
2026-02-06 14:06:36 +0000

    Form rather than select

--- app/controllers/support_interface/provider_recruitment_reports_controller.rb
+++ app/controllers/support_interface/provider_recruitment_reports_controller.rb
@@ -1,12 +1,13 @@
 module SupportInterface
   class ProviderRecruitmentReportsController < SupportInterfaceController
+    before_action :set_provider
+    before_action :set_region, only: %i[show new]
+
     def show
-      @provider = Provider.find(provider_id)
       @provider_report = latest_report.present? ? Publications::ProviderRecruitmentPerformanceReportPresenter.new(latest_report) : nil
       @provider_data = @provider_report&.statistics
 
-      all_of_england = Publications::RegionalRecruitmentPerformanceReport::ALL_REGIONS
-      @region = params[:region] || all_of_england
+      all_of_england = Shared::RegionalReportForm::ALL_REGIONS
       @report_type = @region == all_of_england ? :NATIONAL : :REGIONAL
       @statistics = @region == all_of_england ? national_report&.statistics : regional_report&.statistics
 
@@ -16,7 +17,29 @@ module SupportInterface
       ).disability_data
     end
 
+    def new
+      @form = Shared::RegionalReportForm.new(region: @region)
+    end
+
+    def create
+      @form = Shared::RegionalReportForm.new(expected_params)
+
+      if @form.save
+        redirect_to support_interface_provider_recruitment_report_path(region: @form.region)
+      else
+        render :new
+      end
+    end
+
   private
+
+    def set_provider
+      @provider ||= Provider.find(provider_id)
+    end
+
+    def set_region
+      @region = params[:region] || Shared::RegionalReportForm::ALL_REGIONS
+    end
 
     def regional_report
       if @provider_report.present?
@@ -46,6 +69,12 @@ module SupportInterface
 
     def provider_id
       params.permit(:provider_id)[:provider_id]
+    end
+
+    def expected_params
+      params.expect(
+        shared_regional_report_form: [:region],
+      )
     end
   end
 end
