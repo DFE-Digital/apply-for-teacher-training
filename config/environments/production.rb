e37a50f856e4c78d32b6bff4d27d29b7eba2eeb6
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2026-02-12 16:11:53 +0000

    Lb/1638 bug rate limiting for vendor api not configured correctly (#11653)
    ## Context
    
    Our pen test revealed that our rate limiting configuration was not
    working.
    
    The identifier used to determine where the repeat requests were coming
    from (`remote_ip`) returned the front door ip address, not the
    originator. Since Front Door uses lots of IP addresses, you could hit a
    vendor api end point about 2000 times before you got an error, instead
    of 120 times.
    
    ## Changes proposed in this pull request
    
    - Removed unnecessary Front door 'fix'. It wasn't doing anything -- you
    can see this on sandbox at the moment. If you go to
    https://sandbox.apply-for-teacher-training.service.gov.uk/sandbox-rate-limiting-tests,
    you can see that there front door already gives us the header with the
    ip addresses in a joined string.
    - Use the first ip address in the forwarded from list as the unique
    identifier for requests.
    
    ## Guidance to review
    
    The fix is on sand box at the moment. 
    - Get yourself an API token
    - Set up a postman (or curl request) to hit an end point in sandbox that
    isn't going to hurt anything, eg,
    https://sandbox.apply-for-teacher-training.service.gov.uk/api/v1.6/reference-data/gcse-subjects
    - Set up to run 121 times in succession. The first 120 times, you should
    get 200OK, the 121st time, you should get 429
    
    | 121 requests, the last one is a 429 |
    | ---- | 
    | <img width="1044" height="603" alt="image"
    src="https://github.com/user-attachments/assets/d822d935-53c4-4f0d-89a2-99be1d74e9dd"
    /> |
    
    
    
    ## Things to check
    
    - [ ] If the code removes any existing feature flags, a data migration
    has also been added to delete the entry from the database
    - [ ] API release notes have been updated if necessary
    - [ ] If it adds a significant user-facing change, is it documented in
    the [CHANGELOG](CHANGELOG.md)?
    - [ ] Attach the PR to the Trello card
    - [ ] This code adds a column or table to the database
      - [ ] This code does not rely on migrations in the same Pull Request
    - [ ] decide whether it needs to be in analytics yml file or analytics
    blocklist
    - [ ] data insights team has been informed of the change and have
    updated the pipeline
    - [ ] the sanitise.sql script and
    0025-protecting-personal-data-in-production-dump.md ADR have been
    updated
      - [ ] does the code safely backfill existing records for consistency

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -136,23 +136,6 @@ Rails.application.configure do
     end
 
     def call(env)
-      # ActionDispatch::RemoteIp::GetIp does not support IP addresses with
-      # ports included in the CLIENT_IP or X_FORWARDED_FOR headers. Azure includes
-      # ports with these IPs, so they're ignored when remote_ip is calculated.
-      #
-      # In practice this means remote_ip always returns REMOTE_ADDR on Azure,
-      # even though it falls within 172.16.0.0/12 and is therefore known to be
-      # a private IP.
-      #
-      # Rack has solved this issue long ago: https://github.com/rack/rack/issues/1227
-      # so use Rack's own parsing to overwrite this header before it
-      # gets to ActionDispatch::RemoteIp
-      req = Rack::Request.new(env)
-
-      if req.forwarded_for.present? && !HostingEnvironment.sandbox_mode?
-        env["HTTP_X_FORWARDED_FOR"] = req.forwarded_for.join(",")
-      end
-
       # preserves access to sidekiq web
       # see https://github.com/sinatra/sinatra/blob/master/rack-protection/lib/rack/protection/ip_spoofing.rb#L17
       if env["HTTP_X_CLIENT_IP"].present?
