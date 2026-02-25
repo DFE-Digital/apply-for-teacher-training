1580cdb6947862fd379caccbca0ec722cb790cee
Dan Daly <dan.daly@education.gov.uk>
2026-02-25 07:43:20 +0000

    Only track once published

--- app/models/previous_teacher_training.rb
+++ app/models/previous_teacher_training.rb
@@ -4,10 +4,8 @@ class PreviousTeacherTraining < ApplicationRecord
   belongs_to :duplicate_previous_teacher_training, class_name: 'PreviousTeacherTraining', optional: true
   has_one :source_previous_teacher_training, class_name: 'PreviousTeacherTraining', foreign_key: 'duplicate_previous_teacher_training_id'
 
-  audited
-
+  audited associated_with: :application_form, if: :audit_enabled?
   has_associated_audits
-  audited associated_with: :application_form
 
   enum :started, {
     yes: 'yes',
@@ -20,6 +18,10 @@ class PreviousTeacherTraining < ApplicationRecord
   }, default: 'draft'
 
   scope :order_by_dates, -> { order(:started_at, :ended_at) }
+
+  def audit_enabled?
+    status == 'published'
+  end
 
   def create_draft_dup!
     duplicate_previous_teacher_training&.destroy! if duplicate_previous_teacher_training&.draft?
