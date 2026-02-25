da5781e3b720647d586b17ab79a0f3018244b3ba
Dan Daly <dan.daly@education.gov.uk>
2026-02-25 14:53:22 +0000

    Fix flakey spec

--- spec/system/support_interface/editing_address_details_spec.rb
+++ spec/system/support_interface/editing_address_details_spec.rb
@@ -3,6 +3,12 @@ require 'rails_helper'
 RSpec.describe 'Editing address' do
   include DfESignInHelpers
 
+  before do
+    allow(Pagy).to receive(:new).and_return(
+      Pagy.new(count: 1, page: 1, items: 100),
+    )
+  end
+
   scenario 'Support user edits address', :with_audited do
     given_i_am_a_support_user
     and_an_application_exists
@@ -81,7 +87,7 @@ RSpec.describe 'Editing address' do
     find(:css, "[autocomplete='address-line1']").fill_in with: '42 Much Wow Street'
     fill_in t('application_form.contact_details.address_line3.label.uk'), with: 'London'
     fill_in t('application_form.contact_details.postcode.label.uk'), with: 'SW1P 3BT'
-    fill_in 'support_interface_application_forms_edit_address_details_form[audit_comment]', with: 'Updated as part of Zen Desk ticket #12345'
+    fill_in 'support_interface_application_forms_edit_address_details_form[audit_comment]', with: 'Updated as part of Zendesk ticket #12345'
   end
 
   def then_i_see_a_flash_message
@@ -96,7 +102,7 @@ RSpec.describe 'Editing address' do
 
   def and_i_see_my_details_comment_in_the_audit_log
     click_link_or_button 'History'
-    expect(page).to have_content 'Updated as part of Zen Desk ticket #12345'
+    expect(page).to have_content 'Updated as part of Zendesk ticket #12345'
   end
 
   def and_i_select_outside_the_uk
@@ -118,7 +124,7 @@ RSpec.describe 'Editing address' do
     fill_in 'support_interface_application_forms_edit_address_details_form[address_line1]', with: '123 Chandni Chowk'
     fill_in 'support_interface_application_forms_edit_address_details_form[address_line3]', with: 'New Delhi'
     fill_in 'support_interface_application_forms_edit_address_details_form[address_line4]', with: '110006'
-    fill_in 'support_interface_application_forms_edit_address_details_form[audit_comment]', with: 'Updated as part of Zen Desk ticket #56789'
+    fill_in 'support_interface_application_forms_edit_address_details_form[audit_comment]', with: 'Updated as part of Zendesk ticket #56789'
   end
 
   def and_i_see_the_new_international_address_details
@@ -130,7 +136,6 @@ RSpec.describe 'Editing address' do
 
   def and_i_see_my_international_address_details_comment_in_the_audit_log
     click_link_or_button 'History'
-    click_link_or_button 'Next'
-    expect(page).to have_content 'Updated as part of Zen Desk ticket #56789'
+    expect(page).to have_content 'Updated as part of Zendesk ticket #56789'
   end
 end
