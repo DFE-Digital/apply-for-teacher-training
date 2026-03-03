f154ed8ffc4f1f783e67b86b9eb4d362c56e2900
RMcVelia <94844345+RMcVelia@users.noreply.github.com>
2026-03-03 10:48:50 +0000

    Update azurerm and terraform versions (#11692)
    ## Context
    
    Terraform updates
    
    ## Changes proposed in this pull request
    
    - azurerm 4.61.0
    - terraform 1.14.5
    - update TERRAFORM_MODULES_TAG to use testing-azurerm-v4 and
    stable-azurerm-v4
    
    ## Guidance to review
    
    make qa deploy-plan
    make staging deploy-plan
    make sandbox deploy-plan
    make production deploy-plan
    make domains-infra-plan
    make qa domains-plan
    make staging domains-plan
    make sandbox domains-plan
    make production domains-plan
    
    ## Things to check
    
    - [ ] If the code removes any existing feature flags, a data migration
    has also been added to delete the entry from the database
    - [ ] API release notes have been updated if necessary
    - [ ] If it adds a significant user-facing change, is it documented in
    the [CHANGELOG](CHANGELOG.md)?
    - [ ] Attach the PR to the Trello card
    - [ ] This code adds a column or table to the database
      - [ ] This code does not rely on migrations in the same Pull Request
    - [ ] decide whether it needs to be in analytics yml file or analytics
    blocklist
    - [ ] data insights team has been informed of the change and have
    updated the pipeline
    - [ ] the sanitise.sql script and
    0025-protecting-personal-data-in-production-dump.md ADR have been
    updated
      - [ ] does the code safely backfill existing records for consistency

--- terraform/custom_domains/environment_domains/terraform.tf
+++ terraform/custom_domains/environment_domains/terraform.tf
@@ -1,10 +1,10 @@
 terraform {
 
-  required_version = "= 1.7.4"
+  required_version = "= 1.14.5"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.116.0"
+      version = "4.61.0"
     }
   }
   backend "azurerm" {
@@ -14,5 +14,5 @@ terraform {
 provider "azurerm" {
   features {}
 
-  skip_provider_registration = true
+  resource_provider_registrations = "none"
 }
