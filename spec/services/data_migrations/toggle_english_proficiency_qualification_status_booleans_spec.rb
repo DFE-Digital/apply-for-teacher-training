b88d27c31a8dc0334a6c5545f3b24ff6f2699804
Jamie <jamiecleare2525@hotmail.com>
2026-03-18 11:32:49 +0000

    Rubocop

--- spec/services/data_migrations/toggle_english_proficiency_qualification_status_booleans_spec.rb
+++ spec/services/data_migrations/toggle_english_proficiency_qualification_status_booleans_spec.rb
@@ -45,26 +45,26 @@ RSpec.describe DataMigrations::ToggleEnglishProficiencyQualificationStatusBoolea
 
     has_qualifications.each do |has_qualification_proficiency|
       has_qualification_proficiency.reload
-      expect(has_qualification_proficiency.has_qualification).to eq(true)
+      expect(has_qualification_proficiency.has_qualification).to be(true)
 
-      expect(has_qualification_proficiency.no_qualification).to eq(false)
-      expect(has_qualification_proficiency.qualification_not_needed).to eq(false)
+      expect(has_qualification_proficiency.no_qualification).to be(false)
+      expect(has_qualification_proficiency.qualification_not_needed).to be(false)
     end
 
     no_qualifications.each do |no_qualification_proficiency|
       no_qualification_proficiency.reload
-      expect(no_qualification_proficiency.no_qualification).to eq(true)
+      expect(no_qualification_proficiency.no_qualification).to be(true)
 
-      expect(no_qualification_proficiency.has_qualification).to eq(false)
-      expect(no_qualification_proficiency.qualification_not_needed).to eq(false)
+      expect(no_qualification_proficiency.has_qualification).to be(false)
+      expect(no_qualification_proficiency.qualification_not_needed).to be(false)
     end
 
     qualifications_not_needed.each do |qualifications_not_needed_proficiency|
       qualifications_not_needed_proficiency.reload
-      expect(qualifications_not_needed_proficiency.qualification_not_needed).to eq(true)
+      expect(qualifications_not_needed_proficiency.qualification_not_needed).to be(true)
 
-      expect(qualifications_not_needed_proficiency.has_qualification).to eq(false)
-      expect(qualifications_not_needed_proficiency.no_qualification).to eq(false)
+      expect(qualifications_not_needed_proficiency.has_qualification).to be(false)
+      expect(qualifications_not_needed_proficiency.no_qualification).to be(false)
     end
   end
 end
