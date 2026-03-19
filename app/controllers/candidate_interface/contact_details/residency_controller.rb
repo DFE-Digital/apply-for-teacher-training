2890e0004595f4793d4424d816e266b1ad7a2677
Dan Daly <dan.daly@education.gov.uk>
2026-03-19 13:41:06 +0000

    Complete residency step

--- app/controllers/candidate_interface/contact_details/residency_controller.rb
+++ app/controllers/candidate_interface/contact_details/residency_controller.rb
@@ -7,21 +7,48 @@ module CandidateInterface
 
     def edit
       @country_of_residence = country_of_residence
-      @residency_form = CandidateInterface::ResidencyForm.new # change to build from ...
+      @residency_form = CandidateInterface::ResidencyForm.build_from_application(current_application)
       # @return_to = return_to_after_edit(default: candidate_interface_contact_information_review_path)
     end
 
-    def create; end
+    def create
+      @residency_form = CandidateInterface::ResidencyForm.new(residency_params)
+
+      if @residency_form.valid?
+        @residency_form.save(current_application)
+        path = @residency_form.since_birth? ? candidate_interface_contact_information_review_path : candidate_interface_new_residency_dates_path
+        redirect_to path
+      else
+        track_validation_error(@residency_form)
+        render(:new, status: :unprocessable_entity)
+      end
+    end
 
     def update
       # @return_to = return_to_after_edit(default: candidate_interface_contact_information_review_path)
       # redirect_to @return_to[:back_path]
+      @residency_form = CandidateInterface::ResidencyForm.new(residency_params)
+
+      if @residency_form.valid?
+        @residency_form.save(current_application)
+        path = @residency_form.since_birth? ? candidate_interface_contact_information_review_path : candidate_interface_new_residency_dates_path
+        redirect_to path
+      else
+        track_validation_error(@residency_form)
+        render(:edit, status: :unprocessable_entity)
+      end
     end
 
   private
 
     def country_of_residence
       COUNTRIES_AND_TERRITORIES[current_application.country]
+    end
+
+    def residency_params
+      params
+      .fetch(:candidate_interface_residency_form, {})
+      .permit(:since_birth)
     end
   end
 end
--- app/forms/candidate_interface/residency_form.rb
+++ app/forms/candidate_interface/residency_form.rb
@@ -1,6 +1,26 @@
 module CandidateInterface
   class ResidencyForm
     include ActiveModel::Model
-    # TODO
+    include ActiveModel::Attributes
+
+    attribute :since_birth
+
+    validates :since_birth, presence: true
+
+    def save(application)
+      return false unless valid?
+
+      application.update(country_residency_since_birth: since_birth?)
+    end
+
+    def since_birth?
+      since_birth == 'yes'
+    end
+
+    def self.build_from_application(application_form)
+      new(
+        since_birth: application_form.country_residency_since_birth ? 'yes' : 'no',
+      )
+    end
   end
 end
