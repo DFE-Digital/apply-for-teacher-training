ac46f14f88f810a21eceea0bdd2a3e0f92a10098
Jamie <jamiecleare2525@hotmail.com>
2026-03-26 10:23:22 +0000

    Try raise

--- spec/support/test_helpers/efl_helper.rb
+++ spec/support/test_helpers/efl_helper.rb
@@ -1,20 +1,14 @@
 module EFLHelper
   def when_i_declare_a_non_english_nationality
-    puts "EFL @current_candidate #{@current_candidate&.id}"
-    puts "EFL current_candidate #{current_candidate&.id}"
-
-    @application_form ||= if current_candidate&.application_forms.present?
-                            current_candidate.current_application.update(
-                              first_nationality: 'Hong Konger',
-                              second_nationality: 'Pakistani',
-                            )
-                          else
-                            create(:application_form,
-                                   first_nationality: 'Hong Konger',
-                                   second_nationality: 'Pakistani',
-                                   candidate: current_candidate)
-                          end
+    @application_form ||= current_candidate.current_application.update(
+                            first_nationality: 'Hong Konger',
+                            second_nationality: 'Pakistani',
+                          )
     visit candidate_interface_details_path
+  rescue StandardError => e
+    puts "current_candidate: #{current_candidate.id}"
+    puts "@current_candidate: #{@current_candidate.id}"
+    raise e
   end
 
   def when_i_click_on_the_efl_section_link
