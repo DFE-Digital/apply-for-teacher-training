d2c493d3f65de099a3fee6f05f5023b3d7c7aa31
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2026-03-31 15:21:39 +0100

    Only add index if it does not already exist

--- db/migrate/20260330090636_add_indexes_to_vendor_api_requests.rb
+++ db/migrate/20260330090636_add_indexes_to_vendor_api_requests.rb
@@ -2,8 +2,8 @@ class AddIndexesToVendorAPIRequests < ActiveRecord::Migration[8.0]
   disable_ddl_transaction!
 
   def change
-    add_index :vendor_api_requests, :provider_id, algorithm: :concurrently
-    add_index :vendor_api_requests, :status_code, algorithm: :concurrently
-    add_index :vendor_api_requests, :request_path, algorithm: :concurrently
+    add_index :vendor_api_requests, :provider_id, algorithm: :concurrently unless index_exists?(:vendor_api_requests, :provider_id)
+    add_index :vendor_api_requests, :status_code, algorithm: :concurrently unless index_exists?(:vendor_api_requests, :status_code)
+    add_index :vendor_api_requests, :request_path, algorithm: :concurrently unless index_exists?(:vendor_api_requests, :request_path)
   end
 end
