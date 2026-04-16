d3dd39579ae8d64c7d75539e2bfd8e247290e733
Jamie <jamiecleare2525@hotmail.com>
2026-04-16 14:46:45 +0100

    Prevent NonDisclosureTraineeWithdrawalWorker on unless on production QA or test

--- app/workers/non_disclosure_trainee_withdrawal_worker.rb
+++ app/workers/non_disclosure_trainee_withdrawal_worker.rb
@@ -4,6 +4,8 @@ class NonDisclosureTraineeWithdrawalWorker
   sidekiq_options retry: 3, queue: :default
 
   def perform(candidate_id)
+    return unless HostingEnvironment.production? || HostingEnvironment.qa? || HostingEnvironment.test_environment?
+
     candidate = Candidate.find(candidate_id)
     GeneratePossiblePreviousTeacherTraining.new(candidate).call
   end
