288190e39c6be167b50a27c11a86c4608a713d97
Dan Daly <dan.daly@education.gov.uk>
2026-04-30 07:33:04 +0100

    Test edited job

--- app/jobs/solid_queue_test_job.rb
+++ app/jobs/solid_queue_test_job.rb
@@ -1,9 +1,12 @@
 class SolidQueueTestJob < ApplicationJob
   self.queue_adapter = :solid_queue
 
+  # def perform
+  # Rails.root.join('test_success.txt').open('a') do |file|
+  #   file.puts "✅ Solid Queue job ran successfully at #{Time.current}"
+  # end
+
   def perform
-    Rails.root.join('test_success.txt').open('a') do |file|
-      file.puts "✅ Solid Queue job ran successfully at #{Time.current}"
-    end
+    ApplicationForm.last&.update(updated_at: Time.zone.now)
   end
 end
