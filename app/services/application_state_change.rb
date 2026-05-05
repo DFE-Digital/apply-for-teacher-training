6acc7adbbcdf36fb0aa3b60bd113a9678422fe99
Jamie <jamiecleare2525@hotmail.com>
2026-05-05 13:22:13 +0100

    Move statuses to ApplicationStateChange

--- app/services/application_state_change.rb
+++ app/services/application_state_change.rb
@@ -13,6 +13,7 @@ class ApplicationStateChange
   SUCCESSFUL_STATES = %i[pending_conditions offer offer_deferred recruited].freeze
   DECISION_PENDING_STATUSES = %i[awaiting_provider_decision interviewing].freeze
   DECISION_PENDING_AND_INACTIVE_STATUSES = %i[awaiting_provider_decision interviewing inactive].freeze
+  ACTIVE_PREVIOUS_STATUSES = (ApplicationStateChange::IN_PROGRESS_STATES + %i[inactive] - %i[offer_deferred]).freeze
 
   REAPPLY_STATUSES = %i[rejected cancelled withdrawn declined offer_withdrawn].freeze
 
