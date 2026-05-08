244608a4989c95c0d0a5fd72709d8c990608ed66
Dan Daly <dan.daly@education.gov.uk>
2026-05-08 12:10:04 +0100

    Expand spec

--- spec/forms/provider_interface/application_data_export_form_spec.rb
+++ spec/forms/provider_interface/application_data_export_form_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe ProviderInterface::ApplicationDataExportForm do
   context 'when there are application choices' do
     let(:provider) { create(:provider) }
-    let(:provider_user) { create(:provider_user, providers: [provider]) }
+    let(:provider_2) { create(:provider) }
+    let(:provider_3) { create(:provider) }
+    let(:provider_user) { create(:provider_user, providers: [provider, provider_2]) }
 
     let(:application_form) { create(:completed_application_form, :with_degree) }
     let!(:application_choice) do
@@ -12,6 +14,24 @@ RSpec.describe ProviderInterface::ApplicationDataExportForm do
         :offered,
         application_form: application_form,
         provider_ids: [provider.id],
+      )
+    end
+    let!(:application_choice_2) do
+      create(
+        :application_choice,
+        :withdrawn,
+        current_recruitment_cycle_year: 2025,
+        application_form: application_form,
+        provider_ids: [provider_2.id],
+      )
+    end
+    let!(:application_choice_3) do
+      create(
+        :application_choice,
+        :declined,
+        current_recruitment_cycle_year: 2024,
+        application_form: application_form,
+        provider_ids: [provider_3.id],
       )
     end
 
@@ -28,7 +48,8 @@ RSpec.describe ProviderInterface::ApplicationDataExportForm do
 
       years = form.years_to_export
 
-      expect(years.keys.map(&:to_i)).to include(application_choice.current_recruitment_cycle_year)
+      expect(years.keys.map(&:to_i)).to eq([application_choice.current_recruitment_cycle_year, application_choice_2.current_recruitment_cycle_year])
+      expect(years.keys.map(&:to_i)).not_to include(application_choice_3.current_recruitment_cycle_year)
     end
   end
 end
