6c6ff3702b86caaa7efb355afa40602ab1e94778
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2026-05-26 10:48:57 +0100

    Release v1.8 to production

--- app/lib/vendor_api.rb
+++ app/lib/vendor_api.rb
@@ -9,9 +9,9 @@ module VendorAPI
   VERSION_1_5 = '1.5'.freeze
   VERSION_1_6 = '1.6'.freeze
   VERSION_1_7 = '1.7'.freeze
-  VERSION_1_8 = '1.8pre'.freeze # the pre suffix should make v1.8 available to dev envs and sandbox only
+  VERSION_1_8 = '1.8'.freeze # the pre suffix should make v1.8 available to dev envs and sandbox only
 
-  VERSION = '1.8'.freeze
+  VERSION = VERSION_1_8
 
   VERSIONS = {
     '1.0' => [
@@ -76,7 +76,7 @@ module VendorAPI
     '1.7' => [
       Changes::V17::AddPossibleUndeclaredPreviousTeacherTrainingUrl,
     ],
-    '1.8pre' => [
+    '1.8' => [
       Changes::V18::EnglishAsAForeignLanguageCandidateResponse,
       Changes::V18::VisaExpiry,
       Changes::V18::LengthOfResidency,
