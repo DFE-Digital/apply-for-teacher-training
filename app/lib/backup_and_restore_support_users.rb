51cf4b86259c5f5ed3b8730a5767bbf2314383d6
CatalinVoineag <11318084+CatalinVoineag@users.noreply.github.com>
2026-05-26 15:51:36 +0100

    comments

--- app/lib/backup_and_restore_support_users.rb
+++ app/lib/backup_and_restore_support_users.rb
@@ -1,18 +1,18 @@
 class BackupAndRestoreSupportUsers
-  REDIS_KEY = 'support_users'.freeze
+  CACHE_KEY = 'support_users'.freeze
 
   def self.backup!
     existing_users = SupportUser.pluck(:email_address, :dfe_sign_in_uid)
 
     if existing_users.any?
-      Rails.cache.write(REDIS_KEY, JSON.generate(existing_users))
+      Rails.cache.write(CACHE_KEY, JSON.generate(existing_users))
     end
 
     existing_users.count
   end
 
   def self.restore!
-    restored_users = JSON.parse(Rails.cache.read(REDIS_KEY))
+    restored_users = JSON.parse(Rails.cache.read(CACHE_KEY))
 
     restored_users.each do |(email, uid)|
       SupportUser.find_or_create_by(email_address: email, dfe_sign_in_uid: uid)
