623c439adcb524d48534d522f88b0bc0bb4d256d
Dan Daly <166535825+dp-daly@users.noreply.github.com>
2026-05-27 15:11:06 +0100

    queue.yml deploy (#11944)
    ## Context
    
    Previous PRs have installed and configured SolidQueue and migrated five
    jobs to its workers. This PR introduces a new queue design to expand the
    queue.yml configuration.
    
    ## Changes proposed in this pull request
    
    - Add queues: mailers, big_query_sq, and low_priority ('default' was
    always there but is now explicitly named and configured).
    - Remove dispatcher: we shouldn't need it to poll for jobs to schedule
    since we are scheduling using Clockwork. We would need to add this back
    in if we decide to use the `set` or `perform_in` methods.
    
    ## Guidance to review
    
    <!-- How could someone else check this work? Which parts do you want
    more feedback on? -->
    
    
    ## Things to check
    
    - [ ] If the code removes any existing feature flags, a data migration
    has also been added to delete the entry from the database
    - [ ] API release notes have been updated if necessary
    - [ ] If it adds a significant user-facing change, is it documented in
    the [CHANGELOG](CHANGELOG.md)?
    - [ ] Attach the PR to the Trello card
    - [ ] This code adds a column or table to the database
      - [ ] This code does not rely on migrations in the same Pull Request
    - [ ] decide whether it needs to be in analytics yml file or analytics
    blocklist
    - [ ] data insights team has been informed of the change and have
    updated the pipeline
    - [ ] the sanitise.sql script and
    0025-protecting-personal-data-in-production-dump.md ADR have been
    updated
      - [ ] does the code safely backfill existing records for consistency

--- app/workers/delete_finished_jobs_worker.rb
+++ app/workers/delete_finished_jobs_worker.rb
@@ -1,6 +1,8 @@
 class DeleteFinishedJobsWorker < ApplicationJob
   self.queue_adapter = :solid_queue
 
+  queue_as :low_priority
+
   def perform
     SolidQueue::Job.clear_finished_in_batches(sleep_between_batches: 0.3)
   end
