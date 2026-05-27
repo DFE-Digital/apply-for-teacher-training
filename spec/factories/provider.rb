5eb075716a0d10fa1fcd13c6c33052a6c8e5371a
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2026-05-27 12:05:58 +0100

    wip

--- spec/factories/provider.rb
+++ spec/factories/provider.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :provider do
-    sequence(:code) { |n| (SecureRandom.alphanumeric(2).upcase + n.to_s).chars.first(3).join }
+    sequence(:code, "AAA")
     sequence(:name) { |n| "#{Faker::University.name}-#{n}" }
     phone_number { Faker::PhoneNumber.phone_number }
     email_address { Faker::Internet.email }
