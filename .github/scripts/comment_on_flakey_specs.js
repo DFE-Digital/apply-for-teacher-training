3a073ccd9756581d567cf550f088d8b0de1890f4
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2026-05-27 08:25:06 +0100

    Fix error occurring when there are multiple flakey tests

--- .github/scripts/comment_on_flakey_specs.js
+++ .github/scripts/comment_on_flakey_specs.js
@@ -7,18 +7,38 @@ module.exports = ({github, context}) => {
   }
 
   const { FLAKEY_TEST_DATA, GITHUB_HEAD_REF } = process.env;
-  if (FLAKEY_TEST_DATA.length) {
+  if (FLAKEY_TEST_DATA && FLAKEY_TEST_DATA.length) {
+    const matches = FLAKEY_TEST_DATA.match(/\[.*?\]/gs);
+    if (!matches) {
+      return;
+    }
+
+    const flakeyErrors = [];
+
+    matches.forEach((chunk) => {
+      const parsed = JSON.parse(chunk);
+      if (Array.isArray(parsed)) {
+        flakeyErrors.push(...parsed);
+      }
+    });
+
+    if (!flakeyErrors.length) {
+      return;
+    }
+
     const { issue: { number: issue_number }, repo: { owner, repo } } = context;
     const heading = 'You have one or more flakey tests on this branch!';
     let commentBody = `<h2>${heading} :snowflake: :snowflake: :snowflake:</h2>`;
     let branchName = GITHUB_HEAD_REF.split('/').pop();
     let createComment = false;
-    JSON.parse(FLAKEY_TEST_DATA).forEach(function(error) {
+
+    flakeyErrors.forEach(function(error) {
       let errorPath = `/${owner}/${repo}/blob/${branchName}/${error['location'].replace(':', '#L')}`;
       let errorLink = `<a href="${errorPath}">${error['location']}</a>`;
       commentBody += `Failed ${error['attempts']} out of ${error['retry_count']} times at ${errorLink}: :warning: ${error['messages'].toString()}<br>`;
       createComment = true;
     })
+
     if (createComment) {
       github.rest.issues.createComment({ issue_number, owner, repo, body: commentBody });
       throw new FlakeySpecError(heading);
