960d6fad9e0e0a7b93529c28a4b262eb5ad1dc32
Dan Daly <dan.daly@education.gov.uk>
2026-05-28 16:35:46 +0100

    Add staging

--- config/application.rb
+++ config/application.rb
@@ -56,7 +56,7 @@ module ApplyForPostgraduateTeacherTraining
 
     config.before_configuration do
       ENV["SOLID_QUEUE_CONFIG"] =
-        if %w[qa sandbox review].include?(ENV["HOSTING_ENVIRONMENT_NAME"])
+        if %w[qa sandbox staging review].include?(ENV["HOSTING_ENVIRONMENT_NAME"])
           "config/non_production_queue.yml"
         else
           "config/queue.yml"
