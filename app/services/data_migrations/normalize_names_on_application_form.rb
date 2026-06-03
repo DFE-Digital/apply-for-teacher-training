690f71bba2e2c6ff62b605352155d928b16792b2
Dan Daly <dan.daly@education.gov.uk>
2026-06-03 09:57:54 +0100

    Bulk change rather than iterating by record

--- app/services/data_migrations/normalize_names_on_application_form.rb
+++ app/services/data_migrations/normalize_names_on_application_form.rb
@@ -4,29 +4,14 @@ module DataMigrations
     MANUAL_RUN = false
 
     def change
-      forms_with_unnormalized_names do |application_form|
-        columns.each do |column|
-          next if application_form[column].nil?
-
-          application_form[column] = application_form[column].strip
-        end
-
-        application_form.save!
-      end
-    end
-
-  private
-
-    def forms_with_unnormalized_names
-      current_forms.where('first_name <> TRIM(first_name) OR last_name <> TRIM(last_name)')
-    end
-
-    def current_forms
-      @current_forms ||= ApplicationForm.where(recruitment_cycle_year: [2025, 2026])
-    end
-
-    def columns
-      %i[first_name last_name]
+      ActiveRecord::Base.connection.execute(<<~SQL)
+        UPDATE application_forms
+        SET first_name = TRIM(first_name),
+            last_name  = TRIM(last_name)
+        WHERE (first_name <> TRIM(first_name)
+            OR last_name  <> TRIM(last_name))
+          AND recruitment_cycle_year IN (2025, 2026);
+      SQL
     end
   end
 end
