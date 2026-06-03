e085cba150fbe0068757cfbbec0a26e6d4c40513
Dan Daly <166535825+dp-daly@users.noreply.github.com>
2026-06-03 16:29:51 +0100

    Add grace period for inactive user to accept new invitation (#11990)
    ## Context
    
    This originated with a support ticket. A user with organisational
    permissions invited an inactive user but they could not accept the
    invitation in time.
    
    ## Changes proposed in this pull request
    
    - Do not remove the provider user if any of their permissions were
    created in the last 7 days
    
    ## Guidance to review
    
    - Review small code change and spec
    
    ## Things to check
    
    - [ ] If the code removes any existing feature flags, a data migration
    has also been added to delete the entry from the database
    - [ ] API release notes have been updated if necessary
    - [ ] If it adds a significant user-facing change, is it documented in
    the [CHANGELOG](CHANGELOG.md)?
    - [ ] Attach the PR to the Trello card
    - [ ] This code adds a column or table to the database
      - [ ] This code does not rely on migrations in the same Pull Request
    - [ ] decide whether it needs to be in analytics yml file or analytics
    blocklist
    - [ ] data insights team has been informed of the change and have
    updated the pipeline
    - [ ] the sanitise.sql script and
    0025-protecting-personal-data-in-production-dump.md ADR have been
    updated
      - [ ] does the code safely backfill existing records for consistency

--- app/workers/remove_inactive_provider_users_worker.rb
+++ app/workers/remove_inactive_provider_users_worker.rb
@@ -12,6 +12,9 @@ class RemoveInactiveProviderUsersWorker
       .or(ProviderUser.where(
             'last_signed_in_at IS NULL AND created_at < ?', INACTIVE_MONTHS_AGO.months.ago
           )).each do |provider_user|
+      # Skip if they were re-added recently (permissions created within the last week)
+      next if provider_user.provider_permissions.exists?(['created_at > ?', 1.week.ago])
+
       provider_user.provider_permissions.each do |permissions_to_remove|
         SupportInterface::RemoveUserFromProvider.new(permissions_to_remove:).call!
       end
