92110bd00fae634cf9483a16f2f4e30ae0158761
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2026-06-09 14:44:47 +0100

    Lb/remove unnecessary js from flaky test (#12014)
    ## Context
    
    This fixes two flaky specs. Obviously, there is more to do, but it's a
    start.
    
    ## Changes proposed in this pull request
    
    - Duplicate course entry -- we didn't need JS to test this one. 
    - WinterRejectByDefault worker -- the courses are created with random
    start dates (in the factory), so unless we specify the date, we'll
    sometimes get one that meets the jan criteria.
    
    ## Guidance to review
    
    <!-- How could someone else check this work? Which parts do you want
    more feedback on? -->
    
    
    ## Things to check
    
    - [ ] If the code removes any existing feature flags, a data migration
    has also been added to delete the entry from the database
    - [ ] API release notes have been updated if necessary
    - [ ] If it adds a significant user-facing change, is it documented in
    the [CHANGELOG](CHANGELOG.md)?
    - [ ] Attach the PR to the Trello card
    - [ ] This code adds a column or table to the database
      - [ ] This code does not rely on migrations in the same Pull Request
    - [ ] decide whether it needs to be in analytics yml file or analytics
    blocklist
    - [ ] data insights team has been informed of the change and have
    updated the pipeline
    - [ ] the sanitise.sql script and
    0025-protecting-personal-data-in-production-dump.md ADR have been
    updated
      - [ ] does the code safely backfill existing records for consistency

--- spec/system/candidate_interface/course_selection/candidate_selecting_a_duplicate_course_site_spec.rb
+++ spec/system/candidate_interface/course_selection/candidate_selecting_a_duplicate_course_site_spec.rb
@@ -1,36 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe 'Selecting a course', :js do
+RSpec.describe 'Selecting a course' do
   include CandidateHelper
 
   # Regression test for DuplicateCourseSelection to avoid ActiveRecord::RecordInvalid
   it 'Candidate selects a course they have already applied to' do
     given_i_am_signed_in_with_one_login
     and_there_are_course_options
-
-    window_1 = windows.first
-    window_2 = open_new_window
-
-    # Window 1
-    visit_course_selection_page
-    and_i_choose_a_location
-
-    # Window 2
-    Capybara.current_session.switch_to_window window_2
+    and_i_have_already_applied_to_the_course
     visit_course_selection_page
 
-    and_i_choose_a_location
-    and_i_click_continue
-    then_i_am_on_the_application_choice_review_page
-
-    # Window 1
-    Capybara.current_session.switch_to_window window_1
-    and_i_click_continue
     then_i_am_on_the_application_choice_duplicate_page
   end
 
   def visit_course_selection_page
     visit "/candidate/application/course-choices/provider/#{@provider.id}/courses/#{@course.id}/full_time"
+  end
+
+  def and_i_have_already_applied_to_the_course
+    create(
+      :application_choice,
+      application_form: current_candidate.current_application,
+      course: @course,
+    )
   end
 
   def then_i_am_on_the_application_choice_duplicate_page
@@ -42,13 +34,5 @@ RSpec.describe 'Selecting a course', :js do
     @course = create(:course, :open, name: 'Primary', code: '2XT2', provider: @provider)
     site = create(:site, name: 'Main site', provider: @provider)
     create(:course_option, course: @course, site:)
-  end
-
-  def and_i_choose_a_location
-    choose 'Main site'
-  end
-
-  def and_i_click_continue
-    click_link_or_button t('continue')
   end
 end
