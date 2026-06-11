2005857b99432ecee494dcf3b9f2b01fcd23e1f5
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2026-06-11 18:37:19 +0100

    rubocop]

--- spec/lib/international_qualifications/structured_gcse_option_finder_spec.rb
+++ spec/lib/international_qualifications/structured_gcse_option_finder_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe InternationalQualifications::StructuredGcseOptionFinder do
   describe '.equivalent_qualifications' do
     context 'where country has an equivalent qualification' do
       %w[NG GH SL GM LR].each do |country_code|
-        it "returns equivalent qualifications for #{country_code}" do
+        it "returns WASSCE for #{country_code}" do
           equivalent_qualifications = described_class.new(country_code).equivalent_qualifications
           expect(equivalent_qualifications.count).to eq 1
           equivalent_qualification = equivalent_qualifications.first
@@ -12,7 +12,7 @@ RSpec.describe InternationalQualifications::StructuredGcseOptionFinder do
           expect(equivalent_qualification.countries).to eq(%w[NG GH SL GM LR])
         end
 
-        it 'attaches the relevant grade schemes' do
+        it "attaches the relevant WASSCE grade schemes for #{country_code}" do
           equivalent_qualification = described_class.new(country_code).equivalent_qualifications.first
 
           expect(equivalent_qualification.grade_schemas.count).to eq 1
@@ -23,7 +23,7 @@ RSpec.describe InternationalQualifications::StructuredGcseOptionFinder do
       end
 
       %w[KE].each do |country_code|
-        it "returns equivalent qualifications for #{country_code}" do
+        it "returns KCSE for #{country_code}" do
           equivalent_qualifications = described_class.new(country_code).equivalent_qualifications
           expect(equivalent_qualifications.count).to eq 1
           equivalent_qualification = equivalent_qualifications.first
@@ -31,7 +31,7 @@ RSpec.describe InternationalQualifications::StructuredGcseOptionFinder do
           expect(equivalent_qualification.countries).to eq(%w[KE])
         end
 
-        it 'attaches the relevant grade schemes' do
+        it "attaches the relevant KCSE grade schemes for #{country_code}" do
           equivalent_qualification = described_class.new(country_code).equivalent_qualifications.first
 
           expect(equivalent_qualification.grade_schemas.count).to eq 1
