bab591ed5deabfeeaa285a2b5e442aa5261e8a64
Dan Daly <dan.daly@education.gov.uk>
2026-06-18 18:05:41 +0100

    Preload other attribute on edit

--- app/forms/candidate_interface/gcse_equivalent_qualification_form.rb
+++ app/forms/candidate_interface/gcse_equivalent_qualification_form.rb
@@ -2,16 +2,23 @@ module CandidateInterface
   class GcseEquivalentQualificationForm
     include ActiveModel::Model
 
-    attr_accessor :qualification, :non_structured_qualification
+    attr_accessor :qualification, :non_structured_qualification, :equivalent_qualifications
 
     validates :qualification, presence: true
     validates :non_structured_qualification, presence: true, if: :non_structured?
     validates :non_structured_qualification, length: { maximum: ApplicationQualification::MAX_QUALIFICATION_TYPE_LENGTH }
 
-    def self.build_from_qualification(application_qualification)
+    def self.build_from_qualification(application_qualification, equivalent_qualifications: [])
+      qualification = application_qualification.non_uk_qualification_type
+      structured = qualification.present? && qualification.in?(equivalent_qualifications)
+
       new(
-        qualification: application_qualification.non_uk_qualification_type,
-        # TODO: Add flag for conditionally preloading the non_structured_value
+        qualification: if structured
+                         qualification
+                       else
+                         (qualification.present? ? 'other' : nil)
+                       end,
+        non_structured_qualification: structured ? nil : qualification,
       )
     end
 
--- app/forms/candidate_interface/gcse_international_structured_grades_form.rb
+++ app/forms/candidate_interface/gcse_international_structured_grades_form.rb
@@ -2,15 +2,22 @@ module CandidateInterface
   class GcseInternationalStructuredGradesForm
     include ActiveModel::Model
 
-    attr_accessor :grade, :non_structured_grade
+    attr_accessor :grade, :non_structured_grade, :structured_grades
 
     validates :grade, presence: true
     validates :non_structured_grade, presence: true, if: :non_structured?
 
-    def self.build_from_qualification(application_qualification)
+    def self.build_from_qualification(application_qualification, structured_grades: [])
+      grade = application_qualification.grade
+      structured = grade.present? && grade.in?(structured_grades)
+
       new(
-        grade: application_qualification.grade,
-        # TODO: Add flag for conditionally preloading the non_structured_value
+        grade: if structured
+                 grade
+               else
+                 (grade.present? ? 'other' : nil)
+               end,
+        non_structured_grade: structured ? nil : grade,
       )
     end
 
