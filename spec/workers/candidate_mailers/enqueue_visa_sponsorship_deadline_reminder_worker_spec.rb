213b2b2e3acd969d28c70caed1469d7af3cda6b5
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2026-06-19 16:56:11 +0100

    SendVisaSponsorshipDeadlineReminderWorker specs

--- spec/workers/candidate_mailers/enqueue_visa_sponsorship_deadline_reminder_worker_spec.rb
+++ spec/workers/candidate_mailers/enqueue_visa_sponsorship_deadline_reminder_worker_spec.rb
@@ -8,10 +8,13 @@ module CandidateMailers
         allow(ApplicationChoicesVisaSponsorshipDeadlineReminder).to(
           receive(:call).and_return(ApplicationChoice.all),
         )
-        allow(SendVisaSponsorshipDeadlineReminderWorker).to receive(:perform_at)
+        worker = instance_double(ActiveJob::ConfiguredJob)
+        allow(SendVisaSponsorshipDeadlineReminderWorker).to receive(:set).and_return(worker)
+        allow(worker).to receive(:perform_later)
 
         described_class.new.perform
-        expect(SendVisaSponsorshipDeadlineReminderWorker).to have_received(:perform_at)
+        expect(SendVisaSponsorshipDeadlineReminderWorker).to have_received(:set)
+        expect(worker).to have_received(:perform_later).with(ApplicationChoice.all.pluck(:id))
       end
     end
   end
