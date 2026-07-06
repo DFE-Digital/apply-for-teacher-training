0a8d8be9e5b1949daf8ff31624b41ed6230184f6
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2026-07-06 15:20:13 +0100

    Only deliver_now for the authentication mailer

--- app/services/chasers/candidate/offer_email_service.rb
+++ app/services/chasers/candidate/offer_email_service.rb
@@ -2,7 +2,7 @@ module Chasers
   module Candidate
     class OfferEmailService
       def self.call(application_choice:, mailer:, chaser_type:)
-        CandidateMailer.send(mailer, application_choice).deliver_now
+        CandidateMailer.send(mailer, application_choice).deliver_later
         ChaserSent.create!(chased: application_choice, chaser_type:)
       end
     end
--- app/workers/find_a_candidate/send_chaser_worker.rb
+++ app/workers/find_a_candidate/send_chaser_worker.rb
@@ -3,17 +3,15 @@ module FindACandidate
     self.queue_adapter = :solid_queue
 
     def perform(invite_ids)
-      ActiveRecord::Base.transaction do
-        invites = Pool::Invite.current_cycle.published.not_responded
-          .where.missing(:chasers_sent)
-          .where(id: invite_ids)
+      invites = Pool::Invite.current_cycle.published.not_responded
+        .where.missing(:chasers_sent)
+        .where(id: invite_ids)
 
-        if invites.present?
-          CandidateMailer.invites_chaser(invites).deliver_now
+      if invites.present?
+        CandidateMailer.invites_chaser(invites).deliver_later
 
-          invites.each do |invite|
-            ChaserSent.create!(chased: invite, chaser_type: 'pool_invite')
-          end
+        invites.each do |invite|
+          ChaserSent.create!(chased: invite, chaser_type: 'pool_invite')
         end
       end
     end
