865b83daa1af0ce71ec43c797444116af69840ee
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2026-07-14 14:24:21 +0100

    Candidate email timetabler update

--- app/lib/end_of_cycle/candidate_email_timetabler.rb
+++ app/lib/end_of_cycle/candidate_email_timetabler.rb
@@ -14,28 +14,39 @@ module EndOfCycle
     def email_schedule
       {
         # All dates should be reviewed as part of end-of / start-of cycle planning
+        # We have delayed the "apply has opened" email in the past to deal with rate limiting from Notify on the first day of applications.
+        find_has_opened_announcement_date: find_opens_at.to_date,
+        apply_has_opened_announcement_date: (apply_opens_at + 2.days).to_date,
         apply_deadline_first_reminder_date: get_weekday(apply_deadline_at - 2.months).to_date,
         apply_deadline_second_reminder_date: get_weekday(apply_deadline_at - 1.month).to_date,
         application_deadline_has_passed_email_date: (apply_deadline_at + 1.day).to_date,
         reject_by_default_explainer_date: (reject_by_default_at + 1.day).to_date,
         decline_by_default_explainer_date: (decline_by_default_at + 1.day).to_date,
-        find_has_opened_announcement_date: find_opens_at.to_date,
-        # We have delayed the "apply has opened" email in the past to deal with rate limiting from Notify on the first day of applications.
-        apply_has_opened_announcement_date: (apply_opens_at + 2.days).to_date,
         winter_reject_by_default_explainer_date:,
         winter_decline_by_default_explainer_date:,
       }
     end
 
     def winter_reject_by_default_explainer_date
-      if timetable.try(:winter_reject_by_default_at).present?
-        (timetable.try(:winter_reject_by_default_at) + 1.day).to_date
+      [].tap do |possible_dates|
+        possible_dates << (previous_timetable.winter_reject_by_default_at + 1.day).to_date if previous_timetable.winter_reject_by_default_at.present?
+        possible_dates << (timetable.winter_reject_by_default_at + 1.day).to_date
+      end.rfind do |possible_date|
+        possible_date.before?(winter_decline_by_default_explainer_date)
       end
     end
 
     def winter_decline_by_default_explainer_date
-      if timetable.try(:winter_decline_by_default_at).present?
-        (timetable.try(:winter_decline_by_default_at) + 1.day).to_date
+      if timetable.current_year?
+
+        [].tap do |possible_dates|
+          possible_dates << (previous_timetable.winter_decline_by_default_at + 1.day).to_date if previous_timetable.winter_decline_by_default_at.present?
+          possible_dates << (timetable.winter_decline_by_default_at + 1.day).to_date
+        end.find do |possible_date|
+          current_date == possible_date || current_date.before?(possible_date)
+        end
+      else
+        (timetable.winter_decline_by_default_at + 1.day).to_date
       end
     end
 
@@ -76,6 +87,10 @@ module EndOfCycle
     end
 
   private
+
+    def previous_timetable
+      @previous_timetable ||= timetable.relative_previous_timetable
+    end
 
     def current_date
       Time.zone.now.to_date
--- spec/lib/end_of_cycle/candidate_email_timetabler_spec.rb
+++ spec/lib/end_of_cycle/candidate_email_timetabler_spec.rb
@@ -1,8 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe EndOfCycle::CandidateEmailTimetabler do
-  let(:instance) { described_class.new(timetable:) }
-  let(:timetable) { RecruitmentCycleTimetable.previous_timetable }
+  let(:instance) { described_class.new }
 
   describe '.email_schedule' do
     subject(:email_schedule) { instance.email_schedule }
@@ -11,46 +10,106 @@ RSpec.describe EndOfCycle::CandidateEmailTimetabler do
       it 'returns the decline_by_default_at plus one day' do
         expect(
           email_schedule[:decline_by_default_explainer_date],
-        ).to eq timetable.decline_by_default_at.to_date + 1.day
+        ).to eq current_timetable.decline_by_default_at.to_date + 1.day
       end
     end
 
     describe 'winter_reject_by_default_explainer_date' do
-      context 'when the timetable winter_reject_by_default_at attribute is nil' do
-        before do
-          allow(instance).to receive(:timetable).and_return(RecruitmentCycleTimetable.new)
-        end
-
-        it 'returns nil' do
-          expect(instance.winter_reject_by_default_explainer_date).to be_nil
+      context 'it is at the start of new cycle' do
+        it 'returns date calculated on previous cycle winter reject by default at' do
+          travel_temporarily_to(2026, 10, 10) do
+            expect(
+              email_schedule[:winter_reject_by_default_explainer_date],
+            ).to eq Date.new(2027, 1, 21)
+          end
         end
       end
 
-      context 'when the timetable winter_reject_by_default_at attribute is not nil' do
-        it 'returns the winter_reject_by_default_at plus one day' do
-          expect(
-            email_schedule[:winter_reject_by_default_explainer_date],
-          ).to eq timetable.winter_reject_by_default_at.to_date + 1.day
+      context 'it is in Jan, before the winter reject by default at' do
+        it 'returns date calculated on previous cycle winter reject by default at' do
+          travel_temporarily_to(2027, 1, 19) do
+            expect(
+              email_schedule[:winter_reject_by_default_explainer_date],
+            ).to eq Date.new(2027, 1, 21)
+          end
+        end
+      end
+
+      context 'it is in Jan, between the winter reject by default and winter decline by default dates' do
+        it 'returns date calculated on previous cycle winter reject by default at' do
+          travel_temporarily_to(2027, 1, 24) do
+            expect(
+              email_schedule[:winter_reject_by_default_explainer_date],
+            ).to eq Date.new(2027, 1, 21)
+          end
+        end
+      end
+
+      context 'it is after winter dates have passed' do
+        it 'returns date calculated on current cycle winter reject by default at' do
+          travel_temporarily_to(2027, 2, 1) do
+            expect(
+              email_schedule[:winter_reject_by_default_explainer_date],
+            ).to eq Date.new(2028, 1, 27)
+          end
+        end
+      end
+
+      context 'when a specific, future timetable is provided' do
+        it 'returns date calculated on given timetable' do
+          timetable = RecruitmentCycleTimetable.find_by(recruitment_cycle_year: current_year + 2)
+          schedule = described_class.new(timetable:).email_schedule
+          expect(schedule[:winter_reject_by_default_explainer_date]).to eq((timetable.winter_reject_by_default_at + 1.day).to_date)
         end
       end
     end
 
     describe 'winter_decline_by_default_explainer_date' do
-      context 'when the timetable winter_decline_by_default_at attribute is nil' do
-        before do
-          allow(instance).to receive(:timetable).and_return(RecruitmentCycleTimetable.new)
-        end
-
-        it 'returns nil' do
-          expect(instance.winter_decline_by_default_explainer_date).to be_nil
+      context 'it is at the start of new cycle' do
+        it 'returns date calculated on the previous cycle' do
+          travel_temporarily_to(2026, 10, 10) do
+            expected_date = Date.new(2027, 1, 25)
+            expect(
+              email_schedule[:winter_decline_by_default_explainer_date],
+            ).to eq(expected_date)
+            expect(expected_date).to eq((previous_timetable.winter_decline_by_default_at + 1.day).to_date)
+          end
         end
       end
 
-      context 'when the timetable winter_decline_by_default_at attribute is not nil' do
-        it 'returns the winter_decline_by_default_at plus one day' do
-          expect(
-            email_schedule[:winter_decline_by_default_explainer_date],
-          ).to eq timetable.winter_decline_by_default_at.to_date + 1.day
+      context 'it is in Jan, before the winter reject by default at' do
+        it 'returns date calculated on previous cycle winter reject by default at' do
+          travel_temporarily_to(2027, 1, 19) do
+            expected_date = Date.new(2027, 1, 25)
+            expect(
+              email_schedule[:winter_decline_by_default_explainer_date],
+            ).to eq(expected_date)
+            expect(expected_date).to eq((previous_timetable.winter_decline_by_default_at.to_date + 1.day).to_date)
+          end
+        end
+      end
+
+      context 'it is in Jan, between the winter reject by default and winter decline by default dates' do
+        it 'returns date calculated on previous cycle winter reject by default at' do
+          travel_temporarily_to(2027, 1, 24) do
+            expected_date = Date.new(2027, 1, 25)
+            expect(
+              email_schedule[:winter_decline_by_default_explainer_date],
+            ).to eq(expected_date)
+            expect(expected_date).to eq((previous_timetable.winter_decline_by_default_at.to_date + 1.day).to_date)
+          end
+        end
+      end
+
+      context 'it is after winter dates have passed' do
+        it 'returns date calculated on current cycle winter reject by default at' do
+          travel_temporarily_to(2027, 2, 1) do
+            expected_date = Date.new(2028, 1, 31)
+            expect(
+              email_schedule[:winter_decline_by_default_explainer_date],
+            ).to eq expected_date
+            expect(expected_date).to eq((current_timetable.winter_decline_by_default_at + 1.day).to_date)
+          end
         end
       end
     end
@@ -89,16 +148,6 @@ RSpec.describe EndOfCycle::CandidateEmailTimetabler do
   describe 'send_winter_decline_by_default_explainer?' do
     subject(:send_winter_decline_by_default_explainer?) { instance.send_winter_decline_by_default_explainer? }
 
-    context 'when winter_decline_by_default_explainer_date is nil' do
-      before do
-        allow(instance).to receive(:winter_decline_by_default_explainer_date).and_return(nil)
-      end
-
-      it 'returns false' do
-        expect(send_winter_decline_by_default_explainer?).to be(false)
-      end
-    end
-
     context 'when the current date does not match the winter decline by default explainer date' do
       it 'returns false' do
         travel_temporarily_to(current_timetable.winter_decline_by_default_at + 1.month, freeze: true) do
@@ -118,8 +167,6 @@ RSpec.describe EndOfCycle::CandidateEmailTimetabler do
 
   describe 'send_decline_by_default_explainer?' do
     subject(:send_decline_by_default_explainer?) { instance.send_decline_by_default_explainer? }
-
-    let(:timetable) { current_timetable }
 
     context 'when the current date does not match the decline by default explainer date' do
       it 'returns false' do
