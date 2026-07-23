aad8978c46d958907629cd69f9634cd220c3effa
Jamie <jamiecleare2525@hotmail.com>
2026-07-23 09:40:05 +0100

    Missing to

--- spec/mailers/provider_mailer/provider_mailer_permissions_removed_spec.rb
+++ spec/mailers/provider_mailer/provider_mailer_permissions_removed_spec.rb
@@ -35,7 +35,7 @@ RSpec.describe ProviderMailer do
       'Your access to your Manage account has been removed - manage teacher training applications',
       'salutation' => 'Dear Princess Fiona',
       'access_removed' => 'Your access to your Manage account has been removed.',
-      'not_signed_in' => 'This is because your account had not been signed in for 12 months.',
+      'not_signed_in' => 'This is because your account had not been signed in to for 12 months.',
       'no_longer_able' => 'You are no longer able to manage teacher training applications for Hogwarts University.',
       'manage_again' => 'If you want to use Manage again',
       'access_back' => 'You will need to contact us at [becomingateacher@digital.education.gov.uk](mailto:becomingateacher@digital.education.gov.uk) to get your access back.',
