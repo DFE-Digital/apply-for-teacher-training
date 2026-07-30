668e12cfdf4194c84e13ef3bde6bab9edf42674e
Dan Daly <dan.daly@education.gov.uk>
2026-07-30 11:17:24 +0100

    Stop saving empty attrs unconditionally in EnicSelectionForm

--- app/forms/candidate_interface/gcse_enic_selection_form.rb
+++ app/forms/candidate_interface/gcse_enic_selection_form.rb
@@ -2,26 +2,29 @@ module CandidateInterface
   class GcseEnicSelectionForm
     include ActiveModel::Model
 
-    attr_accessor :enic_reason, :enic_reference, :comparable_uk_qualification
+    attr_accessor :enic_reason
 
     validates :enic_reason, presence: true
 
     def self.build_from_qualification(qualification)
       new(
         enic_reason: qualification.enic_reason,
-        enic_reference: qualification.enic_reference,
-        comparable_uk_qualification: qualification.comparable_uk_qualification,
       )
     end
 
     def save(qualification)
       return false unless valid?
 
-      qualification.update!(
-        enic_reason: enic_reason,
-        enic_reference: enic_reference,
-        comparable_uk_qualification: comparable_uk_qualification,
-      )
+      attrs = { enic_reason: enic_reason }
+
+      if enic_reason != 'obtained'
+        attrs.merge!(
+          enic_reference: nil,
+          comparable_uk_qualification: nil,
+        )
+      end
+
+      qualification.update!(attrs)
     end
   end
 end
