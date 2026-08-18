292cdb189d697e3477718216a7fc7d01b62b6660
Robert Gwenter <Robert.GWENTER@education.gov.uk>
2026-08-18 12:00:40 +0100

    Enable privatelink for backup storage for QA

--- terraform/aks/variables.tf
+++ terraform/aks/variables.tf
@@ -89,22 +89,22 @@ variable "enable_prometheus_monitoring" {
 }
 
 variable "solid_queue_worker_memory_max" {
-  type = string
+  type    = string
   default = "2Gi"
 }
 
 variable "solid_queue_worker_replicas" {
-  type = number
+  type    = number
   default = 1
 }
 
 variable "solid_queue_secondary_worker_memory_max" {
-  type = string
+  type    = string
   default = "2Gi"
 }
 
 variable "solid_queue_secondary_worker_replicas" {
-  type = number
+  type    = number
   default = 1
 }
 
@@ -127,10 +127,10 @@ locals {
     var.app_name_suffix != null ? local.review_url_vars : {},
     { DB_SSLMODE = var.db_sslmode },
     {
-      BIGQUERY_AIRBYTE_DATASET                    = var.airbyte_enabled ? local.gcp_dataset_name : null
-      AIRBYTE_SERVER_URL                          = var.airbyte_enabled ? "https://airbyte-${var.namespace}.${module.cluster_data.ingress_domain}" : null
-      BIGQUERY_HIDDEN_POLICY_TAG                  = var.airbyte_enabled ? "projects/rugged-abacus-218110/locations/europe-west2/taxonomies/69524444121704657/policyTags/6523652585511281766" : null
-      AIRBYTE_INTERNAL_DATASET                    = var.airbyte_enabled ? "${local.gcp_dataset_name}_internal" : null
+      BIGQUERY_AIRBYTE_DATASET   = var.airbyte_enabled ? local.gcp_dataset_name : null
+      AIRBYTE_SERVER_URL         = var.airbyte_enabled ? "https://airbyte-${var.namespace}.${module.cluster_data.ingress_domain}" : null
+      BIGQUERY_HIDDEN_POLICY_TAG = var.airbyte_enabled ? "projects/rugged-abacus-218110/locations/europe-west2/taxonomies/69524444121704657/policyTags/6523652585511281766" : null
+      AIRBYTE_INTERNAL_DATASET   = var.airbyte_enabled ? "${local.gcp_dataset_name}_internal" : null
     }
   )
 
