ad850260bc467a7848ca2300c609f0591ee8d76b
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2026-08-26 16:38:54 +0100

    Add spec

--- spec/components/candidate_interface/carry_over_task_list_component_spec.rb
+++ spec/components/candidate_interface/carry_over_task_list_component_spec.rb
@@ -100,5 +100,23 @@ RSpec.describe CandidateInterface::CarryOverTaskListComponent do
         expect(rendered_component).to have_no_text('Before you can apply again, you’ll need to:')
       end
     end
+
+    context 'when a candidate is british and has not completed the efl or visa section' do
+      let(:previous_application_form) { create(:application_form, :submitted, efl_completed: false) }
+      let(:application_form) do
+        create(
+          :application_form,
+          :minimum_info,
+          previous_application_form:,
+          first_nationality: 'British',
+        )
+      end
+
+      it 'they do not see the efl or visa link' do
+        expect(rendered_component).to have_link('confirm your contact information')
+        expect(rendered_component).to have_no_link('enter or confirm your visa information')
+        expect(rendered_component).to have_no_link('enter or confirm your English language skills')
+      end
+    end
   end
 end
