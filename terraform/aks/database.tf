b34a0ebf62fb6b06b0c29bece7f3e674effb7960
Steve Steel <steven.steel@education.gov.uk>
2026-09-03 09:25:56 +0100

    2963 - Updates for new postgres module

--- terraform/aks/database.tf
+++ terraform/aks/database.tf
@@ -1,5 +1,5 @@
 module "postgres" {
-  source = "./vendor/modules/aks//aks/postgres"
+  source = "git::https://github.com/DFE-Digital/terraform-modules.git//aks/postgres?ref=2963-add-postgres-read-replica"
 
   namespace                      = var.namespace
   environment                    = local.app_name_suffix
@@ -22,31 +22,9 @@ module "postgres" {
   azure_extensions               = ["PG_BUFFERCACHE", "PG_STAT_STATEMENTS", "PGCRYPTO", "UNACCENT", "plpgsql"]
   alert_window_size              = var.alert_window_size
   use_airbyte                    = var.pg_airbyte_enabled
-}
-
-module "postgres_replica" {
-  source = "./vendor/modules/aks//aks/postgres"
-
-  namespace                      = var.namespace
-  environment                    = local.app_name_suffix
-  azure_resource_prefix          = var.azure_resource_prefix
-  service_name                   = var.service_name
-  service_short                  = var.service_short
-  config_short                   = var.config_short
-  azure_name_override            = "${var.azure_resource_prefix}-${var.service_short}-${local.app_name_suffix}-psql-replica"
-  cluster_configuration_map      = module.cluster_data.configuration_map
-  use_azure                      = var.deploy_azure_backing_services
-  pg_create_mode                 = "Replica"
-  source_server_id               = module.postgres.azure_server_id
-  azure_enable_monitoring        = var.enable_alerting
-  azure_enable_backup_storage    = false
-  server_version                 = var.postgres_replica_version
-  admin_username                 = data.azurerm_key_vault_secret.postgres_admin_username.value
-  admin_password                 = data.azurerm_key_vault_secret.postgres_admin_password.value
-  azure_sku_name                 = var.postgres_replica_sku
-  azure_storage_mb               = var.postgres_replica_storage_mb
-  azure_enable_high_availability = var.replica_enable_high_availability
-  azure_maintenance_window       = var.azure_maintenance_window
-  azure_extensions               = ["PG_BUFFERCACHE", "PG_STAT_STATEMENTS", "PGCRYPTO", "UNACCENT"]
-  alert_window_size              = var.alert_window_size
+  read_replica_count             = 3
+  extra_databases                = [
+    "audit",
+    "reporting"
+  ]
 }
