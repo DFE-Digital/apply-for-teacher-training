9a7c06d8c4ed493435efdc05387a2f722e105ef7
Lori Bailey <44073106+elceebee@users.noreply.github.com>
2026-09-09 08:05:44 +0100

    wip

--- app/components/publications/recruitment_cycle_timetable_card.rb
+++ app/components/publications/recruitment_cycle_timetable_card.rb
@@ -1,10 +1,11 @@
 module Publications
   class RecruitmentCycleTimetableCard < ApplicationComponent
-    def initialize(timetable)
+    def initialize(timetable, show_link: false)
       @timetable = timetable
+      @show_link = show_link
     end
 
-    attr_reader :timetable
+    attr_reader :timetable, :show_link
 
     delegate :recruitment_cycle_year, :cycle_range_name, to: :timetable
 
@@ -19,10 +20,15 @@ module Publications
                         elsif recruitment_cycle_year == current_year - 1
                           t('.previous_year')
                         end
-      govuk_link_to(
-        "#{additional_text} #{cycle_range_name}".strip,
-        publications_recruitment_cycle_timetable_path(recruitment_cycle_year:)
-      )
+
+      if show_link
+        govuk_link_to(
+          "#{additional_text} #{cycle_range_name}".strip,
+          publications_recruitment_cycle_timetable_path(recruitment_cycle_year:)
+        )
+      else
+        "#{additional_text} #{cycle_range_name}".strip
+      end
     end
   end
 end
