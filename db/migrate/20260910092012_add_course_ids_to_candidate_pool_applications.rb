40a43f5f126a26e0b5a63d6a6a3c1af8aaad2053
Jamie <jamiecleare2525@hotmail.com>
2026-09-11 14:07:21 +0100

    Delete duplicate migration

--- db/migrate/20260910092012_add_course_ids_to_candidate_pool_applications.rb
+++
@@ -1,5 +0,0 @@
-class AddCourseIdsToCandidatePoolApplications < ActiveRecord::Migration[8.1]
-  def change
-    add_column :candidate_pool_applications, :course_ids, :bigint, array: true, null: false, default: []
-  end
-end
