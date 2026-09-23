07b7133095a481a5180d31982014cd785bc80d53
Jamie <jamiecleare2525@hotmail.com>
2026-09-23 16:05:40 +0100

    Return true after delete subsequent applications

--- app/services/concerns/subsequent_application_deletable.rb
+++ app/services/concerns/subsequent_application_deletable.rb
@@ -3,8 +3,9 @@ private
 
   def delete_subsequent_application_form(application_form)
     subsequent_application_form = application_form.subsequent_application_form
-    return if subsequent_application_form.blank? || subsequent_application_form.application_choices.present?
+    return true if subsequent_application_form.blank? || subsequent_application_form.application_choices.present?
 
     application_form.subsequent_application_form.destroy!
+    true
   end
 end
