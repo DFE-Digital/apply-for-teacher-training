655aa53d5f341361ca8ae41cc706aae9cc7953d1
Jamie <jamiecleare2525@hotmail.com>
2026-09-25 15:56:51 +0100

    Remove unnecessary limit reached preview

--- spec/mailers/previews/candidate/find_a_candidate_preview.rb
+++ spec/mailers/previews/candidate/find_a_candidate_preview.rb
@@ -28,43 +28,6 @@ class Candidate::FindACandidatePreview < ActionMailer::Preview
     CandidateMailer.candidate_invite(invite)
   end
 
-  def candidate_invite_limit_reached
-    candidate = FactoryBot.create(:candidate)
-    application_form = FactoryBot.build_stubbed(
-      :application_form,
-      :minimum_info,
-      candidate: candidate,
-      first_name: 'Fred',
-    )
-
-    provider = FactoryBot.build(:provider)
-    course = FactoryBot.build(:course,
-                              provider:,
-                              fee_domestic: 9535,
-                              fee_international: 15430)
-
-    invite = FactoryBot.create(
-      :pool_invite,
-      :sent_to_candidate,
-      candidate:,
-      application_form:,
-      provider:,
-      course:,
-      provider_message: true,
-      message_content: "# Hello\r\n## Please apply to my course\r\n\r\n^ Some content\r\n\r\nByee",
-    )
-    _second_invite = FactoryBot.create(
-      :pool_invite,
-      :sent_to_candidate,
-      candidate:,
-      application_form:,
-      provider:,
-      course:,
-    )
-
-    CandidateMailer.candidate_invite(invite)
-  end
-
   def invites_chaser
     application_form = FactoryBot.build_stubbed(
       :application_form,
